4d952b9a87334950a15b5060ec8272bdb70f59c6
dshaligram <dshaligram@c06c8d41-db1a-0410-9941-cceddc491573>
2006-10-28 08:07:38 +0000

    Further chain lightning balancing: restored old power attenuation, slightly dropped base damage.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/branches/stone_soup@301 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/spells1.cc
+++ crawl-ref/source/spells1.cc
@@ -258,7 +258,7 @@ void cast_chain_lightning( int powc )
 
     for (sx = you.x_pos, sy = you.y_pos; 
          powc > 0; 
-         powc -= 7 + random2(8), sx = tx, sy = ty)
+         powc -= 8 + random2(13), sx = tx, sy = ty)
     {
         // infinity as far as this spell is concerned
         // (Range - 1) is used because the distance is randomized and
@@ -370,7 +370,7 @@ void cast_chain_lightning( int powc )
         beam.target_x = tx;
         beam.target_y = ty;
         beam.colour = LIGHTBLUE;
-        beam.damage = calc_dice( 5, 15 + powc * 2 / 3 );
+        beam.damage = calc_dice( 5, 12 + powc * 2 / 3 );
 
         // Be kinder to the player
         if (tx == you.x_pos && ty == you.y_pos)
