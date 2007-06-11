f8ef48576784cd5d02584f7afd69a6299e6e32d5
dshaligram <dshaligram@c06c8d41-db1a-0410-9941-cceddc491573>
2007-06-11 21:37:04 +0000

    Fixed off-by-one error in Vestibule depth calculation.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@1579 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/misc.cc
+++ crawl-ref/source/misc.cc
@@ -2041,7 +2041,7 @@ std::string prep_branch_level_name()
 int absdungeon_depth(branch_type branch, int subdepth)
 {
     if (branch >= BRANCH_VESTIBULE_OF_HELL && branch <= BRANCH_THE_PIT)
-        return subdepth + 27;
+        return subdepth + 27 - (branch == BRANCH_VESTIBULE_OF_HELL);
     else
     {
         --subdepth;
