df1c40f399ce352c36c08c86d42b98c46e04f577
dshaligram <dshaligram@c06c8d41-db1a-0410-9941-cceddc491573>
2007-09-17 20:24:06 +0000

    Disabling the Shoals for 0.3.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/branches/stone_soup-0.3@2133 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/newgame.cc
+++ crawl-ref/source/newgame.cc
@@ -394,16 +394,8 @@ static void initialise_branch_depths()
     branches[BRANCH_LAIR].startdepth = random_range(8, 13);
     branches[BRANCH_HIVE].startdepth = random_range(11, 16);
     branches[BRANCH_SLIME_PITS].startdepth = random_range(3, 9);
-    if ( coinflip() )
-    {
-        branches[BRANCH_SWAMP].startdepth = random_range(2, 7);
-        branches[BRANCH_SHOALS].startdepth = -1;
-    }
-    else
-    {
-        branches[BRANCH_SWAMP].startdepth = -1;
-        branches[BRANCH_SHOALS].startdepth = random_range(2, 7);
-    }
+    branches[BRANCH_SWAMP].startdepth = random_range(2, 7);
+    branches[BRANCH_SHOALS].startdepth = -1;
     branches[BRANCH_SNAKE_PIT].startdepth = random_range(3, 8);
     branches[BRANCH_VAULTS].startdepth = random_range(14, 19);
     branches[BRANCH_CRYPT].startdepth = random_range(2, 4);
