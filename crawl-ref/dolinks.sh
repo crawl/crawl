45ea038992c6ea1af69437af08d6da3eb3f32aba
dshaligram <dshaligram@c06c8d41-db1a-0410-9941-cceddc491573>
2007-10-25 13:02:05 +0000

    [1786778] Fixed dolinks.sh and domake.sh (dolorous).
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@2563 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/dolinks.sh
+++ crawl-ref/dolinks.sh
@@ -3,6 +3,7 @@
 mkdir -p NORMAL
 mkdir -p WIZARD
 
-pushd NORMAL ; ln -s ../source/*.h ../source/*.cc ../source/makefile* . ; popd
-pushd WIZARD ; ln -s ../source/*.h ../source/*.cc ../source/makefile* . ; popd
+pushd NORMAL ; ln -s ../source/util . ; ln -s ../source/*.h ../source/*.cc ../source/makefile* . ; popd
+pushd WIZARD ; ln -s ../source/util . ; ln -s ../source/*.h ../source/*.cc ../source/makefile* . ; popd
 
+ln -s source/dat dat
