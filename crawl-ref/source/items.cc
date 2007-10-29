2c0a2b2364fbb07b596aba9c0b8e46dc524c27b3
dshaligram <dshaligram@c06c8d41-db1a-0410-9941-cceddc491573>
2007-10-29 06:52:14 +0000

    [1817715] Fixed autopickup trying to pick up nets you're caught in.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@2651 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/items.cc
+++ crawl-ref/source/items.cc
@@ -1438,9 +1438,7 @@ static void got_item(item_def& item, int quant)
 // the player's inventory is full.
 int move_item_to_player( int obj, int quant_got, bool quiet )
 {
-    if (you.attribute[ATTR_HELD] && mitm[obj].base_type == OBJ_MISSILES
-        && mitm[obj].sub_type == MI_THROWING_NET
-        && item_is_stationary(mitm[obj]))
+    if (item_is_stationary(mitm[obj]))
     {
         mpr("You cannot pick up the net that holds you!");
         return (1);
@@ -2928,6 +2926,9 @@ static bool is_forced_autopickup(const item_def &item, std::string &iname)
 
 bool item_needs_autopickup(const item_def &item)
 {
+    if (item_is_stationary(item))
+        return (false);
+    
     if (strstr(item.inscription.c_str(), "=g") != 0)
         return (true);
 
