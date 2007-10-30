d1b37515d1cd7bfaf63c659fef0379d4ac8a372b
dshaligram <dshaligram@c06c8d41-db1a-0410-9941-cceddc491573>
2007-10-30 14:31:32 +0000

    Remove -svn version qualifier.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/branches/stone_soup-0.3@2687 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/version.h
+++ crawl-ref/source/version.h
@@ -38,7 +38,7 @@
 #define CRAWL "Dungeon Crawl Stone Soup"
 
 #define VER_NUM  "0.3"
-#define VER_QUAL "-svn"
+#define VER_QUAL ""
 
 // last updated 07august2001 {mv}
 /* ***********************************************************************
