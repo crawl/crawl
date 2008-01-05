506edfaeff64385b78b199e3f4e4f7358a1580ec
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2008-01-05 03:45:04 +0000

    Adjust dolinks.sh to properly handle a tiles-enabled build, and add commented-out makefile entries for the two platform-specific tiles-enabled makefiles.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@3195 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/dolinks.sh
+++ crawl-ref/dolinks.sh
@@ -3,7 +3,8 @@
 mkdir -p NORMAL
 mkdir -p WIZARD
 
-pushd NORMAL ; ln -s ../source/util . ; ln -s ../source/*.h ../source/*.cc ../source/makefile* . ; popd
-pushd WIZARD ; ln -s ../source/util . ; ln -s ../source/*.h ../source/*.cc ../source/makefile* . ; popd
+pushd NORMAL ; ln -s ../source/rltiles . ; ln -s ../source/util . ; ln -s ../source/*.h ../source/*.cc ../source/makefile* . ; popd
+pushd WIZARD ; ln -s ../source/rltiles . ; ln -s ../source/util . ; ln -s ../source/*.h ../source/*.cc ../source/makefile* . ; popd
 
+ln -s source/rltiles tiles
 ln -s source/dat dat
