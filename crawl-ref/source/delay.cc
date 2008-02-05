be4c1c2cc61886b6de0fa720599c92f1297700a2
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2008-02-05 04:16:22 +0000

    Remove unneeded whitespace.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@3402 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/delay.cc
+++ crawl-ref/source/delay.cc
@@ -1,6 +1,6 @@
 /*
  *  File:     delay.cc
- *  Summary:  Functions for handling multi-turn actions.    
+ *  Summary:  Functions for handling multi-turn actions.
  *
  *  Modified for Crawl Reference by $Author$ on $Date$
  *
