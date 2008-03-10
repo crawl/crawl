de978e5fed84aaa1b077fed70b93eba1a3d82edc
pauldubois <pauldubois@c06c8d41-db1a-0410-9941-cceddc491573>
2008-03-10 03:27:07 +0000

    Another instance of !expr != foo; removed the double negative.  Fixes compile error, but I still think the logic is still incorrect; can someone check who knows the code better check this out?
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@3570 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/beam.cc
+++ crawl-ref/source/beam.cc
@@ -1268,7 +1268,7 @@ static bool affect_mon_in_wall(bolt &pbolt, item_def *item, int tx, int ty)
     if (pbolt.is_enchant
         || (!pbolt.is_explosion && !pbolt.is_big_cloud
             && (grd[tx][ty] != DNGN_METAL_WALL
-                || !pbolt.flavour != BEAM_ELECTRICITY)))
+                || pbolt.flavour == BEAM_ELECTRICITY)))
     {
         monsters *mon = &menv[mid];
         if (!mons_is_wall_shielded(mon->type))
