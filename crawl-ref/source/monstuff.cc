b14789f42ec94287af35caf9175ebafea080f68b
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2008-03-20 02:21:06 +0000

    Fix spelling inconsistency: invigorated -> invigourated.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@3739 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/monstuff.cc
+++ crawl-ref/source/monstuff.cc
@@ -881,7 +881,7 @@ void monster_die(monsters *monster, killer_type killer, int i, bool silent)
                     monsters *mon = &menv[i];
                     if (mon->alive() && mon->hit_points < mon->max_hit_points)
                     {
-                        simple_monster_message(mon, " looks invigorated.");
+                        simple_monster_message(mon, " looks invigourated.");
                         heal_monster( mon, 1 + random2(monster->hit_dice / 4),
                                       false );
                     }
