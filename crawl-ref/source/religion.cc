46c7199d6ae8c006a78799a1c59dacf4e2d77c8f
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2008-04-17 20:47:15 +0000

    Make TSO's "extended stay" blessing result in permanent stays more often.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@4313 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/religion.cc
+++ crawl-ref/source/religion.cc
@@ -930,7 +930,7 @@ static bool _tso_blessing_extend_stay(monsters *mon)
     // that we have to delete the enchantment without removing the
     // enchantment effect, in order to keep the monster from
     // disappearing.
-    if (abj.duration > 3699)
+    if (abj.duration > 3499)
         mon->del_ench(ENCH_ABJ, true, false);
     else
         mon->update_ench(abj);
