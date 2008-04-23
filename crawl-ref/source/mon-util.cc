c9a84fd11ed9da1d89a2d8c93f5dd035f0267b47
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2008-04-23 04:16:08 +0000

    Typo fix.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@4504 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/mon-util.cc
+++ crawl-ref/source/mon-util.cc
@@ -343,7 +343,7 @@ static int _scan_mon_inv_randarts( const monsters *mon,
     if (mons_itemuse( mon->type ) >= MONUSE_STARTING_EQUIPMENT)
     {
         const int weapon = mon->inv[MSLOT_WEAPON];
-        const int second = mon->inv[MSLOT_MISSILE]; // ettins/two-head oges
+        const int second = mon->inv[MSLOT_MISSILE]; // ettins/two-head ogres
         const int armour = mon->inv[MSLOT_ARMOUR];
 
         if (weapon != NON_ITEM && mitm[weapon].base_type == OBJ_WEAPONS
