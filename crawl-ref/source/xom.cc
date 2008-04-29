84114d18dd25ea888b6e50ae076c44d033533fae
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2008-04-29 16:24:30 +0000

    Comment fixes.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@4767 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/xom.cc
+++ crawl-ref/source/xom.cc
@@ -46,8 +46,8 @@
 #    define DEBUG_GIFTS       1
 #endif
 
-// Which spell?  First I copied all spells from your_spells(), then I
-// filtered some out (especially conjurations).  Then I sorted them in
+// Which spells?  First I copied all spells from your_spells(), then I
+// filtered some out, especially conjurations.  Then I sorted them in
 // roughly ascending order of power.
 static const spell_type xom_spells[] =
 {
