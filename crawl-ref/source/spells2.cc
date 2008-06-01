ed64917822c4df9882aaf7cbf96ebcb46a452168
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2008-06-01 23:07:49 +0000

    Reduce the chance of summoning a very ugly thing a bit more.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@5419 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/spells2.cc
+++ crawl-ref/source/spells2.cc
@@ -1512,7 +1512,7 @@ void summon_scorpions(int pow)
 
 void summon_ugly_thing(int pow)
 {
-    int chance = std::max(6 - (pow / 11) + random2(2), 1);
+    int chance = std::max(6 - (pow / 12), 1);
 
     monster_type ugly = (one_chance_in(chance)) ?
         MONS_VERY_UGLY_THING : MONS_UGLY_THING;
