aa213e31912154c93958be10d7b2f09c8fe068b3
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2008-06-08 05:57:39 +0000

    Comment fix.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@5589 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/monplace.h
+++ crawl-ref/source/monplace.h
@@ -241,7 +241,7 @@ int mons_place( mgen_data mg );
 /* ***********************************************************************
  * This isn't really meant to be a public function.  It is a low level
  * monster placement function used by dungeon building routines and
- * mons_place().  If you need to put a monster somewhere,  use mons_place().
+ * mons_place().  If you need to put a monster somewhere, use mons_place().
  * Summoned creatures can be created with create_monster().
  * *********************************************************************** */
 int place_monster( mgen_data mg, bool force_pos = false );
