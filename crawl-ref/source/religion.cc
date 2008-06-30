2a0b4dc76b699aa56ba5ab652368e188a97d7a1e
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2008-06-30 22:18:12 +0000

    Only attempt to change the attitudes of holy beings if we're dumping a good god, not if we're dumping any god.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@6268 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/religion.cc
+++ crawl-ref/source/religion.cc
@@ -4910,11 +4910,14 @@ void excommunication(god_type new_god)
 
     // When you leave one of the good gods for a non-good god, or no
     // god, you make all non-hostile holy beings hostile.
-    if (!is_good_god(new_god) && _holy_beings_attitude_change())
+    if (is_good_god(old_god) && !is_good_god(new_god)
+        && _holy_beings_attitude_change())
+    {
         mpr("The divine host forsakes you.", MSGCH_MONSTER_ENCHANT);
+    }
 
     learned_something_new(TUT_EXCOMMUNICATE, (int)new_god, old_piety);
-}                               // end excommunication()
+}
 
 static bool _bless_weapon( god_type god, int brand, int colour )
 {
