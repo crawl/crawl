592a6c88a467f707864336f0ac06367b99ff8fe7
zelgadis <zelgadis@c06c8d41-db1a-0410-9941-cceddc491573>
2008-07-02 06:10:57 +0000

    Make fake raksha summons invisible before checking player_angers_monster() so the player won't be told about enraged monsters s/he can't see.  Most probably can only occur as the result of wizard commands, but better safe than sorry.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@6324 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/monplace.cc
+++ crawl-ref/source/monplace.cc
@@ -1939,14 +1939,14 @@ int mons_place( mgen_data mg )
             creation->add_ench(ENCH_CHARM);
         }
 
+        if (creation->type == MONS_RAKSHASA_FAKE && !one_chance_in(3))
+            creation->add_ench(ENCH_INVIS);
+
         if (!(mg.flags & MG_FORCE_BEH))
             player_angers_monster(creation);
 
         // Make summoned being aware of player's presence.
         behaviour_event(creation, ME_ALERT, MHITYOU);
-
-        if (creation->type == MONS_RAKSHASA_FAKE && !one_chance_in(3))
-            creation->add_ench(ENCH_INVIS);
     }
 
     return (mid);
