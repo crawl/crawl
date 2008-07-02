bae5b88570c9da4e2529f0dd4e8cab25e7c5cef1
zelgadis <zelgadis@c06c8d41-db1a-0410-9941-cceddc491573>
2008-07-02 07:13:12 +0000

    Bug 2007644: necromanctic miscasts could cause an undead player to rot even though the undead should be immune to rotting.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@6328 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -2961,7 +2961,10 @@ static void _miscast_necromancy(int severity, const char* cause)
             break;
         case 2:
             // josh declares mummies cannot smell {dlb}
-            if (player_can_smell())
+            if (player_can_smell()
+                && !(you.is_undead
+                     && (you.is_undead != US_SEMI_UNDEAD
+                         || you.hunger_state < HS_SATIATED) ))
             {
                 mpr("You smell decay."); // identical to a harmless message
                 you.rotting++;
