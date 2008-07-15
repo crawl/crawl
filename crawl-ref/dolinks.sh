d1fe64be4d7b10fe8deea4cb9ee6dbf3e99f259f
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2008-07-15 19:00:57 +0000

    Vastly simplify dolinks.sh and domake.sh.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@6565 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/dolinks.sh
+++ crawl-ref/dolinks.sh
@@ -3,18 +3,11 @@
 mkdir -p NORMAL
 mkdir -p WIZARD
 
-pushd NORMAL ; ln -sf ../source/rltiles . ; ln -sf ../source/util . ; ln -sf ../source/*.h ../source/*.cc ../source/makefile* . ; popd
-pushd WIZARD ; ln -sf ../source/rltiles . ; ln -sf ../source/util . ; ln -sf ../source/*.h ../source/*.cc ../source/makefile* . ; popd
+pushd NORMAL ; ln -sf ../source . ; popd
+pushd WIZARD ; ln -sf ../source . ; popd
 
 if [ -d dat ]; then
 	true
 else
 	ln -sf source/dat dat
-fi
-
-if [ -d tiles ]; then
-	true
-else
-	ln -sf source/rltiles tiles
-	pushd source/dat/tiles ; ln -sf ../../rltiles/*.png . ; popd
 fi
--- crawl-ref/domake.sh
+++ crawl-ref/domake.sh
@@ -2,12 +2,13 @@
 #jmf: automate making normal & wizard binaries, until they merge
 #jmf: N.B.: requires symlink directories; run ./dolinks.sh to make
 
-pushd NORMAL && make && popd && pushd WIZARD && make wizard ; popd
+pushd NORMAL/source ; make ; popd
+pushd WIZARD/source ; make wizard ; popd
 
-if [ -f NORMAL/crawl ]; then
-	ln -sf NORMAL/crawl ncrawl
+if [ -f NORMAL/source/crawl ]; then
+	ln -sf NORMAL/source/crawl ncrawl
 fi
 
-if [ -f WIZARD/crawl ]; then
-	ln -sf WIZARD/crawl wcrawl
+if [ -f WIZARD/source/crawl ]; then
+	ln -sf WIZARD/source/crawl wcrawl
 fi
