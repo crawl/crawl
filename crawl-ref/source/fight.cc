ed2216e8f9a93c5ba091659fd5e4ee5750187e80
j-p-e-g <j-p-e-g@c06c8d41-db1a-0410-9941-cceddc491573>
2008-07-27 21:29:46 +0000

    Fix 2028969: excess comma in autoinscriptions of brand-only artefacts. Also: Tweak inscription prompt.
    Fix 2027600: Don't prompt when attacking with inappropriate weapon while
                 confused.
    Fix 2027328: Throwing projectiles at enslaved monsters.
    
    When saccing to Nemelex, count chunks and !blood like corpses (deck of
    summoning rather than wonder). Chunks used to be ignored completely but
    since they're just chopped up corpses and corpses are worth much more
    than 4 per piece I don't think it hurts. (Rotten chunks have a value of
    zero.) Also, decrease !blood price to that of !porridge and lower.
    
    
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@6700 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/fight.cc
+++ crawl-ref/source/fight.cc
@@ -4075,7 +4075,7 @@ int melee_attack::mons_to_hit()
 static bool wielded_weapon_check(const item_def *weapon)
 {
     bool result = true;
-    if (!you.received_weapon_warning
+    if (!you.received_weapon_warning && !you.duration[DUR_CONF]
         && (weapon && weapon->base_type != OBJ_STAVES
                && (weapon->base_type != OBJ_WEAPONS || is_range_weapon(*weapon))
             || you.attribute[ATTR_WEAPON_SWAP_INTERRUPTED]))
--- crawl-ref/source/shopping.cc
+++ crawl-ref/source/shopping.cc
@@ -1284,7 +1284,6 @@ unsigned int item_value( item_def item, bool ident )
             case POT_RESTORE_ABILITIES:
                 valued += 50;
                 break;
-            case POT_BLOOD:
             case POT_BERSERK_RAGE:
             case POT_HEAL_WOUNDS:
                 valued += 30;
@@ -1297,9 +1296,12 @@ unsigned int item_value( item_def item, bool ident )
             case POT_LEVITATION:
                 valued += 20;
                 break;
-            case POT_BLOOD_COAGULATED:
+            case POT_BLOOD:
             case POT_PORRIDGE:
                 valued += 10;
+                break;
+            case POT_BLOOD_COAGULATED:
+                valued += 5;
                 break;
             case POT_CONFUSION:
             case POT_DECAY:
@@ -1353,11 +1355,14 @@ unsigned int item_value( item_def item, bool ident )
             valued = 8;
             break;
 
+        case FOOD_CHUNK:
+            if (food_is_rotten(item))
+                break;
+
         case FOOD_CHOKO:
         case FOOD_LYCHEE:
         case FOOD_RAMBUTAN:
         case FOOD_SNOZZCUMBER:
-        case FOOD_CHUNK:
             valued = 4;
             break;
 
