f6ecf26afba1584016b1c1e7ca3ed8b88fd8fdd2
dploog <dploog@c06c8d41-db1a-0410-9941-cceddc491573>
2008-11-18 21:23:48 +0000

    Each new type of portal vault should get its own des file.
    Renamed portal.des to sewer.des. Added description.
    
    
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@7483 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/dat/clua/loadmaps.lua
+++ crawl-ref/source/dat/clua/loadmaps.lua
@@ -9,7 +9,7 @@
 local des_files = {
    "altar.des", "bazaar.des", "entry.des", "elf.des", "float.des", "hells.des",
    "hive.des", "lab.des", "lair.des", "large.des", "layout.des", "mini.des", 
-   "orc.des", "pan.des", "portal.des", "temple.des", "vaults.des", "crypt.des",
+   "orc.des", "pan.des", "sewer.des", "temple.des", "vaults.des", "crypt.des",
    "zot.des"
 }
 
