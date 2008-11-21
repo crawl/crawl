6dc77c25f680baa205484f7f4e29e1a8f0fb20aa
zelgadis <zelgadis@c06c8d41-db1a-0410-9941-cceddc491573>
2008-11-21 13:00:21 +0000

    Added a convenience script for invoking Valgrind to find memory errors/bugs, and a Valgrind suppressions file to suppress things that either aren't really memory bugs or are bugs in third party libraries which we can't fix.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@7526 c06c8d41-db1a-0410-9941-cceddc491573

---
+++ crawl-ref/source/misc/grind.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Convenience caller for the valgrind memory debugger
+
+nice -n 7 valgrind --tool=memcheck --leak-check=full --log-file=grind.log \
+	--suppressions=misc/valgrind-suppress.txt ./crawl $*
