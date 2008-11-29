0e263f7d430aa39e563502678657f2c55c507503
dploog <dploog@c06c8d41-db1a-0410-9941-cceddc491573>
2008-11-29 01:33:13 +0000

    Added miniature tomb portal vault (Zaba).
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@7681 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/dat/clua/loadmaps.lua
+++ crawl-ref/source/dat/clua/loadmaps.lua
@@ -8,12 +8,12 @@
 
 local des_files = {
   -- The dummy vaults that define global vault generation odds.
-  "dummy.des", 
-  
+  "dummy.des",
+
   "altar.des", "bazaar.des", "entry.des", "elf.des", "float.des", "hells.des",
   "hive.des", "icecave.des", "lab.des", "lair.des", "large.des", "layout.des",
-  "mini.des", "orc.des", "pan.des", "sewer.des", "temple.des", "vaults.des",
-  "crypt.des", "ziggurat.des", "zot.des", "rooms.des"
+  "mini.des", "minitomb.des", "orc.des", "pan.des", "sewer.des", "temple.des",
+  "vaults.des", "crypt.des", "ziggurat.des", "zot.des", "rooms.des"
 }
 
 for _, file in ipairs(des_files) do
