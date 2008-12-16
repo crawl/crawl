d6dc4cd62e84baedf1b9e8885f2f7c1685c3f52d
zelgadis <zelgadis@c06c8d41-db1a-0410-9941-cceddc491573>
2008-12-16 01:30:06 +0000

    Merge r7840: fix for bug 2430493: forget about interrupted butchering when doing auto-explore or inter-level travel.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/branches/stone_soup-0.4@7841 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/travel.cc
+++ crawl-ref/source/travel.cc
@@ -2496,6 +2496,9 @@ void start_translevel_travel(bool prompt_for_destination)
 
     if (level_target.p.id.depth > 0)
     {
+        // Forget interrupted butchering.
+        you.attribute[ATTR_WEAPON_SWAP_INTERRUPTED] = 0;
+
         you.running = RMODE_INTERLEVEL;
         you.running.x = you.running.y = 0;
         last_stair.depth = -1;
@@ -2918,6 +2921,9 @@ void start_explore(bool grab_items)
 
     if (!i_feel_safe(true, true))
         return;
+
+    // Forget interrupted butchering.
+    you.attribute[ATTR_WEAPON_SWAP_INTERRUPTED] = 0;
 
     you.running = grab_items? RMODE_EXPLORE_GREEDY : RMODE_EXPLORE;
     if (you.running == RMODE_EXPLORE_GREEDY
