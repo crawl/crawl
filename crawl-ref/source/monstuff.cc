eef6876fa455bad0e5cec08fd8b39a5d602328b7
zelgadis <zelgadis@c06c8d41-db1a-0410-9941-cceddc491573>
2008-12-23 23:31:32 +0000

    Bug #2455248: open Slime:6 vaults if the Royal Jelly is polymorphed.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@7925 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/monstuff.cc
+++ crawl-ref/source/monstuff.cc
@@ -1821,6 +1821,11 @@ bool monster_polymorph(monsters *monster, monster_type targetc,
     // the monster is still a mermaid/siren.
     update_beholders(monster, true);
 
+    // Inform listenerns that the original monster is gone (and
+    // unlock the vaults on Slime:6 if it's the Royal Jelly which
+    // was changed).
+    _fire_monster_death_event(monster, KILL_MISC, NON_MONSTER);
+
     // the actual polymorphing:
     const int  old_hp     = monster->hit_points;
     const int  old_hp_max = monster->max_hit_points;
