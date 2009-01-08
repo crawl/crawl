f0c54ae2b5ef64fa7ca801ef3c4db9eedde698fd
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2009-01-08 17:49:47 +0000

    Add spacing fixes.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@8332 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/mon-util.cc
+++ crawl-ref/source/mon-util.cc
@@ -7300,7 +7300,7 @@ actor *monsters::get_foe() const
     if (foe == MHITNOT)
         return (NULL);
     else if (foe == MHITYOU)
-        return (mons_friendly(this)? NULL : &you);
+        return (mons_friendly(this) ? NULL : &you);
 
     // Must be a monster!
     monsters *my_foe = &menv[foe];
