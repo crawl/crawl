a8b134e880670b0dbd21df0c79ffbabc1a7911ae
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2009-01-19 00:09:59 +0000

    Make draining from spell miscasts take full negative energy resistance into account, too.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@8564 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -3853,7 +3853,7 @@ void MiscastEffect::_necromancy(int severity)
             if (lethality_margin == 0 || you.experience > 0
                 || !avoid_lethal(you.hp))
             {
-                if (!target->res_negative_energy() && one_chance_in(3))
+                if (target->res_negative_energy() < 3 && one_chance_in(3))
                 {
                     target->drain_exp(act_source);
                     break;
@@ -3925,7 +3925,7 @@ void MiscastEffect::_necromancy(int severity)
             if (lethality_margin == 0 || you.experience > 0
                 || !avoid_lethal(you.hp))
             {
-                if (!target->res_negative_energy())
+                if (target->res_negative_energy() < 3)
                 {
                     target->drain_exp(act_source);
                     break;
