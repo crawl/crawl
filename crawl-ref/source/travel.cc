345c5b18e422b059a183f4820e7ea870ab752abe
ennewalker <ennewalker@c06c8d41-db1a-0410-9941-cceddc491573>
2009-01-20 23:22:14 +0000

    [2523738] Fix assertion in inter-level travel.  (Sorry!)
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@8637 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/travel.cc
+++ crawl-ref/source/travel.cc
@@ -2628,8 +2628,11 @@ static int _find_transtravel_stair( const level_id &cur,
             return (-1);
 
         // Can we even travel to this square?
-        if (!is_travelsafe_square(target.pos.x, target.pos.y, true))
+        if (in_bounds(target.pos)
+            && !is_travelsafe_square(target.pos.x, target.pos.y, true))
+        {
             return (-1);
+        }
 
         // If there's no target position on the target level, or we're on the
         // target, we're home.
