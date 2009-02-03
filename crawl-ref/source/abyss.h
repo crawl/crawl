09aad943f1aae98c6f0fd03c94583ad2afc65e09
haranp <haranp@c06c8d41-db1a-0410-9941-cceddc491573>
2009-02-03 23:14:48 +0000

    Very minor cleanup. (OK, so I wanted r8888. Sue me.)
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@8888 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/abyss.h
+++ crawl-ref/source/abyss.h
@@ -10,31 +10,11 @@
 #ifndef ABYSS_H
 #define ABYSS_H
 
-
-// last updated 02apr2001 {gdl}
-/* ***********************************************************************
- * called from: dungeon
- * *********************************************************************** */
-void generate_abyss(void);
-
-
-// last updated 12may2000 {dlb}
-/* ***********************************************************************
- * called from: acr
- * *********************************************************************** */
-void area_shift(void);
-
-
-// last updated 12may2000 {dlb}
-/* ***********************************************************************
- * called from: spells1 - spells3 - spells4
- * *********************************************************************** */
-void abyss_teleport( bool new_area );
-
+void generate_abyss();
+void area_shift();
+void abyss_teleport(bool new_area);
 void save_abyss_uniques();
-
 bool lugonu_corrupt_level(int power);
-
 void run_corruption_effects(int duration);
 
 #endif
