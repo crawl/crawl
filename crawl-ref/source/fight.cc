f8ddd7fae6483fe993ed27cedd8bde6f616de03b
evktalo <evktalo@c06c8d41-db1a-0410-9941-cceddc491573>
2009-07-06 20:00:19 +0000

    Apply jarmok's patch (FR#1768835): visored helmet prevents bite attack.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@10116 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/fight.cc
+++ crawl-ref/source/fight.cc
@@ -1179,6 +1179,12 @@ bool melee_attack::player_aux_unarmed()
             {
                 continue;
             }
+            // no biting with visored helmet
+            if (you.equip[EQ_HELMET] != -1 
+                && (get_helmet_desc((you.inv[you.equip[EQ_HELMET]])) == THELM_DESC_VISORED))
+            {
+                continue;
+            }
 
             unarmed_attack = "bite";
             simple_miss_message = true;
