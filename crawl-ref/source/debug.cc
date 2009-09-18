85643d2e2070a731dfb74245085e9e43746f4db9
dolorous <dolorous@c06c8d41-db1a-0410-9941-cceddc491573>
2009-09-18 01:37:57 +0000

    Display acid and sticky flame resistance in the debugging stethoscope.
    git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@10704 c06c8d41-db1a-0410-9941-cceddc491573

--- crawl-ref/source/debug.cc
+++ crawl-ref/source/debug.cc
@@ -2680,12 +2680,15 @@ void debug_stethoscope(int mon)
          god_name(mons.god).c_str() );
 
     // Print resistances.
-    mprf(MSGCH_DIAGNOSTICS, "resist: fire=%d cold=%d elec=%d pois=%d neg=%d",
+    mprf(MSGCH_DIAGNOSTICS, "resist: fire=%d cold=%d elec=%d pois=%d neg=%d "
+                            "acid=%d sticky=%d",
          mons_res_fire( &mons ),
          mons_res_cold( &mons ),
          mons_res_elec( &mons ),
          mons_res_poison( &mons ),
-         mons_res_negative_energy( &mons ) );
+         mons_res_negative_energy( &mons ),
+         mons_res_acid( &mons ),
+         mons_res_sticky_flame( &mons ) );
 
     mprf(MSGCH_DIAGNOSTICS, "ench: %s",
          mons.describe_enchantments().c_str());
