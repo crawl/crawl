43fefcd4cf312be17f79139e04345a4ab29998ec
Steven Noonan <steven@uplinklabs.net>
2009-10-14 02:29:00 -0700

    tilesdl.cc: fix loader for app icon to allow absolute paths
    If someone uses DATA_DIR_PATH, we don't want the game to break.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- crawl-ref/source/tilesdl.cc
+++ crawl-ref/source/tilesdl.cc
@@ -249,7 +249,7 @@ bool TilesFramework::initialise()
             "dat/tiles/stone_soup_icon-32x32.png";
 #endif
 
-    SDL_Surface *icon = IMG_Load(datafile_path(icon_name).c_str());
+    SDL_Surface *icon = IMG_Load(datafile_path(icon_name, true, true).c_str());
     if (!icon)
     {
         printf("Failed to load icon: %s\n", SDL_GetError());
