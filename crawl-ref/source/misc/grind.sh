2c88c64bea5a1dac47296e883c5a4fa369f8c717
Matthew Cline <zelgadis@sourceforge.net>
2009-10-20 16:04:37 -0700

    misc/grind.sh: Make work with -arena option

--- crawl-ref/source/misc/grind.sh
+++ crawl-ref/source/misc/grind.sh
@@ -3,4 +3,4 @@
 # Convenience caller for the valgrind memory debugger
 
 nice -n 7 valgrind --tool=memcheck --leak-check=full --log-file=grind.log \
-	--suppressions=misc/valgrind-suppress.txt ./crawl $*
+	--suppressions=misc/valgrind-suppress.txt ./crawl "$@"
