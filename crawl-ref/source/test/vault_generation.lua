ea81f9787b7cba7a18ec7c098fcd0fe37fe7daef
Darshan Shaligram <dshaligram@users.sourceforge.net>
2009-10-24 16:31:51 +0530

    Fix minor bugs in vault_generation test (due).

--- crawl-ref/source/test/vault_generation.lua
+++ crawl-ref/source/test/vault_generation.lua
@@ -12,10 +12,12 @@ local checks = 10
 -- Output to this file, will append iteration to the end, ie,
 -- output_to.map.1, output_to.map.2, etc.
 local output_to = ""
+
 -- Should we run these tests?
-local run_test = false
+local run_test = map_to_test ~= ""
 
 local function generate_map()
+    output_to = output_to == "" and map_to_test or output_to
     if map_to_test == "" or
        (des_file == "" and need_to_load_des) or
        output_to == "" then
@@ -39,7 +41,7 @@ local function generate_map()
         dgn.tags(map, "no_rotate no_vmirror no_hmirror no_pool_fixup")
         dgn.place_map(map, true, true)
         crawl.mpr("Placed " .. map_to_test .. ":" .. iter_i .. ", dumping to " .. output_to .. "." .. iter_i)
-        debug.dump_map(map_to_test .. "." .. iter_i)
+        debug.dump_map(output_to .. "." .. iter_i)
     end
 end
 
