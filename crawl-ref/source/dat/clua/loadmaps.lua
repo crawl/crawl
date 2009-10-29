cecf00e3299727153d5cf130c0b9ae30c200f75d
Eino Keskitalo <evktalo@users.sourceforge.net>
2009-10-29 12:46:41 +0200

    Regroup .des files in loadmaps.lua. Signed-off-by: Eino Keskitalo <evktalo@users.sourceforge.net>

--- crawl-ref/source/dat/clua/loadmaps.lua
+++ crawl-ref/source/dat/clua/loadmaps.lua
@@ -10,17 +10,21 @@ local des_files = {
   -- The dummy vaults that define global vault generation odds.
   "dummy.des",
 
-  -- Example vaults, included here so that Crawl will syntax-check them
+  -- Example vaults, included here so that Crawl will syntax-check them.
   "didact.des",
 
   "arena.des",
 
-  "altar.des", "bazaar.des", "bailey.des", "entry.des", "elf.des", 
-  "float.des", "hells.des", "hive.des", "icecave.des", "lab.des", 
-  "lair.des", "large.des", "layout.des", "mini.des", "ossuary.des", 
-  "orc.des", "pan.des", "sewer.des", "shoals.des", "temple.des",
-  "trove.des", "shrine.des",
-  "vaults.des", "crypt.des", "ziggurat.des", "zot.des", "rooms.des"
+  "layout.des", "rooms.des",
+
+  -- Portal vaults.
+  "bazaar.des", "bailey.des", "icecave.des", "lab.des", "ossuary.des",
+  "sewer.des", "trove.des", "shrine.des", "ziggurat.des",
+
+  -- Normal vaults.
+  "altar.des", "entry.des", "elf.des", "float.des", "hells.des", "hive.des",
+  "lair.des", "large.des", "mini.des", "orc.des", "pan.des", "shoals.des",
+  "temple.des", "vaults.des", "crypt.des", "zot.des"
 }
 
 for _, file in ipairs(des_files) do
