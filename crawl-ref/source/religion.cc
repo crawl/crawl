1a22a6c5f2cfb0ddf0ff713689c953ea86ff055c
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2009-10-29 15:54:37 -0500

    Readd prayer duration fixes for gods who accept fresh corpses or butchery, as long prayers in such cases are redundant.

--- crawl-ref/source/religion.cc
+++ crawl-ref/source/religion.cc
@@ -2494,13 +2494,22 @@ void pray()
             you.duration[DUR_PRAYER] *= 2;
     }
 
-    if (you.religion == GOD_ZIN || you.religion == GOD_BEOGH
-        || you.religion == GOD_NEMELEX_XOBEH || you.religion == GOD_JIYVA)
+    // Assume for now that gods who like fresh corpses and/or butchery
+    // don't use prayer for anything else.
+    if (you.religion == GOD_ZIN
+        || you.religion == GOD_BEOGH
+        || you.religion == GOD_NEMELEX_XOBEH
+        || you.religion == GOD_JIYVA
+        || god_likes_fresh_corpses(you.religion)
+        || god_likes_butchery(you.religion))
     {
         you.duration[DUR_PRAYER] = 1;
     }
-    else if (you.religion == GOD_ELYVILON || you.religion == GOD_YREDELEMNUL)
+    else if (you.religion == GOD_ELYVILON
+        || you.religion == GOD_YREDELEMNUL)
+    {
         you.duration[DUR_PRAYER] = 20;
+    }
     else if (you.religion == GOD_FEAWN)
     {
         if (you.duration[DUR_SLOW] < you.duration[DUR_PRAYER])
