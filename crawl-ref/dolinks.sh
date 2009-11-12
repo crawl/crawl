934e30171765eb1e46d0a2d519ab055f14f506ed
Matthew Cline <zelgadis@sourceforge.net>
2009-11-11 22:07:25 -0800

    dolinsks.sh: only link needed files

--- crawl-ref/dolinks.sh
+++ crawl-ref/dolinks.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+dolinks() {
+    mkdir -p source
+    cd source
+    ln -sf ../../source/*.{cc,h} .
+    ln -sf ../../source/makefile* .
+    ln -sf ../../source/{util,contrib,rltiles,prebuilt,dat} .
+}
+
 mkdir -p NORMAL
 mkdir -p WIZARD
 
-pushd NORMAL ; mkdir -p source ; cd source ; ln -sf ../../source/* . ; popd
-pushd WIZARD ; mkdir -p source ; cd source ; ln -sf ../../source/* . ; popd
+pushd NORMAL ; dolinks ; popd
+pushd WIZARD ; dolinks ; popd
 
 if [ -d dat ]; then
 	true
