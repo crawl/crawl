e3392e343f7de1eadb12f8080c96be6b65c432e7
Robert Vollmert <rvollmert@gmx.net>
2009-11-14 16:53:43 +0100

    Document random_choose_weighted.

--- crawl-ref/source/random-weight.h
+++ crawl-ref/source/random-weight.h
@@ -1,6 +1,12 @@
 #ifndef RANDOM_WEIGHT_H
 #define RANDOM_WEIGHT_H
 
+/*
+ * Weighted choice.
+ *
+ * Weights are assumed to be non-negative, but are allowed to be zero.
+ * Returns NULL if nothing found, i.e., if all weights are zero.
+ */
 template <typename T>
 T* random_choose_weighted(std::vector<std::pair<T, int> > choices)
 {
