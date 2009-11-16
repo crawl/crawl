5d53fe4d764de92058f505a240af27438c07785e
Stefan O'Rear <stefanor@cox.net>
2009-11-15 22:55:29 -0800

    Remove speed from polearms, long blades, and maces (dpeg)

--- crawl-ref/source/makeitem.cc
+++ crawl-ref/source/makeitem.cc
@@ -1278,9 +1278,6 @@ static brand_type _determine_weapon_brand(const item_def& item, int item_level)
         case WPN_SCIMITAR:
             if (one_chance_in(25))
                 rc = SPWPN_PAIN;
-
-            if (one_chance_in(7))
-                rc = SPWPN_SPEED;
             // **** intentional fall through here ****
         case WPN_GREAT_SWORD:
         case WPN_DOUBLE_SWORD:
@@ -1363,9 +1360,6 @@ static brand_type _determine_weapon_brand(const item_def& item, int item_level)
                 rc = SPWPN_REACHING;
 
             if (one_chance_in(5))
-                rc = SPWPN_SPEED;
-
-            if (one_chance_in(5))
                 rc = SPWPN_ELECTROCUTION;
             break;
 
@@ -1383,8 +1377,6 @@ static brand_type _determine_weapon_brand(const item_def& item, int item_level)
             if (one_chance_in(4))
                 rc = SPWPN_PROTECTION;
 
-            if (one_chance_in(5))
-                rc = SPWPN_SPEED;
             // **** intentional fall through here ****
         case WPN_SPEAR:
             if (one_chance_in(25))
