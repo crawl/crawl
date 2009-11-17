3a6efa8421c1461ae7b8191d3ac57a819abe5eb4
Matthew Cline <zelgadis@sourceforge.net>
2009-11-17 14:48:04 -0800

    Bug 2899436: Seen tiles not drawn on load game
    Seems adding "you.update_los();" right before "viewwindow()" in
    _initialise() fixes the problem.

--- crawl-ref/source/main.cc
+++ crawl-ref/source/main.cc
@@ -3847,6 +3847,7 @@ static bool _initialise(void)
     maybe_update_stashes();
 
     // This just puts the view up for the first turn.
+    you.update_los(); // Needed for tiles to draw in-LOS terrain.
     viewwindow(false);
 
     activate_notes(true);
