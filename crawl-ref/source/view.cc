4a10fcf0a1c02d83dc8c2accce057ab180544e5a
Robert Vollmert <rvollmert@gmx.net>
2009-12-04 19:27:34 +0100

    Do out-of-LOS staircase emphasis when drawing.
    This fixes emphasised stair colours being out of date until they
    come into LOS again.

--- crawl-ref/source/view.cc
+++ crawl-ref/source/view.cc
@@ -709,7 +709,7 @@ static int player_view_update_at(const coord_def &gc)
         ret |= UF_AFFECT_EXCLUDES;
 
     set_terrain_seen(gc);
-    set_map_knowledge_obj(gc, to_knowledge(env.show(ep), emphasise(gc)));
+    set_map_knowledge_obj(gc, to_knowledge(env.show(ep)));
     set_map_knowledge_detected_mons(gc, false);
     set_map_knowledge_detected_item(gc, false);
 
@@ -724,8 +724,7 @@ static int player_view_update_at(const coord_def &gc)
 #endif
 
     if (Options.clean_map && env.show.get_backup(ep))
-        set_map_knowledge_obj(gc, to_knowledge(env.show.get_backup(ep),
-                                               emphasise(gc)));
+        set_map_knowledge_obj(gc, to_knowledge(env.show.get_backup(ep)));
 
     return (ret);
 }
