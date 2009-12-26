1c6fbd31a13a5c3bb8ef2b08bd2851d77f1ea598
Johanna Ploog <j-p-e-g@users.sourceforge.net>
2009-12-26 01:38:35 +0100

    Make wizard heal clear the misled effect.

--- crawl-ref/source/wiz-you.cc
+++ crawl-ref/source/wiz-you.cc
@@ -210,7 +210,8 @@ void wizard_heal(bool super_heal)
     // Clear most status ailments.
     you.rotting = 0;
     you.disease = 0;
-    you.duration[DUR_CONF] = 0;
+    you.duration[DUR_CONF]      = 0;
+    you.duration[DUR_MISLED]    = 0;
     you.duration[DUR_POISONING] = 0;
     set_hp(you.hp_max, false);
     set_mp(you.max_magic_points, false);
