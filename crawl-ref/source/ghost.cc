71dd6514367e7a345fa9d2480c353c93826c5b84
Darshan Shaligram <dshaligram@users.sourceforge.net>
2009-12-30 20:53:40 +0530

    [Mantis 270] Fix excessive ghosts on early levels.

--- crawl-ref/source/ghost.cc
+++ crawl-ref/source/ghost.cc
@@ -850,7 +850,7 @@ void ghost_demon::announce_ghost(const ghost_demon &g)
 
 void ghost_demon::find_extra_ghosts( std::vector<ghost_demon> &gs, int n )
 {
-    for (monster_iterator mi; mi; ++mi)
+    for (monster_iterator mi; mi && n > 0; ++mi)
     {
         if (mi->type == MONS_PLAYER_GHOST && mi->ghost.get())
         {
