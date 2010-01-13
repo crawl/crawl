f6a7496a215d11ab5d72736aea2f3f74df84fb11
Adam Borowski <kilobyte@angband.pl>
2010-01-13 20:48:24 +0100

    Don't mess with memory of slime creatures when in tiles.

--- crawl-ref/source/show.cc
+++ crawl-ref/source/show.cc
@@ -368,8 +368,10 @@ void show_def::_update_monster(const monsters* mons)
     grid(e).cls = SH_MONSTER;
     if (!crawl_state.arena && you.misled())
         grid(e).mons = mons->get_mislead_type();
+#ifndef USE_TILE
     else if (mons->type == MONS_SLIME_CREATURE && mons->number > 1)
         grid(e).mons = MONS_MERGED_SLIME_CREATURE;
+#endif
     else
         grid(e).mons = mons->type;
     grid(e).colour = get_mons_glyph(mons).col;
