73905f957c3a71355a090467b59013bbf6d235bc
Darshan Shaligram <dshaligram@users.sourceforge.net>
2010-01-24 18:49:57 +0530

    Fix stash-tracker item annotations and restrict user scripts to access items only on the same turn the items were wrapped for Lua by the core C++ code.

--- crawl-ref/source/l_libs.h
+++ crawl-ref/source/l_libs.h
@@ -78,4 +78,6 @@ class map_lines;
 int dgn_map_add_transform(lua_State *ls,
           std::string (map_lines::*add)(const std::string &s));
 
+void clua_push_item(lua_State *ls, item_def *item);
+
 #endif
