1c685053d03c7c475f1e39ebead73977aac6af41
Robert Vollmert <rvollmert@gmx.net>
2010-02-05 12:54:05 +0100

    Revert "Make travel refuse to enter cells adjacent to mimics."
    This reverts commit 468b5880fcee3bc59677b3a963ba47c819674ae5.
    
    Fixes issue #690:
    
    As due noted, the additional check in is_travelsafe_square is possibly too
    expensive: showing the colour coded X map now takes over a second on his
    computer. To reproduce, go to vault:8, teleport to a corner, magic map and
    detect monsters, and then bring up the X map.
    
    Also, this leaks information: After magic mapping, you'll see all monster
    locations by the grayed out cells on the X map. autotravel/explore should
    work on player knowledge only, hence shouldn't consider the actual monster
    grid (monster_at, mons_is_safe) but only env.map_knowledge(c).mons
    (remembered monster class).

--- crawl-ref/source/main.cc
+++ crawl-ref/source/main.cc
@@ -720,7 +720,7 @@ static void _start_running( int dir, int mode )
     if (Tutorial.tutorial_events[TUT_SHIFT_RUN] && mode == RMODE_START)
         Tutorial.tutorial_events[TUT_SHIFT_RUN] = false;
 
-    if (i_feel_safe(true, true))
+    if (i_feel_safe(true))
         you.running.initialise(dir, mode);
 }
 
@@ -842,7 +842,7 @@ static bool _cmd_is_repeatable(command_type cmd, bool is_again = false)
     case CMD_MOVE_DOWN_LEFT:
     case CMD_MOVE_UP_RIGHT:
     case CMD_MOVE_DOWN_RIGHT:
-        if (!i_feel_safe(false, true))
+        if (!i_feel_safe())
         {
             return yesno("Really repeat movement command while monsters "
                          "are nearby?", false, 'n');
