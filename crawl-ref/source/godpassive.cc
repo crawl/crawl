47c9809ab6e954c02c2384e751a6b188718a4d80
Haran Pilpel <haranp@users.sourceforge.net>
2010-02-09 23:07:04 +0200

    Implement n(n+1)/2 ponderousness boost for Chei (dpeg).
    No counter-nerf yet.

--- crawl-ref/source/godpassive.cc
+++ crawl-ref/source/godpassive.cc
@@ -28,7 +28,7 @@ int che_boost(che_boost_type bt, int level)
     case CB_RFIRE:
         return (level > 2 ? 1 : 0);
     case CB_STATS:
-        return level;
+        return (level * (level+1)) / 2;
     default:
         return 0;
     }
@@ -42,9 +42,10 @@ void che_handle_change(che_change_type ct, int diff)
     const std::string typestr = (ct == CB_PIETY ? "piety" : "ponderous");
 
     // Values after the change.
-    int ponder = player_equip_ego_type(EQ_ALL_ARMOUR, SPARM_PONDEROUSNESS);
-    int prank = piety_rank() - 1;
-    int newlev = std::min(ponder, prank);
+    const int ponder = player_equip_ego_type(EQ_ALL_ARMOUR,
+                                             SPARM_PONDEROUSNESS);
+    const int prank = piety_rank() - 1;
+    const int newlev = std::min(ponder, prank);
 
     // Reconstruct values before the change.
     int oldponder = ponder;
@@ -53,18 +54,18 @@ void che_handle_change(che_change_type ct, int diff)
         oldprank -= diff;
     else // ct == CB_PONDEROUS
         oldponder -= diff;
-    int oldlev = std::min(oldponder, oldprank);
+    const int oldlev = std::min(oldponder, oldprank);
 
     for (int i = 0; i < NUM_BOOSTS; ++i)
     {
-        che_boost_type bt = static_cast<che_boost_type>(i);
-        int boostdiff = che_boost(bt, newlev) - che_boost(bt, oldlev);
+        const che_boost_type bt = static_cast<che_boost_type>(i);
+        const int boostdiff = che_boost(bt, newlev) - che_boost(bt, oldlev);
         if (boostdiff == 0)
             continue;
 
-        std::string elemmsg = god_name(you.religion)
-                              + (boostdiff > 0 ? " " : " no longer ")
-                              + "protects you from %s.";
+        const std::string elemmsg = god_name(you.religion)
+                                    + (boostdiff > 0 ? " " : " no longer ")
+                                    + "protects you from %s.";
         switch (bt)
         {
         case CB_RNEG:
