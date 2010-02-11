c9a93f01675539dcd1cf2d3a76ccaf471420b05e
Johanna Ploog <j-p-e-g@users.sourceforge.net>
2010-02-11 22:49:29 +0100

    Don't autoinscribe items with "was cursed" that you saw getting cursed.
    This should cover ?curse weapon/armour and mummy curses.
    
    The original intent was that items *found* cursed are more likely to
    have other unpalatable properties as well, such as a negative base type
    (hunger), bad randart properties or negative enchantment.
    Also inscribing items that became cursed at a later point muddles that
    purpose.

--- crawl-ref/source/enum.h
+++ crawl-ref/source/enum.h
@@ -1406,7 +1406,7 @@ enum item_status_flag_type  // per item flags: ie. ident status, cursed status
 
     ISFLAG_CURSED            = 0x00000100,  // cursed
     ISFLAG_BLESSED_WEAPON    = 0x00000200,  // personalized TSO's gift
-    ISFLAG_RESERVED_2        = 0x00000400,  // reserved
+    ISFLAG_SEEN_CURSED       = 0x00000400,  // was seen being cursed
     ISFLAG_RESERVED_3        = 0x00000800,  // reserved
 
     ISFLAG_RANDART           = 0x00001000,  // special value is seed
