63b5356f82f1b793e188da0d351acbef86350224
Darshan Shaligram <dshaligram@users.sourceforge.net>
2010-02-13 02:08:30 +0530

    Che monster-player speed delta ignores burden state and the Slow spell. Che piety for killing slow monsters is now awarded only if random2(level + 18 - you.experience_level / 2) > 5 (matching piety gain from DID_KILL_LIVING for the blood gods).

--- crawl-ref/source/mon-util.h
+++ crawl-ref/source/mon-util.h
@@ -362,8 +362,8 @@ bool mons_class_is_stationary(int mc);
 bool mons_is_stationary(const monsters *mon);
 bool mons_is_firewood(const monsters *mon);
 
-int monster_player_speed_delta(const monsters *mon);
-bool mons_is_fast( const monsters *mon );
+int cheibriados_monster_player_speed_delta(const monsters *mon);
+bool cheibriados_thinks_mons_is_fast( const monsters *mon );
 bool mons_is_projectile(int mc);
 bool mons_is_insubstantial(int mc);
 bool mons_has_blood(int mc);
