0dd88ee4b5f02cf5aea0d0025b097b4d7916133e
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-02-13 17:57:18 -0600

    Fix compilation. (cherry picked from commit 1d0c00d6f71a7512da5ee08bd5bc6fabab8e4edd)

--- crawl-ref/source/mon-abil.cc
+++ crawl-ref/source/mon-abil.cc
@@ -953,6 +953,7 @@ bool mon_special_ability(monsters *monster, bolt & beem)
     case MONS_ACID_BLOB:
     case MONS_OKLOB_PLANT:
     case MONS_YELLOW_DRACONIAN:
+    {
         if (monster->has_ench(ENCH_CONFUSION))
             break;
 
@@ -980,6 +981,7 @@ bool mon_special_ability(monsters *monster, bolt & beem)
             }
         }
         break;
+    }
 
     case MONS_MOTH_OF_WRATH:
         if (one_chance_in(3))
