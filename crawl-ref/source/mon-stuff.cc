fe0a0a46a721a03b892683ec6490e03042e64b1e
Jude Brown <bookofjude@users.sourceforge.net>
2010-02-15 18:03:16 +1000

    Clean up monster death effects, elven twin death effects, mon-death.cc.
    This commit moves the monster "death effects" (primarily for uniques)
    out of mon-stuff.cc, and into mon-death.cc. This includes Kirke's
    hogs_to_humans(), Pikel's band neutralisation, and Dowan and Duvessa's
    death effects.
    
    Introduce some new functions which should condense checks to
    monster->type and monster->propes["original_name"]: mons_is_dowan,
    mons_is_duvessa, mons_is_pikel, mons_is_kirke. This means that death
    effects should trigger and affect monsters even through multiple
    polymorphs.
    
    It also with #797, pacification effects: pacifying one pacifies the
    other, and experience and piety gain for Elyvilon are similar. This
    should work for all forms of pacification (but not necessarily all forms
    of neutrality). Attacking one of them while pacified will cause both of
    them to turn hostile.
    
    Finally, this commit introduces code and flavour speech for instances
    where one of them accidentally kills the other (enslavement wands,
    frenzy needles).
    
    Still need to be looked at: turning hostile if friendly when killing a
    twin, both confused when dying (#459), and stabbing when neutral
    (doens't give a chance for the behaviour code to trigger).
    (cherry picked from commit 04479e72bd97c394a52170c4b5a81bf00bd34946)

--- crawl-ref/source/mon-stuff.cc
+++ crawl-ref/source/mon-stuff.cc
@@ -35,6 +35,7 @@
 #include "mon-abil.h"
 #include "mon-behv.h"
 #include "mon-clone.h"
+#include "mon-death.h"
 #include "mon-iter.h"
 #include "mon-place.h"
 #include "mon-util.h"
@@ -1081,238 +1082,6 @@ void _monster_die_cloud(const monsters* monster, bool corpse, bool silent,
     }
 }
 
-// XXX: Another hackish function! May do weird things if multiple copies of
-//      the band have been placed using wizard mode. {due}
-static void _elven_twin_died(monsters* twin, bool in_transit)
-{
-    bool found_duvessa = false;
-    bool found_dowan = false;
-    monsters *monster;
-
-    for (monster_iterator mi; mi; ++mi)
-    {
-        if (*mi == twin)
-            continue;
-
-        if (mi->type == MONS_DUVESSA
-            || (mi->props.exists("original_name")
-                && mi->props["original_name"].get_string() == "Duvessa"))
-        {
-            monster = *mi;
-            found_duvessa = true;
-            break;
-        }
-        else if (mi->type == MONS_DOWAN
-                 || (mi->props.exists("original_name")
-                     && mi->props["original_name"].get_string() == "Dowan"))
-        {
-            monster = *mi;
-            found_dowan = true;
-            break;
-        }
-    }
-
-    if (!found_duvessa && !found_dowan)
-        return;
-
-    // Okay, let them climb stairs now.
-    monster->props["can_climb"] = "yes";
-    if (!in_transit)
-        monster->props["speech_prefix"] = "twin_died";
-    else
-        monster->props["speech_prefix"] = "twin_banished";
-
-    // If you've stabbed one of them, the other one is likely asleep still.
-    if (monster->asleep())
-        behaviour_event(monster, ME_DISTURB, MHITNOT, monster->pos());
-
-    // Will generate strings such as 'Duvessa_Duvessa_dies' or, alternately
-    // 'Dowan_Dowan_dies', but as neither will match, these can safely be
-    // ignored.
-    std::string key = "_" + monster->name(DESC_CAP_THE, true) + "_"
-                          + twin->name(DESC_CAP_THE) + "_dies_";
-
-    if (mons_near(monster) && !monster->observable())
-        key += "invisible_";
-    else if (!mons_near(monster))
-        key += "distance_";
-
-    std::string death_message = getSpeakString(key);
-
-    // Check if they can speak or not: they may have been polymorphed.
-    if (mons_near(monster) && !death_message.empty() && monster->can_speak())
-        mons_speaks_msg(monster, death_message, MSGCH_TALK, silenced(you.pos()));
-    else if (monster->can_speak())
-        mprf("%s", death_message.c_str());
-
-    if (found_duvessa)
-    {
-        if (mons_near(monster))
-            // Provides its own flavour message.
-            monster->go_berserk(true);
-        else
-            // She'll go berserk the next time she sees you
-            monster->flags |= MF_GOING_BERSERK;
-    }
-    else if (found_dowan)
-    {
-        if (monster->observable())
-        {
-            monster->add_ench(ENCH_HASTE);
-            simple_monster_message(monster, " seems to find hidden reserves of power!");
-        }
-        else
-            monster->props["dowan_upgrade"] = bool(true);
-
-        monster->spells[0] = SPELL_THROW_ICICLE;
-        monster->spells[1] = SPELL_BLINK;
-        monster->spells[3] = SPELL_STONE_ARROW;
-        monster->spells[4] = SPELL_HASTE;
-        // Nothing with 6.
-    }
-}
-
-void pikel_band_neutralise (bool check_tagged)
-{
-    bool message_made = false;
-
-    for (monster_iterator mi; mi; ++mi)
-    {
-        if (mi->type == MONS_SLAVE
-            && testbits(mi->flags, MF_BAND_MEMBER)
-            && mi->props.exists("pikel_band")
-            && mi->mname != "freed slave")
-        {
-            // Don't neutralise band members that are leaving the level with us.
-            if (check_tagged && testbits(mi->flags, MF_TAKING_STAIRS))
-                continue;
-
-            if (mi->observable() && !message_made)
-            {
-                if (check_tagged)
-                    mprf("With Pikel's spell partly broken, some of the slaves are set free!");
-                else
-                    mprf("With Pikel's spell broken, the former slaves thank you for their freedom.");
-
-                message_made = true;
-            }
-            mi->flags |= MF_NAME_DESCRIPTOR | MF_NAME_REPLACE;
-            mi->mname = "freed slave";
-            mons_pacify(*mi);
-        }
-    }
-}
-
-static void _hogs_to_humans()
-{
-    // Simplification: if, in a rare event, another hog which was not created
-    // as a part of Kirke's band happens to be on the level, the player can't
-    // tell them apart anyway.
-    // On the other hand, hogs which left the level are too far away to be
-    // affected by the magic of Kirke's death.
-    int any = 0, human = 0;
-
-    for (monster_iterator mi; mi; ++mi)
-    {
-        if (mi->type != MONS_HOG)
-            continue;
-
-        // Shapeshifters will stop being a hog when they feel like it.
-        if (mi->is_shapeshifter())
-            continue;
-
-        const bool could_see = you.can_see(*mi);
-
-        monsters orig;
-
-        if (mi->props.exists(ORIG_MONSTER_KEY))
-            // Copy it, since the instance in props will get deleted
-            // as soon a **mi is assigned to.
-            orig = mi->props[ORIG_MONSTER_KEY].get_monster();
-        else
-        {
-            orig.type     = MONS_HUMAN;
-            orig.attitude = mi->attitude;
-            define_monster(orig);
-        }
-
-        // Keep at same spot.
-        const coord_def pos = mi->pos();
-        // Preserve relative HP.
-        const float hp
-            = (float) mi->hit_points / (float) mi->max_hit_points;
-        // Preserve some flags.
-        const unsigned long preserve_flags =
-            mi->flags & ~(MF_JUST_SUMMONED | MF_WAS_IN_VIEW);
-        // Preserve enchantments.
-        mon_enchant_list enchantments = mi->enchantments;
-
-        // Restore original monster.
-        **mi = orig;
-
-        mi->set_position(pos);
-        mi->enchantments = enchantments;
-        mi->hit_points   = std::max(1, (int) (mi->max_hit_points * hp));
-        mi->flags        = mi->flags | preserve_flags;
-
-        const bool can_see = you.can_see(*mi);
-
-        // A monster changing factions while in the arena messes up
-        // arena book-keeping.
-        if (!crawl_state.arena)
-        {
-            // * A monster's attitude shouldn't downgrade from friendly
-            //   or good-neutral because you helped it.  It'd suck to
-            //   lose a permanent ally that way.
-            //
-            // * A monster has to be smart enough to realize that you
-            //   helped it.
-            if (mi->attitude == ATT_HOSTILE
-                && mons_intel(*mi) >= I_NORMAL)
-            {
-                mi->attitude = ATT_GOOD_NEUTRAL;
-                mi->flags   |= MF_WAS_NEUTRAL;
-            }
-        }
-
-        behaviour_event(*mi, ME_EVAL);
-
-        if (could_see && can_see)
-        {
-            any++;
-            if (mi->type == MONS_HUMAN)
-                human++;
-        }
-        else if (could_see && !can_see)
-            mpr("The hog vanishes!");
-        else if (!could_see && can_see)
-            mprf("%s appears from out of thin air!",
-                 mi->name(DESC_CAP_A).c_str());
-    }
-
-    if (any == 1)
-    {
-        if (any == human)
-            mpr("No longer under Kirke's spell, the hog turns into a human!");
-        else
-            mpr("No longer under Kirke's spell, the hog returns to its "
-                "original form!");
-    }
-    else if (any > 1)
-    {
-        if (any == human)
-            mpr("No longer under Kirke's spell, all hogs revert to their "
-                "human forms!");
-        else
-            mpr("No longer under Kirke's spell, all hogs revert to their "
-                "original forms!");
-    }
-
-    // Revert the player as well.
-    if (you.attribute[ATTR_TRANSFORMATION] == TRAN_PIG)
-        untransform();
-}
-
 static int _tentacle_too_far(monsters *head, monsters *tentacle)
 {
     // The Shoals produce no disjoint bodies of water.
@@ -2104,16 +1873,11 @@ int monster_die(monsters *monster, killer_type killer,
         // And his vault can  be placed again.
         you.uniq_map_names.erase("uniq_boris");
     }
-    else if (monster->type == MONS_KIRKE
-             || (monster->props.exists("original_name")
-                && monster->props["original_name"].get_string() == "Kirke")
-                   && !in_transit)
+    else if (mons_is_kirke(monster) && !in_transit)
     {
-        _hogs_to_humans();
+        hogs_to_humans();
     }
-    else if (monster->type == MONS_PIKEL
-            || (monster->props.exists("original_name")
-                && monster->props["original_name"].get_string() == "Pikel"))
+    else if (mons_is_pikel(monster))
     {
         // His slaves don't care if he's dead or not, just whether or not
         // he goes away.
@@ -2127,13 +1891,9 @@ int monster_die(monsters *monster, killer_type killer,
                 "back into the water like the carrion they now are.");
         }
     }
-    else if (((monster->type == MONS_DOWAN || monster->type == MONS_DUVESSA)
-              || (monster->props.exists("original_name")
-                 && (monster->props["original_name"].get_string() == "Dowan"
-                     || monster->props["original_name"].get_string() == "Duvessa")))
-               && mons_near(monster))
+    else if (mons_is_elven_twin(monster) && mons_near(monster))
     {
-        _elven_twin_died(monster, in_transit);
+        elven_twin_died(monster, in_transit, killer, killer_index);
     }
     else if (mons_is_mimic(monster->type))
         drop_items = false;
@@ -2143,7 +1903,7 @@ int monster_die(monsters *monster, killer_type killer,
             _mummy_curse(monster, killer, killer_index);
     }
 
-    if(monster->type == MONS_BALLISTOMYCETE)
+    if (monster->type == MONS_BALLISTOMYCETE)
     {
         activate_ballistomycetes(monster, monster->pos(),
                                  YOU_KILL(killer) || pet_kill);
