crawl/crawl

commit 917cc2df17cdfefdf2bd9743804e0da21101d467

Split out did_god_conduct and some other things to godconduct.{h,cc}.

crawl-ref/source/godconduct.h
@@ -1,0 +1,38 @@
+/*
+ * File:     godconduct.h
+ * Summary:  Stuff related to conducts.
+ */
+
+#ifndef GODCONDUCT_H
+#define GODCONDUCT_H
+
+// Calls did_god_conduct() when the object goes out of scope.
+struct god_conduct_trigger
+{
+    conduct_type conduct;
+    int pgain;
+    bool known;
+    bool enabled;
+    std::auto_ptr<monsters> victim;
+
+    god_conduct_trigger(conduct_type c = NUM_CONDUCTS,
+                        int pg = 0,
+                        bool kn = true,
+                        const monsters *vict = NULL);
+
+    void set(conduct_type c = NUM_CONDUCTS,
+             int pg = 0,
+             bool kn = true,
+             const monsters *vict = NULL);
+
+    ~god_conduct_trigger();
+};
+
+bool did_god_conduct(conduct_type thing_done, int level, bool known = true,
+                     const monsters *victim = NULL);
+void set_attack_conducts(god_conduct_trigger conduct[3], const monsters *mon,
+                         bool known = true);
+void enable_attack_conducts(god_conduct_trigger conduct[3]);
+void disable_attack_conducts(god_conduct_trigger conduct[3]);
+
+#endif

crawl-ref/source/main.cc
@@ -101,6 +101,7 @@
 #include "quiver.h"
 #include "random.h"
 #include "religion.h"
+#include "godconduct.h"
 #include "shopping.h"
 #include "skills.h"
 #include "skills2.h"

crawl-ref/source/areas.cc
@@ -22,6 +22,7 @@
 #include "monster.h"
 #include "player.h"
 #include "religion.h"
+#include "godconduct.h"
 #include "stuff.h"
 #include "terrain.h"
 #include "traps.h"

crawl-ref/source/art-func.h
@@ -25,6 +25,7 @@
 #include "effects.h"  // For Sceptre of Torment tormenting
 #include "env.h"      // For storm bow env.cgrid
 #include "food.h"     // For evokes
+#include "godconduct.h" // did_god_conduct.
 #include "mgen_data.h" // For Sceptre of Asmodeus evoke
 #include "mon-place.h" // For Sceptre of Asmodeus evoke
 #include "mon-stuff.h" // For Scythe of Curses cursing items

crawl-ref/source/beam.cc
@@ -56,6 +56,7 @@
 #include "ouch.h"
 #include "player.h"
 #include "religion.h"
+#include "godconduct.h"
 #include "skills.h"
 #include "spells1.h"
 #include "spells3.h"

crawl-ref/source/decks.cc
@@ -43,6 +43,7 @@
 #include "ouch.h"
 #include "player.h"
 #include "religion.h"
+#include "godconduct.h"
 #include "skills2.h"
 #include "spells1.h"
 #include "spells2.h"

crawl-ref/source/delay.cc
@@ -44,6 +44,7 @@
 #include "player.h"
 #include "random.h"
 #include "religion.h"
+#include "godconduct.h"
 #include "spells4.h"
 #include "spl-util.h"
 #include "stash.h"

crawl-ref/source/fight.cc
@@ -53,6 +53,7 @@
 #include "ouch.h"
 #include "player.h"
 #include "religion.h"
+#include "godconduct.h"
 #include "shopping.h"
 #include "skills.h"
 #include "spells1.h"

commit 49c9c58824338e43df290ca9372a699b7c2f784a

Fix DID_ATTACK_FRIEND not getting triggered
The DID_ATTACK_FRIEND conduct seems to have been broken by commit
917cc2df1. Some initialization code used for evaluating attack conducts
was moved out of religion_turn_start, and then never called from
anywhere else.

crawl-ref/source/godconduct.h
@@ -35,4 +35,6 @@ void set_attack_conducts(god_conduct_trigger conduct[3], const monsters *mon,
 void enable_attack_conducts(god_conduct_trigger conduct[3]);
 void disable_attack_conducts(god_conduct_trigger conduct[3]);
 
+void god_conduct_turn_start();
+
 #endif

crawl-ref/source/main.cc
@@ -889,6 +889,7 @@ static void _input()
     crawl_state.clear_mon_acting();
 
     religion_turn_start();
+    god_conduct_turn_start();
     you.update_beholders();
 
     // Currently only set if Xom accidentally kills the player.