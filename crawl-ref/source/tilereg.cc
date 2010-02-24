90b3393b4ddc2850fe97e2c2c09f3ab8794d5cdc
Johanna Ploog <j-p-e-g@users.sourceforge.net>
2010-02-24 13:41:54 +0100

    Fix the Tiles tooltip giving away the presence of invisible monsters. (cherry picked from commit 9e0ca811eba95308c39e324cb35fa5eef687ec0f)

--- crawl-ref/source/tilereg.cc
+++ crawl-ref/source/tilereg.cc
@@ -2134,7 +2134,7 @@ bool DungeonRegion::update_tip_text(std::string& tip)
         if (!cell_is_solid(m_cursor[CURSOR_MOUSE]))
         {
             const monsters *mon = monster_at(m_cursor[CURSOR_MOUSE]);
-            if (!mon || mon->friendly())
+            if (!mon || mon->friendly() || !mon->visible_to(&you))
                 tip = "[L-Click] Move\n";
             else if (mon)
             {
