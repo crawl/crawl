8e5ef4a0bcbaf3069bf22780ce37aa88ffeeb216
Enne Walker <ennewalker@users.sourceforge.net>
2010-02-23 22:34:55 -0500

    Make tiles use viewwindow for 'X' map display.
    This eliminates a duplicate function that had gotten out of sync with the
    original viewwindow.  (In particular, this fixes a bug where using X in
    the Abyss would show out of sight tiles and the presence of items.)
    (cherry picked from commit 985f4d906106e58a16ca24232ca9b746a02886c1)

--- crawl-ref/source/viewgeom.h
+++ crawl-ref/source/viewgeom.h
@@ -51,6 +51,8 @@ public:
     void set_player_at(const coord_def &c, bool force_centre = false);
     // Set new location, but preserve scrolling as if the player didn't move.
     void shift_player_to(const coord_def &c);
+    // Recalculate vlos1 and vlos2.
+    void calc_vlos();
 
     coord_def view_centre() const
     {
