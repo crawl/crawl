2daf16efa4f9b2a6ad96276c5374cd896d94d823
Robert Vollmert <rvollmert@gmx.net>
2010-02-25 16:18:21 +0100

    Describe wandering stationary monsters as "unaware" in monster list.
    Fixes issue #611.

--- crawl-ref/source/mon-info.cc
+++ crawl-ref/source/mon-info.cc
@@ -195,7 +195,7 @@ static std::string _verbose_info0(const monsters* m)
         {
             // Labeling strictly neutral monsters as fellow slimes
             // is more important.
-            return ("wandering");
+            return (mons_is_stationary(m) ? "unaware" : "wandering");
         }
         if (m->foe == MHITNOT && !mons_is_batty(m) && !m->neutral()
             && !m->friendly())
