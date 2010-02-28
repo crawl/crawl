331385dc4105a2793e4035f024aeb02105270bdd
Darshan Shaligram <dshaligram@users.sourceforge.net>
2010-03-01 01:23:22 +0530

    Revert "Fix an off-by-1 error which broke freed slave coloring."
    init_pair(64, ...) is bad news if COLOR_PAIRS=64.
    
    The original commits by sorear fix real problems, so we'll revisit these later;
    this revert is just a temporary fix for various cursor and colour issues.
    
    This reverts commit e9e87715e5725e2651ac1ae8204a7648cabffee8.

--- crawl-ref/source/libunix.cc
+++ crawl-ref/source/libunix.cc
@@ -160,7 +160,7 @@ static void setup_colour_pairs( void )
                 init_pair(i * 8 + j, j, i);
         }
 
-    init_pair(64, COLOR_BLACK, Options.background_colour);
+    init_pair(63, COLOR_BLACK, Options.background_colour);
 }
 
 #ifdef UNICODE_GLYPHS
@@ -873,7 +873,7 @@ static int curs_fg_attr(int col)
     bg &= 0x0007;
 
     // figure out which colour pair we want
-    const int pair = (fg == 0 && bg == 0) ? 64 : (bg * 8 + fg);
+    const int pair = (fg == 0 && bg == 0) ? 63 : (bg * 8 + fg);
 
     return ( COLOR_PAIR(pair) | flags );
 }
@@ -933,7 +933,7 @@ static int curs_bg_attr(int col)
     bg &= 0x0007;
 
     // figure out which colour pair we want
-    const int pair = (fg == 0 && bg == 0) ? 64 : (bg * 8 + fg);
+    const int pair = (fg == 0 && bg == 0) ? 63 : (bg * 8 + fg);
 
     return ( COLOR_PAIR(pair) | flags );
 }
@@ -978,7 +978,7 @@ static void flip_colour(cchar_t &ch)
     int fg     = pair & 7;
     int bg     = (pair >> 3) & 7;
 
-    if (pair == 64)
+    if (pair == 63)
     {
         fg    = COLOR_WHITE;
         bg    = COLOR_BLACK;
@@ -1005,7 +1005,7 @@ static void flip_colour(char_info &ch)
     int fg     = pair & 7;
     int bg     = (pair >> 3) & 7;
 
-    if (pair == 64)
+    if (pair == 63)
     {
         fg    = COLOR_WHITE;
         bg    = COLOR_BLACK;
