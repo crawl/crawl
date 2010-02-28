25538e61d4471b4771816d055aec3bfde633563e
Ixtli <cg@325i.org>
2010-03-01 02:59:00 +0900

    Removed GLState and GLStateManager classes from tilebuf.cc/h and moved them to the new glwrapper files.  Removed SDL_opengl from these files and instead have them call static members of the GLStateManager class.

--- crawl-ref/source/tilebuf.cc
+++ crawl-ref/source/tilebuf.cc
@@ -17,84 +17,6 @@
 #include "cgcontext-sdl.h"
 #endif
 
-#include <SDL_opengl.h>
-
-/////////////////////////////////////////////////////////////////////////////
-// GLState
-
-// Note: these defaults should match the OpenGL defaults
-GLState::GLState() :
-    array_vertex(false),
-    array_texcoord(false),
-    array_colour(false),
-    blend(false),
-    texture(false),
-    depthtest(false),
-    alphatest(false),
-    alpharef(0)
-{
-}
-
-/////////////////////////////////////////////////////////////////////////////
-// GLStateManager
-
-void GLStateManager::init()
-{
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glClearColor(0.0, 0.0, 0.0, 1.0f);
-    glDepthFunc(GL_LEQUAL);
-}
-
-void GLStateManager::set(const GLState& state)
-{
-    if (state.array_vertex)
-        glEnableClientState(GL_VERTEX_ARRAY);
-    else
-        glDisableClientState(GL_VERTEX_ARRAY);
-
-    if (state.array_texcoord)
-        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-    else
-        glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-
-    if (state.array_colour)
-    {
-        glEnableClientState(GL_COLOR_ARRAY);
-    }
-    else
-    {
-        glDisableClientState(GL_COLOR_ARRAY);
-
-        // [enne] This should *not* be necessary, but the Linux OpenGL
-        // driver that I'm using sets this to the last colour of the
-        // colour array.  So, we need to unset it here.
-        glColor3f(1.0f, 1.0f, 1.0f);
-    }
-
-    if (state.texture)
-        glEnable(GL_TEXTURE_2D);
-    else
-        glDisable(GL_TEXTURE_2D);
-
-    if (state.blend)
-        glEnable(GL_BLEND);
-    else
-        glDisable(GL_BLEND);
-
-    if (state.depthtest)
-        glEnable(GL_DEPTH_TEST);
-    else
-        glDisable(GL_DEPTH_TEST);
-
-    if (state.alphatest)
-    {
-        glEnable(GL_ALPHA_TEST);
-        glAlphaFunc(GL_NOTEQUAL, state.alpharef);
-    }
-    else
-        glDisable(GL_ALPHA_TEST);
-}
-
 /////////////////////////////////////////////////////////////////////////////
 // VColour
 
@@ -145,9 +67,8 @@ void VertBuffer<PTVert>::draw() const
     GLStateManager::set(m_state);
 
     m_tex->bind();
-    glVertexPointer(2, GL_FLOAT, sizeof(Vert), &(*this)[0].pos_x);
-    glTexCoordPointer(2, GL_FLOAT, sizeof(Vert), &(*this)[0].tex_x);
-    glDrawArrays(m_prim, 0, size());
+    GLStateManager::drawPTVert(sizeof(Vert), &(*this)[0].pos_x),
+        &(*this)[0].tex_x), m_prim, size());
 }
 
 template<>
@@ -169,10 +90,8 @@ void VertBuffer<PCVert>::draw() const
     ASSERT(!m_tex);
 
     GLStateManager::set(m_state);
-
-    glVertexPointer(2, GL_FLOAT, sizeof(Vert), &(*this)[0].pos_x);
-    glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(Vert), &(*this)[0].col);
-    glDrawArrays(m_prim, 0, size());
+    GLStateManager::drawPCVert(sizeof(Vert), &(*this)[0].pos_x,
+        &(*this)[0].col, m_prim, size());
 }
 
 template<>
@@ -197,10 +116,8 @@ void VertBuffer<PTCVert>::draw() const
     GLStateManager::set(m_state);
 
     m_tex->bind();
-    glVertexPointer(2, GL_FLOAT, sizeof(Vert), &(*this)[0].pos_x);
-    glTexCoordPointer(2, GL_FLOAT, sizeof(Vert), &(*this)[0].tex_x);
-    glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(Vert), &(*this)[0].col);
-    glDrawArrays(m_prim, 0, size());
+    GLStateManager::drawPTCVert(sizeof(Vert) &(*this)[0].pos_x,
+        &(*this)[0].tex_x, &(*this)[0].col, m_prim, size());
 }
 
 template<>
@@ -228,10 +145,9 @@ void VertBuffer<P3TCVert>::draw() const
     GLStateManager::set(m_state);
 
     m_tex->bind();
-    glVertexPointer(3, GL_FLOAT, sizeof(Vert), &(*this)[0].pos_x);
-    glTexCoordPointer(2, GL_FLOAT, sizeof(Vert), &(*this)[0].tex_x);
-    glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(Vert), &(*this)[0].col);
-    glDrawArrays(m_prim, 0, size());
+
+    GLStateManager::drawP3TCVert(sizeof(Vert) &(*this)[0].pos_x,
+        &(*this)[0].tex_x, &(*this)[0].col, m_prim, size());
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -550,10 +466,10 @@ void SubmergedTileBuffer::draw() const
         // z-values than this maximum, they will not draw over these
         // transparent pixels, effectively masking them out. (My kingdom for a
         // shader.)
-        glPushMatrix();
-        glTranslatef(0, 0, m_max_z + 1);
+        GLStateManager::pushMatrix();
+        GLStateManager::translatef(0, 0, m_max_z + 1);
         m_mask.draw();
-        glPopMatrix();
+        GLStateManager::popMatrix();
 
         // Now, draw all the above water tiles.  Some of these may draw over
         // top of part of the below water tiles, but that's fine as the mask
