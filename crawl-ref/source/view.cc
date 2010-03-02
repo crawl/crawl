crawl/crawl

commit 289d30469b8e1e8aa364daa6f055c1aa587c811a

Allow 'x' and mouseclick travel in the Abyss.
Two changes for unmappable areas (Abyss only, right?):
1. mark seen cells as known
2. handle currently unseen cells as UNSEEN

The first makes sure that travel will recognize cells in los, and thus
accept them as a travel target. The second makes sure that the display
behaviour is unchanged, i.e. you still don't remember cells outside
los.

crawl-ref/source/view.cc
@@ -734,7 +734,11 @@ static int player_view_update_at(const coord_def &gc)
         tutorial_observe_cell(gc);
 
     if (!player_in_mappable_area())
+    {
+        set_terrain_seen(gc);
+        set_map_knowledge_obj(gc, to_knowledge(env.show(ep)));
         return (ret);
+    }
 
     if (is_terrain_changed(gc) || !is_terrain_seen(gc))
         ret |= UF_AFFECT_EXCLUDES;
@@ -954,8 +958,11 @@ void viewwindow(bool monster_updates, bool show_updates)
         const coord_def gc = view2grid(*ri);
         const coord_def ep = view2show(grid2view(gc));
 
-        if (!map_bounds(gc))
+        if (!map_bounds(gc)
+            || !player_in_mappable_area() && !you.see_cell(gc))
+        {
             draw_unseen(&buffy[bufcount], gc);
+        }
         else if (!crawl_view.in_grid_los(gc))
             draw_outside_los(&buffy[bufcount], gc);
         else if (gc == you.pos()

crawl-ref/source/travel.cc
@@ -388,20 +388,14 @@ bool is_travelsafe_square(const coord_def& c, bool ignore_hostile)
     // If 'ignore_hostile' is true, we're ignoring hazards that can be
     // navigated over if the player is willing to take damage, or levitate.
     if (ignore_hostile && _is_reseedable(c))
-    {
         return (true);
-    }
 
     // Excluded squares are never safe.
     if (is_excluded(c))
-    {
         return (false);
-    }
 
     if (is_trap(c) && _is_safe_trap(c))
-    {
         return (true);
-    }
 
     return (feat_is_traversable(grid));
 }

commit 56dd07f6faa82c2b1f0baecb0512f0e59119cb03

Fix env.map_knowledge being filled inappropriately in the abyss.
289d3046 caused map_knowledge to be filled as elsewhere in
unmappable areas; this was then overridden when drawing outside
LOS, but is bound to cause information leak at some point, since
env.map_knowledge is assumed to contain player knowledge all
over the place.

So instead, sync env.map_knowledge to env.show by clearing it
at the start of a viewwindow call.

This may affect divination effects in unmappable areas.

crawl-ref/source/view.cc
@@ -729,13 +729,6 @@ static int player_view_update_at(const coord_def &gc)
     if (Tutorial.tutorial_left)
         tutorial_observe_cell(gc);
 
-    if (!player_in_mappable_area())
-    {
-        set_terrain_seen(gc);
-        set_map_knowledge_obj(gc, to_knowledge(env.show(ep)));
-        return (ret);
-    }
-
     if (is_terrain_changed(gc) || !is_terrain_seen(gc))
         ret |= UF_AFFECT_EXCLUDES;
 
@@ -745,6 +738,9 @@ static int player_view_update_at(const coord_def &gc)
     set_map_knowledge_detected_item(gc, false);
 
 #ifdef USE_TILE
+    if (!player_in_mappable_area())
+        return (ret); // XXX: is this necessary?
+
     // We remove any references to mcache when
     // writing to the background.
     if (Options.clean_map)
@@ -917,6 +913,9 @@ void viewwindow(bool monster_updates, bool show_updates)
 
     if (show_updates)
     {
+        if (!player_in_mappable_area())
+            env.map_knowledge.init(map_cell());
+
         you.update_los();
 
 #ifdef USE_TILE
@@ -955,11 +954,8 @@ void viewwindow(bool monster_updates, bool show_updates)
         const coord_def gc = view2grid(*ri);
         const coord_def ep = view2show(grid2view(gc));
 
-        if (!map_bounds(gc)
-            || !player_in_mappable_area() && !you.see_cell(gc))
-        {
+        if (!map_bounds(gc))
             draw_unseen(&buffy[bufcount], gc);
-        }
         else if (!crawl_view.in_grid_los(gc))
             draw_outside_los(&buffy[bufcount], gc);
         else if (gc == you.pos() && you.on_current_level