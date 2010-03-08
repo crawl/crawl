be59e0354549eac698b2bade80c63260b2d2160d
Robert Vollmert <rvollmert@gmx.net>
2010-03-08 16:10:35 +0100

    Make get_mons_glyph return real colour by default.
    This is a ugly (colour handling is in general).
    
    Fixes elementally coloured monsters showing up wrongly in the
    monster list and some other places.

--- crawl-ref/source/showsymb.h
+++ crawl-ref/source/showsymb.h
@@ -6,7 +6,7 @@
 struct glyph
 {
     unsigned ch;
-    unsigned short col;
+    unsigned short col; // XXX: real or unreal depending on context...
 };
 
 std::string glyph_to_tagstr(const glyph& g);
@@ -14,7 +14,7 @@ std::string glyph_to_tagstr(const glyph& g);
 unsigned get_feat_symbol(dungeon_feature_type feat);
 unsigned get_item_symbol(show_item_type it);
 glyph get_item_glyph(const item_def *item);
-glyph get_mons_glyph(const monsters *mons);
+glyph get_mons_glyph(const monsters *mons, bool realcol=true);
 glyph get_show_glyph(show_type object);
 
 unsigned get_screen_glyph( const coord_def &p );
