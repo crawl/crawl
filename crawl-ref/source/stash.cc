aa06484de6a8d6c3be7f046dc64b8488a2c6bb41
Robert Vollmert <rvollmert@gmx.net>
2010-03-08 10:52:13 +0100

    Revert "Add ability to ^f and set annotations for fruit"
    This reverts commit a2af13b26dbad105493aedff6277b383f3ec8a8d.
    
    Will reimplement using stash.lua.

--- crawl-ref/source/stash.cc
+++ crawl-ref/source/stash.cc
@@ -95,11 +95,6 @@ std::string stash_annotate_item(const char *s,
         text += EOL;
         text += fs.tostring(2, -2);
     }
-    if (item && is_fruit(*item))
-    {
-        text += EOL;
-        text += "fruit";
-    }
     return text;
 }
 
