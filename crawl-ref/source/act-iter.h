a5f41f3843f1deb3e1b81743509e9f00e8bdfbde
Robert Vollmert <rvollmert@gmx.net>
2010-03-21 16:14:25 +0100

    actor_iterator, a variant of monster_iterator that includes the player.

---
+++ crawl-ref/source/act-iter.h
@@ -0,0 +1,38 @@
+/*
+ * Provide a way to iterator over all actors,
+ * subject to a few common restrictions.
+ */
+
+#ifndef ACT_ITER_H
+#define ACT_ITER_H
+
+#include "mon-iter.h"
+
+class actor_iterator
+{
+public:
+    actor_iterator();
+    actor_iterator(const circle_def* circle_);
+    actor_iterator(const los_def* los_);
+    actor_iterator(const actor* act_);
+
+    operator bool() const;
+    actor* operator*() const;
+    actor* operator->() const;
+    actor_iterator& operator++();
+    actor_iterator operator++(int);
+
+protected:
+    restr_type restr;
+    const circle_def* circle;
+    const los_def* los;
+    const actor* act;
+    bool did_you;
+    monster_iterator mi;
+
+    bool valid(const actor* a) const;
+    void raw_advance();
+    void advance(bool may_stay=false);
+};
+
+#endif
