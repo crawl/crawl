1d9ce342ce0a75266c0b708fb30101130d72d5be
Robert Vollmert <rvollmert@gmx.net>
2010-03-22 13:41:32 +0100

    Fix monster silence not invalidating agrid as appropriate.

--- crawl-ref/source/mon-cast.cc
+++ crawl-ref/source/mon-cast.cc
@@ -1750,6 +1750,7 @@ void mons_cast(monsters *monster, bolt &pbolt, spell_type spell_cast,
 
     case SPELL_SILENCE:
         monster->add_ench(ENCH_SILENCE);
+        invalidate_agrid();
         mpr("Everything around you gets eerily quiet.");
         return;
 
