419cd61f82df0c210af61a6d3c7e7bf489aa5c44
Robert Vollmert <rvollmert@gmx.net>
2010-03-24 18:12:14 +0100

    Fix monsters not opening doors (issue #1151).
    There was an extra 'else'.

--- crawl-ref/source/mon-act.cc
+++ crawl-ref/source/mon-act.cc
@@ -2653,7 +2653,6 @@ static void _mons_open_door(monsters* monster, const coord_def &pos)
 
         if (you.see_cell(dc))
             was_seen = true;
-        else
 
         grd(dc) = DNGN_OPEN_DOOR;
         set_terrain_changed(dc);
