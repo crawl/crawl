963e1d1c8534a9458d176e71e25f03b9c25114f8
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-03-31 14:09:23 -0500

    Tweak wording one more time.

--- crawl-ref/source/mutation.cc
+++ crawl-ref/source/mutation.cc
@@ -896,8 +896,8 @@ mutation_def mutation_defs[] = {
        "You slip further into the darkness.",
        "You are surrounded by darkness."},
 
-      {"The dungeon appears normal; the darkness is gone.",
-       "The dungeon becomes less dark.",
+      {"Your affinity for the darkness vanishes.",
+       "Your affinity for the darkness weakens.",
        "Your affinity for the darkness weakens."},
 
       "nightstalker"
