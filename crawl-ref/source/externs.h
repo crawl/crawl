6a85c4874b0959245581c22772603a490e8dfca3
Robert Vollmert <rvollmert@gmx.net>
2010-04-01 11:42:47 +0200

    Move PlaceInfo to place-info.{cc,h}.

--- crawl-ref/source/externs.h
+++ crawl-ref/source/externs.h
@@ -597,49 +597,6 @@ private:
     bool run_grids_changed() const;
 };
 
-class PlaceInfo
-{
-public:
-    int level_type;     // enum level_area_type
-    int branch;         // enum branch_type if LEVEL_DUNGEON; otherwise -1
-
-    unsigned long num_visits;
-    unsigned long levels_seen;
-
-    unsigned long mon_kill_exp;
-    unsigned long mon_kill_exp_avail;
-    unsigned long mon_kill_num[KC_NCATEGORIES];
-
-    long turns_total;
-    long turns_explore;
-    long turns_travel;
-    long turns_interlevel;
-    long turns_resting;
-    long turns_other;
-
-    long elapsed_total;
-    long elapsed_explore;
-    long elapsed_travel;
-    long elapsed_interlevel;
-    long elapsed_resting;
-    long elapsed_other;
-
-public:
-    PlaceInfo();
-
-    bool is_global() const;
-    void make_global();
-
-    void assert_validity() const;
-
-    const std::string short_name() const;
-
-    const PlaceInfo &operator += (const PlaceInfo &other);
-    const PlaceInfo &operator -= (const PlaceInfo &other);
-    PlaceInfo operator + (const PlaceInfo &other) const;
-    PlaceInfo operator - (const PlaceInfo &other) const;
-};
-
 typedef std::vector<delay_queue_item> delay_queue_type;
 
 class monster_spells : public FixedVector<spell_type, NUM_MONSTER_SPELL_SLOTS>
