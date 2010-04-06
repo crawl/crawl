e33120e046f9bf52ae1d8814f05870e30c89d471
Robert Vollmert <rvollmert@gmx.net>
2010-04-06 16:15:12 +0200

    Remove polymorph cloud effect.
    As remarked in #1265, it doesn't make sense for the polymorph beam
    to affect both cells it passes through and the monster at the end.
    Also, cloud polymorphing seems quite nethackish.

--- crawl-ref/source/beam.cc
+++ crawl-ref/source/beam.cc
@@ -2441,31 +2441,6 @@ void bolt::affect_place_clouds()
     if (cloudidx != EMPTY_CLOUD)
     {
         cloud_type& ctype = env.cloud[cloudidx].type;
-        // Polymorph randomly changes clouds in its path
-        if (flavour == BEAM_POLYMORPH)
-        {
-            cloud_type new_type = static_cast<cloud_type>(1 + random2(8));
-
-            if (new_type == ctype)
-                return;
-
-            if (p == you.pos())
-            {
-                mprf("The %s you are in turns into %s!",
-                     cloud_name(cloudidx).c_str(), cloud_name(new_type).c_str());
-                obvious_effect = true;
-            }
-            else if (you.see_cell(p))
-            {
-                mprf("A cloud of %s turns into %s.",
-                     cloud_name(cloudidx).c_str(), cloud_name(new_type).c_str());
-                obvious_effect = true;
-            }
-
-            ctype = new_type;
-            env.cloud[cloudidx].name = "";
-            return;
-        }
 
         // fire cancelling cold & vice versa
         if ((ctype == CLOUD_COLD
