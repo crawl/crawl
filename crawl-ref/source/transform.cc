1a2e72dcfed3685f0bc0220585d0c9c24529e520
Fangorn <matt.tadd@gmail.com>
2010-04-09 07:21:43 -0700

    Fix #1320: Oz's armour in heavy armour by casting when transformed.

--- crawl-ref/source/transform.cc
+++ crawl-ref/source/transform.cc
@@ -831,6 +831,17 @@ void untransform(bool skip_wielding, bool skip_move)
             remove_one_equip(EQ_BOOTS);
     }
 
+    // End Ozocubu's Icy Armour if you unmelded wearing heavy armour
+    if (you.duration[DUR_ICY_ARMOUR]
+        && !player_effectively_in_light_armour())
+    {
+        you.duration[DUR_ICY_ARMOUR] = 1;
+
+        const item_def *armour = you.slot_item(EQ_BODY_ARMOUR, false);
+        mprf(MSGCH_DURATION, "%s cracks your icy armour.",
+             armour->name(DESC_CAP_YOUR).c_str());
+    }
+
     if (hp_downscale != 10 && you.hp != you.hp_max)
     {
         you.hp = you.hp * 10 / hp_downscale;
