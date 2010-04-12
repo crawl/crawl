7375539616192ee7972e0a66adf3c72d74480922
Adam Borowski <kilobyte@angband.pl>
2010-04-13 01:26:23 +0200

    Make the slow mutation actually work.  Make naganess work same as slow 2.
    Having a random mutation slow you all the way to naga is way too harsh.

--- crawl-ref/source/newgame.cc
+++ crawl-ref/source/newgame.cc
@@ -1505,7 +1505,7 @@ void give_basic_mutations(species_type speci)
         you.mutation[MUT_ACUTE_VISION]      = 1;
         you.mutation[MUT_POISON_RESISTANCE] = 1;
         you.mutation[MUT_DEFORMED]          = 1;
-        you.mutation[MUT_SLOW]              = 1;
+        you.mutation[MUT_SLOW]              = 2;
         break;
     case SP_MUMMY:
         you.mutation[MUT_TORMENT_RESISTANCE]         = 1;
