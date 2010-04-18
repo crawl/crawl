fc49ef72ebbf562112138b0a18659a016111977b
Ixtli <cg@325i.org>
2010-04-19 02:05:46 +0900

    term colours struct seems to have gotten misplaced.

--- crawl-ref/source/fontwrapper-ft.cc
+++ crawl-ref/source/fontwrapper-ft.cc
@@ -20,26 +20,6 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
-const VColour term_colours[MAX_TERM_COLOUR] =
-{
-    VColour(  0,   0,   0), // BLACK
-    VColour(  0,  82, 255), // BLUE
-    VColour(100, 185,  70), // GREEN
-    VColour(  0, 180, 180), // CYAN
-    VColour(255,  48,   0), // RED
-    VColour(238,  92, 238), // MAGENTA
-    VColour(165,  91,   0), // BROWN
-    VColour(162, 162, 162), // LIGHTGREY
-    VColour( 82,  82,  82), // DARKGREY
-    VColour( 82, 102, 255), // LIGHTBLUE
-    VColour( 82, 255,  82), // LIGHTGREEN
-    VColour( 82, 255, 255), // LIGHTCYAN
-    VColour(255,  82,  82), // LIGHTRED
-    VColour(255,  82, 255), // LIGHTMAGENTA
-    VColour(255, 255,  82), // YELLOW
-    VColour(255, 255, 255)  // WHITE
-};
-
 FTFontWrapper::FTFontWrapper() :
     m_glyphs(NULL),
     m_max_advance(0, 0),
--- crawl-ref/source/tilefont.cc
+++ crawl-ref/source/tilefont.cc
@@ -7,11 +7,33 @@
 #include "fontwrapper-ft.h"
 #endif
 
+const VColour term_colours[MAX_TERM_COLOUR] =
+{
+    VColour(  0,   0,   0), // BLACK
+    VColour(  0,  82, 255), // BLUE
+    VColour(100, 185,  70), // GREEN
+    VColour(  0, 180, 180), // CYAN
+    VColour(255,  48,   0), // RED
+    VColour(238,  92, 238), // MAGENTA
+    VColour(165,  91,   0), // BROWN
+    VColour(162, 162, 162), // LIGHTGREY
+    VColour( 82,  82,  82), // DARKGREY
+    VColour( 82, 102, 255), // LIGHTBLUE
+    VColour( 82, 255,  82), // LIGHTGREEN
+    VColour( 82, 255, 255), // LIGHTCYAN
+    VColour(255,  82,  82), // LIGHTRED
+    VColour(255,  82, 255), // LIGHTMAGENTA
+    VColour(255, 255,  82), // YELLOW
+    VColour(255, 255, 255)  // WHITE
+};
+
 FontWrapper* FontWrapper::create()
 {
+    FontWrapper *ret = NULL;
 #ifdef USE_FT
-    return (FontWrapper *) new FTFontWrapper();
+    ret = (FontWrapper *) new FTFontWrapper();
 #endif
+    return (ret);
 }
 
 #endif // USE_TILE
