crawl/crawl

commit 401bc10845f6d3be6c09b00b624809bc48228ffd

files.cc: fix 'tar' to use non-GNU options
This fixes the save system for BSD-like 'tar' versions (including Mac OS X).

Patch povided by Mantis user 'JoachimSchipper', in #1470.

Signed-off-by: Steven Noonan <steven@uplinklabs.net>

crawl-ref/source/files.cc
@@ -1862,7 +1862,7 @@ static void _save_game_exit()
 
 #ifdef USE_TAR
     snprintf( cmd_buff, sizeof(cmd_buff),
-              "cd %s && "SAVE_PACKAGE_CMD" -zcf %s"PACKAGE_SUFFIX" --remove-files %s.*",
+              "cd %s && FILES=%s.* && "SAVE_PACKAGE_CMD" -zcf %s"PACKAGE_SUFFIX" $FILES && rm $FILES",
               dirname.c_str(), basename.c_str(), basename.c_str());
 #else
 # ifdef USE_ZIP

commit 3091199d4a9414e692004c2ce4f62a5841ccab46

JoachimSchipper's fix to the save-eating bug introduced in 401bc108

crawl-ref/source/files.cc
@@ -134,10 +134,17 @@ static void _redraw_all(void)
 
 static std::string _uid_as_string()
 {
-#ifdef MULTIUSER
+#ifndef MULTIUSER
+    return std::string();
+#else
+#ifndef SAVE_DIR_PATH
     return make_stringf("-%d", static_cast<int>(getuid()));
 #else
-    return std::string();
+    if (SAVE_DIR_PATH[0] != '~')
+        return make_stringf("-%d", static_cast<int>(getuid()));
+    else
+        return std::string();
+#endif
 #endif
 }
 
@@ -1862,7 +1869,7 @@ static void _save_game_exit()
 
 #ifdef USE_TAR
     snprintf( cmd_buff, sizeof(cmd_buff),
-              "cd %s && FILES=%s.* && "SAVE_PACKAGE_CMD" -zcf %s"PACKAGE_SUFFIX" $FILES && rm $FILES",
+              "cd %s && FILES=`echo %s.*` && "SAVE_PACKAGE_CMD" -zcf %s"PACKAGE_SUFFIX" $FILES && rm $FILES",
               dirname.c_str(), basename.c_str(), basename.c_str());
 #else
 # ifdef USE_ZIP