576ad8ea3d4a32be5001ac0c8892bbb24b580131
Adam Borowski <kilobyte@angband.pl>
2010-05-06 10:18:49 +0200

    Remove the block for magic mapping in Pandemonium.
    Without the spell, scrolls are a sharply limited resource.

--- crawl-ref/source/item_use.cc
+++ crawl-ref/source/item_use.cc
@@ -5557,15 +5557,7 @@ void read_scroll(int slot)
         break;
 
     case SCR_MAGIC_MAPPING:
-        if (you.level_type == LEVEL_PANDEMONIUM)
-        {
-            if (!item_type_known(scroll))
-                mpr("You feel momentarily disoriented.");
-            else
-                mpr("Your Earth magic cannot map Pandemonium.");
-        }
-        else
-            magic_mapping(50, 90 + random2(11), false);
+        magic_mapping(50, 90 + random2(11), false);
         break;
 
     case SCR_TORMENT:
