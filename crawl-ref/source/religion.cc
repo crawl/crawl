21b2957e9add1352863a1afaf0d49e8b505970ad
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-05-07 10:54:59 -0500

    Add spacing fixes.

--- crawl-ref/source/religion.cc
+++ crawl-ref/source/religion.cc
@@ -2012,7 +2012,7 @@ bool do_god_gift(bool prayed_for, bool forced)
     ASSERT(you.religion != GOD_NO_GOD);
 
     // Zin and Jiyva worshippers are the only ones who can pray to ask their
-    // god for stuff. Jiyva also gives regular gifts.
+    // god for stuff.  Jiyva also gives regular gifts.
     if (prayed_for && you.religion != GOD_ZIN && you.religion != GOD_JIYVA
         || !prayed_for && you.religion == GOD_ZIN)
     {
