0af6b83606c3d9edebe5f9e9c3e8c0ef23329598
Adam Borowski <kilobyte@angband.pl>
2010-05-23 22:25:16 +0200

    Allow vampires/ghouls to evoke rods when starving, for consistency with spells.

--- crawl-ref/source/spl-book.cc
+++ crawl-ref/source/spl-book.cc
@@ -2072,7 +2072,8 @@ int staff_spell( int staff )
         food = calc_hunger(food);
     }
 
-    if (food && (you.hunger_state == HS_STARVING || you.hunger <= food))
+    if (food && (you.hunger_state == HS_STARVING || you.hunger <= food)
+        && !you.is_undead)
     {
         mpr("You don't have the energy to cast that spell.");
         crawl_state.zero_turns_taken();
