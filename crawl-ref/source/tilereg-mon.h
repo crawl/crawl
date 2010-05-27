80a4537a175fad9d300e12086cac0b555828b028
Enne Walker <enne.walker@gmail.com>
2010-05-26 22:59:28 -0400

    Merge ActorRegion and MonsterRegion.

--- crawl-ref/source/tilereg-mon.h
+++ crawl-ref/source/tilereg-mon.h
@@ -6,19 +6,30 @@
 #ifndef TILEREG_MON_H
 #define TILEREG_MON_H
 
-#include "tilereg-act.h"
+#include "mon-info.h"
+#include "tilereg-grid.h"
 
-class MonsterRegion : public ActorRegion 
+class MonsterRegion : public GridRegion 
 {
 public:
     MonsterRegion(const TileRegionInit &init);
 
     virtual void update();
+    virtual int handle_mouse(MouseEvent &event);
+    virtual bool update_tip_text(std::string &tip);
+    virtual bool update_tab_tip_text(std::string &tip, bool active);
+    virtual bool update_alt_text(std::string &alt);
 
     virtual const std::string name() const { return "Monsters"; }
 
 protected:
+    const monsters *get_monster(unsigned int idx) const;
+
+    virtual void pack_buffers();
+    virtual void draw_tag();
     virtual void activate();
+
+    std::vector<monster_info> m_mon_info;
 };
 
 #endif
