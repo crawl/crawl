cf737db339cbc7f0ed5f8501f4a74527e19deb91
Robert Vollmert <rvollmert@gmx.net>
2010-05-29 17:52:36 +0200

    Remove unimplemented player::set_arena_los from header.

--- crawl-ref/source/player.h
+++ crawl-ref/source/player.h
@@ -336,8 +336,6 @@ public:
 
     // Is c in view but behind a transparent wall?
     bool trans_wall_blocking(const coord_def &c) const;
-    // Override LOS for arena.
-    void set_arena_los(const coord_def &c);
 
     bool is_icy() const;
     bool is_fiery() const;
