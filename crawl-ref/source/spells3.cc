09057ebefcf697a613307382e2e755461790488e
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-05-30 14:36:11 -0500

    Comment fix.

--- crawl-ref/source/spells3.cc
+++ crawl-ref/source/spells3.cc
@@ -2048,7 +2048,7 @@ bool project_noise(void)
 
 // Type recalled:
 // 0 = anything
-// 1 = undead only (Kiku/Yred religion ability)
+// 1 = undead only (Yred religion ability)
 // 2 = orcs only (Beogh religion ability)
 bool recall(char type_recalled)
 {
