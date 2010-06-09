1583ebae1559aeb681d22c783f89fddaa47cb8ad
Enne Walker <enne.walker@gmail.com>
2010-06-09 13:52:18 -0400

    Fix hardcoded "classic_colours" option in tiles.
    Instead of using item colour (which is set only at item create time and
    depends on the colour options), use a random number stored with each
    item.  Through the magic of member rearranging, this new value doesn't
    take up extra space.  Also, rearrange the doll tiles so that reasonable,
    non-unrandart and non-goofy tiles are contiguous so that we can use this
    new random value to select one of them.
    
    On the upside, more tiles are now used for dolls.  Also, this is a prereq
    for a glyphs-in-tiles mode.

--- crawl-ref/source/externs.h
+++ crawl-ref/source/externs.h
@@ -491,8 +491,9 @@ struct item_def
     short          plus2;          // +to dam, sub-sub type for boots/helms
     long           special;        // special stuff
     unsigned char  colour;         // item colour
-    unsigned long  flags;          // item status flags
+    unsigned char  rnd;            // random number, used for tile choice
     short          quantity;       // number of items
+    unsigned long  flags;          // item status flags
 
     coord_def pos;     // for inventory items == (-1, -1)
     short  link;       // link to next item;  for inventory items = slot
@@ -507,7 +508,7 @@ struct item_def
 
 public:
     item_def() : base_type(OBJ_UNASSIGNED), sub_type(0), plus(0), plus2(0),
-                 special(0L), colour(0), flags(0L), quantity(0),
+                 special(0L), colour(0), rnd(0), quantity(0), flags(0L),
                  pos(), link(NON_ITEM), slot(0), orig_place(0),
                  orig_monnum(0), inscription()
     {
