24927105884eb23ec0988b940999dd6f2b2fe847
Enne Walker <enne.walker@gmail.com>
2010-06-17 09:21:55 -0400

    [1744] Fix describe view crash on neutral mon.
    The flags weren't stripped so it was using an invalid tile idx.

--- crawl-ref/source/menu.cc
+++ crawl-ref/source/menu.cc
@@ -887,9 +887,15 @@ bool MonsterMenuEntry::get_tiles(std::vector<tile_def>& tileset) const
             tileset.push_back(tile_def(job, TEX_PLAYER));
     }
     else if (mons_is_mimic(m->type))
-        tileset.push_back(tile_def(tileidx_monster(m), TEX_DEFAULT));
+    {
+        tileidx_t idx = tileidx_monster(m) & TILE_FLAG_MASK;
+        tileset.push_back(tile_def(idx, TEX_DEFAULT));
+    }
     else
-        tileset.push_back(tile_def(tileidx_monster(m), TEX_PLAYER));
+    {
+        tileidx_t idx = tileidx_monster(m) & TILE_FLAG_MASK;
+        tileset.push_back(tile_def(idx, TEX_PLAYER));
+    }
 
     // A fake monster might not have it's ghost member set up properly,
     // and mons_flies() looks at ghost.
