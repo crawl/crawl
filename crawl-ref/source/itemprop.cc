ea8bd6b95e71fe382e543534ec25e24029d0cfbd
Adam Borowski <kilobyte@angband.pl>
2010-06-17 16:23:08 +0200

    Make holy blades eudemon (without "blessed") again.  Evil ones don't get "cacodemon", though. (Yeah, those bad ugly racists somehow think any demon is evil by default, go figure...)
    Note that they appear in their base form only on Daevas, in all other cases
    you'll have a Blessed Blade instead.
    
    Only "holy scourge of holy wrath" to go!
    Death to all what is holy and pure!

--- crawl-ref/source/itemprop.cc
+++ crawl-ref/source/itemprop.cc
@@ -262,7 +262,7 @@ static weapon_def Weapon_prop[NUM_WEAPONS] =
     { WPN_DEMON_BLADE,           "demon blade",           13, -1, 15, 200,  4,
         SK_LONG_BLADES,  HANDS_ONE,    SIZE_MEDIUM, MI_NONE, false,
         DAMV_SLICING, 2 },
-    { WPN_HOLY_BLADE,            "holy blade",            14, -2, 14, 200,  4,
+    { WPN_EUDEMON_BLADE,         "eudemon blade",         14, -2, 14, 200,  4,
         SK_LONG_BLADES,  HANDS_ONE,    SIZE_MEDIUM, MI_NONE, false,
         DAMV_SLICING, 0 },
     { WPN_DOUBLE_SWORD,          "double sword",          16, -1, 15, 220,  5,
@@ -1327,7 +1327,7 @@ int weapon_rarity( int w_type )
     case WPN_BLESSED_LONG_SWORD:
     case WPN_BLESSED_SCIMITAR:
     case WPN_BLESSED_KATANA:
-    case WPN_HOLY_BLADE:
+    case WPN_EUDEMON_BLADE:
     case WPN_BLESSED_DOUBLE_SWORD:
     case WPN_BLESSED_GREAT_SWORD:
     case WPN_BLESSED_TRIPLE_SWORD:
@@ -1517,7 +1517,7 @@ bool is_blessed(const item_def &item)
         case WPN_BLESSED_LONG_SWORD:
         case WPN_BLESSED_SCIMITAR:
         case WPN_BLESSED_KATANA:
-        case WPN_HOLY_BLADE:
+        case WPN_EUDEMON_BLADE:
         case WPN_BLESSED_DOUBLE_SWORD:
         case WPN_BLESSED_GREAT_SWORD:
         case WPN_BLESSED_TRIPLE_SWORD:
@@ -1575,7 +1575,7 @@ bool convert2good(item_def &item, bool allow_blessed)
         if (!allow_blessed)
             item.sub_type = WPN_SCIMITAR;
         else
-            item.sub_type = WPN_HOLY_BLADE;
+            item.sub_type = WPN_EUDEMON_BLADE;
         break;
 
     case WPN_KATANA:
@@ -1645,7 +1645,7 @@ bool convert2bad(item_def &item)
         item.sub_type = WPN_SCIMITAR;
         break;
 
-    case WPN_HOLY_BLADE:
+    case WPN_EUDEMON_BLADE:
         item.sub_type = WPN_DEMON_BLADE;
         break;
 
