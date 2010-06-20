244cd68d098433da0a22333a159e82a16886a613
Darshan Shaligram <dshaligram@users.sourceforge.net>
2010-06-20 19:41:20 +0530

    Don't allow Fedhas mushrooms to attack plants and fungi to reach player.

--- crawl-ref/source/mon-act.cc
+++ crawl-ref/source/mon-act.cc
@@ -2811,7 +2811,7 @@ static bool _mon_can_move_to_pos(const monsters *monster,
         {
             return (true);
         }
- 
+
         if (!monster->friendly()
                 && you.see_cell(targ)
             || mon_enemies_around(monster))
@@ -3146,10 +3146,13 @@ static bool _do_move_monster(monsters *monster, const coord_def& delta)
 static bool _may_cutdown(monsters* mons, monsters* targ)
 {
     // Save friendly plants from allies.
-    bool bad_align = mons->attitude == ATT_GOOD_NEUTRAL
-                     && targ->attitude == ATT_FRIENDLY
-                  || mons->attitude == ATT_FRIENDLY
-                     && targ->attitude > ATT_HOSTILE;
+    // [ds] I'm deliberately making the alignment checks symmetric here.
+    // The previous check involved good-neutrals never attacking friendlies
+    // and friendlies never attacking anything other than hostiles.
+    const bool bad_align =
+        ((mons->friendly() || mons->good_neutral())
+         ==
+         (targ->friendly() || targ->good_neutral()));
     return (mons_is_firewood(targ) && !bad_align);
 }
 
