4b241fe7de48e7dc41970a5386f8bb484ef5435c
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-06-20 13:55:56 -0500

    Add missing parentheses.

--- crawl-ref/source/spells3.cc
+++ crawl-ref/source/spells3.cc
@@ -967,7 +967,7 @@ static bool _raise_remains(const coord_def &pos, int corps, beh_type beha,
         uint64_t name_type = 0;
         std::string name = get_corpse_name(item, &name_type);
 
-        if (name_type == 0 || name_type & MF_NAME_MASK == MF_NAME_REPLACE)
+        if (name_type == 0 || (name_type & MF_NAME_MASK) == MF_NAME_REPLACE)
             name_zombie(&menv[monster], monnum, name);
     }
 
