a358de8dd15a8d458bc0d38f2f7e1b00ee4c46dd
Darshan Shaligram <dshaligram@users.sourceforge.net>
2010-06-24 02:51:16 +0530

    Fix crash when activating a marker causes it to remove itself from marker list (Mu).

--- crawl-ref/source/mapmark.cc
+++ crawl-ref/source/mapmark.cc
@@ -646,18 +646,22 @@ void map_markers::init_from(const map_markers &c)
 
 void map_markers::activate_all(bool verbose)
 {
-    std::vector<map_marker*> to_remove;
-
     for (dgn_marker_map::iterator i = markers.begin();
-         i != markers.end(); ++i)
+         i != markers.end(); )
     {
-        i->second->activate(verbose);
-        if (i->second->property("post_activate_remove") != "")
-            to_remove.push_back(i->second);
+        map_marker *marker = i->second;
+        ++i;
+        marker->activate(verbose);
     }
 
-    for (unsigned int i = 0; i < to_remove.size(); i++)
-        remove(to_remove[i]);
+    for (dgn_marker_map::iterator i = markers.begin();
+         i != markers.end(); )
+    {
+        map_marker *marker = i->second;
+        ++i;
+        if (!marker->property("post_activate_remove").empty())
+            remove(marker);
+    }
 }
 
 void map_markers::add(map_marker *marker)
