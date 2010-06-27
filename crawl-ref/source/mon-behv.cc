3262f4d8e370c087d59b58679a009d73dda443bb
Darshan Shaligram <dshaligram@users.sourceforge.net>
2010-06-28 00:59:41 +0530

    Fix neutral monsters aggressively pursuing and attacking the player (78291).
    Also fix neutral hydras attacking the player if their primary target dies with some head attacks unused.
    (cherry picked from commit 6e325f770a1011496e94e21ac743006b3111aa83)
    
    Signed-off-by: Darshan Shaligram <dshaligram@users.sourceforge.net>

--- crawl-ref/source/mon-behv.cc
+++ crawl-ref/source/mon-behv.cc
@@ -835,6 +835,17 @@ void behaviour_event(monsters *mon, mon_event_type event, int src,
         if (mon->friendly() && mon->type == MONS_GIANT_SPORE)
             break;
 
+        // [ds] Neutral monsters don't react to your presence.
+        // XXX: Neutral monsters are a tangled mess of arbitrary logic.
+        // It's not even clear any more what behaviours are intended for
+        // neutral monsters and what are merely accidents of the code.
+        if (mon->neutral() && mon->attitude == ATT_NEUTRAL)
+        {
+            if (mon->asleep())
+                mon->behaviour = BEH_WANDER;
+            break;
+        }
+
         if (mon->asleep() && mons_near(mon))
             remove_auto_exclude(mon, true);
 
