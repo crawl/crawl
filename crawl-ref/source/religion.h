41e28e1bd8c7571b06716787e321dbebfdf1cca1
Darshan Shaligram <dshaligram@users.sourceforge.net>
2010-07-03 00:17:53 +0530

    Fix crash in get_monster_tension.

--- crawl-ref/source/religion.h
+++ crawl-ref/source/religion.h
@@ -104,7 +104,7 @@ void religion_turn_start();
 void religion_turn_end();
 
 int get_tension(god_type god = you.religion);
-int get_monster_tension(monster_iterator mons, god_type god = you.religion);
+int get_monster_tension(const monsters *mons, god_type god = you.religion);
 
 bool do_god_gift(bool prayed_for = false, bool forced = false);
 
