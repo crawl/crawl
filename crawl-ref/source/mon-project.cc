23807b569181c4d41e60b5756a539a56e17edf6e
Adam Borowski <kilobyte@angband.pl>
2010-07-03 11:56:04 +0200

    Whitespace fixes.
    This includes tabs, which are mishandled by MSVC -- and apparently at least
    one file was edited using it.

--- crawl-ref/source/mon-project.cc
+++ crawl-ref/source/mon-project.cc
@@ -274,7 +274,7 @@ move_again:
 
     if (mon.props["iood_kc"].get_byte() == KC_YOU
         && (you.pos() - pos).rdist() > LOS_RADIUS)
-    { 	// not actual vision, because of the smoke trail
+    {   // not actual vision, because of the smoke trail
         _iood_dissipate(mon);
         return (true);
     }
