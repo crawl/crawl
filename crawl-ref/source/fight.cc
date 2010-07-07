1a2035778593f1530ee309df690a3fe98382b75c
Robert Burnham <burnhamrobertp@gmail.com>
2010-07-07 15:09:09 -0500

    Adjust Spiny to-hit scalar values
    Lowers the to-hit from 6 * mutlevel to random2(7-evp)+2 * mutlevel
    (minimum 2-6, maximum 8-24) with a mean of 5-15 down from 6-18.

--- crawl-ref/source/fight.cc
+++ crawl-ref/source/fight.cc
@@ -5246,6 +5246,7 @@ void melee_attack::mons_do_eyeball_confusion()
 void melee_attack::mons_do_spines()
 {
     const item_def *body = you.slot_item(EQ_BODY_ARMOUR, false);
+    const mut = player_mutation_level(MUT_SPINY);
     int evp = 0;
     defer_rand r;
 
@@ -5256,15 +5257,15 @@ void melee_attack::mons_do_spines()
         && attacker->alive()
         && one_chance_in(evp + 1))
     {
-        if (test_melee_hit(6 * player_mutation_level(MUT_SPINY),
-                          defender->melee_evasion(attacker), r))
+        if (test_melee_hit((random2(6 - evp) + 2) * mut,
+                           defender->melee_evasion(attacker), r))
         {
             simple_monster_message(attacker->as_monster(),
                                    " dodges your spines.");
             return;
         }
 
-        int dmg = roll_dice(player_mutation_level(MUT_SPINY), 6);
+        int dmg = roll_dice(mut, 6);
         int ac = random2(1+attacker->as_monster()->armour_class());
 
         int hurt = dmg - ac - evp;
