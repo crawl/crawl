767e1b7ff872c9fd50af13890bfeda3623f019d9
Adam Borowski <kilobyte@angband.pl>
2010-07-08 16:39:09 +0200

    Cats: actually store the number of lives left.

--- crawl-ref/source/tag-version.h
+++ crawl-ref/source/tag-version.h
@@ -13,7 +13,7 @@ enum tag_minor_version
     TAG_MINOR_DA_MSTATS    = 2, // Counts of monsters affectable by dactions.
     TAG_MINOR_MAPDESC      = 3, // Save individual map descriptions.
     TAG_MINOR_SAVEVER      = 4, // Save full game version into .chr file.
-    TAG_MINOR_RESERVED     = 5, // Reserved for a secret project.
+    TAG_MINOR_CAT_LIVES    = 5, // Let cats accumulate lives.
     TAG_MINOR_NO_ROD_DISCO = 6, // Remove rods of discovery.
     TAG_MINOR_VERSION      = 6, // Current version.  (Keep equal to max.)
 };
