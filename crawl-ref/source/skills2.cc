469b2c5f608e49a3fa0a9166c5e659b7bb663d84
Adam Borowski <kilobyte@angband.pl>
2010-07-13 21:03:29 +0200

    Cats: improve Tloc apt by a bit (Mu_)

--- crawl-ref/source/skills2.cc
+++ crawl-ref/source/skills2.cc
@@ -1274,7 +1274,7 @@ static const species_skill_aptitude species_skill_aptitudes[] =
     APT(SP_CAT,             SK_ENCHANTMENTS,    2),
     APT(SP_CAT,             SK_SUMMONINGS,      1),
     APT(SP_CAT,             SK_NECROMANCY,      0),
-    APT(SP_CAT,             SK_TRANSLOCATIONS,  1),
+    APT(SP_CAT,             SK_TRANSLOCATIONS,  2),
     APT(SP_CAT,             SK_TRANSMUTATIONS,  3),
     APT(SP_CAT,             SK_FIRE_MAGIC,     -1),
     APT(SP_CAT,             SK_ICE_MAGIC,      -1),
