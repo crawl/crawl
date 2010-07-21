c0380fb6044e997730fdb525bfa1012a77a45707
Darshan Shaligram <dshaligram@users.sourceforge.net>
2010-07-21 19:17:27 +0530

    [1848] Fix Sprint starter kit not stacking with items in the character's initial inventory. (cherry picked from commit 662309f6eb16362cafff0849c6175d79ae207f5d)
    Signed-off-by: Darshan Shaligram <dshaligram@users.sourceforge.net>

--- crawl-ref/source/sprint.cc
+++ crawl-ref/source/sprint.cc
@@ -5,48 +5,18 @@
 #include "maps.h"
 #include "mon-util.h"
 #include "mpr.h"
+#include "ng-setup.h"
 #include "player.h"
 #include "random.h"
 
 void sprint_give_items()
 {
-    item_def item;
-
-    item.quantity = 1;
-    item.base_type = OBJ_POTIONS;
-    item.sub_type  = POT_HEALING;
-    const int slot2 = find_free_slot(item);
-    you.inv[slot2] = item;
-
-    item.quantity = 1;
-    item.base_type = OBJ_POTIONS;
-    item.sub_type  = POT_HEAL_WOUNDS;
-    const int slot3 = find_free_slot(item);
-    you.inv[slot3] = item;
-
-    item.quantity = 1;
-    item.base_type = OBJ_POTIONS;
-    item.sub_type  = POT_SPEED;
-    const int slot4 = find_free_slot(item);
-    you.inv[slot4] = item;
-
-    item.quantity = 1;
-    item.base_type = OBJ_SCROLLS;
-    item.sub_type  = SCR_BLINKING;
-    const int slot5 = find_free_slot(item);
-    you.inv[slot5] = item;
-
-    item.quantity = 2;
-    item.base_type = OBJ_POTIONS;
-    item.sub_type  = POT_MAGIC;
-    const int slot6 = find_free_slot(item);
-    you.inv[slot6] = item;
-
-    item.quantity = 1;
-    item.base_type = OBJ_POTIONS;
-    item.sub_type  = POT_BERSERK_RAGE;
-    const int slot7 = find_free_slot(item);
-    you.inv[slot7] = item;
+    newgame_give_item(OBJ_POTIONS, POT_HEALING);
+    newgame_give_item(OBJ_POTIONS, POT_HEAL_WOUNDS);
+    newgame_give_item(OBJ_POTIONS, POT_SPEED);
+    newgame_give_item(OBJ_POTIONS, POT_MAGIC, 2);
+    newgame_give_item(OBJ_POTIONS, POT_BERSERK_RAGE);
+    newgame_give_item(OBJ_SCROLLS, SCR_BLINKING);
 }
 
 int sprint_modify_exp(int exp)
