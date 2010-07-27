fc33de376e75fb00fd5119aa00ea1f9c1632c1e2
Eino Keskitalo <evktalo@users.sourceforge.net>
2010-07-27 15:46:22 +0300

    Implement jumping spiders for Spider's Nest.
    Once again, the tile is a placeholder.
    
    Signed-off-by: Eino Keskitalo <evktalo@users.sourceforge.net>

--- crawl-ref/source/tilepick.cc
+++ crawl-ref/source/tilepick.cc
@@ -864,6 +864,8 @@ static tileidx_t _tileidx_monster_base(int type, bool in_water, int colour,
         return TILEP_MONS_SCORPION;
     case MONS_GIANT_SCORPION:
         return TILEP_MONS_GIANT_SCORPION;
+    case MONS_JUMPING_SPIDER:
+        return TILEP_MONS_JUMPING_SPIDER;
     case MONS_WOLF_SPIDER:
         return TILEP_MONS_WOLF_SPIDER;
     case MONS_TRAPDOOR_SPIDER:
@@ -2566,6 +2568,8 @@ static tileidx_t _tileidx_corpse(const item_def &item)
         return TILE_CORPSE_SCORPION;
     case MONS_GIANT_SCORPION:
         return TILE_CORPSE_GIANT_SCORPION;
+    case MONS_JUMPING_SPIDER:
+        return TILE_CORPSE_JUMPING_SPIDER;
     case MONS_WOLF_SPIDER:
         return TILE_CORPSE_WOLF_SPIDER;
     case MONS_TRAPDOOR_SPIDER:
