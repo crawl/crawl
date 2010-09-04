71cca75722c446977da80f161216db4392dd41ba
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-09-04 13:22:23 -0500

    Properly use mons_base_type() to determine if zombified monsters fly.
    This preserves the previous fix to ensure that spectral things levitate.

--- crawl-ref/source/mon-util.cc
+++ crawl-ref/source/mon-util.cc
@@ -1335,10 +1335,7 @@ flight_type mons_flies(const monsters *mon, bool randarts)
     if (mons_is_ghost_demon(mon->type) && mon->ghost.get())
         return (mon->ghost->fly);
 
-    const int montype = mons_is_zombified(mon) ? mons_zombie_base(mon)
-                                               : mon->type;
-
-    flight_type ret = mons_class_flies(montype);
+    flight_type ret = mons_class_flies(mons_base_type(mon));
 
     // Handle the case where the zombified base monster can't fly, but
     // the zombified monster can (e.g. spectral things).
