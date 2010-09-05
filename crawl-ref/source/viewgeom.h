379767d64d39e68e9afe355c156c06debfe92253
Robert Vollmert <rvollmert@gmx.net>
2010-09-05 08:02:30 +0200

    Remove some unused coordinate tranformations.

--- crawl-ref/source/viewgeom.h
+++ crawl-ref/source/viewgeom.h
@@ -111,29 +111,9 @@ public:
 
 extern crawl_view_geometry crawl_view;
 
-inline int view2gridX(int vx)
-{
-    return (crawl_view.vgrdc.x + vx - crawl_view.view_centre().x);
-}
-
-inline int view2gridY(int vy)
-{
-    return (crawl_view.vgrdc.y + vy - crawl_view.view_centre().y);
-}
-
 inline coord_def view2grid(const coord_def &pos)
 {
     return pos - crawl_view.view_centre() + crawl_view.vgrdc;
-}
-
-inline int grid2viewX(int gx)
-{
-    return (gx - crawl_view.vgrdc.x + crawl_view.view_centre().x);
-}
-
-inline int grid2viewY(int gy)
-{
-    return (gy - crawl_view.vgrdc.y + crawl_view.view_centre().y);
 }
 
 inline coord_def grid2view(const coord_def &pos)
