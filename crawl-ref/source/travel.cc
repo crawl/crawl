d5b38212b37216abad78794a3c59c0d3e59f6f6c
Robert Vollmert <rvollmert@gmx.net>
2010-09-10 15:19:00 +0200

    Make running stop check use map_knowledge for features.
    This removes the need to worry about traps and secret doors.

--- crawl-ref/source/travel.cc
+++ crawl-ref/source/travel.cc
@@ -3901,15 +3901,11 @@ const runrest &runrest::operator = (int newrunmode)
 
 static dungeon_feature_type _base_feat_type( dungeon_feature_type grid )
 {
-    // Don't stop for undiscovered traps:
-    if ((grid >= DNGN_FLOOR_MIN && grid <= DNGN_FLOOR_MAX)
-        || grid == DNGN_UNDISCOVERED_TRAP)
-    {
+    if (grid >= DNGN_FLOOR_MIN && grid <= DNGN_FLOOR_MAX)
         return (DNGN_FLOOR);
-    }
 
-    // Merge walls and secret doors.
-    if (feat_is_wall(grid) || grid == DNGN_SECRET_DOOR)
+    // Merge walls.
+    if (feat_is_wall(grid))
         return (DNGN_ROCK_WALL);
 
     return (grid);
@@ -3919,9 +3915,8 @@ void runrest::set_run_check(int index, int dir)
 {
     run_check[index].delta = Compass[dir];
 
-    const coord_def targ = you.pos() + Compass[dir];
-
-    run_check[index].grid = _base_feat_type( grd(targ) );
+    const coord_def p = you.pos() + Compass[dir];
+    run_check[index].grid = _base_feat_type(env.map_knowledge(p).feat());
 }
 
 bool runrest::check_stop_running()
@@ -3934,8 +3929,8 @@ bool runrest::check_stop_running()
     return (false);
 }
 
-// This function creates "equivalence classes" so that undiscovered
-// traps and secret doors aren't running stopping points.
+// This function creates "equivalence classes" so that changes
+// in wall and floor type aren't running stopping points.
 bool runrest::run_grids_changed() const
 {
     if (env.cgrid(you.pos() + pos) != EMPTY_CLOUD)
@@ -3947,10 +3942,11 @@ bool runrest::run_grids_changed() const
 
     for (int i = 0; i < 3; i++)
     {
-        const coord_def targ = you.pos() + run_check[i].delta;
-        const dungeon_feature_type targ_grid = _base_feat_type(grd(targ));
+        const coord_def p = you.pos() + run_check[i].delta;
+        const dungeon_feature_type feat =
+            _base_feat_type(env.map_knowledge(p).feat());
 
-        if (run_check[i].grid != targ_grid)
+        if (run_check[i].grid != feat)
             return (true);
     }
 
