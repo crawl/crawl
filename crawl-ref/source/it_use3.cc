8caa63d639bb580a0ad5cdc1d373840fbda739aa
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-09-13 10:32:21 -0500

    Fix punctuation inconsistency.

--- crawl-ref/source/it_use3.cc
+++ crawl-ref/source/it_use3.cc
@@ -865,7 +865,7 @@ bool evoke_item(int slot)
             if (item_type_known(item)
                 && !you.is_undead && you.hunger_state == HS_STARVING)
             {
-                mpr("You're too hungry!");
+                mpr("You're too hungry.");
                 return (false);
             }
             else if (you.magic_points < you.max_magic_points
