86c531a0766742ca5d43558becad827dd919aad5
Robert Vollmert <rvollmert@gmx.net>
2010-09-15 16:15:25 +0200

    Fix passwall and teleportation, trampling. (#2582)
    Passwall is now interrupted exactly like stair delays, and
    trampling interrupts like teleportation and nets.
    
    Stair delays will now be interrupted properly when trampled,
    instead of failing because the player position changed.
    You can no longer be trampled from one stair to the next
    and ascend that instead.
    
    It's not completely clear to me that this is all implemented
    optimally, but if throwing nets pretend to be teleportation,
    surely trampling can, too.

--- crawl-ref/source/delay.cc
+++ crawl-ref/source/delay.cc
@@ -354,6 +354,14 @@ void stop_delay( bool stop_stair_travel )
         }
         break;
 
+    case DELAY_PASSWALL:
+        if (stop_stair_travel)
+        {
+            mpr("Your meditation is interrupted.");
+            _pop_delay();
+        }
+        break;
+
     case DELAY_WEAPON_SWAP:       // one turn... too much trouble
     case DELAY_DROP_ITEM:         // one turn... only used for easy armour drops
     case DELAY_JEWELLERY_ON:      // one turn
