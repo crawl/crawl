9c3a28b674c0fb766c9ee2513cfd697102a3f494
Adam Borowski <kilobyte@angband.pl>
2010-09-16 16:07:06 +0200

    Basic charset conversions: local<->utf8<->utf16.
    Their intended use is:
    * local: config files, morgues.  File names (non-Windows).
    * utf8: everything internal.  Data files (des, database).
    * utf16: (Windows only) file names and similar syscalls.
    
    On Unix systems, the "local" charset is in a vast majority of cases UTF-8,
    but we can't rely on that and have to convert anyway.

---
+++ crawl-ref/source/unicode.h
@@ -0,0 +1,27 @@
+/*
+ *  File:       unicode.h
+ *  Summary:    Conversions between Unicode and local charsets, string
+ *              manipulation functions that act on character types.
+ *  Written by: Adam Borowski
+ */
+
+typedef unsigned int ucs_t;
+
+int wctoutf8(char *d, ucs_t s);
+int utf8towc(ucs_t *d, const char *s);
+#ifdef TARGET_OS_WINDOWS
+std::wstring utf8_to_16(const char *s);
+std::string utf16_to_8(const wchar_t *s);
+#endif
+std::string utf8_to_mb(const char *s);
+std::string mb_to_utf8(const char *s);
+
+static inline std::string utf8_to_mb(const std::string &s)
+{
+    return utf8_to_mb(s.c_str());
+}
+static inline std::string mb_to_utf8(const std::string &s)
+{
+    return mb_to_utf8(s.c_str());
+}
+
