ff4bfaa76643b985f0060d537592b0ead5928578
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-09-18 12:51:19 -0500

    Typo fix.

--- crawl-ref/source/abl-show.h
+++ crawl-ref/source/abl-show.h
@@ -64,7 +64,7 @@ struct ability_def
     scaling_cost        hp_cost;        // hit point cost of ability
     unsigned int        food_cost;      // + rand2avg( food_cost, 2 )
     generic_cost        piety_cost;     // + random2( (piety_cost + 1) / 2 + 1 )
-    unsigned int        flags;          // used for additonal cost notices
+    unsigned int        flags;          // used for additional cost notices
 };
 
 struct talent
