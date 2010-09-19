27cdbd6def19c4ac6409c00fa28acb2e5315f0d1
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-09-18 23:56:30 -0500

    Remove constant parameter value from yred_drain_life().

--- crawl-ref/source/godabil.h
+++ crawl-ref/source/godabil.h
@@ -40,7 +40,7 @@ bool jiyva_remove_bad_mutation();
 bool beogh_water_walk();
 
 bool yred_injury_mirror(bool actual = true);
-void yred_drain_life(int pow);
+void yred_drain_life();
 void yred_make_enslaved_soul(monster* mon, bool force_hostile = false);
 
 bool kiku_receive_corpses(int pow, coord_def where);
