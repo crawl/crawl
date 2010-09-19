ed6c8352b3962ca5d185bc7d37b02c5f83156cc3
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-09-19 00:33:41 -0500

    Remove obsolete references to recharging and weapons of electrocution.

--- crawl-ref/source/item_use.cc
+++ crawl-ref/source/item_use.cc
@@ -4450,8 +4450,6 @@ static bool _scroll_modify_item(item_def scroll)
     case SCR_RECHARGING:
         if (item_is_rechargeable(item, false, true))
         {
-            // Might still fail on highly enchanted weapons of electrocution.
-            // (If so, already prints the "Nothing happens" message.)
             if (recharge_wand(item_slot, false))
                 return (true);
             return (false);
