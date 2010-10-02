fca7340fe04b74233c1e0cad42cd1fbb10da17cb
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-10-02 10:17:38 -0500

    Add another formatting fix.

--- crawl-ref/source/mapdef.cc
+++ crawl-ref/source/mapdef.cc
@@ -3520,7 +3520,8 @@ mons_list::mons_spec_slot mons_list::parse_mons_spec(std::string spec)
         std::string serpent_of_hell_flavour = strip_tag_prefix(mon_str, "serpent_of_hell_flavour:");
         if (serpent_of_hell_flavour.empty())
             serpent_of_hell_flavour = strip_tag_prefix(mon_str, "soh_flavour:");
-        if (!serpent_of_hell_flavour.empty()) {
+        if (!serpent_of_hell_flavour.empty())
+        {
             serpent_of_hell_flavour = upcase_first(lowercase(serpent_of_hell_flavour)).substr(0, 3);
             mspec.props["serpent_of_hell_flavour"].get_int() = str_to_branch(serpent_of_hell_flavour, BRANCH_GEHENNA);
         }
