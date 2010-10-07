98025dfdc9812864c2fd7ba672ba145bb5f12ff8
Adam Borowski <kilobyte@angband.pl>
2010-10-08 01:19:15 +0200

    Add an assertion for missing kraken connectivity data.
    It doesn't fix the bug, merely makes it slightly easier to debug by crashing
    consistently instead of doing a fandango on core.

--- crawl-ref/source/mon-abil.cc
+++ crawl-ref/source/mon-abil.cc
@@ -1503,6 +1503,7 @@ bool tentacle_pathfind(monster* tentacle,
     temp.pos = tentacle->pos();
 
     std::map<coord_def, std::set<int> >::iterator probe = attack_constraints.connection_constraints->find(temp.pos);
+    ASSERT(probe != attack_constraints.connection_constraints->end());
     temp.connect_level = 0;
     while (probe->second.find(temp.connect_level + 1 ) != probe->second.end())
         temp.connect_level++;
