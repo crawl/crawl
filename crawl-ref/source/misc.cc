89e52a0b012531e5fbb3f8f6ef221b1ffd471eb6
Adam Borowski <kilobyte@angband.pl>
2010-10-10 00:29:36 +0200

    Ashenzari: passive detection of traps and secret doors.
    It effectively bumps the skill by piety/15, but only for purposes of detection.

--- crawl-ref/source/misc.cc
+++ crawl-ref/source/misc.cc
@@ -1269,9 +1269,10 @@ void search_around(bool only_adjacent)
 {
     ASSERT(!crawl_state.game_is_arena());
 
+    const int skill = you.traps_skill();
     // Traps and doors stepdown skill:
     // skill/(2x-1) for squares at distance x
-    int max_dist = (you.skills[SK_TRAPS_DOORS] + 1) / 2;
+    int max_dist = (skill + 1) / 2;
     if (max_dist > 5)
         max_dist = 5;
     if (only_adjacent && max_dist > 1 || max_dist < 1)
@@ -1291,7 +1292,7 @@ void search_around(bool only_adjacent)
                 ++dist;
 
             // Making this harsher by removing the old +1...
-            int effective = you.skills[SK_TRAPS_DOORS] / (2*dist - 1);
+            int effective = skill / (2*dist - 1);
 
             if (grd(*ri) == DNGN_SECRET_DOOR && x_chance_in_y(effective+1, 17))
             {
