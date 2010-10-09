2192eeb00aa6122d2e794c52f0530eb8fad20668
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-10-09 10:43:26 -0500

    Add proper message for felids' Injury Mirror (cf. their altar message).

--- crawl-ref/source/abl-show.cc
+++ crawl-ref/source/abl-show.cc
@@ -1608,7 +1608,8 @@ static bool _do_ability(const ability_def& abil)
         }
 
         mprf("You %s in prayer and are bathed in unholy energy.",
-             you.species == SP_NAGA ? "coil"
+             you.species == SP_NAGA ? "coil" :
+             you.species == SP_CAT  ? "sit"
                                     : "kneel");
         you.duration[DUR_MIRROR_DAMAGE] = 9 * BASELINE_DELAY
                      + random2avg(you.piety * BASELINE_DELAY, 2) / 10;
