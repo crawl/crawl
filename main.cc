e7b820d37dcf4a8a51df39e0c92f9fb1f5e5c1b1
Adam Borowski <kilobyte@angband.pl>
2010-10-26 16:38:28 +0200

    Show the map generated.

--- main.cc
+++ main.cc
@@ -1,6 +1,32 @@
 #include "AppHdr.h"
+#include "env.h"
+
+static char dchar(dungeon_feature_type g)
+{
+    switch(g)
+    {
+    case DNGN_FLOOR:
+        return '.';
+    case DNGN_ROCK_WALL:
+        return '#';
+    default:
+        return '?';
+    }
+}
+
+void draw()
+{
+    for (int y=0; y<GYM; y++)
+    {
+        for (int x=0; x<GXM; x++)
+            printf("%c", dchar(grd[x][y]));
+        printf("\n");
+    }
+}
 
 int main()
 {
+    grd.init(DNGN_ROCK_WALL);
+    draw();
     return 0;
 }
