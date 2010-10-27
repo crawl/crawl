8d1856841d2b5529c2a94d8b4fffedc08d7d5d01
Adam Borowski <kilobyte@angband.pl>
2010-10-27 15:59:36 +0200

    Display the map in Unicode.

--- main.cc
+++ main.cc
@@ -1,7 +1,8 @@
 #include "AppHdr.h"
 #include "env.h"
+#include <locale.h>
 
-static char dchar(dungeon_feature_type g)
+static wchar_t dchar(dungeon_feature_type g)
 {
     switch(g)
     {
@@ -9,6 +10,8 @@ static char dchar(dungeon_feature_type g)
         return '.';
     case DNGN_ROCK_WALL:
         return '#';
+    case DNGN_STONE_ARCH:
+        return 0x2229;
     default:
         return '?';
     }
@@ -19,13 +22,14 @@ void draw()
     for (int y=0; y<GYM; y++)
     {
         for (int x=0; x<GXM; x++)
-            printf("%c", dchar(grd[x][y]));
+            printf("%C", dchar(grd[x][y]));
         printf("\n");
     }
 }
 
 int main()
 {
+    setlocale(LC_CTYPE, "");
     grd.init(DNGN_ROCK_WALL);
     draw();
     return 0;
