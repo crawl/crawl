56ff0e94d1aa7b8338ea690f6798ff9e336c6141
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2010-11-05 08:14:35 -0500

    Fix indentation.

--- crawl-ref/source/mon-act.cc
+++ crawl-ref/source/mon-act.cc
@@ -1139,8 +1139,8 @@ static bool _handle_rod(monster *mons, bolt &beem)
             _rod_fired_pre(mons, nice_spell);
             dprf("mon-act:_handle_rod():SPELL_SUMMON_DEMON");
             mons_cast(mons, beem, mzap, false);
-           _rod_fired_post(mons, rod, weapon, beem, 500, was_visible);
-           return true;
+            _rod_fired_post(mons, rod, weapon, beem, 500, was_visible);
+            return true;
         }
         else if (rod.plus > 300)
         {
@@ -1213,8 +1213,8 @@ static bool _handle_rod(monster *mons, bolt &beem)
 
     dprf("using rod with power %d", power);
 
-    bolt theBeam      = mons_spells(mons, mzap, power, check_validity);
-    beem = _generate_item_beem(beem, theBeam, mons);
+    bolt theBeam = mons_spells(mons, mzap, power, check_validity);
+    beem         = _generate_item_beem(beem, theBeam, mons);
 
     if (mons->confused())
     {
