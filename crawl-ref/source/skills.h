d380faca2840f38c912893ba126ea149239ba827
Raphael Langella <raphael.langella@gmail.com>
2010-11-30 23:20:44 +0100

    Removes an obsolete parameter in the exercise function
    It's not needed anymore now that reskilling doesn't use exercise.

--- crawl-ref/source/skills.h
+++ crawl-ref/source/skills.h
@@ -16,6 +16,6 @@ void check_skill_level_change(skill_type sk, bool do_level_up = true);
 void change_skill_level(skill_type exsk, int num_level);
 void change_skill_points(skill_type sk, int points, bool do_level_up);
 
-int exercise(skill_type exsk, int deg, bool change_level = true);
+int exercise(skill_type exsk, int deg);
 
 #endif
