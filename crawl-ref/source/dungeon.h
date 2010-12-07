7a8d2af585f2a0f0d80fe5cf270a8859f5d6f1ba
Vsevolod Kozlov <zaba@wybt.net>
2010-12-07 21:19:00 +0300

    Remove the 'seeded' parameter from spotty_level.
    It's always called with seeded set to false.

--- crawl-ref/source/dungeon.h
+++ crawl-ref/source/dungeon.h
@@ -303,7 +303,7 @@ struct spec_room
 
 bool join_the_dots(const coord_def &from, const coord_def &to,
                    unsigned mmask, bool early_exit = false);
-void spotty_level(bool seeded, int iterations, bool boxy);
+void spotty_level(int iterations, bool boxy);
 int process_disconnected_zones(int x1, int y1, int x2, int y2,
                                bool choose_stairless,
                                dungeon_feature_type fill);
