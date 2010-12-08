98fcf5f9a390660f63b82400af55fca795a87392
Adam Borowski <kilobyte@angband.pl>
2010-12-08 15:56:40 +0100

    A monster flag M_UNFINISHED, for non-dummies that should be never placed.
    Arena and Gretell are supposed to warn for those, too.

--- crawl-ref/source/zotdef.cc
+++ crawl-ref/source/zotdef.cc
@@ -579,6 +579,8 @@ static monster_type _get_zotdef_monster(level_id &place, int power)
             continue;        // sanity
         if (mons_class_flag(mon_type, M_NO_POLY_TO))
             continue;
+        if (mons_class_flag(mon_type, M_UNFINISHED))
+            continue;
         if (mons_is_unique(mon_type))
             continue;        // No uniques here!
         if (mons_class_is_stationary(mon_type))
