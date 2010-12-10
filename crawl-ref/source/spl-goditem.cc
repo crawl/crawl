f918a240f05c83fa3b1d9599004acd3f083705f4
Adam Borowski <kilobyte@angband.pl>
2010-12-10 02:09:09 +0100

    Fix a few cases of LOS distortion.

--- crawl-ref/source/spl-goditem.cc
+++ crawl-ref/source/spl-goditem.cc
@@ -521,14 +521,14 @@ int detect_creatures(int pow, bool telepathic)
         _fuzz_detect_creatures(pow, &fuzz_radius, &fuzz_chance);
 
     int creatures_found = 0;
-    const int map_radius = 8 + random2(8) + pow;
+    const int map_radius = 10 + random2(8) + pow;
 
     // Clear the map so detect creatures is more useful and the detection
     // fuzz is harder to analyse by averaging.
     if (!telepathic)
         clear_map(false);
 
-    for (radius_iterator ri(you.pos(), map_radius, C_SQUARE); ri; ++ri)
+    for (radius_iterator ri(you.pos(), map_radius, C_ROUND); ri; ++ri)
     {
         if (monster* mon = monster_at(*ri))
         {
