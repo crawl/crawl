50e042c5da535dd3efd915500f9205159766fce2
Adam Borowski <kilobyte@angband.pl>
2010-12-22 12:55:14 +0100

    Use a water level that ensures 60% coverage of shallow water.
    Unlike Nolithius, it seems that using a percentage for deep as well
    rather than a fixed distance below shallow might be better.

--- crawl-ref/source/dgn-perlin.cc
+++ crawl-ref/source/dgn-perlin.cc
@@ -6,15 +6,43 @@
 #include "random.h"
 #include "dgn-perlin.h"
 
+#define MAX_HEIGHT 100
+
 void layout_perlin()
 {
+    int height[GXM][GYM];
+    int buckets[MAX_HEIGHT + 1];
+    for (int i = 0; i <= MAX_HEIGHT; i++)
+        buckets[i] = 0;
+    int area = 0;
+
     int z = random2(INT_MAX); // no real time coord for now
     for (rectangle_iterator ri(1); ri; ++ri)
     {
-        double h = perlin(ri->x, ri->y, z);
-        if (h < -0.1)
+        double H = perlin(ri->x, ri->y, z);
+        int h = (H + 1) * MAX_HEIGHT / 2; // from -1..1 to 0..100 ints
+        ASSERT(h >= 0 && h <= MAX_HEIGHT);
+        height[ri->x][ri->y] = h;
+        buckets[h]++;
+        area++; // currently constant, might change later
+    }
+
+    int h0;
+    int covered = 0;
+    for (h0 = 0; h0 <= MAX_HEIGHT; h0++)
+    {
+        covered += buckets[h0];
+        if (covered >= area * 6 / 10)
+            break;
+    }
+    printf("Water level %d, covered = %d/%d\n", h0, covered, area);
+
+    for (rectangle_iterator ri(1); ri; ++ri)
+    {
+        int h = height[ri->x][ri->y];
+        if (h < h0 - 16)
             grd(*ri) = DNGN_DEEP_WATER;
-        else if (h < 0.1)
+        else if (h < h0)
             grd(*ri) = DNGN_SHALLOW_WATER;
         else
             grd(*ri) = DNGN_FLOOR;
