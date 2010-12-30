54f83993dcab57b2a8ce8fc2a127984312250064
Johanna Ploog <j-p-e-g@users.sourceforge.net>
2010-12-30 12:19:01 +0100

    New temporary option tile_allow_detached_montab.
    As long as the monster tab can cause crashes, players with a large
    enough screen to have it open all the time are at a disadvantage.
    
    Setting this option to false forces the monster tab to stay
    undetached.
    
    Should be reverted once the crashes are fixed or a more configurable
    tab system is introduced, whichever comes first.

--- crawl-ref/source/initfile.cc
+++ crawl-ref/source/initfile.cc
@@ -984,6 +984,8 @@ void game_options::reset_options()
     tile_show_minihealthbar  = true;
     tile_show_minimagicbar   = true;
     tile_show_demon_tier     = true;
+    // Temporary option until the montab crashes are fixed.
+    tile_allow_detached_montab = true;
 #endif
 
     // map each colour to itself as default
@@ -3301,6 +3303,7 @@ void game_options::read_option_line(const std::string &str, bool runscript)
     else BOOL_OPTION(tile_show_minihealthbar);
     else BOOL_OPTION(tile_show_minimagicbar);
     else BOOL_OPTION(tile_show_demon_tier);
+    else BOOL_OPTION(tile_allow_detached_montab);
     else if (key == "tile_tag_pref")
     {
         tile_tag_pref = _str_to_tag_pref(field.c_str());
