5cc8e1883ac68c2cd10c633036abaf2500937d45
Darshan Shaligram <dshaligram@users.sourceforge.net>
2011-01-10 16:28:56 +0530

    Refix global_function() call for OneWayStair onclimb.

--- crawl-ref/source/dat/clua/lm_1way.lua
+++ crawl-ref/source/dat/clua/lm_1way.lua
@@ -8,7 +8,7 @@ util.subclass(PortalDescriptor, "OneWayStair")
 function OneWayStair:new(props)
   local instance = PortalDescriptor.new(self, props)
   if instance.props and instance.props.onclimb then
-    instance.props.onclimb = global_function(instance.onclimb)
+    instance.props.onclimb = global_function(instance.props.onclimb)
   end
   return instance
 end
