1d7b44a065577c17874b110b3c27096cb76cb478
Johanna Ploog <j-p-e-g@users.sourceforge.net>
2011-01-15 00:22:03 +0100

    Don't display item/monster quotes in the tutorial.

--- crawl-ref/source/describe.h
+++ crawl-ref/source/describe.h
@@ -78,7 +78,8 @@ void describe_monsters(const monster_info &mi, bool force_seen = false,
                        bool wait_until_key_pressed = true);
 
 void get_monster_db_desc(const monster_info &mi, describe_info &inf,
-                         bool &has_stat_desc, bool force_seen = false);
+                         bool &has_stat_desc, bool force_seen = false,
+                         bool noquote = false);
 
 void get_spell_desc(const spell_type spell, describe_info &inf);
 void describe_spell(spell_type spelled, const item_def* item = NULL);
