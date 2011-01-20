347b8bda7d19387853ebbbe3b70f94eb0f22b84e
MarvintheParanoidAndroid <chriscampbell89@gmail.com>
2011-01-20 16:21:31 +0000

    Don't feed undead from slurps

--- crawl-ref/source/godpassive.cc
+++ crawl-ref/source/godpassive.cc
@@ -185,7 +185,8 @@ void jiyva_slurp_bonus(int item_value, int *js)
         return;
 
     if (you.piety >= piety_breakpoint(1)
-        && x_chance_in_y(you.piety, MAX_PIETY))
+        && x_chance_in_y(you.piety, MAX_PIETY)
+        && you.is_undead != US_UNDEAD)
     {
         //same as a sultana
         lessen_hunger(70, true);
