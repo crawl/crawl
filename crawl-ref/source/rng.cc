eefd6b21be44b035eb1c4e6949dd43b0eb175d48
Adam Borowski <kilobyte@angband.pl>
2011-01-28 19:43:38 +0100

    Use repeatable pseudo-randomness for invis monster traces.

--- crawl-ref/source/rng.cc
+++ crawl-ref/source/rng.cc
@@ -7,6 +7,7 @@
 
 #include "rng.h"
 
+#include "endianness.h"
 #include "mt19937ar.h"
 #include "syscalls.h"
 
@@ -83,7 +84,7 @@ void seed_rng()
 }
 
 // MT19937 -- see mt19937ar.cc for details
-unsigned long random_int(void)
+uint32_t random_int(void)
 {
 #ifndef MORE_HARDENED_PRNG
     return (genrand_int32());
@@ -108,4 +109,50 @@ void pop_rng_state()
 #else
     pop_sha256_state();
 #endif
+}
+
+//-----------------------------------------------------------------------------
+// MurmurHash2, by Austin Appleby
+uint32_t hash(const void *data, int len)
+{
+    // 'm' and 'r' are mixing constants generated offline.
+    // They're not really 'magic', they just happen to work well.
+    const uint32_t m = 0x5bd1e995;
+
+    // Initialize the hash to a 'random' value
+    uint32_t h = len;
+
+    const uint8_t *d = (const uint8_t*)data;
+    // Mix 4 bytes at a time into the hash
+    while(len >= 4)
+    {
+        uint32_t k = htole32(*(uint32_t *)d);
+
+        k *= m;
+        k ^= k >> 24;
+        k *= m;
+
+        h *= m;
+        h ^= k;
+
+        d += 4;
+        len -= 4;
+    }
+
+    // Handle the last few bytes of the input array
+    switch(len)
+    {
+    case 3: h ^= (uint32_t)d[2] << 16;
+    case 2: h ^= (uint32_t)d[1] << 8;
+    case 1: h ^= (uint32_t)d[0];
+            h *= m;
+    };
+
+    // Do a few final mixes of the hash to ensure the last few
+    // bytes are well-incorporated.
+    h ^= h >> 13;
+    h *= m;
+    h ^= h >> 15;
+
+    return h;
 }
