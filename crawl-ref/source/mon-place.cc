9e76db25f5619b6e1328b7b80aca6d2dfbcf6b7b
Chris Campbell <chriscampbell89@gmail.com>
2011-02-10 22:57:10 +0000

    Don't spawn monster bands on stairs
    Zotdef changed this to be possible, but it results in bands appearing
    all at once in LOS when the leader spawns on a staircase. Fixes #3102.

--- crawl-ref/source/mon-place.cc
+++ crawl-ref/source/mon-place.cc
@@ -1280,11 +1280,8 @@ int place_monster(mgen_data mg, bool force_pos)
     // Zotdef change - banding allowed on stairs for extra challenge!
     // Frequency reduced, though, and only after 2K turns.
     if (id >= MAX_MONSTERS - 30
-        || (mg.proximity == PROX_NEAR_STAIRS
-            && !crawl_state.game_is_zotdef()
-            && coinflip())
-        || (crawl_state.game_is_zotdef() && you.num_turns<2000)
-        )
+        || (mg.proximity == PROX_NEAR_STAIRS && !crawl_state.game_is_zotdef())
+        || (crawl_state.game_is_zotdef() && you.num_turns<2000))
         return (id);
 
     // Not PROX_NEAR_STAIRS, so it will be part of a band, if there is any.
