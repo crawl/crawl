87218e4b8145dbfd1308018736b724d9d4696778
Johanna Ploog <j-p-e-g@users.sourceforge.net>
2011-02-21 22:01:03 +0100

    Add corner overlays for eldritch tentacles.

--- crawl-ref/source/tiledgnbuf.cc
+++ crawl-ref/source/tiledgnbuf.cc
@@ -553,12 +553,20 @@ void DungeonCellBuffer::pack_background(int x, int y, const packed_cell &cell)
         {
             if (bg & TILE_FLAG_KRAKEN_NW)
                 m_buf_feat.add(TILE_KRAKEN_OVERLAY_NW, x, y);
+            else if (bg & TILE_FLAG_ELDRITCH_NW)
+                m_buf_feat.add(TILE_ELDRITCH_OVERLAY_NW, x, y);
             if (bg & TILE_FLAG_KRAKEN_NE)
                 m_buf_feat.add(TILE_KRAKEN_OVERLAY_NE, x, y);
+            else if (bg & TILE_FLAG_ELDRITCH_NE)
+                m_buf_feat.add(TILE_ELDRITCH_OVERLAY_NE, x, y);
             if (bg & TILE_FLAG_KRAKEN_SE)
                 m_buf_feat.add(TILE_KRAKEN_OVERLAY_SE, x, y);
+            else if (bg & TILE_FLAG_ELDRITCH_SE)
+                m_buf_feat.add(TILE_ELDRITCH_OVERLAY_SE, x, y);
             if (bg & TILE_FLAG_KRAKEN_SW)
                 m_buf_feat.add(TILE_KRAKEN_OVERLAY_SW, x, y);
+            else if (bg & TILE_FLAG_ELDRITCH_SW)
+                m_buf_feat.add(TILE_ELDRITCH_OVERLAY_SW, x, y);
         }
 
         if (cell.is_haloed)
