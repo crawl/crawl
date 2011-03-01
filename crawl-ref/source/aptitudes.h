01a2254fcae1ff3081e8281e3dce0398c3e637ed
Chris Campbell <chriscampbell89@gmail.com>
2011-03-01 04:48:41 +0000

    Lower Felid Conjurations apt to -2

--- crawl-ref/source/aptitudes.h
+++ crawl-ref/source/aptitudes.h
@@ -1152,7 +1152,7 @@ static const species_skill_aptitude species_skill_aptitudes[] =
     APT(SP_CAT,             SK_TRAPS_DOORS,     2),
     APT(SP_CAT,             SK_UNARMED_COMBAT,  0),
     APT(SP_CAT,             SK_SPELLCASTING,    0),
-    APT(SP_CAT,             SK_CONJURATIONS,   -1),
+    APT(SP_CAT,             SK_CONJURATIONS,   -2),
     APT(SP_CAT,             SK_HEXES,           3),
     APT(SP_CAT,             SK_CHARMS,          2),
     APT(SP_CAT,             SK_SUMMONINGS,      1),
