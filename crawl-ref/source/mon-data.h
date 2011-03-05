dd1fce6efaeccf2b5d1c75d5673fb8f5eb70fbf7
Adam Borowski <kilobyte@angband.pl>
2011-03-05 12:11:08 +0100

    Set the xp for vanilla orcs to 3.
    Trying to improve threat tiers code, I realized it's not their formula
    what's wrong but the xp for orcs being not representative for their
    difficulty.  They might be nearly same as hobgoblins when naked, but they
    come in packs and get morningstars and war axes rather than clubs or
    nothing.
    
    The extra xp shouldn't upset balance of early game much -- you rarely find
    them before snakes/gnolls and most of orcs can be found together with 30/100
    xp buddies.

--- crawl-ref/source/mon-data.h
+++ crawl-ref/source/mon-data.h
@@ -1429,7 +1429,7 @@ static monsterentry mondata[] = {
     MONS_ORC, 'o', LIGHTRED, "orc",
     M_WARM_BLOOD | M_SPEAKS,
     MR_NO_FLAGS,
-    600, 10, MONS_ORC, MONS_ORC, MH_NATURAL, -3,
+    600, 15, MONS_ORC, MONS_ORC, MH_NATURAL, -3,
     { {AT_HIT, AF_PLAIN, 5}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
     { 1, 4, 6, 0 },
     0, 10, MST_NO_SPELLS, CE_CONTAMINATED, Z_SMALL, S_SHOUT,
