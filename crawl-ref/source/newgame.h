838496fda784955481913b800668f5446c396a1b
Raphael Langella <raphael.langella@gmail.com>
2011-03-11 22:57:13 +0100

    New background choice layout.
    Grouped by category, as suggested on the tavern:
    https://crawl.develz.org/tavern/viewtopic.php?p=9905#p9905

--- crawl-ref/source/newgame.h
+++ crawl-ref/source/newgame.h
@@ -9,6 +9,7 @@
 #define NEWGAME_H
 
 #include "newgame_def.h"
+#include "menu.h"
 
 undead_state_type get_undead_state(const species_type sp);
 void choose_tutorial_character(newgame_def* ng_choice);
@@ -19,5 +20,21 @@ bool choose_game(newgame_def *ng, newgame_def* choice,
 void make_rod(item_def &item, stave_type rod_type, int ncharges);
 int start_to_wand(int wandtype, bool& is_rod);
 int start_to_book(int firstbook, int booktype);
+
+#define JOB_GROUP_SIZE 9
+/*
+ * A structure for grouping backgrounds by category.
+ */
+struct job_group
+{
+    const char* name;   //<! Name of the group.
+    coord_def position; //<! Relative coordinates of the title.
+    int width;          //<! Column width.
+    job_type jobs[JOB_GROUP_SIZE]; //<! List of jobs in the group.
+
+    //! A method to attach the group to a freeform.
+    void attach(const newgame_def* ng, const newgame_def& defaults,
+                MenuFreeform* menu, menu_letter &letter);
+};
 
 #endif
