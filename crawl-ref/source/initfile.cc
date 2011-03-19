crawl/crawl

commit 3620f568833ce84cf4480a00c95a934e30cc04dc

Fix butchering not interrupted when a monster comes into view.

crawl-ref/source/initfile.cc
@@ -509,7 +509,7 @@ void game_options::set_default_activity_interrupts()
         "interrupt_drop_item = interrupt_armour_on",
         "interrupt_jewellery_on = interrupt_armour_on",
         "interrupt_memorise = interrupt_armour_on, stat",
-        "interrupt_butcher = interrupt_armour_on, teleport, stat",
+        "interrupt_butcher = interrupt_armour_on, teleport, stat, monster",
         "interrupt_bottle_blood = interrupt_butcher",
         "interrupt_vampire_feed = interrupt_butcher",
         "interrupt_multidrop = interrupt_butcher",

commit 505bd8adf1cd4d58bade98d69e64d5408cb55185

Fix multidrop with monsters in sight.
Bug #3618, introduced by 3620f568.

crawl-ref/source/initfile.cc
@@ -512,10 +512,10 @@ void game_options::set_default_activity_interrupts()
         "interrupt_butcher = interrupt_armour_on, teleport, stat, monster",
         "interrupt_bottle_blood = interrupt_butcher",
         "interrupt_vampire_feed = interrupt_butcher",
-        "interrupt_multidrop = interrupt_butcher",
+        "interrupt_multidrop = interrupt_armour_on, teleport, stat",
         "interrupt_macro = interrupt_multidrop",
         "interrupt_travel = interrupt_butcher, statue, hungry, "
-                            "burden, monster, hit_monster",
+                            "burden, hit_monster",
         "interrupt_run = interrupt_travel, message",
         "interrupt_rest = interrupt_run, full_hp, full_mp",
 