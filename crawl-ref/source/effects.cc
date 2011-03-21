447b19a5d10d9cda34b4abdf05b8673c9c190bae
William Parker <thewparker@gmail.com>
2011-03-21 16:04:52 -0700

    Differentiate hell effect miscasts from miscellaneous miscasts

--- crawl-ref/source/effects.cc
+++ crawl-ref/source/effects.cc
@@ -1325,7 +1325,7 @@ static void _hell_effects()
         else                // 1 in 8 odds {dlb}
             which_miscast = coinflip() ? SPTYP_HEXES : SPTYP_CHARMS;
 
-        MiscastEffect(&you, MISC_MISCAST, which_miscast,
+        MiscastEffect(&you, HELL_EFFECT_MISCAST, which_miscast,
                       4 + random2(6), random2avg(97, 3),
                       "the effects of Hell");
     }
@@ -1381,7 +1381,7 @@ static void _hell_effects()
         }
         else
         {
-            MiscastEffect(&you, MISC_MISCAST, which_miscast,
+            MiscastEffect(&you, HELL_EFFECT_MISCAST, which_miscast,
                           4 + random2(6), random2avg(97, 3),
                           "the effects of Hell");
         }
