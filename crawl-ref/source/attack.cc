36dc4a416cb00f552d30a4ecbb42308731ba3230
Robert Burnham <burnhamrobertp@gmail.com>
2011-03-21 16:11:18 -0500

    Additional development of attack.cc, reording of class functions in attack.h
    Implements more of non-overloaded class functions of attack.cc and sets up
    attack::init_attack to do basic initialization before the derived class calls
    its init_attack.

--- crawl-ref/source/attack.cc
+++ crawl-ref/source/attack.cc
@@ -18,6 +18,10 @@
 #include "enum.h"
 #include "itemprop.h"
 
+const int HIT_WEAK   = 7;
+const int HIT_MED    = 18;
+const int HIT_STRONG = 36;
+
 /*
  **************************************************
  *             BEGIN PUBLIC FUNCTIONS             *
@@ -41,31 +45,32 @@ attack::attack(actor *attk, actor *defn, bool allow_unarmed)
     // of attack actually being made (melee, ranged, etc.)
 }
 
+/* Returns an actor's name
+ *
+ * Takes into account actor visibility/invisibility and the type of description
+ * to be used (capitalization, possessiveness, etc.)
+ */
 std::string attack::actor_name(const actor *a, description_level_type desc,
                                bool actor_visible, bool actor_invisible)
 {
     return (actor_visible ? a->name(desc) : anon_name(desc, actor_invisible));
 }
 
+/* Returns an actor's pronoun
+ *
+ * Takes into account actor visibility
+ */
 std::string attack::pronoun(const actor *a, pronoun_type pron, 
                             bool actor_visible)
 {
     return (actor_visible ? a->pronoun(pron) : anon_pronoun(pron));
 }
 
-std::string attack::anon_pronoun(pronoun_type pron)
-{
-    switch (pron)
-    {
-    default:
-    case PRONOUN_CAP:              return "It";
-    case PRONOUN_NOCAP:            return "it";
-    case PRONOUN_CAP_POSSESSIVE:   return "Its";
-    case PRONOUN_NOCAP_POSSESSIVE: return "its";
-    case PRONOUN_REFLEXIVE:        return "itself";
-    }
-}
-
+/* Returns an anonymous actor's name
+ *
+ * Given the actor visible or invisible, returns the
+ * appropriate possessive, capitalized pronoun.
+ */
 std::string attack::anon_name(description_level_type desc,
                                     bool actor_invisible)
 {
@@ -89,16 +94,121 @@ std::string attack::anon_name(description_level_type desc,
     }
 }
 
+/* Returns an anonymous actor's pronoun
+ *
+ * Given invisibility (whether out of LOS or just invisible), returns the
+ * appropriate possessive, inflexive, capitalized pronoun.
+ */
+std::string attack::anon_pronoun(pronoun_type pron)
+{
+    switch (pron)
+    {
+    default:
+    case PRONOUN_CAP:              return "It";
+    case PRONOUN_NOCAP:            return "it";
+    case PRONOUN_CAP_POSSESSIVE:   return "Its";
+    case PRONOUN_NOCAP_POSSESSIVE: return "its";
+    case PRONOUN_REFLEXIVE:        return "itself";
+    }
+}
+
+/* Initializes an attack, setting up base variables and values
+ *
+ * Does not make any changes to any actors, items, or the environment,
+ * in case the attack is canceled or otherwise fails. Only initializations
+ * that are universal to all types of attacks should go into this method,
+ * any initialization properties that are specific to one attack or another
+ * should go into their respective init_attack.
+ *
+ * Although this method will get overloaded by the derived class, we are
+ * calling it from attack::attack(), before the overloading has taken place.
+ */
+void attack::init_attack()
+{
+    mprf("attack::init_attack()");
+}
+
+/* If debug, return formatted damage done
+ *
+ */
+std::string attack::debug_damage_number()
+{
+#ifdef DEBUG_DIAGNOSTICS
+    return make_stringf(" for %d", damage_done);
+#else
+    return ("");
+#endif
+}
+
+/* Returns special punctuation
+ *
+ * Used (mostly) for elemental or branded attacks (napalm, dragon slaying, orc
+ * slaying, holy, etc.)
+ */
+std::string attack::special_attack_punctuation()
+{
+    if (special_damage < 6)
+        return ".";
+    else
+        return "!";
+}
+
+/* Returns standard attack punctuation
+ *
+ * Used in player / monster (both primary and aux) attacks
+ */
+std::string attack::attack_strength_punctuation()
+{
+    if (attacker->atype() == ACT_PLAYER)
+    {
+        if (damage_done < HIT_WEAK)
+            return ".";
+        else if (damage_done < HIT_MED)
+            return "!";
+        else if (damage_done < HIT_STRONG)
+            return "!!";
+        else
+            return "!!!";
+    }
+    else
+        return (damage_done < HIT_WEAK ? "." : "!");
+}
+
+/* Returns evasion adverb
+ *
+ */
+std::string attack::evasion_margin_adverb()
+{
+    return (ev_margin <= -20) ? " completely" :
+           (ev_margin <= -12) ? "" :
+           (ev_margin <= -6)  ? " closely"
+                              : " barely";
+}
+
+/* Returns the attacker's name
+ *
+ * Helper method to easily access the attacker's name
+ */
 std::string attack::atk_name(description_level_type desc) const
 {
     return actor_name(attacker, desc, attacker_visible, attacker_invisible);
 }
 
+/* Returns the defender's name
+ *
+ * Helper method to easily access the defender's name
+ */
 std::string attack::def_name(description_level_type desc) const
 {
     return actor_name(defender, desc, defender_visible, defender_invisible);
 }
 
+/* Returns the attacking weapon's name
+ *
+ * Sets upthe appropriate descriptive level and obtains the name of a weapon
+ * based on if the attacker is a player or non-player (non-players use a 
+ * plain name and a manually entered pronoun)
+ */
 std::string attack::wep_name(description_level_type desc,
                              iflags_t ignore_flags) const
 {
@@ -126,4 +236,28 @@ std::string attack::wep_name(description_level_type desc,
     name += weapon->name(DESC_PLAIN, false, false, false, false, ignore_flags);
 
     return (name);
+}
+
+/* Calculates special damage, prints appropriate combat text
+ *
+ * Applies a particular damage brand to the current attack, the setup and
+ * calculation of base damage and other effects varies based on the type
+ * of attack, but the calculation of elemental damage should be consistent.
+ */
+void attack::calc_elemental_brand_damage(beam_type flavour,
+                                                int res,
+                                                const char *verb)
+{
+    special_damage = resist_adjust_damage(defender, flavour, res,
+                                          random2(damage_done) / 2 + 1);
+
+    if (needs_message && special_damage > 0 && verb)
+    {
+        special_damage_message = make_stringf(
+            "%s %s %s%s",
+            atk_name(DESC_CAP_THE).c_str(),
+            attacker->conj_verb(verb).c_str(),
+            mons_defender_name().c_str(),
+            special_attack_punctuation().c_str());
+    }
 }
