crawl/crawl

commit 9a18e06c7f22a25a7d73cb05c88634f980ddc6b6

Fix on-sight identification messing up with autopickup.
Mantis #3655.

crawl-ref/source/items.cc
@@ -1728,7 +1728,11 @@ int move_item_to_player(int obj, int quant_got, bool quiet,
     if (!item.slot)
         item.slot = index_to_letter(item.link);
 
-    ash_id_item(item);
+    if (item.props.exists("needs_autopickup"))
+        item.props.erase("needs_autopickup");
+    else
+        ash_id_item(item);
+
     note_inscribe_item(item);
 
     item.quantity = quant_got;
@@ -2467,6 +2471,9 @@ bool item_needs_autopickup(const item_def &item)
 {
     if (item_is_stationary(item))
         return (false);
+
+    if (item.props.exists("needs_autopickup"))
+        return (true);
 
     if (item.inscription.find("=g") != std::string::npos)
         return (true);

crawl-ref/source/godpassive.cc
@@ -369,6 +369,9 @@ bool ash_id_item(item_def& item, bool silent)
     if (you.religion != GOD_ASHENZARI)
         return false;
 
+    if (item_needs_autopickup(item))
+        item.props["needs_autopickup"] = true;
+
     iflags_t old_ided = item.flags & ISFLAG_IDENT_MASK;
     iflags_t ided = ISFLAG_KNOW_CURSE;
 

commit 1b6ac9e46356be088c3a333b10dbfe42547d5fe5

Fix autopickup of stacks under Ashenzari.
Bug introduced by 9a18e06c7. Mantis #3667.

crawl-ref/source/items.cc
@@ -1520,6 +1520,9 @@ static void _got_item(item_def& item, int quant)
     seen_item(item);
     shopping_list.cull_identical_items(item);
 
+    if (item.props.exists("needs_autopickup"))
+        item.props.erase("needs_autopickup");
+
     if (!item_is_rune(item))
         return;
 
@@ -1728,10 +1731,7 @@ int move_item_to_player(int obj, int quant_got, bool quiet,
     if (!item.slot)
         item.slot = index_to_letter(item.link);
 
-    if (item.props.exists("needs_autopickup"))
-        item.props.erase("needs_autopickup");
-    else
-        ash_id_item(item);
+    ash_id_item(item);
 
     note_inscribe_item(item);
 