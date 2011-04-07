202185651e2cdbeb515d7218f0e4419e231f5ba2
Eronarn Palazzo <eronarn@gmail.com>
2011-04-07 17:26:17 -0400

    Lava orcs can swim in lava unless it is flagged as dangerous to them.
    Being immersed in lava maxes temperature and prevents any decline.

--- crawl-ref/source/species.h
+++ crawl-ref/source/species.h
@@ -12,6 +12,7 @@ enum genus_type
 genus_type species_genus(species_type species);
 int species_has_claws(species_type species, bool mut_level = false);
 bool species_likes_water(species_type species);
+bool species_likes_lava(species_type species);
 size_type species_size(species_type species,
                        size_part_type psize = PSIZE_TORSO);
 
