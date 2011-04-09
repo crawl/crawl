86f4b0ab26025ad62c5754f2a434732ef3a871f4
Chris Campbell <chriscampbell89@gmail.com>
2011-04-09 21:58:27 +0100

    Rename some casting/aptitude adjectives for clarity
    Also removed the aptitude conversion, since it's been changed for two
    versions now. It's not really relevant or useful information for a
    help screen.

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -1978,7 +1978,7 @@ const char* failure_rate_to_string(int fail)
 {
     return (fail == 100) ? "Useless"   : // 0% success chance
            (fail > 77)   ? "Terrible"  : // 0-5%
-           (fail > 59)   ? "Bad" :       // 5-30%
+           (fail > 59)   ? "Very Poor" : // 5-30%
            (fail > 50)   ? "Poor"      : // 30-50%
            (fail > 40)   ? "Fair"      : // 50-70%
            (fail > 35)   ? "Good"      : // 70-80%
