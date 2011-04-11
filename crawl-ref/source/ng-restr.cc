278f978b45f49804b2bca561756d098a34e656fa
Chris Campbell <chriscampbell89@gmail.com>
2011-04-11 06:16:51 +0100

    Don't suggest AM when choosing random/viable characters (cherry picked from commit 07159ce1c851071f054a5738398bd1066b09eb95)

--- crawl-ref/source/ng-restr.cc
+++ crawl-ref/source/ng-restr.cc
@@ -562,29 +562,9 @@ char_choice_restriction job_allowed(species_type speci, job_type job)
                 return (CC_UNRESTRICTED);
         }
 
+// XXX: Arcane Marksmen are temporarily disabled
         case JOB_ARCANE_MARKSMAN:
-            switch (speci)
-        {
-            case SP_CAT:
-                return (CC_BANNED);
-            case SP_SLUDGE_ELF:
-            case SP_HILL_ORC:
-            case SP_MERFOLK:
-            case SP_SPRIGGAN:
-            case SP_NAGA:
-            case SP_OGRE:
-            case SP_TROLL:
-            case SP_MINOTAUR:
-            case SP_KENKU:
-            case SP_BASE_DRACONIAN:
-            case SP_DEMIGOD:
-            case SP_MUMMY:
-            case SP_GHOUL:
-            case SP_VAMPIRE:
-                return (CC_RESTRICTED);
-            default:
-                return (CC_UNRESTRICTED);
-        }
+            return (CC_BANNED);
 
         case JOB_WANDERER:
             return (CC_RESTRICTED);
