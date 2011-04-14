b48264784fccd2d062f68008473b2f104819312c
Johanna Ploog <j-p-e-g@users.sourceforge.net>
2011-04-14 22:56:46 +0200

    Fix #781: handle throwing nets correctly in the Shoals tides.
    First, there a 10% chance of a net simply being washed away (as in,
    the item is moved by the tide even if the trapped critter isn't).
    If this happens, clean up the net properly.
    
    Second, whenever a monster (or the player) that happens to be caught
    in a net is shifted by the tides, move the net with it and *don't*
    clear the trapping status.
    (cherry picked from commit 6c2e1adc9375f3e35bad96cb5a89be0d90a22229)

--- crawl-ref/source/player.h
+++ crawl-ref/source/player.h
@@ -357,8 +357,8 @@ public:
     // Set player position without updating view geometry.
     void set_position(const coord_def &c);
     // Low-level move the player. Use this instead of changing pos directly.
-    void moveto(const coord_def &c);
-    bool move_to_pos(const coord_def &c);
+    void moveto(const coord_def &c, bool clear_net = true);
+    bool move_to_pos(const coord_def &c, bool clear_net = true);
     // Move the player during an abyss shift.
     void shiftto(const coord_def &c);
     bool blink_to(const coord_def& c, bool quiet = false);
