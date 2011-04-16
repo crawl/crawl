4ecdec6d30af8d5f3ef744555933d3e356418e8b
Adam Borowski <kilobyte@angband.pl>
2011-04-16 20:45:12 +0200

    Fix mute spellcasters being able to speak.

--- crawl-ref/source/monster.cc
+++ crawl-ref/source/monster.cc
@@ -679,6 +679,9 @@ bool monster::can_throw_large_rocks() const
 
 bool monster::can_speak()
 {
+    if (has_ench(ENCH_MUTE))
+        return (false);
+
     // Priest and wizard monsters can always speak.
     if (is_priest() || is_actual_spellcaster())
         return (true);
@@ -689,9 +692,6 @@ bool monster::can_speak()
     {
         return (false);
     }
-
-    if (has_ench(ENCH_MUTE))
-        return (false);
 
     // Does it have the proper vocal equipment?
     const mon_body_shape shape = get_mon_shape(this);
