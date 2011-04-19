0e2bb6d9728efb619b13e00a6aca9f66b26c3265
Adam Borowski <kilobyte@angband.pl>
2011-04-19 22:09:25 +0200

    Don't acquire books of Minor Magic II and III.

--- crawl-ref/source/acquire.cc
+++ crawl-ref/source/acquire.cc
@@ -1006,6 +1006,13 @@ static bool _do_book_acquirement(item_def &book, int agent)
                 weights[bk] = 0;
                 continue;
             }
+#if TAG_MAJOR_VERSION == 32
+            if (bk == BOOK_MINOR_MAGIC_II || bk == BOOK_MINOR_MAGIC_III)
+            {
+                weights[bk] = 0;
+                continue;
+            }
+#endif
             weights[bk]    = _book_weight(static_cast<book_type>(bk));
             total_weights += weights[bk];
         }
