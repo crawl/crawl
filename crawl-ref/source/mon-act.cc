crawl/crawl

commit 4e5e0e8f02bf2001870c91763d9aad92f62831f9

Fix some clinging checks.
This should make monster clinging more reliable and fix #3830.

crawl-ref/source/mon-act.cc
@@ -2382,7 +2382,6 @@ void handle_monster_move(monster* mons)
         }
         you.update_beholder(mons);
         you.update_fearmonger(mons);
-        mons->check_clinging(true);
 
         // Reevaluate behaviour, since the monster's surroundings have
         // changed (it may have moved, or died for that matter).  Don't
@@ -3519,6 +3518,7 @@ static bool _do_move_monster(monster* mons, const coord_def& delta)
 
     mgrd(mons->pos()) = mons->mindex();
 
+    mons->check_clinging(true);
     ballisto_on_move(mons, old_pos);
 
     mons->check_redraw(mons->pos() - delta);

crawl-ref/source/mon-stuff.cc
@@ -3919,8 +3919,8 @@ int mons_natural_regen_rate(monster* mons)
 void mons_check_pool(monster* mons, const coord_def &oldpos,
                      killer_type killer, int killnum)
 {
-    // Levitating/flying monsters don't make contact with the terrain.
-    if (mons->airborne() || mons->can_cling_to(oldpos))
+    // Levitating/flying/clinging monsters don't make contact with the terrain.
+    if (!mons->ground_level())
         return;
 
     dungeon_feature_type grid = grd(mons->pos());

commit 050547b9b17d6bb99a61947d6b59cd845e46b4cf

Fix clinging status not updated when monster doesn't move.
Bug introduced by 4e5e0e8.

crawl-ref/source/mon-act.cc
@@ -2378,7 +2378,10 @@ void handle_monster_move(monster* mons)
             }
 
             if (mons->cannot_move() || !_monster_move(mons))
+            {
                 mons->speed_increment -= non_move_energy;
+                mons->check_clinging(false);
+            }
         }
         you.update_beholder(mons);
         you.update_fearmonger(mons);