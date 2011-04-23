a6bf85661ea5ff4f1359bf69db99955d7ac2b958
Chris Campbell <chriscampbell89@gmail.com>
2011-04-23 23:14:00 +0100

    Make weapon and armour unrandarts a bit rarer

--- crawl-ref/source/makeitem.cc
+++ crawl-ref/source/makeitem.cc
@@ -942,8 +942,8 @@ static bool _try_make_weapon_artefact(item_def& item, int force_type,
     {
         // Make a randart or unrandart.
 
-        // 1 in 12 randarts are unrandarts.
-        if (one_chance_in(item_level == MAKE_GOOD_ITEM ? 7 : 12)
+        // 1 in 20 randarts are unrandarts.
+        if (one_chance_in(item_level == MAKE_GOOD_ITEM ? 7 : 20)
             && !force_randart)
         {
             if (_try_make_item_unrand(item, force_type))
@@ -2069,8 +2069,8 @@ static bool _try_make_armour_artefact(item_def& item, int force_type,
     {
         // Make a randart or unrandart.
 
-        // 1 in 12 randarts are unrandarts.
-        if (one_chance_in(item_level == MAKE_GOOD_ITEM ? 7 : 12)
+        // 1 in 20 randarts are unrandarts.
+        if (one_chance_in(item_level == MAKE_GOOD_ITEM ? 7 : 20)
             && !force_randart)
         {
             if (_try_make_item_unrand(item, force_type))
