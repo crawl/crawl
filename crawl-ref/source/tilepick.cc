6ed58c66e97dae90f7d559befacbeca88a4a0059
Adam Borowski <kilobyte@angband.pl>
2011-04-30 11:52:25 +0200

    Use the hill giant tile for detected giants.
    (It's only the Dowsing card and Xom casting Detect Monsters left, right?)

--- crawl-ref/source/tilepick.cc
+++ crawl-ref/source/tilepick.cc
@@ -1189,6 +1189,7 @@ static tileidx_t _tileidx_monster_base(int type, bool in_water, int colour,
         return TILEP_MONS_BORING_BEETLE;
 
     // cyclopes and giants ('C')
+    case MONS_GIANT:
     case MONS_HILL_GIANT:
         return TILEP_MONS_HILL_GIANT;
     case MONS_ETTIN:
