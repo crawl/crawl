5b7d5a2b4998effbb8114f67c48b621c5b9868ff
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2011-05-02 10:31:59 -0500

    Disallow artefact holy wrath weapons with Ashenzari's name on them.
    The holy wrath brand repels curses, so this seems appropriate.

--- crawl-ref/source/artefact.cc
+++ crawl-ref/source/artefact.cc
@@ -220,6 +220,12 @@ static bool _god_fits_artefact(const god_type which_god, const item_def &item,
         }
         break;
 
+    case GOD_ASHENZARI:
+        // Cursed god: no holy wrath (since that brand repels curses).
+        if (brand == SPWPN_HOLY_WRATH)
+            return (false);
+        break;
+
     default:
         break;
     }
