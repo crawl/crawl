6beab857ba32a017fe9a937ef54a3f21d0296459
Florian Diebold <flodiebold@gmail.com>
2011-05-22 14:01:59 +0200

    Send differences in text areas per-line.

--- crawl-ref/source/tileweb-text.h
+++ crawl-ref/source/tileweb-text.h
@@ -24,6 +24,9 @@ protected:
     ucs_t   *m_cbuf; // Character buffer
     uint8_t *m_abuf; // Color buffer
 
+    ucs_t   *m_old_cbuf;
+    uint8_t *m_old_abuf;
+
     std::string m_client_side_name;
 
     bool m_dirty;
