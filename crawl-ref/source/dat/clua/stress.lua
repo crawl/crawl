b455029a728bfaadba35dd10df90aba98067b738
Adam Borowski <kilobyte@angband.pl>
2011-06-02 19:10:12 +0200

    A show of fireworks!
    To use:
    ./crawl -rc test/eyecandy/fireworks.rc
    (wizard mode must be compiled in, preferably no debug)

--- crawl-ref/source/dat/clua/stress.lua
+++ crawl-ref/source/dat/clua/stress.lua
@@ -19,3 +19,20 @@ function stress.entomb()
     dgn.terrain_changed(p.x, p.y, "metal_wall", false, false, false)
   end
 end
+
+function stress.boost_monster_hp()
+  local gxm, gym = dgn.max_bounds()
+  for p in iter.rect_iterator(dgn.point(1, 1), dgn.point(gxm-2, gym-2)) do
+    local mons = dgn.mons_at(p.x, p.y)
+    if mons ~= nil then
+      mons.set_max_hp(10000)
+    end
+  end
+end
+
+function stress.fill_level(x)
+  local gxm, gym = dgn.max_bounds()
+  for p in iter.rect_iterator(dgn.point(1, 1), dgn.point(gxm-2, gym-2)) do
+    dgn.terrain_changed(p.x, p.y, x, false, false, false)
+  end
+end
