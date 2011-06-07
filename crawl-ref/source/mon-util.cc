5037c73e30b3bd5c4fce869643474868b61b4a00
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2011-06-06 23:31:13 -0500

    Fix naming of the zombified Serpent of Hell.

--- crawl-ref/source/mon-util.cc
+++ crawl-ref/source/mon-util.cc
@@ -1288,6 +1288,14 @@ bool name_zombie(monster* mon, int mc, const std::string mon_name)
         mon->mname = "Lernaean";
         mon->flags |= MF_NAME_ADJECTIVE;
     }
+    // Also for the Serpent of Hell: treat Serpent of Hell as an
+    // adjective to avoid mentions of "the Serpent of Hell the dragon
+    // zombie".
+    else if (mc == MONS_SERPENT_OF_HELL)
+    {
+        mon->mname = "Serpent of Hell";
+        mon->flags |= MF_NAME_ADJECTIVE;
+    }
 
     if (starts_with(mon->mname, "shaped "))
         mon->flags |= MF_NAME_SUFFIX;
