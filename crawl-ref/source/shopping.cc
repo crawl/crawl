f88fa49f5e892439a01e7d6bbcd20370415bbd61
David Ploog <dploog@users.sourceforge.net>
2011-07-02 23:40:38 +0200

    Price reform: scrolls.
    Reasoning as for potions. Scroll of blinking is now at 55 (was 30).

--- crawl-ref/source/shopping.cc
+++ crawl-ref/source/shopping.cc
@@ -1692,53 +1692,38 @@ unsigned int item_value(item_def item, bool ident)
                 valued += 75;
                 break;
 
+            case SCR_RECHARGING:
+            case SCR_AMNESIA:
+            case SCR_ENCHANT_ARMOUR:
+            case SCR_ENCHANT_WEAPON_I:
             case SCR_ENCHANT_WEAPON_II:
+            case SCR_BLINKING:
                 valued += 55;
                 break;
 
-            case SCR_RECHARGING:
-            case SCR_AMNESIA:
-                valued += 50;
-                break;
-
-            case SCR_ENCHANT_ARMOUR:
-            case SCR_ENCHANT_WEAPON_I:
-                valued += 48;
-                break;
-
             case SCR_FEAR:
-                valued += 45;
-                break;
-
             case SCR_MAGIC_MAPPING:
                 valued += 35;
                 break;
 
-            case SCR_BLINKING:
             case SCR_REMOVE_CURSE:
             case SCR_TELEPORTATION:
                 valued += 30;
                 break;
 
+            case SCR_FOG:
             case SCR_DETECT_CURSE:
             case SCR_IDENTIFY:
+            case SCR_CURSE_ARMOUR:
+            case SCR_CURSE_WEAPON:
+            case SCR_CURSE_JEWELLERY:
                 valued += 20;
-                break;
-
-            case SCR_FOG:
-                valued += 10;
                 break;
 
             case SCR_NOISE:
             case SCR_RANDOM_USELESSNESS:
-                valued += 2;
-                break;
-
-            case SCR_CURSE_ARMOUR:
-            case SCR_CURSE_WEAPON:
-            case SCR_CURSE_JEWELLERY:
             case SCR_IMMOLATION:
-                valued++;
+                valued += 10;
                 break;
             }
         }
