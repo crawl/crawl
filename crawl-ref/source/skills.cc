4863d6100844312e5f1944c008ce5920c155be3e
Raphael Langella <raphael.langella@gmail.com>
2011-07-17 10:53:13 +0200

    Fix free XP and inifinite loop at max skill_cost_level (oops).

--- crawl-ref/source/skills.cc
+++ crawl-ref/source/skills.cc
@@ -553,11 +553,16 @@ void train_skills()
     do
     {
         cost = calc_skill_cost(you.skill_cost_level);
-        // Amount of skill points needed to reach the next skill cost level
-        // divided by 10 (integer divison rounded up).
-        const int next_level = (skill_cost_needed(you.skill_cost_level + 1)
-                                - you.total_skill_points + 9) / 10;
-    train_skills(std::min(you.exp_available, cost * next_level), cost);
+        if (you.skill_cost_level == 27)
+            train_skills(you.exp_available, cost);
+        else
+        {
+            // Amount of skill points needed to reach the next skill cost level
+            // divided by 10 (integer divison rounded up).
+            const int next_level = (skill_cost_needed(you.skill_cost_level + 1)
+                                    - you.total_skill_points + 9) / 10;
+            train_skills(std::min(you.exp_available, cost * next_level), cost);
+        }
     }
     while (you.exp_available >= cost);
 
