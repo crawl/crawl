9343556beebde874895769e1a4d04c871a1b194d
Brendan Hickey <brendan@bhickey.net>
2011-07-18 19:01:26 +0100

    Felid's can't use sureblade.

--- crawl-ref/source/spl-book.cc
+++ crawl-ref/source/spl-book.cc
@@ -588,6 +588,7 @@ bool you_cannot_memorise(spell_type spell, bool &undead)
          || spell == SPELL_EXCRUCIATING_WOUNDS
          || spell == SPELL_POISON_WEAPON
          || spell == SPELL_MAXWELLS_SILVER_HAMMER
+         || spell == SPELL_SURE_BLADE
          // could be useful if it didn't require wielding
          || spell == SPELL_TUKIMAS_DANCE))
     {
