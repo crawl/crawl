d8fa4257be1381d75e5eb36513078464f6410e1e
Chris Campbell <chriscampbell89@gmail.com>
2011-07-18 16:45:41 +0100

    Remove an out-of-date reference to the ring of charms

--- crawl-ref/source/mon-util.cc
+++ crawl-ref/source/mon-util.cc
@@ -2673,8 +2673,8 @@ void mons_pacify(monster* mon, mon_attitude_type att)
 {
     // If the _real_ (non-charmed) attitude is already that or better,
     // don't degrade it.  This can happen, for example, with a high-power
-    // Crusade card or the Ring of Charms on Pikel's slaves who would then
-    // go down from friendly to good_neutral when you kill Pikel.
+    // Crusade card on Pikel's slaves who would then go down from friendly
+    // to good_neutral when you kill Pikel.
     if (mon->attitude >= att)
         return;
 
