crawl/crawl

commit 294db2cc0f0053d87eb9ef87ba149f5f7e617e22

Allow searching by equipment slot.
The keywords are:
cloak helmet gloves boots shield body

crawl-ref/source/l_item.cc
@@ -325,6 +325,8 @@ IDEF(subtype)
                 else
                     s = "spellbook";
             }
+            else if (item->base_type == OBJ_ARMOUR)
+                s = item_slot_name(get_armour_slot(*item), true).c_str();
 
             if (s)
                 lua_pushstring(ls, s);

crawl-ref/source/dat/lua/stash.lua
@@ -63,7 +63,12 @@ function ch_stash_search_annotate_item(it)
   end
 
   if ch_annotate_item_class then
-    annot = annot .. "{" .. it.class(true) .. "}"
+    if it.class(true) == "armour" then
+        annot = annot .. "{" .. it.subtype .. " "
+    else
+        annot = annot .. "{"
+    end
+    annot = annot .. it.class(true) .. "}"
   end
 
   return annot

crawl-ref/source/invent.cc
@@ -1010,6 +1010,36 @@ std::string item_class_name(int type, bool terse)
     return ("");
 }
 
+std::string item_slot_name(equipment_type type, bool terse)
+{
+    if (terse)
+    {
+        switch (type)
+        {
+        case EQ_CLOAK:       return "cloak";
+        case EQ_HELMET:      return "helmet";
+        case EQ_GLOVES:      return "gloves";
+        case EQ_BOOTS:       return "boots";
+        case EQ_SHIELD:      return "shield";
+        case EQ_BODY_ARMOUR: return "body";
+        default:             return "";
+        }
+    }
+    else
+    {
+        switch (type)
+        {
+        case EQ_CLOAK:       return "Cloak";
+        case EQ_HELMET:      return "Helmet";
+        case EQ_GLOVES:      return "Gloves";
+        case EQ_BOOTS:       return "Boots";
+        case EQ_SHIELD:      return "Shield";
+        case EQ_BODY_ARMOUR: return "Body";
+        default:             return "";
+        }
+    }
+}
+
 std::vector<SelItem> select_items(const std::vector<const item_def*> &items,
                                    const char *title, bool noselect,
                                    menu_type mtype)

crawl-ref/source/invent.h
@@ -238,6 +238,7 @@ unsigned char get_invent(int invent_type);
 bool in_inventory(const item_def &i);
 
 std::string item_class_name(int type, bool terse = false);
+std::string item_slot_name(equipment_type type, bool terse);
 
 bool check_old_item_warning(const item_def& item, operation_types oper);
 bool check_warning_inscriptions(const item_def& item, operation_types oper);

crawl-ref/source/stash.cc
@@ -81,7 +81,7 @@ std::string userdef_annotate_item(const char *s, const item_def *item,
 
 std::string stash_annotate_item(const char *s,
                                 const item_def *item,
-                                bool exclusive = false)
+                                bool exclusive)
 {
     std::string text = userdef_annotate_item(s, item, exclusive);
 

crawl-ref/source/stash.h
@@ -427,6 +427,9 @@ std::vector<item_def> item_list_in_stash(const coord_def& pos);
 
 std::string userdef_annotate_item(const char *s, const item_def *item,
                                   bool exclusive = false);
+std::string stash_annotate_item(const char *s,
+                                const item_def *item,
+                                bool exclusive = false);
 
 #define STASH_LUA_SEARCH_ANNOTATE "ch_stash_search_annotate_item"
 #define STASH_LUA_DUMP_ANNOTATE   "ch_stash_dump_annotate_item"

commit b0596111c427aed307616cfe30dcf3362d8c932c

Fix lua bugs with unidentied armour.
Bug introduced by 294db2cc.

crawl-ref/source/l_item.cc
@@ -287,9 +287,11 @@ IDEF(subtype)
 {
     if (item)
     {
+        const char *s = NULL;
+        if (item->base_type == OBJ_ARMOUR)
+            s = item_slot_name(get_armour_slot(*item), true).c_str();
         if (item_type_known(*item))
         {
-            const char *s = NULL;
             if (item->base_type == OBJ_JEWELLERY)
             {
                 if (jewellery_is_amulet(*item))
@@ -325,17 +327,15 @@ IDEF(subtype)
                 else
                     s = "spellbook";
             }
-            else if (item->base_type == OBJ_ARMOUR)
-                s = item_slot_name(get_armour_slot(*item), true).c_str();
-
-            if (s)
-                lua_pushstring(ls, s);
-            else
-                lua_pushnil(ls);
-
-            lua_pushnumber(ls, item->sub_type);
-            return (2);
         }
+
+        if (s)
+            lua_pushstring(ls, s);
+        else
+            lua_pushnil(ls);
+
+        lua_pushnumber(ls, item->sub_type);
+        return (2);
     }
 
     lua_pushnil(ls);