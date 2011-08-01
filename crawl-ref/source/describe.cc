9bc750afe684e1dcf7d41ec34b9cfbd008560d7f
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2011-08-01 08:31:19 -0500

    For unrandarts with no description, display the item base type description.
    This is so e.g. the armour of the Dragon King gets the description of a
    gold dragon armour.

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -1781,6 +1781,8 @@ std::string get_item_description(const item_def &item, bool verbose,
     {
         description << "\n\n";
 
+        bool need_base_desc = false;
+
         if (dump)
         {
             description << "["
@@ -1798,8 +1800,11 @@ std::string get_item_description(const item_def &item, bool verbose,
                 description << desc_id << "\n";
             else if (desc[0] != '\0')
                 description << desc << "\n";
+            else
+                need_base_desc = true;
         }
-        else
+
+        if (need_base_desc)
         {
             std::string db_name = item.name(DESC_DBNAME, true, false, false);
             std::string db_desc = getLongDescription(db_name);
