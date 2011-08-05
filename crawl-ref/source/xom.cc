c45eb9c56a47758e3ca76debb59340a01c74fe4f
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2011-08-04 22:19:20 -0500

    Don't let Xom try to brand OBJ_STAVES-type staves with chaos.
    They don't have brands in the same way, so the attempt will fail.

--- crawl-ref/source/xom.cc
+++ crawl-ref/source/xom.cc
@@ -910,6 +910,11 @@ static bool _is_chaos_upgradeable(const item_def &item,
     if (is_unrandom_artefact(item))
        return (false);
 
+    // Staves can't be changed either, since they don't have brands in
+    // the way other weapons do.
+    if (item.base_type == OBJ_STAVES)
+       return (false);
+
     // Only upgrade permanent items, since the player should get a
     // chance to use the item if he or she can defeat the monster.
     if (item.flags & ISFLAG_SUMMONED)
