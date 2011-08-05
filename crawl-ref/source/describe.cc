crawl/crawl

commit 8fe5221d8772789b91ada56716a9e6c74d04bcaa

For unrandarts with no description, display the item base type description.
This is so e.g. the armour of the Dragon King gets the description of a
gold dragon armour.

crawl-ref/source/describe.cc
@@ -1784,6 +1784,8 @@ std::string get_item_description(const item_def &item, bool verbose,
     {
         description << "\n\n";
 
+        bool need_base_desc = false;
+
         if (dump)
         {
             description << "["
@@ -1801,8 +1803,11 @@ std::string get_item_description(const item_def &item, bool verbose,
                 description << desc_id << "\n";
             else if (desc[0] != '\0')
                 description << desc << "\n";
+            else
+                need_base_desc = true;
         }
-        else
+
+        if (need_base_desc)
         {
             std::string db_name = item.name(DESC_DBNAME, true, false, false);
             std::string db_desc = getLongDescription(db_name);

commit 4bbafb596857550da960494eeb0a7b7cff8b9ca3

Fix Mantis 4358 (regression caused by 8fe5221).
Display base type descriptions properly again for items that don't have
dumped descriptions and aren't unrandarts without descriptions.  The
fallthrough case for them had inverted logic.

crawl-ref/source/describe.cc
@@ -1784,13 +1784,14 @@ std::string get_item_description(const item_def &item, bool verbose,
     {
         description << "\n\n";
 
-        bool need_base_desc = false;
+        bool need_base_desc = true;
 
         if (dump)
         {
             description << "["
                         << item.name(DESC_DBNAME, true, false, false)
                         << "]";
+            need_base_desc = false;
         }
         else if (is_unrandom_artefact(item)
                  && (unrandart_descrip(0, item)[0] != '\0'
@@ -1800,11 +1801,15 @@ std::string get_item_description(const item_def &item, bool verbose,
             const char *desc_id = unrandart_descrip(1, item);
 
             if (item_type_known(item) && desc_id[0] != '\0')
+            {
                 description << desc_id << "\n";
+                need_base_desc = false;
+            }
             else if (desc[0] != '\0')
+            {
                 description << desc << "\n";
-            else
-                need_base_desc = true;
+                need_base_desc = false;
+            }
         }
 
         if (need_base_desc)