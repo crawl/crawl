94df9e9f65df7ff02f40d0ca9375016d3ba7b584
Raphael Langella <raphael.langella@gmail.com>
2011-08-13 02:00:04 +0200

    Don't move stairs on traps (#4110).

--- crawl-ref/source/terrain.cc
+++ crawl-ref/source/terrain.cc
@@ -1346,6 +1346,9 @@ static bool _ok_dest_cell(const actor* orig_actor,
     if (is_notable_terrain(dest_feat))
         return (false);
 
+    if (find_trap(dest_pos))
+        return (false);
+
     actor* dest_actor = actor_at(dest_pos);
 
     if (orig_actor && !orig_actor->is_habitable_feat(dest_feat))
