b0518eedd5ba78dcb67b74e06189706e32fc7b78
Chris Campbell <chriscampbell89@gmail.com>
2011-08-21 21:42:19 +0100

    Don't duplicate banished uniques when they follow you out of the abyss
    Abyssed monsters were able to be cloned by banishing them, finding them
    in the abyss and then having them follow you out of the abyss through
    a gate - they'd appear back in the dungeon with you, but also still
    be spawned in the abyss next time you visited.
    (cherry picked from commit 4112403894c761970cc674376bd3237f305e8887)

--- crawl-ref/source/abyss.cc
+++ crawl-ref/source/abyss.cc
@@ -1116,8 +1116,11 @@ void maybe_shift_abyss_around_player()
 void save_abyss_uniques()
 {
     for (monster_iterator mi; mi; ++mi)
-        if (mi->needs_abyss_transit())
+        if (mi->needs_abyss_transit()
+            && !testbits(mi->flags, MF_TAKING_STAIRS))
+        {
             mi->set_transit(level_id(LEVEL_ABYSS));
+        }
 }
 
 #ifdef NEW_ABYSS
