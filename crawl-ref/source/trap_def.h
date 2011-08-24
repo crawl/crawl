892b19d972141ee9421ea703c82044520f360754
Raphael Langella <raphael.langella@gmail.com>
2011-08-24 14:56:21 +0000

    Clear traps from stash tracking and map knowledge when they are destroyed.
    Only if the player is aware of the destruction, but it's the case most of the
    time. Fixes #4429.

--- crawl-ref/source/trap_def.h
+++ crawl-ref/source/trap_def.h
@@ -13,7 +13,7 @@ struct trap_def
     bool is_safe(actor* act = 0) const;
     void trigger(actor& triggerer, bool flat_footed = false);
     void disarm();
-    void destroy();
+    void destroy(bool known = true);
     void hide();
     void reveal();
     void prepare_ammo();
