310f501f2099bede2387215b43f6335c14598781
Adam Borowski <kilobyte@angband.pl>
2011-09-11 13:51:48 +0200

    Assert out if a character would start with an useless skill.

--- crawl-ref/source/skills.cc
+++ crawl-ref/source/skills.cc
@@ -120,6 +120,8 @@ void reassess_starting_skills()
         skill_type sk = static_cast<skill_type>(i);
         if (you.skills[sk] == 0)
             continue;
+        ASSERT(!is_invalid_skill(sk));
+        ASSERT(!is_useless_skill(sk));
 
         // Grant the amount of skill points required for a human.
         you.skill_points[sk] = skill_exp_needed(you.skills[sk], sk,
