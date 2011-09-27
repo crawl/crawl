033c30651fc409888bac6644fdbdbf4b665a7999
Adam Borowski <kilobyte@angband.pl>
2011-09-27 03:08:44 +0200

    Ettins on giant Zig levels.

--- crawl-ref/source/dat/dlua/ziggurat.lua
+++ crawl-ref/source/dat/dlua/ziggurat.lua
@@ -342,7 +342,7 @@ mset(with_props("place:Slime:$", { jelly_protect = true }),
      with_props("place:Tar:$", { weight = 5 }),
      with_props("daeva / angel / cherub / pearl dragon / shedu band / ophan / apis / holy swine", { weight = 2 }),
      with_props("hill giant / cyclops / stone giant / fire giant / " ..
-                "frost giant / titan", { weight = 2 }),
+                "frost giant / ettin / titan", { weight = 2 }),
      with_props("fire elemental / fire drake / hell hound / efreet / " ..
                 "dragon / fire giant / orb of fire", { weight = 2 }),
      with_props("ice beast / polar bear / freezing wraith / ice dragon / " ..
