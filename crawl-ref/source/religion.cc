696e3484f018e9842cf39b5f403fbe9fc477b085
Adam Borowski <kilobyte@angband.pl>
2011-10-03 12:20:55 +0200

    Give TSO piety for seeing new monsters, decay over time.
    The theme explanation is that you're supposed to diligently search for
    traces of evil among inhabitants of the world.  This is a form of piety for
    exploration, but compared to Ashenzari, it is slightly harder to scum by
    wandering in areas known to be uninhabited.
    
    Actually, it resembles piety for killing since that's what you're going to
    do anyway :p

--- crawl-ref/source/religion.cc
+++ crawl-ref/source/religion.cc
@@ -583,6 +583,12 @@ std::string get_god_likes(god_type which_god, bool verbose)
         likes.push_back(info);
         break;
 
+    case GOD_SHINING_ONE:
+        snprintf(info, INFO_SIZE, "you meet creatures to determine whether "
+                                  "they need to be eraticated");
+        likes.push_back(info);
+        break;
+
     default:
         break;
     }
@@ -3763,15 +3769,6 @@ bool god_protects_from_harm()
     return false;
 }
 
-// Returns true if the player can use the good gods' passive piety gain.
-static bool _need_free_piety()
-{
-    return (!crawl_state.game_is_sprint()
-            && (you.piety < 150
-                || you.gift_timeout
-                || you.penance[you.religion]));
-}
-
 //jmf: moved stuff from effects::handle_time()
 void handle_god_time()
 {
@@ -3814,9 +3811,9 @@ void handle_god_time()
             return;
 
         case GOD_SHINING_ONE:
-            if (_need_free_piety())
-                gain_piety(1, 15);
-            return;
+            if (one_chance_in(35))
+                lose_piety(1);
+            break;
 
         case GOD_JIYVA:
             if (one_chance_in(20))
