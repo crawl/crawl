d2dbe0ea19d1b5a906975b2f901140fd7653e9c6
Adam Borowski <kilobyte@angband.pl>
2011-10-07 14:24:38 +0200

    Rename "Light Beam" to "Holy Light" to match the BEAM and to disambiguate vs Sunray.

--- crawl-ref/source/spl-zap.cc
+++ crawl-ref/source/spl-zap.cc
@@ -101,8 +101,8 @@ zap_type spell_to_zap(spell_type spell)
         return ZAP_INVISIBILITY;
     case SPELL_DIG:
         return ZAP_DIGGING;
-    case SPELL_LIGHT_BEAM:
-        return ZAP_LIGHT_BEAM;
+    case SPELL_HOLY_LIGHT:
+        return ZAP_HOLY_LIGHT;
     case SPELL_DEBUGGING_RAY:
         return ZAP_DEBUGGING_RAY;
     default:
