f7c60d75c4d5ae714db19bd5f3a2265c2428d215
Chris Campbell <chriscampbell89@gmail.com>
2011-10-07 20:47:37 +0100

    Better messages for mummies drowning, tweak lava messages

--- crawl-ref/source/terrain.cc
+++ crawl-ref/source/terrain.cc
@@ -1488,25 +1488,15 @@ bool fall_into_a_pool(const coord_def& entry, bool allow_shift,
             int damage = 10 + roll_dice(2, 50) / resist;
 
             if (damage > 100)
-            {
                 mpr("The lava scorches you!!");
-            }
             else if (damage > 70)
-            {
                 mpr("The lava roasts you!!");
-            }
             else if (damage > 40)
-            {
                 mpr("The lava burns you!!");
-            }
             else if (damage < 20)
-            {
                 mpr("The lava scalds you!");
-            }
             else
-            {
-                mpr("The lava burns you!");
-            }
+                mpr("The lava sears you!");
 
             ouch(damage, NON_MONSTER, KILLED_BY_LAVA);
         }
@@ -1562,7 +1552,15 @@ bool fall_into_a_pool(const coord_def& entry, bool allow_shift,
         }
     }
 
-    mpr("You drown...");
+    if (you.species == SP_MUMMY)
+    {
+        if (terrain == DNGN_LAVA)
+            mpr("You burn to ash...");
+        else if (terrain == DNGN_DEEP_WATER)
+            mpr("You fall apart...");
+    }
+    else
+        mpr("You drown...");
 
     if (terrain == DNGN_LAVA)
         ouch(INSTANT_DEATH, NON_MONSTER, KILLED_BY_LAVA);
