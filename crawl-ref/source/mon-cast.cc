f8dc48686196b0871c941a65399dbded9f341565
Chris Campbell <chriscampbell89@gmail.com>
2011-10-16 19:15:01 +0100

    Don't let monsters cast Regen while under DDoor

--- crawl-ref/source/mon-cast.cc
+++ crawl-ref/source/mon-cast.cc
@@ -4318,7 +4318,7 @@ bool ms_waste_of_time(const monster* mon, spell_type monspell)
         break;
 
     case SPELL_REGENERATION:
-        if (mon->has_ench(ENCH_REGENERATION))
+        if (mon->has_ench(ENCH_REGENERATION) || mon->has_ench(ENCH_DEATHS_DOOR))
             ret = true;
         break;
 
