c833c63e1eceb1180d968c1ceb186a0f30a815fc
Adam Borowski <kilobyte@angband.pl>
2011-10-21 15:47:47 +0200

    Purge Lua's dgn.persist on a new game.
    It included stuff like Slime:6 status, etc.

--- crawl-ref/source/dat/dlua/dungeon.lua
+++ crawl-ref/source/dat/dlua/dungeon.lua
@@ -44,6 +44,10 @@ dgn_init_hook_tables(dgn.MAP_GLOBAL_HOOKS)
 -- Table that will be saved in <foo>.sav.
 dgn.persist = { }
 
+function dgn_clear_data()
+  dgn.persist = { }
+end
+
 function dgn_save_data(th)
   lmark.marshall_table(th, dgn.persist)
 end
