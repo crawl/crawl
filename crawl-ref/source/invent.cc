f3f871e130f0e2482eb8bd76ab8210ea8edb2420
Chris Campbell <chriscampbell89@gmail.com>
2011-10-22 18:05:57 +0100

    Don't display weight toggle on inventory screens, it's shown on the help page instead

--- crawl-ref/source/invent.cc
+++ crawl-ref/source/invent.cc
@@ -427,7 +427,7 @@ void InvMenu::set_title(const std::string &s)
             (you.burden * 100) / cap,
             inv_count());
 
-        std::string prompt = "_: help; Ctrl-W: toggle weight display";
+        std::string prompt = "_: help";
         stitle = stitle + std::string(std::max(0, get_number_of_cols()
                                                   - strwidth(stitle)
                                                   - strwidth(prompt)),
