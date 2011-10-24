3bbaa539a973d39b969bd5d45209daae4f52d6ce
Adam Borowski <kilobyte@angband.pl>
2011-10-25 01:29:52 +0200

    Move the one-vault wonder gnome from 'q' to 'g'.
    About as related to boggarts than qwarves.

--- crawl-ref/source/mon-data.h
+++ crawl-ref/source/mon-data.h
@@ -936,6 +936,18 @@ static monsterentry mondata[] = {
     MONUSE_WEAPONS_ARMOUR, MONEAT_NOTHING, SIZE_LITTLE
 },
 
+{ // one vault.  Polymorph disabled.
+    MONS_GNOME, 'g', LIGHTBLUE, "gnome",
+    M_WARM_BLOOD | M_SPEAKS | M_NO_POLY_TO,
+    MR_NO_FLAGS,
+    400, 10, MONS_GNOME, MONS_GNOME, MH_NATURAL, -3,
+    { {AT_HIT, AF_PLAIN, 10}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
+    { 5, 3, 5, 0 },
+    2, 12, MST_NO_SPELLS, CE_CONTAMINATED, Z_NOZOMBIE, S_SHOUT,
+    I_NORMAL, HT_LAND, FL_NONE, 10, DEFAULT_ENERGY,
+    MONUSE_WEAPONS_ARMOUR, MONEAT_NOTHING, SIZE_SMALL
+},
+
 // hounds and hogs ('h')
 {
     MONS_JACKAL, 'h', BROWN, "jackal",
@@ -1791,18 +1803,6 @@ static monsterentry mondata[] = {
     2, 10, MST_NO_SPELLS, CE_NOCORPSE, Z_NOZOMBIE, S_SHOUT,
     I_HIGH, HT_LAND, FL_NONE, 10, DEFAULT_ENERGY,
     MONUSE_WEAPONS_ARMOUR, MONEAT_NOTHING, SIZE_MEDIUM
-},
-
-{ // one vault.  Polymorph disabled.
-    MONS_GNOME, 'q', LIGHTBLUE, "gnome",
-    M_WARM_BLOOD | M_SPEAKS | M_NO_POLY_TO,
-    MR_NO_FLAGS,
-    400, 10, MONS_GNOME, MONS_GNOME, MH_NATURAL, -3,
-    { {AT_HIT, AF_PLAIN, 10}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
-    { 5, 3, 5, 0 },
-    2, 12, MST_NO_SPELLS, CE_CONTAMINATED, Z_NOZOMBIE, S_SHOUT,
-    I_NORMAL, HT_LAND, FL_NONE, 10, DEFAULT_ENERGY,
-    MONUSE_WEAPONS_ARMOUR, MONEAT_NOTHING, SIZE_SMALL
 },
 
 // rodents ('r')
