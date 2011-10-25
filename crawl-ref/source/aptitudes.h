477dc572746f3fc9f1d7f47a7ec2cd6d3e85020e
Chris Campbell <chriscampbell89@gmail.com>
2011-10-25 19:01:24 +0100

    Boost Kenku mp, and Hill Orc spellcasting, base int and mp slightly

--- crawl-ref/source/aptitudes.h
+++ crawl-ref/source/aptitudes.h
@@ -243,7 +243,7 @@ static const species_skill_aptitude species_skill_aptitudes[] =
     APT(SP_HILL_ORC,        SK_SHIELDS,         1),
     APT(SP_HILL_ORC,        SK_TRAPS_DOORS,     0),
     APT(SP_HILL_ORC,        SK_UNARMED_COMBAT,  1),
-    APT(SP_HILL_ORC,        SK_SPELLCASTING,   -3),
+    APT(SP_HILL_ORC,        SK_SPELLCASTING,   -2),
     APT(SP_HILL_ORC,        SK_CONJURATIONS,    0),
     APT(SP_HILL_ORC,        SK_HEXES,           0),
     APT(SP_HILL_ORC,        SK_CHARMS,         -1),
