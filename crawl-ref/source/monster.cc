826b579ae17d94d4f533581a48f7db77f8da570d
Adam Borowski <kilobyte@angband.pl>
2011-10-25 14:40:48 +0200

    Don't allow monster draconians, octopodes and Gastronok to pick up body armour.

--- crawl-ref/source/monster.cc
+++ crawl-ref/source/monster.cc
@@ -1700,6 +1700,12 @@ bool monster::pickup_armour(item_def &item, int near, bool force)
 
     equipment_type eq = EQ_NONE;
 
+    if (eq == EQ_BODY_ARMOUR && mons_genus(type) == MONS_DRACONIAN)
+        return false;
+
+    if (eq != EQ_HELMET && (type == MONS_OCTOPODE || type == MONS_GASTRONOK))
+        return false;
+
     // HACK to allow nagas/centaurs to wear bardings. (jpeg)
     switch (item.sub_type)
     {
