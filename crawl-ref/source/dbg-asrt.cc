725320e74f7ea797cc6c64e5427520daac25e454
Samuel Bronson <naesten@gmail.com>
2011-10-31 21:41:11 -0400

    Windows (MinGW): Do crash dumps, same as on *nix.
    We do this by defining USE_UNIX_SIGNALS there and fixing the resulting
    compiler errors.
    
    For some reason they aren't being written to files yet, though :-(.

--- crawl-ref/source/dbg-asrt.cc
+++ crawl-ref/source/dbg-asrt.cc
@@ -744,15 +744,13 @@ static NORETURN void _BreakStrToDebugger(const char *mesg, bool assert)
     *p = 0;
     abort();
 
-#elif defined(TARGET_OS_MACOSX) || defined(TARGET_COMPILER_MINGW)
-    fprintf(stderr, "%s", mesg);
+#else
+    fprintf(stderr, "%s\n", mesg);
+#if defined(TARGET_OS_MACOSX) || defined(TARGET_COMPILER_MINGW)
 // raise(SIGINT);               // this is what DebugStr() does on OS X according to Tech Note 2030
     int* p = NULL;              // but this gives us a stack crawl...
     *p = 0;
-    abort();                    // just to be sure
-
-#else
-    fprintf(stderr, "%s\n", mesg);
+#endif
     abort();
 #endif
 }
