23f31d8f2d5cd8023b7142c9f34f4c07a95cab9a
Florian Diebold <flodiebold@gmail.com>
2011-11-15 23:04:54 +0100

    Webtiles: Don't delay ping responses.

--- crawl-ref/source/webserver/static/scripts/comm.js
+++ crawl-ref/source/webserver/static/scripts/comm.js
@@ -10,31 +10,45 @@ define(["jquery", "contrib/jquery.json"], function ($) {
 
     // JSON message handlers
     var message_handlers = {};
+    var immediate_handlers = {};
 
     function register_message_handlers(dict)
     {
         $.extend(message_handlers, dict);
     }
 
+    function register_immediate_handlers(dict)
+    {
+        $.extend(immediate_handlers, dict);
+    }
+
     function handle_message(msg)
     {
-        var msgobj;
-        if (typeof msg === "string")
-            msgobj = eval("(" + msg + ")");
-        else
-            msgobj = msg;
-        var handler = message_handlers[msgobj.msg];
+        var handler = message_handlers[msg.msg];
         if (!handler)
         {
-            console.error("Unknown message type: " + msgobj.msg);
+            console.error("Unknown message type: " + msg.msg);
             return;
         }
-        handler(msgobj);
+        handler(msg);
+    }
+
+    function handle_message_immediately(msg)
+    {
+        var handler = immediate_handlers[msg.msg];
+        if (handler)
+        {
+            return handler(msg);
+        }
+        else
+            return false;
     }
 
     return {
         send_message: send_message,
         register_handlers: register_message_handlers,
+        register_immediate_handlers: register_immediate_handlers,
         handle_message: handle_message,
+        handle_message_immediately: handle_message_immediately,
     };
 });
