a1f1e4f69476ba83ee14588cb3379e44865cdf62
Michael Gagno <evilmike@gmail.com>
2011-11-21 17:32:15 -0800

    Remove summon undead from Deep Dwarf Death Knights.
    If these ever spawn normally they should come with bands of undead monsters.
    In vaults, just place undead along with them. Having this as a summon makes
    little sense.

--- crawl-ref/source/mon-spll.h
+++ crawl-ref/source/mon-spll.h
@@ -91,9 +91,9 @@
        SPELL_NO_SPELL,
        SPELL_ANIMATE_DEAD,
        SPELL_MIRROR_DAMAGE,
-       SPELL_SUMMON_UNDEAD, // cheaper to fake recall and gift with summon spell
+       SPELL_NO_SPELL,
        SPELL_DRAIN_LIFE,
-       SPELL_MIRROR_DAMAGE
+       SPELL_DRAIN_LIFE
       }
     },
 
