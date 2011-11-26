25780d98ef26d837c163a419c226da7163c6fdbe
Robert Burnham <burnhamrobertp@gmail.com>
2011-11-25 22:06:13 -0600

    Fix "It attacks It" messages
    Missing a check on needs_message

--- crawl-ref/source/melee_attack.cc
+++ crawl-ref/source/melee_attack.cc
@@ -487,7 +487,7 @@ bool melee_attack::handle_phase_hit()
             beogh_follower_convert(defender->as_monster(), true);
         }
     }
-    else
+    else if(needs_message)
     {
         attack_verb = attacker->atype() == ACT_PLAYER
                       ? attack_verb
