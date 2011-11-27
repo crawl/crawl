7400dddb384813a49a80f8790df9b4dc71ce7e26
blueDave <dshackspam@comcast.net>
2011-11-27 02:01:27 -0700

    Save compatibility

--- crawl-ref/source/tag-version.h
+++ crawl-ref/source/tag-version.h
@@ -62,6 +62,7 @@ enum tag_minor_version
     TAG_MINOR_TEMPORARY_CLOUDS,    // Clouds are now marked if they're temporary or not.
                                    // XXX: When clearing, check dat/dlua/lm_fog.lua
     TAG_MINOR_PHOENIX_ATTITUDE,    // Store the attitudes of dead phoenixes
+    TAG_MINOR_CONSTRICTION,        // Constriction available
     NUM_TAG_MINORS,
     TAG_MINOR_VERSION = NUM_TAG_MINORS - 1
 };
