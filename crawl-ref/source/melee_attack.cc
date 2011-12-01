80aa2223e980fb93ba818ff2a8bab4fb1ded4c2b
elliptic <hyperelliptical@gmail.com>
2011-11-30 19:10:48 -0500

    Don't clean up dead monsters in combat until handle_phase_killed().
    This should fix several crashes caused by doing something involving a wiped-out
    monster.

--- crawl-ref/source/melee_attack.cc
+++ crawl-ref/source/melee_attack.cc
@@ -619,7 +619,7 @@ bool melee_attack::handle_phase_damaged()
                 mprf("%s", special_damage_message.c_str());
 
             if (special_damage > 0)
-                inflict_damage(special_damage, special_damage_flavour, true);
+                inflict_damage(special_damage, special_damage_flavour);
         }
 
         const bool chaos_attack = damage_brand == SPWPN_CHAOS
@@ -3043,7 +3043,7 @@ bool melee_attack::apply_damage_brand()
     }
 
     if (special_damage > 0)
-        inflict_damage(special_damage, special_damage_flavour, true);
+        inflict_damage(special_damage, special_damage_flavour);
 
     if (obvious_effect && attacker_visible && weapon != NULL)
     {
