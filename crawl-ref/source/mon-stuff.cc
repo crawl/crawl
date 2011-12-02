844443f0479ddb9332096742c9ba4fad0fbe727e
Adam Borowski <kilobyte@angband.pl>
2011-12-02 14:15:12 +0100

    Don't let Prince Ribbit be polymorphed into a human.
    If you have access to a wand or the spell, you'll usually try it multiple
    times; Ribbit was special-cased only on the first polymorph.

--- crawl-ref/source/mon-stuff.cc
+++ crawl-ref/source/mon-stuff.cc
@@ -2594,7 +2594,9 @@ static bool _valid_morph(monster* mons, monster_type new_mclass)
         || mons_is_tentacle(new_mclass)
 
         // The spell on Prince Ribbit can't be broken so easily.
-        || (new_mclass == MONS_HUMAN && mons->type == MONS_PRINCE_RIBBIT))
+        || (new_mclass == MONS_HUMAN
+            && (mons->type == MONS_PRINCE_RIBBIT
+             || mons->mname == "Prince Ribbit")))
     {
         return (false);
     }
