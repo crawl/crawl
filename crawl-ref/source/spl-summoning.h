74e9989d9a62f1021a1e49a3b648c2f8fd7b8111
Chris Campbell <chriscampbell89@gmail.com>
2011-12-05 20:46:14 +0000

    Don't allow abjuring through walls either

--- crawl-ref/source/spl-summoning.h
+++ crawl-ref/source/spl-summoning.h
@@ -87,7 +87,7 @@ bool twisted_resurrection(actor *caster, int pow, beh_type beha,
 
 spret_type cast_haunt(int pow, const coord_def& where, god_type god, bool fail);
 
-spret_type cast_abjuration(int pow, monster *mon = NULL, bool fail = false);
+spret_type cast_abjuration(int pow, const coord_def& where, bool fail = false);
 spret_type cast_mass_abjuration(int pow, bool fail = false);
 
 #endif
