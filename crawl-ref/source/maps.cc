a19183ff7d526a8f2dd5c70262c60718a1f3a3e9
Adam Borowski <kilobyte@angband.pl>
2011-12-29 12:59:16 +0100

    Don't rebuild .dsc when only the executable is newer.
    This used to be needed when tiles were stored as enums rather than strings,
    today versioning is supposed to handle all incompatibilities.

--- crawl-ref/source/maps.cc
+++ crawl-ref/source/maps.cc
@@ -1191,18 +1191,6 @@ static bool load_map_cache(const std::string &filename, const std::string &cache
     if (is_newer(filename, file_idx) || is_newer(filename, file_dsc))
         return (false);
 
-    // When the executable is rebuilt for tiles, it's possible that
-    // the tile enums have changed, breaking old cache files.
-    // So, conservatively regenerate cache files after rebuilding.
-
-    // Is this still the case?  Don't we use names as strings now? --1KB
-    {
-        std::string exe_path = SysEnv.crawl_base + FILE_SEPARATOR
-                               + SysEnv.crawl_exe;
-        if (is_newer(exe_path, file_idx) || is_newer(exe_path, file_dsc))
-            return (false);
-    }
-
     if (!verify_map_index(descache_base) || !verify_map_full(descache_base))
         return (false);
 
