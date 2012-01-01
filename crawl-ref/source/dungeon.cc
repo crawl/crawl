93ffa54f8dec45e264feb617e1a788a7eaf703fb
Florian Diebold <flodiebold@gmail.com>
2012-01-01 17:39:56 +0100

    Hopefully fix #4872 (black tiles in an abyss vault).
    The black tiles were caused by tile_flv being empty. I'm not
    completely sure as it is hard to reproduce, but I think this happened
    because vault_placement::apply_grid() clears the flavour, but then
    dungeon_terrain_changed only re-inits it if the feature actually
    changed.

--- crawl-ref/source/dungeon.cc
+++ crawl-ref/source/dungeon.cc
@@ -6345,6 +6345,10 @@ void vault_placement::apply_grid()
                 // Have to link items each square at a time, or
                 // dungeon_terrain_changed could blow up.
                 link_items();
+                // Init tile flavour -- dungeon_terrain_changed does
+                // this too, but only if oldgrid != newgrid, so we
+                // make sure here.
+                tile_init_flavour(*ri);
                 const dungeon_feature_type newgrid = grd(*ri);
                 grd(*ri) = oldgrid;
                 dungeon_terrain_changed(*ri, newgrid, true, true);
