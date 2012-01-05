9b7a088bb276c90a7665074873d26285da804cce
Neil Moore <neil@s-z.org>
2012-01-04 22:50:33 -0500

    Display (studied) for active manuals, treat as equipped.
    That's supposed to be short for "being studied"; hopefully it won't
    lead to confusion.
    
    Manuals are also given the "equipped" prefix so they will be highlighted
    in the inventory; and are drawn with the equipped flag in tiles.

--- crawl-ref/source/tilereg-inv.cc
+++ crawl-ref/source/tilereg-inv.cc
@@ -703,6 +703,9 @@ void InventoryRegion::update()
                 }
             }
 
+            if (item_is_active_manual(you.inv[i]))
+                desc.flag |= TILEI_FLAG_EQUIP;
+
             inv_shown[i] = true;
             m_items.push_back(desc);
         }
