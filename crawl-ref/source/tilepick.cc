2ba0b312eb419988f99b5ab6f2458f39b464bbce
Adam Borowski <kilobyte@angband.pl>
2012-01-12 23:09:34 +0100

    XuaXua's tiles for Mass Abjuration, modified Abjuration.
    The pentagram is yellow to make it differ somehow from actual summons which
    look too similar.

--- crawl-ref/source/tilepick.cc
+++ crawl-ref/source/tilepick.cc
@@ -4318,6 +4318,7 @@ tileidx_t tileidx_spell(spell_type spell)
     case SPELL_CALL_CANINE_FAMILIAR:     return TILEG_CALL_CANINE_FAMILIAR;
     case SPELL_CALL_IMP:                 return TILEG_CALL_IMP;
     case SPELL_ABJURATION:               return TILEG_ABJURATION;
+    case SPELL_MASS_ABJURATION:          return TILEG_MASS_ABJURATION;
     case SPELL_SUMMON_SCORPIONS:         return TILEG_SUMMON_SCORPIONS;
     case SPELL_SUMMON_ELEMENTAL:         return TILEG_SUMMON_ELEMENTAL;
     case SPELL_SUMMON_DEMON:             return TILEG_SUMMON_DEMON;
