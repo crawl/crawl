1e941703e7a9f6dfd9a6a0177546166183b6cb16
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2012-01-13 20:43:11 -0600

    Fix message wrt how many tentacles are needed for a 2-hander and a shield.
    If octopodes use four to wield weapons and four to move, as the manual
    states, they'd need five, not nine, to wield the 2-hander.

--- crawl-ref/source/item_use.cc
+++ crawl-ref/source/item_use.cc
@@ -813,7 +813,7 @@ bool do_wear_armour(int item, bool quiet)
     {
         if (!quiet)
         {
-            const char* how_many = you.has_tentacles(false) == 3 ? "nine"
+            const char* how_many = you.has_tentacles(false) == 3 ? "five"
                                                                  : "three";
             mprf("You'd need %s %s to do that!", how_many,
                  you.hand_name(true).c_str());
