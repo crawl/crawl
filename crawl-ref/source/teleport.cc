df8490ae6804aa3ab63c7c3aee3facc5296dbcf8
Raphael Langella <raphael.langella@gmail.com>
2012-01-24 13:13:31 +0100

    Allow random blinks and teleport to put the player on mimics.

--- crawl-ref/source/teleport.cc
+++ crawl-ref/source/teleport.cc
@@ -233,7 +233,6 @@ bool random_near_space(const coord_def& origin, coord_def& target,
             || restrict_los && !you.see_cell(target)
             || grd(target) < limit
             || actor_at(target)
-            || mimic_at(target)
             || !allow_adjacent && distance(origin, target) <= 2
             || forbid_sanctuary && is_sanctuary(target))
         {
