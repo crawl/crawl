1c99e543e9309733c6533dcbe78300c8ad8d5c23
Michael Gagno <evilmike@gmail.com>
2012-03-13 15:12:58 -0700

    Enable the Spider Nest.
    The lair rotation is now one of spider/snake and one of shoals/swamp.
    
    Spider still isn't finished, but it's playable. I don't know about the
    branch difficulty, but we should find out soon enough.

--- crawl-ref/source/branch.cc
+++ crawl-ref/source/branch.cc
@@ -201,6 +201,6 @@ branch_type get_branch_at(const coord_def& pos)
 
 bool branch_is_unfinished(branch_type branch)
 {
-    return branch == BRANCH_SPIDER_NEST || branch == BRANCH_FOREST
-           || branch == BRANCH_DWARVEN_HALL || branch == BRANCH_HIVE;
+    return branch == BRANCH_FOREST || branch == BRANCH_DWARVEN_HALL
+           || branch == BRANCH_HIVE;
 }
