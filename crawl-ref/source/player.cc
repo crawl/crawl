844e08c9dd00e13cbd29468fd38713c5a87cf085
Neil Moore <neil@s-z.org>
2012-03-15 09:15:31 -0400

    Redraw the screen while gaining a level.
    Just after increasing you.experience_level, before giving the message
    about the new level (and before prompting for stat increases).  This
    way, at the stat increase prompt and every "more" prompt, the player
    will be the correct level and the just-killed monster will be dead.
    
    Fixes #5380.

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -3036,6 +3036,15 @@ void level_change(bool skip_attribute_increase)
         }
         else  // Character has gained a new level
         {
+            crawl_state.stat_gain_prompt = false;
+            you.experience_level = new_exp;
+            you.max_level = you.experience_level;
+            
+            // Don't want to see the dead creature at the prompt.
+            redraw_screen();
+            // There may be more levels left to gain.
+            you.redraw_experience = true;
+         
             if (new_exp == 27)
             {
                mpr("You have reached level 27, the final one!",
@@ -3049,10 +3058,6 @@ void level_change(bool skip_attribute_increase)
 
             if (!(new_exp % 3) && !skip_attribute_increase)
                 attribute_increase();
-
-            crawl_state.stat_gain_prompt = false;
-            you.experience_level = new_exp;
-            you.max_level = you.experience_level;
 
             switch (you.species)
             {
