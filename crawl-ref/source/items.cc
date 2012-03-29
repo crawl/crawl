855e5d858040d3a15ad1d6b5a699873561d2c8c9
Raphael Langella <raphael.langella@gmail.com>
2012-03-30 01:01:04 +0200

    Mimic abyssal rune don't prevent the placement of a real one.

--- crawl-ref/source/items.cc
+++ crawl-ref/source/items.cc
@@ -2997,7 +2997,8 @@ item_def *find_floor_item(object_class_type cls, int sub_type)
         for (int x = 0; x < GXM; ++x)
             for (stack_iterator si(coord_def(x,y)); si; ++si)
                 if (si->defined()
-                    && si->base_type == cls && si->sub_type == sub_type)
+                    && si->base_type == cls && si->sub_type == sub_type
+                    && !si->flags & ISFLAG_MIMIC)
                 {
                     return (& (*si));
                 }
