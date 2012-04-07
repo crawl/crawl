5290ff6baed80e2dba83af089822e33b83ea250a
Raphael Langella <raphael.langella@gmail.com>
2012-04-07 15:45:14 +0200

    New fight simulator (nfogravity).

--- crawl-ref/source/wiz-fsim.h
+++ crawl-ref/source/wiz-fsim.h
@@ -1,11 +1,26 @@
 /**
  * @file
- * @brief Fight simualtion wizard functions.
+ * @brief Fight simulation wizard functions.
 **/
 
 #ifndef WIZFSIM_H
 #define WIZFSIM_H
 
-void debug_fight_statistics(bool use_init_defaults, bool defence = false);
+struct fight_data {
+double av_hit_dam;
+int max_dam;
+int accuracy;
+double av_dam;
+double av_time;
+double av_eff_dam;
+};
+
+void wiz_run_fight_sim(monster_type mtype = MONS_WORM,
+                        int iter_limit = 5000);
+void wiz_fight_sim_file(bool defend = false,
+                        monster_type mtype = MONS_WORM,
+                        int iter_limit = 5000,
+                        const char * fightstat = "fight.stat");
+void skill_vs_fighting(int iter_limit = 5000);
 
 #endif
