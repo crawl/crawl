fadbbaf9c302ae1dd1f7b68823746d8434f1afae
Raphael Langella <raphael.langella@gmail.com>
2012-04-08 15:59:19 +0200

    fsim: fix a lot of code duplication.
    Also allows cancelling double scale simulation.

--- crawl-ref/source/wiz-fsim.h
+++ crawl-ref/source/wiz-fsim.h
@@ -15,12 +15,7 @@ double av_time;
 double av_eff_dam;
 };
 
-void wiz_run_fight_sim(monster_type mtype = MONS_WORM,
-                        int iter_limit = 5000);
-void wiz_fight_sim_file(bool defend = false,
-                        monster_type mtype = MONS_WORM,
-                        int iter_limit = 5000,
-                        const char * fightstat = "fight.stat");
-void skill_vs_fighting(int iter_limit = 5000);
+void wizard_quick_fsim();
+void wizard_fight_sim(bool double_scale);
 
 #endif
