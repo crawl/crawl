64c133e5dc209f5126ca46b8aba4b7307cc45aef
Adam Borowski <kilobyte@angband.pl>
2012-04-14 00:54:38 +0200

    Remove the now-unused code for emergency saves.
    If we want them back for assertions, it'd be far better to save _before_
    going into the crash handler.

--- crawl-ref/source/files.cc
+++ crawl-ref/source/files.cc
@@ -1528,7 +1528,6 @@ static void _save_game_exit()
     // Prompt for saving macros.
     if (crawl_state.unsaved_macros
         && !crawl_state.seen_hups
-        && !crawl_state.game_wants_emergency_save
         && yesno("Save macros?", true, 'n'))
     {
         macro_save();
@@ -1567,13 +1566,8 @@ void save_game(bool leave_game, const char *farewellmsg)
     // so Valgrind doesn't complain.
     _save_game_exit();
 
-    // Exit unless this is an emergency save, in which case let the
-    // crash handler re-raise the crashy signal.
-    if (!crawl_state.game_wants_emergency_save)
-    {
-        end(0, false, farewellmsg? "%s" : "See you soon, %s!",
-            farewellmsg? farewellmsg : you.your_name.c_str());
-    }
+    end(0, false, farewellmsg? "%s" : "See you soon, %s!",
+        farewellmsg? farewellmsg : you.your_name.c_str());
 }
 
 // Saves the game without exiting.
--- crawl-ref/source/state.h
+++ crawl-ref/source/state.h
@@ -33,10 +33,6 @@ struct game_state
     bool game_crashed;      // The game crashed and is now in the process of
                             // dumping crash info.
 
-    // An assert was triggered, but an emergency save here should be possible
-    // without corruption.
-    bool game_wants_emergency_save;
-
     bool mouse_enabled;     // True if mouse input is currently relevant.
 
     bool waiting_for_command; // True when the game is waiting for a command.
