crawl/crawl

commit 215dce49f5d64889edffe5874b7ab1353b104dd2

Fix Webtiles compatibility with tornado 2.2.

crawl-ref/source/webserver/ws_handler.py
@@ -129,6 +129,8 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
             "set_rc": self.set_rc,
             }
 
+    client_closed = property(lambda self: self.ws_connection and self.ws_connection.client_terminated)
+
     def _process_log_msg(self, msg, kwargs):
         return "#%-5s %s" % (self.id, msg), kwargs
 
@@ -201,7 +203,7 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
                 self.logger.info("Stopping crawl after idle time limit.")
                 self.process.stop()
 
-        if not self.client_terminated:
+        if not self.client_closed:
             self.reset_timeout()
 
     def start_crawl(self, game_id):
@@ -253,7 +255,7 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
                     socket.send_message("lobby_remove", id=self.process.id)
         self.process = None
 
-        if self.client_terminated:
+        if self.client_closed:
             sockets.remove(self)
         else:
             if shutting_down:
@@ -287,7 +289,7 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
             self.send_message("go_lobby")
 
     def shutdown(self):
-        if not self.client_terminated:
+        if not self.client_closed:
             msg = self.render_string("shutdown.html", game=self)
             self.send_message("close", reason = msg)
             self.close()
@@ -438,7 +440,7 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
     def write_message(self, msg):
         try:
             msg = to_unicode(msg)
-            if not self.client_terminated:
+            if not self.client_closed:
                 super(CrawlWebSocket, self).write_message(msg)
         except:
             self.logger.warning("Exception trying to send message.", exc_info = True)
@@ -447,7 +449,7 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
     def send_message(self, msg, **data):
         """Sends a JSON message to the client."""
         data["msg"] = msg
-        if not self.client_terminated:
+        if not self.client_closed:
             self.write_message(json_encode(data))
 
     def on_close(self):

commit 05e4a9a5763e064a9f85600d1cd9dc5c9d69e2fd

Fix an exception in the webtiles server when a websocket is closed.
This was caused by my commit 215dce49f5.

crawl-ref/source/webserver/ws_handler.py
@@ -129,7 +129,7 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
             "set_rc": self.set_rc,
             }
 
-    client_closed = property(lambda self: self.ws_connection and self.ws_connection.client_terminated)
+    client_closed = property(lambda self: (not self.ws_connection) or self.ws_connection.client_terminated)
 
     def _process_log_msg(self, msg, kwargs):
         return "#%-5s %s" % (self.id, msg), kwargs