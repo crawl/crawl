5c1d877c114fad9c0e102c0b1dc1b50ef87ef855
Adam Borowski <kilobyte@angband.pl>
2012-04-16 11:15:39 +0200

    Fix a crash when a pile of items goes past 32767 items.
    The only way to get that was to combine two lesser piles -- they will now
    fail to stack.
    (cherry picked from commit 6a46e17d4c4359ae8cf8200008c941e8592cad08)

--- crawl-ref/source/items.cc
+++ crawl-ref/source/items.cc
@@ -1499,8 +1499,11 @@ bool items_stack(const item_def &item1, const item_def &item2,
 {
     // Both items must be stackable.
     if (!force_merge
-        && (!is_stackable_item(item1) || !is_stackable_item(item2)))
+        && (!is_stackable_item(item1) || !is_stackable_item(item2))
+        || static_cast<int>(item1.quantity) + item2.quantity > 32767)
     {
+        COMPILE_CHECK(sizeof(item1.quantity) == 2); // can be relaxed otherwise
+
         return (false);
     }
 
