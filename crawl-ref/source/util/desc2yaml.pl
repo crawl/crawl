104ac526ce59b61a8d83bd1c9022049bac67b286
Raphael Langella <raphael.langella@gmail.com>
2012-04-25 16:18:50 +0200

    A script to convert a description file in yaml format.
    It works nicely with transifex.

---
+++ crawl-ref/source/util/desc2yaml.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use YAML ();
+use File::Basename;
+
+die "Usage: $0 description_files\n" unless (@ARGV);
+
+main();
+
+sub main {
+   foreach (@ARGV) {
+        my $basename = basename($_, '.txt');
+        open OUT, ">$basename.yaml";
+        my %DESCRIPTIONS;
+        load_file($_, \%DESCRIPTIONS);
+        print OUT YAML::Dump({en_AU => \%DESCRIPTIONS});
+   }
+}
+
+sub load_file {
+    my ($input, $hash) = @_;
+    open IN, $input or die "Cannot open $input";
+    my ($key, $value);
+    while(<IN>) {
+        next if (/^#/);
+        chomp;
+        if (/^%%%%$/) {
+            if ($key) {
+                $hash->{$key} = $value;
+            }
+            $key = "";
+            $value = "";
+        }
+        elsif ($key) {
+            if ($value) {
+                $value .= (/^\s+/ or /^$/) ? "\n" : " ";
+            }
+            $value .= $_;
+        } else {
+            $key = $_;
+        }
+   }
+   close IN;
+}
