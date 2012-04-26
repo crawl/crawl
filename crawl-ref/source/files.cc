3f55c25f4c9639b87652a6055f63bf176845b4ac
Adam Borowski <kilobyte@angband.pl>
2012-04-26 22:43:21 +0200

    Clear exclusion annotations when deleting a level.

--- crawl-ref/source/files.cc
+++ crawl-ref/source/files.cc
@@ -1870,6 +1870,7 @@ void delete_level(const level_id &level)
 {
     travel_cache.erase_level_info(level);
     StashTrack.remove_level(level);
+    clear_level_exclusion_annotation(level);
     if (you.save)
         you.save->delete_chunk(level.describe());
     if (level.branch == BRANCH_ABYSS)
