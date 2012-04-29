5fe3e9cf535eed2b50805b392b3220cfe5c4bda1
Raphael Langella <raphael.langella@gmail.com>
2012-04-29 14:47:53 +0200

    Remove the useless main block.
    I duplicated those from existing scripts, but then realised how useless they
    are. Looks like some C++ coder felt insecure without it.

--- crawl-ref/source/util/txt2ini.pl
+++ crawl-ref/source/util/txt2ini.pl
@@ -7,36 +7,32 @@ use open ':encoding(utf8)';
 
 die "Usage: $0 description_files\n" unless (@ARGV);
 
-main();
-
-sub main {
-   foreach my $file (@ARGV) {
-        my ($basename,$path) = fileparse($file, '.txt');
-        open IN, $file or die "Cannot open $file";
-        open OUT, ">$path/$basename.ini";
-        my ($key, $value);
-        while(<IN>) {
-            next if (/^#/);
-            chomp;
-            if (/^%%%%$/) {
-                print OUT "$key=$value\n" if ($key);
-                $key = "";
-                $value = "";
-            }
-            elsif ($key) {
-                if ($value) {
-                    if (/^\s*$/ or /^\s+/ or substr($value,-2) eq '\n') {
-                        $value .= '\n';
-                    } else {
-                        $value .= " ";
-                    }
-                }
-                $value .= $_;
-            } else {
-                $key = $_;
-            }
+foreach my $file (@ARGV) {
+    my ($basename,$path) = fileparse($file, '.txt');
+    open IN, $file or die "Cannot open $file";
+    open OUT, ">$path/$basename.ini";
+    my ($key, $value);
+    while(<IN>) {
+        next if (/^#/);
+        chomp;
+        if (/^%%%%$/) {
+            print OUT "$key=$value\n" if ($key);
+            $key = "";
+            $value = "";
         }
-        close IN;
-        close OUT;
-   }
+        elsif ($key) {
+            if ($value) {
+                if (/^\s*$/ or /^\s+/ or substr($value,-2) eq '\n') {
+                    $value .= '\n';
+                } else {
+                    $value .= " ";
+                }
+            }
+            $value .= $_;
+        } else {
+            $key = $_;
+        }
+    }
+    close IN;
+    close OUT;
 }
