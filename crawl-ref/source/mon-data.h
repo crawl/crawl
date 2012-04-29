926646af24e0e309877bce8743d6ea4ba1c97607
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2012-04-29 11:26:29 -0500

    Make flaming corpses resist hellfire, as they're permanently on fire as it is.

--- crawl-ref/source/mon-data.h
+++ crawl-ref/source/mon-data.h
@@ -2472,7 +2472,7 @@ static monsterentry mondata[] = {
 {
     MONS_FLAMING_CORPSE, 'z', RED, "flaming corpse",
     M_SENSE_INVIS | M_GLOWS_LIGHT,
-    mrd(MR_RES_FIRE, 3) | MR_RES_STICKY_FLAME,
+    MR_RES_HELLFIRE | MR_RES_STICKY_FLAME,
     0, 17, MONS_FLAMING_CORPSE, MONS_FLAMING_CORPSE, MH_UNDEAD, -4,
     { {AT_HIT, AF_NAPALM, 20}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
     { 8, 3, 5, 0 },
