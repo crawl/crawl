58c5daf3c8086e44692e26779c14f59e92d0594a
Adam Borowski <kilobyte@angband.pl>
2012-05-05 20:30:46 +0200

    Fix truncation of character codes in some cases.
    Even if you accept only certain ASCII ones, you still need to read the whole
    thing, as the lowest 8 bits may match something.

--- crawl-ref/source/stuff.h
+++ crawl-ref/source/stuff.h
@@ -26,7 +26,7 @@ double stepdown(double value, double step);
 int stepdown(int value, int step, rounding_type = ROUND_CLOSE, int max = 0);
 int stepdown_value(int base_value, int stepping, int first_step,
                    int last_step, int ceiling_value);
-unsigned char get_ch();
+int get_ch();
 
 void cio_init();
 void cio_cleanup();
