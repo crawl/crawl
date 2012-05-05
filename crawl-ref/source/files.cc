ec5614216bcd176ed1b5203bacd9ea1583fb23a4
Adam Borowski <kilobyte@angband.pl>
2012-05-05 04:29:33 +0200

    Remove an obsolete special case.

--- crawl-ref/source/files.cc
+++ crawl-ref/source/files.cc
@@ -960,8 +960,6 @@ static bool _grab_follower_at(const coord_def &pos)
         return (true);
 
     level_id dest = level_id::current();
-    if (you.char_direction == GDT_GAME_START)
-        dest.depth = 1;
 
     dprf("%s is following to %s.", fol->name(DESC_THE, true).c_str(),
          dest.describe().c_str());
