crawl/crawl

commit 5ee9feefdf8e7ed360311d203015c34859b3c8ce

Use the proper IMMOLATION-type enum when affixing the fire brand.

crawl-ref/source/item_use.cc
@@ -4466,7 +4466,7 @@ static bool _vorpalise_weapon(bool already_known)
     case SPWPN_FLAME:
     case SPWPN_FLAMING:
         mprf("%s is engulfed in an explosion of flames!", itname.c_str());
-        immolation(10, IMMOLATION_SCROLL, you.pos(), already_known, &you);
+        immolation(10, IMMOLATION_SPELL, you.pos(), already_known, &you);
         break;
 
     case SPWPN_FROST:

commit c78923c675b1b02f9021d73af1ee7138960420e1

Re-fix affixing Flaming destroying scrolls.
Was broken by 5ee9feef.

crawl-ref/source/effects.cc
@@ -352,22 +352,21 @@ void immolation(int pow, int caster, coord_def where, bool known,
 
     bolt beam;
 
-    if (caster < 0)
+    switch (caster)
     {
-        switch (caster)
-        {
-        case IMMOLATION_SCROLL:
-            aux = "scroll of immolation";
-            break;
+    case IMMOLATION_SCROLL:
+        aux = "scroll of immolation";
+        break;
 
-        case IMMOLATION_SPELL:
-            aux = "a fiery explosion";
-            break;
+    case IMMOLATION_AFFIX:
+        aux = "a fiery explosion";
+        break;
 
-        case IMMOLATION_TOME:
-            aux = "an exploding Tome of Destruction";
-            break;
-        }
+    case IMMOLATION_TOME:
+        aux = "an exploding Tome of Destruction";
+        break;
+
+    default:;
     }
 
     beam.flavour       = BEAM_FIRE;
@@ -380,7 +379,8 @@ void immolation(int pow, int caster, coord_def where, bool known,
     beam.ex_size       = 2;
     beam.is_explosion  = true;
     beam.effect_known  = known;
-    beam.affects_items = (caster != IMMOLATION_SCROLL);
+    beam.affects_items = caster != IMMOLATION_SCROLL
+                         && caster != IMMOLATION_AFFIX;
 
     if (caster == IMMOLATION_GENERIC)
     {

crawl-ref/source/enum.h
@@ -1661,7 +1661,7 @@ enum immolation_source_type
 {
     IMMOLATION_GENERIC = -1,
     IMMOLATION_SCROLL  = -2,
-    IMMOLATION_SPELL   = -3, // effect when fixing fire brand
+    IMMOLATION_AFFIX   = -3, // effect when fixing fire brand
     IMMOLATION_TOME    = -4, // exploding Tome of Destruction
 };
 

crawl-ref/source/item_use.cc
@@ -4503,7 +4503,7 @@ static bool _vorpalise_weapon(bool already_known)
     case SPWPN_FLAME:
     case SPWPN_FLAMING:
         mprf("%s is engulfed in an explosion of flames!", itname.c_str());
-        immolation(10, IMMOLATION_SPELL, you.pos(), already_known, &you);
+        immolation(10, IMMOLATION_AFFIX, you.pos(), already_known, &you);
         break;
 
     case SPWPN_FROST: