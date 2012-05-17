65f69beb77529cd219cb56b4606d9b1bb882d3da
Ben Striegel <ben.striegel@gmail.com>
2012-05-17 16:13:25 -0400

    Suppression: block all evocables but reaching

--- crawl-ref/source/stuff.cc
+++ crawl-ref/source/stuff.cc
@@ -490,7 +490,7 @@ void canned_msg(canned_message_type which_message)
         mpr("The deck of cards disappears in a puff of smoke.");
         break;
     case MSG_EVOCATION_SUPPRESSED:
-        mpr("You may not evoke equipped items while suppressed!");
+        mpr("You may not evoke while suppressed!");
         break;
     }
 }
