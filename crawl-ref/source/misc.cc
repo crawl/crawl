4c0e3ab7b65af9a7da55723f108da90ef80a2c26
Neil Moore <neil@s-z.org>
2012-05-18 08:38:33 -0400

    Redraw weapon on auto-id.

--- crawl-ref/source/misc.cc
+++ crawl-ref/source/misc.cc
@@ -2689,6 +2689,7 @@ bool maybe_id_weapon(item_def &item, const char *msg)
         set_ident_flags(item, id);
         if (msg)
             mprf("%s%s", msg, item.name(DESC_INVENTORY_EQUIP).c_str());
+        you.wield_change = true;
         return true;
     }
 
