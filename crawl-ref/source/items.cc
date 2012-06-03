82eafc007909e143cf1efdbae316e8bff272c46d
Neil Moore <neil@s-z.org>
2012-06-03 12:04:19 -0400

    Indicated gained gold pieces on pickup, too. (cherry picked from commit 4b40fa763dd323a807fad835c24f349786a2b89e)

--- crawl-ref/source/items.cc
+++ crawl-ref/source/items.cc
@@ -1626,8 +1626,12 @@ static void _got_gold(item_def& item, int quant, bool quiet)
 
     if (!quiet)
     {
-        mprf("You now have %d gold piece%s.",
-             you.gold, you.gold != 1 ? "s" : "");
+        const std::string gain = quant != you.gold
+                                 ? make_stringf(" (gained %d)", quant)
+                                 : "";
+
+        mprf("You now have %d gold piece%s%s.",
+             you.gold, you.gold != 1 ? "s" : "", gain.c_str());
         learned_something_new(HINT_SEEN_GOLD);
     }
 }
