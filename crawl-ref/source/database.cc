57d56f52de1831c56b6b492f2e873f4ed121a2f8
Adam Borowski <kilobyte@angband.pl>
2012-06-09 21:32:08 +0200

    Kludge around coloured database entries not showing.
    One-line ones are still broken, this kludge assumes everything that matches
    /^<([^\n]*)>\n$/s is an alias reference.
    
    The choice of syntax wasn't most fortunate...

--- crawl-ref/source/database.cc
+++ crawl-ref/source/database.cc
@@ -732,7 +732,8 @@ static std::string _query_database(TextDB &db, std::string key,
     std::string str((const char *)result.dptr, result.dsize);
 
     // <foo> is an alias to key foo
-    if (str[0] == '<' and str[str.size() - 2] == '>')
+    if (str[0] == '<' and str[str.size() - 2] == '>'
+        && str.find("\n") == str.size() - 1)
     {
         return _query_database(db, str.substr(1, str.size() - 3),
                                canonicalise_key, run_lua, untranslated);
