f90c9975bc729381a88ea6b260ed4a7701a95974
Neil Moore <neil@s-z.org>
2012-06-16 11:32:48 -0400

    No longer guarantee Spider.
    The biggest problems have been dealt with, and it would be nice to
    get some feedback on the balance of the roulette pairs before 0.11
    is released.
    
    This reverts commit 2f186193e81e2b0bfa6ab956a2cfa1bfc88590c6.

--- crawl-ref/source/ng-init.cc
+++ crawl-ref/source/ng-init.cc
@@ -59,7 +59,6 @@ void initialise_branch_depths()
             startdepth[branch] = random_range(b->mindepth, b->maxdepth);
     }
 
-#if 0
     // You will get one of Shoals/Swamp and one of Spider/Snake.
     // This way you get one "water" branch and one "poison" branch.
     const branch_type disabled_branch[] =
@@ -67,13 +66,6 @@ void initialise_branch_depths()
         random_choose(BRANCH_SWAMP, BRANCH_SHOALS, -1),
         random_choose(BRANCH_SNAKE_PIT, BRANCH_SPIDER_NEST, -1),
     };
-#else
-    // For the time being, let's make Spider guaranteed.
-    branch_type disabled_branch[2];
-    disabled_branch[0] = random_choose(BRANCH_SWAMP, BRANCH_SHOALS, BRANCH_SNAKE_PIT, -1);
-    do disabled_branch[1] = random_choose(BRANCH_SWAMP, BRANCH_SHOALS, BRANCH_SNAKE_PIT, -1);
-    while (disabled_branch[0] == disabled_branch[1]);
-#endif
 
     for (unsigned int i = 0; i < ARRAYSZ(disabled_branch); ++i)
     {
