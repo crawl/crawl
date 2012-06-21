f15e6aeef66846aaae46c4cf61f8cda6a778240d
Adam Borowski <kilobyte@angband.pl>
2012-06-21 15:49:19 +0200

    Store the overview of portal vaults as branches.
    It was a mess of a private enum for Labs, Abyss, Pan and Hell, and strings
    for the rest.  This was missed during the portal_branches rewrite.
    
    Fixes "Buggy:" portals.

--- crawl-ref/source/tag-version.h
+++ crawl-ref/source/tag-version.h
@@ -22,6 +22,7 @@ enum tag_minor_version
     TAG_MINOR_DETECT_CURSE_REMOVAL,// Convert detect curse -> remove curse
     TAG_MINOR_OBJ_RODS,            // Rods are a separate item class
     TAG_MINOR_MONSTER_JEWELLERY,   // Allow monsters to wear rings/amulets
+    TAG_MINOR_UNIFIED_PORTALS,     // Use branch enums for remembered portals.
     NUM_TAG_MINORS,
     TAG_MINOR_VERSION = NUM_TAG_MINORS - 1
 };
