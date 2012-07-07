75cfdc08f47c2e37f8bd47273d4505ad0a6a2a41
Neil Moore <neil@s-z.org>
2012-07-07 05:24:36 -0400

    Re-add shedu bands to holy pan and zig monster lists.
    Also, since Shadow Creatures can now summon a shedu band, make sure that
    a worshipper of an evil god angers both shedu at the same time.
    
    Note that other angerable monsters in bands will have the same issue
    (only the band leader will be angered)---and it is not possible to use
    the same method to solve that problem as we did here.  Therefore, bands
    in random monster lists should for the moment be restricted to
    non-angerable creatures (other than shedu).
    
    Fixes #5072.
    
    This reverts commit d48ceeeb5743a9a80217dd4b259ec67c4d43fec3.

--- crawl-ref/source/dat/dlua/ziggurat.lua
+++ crawl-ref/source/dat/dlua/ziggurat.lua
@@ -257,7 +257,7 @@ mset(with_props("place:Slime:$", { jelly_protect = true }),
      with_props("place:Geh:$", { weight = 5 }),
      with_props("place:Dis:$", { weight = 5 }),
      with_props("place:Tar:$", { weight = 5 }),
-     with_props("daeva / angel / cherub / pearl dragon / ophan / " ..
+     with_props("daeva / angel / cherub / pearl dragon / shedu band / ophan / " ..
                 "apis / paladin / phoenix / w:5 silver star", { weight = 2 }),
      with_props("hill giant / cyclops / stone giant / fire giant / " ..
                 "frost giant / ettin / titan", { weight = 2 }),
