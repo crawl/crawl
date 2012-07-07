crawl/crawl

commit 84529d68d19b47489f41615bf64f90e1639ff0a7

Make feature renames local to a vault rather than the whole level.
For technical reasons (vault_placement), subvaults use renaming data of the
top-level vault, this might be changed one day; that'd require a bit more
writing but would allow different renames in different subvaults.

TODO: "line of fire blocked by XXX".

crawl-ref/source/stash.cc
@@ -551,10 +551,7 @@ std::string Stash::feature_description() const
     if (feat == DNGN_FLOOR)
         return ("");
 
-    if (feat == DNGN_ENTER_PORTAL_VAULT)
-        return (::feature_description(coord_def(x, y), false));
-
-    return (::feature_description(feat, trap));
+    return (::feature_description_at(coord_def(x, y), false));
 }
 
 bool Stash::matches_search(const std::string &prefix,

crawl-ref/source/tag-version.h
@@ -25,6 +25,7 @@ enum tag_minor_version
     TAG_MINOR_UNIFIED_PORTALS,     // Use branch enums for remembered portals.
     TAG_MINOR_CLEAR_APTABLE,       // Reset old broken force_autopickup tables.
     TAG_MINOR_CONSTRICT_MID,       // Store constriction by mid, not mindex.
+    TAG_MINOR_FEAT_RENAMES,        // Per-vault feature renames.
     NUM_TAG_MINORS,
     TAG_MINOR_VERSION = NUM_TAG_MINORS - 1
 };

crawl-ref/source/beam.cc
@@ -1103,11 +1103,11 @@ bool bolt::hit_wall()
         else
         {
             prompt += "the targeted "
-                    + feature_description(target, false, DESC_PLAIN, false);
+                    + feature_description_at(target, false, DESC_PLAIN, false);
         }
 
         prompt += " is blocked by "
-                + feature_description(pos(), false, DESC_A, false);
+                + feature_description_at(pos(), false, DESC_A, false);
 
         prompt += ". Continue anyway?";
 
@@ -1179,7 +1179,7 @@ void bolt::affect_cell()
             else
             {
                 mprf("The %s protects %s from harm.",
-                     raw_feature_description(grd(act->pos())).c_str(),
+                     raw_feature_description(act->pos()).c_str(),
                      act->name(DESC_THE).c_str());
             }
         }

crawl-ref/source/dat/des/branches/crypt.des
@@ -1090,8 +1090,8 @@ KMONS:  F = patrolling reaper
 : end
 SUBST:  n : ncc
 KFEAT: ABCEFH = .
-: dgn.set_feature_desc_short("granite statue", "gravestone")
-: dgn.set_feature_desc_short("tree", "dead tree")
+: set_feature_name("granite_statue", "gravestone")
+: set_feature_name("tree", "dead tree")
 MAP
 tttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
 tttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt

crawl-ref/source/dat/des/branches/hells.des
@@ -10,7 +10,7 @@ function dis_setup(e)
    e.lflags("no_tele_control")
 -- turn granite statues into iron statues
 -- you still have to colour the statues, if desired
-   dgn.set_feature_desc_short("granite statue", "iron statue")
+   e.set_feature_name("granite_statue", "iron statue")
 end
 
 function dis_rune(e)
@@ -27,8 +27,8 @@ function geh_setup(e)
    e.lfloorcol("lightgrey")
    e.lfloortile("floor_pebble_darkgray")
    e.colour("v = red")
-   dgn.set_feature_desc_short("floor", "Ashen ground")
-   dgn.set_feature_desc_short("metal wall", "tempered metal wall")
+   e.set_feature_name("floor", "Ashen ground")
+   e.set_feature_name("metal_wall", "tempered metal wall")
 end
 
 function coc_setup(e)
@@ -41,9 +41,9 @@ function coc_setup(e)
    e.lfloorcol("white")
    e.lfloortile("floor_ice")
    e.colour("c = lightblue")
-   dgn.set_feature_desc_short("rock wall", "ice-covered rock wall")
-   dgn.set_feature_desc_short("stone wall", "ice-covered stone wall")
-   dgn.set_feature_desc_short("floor", "Frost-covered floor")
+   e.set_feature_name("rock_wall", "ice-covered rock wall")
+   e.set_feature_name("stone_wall", "ice-covered stone wall")
+   e.set_feature_name("floor", "Frost-covered floor")
 end
 
 function tar_setup(e)

crawl-ref/source/dat/des/branches/pan.des
@@ -552,8 +552,7 @@ epilogue{{
 COLOUR:     n = blue
 TILE:       n = dngn_metal_wall_cracked
 TILE:       v = dngn_metal_wall_brown
-:           dgn.set_feature_desc_short("translucent stone wall",
-:                                      "cracked metal wall")
+:           set_feature_name("clear_stone_wall", "cracked metal wall")
 MAP
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -1762,12 +1761,10 @@ FTILE:  "E0 = floor_grass
 COLOUR: "0 = green
 FTILE:  -ZBghi&v = floor_vault
 SUBST:  D& = +, '"- = ., H = G, C = c
-: dgn.set_feature_desc_short("granite statue",
-:                            "golden statue")
-: dgn.set_feature_desc_short("metal wall",
-:                            "silver wall")
-: _G.set_random_mon_list("angel / cherub / ophan / daeva / phoenix / " ..
-:                        "apis / paladin / apis / ophan / angel")
+: set_feature_name("granite_statue", "golden statue")
+: set_feature_name("metal_wall", "silver wall")
+: set_random_mon_list("angel / cherub / ophan / daeva / phoenix / " ..
+:                     "apis / paladin / apis / ophan / angel")
 # Do not place if you are with a holy god. It's just free loot in that case.
 veto {{
     if you.god() == "the Shining One" or you.god() == "Zin" or you.god() == "Elyvilon" then

crawl-ref/source/dat/des/branches/slime.des
@@ -293,7 +293,7 @@ fixup_marker:add_triggerer(DgnTriggerer:new {
 : lua_marker("Z", fixup_marker)
 
 {{
-    dgn.set_feature_desc_short("stone wall", "rune carved stone wall")
+    set_feature_name("stone_wall", "rune carved stone wall")
 }}
 MAP
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

crawl-ref/source/dat/des/branches/tomb.des
@@ -185,7 +185,7 @@ SUBST:   3 = 3., 4 = 4., 5 = 5.
 MONS:    mummy, guardian mummy, mummy priest, sphinx, greater mummy
 MONS:    silver statue / orange crystal statue / ice statue
 #
-: dgn.set_feature_desc_short("granite statue", "sarcophagus")
+: set_feature_name("granite_statue", "sarcophagus")
 MAP
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

commit 51a2f406346d6c5f295618cf0563e21c00cca7be

Use correct feature name in stashes.
We were looking up the feature description for the item's (x,y)
position... on the current level.  The problem was introduced by
commit 84529d6, but portals had the same problem before then
(which is why  Ctrl-F trove  never worked).

Now we also store the feature's description in the stash.  For old
saves, we use the default description for that type of feature (ignoring
overrides) until we next update the stash.  Floor and other
uninteresting features are stored as the empty string.

Fixes #5880.

crawl-ref/source/stash.cc
@@ -282,6 +282,11 @@ void Stash::update()
             feat = DNGN_FLOOR, trap = TRAP_UNASSIGNED;
     }
 
+    if (feat == DNGN_FLOOR)
+        feat_desc = "";
+    else
+        feat_desc = feature_description_at(coord_def(x, y), false);
+
     // If this is your position, you know what's on this square
     if (p == you.pos())
     {
@@ -554,10 +559,7 @@ std::string Stash::description() const
 
 std::string Stash::feature_description() const
 {
-    if (feat == DNGN_FLOOR)
-        return "";
-
-    return (::feature_description_at(coord_def(x, y), false));
+    return feat_desc;
 }
 
 bool Stash::matches_search(const std::string &prefix,
@@ -747,6 +749,8 @@ void Stash::save(writer& outf) const
     marshallByte(outf, feat);
     marshallByte(outf, trap);
 
+    marshallString(outf, feat_desc);
+
     // Note: Enabled save value is inverted logic, so that it defaults to true
     marshallByte(outf, ((verified ? 1 : 0) | (!enabled ? 2 : 0)
                         | (is_stack ? 4 : 0)));
@@ -770,6 +774,14 @@ void Stash::load(reader& inf)
 #if TAG_MAJOR_VERSION == 33
     if (trap == TRAP_AXED)
         trap = TRAP_SPEAR;
+
+    if (inf.getMinorVersion() < TAG_MINOR_STASH_FEATDESC)
+    {
+        feat_desc = (is_boring_feature(feat)
+                     ? "" : ::feature_description(feat, trap));
+    }
+    else
+        feat_desc = unmarshallString(inf);
 #endif
 
     uint8_t flags = unmarshallUByte(inf);

crawl-ref/source/stash.h
@@ -94,6 +94,7 @@ private:
     uint8_t x, y;
     int  abspos;
     dungeon_feature_type feat;
+    std::string feat_desc; // Only for interesting features.
     trap_type trap;
 
     std::vector<item_def> items;

crawl-ref/source/tag-version.h
@@ -26,6 +26,7 @@ enum tag_minor_version
     TAG_MINOR_CLEAR_APTABLE,       // Reset old broken force_autopickup tables.
     TAG_MINOR_CONSTRICT_MID,       // Store constriction by mid, not mindex.
     TAG_MINOR_FEAT_RENAMES,        // Per-vault feature renames.
+    TAG_MINOR_STASH_FEATDESC,      // Store feature descriptions in stashes.
     NUM_TAG_MINORS,
     TAG_MINOR_VERSION = NUM_TAG_MINORS - 1
 };