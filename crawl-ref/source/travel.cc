crawl/crawl

commit 8d595aab955c10796a119f22f4e32ae1127c25a4

Improve autoexplore by trying to have a look at unreachable places.
It's mostly useful in Lair and Slime. It's far from the optimal explore path since it
will only happen on the second flood. It means it will start by exploring reachable
points and once done will try to take a look at unreachable ones. Doing it sooner would
be way too taxing anyway, since it involves LOS iteration, and it's not really possible
anyway.

The list of unreachable points is saved in a set, so it could be possible to propose
seeing them in map mode to the player.

crawl-ref/source/travel.cc
@@ -299,6 +299,10 @@ static bool _is_reseedable(const coord_def& c, bool ignore_danger = false)
 
     map_cell &cell(env.map_knowledge(c));
     const dungeon_feature_type grid = cell.feat();
+
+    if (feat_is_wall(grid))
+        return false;
+
     return (feat_is_water(grid)
             || grid == DNGN_LAVA
             || is_trap(c)
@@ -662,9 +666,11 @@ void stop_running()
 
 static bool _is_valid_explore_target(const coord_def& where)
 {
-    // If an adjacent square is unmapped, it's valid.
-    for (adjacent_iterator ai(where); ai; ++ai)
-        if (!env.map_knowledge(*ai).seen())
+    // If a square in LOS is unmapped, it's valid.
+    los_def los(where);
+    los.update();
+    for (radius_iterator ri(&los, true); ri; ++ri)
+        if (!env.map_knowledge(*ri).seen())
             return (true);
 
     if (you.running == RMODE_EXPLORE_GREEDY)
@@ -698,7 +704,7 @@ static int _find_explore_status(const travel_pathfind &tp)
         explore_status |= EST_GREED_UNFULFILLED;
 
     const coord_def unexplored = tp.unexplored_square();
-    if (unexplored.x || unexplored.y)
+    if (unexplored.x || unexplored.y || !tp.get_unreachables().empty())
         explore_status |= EST_PARTLY_EXPLORED;
 
     return (explore_status);
@@ -1148,7 +1154,7 @@ travel_pathfind::travel_pathfind()
       ignore_danger(false), annotate_map(false), ls(NULL),
       need_for_greed(false), unexplored_place(), greedy_place(),
       unexplored_dist(0), greedy_dist(0), refdist(NULL), reseed_points(),
-      features(NULL), point_distance(travel_point_distance),
+      features(NULL), unreachables(), point_distance(travel_point_distance),
       points(0), next_iter_points(0), traveled_distance(0),
       circ_index(0)
 {
@@ -1458,6 +1464,11 @@ void travel_pathfind::get_features()
     }
 }
 
+const std::set<coord_def> travel_pathfind::get_unreachables() const
+{
+    return unreachables;
+}
+
 bool travel_pathfind::square_slows_movement(const coord_def &c)
 {
     // c is a known (explored) location - we never put unknown points in the
@@ -1495,7 +1506,7 @@ void travel_pathfind::check_square_greed(const coord_def &c)
 
 bool travel_pathfind::path_flood(const coord_def &c, const coord_def &dc)
 {
-    if (!in_bounds(dc))
+    if (!in_bounds(dc) || unreachables.count(dc))
         return (false);
 
     if (floodout
@@ -1503,19 +1514,50 @@ bool travel_pathfind::path_flood(const coord_def &c, const coord_def &dc)
     {
         if (!env.map_knowledge(dc).seen())
         {
-            if (!need_for_greed)
+            if (ignore_hostile)
+            {
+                // This point is unexplored but unreachable. Let's find a
+                // place from where we can see it.
+                los_def los(dc);
+                los.update();
+                for (radius_iterator ri(&los, true); ri; ++ri)
+                {
+                    const int dist = point_distance[ri->x][ri->y];
+                    if (dist > 0
+                        && (dist < unexplored_dist || unexplored_dist < 0))
+                    {
+                        unexplored_dist = dist;
+                        unexplored_place = *ri;
+                    }
+
+                    // We can't do better than that.
+                    if (unexplored_dist == 1)
+                    {
+                        _set_target_square(unexplored_place);
+                        return true;
+                    }
+                }
+
+                // We can't even see the place.
+                // Let's store it and look for another.
+                if (unexplored_dist < 0)
+                    unreachables.insert(dc);
+                else
+                    _set_target_square(unexplored_place);
+
+            }
+            else if (!need_for_greed)
             {
                 // Found explore target!
                 unexplored_place = c;
                 unexplored_dist  = traveled_distance;
                 return (true);
             }
-
-            if (unexplored_dist == UNFOUND_DIST)
+            else if (unexplored_dist == UNFOUND_DIST)
             {
                 unexplored_place = c;
-                unexplored_dist  =
-                    traveled_distance + Options.explore_item_greed;
+                unexplored_dist  = traveled_distance
+                                   + Options.explore_item_greed;
             }
         }
 

crawl-ref/source/travel.h
@@ -42,9 +42,9 @@ enum run_dir_type
 
 enum run_mode_type
 {
-    RMODE_INTERLEVEL     = -4, // Interlevel travel (Ctrl+G)
-    RMODE_EXPLORE_GREEDY = -3, // Explore + grab items (Tab/Ctrl+I)
-    RMODE_EXPLORE        = -2, // Exploring (Ctrl+O)
+    RMODE_INTERLEVEL     = -4, // Interlevel travel
+    RMODE_EXPLORE_GREEDY = -3, // Explore + grab items
+    RMODE_EXPLORE        = -2, // Exploring
     RMODE_TRAVEL         = -1, // Classic or Plain Old travel
     RMODE_NOT_RUNNING    = 0,  // must remain equal to 0
     RMODE_CONTINUE,
@@ -502,6 +502,8 @@ public:
     // Extract features without pathfinding
     void get_features();
 
+    const std::set<coord_def> get_unreachables() const;
+
     // The next square to go to to move towards the travel destination. Return
     // value is undefined if pathfind was not called with RMODE_TRAVEL.
     const coord_def travel_move() const;
@@ -584,6 +586,9 @@ protected:
     std::vector<coord_def> reseed_points;
 
     std::vector<coord_def> *features;
+
+    // List of unexplored and unreachable points.
+    std::set<coord_def> unreachables;
 
     travel_distance_col *point_distance;
 

commit 6cdbd4bb58f89f9ffe19b983a7ff5e59d7a60c68

Autoexplore doesn't try to see unreachable places in Shoals.
Autoexplore looping through the whole level was caused by 8d595aab. This
commit prevent this behaviour in Shoals. This is a better and simpler fix
than the autoexplore limit I experimented. It was a bit more efficient, but
less exhaustive, and a bit confusing.

Now that I think about it, we could use a similar fix for swamp instead of
making mangroves fully opaque. Visually, seeing only one row makes it look
less like a forest.

crawl-ref/source/travel.cc
@@ -1461,7 +1461,7 @@ bool travel_pathfind::path_flood(const coord_def &c, const coord_def &dc)
     {
         if (!env.map_knowledge(dc).seen())
         {
-            if (ignore_hostile)
+            if (ignore_hostile && !player_in_branch(BRANCH_SHOALS))
             {
                 // This point is unexplored but unreachable. Let's find a
                 // place from where we can see it.