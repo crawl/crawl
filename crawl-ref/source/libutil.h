5cdea5a2774c34da6fda30f09d81de5bdf0c8e1d
Adam Borowski <kilobyte@angband.pl>
2012-07-29 18:20:00 +0200

    Grab two functions from libutil.

---
+++ crawl-ref/source/libutil.h
@@ -0,0 +1,16 @@
+#ifndef LIBUTIL_H
+#define LIBUTIL_H
+
+template <typename Z>
+static void erase_any(std::vector<Z> &vec, unsigned long which)
+{
+    if (which != vec.size() - 1)
+        vec[which] = vec[vec.size() - 1];
+    vec.pop_back();
+}
+
+static inline int sqr(int x)
+{
+    return x * x;
+}
+#endif
