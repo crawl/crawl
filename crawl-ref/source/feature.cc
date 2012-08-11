crawl/crawl

commit 65b7d4d64049779d770167e21b638079c70702b0

roctavian's detected altar and shop/portal tiles (#6041).
This adds in two new feature types, DNGN_UNKNOWN_ALTAR and
DNGN_UNKNOWN_PORTAL, to which these tiles are assigned; the magic
mapping code returns these two features for unknown altars and portals
respectively.

crawl-ref/source/feature.cc
@@ -60,6 +60,8 @@ static void _init_feat(feature_def &f, dungeon_feature_type feat)
     {
         case DNGN_UNSEEN:
         case DNGN_EXPLORE_HORIZON:
+        case DNGN_UNKNOWN_ALTAR:
+        case DNGN_UNKNOWN_PORTAL:
         default:
             break;
 
@@ -839,13 +841,13 @@ dungeon_feature_type magic_map_base_feat(dungeon_feature_type feat)
     case DCHAR_WAVY:
         return DNGN_SHALLOW_WATER;
     case DCHAR_ARCH:
-        return DNGN_ABANDONED_SHOP;
+        return DNGN_UNKNOWN_PORTAL;
     case DCHAR_FOUNTAIN:
         return DNGN_FOUNTAIN_BLUE;
     case DCHAR_WALL:
         return DNGN_ROCK_WALL;
     case DCHAR_ALTAR:
-        return DNGN_ALTAR_FIRST_GOD;
+        return DNGN_UNKNOWN_ALTAR;
     default:
         // We could do more, e.g. map the different upstairs together.
         return feat;

crawl-ref/source/directn.cc
@@ -3156,6 +3156,10 @@ static std::string _base_feature_desc(dungeon_feature_type grid,
         return "dry fountain";
     case DNGN_EXPLORE_HORIZON:
         return "explore horizon";
+    case DNGN_UNKNOWN_ALTAR:
+        return "detected altar";
+    case DNGN_UNKNOWN_PORTAL:
+        return "detected shop or portal";
     default:
         return "";
     }

crawl-ref/source/enum.h
@@ -1343,6 +1343,9 @@ enum dungeon_feature_type
     // Not meant to ever appear in grd().
     DNGN_EXPLORE_HORIZON, // dummy for redefinition
 
+    DNGN_UNKNOWN_ALTAR,
+    DNGN_UNKNOWN_PORTAL,
+
     NUM_FEATURES
 };
 

crawl-ref/source/terrain.cc
@@ -1700,6 +1700,7 @@ static const char *dngn_feature_names[] =
 "permadry_fountain",
 
 "explore_horizon",
+"unknown_altar", "unknown_portal",
 };
 
 dungeon_feature_type dungeon_feature_by_name(const std::string &name)

crawl-ref/source/tilepick.cc
@@ -371,6 +371,10 @@ static tileidx_t _tileidx_feature_base(dungeon_feature_type feat)
     case DNGN_DRY_FOUNTAIN_BLOOD:
     case DNGN_PERMADRY_FOUNTAIN:
         return TILE_DNGN_DRY_FOUNTAIN;
+    case DNGN_UNKNOWN_ALTAR:
+        return TILE_DNGN_UNKNOWN_ALTAR;
+    case DNGN_UNKNOWN_PORTAL:
+        return TILE_DNGN_UNKNOWN_PORTAL;
     default:
         return TILE_DNGN_ERROR;
     }

commit a71ade18b5edd9014cef0e66f49dbea2ed213f22

Have detected altars and shop/portals show up again in console.
Fixes a bug introduced by 65b7d4d6.

crawl-ref/source/feature.cc
@@ -60,8 +60,6 @@ static void _init_feat(feature_def &f, dungeon_feature_type feat)
     {
         case DNGN_UNSEEN:
         case DNGN_EXPLORE_HORIZON:
-        case DNGN_UNKNOWN_ALTAR:
-        case DNGN_UNKNOWN_PORTAL:
         default:
             break;
 
@@ -690,6 +688,18 @@ static void _init_feat(feature_def &f, dungeon_feature_type feat)
         case DNGN_PERMADRY_FOUNTAIN:
             f.colour  = LIGHTGREY;
             f.dchar   = DCHAR_FOUNTAIN;
+            f.minimap = MF_FEATURE;
+            break;
+
+        case DNGN_UNKNOWN_ALTAR:
+            f.colour  = LIGHTGREY;
+            f.dchar   = DCHAR_ALTAR;
+            f.minimap = MF_FEATURE;
+            break;
+
+        case DNGN_UNKNOWN_PORTAL:
+            f.colour  = LIGHTGREY;
+            f.dchar   = DCHAR_ARCH;
             f.minimap = MF_FEATURE;
             break;
     }