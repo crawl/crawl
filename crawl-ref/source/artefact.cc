68307dda27813791835695bd406940ed597a3440
Adam Borowski <kilobyte@angband.pl>
2012-08-15 21:57:21 +0200

    Don't generate rings or randarts with *TELE in Sprint.
    They just cause unnecessary spam.  Scrolls of teleportation already get skipped.

--- crawl-ref/source/artefact.cc
+++ crawl-ref/source/artefact.cc
@@ -1024,7 +1024,7 @@ static void _get_randart_properties(const item_def &item,
             proprt[ARTP_PREVENT_SPELLCASTING] = 1;
             break;
         case 2:                     // random teleport
-            if (aclass != OBJ_WEAPONS)
+            if (aclass != OBJ_WEAPONS || crawl_state.game_is_sprint())
                 break;
             proprt[ARTP_CAUSE_TELEPORTATION] = 5 + random2(15);
             break;
