184fcfc46b311f20a988f8c04dad9d7d56fa2969
Neil Moore <neil@s-z.org>
2012-08-20 11:00:19 -0400

    Fix PROX_NEAR_STAIRS in ZotDef (atomjack; #6095). (cherry picked from commit 96a66cb2e87f7984e2744d901fb41b769ccca36f)

--- crawl-ref/source/terrain.cc
+++ crawl-ref/source/terrain.cc
@@ -606,7 +606,8 @@ coord_def get_random_stair()
     {
         const dungeon_feature_type feat = grd(*ri);
         if (feat_is_travelable_stair(feat) && !feat_is_escape_hatch(feat)
-            && feat != DNGN_EXIT_DUNGEON && feat != DNGN_EXIT_HELL)
+            && (crawl_state.game_is_zotdef() || feat != DNGN_EXIT_DUNGEON)
+            && feat != DNGN_EXIT_HELL)
         {
             st.push_back(*ri);
         }
