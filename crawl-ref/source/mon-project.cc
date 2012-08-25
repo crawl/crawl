6c7652a2f519d9e21ef4c43a49ec65f82a3f3306
Steve Melenchuk <smelenchuk@gmail.com>
2012-08-24 18:36:22 -0600

    Remove duplicate "The orb of destruction hits you!" message.
    Apparently this was present on account of some inconsistencies in the
    beam code which have since been addressed.
    
    Fixes #6118.

--- crawl-ref/source/mon-project.cc
+++ crawl-ref/source/mon-project.cc
@@ -498,10 +498,6 @@ move_again:
             goto move_again;
         }
 
-        // Yay for inconsistencies in beam-vs-player and beam-vs-monsters.
-        if (victim && victim->is_player() && !mons_is_boulder(&mon))
-            mprf("%s hits you!", mon.name(DESC_THE, true).c_str());
-
         if (_iood_hit(mon, pos))
         {
             if (!iood)
