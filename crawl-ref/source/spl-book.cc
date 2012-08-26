5cd77a44fa80ee94063870a6ca69467a068b6cb3
Adam Borowski <kilobyte@angband.pl>
2012-08-26 13:27:30 +0200

    Require at least 1.0 mana to zap a lightning rod.
    When I allowed using fractional parts, I forgot that without throwing
    fractions away, 0.01 > 0.  We want 1.0 <= charge <= 5.0.

--- crawl-ref/source/spl-book.cc
+++ crawl-ref/source/spl-book.cc
@@ -1431,6 +1431,8 @@ int rod_spell(int rod)
         // Starting it up takes 2 mana, continuing any amount up to 5.
         // You don't get to expend less (other than stopping the zap completely).
         mana = min(5 * ROD_CHARGE_MULT, (int)irod.plus);
+        // Never allow using less that a whole point of charge.
+        mana = max(mana, ROD_CHARGE_MULT);
         you.props["thunderbolt_mana"].get_int() = mana;
     }
 
