c1df9cf33f93c740b70a5ed14637238c7061b164
Adam Borowski <kilobyte@angband.pl>
2012-09-02 01:47:28 +0200

    Be more forgiving about broken versions of sqlite.
    Note that I intentionally put in into 0.11 and not trunk, we're supposed to
    fix bugs rather than paper them over there.
    
    I'm doubtful whether ignoring this is good in the first place, too.  Not any
    different from, say, not having working ncursesw.

--- crawl-ref/source/database.cc
+++ crawl-ref/source/database.cc
@@ -332,12 +332,13 @@ void databaseSystemInit()
     // Note: if you're building contrib libraries initially checked out
     // before 2011-12-28 and this assertion fails, please make sure you have
     // the current version ("git submodule sync;git submodule update --init").
-    ASSERT(sqlite3_threadsafe());
+    bool bad_sqlite = !sqlite3_threadsafe();
 
     thread_t th[NUM_DB];
     for (unsigned int i = 0; i < NUM_DB; i++)
 #ifndef DGAMELAUNCH
-        if (thread_create_joinable(&th[i], init_db, (void*)(intptr_t)i))
+        if (bad_sqlite
+            || thread_create_joinable(&th[i], init_db, (void*)(intptr_t)i))
 #endif
         {
             // if thread creation fails, do it serially
