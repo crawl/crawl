b86d57a45c300c295fafb5f4a56cb0049d1e34d2
Raphael Langella <raphael.langella@gmail.com>
2012-09-15 23:24:07 +0200

    Cleave effect for Axes.
    * Additional targets are acquired by rotating from the main target in both
      directions but no more than 3 cells. Solid features block cleaving. On
      open grounds, it attacks 7 cells (all but the opposite one from the main
      target).
      A single wall on the side can block one or two targets since it doesn't go
      all around.
    * Additional attacks do 75% damage. Main target still takes full damage.
    * Allies don't block cleaving and are not harmed by it unless attacker
      is confused.
    * Monsters wielding axes get cleaving too.
    * Broad axe, battleaxe and executioner's axe base damage is reduced by 1.
      More nerfing might be needed for the latter 2.
    * Cleaving works when attacking empty spaces (useful against invisible
      monsters)
    * Monsters dual wielding axes should work.
    
    Thanks to LexAckson for helping with this implementation
    (especially rotate_adjacent).

--- crawl-ref/source/coord.h
+++ crawl-ref/source/coord.h
@@ -57,5 +57,6 @@ bool adjacent(const coord_def& p1, const coord_def& p2);
 
 coord_def player2grid(const coord_def& pc);
 coord_def grid2player(const coord_def& pc);
+coord_def rotate_adjacent(coord_def vector, int direction);
 
 #endif
