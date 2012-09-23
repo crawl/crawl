d12e1fedd254f60c59814153171e311dc6786a34
Neil Moore <neil@s-z.org>
2012-09-23 12:03:13 -0400

    Allow "lang = latviešu" and two 7-bit versions.

--- crawl-ref/source/initfile.cc
+++ crawl-ref/source/initfile.cc
@@ -2314,8 +2314,12 @@ void game_options::read_option_line(const string &str, bool runscript)
             lang = LANG_KO, lang_name = "ko";
         else if (field == "lt" || field == "lithuanian" || field == "lietuvos")
             lang = LANG_LT, lang_name = "lt";
-        else if (field == "lv" || field == "latvian" || field == "lettish" || field == "latvijas")
+        else if (field == "lv" || field == "latvian" || field == "lettish"
+                 || field == "latvijas" || field == "latviešu"
+                 || field == "latvieshu" || field == "latviesu")
+        {
             lang = LANG_LV, lang_name = "lv";
+        }
         else if (field == "pl" || field == "polish" || field == "polski")
             lang = LANG_PL, lang_name = "pl";
         else if (field == "pt" || field == "portuguese" || field == "português" || field == "portugues")
