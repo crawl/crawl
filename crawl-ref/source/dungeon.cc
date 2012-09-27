crawl/crawl

commit fea71b51790dfb1c5babc22c32226a976a1d5fde

Make staves and rods separate object classes.
They have about no overlap, more than both being usable as weapons.  A vast
majority of uses immediately checked item_is_rod()/item_is_staff().

I kept them shared for acquirement, for now.

Also, eliminate rods of smiting -- hardly ever used, problematic theme-wise
as they use "divine providence" without worship.  Keeping rods of striking
for now, could be used to fix artificer problems.  Adding/removing rod enums
is a mess due to tile handling, renaming ROD_SMITING to ROD_LIGHTNING for now.

crawl-ref/source/dungeon.cc
@@ -4633,12 +4633,13 @@ static void _vault_grid_glyph(vault_placement &place, const coord_def& where,
         else if (vgrid == '|')
         {
             which_class = random_choose_weighted(
-                            2, OBJ_WEAPONS,
-                            1, OBJ_ARMOUR,
-                            1, OBJ_JEWELLERY,
-                            1, OBJ_BOOKS,
+                            4, OBJ_WEAPONS,
+                            2, OBJ_ARMOUR,
+                            2, OBJ_JEWELLERY,
+                            2, OBJ_BOOKS,
                             1, OBJ_STAVES,
-                            1, OBJ_MISCELLANY,
+                            1, OBJ_RODS,
+                            2, OBJ_MISCELLANY,
                             0);
             which_depth = MAKE_GOOD_ITEM;
         }

commit 1e95962812cb389b5189d2103d4ef4d47260beaf

Fix rod generation in vaults being 5 times higher than it should.
Bug introduced by fea71b51. Only one staff in 10 was turned into a rod,
not 1 in 2.

crawl-ref/source/dungeon.cc
@@ -4771,14 +4771,14 @@ static void _vault_grid_glyph(vault_placement &place, const coord_def& where,
         else if (vgrid == '|')
         {
             which_class = random_choose_weighted(
-                            4, OBJ_WEAPONS,
-                            2, OBJ_ARMOUR,
-                            2, OBJ_JEWELLERY,
-                            2, OBJ_BOOKS,
-                            1, OBJ_STAVES,
-                            1, OBJ_RODS,
-                            2, OBJ_MISCELLANY,
-                            0);
+                            20, OBJ_WEAPONS,
+                            10, OBJ_ARMOUR,
+                            10, OBJ_JEWELLERY,
+                            10, OBJ_BOOKS,
+                             9, OBJ_STAVES,
+                             1, OBJ_RODS,
+                            10, OBJ_MISCELLANY,
+                             0);
             which_depth = MAKE_GOOD_ITEM;
         }
         else if (vgrid == '*')