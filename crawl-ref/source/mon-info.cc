bfa4146bbb7f895fc58927e9d6b59a5c46ce2a6f
Raphael Langella <raphael.langella@gmail.com>
2012-10-16 13:40:53 +0200

    Give zombies and such the same size as the base monster (#5722).

--- crawl-ref/source/mon-info.cc
+++ crawl-ref/source/mon-info.cc
@@ -1516,7 +1516,8 @@ reach_type monster_info::reach_range() const
 
 size_type monster_info::body_size() const
 {
-    const monsterentry *e = get_monster_data(type);
+    const monsterentry *e = base_type != type ? get_monster_data(base_type)
+                                              : get_monster_data(type);
     size_type ret = (e ? e->size : SIZE_MEDIUM);
 
     // Slime creature size is increased by the number merged.
