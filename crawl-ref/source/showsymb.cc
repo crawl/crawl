58b729e5497da28ad9ed415560c4542376701905
Adam Borowski <kilobyte@angband.pl>
2012-10-18 00:43:02 +0200

    Allow colouring/reglyphing items based on their filter prefixes.
    These are partially redundant with stash prefixes, it'd be good to unify
    these two sets somehow.

--- crawl-ref/source/showsymb.cc
+++ crawl-ref/source/showsymb.cc
@@ -11,6 +11,7 @@
 
 #include "colour.h"
 #include "env.h"
+#include "itemname.h"
 #include "libutil.h"
 #include "map_knowledge.h"
 #include "mon-util.h"
@@ -210,6 +211,7 @@ static cglyph_t _get_item_override(const item_def &item)
         return g;
 
     string name = stash_annotate_item(STASH_LUA_SEARCH_ANNOTATE, &item)
+                + " {" + filtering_item_prefix(item, false) + "} "
                 + item.name(DESC_PLAIN);
 
     {
