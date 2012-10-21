badb3d9049d50afda2381c0a923312c20091e274
Raphael Langella <raphael.langella@gmail.com>
2012-10-21 15:18:23 +0200

    Properly fix the monster list health indicator under windows.
    The previous fix inverted the player glyph (how could I miss that?).
    This time, we're just not applying the heap_brand fix on windows since it
    doesn't seem to be affected by the bug anyway.
    
    This reverts commit 7b2d18452f1af3bec283ef3f6841d5fc5cc8da21.
    This reverts commit 80ba7f984ee2c939f3b4ddff4e5ca7016fe524a3.

--- crawl-ref/source/output.cc
+++ crawl-ref/source/output.cc
@@ -1096,7 +1096,11 @@ static void _print_next_monster_desc(const vector<monster_info>& mons,
             cprintf(" ");
 
             monster_info mi = mons[start];
+#ifdef TARGET_OS_WINDOWS
+            textcolor(real_colour(dam_colour(mi) | COLFLAG_ITEM_HEAP));
+#else
             textcolor(real_colour(dam_colour(mi) | COLFLAG_REVERSE));
+#endif
             cprintf(" ");
             textbackground(BLACK);
             textcolor(LIGHTGREY);
