crawl/crawl

commit c90328b3482a111417ce0502644bfc5690ec7a61

Fix trapdoor spiders not lurking and sometimes getting stuck (#6215).

crawl-ref/source/mon-behv.cc
@@ -432,10 +432,12 @@ void handle_behaviour(monster* mon)
             break;
 
         case BEH_LURK:
-            // Lurking mimics stay put when player is away.
-            if (mons_is_mimic(mon->type) && !proxPlayer)
-                break;
-            //Else Fall through
+            // Make sure trapdoor spiders are not hiding in plain sight
+            if (mon->type == MONS_TRAPDOOR_SPIDER && !mon->submerged())
+                mon->add_ench(ENCH_SUBMERGED);
+            // Lurking monsters stay put when player is away.
+            break;
+
         case BEH_SEEK:
             // No foe?  Then wander or seek the player.
             if (mon->foe == MHITNOT)

crawl-ref/source/mon-ench.cc
@@ -627,7 +627,7 @@ void monster::remove_enchantment_effect(const mon_enchant &me, bool quiet)
         monster_die(this, KILL_TIMEOUT, NON_MONSTER);
         break;
     case ENCH_SUBMERGED:
-        if (mons_is_wandering(this))
+        if (mons_is_wandering(this) || mons_is_lurking(this))
         {
             behaviour = BEH_SEEK;
             behaviour_event(this, ME_EVAL);

crawl-ref/source/mon-place.cc
@@ -1637,6 +1637,9 @@ static monster* _place_monster_aux(const mgen_data &mg, const monster *leader,
     // more aware of the player than they'd be otherwise).
     if (mons_is_statue(mg.cls))
         mon->behaviour = BEH_WANDER;
+    // Trapdoor spiders lurk, they don't sleep
+    if (mg.cls == MONS_TRAPDOOR_SPIDER)
+        mon->behaviour = BEH_LURK;
 
     mon->foe_memory = 0;
 

commit 37b35a5546c4e692bd3823fe3893e141179b7046

Let lurking monsters get a target.
This fix revealed mimics not attacking nearby targets (bug introduced by
c90328b3).
The behaviour of lurking and submerged monsters is functionally identical,
would be good to unify the code. And trapdoor spiders are both.
Would be better to have the behaviour logic depend only monster's
behaviour, not enchantment.

I checked out an old build, before I made revealed mimics lurking, and
the lurking behaviour was completely disabled because trapdoor spiders
turned immediately to wandering behaviour while being submerged.
All the logic was done through submerged.

crawl-ref/source/mon-behv.cc
@@ -435,8 +435,8 @@ void handle_behaviour(monster* mon)
             // Make sure trapdoor spiders are not hiding in plain sight
             if (mon->type == MONS_TRAPDOOR_SPIDER && !mon->submerged())
                 mon->add_ench(ENCH_SUBMERGED);
-            // Lurking monsters stay put when player is away.
-            break;
+
+            // Fall through to get a target, but don't change to wandering.
 
         case BEH_SEEK:
             // No foe?  Then wander or seek the player.
@@ -447,7 +447,8 @@ void handle_behaviour(monster* mon)
                     || isNeutral || patrolling
                     || mon->type == MONS_GIANT_SPORE)
                 {
-                    new_beh = BEH_WANDER;
+                    if (mon->behaviour != BEH_LURK)
+                        new_beh = BEH_WANDER;
                 }
                 else
                 {