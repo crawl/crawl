crawl/crawl

commit 4d7bf57569f3efcd7ba15654b9a755d3cd5daa64

Added 'god_likes_your_god'. Gods do not hate themselves

crawl-ref/source/godwrath.cc
@@ -1215,7 +1215,7 @@ bool divine_retribution(god_type god, bool no_bonus, bool force)
     // gods don't use divine retribution on followers of gods they don't
     // hate.
     if (!force && ((god == you.religion && is_good_god(god))
-        || (god != you.religion && !god_hates_your_god(god))))
+        || (!god_hates_your_god(god))))
     {
         return (false);
     }

crawl-ref/source/dgn-overview.cc
@@ -554,7 +554,7 @@ static std::string _print_altars_for_gods(const std::vector<god_type>& gods,
             colour = (you.penance[god] > 10) ? "red" : "lightred";
         }  else if (you.religion == god) {
             colour = "yellow";
-        }  else if (is_good_god(god) && is_good_god(you.religion)) {
+        }  else if (god_likes_your_god(god)) {
             colour = "brown";
         }
 

crawl-ref/source/fight.cc
@@ -1215,9 +1215,8 @@ bool melee_attack::player_aux_test_hit()
     const int helpful_evasion =
         defender->melee_evasion(attacker, EV_IGNORE_HELPLESS);
 
-    if (you.religion != GOD_ELYVILON
-        && you.penance[GOD_ELYVILON]
-        && god_hates_your_god(GOD_ELYVILON, you.religion)
+    if (you.penance[GOD_ELYVILON]
+        && god_hates_your_god(GOD_ELYVILON)
         && to_hit >= evasion
         && one_chance_in(20))
     {
@@ -1541,9 +1540,8 @@ int melee_attack::player_hits_monster()
         defender->melee_evasion(attacker, EV_IGNORE_HELPLESS);
     dprf("your to-hit: %d; defender effective EV: %d", to_hit, evasion);
 
-    if (you.religion != GOD_ELYVILON
-        && you.penance[GOD_ELYVILON]
-        && god_hates_your_god(GOD_ELYVILON, you.religion)
+    if (you.penance[GOD_ELYVILON]
+        && god_hates_your_god(GOD_ELYVILON)
         && to_hit >= evasion
         && one_chance_in(20))
     {

crawl-ref/source/mon-stuff.cc
@@ -876,9 +876,6 @@ static bool _ely_protect_ally(monster* mons)
 // be killed by you or one of your friends.
 static bool _ely_heal_monster(monster* mons, killer_type killer, int i)
 {
-    if (you.religion == GOD_ELYVILON)
-        return (false);
-
     god_type god = GOD_ELYVILON;
 
     if (!you.penance[god] || !god_hates_your_god(god))

crawl-ref/source/religion.cc
@@ -3524,9 +3524,17 @@ void god_pitch(god_type which_god)
     learned_something_new(HINT_CONVERT);
 }
 
+bool god_likes_your_god(god_type god, god_type your_god)
+{
+    return (is_good_god(god) && is_good_god(your_god));
+}
+
 bool god_hates_your_god(god_type god, god_type your_god)
 {
-    ASSERT(god != your_god);
+    
+    // Gods do not hate themselves
+    if (god != your_god)
+        return (false);
 
     // Non-good gods always hate your current god.
     if (!is_good_god(god))

crawl-ref/source/religion.h
@@ -58,8 +58,8 @@ bool transformed_player_can_join_god(god_type which_god);
 void god_pitch(god_type which_god);
 int piety_rank(int piety = -1);
 int piety_scale(int piety_change);
-bool god_hates_your_god(god_type god,
-                        god_type your_god = you.religion);
+bool god_likes_your_god(god_type god, god_type your_god = you.religion);
+bool god_hates_your_god(god_type god, god_type your_god = you.religion);
 std::string god_hates_your_god_reaction(god_type god,
                                         god_type your_god = you.religion);
 bool god_hates_cannibalism(god_type god);

commit 5b68420be9ce026694d33f70be5d88c672748695

Fix non-good gods not using divine retribution on their followers (rwbarton).
The logic for this was broken in 4d7bf5756.

crawl-ref/source/godwrath.cc
@@ -1188,7 +1188,7 @@ bool divine_retribution(god_type god, bool no_bonus, bool force)
     // gods don't use divine retribution on followers of gods they don't
     // hate.
     if (!force && ((god == you.religion && is_good_god(god))
-        || (!god_hates_your_god(god))))
+        || (god != you.religion && !god_hates_your_god(god))))
     {
         return false;
     }