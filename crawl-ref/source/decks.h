03b66151d677229b51546c3678d3789ba286986e
Adam Borowski <kilobyte@angband.pl>
2012-11-22 03:58:06 +0100

    Restart Triple Draw in case of a disconnect.
    Besides not depriving the player of a draw during bona fide disconnects,
    this also fixes two cheats:
    * you had a way out from getting three Wild Magics
    * a crash on demand has plenty of uses

--- crawl-ref/source/decks.h
+++ crawl-ref/source/decks.h
@@ -127,6 +127,8 @@ bool choose_deck_and_draw();
 void nemelex_shuffle_decks();
 void shuffle_all_decks_on_level();
 
+bool draw_three(int slot);
+
 void card_effect(card_type which_card, deck_rarity_type rarity,
                  uint8_t card_flags = 0, bool tell_card = true);
 void draw_from_deck_of_punishment(bool deal = false);
