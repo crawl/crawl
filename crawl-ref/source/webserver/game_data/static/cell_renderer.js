d30d1905a36c09f88fe8458872c83a67515c6383
Andre-Patrick Bubel <code@andre-bubel.de>
2012-12-06 07:11:15 +0100

    Webtiles: fixes for mana minibar
    - draw mana minibar with correct height
    - display mana minibar when mana is depleted
    - only display mana minibar when character can have mana
    
    This makes appearance of the minibars more consistent with the Tiles version.

--- crawl-ref/source/webserver/game_data/static/cell_renderer.js
+++ crawl-ref/source/webserver/game_data/static/cell_renderer.js
@@ -309,7 +309,7 @@ function ($, view_data, main, tileinfo_player, icons, dngn, enums, map_knowledge
             var hp_bar_offset = 2;
 
             // TODO: use different colors if heavily wounded, like in the tiles version
-            if (player.mp > 0) {
+            if (player.mp >= 0 && player.mp_max > 0) {
                 var mp_percent = player.mp / player.mp_max;
 
                 this.ctx.fillStyle = magic_spend;
@@ -318,7 +318,7 @@ function ($, view_data, main, tileinfo_player, icons, dngn, enums, map_knowledge
 
                 this.ctx.fillStyle = magic;
                 this.ctx.fillRect(x, y + this.cell_height - 2,
-                                  this.cell_width * mp_percent, 1);
+                                  this.cell_width * mp_percent, 2);
 
                 hp_bar_offset += 2;
             }
