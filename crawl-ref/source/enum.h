5c4043574dcd2377a8a8a5f5a4dcaac4d705e526
Neil Moore <neil@s-z.org>
2012-12-26 17:53:39 -0500

    LOS_* documentation table
    [Committer's note: updated for changes in the last two years.
     Thanks to wheals for pointing out this commit that I never
     pushed.]

--- crawl-ref/source/enum.h
+++ crawl-ref/source/enum.h
@@ -4558,12 +4558,19 @@ enum seen_context_type
 
 enum los_type
 {
-    LOS_NONE         = 0,
-    LOS_ARENA        = LOS_NONE,
-    LOS_DEFAULT      = (1 << 0),
-    LOS_NO_TRANS     = (1 << 1),
-    LOS_SOLID        = (1 << 2),
-    LOS_SOLID_SEE    = (1 << 3),
+    LOS_NONE         = 0,        // w g s c
+    LOS_ARENA        = LOS_NONE, // -------  See key below
+    LOS_DEFAULT      = (1 << 0), // o T T h
+    LOS_NO_TRANS     = (1 << 1), // o o T h
+    LOS_SOLID        = (1 << 2), // o o o T
+    LOS_SOLID_SEE    = (1 << 3), // o o o h
+    // KEY:
+    //   o: opaque, T: transparent, h: half-opaque (two cells block LOS)
+    // Columns:
+    //   w: FFT_OPAQUE features: rock walls, closed doors, trees, etc.
+    //   g: glass (transparent walls)
+    //   s: other FFT_SOLID features: grate, statue/idol, open/lava sea
+    //   c: semi-opaque clouds (fog, etc.); bushes
 };
 
 enum ac_type
