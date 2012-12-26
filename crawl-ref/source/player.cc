1e8ae84d9398bf284e20c68496b3bb786b042ea2
Raphael Langella <raphael.langella@gmail.com>
2012-12-26 18:31:46 +0100

    Fix a compiler warning.
    declaration of 'strength' shadows a member of 'this'

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -6463,10 +6463,11 @@ bool player::poison(actor *agent, int amount, bool force)
                            force);
 }
 
-void player::expose_to_element(beam_type element, int strength,
+void player::expose_to_element(beam_type element, int _strength,
                                bool damage_inventory, bool slow_cold_blood)
 {
-    ::expose_player_to_element(element, strength, damage_inventory, slow_cold_blood);
+    ::expose_player_to_element(element, _strength, damage_inventory,
+                               slow_cold_blood);
 }
 
 void player::blink(bool allow_partial_control)
