4fab2214f74e7f130492c5763c54d882d52e0957
Raphael Langella <raphael.langella@gmail.com>
2012-12-27 21:33:35 +0100

    Fix aborted weapon switch leaving the player unarmed (#6187).
    Unless the failure was caused by an incompatible and unidientifed brand.

--- crawl-ref/source/item_use.h
+++ crawl-ref/source/item_use.h
@@ -35,7 +35,8 @@ bool can_wear_armour(const item_def &item, bool verbose, bool ignore_temporary);
 bool do_wear_armour(int item, bool quiet);
 
 bool can_wield(item_def *weapon, bool say_why = false,
-               bool ignore_temporary_disability = false, bool unwield = false);
+               bool ignore_temporary_disability = false, bool unwield = false,
+               bool only_known = true);
 
 bool wield_weapon(bool auto_wield, int slot = -1,
                   bool show_weff_messages = true, bool force = false,
