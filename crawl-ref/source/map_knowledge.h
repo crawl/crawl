jmeickle/Crawling-Chaos

Lava orc heat aura!
Conflicts:

	crawl-ref/source/actor.h
	crawl-ref/source/areas.cc
	crawl-ref/source/areas.h
	crawl-ref/source/map_knowledge.h
	crawl-ref/source/monster.h
	crawl-ref/source/player.h
	crawl-ref/source/show.cc
	crawl-ref/source/showsymb.cc

crawl-ref/source/map_knowledge.h
@@ -49,6 +49,7 @@ struct cloud_info
 #define MAP_UMBRAED        0x1000000
 #define MAP_SUPPRESSED     0X2000000
 #define MAP_QUAD_HALOED    0X4000000
+#define MAP_HOT            0x8000000
 
 /*
  * A map_cell stores what the player knows about a cell.

crawl/crawl

crawl-ref/source/map_knowledge.h
@@ -50,6 +50,7 @@ struct cloud_info
 #define MAP_SUPPRESSED     0X2000000
 #define MAP_QUAD_HALOED    0X4000000
 #define MAP_DISJUNCT       0X8000000
+#define MAP_HOT           0x10000000
 
 /*
  * A map_cell stores what the player knows about a cell.