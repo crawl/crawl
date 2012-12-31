ad2c1fc51bdf088078521e114c189af0152116f0
Brendan Hickey <brendan@bhickey.net>
2012-12-30 18:41:52 -0800

    Don't generate stairs on Abyss 5

--- crawl-ref/source/abyss.cc
+++ crawl-ref/source/abyss.cc
@@ -1180,7 +1180,7 @@ static void _abyss_apply_terrain(const map_bitmask &abyss_genlevel_mask,
                                 _abyss_pick_altar(),
                                 abyss_genlevel_mask)
         ||
-        (level_id::current().depth < 27 &&
+        (level_id::current().depth < 5 &&
         _abyss_check_place_feat(p, _abyssal_stair_chance(), NULL, NULL,
                                 DNGN_ABYSSAL_STAIR,
                                 abyss_genlevel_mask))
