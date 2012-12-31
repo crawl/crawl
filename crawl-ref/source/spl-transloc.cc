383dc24dee22e1218458baffe1cff401bef5d906
Neil Moore <neil@s-z.org>
2012-12-31 13:59:38 -0500

    Don't lose mana and time on HUP-aborted cblink. (cherry picked from commit 16b5b170620b4359451b5e5030d2d06433914f65)

--- crawl-ref/source/spl-transloc.cc
+++ crawl-ref/source/spl-transloc.cc
@@ -123,7 +123,7 @@ int blink(int pow, bool high_level_controlled_blink, bool wizard_blink,
     else
     {
         // query for location {dlb}:
-        while (!crawl_state.seen_hups)
+        while (1)
         {
             direction_chooser_args args;
             args.restricts = DIR_TARGET;
@@ -131,6 +131,11 @@ int blink(int pow, bool high_level_controlled_blink, bool wizard_blink,
             args.may_target_monster = false;
             args.top_prompt = "Blink to where?";
             direction(beam, args);
+
+            if (crawl_state.seen_hups) {
+                mpr("Cancelling blink due to HUP.");
+                return -1;
+            }
 
             if (!beam.isValid || beam.target == you.pos())
             {
