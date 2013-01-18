crawl/crawl

commit b8d5efd75ad4f36e5017e07e84a178af632c0ab8

Fix blade hands not being able to wear and remove armour.

crawl-ref/source/item_use.cc
@@ -562,7 +562,7 @@ void wear_armour(int slot) // slot is for tiles
         return;
     }
 
-    if (!player_can_handle_equipment())
+    if (!form_can_wear())
     {
         mpr("You can't wear anything in your present form.");
         return;
@@ -3920,7 +3920,7 @@ static bool _dont_use_invis()
 
 void zap_wand(int slot)
 {
-    if (you.species == SP_FELID || !player_can_handle_equipment())
+    if (you.species == SP_FELID || !form_can_wield())
     {
         mpr("You have no means to grasp a wand firmly enough.");
         return;
@@ -5755,7 +5755,7 @@ void tile_item_use(int idx)
             return;
 
         case OBJ_ARMOUR:
-            if (!player_can_handle_equipment())
+            if (!form_can_wear())
             {
                 mpr("You can't wear or remove anything in your present form.");
                 return;

crawl-ref/source/transform.cc
@@ -63,6 +63,11 @@ bool form_can_wield(transformation_type form)
             || form == TRAN_APPENDAGE);
 }
 
+bool form_can_wear(transformation_type form)
+{
+    return form_can_wield(form) || form == TRAN_BLADE_HANDS;
+}
+
 bool form_can_fly(transformation_type form)
 {
     if (you.species == SP_TENGU

crawl-ref/source/transform.h
@@ -13,6 +13,7 @@
 #include "player.h"
 
 bool form_can_wield(transformation_type form = you.form);
+bool form_can_wear(transformation_type form = you.form);
 bool form_can_fly(transformation_type form = you.form);
 bool form_can_swim(transformation_type form = you.form);
 bool form_likes_water(transformation_type form = you.form);

crawl-ref/source/main.cc
@@ -1693,7 +1693,7 @@ static void _do_remove_armour()
         return;
     }
 
-    if (!player_can_handle_equipment())
+    if (!form_can_wear())
     {
         mpr("You can't wear or remove anything in your present form.");
         return;
@@ -3430,8 +3430,8 @@ static bool _untrap_target(const coord_def move, bool check_confused)
     monster* mon = monster_at(target);
     if (mon && player_can_hit_monster(mon))
     {
-        if (mon->caught() && mon->friendly()
-            && player_can_handle_equipment() && !you.confused())
+        if (mon->caught() && mon->friendly() && form_can_wield()
+            && !you.confused())
         {
             const std::string prompt =
                 make_stringf("Do you want to try to take the net off %s?",
@@ -3457,7 +3457,7 @@ static bool _untrap_target(const coord_def move, bool check_confused)
     {
         if (!you.confused())
         {
-            if (!player_can_handle_equipment())
+            if (!form_can_wield())
             {
                 mpr("You can't disarm traps in your present form.");
                 return (true);

crawl-ref/source/player.cc
@@ -501,15 +501,6 @@ bool player_can_open_doors()
     return (you.form != TRAN_BAT);
 }
 
-bool player_can_handle_equipment()
-{
-    return you.form != TRAN_BAT
-        && you.form != TRAN_PIG
-        && you.form != TRAN_SPIDER
-        && you.form != TRAN_ICE_BEAST
-        && you.form != TRAN_BLADE_HANDS;
-}
-
 bool player_can_reach_floor(std::string feat, bool quiet)
 {
     if (you.flight_mode() != FL_LEVITATE)

crawl-ref/source/player.h
@@ -928,7 +928,6 @@ void gain_exp(unsigned int exp_gained, unsigned int* actual_gain = NULL);
 
 bool player_in_bat_form();
 bool player_can_open_doors();
-bool player_can_handle_equipment();
 bool player_can_reach_floor(std::string feat = "", bool quiet = false);
 
 void level_change(bool skip_attribute_increase = false);

commit 32428f99864bbc8e9852f0b44a7608c52f18e3b8

Fix dragon form being unable to use wands
This was unintentionally caused by b8d5efd75.

crawl-ref/source/item_use.cc
@@ -1735,7 +1735,7 @@ static targetter *_wand_targetter(const item_def *wand)
 
 void zap_wand(int slot)
 {
-    if (you.species == SP_FELID || !form_can_wield())
+    if (you.species == SP_FELID || !form_can_use_wand())
     {
         mpr("You have no means to grasp a wand firmly enough.");
         return;

crawl-ref/source/transform.cc
@@ -113,6 +113,11 @@ bool form_changed_physiology(transformation_type form)
             && form != TRAN_BLADE_HANDS);
 }
 
+bool form_can_use_wand(transformation_type form)
+{
+    return (form_can_wield(form) || form == TRAN_DRAGON);
+}
+
 bool form_can_wear_item(const item_def& item, transformation_type form)
 {
     if (item.base_type == OBJ_JEWELLERY)

crawl-ref/source/transform.h
@@ -19,6 +19,7 @@ bool form_can_swim(transformation_type form = you.form);
 bool form_likes_water(transformation_type form = you.form);
 bool form_can_butcher_barehanded(transformation_type form = you.form);
 bool form_changed_physiology(transformation_type form = you.form);
+bool form_can_use_wand(transformation_type form = you.form);
 bool form_can_wear_item(const item_def& item,
                         transformation_type form = you.form);
 // Does the form keep the benefits of resistance, scale, and aux mutations?