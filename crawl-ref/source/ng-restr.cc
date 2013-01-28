ee82eafa840a9bbe3d5dcf0506245e1c10750f52
elliptic <hyperelliptical@gmail.com>
2013-01-28 17:28:11 -0500

    Unrestrict FeCj, HOCj, SpCj.
    These combos all gained significantly from conjurer changing from ice/air
    to hex (with a little charms).

--- crawl-ref/source/ng-restr.cc
+++ crawl-ref/source/ng-restr.cc
@@ -273,18 +273,15 @@ char_choice_restriction job_allowed(species_type speci, job_type job)
         {
         case SP_SLUDGE_ELF:
         case SP_DEEP_DWARF:
-        case SP_HILL_ORC:
         case SP_MERFOLK:
         case SP_HALFLING:
         case SP_KOBOLD:
-        case SP_SPRIGGAN:
         case SP_CENTAUR:
         case SP_OGRE:
         case SP_TROLL:
         case SP_MINOTAUR:
         case SP_GHOUL:
         case SP_VAMPIRE:
-        case SP_FELID:
             return CC_RESTRICTED;
         default:
             return CC_UNRESTRICTED;
