a5f631d931a72bf3863aecf38fb2bfdb3bddffb6
Samuel Bronson <naesten@gmail.com>
2013-02-25 17:10:36 -0500

    Reduce message spam & improve progress messages in test/uniquegen.lua.
    This seems to make it run a good deal faster.

--- crawl-ref/source/test/uniquegen.lua
+++ crawl-ref/source/test/uniquegen.lua
@@ -1,6 +1,7 @@
 -- Test that randomly generated monsters aren't uniques.
 
 local place = dgn.point(20, 20)
+local place2 = dgn.point(50, 50) -- out-of-view of place, to avoid message spam
 local count = 100
 
 local function test_place_random_monster()
@@ -12,10 +13,13 @@ local function test_place_random_monster()
 end
 
 local function test_random_unique(branch, depth)
-  crawl.message("Running random monster unique tests in branch " .. branch)
+  crawl.message("Running random monster unique tests in branch " .. branch
+                .. " through depth " .. depth)
   debug.flush_map_memory()
   for d = 1, depth do
     debug.goto_place(branch .. ":" .. d)
+    dgn.grid(place2.x, place2.y, "floor")
+    you.moveto(place2.x, place2.y)
     for i = 1, count do
       test_place_random_monster()
     end
