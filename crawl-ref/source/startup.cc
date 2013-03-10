773468989182c176960bc8aca00faa1dd9ac8946
Adam Borowski <kilobyte@angband.pl>
2013-03-10 13:19:44 +0100

    Simplify.
    Neither zapping monsters in LOS nor marking inventory items can affect tiles
    out of view.

--- crawl-ref/source/startup.cc
+++ crawl-ref/source/startup.cc
@@ -282,6 +282,7 @@ static void _post_init(bool newc)
     ash_check_bondage(false);
 
     trackers_init_new_level(false);
+    tile_new_level(newc);
 
     if (newc) // start a new game
     {
@@ -293,14 +294,10 @@ static void _post_init(bool newc)
         // For a new game, wipe out monsters in LOS, and
         // for new hints mode games also the items.
         zap_los_monsters(Hints.hints_events[HINT_SEEN_FIRST_OBJECT]);
-    }
 
-    tile_new_level(newc);
-    
-    // For ZotDef, map the level *after* assigning tiles so the tiles
-    // propagate properly out of LoS and the view doesn't look black.
-    if (newc && crawl_state.game_is_zotdef())
-        fully_map_level();
+        if (crawl_state.game_is_zotdef())
+            fully_map_level();
+    }
 
     // This just puts the view up for the first turn.
     viewwindow();
