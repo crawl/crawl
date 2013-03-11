c437710e1a5f1b6540a038ab0185659c1e5102cf
DracoOmega <draco_omega@live.com>
2013-03-11 17:09:52 -0230

    Don't kill Yred-created zombies when you leave and return to their level

--- crawl-ref/source/monster.cc
+++ crawl-ref/source/monster.cc
@@ -5943,5 +5943,6 @@ bool monster::is_divine_companion() const
     return (attitude == ATT_FRIENDLY
             && !is_summoned()
             && (mons_is_god_gift(this, GOD_BEOGH)
-                || mons_is_god_gift(this, GOD_YREDELEMNUL)));
+                || mons_is_god_gift(this, GOD_YREDELEMNUL))
+            && mons_can_use_stairs(this));
 }
