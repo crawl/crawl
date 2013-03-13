8500c204f9fb5d57483c18dd09fec35c6e9d6d77
DracoOmega <draco_omega@live.com>
2013-03-13 21:02:01 -0230

    Fix being unable to recall companions from one Pan level to another
    This was caused by each Pan level having the same level ID, so recall
    assuming that monsters left on other levels were still present on yours.
    (This also meant that you could recall monsters lost in Pan once outside
    of Pan, but not while inside it)

--- crawl-ref/source/godcompanions.cc
+++ crawl-ref/source/godcompanions.cc
@@ -89,7 +89,7 @@ void populate_offlevel_recall_list(vector<pair<mid_t, int> > &recall_list)
     {
         int mid = i->first;
         companion* comp = &i->second;
-        if (comp->level != level_id::current())
+        if (companion_is_elsewhere(mid, true))
         {
             // Recall can't pull monsters out of the Abyss
             if (comp->level.branch == BRANCH_ABYSS)
@@ -107,46 +107,43 @@ bool recall_offlevel_ally(mid_t mid)
         return false;
 
     companion* comp = &companion_list[mid];
-    if (comp->level != level_id::current())
+    if (comp->mons.place(true))
     {
-        if (comp->mons.place(true))
+        monster* mons = monster_by_mid(mid);
+
+        // The monster is now on this level
+        remove_monster_from_transit(comp->level, mid);
+        comp->level = level_id::current();
+        simple_monster_message(mons, " is recalled.");
+
+        // Catch up time for off-level monsters
+        // (We move the player away so that we don't get expiry
+        // messages for things that supposed wore off ages ago)
+        const coord_def old_pos = you.pos();
+        you.moveto(coord_def(0, 0));
+
+        int turns = you.elapsed_time - comp->timestamp;
+        if (mons_can_regenerate(mons))
         {
-            monster* mons = monster_by_mid(mid);
-
-            // The monster is now on this level
-            remove_monster_from_transit(comp->level, mid);
-            comp->level = level_id::current();
-            simple_monster_message(mons, " is recalled.");
-
-            // Catch up time for off-level monsters
-            // (We move the player away so that we don't get expiry
-            // messages for things that supposed wore off ages ago)
-            const coord_def old_pos = you.pos();
-            you.moveto(coord_def(0, 0));
-
-            int turns = you.elapsed_time - comp->timestamp;
-            if (mons_can_regenerate(mons))
+            if (monster_descriptor(mons->type, MDSC_REGENERATES))
+                mons->heal(turns);
+            else
             {
-                if (monster_descriptor(mons->type, MDSC_REGENERATES))
-                    mons->heal(turns);
-                else
-                {
-                    const int regen_rate =
-                        max(mons_natural_regen_rate(mons) * 2, 5);
-                    mons->heal(div_rand_round(turns * regen_rate, 50));
-                }
+                const int regen_rate =
+                    max(mons_natural_regen_rate(mons) * 2, 5);
+                mons->heal(div_rand_round(turns * regen_rate, 50));
             }
-            if (turns >= 10 && mons->alive())
-            {
-                // Remove confusion manually (so that the monster
-                // doesn't blink after being recalled)
-                mons->del_ench(ENCH_CONFUSION, true);
-                mons->timeout_enchantments(turns / 10);
-            }
-            you.moveto(old_pos);
-
-            return true;
         }
+        if (turns >= 10 && mons->alive())
+        {
+            // Remove confusion manually (so that the monster
+            // doesn't blink after being recalled)
+            mons->del_ench(ENCH_CONFUSION, true);
+            mons->timeout_enchantments(turns / 10);
+        }
+        you.moveto(old_pos);
+
+        return true;
     }
 
     return false;
@@ -155,7 +152,12 @@ bool recall_offlevel_ally(mid_t mid)
 bool companion_is_elsewhere(mid_t mid, bool must_exist)
 {
     if (companion_list.find(mid) != companion_list.end())
-        return (companion_list[mid].level != level_id::current());
+    {
+        return (companion_list[mid].level != level_id::current()
+                || (player_in_branch(BRANCH_PANDEMONIUM)
+                    && companion_list[mid].level.branch == BRANCH_PANDEMONIUM
+                    && !monster_by_mid(mid)));
+    }
 
     return (!must_exist);
 }
