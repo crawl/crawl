07136b026ac0fadf6cb32c404bedd825535a3877
Adam Borowski <kilobyte@angband.pl>
2013-03-21 01:53:40 +0100

    Reintroduce some special handling of wands of digging (Grunt).
    This fixes monsters zapping walls behind you for no reason.

--- crawl-ref/source/mon-act.cc
+++ crawl-ref/source/mon-act.cc
@@ -1470,6 +1470,10 @@ static bool _handle_wand(monster* mons, bolt &beem)
         }
         return false;
 
+    case WAND_DIGGING:
+        // This is handled elsewhere.
+        return false;
+
     default:
         break;
     }
