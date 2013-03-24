e82c9d0a1009e3d415354986edc0fe4e91859860
Adam Borowski <kilobyte@angband.pl>
2013-03-24 22:58:46 +0100

    Upgrade old PUREs to IMMUTABLE.
    Unlike recent additions, these two don't depend on any non-const memory,
    even though their implementation technically does.

--- crawl-ref/source/mon-util.h
+++ crawl-ref/source/mon-util.h
@@ -173,8 +173,8 @@ static inline int get_resist(resists_t all, mon_resist_flags res)
 
 dungeon_feature_type habitat2grid(habitat_type ht);
 
-monsterentry *get_monster_data(monster_type mc) PURE;
-resists_t get_mons_class_resists(monster_type mc) PURE;
+monsterentry *get_monster_data(monster_type mc) IMMUTABLE;
+resists_t get_mons_class_resists(monster_type mc) IMMUTABLE;
 resists_t get_mons_resists(const monster* mon);
 int get_mons_resist(const monster* mon, mon_resist_flags res);
 
