5bc3df1f5f0847a3fd6f4959f84c8e2d0b140b18
Pete Hurst <pete@streamuniverse.tv>
2013-03-29 03:09:55 +0000

    Optimise zonify and add support for filling deep water zones

--- crawl-ref/source/dat/dlua/layout/zonify.lua
+++ crawl-ref/source/dat/dlua/layout/zonify.lua
@@ -19,48 +19,47 @@ function zonify.map(bounds,fcell,fgroup)
     end
     return zonegrid[y][x]
   end
-  for y = y1,y2,1 do
-    for x = x1,x2,1 do
-      local cell = fcell(x,y)
-      if cell ~= nil then
-        local grid = fgrid(x,y)
-        if grid == nil then
-          local group = fgroup(cell)
-          if group ~= nil then
-            if zonemap[group] == nil then zonemap[group] = {} end
-            -- Recursively walk and populate the zone
-            local zone = { group = group, borders = {}, cells = {} }
-            zonify.walk({x=x,y=y},cell,zone,fgrid,fcell,fgroup)
-            table.insert(zonemap[group],zone)
-          end
-        end
-      end
-    end
-  end
+  zonify.walk({x=x1,y=y1},zonemap,fgrid,fcell,fgroup)
   return zonemap
 end
 
 -- Recursively walks cells (like a flood fill) to discover all
 -- cells in the defined zone
-function zonify.walk(pos,cell,zone,fgrid,fcell,fgroup)
-  table.insert(zone.cells, { pos = pos, cell = cell })
-  fgrid(pos.x,pos.y,zone)
-  for i,dir in ipairs(vector.directions) do
-    local x,y = pos.x+dir.x,pos.y+dir.y
-    ncell = fcell(x,y)
-    if ncell ~= nil then
-      local grid = fgrid(x,y)
-      if grid == nil then
-        local group = fgroup(ncell)
-        if group ~= nil then
-          if group == zone.group then
-            zonify.walk({x=x,y=y},ncell,zone,fgrid,fcell,fgroup)
-          else
-            -- TODO: Could recurse further at this point and explore the specified gruop
-            table.insert(zone.borders,{ pos = {x=x,y=y}, anchor = { pos = pos, cell = cell }, cell = cell, group = group } )
-          end
-        end
-      end
+function zonify.walk(pos,zonemap,fgrid,fcell,fgroup,from,zone)
+  local x,y = pos.x,pos.y
+  local grid = fgrid(x,y)
+  if grid ~= nil then return end
+  local cell = fcell(x,y)
+  if cell == nil then return end
+  -- This is a currently unexplored cell
+  local group = fgroup(cell)
+  if group == nil then return end
+  local newzone = false
+
+  if zone == nil then
+    -- Create a new zone
+    zone = { group = group, borders = {}, cells = {} }
+    if zonemap[group] == nil then zonemap[group] = {} end
+    table.insert(zonemap[group],zone)
+    newzone = true
+  end
+
+  if group == zone.group then
+    table.insert(zone.cells, { pos = pos, cell = cell })
+    fgrid(x,y,zone)
+    -- Now recursively check all directions for more zones
+    for i,dir in ipairs(vector.directions) do
+      local nx,ny = pos.x+dir.x,pos.y+dir.y
+      zonify.walk({x=nx,y=ny},zonemap,fgrid,fcell,fgroup,cell,zone)
+    end
+  else
+    table.insert(zone.borders,{ pos = pos, anchor = { pos = pos, cell = from }, cell = cell, group = group } )
+  end
+  if newzone then
+    zone.done = true
+    -- Recurse with a new zone for each bordering square
+    for i,b in ipairs(zone.borders) do
+      zonify.walk(b.pos,zonemap,fgrid,fcell,fgroup,nil,nil)
     end
   end
 end
@@ -110,7 +109,7 @@ function zonify.map_map(e)
   -- TODO: Can we check size of current map after extend_map?
   local gxm,gym = dgn.max_bounds()
   return zonify.map(
-    { x1 = 0, y1 = 0, x2 = gxm-1, y2 = gym-1 },
+    { x1 = 1, y1 = 1, x2 = gxm-2, y2 = gym-2 },
     function(x,y)
       return dgn.in_bounds(x,y) and e.mapgrd[x][y] or nil
     end,
@@ -127,7 +126,6 @@ function zonify.map_fill_zones(e,num_to_keep,glyph)
 
   local zonemap = zonify.map_map(e)
   zonify.fill_smallest_zones(zonemap,1,"floor",function(x,y,cell) e.mapgrd[x][y] = glyph end)
-
 end
 
 -- Zonifies the current dungeon grid
@@ -136,25 +134,46 @@ function zonify.grid_map(e)
   -- TODO: Allow analysing within arbitrary bounds
   local gxm,gym = dgn.max_bounds()
   return zonify.map(
-    { x1 = 0, y1 = 0, x2 = gxm-1, y2 = gym-1 },
-    function(x,y)
-      -- Out of bounds
-      if not dgn.in_bounds(x,y) then return nil end
-      return {
-        feat = dgn.grid(x,y),
-        vault = dgn.in_vault(x,y),
-        passable = dgn.is_passable(x,y)
-      }
-    end,
-    function(val)
-      if val.vault then return "vault" end
-      return val.passable and "floor" or "wall"
-    end
-  )
+    { x1 = 1, y1 = 1, x2 = gxm-2, y2 = gym-2 },
+    zonify.grid_analyse_coord,zonify.grid_group_feature)
+end
+
+function zonify.grid_analyse_coord(x,y)
+  -- Out of bounds
+  if not dgn.in_bounds(x,y) then return nil end
+  return {
+    feat = dgn.feature_name(dgn.grid(x,y)),
+    vault = dgn.in_vault(x,y),
+    passable = dgn.is_passable(x,y)
+  }
+end
+
+function zonify.grid_group_feature(val)
+  if val.vault then return "vault" end
+  return val.passable and "floor" or "wall"
 end
 
 function zonify.grid_fill_zones(num_to_keep,feature)
   if feature == nil then feature = 'rock_wall' end
   local zonemap = zonify.grid_map()
+  zonify.fill_smallest_zones(zonemap,1,"floor",function(x,y,cell) dgn.grid(x,y,feature) end)
+end
+
+-- This is a little brutish but for some layouts (e.g. swamp) a different pass
+-- is needed to fill in deep water zones and stop flyers/swimmers getting trapped.
+-- TODO: Need to simplify into a single pass that understands connectivity issues
+-- between different zone groups and can handle everything more elegantly.
+function zonify.grid_fill_water_zones(num_to_keep,feature)
+  if feature == nil then feature = 'rock_wall' end
+  local gxm,gym = dgn.max_bounds()
+  local zonemap = zonify.map(
+    { x1 = 1, y1 = 1, x2 = gxm-2, y2 = gym-2 },
+    zonify.grid_analyse_coord,
+    -- This grouping function treats deep water as floor so it'll get filled
+    -- in when surrounded by wall
+    function(val)
+      if val.vault then return "vault" end
+      return (val.passable or val.feat == "deep_water") and "floor" or "wall"
+    end)
   zonify.fill_smallest_zones(zonemap,1,"floor",function(x,y,cell) dgn.grid(x,y,feature) end)
 end
