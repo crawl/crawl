8b2b6b6a42387a48c808348270b445e0543661da
Raphael Langella <raphael.langella@gmail.com>
2013-03-30 15:52:31 +0100

    Disable energy randomization for allies following the player.
    Like f542eec, it doesn't serve much other purpose than traffic jam.

--- crawl-ref/source/monster.cc
+++ crawl-ref/source/monster.cc
@@ -5270,7 +5270,8 @@ void monster::lose_energy(energy_use_type et, int div, int mult)
 
     // Randomize movement cost slightly, to make it less predictable,
     // and make pillar-dancing not entirely safe.
-    if (et == EUT_MOVE || et == EUT_SWIM)
+    // No randomization for allies following you to avoid traffic jam
+    if ((et == EUT_MOVE || et == EUT_SWIM) && (!friendly() or foe != MHITYOU))
         energy_loss += random2(3) - 1;
 
     speed_increment -= energy_loss;
