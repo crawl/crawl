e904e0d0ebbe3381f1b6455d45c1e7bb66ca8d66
DracoOmega <draco_omega@live.com>
2013-04-02 02:15:37 -0230

    Make Chaos Knights start with +0 chaos weapons instead of +2 normal ones
    This is generally a buff, but such a thing is also not really worrisome
    for CKs and is also highly in-theme (and rather fun to boot).

--- crawl-ref/source/ng-setup.cc
+++ crawl-ref/source/ng-setup.cc
@@ -619,8 +619,8 @@ static void _give_items_skills(const newgame_def& ng)
         you.piety = 100;
         you.gift_timeout = max(5, random2(40) + random2(40));
 
-        newgame_make_item(0, EQ_WEAPON, OBJ_WEAPONS, WPN_SHORT_SWORD, -1, 1,
-                           2, 2);
+        newgame_make_item(0, EQ_WEAPON, OBJ_WEAPONS, WPN_SHORT_SWORD);
+        set_item_ego_type(you.inv[0], OBJ_WEAPONS, SPWPN_CHAOS);
         _update_weapon(ng);
 
         newgame_make_item(1, EQ_BODY_ARMOUR, OBJ_ARMOUR, ARM_LEATHER_ARMOUR,
