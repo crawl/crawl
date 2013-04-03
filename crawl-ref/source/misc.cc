86acf04fdf9e68fb967a94a0f1bf2cfc43fd7329
Adam Borowski <kilobyte@angband.pl>
2013-04-03 16:24:52 +0200

    Don't confirm attacks if confirmations are disabled.
    This notoriously blocks the Abyss stress bot, due to Xom messing with
    starspawn tentacles.

--- crawl-ref/source/misc.cc
+++ crawl-ref/source/misc.cc
@@ -2322,6 +2322,9 @@ bool bad_attack(const monster *mon, string& adj, string& suffix)
 bool stop_attack_prompt(const monster* mon, bool beam_attack,
                         coord_def beam_target, bool autohit_first)
 {
+    if (crawl_state.disables[DIS_CONFIRMATIONS])
+        return false;
+
     if (you.confused() || !you.can_see(mon))
         return false;
 
@@ -2372,6 +2375,9 @@ bool stop_attack_prompt(const monster* mon, bool beam_attack,
 bool stop_attack_prompt(targetter &hitfunc, string verb,
                         bool (*affects)(const actor *victim))
 {
+    if (crawl_state.disables[DIS_CONFIRMATIONS])
+        return false;
+
     if (crawl_state.which_god_acting() == GOD_XOM)
         return false;
 
