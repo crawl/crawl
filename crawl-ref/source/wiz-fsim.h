e6f1cd633cff7d7370d4521f7c9f11290087e7a0
Adam Borowski <kilobyte@angband.pl>
2013-04-03 14:29:32 +0200

    Add a missing initializer.

--- crawl-ref/source/wiz-fsim.h
+++ crawl-ref/source/wiz-fsim.h
@@ -6,14 +6,15 @@
 #ifndef WIZFSIM_H
 #define WIZFSIM_H
 
-struct fight_data {
-double av_hit_dam;
-int max_dam;
-int accuracy;
-double av_dam;
-int av_time;
-double av_speed;
-double av_eff_dam;
+struct fight_data
+{
+    double av_hit_dam;
+    int max_dam;
+    int accuracy;
+    double av_dam;
+    int av_time;
+    double av_speed;
+    double av_eff_dam;
 };
 
 void wizard_quick_fsim();
