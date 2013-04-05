6ca4a9d1f8d78b56622b7af78bc351f9052b2c56
Pekka Lampila <pekka.lampila@iki.fi>
2013-04-05 05:24:56 +0300

    Don't allow treeform to go up or down stairs

--- crawl-ref/source/stairs.cc
+++ crawl-ref/source/stairs.cc
@@ -317,6 +317,12 @@ void up_stairs(dungeon_feature_type force_stair)
                                        : grd(you.pos()));
     const level_id  old_level = level_id::current();
 
+    if (you.form == TRAN_TREE)
+    {
+        mpr("You cannot move.");
+        return;
+    }
+
     // Up and down both work for shops.
     if (stair_find == DNGN_ENTER_SHOP)
     {
@@ -640,6 +646,12 @@ void down_stairs(dungeon_feature_type force_stair)
                             && get_trap_type(you.pos()) == TRAP_SHAFT
                         || force_stair == DNGN_TRAP_NATURAL);
     level_id shaft_dest;
+
+    if (you.form == TRAN_TREE)
+    {
+        mpr("You cannot move.");
+        return;
+    }
 
     // Up and down both work for shops.
     if (stair_find == DNGN_ENTER_SHOP)
