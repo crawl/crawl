3e2da083cd007ac913ac293d8409764697a8ec3a
Brendan Hickey <brendan@bhickey.net>
2013-04-06 21:02:21 -0700

    Collaborative Diffusion
    Define an abstract DiffusionGrid class. Implementors will provide a score() function which consumes an initial score, a coordinate and renders score for this coordinate.
    process() constructs a gradient grid (FixedArray< float, GXM, GYM>) using seed points created with addPoint. Low scores are more important.
    
    Please see www.cs.colorado.edu/~ralex/papers/PDF/OOPSLA06antiobjects.pdf for more information.

---
+++ crawl-ref/source/diffusion.h
@@ -0,0 +1,89 @@
+/**
+ * @file
+ * @brief Collaborative diffusion for monster AI.
+ **/
+
+#ifndef DIFFUSION_H
+#define DIFFUSION_H
+
+#include "AppHdr.h"
+
+#include <float.h>
+#include <queue>
+#include <vector>
+
+#include "coordit.h"
+#include "externs.h"
+
+class DECompare;
+class DiffusionElement;
+class DiffusionGrid;
+
+typedef std::priority_queue<
+    DiffusionElement,
+    std::vector<DiffusionElement>,
+    DECompare> diffusion_queue;
+
+class DiffusionElement
+{
+    public:
+        DiffusionElement(const float _f, const coord_def &_c) :
+            f(_f), c(_c) {}
+    private:
+        float f;
+        coord_def c;
+    friend class DECompare;
+    friend class DiffusionGrid;
+};
+
+class DECompare
+{
+    public:
+        bool operator() (const DiffusionElement &lhs,
+            const DiffusionElement &rhs)
+        {
+            return lhs.f > rhs.f;
+        }
+};
+
+class DiffusionGrid {
+  public:
+    DiffusionGrid()
+    {
+        arr = FixedArray< float, GXM, GYM>(DBL_MIN);
+    }
+
+    void addPoint(float f, coord_def c)
+    {
+        DiffusionElement de = score(f, c);
+        diffeq.push(de);
+        arr(c) = de.f;
+    }
+
+    FixedArray< float, GXM, GYM > process()
+    {
+        while (!diffeq.empty()) {
+            DiffusionElement e = diffeq.top();
+            for (adjacent_iterator ai(e.c); ai; ++ai)
+            {
+                if (arr(e.c) == DBL_MIN) {
+                    addPoint(e.f, e.c);
+                }
+            }
+            diffeq.pop();
+        }
+        return arr;
+    }
+
+    void clear()
+    {
+        arr = FixedArray< float, GXM, GYM >(DBL_MIN);
+    }
+  protected:
+    virtual DiffusionElement score(float f, coord_def c);
+  private:
+    FixedArray< float, GXM, GYM > arr;
+    diffusion_queue diffeq;
+};
+
+#endif /* DIFFUSION_H */
