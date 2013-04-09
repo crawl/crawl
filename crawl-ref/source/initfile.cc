crawl/crawl

commit 8bf01c43c17c1111a580daa0e3906cae1cccef6a

Use FixedBitArray instead of large arrays of bools.
The only cost is having assignments be a function, as you can't overload
operator[] to have separate bits be lvalues.

crawl-ref/source/initfile.cc
@@ -552,8 +552,8 @@ void game_options::set_default_activity_interrupts()
     for (int adelay = 0; adelay < NUM_DELAYS; ++adelay)
         for (int aint = 0; aint < NUM_AINTERRUPTS; ++aint)
         {
-            activity_interrupts[adelay][aint]
-                = is_delay_interruptible(static_cast<delay_type>(adelay));
+            activity_interrupts[adelay].set(aint,
+                is_delay_interruptible(static_cast<delay_type>(adelay)));
         }
 
     const char *default_activity_interrupts[] = {
@@ -588,15 +588,8 @@ void game_options::set_default_activity_interrupts()
         read_option_line(default_activity_interrupts[i], false);
 }
 
-void game_options::clear_activity_interrupts(
-        FixedVector<bool, NUM_AINTERRUPTS> &eints)
-{
-    for (int i = 0; i < NUM_AINTERRUPTS; ++i)
-        eints[i] = false;
-}
-
 void game_options::set_activity_interrupt(
-        FixedVector<bool, NUM_AINTERRUPTS> &eints,
+        FixedBitVector<NUM_AINTERRUPTS> &eints,
         const string &interrupt)
 {
     if (interrupt.find(interrupt_prefix) == 0)
@@ -606,13 +599,10 @@ void game_options::set_activity_interrupt(
         if (delay == NUM_DELAYS)
             return report_error("Unknown delay: %s\n", delay_name.c_str());
 
-        FixedVector<bool, NUM_AINTERRUPTS> &refints =
+        FixedBitVector<NUM_AINTERRUPTS> &refints =
             activity_interrupts[delay];
 
-        for (int i = 0; i < NUM_AINTERRUPTS; ++i)
-            if (refints[i])
-                eints[i] = true;
-
+        refints |= eints;
         return;
     }
 
@@ -623,7 +613,7 @@ void game_options::set_activity_interrupt(
                             interrupt.c_str());
     }
 
-    eints[ai] = true;
+    eints.set(ai);
 }
 
 void game_options::set_activity_interrupt(const string &activity_name,
@@ -636,29 +626,28 @@ void game_options::set_activity_interrupt(const string &activity_name,
         return report_error("Unknown delay: %s\n", activity_name.c_str());
 
     vector<string> interrupts = split_string(",", interrupt_names);
-    FixedVector<bool, NUM_AINTERRUPTS> &eints = activity_interrupts[ delay ];
+    FixedBitVector<NUM_AINTERRUPTS> &eints = activity_interrupts[ delay ];
 
     if (remove_interrupts)
     {
-        FixedVector<bool, NUM_AINTERRUPTS> refints;
-        clear_activity_interrupts(refints);
+        FixedBitVector<NUM_AINTERRUPTS> refints;
         for (int i = 0, size = interrupts.size(); i < size; ++i)
             set_activity_interrupt(refints, interrupts[i]);
 
         for (int i = 0; i < NUM_AINTERRUPTS; ++i)
             if (refints[i])
-                eints[i] = false;
+                eints.set(i, false);
     }
     else
     {
         if (!append_interrupts)
-            clear_activity_interrupts(eints);
+            eints.reset();
 
         for (int i = 0, size = interrupts.size(); i < size; ++i)
             set_activity_interrupt(eints, interrupts[i]);
     }
 
-    eints[AI_FORCE_INTERRUPT] = true;
+    eints.set(AI_FORCE_INTERRUPT);
 }
 
 #if defined(DGAMELAUNCH)

crawl-ref/source/arena.cc
@@ -411,7 +411,7 @@ namespace arena
         for (int i = 0; i < MAX_MONSTERS; i++)
             to_respawn[i] = -1;
 
-        unwind_var< FixedVector<bool, NUM_MONSTERS> >
+        unwind_var< FixedBitVector<NUM_MONSTERS> >
             uniq(you.unique_creatures);
 
         place_a = dgn_find_feature_marker(DNGN_STONE_STAIRS_UP_I);
@@ -1024,7 +1024,7 @@ monster_type arena_pick_random_monster(const level_id &place)
         const vector<monster_type> &uniques = arena::uniques_list;
 
         const monster_type type = uniques[random2(uniques.size())];
-        you.unique_creatures[type] = false;
+        you.unique_creatures.set(type, false);
 
         return type;
     }

crawl-ref/source/bitary.h
@@ -94,6 +94,13 @@ public:
             data[i] &= x.data[i];
         return *this;
     }
+
+    void init(bool value)
+    {
+        long fill = value ? ~(long)0 : 0;
+        for (unsigned int i = 0; i < ARRAYSZ(data); i++)
+            data[i] = fill;
+    }
 };
 
 template <unsigned int SIZEX, unsigned int SIZEY> class FixedBitArray

crawl-ref/source/dbg-maps.cc
@@ -199,7 +199,7 @@ static void mg_build_levels(int niters)
         dlua.callfn("dgn_clear_data", "");
         you.uniq_map_tags.clear();
         you.uniq_map_names.clear();
-        you.unique_creatures.init(false);
+        you.unique_creatures.reset();
         init_level_connectivity();
         if (!mg_build_dungeon())
             break;

crawl-ref/source/dbg-scan.cc
@@ -71,8 +71,7 @@ void debug_item_scan(void)
     int   i;
     char  name[256];
 
-    FixedVector<bool, MAX_ITEMS> visited;
-    visited.init(false);
+    FixedBitVector<MAX_ITEMS> visited;
 
     // First we're going to check all the stacks on the level:
     for (rectangle_iterator ri(0); ri; ++ri)
@@ -124,7 +123,7 @@ void debug_item_scan(void)
                      "Potential INFINITE STACK at (%d, %d)", ri->x, ri->y);
                 break;
             }
-            visited[obj] = true;
+            visited.set(obj);
         }
     }
 

crawl-ref/source/describe.cc
@@ -4251,7 +4251,7 @@ static void _detailed_god_description(god_type which_god)
         && keyin >= 'a' && keyin < 'a' + (char) NUM_NEMELEX_GIFT_TYPES)
     {
         const int num = keyin - 'a';
-        you.nemelex_sacrificing[num] = !you.nemelex_sacrificing[num];
+        you.nemelex_sacrificing.set(num, !you.nemelex_sacrificing[num]);
         _detailed_god_description(which_god);
     }
     else if (keyin == '!' || keyin == CK_MOUSE_CMD)

crawl-ref/source/dungeon.cc
@@ -199,7 +199,7 @@ static void _mark_solid_squares();
 
 // A mask of vaults and vault-specific flags.
 vector<vault_placement> Temp_Vaults;
-static FixedVector<bool, NUM_MONSTERS> temp_unique_creatures;
+static FixedBitVector<NUM_MONSTERS> temp_unique_creatures;
 static FixedVector<unique_item_status_type, MAX_UNRANDARTS> temp_unique_items;
 
 const map_bitmask *Vault_Placement_Mask = NULL;
@@ -4878,7 +4878,7 @@ static void _vault_grid_glyph_mons(vault_placement &place,
                      "else; please file a bug report.",
                      mons_type_name(mt, DESC_THE).c_str());
                 // Force it to be generated anyway.
-                you.unique_creatures[mt] = false;
+                you.unique_creatures.set(mt, false);
             }
         }
 

crawl-ref/source/files.cc
@@ -1092,7 +1092,7 @@ static void _do_lost_monsters()
     if (player_in_branch(BRANCH_PANDEMONIUM))
         for (monster_iterator mi; mi; ++mi)
             if (mons_is_unique(mi->type) && !(mi->flags & MF_TAKING_STAIRS))
-                you.unique_creatures[mi->type] = false;
+                you.unique_creatures.set(mi->type, false);
 }
 
 // Should be called after _grab_followers(), so that items carried by

commit 09d78cf8c7553742b6a63ac15c4a11c7f6997217

Fix some reversed logic causing initfile interrupts to break (#6897).
The interrupts was broken by 8bf01c4 thanks to this line's assignment
erroneously being reversed.

crawl-ref/source/initfile.cc
@@ -602,7 +602,7 @@ void game_options::set_activity_interrupt(
         FixedBitVector<NUM_AINTERRUPTS> &refints =
             activity_interrupts[delay];
 
-        refints |= eints;
+        eints |= refints;
         return;
     }
 