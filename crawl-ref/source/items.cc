f2319b7c5f6468891df710685f3312144d2a175a
Pekka Lampila <pekka.lampila@iki.fi>
2013-04-12 19:35:17 +0300

    Copy needs_autopickup prop in get_item_info
    Fixes autopickup graphic not showing up for certain items (some
    jewellery identified by Ash).

--- crawl-ref/source/items.cc
+++ crawl-ref/source/items.cc
@@ -3985,7 +3985,8 @@ item_info get_item_info(const item_def& item)
     const char* copy_props[] = {ARTEFACT_APPEAR_KEY, KNOWN_PROPS_KEY,
                                 CORPSE_NAME_KEY, CORPSE_NAME_TYPE_KEY,
                                 "drawn_cards", "item_tile", "item_tile_name",
-                                "worn_tile", "worn_tile_name"};
+                                "worn_tile", "worn_tile_name",
+                                "needs_autopickup"};
     for (unsigned i = 0; i < ARRAYSZ(copy_props); ++i)
     {
         if (item.props.exists(copy_props[i]))
