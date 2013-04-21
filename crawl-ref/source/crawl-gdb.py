252a1e6100ca6a277dcee9529add090f9ce7d64a
Samuel Bronson <naesten@gmail.com>
2013-04-20 21:36:01 -0400

    Preliminary GDB pretty-printer for item_def.

--- crawl-ref/source/crawl-gdb.py
+++ crawl-ref/source/crawl-gdb.py
@@ -33,6 +33,72 @@ class actor_printer:
             self.val["type"], int(self.val["mid"]), self.val["position"])
 
 
+class item_def_printer:
+    def __init__(self, val):
+        self.val = val
+
+    def to_string(self):
+        return None
+
+    def children(self):
+        # Since C++ hasn't got dependant types, we have to simulate them here.
+        def f(fname):
+            return (fname, self.val[fname])
+        def g(fname, typename):
+            return (fname, self.val[fname].cast(gdb.lookup_type(typename)))
+
+        ty = str(self.val['base_type'])
+        sub_type_type = {
+            'OBJ_WEAPONS':   'weapon_type',
+            'OBJ_MISSILES':  'missile_type',
+            'OBJ_ARMOUR':    'armour_type',
+            'OBJ_WANDS':     'wand_type',
+            'OBJ_FOOD':      'food_type',
+            'OBJ_SCROLLS':   'scroll_type',
+            'OBJ_JEWELLERY': 'jewellery_type',
+            'OBJ_POTIONS':   'potion_type',
+            'OBJ_BOOKS':     'book_type',
+            'OBJ_STAVES':    'stave_type',
+            'OBJ_ORBS':      'orb_type',
+            'OBJ_MISCELLANY':'misc_item_type',
+            'OBJ_CORPSES':   'corpse_type',
+            # 'OBJ_GOLD':
+            'OBJ_RODS':      'rod_type'
+            }.get(f, 'uint8_t')
+        sub_ty = str(self.val['sub_type'].cast(gdb.lookup_type(sub_type_type)))
+
+        yield f('base_type')
+        yield g('sub_type', sub_type_type)
+
+        if ty == 'OBJ_CORPSE' or (ty == 'OBJ_FOOD' and sub_ty == 'FOOD_CHUNK'):
+            yield f('mon_type')
+        else:
+            yield f('plus')
+
+        plus2typename = {
+            ('OBJ_ARMOUR', 'ARM_HELMET'):'helmet_desc_type',
+            ('OBJ_ARMOUR', 'ARM_BOOTS'):'type_boots',
+            ('OBJ_ARMOUR', 'ARM_GLOVES'):'gloves_desc_type',
+            }.get((ty, sub_ty), 'short')
+        yield g('plus2', plus2typename)
+
+        yield f('special')
+        yield f('colour')
+        yield f('rnd')
+        yield f('quantity')
+        yield f('flags')
+
+        yield f('pos')
+        yield f('link')
+        yield f('slot')
+
+        yield f('orig_place')
+        yield f('orig_monnum')
+
+        yield f('inscription')
+        yield f('props')
+
+
 class FixedVector_printer:
     def __init__(self, val):
         self.val = val
@@ -130,6 +196,7 @@ def build_pretty_printer():
 
     pp.add_printer('coord_def', '^coord_def$', coord_def_printer)
 #   pp.add_printer('actor', '^actor$', actor_printer)
+    pp.add_printer('item_def', '^item_def$', item_def_printer)
 
     pp.add_printer('FixedVector', '^FixedVector<.*>$', FixedVector_printer)
     pp.add_printer('FixedArray', '^FixedArray<.*>$', FixedVector_printer)
