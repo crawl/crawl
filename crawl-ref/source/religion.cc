68e6ee8d9022a74db510756b727402b40379524f
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2013-05-23 14:05:06 -0500

    Mention destroying decks as disliked by Nemelex in the "^!" screen.

--- crawl-ref/source/religion.cc
+++ crawl-ref/source/religion.cc
@@ -876,6 +876,10 @@ string get_god_dislikes(god_type which_god, bool /*verbose*/)
         really_dislikes.push_back("you destroy spellbooks");
         break;
 
+    case GOD_NEMELEX_XOBEH:
+        really_dislikes.push_back("you destroy decks");
+        break;
+
     default:
         break;
     }
