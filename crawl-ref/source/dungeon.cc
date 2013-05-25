d8d3bbd920f51e2ba5a499495894d874439ce045
Neil Moore <neil@s-z.org>
2013-05-25 14:46:11 -0400

    Make superb_item etc. cat-agnostic.
    It was using only cat-friendly base types, affecting tomb, zig, and elf
    loot.  This affected "ITEM: x = |" but not the plain "|" glyph, which
    was and still is handled by different code (!)

--- crawl-ref/source/dungeon.cc
+++ crawl-ref/source/dungeon.cc
@@ -4285,7 +4285,7 @@ int dgn_place_item(const item_spec &spec,
         else if (adjust_type && base_type == OBJ_RANDOM)
         {
             base_type = _acquirement_item_classes[random2(
-                            you.species == SP_FELID ? NC_KITTEHS :
+                            (you.species == SP_FELID && acquire) ? NC_KITTEHS :
                             NC_LESSER_LIFE_FORMS)];
         }
     }
