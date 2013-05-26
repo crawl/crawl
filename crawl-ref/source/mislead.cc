3b50b19d909d12ac1a11b1b0bd5d0b8c86423815
Adam Borowski <kilobyte@angband.pl>
2013-05-26 07:43:43 +0200

    A monster flag M_CANT_SPAWN, to mark dummy monsters.
    Genus-only, recolouring, etc.  Not for "dummies" which are valid monsters
    otherwise.
    
    This is mostly redundant with M_NO_POLY_TO which it implies, for now.

--- crawl-ref/source/mislead.cc
+++ crawl-ref/source/mislead.cc
@@ -29,7 +29,7 @@ static bool _unsuitable_misled_monster(monster_type mons)
             || mons_class_is_stationary(mons)
             || mons_class_is_zombified(mons)
             || mons_is_tentacle(mons)
-            || mons_class_flag(mons, M_NO_POLY_TO)
+            || mons_class_flag(mons, M_NO_POLY_TO | M_CANT_SPAWN)
             || mons_class_flag(mons, M_UNFINISHED)
             || mons_genus(mons) == MONS_DRACONIAN
             || mons == MONS_MANTICORE
