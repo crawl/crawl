006c083f99f6ef797d9acfa2fc74b175bcdff765
David Lawrence Ramsey <dolorous@users.sourceforge.net>
2013-05-26 20:14:57 -0500

    Use random_choose_weighted() for Haunt's monster selection.

--- crawl-ref/source/spl-summoning.cc
+++ crawl-ref/source/spl-summoning.cc
@@ -2264,13 +2264,14 @@ spret_type cast_haunt(int pow, const coord_def& where, god_type god, bool fail)
 
     while (to_summon--)
     {
-        const int chance = random2(25);
-        monster_type mon = ((chance > 22) ? MONS_PHANTOM :            //  8%
-                            (chance > 20) ? MONS_HUNGRY_GHOST :       //  8%
-                            (chance > 18) ? MONS_SHADOW_WRAITH:       //  8%
-                            (chance >  8) ? MONS_WRAITH :             // 41%
-                            (chance >  4) ? MONS_FREEZING_WRAITH      // 16%
-                                          : MONS_PHANTASMAL_WARRIOR); // 16%
+        const monster_type mon =
+            random_choose_weighted(1, MONS_PHANTOM,
+                                   1, MONS_HUNGRY_GHOST,
+                                   1, MONS_SHADOW_WRAITH,
+                                   5, MONS_WRAITH,
+                                   2, MONS_FREEZING_WRAITH,
+                                   2, MONS_PHANTASMAL_WARRIOR,
+                                   0);
 
         if (monster *mons = create_monster(
                 mgen_data(mon,
