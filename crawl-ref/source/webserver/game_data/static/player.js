1f3dc0b68c543cca2113e7f1f765062d77153914
Florian Diebold <flodiebold@gmail.com>
2013-05-30 12:20:48 +0200

    Send correct max hp for Djinni to the webtiles client.

--- crawl-ref/source/webserver/game_data/static/player.js
+++ crawl-ref/source/webserver/game_data/static/player.js
@@ -255,13 +255,8 @@ function ($, comm, enums, map_knowledge, messages) {
         else
             $(".stats_zot_defense").hide();
 
-        // Adjust max HP correctly for Djinni
-        var max_max_hp = player.real_hp_max;
-        if (player.species == "Djinni")
-            max_max_hp += player.mp_max;
-
-        if (max_max_hp != player.hp_max)
-            $("#stats_real_hp_max").text("(" + max_max_hp + ")");
+        if (player.real_hp_max != player.hp_max)
+            $("#stats_real_hp_max").text("(" + player.real_hp_max + ")");
         else
             $("#stats_real_hp_max").text("");
 
