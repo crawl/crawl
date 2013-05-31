5349e66d5bd3e31b6d93a18358659d417f0a2b5f
Adam Borowski <kilobyte@angband.pl>
2013-05-31 03:19:51 +0200

    Revert "Djinn: ... including Jiyva's one."
    That mutation is no more, and no djinni was around when it could be given.
    
    This reverts commit 72b3ab37f0fbe0fb5e98a90fb9c5b3a8802eb388.

--- crawl-ref/source/mutation.cc
+++ crawl-ref/source/mutation.cc
@@ -1348,8 +1348,7 @@ bool physiology_mutation_conflict(mutation_type mutat)
     // Heat doesn't hurt fire, djinn don't care about hunger.
     if (you.species == SP_DJINNI && (mutat == MUT_HEAT_RESISTANCE
         || mutat == MUT_FAST_METABOLISM || mutat == MUT_SLOW_METABOLISM
-        || mutat == MUT_CARNIVOROUS || mutat == MUT_HERBIVOROUS
-        || mutat == MUT_FOOD_JELLY))
+        || mutat == MUT_CARNIVOROUS || mutat == MUT_HERBIVOROUS))
     {
         return true;
     }
