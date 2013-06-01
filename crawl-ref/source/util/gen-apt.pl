d437afd87a1f9dbd39be24667918a4a98fde056a
Adam Borowski <kilobyte@angband.pl>
2013-06-01 08:17:41 +0200

    Remove the line for sludge Elves from the aptitudes table.

--- crawl-ref/source/util/gen-apt.pl
+++ crawl-ref/source/util/gen-apt.pl
@@ -123,6 +123,8 @@ sub aptitude_table
     my $seen_draconian_length;
     for my $sp (sort_species(@SPECIES))
     {
+        next if $sp eq 'Sludge Elf';
+
         my $line = '';
         $line .= fix_draco_species($sp, \$seen_draconian_length);
 
