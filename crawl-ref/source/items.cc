crawl/crawl

commit f5d745b671aed2eb61b24e2eda74d089323679b0

Rework and unify passive acid code, give it (and active acid) to jelly form.
This makes monsters suffer acid when attacking a monster jelly, too.

I removed the message when an item resists acid: it was given in some
cases but not others.

crawl-ref/source/items.cc
@@ -4099,3 +4099,82 @@ bool is_valid_mimic_item(object_class_type type)
             return true;
     return false;
 }
+
+void corrode_item(item_def &item, actor *holder)
+{
+    // Artefacts don't corrode.
+    if (is_artefact(item))
+        return;
+
+    // Anti-corrosion items protect against 90% of corrosion.
+    if (holder && holder->res_corr() && !one_chance_in(10))
+    {
+        dprf("Amulet protects.");
+        return;
+    }
+
+    int how_rusty = ((item.base_type == OBJ_WEAPONS) ? item.plus2 : item.plus);
+    // Already very rusty.
+    if (how_rusty < -5)
+        return;
+
+    // determine possibility of resistance by object type {dlb}:
+    switch (item.base_type)
+    {
+    case OBJ_ARMOUR:
+        if ((item.sub_type == ARM_CRYSTAL_PLATE_ARMOUR
+             || get_equip_race(item) == ISFLAG_DWARVEN)
+            && !one_chance_in(5))
+        {
+            return;
+        }
+        break;
+
+    case OBJ_WEAPONS:
+        if (get_equip_race(item) == ISFLAG_DWARVEN && !one_chance_in(5))
+            return;
+        break;
+
+    default:
+        // Other items can't corrode.
+        return;
+    }
+
+    // determine chance of corrosion {dlb}:
+    const int chance = abs(how_rusty);
+
+    // The embedded equation may look funny, but it actually works well
+    // to generate a pretty probability ramp {6%, 18%, 34%, 58%, 98%}
+    // for values [0,4] which closely matches the original, ugly switch.
+    // {dlb}
+    if (chance < 0 || chance > 4
+        || !x_chance_in_y(2 + (4 << chance) + chance * 8, 100))
+    {
+        return;
+    }
+
+    how_rusty--;
+
+    if (item.base_type == OBJ_WEAPONS)
+        item.plus2 = how_rusty;
+    else
+        item.plus  = how_rusty;
+
+    if (holder && holder->is_player())
+    {
+        mprf("The acid corrodes %s!", item.name(DESC_YOUR).c_str());
+        xom_is_stimulated(50);
+
+        if (item.base_type == OBJ_ARMOUR)
+            you.redraw_armour_class = true;
+
+        if (you.equip[EQ_WEAPON] == item.link)
+            you.wield_change = true;
+    }
+    else if (holder)
+    {
+        mprf("The acid corrodes %s %s!",
+             apostrophise(holder->name(DESC_THE)).c_str(),
+             item.name(DESC_PLAIN).c_str());
+    }
+}

crawl-ref/source/items.h
@@ -140,6 +140,7 @@ equipment_type item_equip_slot(const item_def &item);
 
 void item_was_lost(const item_def &item);
 void item_was_destroyed(const item_def &item, int cause = -1);
+void corrode_item(item_def &item, actor *holder);
 
 bool get_item_by_name(item_def *item, char* specs,
                       object_class_type class_wanted,

crawl-ref/source/melee_attack.cc
@@ -3412,9 +3412,38 @@ void melee_attack::attacker_sustain_passive_damage()
     if (defender->type == MONS_PROGRAM_BUG)
         return;
 
-    // FIXME: monsters should suffer from attacking jellies
-    if (attacker->is_player() && mons_class_flag(defender->type, M_ACID_SPLASH))
-        weapon_acid(5);
+    if (mons_class_flag(defender->type, M_ACID_SPLASH)
+        || defender->is_player() && you.form == TRAN_JELLY)
+    {
+        int rA = attacker->res_acid();
+        if (rA < 3)
+        {
+            int acid_strength = resist_adjust_damage(attacker, BEAM_ACID, rA, 5);
+            item_def *weap = weapon;
+
+            if (!weap)
+                weap = attacker->slot_item(EQ_GLOVES);
+
+            if (weap)
+            {
+                if (x_chance_in_y(acid_strength + 1, 20))
+                    corrode_item(*weap, attacker);
+            }
+            else if (attacker->is_player())
+            {
+                mprf("Your %s burn!", you.hand_name(true).c_str());
+                ouch(roll_dice(1, acid_strength), defender->mindex(),
+                     KILLED_BY_ACID);
+            }
+            else
+            {
+                simple_monster_message(attacker->as_monster(),
+                                       " is burned by acid!");
+                attacker->hurt(defender, roll_dice(1, acid_strength),
+                    BEAM_ACID, false);
+            }
+        }
+    }
 }
 
 int melee_attack::staff_damage(skill_type skill)

crawl-ref/source/mon-stuff.cc
@@ -3165,35 +3165,7 @@ void corrode_monster(monster* mons, const actor* evildoer)
     if (!one_chance_in(3) && (has_shield || has_armour))
     {
         item_def &thing_chosen = (has_armour ? *has_armour : *has_shield);
-        if (is_artefact(thing_chosen)
-           || (get_equip_race(thing_chosen) == ISFLAG_DWARVEN
-              && one_chance_in(5)))
-        {
-            return;
-        }
-        else
-        {
-            // same formula as for players
-            bool resists = false;
-            int enchant = abs(thing_chosen.plus);
-
-            if (enchant >= 0 && enchant <= 4)
-                resists = x_chance_in_y(2 + (4 << enchant) + enchant * 8, 100);
-            else
-                resists = true;
-
-            if (!resists)
-            {
-                thing_chosen.plus--;
-                mons->ac--;
-                if (you.can_see(mons))
-                {
-                    mprf("The acid corrodes %s %s!",
-                         apostrophise(mons->name(DESC_THE)).c_str(),
-                         thing_chosen.name(DESC_PLAIN).c_str());
-                }
-            }
-        }
+        corrode_item(thing_chosen, mons);
     }
     else if (!one_chance_in(3) && !(has_shield || has_armour)
              && mons->can_bleed() && !mons->res_acid())

crawl-ref/source/ouch.cc
@@ -69,7 +69,6 @@
 #include "xom.h"
 
 static void _end_game(scorefile_entry &se);
-static void _item_corrode(int slot);
 
 static void _maybe_melt_player_enchantments(beam_type flavour, int damage)
 {
@@ -371,14 +370,12 @@ void splash_with_acid(int acid_strength, bool corrode_items, string hurt_message
 
         if (!cloak_protects)
         {
-            if (!player_wearing_slot(slot) && slot != EQ_SHIELD)
+            item_def *item = you.slot_item(static_cast<equipment_type>(slot));
+            if (!item && slot != EQ_SHIELD)
                 dam++;
 
-            if (player_wearing_slot(slot) && corrode_items
-                && x_chance_in_y(acid_strength + 1, 20))
-            {
-                _item_corrode(you.equip[slot]);
-            }
+            if (item && corrode_items && x_chance_in_y(acid_strength + 1, 20))
+                corrode_item(*item, &you);
         }
     }
 
@@ -401,123 +398,6 @@ void splash_with_acid(int acid_strength, bool corrode_items, string hurt_message
             canned_msg(MSG_YOU_RESIST);
 
         ouch(post_res_dam, NON_MONSTER, KILLED_BY_ACID);
-    }
-}
-
-void weapon_acid(int acid_strength)
-{
-    // Currently acid_strength is fixed at 5.
-    acid_strength = acid_strength * player_acid_resist_factor() / 100;
-    if (acid_strength <= 0)
-        return;
-
-    int hand_thing = -1;
-
-    if (!you.melded[EQ_WEAPON])
-        hand_thing = you.equip[EQ_WEAPON];
-
-    if (hand_thing == -1 && !you.melded[EQ_GLOVES])
-        hand_thing = you.equip[EQ_GLOVES];
-
-    if (hand_thing == -1)
-    {
-        msg::stream << "Your " << you.hand_name(true) << " burn!" << endl;
-        ouch(roll_dice(1, acid_strength), NON_MONSTER, KILLED_BY_ACID);
-    }
-    else if (x_chance_in_y(acid_strength + 1, 20))
-        _item_corrode(hand_thing);
-}
-
-static void _item_corrode(int slot)
-{
-    bool it_resists = false;
-    bool suppress_msg = false;
-    item_def& item = you.inv[slot];
-
-    // Artefacts don't corrode.
-    if (is_artefact(item))
-        return;
-
-    // Anti-corrosion items protect against 90% of corrosion.
-    if (you.res_corr() && !one_chance_in(10))
-    {
-        dprf("Amulet protects.");
-        return;
-    }
-
-    int how_rusty = ((item.base_type == OBJ_WEAPONS) ? item.plus2 : item.plus);
-    // Already very rusty.
-    if (how_rusty < -5)
-        return;
-
-    // determine possibility of resistance by object type {dlb}:
-    switch (item.base_type)
-    {
-    case OBJ_ARMOUR:
-        if ((item.sub_type == ARM_CRYSTAL_PLATE_ARMOUR
-             || get_equip_race(item) == ISFLAG_DWARVEN)
-            && !one_chance_in(5))
-        {
-            it_resists = true;
-            suppress_msg = false;
-        }
-        break;
-
-    case OBJ_WEAPONS:
-        if (get_equip_race(item) == ISFLAG_DWARVEN && !one_chance_in(5))
-        {
-            it_resists = true;
-            suppress_msg = false;
-        }
-        break;
-
-    default:
-        // Other items can't corrode.
-        return;
-    }
-
-    // determine chance of corrosion {dlb}:
-    if (!it_resists)
-    {
-        const int chance = abs(how_rusty);
-
-        // The embedded equation may look funny, but it actually works well
-        // to generate a pretty probability ramp {6%, 18%, 34%, 58%, 98%}
-        // for values [0,4] which closely matches the original, ugly switch.
-        // {dlb}
-        if (chance >= 0 && chance <= 4)
-            it_resists = x_chance_in_y(2 + (4 << chance) + chance * 8, 100);
-        else
-            it_resists = true;
-
-        // If the checks get this far, you should hear about it. {dlb}
-        suppress_msg = false;
-    }
-
-    // handle message output and item damage {dlb}:
-    if (!suppress_msg)
-    {
-        if (it_resists)
-            mprf("%s resists.", item.name(DESC_YOUR).c_str());
-        else
-            mprf("The acid corrodes %s!", item.name(DESC_YOUR).c_str());
-    }
-
-    if (!it_resists)
-    {
-        how_rusty--;
-        xom_is_stimulated(50);
-
-        if (item.base_type == OBJ_WEAPONS)
-            item.plus2 = how_rusty;
-        else
-            item.plus  = how_rusty;
-
-        if (item.base_type == OBJ_ARMOUR)
-            you.redraw_armour_class = true;
-
-        if (you.equip[EQ_WEAPON] == slot)
-            you.wield_change = true;
     }
 }
 

crawl-ref/source/ouch.h
@@ -63,7 +63,6 @@ int check_your_resists(int hurted, beam_type flavour, string source,
                        bolt *beam = 0, bool doEffects = true);
 void splash_with_acid(int acid_strength, bool corrode_items = true,
                       string hurt_msg = "");
-void weapon_acid(int acid_strength);
 
 class actor;
 int actor_to_death_source(const actor* agent);

crawl-ref/source/player-act.cc
@@ -253,12 +253,16 @@ brand_type player::damage_brand(int)
                 ret = SPWPN_VAMPIRICISM;
             break;
 
+        case TRAN_JELLY:
+            ret = SPWPN_ACID;
+            break;
+
         default:
             break;
         }
     }
 
-    return (static_cast<brand_type>(ret));
+    return ret;
 }
 
 // Returns the item in the given equipment slot, NULL if the slot is empty.

commit 4b2e8381b13eac546d6910148fa89c983dd1e7fa

Fix inverted acidic corrosion probability
The refactoring in f5d745b671aed2eb61b24e2eda74d089323679b0
inadvertently flipped the corrosion probability curve: items with an
enchantment of 0 were made far *more* resistant to corrosion than items
with an enchantment of ±4. This is the opposite of the formula used
prior to the refactoring and was probably not intended.

crawl-ref/source/items.cc
@@ -4140,7 +4140,7 @@ void corrode_item(item_def &item, actor *holder)
     // for values [0,4] which closely matches the original, ugly switch.
     // {dlb}
     if (chance < 0 || chance > 4
-        || !x_chance_in_y(2 + (4 << chance) + chance * 8, 100))
+        || x_chance_in_y(2 + (4 << chance) + chance * 8, 100))
     {
         return;
     }