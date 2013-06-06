ddcaf8cdcb97f03d9c37fe7fb991455b3c843517
Pete Hurst <pete@streamuniverse.tv>
2013-06-06 19:48:51 +0100

    Start wizmode Box of Beasts with 50 charges

--- crawl-ref/source/items.cc
+++ crawl-ref/source/items.cc
@@ -3674,6 +3674,8 @@ bool get_item_by_name(item_def *item, char* specs,
         break;
 
     case OBJ_MISCELLANY:
+        if (item->sub_type == MISC_BOX_OF_BEASTS)
+            item->plus = 50;
         if (!item_is_rune(*item) && !is_deck(*item) && !is_elemental_evoker(*item))
             item->plus2 = 50;
         break;
