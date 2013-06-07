4c2672bbcf7f90ea49654441c1ded431524de0b2
Neil Moore <neil@s-z.org>
2013-06-07 02:49:11 -0400

    Don't leak unided rC in draconian A screen (#7173)
    (cherry picked from commit 11bc70174075e766d2240c6f9e95dd13e7ccc250)

--- crawl-ref/source/mutation.cc
+++ crawl-ref/source/mutation.cc
@@ -569,7 +569,7 @@ string describe_mutations(bool center_title)
         result += "Your body does not fit into most forms of armour.\n";
 
         msg = "Your cold-blooded metabolism reacts poorly to cold.";
-        if (you.res_cold() <= 0)
+        if (player_res_cold(false) <= 0)
             result += msg + "\n";
         else
             result += "<darkgrey>" + msg + "</darkgrey>\n";
