d04d0403fe63d4bcfbde9d312ed2e4aab7639566
Neil Moore <neil@s-z.org>
2013-06-07 21:13:50 -0400

    Formatting fixes.

--- crawl-ref/source/abyss.cc
+++ crawl-ref/source/abyss.cc
@@ -1247,7 +1247,8 @@ static void _abyss_apply_terrain(const map_bitmask &abyss_genlevel_mask,
         bool shoved = you.shove();
         ASSERT(shoved);
     }
-    for (rectangle_iterator ri(MAPGEN_BORDER); ri; ++ri) {
+    for (rectangle_iterator ri(MAPGEN_BORDER); ri; ++ri)
+    {
         ASSERT(grd(*ri) > DNGN_UNSEEN);
         ASSERT(grd(*ri) < NUM_FEATURES);
     }
--- crawl-ref/source/dgn-proclayouts.h
+++ crawl-ref/source/dgn-proclayouts.h
@@ -19,13 +19,13 @@ dungeon_feature_type sanitize_feature(dungeon_feature_type feature,
 class ProceduralSample
 {
     public:
-        ProceduralSample(const coord_def _c,
-            const dungeon_feature_type _ft,
-            const uint32_t _cp, map_mask_type _m = MMT_NONE) :
-            c(_c), ft(_ft), cp(_cp), m(_m) {
-                ASSERT(ft > DNGN_UNSEEN);
-                ASSERT(ft < NUM_FEATURES);
-            }
+        ProceduralSample(const coord_def _c, const dungeon_feature_type _ft,
+                         const uint32_t _cp, map_mask_type _m = MMT_NONE)
+            : c(_c), ft(_ft), cp(_cp), m(_m)
+        {
+            ASSERT(ft > DNGN_UNSEEN);
+            ASSERT(ft < NUM_FEATURES);
+        }
         coord_def coord() const { return c; }
         dungeon_feature_type feat() const { return ft; }
         uint32_t changepoint() const { return cp; }
