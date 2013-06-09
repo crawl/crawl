12d336a50a1ae6e7985ff987328f1f36553e5024
Adam Borowski <kilobyte@angband.pl>
2013-06-09 02:31:21 +0200

    Brace fixes.

--- crawl-ref/source/melee_attack.cc
+++ crawl-ref/source/melee_attack.cc
@@ -5239,9 +5239,8 @@ bool melee_attack::do_knockback(bool trample)
     {
         if (defender->is_player() && you.mutation[MUT_TRAMPLE_RESISTANCE])
         {
-          if (x_chance_in_y(9, 10)) {
-            return false;
-          }
+            if (x_chance_in_y(9, 10))
+                return false;
         }
         monster* def_monster = defender->as_monster();
         if (def_monster && mons_is_stationary(def_monster))
