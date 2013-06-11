4d5e686334f40d23ce9cec0e354ac995984b1f01
lain <lain@maureen.(none)>
2013-06-11 07:05:44 -0700

    make spectral weapon not fragile again, reintroduce damage sharing
    playing with the fragile spectral weapon was not fun at all, so i think going back to the damage-sharing-strong-spectral weapon is worthwhile. the shared damage has been upped to 66% but also can never actually kill you (ideally) --- if you are at 1 hp, you shouldn't take any shared damage (although you are probably going to die from other sources).

--- crawl-ref/source/spl-summoning.cc
+++ crawl-ref/source/spl-summoning.cc
@@ -875,11 +875,11 @@ spret_type cast_spectral_weapon(int pow, bool fail)
 
     you.props["spectral_weapon"].get_int() = mons->mid;
     mons->hit_dice = skill_with_weapon;
-    int hp = max(div_rand_round(pow,5), 5);
+    int hp = 10 + div_rand_round(pow,3);
     mons->hit_points = hp;
     mons->max_hit_points = hp;
-    mons->ac = div_rand_round(pow,20);
-    mons->ev = div_rand_round(pow,20);
+    mons->ac = 2 + div_rand_round(pow,12);
+    mons->ev = 2 + div_rand_round(pow,12);
 
     return SPRET_SUCCESS;
 }
