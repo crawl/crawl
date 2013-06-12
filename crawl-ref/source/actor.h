fd608df5b4c645ecb5d5ed0df3fee692d7168988
Ed Gonzalez <ed.gonzalez3@gmail.com>
2013-06-11 22:34:44 -0700

    Added reach_range() to actor and player classes.

--- crawl-ref/source/actor.h
+++ crawl-ref/source/actor.h
@@ -171,6 +171,7 @@ public:
     virtual bool can_see_invisible() const = 0;
     virtual bool invisible() const = 0;
     virtual bool nightvision() const = 0;
+    virtual reach_type reach_range() const = 0;
 
     // Would looker be able to see the actor when in LOS?
     virtual bool visible_to(const actor *looker) const = 0;
