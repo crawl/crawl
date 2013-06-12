4d73cd294457b21324a9bb8ac6377d4f333e4ed6
lain <lain@maureen.(none)>
2013-06-11 17:01:09 -0700

    fix a crash when the spectral weapon was damaged by a non-actor source
    ideally in the future the spectral weapon properly shares damage from all sources; for now, this just fixes a crash that can occur when it is damaged by things like slime walls.

--- crawl-ref/source/monster.cc
+++ crawl-ref/source/monster.cc
@@ -4188,7 +4188,7 @@ int monster::hurt(const actor *agent, int amount, beam_type flavour,
     // The damage shared should not be directly lethal to the player
     // XXX: It might be possible that the damage might be lethal if the player is at low health and is vulnerable? 
     // This makes a lot of messages, especially when the spectral weapon is hit by a monster with multiple attacks and is frozen, burned, etc.
-    if (type == MONS_SPECTRAL_WEAPON && agent->is_monster())
+    if (type == MONS_SPECTRAL_WEAPON && agent && agent->is_monster())
     {
         int shared_damage = min(2*amount/3, you.hp-1);
         if (shared_damage>0)
