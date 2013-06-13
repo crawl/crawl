03ffd2dd3931035471d4ec51842b0e4dd7918a6d
Ed Gonzalez <ed.gonzalez3@gmail.com>
2013-06-13 00:32:14 -0700

    Revert "Don't let spectral weapon reach past features."
    The new spectral weapon AI handles things much better.
    No longer need to expose functions from mon-act.cc.
    
    This reverts commit 39ff108d3a4bdebfed750e05b89eecdc3c10cc0f.
    
    Conflicts:
    	crawl-ref/source/spl-summoning.cc

--- crawl-ref/source/mon-act.cc
+++ crawl-ref/source/mon-act.cc
@@ -876,7 +876,7 @@ static bool _handle_evoke_equipment(monster* mons, bolt & beem)
     return rc;
 }
 
-bool handle_monster_reaching(monster* mons)
+static bool _handle_reaching(monster* mons)
 {
     bool       ret = false;
     const reach_type range = mons->reach_range();
@@ -2171,7 +2171,7 @@ void handle_monster_move(monster* mons)
                 return;
             }
 
-            if (handle_monster_reaching(mons))
+            if (_handle_reaching(mons))
             {
                 DEBUG_ENERGY_USE("_handle_reaching()");
                 return;
