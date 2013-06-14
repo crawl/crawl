c48fbbd3ef53e707ae95e866a61fffba0ae72fdc
Adam Borowski <kilobyte@angband.pl>
2013-06-14 05:13:57 +0200

    Remove include guards from *-data.h
    Their content results in actual code (rather than prototypes or templates)
    and thus trying to include them twice should result in a fatal error.

--- crawl-ref/source/mon-data.h
+++ crawl-ref/source/mon-data.h
@@ -1,6 +1,3 @@
-#ifndef MONDATA_H
-#define MONDATA_H
-
 #define AT_NO_ATK       {AT_NONE, AF_PLAIN, 0}
 
 #include "enum.h"
@@ -7042,6 +7039,5 @@ static monsterentry mondata[] = {
     - row 8: intel, habitat, flight class, speed, energy_usage
     - row 9: gmon_use class, gmon_eat class, body size
 */
-#endif
 
 };
