f9ac0ffcc320611df9ef36847b84a1567d7b42c0
DracoOmega <draco_omega@live.com>
2013-06-21 04:17:57 -0230

    Adjust spriggan assassin/enchanter starting weapons
    The chance for a quick blade has been greatly reduced, but instead
    they have a 1-in-3 chance of their weapon's quality being good.

--- crawl-ref/source/mon-gear.cc
+++ crawl-ref/source/mon-gear.cc
@@ -1369,8 +1369,11 @@ static item_make_species_type _give_weapon(monster* mon, int level,
         // deliberate fall-through
     case MONS_SPRIGGAN_ENCHANTER:
         item_race = MAKE_ITEM_NO_RACE;
+        if (one_chance_in(3))
+            level = MAKE_GOOD_ITEM;
+        force_item = true;
         item.base_type = OBJ_WEAPONS;
-        item.sub_type  = one_chance_in(3) ? WPN_QUICK_BLADE : WPN_DAGGER;
+        item.sub_type  = one_chance_in(12) ? WPN_QUICK_BLADE : WPN_DAGGER;
         break;
 
     case MONS_SPRIGGAN_RIDER:
