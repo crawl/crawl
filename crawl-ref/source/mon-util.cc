e7efbb0ace1242379b4a5157c7c79c804a85f7d0
Brendan Hickey <brendan@bhickey.net>
2013-06-26 00:12:52 -0700

    Nerf Polymoth

--- crawl-ref/source/mon-util.cc
+++ crawl-ref/source/mon-util.cc
@@ -1529,7 +1529,8 @@ bool mons_class_can_use_stairs(monster_type mc)
             && mc != MONS_PLAYER_GHOST
             && mc != MONS_GERYON
             && mc != MONS_ROYAL_JELLY
-            && mc != MONS_MOTH_OF_SUPPRESSION);
+            && mc != MONS_MOTH_OF_SUPPRESSION
+            && mc != MONS_POLYMOTH);
 }
 
 bool mons_can_use_stairs(const monster* mon)
