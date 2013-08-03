4d1f75f4801129b7f2c9a75fcf7d5046726c682b
Chris Campbell <chriscampbell89@gmail.com>
2013-08-03 03:21:01 +0100

    Don't use spirit wolves in chimeras
    As pointed out in #7385, it's sort of weird for a "spirit" creature to be part
    of a chimera.

--- crawl-ref/source/evoke.cc
+++ crawl-ref/source/evoke.cc
@@ -672,7 +672,6 @@ static const pop_entry pop_beasts[] =
   { 15, 27,   50, PEAK, MONS_CATOBLEPAS },
   { 15, 27,  100, PEAK, MONS_DEATH_YAK },
   { 16, 27,  100, PEAK, MONS_ANACONDA },
-  { 16, 27,  100, PEAK, MONS_SPIRIT_WOLF },
   { 16, 27,   50, PEAK, MONS_RAVEN },
   { 18, 27,   50, UP,   MONS_DIRE_ELEPHANT },
   { 20, 27,   25, UP,   MONS_DRAGON },
