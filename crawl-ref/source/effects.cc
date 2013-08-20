crawl/crawl

commit c56f7848c24e60f938bec429afc467bfb06599a6

Use a finer scale for magic contamination.
It's now updated every turn instead of every 20 turns and it's almost
deterministic (only the time scaling use randomized rounding).
The bad effects still happen every 20 turns though (just like hell effects and
other stuff).
It has been scaled by 1000. I tried with 100, but I still had some significant
rounding issues, especially with scaling to time_taken.
It shouldn't be possible anymore to gain yellow glow from a single cast of
invisibility even at max power. Although, in theory, it has a slight chance
of putting you just 1 point short of yellow (4999).

crawl-ref/source/effects.cc
@@ -2079,6 +2079,106 @@ static void _rot_inventory_food(int time_delta)
     }
 }
 
+static void _handle_magic_contamination()
+{
+    int added_contamination = 0;
+
+    // Scale has been increased by a factor of 1000, but the effect now happens
+    // every turn instead of every 20 turns, so everything has been multiplied
+    // by 50 and scaled to you.time_taken.
+
+    if (you.duration[DUR_INVIS])
+        added_contamination += 30;
+
+    if (you.duration[DUR_HASTE])
+        added_contamination += 30;
+
+    // Is there a point to this? It's not even strong enough to cancel normal
+    // dissipation, so it only slows it down. Shouldn't it cancel dissipation
+    // like haste and invis do?
+    if (you.duration[DUR_FINESSE])
+        added_contamination += 20;
+
+    if (you.duration[DUR_REGENERATION] && you.species == SP_DJINNI)
+        added_contamination += 30;
+
+    // The Orb halves dissipation (well a bit more, I had to round it),
+    // but won't cause glow on its own -- otherwise it'd spam the player
+    // with messages about contamination oscillating near zero.
+    if (you.magic_contamination && orb_haloed(you.pos()))
+        added_contamination += 13;
+
+    // Normal dissipation
+    if (!you.duration[DUR_INVIS] && !you.duration[DUR_HASTE])
+        added_contamination -= 25;
+
+    // Scaling to turn length
+    added_contamination = div_rand_round(added_contamination * you.time_taken,
+                                         BASELINE_DELAY);
+
+    contaminate_player(added_contamination, false);
+}
+
+static void _magic_contamination_effects()
+{
+    // [ds] Move magic contamination effects closer to b26 again.
+    const bool glow_effect = you.species == SP_DJINNI ?
+        get_contamination_level() > 2
+            && x_chance_in_y(you.magic_contamination, 24000):
+        get_contamination_level() > 1
+            && x_chance_in_y(you.magic_contamination, 12000);
+
+    if (glow_effect && is_sanctuary(you.pos()))
+    {
+        mpr("Your body momentarily shudders from a surge of wild "
+            "energies until Zin's power calms it.", MSGCH_GOD);
+    }
+    else if (glow_effect)
+    {
+        mpr("Your body shudders with the violent release "
+            "of wild energies!", MSGCH_WARN);
+
+        // For particularly violent releases, make a little boom.
+        if (you.magic_contamination > 10000 && coinflip())
+        {
+            bolt beam;
+
+            beam.flavour      = BEAM_RANDOM;
+            beam.glyph        = dchar_glyph(DCHAR_FIRED_BURST);
+            beam.damage       = dice_def(3,
+                                 div_rand_round(you.magic_contamination, 2000));
+            beam.target       = you.pos();
+            beam.name         = "magical storm";
+            beam.beam_source  = NON_MONSTER;
+            beam.aux_source   = "a magical explosion";
+            beam.ex_size      = max(1, min(9,
+                               div_rand_round(you.magic_contamination, 15000)));
+            beam.ench_power   = div_rand_round(you.magic_contamination, 200);
+            beam.is_explosion = true;
+
+            // Undead enjoy extra contamination explosion damage because
+            // the magical contamination has a harder time dissipating
+            // through non-living flesh. :-)
+            if (you.is_undead)
+                beam.damage.size *= 2;
+
+            beam.explode();
+        }
+
+        // We want to warp the player, not do good stuff!
+        mutate(one_chance_in(5) ? RANDOM_MUTATION : RANDOM_BAD_MUTATION,
+               "mutagenic glow", true,
+               coinflip(),
+               false, false, false, false,
+               you.species == SP_DJINNI);
+
+        // we're meaner now, what with explosions and whatnot, but
+        // we dial down the contamination a little faster if its actually
+        // mutating you.  -- GDL
+        contaminate_player(-(random2(you.magic_contamination / 4) + 1000));
+    }
+}
+
 // Get around C++ dividing integers towards 0.
 static int _div(int num, int denom)
 {
@@ -2109,6 +2209,9 @@ void handle_time()
     // Labyrinth and Abyss maprot.
     if (player_in_branch(BRANCH_LABYRINTH) || player_in_branch(BRANCH_ABYSS))
         forget_map(true);
+
+    // Magic contamination from spells and Orb.
+    _handle_magic_contamination();
 
     // Every 20 turns, a variety of other effects.
     if (! (_div(base_time, 200) > _div(old_time, 200)))
@@ -2173,104 +2276,15 @@ void handle_time()
         }
     }
 
+    // Bad effects from magic contamination.
+    if (coinflip())
+        _magic_contamination_effects();
+
     // Adjust the player's stats if s/he has the deterioration mutation.
     if (player_mutation_level(MUT_DETERIORATION)
         && x_chance_in_y(player_mutation_level(MUT_DETERIORATION) * 5 - 1, 200))
     {
         lose_stat(STAT_RANDOM, 1, false, "deterioration mutation");
-    }
-
-    int added_contamination = 0;
-
-    // Account for mutagenic radiation.  Invis and haste will give the
-    // player about .1 points per turn, mutagenic randarts will give
-    // about 1.5 points on average, so they can corrupt the player
-    // quite quickly.  Wielding one for a short battle is OK, which is
-    // as things should be.   -- GDL
-    if (you.duration[DUR_INVIS] && x_chance_in_y(6, 10))
-        added_contamination++;
-
-    if (you.duration[DUR_HASTE] && x_chance_in_y(6, 10))
-        added_contamination++;
-
-    if (you.duration[DUR_FINESSE] && x_chance_in_y(4, 10))
-        added_contamination++;
-
-    if (you.duration[DUR_REGENERATION] && you.species == SP_DJINNI
-        && x_chance_in_y(6, 10))
-    {
-        added_contamination++;
-    }
-
-    // The Orb adds .25 points per turn (effectively halving dissipation),
-    // but won't cause glow on its own -- otherwise it'd spam the player
-    // with messages about contamination oscillating near zero.
-    if (you.magic_contamination && orb_haloed(you.pos()) && one_chance_in(4))
-        added_contamination++;
-
-    // We take off about .5 points per turn.
-    if (!you.duration[DUR_INVIS] && !you.duration[DUR_HASTE] && coinflip())
-        added_contamination--;
-
-    // Don't punish for this contamination.
-    // (Haste and invisibility already penalised earlier).
-    contaminate_player(added_contamination, false);
-
-    // Only check for badness once every other turn.
-    if (coinflip())
-    {
-        // [ds] Move magic contamination effects closer to b26 again.
-        const bool glow_effect = you.species == SP_DJINNI ?
-            get_contamination_level() > 2
-                && x_chance_in_y(you.magic_contamination, 24):
-            get_contamination_level() > 1
-                && x_chance_in_y(you.magic_contamination, 12);
-
-        if (glow_effect && is_sanctuary(you.pos()))
-        {
-            mpr("Your body momentarily shudders from a surge of wild "
-                "energies until Zin's power calms it.", MSGCH_GOD);
-        }
-        else if (glow_effect)
-        {
-            mpr("Your body shudders with the violent release "
-                "of wild energies!", MSGCH_WARN);
-
-            // For particularly violent releases, make a little boom.
-            // Undead enjoy extra contamination explosion damage because
-            // the magical contamination has a harder time dissipating
-            // through non-living flesh. :-)
-            if (you.magic_contamination > 10 && coinflip())
-            {
-                bolt beam;
-
-                beam.flavour      = BEAM_RANDOM;
-                beam.glyph        = dchar_glyph(DCHAR_FIRED_BURST);
-                beam.damage       = dice_def(3, you.magic_contamination
-                                             * (you.is_undead ? 4 : 2) / 4);
-                beam.target       = you.pos();
-                beam.name         = "magical storm";
-                beam.beam_source  = NON_MONSTER;
-                beam.aux_source   = "a magical explosion";
-                beam.ex_size      = max(1, min(9, you.magic_contamination / 15));
-                beam.ench_power   = you.magic_contamination * 5;
-                beam.is_explosion = true;
-
-                beam.explode();
-            }
-
-            // We want to warp the player, not do good stuff!
-            mutate(one_chance_in(5) ? RANDOM_MUTATION : RANDOM_BAD_MUTATION,
-                   "mutagenic glow", true,
-                   coinflip(),
-                   false, false, false, false,
-                   you.species == SP_DJINNI);
-
-            // we're meaner now, what with explosions and whatnot, but
-            // we dial down the contamination a little faster if its actually
-            // mutating you.  -- GDL
-            contaminate_player(-(random2(you.magic_contamination / 4) + 1));
-        }
     }
 
     // Check to see if an upset god wants to do something to the player.

crawl-ref/source/abl-show.cc
@@ -2141,7 +2141,7 @@ static bool _do_ability(const ability_def& abil)
 
     case ABIL_EVOKE_TURN_INVISIBLE:     // ring, randarts, darkness items
         potion_effect(POT_INVISIBILITY, you.skill(SK_EVOCATIONS, 2) + 5);
-        contaminate_player(1 + random2(3), true);
+        contaminate_player(1000 + random2(2000), true);
         break;
 
     case ABIL_EVOKE_TURN_VISIBLE:

crawl-ref/source/beam.cc
@@ -3417,7 +3417,7 @@ void bolt::affect_player_enchantment()
 
     case BEAM_HASTE:
         potion_effect(POT_SPEED, ench_power, false, blame_player);
-        contaminate_player(1, effect_known);
+        contaminate_player(1000, effect_known);
         obvious_effect = true;
         nasty = false;
         nice  = true;
@@ -3440,7 +3440,7 @@ void bolt::affect_player_enchantment()
     case BEAM_INVISIBILITY:
         you.attribute[ATTR_INVIS_UNCANCELLABLE] = 1;
         potion_effect(POT_INVISIBILITY, ench_power, false, blame_player);
-        contaminate_player(1 + random2(2), effect_known);
+        contaminate_player(1000 + random2(1000), effect_known);
         obvious_effect = true;
         nasty = false;
         nice  = true;

crawl-ref/source/food.cc
@@ -86,7 +86,7 @@ void make_hungry(int hunger_amount, bool suppress_msg,
         if (!magic)
             return;
 
-        contaminate_player(div_rand_round(hunger_amount, 250), true);
+        contaminate_player(4 * hunger_amount, true);
         return;
     }
 

crawl-ref/source/godwrath.cc
@@ -949,7 +949,7 @@ static bool _jiyva_retribution()
     else if (!one_chance_in(3))
     {
             god_speaks(god, "Mutagenic energy floods into your body!");
-            contaminate_player(random2(you.penance[GOD_JIYVA]) / 2);
+            contaminate_player(random2(you.penance[GOD_JIYVA] * 500));
 
             if (coinflip())
             {

crawl-ref/source/output.cc
@@ -580,7 +580,7 @@ static void _print_stats_contam(int x, int y)
     if (you.species != SP_DJINNI)
         return;
 
-    const int max_contam = 16;
+    const int max_contam = 16000;
     int contam = min(you.magic_contamination, max_contam);
 
     // Calculate colour

crawl-ref/source/player-equip.cc
@@ -414,7 +414,7 @@ static void _unequip_artefact_effect(item_def &item,
     if (proprt[ARTP_MUTAGENIC] && !meld)
     {
         mpr("Mutagenic energies flood into your body!");
-        contaminate_player(7, true);
+        contaminate_player(7000, true);
     }
 
     if (is_unrandom_artefact(item))
@@ -1392,7 +1392,8 @@ static void _equip_jewellery_effect(item_def &item, bool unmeld)
                                   "");
             ident = ID_KNOWN_TYPE;
 
-            contaminate_player(pow(amount, 0.333), item_type_known(item));
+            // XXX: This can probably be improved.
+            contaminate_player(pow(amount, 0.333) * 1000, item_type_known(item));
 
             int dir = 0;
             if (you.duration[DUR_HASTE])

crawl-ref/source/player.cc
@@ -4949,15 +4949,15 @@ int get_contamination_level()
 {
     const int glow = you.magic_contamination;
 
-    if (glow > 60)
-        return (glow / 20 + 3);
-    if (glow > 40)
+    if (glow > 60000)
+        return (glow / 20000 + 3);
+    if (glow > 40000)
         return 5;
-    if (glow > 25)
+    if (glow > 25000)
         return 4;
-    if (glow > 15)
+    if (glow > 15000)
         return 3;
-    if (glow > 5)
+    if (glow > 5000)
         return 2;
     if (glow > 0)
         return 1;
@@ -4997,7 +4997,7 @@ void contaminate_player(int change, bool controlled, bool msg)
     int old_level  = get_contamination_level();
     int new_level  = 0;
 
-    you.magic_contamination = max(0, min(250, you.magic_contamination + change));
+    you.magic_contamination = max(0, min(250000, you.magic_contamination + change));
 
     new_level = get_contamination_level();
 
@@ -5437,7 +5437,7 @@ bool haste_player(int turns, bool rageext)
     else if (!rageext)
     {
         mpr("You feel as though your hastened speed will last longer.");
-        contaminate_player(1, true); // always deliberate
+        contaminate_player(1000, true); // always deliberate
     }
 
     you.increase_duration(DUR_HASTE, turns, threshold);

commit 440019e6197fa31346d30a2a0634359b89c9a06c

Don't contaminate the arena.
Fixes arena crash caused by c56f7848.

Signed-off-by: Raphael Langella <raphael.langella@gmail.com>

crawl-ref/source/effects.cc
@@ -2211,7 +2211,8 @@ void handle_time()
         forget_map(true);
 
     // Magic contamination from spells and Orb.
-    _handle_magic_contamination();
+    if (!crawl_state.game_is_arena())
+        _handle_magic_contamination();
 
     // Every 20 turns, a variety of other effects.
     if (! (_div(base_time, 200) > _div(old_time, 200)))