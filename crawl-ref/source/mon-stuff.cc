e2d9698c3083734ade4d4312452ce6a2c7eedc08
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-08-21 11:06:58 +0200

    No Jiyva penance for slime kills by confused monsters
    Yes, this includes confusion sources that are not "eyeballs" mutation.
    I do not see any potential of abuse here though, so an even more verbose
    special case is probably not necessary.
    
    Fixes #5112 [confused monsters killing jellies trigger jiyva penance]

--- crawl-ref/source/mon-stuff.cc
+++ crawl-ref/source/mon-stuff.cc
@@ -1973,7 +1973,9 @@ int monster_die(monster* mons, killer_type killer,
                                     mons->hit_dice, true, mons);
                 }
 
-                if (mons_is_slime(mons))
+                // Jiyva hates you killing slimes, but eyeballs
+                // mutation can confuse without you meaning it.
+                if (mons_is_slime(mons) && killer != KILL_YOU_CONF)
                 {
                     did_god_conduct(DID_KILL_SLIME, mons->hit_dice,
                                     true, mons);
