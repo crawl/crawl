8d7212f5ffa44db68772c4713ed60f4f543a50bc
Adam Borowski <kilobyte@angband.pl>
2013-08-27 23:28:18 +0200

    Forbid druids from calling shapeshifters.

--- crawl-ref/source/mon-cast.cc
+++ crawl-ref/source/mon-cast.cc
@@ -1340,6 +1340,7 @@ static bool _valid_encircle_ally(const monster* caster, const monster* target,
 static bool _valid_druids_call_target(const monster* caller, const monster* callee)
 {
     return (mons_aligned(caller, callee) && mons_is_beast(callee->type)
+            && !callee->is_shapeshifter()
             && !caller->see_cell(callee->pos())
             && mons_habitat(callee) != HT_WATER
             && mons_habitat(callee) != HT_LAVA);
