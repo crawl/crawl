64e0ec3cf31adcfb09c86e198bd807c527e59d78
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-09-05 05:29:56 +0200

    Let godprayer messages and ^ description coincide with piety ranks
    Using anything else than the piety breakpoints which are displayed to
    players as `*` rank is just a tedious way of approximating your actual
    amount of piety better. The initial difference between 5 (for prayer)
    and 1 (for breakpoint) was kept because seeing a new message early on
    tells players that they're doing the right things, if they are.

--- crawl-ref/source/godprayer.cc
+++ crawl-ref/source/godprayer.cc
@@ -68,11 +68,11 @@ string god_prayer_reaction()
     else
         result += " is ";
     result +=
-        (you.piety > 130) ? "exalted by your worship" :
+        (you.piety > 120) ? "exalted by your worship" :
         (you.piety > 100) ? "extremely pleased with you" :
-        (you.piety >  70) ? "greatly pleased with you" :
-        (you.piety >  40) ? "most pleased with you" :
-        (you.piety >  20) ? "pleased with you" :
+        (you.piety >  75) ? "greatly pleased with you" :
+        (you.piety >  50) ? "most pleased with you" :
+        (you.piety >  30) ? "pleased with you" :
         (you.piety >   5) ? "noncommittal"
                           : "displeased";
     result += ".";
@@ -474,14 +474,14 @@ static bool _zin_donate_gold()
     {
         string result = "You feel that " + god_name(GOD_ZIN) + " will soon be ";
         result +=
-            (estimated_piety > 130) ? "exalted by your worship" :
+            (estimated_piety > 120) ? "exalted by your worship" :
             (estimated_piety > 100) ? "extremely pleased with you" :
-            (estimated_piety >  70) ? "greatly pleased with you" :
-            (estimated_piety >  40) ? "most pleased with you" :
-            (estimated_piety >  20) ? "pleased with you" :
+            (estimated_piety >  75) ? "greatly pleased with you" :
+            (estimated_piety >  50) ? "most pleased with you" :
+            (estimated_piety >  30) ? "pleased with you" :
             (estimated_piety >   5) ? "noncommittal"
                                     : "displeased";
-        result += (donation >= 30 && you.piety <= 170) ? "!" : ".";
+        result += (donation >= 30 && you.piety <= 160) ? "!" : ".";
 
         mpr(result.c_str());
     }
