f0ae9ddb47884aa82a475d88738561f72033deee
Adam Borowski <kilobyte@angband.pl>
2013-09-15 18:54:57 +0200

    Make the Forest trunk-only.
    It's in a better shape than LO or Dj, but the consensus seems to be that
    it still needs a lot of polishing and balancing.

--- crawl-ref/source/ng-init.cc
+++ crawl-ref/source/ng-init.cc
@@ -66,12 +66,14 @@ void initialise_branch_depths()
 
     // You will get one of Shoals/Swamp and one of Spider/Snake.
     // This way you get one "water" branch and one "poison" branch.
-    const branch_type disabled_branch[] =
+    branch_type disabled_branch[] =
     {
         random_choose(BRANCH_SWAMP, BRANCH_SHOALS, -1),
         random_choose(BRANCH_SNAKE_PIT, BRANCH_SPIDER_NEST, -1),
         random_choose(BRANCH_CRYPT, BRANCH_FOREST, -1),
     };
+    if (Version::ReleaseType != VER_ALPHA)
+        disabled_branch[2] = BRANCH_FOREST;
 
     for (unsigned int i = 0; i < ARRAYSZ(disabled_branch); ++i)
     {
