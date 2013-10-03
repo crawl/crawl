80266f1b14706a93aa99794756d23254278a91d9
Chris Campbell <chriscampbell89@gmail.com>
2013-10-03 11:14:16 +0100

    Change cTele inscription to +cTele
    Since it's now evokable rather than passive.

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -171,7 +171,7 @@ static const char* _jewellery_base_ability_string(int subtype)
     case RING_FIRE:              return "Fire";
     case RING_ICE:               return "Ice";
     case RING_TELEPORTATION:     return "+/*Tele";
-    case RING_TELEPORT_CONTROL:  return "cTele";
+    case RING_TELEPORT_CONTROL:  return "+cTele";
     case AMU_CLARITY:            return "Clar";
     case AMU_WARDING:            return "Ward";
     case AMU_RESIST_CORROSION:   return "rCorr";
