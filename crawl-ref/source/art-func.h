3e0928f93c3f8a78ac6327c26986b406f7d7f603
Adam Borowski <kilobyte@angband.pl>
2013-10-05 08:40:13 +0200

    Properly start/stop clinging when wearing/removing boots.
    Also, warn when removing them over lava.

--- crawl-ref/source/art-func.h
+++ crawl-ref/source/art-func.h
@@ -842,4 +842,10 @@ static void _WOE_melee_effect(item_def* weapon, actor* attacker,
 static void _SPIDER_equip(item_def *item, bool *show_msgs, bool unmeld)
 {
     _equip_mpr(show_msgs, "The boots cling to your feet.");
+    you.check_clinging(false);
+}
+
+static void _SPIDER_unequip(item_def *item, bool *show_msgs)
+{
+    you.check_clinging(false);
 }
