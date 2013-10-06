679ee20a6f825c3bee739dd8d2fad5073cdf0d21
Adam Borowski <kilobyte@angband.pl>
2013-10-06 17:00:22 +0200

    Don't wrap low-level RNG calls twice.
    random.cc wrapped rng.cc, rng.cc wrapped asg.cc.  The middle level did
    nothing but pass calls as-is.

--- crawl-ref/source/rng.h
+++ crawl-ref/source/rng.h
@@ -1,11 +1,6 @@
 #ifndef RNG_H
 #define RNG_H
 
-void seed_rng();
-void seed_rng(uint32_t seed);
-
-uint32_t random_int();
-
 uint32_t hash32(const void *data, int len) PURE;
 
 unsigned int hash_rand(int x, uint32_t seed, uint32_t id = 0);
