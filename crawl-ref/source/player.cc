c71e4c71abacc07063af2d2f72da2d5eb5f21093
Adam Borowski <kilobyte@angband.pl>
2013-10-08 12:46:28 +0200

    Reorder checks in forget_map.
    An expensive LOS check was done first, while most of the map can be rejected
    on the account of it being not known in the first place, or being too close
    to the player.
    
    On a profiler run, this managed to score 10%.

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -2951,7 +2951,7 @@ void forget_map(bool rot)
     for (rectangle_iterator ri(0); ri; ++ri)
     {
         const coord_def &p = *ri;
-        if (you.see_cell(p) || !env.map_knowledge(p).known())
+        if (!env.map_knowledge(p).known() || you.see_cell(p))
             continue;
 
         if (rot)
@@ -2962,6 +2962,9 @@ void forget_map(bool rot)
             if (x_chance_in_y(chance, scalar))
                 continue;
         }
+
+        if (you.see_cell(p))
+            continue;
 
         env.map_knowledge(p).clear();
         if (env.map_forgotten.get())
