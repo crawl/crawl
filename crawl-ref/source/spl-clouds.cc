548a4c655c81c6fc04d8217ab7d451ef65d1fe0b
Adam Borowski <kilobyte@angband.pl>
2013-10-18 06:34:48 +0200

    Don't place a cloud covering the malign gateway itself, just next to it.

--- crawl-ref/source/spl-clouds.cc
+++ crawl-ref/source/spl-clouds.cc
@@ -225,6 +225,9 @@ static int _make_a_normal_cloud(coord_def where, int pow, int spread_rate,
                                 cloud_type ctype, const actor *agent, int colour,
                                 string name, string tile, int excl_rad)
 {
+    if (cell_is_solid(where))
+        return 0;
+
     place_cloud(ctype, where,
                 (3 + random2(pow / 4) + random2(pow / 4) + random2(pow / 4)),
                 agent, spread_rate, colour, name, tile, excl_rad);
@@ -236,6 +239,7 @@ void big_cloud(cloud_type cl_type, const actor *agent,
                const coord_def& where, int pow, int size, int spread_rate,
                int colour, string name, string tile)
 {
+    // The starting point _may_ be a place no cloud can be placed on.
     apply_area_cloud(_make_a_normal_cloud, where, pow, size,
                      cl_type, agent, spread_rate, colour, name, tile,
                      -1);
