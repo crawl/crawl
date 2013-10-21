cff28afa16543736eb43eb14f8ffb017cddfddc4
Adam Borowski <kilobyte@angband.pl>
2013-10-21 09:02:00 +0200

    Revert "Don't give Gargoyles innate rElec"
    There is not a single reason for this change provided, other than a vague
    feeling that "this feels weird".  On the other hand, there are multiple
    reasons for them to do have rElec:
    * stone
    * consistency with monster gargoyles
    * player confusion about which monster has which resistance
    * the closer races are to each other, the more boring they are
    * consistency with statue form
    
    This reverts commit 1c54f6e1cd9b1963f74873faaf739c088f791bfd.

--- crawl-ref/source/ng-setup.cc
+++ crawl-ref/source/ng-setup.cc
@@ -313,6 +313,7 @@ void give_basic_mutations(species_type speci)
     case SP_GARGOYLE:
         you.mutation[MUT_PETRIFICATION_RESISTANCE]   = 1;
         you.mutation[MUT_NEGATIVE_ENERGY_RESISTANCE] = 1;
+        you.mutation[MUT_SHOCK_RESISTANCE]           = 1;
         you.mutation[MUT_FANGS]                      = 1;
         you.mutation[MUT_TALONS]                     = 2;
         you.mutation[MUT_SLOW_METABOLISM]            = 1;
