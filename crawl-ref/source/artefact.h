8de7c5b38b6efcf32ce442a8eb5fe78304165431
Steve Melenchuk <smelenchuk@gmail.com>
2013-10-29 16:23:42 -0600

    Allow unrands to have a special string added to their inscription.
    For example, Hellfire gains a fake "hellfire" brand inscription.

--- crawl-ref/source/artefact.h
+++ crawl-ref/source/artefact.h
@@ -45,6 +45,7 @@ struct unrandart_entry
     const char *name;        // true name of unrandart
     const char *unid_name;   // un-id'd name of unrandart
     const char *type_name;   // custom item type
+    const char *inscrip;     // extra inscription
 
     object_class_type base_type;
     uint8_t           sub_type;
