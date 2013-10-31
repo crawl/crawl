eee26a46bbd626d036c58a600bbe7bf7cda5a93b
Brendan Hickey <brendan@bhickey.net>
2013-10-30 23:29:11 -0700

    Add Ignis, a lantern monster
    The Ignis will be a new abyss denizen.
    
    It's slightly fast, maintains its distance and spams you with corona.
    It requires some fiddling, requiring greater HD so everything doesn't
    resist the attack, without being just a sack of experience.
    
    ontoclasm and I also discussed making it noisy or mark you. Mark is
    probably highly undesirable in the abyss.

--- crawl-ref/source/enum.h
+++ crawl-ref/source/enum.h
@@ -2750,6 +2750,8 @@ enum monster_type                      // menv[].type
 
     MONS_DEATHCAP,
 
+    MONS_IGNIS,
+
     NUM_MONSTERS,               // used for polymorph
 
     // MONS_NO_MONSTER can get put in savefiles, so it shouldn't change
