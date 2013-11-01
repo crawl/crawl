76914852812a2b036fd1764a6764b34907b3557e
Adam Borowski <kilobyte@angband.pl>
2013-11-01 13:07:52 +0100

    Dj: allow using Death's Door.
    The spell is notably worse than for other races, but here you go.

--- crawl-ref/source/spl-book.cc
+++ crawl-ref/source/spl-book.cc
@@ -575,7 +575,6 @@ bool you_cannot_memorise(spell_type spell, bool &form)
     if (you.species == SP_DJINNI
         && (spell == SPELL_ICE_FORM
          || spell == SPELL_OZOCUBUS_ARMOUR
-         || spell == SPELL_DEATHS_DOOR
          || spell == SPELL_LEDAS_LIQUEFACTION))
     {
         rc = true, form = false;
