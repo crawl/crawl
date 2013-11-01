2139904f20734daa4b4769b534028575d8a197b0
Neil Moore <neil@s-z.org>
2013-10-31 23:31:40 -0400

    Fix a disc of storms rain-placement crash.

--- crawl-ref/source/evoke.cc
+++ crawl-ref/source/evoke.cc
@@ -440,12 +440,14 @@ bool disc_of_storms(bool drac_breath)
         {
             for (radius_iterator ri(you.pos(), LOS_RADIUS, false); ri; ++ri)
             {
-                if (!in_bounds(*ri) || grd(*ri) < DNGN_MAXWALL)
+                if (!in_bounds(*ri) || cell_is_solid(*ri))
                     continue;
 
                 if (one_chance_in(60 - you.skill(SK_EVOCATIONS)))
+                {
                     place_cloud(CLOUD_RAIN, *ri,
                                 random2(you.skill(SK_EVOCATIONS)), &you);
+                }
             }
         }
     }
