2999d4af3666a04748cdb17681a08902a8803ad1
Adam Borowski <kilobyte@angband.pl>
2013-11-03 15:38:21 +0100

    Move set_terrain_changed() away from map_knowledge.cc
    It hardly deals with remembered map.

--- crawl-ref/source/terrain.cc
+++ crawl-ref/source/terrain.cc
@@ -1812,6 +1812,29 @@ const char* feat_type_name(dungeon_feature_type feat)
     return "floor";
 }
 
+void set_terrain_changed(const coord_def p)
+{
+    if (cell_is_solid(p))
+    {
+        int cl = env.cgrid(p);
+        if (cl != EMPTY_CLOUD)
+            delete_cloud(cl);
+    }
+
+    if (grd(p) == DNGN_SLIMY_WALL)
+        env.level_state |= LSTATE_SLIMY_WALL;
+
+    env.map_knowledge(p).flags |= MAP_CHANGED_FLAG;
+
+    dungeon_events.fire_position_event(DET_FEAT_CHANGE, p);
+
+    los_terrain_changed(p);
+
+    for (orth_adjacent_iterator ai(p); ai; ++ai)
+        if (actor *act = actor_at(*ai))
+            act->check_clinging(false, feat_is_door(grd(p)));
+}
+
 bool is_boring_terrain(dungeon_feature_type feat)
 {
     if (!is_notable_terrain(feat))
