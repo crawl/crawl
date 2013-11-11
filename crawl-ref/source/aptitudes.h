76cd1e1177ea188bba495bc77d5830cb8dd12f26
Adam Borowski <kilobyte@angband.pl>
2013-11-11 23:33:59 +0100

    Unbreak formicid aptitudes for versions > 34.

--- crawl-ref/source/aptitudes.h
+++ crawl-ref/source/aptitudes.h
@@ -1428,8 +1428,13 @@ static const species_skill_aptitude species_skill_aptitudes[] =
     APT(SP_FORMICID,        SK_ARMOUR,          2),
     APT(SP_FORMICID,        SK_DODGING,        -1),
     APT(SP_FORMICID,        SK_STEALTH,         3),
+#if TAG_MAJOR_VERSION == 34
+    APT(SP_FORMICID,        SK_STABBING,      -99),
+#endif
     APT(SP_FORMICID,        SK_SHIELDS,         0),
-    APT(SP_FORMICID,        SK_TRAPS,           1),
+#if TAG_MAJOR_VERSION == 34
+    APT(SP_FORMICID,        SK_TRAPS,         -99),
+#endif
     APT(SP_FORMICID,        SK_UNARMED_COMBAT,  0),
     APT(SP_FORMICID,        SK_SPELLCASTING,   -1),
     APT(SP_FORMICID,        SK_CONJURATIONS,   -1),
