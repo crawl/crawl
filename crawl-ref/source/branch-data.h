3ae147d4a54b91eacc584c27312aeeecd8edeeed
Steve Melenchuk <smelenchuk@gmail.com>
2013-11-13 20:31:41 -0700

    Move Vaults entry to Depths:1-3 (was 2-5).
    Consensus is that Depths is pretty scary, and having the entrance appear
    in a place that could be considered the equivalent of D:26 is too deep.

--- crawl-ref/source/branch-data.h
+++ crawl-ref/source/branch-data.h
@@ -102,7 +102,7 @@ const Branch branches[NUM_BRANCHES] =
       true, GREEN, BROWN,
       'M', true, -5 },
 
-    { BRANCH_VAULTS, BRANCH_DEPTHS, 2, 5, 5, 19,
+    { BRANCH_VAULTS, BRANCH_DEPTHS, 1, 3, 5, 19,
       0, 0,
       DNGN_ENTER_VAULTS, DNGN_RETURN_FROM_VAULTS,
       "Vaults", "the Vaults", "Vaults",
