a171c3a09205a1fb2502ab1b15f14deb54538543
Neil Moore <neil@s-z.org>
2013-11-16 09:38:39 -0500

    Make statue-form vampires poison-immune.
    (cherry picked from commit 02ff6a7dff2afa53467b1810abae89c7cf119531)

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -1856,9 +1856,9 @@ int player_kiku_res_torment()
 // If temp is set to false, temporary sources or resistance won't be counted.
 int player_res_poison(bool calc_unid, bool temp, bool items)
 {
-    if (you.is_undead == US_SEMI_UNDEAD ? you.hunger_state == HS_STARVING
-            : you.is_undead && (temp || you.form != TRAN_LICH)
-              || you.is_artificial())
+    if ((you.is_undead == US_SEMI_UNDEAD ? you.hunger_state == HS_STARVING
+            : you.is_undead && (temp || you.form != TRAN_LICH))
+        || you.is_artificial())
     {
         return 3;
     }
