7ffe460e6b7a988a0d82b6339dc77cee351f8c2a
Chris Campbell <chriscampbell89@gmail.com>
2013-11-26 14:17:19 +0000

    Identify starting items before checking them for uselessness
    Fixes FoWr being given scrolls of blinking. Still lets them recognise the
    item type, which is probably fine but has some slightly weird results in
    Sprint and Zotdef (Mummies recognising a bunch of potion types).

--- crawl-ref/source/ng-setup.cc
+++ crawl-ref/source/ng-setup.cc
@@ -1209,9 +1209,19 @@ static void _give_basic_spells(job_type which_job)
     return;
 }
 
-// Give knowledge of things that aren't in the starting inventory.
 static void _give_basic_knowledge(job_type which_job)
 {
+    // Identify all items in pack.
+    for (int i = 0; i < ENDOFPACK; ++i)
+    {
+        if (you.inv[i].defined())
+        {
+            set_ident_type(you.inv[i], ID_KNOWN_TYPE);
+            set_ident_flags(you.inv[i], ISFLAG_IDENT_MASK);
+        }
+    }
+
+    // Recognisable by appearance.
     you.type_ids[OBJ_POTIONS][POT_BLOOD] = ID_KNOWN_TYPE;
     you.type_ids[OBJ_POTIONS][POT_BLOOD_COAGULATED] = ID_KNOWN_TYPE;
     you.type_ids[OBJ_POTIONS][POT_PORRIDGE] = ID_KNOWN_TYPE;
@@ -1360,18 +1370,16 @@ static void _setup_generic(const newgame_def& ng)
     initialise_item_descriptions();
 
     for (int i = 0; i < ENDOFPACK; ++i)
+    {
         if (you.inv[i].defined())
         {
-            // Identify all items in pack.
-            set_ident_type(you.inv[i], ID_KNOWN_TYPE);
-            set_ident_flags(you.inv[i], ISFLAG_IDENT_MASK);
-
             // link properly
             you.inv[i].pos.set(-1, -1);
             you.inv[i].link = i;
             you.inv[i].slot = index_to_letter(you.inv[i].link);
             item_colour(you.inv[i]);  // set correct special and colour
         }
+    }
 
     reassess_starting_skills();
     init_skill_order();
