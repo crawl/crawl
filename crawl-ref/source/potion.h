5d6eb580c3c0b527c756a8ed95ffbd7f11c6d019
Adam Borowski <kilobyte@angband.pl>
2013-11-27 23:05:25 +0100

    Abort drinking known potions of a few types when they'd have no effect.
    Only a few types of potions for the moment ({cure ,,beneficial }mutation,
    berserk, lignify) as I suspect this commit might be reverted.
    
    Mostly, I'd want to hear if this is a good idea.

--- crawl-ref/source/potion.h
+++ crawl-ref/source/potion.h
@@ -11,7 +11,7 @@
 
 // drank_it should be true for real potion effects (as opposed
 // to abilities which duplicate such effects.)
-void potion_effect(potion_type pot_eff, int pow,
+bool potion_effect(potion_type pot_eff, int pow,
                    item_def *potion = nullptr, bool was_known = true,
                    bool from_fountain = false);
 
