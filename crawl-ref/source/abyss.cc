f3e904b9a066ef6d114b9338e005e7cd7aa5a612
Brendan Hickey <brendan@bhickey.net>
2013-12-07 15:09:30 -0800

    Increase Abyssal Stair Chance
    This increases the abyssal stair chance. On one hand, stairs provide
    the player with an opportunity to escape danger. On the other, you're
    more likely to get a nasty party patrol on a deeper level.
    
    Ultimately I think this will be a wash in terms of difficulty.

--- crawl-ref/source/abyss.cc
+++ crawl-ref/source/abyss.cc
@@ -1177,7 +1177,7 @@ static void _update_abyss_terrain(const coord_def &p,
 
 static int _abyssal_stair_chance()
 {
-    return you.char_direction == GDT_GAME_START ? 0 : 2800 - (200 * you.depth / 3);
+    return you.char_direction == GDT_GAME_START ? 0 : 2500 - (200 * you.depth / 3);
 }
 
 static void _nuke_all_terrain(bool vaults)
