1853da09c5527e013301617691506aad4225634b
Sage <SageBasilMint@gmail.com>
2013-12-30 21:33:59 -0800

    Reduce vine stalker starting regen to II, upgrades to III at xl8.

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -3579,6 +3579,7 @@ void level_change(int source, const char* aux, bool skip_attribute_increase)
 
                 if (you.experience_level == 8)
                     perma_mutate(MUT_FANGS, 1, "vine stalker growth");
+                    perma_mutate(MUT_REGENERATION, 1, "vine stalker growth");
                 break;
 
             default:
