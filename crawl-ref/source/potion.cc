46a73a5be5277438b8a28657cc57c4d08d2e2b67
Pekka Lampila <pekka.lampila@iki.fi>
2014-01-02 09:08:09 +0200

    Message for restore abil partially curing draining

--- crawl-ref/source/potion.cc
+++ crawl-ref/source/potion.cc
@@ -423,6 +423,9 @@ bool potion_effect(potion_type pot_eff, int pow, item_def *potion, bool was_know
             you.attribute[ATTR_XP_DRAIN] = max(0, you.attribute[ATTR_XP_DRAIN] / 2 - 25);
             if (!you.attribute[ATTR_XP_DRAIN])
                 mprf(MSGCH_RECOVERY, "Your life force feels restored.");
+            else
+                mpr("Your life force feels less drained.");
+            nothing_happens = false;
         }
 
         // Give a message if no message otherwise.
