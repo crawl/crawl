68bd4261cb0a3b3318f271d3b3fa11119628d1e3
DracoOmega <draco_omega@live.com>
2014-01-15 20:52:59 -0330

    Let shambling mangroves sometimes nest ravens instead of wasps
    Intended as part of another overall nerf compared to old treants.
    
    I wouldn't mind seeing a 3rd option for what kind of fauna might
    be hiding in their foliage, but I'm not sure what a sensible choice
    might be (there seem to be few options that are both thematically
    plausible and mechanically reasonable).

--- crawl-ref/source/mon-abil.cc
+++ crawl-ref/source/mon-abil.cc
@@ -2078,24 +2078,29 @@ bool lost_soul_spectralize(monster* mons)
     return false;
 }
 
-void treant_release_wasps(monster* mons)
+void treant_release_fauna(monster* mons)
 {
     int count = mons->number;
     bool created = false;
+
+    monster_type base_t = (one_chance_in(3) ? MONS_YELLOW_WASP
+                                            : MONS_RAVEN);
+
     for (int i = 0; i < count; ++i)
     {
-        monster_type wasp_t = (one_chance_in(3) ? MONS_RED_WASP
-                                                : MONS_YELLOW_WASP);
+        monster_type fauna_t = (base_t == MONS_YELLOW_WASP && one_chance_in(3)
+                                                ? MONS_RED_WASP
+                                                : base_t);
 
-        mgen_data wasp_data(wasp_t, SAME_ATTITUDE(mons),
+        mgen_data fauna_data(fauna_t, SAME_ATTITUDE(mons),
                             mons, 0, SPELL_NO_SPELL, mons->pos(),
                             mons->foe);
-        wasp_data.extra_flags |= MF_WAS_IN_VIEW;
-        monster* wasp = create_monster(wasp_data);
+        fauna_data.extra_flags |= MF_WAS_IN_VIEW;
+        monster* fauna = create_monster(fauna_data);
 
-        if (wasp)
+        if (fauna)
         {
-            wasp->props["band_leader"].get_int() = mons->mid;
+            fauna->props["band_leader"].get_int() = mons->mid;
             created = true;
             mons->number--;
         }
@@ -2103,8 +2108,16 @@ void treant_release_wasps(monster* mons)
 
     if (created && you.can_see(mons))
     {
-        mprf("Angry insects surge out from beneath %s foliage!",
-            mons->name(DESC_ITS).c_str());
+        if (base_t == MONS_YELLOW_WASP)
+        {
+                mprf("Angry insects surge out from beneath %s foliage!",
+                        mons->name(DESC_ITS).c_str());
+        }
+        else if (base_t == MONS_RAVEN)
+        {
+                mprf("Agitated ravens fly out from beneath %s foliage!",
+                        mons->name(DESC_ITS).c_str());
+        }
     }
 }
 
@@ -4240,8 +4253,8 @@ bool mon_special_ability(monster* mons, bolt & beem)
     {
         if (mons->hit_points * 2 < mons->max_hit_points && mons->number > 0)
         {
-            treant_release_wasps(mons);
-            // Intentionally takes no energy; the insects are flying free
+            treant_release_fauna(mons);
+            // Intentionally takes no energy; the creatures are flying free
             // on their own time.
         }
 
