b17be70d123628af7834533e4764548a6e294013
Chris Campbell <chriscampbell89@gmail.com>
2014-01-19 22:21:51 +0000

    Make umbra/corona's effects on stealth independent of current LOS radius
    Not many things change LOS radius, so it's mainly just a weird hidden
    interaction.

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -3925,12 +3925,11 @@ int check_stealth(void)
 
     // If you've been tagged with Corona or are Glowing, the glow
     // makes you extremely unstealthy.
-    // The darker it is, the bigger the penalty.
     if (you.backlit())
-        stealth = (2 * you.current_vision * stealth) / (5 * LOS_RADIUS);
+        stealth = stealth * 2 / 5;
     // On the other hand, shrouding has the reverse effect:
     if (you.umbra())
-        stealth = (2 * LOS_RADIUS * stealth) / you.current_vision;
+        stealth *= 2;
     // The shifting glow from the Orb, while too unstable to negate invis
     // or affect to-hit, affects stealth even more than regular glow.
     if (orb_haloed(you.pos()))
