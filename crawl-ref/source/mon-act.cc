0d6be078a4445c9ad6efac7254384650a2756be0
DracoOmega <draco_omega@live.com>
2014-01-21 15:09:03 -0330

    A few more siren behavioral tweaks
    Make sirens even more likely to prefer deep water than before (to
    the point of being willing to move past the player to reach some, when
    previously they never would if shallow was available closer to them).
    Given how key it is to their offense, it seems important to prioritize
    it further.
    
    Also adjust siren pathfinding slightly so that they will attempt to
    move around the player instead of stopping in front of them, if you
    happen to bar their path.
    
    Finally, remove siren's melee attack altogether. It almost never
    works anyway and was harmless even when it did. It is better not to
    give the illusion that she will actually hit something when she so
    rarely does, I think.

--- crawl-ref/source/mon-act.cc
+++ crawl-ref/source/mon-act.cc
@@ -3168,10 +3168,14 @@ bool mon_can_move_to_pos(const monster* mons, const coord_def& delta,
         }
     }
 
-    // Don't voluntarily break LoS with a player we're mesmerising
     if (mons->type == MONS_SIREN)
     {
+        // Don't voluntarily break LoS with a player we're mesmerising
         if (you.beheld_by(mons) && !you.see_cell(targ))
+            return false;
+
+        // And path around players instead of into them
+        if (you.pos() == targ)
             return false;
     }
 
