208d70f378556486fc4980440a4c20696ab3a747
Keanan Smith <keanan.smith@gmail.com>
2014-01-28 16:00:07 -0700

    Adding Over 100 percent fails to spell descriptions

--- crawl-ref/source/spl-cast.h
+++ crawl-ref/source/spl-cast.h
@@ -57,7 +57,7 @@ int list_spells(bool toggle_with_I = true, bool viewing = false,
                 bool allow_preselect = true,
                 const string &title = "Your Spells",
                 spell_selector selector = NULL);
-int spell_fail(spell_type spell);
+int spell_fail(spell_type spell,bool capped = true);
 int calc_spell_power(spell_type spell, bool apply_intel,
                      bool fail_rate_chk = false, bool cap_power = true,
                      bool rod = false);
