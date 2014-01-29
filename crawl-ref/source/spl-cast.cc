6319e4977d2fa6f253e0243c61b8af8c88647805
Chris Campbell <chriscampbell89@gmail.com>
2014-01-29 17:52:55 +0000

    Don't shadow mimic spells that don't target monsters
    Which is currently just Dig, out of the previous set of mimicable spells.

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -1341,6 +1341,7 @@ spret_type your_spells(spell_type spell, int powc,
         actor* victim = actor_at(beam.target);
         if (you_worship(GOD_DITHMENGOS)
             && (flags & SPFLAG_TARGETING_MASK)
+            && !(flags & SPFLAG_NEUTRAL)
             && (!old_target || (victim && !victim->is_player())))
         {
             dithmengos_shadow_spell(beam.target, spell);
