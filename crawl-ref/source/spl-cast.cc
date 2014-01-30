a376ec7f6557a313cbbe1e8040636e5fdb30dcd2
Chris Campbell <chriscampbell89@gmail.com>
2014-01-30 23:11:06 +0000

    Don't shadow mimic rod spells
    Since no other evocables are mimicked.

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -1340,6 +1340,7 @@ spret_type your_spells(spell_type spell, int powc,
             trigger_battlesphere(&you, beam);
         actor* victim = actor_at(beam.target);
         if (you_worship(GOD_DITHMENGOS)
+            && allow_fail
             && (flags & SPFLAG_TARGETING_MASK)
             && !(flags & SPFLAG_NEUTRAL)
             && (!old_target || (victim && !victim->is_player())))
