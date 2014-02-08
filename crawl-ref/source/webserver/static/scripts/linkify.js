3228fd263c54a8fd5b3499222bc2f827059f43a8
Pekka Lampila <pekka.lampila@iki.fi>
2014-02-08 04:55:10 +0200

    Fix a missing escape in chat linkify

--- crawl-ref/source/webserver/static/scripts/linkify.js
+++ crawl-ref/source/webserver/static/scripts/linkify.js
@@ -6,6 +6,10 @@ define(["jquery", "contrib/ba-linkify.min"], function ($) {
     var ba_linkify = window.linkify;
     delete window.linkify;
 
+    function escape_html(str) {
+        return str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+    }
+
     function linkify(text)
     {
         return ba_linkify(text,
@@ -13,11 +17,11 @@ define(["jquery", "contrib/ba-linkify.min"], function ($) {
             callback: function (text, href)
             {
                 if (!href)
-                    return text;
+                    return escape_html(text);
                 if (!ALLOWED_PROTOCOLS.some(function (p) {
                         return href.indexOf(p+ "://") === 0; }))
                 {
-                    return text;
+                    return escape_html(text);
                 }
                 return $("<a>").attr("href", href).attr("target", "_blank")
                     .text(text)[0].outerHTML;
