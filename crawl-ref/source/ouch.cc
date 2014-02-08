crawl/crawl

commit b36e17b1acfe60c734b228eb5805a17e9f1f3c57

Display a dialog on WebTiles after the game ends.
For players this is displayed in a case of a crash/error/unexpected end.
For watchers the dialog is displayed regardless of how the game ended.

crawl-ref/source/ouch.cc
@@ -1403,11 +1403,14 @@ void _end_game(scorefile_entry &se)
         clrscr();
     }
 
+#if defined(DGL_WHEREIS) || defined(USE_TILE_WEB)
+    string reason = se.get_death_type() == KILLED_BY_QUITTING? "quit" :
+                    se.get_death_type() == KILLED_BY_WINNING ? "won"  :
+                    se.get_death_type() == KILLED_BY_LEAVING ? "bailed out"
+                                                             : "dead";
 #ifdef DGL_WHEREIS
-    whereis_record(se.get_death_type() == KILLED_BY_QUITTING? "quit" :
-                   se.get_death_type() == KILLED_BY_WINNING ? "won"  :
-                   se.get_death_type() == KILLED_BY_LEAVING ? "bailed out"
-                                                            : "dead");
+    whereis_record(reason);
+#endif
 #endif
 
     if (!crawl_state.seen_hups)
@@ -1448,6 +1451,10 @@ void _end_game(scorefile_entry &se)
 
     if (se.get_death_type() == KILLED_BY_WINNING)
         crawl_state.last_game_won = true;
+
+#ifdef USE_TILE_WEB
+    tiles.send_exit_reason(reason, hiscore);
+#endif
 
     game_ended();
 }

crawl-ref/source/crash.cc
@@ -153,6 +153,7 @@ void crash_signal_handler(int sig_num)
 #endif
 
 #ifdef USE_TILE_WEB
+    tiles.send_exit_reason("crash");
     tiles.shutdown();
 #endif
 

crawl-ref/source/files.cc
@@ -1591,6 +1591,9 @@ static void _save_game_exit()
 #ifdef DGL_WHEREIS
     whereis_record("saved");
 #endif
+#ifdef USE_TILE_WEB
+    tiles.send_exit_reason("saved");
+#endif
 
     delete you.save;
     you.save = 0;

crawl-ref/source/stuff.cc
@@ -283,6 +283,10 @@ NORETURN void game_ended_with_error(const string &message)
     if (crawl_state.seen_hups)
         end(1);
 
+#ifdef USE_TILE_WEB
+    tiles.send_exit_reason("error", message);
+#endif
+
     if (Options.restart_after_game)
     {
         if (crawl_state.io_inited)

crawl-ref/source/tileweb.cc
@@ -149,6 +149,7 @@ bool TilesFramework::initialise()
         _await_connection();
 
     _send_version();
+    send_exit_reason("unknown");
     _send_options();
 
     m_cursor[CURSOR_MOUSE] = NO_CURSOR;
@@ -440,6 +441,20 @@ void TilesFramework::dump()
                 m_json_stack[i].start, m_json_stack[i].prefix_end,
                 m_json_stack[i].type);
     }
+}
+
+void TilesFramework::send_exit_reason(const string& type, const string& message)
+{
+    write_message("*");
+    write_message("{\"msg\":\"exit_reason\",\"type\":\"");
+    write_message_escaped(type);
+    if (!message.empty())
+    {
+        write_message("\",\"message\":\"");
+        write_message_escaped(message);
+    }
+    write_message("\"}");
+    finish_message();
 }
 
 void TilesFramework::_send_version()

crawl-ref/source/tileweb.h
@@ -132,6 +132,8 @@ public:
     void pop_menu();
     void close_all_menus();
 
+    void send_exit_reason(const string& type, const string& message = "");
+
     string get_message();
     void write_message(PRINTF(1, ));
     void finish_message();

crawl-ref/source/webserver/process_handler.py
@@ -158,6 +158,8 @@ class CrawlProcessHandlerBase(object):
 
         for watcher in list(self._receivers):
             if watcher.watched_game == self:
+                watcher.send_message("game_ended", reason = self.exit_reason,
+                                     message = self.exit_message)
                 watcher.go_lobby()
 
         if self.end_callback:
@@ -210,10 +212,10 @@ class CrawlProcessHandlerBase(object):
 
     def add_watcher(self, watcher):
         self.last_watcher_join = time.time()
-        self._receivers.add(watcher)
         if self.client_path:
             self._send_client(watcher)
             watcher.send_json_options(self.game_params["id"], self.username)
+        self._receivers.add(watcher)
         self.update_watcher_description()
 
     def remove_watcher(self, watcher):
@@ -354,6 +356,9 @@ class CrawlProcessHandler(CrawlProcessHandlerBase):
         self.ttyrec_filename = None
         self.inprogress_lock = None
         self.inprogress_lock_file = None
+
+        self.exit_reason = None
+        self.exit_message = None
 
         self._stale_pid = None
         self._stale_lockfile = None
@@ -658,6 +663,12 @@ class CrawlProcessHandler(CrawlProcessHandlerBase):
                 # only queue, once we know the crawl process asks for flushes
                 self.queue_messages = True;
                 self.flush_messages_to_all()
+            elif msgobj["msg"] == "exit_reason":
+                self.exit_reason = msgobj["type"]
+                if "message" in msgobj:
+                    self.exit_message = msgobj["message"]
+                else:
+                    self.exit_message = None
             else:
                 self.logger.warning("Unknown message from the crawl process: %s",
                                     msgobj["msg"])

crawl-ref/source/webserver/static/scripts/client.js
@@ -15,9 +15,13 @@ function (exports, $, key_conversion, chat, comm) {
     var message_inhibit = 0;
     var message_queue = [];
 
+    var watching = false;
+    var watching_username;
+    var playing = false;
     var logging_in = false;
     var showing_close_message = false;
     var current_hash;
+    var exit_reason, exit_message;
 
     var send_message = comm.send_message;
 
@@ -505,6 +509,62 @@ function (exports, $, key_conversion, chat, comm) {
         show_dialog(dialog);
     }
 
+    function possessive(name)
+    {
+        return name + "'" + (name.slice(-1) === "s" ? "" : "s");
+    }
+
+    function exit_reason_message(reason, watched_name)
+    {
+        if (watched_name)
+        {
+            switch (reason)
+            {
+            case "saved":
+                return watched_name + " stopped playing (saved).";
+            case "quit":
+                return watched_name + " quit his game.";
+            case "won":
+                return watched_name + " won his game.";
+            case "bailed out":
+                return watched_name + " bailed out of his game.";
+            case "dead":
+                return watched_name + " died in his game.";
+            case "crash":
+                return possessive(watched_name) + " game crashed.";
+            case "error":
+                return possessive(watched_name)
+                       + " game was terminated due to an error.";
+            default:
+                return possessive(watched_name) + " game ended unexpectedly."
+                       + (reason != "unknown" ? " (" + reason + ")" : "");
+            }
+        }
+        else
+        {
+            switch (reason)
+            {
+            case "crash":
+                return "Unfortunately your game crashed.";
+            case "error":
+                return "Unfortunately your game terminated due to an error.";
+            default:
+                return "Unfortunately your game ended unexpectedly."
+                       + (reason != "unknown" ? " (" + reason + ")" : "");
+            }
+        }
+    }
+
+    function show_exit_dialog(reason, message, watched_name)
+    {
+        $("#exit_game_reason").text(exit_reason_message(reason, watched_name));
+        if (message)
+            $("#exit_game_message").text(message).show();
+        else
+            $("#exit_game_message").hide();
+        show_dialog("#exit_game");
+    }
+
     function start_register()
     {
         show_dialog("#register");
@@ -628,13 +688,27 @@ function (exports, $, key_conversion, chat, comm) {
         watching = false;
     }
 
+    var normal_exit = ["saved", "quit", "won", "bailed out", "dead"];
     function go_lobby()
     {
+        var was_watching = watching_username;
+
         cleanup();
         current_hash = "#lobby";
         location.hash = "#lobby";
         set_layer("lobby");
         $("#username").focus();
+
+        if (exit_reason)
+        {
+            if (was_watching || normal_exit.indexOf(exit_reason) === -1)
+            {
+                show_exit_dialog(exit_reason, exit_message,
+                                 was_watching ? watching_username : null);
+            }
+        }
+        exit_reason = null;
+        exit_message = null;
     }
 
     function login_required(data)
@@ -866,10 +940,10 @@ function (exports, $, key_conversion, chat, comm) {
         "force_terminate?": handle_force_terminate
     });
 
-    var watching = false;
-    function watching_started()
+    function watching_started(data)
     {
         watching = true;
+        watching_username = data.username;
         playing = false;
     }
     exports.is_watching = function ()
@@ -877,15 +951,17 @@ function (exports, $, key_conversion, chat, comm) {
         return watching;
     }
 
-    var playing = false;
     function crawl_started()
     {
         playing = true;
         watching = false;
     }
-    function crawl_ended()
+    function crawl_ended(data)
     {
         playing = false;
+
+        exit_reason = data.reason;
+        exit_message = data.message;
     }
 
     function hash_changed()
@@ -1081,6 +1157,8 @@ function (exports, $, key_conversion, chat, comm) {
                 return "Really save and quit the game?";
             }
         });
+
+        $(".hide_dialog").click(hide_dialog);
 
         $("#login_form").bind("submit", login);
         $("#remember_me").bind("click", remember_me_click);

commit 197e7d5713c86faf8405211eef4ac91c96ccab31

Fix DGL compilation.
Broken by b36e17b.

crawl-ref/source/ouch.cc
@@ -1413,7 +1413,7 @@ void _end_game(scorefile_entry &se)
                     se.get_death_type() == KILLED_BY_LEAVING ? "bailed out"
                                                              : "dead";
 #ifdef DGL_WHEREIS
-    whereis_record(reason);
+    whereis_record(reason.c_str());
 #endif
 #endif
 