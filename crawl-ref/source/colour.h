f57b90b356425e982ac999b0bf2c52cd827675b3
Shmuale Mark <shm.mark@gmail.com>
2014-03-03 02:27:15 -0500

    Restore Swamp tree colours; remove ETC_MANGROVE

--- crawl-ref/source/colour.h
+++ crawl-ref/source/colour.h
@@ -52,7 +52,9 @@ enum element_type
     ETC_RANDOM,         // any colour (except BLACK)
     ETC_TORNADO,        // twisting swirls of gray
     ETC_LIQUEFIED,      // ripples of yellow and brown.
+#if TAG_MAJOR_VERSION == 34
     ETC_MANGROVE,       // colour of trees on water
+#endif
     ETC_ORB_GLOW,       // halo coming from the Orb of Zot
     ETC_DISJUNCTION,    // halo from Disjunction
     ETC_DITHMENOS,      // Dithmenos altar colours
