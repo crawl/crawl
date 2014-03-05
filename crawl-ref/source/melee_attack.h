2fa008d7ef6b548af99d07590a98d912737fec5a
DracoOmega <draco_omega@live.com>
2014-03-05 15:38:30 -0330

    Refactor some melee poison checks

--- crawl-ref/source/melee_attack.h
+++ crawl-ref/source/melee_attack.h
@@ -148,7 +148,7 @@ private:
     string mons_attack_verb();
     string mons_attack_desc();
     // TODO: Unify do_poison and poison_monster
-    bool mons_do_poison(bool always = false);
+    bool mons_do_poison();
     void mons_do_napalm();
     void mons_do_eyeball_confusion();
     void apply_black_mark_effects();
