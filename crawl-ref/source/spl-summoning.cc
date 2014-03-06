crawl/crawl

commit a5274fd6dc5599ce2afdc882a46526b036e8f25f

Refactored random_near_space(); fixed bugs
Cleaned up bugs in random_near_space(). Monsters can no longer blink
through walls, or into lava/deep water. Ball lightning can be summoned
over lava/deep water.

[Committer's note: cleaned up a bit of code formatting and comments.]

Signed-off-by: Steve Melenchuk <smelenchuk@gmail.com>

crawl-ref/source/spl-summoning.cc
@@ -814,7 +814,7 @@ spret_type cast_conjure_ball_lightning(int pow, god_type god, bool fail)
         bool found = false;
         for (int j = 0; j < 10; ++j)
         {
-            if (random_near_space(you.pos(), target, true, true, false)
+            if (random_near_space(you.pos(), target, true, false, false)
                 && distance2(you.pos(), target) <= 5)
             {
                 found = true;

crawl-ref/source/teleport.cc
@@ -117,7 +117,7 @@ bool monster::blink_to(const coord_def& dest, bool quiet, bool jump)
 typedef pair<coord_def, int> coord_weight;
 
 // Try to find a "safe" place for moved close or far from the target.
-// keep_los indicates that the destination should be in view of the target.
+// keep_los indicates that the destination should be in LOS of the target.
 //
 // XXX: Check the result against in_bounds(), not coord_def::origin(),
 // because of a memory problem described below. (isn't this fixed now? -rob)
@@ -130,12 +130,12 @@ static coord_def random_space_weighted(actor* moved, actor* target,
 
     for (radius_iterator ri(moved->pos(), LOS_NO_TRANS); ri; ++ri)
     {
-        if (!moved->is_habitable(*ri) || actor_at(*ri)
-            || keep_los && !target->see_cell_no_trans(*ri)
-            || !allow_sanct && is_sanctuary(*ri))
+        if (!valid_blink_destination(moved, *ri, !allow_sanct)
+            || (keep_los && !target->see_cell_no_trans(*ri)))
         {
             continue;
         }
+
         int weight;
         int dist = (tpos - *ri).rdist();
         if (close)
@@ -224,34 +224,39 @@ void blink_close(monster* mon)
 #endif
 }
 
-bool random_near_space(const coord_def& origin, coord_def& target,
-                       bool allow_adjacent, bool restrict_los,
-                       bool forbid_dangerous, bool forbid_sanctuary)
+// This only checks the contents of the tile - nothing in between.
+// Could compact most of this into a big boolean if you wanted to trade
+// readability for dubious speed improvements.
+bool valid_blink_destination(const actor* moved, const coord_def& target,
+                             bool forbid_sanctuary,
+                             bool forbid_unhabitable)
 {
-    // This might involve ray tracing (via num_feats_between()), so
-    // cache results to avoid duplicating ray traces.
+
+    if (!in_bounds(target))
+        return false;
+    if (actor_at(target))
+        return false;
+    if (forbid_unhabitable && !moved->is_habitable(target))
+        return false;
+    if (forbid_sanctuary && is_sanctuary(target))
+        return false;
+    if (!moved->see_cell_no_trans(target))
+        return false;
+
+    return true;
+}
+
+bool random_near_space(const coord_def& origin, coord_def& target,
+                       bool allow_adjacent, bool forbid_sanctuary,
+                       bool forbid_unhabitable)
+{
+    // This might involve ray tracing (LOS calcs in valid_blink_destination),
+    // so cache results to avoid duplicating ray traces.
 #define RNS_OFFSET 6
 #define RNS_WIDTH (2*RNS_OFFSET + 1)
     FixedArray<bool, RNS_WIDTH, RNS_WIDTH> tried;
     const coord_def tried_o = coord_def(RNS_OFFSET, RNS_OFFSET);
     tried.init(false);
-
-    // Is the monster on the other side of a transparent wall?
-    const bool trans_wall_block  = you.trans_wall_blocking(origin);
-    const bool origin_is_player  = (you.pos() == origin);
-    int min_walls_between = 0;
-
-    // Skip ray tracing if possible.
-    if (trans_wall_block && !crawl_state.game_is_arena())
-    {
-        // XXX: you.pos() is invalid in the arena.
-        min_walls_between = num_feats_between(origin, you.pos(),
-                                              DNGN_MINSEE,
-                                              DNGN_MAX_NONREACH);
-    }
-
-    const bool lava_dangerous = is_feat_dangerous(DNGN_LAVA, true);
-    const bool water_dangerous = is_feat_dangerous(DNGN_DEEP_WATER, true);
 
     for (int tries = 0; tries < 150; tries++)
     {
@@ -263,81 +268,12 @@ bool random_near_space(const coord_def& origin, coord_def& target,
 
         target = origin + (p - tried_o);
 
-        // Origin is not 'near'.
-        if (target == origin)
-            continue;
-
-        if (!in_bounds(target)
-            || restrict_los && !you.see_cell(target)
-            || grd(target) < DNGN_MINMOVE   // Target always invalid
-            || (grd(target) < DNGN_MINWALK  // Target maybe invalid
-                && (grd(target) == DNGN_LAVA && lava_dangerous
-                    || grd(target) == DNGN_DEEP_WATER && water_dangerous))
-            || actor_at(target)
-            || !allow_adjacent && distance2(origin, target) <= 2
-            || forbid_sanctuary && is_sanctuary(target))
+        if (valid_blink_destination(actor_at(origin), target,
+                                    forbid_sanctuary, forbid_unhabitable)
+            && (allow_adjacent || distance2(origin, target) > 2))
         {
-            continue;
-        }
-
-        // Don't pick grids that contain a dangerous cloud.
-        if (forbid_dangerous)
-        {
-            const int cloud = env.cgrid(target);
-
-            if (cloud != EMPTY_CLOUD
-                && is_damaging_cloud(env.cloud[cloud].type, true))
-            {
-                continue;
-            }
-        }
-
-        if (!trans_wall_block && !origin_is_player)
             return true;
-
-        // If the monster is on a visible square which is on the other
-        // side of one or more translucent walls from the player, then it
-        // can only blink through translucent walls if the end point
-        // is either not visible to the player, or there are at least
-        // as many translucent walls between the player and the end
-        // point as between the player and the start point.  However,
-        // monsters can still blink through translucent walls to get
-        // away from the player, since in the absence of translucent
-        // walls monsters can blink to places which are not in either
-        // the monster's nor the player's LOS.
-        if (!origin_is_player && !you.see_cell(target))
-            return true;
-
-        // Player can't randomly pass through translucent walls.
-        if (origin_is_player)
-        {
-            if (you.see_cell_no_trans(target))
-                return true;
-
-            continue;
         }
-
-        int walls_passed = num_feats_between(target, origin,
-                                             DNGN_MINSEE,
-                                             DNGN_MAX_NONREACH,
-                                             true, true);
-        if (walls_passed == 0)
-            return true;
-
-        // Player can't randomly pass through translucent walls.
-        if (origin_is_player)
-            continue;
-
-        int walls_between = 0;
-        if (!crawl_state.game_is_arena())
-        {
-            walls_between = num_feats_between(target, you.pos(),
-                                              DNGN_MINSEE,
-                                              DNGN_MAX_NONREACH);
-        }
-
-        if (walls_between >= min_walls_between)
-            return true;
     }
 
     return false;

crawl-ref/source/teleport.h
@@ -10,9 +10,12 @@ bool blink_away(monster* mon, actor* caster, bool from_seen = true, bool self_ca
 void blink_range(monster* mon);
 void blink_close(monster* mon);
 
+bool valid_blink_destination(const actor* moved, const coord_def& target,
+                             bool forbid_sanctuary = false,
+                             bool forbid_unhabitable = true);
 bool random_near_space(const coord_def& origin, coord_def& target,
-                       bool allow_adjacent = false, bool restrict_LOS = true,
-                       bool forbid_dangerous = false,
-                       bool forbid_sanctuary = false);
+                       bool allow_adjacent = false,
+                       bool forbid_sanctuary = false,
+                       bool forbid_unhabitable = true);
 
 #endif

crawl-ref/source/throw.cc
@@ -681,39 +681,16 @@ static bool _dispersal_hit_victim(bolt& beam, actor* victim, int dmg)
 
     coord_def pos, pos2;
 
-    int tries = 0;
-    do
-    {
-        if (!random_near_space(victim->pos(), pos, false, true, false,
-                               no_sanct))
-        {
-            return false;
-        }
-    }
-    while (!victim->is_habitable(pos) && tries++ < 100);
-
-    if (!victim->is_habitable(pos))
+    if (!random_near_space(victim->pos(), pos, false, no_sanct))
         return false;
 
-    tries = 0;
-    do
-        if (!random_near_space(victim->pos(), pos2, false, true, false,
-                               no_sanct))
-        {
-            return false;
-        }
-    while (!victim->is_habitable(pos2) && tries++ < 100);
-
-    if (!victim->is_habitable(pos2))
+    if (!random_near_space(victim->pos(), pos2, false, no_sanct))
         return false;
 
     // Pick the square further away from the agent.
     const coord_def from = agent->pos();
-    if (in_bounds(pos2) && distance2(pos2, from) > distance2(pos, from))
+    if (distance2(pos2, from) > distance2(pos, from))
         pos = pos2;
-
-    if (pos == victim->pos())
-        return false;
 
     const coord_def oldpos = victim->pos();
     victim->clear_clinging();

crawl-ref/source/xom.cc
@@ -1981,9 +1981,9 @@ static int _xom_change_scenery(bool debug = false)
         const int max_altars = max(1, random2(random2(14)));
         for (int tries = max_altars; tries > 0; --tries)
         {
-            if ((random_near_space(you.pos(), place)
+            if ((random_near_space(you.pos(), place, false)
                     || random_near_space(you.pos(), place, true))
-                && grd(place) == DNGN_FLOOR && you.see_cell(place))
+                && grd(place) == DNGN_FLOOR)
             {
                 if (debug)
                     return XOM_GOOD_SCENERY;

commit 587b4fded1ad0b496cce0a708282ca00d3c1dc84

Don't crash on semicontrolled blink (#8236).
Broken by a5274fd.

crawl-ref/source/spl-summoning.cc
@@ -966,6 +966,11 @@ spret_type cast_conjure_ball_lightning(int pow, god_type god, bool fail)
     fail_check();
     bool success = false;
 
+    // XXX: for finding a nearby space
+    monster temp;
+    temp.type = MONS_BALL_LIGHTNING;
+    temp.position = you.pos();
+
     // Restricted so that the situation doesn't get too gross.  Each of
     // these will explode for 3d20 damage. -- bwr
     const int how_many = min(8, 3 + random2avg(2 + pow / 50, 2));
@@ -976,7 +981,7 @@ spret_type cast_conjure_ball_lightning(int pow, god_type god, bool fail)
         bool found = false;
         for (int j = 0; j < 10; ++j)
         {
-            if (random_near_space(you.pos(), target, true, false, false)
+            if (random_near_space(&temp, you.pos(), target, true, false, false)
                 && distance2(you.pos(), target) <= 5)
             {
                 found = true;

crawl-ref/source/spl-transloc.cc
@@ -319,8 +319,8 @@ void random_blink(bool allow_partial_control, bool override_abyss, bool override
     }
     // First try to find a random square not adjacent to the player,
     // then one adjacent if that fails.
-    else if (!random_near_space(you.pos(), target)
-             && !random_near_space(you.pos(), target, true))
+    else if (!random_near_space(&you, you.pos(), target)
+             && !random_near_space(&you, you.pos(), target, true))
     {
         mpr("You feel jittery for a moment.");
     }
@@ -989,8 +989,8 @@ static bool _quadrant_blink(coord_def dir, int pow)
         // Find a space near our base point...
         // First try to find a random square not adjacent to the basepoint,
         // then one adjacent if that fails.
-        if (!random_near_space(base, target)
-            && !random_near_space(base, target, true))
+        if (!random_near_space(&you, base, target)
+            && !random_near_space(&you, base, target, true))
         {
             // Uh oh, WHY should this fail the blink?
             return false;

crawl-ref/source/teleport.cc
@@ -231,6 +231,7 @@ bool valid_blink_destination(const actor* moved, const coord_def& target,
                              bool forbid_sanctuary,
                              bool forbid_unhabitable)
 {
+    ASSERT(moved);
 
     if (!in_bounds(target))
         return false;
@@ -246,7 +247,8 @@ bool valid_blink_destination(const actor* moved, const coord_def& target,
     return true;
 }
 
-bool random_near_space(const coord_def& origin, coord_def& target,
+bool random_near_space(const actor* victim,
+                       const coord_def& origin, coord_def& target,
                        bool allow_adjacent, bool forbid_sanctuary,
                        bool forbid_unhabitable)
 {
@@ -268,7 +270,7 @@ bool random_near_space(const coord_def& origin, coord_def& target,
 
         target = origin + (p - tried_o);
 
-        if (valid_blink_destination(actor_at(origin), target,
+        if (valid_blink_destination(victim, target,
                                     forbid_sanctuary, forbid_unhabitable)
             && (allow_adjacent || distance2(origin, target) > 2))
         {

crawl-ref/source/teleport.h
@@ -13,7 +13,8 @@ void blink_close(monster* mon);
 bool valid_blink_destination(const actor* moved, const coord_def& target,
                              bool forbid_sanctuary = false,
                              bool forbid_unhabitable = true);
-bool random_near_space(const coord_def& origin, coord_def& target,
+bool random_near_space(const actor* victim,
+                       const coord_def& origin, coord_def& target,
                        bool allow_adjacent = false,
                        bool forbid_sanctuary = false,
                        bool forbid_unhabitable = true);

crawl-ref/source/throw.cc
@@ -682,10 +682,10 @@ static bool _dispersal_hit_victim(bolt& beam, actor* victim, int dmg)
 
     coord_def pos, pos2;
 
-    if (!random_near_space(victim->pos(), pos, false, no_sanct))
+    if (!random_near_space(victim, victim->pos(), pos, false, no_sanct))
         return false;
 
-    if (!random_near_space(victim->pos(), pos2, false, no_sanct))
+    if (!random_near_space(victim, victim->pos(), pos2, false, no_sanct))
         return false;
 
     // Pick the square further away from the agent.

crawl-ref/source/xom.cc
@@ -1978,8 +1978,8 @@ static int _xom_change_scenery(bool debug = false)
         const int max_altars = max(1, random2(random2(14)));
         for (int tries = max_altars; tries > 0; --tries)
         {
-            if ((random_near_space(you.pos(), place, false)
-                    || random_near_space(you.pos(), place, true))
+            if ((random_near_space(&you, you.pos(), place, false)
+                    || random_near_space(&you, you.pos(), place, true))
                 && grd(place) == DNGN_FLOOR)
             {
                 if (debug)