crawl/crawl

commit c60660aeeb46ff28d2db71ce72d83e894294265f

Revert "Negate swiftness with stasis."
Formicids shouldn't change how stasis works for all other species. If this is
to be changed for everyone it shouldn't be hidden as part of the merge.

This reverts commit c1a2e1451a6ea5a45cc18280012de944af2e1284.

crawl-ref/source/spl-selfench.cc
@@ -12,7 +12,6 @@
 #include "env.h"
 #include "godconduct.h"
 #include "hints.h"
-#include "item_use.h"
 #include "libutil.h"
 #include "message.h"
 #include "misc.h"
@@ -193,9 +192,10 @@ spret_type cast_swiftness(int power, bool fail)
         return SPRET_ABORT;
     }
 
-    if (stasis_blocks_effect(true, true, "%s emits a piercing whistle.", 20,
-                             "%s makes your neck tingle."))
+    if (you.in_water() || you.liquefied_ground())
     {
+        mprf("The %s foams!", you.in_water() ? "water"
+                                             : "liquid ground");
         return SPRET_ABORT;
     }
 

crawl-ref/source/player-equip.cc
@@ -1396,8 +1396,7 @@ static void _equip_jewellery_effect(item_def &item, bool unmeld)
         // Berserk is possible with a Battlelust card or with a moth of wrath
         // that affects you while donning the amulet.
         int amount = you.duration[DUR_HASTE] + you.duration[DUR_SLOW]
-                     + you.duration[DUR_BERSERK] + you.duration[DUR_FINESSE]
-                     + you.duration[DUR_SWIFTNESS];
+                     + you.duration[DUR_BERSERK] + you.duration[DUR_FINESSE];
         if (you.duration[DUR_TELEPORT])
             amount += 30 + random2(150);
         if (amount)
@@ -1427,14 +1426,11 @@ static void _equip_jewellery_effect(item_def &item, bool unmeld)
             // my thesaurus says this usage is correct
             if (you.duration[DUR_FINESSE])
                 mpr("Your hands get arrested.", MSGCH_DURATION);
-            if (you.duration[DUR_SWIFTNESS])
-                mpr("You feel less swift.", MSGCH_DURATION);
             you.duration[DUR_HASTE] = 0;
             you.duration[DUR_SLOW] = 0;
             you.duration[DUR_TELEPORT] = 0;
             you.duration[DUR_BERSERK] = 0;
             you.duration[DUR_FINESSE] = 0;
-            you.duration[DUR_SWIFTNESS] = 0;
         }
         break;
 

commit 8c4a6f6881f4fdffe334ee66afce2c634b4dabd0

Fix Swiftness being uncastable in liquids
Broken by c60660aeeb46ff.

crawl-ref/source/spl-selfench.cc
@@ -192,13 +192,6 @@ spret_type cast_swiftness(int power, bool fail)
         return SPRET_ABORT;
     }
 
-    if (you.in_water() || you.liquefied_ground())
-    {
-        mprf("The %s foams!", you.in_water() ? "water"
-                                             : "liquid ground");
-        return SPRET_ABORT;
-    }
-
     if (!you.duration[DUR_SWIFTNESS] && player_movement_speed() <= 6)
     {
         mpr("You can't move any more quickly.");