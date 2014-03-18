e645f60c7147ca81d07a8efff1080fcb275ae681
Shmuale Mark <shm.mark@gmail.com>
2014-03-18 13:19:24 -0400

    Don't let Wanderers start with a 2h weapon and shield equipped (#7929).
    (cherry picked from commit 5be82303a5289991ac680c19921c8d949ebf9daa)
    (cherry picked from commit aa30ffd8de87c5afb8ad6b56c77612c99f42ce82)
    
    [The second of these commits fixed a crash introduced by the first one]

--- crawl-ref/source/ng-setup.cc
+++ crawl-ref/source/ng-setup.cc
@@ -446,6 +446,12 @@ void newgame_make_item(int slot, equipment_type eqslot,
     if (item.base_type == OBJ_ARMOUR && !can_wear_armour(item, false, false))
         return;
 
+    if (is_shield(item) && you.weapon()
+        && is_shield_incompatible(*you.weapon(), &item))
+    {
+        return;
+    }
+
     if (eqslot == EQ_WEAPON && !can_wield(&item, false, false))
         return;
 
