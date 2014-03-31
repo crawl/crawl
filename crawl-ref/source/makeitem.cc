crawl/crawl

commit 21d5f32e6cd0d1d9c1fa285a8c08a18c26703dc0

Allow Olgeb's / Wucad Mu to generate via Stave acquirement (Mantis #7047)
Since these are elemental/magical staves they should stay as WPN_STAFF,
this alters acquirement code to allow them to generate.

crawl-ref/source/makeitem.cc
@@ -2888,8 +2888,21 @@ static void _generate_book_item(item_def& item, bool allow_uniques,
     }
 }
 
-static void _generate_staff_item(item_def& item, int force_type, int item_level)
+static void _generate_staff_item(item_def& item, bool allow_uniques, int force_type, int item_level)
 {
+    // If we make the unique roll, no further generation necessary.
+    // Copied unrand code from _try_make_weapon_artefact since randart elemental staves
+    // can't happen.
+    if (allow_uniques
+        && one_chance_in(item_level == MAKE_GOOD_ITEM ? 7 : 20))
+    {
+        // Temporarily fix the base_type to get elemental staves
+        item.base_type = OBJ_WEAPONS;
+        if (_try_make_item_unrand(item, WPN_STAFF))
+            return;
+        item.base_type = OBJ_STAVES;
+    }
+
     if (force_type == OBJ_RANDOM)
     {
 #if TAG_MAJOR_VERSION == 34
@@ -3248,7 +3261,7 @@ int items(bool allow_uniques,
         break;
 
     case OBJ_STAVES:
-        _generate_staff_item(item, force_type, item_level);
+        _generate_staff_item(item, allow_uniques, force_type, item_level);
         break;
 
     case OBJ_RODS:

commit 761c3c6f954097d468c452da7992dec243ae756d

Fix unrand staves being far more common than other unrand weapons.
21d5f32e6 caused any enhancer staff to have a chance at being replaced by
an unrand staff, not just those created by acquirement. Now unrand staves
normally only replace weapons (like every other unrand weapon), though they
are still produced by staff acquirement.

crawl-ref/source/makeitem.cc
@@ -2946,7 +2946,9 @@ int items(bool allow_uniques,
         break;
 
     case OBJ_STAVES:
-        _generate_staff_item(item, allow_uniques, force_type, item_level);
+        // Don't generate unrand staves this way except through acquirement,
+        // since they also generate as OBJ_WEAPONS.
+        _generate_staff_item(item, (agent != -1), force_type, item_level);
         break;
 
     case OBJ_RODS: