665ce8d164e144ef5fc9e7cb86082b9f36d71a13
Brendan Hickey <brendan@bhickey.net>
2014-03-31 19:33:26 -0400

    Revert "Revert "Make Large Rocks penetrating""
    Revert "Revert "Make Large Rocks penetrating""
    
    Players love penetrating rocks.
    
    This reverts commit 55eae48bbef799c535a5348705a30613e1907b32.

--- crawl-ref/source/beam.cc
+++ crawl-ref/source/beam.cc
@@ -3304,9 +3304,13 @@ void bolt::tracer_affect_player()
 }
 
 // Magical penetrating projectiles should pass through shields.
+// Natural ones (large rocks) shouldn't.
 static bool _shield_piercing(bolt *pbolt)
 {
-    return pbolt->range_used_on_hit() == 0;
+    return pbolt->range_used_on_hit() == 0
+           && (!pbolt->item
+               || pbolt->item->base_type != OBJ_MISSILES
+               || pbolt->item->sub_type  != MI_LARGE_ROCK);
 }
 
 bool bolt::misses_player()
--- crawl-ref/source/throw.cc
+++ crawl-ref/source/throw.cc
@@ -43,6 +43,7 @@
 #include "teleport.h"
 #include "terrain.h"
 #include "transform.h"
+#include "version.h"
 #include "view.h"
 #include "viewchar.h"
 
@@ -1108,7 +1109,10 @@ static bool _setup_missile_beam(const actor *agent, bolt &beam, item_def &item,
 
     const bool exploding    = (ammo_brand == SPMSL_EXPLODING);
     const bool penetrating  = (bow_brand  == SPWPN_PENETRATION
-                                || ammo_brand == SPMSL_PENETRATION);
+                                || ammo_brand == SPMSL_PENETRATION
+                                || (Version::ReleaseType == VER_ALPHA
+                                    && item.base_type == OBJ_MISSILES
+                                    && item.sub_type == MI_LARGE_ROCK));
     const bool silver       = (ammo_brand == SPMSL_SILVER);
     const bool disperses    = (ammo_brand == SPMSL_DISPERSAL);
     const bool charged      = bow_brand  == SPWPN_ELECTROCUTION;
@@ -1194,7 +1198,10 @@ static bool _setup_missile_beam(const actor *agent, bolt &beam, item_def &item,
     if (penetrating)
     {
         beam.range_funcs.push_back(_item_penetrates_victim);
-        beam.hit_verb = "pierces through";
+        if (item.base_type == OBJ_MISSILES && item.sub_type == MI_LARGE_ROCK)
+            beam.hit_verb = "crashes through";
+        else
+            beam.hit_verb = "pierces through";
     }
     if (disperses)
         beam.hit_funcs.push_back(_dispersal_hit_victim);
@@ -1243,7 +1250,8 @@ static bool _setup_missile_beam(const actor *agent, bolt &beam, item_def &item,
         ammo_name = "poisoned " + ammo_name;
     }
 
-    if (penetrating && item.special != SPMSL_PENETRATION)
+    if (penetrating && item.special != SPMSL_PENETRATION
+        && item.sub_type != MI_LARGE_ROCK)
     {
         beam.name = "penetrating " + beam.name;
         ammo_name = "penetrating " + ammo_name;
