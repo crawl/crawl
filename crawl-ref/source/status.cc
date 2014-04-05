a4595f383eee4f428b1d7a1e94596ec8940c69ad
Chris Oelmueller <chris.oelmueller@gmail.com>
2014-04-05 03:12:26 +0200

    Standardize some MR boosts.
    Trog's Hand gets 10 more MR (to 80), and the status line is updated
    to account for correct amount of pluses added.
    
    Lich form MR loses 10 MR (to 40).
    
    [squashed commits and combined messages --wheals.]

--- crawl-ref/source/status.cc
+++ crawl-ref/source/status.cc
@@ -791,7 +791,7 @@ static void _describe_regen(status_info* inf)
             inf->light_colour = _dur_colour(BLUE, dur_expiring(DUR_TROGS_HAND));
         inf->light_text   = "Regen";
         if (you.duration[DUR_TROGS_HAND])
-            inf->light_text += " MR";
+            inf->light_text += " MR++";
         else if (no_heal)
             inf->light_colour = DARKGREY;
     }
