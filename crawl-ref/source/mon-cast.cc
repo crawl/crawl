c53826d48bf9b0e68a2cf7474330cf6c932e0aea
Chris Campbell <chriscampbell89@gmail.com>
2014-04-06 15:56:23 +0100

    Don't let Phantom Mirror clone conjured monsters (#8349)
    (cherry picked from commit 20579a2dff7263dcecfa2a46061791a748d4fd0d)

--- crawl-ref/source/mon-cast.cc
+++ crawl-ref/source/mon-cast.cc
@@ -1997,7 +1997,7 @@ static bool _ms_waste_of_time(const monster* mon, spell_type monspell)
             for (monster_near_iterator mi(mon); mi; ++mi)
             {
                 if (*mi != mon && mons_aligned(*mi, mon) && !mi->is_stationary()
-                    && !mi->is_summoned())
+                    && !mi->is_summoned() && !mi->is_conjured())
                 {
                     // A single valid target is enough.
                     return false;
