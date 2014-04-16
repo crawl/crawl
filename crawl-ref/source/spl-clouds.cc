721957f40604a60269fbcae1d08d05200971a224
Chris Campbell <chriscampbell89@gmail.com>
2014-04-16 02:53:05 +0100

    Don't let Conjure Flame target trees
    Since it requires empty space to cast in all other cases.

--- crawl-ref/source/spl-clouds.cc
+++ crawl-ref/source/spl-clouds.cc
@@ -35,18 +35,6 @@
 #include "viewchar.h"
 #include "shout.h"
 
-static void _burn_tree(coord_def pos)
-{
-    bolt beam;
-    beam.origin_spell = SPELL_CONJURE_FLAME;
-    beam.range = 1;
-    beam.flavour = BEAM_FIRE;
-    beam.name = "fireball"; // yay doing this by name
-    beam.source = beam.target = pos;
-    beam.set_agent(&you);
-    beam.fire();
-}
-
 spret_type conjure_flame(int pow, const coord_def& where, bool fail)
 {
     // FIXME: This would be better handled by a flag to enforce max range.
@@ -60,22 +48,7 @@ spret_type conjure_flame(int pow, const coord_def& where, bool fail)
 
     if (cell_is_solid(where))
     {
-        switch (grd(where))
-        {
-        case DNGN_METAL_WALL:
-            mpr("You can't ignite solid metal!");
-            break;
-        case DNGN_GREEN_CRYSTAL_WALL:
-            mpr("You can't ignite solid crystal!");
-            break;
-        case DNGN_TREE:
-            fail_check();
-            _burn_tree(where);
-            return SPRET_SUCCESS;
-        default:
-            mpr("You can't ignite solid rock!");
-            break;
-        }
+        mpr("You can't place clouds on a wall.");
         return SPRET_ABORT;
     }
 
