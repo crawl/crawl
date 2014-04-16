dc91ade0243adba03fa5160dc3f0fb8aac84200c
Steve Melenchuk <smelenchuk@gmail.com>
2014-04-15 22:04:40 -0600

    A skeleton ranged_attack class, derived from attack.
    TODO: tie this up to the beam code; move shared code between melee and
    ranged to plain attack; write ranged-specific code.

---
+++ crawl-ref/source/ranged_attack.cc
@@ -0,0 +1,68 @@
+/**
+ * @file
+ * @brief ranged_attack class and associated ranged_attack methods
+ */
+
+#include "AppHdr.h"
+
+#include "ranged_attack.h"
+
+ranged_attack::ranged_attack(actor *attk, actor *defn, item_def *proj) :
+                             attack(attk, defn), projectile(proj)
+{
+}
+
+int ranged_attack::calc_to_hit(bool)
+{
+    return 0;
+}
+
+bool ranged_attack::handle_phase_attempted()
+{
+    return false;
+}
+
+bool ranged_attack::handle_phase_dodged()
+{
+    return false;
+}
+
+bool ranged_attack::handle_phase_blocked()
+{
+    return false;
+}
+
+bool ranged_attack::handle_phase_hit()
+{
+    return false;
+}
+
+bool ranged_attack::handle_phase_damaged()
+{
+    return false;
+}
+
+bool ranged_attack::handle_phase_killed()
+{
+    return false;
+}
+
+bool ranged_attack::handle_phase_end()
+{
+    return false;
+}
+
+bool ranged_attack::attack_shield_blocked(bool verbose)
+{
+    return false;
+}
+
+bool ranged_attack::apply_damage_brand()
+{
+    return false;
+}
+
+bool ranged_attack::check_unrand_effects()
+{
+    return false;
+}
