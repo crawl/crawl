crawl/crawl

commit e4f847d599ab6bdf3f0c52fcf3e7dfef59ca00e8

Rework the "extra" tag for non-minivaults.
Instead of mixing extra vaults in with primary vaults, a primary vault
without the extra tag is placed first, and vaults with ORIENT: tags and
the extra tag are placed last in a manner similar to how abyssal vaults
are handled

Handling of minivaults is unaffected.

This is intended to prevent Depths from placing two huge scary primary
vaults on one level, but it also has handling relevant for upcoming
tweaks to areas of the dungeon currently dependent on tagged vaults.

crawl-ref/source/dungeon.cc
@@ -2659,7 +2659,8 @@ static bool _pan_level()
     if (which_demon >= 0)
     {
         const map_def *vault =
-            random_map_for_tag(pandemon_level_names[which_demon], false);
+            random_map_for_tag(pandemon_level_names[which_demon], false,
+                               false, MB_FALSE);
 
         ASSERT(vault);
 
@@ -2764,10 +2765,10 @@ static const map_def *_dgn_random_map_for_place(bool minivault)
 
     if (!vault)
         // Pick a normal map
-        vault = random_map_for_place(lid, minivault);
+        vault = random_map_for_place(lid, minivault, MB_FALSE);
 
     if (!vault && lid.branch == root_branch && lid.depth == 1)
-        vault = random_map_for_tag("entry");
+        vault = random_map_for_tag("entry", false, false, MB_FALSE);
 
     return vault;
 }
@@ -3144,7 +3145,7 @@ static void _place_chance_vaults()
                 const string fallback_tag =
                     "fallback_" + chance_tag.substr(7); // "chance_"
                 const map_def *fallback =
-                    random_map_for_tag(fallback_tag, true);
+                    random_map_for_tag(fallback_tag, true, false, MB_FALSE);
                 if (fallback)
                 {
                     dprf("Found fallback vault %s for chance tag %s",
@@ -3190,7 +3191,7 @@ static bool _builder_normal()
     }
 
     if (use_random_maps)
-        vault = random_map_in_depth(level_id::current());
+        vault = random_map_in_depth(level_id::current(), false, MB_FALSE);
 
     // We'll accept any kind of primary vault in the main dungeon, but only
     // ORIENT: encompass primary vaults in other branches. Other kinds of vaults
@@ -3562,23 +3563,27 @@ static void _place_branch_entrances(bool use_vaults)
 
 static void _place_extra_vaults()
 {
+    int tries = 0;
     while (true)
     {
         if (!player_in_branch(BRANCH_DUNGEON) && use_random_maps)
         {
-            const map_def *vault = random_map_in_depth(level_id::current());
+            const map_def *vault = random_map_in_depth(level_id::current(),
+                                                       false, MB_TRUE);
 
             // Encompass vaults can't be used as secondaries.
             if (!vault || vault->orient == MAP_ENCOMPASS)
                 break;
 
-            if (vault && _build_secondary_vault(vault))
+            if (_build_secondary_vault(vault))
             {
                 const map_def &map(*vault);
                 if (map.has_tag("extra"))
                     continue;
                 use_random_maps = false;
             }
+            else if (++tries >= 3)
+                break;
         }
         break;
     }

crawl-ref/source/abyss.cc
@@ -216,7 +216,7 @@ static bool _abyss_place_map(const map_def *mdef)
 static bool _abyss_place_vault_tagged(const map_bitmask &abyss_genlevel_mask,
                                       const string &tag)
 {
-    const map_def *map = random_map_for_tag(tag, true, true);
+    const map_def *map = random_map_for_tag(tag, true, true, MB_FALSE);
     if (map)
     {
         unwind_vault_placement_mask vaultmask(&abyss_genlevel_mask);

crawl-ref/source/maps.cc
@@ -747,29 +747,35 @@ public:
         return sel == TAG || place.is_valid();
     }
 
-    static map_selector by_place(const level_id &_place, bool _mini)
+    static map_selector by_place(const level_id &_place, bool _mini,
+                                 maybe_bool _extra)
     {
-        return map_selector(map_selector::PLACE, _place, "", _mini, false);
+        return map_selector(map_selector::PLACE, _place, "", _mini, _extra,
+                            false);
     }
 
-    static map_selector by_depth(const level_id &_place, bool _mini)
+    static map_selector by_depth(const level_id &_place, bool _mini,
+                                 maybe_bool _extra)
     {
-        return map_selector(map_selector::DEPTH, _place, "", _mini, true);
+        return map_selector(map_selector::DEPTH, _place, "", _mini, _extra,
+                            true);
     }
 
-    static map_selector by_depth_chance(const level_id &_place)
+    static map_selector by_depth_chance(const level_id &_place,
+                                        maybe_bool _extra)
     {
         return map_selector(map_selector::DEPTH_AND_CHANCE, _place, "", false,
-                            true);
+                            _extra, true);
     }
 
     static map_selector by_tag(const string &_tag,
                                bool _check_depth,
                                bool _check_chance,
+                               maybe_bool _extra,
                                const level_id &_place = level_id::current())
     {
         map_selector msel = map_selector(map_selector::TAG, _place, _tag,
-                                         false, _check_depth);
+                                         false, _extra, _check_depth);
         msel.ignore_chance = !_check_chance;
         return msel;
     }
@@ -777,10 +783,10 @@ public:
 private:
     map_selector(select_type _typ, const level_id &_pl,
                  const string &_tag,
-                 bool _mini, bool _check_depth)
+                 bool _mini, maybe_bool _extra, bool _check_depth)
         : ignore_chance(false), preserve_dummy(false),
           sel(_typ), place(_pl), tag(_tag),
-          mini(_mini), check_depth(_check_depth),
+          mini(_mini), extra(_extra), check_depth(_check_depth),
           check_layout((sel == DEPTH || sel == DEPTH_AND_CHANCE)
                     && place == level_id::current())
     {
@@ -797,6 +803,7 @@ public:
     const level_id place;
     const string tag;
     const bool mini;
+    const maybe_bool extra;
     const bool check_depth;
     const bool check_layout;
 };
@@ -816,6 +823,13 @@ bool map_selector::depth_selectable(const map_def &mapdef) const
            && (!check_layout || _map_matches_layout_type(mapdef));
 }
 
+static bool _is_extra_compatible(maybe_bool want_extra, bool have_extra)
+{
+    return want_extra == MB_MAYBE
+           || (want_extra == MB_TRUE && have_extra)
+           || (want_extra == MB_FALSE && !have_extra);
+}
+
 bool map_selector::accept(const map_def &mapdef) const
 {
     switch (sel)
@@ -828,6 +842,7 @@ bool map_selector::accept(const map_def &mapdef) const
             return false;
         }
         return mapdef.is_minivault() == mini
+               && _is_extra_compatible(extra, mapdef.has_tag("extra"))
                && mapdef.place.is_usable_in(place)
                && _map_matches_layout_type(mapdef)
                && !mapdef.map_already_used();
@@ -836,6 +851,7 @@ bool map_selector::accept(const map_def &mapdef) const
     {
         const map_chance chance(mapdef.chance(place));
         return mapdef.is_minivault() == mini
+               && _is_extra_compatible(extra, mapdef.has_tag("extra"))
                && (!chance.valid() || chance.dummy_chance())
                && depth_selectable(mapdef)
                && !mapdef.map_already_used();
@@ -848,6 +864,7 @@ bool map_selector::accept(const map_def &mapdef) const
         return chance.valid()
                && !chance.dummy_chance()
                && depth_selectable(mapdef)
+               && _is_extra_compatible(extra, mapdef.has_tag("extra"))
                && !mapdef.map_already_used();
     }
 
@@ -1000,6 +1017,7 @@ static const map_def *_resolve_chance_vault(const map_selector &sel,
         map_selector msel = map_selector::by_tag(chance_tag,
                                                  sel.check_depth,
                                                  false,
+                                                 sel.extra,
                                                  sel.place);
         return _random_map_by_selector(msel);
     }
@@ -1108,30 +1126,35 @@ static const map_def *_random_map_by_selector(const map_selector &sel)
 }
 
 // Returns a map for which PLACE: matches the given place.
-const map_def *random_map_for_place(const level_id &place, bool minivault)
-{
-    return _random_map_by_selector(map_selector::by_place(place, minivault));
-}
-
-const map_def *random_map_in_depth(const level_id &place, bool want_minivault)
+const map_def *random_map_for_place(const level_id &place, bool minivault,
+                                    maybe_bool extra)
 {
     return _random_map_by_selector(
-        map_selector::by_depth(place, want_minivault));
+        map_selector::by_place(place, minivault, extra));
 }
 
-mapref_vector random_chance_maps_in_depth(const level_id &place)
+const map_def *random_map_in_depth(const level_id &place, bool want_minivault,
+                                   maybe_bool extra)
 {
-    map_selector sel = map_selector::by_depth_chance(place);
+    return _random_map_by_selector(
+        map_selector::by_depth(place, want_minivault, extra));
+}
+
+mapref_vector random_chance_maps_in_depth(const level_id &place,
+                                          maybe_bool extra)
+{
+    map_selector sel = map_selector::by_depth_chance(place, extra);
     const vault_indices eligible = _eligible_maps_for_selector(sel);
     return _random_chance_maps_in_list(sel, eligible);
 }
 
 const map_def *random_map_for_tag(const string &tag,
                                   bool check_depth,
-                                  bool check_chance)
+                                  bool check_chance,
+                                  maybe_bool extra)
 {
     return _random_map_by_selector(
-        map_selector::by_tag(tag, check_depth, check_chance));
+        map_selector::by_tag(tag, check_depth, check_chance, extra));
 }
 
 int map_count()
@@ -1534,7 +1557,7 @@ static weighted_map_names mg_find_random_vaults(
 
     map_count_t map_counts;
 
-    map_selector sel = map_selector::by_depth(place, wantmini);
+    map_selector sel = map_selector::by_depth(place, wantmini, MB_MAYBE);
     sel.preserve_dummy = true;
 
     no_messages mx;

crawl-ref/source/maps.h
@@ -32,13 +32,18 @@ int map_count();
 string vault_chance_tag(const map_def &map);
 
 const map_def *find_map_by_name(const string &name);
-const map_def *random_map_for_place(const level_id &place, bool minivault);
+const map_def *random_map_for_place(const level_id &place,
+                                    bool minivault,
+                                    maybe_bool extra = MB_MAYBE);
 const map_def *random_map_in_depth(const level_id &lid,
-                                   bool want_minivault = false);
+                                   bool want_minivault = false,
+                                   maybe_bool extra = MB_MAYBE);
 const map_def *random_map_for_tag(const string &tag,
                                   bool check_depth = false,
-                                  bool check_chance = false);
-mapref_vector random_chance_maps_in_depth(const level_id &place);
+                                  bool check_chance = false,
+                                  maybe_bool extra = MB_MAYBE);
+mapref_vector random_chance_maps_in_depth(const level_id &place,
+                                          maybe_bool extra = MB_MAYBE);
 
 void dump_map(const map_def &map);
 void add_parsed_map(const map_def &md);

commit 6b0fb6cbc01d98d19167e025c01d14d0cad27afd

Allow everywhere to place non-extra full vaults again.
Broken by e4f847d.

crawl-ref/source/dungeon.cc
@@ -3202,12 +3202,7 @@ static bool _builder_normal()
     if (use_random_maps)
         vault = random_map_in_depth(level_id::current(), false, MB_FALSE);
 
-    // We'll accept any kind of primary vault in the main dungeon, but only
-    // ORIENT: encompass primary vaults in other branches. Other kinds of vaults
-    // can still be placed in other branches as secondary vaults.
-    if (vault && (player_in_branch(BRANCH_DUNGEON)
-                  || player_in_branch(BRANCH_DEPTHS)
-                  || vault->orient == MAP_ENCOMPASS))
+    if (vault)
     {
         env.level_build_method += " random_map_in_depth";
         _ensure_vault_placed_ex(_build_primary_vault(vault), vault);