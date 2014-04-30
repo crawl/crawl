699a05116c1e3d6e820c248e16687d7aca8535fb
Chris Campbell <chriscampbell89@gmail.com>
2014-04-30 18:57:09 +0100

    Make cleave hit 8 squares instead of 7
    Hitting 7 squares results in occasional awkwardness where attacking
    empty space is the best way to target enemies, and the difference between
    7 and 8 isn't a significant factor in cleaving's balance.

--- crawl-ref/source/fight.h
+++ crawl-ref/source/fight.h
@@ -44,7 +44,7 @@ stab_type find_stab_type(const actor *attacker,
 
 void chaos_affect_actor(actor *victim);
 void get_cleave_targets(const actor* attacker, const coord_def& def, int dir,
-                        list<actor*> &targets);
+                        list<actor*> &targets, bool behind);
 void get_all_cleave_targets(const actor* attacker, const coord_def& def,
                             list<actor*> &targets);
 void attack_cleave_targets(actor* attacker, list<actor*> &targets,
