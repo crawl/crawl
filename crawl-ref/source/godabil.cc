92a68ea5832cb32b379a0b631bcff9bf4f71cebc
Steve Melenchuk <smelenchuk@gmail.com>
2014-05-07 09:02:58 -0600

    Warn about Upheavaling yourself/allies (MarvinPA).
    Also don't print MSG_OK more than once on various aborts for it.

--- crawl-ref/source/godabil.cc
+++ crawl-ref/source/godabil.cc
@@ -4611,6 +4611,18 @@ bool qazlal_upheaval(coord_def target, bool quiet)
         {
             return false;
         }
+        bolt tempbeam;
+        tempbeam.source    = beam.target;
+        tempbeam.target    = beam.target;
+        tempbeam.flavour   = BEAM_MISSILE;
+        tempbeam.ex_size   = max_radius;
+        tempbeam.hit       = AUTOMATIC_HIT;
+        tempbeam.damage    = dice_def(AUTOMATIC_HIT, 1);
+        tempbeam.thrower   = KILL_YOU;
+        tempbeam.is_tracer = true;
+        tempbeam.explode(false);
+        if (tempbeam.beam_cancelled)
+            return false;
     }
     else
         beam.target = target;
