b0bf75c565f3ed9c26439434f212164334cbde0d
Chris Campbell <chriscampbell89@gmail.com>
2014-05-08 23:50:27 +0100

    Don't distract non-hostile monsters with gold under Gozag

--- crawl-ref/source/mon-act.cc
+++ crawl-ref/source/mon-act.cc
@@ -2066,7 +2066,7 @@ void handle_monster_move(monster* mons)
     }
 
     const int gold = gozag_gold_in_los(mons);
-    if (gold > 0)
+    if (!mons->wont_attack() && gold > 0)
     {
         for (int i = 0; i < gold; i++)
             if (one_chance_in(20))
