319a1603b20bb525067320b5c4b33015598e323c
Steve Melenchuk <smelenchuk@gmail.com>
2014-05-23 13:43:07 -0600

    Really fix Gozag gold detection (#8584).
    It was moving the wrong item to the top of the cell - I wanted
    item_def::index(), not item_def::link.
    
    Also, if the gold was the second item on the cell, it would get missed, and
    if more than one identical item was being generated on the same tile before
    and after the gold, it could also get missed.

--- crawl-ref/source/dactions.cc
+++ crawl-ref/source/dactions.cc
@@ -326,7 +326,7 @@ static void _apply_daction(daction_type act)
                 if (j->base_type == OBJ_GOLD)
                 {
                     bool detected = false;
-                    int dummy = j->link;
+                    int dummy = j->index();
                     j->special = 0;
                     unlink_item(dummy);
                     move_item_to_grid(&dummy, *ri, true);
