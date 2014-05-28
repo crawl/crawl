9b3a599bbdb70f05479d1d8d886caaf4e68d7aec
Shayne Halvorson <N78291@gmail.com>
2014-05-28 14:57:55 -0500

    Allow a weapon swap when drawing the Illusion.
    Now the player illusion won't be deck bashing if the deck was wielded
    and evoked.

--- crawl-ref/source/decks.cc
+++ crawl-ref/source/decks.cc
@@ -2015,6 +2015,18 @@ static void _helm_card(int power, deck_rarity_type rarity)
     }
 }
 
+static void _do_weapon_swap()
+{
+    // Pause before jumping to the list.
+    more();
+
+    // Don't take less time if we're swapping weapons.
+    int old_time = you.time_taken;
+    wield_weapon(false);
+    you.time_taken = old_time;
+}
+
+
 static void _blade_card(int power, deck_rarity_type rarity)
 {
     if (you.species == SP_FELID)
@@ -2023,13 +2035,7 @@ static void _blade_card(int power, deck_rarity_type rarity)
         return;
     }
 
-    // Pause before jumping to the list.
-    more();
-
-    // Don't take less time if we're swapping weapons.
-    int old_time = you.time_taken;
-    wield_weapon(false);
-    you.time_taken = old_time;
+    _do_weapon_swap();
 
     const int power_level = _get_power_level(power, rarity);
     brand_type brand;
@@ -2804,6 +2810,8 @@ static void _illusion_card(int power, deck_rarity_type rarity)
 
     if (!mon || monster_at(you.pos()))
         return;
+
+    _do_weapon_swap();
 
     mon->type = MONS_PLAYER;
     mon->behaviour = BEH_SEEK;
