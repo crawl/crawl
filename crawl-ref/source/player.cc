crawl/crawl

commit 614cdcf0426223561bf7a58ebb999e47c366fe82

As per FR 2795134, nerf damage reduction of elemental resistances. Damage is now reduced to 50% (level 1, unchanged), 33% (level 2, was 20%), and 20% (level 3, was 10%), respectively.
This only applies to players, monster resists are unchanged. The purpose of 
this change is to make the mid and late game harder without unduly affecting 
the early game.


git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@10554 c06c8d41-db1a-0410-9941-cceddc491573

crawl-ref/source/player.cc
@@ -5527,12 +5527,12 @@ void dec_napalm_player()
         {
             ouch(((random2avg(9, 2) + 1) * you.time_taken) / 10,
                  NON_MONSTER, KILLED_BY_BURNING);
-        }
 
-        if (res_fire < 0)
-        {
-            ouch(((random2avg(9, 2) + 1) * you.time_taken) / 10,
-                 NON_MONSTER, KILLED_BY_BURNING);
+            if (res_fire < 0)
+            {
+                ouch(((random2avg(9, 2) + 1) * you.time_taken) / 10,
+                     NON_MONSTER, KILLED_BY_BURNING);
+            }
         }
 
         if (you.duration[DUR_CONDENSATION_SHIELD] > 0)

crawl-ref/source/cloud.cc
@@ -237,7 +237,7 @@ int steam_cloud_damage(const cloud_struct &cloud)
 {
     return steam_cloud_damage(cloud.decay);
 }
- 
+
 int steam_cloud_damage(int decay)
 {
     decay = std::min(decay, 60);
@@ -433,6 +433,17 @@ beam_type cloud2beam(cloud_type flavour)
     }
 }
 
+// Returns by how much damage gets divided due to elemental resistances.
+// Damage is reduced to, level 1 -> 1/2, level 2 -> 1/3, level 3 -> 1/5, or
+// for "boolean" attacks (which use bonus_res = 1, sticky flame/electricity)
+// to level 1 -> 1/3, level 2 -> 1/4, or level 3 -> 1/6.
+// With the old formula (1 + resist * resist) this used to be
+// 1/2, 1/5, 1/10 (normal) and 1/3, 1/6, 1/11 (boolean), respectively.
+int resist_fraction(int resist, int bonus_res)
+{
+    return ((3*resist + 1)/2 + bonus_res);
+}
+
 // NOTE: Keep in sync with in_a_cloud()
 int max_cloud_damage(cloud_type cl_type, int power)
 {
@@ -462,7 +473,7 @@ int max_cloud_damage(cloud_type cl_type, int power)
         else
         {
             dam += 32 * speed / 10;
-            dam /= (1 + resist * resist);
+            dam /= resist_fraction(resist);
         }
         break;
 
@@ -554,7 +565,7 @@ void in_a_cloud()
         {
             canned_msg(MSG_YOU_RESIST);
             hurted += ((random2avg(23, 3) + 10) * you.time_taken) / 10;
-            hurted /= (1 + resist * resist);
+            hurted /= resist_fraction(resist);
             ouch(hurted, cl, KILLED_BY_CLOUD, "flame");
         }
         expose_player_to_element(BEAM_FIRE, 7);
@@ -602,7 +613,7 @@ void in_a_cloud()
         {
             canned_msg(MSG_YOU_RESIST);
             hurted += ((random2avg(23, 3) + 10) * you.time_taken) / 10;
-            hurted /= (1 + resist * resist);
+            hurted /= resist_fraction(resist);
             ouch(hurted, cl, KILLED_BY_CLOUD, "freezing vapour");
         }
         expose_player_to_element(BEAM_COLD, 7);

crawl-ref/source/cloud.h
@@ -60,6 +60,7 @@ int steam_cloud_damage(int decay);
 cloud_type beam2cloud(beam_type flavour);
 beam_type cloud2beam(cloud_type flavour);
 
+int resist_fraction(int resist, int bonus_res = 0);
 int  max_cloud_damage(cloud_type cl_type, int power = -1);
 void in_a_cloud(void);
 

crawl-ref/source/fight.cc
@@ -2045,10 +2045,6 @@ int resist_adjust_damage(actor *defender, beam_type flavour,
 
     const bool monster = (defender->atype() == ACT_MONSTER);
 
-    // Check if this is a resist that pretends to be boolean for damage
-    // purposes.  Only electricity and sticky flame (napalm) do this at
-    // the moment; raw poison damage uses the normal formula.
-    int res_base = (is_boolean_resist(flavour) ? 2 : 1);
     const int resistible_fraction = get_resistible_fraction(flavour);
 
     int resistible = rawdamage * resistible_fraction / 100;
@@ -2059,7 +2055,19 @@ int resist_adjust_damage(actor *defender, beam_type flavour,
         if (monster && res >= 3)
             resistible = 0;
         else
-            resistible /= res_base + res * res;
+        {
+            // Check if this is a resist that pretends to be boolean for damage
+            // purposes.  Only electricity and sticky flame (napalm) do this at
+            // the moment; raw poison damage uses the normal formula.
+            const int bonus_res = (is_boolean_resist(flavour) ? 1 : 0);
+
+            // Use a new formula for players, but keep the old, more effective
+            // for monsters.
+            if (monster)
+                resistible /= 1 + bonus_res + res * res;
+            else
+                resistible /= resist_fraction(res, bonus_res);
+        }
     }
     else if (res < 0)
         resistible = resistible * (ranged? 15 : 20) / 10;
@@ -3033,9 +3041,7 @@ bool melee_attack::apply_damage_brand()
         break;
 
     case SPWPN_ELECTROCUTION:
-        if (defender->airborne())
-            break;
-        else if (defender->res_elec() > 0)
+        if (defender->airborne() || defender->res_elec() > 0)
             break;
         else if (one_chance_in(3))
         {

commit 6221ae0b9b0643c0e8dc38211c9cb1d3cab1e63e

Fix sticky flame using the old formulae for resistance
Broken since 614cdcf042622, oops.

crawl-ref/source/player.cc
@@ -5198,31 +5198,14 @@ void dec_napalm_player(int delay)
     expose_player_to_element(BEAM_NAPALM,
                              div_rand_round(delay * 4, BASELINE_DELAY));
 
-    const int res_fire = player_res_fire();
-
-    if (res_fire > 0)
-    {
-        ouch((((random2avg(9, 2) + 1) * delay) /
-                (1 + (res_fire * res_fire))) / BASELINE_DELAY, NON_MONSTER,
-                KILLED_BY_BURNING);
-    }
-
-    if (res_fire <= 0)
-    {
-        ouch(((random2avg(9, 2) + 1) * delay) / BASELINE_DELAY,
-             NON_MONSTER, KILLED_BY_BURNING);
-
-        if (res_fire < 0)
-        {
-            ouch(((random2avg(9, 2) + 1) * delay)
-                    / BASELINE_DELAY, NON_MONSTER, KILLED_BY_BURNING);
-        }
-    }
+    const int hurted = resist_adjust_damage(&you, BEAM_FIRE, player_res_fire(),
+                                            random2avg(9, 2) + 1);
 
     if (you.duration[DUR_CONDENSATION_SHIELD] > 0)
         remove_condensation_shield();
     if (you.duration[DUR_ICY_ARMOUR] > 0)
         remove_ice_armour();
+    ouch(hurted * delay / BASELINE_DELAY, NON_MONSTER, KILLED_BY_BURNING);
 
     you.duration[DUR_LIQUID_FLAMES] -= delay;
     if (you.duration[DUR_LIQUID_FLAMES] <= 0)