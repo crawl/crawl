fd0dc405fee0f2d307e8b134f5d0acbc8ccff674
Nicholas Feinberg <pleasingfung@gmail.com>
2014-05-29 20:42:41 -0700

    Double the SH bonus from shield enchantments
    Shield enchantments have been irrelevant ever since
    SH was rescaled. This was particularly annoying because
    enchant armour scrolls could still be used on them, even
    though you'd never want to - a 'newbie trap'. This should
    go some way toward fixing that.
    
    (You could halve all other sources of SH and re-do the
    calculations, but then you'd have the same problem as before,
    that SH isn't on the same scale as AC and EV...)

--- crawl-ref/source/monster.cc
+++ crawl-ref/source/monster.cc
@@ -3334,7 +3334,7 @@ int monster::shield_bonus() const
         if (incapacitated())
             return 0;
 
-        int shld_c = property(*shld, PARM_AC) + shld->plus;
+        int shld_c = property(*shld, PARM_AC) + shld->plus * 2;
         shld_c = shld_c * 2 + (body_size(PSIZE_TORSO) - SIZE_MEDIUM)
                             * (shld->sub_type - ARM_LARGE_SHIELD);
         return random2avg(shld_c + hit_dice * 4 / 3, 2) / 2;
