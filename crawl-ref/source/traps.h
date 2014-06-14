64ebc5910fabcb5ad9e9b1813df24c2ff9cb71c6
Nicholas Feinberg <pleasingfung@gmail.com>
2014-06-13 20:40:08 -0700

    Tidy up some web/net behaviour
    Monsters should now try to escape from Arachne's
    ensnare immediately (on whatever incredibly rare
    occasions that is relevant), and shouldn't try to
    break out of net traps on the turn they get netted.
    
    This technically breaks save compat with the old
    newly_webbed prop, but in a harmless way.

--- crawl-ref/source/traps.h
+++ crawl-ref/source/traps.h
@@ -9,7 +9,7 @@
 #include "enum.h"
 #include "externs.h"
 
-#define NEW_WEB_KEY "freshly_webbed"
+#define NEWLY_TRAPPED_KEY "newly_trapped"
 
 struct bolt;
 class monster;
@@ -21,7 +21,7 @@ void free_stationary_net(int item_index);
 
 void handle_traps(trap_type trt, int i, bool trap_known);
 int get_trapping_net(const coord_def& where, bool trapped = true);
-void monster_caught_in_net(monster* mon, actor *agent);
+bool monster_caught_in_net(monster* mon, actor *agent);
 bool player_caught_in_net();
 void clear_trapping_net();
 void check_net_will_hold_monster(monster* mon);
