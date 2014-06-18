af43692c91457dbb695a156d43a1298aefc8a111
Shmuale Mark <shm.mark@gmail.com>
2014-06-18 10:59:50 -0400

    Add a failure check in Necronomicon gifting (floatingatoll, #8417).

--- crawl-ref/source/godprayer.cc
+++ crawl-ref/source/godprayer.cc
@@ -258,10 +258,8 @@ static bool _altar_prayer()
         int thing_created = items(1, OBJ_BOOKS, BOOK_NECRONOMICON, true, 1,
                                   0, 0, 0, you.religion);
 
-        if (thing_created == NON_ITEM)
+        if (thing_created == NON_ITEM || !move_item_to_grid(&thing_created, you.pos()))
             return false;
-
-        move_item_to_grid(&thing_created, you.pos());
 
         simple_god_message(" grants you a gift!");
         more();
