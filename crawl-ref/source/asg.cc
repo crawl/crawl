c38c796cc5873a39d69dd465db6c8362ebfde04e
Neil Moore <neil@s-z.org>
2014-06-27 01:04:31 -0400

    Make a sign conversion explicit.

--- crawl-ref/source/asg.cc
+++ crawl-ref/source/asg.cc
@@ -61,7 +61,7 @@ AsgKISS::AsgKISS(uint32_t init_key[], int key_length)
 uint32_t get_uint32(int generator)
 {
     ASSERT(generator >= 0);
-    ASSERT(generator < ARRAYSZ(asg_rng));
+    ASSERT((size_t) generator < ARRAYSZ(asg_rng));
     return asg_rng[generator].get_uint32();
 }
 
