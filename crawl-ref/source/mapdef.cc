779e12131079b2b96f63ee741e55198ed89c5561
Vsevolod Kozlov <zaba@wybt.net>
2014-06-28 21:41:40 +0400

    Rename the 'can_overwrite' vault tag to 'overwrite_floor_cell'.
    In an attempt to clarify its new, more restricted behaviour.

--- crawl-ref/source/mapdef.cc
+++ crawl-ref/source/mapdef.cc
@@ -2845,9 +2845,9 @@ string map_def::validate_map_def(const depth_ranges &default_depths)
             return err;
     }
 
-    if (has_tag("can_overwrite") && (map.width() != 1 || map.height() != 1))
+    if (has_tag("overwrite_floor_cell") && (map.width() != 1 || map.height() != 1))
     {
-        return "Map tagged 'can_overwrite' must be 1x1";
+        return "Map tagged 'overwrite_floor_cell' must be 1x1";
     }
 
     // Abyssal vaults have additional size and orientation restrictions.
