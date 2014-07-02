9c21a484839b7d2bff1a94a51ad0b1c4fbed67be
Shmuale Mark <shm.mark@gmail.com>
2014-07-02 11:18:55 -0400

    Disallow eating off the ground for non-vampires.
    It makes the 'e' key less consistent for no obvious benefit. Not to
    mention that it's incredibly unsanitary.

--- crawl-ref/source/food.cc
+++ crawl-ref/source/food.cc
@@ -540,6 +540,10 @@ bool butchery(int which_corpse, bool bottle_blood)
 
 bool prompt_eat_inventory_item(int slot)
 {
+    // There's nothing in inventory that a vampire can 'e'.
+    if (you.species == SP_VAMPIRE)
+        return false;
+
     if (inv_count() < 1)
     {
         canned_msg(MSG_NOTHING_CARRIED);
@@ -551,11 +555,8 @@ bool prompt_eat_inventory_item(int slot)
     if (slot == -1)
     {
         which_inventory_slot =
-                prompt_invent_item(you.species == SP_VAMPIRE ? "Drain what?"
-                                                             : "Eat which item?",
-                                   MT_INVLIST,
-                                   you.species == SP_VAMPIRE ? (int)OSEL_VAMP_EAT
-                                                             : OBJ_FOOD,
+                prompt_invent_item("Eat which item?",
+                                   MT_INVLIST, OBJ_FOOD,
                                    true, true, true, 0, -1, NULL,
                                    OPER_EAT);
 
@@ -566,21 +567,10 @@ bool prompt_eat_inventory_item(int slot)
     // This conditional can later be merged into food::can_ingest() when
     // expanded to handle more than just OBJ_FOOD 16mar200 {dlb}
     item_def &item(you.inv[which_inventory_slot]);
-    if (you.species != SP_VAMPIRE)
+    if (item.base_type != OBJ_FOOD)
     {
-        if (item.base_type != OBJ_FOOD)
-        {
-            mpr("You can't eat that!");
-            return false;
-        }
-    }
-    else
-    {
-        if (item.base_type != OBJ_CORPSES || item.sub_type != CORPSE_BODY)
-        {
-            mpr("You crave blood!");
-            return false;
-        }
+        mpr("You can't eat that!");
+        return false;
     }
 
     if (!can_ingest(item, false))
@@ -632,21 +622,15 @@ bool eat_food(int slot)
         int result = prompt_eat_chunks();
         if (result == 1 || result == -1)
             return result > 0;
-
-        if (result != -2) // else skip ahead to inventory
-        {
-            if (you.visible_igrd(you.pos()) != NON_ITEM)
-            {
-                result = eat_from_floor(true);
-                if (result == 1)
-                    return true;
-                if (result == -1)
-                    return false;
-            }
-        }
     }
 
-    return prompt_eat_inventory_item(slot);
+    if (you.species != SP_VAMPIRE)
+        return prompt_eat_inventory_item(slot);
+    else
+    {
+        mpr("There's nothing here to drain!");
+        return false;
+    }
 }
 
 //     END PUBLIC FUNCTIONS
@@ -906,173 +890,6 @@ static string _floor_eat_menu_title(const Menu *menu, const string &oldt)
     return oldt;
 }
 #endif
-// Returns -1 for cancel, 1 for eaten, 0 for not eaten.
-int eat_from_floor(bool skip_chunks)
-{
-    if (!_eat_check())
-        return false;
-
-    // Corpses should have been handled before.
-    if (you.species == SP_VAMPIRE && skip_chunks)
-        return 0;
-
-    bool need_more = false;
-    int unusable_corpse = 0;
-    int inedible_food = 0;
-    item_def wonteat;
-    bool found_valid = false;
-
-    vector<item_def*> food_items;
-    for (stack_iterator si(you.pos(), true); si; ++si)
-    {
-        if (you.species == SP_VAMPIRE)
-        {
-            if (si->base_type != OBJ_CORPSES || si->sub_type != CORPSE_BODY)
-                continue;
-
-            if (!mons_has_blood(si->mon_type))
-            {
-                unusable_corpse++;
-                continue;
-            }
-        }
-        else
-        {
-            if (si->base_type != OBJ_FOOD)
-                continue;
-
-            // Chunks should have been handled before.
-            if (skip_chunks && si->sub_type == FOOD_CHUNK)
-                continue;
-
-            if (is_bad_food(*si))
-                continue;
-        }
-
-        if (!skip_chunks && food_is_rotten(*si)
-            && !_player_can_eat_rotten_meat())
-        {
-            unusable_corpse++;
-            continue;
-        }
-        else if (!can_ingest(*si, true))
-        {
-            if (!inedible_food)
-            {
-                wonteat = *si;
-                inedible_food++;
-            }
-            else
-            {
-                // Increase only if we're dealing with different subtypes.
-                // FIXME: Use a common check for herbivorous/carnivorous
-                //        dislikes, for e.g. "Blech! You need blood!"
-                ASSERT(wonteat.defined());
-                if (wonteat.sub_type != si->sub_type)
-                    inedible_food++;
-            }
-
-            continue;
-        }
-
-        found_valid = true;
-        food_items.push_back(&(*si));
-    }
-
-    if (found_valid)
-    {
-#ifdef TOUCH_UI
-        vector<SelItem> selected =
-            select_items(food_items,
-                         you.species == SP_VAMPIRE ? "Drink blood from" : "Eat",
-                         false, MT_SELONE, _floor_eat_menu_title);
-        redraw_screen();
-        for (int i = 0, count = selected.size(); i < count; ++i)
-        {
-            const item_def *item = selected[i].item;
-            if (!check_warning_inscriptions(*item, OPER_EAT))
-                break;
-
-            if (can_ingest(*item, false))
-                return eat_item(*item);
-        }
-#else
-        sort(food_items.begin(), food_items.end(), compare_by_freshness());
-        for (unsigned int i = 0; i < food_items.size(); ++i)
-        {
-            item_def *item = food_items[i];
-            string item_name = get_menu_colour_prefix_tags(*item, DESC_A);
-
-            mprf(MSGCH_PROMPT, "%s %s%s? (ye/n/q/i?)",
-                 (you.species == SP_VAMPIRE ? "Drink blood from" : "Eat"),
-                 ((item->quantity > 1) ? "one of " : ""),
-                 item_name.c_str());
-
-            int keyin = toalower(getchm(KMC_CONFIRM));
-            switch (keyin)
-            {
-            case 'q':
-            CASE_ESCAPE
-                canned_msg(MSG_OK);
-                return -1;
-            case 'e':
-            case 'y':
-                if (!check_warning_inscriptions(*item, OPER_EAT))
-                    break;
-
-                if (can_ingest(*item, false))
-                    return eat_item(*item);
-                need_more = true;
-                break;
-            case 'i':
-            case '?':
-                // Directly skip ahead to inventory.
-                return 0;
-            default:
-                // Else no: try next one.
-                break;
-            }
-        }
-#endif
-    }
-    else
-    {
-        // Give a message about why these food items can not actually be eaten.
-        if (unusable_corpse)
-        {
-            if (you.species == SP_VAMPIRE)
-            {
-                mprf("%s devoid of blood.",
-                     unusable_corpse == 1 ? "This corpse is"
-                                          : "These corpses are");
-            }
-            else
-                _player_can_eat_rotten_meat(true);
-            need_more = true;
-        }
-        else if (inedible_food)
-        {
-            if (inedible_food == 1)
-            {
-                ASSERT(wonteat.defined());
-                // Use the normal cannot ingest message.
-                if (can_ingest(wonteat, false))
-                {
-                    mprf(MSGCH_DIAGNOSTICS, "Error: Can eat %s after all?",
-                         wonteat.name(DESC_PLAIN).c_str());
-                }
-            }
-            else // Several different food items.
-                mpr("You refuse to eat these food items.");
-            need_more = true;
-        }
-    }
-
-    if (need_more)
-        more();
-
-    return 0;
-}
 
 bool eat_from_inventory()
 {
