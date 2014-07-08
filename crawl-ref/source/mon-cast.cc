bc841a2a9115e308279fae3ce0c871fd6d054942
Neil Moore <neil@s-z.org>
2014-07-08 17:04:18 -0400

    Allow toggling monster speech diagnostics in &^Q
    This is currently predicated on DEBUG_MONSPEAK, since all the relevant
    messages are.

--- crawl-ref/source/mon-cast.cc
+++ crawl-ref/source/mon-cast.cc
@@ -6155,8 +6155,8 @@ static string _noise_message(const vector<string>& key_list,
         const string key = key_list[i];
 
 #ifdef DEBUG_MONSPEAK
-        mprf(MSGCH_DIAGNOSTICS, "monster casting lookup: %s%s", prefix.c_str(),
-                                                                key.c_str());
+        dprf(DIAG_SPEECH, "monster casting lookup: %s%s",
+             prefix.c_str(), key.c_str());
 #endif
 
         msg = getSpeakString(prefix + key);
