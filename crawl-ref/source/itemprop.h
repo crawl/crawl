603073ef4d6b98ef9e123882a8b326395ddbffe5
Nicholas Feinberg <pleasingfung@gmail.com>
2014-07-10 22:55:13 -0700

    Display less misleading sling base damage

--- crawl-ref/source/itemprop.h
+++ crawl-ref/source/itemprop.h
@@ -128,6 +128,7 @@ launch_retval is_launched(const actor *actor, const item_def *launcher,
 bool ammo_always_destroyed(const item_def &missile) PURE;
 bool ammo_never_destroyed(const item_def &missile) PURE;
 int  ammo_type_destroy_chance(int missile_type) PURE;
+int  ammo_type_damage(int missile_type) PURE;
 
 
 reach_type weapon_reach(const item_def &item) PURE;
