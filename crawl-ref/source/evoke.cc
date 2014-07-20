eb426df56664aee66ec2f04987c1e71c6c22c975
Corin Buchanan-Howland <corin@buchananhowland.net>
2014-07-20 09:32:50 -0400

    Ru: Fix summoning evokers while under Sac Love
    This makes all elemental evokers summon hostile elementals and fixes a
    crash when attempting to evoke box of beasts when all possible beasts
    selected hate you.

--- crawl-ref/source/evoke.cc
+++ crawl-ref/source/evoke.cc
@@ -689,15 +689,25 @@ static bool _box_of_beasts(item_def &box)
         monster_type mon3 = pick_monster_from(pop_beasts, pick_level_3,
                                               _box_of_beasts_veto_mon);
 
-        mgen_data mg = mgen_data(MONS_CHIMERA,
-                                 BEH_FRIENDLY, &you,
-                                 3 + random2(3), 0,
-                                 you.pos(),
-                                 MHITYOU, MG_AUTOFOE);
-        mg.define_chimera(mon, mon2, mon3);
-        mons = create_monster(mg);
-        if (mons)
-            success = true;
+        if (mon && mon2 && mon3)
+        {
+            mgen_data mg = mgen_data(MONS_CHIMERA,
+                                     BEH_FRIENDLY, &you,
+                                     3 + random2(3), 0,
+                                     you.pos(),
+                                     MHITYOU, MG_AUTOFOE);
+            mg.define_chimera(mon, mon2, mon3);
+            mons = create_monster(mg);
+            if (mons)
+                success = true;
+        }
+        else
+        {
+            // we weren't able to come up with acceptable monsters
+            mpr("...but nothing happens.");
+            return false;
+        }
+
     }
 
     if (success)
@@ -1075,9 +1085,12 @@ static bool _lamp_of_fire()
             beams[n].fire();
         }
 
+        beh_type attitude = BEH_FRIENDLY;
+        if (player_will_anger_monster(MONS_FIRE_ELEMENTAL))
+            attitude = BEH_HOSTILE;
         for (unsigned int n = 0; n < elementals.size(); ++n)
         {
-            mgen_data mg(MONS_FIRE_ELEMENTAL, BEH_FRIENDLY, &you, 3,
+            mgen_data mg(MONS_FIRE_ELEMENTAL, attitude, &you, 3,
                          SPELL_NO_SPELL, elementals[n], 0,
                          MG_FORCE_BEH | MG_FORCE_PLACE, GOD_NO_GOD,
                          MONS_FIRE_ELEMENTAL, 0, BLACK, PROX_CLOSE_TO_PLAYER);
@@ -1330,9 +1343,12 @@ static void _fan_of_gales_elementals()
     int num_elementals = _num_evoker_elementals();
 
     bool created = false;
+    beh_type attitude = BEH_FRIENDLY;
+    if (player_will_anger_monster(MONS_AIR_ELEMENTAL))
+        attitude = BEH_HOSTILE;
     for (int n = 0; n < min(num_elementals, (int)elementals.size()); ++n)
     {
-        mgen_data mg (MONS_AIR_ELEMENTAL, BEH_FRIENDLY, &you, 3, SPELL_NO_SPELL,
+        mgen_data mg (MONS_AIR_ELEMENTAL, attitude, &you, 3, SPELL_NO_SPELL,
                       elementals[n], 0, MG_FORCE_BEH | MG_FORCE_PLACE,
                       GOD_NO_GOD, MONS_AIR_ELEMENTAL, 0, BLACK,
                       PROX_CLOSE_TO_PLAYER);
@@ -1453,13 +1469,17 @@ static bool _stone_of_tremors()
 
     // Create elementals.
     bool created = false;
+    beh_type attitude = BEH_FRIENDLY;
+    if (player_will_anger_monster(MONS_EARTH_ELEMENTAL))
+        attitude = BEH_HOSTILE;
+
     for (int n = 0; n < min(num_elementals, (int)rubble_pos.size()); ++n)
     {
         // Skip occupied positions
         if (actor_at(rubble_pos[n]))
             continue;
 
-        mgen_data mg(MONS_EARTH_ELEMENTAL, BEH_FRIENDLY, &you, 3, SPELL_NO_SPELL,
+        mgen_data mg(MONS_EARTH_ELEMENTAL, attitude, &you, 3, SPELL_NO_SPELL,
                      rubble_pos[n], 0, MG_FORCE_BEH | MG_FORCE_PLACE, GOD_NO_GOD,
                      MONS_EARTH_ELEMENTAL, 0, BLACK, PROX_CLOSE_TO_PLAYER);
         mg.hd = 6 + you.skill_rdiv(SK_EVOCATIONS, 2, 13);
@@ -1523,9 +1543,12 @@ static bool _phial_of_floods()
         bool created = false;
         num = min(num_elementals,
                   min((int)elementals.size(), (int)elementals.size() / 5 + 1));
+        beh_type attitude = BEH_FRIENDLY;
+        if (player_will_anger_monster(MONS_WATER_ELEMENTAL))
+            attitude = BEH_HOSTILE;
         for (int n = 0; n < num; ++n)
         {
-            mgen_data mg (MONS_WATER_ELEMENTAL, BEH_FRIENDLY, &you, 3,
+            mgen_data mg (MONS_WATER_ELEMENTAL, attitude, &you, 3,
                           SPELL_NO_SPELL, elementals[n], 0,
                           MG_FORCE_BEH | MG_FORCE_PLACE, GOD_NO_GOD,
                           MONS_WATER_ELEMENTAL, 0, BLACK, PROX_CLOSE_TO_PLAYER);
