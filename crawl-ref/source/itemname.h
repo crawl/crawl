b6cf2bc58d6b713433ea508f86a05975d3333713
Nicholas Feinberg <pleasingfung@gmail.com>
2014-07-22 22:53:48 -0700

    Refactor out a redundant list of jewellery names
    And de-indent l_item_do_subtype() slightly.

--- crawl-ref/source/itemname.h
+++ crawl-ref/source/itemname.h
@@ -145,4 +145,5 @@ string sub_type_string(const item_def &item, bool known = true);
 string ego_type_string(const item_def &item, bool terse = false, int override_brand = 0);
 
 const char* potion_type_name(int potiontype);  //used in xom.cc
+const char* jewellery_effect_name(int jeweltype) PURE; //used in l_item.cc
 #endif
