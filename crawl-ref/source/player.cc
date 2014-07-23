49774bb0813ad14a798730456e8e65d50b0fbacc
Nicholas Feinberg <pleasingfung@gmail.com>
2014-07-22 18:33:43 -0700

    Don't trigger zot traps when losing flight (5090, wheals)

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -503,8 +503,10 @@ void moveto_location_effects(dungeon_feature_type old_feat,
         }
     }
 
-    // Traps go off.
-    if (trap_def* ptrap = find_trap(you.pos()))
+    // Traps go off. (But magical traps don't go off again when losing flight
+    // - i.e., moving into the same tile)
+    trap_def* ptrap = find_trap(you.pos());
+    if (ptrap && (old_pos != you.pos() || ptrap->ground_only()))
         ptrap->trigger(you, !stepped); // blinking makes it hard to evade
 
     if (stepped)
