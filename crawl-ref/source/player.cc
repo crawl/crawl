f11e77305c2c7c242be67821183def72ec7b1b90
Nicholas Feinberg <pleasingfung@gmail.com>
2014-07-24 21:24:30 -0700

    Tweak Stoneskin/Ozocubu's Armour
    Reduce the spellpower cap to 100 (closely approximating the old
    effective power cap), and slightly buff ozo's at old power to
    bring it up to the old numbers.

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -6489,7 +6489,7 @@ int player::armour_class() const
     AC += scan_artefacts(ARTP_AC) * 100;
 
     if (duration[DUR_ICY_ARMOUR])
-        AC += 400 + you.props[ICY_ARMOUR_KEY].get_int() * 8;
+        AC += 500 + you.props[ICY_ARMOUR_KEY].get_int() * 8;
 
     AC += _stoneskin_bonus();
 
