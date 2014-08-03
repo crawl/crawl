crawl/crawl

commit cb4b6e118dc6ff279597e484c3415fc68ab7bd6f

allow stash titles to wrap if they are too long (1260)

crawl-ref/source/stash.cc
@@ -464,7 +464,10 @@ public:
     bool can_travel;
 protected:
     void draw_title();
+    int title_height() const;
     bool process_key(int key);
+private:
+    formatted_string create_title_string() const;
 };
 
 void StashMenu::draw_title()
@@ -472,42 +475,76 @@ void StashMenu::draw_title()
     if (title)
     {
         cgotoxy(1, 1);
-        formatted_string fs = formatted_string(title->colour);
-        fs.cprintf("%s", title->text.c_str());
-        if (title->quantity)
-        {
-            fs.cprintf(", %d item%s", title->quantity,
-                                      title->quantity == 1? "" : "s");
-        }
-        fs.cprintf(")");
-
-        if (action_cycle == Menu::CYCLE_TOGGLE)
-        {
-            fs.cprintf("  [a-z: %s  ?/!: %s]",
-                       menu_action == ACT_EXAMINE ? "examine" : "shopping",
-                       menu_action == ACT_EXAMINE ? "shopping" : "examine");
-        }
-
-        if (can_travel)
-        {
-            if (action_cycle == Menu::CYCLE_TOGGLE)
-            {
-                // XXX: This won't fit in the title, so it goes into the
-                // footer/-more-.  Not ideal, but I don't know where else
-                // to put it.
-                string str = "<w>[ENTER: travel]</w>";
-                set_more(formatted_string::parse_string(str));
-                flags |= MF_ALWAYS_SHOW_MORE;
-            }
-            else
-                fs.cprintf("  [ENTER: travel]");
-        }
-        fs.display();
+        create_title_string().display();
 
 #ifdef USE_TILE_WEB
         webtiles_set_title(fs);
 #endif
     }
+}
+
+int StashMenu::title_height() const
+{
+    if (title)
+    {
+        return 1 + (create_title_string().width() - 1) / get_number_of_cols();
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+formatted_string StashMenu::create_title_string() const
+{
+    formatted_string fs = formatted_string(title->colour);
+    fs.cprintf("%s", title->text.c_str());
+    if (title->quantity)
+    {
+        fs.cprintf(", %d item%s", title->quantity,
+                                  title->quantity == 1? "" : "s");
+    }
+    fs.cprintf(")");
+
+    vector<string> extra_parts;
+
+    string part = "[a-z: ";
+    part += string(menu_action == ACT_EXAMINE ? "examine" : "shopping");
+    part += "  ?/!: ";
+    part += string(menu_action == ACT_EXAMINE ? "shopping" : "examine");
+    part += "]";
+    extra_parts.push_back(part);
+
+    if (can_travel)
+        extra_parts.push_back(string("[ENTER: travel]"));
+
+    int term_width = get_number_of_cols();
+    int remaining = term_width - fs.width();
+    unsigned int extra_idx = 0;
+    while (static_cast<int>(extra_parts[extra_idx].length()) + 2 <= remaining)
+    {
+        fs.cprintf("  %s", extra_parts[extra_idx].c_str());
+
+        remaining -= extra_parts[extra_idx].length() + 2;
+        extra_idx++;
+        if (extra_idx >= extra_parts.size())
+            break;
+    }
+    // XXX assuming only two rows are possible for now
+    if (extra_idx < extra_parts.size())
+    {
+        fs.cprintf("%s", string(remaining, ' ').c_str());
+
+        string second_line;
+        for (unsigned int i = extra_idx; i < extra_parts.size(); ++i)
+            second_line += string("  ") + extra_parts[i];
+
+        fs.cprintf("%s%s",
+                   string(term_width - second_line.length(), ' ').c_str(),
+                   second_line.c_str());
+    }
+
+    return fs;
 }
 
 bool StashMenu::process_key(int key)

crawl-ref/source/menu.cc
@@ -314,13 +314,13 @@ vector<MenuEntry *> Menu::show(bool reuse_selections)
     }
 
     // Reset offset to default.
-    mdisplay->set_offset(1 + !!title);
+    mdisplay->set_offset(1 + title_height());
 
     // Lose lines for the title + room for -more- line.
 #ifdef USE_TILE_LOCAL
-    pagesize = max_pagesize - !!title - 1;
+    pagesize = max_pagesize - title_height() - 1;
 #else
-    pagesize = get_number_of_lines() - !!title - 1;
+    pagesize = get_number_of_lines() - title_height() - 1;
     if (max_pagesize > 0 && pagesize > max_pagesize)
         pagesize = max_pagesize;
 #endif
@@ -1274,7 +1274,7 @@ void Menu::draw_menu()
 
     draw_title();
     draw_select_count(sel.size());
-    y_offset = 1 + !!title;
+    y_offset = 1 + title_height();
 
     mdisplay->set_offset(y_offset);
 
@@ -1350,6 +1350,11 @@ void Menu::write_title()
     if (x > 1 || text.empty())
         cprintf("%-*s", get_number_of_cols() + 1 - x, "");
     cgotoxy(x, y);
+}
+
+int Menu::title_height() const
+{
+    return !!title;
 }
 
 bool Menu::in_page(int index) const

crawl-ref/source/menu.h
@@ -462,6 +462,7 @@ protected:
 
     virtual void draw_title();
     virtual void write_title();
+    virtual int title_height() const;
     virtual void draw_menu();
     virtual bool page_down();
     virtual bool line_down();

commit 5b98bf0440052f5b33ff3fc17341559878181b0d

Fix Webtiles compilation.
Broken by cb4b6e1; the stash title is now unwrapped again in Webtiles.

crawl-ref/source/stash.cc
@@ -467,7 +467,7 @@ protected:
     int title_height() const;
     bool process_key(int key);
 private:
-    formatted_string create_title_string() const;
+    formatted_string create_title_string(bool wrap = true) const;
 };
 
 void StashMenu::draw_title()
@@ -478,7 +478,7 @@ void StashMenu::draw_title()
         create_title_string().display();
 
 #ifdef USE_TILE_WEB
-        webtiles_set_title(fs);
+        webtiles_set_title(create_title_string(false));
 #endif
     }
 }
@@ -495,7 +495,7 @@ int StashMenu::title_height() const
     }
 }
 
-formatted_string StashMenu::create_title_string() const
+formatted_string StashMenu::create_title_string(bool wrap) const
 {
     formatted_string fs = formatted_string(title->colour);
     fs.cprintf("%s", title->text.c_str());
@@ -521,7 +521,8 @@ formatted_string StashMenu::create_title_string() const
     int term_width = get_number_of_cols();
     int remaining = term_width - fs.width();
     unsigned int extra_idx = 0;
-    while (static_cast<int>(extra_parts[extra_idx].length()) + 2 <= remaining)
+    while (static_cast<int>(extra_parts[extra_idx].length()) + 2 <= remaining
+           || !wrap)
     {
         fs.cprintf("  %s", extra_parts[extra_idx].c_str());
 