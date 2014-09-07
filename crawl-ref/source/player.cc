crawl/crawl

commit 560c00287ab2b199161b97ccc2850819a1d4cc9b

Always use the better umbral boost (plaidman, #8463).

crawl-ref/source/player.cc
@@ -3913,10 +3913,12 @@ int check_stealth(void)
     // how to make use of it:
     if (you.umbra())
     {
+        int umbra_multiplier = 1;
         if (you_worship(GOD_DITHMENOS) || you_worship(GOD_YREDELEMNUL))
-            stealth = stealth * (you.piety + MAX_PIETY) / MAX_PIETY;
-        else if (player_equip_unrand(UNRAND_SHADOWS))
-            stealth = stealth * 3 / 2;
+            umbra_multiplier = (you.piety + MAX_PIETY) / MAX_PIETY;
+        if (player_equip_unrand(UNRAND_SHADOWS))
+            umbra_multiplier = max(umbra_multiplier, 3 / 2);
+        stealth *= umbra_multiplier;
     }
     // The shifting glow from the Orb, while too unstable to negate invis
     // or affect to-hit, affects stealth even more than regular glow.

commit 6de1aded2cbcedf789bf95b5258409bc04870964

Fix Dith not actually increasing player stealth
This was broken by 560c0028.

Yred worshippers in umbras and players wearing the ring of shadows were
also less stealthy due to this bug.

crawl-ref/source/player.cc
@@ -3832,17 +3832,19 @@ int check_stealth()
     // makes you extremely unstealthy.
     if (you.backlit())
         stealth = stealth * 2 / 5;
+
     // On the other hand, shrouding has the reverse effect, if you know
     // how to make use of it:
     if (you.umbra())
     {
-        int umbra_multiplier = 1;
+        double umbra_multiplier = 1;
         if (you_worship(GOD_DITHMENOS) || you_worship(GOD_YREDELEMNUL))
-            umbra_multiplier = (you.piety + MAX_PIETY) / MAX_PIETY;
+            umbra_multiplier = double(you.piety + MAX_PIETY) / MAX_PIETY;
         if (player_equip_unrand(UNRAND_SHADOWS))
-            umbra_multiplier = max(umbra_multiplier, 3 / 2);
+            umbra_multiplier = max(umbra_multiplier, 1.5);
         stealth *= umbra_multiplier;
     }
+
     // If you're surrounded by a storm, you're inherently pretty conspicuous.
     if (in_good_standing(GOD_QAZLAL, 0))
     {