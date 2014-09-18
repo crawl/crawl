0711bb4a1aa6eda5cfc4b686d7bcaf139d416e6d
Shmuale Mark <shm.mark@gmail.com>
2014-09-18 08:14:19 -0400

    Don't prompt for removing an amulet of faith for 6* Ru worshippers.

--- crawl-ref/source/invent.cc
+++ crawl-ref/source/invent.cc
@@ -1701,6 +1701,7 @@ bool needs_handle_warning(const item_def &item, operation_types oper)
     if (oper == OPER_REMOVE
         && item.base_type == OBJ_JEWELLERY
         && item.sub_type == AMU_FAITH
+        && !(you_worship(GOD_RU) && you.piety >= piety_breakpoint(6))
         && !you_worship(GOD_NO_GOD)
         && !you_worship(GOD_XOM))
     {
