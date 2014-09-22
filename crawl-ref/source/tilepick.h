d9090ed7355b15b51f2dd71cf9e3c84ff9e1301b
gammafunk <gammafunk@gmail.com>
2014-09-21 19:20:23 -0500

    Add option tile_player_tile and show player weapon and shield
    The option tile_player_tile can be set to use a monster's tile instead
    of the conventional player tiles for the player doll. The value
    playermons will duplicate the behavior of tile_show_player_species =
    true, using the player species monster tile. Values with the prefixes
    mons: and tile: can be used to set a base monster or a specific
    monster tile to use for the tile (mostly useful for using tile
    variants). For instance, use the following in an rc file to use the
    deep elf demonologist tile:
    
        tile_player_tile = mons:deep elf demonologist
    
    and use the following to get a hydra tile with all heads drawn:
    
        tile_player_tile = tile:mons_hydra_7
    
    All forms of these options show the player's weapon and shield on the
    tile, and in the correction location for those tiles that have mcache
    entries or otherwise look fine with default offsets. Many tiles don't
    have mcache entries with these offsets yet need them, and will show
    the weapon and/or shield in strange (but sometimes amusing!)
    locations on the tile. For these tiles, we can add the offsets as good
    values for them are found.

--- crawl-ref/source/tilepick.h
+++ crawl-ref/source/tilepick.h
@@ -66,5 +66,7 @@ static inline void bind_item_tile(item_def &item) {}
 string tile_debug_string(tileidx_t fg, tileidx_t bg, tileidx_t cloud, char prefix);
 
 void tile_init_props(monster* mon);
-
+tileidx_t tileidx_monster_base(int type, bool in_water = false, int colour = 0,
+                               int number = 0, int tile_num_prop = 0);
+tileidx_t tileidx_mon_clamp(tileidx_t tile, int offset);
 #endif
