88694198e5dabaf26683f78f2f2fdd6216dfa641
Shmuale Mark <shm.mark@gmail.com>
2014-09-29 16:21:12 -0400

    Lower the zig rune requirement to 2 (dpeg).

--- crawl-ref/source/stairs.cc
+++ crawl-ref/source/stairs.cc
@@ -800,7 +800,7 @@ void down_stairs(dungeon_feature_type force_stair, bool force_known_shaft,
 
     if (stair_find == DNGN_ENTER_ZIGGURAT)
     {
-        #define ZIG_RUNES 3
+        #define ZIG_RUNES 2
         if (runes_in_pack() < ZIG_RUNES)
         {
             mprf("You need at least %d runes to enter this place.", ZIG_RUNES);
