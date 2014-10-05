730199c863e0e712e3f20067d9d3c8799f55ca8c
Nicholas Feinberg <pleasingfung@gmail.com>
2014-10-05 16:22:05 -0700

    Revert unintended changes

--- crawl-ref/source/mon-util.cc
+++ crawl-ref/source/mon-util.cc
@@ -1599,10 +1599,14 @@ bool name_zombie(monster* mon, const monster* orig)
     if (!mons_is_unique(orig->type) && orig->mname.empty())
         return false;
 
-    const monster_type species = mons_species(orig->type);
-    const string orig_name = mons_class_name(species);
-    dprf("orig name: %s", orig_name.c_str());
-    return name_zombie(mon, orig->type, orig_name);
+    string name;
+
+    if (!orig->mname.empty())
+        name = orig->mname;
+    else
+        name = mons_type_name(orig->type, DESC_PLAIN);
+
+    return name_zombie(mon, orig->type, name);
 }
 
 static int _downscale_zombie_damage(int damage)
