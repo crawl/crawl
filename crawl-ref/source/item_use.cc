07fbd84dc422a74a5dfb163788ce935b4a154da0
Shmuale Mark <shm.mark@gmail.com>
2014-10-05 17:37:12 -0400

    Fix compilation.

--- crawl-ref/source/item_use.cc
+++ crawl-ref/source/item_use.cc
@@ -460,7 +460,7 @@ bool can_wear_armour(const item_def &item, bool verbose, bool ignore_temporary)
     const int sub_type = item.sub_type;
     const equipment_type slot = get_armour_slot(item);
 
-    if (you.species == SP_OCTOPODE && slot != EQ_HELMETSHIELD && slot != EQ_SHIELD)
+    if (you.species == SP_OCTOPODE && slot != EQ_HELMET && slot != EQ_SHIELD)
     {
         if (verbose)
             mpr("You can't wear that!");
