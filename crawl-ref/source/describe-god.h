19e27bad7373384f9b6881624755897debb8c311
Shmuale Mark <shm.mark@gmail.com>
2014-10-18 20:38:28 -0400

    Refactor describe_god() a bit.
    To get rid of the icky recursion, and keep god_desc_type contained to
    the file.

--- crawl-ref/source/describe-god.h
+++ crawl-ref/source/describe-god.h
@@ -6,16 +6,7 @@
 #ifndef DESCRIBE_GOD_H
 #define DESCRIBE_GOD_H
 
-enum god_desc_type
-{
-    GDESC_OVERVIEW,
-    GDESC_DETAILED,
-    GDESC_WRATH,
-    NUM_GDESCS
-};
-
 string god_title(god_type which_god, species_type which_species, int piety);
-void describe_god(god_type which_god, bool give_title,
-                  god_desc_type gdesc = GDESC_OVERVIEW);
+void describe_god(god_type which_god, bool give_title);
 
 #endif
