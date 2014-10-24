14269fa1bc4c4f25dae786431387d7915ce2a94f
Shmuale Mark <shm.mark@gmail.com>
2014-10-24 10:16:58 -0400

    Salamanders are in the same genus as nagas.

--- crawl-ref/source/mon-util.cc
+++ crawl-ref/source/mon-util.cc
@@ -786,8 +786,7 @@ bool mons_is_native_in_branch(const monster* mons,
 
     case BRANCH_SNAKE:
         return mons_genus(mons->type) == MONS_NAGA
-               || mons_genus(mons->type) == MONS_SNAKE
-               || mons_genus(mons->type) == MONS_SALAMANDER;
+               || mons_genus(mons->type) == MONS_SNAKE;
 
     case BRANCH_ZOT:
         return mons_genus(mons->type) == MONS_DRACONIAN
