bef8693de09938a79982efe602dcb38e4422bb4e
Neil Moore <neil@s-z.org>
2014-10-24 21:04:06 -0400

    Spell (#9077)

--- crawl-ref/source/mutation.cc
+++ crawl-ref/source/mutation.cc
@@ -528,7 +528,7 @@ string describe_mutations(bool center_title)
 #endif
 
     case SP_FORMICID:
-        result += "Your are under a permanent stasis effect.\n";
+        result += "You are under a permanent stasis effect.\n";
         result += "You can dig through walls and to a lower floor.\n";
         result += "Your four strong arms can wield two-handed weapons with a shield.\n";
         have_any = true;
