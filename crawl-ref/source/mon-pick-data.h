67713a8e9990b81e26ef94fb190726e6342197ee
Corin Buchanan-Howland <corin@buchananhowland.net>
2014-11-02 18:09:24 -0500

    Slightly adjust caustic bee spawn rates
    Make it less likely for them to spawn in D and also change their
    Depths distribution from SEMI to FLAT.

--- crawl-ref/source/mon-pick-data.h
+++ crawl-ref/source/mon-pick-data.h
@@ -135,9 +135,9 @@ static const pop_entry pop_d[] =
   { 24, 27,    8, UP,   MONS_DANCING_WEAPON },
   { 24, 27,   25, FLAT, MONS_WAR_GARGOYLE },
   { 25, 27,   12, UP,   MONS_CRYSTAL_GUARDIAN },
-  { 25, 30,   25, SEMI, MONS_CAUSTIC_BEE },
   { 27, 27,   18, FLAT, MONS_SHADOW_DRAGON },
   { 27, 27,    8, FLAT, MONS_IRON_DRAGON },
+  { 30, 30,   25, UP,   MONS_CAUSTIC_BEE },
   { 0,0,0,FLAT,MONS_0 }
 };
 
@@ -1095,7 +1095,7 @@ static const pop_entry pop_depths[] =
   {  2,  8,  135, SEMI, MONS_SPRIGGAN_AIR_MAGE },
   {  2,  8,  185, SEMI, MONS_SPRIGGAN_BERSERKER },
   {  3, 14,   45, FLAT, MONS_SPRIGGAN_DEFENDER },
-  {  3, 14,   80, SEMI, MONS_CAUSTIC_BEE },
+  {  3, 14,   80, FLAT, MONS_CAUSTIC_BEE },
   {  4, 14,   80, SEMI, MONS_TENTACLED_MONSTROSITY },
   {  4, 14,   40, FLAT, MONS_STORM_DRAGON },
   {  4, 14,   40, FLAT, MONS_SHADOW_DRAGON },
