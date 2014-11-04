7c4013b5da9e3426658917db0ff5195ca6cc663e
Shayne Halvorson <N78291@gmail.com>
2014-11-03 21:23:07 -0600

    Replace butterflies with ravens in Foxfire (theTower).

--- crawl-ref/source/decks.cc
+++ crawl-ref/source/decks.cc
@@ -2278,7 +2278,7 @@ static void _summon_flying(int power, deck_rarity_type rarity)
 
     const monster_type flytypes[] =
     {
-        MONS_BUTTERFLY, MONS_INSUBSTANTIAL_WISP, MONS_KILLER_BEE,
+        MONS_INSUBSTANTIAL_WISP, MONS_KILLER_BEE, MONS_RAVEN,
         MONS_VAMPIRE_MOSQUITO, MONS_YELLOW_WASP, MONS_RED_WASP
     };
     const int num_flytypes = ARRAYSZ(flytypes);
