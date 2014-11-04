e5996b89f0ea64234d3e4bd1d2204afc53a326ac
Neil Moore <neil@s-z.org>
2014-11-04 16:49:44 -0500

    Replace some magic constants.
    But books use constants here that differ from NDSC_*, so leave those.

--- crawl-ref/source/itemname.cc
+++ crawl-ref/source/itemname.cc
@@ -651,7 +651,7 @@ static const char* _wand_type_name(int wandtype)
 
 static const char* wand_secondary_string(int s)
 {
-    switch (s % 16)
+    switch (s % NDSC_WAND_SEC)
     {
     case 0:  return "";
     case 1:  return "jewelled ";
@@ -675,7 +675,7 @@ static const char* wand_secondary_string(int s)
 
 static const char* wand_primary_string(int p)
 {
-    switch (p % 12)
+    switch (p % NDSC_WAND_PRI)
     {
     case 0:  return "iron";
     case 1:  return "brass";
@@ -873,7 +873,7 @@ static string jewellery_type_name(int jeweltype)
 
 static const char* ring_secondary_string(int s)
 {
-    switch (s % 13)
+    switch (s % NDSC_JEWEL_SEC)
     {
     case 1:  return "encrusted ";
     case 2:  return "glowing ";
@@ -893,7 +893,7 @@ static const char* ring_secondary_string(int s)
 
 static const char* ring_primary_string(int p)
 {
-    switch (p % 29)
+    switch (p % NDSC_JEWEL_PRI)
     {
     case 0:  return "wooden";
     case 1:  return "silver";
@@ -930,7 +930,7 @@ static const char* ring_primary_string(int p)
 
 static const char* amulet_secondary_string(int s)
 {
-    switch (s % 13)
+    switch (s % NDSC_JEWEL_SEC)
     {
     case 0:  return "dented ";
     case 1:  return "square ";
@@ -951,7 +951,7 @@ static const char* amulet_secondary_string(int s)
 
 static const char* amulet_primary_string(int p)
 {
-    switch (p % 29)
+    switch (p % NDSC_JEWEL_PRI)
     {
     case 0:  return "zirconium";
     case 1:  return "sapphire";
@@ -1077,7 +1077,7 @@ static const char* misc_type_name(int type, bool known)
 
 static const char* book_secondary_string(int s)
 {
-    switch (s % 12)
+    switch (s % 12) // larger than NDSC_BOOK_SEC?
     {
     case 0:  return "";
     case 1:  return "chunky ";
@@ -1097,7 +1097,7 @@ static const char* book_secondary_string(int s)
 
 static const char* book_primary_string(int p)
 {
-    switch (p % 7)
+    switch (p % 7) // smaller than NDSC_BOOK_PRI??
     {
     case 0:  return "paperback ";
     case 1:  return "hardcover ";
@@ -1163,7 +1163,7 @@ static const char* _book_type_name(int booktype)
 
 static const char* staff_secondary_string(int p)
 {
-    switch (p % 10) // general descriptions
+    switch (p % NDSC_STAVE_SEC) // general descriptions
     {
     case 0:  return "crooked ";
     case 1:  return "knobbly ";
@@ -1181,7 +1181,7 @@ static const char* staff_secondary_string(int p)
 
 static const char* staff_primary_string(int p)
 {
-    switch (p % 4) // special attributes
+    switch (p % NDSC_STAVE_PRI) // special attributes
     {
     case 0:  return "glowing ";
     case 1:  return "jewelled ";
