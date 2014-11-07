ca384b8900a910fd45813bc3db49f0101e121aac
Shmuale Mark <shm.mark@gmail.com>
2014-11-06 21:42:05 -0500

    Give feature_def a constructor.
    And clean up a little code.

--- crawl-ref/source/feature.h
+++ crawl-ref/source/feature.h
@@ -10,20 +10,34 @@ struct feature_def
     const char*          vaultname;       // used for KFEAT and &(
     dungeon_char_type    dchar;           // the symbol
     dungeon_char_type    magic_dchar;     // the symbol shown when magic mapped
-    unsigned short       colour;          // normal in LoS colour
-    unsigned short       map_colour;      // colour when out of LoS on display
-    unsigned short       seen_colour;     // map_colour when env.map_knowledge().seen()
-    unsigned short       em_colour;       // Emphasised colour when in LoS.
-    unsigned short       seen_em_colour;  // Emphasised colour when out of LoS
+    colour_t             colour;          // normal in LoS colour
+    colour_t             map_colour;      // colour when out of LoS on display
+    colour_t             seen_colour;     // map_colour when env.map_knowledge().seen()
+    colour_t             em_colour;       // Emphasised colour when in LoS.
+    colour_t             seen_em_colour;  // Emphasised colour when out of LoS
     unsigned             flags;
     map_feature          minimap;         // mini-map categorization
+
+    feature_def(
+        dungeon_feature_type feat_ = DNGN_UNSEEN,
+        const char *name_ = "", const char *vaultname_ = "",
+        dungeon_char_type dchar_ = NUM_DCHAR_TYPES,
+        dungeon_char_type magic_dchar_ = NUM_DCHAR_TYPES,
+        colour_t colour_ = BLACK, colour_t map_colour_ = DARKGREY,
+        colour_t seen_colour_ = BLACK, colour_t em_colour_ = BLACK,
+        colour_t seen_em_colour_ = BLACK, unsigned flags_ = FFT_NONE,
+        map_feature minimap_ = MF_UNSEEN) :
+        feat{feat_}, name{name_}, vaultname{vaultname_}, dchar{dchar_},
+        magic_dchar{magic_dchar_}, colour{colour_}, map_colour{map_colour_},
+        seen_colour{seen_colour_}, em_colour{em_colour_},
+        seen_em_colour{seen_em_colour_}, flags{flags_}, minimap{minimap_}
+    {}
 
     bool is_notable() const { return flags & FFT_NOTABLE; }
     ucs_t symbol() const;
     ucs_t magic_symbol() const;
 };
 
-void init_fd(feature_def& fd);
 void init_show_table();
 
 const feature_def &get_feature_def(show_type object);
