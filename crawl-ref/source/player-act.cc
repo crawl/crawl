crawl/crawl

commit 99fa9bcddb75f68c9cae7d206c3e6477a827b84b

Add actor::heads().
For a bit more encapsulation of monster::number, and some code
deduplication.

crawl-ref/source/player-act.cc
@@ -971,3 +971,11 @@ int player::constriction_damage() const
 {
     return roll_dice(2, div_rand_round(strength(), 5));
 }
+
+int player::heads() const
+{
+    if (form != TRAN_HYDRA)
+        return 1; // not actually always true
+    ASSERT(props.exists(HYDRA_FORM_HEADS_KEY));
+    return props[HYDRA_FORM_HEADS_KEY].get_int();
+}

crawl-ref/source/actor.h
@@ -246,6 +246,8 @@ public:
 
     bool torpor_slowed() const;
 
+    virtual int heads() const = 0;
+
     virtual int stat_hp() const = 0;
     virtual int stat_maxhp() const = 0;
 

crawl-ref/source/fight.cc
@@ -157,7 +157,7 @@ bool fight_melee(actor *attacker, actor *defender, bool *did_hit, bool simu)
     }
 
     const int nrounds = attacker->as_monster()->has_hydra_multi_attack()
-        ? attacker->as_monster()->number + MAX_NUM_ATTACKS - 1
+        ? attacker->heads() + MAX_NUM_ATTACKS - 1
         : MAX_NUM_ATTACKS;
     coord_def pos = defender->pos();
 
@@ -450,7 +450,7 @@ bool actor_can_cleave(const actor &attacker, skill_type attack_skill)
         return false;
 
     if (attacker.is_player()
-        && you.form == TRAN_HYDRA && hydra_form_heads() > 1)
+        && you.form == TRAN_HYDRA && you.heads() > 1)
     {
         return true;
     }

crawl-ref/source/melee_attack.cc
@@ -2161,10 +2161,7 @@ bool melee_attack::consider_decapitation(int dam, int damage_type)
         return true;
     }
 
-    // XXX: deduplicate this headcount
-    const int heads = defender->is_monster() ?
-                        defender->as_monster()->number :
-                        hydra_form_heads();
+    int heads = defender->heads();
     if (heads >= limit - 1)
         return false; // don't overshoot the head limit!
 
@@ -2178,7 +2175,6 @@ bool melee_attack::consider_decapitation(int dam, int damage_type)
     {
         mpr("You grow two more!");
         set_hydra_form_heads(heads + 2);
-        you.wield_change        = true;
     }
 
     return false;
@@ -2280,9 +2276,7 @@ void melee_attack::decapitate(int dam_type)
         verb = RANDOM_ELEMENT(slice_verbs);
     }
 
-    const int heads = defender->is_monster() ?
-                        defender->as_monster()->number :
-                        hydra_form_heads();
+    int heads = defender->heads();
     if (heads == 1) // will be zero afterwards
     {
         if (defender_visible)
@@ -2320,10 +2314,7 @@ void melee_attack::decapitate(int dam_type)
     }
 
     if (defender->is_player())
-    {
         set_hydra_form_heads(heads - 1);
-        you.wield_change        = true;
-    }
     else
         defender->as_monster()->number--;
 }

crawl-ref/source/mon-cast.cc
@@ -4805,9 +4805,9 @@ void mons_cast(monster* mons, bolt &pbolt, spell_type spell_cast,
 #endif
         ASSERT(idx < (int)ARRAYSZ(serpent_of_hell_breaths));
         ASSERT(idx >= 0);
-        ASSERT(mons->number == ARRAYSZ(serpent_of_hell_breaths[idx]));
+        ASSERT(mons->heads() == ARRAYSZ(serpent_of_hell_breaths[idx]));
 
-        for (unsigned int i = 0; i < mons->number; ++i)
+        for (int i = 0; i < mons->heads(); ++i)
         {
             spell_type head_spell = serpent_of_hell_breaths[idx][i];
             setup_mons_cast(mons, pbolt, head_spell);

crawl-ref/source/mon-death.cc
@@ -1331,7 +1331,7 @@ static void _make_spectral_thing(monster* mons, bool quiet)
             shapeshift = ENCH_GLOWING_SHAPESHIFTER;
 
         // Headless hydras cannot be made spectral hydras, sorry.
-        if (spectre_type == MONS_HYDRA && mons->number == 0)
+        if (spectre_type == MONS_HYDRA && mons->heads() == 0)
         {
             mpr("A glowing mist gathers momentarily, then fades.");
             return;

crawl-ref/source/mon-util.cc
@@ -1651,7 +1651,7 @@ mon_attack_def mons_attack_spec(const monster* mon, int attk_number, bool base_f
     const bool zombified = mons_is_zombified(mon);
 
     if (mon->has_hydra_multi_attack())
-        attk_number -= mon->number - 1;
+        attk_number -= mon->heads() - 1;
 
     if (attk_number < 0 || attk_number >= MAX_NUM_ATTACKS)
         attk_number = 0;

crawl-ref/source/monster.cc
@@ -5039,6 +5039,18 @@ bool monster::has_hydra_multi_attack() const
         || mons_species(false) == MONS_SERPENT_OF_HELL;
 }
 
+int monster::heads() const
+{
+    if (has_hydra_multi_attack())
+        return number;
+    else if (mons_shouts(mons_species(true)) == S_SHOUT2)
+        return 2;
+    // There are lots of things with more or fewer heads, but the return value
+    // here doesn't actually matter for non-hydra-type monsters.
+    else
+        return 1;
+}
+
 bool monster::has_multitargeting() const
 {
     if (mons_wields_two_weapons(this))

commit 85c8eae05c0f6829ab2eac202a17142d7bcb75ab

Fix hydra form transform message
Always claimed "you turn into a 1-headed hydra";  was broken by
99fa9bcd.

crawl-ref/source/player-act.cc
@@ -971,10 +971,14 @@ int player::constriction_damage() const
     return roll_dice(2, div_rand_round(strength(), 5));
 }
 
+/**
+ * How many heads does the player have, in their current form?
+ *
+ * Currently only checks for hydra form.
+ */
 int player::heads() const
 {
-    if (form != TRAN_HYDRA)
-        return 1; // not actually always true
-    ASSERT(props.exists(HYDRA_FORM_HEADS_KEY));
-    return props[HYDRA_FORM_HEADS_KEY].get_int();
+    if (props.exists(HYDRA_FORM_HEADS_KEY))
+        return props[HYDRA_FORM_HEADS_KEY].get_int();
+    return 1; // not actually always true
 }