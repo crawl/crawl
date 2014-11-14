b5e8846a6afe3031672ccb5eaad38fcf48acd6fa
Shayne Halvorson <N78291@gmail.com>
2014-11-14 12:50:32 -0600

    Replace Cloud card steam with rare poison clouds.
    Steam is mobile so it doesn't work for surrounding monsters.

--- crawl-ref/source/decks.cc
+++ crawl-ref/source/decks.cc
@@ -2752,7 +2752,8 @@ static void _cloud_card(int power, deck_rarity_type rarity)
 
         switch (power_level)
         {
-            case 0: cloudy = coinflip() ? CLOUD_STEAM : CLOUD_MEPHITIC;
+            case 0: cloudy = (you_worship(GOD_SHINING_ONE) || !one_chance_in(5))
+                              ? CLOUD_MEPHITIC : CLOUD_POISON;
                     break;
 
             case 1: cloudy = (you_worship(GOD_DITHMENOS) || coinflip())
