b036a971f1bbae34fbcfebaa8210123c783a585e
Shmuale Mark <shm.mark@gmail.com>
2014-11-15 21:06:42 -0500

    Restore monsters to their original type in death (minmay).

--- crawl-ref/source/mon-poly.cc
+++ crawl-ref/source/mon-poly.cc
@@ -9,7 +9,6 @@
 
 #include "artefact.h"
 #include "attitude-change.h"
-#include "butcher.h"
 #include "delay.h"
 #include "describe.h"
 #include "dgn-overview.h"
@@ -342,9 +341,6 @@ void change_monster_type(monster* mons, monster_type targetc)
 
     // Forget various speech/shout Lua functions.
     mons->props.erase("speech_prefix");
-
-    // Don't allow polymorphing monsters for hides.
-    mons->props[NEVER_HIDE_KEY] = true;
 
     // Keep spells for named monsters, but don't override innate ones
     // for dragons and the like. This means that Sigmund polymorphed
