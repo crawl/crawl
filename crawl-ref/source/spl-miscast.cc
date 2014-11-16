5327d74d260a0b9b2e97eb1677bddbb706e98174
Neil Moore <neil@s-z.org>
2014-11-16 11:53:30 -0500

    Simplify?  Condense, anyway.

--- crawl-ref/source/spl-miscast.cc
+++ crawl-ref/source/spl-miscast.cc
@@ -1342,12 +1342,8 @@ void MiscastEffect::_translocation(int severity)
         case 5:
         {
             bool success = false;
-
             for (int i = 1 + random2(3); i > 0; --i)
-            {
-                if (_create_monster(MONS_SPATIAL_VORTEX, 3))
-                    success = true;
-            }
+                success |= _create_monster(MONS_SPATIAL_VORTEX, 3);
 
             if (success)
                 all_msg = "Space twists in upon itself!";
@@ -1507,12 +1503,8 @@ void MiscastEffect::_summoning(int severity)
         case 0:
         {
             bool success = false;
-
             for (int i = 1 + random2(3); i > 0; --i)
-            {
-                if (_create_monster(MONS_SPATIAL_VORTEX, 3))
-                    success = true;
-            }
+                success |= _create_monster(MONS_SPATIAL_VORTEX, 3);
 
             if (success)
                 all_msg = "Space twists in upon itself!";
@@ -1532,12 +1524,8 @@ void MiscastEffect::_summoning(int severity)
         case 5:
         {
             bool success = false;
-
             for (int i = 2 + random2(2); i > 0; --i)
-            {
-                if (_create_monster(MONS_ABOMINATION_SMALL, 5, true))
-                    success = true;
-            }
+                success |= _create_monster(MONS_ABOMINATION_SMALL, 5, true);
 
             if (success && neither_end_silenced())
             {
@@ -1562,12 +1550,8 @@ void MiscastEffect::_summoning(int severity)
             case 0:
             {
                 bool success = false;
-
                 for (int i = 1 + random2(3); i > 0; --i)
-                {
-                    if (_create_monster(MONS_WORLDBINDER, 5, true))
-                        success = true;
-                }
+                    success |= _create_monster(MONS_WORLDBINDER, 5, true);
 
                 if (success)
                     all_msg = "Desperate hands claw out from thin air!";
@@ -1585,12 +1569,8 @@ void MiscastEffect::_summoning(int severity)
             case 2:
             {
                 bool success = false;
-
                 for (int i = 1 + random2(2); i > 0; --i)
-                {
-                    if (_create_monster(summon_any_demon(RANDOM_DEMON_COMMON), 3, true))
-                        success = true;
-                }
+                    success |= _create_monster(summon_any_demon(RANDOM_DEMON_COMMON), 3, true);
 
                 if (success)
                 {
@@ -1924,12 +1904,8 @@ void MiscastEffect::_necromancy(int severity)
         case 0:
         {
             bool success = false;
-
             for (int i = 0; i < 2; ++i)
-            {
-                if (_create_monster(MONS_SHADOW, 2, true))
-                    success = true;
-            }
+                success |= _create_monster(MONS_SHADOW, 2, true);
 
             if (success)
             {
@@ -2117,16 +2093,12 @@ void MiscastEffect::_transmutation(int severity)
             break;
         case 5:
         {
+            // Should be actual monsters and not durable summons,
+            // but there's a bunch of problems there (corpses,
+            // piety, perma-allies-on-hostiles-somehow, etc)
             bool success = false;
-
             for (int i = 2 + random2(3); i > 0; --i)
-            {
-                // Should be actual monsters and not durable summons,
-                // but there's a bunch of problems there (corpses,
-                // piety, perma-allies-on-hostiles-somehow, etc)
-                if (_create_monster(MONS_GIANT_MITE, 0, true))
-                    success = true;
-            }
+                success |= _create_monster(MONS_GIANT_MITE, 0, true);
 
             if (success)
             {
@@ -2311,10 +2283,8 @@ void MiscastEffect::_fire(int severity)
         {
             bool success = false;
             for (int i = 1 + random2(2); i > 0; --i)
-            {
-                if (_create_monster(MONS_FIRE_VORTEX, 2, true))
-                    success = true;
-            }
+                success |= _create_monster(MONS_FIRE_VORTEX, 2, true);
+
             if (success)
             {
                 you_msg        = "Fire whirls out from your @hands@!";
@@ -3184,10 +3154,8 @@ void MiscastEffect::_zot()
         case 3:
         case 4:
             for (int i = 1 + random2(2); i > 0; --i)
-            {
-                if (_create_monster(summon_any_demon(RANDOM_DEMON_COMMON), 0, true))
-                    success = true;
-            }
+                success |= _create_monster(summon_any_demon(RANDOM_DEMON_COMMON), 0, true);
+
             if (success)
             {
                 you_msg = "Something turns its malign attention towards "
@@ -3198,10 +3166,8 @@ void MiscastEffect::_zot()
             break;
         case 5:
             for (int i = 2 + random2(2); i > 0; --i)
-            {
-                if (_create_monster(MONS_ABOMINATION_SMALL, 5, true))
-                    success = true;
-            }
+                success |= _create_monster(MONS_ABOMINATION_SMALL, 5, true);
+
             if (success && neither_end_silenced())
             {
                 you_msg        = "A chorus of moans calls out to you!";
@@ -3214,10 +3180,8 @@ void MiscastEffect::_zot()
         case 6:
         case 7:
             for (int i = 2 + random2(4); i > 0; --i)
-            {
-                if (_create_monster(RANDOM_MOBILE_MONSTER, 4, true))
-                    success = true;
-            }
+                success |= _create_monster(RANDOM_MOBILE_MONSTER, 4, true);
+
             if (success)
             {
                 all_msg = "Wisps of shadow whirl around...";
