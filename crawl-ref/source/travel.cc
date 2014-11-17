crawl/crawl

commit 3ec7e53c43c5f2d2cfa6284924051d99d43c6c2c

Implement and use a map_find template function.
The safe way to get a value from a map is to find() and only if that
returned a non-end iterator dereference the iterator to get the value.
Unfortunately, map iterators don't convert to bool, so it's hard to
do this in an if condition, leading to the iterator being declared
before the if, in a higher scope than necessary.

map_find is a wrapper around map::find that returns a pointer to the
value (or null if not found).  Since pointers do convert to bool, this
function is easier to use in conditionals.

crawl-ref/source/travel.cc
@@ -1863,11 +1863,13 @@ static void _find_parent_branch(branch_type br, int depth,
                                 branch_type *pb, int *pd)
 {
     *pb = parent_branch(br);   // Check depth before using *pb.
-    map<branch_type, set<level_id> >::iterator i = stair_level.find(br);
-    if (i == stair_level.end() || i->second.size() == 0)
-        *pd = 0;
-    else
-        *pd = i->second.begin()->depth;
+
+    if (auto levels = map_find(stair_level, br))
+    {
+        if (levels->size() > 0)
+            *pd = levels->begin()->depth;
+    }
+    *pd = 0;
 }
 
 // Appends the passed in branch/depth to the given vector, then attempts to

crawl-ref/source/colour.cc
@@ -11,6 +11,7 @@
 #include "dgn-height.h"
 #include "options.h"
 #include "stringutil.h"
+#include "libutil.h" // map_find
 
 static element_colour_calc* element_colours[NUM_COLOURS] = {};
 static map<string, element_colour_calc*> element_colours_str;
@@ -781,12 +782,10 @@ int str_to_colour(const string &str, int default_colour, bool accept_number)
     if (ret == 16)
     {
         // Maybe we have an element colour attribute.
-        map<string, element_colour_calc*>::const_iterator it
-            = element_colours_str.find(str);
-        if (it != element_colours_str.end())
+        if (element_colour_calc **calc = map_find(element_colours_str, str))
         {
-            ASSERT(it->second);
-            ret = it->second->type;
+            ASSERT(*calc);
+            ret = (*calc)->type;
         }
     }
 

crawl-ref/source/dungeon.cc
@@ -514,11 +514,8 @@ void dgn_erase_unused_vault_placements()
             // Fix new indexes for all higher indexed vaults that are
             // still referenced.
             for (int j = i + 1; j < nvaults; ++j)
-            {
-                map<int, int>::iterator imap = new_vault_index_map.find(j);
-                if (imap != new_vault_index_map.end())
-                    --imap->second;
-            }
+                if (int *newidx = map_find(new_vault_index_map, j))
+                    --*newidx;
         }
         else
         {
@@ -533,9 +530,8 @@ void dgn_erase_unused_vault_placements()
         const int map_index = env.level_map_ids(*ri);
         if (map_index != INVALID_MAP_INDEX)
         {
-            map<int, int>::iterator imap = new_vault_index_map.find(map_index);
-            if (imap != new_vault_index_map.end())
-                env.level_map_ids(*ri) = imap->second;
+            if (int *newidx = map_find(new_vault_index_map, map_index))
+                env.level_map_ids(*ri) = *newidx;
         }
     }
 

crawl-ref/source/godconduct.cc
@@ -920,14 +920,12 @@ static void _handle_your_gods_response(conduct_type thing_done, int level,
     COMPILE_CHECK(ARRAYSZ(divine_likes) == NUM_GODS);
 
     // If your god disliked the action, evaluate its response.
-    auto peeve = divine_peeves[you.religion].find(thing_done);
-    if (peeve != divine_peeves[you.religion].end())
-        (peeve->second)(thing_done, level, known, victim);
+    if (auto peeve = map_find(divine_peeves[you.religion], thing_done))
+        (*peeve)(thing_done, level, known, victim);
 
     // If your god liked the action, evaluate its response.
-    auto like = divine_likes[you.religion].find(thing_done);
-    if (like != divine_likes[you.religion].end())
-        (like->second)(thing_done, level, known, victim);
+    if (auto like = map_find(divine_likes[you.religion], thing_done))
+        (*like)(thing_done, level, known, victim);
 }
 
 // a sad and shrunken function.

crawl-ref/source/hiscores.cc
@@ -2735,11 +2735,10 @@ void xlog_fields::add_field(const string &key, const char *format, ...)
 
 string xlog_fields::str_field(const string &s) const
 {
-    xl_map::const_iterator i = fieldmap.find(s);
-    if (i == fieldmap.end())
+    if (string *value = map_find(fieldmap, s))
+        return *value;
+    else
         return "";
-
-    return i->second;
 }
 
 int xlog_fields::int_field(const string &s) const

crawl-ref/source/initfile.cc
@@ -4920,8 +4920,8 @@ int game_options::o_int(const char *name, int def) const
 {
     int val = def;
     opt_map::const_iterator i = named_options.find(name);
-    if (i != named_options.end())
-        val = atoi(i->second.c_str());
+    if (string *value = map_find(named_options, name))
+        val = atoi(value->c_str());
     return val;
 }
 
@@ -4929,17 +4929,16 @@ bool game_options::o_bool(const char *name, bool def) const
 {
     bool val = def;
     opt_map::const_iterator i = named_options.find(name);
-    if (i != named_options.end())
-        val = _read_bool(i->second, val);
+    if (string *value = map_find(named_options, name))
+        val = _read_bool(*value, val);
     return val;
 }
 
 string game_options::o_str(const char *name, const char *def) const
 {
     string val;
-    opt_map::const_iterator i = named_options.find(name);
-    if (i != named_options.end())
-        val = i->second;
+    if (string *value = map_find(named_options, name))
+        val = *value;
     else if (def)
         val = def;
     return val;

crawl-ref/source/l_global.cc
@@ -3,6 +3,7 @@
 #include "l_libs.h"
 
 #include "clua.h"
+#include "libutil.h" // map_find
 
 //////////////////////////////////////////////////////////////////////
 // Miscellaneous globals
@@ -14,9 +15,8 @@ static pattern_map pattern_cache;
 
 static text_pattern &get_text_pattern(const string &s, bool checkcase)
 {
-    pattern_map::iterator i = pattern_cache.find(s);
-    if (i != pattern_cache.end())
-        return i->second;
+    if (text_pattern *pat = map_find(pattern_cache, s))
+        return *pat;
 
     if (pattern_cache.size() > PATTERN_FLUSH_CEILING)
         pattern_cache.clear();

crawl-ref/source/l_moninf.cc
@@ -13,6 +13,7 @@
 #include "coord.h"
 #include "env.h"
 #include "l_defs.h"
+#include "libutil.h" // map_find
 #include "mon-book.h"
 #include "spl-util.h"
 #include "stringutil.h"
@@ -101,13 +102,14 @@ LUAFN(moninf_get_is)
             _init_mi_flags();
         string flag = luaL_checkstring(ls, 2);
         const map<string, int>::const_iterator f = mi_flags.find(lowercase(flag));
-        if (f == mi_flags.end())
+        if (int *flagnum = map_find(mi_flags, lowercase(flag)))
+            num = *flagnum;
+        else
         {
             luaL_argerror(ls, 2, (string("no such moninf flag: '")
                                   + flag + "'").c_str());
             return 0;
         }
-        num = f->second;
     }
     if (num < 0 || num >= NUM_MB_FLAGS)
     {

commit 1b2be080c47b7d0b165589b77f1d3e1c58de715d

Fix oplace=D:-1.
Caused by 3ec7e53.

crawl-ref/source/travel.cc
@@ -1867,7 +1867,10 @@ static void _find_parent_branch(branch_type br, int depth,
     if (auto levels = map_find(stair_level, br))
     {
         if (levels->size() > 0)
+        {
             *pd = levels->begin()->depth;
+            return;
+        }
     }
     *pd = 0;
 }