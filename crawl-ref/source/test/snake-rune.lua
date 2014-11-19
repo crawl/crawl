d9a7bca558665a351e690c2063a2aa2ea3e86dc9
Jesse Luehrs <doy@tozt.net>
2014-11-19 11:38:51 -0500

    add some debugging output

--- crawl-ref/source/test/snake-rune.lua
+++ crawl-ref/source/test/snake-rune.lua
@@ -13,56 +13,91 @@ local branch_entrance_feats = {
 local junk_feat_fn = dgn.feature_set_fn("rock_wall", "floor", "stone_wall")
 
 local function thing_exists_fn(thing)
+  crawl.message('z')
   return function()
+           crawl.message('y')
            return test.level_contains_item(thing)
          end
 end
 
 local function visit_branch_end_from(start, stair_places, final_predicate)
+  crawl.message('0')
   while true do
     crawl.message("Visiting (" .. current_iter .. ") " .. start)
     debug.goto_place(start)
+    crawl.message('1')
 
     dgn.reset_level()
+    crawl.message('2')
     debug.generate_level()
+    crawl.message('3')
 
     local downstairs = { }
+    crawl.message('4')
     for y = 1, dgn.GYM - 2 do
+      crawl.message('5')
       for x = 1, dgn.GXM - 2 do
+        crawl.message('6')
         local dfeat = dgn.grid(x, y)
+        crawl.message('7')
         if not junk_feat_fn(dfeat) then
+          crawl.message('8')
           for _, place in ipairs(stair_places) do
+            crawl.message('9')
             if dfeat == place[1] then
+              crawl.message('10')
               return visit_branch_end_from(place[2], stair_places,
                                            final_predicate)
             end
+            crawl.message('12')
           end
+          crawl.message('13')
 
           if test.is_down_stair(dfeat) then
+            crawl.message('14')
             table.insert(downstairs, dgn.point(x, y))
+            crawl.message('15')
           end
+          crawl.message('16')
         end
+        crawl.message('17')
       end
+      crawl.message('18')
     end
+    crawl.message('19')
 
     if #downstairs > 0 then
+      crawl.message('20')
       local _, _, branch, depth = string.find(start, "(%w+):(%d+)")
+      crawl.message('21')
       start = branch .. ":" .. (tonumber(depth) + 1)
+      crawl.message('22')
     else
+      crawl.message('23')
       test.map_assert(final_predicate(),
                       "Place " .. start .. " does not satisfy predicate")
+      crawl.message('24')
       return
     end
+    crawl.message('26')
   end
+  crawl.message('27')
 end
 
+crawl.message('a')
 for i = 1, niters do
   crawl.message("Visiting Snake:$ the hard way")
   current_iter = i
+  crawl.message('b')
   debug.flush_map_memory()
+  crawl.message('c')
   entry = crawl.random_range(3, 7)
+  crawl.message('d')
   debug.goto_place("Snake:1", entry)
+  crawl.message('e')
   visit_branch_end_from("D:1",
                         branch_entrance_feats,
                         thing_exists_fn("serpentine rune"))
+  crawl.message('f')
 end
+crawl.message('g')
