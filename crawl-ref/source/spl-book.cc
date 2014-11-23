6823490e3b436b3a6d880a0dbbd59574916c0f8f
Steve Melenchuk <smelenchuk@gmail.com>
2014-11-22 19:03:51 -0700

    Clean up hardbook handling.
    Since it's 100% identical to the set of randbooks, leverage one set of
    data to map to both.

--- crawl-ref/source/spl-book.cc
+++ crawl-ref/source/spl-book.cc
@@ -13,7 +13,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <iomanip>
-#include <set>
+#include <map>
 
 #include "artefact.h"
 #include "colour.h"
@@ -297,9 +297,23 @@ int book_rarity(uint8_t which_book)
 
 static uint8_t _lowest_rarity[NUM_SPELLS];
 
-static const set<book_type> rare_books =
+/**
+ * Rare books require 6 spellcasting and 10 in a specific skill to
+ * memorise from, or worshipping a specific god (usually one who gifts that
+ * spellbook).
+ */
+
+struct rare_book_specs
 {
-    BOOK_ANNIHILATIONS, BOOK_GRAND_GRIMOIRE, BOOK_NECRONOMICON,
+    skill_type skill;
+    god_type   god;
+};
+
+static const map<book_type, rare_book_specs> rare_books =
+{
+    { BOOK_ANNIHILATIONS,  { SK_CONJURATIONS, GOD_NO_GOD } },
+    { BOOK_GRAND_GRIMOIRE, { SK_SUMMONINGS,   GOD_NO_GOD } },
+    { BOOK_NECRONOMICON,   { SK_NECROMANCY,   GOD_KIKUBAAQUDGHA } },
 };
 
 bool is_rare_book(book_type type)
@@ -380,16 +394,11 @@ bool player_can_memorise_from_spellbook(const item_def &book)
     if (book.props.exists(SPELL_LIST_KEY))
         return true;
 
-    if ((book.sub_type == BOOK_ANNIHILATIONS
-         && (you.skill(SK_CONJURATIONS) < 10
-             || you.skill(SK_SPELLCASTING) < 6))
-        || (book.sub_type == BOOK_GRAND_GRIMOIRE
-            && (you.skill(SK_SUMMONINGS) < 10
-                || you.skill(SK_SPELLCASTING) < 6))
-        || (book.sub_type == BOOK_NECRONOMICON
-            && !you_worship(GOD_KIKUBAAQUDGHA)
-            && (you.skill(SK_NECROMANCY) < 10
-                || you.skill(SK_SPELLCASTING) < 6)))
+    auto it = rare_books.find(static_cast<book_type>(book.sub_type));
+
+    if (it != rare_books.end()
+        && (you.skill(SK_SPELLCASTING) < 6 || you.skill(it->second.skill) < 10)
+        && (it->second.god == GOD_NO_GOD || !you_worship(it->second.god)))
     {
         return false;
     }
@@ -1290,7 +1299,7 @@ static void _get_spell_list(vector<spell_type> &spells, int level,
         for (auto i : rare_books)
             for (int j = 0; j < SPELLBOOK_SIZE; ++j)
             {
-                spell_type spell = which_spell_in_book(i, j);
+                spell_type spell = which_spell_in_book(i.first, j);
                 if (spell == SPELL_NO_SPELL)
                     continue;
 
