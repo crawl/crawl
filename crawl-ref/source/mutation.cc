crawl/crawl

commit 54438205d9330e1bf47918ac481ca106770f56f6

Mangle NEVER_HIDE corpses
Specifically, change kiku (& gozag) from applying an invisible
NEVER_HIDE property on corpses (to prevent scumming for dragon
hides); they now instead MANGLE the corpses, which is, well,
visible, in both the item name & description.

Should hopefully be an improvement in usability.

crawl-ref/source/mutation.cc
@@ -2663,7 +2663,7 @@ int handle_pbd_corpses()
             if (j->base_type == OBJ_CORPSES
                 && j->sub_type == CORPSE_BODY
                 && j->special > 50
-                && !j->props.exists(NEVER_HIDE_KEY))
+                && !j->props.exists(MANGLED_CORPSE_KEY))
             {
                 ++corpse_count;
                 if (corpse_count == 7)

crawl-ref/source/butcher.cc
@@ -266,10 +266,9 @@ bool butchery(int which_corpse)
 
 static void _create_monster_hide(const item_def corpse)
 {
-    // kiku_receive_corpses() creates corpses that are easily scummed
-    // for hides.  We prevent this by setting "never_hide" as an item
-    // property of corpses it creates.
-    if (corpse.props.exists(NEVER_HIDE_KEY))
+    // make certain sources of dragon hides less scummable
+    // (kiku's corpse drop, gozag ghoul corpse shops)
+    if (corpse.props.exists(MANGLED_CORPSE_KEY))
         return;
 
     monster_type mons_class = corpse.mon_type;

crawl-ref/source/butcher.h
@@ -6,7 +6,7 @@
 #ifndef BUTCHER_H
 #define BUTCHER_H
 
-#define NEVER_HIDE_KEY "never_hide"
+#define MANGLED_CORPSE_KEY "mangled_corpse"
 
 bool butchery(int which_corpse = -1);
 

crawl-ref/source/describe.cc
@@ -16,6 +16,7 @@
 
 #include "artefact.h"
 #include "branch.h"
+#include "butcher.h"
 #include "clua.h"
 #include "command.h"
 #include "database.h"
@@ -1806,6 +1807,22 @@ string get_item_description(const item_def &item, bool verbose,
     case OBJ_CORPSES:
         if (item.sub_type == CORPSE_SKELETON)
             break;
+
+        if (mons_class_leaves_hide(item.mon_type))
+        {
+            description << "\n\n";
+            if (item.props.exists(MANGLED_CORPSE_KEY))
+            {
+                description << "This corpse is badly mangled; its hide is "
+                               "beyond any hope of recovery.";
+            }
+            else
+            {
+                description << "Butchering may allow you to recover this "
+                               "creature's hide, which can be enchanted into "
+                               "armour.";
+            }
+        }
         // intentional fall-through
     case OBJ_FOOD:
         if (item.base_type == OBJ_FOOD)

crawl-ref/source/dungeon.cc
@@ -5742,7 +5742,9 @@ static int _make_delicious_corpse()
         return NON_ITEM;
     }
 
-    mitm[index_of_corpse_created].props[NEVER_HIDE_KEY] = true;
+    // no hides allowed, I guess?
+    if (mons_class_leaves_hide(mon_type))
+        mitm[index_of_corpse_created].props[MANGLED_CORPSE_KEY] = true;
     return index_of_corpse_created;
 }
 

crawl-ref/source/godabil.cc
@@ -1885,7 +1885,9 @@ bool kiku_receive_corpses(int pow)
             continue;
         }
 
-        mitm[index_of_corpse_created].props[NEVER_HIDE_KEY] = true;
+        // no scumming for hides
+        if (mons_class_leaves_hide(mon_type))
+            mitm[index_of_corpse_created].props[MANGLED_CORPSE_KEY] = true;
 
         ASSERT(valid_corpse >= 0);
 
@@ -1893,8 +1895,6 @@ bool kiku_receive_corpses(int pow)
         int rottedness = 200 -
             (!one_chance_in(10) ? random2(200 - you.piety)
                                 : random2(100 + random2(75)));
-        rottedness = rottedness / 2 + 1; // hack to adjust for rotten corpse
-                                         // removal
         mitm[index_of_corpse_created].special = rottedness;
 
         // Place the corpse.
@@ -4228,7 +4228,7 @@ static string _gozag_special_shop_name(shop_type type)
         if (you.species == SP_VAMPIRE)
             return "Blood";
         else if (you.species == SP_GHOUL)
-            return "Corpse";
+            return "Carrion"; // yum!
     }
 
     return "";

crawl-ref/source/itemname.cc
@@ -14,6 +14,7 @@
 
 #include "artefact.h"
 #include "art-enum.h"
+#include "butcher.h"
 #include "colour.h"
 #include "decks.h"
 #include "describe.h"
@@ -1890,6 +1891,12 @@ string item_def::name_aux(description_level_type desc, bool terse, bool ident,
     {
         if (dbname && item_typ == CORPSE_SKELETON)
             return "decaying skeleton";
+
+        if (item_typ == CORPSE_BODY && props.exists(MANGLED_CORPSE_KEY)
+            && !dbname)
+        {
+            buff << "mangled ";
+        }
 
         uint64_t name_type, name_flags = 0;
 

crawl-ref/source/items.cc
@@ -18,6 +18,7 @@
 #include "art-enum.h"
 #include "beam.h"
 #include "bitary.h"
+#include "butcher.h"
 #include "cio.h"
 #include "clua.h"
 #include "colour.h"
@@ -4741,11 +4742,13 @@ item_info get_item_info(const item_def& item)
     else if (item_type_tried(item))
         ii.flags |= ISFLAG_TRIED;
 
-    const char* copy_props[] = {ARTEFACT_APPEAR_KEY, KNOWN_PROPS_KEY,
-                                CORPSE_NAME_KEY, CORPSE_NAME_TYPE_KEY,
-                                "drawn_cards", "item_tile", "item_tile_name",
-                                "worn_tile", "worn_tile_name",
-                                "needs_autopickup", FORCED_ITEM_COLOUR_KEY};
+    static const char* copy_props[] =
+    {
+        ARTEFACT_APPEAR_KEY, KNOWN_PROPS_KEY, CORPSE_NAME_KEY,
+        CORPSE_NAME_TYPE_KEY, "drawn_cards", "item_tile", "item_tile_name",
+        "worn_tile", "worn_tile_name", "needs_autopickup",
+        FORCED_ITEM_COLOUR_KEY, MANGLED_CORPSE_KEY
+    };
     for (unsigned i = 0; i < ARRAYSZ(copy_props); ++i)
     {
         if (item.props.exists(copy_props[i]))

commit 21a9965b6f6650759087886648d9f38cbc6aa2d4

Let Powered by Death work with all corpses
...including "mangled" (formerly NO_HIDE) corpses from Kiku.

The interaction is and was invisible and counterintuitive, and
the bulk of it was inadvertently broken by 54438205d933 anyway.

crawl-ref/source/mutation.cc
@@ -2638,8 +2638,7 @@ int handle_pbd_corpses()
         {
             if (j->base_type == OBJ_CORPSES
                 && j->sub_type == CORPSE_BODY
-                && j->special > 50
-                && !j->props.exists(MANGLED_CORPSE_KEY))
+                && j->special > 50)
             {
                 ++corpse_count;
                 if (corpse_count == 7)