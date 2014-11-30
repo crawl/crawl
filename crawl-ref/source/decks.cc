87a7c7f5a9b76037cac6c99c929f50432849d166
theTower <mwclaws@gmail.com>
2014-11-30 15:06:44 -0500

    Don't give rPois- via Venom card or poison miscast to the poison immune
    Messages are still mediocre here, but whatever.
    
    The nonliving can still get the reduction if they're not already poison
    immune, but arbitrary resistances around there are a headache and they're
    already blocked from being poisoned.

--- crawl-ref/source/decks.cc
+++ crawl-ref/source/decks.cc
@@ -1881,8 +1881,13 @@ static void _damaging_card(card_type card, int power, deck_rarity_type rarity,
             zapping(ztype, power/6, beam);
             if (venom_vuln)
             {
-                mpr("Releasing that poison leaves you vulnerable to poison.");
-                you.increase_duration(DUR_POISON_VULN, 10 - power_level * 5 + random2(6), 50);
+                if (you.res_poison(false) >= 3)
+                    mpr("Releasing that poison makes you feel less healthy for a moment.");
+                else
+                {
+                    mpr("Releasing that poison leaves you vulnerable to poison.");
+                    you.increase_duration(DUR_POISON_VULN, 10 - power_level * 5 + random2(6), 50);
+                }
             }
     }
     else if (ztype == ZAP_IOOD && power_level == 2)
