711cfe436d321957a4bb2f68497a4ca52625659b
Shmuale Mark <shm.mark@gmail.com>
2014-12-03 16:47:28 -0500

    Don't call ball lightning creatures, either.

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -8784,7 +8784,7 @@ void player_close_door(coord_def doorpos)
         if (monster* mon = monster_at(dc))
         {
             const bool mons_unseen = !you.can_see(mon);
-            if (mons_unseen || mons_is_projectile(mon))
+            if (mons_unseen || mons_is_object(mon->type))
             {
                 mprf("Something is blocking the %s!", waynoun);
                 // No free detection!
