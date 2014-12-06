2352741c4be4b0460ed9743eb46b7d781c6b7148
Steve Melenchuk <smelenchuk@gmail.com>
2014-12-05 19:34:41 -0700

    Remove the book of Wizardry.
    It's long since lost any semblance of thematic connectivity and has
    instead become a dumping ground for spells we can't find any other good
    books for, not to mention making some extremely powerful spells possibly
    too common.
    
    The only spell there not in any other book, Spellforged Servitor, is now
    in the book of Power (which is no longer artificially common as of the
    last commit). Every other spell is in exactly one other book; if you're
    hungry for one of those spells to be common again, you can always
    worship Sif Muna.

--- crawl-ref/source/wiz-item.cc
+++ crawl-ref/source/wiz-item.cc
@@ -48,6 +48,10 @@ static void _make_all_books()
 {
     for (int i = 0; i < NUM_FIXED_BOOKS; ++i)
     {
+#if TAG_MAJOR_VERSION == 34
+        if (i == BOOK_WIZARDRY)
+            continue;
+#endif
         int thing = items(false, OBJ_BOOKS, i, 0, 0, AQ_WIZMODE);
         if (thing == NON_ITEM)
             continue;
