3c0d13aa48b93c52e4af56f1aa5fe97e5c25a91c
Nicholas Feinberg <pleasingfung@gmail.com>
2014-12-08 22:10:01 -0800

    Revert "Don't special-case some actors to not fumble in water"
    It's a really unnecessary nerf to ogres & trolls, and a *really*
    unecessary nerf to poor, poor grey draconians.
    
    Still needs to be communicated better, of course.
    
    This reverts commit 4aec31c2756c0c0558671e1477b5c6053991e914.

--- crawl-ref/source/actor.h
+++ crawl-ref/source/actor.h
@@ -76,6 +76,9 @@ public:
     virtual bool      submerged() const = 0;
     virtual bool      floundering() const = 0;
 
+    // Returns true if the actor is exceptionally well balanced.
+    virtual bool      extra_balanced() const = 0;
+
     virtual int       get_hit_dice() const = 0;
     virtual int       get_experience_level() const = 0;
 
--- crawl-ref/source/player-act.cc
+++ crawl-ref/source/player-act.cc
@@ -132,7 +132,20 @@ bool player::submerged() const
 
 bool player::floundering() const
 {
-    return in_water() && !can_swim();
+    return in_water() && !can_swim() && !extra_balanced();
+}
+
+bool player::extra_balanced() const
+{
+    const dungeon_feature_type grid = grd(pos());
+    return species == SP_GREY_DRACONIAN
+              || form == TRAN_TREE
+              || grid == DNGN_SHALLOW_WATER
+                  && (species == SP_NAGA // tails, not feet
+                      || body_size(PSIZE_BODY) >= SIZE_LARGE)
+                  && (form == TRAN_LICH || form == TRAN_STATUE
+                      || form == TRAN_SHADOW
+                      || !form_changed_physiology());
 }
 
 int player::get_hit_dice() const
