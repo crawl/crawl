377cd6f991b434bceb883ff085937ac8f7b8f20d
Steve Melenchuk <smelenchuk@gmail.com>
2014-12-23 21:44:45 -0700

    Redecorate Cocytus.

--- crawl-ref/source/dat/dlua/sanity.lua
+++ crawl-ref/source/dat/dlua/sanity.lua
@@ -7,7 +7,7 @@ end
 local function sanity_checks()
   local places = {
     "Zot:$", "Snake:$", "Swamp:$", "Spider:$", "Slime:$", "Elf:$",
-    "Vaults:$", "Tomb:$", "Coc:$", "Tar:$", "Dis:$", "Geh:$"
+    "Vaults:$", "Tomb:$", "Nor:$", "Tar:$", "Dis:$", "Geh:$"
   }
   for _, place in ipairs(places) do
     assert_place_has_map(place)
