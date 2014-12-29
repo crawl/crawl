2b443149878b3ad975e9c3b3cf4b6688f38e1edd
Neil Moore <neil@s-z.org>
2014-12-29 18:01:53 -0500

    Fix the enum postincrements, and document (doh)
    Not sure what I was thinking...

--- crawl-ref/source/externs.h
+++ crawl-ref/source/externs.h
@@ -289,11 +289,20 @@ typedef uint32_t mid_t;
 /// Upper bound on the number of monsters that can ever exist in a game.
 #define MID_FIRST_NON_MONSTER MID_ANON_FRIEND
 
+/**
+ * Define overloaded ++ and -- operators for the enum T.
+ *
+ * This macro produces several inline function definitions; use it only at
+ * file/namespace scope. It requires a trailing semicolon.
+ *
+ * @param T A type expression naming the enum type to augument. Evaluated
+ *          several times.
+ */
 #define DEF_ENUM_INC(T) \
     static inline T &operator++(T &x) { return x = static_cast<T>(x + 1); } \
     static inline T &operator--(T &x) { return x = static_cast<T>(x - 1); } \
-    static inline T operator++(T &x, int) { return static_cast<T>(x + 1); } \
-    static inline T operator--(T &x, int) { return static_cast<T>(x - 1); } \
+    static inline T operator++(T &x, int) { T y = x; ++x; return y; } \
+    static inline T operator--(T &x, int) { T y = x; --x; return y; } \
     COMPILE_CHECK(is_enum<T>::value)
 
 DEF_ENUM_INC(monster_type);
