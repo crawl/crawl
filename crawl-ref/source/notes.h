ea6b1737c6901014016d6bd0d4641ca81749ecfd
Chris Campbell <chriscampbell89@gmail.com>
2014-12-30 17:23:05 +0000

    Add a note when drawing the Focus card (#8940)

--- crawl-ref/source/notes.h
+++ crawl-ref/source/notes.h
@@ -53,6 +53,8 @@ enum NOTE_TYPES
     NOTE_FEAT_MIMIC,            /* needs: mimiced feature (string) */
     NOTE_OFFERED_SPELL,         /* needs: spell idx */
     NOTE_PERM_MUTATION,         /* needs: mutation idx, reason (string) */
+    NOTE_FOCUS_CARD,            /* needs: increased stat value, decreased stat value,
+                                   increased stat name (string), decreased stat name (string) */
     NOTE_NUM_TYPES
 };
 
