crawl/crawl

commit c5cb3caafb2084eceb5139ba2332231945db6cf9

Support monster quotes again

crawl-ref/source/describe.cc
@@ -2100,34 +2100,41 @@ void get_feature_desc(const coord_def &pos, describe_info &inf)
     inf.quote = getQuoteString(db_name);
 }
 
-// Returns the pressed key in key
+/// A message explaining how the player can toggle between quote &
+static const string _toggle_message =
+    "Press '<w>!</w>'"
+#ifdef USE_TILE_LOCAL
+    " or <w>Right-click</w>"
+#endif
+    " to toggle between the description and quote.";
+
+/**
+ * If the given description has an associated quote, print a message at the
+ * bottom of the screen explaining how the player can toggle between viewing
+ * that quote & the description, and then check whether the input corresponds
+ * to such a toggle.
+ *
+ * @param inf[in]       The description in question.
+ * @param key[in,out]   The input command. If zero, is set to getchm().
+ * @return              Whether the description & quote should be toggled.
+ */
 static int _print_toggle_message(const describe_info &inf, int& key)
 {
+    mouse_control mc(MOUSE_MODE_MORE);
+    if (!key)
+        key = getchm();
+
     if (inf.quote.empty())
-    {
-        mouse_control mc(MOUSE_MODE_MORE);
-        key = getchm();
         return false;
-    }
-    else
-    {
-        const int bottom_line = min(30, get_number_of_lines());
-        cgotoxy(1, bottom_line);
-        formatted_string::parse_string(
-            "Press '<w>!</w>'"
-#ifdef USE_TILE_LOCAL
-            " or <w>Right-click</w>"
-#endif
-            " to toggle between the description and quote.").display();
 
-        mouse_control mc(MOUSE_MODE_MORE);
-        key = getchm();
+    const int bottom_line = min(30, get_number_of_lines());
+    cgotoxy(1, bottom_line);
+    formatted_string::parse_string(_toggle_message).display();
 
-        if (key == '!' || key == CK_MOUSE_CMD)
-            return true;
+    if (key == '!' || key == CK_MOUSE_CMD)
+        return true;
 
-        return false;
-    }
+    return false;
 }
 
 void describe_feature_wide(const coord_def& pos, bool show_quote)
@@ -2147,7 +2154,7 @@ void describe_feature_wide(const coord_def& pos, bool show_quote)
     if (crawl_state.game_is_hints())
         hints_describe_pos(pos.x, pos.y);
 
-    int key;
+    int key = 0;
     if (_print_toggle_message(inf, key))
         describe_feature_wide(pos, !show_quote);
 }
@@ -3952,11 +3959,30 @@ int describe_monsters(const monster_info &mi, bool force_seen,
     fs.add_text(inf.body.str());
     if (crawl_state.game_is_hints())
         fs.add_text(hints_describe_monster(mi, has_stat_desc).c_str());
+    if (!inf.quote.empty())
+    {
+        fs.add_item_formatted_string(
+                formatted_string::parse_string("\n" + _toggle_message));
+    }
 
     fs.add_item_formatted_string(formatted_string::parse_string(inf.footer));
-    fs.show();
 
-    return fs.get_lastch();
+    bool show_quote = false;
+    while (true)
+    {
+        if (show_quote)
+            _print_quote(inf);
+        else
+            fs.show();
+
+        int keyin = show_quote ? 0 : fs.get_lastch();
+        // this is never actually displayed to the player
+        // we just use it to check whether we should toggle.
+        if (_print_toggle_message(inf, keyin))
+            show_quote = !show_quote;
+        else
+            return keyin;
+    }
 }
 
 static const char* xl_rank_names[] =

crawl-ref/source/describe-spells.cc
@@ -321,9 +321,9 @@ vector<spell_type> map_chars_to_spells(const spellset &spells,
         return flat_spells;
 
     vector<spell_type> column_spells;
-    for (int i = 0; i < flat_spells.size(); i += 2)
+    for (size_t i = 0; i < flat_spells.size(); i += 2)
         column_spells.emplace_back(flat_spells[i]);
-    for (int i = 1; i < flat_spells.size(); i += 2)
+    for (size_t i = 1; i < flat_spells.size(); i += 2)
         column_spells.emplace_back(flat_spells[i]);
     return column_spells;
 }
@@ -463,13 +463,6 @@ void list_spellset(const spellset &spells, const item_def *source_item,
     spell_scroller ssc(spells, source_item);
     ssc.add_item_formatted_string(description);
     ssc.show();
-}
-
-spell_scroller::spell_scroller(const spellset &_spells,
-                               const item_def *_source_item)
-                       : formatted_scroller() {
-    spells = _spells;
-    source_item = _source_item;
 }
 
 /**

crawl-ref/source/describe-spells.h
@@ -28,7 +28,12 @@ typedef vector<spellbook_contents> spellset;
 class spell_scroller : public formatted_scroller
 {
 public:
-    spell_scroller(const spellset &_spells, const item_def *_source_item);
+    spell_scroller(const spellset &_spells, const item_def *_source_item,
+                   bool _show_quote_toggle = false) :
+    formatted_scroller(), spells(_spells), source_item(_source_item),
+    show_quote_toggle(_show_quote_toggle)
+    { };
+
     int get_lastch();
     virtual ~spell_scroller();
 protected:
@@ -36,6 +41,7 @@ protected:
 protected:
     spellset spells;
     const item_def *source_item;
+    bool show_quote_toggle;
 };
 
 spellset item_spellset(const item_def &item);

commit 7c2a80cb56ce1248a1b41c37ee61dad75dab1fcb

Show quote toggle message in feature xv again.
It was broken by c5cb3ca: we were prompting for a key before displaying
the message.

Maybe we should skip printing the prompt altogether if keyin was
provided?

crawl-ref/source/describe.cc
@@ -2134,15 +2134,19 @@ static const string _toggle_message =
 static int _print_toggle_message(const describe_info &inf, int& key)
 {
     mouse_control mc(MOUSE_MODE_MORE);
-    if (!key)
-        key = getchm();
 
     if (inf.quote.empty())
+    {
+        if (!key)
+            key = getchm();
         return false;
+    }
 
     const int bottom_line = min(30, get_number_of_lines());
     cgotoxy(1, bottom_line);
     formatted_string::parse_string(_toggle_message).display();
+    if (!key)
+        key = getchm();
 
     if (key == '!' || key == CK_MOUSE_CMD)
         return true;