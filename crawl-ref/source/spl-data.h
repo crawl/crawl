f734b43db6b2a5b0ee7eaefdec81f814df18a2b1
Nicholas Feinberg <pleasingfung@gmail.com>
2015-01-01 10:02:03 -0800

    Display /disint's success chance (Davzz)

--- crawl-ref/source/spl-data.h
+++ crawl-ref/source/spl-data.h
@@ -1141,7 +1141,7 @@ static const struct spell_desc spelldata[] =
 {
     SPELL_DISINTEGRATE, "Disintegrate",
      SPTYP_CONJURATION,
-     SPFLAG_DIR_OR_TARGET | SPFLAG_NOT_SELF | SPFLAG_NEEDS_TRACER,
+     SPFLAG_DIR_OR_TARGET | SPFLAG_NOT_SELF | SPFLAG_NEEDS_TRACER | SPFLAG_MR_CHECK,
      6,
      200,
      6, 6,
