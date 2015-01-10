c52694e70ae47462a78286ff24cff4547005ca61
Nicholas Feinberg <pleasingfung@gmail.com>
2015-01-10 14:12:46 -0800

    Don't claim that ?immo explodes

--- crawl-ref/source/beam.cc
+++ crawl-ref/source/beam.cc
@@ -5784,8 +5784,7 @@ int bolt::range_used_on_hit() const
 }
 
 // Takes a bolt and refines it for use in the explosion function.
-// Explosions which do not follow from beams (e.g., scrolls of
-// immolation) bypass this function.
+// Explosions which do not follow from beams bypass this function.
 void bolt::refine_for_explosion()
 {
     ASSERT(!special_explosion);
