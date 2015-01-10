8b9d069d0f421e29df4d4a01d428de4b7de6533f
Nicholas Feinberg <pleasingfung@gmail.com>
2015-01-10 12:53:31 -0800

    Tweak artefact stat boosts down again
    Per crawl-dev discussion, 3 was agreed to be the 'minimum
    interesting stat difference', and 7 was the largest reasonable
    value.
    
    Hopefully this doesn't ruin everything.

--- crawl-ref/source/artefact.cc
+++ crawl-ref/source/artefact.cc
@@ -649,8 +649,8 @@ struct artefact_prop_data
 
 /// Generate 'good' values for stat artps (e.g. ARTP_STRENGTH)
 static int _gen_good_stat_artp() {
-    // normally 2-6, max 10
-    return 2 + random2(5) + (one_chance_in(4) ? 1 + random2(4) : 0);
+    // normally 3-5, max 7
+    return 3 + random2(3) + (one_chance_in(4) ? random2(3) : 0);
 }
 
 /// Generate 'bad' values for stat artps (e.g. ARTP_STRENGTH)
