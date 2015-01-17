42ba71a524db4f5fed8d5b6c9deaa9ba8e03f471
gammafunk <gammafunk@gmail.com>
2015-01-17 07:13:50 -0600

    Properly handle an exception

--- crawl-ref/source/webserver/conf.py
+++ crawl-ref/source/webserver/conf.py
@@ -142,7 +142,7 @@ class Conf(object):
             self.scan_titles()
         except EnvironmentError as e:
             raise ConfigError("Couldn't read title_path directory '{0}' "
-                              "({1}).".format(self.static_path, e.strerr))
+                              "({1}).".format(self.static_path, e.strerror))
         if not self.title_images:
             raise ConfigError("No title images (title_*.png) found in "
                               "static_path ({0}).".format(self.static_path))
