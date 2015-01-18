crawl/crawl

commit c0532d524f1aa3403be096562e5a2f8d1a998af4

Reduce buckler EV penalty from 1 to 0.8
This is to partially offset the removal of the 0.8 AEVP discount in
spellcasting penalty and make bucklers more usable for robe & buckler
casters w/ low shield skill.

Fellow devs, you may wanna look this over and make sure I didn't make any
terrible int division mistakes.

crawl-ref/source/player.cc
@@ -2292,7 +2292,7 @@ static int _player_armour_beogh_bonus(const item_def& item)
 bool is_effectively_light_armour(const item_def *item)
 {
     return !item
-           || (abs(property(*item, PARM_EVASION)) < 5);
+           || (abs(property(*item, PARM_EVASION)) / 10 < 5);
 }
 
 bool player_effectively_in_light_armour()
@@ -2357,8 +2357,8 @@ static int _player_adjusted_evasion_penalty(const int scale)
             piece_armour_evasion_penalty += penalty;
     }
 
-    return piece_armour_evasion_penalty * scale +
-           you.adjusted_body_armour_penalty(scale);
+    return piece_armour_evasion_penalty * scale / 10 +
+           you.adjusted_body_armour_penalty(scale) ;
 }
 
 // EV bonuses that work even when helpless.
@@ -6270,7 +6270,7 @@ int player::unadjusted_body_armour_penalty() const
     if (!body_armour)
         return 0;
 
-    return -property(*body_armour, PARM_EVASION);
+    return -property(*body_armour, PARM_EVASION) / 10;
 }
 
 /**
@@ -6306,13 +6306,13 @@ int player::adjusted_shield_penalty(int scale) const
 
     const int base_shield_penalty = -property(*shield_l, PARM_EVASION);
     return max(0, (base_shield_penalty * scale - skill(SK_SHIELDS, scale)
-                  / _player_shield_racial_factor()));
+                  / _player_shield_racial_factor()) / 10);
 }
 
 float player::get_shield_skill_to_offset_penalty(const item_def &item)
 {
     int evp = property(item, PARM_EVASION);
-    return -1 * evp * _player_shield_racial_factor();
+    return -1 * evp * _player_shield_racial_factor() / 10;
 }
 
 int player::armour_tohit_penalty(bool random_factor, int scale) const

crawl-ref/source/describe.cc
@@ -1213,7 +1213,7 @@ void append_armour_stats(string &description, const item_def &item)
     description += "       ";
 
     description += "Encumbrance rating: ";
-    _append_value(description, -property(item, PARM_EVASION), false);
+    _append_value(description, -property(item, PARM_EVASION) / 10, false);
 }
 
 void append_shield_stats(string &description, const item_def &item)

crawl-ref/source/godpassive.cc
@@ -580,7 +580,7 @@ map<skill_type, int8_t> ash_get_boosted_skills(eq_type type)
     // Include melded.
     const item_def* wpn = you.slot_item(EQ_WEAPON, true);
     const item_def* armour = you.slot_item(EQ_BODY_ARMOUR, true);
-    const int evp = armour ? -property(*armour, PARM_EVASION) : 0;
+    const int evp = armour ? -property(*armour, PARM_EVASION / 10) : 0;
     switch (type)
     {
     case (ET_WEAPON):

crawl-ref/source/itemprop.cc
@@ -63,18 +63,18 @@ static const armour_def Armour_prop[] =
         EQ_BODY_ARMOUR, SIZE_LITTLE, SIZE_GIANT, true },
     { ARM_ROBE,                 "robe",                   2,   0,
         EQ_BODY_ARMOUR, SIZE_LITTLE, SIZE_BIG, true },
-    { ARM_LEATHER_ARMOUR,       "leather armour",         3,  -4,
+    { ARM_LEATHER_ARMOUR,       "leather armour",         3,  -40,
         EQ_BODY_ARMOUR, SIZE_SMALL,  SIZE_MEDIUM, true },
 
-    { ARM_RING_MAIL,            "ring mail",              5,  -7,
+    { ARM_RING_MAIL,            "ring mail",              5,  -70,
         EQ_BODY_ARMOUR, SIZE_SMALL,  SIZE_MEDIUM, true },
-    { ARM_SCALE_MAIL,           "scale mail",             6, -10,
+    { ARM_SCALE_MAIL,           "scale mail",             6, -100,
         EQ_BODY_ARMOUR, SIZE_SMALL,  SIZE_MEDIUM, true },
-    { ARM_CHAIN_MAIL,           "chain mail",             8, -15,
+    { ARM_CHAIN_MAIL,           "chain mail",             8, -150,
         EQ_BODY_ARMOUR, SIZE_SMALL,  SIZE_MEDIUM, true },
-    { ARM_PLATE_ARMOUR,         "plate armour",          10, -18,
+    { ARM_PLATE_ARMOUR,         "plate armour",          10, -180,
         EQ_BODY_ARMOUR, SIZE_SMALL, SIZE_MEDIUM, true },
-    { ARM_CRYSTAL_PLATE_ARMOUR, "crystal plate armour",  14, -23,
+    { ARM_CRYSTAL_PLATE_ARMOUR, "crystal plate armour",  14, -230,
         EQ_BODY_ARMOUR, SIZE_SMALL, SIZE_MEDIUM, false },
 
 #define HIDE_ARMOUR(aenum, aname, aac, aevp, henum, hname, res) \
@@ -88,30 +88,30 @@ static const armour_def Armour_prop[] =
                 ARM_ ## id ## _DRAGON_HIDE, name " dragon hide", res)
 
     HIDE_ARMOUR(
-      ARM_TROLL_LEATHER_ARMOUR, "troll leather armour",   4,  -4,
+      ARM_TROLL_LEATHER_ARMOUR, "troll leather armour",   4,  -40,
       ARM_TROLL_HIDE,           "troll hide",
         ARMF_REGENERATION
     ),
 
     DRAGON_ARMOUR(STEAM,       "steam",                   5,   0,
         ARMF_RES_STEAM),
-    DRAGON_ARMOUR(MOTTLED,     "mottled",                 6,  -5,
+    DRAGON_ARMOUR(MOTTLED,     "mottled",                 6,  -50,
         ARMF_RES_STICKY_FLAME),
-    DRAGON_ARMOUR(QUICKSILVER, "quicksilver",            10,  -7,
+    DRAGON_ARMOUR(QUICKSILVER, "quicksilver",            10,  -70,
         ARMF_RES_MAGIC),
-    DRAGON_ARMOUR(SWAMP,       "swamp",                   7,  -7,
+    DRAGON_ARMOUR(SWAMP,       "swamp",                   7,  -70,
         ARMF_RES_POISON),
-    DRAGON_ARMOUR(FIRE,        "fire",                    8, -11,
+    DRAGON_ARMOUR(FIRE,        "fire",                    8, -110,
         ard(ARMF_RES_FIRE, 2) | ARMF_VUL_COLD),
-    DRAGON_ARMOUR(ICE,         "ice",                     9, -11,
+    DRAGON_ARMOUR(ICE,         "ice",                     9, -110,
         ard(ARMF_RES_COLD, 2) | ARMF_VUL_FIRE),
-    DRAGON_ARMOUR(PEARL,       "pearl",                  10, -11,
+    DRAGON_ARMOUR(PEARL,       "pearl",                  10, -110,
         ARMF_RES_NEG),
-    DRAGON_ARMOUR(STORM,       "storm",                  10, -15,
+    DRAGON_ARMOUR(STORM,       "storm",                  10, -150,
         ARMF_RES_ELEC),
-    DRAGON_ARMOUR(SHADOW,      "shadow",                 10, -15,
+    DRAGON_ARMOUR(SHADOW,      "shadow",                 10, -150,
         ard(ARMF_STEALTH, 4)),
-    DRAGON_ARMOUR(GOLD,        "gold",                   12, -23,
+    DRAGON_ARMOUR(GOLD,        "gold",                   12, -230,
         ARMF_RES_FIRE | ARMF_RES_COLD | ARMF_RES_POISON),
 
 #undef DRAGON_HIDE
@@ -140,18 +140,18 @@ static const armour_def Armour_prop[] =
         EQ_BOOTS,       SIZE_SMALL,  SIZE_MEDIUM, true },
     // Changed max. barding size to large to allow for the appropriate
     // monster types (monsters don't differentiate between torso and general).
-    { ARM_CENTAUR_BARDING,      "centaur barding",        4,  -6,
+    { ARM_CENTAUR_BARDING,      "centaur barding",        4,  -60,
         EQ_BOOTS,       SIZE_MEDIUM, SIZE_LARGE, true },
-    { ARM_NAGA_BARDING,         "naga barding",           4,  -6,
+    { ARM_NAGA_BARDING,         "naga barding",           4,  -60,
         EQ_BOOTS,       SIZE_MEDIUM, SIZE_LARGE, true },
 
     // Note: shields use ac-value as sh-value, EV pen is used as the basis
     // to calculate adjusted shield penalty.
-    { ARM_BUCKLER,              "buckler",                3,  -1,
+    { ARM_BUCKLER,              "buckler",                3,  -8,
         EQ_SHIELD,      SIZE_LITTLE, SIZE_MEDIUM, true },
-    { ARM_SHIELD,               "shield",                 8,  -3,
+    { ARM_SHIELD,               "shield",                 8,  -30,
         EQ_SHIELD,      SIZE_SMALL,  SIZE_BIG, true    },
-    { ARM_LARGE_SHIELD,         "large shield",          13,  -5,
+    { ARM_LARGE_SHIELD,         "large shield",          13,  -50,
         EQ_SHIELD,      SIZE_MEDIUM, SIZE_GIANT, true  },
 };
 

crawl-ref/source/l_item.cc
@@ -763,7 +763,7 @@ IDEF(encumbrance)
         return 0;
 
     if (item->base_type == OBJ_ARMOUR)
-        lua_pushnumber(ls, -property(*item, PARM_EVASION));
+        lua_pushnumber(ls, -property(*item, PARM_EVASION) / 10);
     else
         lua_pushnil(ls);
 

crawl-ref/source/monster.cc
@@ -1691,7 +1691,7 @@ bool monster::wants_armour(const item_def &item) const
     // Spellcasters won't pick up restricting armour, although they can
     // start with one.  Applies to arcane spells only, of course.
     if (!pos().origin() && is_actual_spellcaster()
-        && (property(item, PARM_EVASION) < -5
+        && (property(item, PARM_EVASION) / 10 < -5
             || is_artefact(item)
                && artefact_property(item, ARTP_PREVENT_SPELLCASTING)))
     {
@@ -3567,7 +3567,7 @@ int monster::melee_evasion(const actor* /*act*/, ev_ignore_type evit) const
         const item_def* armour = mslot_item(static_cast<mon_inv_type>(slot));
         if (armour)
         {
-            ev += property(*armour, PARM_EVASION)
+            ev += property(*armour, PARM_EVASION) / 10
                   / (is_shield(*armour) ? 2 : 6);
         }
     }

commit a3e2ed81a5889fd032ab553811e88a0f684cb8ce

Let shield skill fully affect penalty reduction again (ManMan)
This was broken by c0532d524f1aa3403be096562e5a2f8d1a998af4.

crawl-ref/source/player.cc
@@ -6305,8 +6305,8 @@ int player::adjusted_shield_penalty(int scale) const
         return 0;
 
     const int base_shield_penalty = -property(*shield_l, PARM_EVASION);
-    return max(0, (base_shield_penalty * scale - skill(SK_SHIELDS, scale)
-                  / _player_shield_racial_factor()) / 10);
+    return max(0, ((base_shield_penalty * scale) - skill(SK_SHIELDS, scale)
+                  / _player_shield_racial_factor() * 10) / 10);
 }
 
 float player::get_shield_skill_to_offset_penalty(const item_def &item)