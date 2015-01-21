ff1288cd0e638f785236adcfc9680d2741261114
Nicholas Feinberg <pleasingfung@gmail.com>
2015-01-21 07:02:34 -0800

    Fix Call Merchant price display for mu (9422)
    I hate this code & regret this change :(

--- crawl-ref/source/ability.cc
+++ crawl-ref/source/ability.cc
@@ -717,7 +717,8 @@ int get_gold_cost(ability_type ability)
     switch (ability)
     {
     case ABIL_GOZAG_CALL_MERCHANT:
-        return gozag_price_for_shop(true);
+        return gozag_price_for_shop(true) *
+            (you.species == SP_MUMMY ? 3 : 2) / 2;
     case ABIL_GOZAG_POTION_PETITION:
         return gozag_potion_price();
     case ABIL_GOZAG_BRIBE_BRANCH:
