crawl/crawl

commit b2544625dcdb8f50e2e49fa3a72008d3d640c680

Give monster gargoyles rPetrify (Hurricos)
To match the player race.

crawl-ref/source/mon-data.h
@@ -5593,7 +5593,7 @@ DUMMY(MONS_GOLEM, '8', LIGHTGREY, "golem")
 {
     MONS_GARGOYLE, '9', LIGHTGREY, "gargoyle",
     M_ARTIFICIAL,
-    mrd(MR_RES_POISON, 3) | MR_RES_ELEC,
+    mrd(MR_RES_POISON, 3) | MR_RES_ELEC | MR_RES_PETRIFY,
     550, 26, MONS_GARGOYLE, MONS_GARGOYLE, MH_NONLIVING, 40,
     { {AT_HIT, AF_PLAIN, 20}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
     { 6, 3, 3, 0 },
@@ -5605,7 +5605,7 @@ DUMMY(MONS_GOLEM, '8', LIGHTGREY, "golem")
 
 {
     MONS_WAR_GARGOYLE, '9', CYAN, "war gargoyle",
-    M_ARTIFICIAL | M_SEE_INVIS | M_FIGHTER,
+    M_ARTIFICIAL | M_SEE_INVIS | M_FIGHTER | MR_RES_PETRIFY,
     MR_RES_ELEC | MR_RES_FIRE | MR_RES_COLD | mrd(MR_RES_POISON, 3),
     550, 18, MONS_GARGOYLE, MONS_WAR_GARGOYLE, MH_NONLIVING, 100,
     { {AT_HIT, AF_PLAIN, 30}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
@@ -5618,7 +5618,7 @@ DUMMY(MONS_GOLEM, '8', LIGHTGREY, "golem")
 
 {
     MONS_MOLTEN_GARGOYLE, '9', RED, "molten gargoyle",
-    M_ARTIFICIAL,
+    M_ARTIFICIAL | MR_RES_PETRIFY,
     mrd(MR_RES_POISON, 3) | MR_RES_ELEC | mrd(MR_RES_FIRE, 3),
     550, 18, MONS_GARGOYLE, MONS_MOLTEN_GARGOYLE, MH_NONLIVING, 60,
     { {AT_HIT, AF_FIRE, 20}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },

commit 2211e245bcd7e6395d0b803740d79df20ace8d50

Fix war/molten gargoyle flags/resists (Hurricos)
Broken by b2544625dcdb8.

oops

crawl-ref/source/mon-data.h
@@ -5604,8 +5604,8 @@ DUMMY(MONS_GOLEM, '8', LIGHTGREY, "golem")
 
 {
     MONS_WAR_GARGOYLE, '9', CYAN, "war gargoyle",
-    M_ARTIFICIAL | M_SEE_INVIS | M_FIGHTER | MR_RES_PETRIFY,
-    MR_RES_ELEC | MR_RES_FIRE | MR_RES_COLD | mrd(MR_RES_POISON, 3),
+    M_ARTIFICIAL | M_SEE_INVIS | M_FIGHTER,
+    MR_RES_ELEC | MR_RES_FIRE | MR_RES_COLD | mrd(MR_RES_POISON, 3) | MR_RES_PETRIFY,
     550, 18, MONS_GARGOYLE, MONS_WAR_GARGOYLE, MH_NONLIVING, 100,
     { {AT_HIT, AF_PLAIN, 30}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
     { 13, 2, 4, 0 },
@@ -5617,8 +5617,8 @@ DUMMY(MONS_GOLEM, '8', LIGHTGREY, "golem")
 
 {
     MONS_MOLTEN_GARGOYLE, '9', RED, "molten gargoyle",
-    M_ARTIFICIAL | MR_RES_PETRIFY,
-    mrd(MR_RES_POISON, 3) | MR_RES_ELEC | mrd(MR_RES_FIRE, 3),
+    M_ARTIFICIAL,
+    mrd(MR_RES_POISON, 3) | MR_RES_ELEC | mrd(MR_RES_FIRE, 3) | MR_RES_PETRIFY,
     550, 18, MONS_GARGOYLE, MONS_MOLTEN_GARGOYLE, MH_NONLIVING, 60,
     { {AT_HIT, AF_FIRE, 20}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
     { 7, 3, 3, 0 },