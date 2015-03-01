a165ed6d0f2bcbb43deb4ff91df576d621cfef87
Neil Moore <neil@s-z.org>
2015-02-28 22:38:14 -0500

    Make ARTP_CURSED less recursive.

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -474,7 +474,7 @@ static string _randart_descrip(const item_def &item)
         { ARTP_PREVENT_TELEPORTATION, "It prevents most forms of teleportation.",
           false},
         { ARTP_ANGRY,  "It may make you go berserk in combat.", false},
-        { ARTP_CURSED, "It may recurse itself when equipped.", false},
+        { ARTP_CURSED, "It may re-curse itself when equipped.", false},
         { ARTP_CLARITY, "It protects you against confusion.", false},
         { ARTP_MUTAGENIC, "It causes magical contamination when unequipped.", false},
         { ARTP_RMSL, "It protects you from missiles.", false},
