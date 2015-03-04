crawl/crawl

commit 8a05d2b9773c80580470e11dca71f3e835a14daa

Clean up some Torment code.

crawl-ref/source/spl-miscast.cc
@@ -2041,7 +2041,7 @@ void MiscastEffect::_necromancy(int severity)
                 do_msg();
             }
             else
-                torment_monsters(target->pos(), 0, TORMENT_GENERIC);
+                torment(NULL, TORMENT_MISCAST, target->pos());
             break;
 
         case 1:

crawl-ref/source/effects.cc
@@ -177,7 +177,7 @@ void holy_word(int pow, holy_word_source_type source, const coord_def& where,
         holy_word_monsters(*ri, pow, source, attacker);
 }
 
-int torment_player(actor *attacker, int taux)
+void torment_player(actor *attacker, torment_source_type taux)
 {
     ASSERT(!crawl_state.game_is_arena());
 
@@ -218,73 +218,72 @@ int torment_player(actor *attacker, int taux)
     if (!hploss)
     {
         mpr("You feel a surge of unholy energy.");
-        return 0;
+        return;
     }
 
     mpr("Your body is wracked with pain!");
 
-    const char *aux = "by torment";
 
     kill_method_type type = KILLED_BY_BEAM;
-    if (invalid_monster_index(taux))
+    if (crawl_state.is_god_acting())
+        type = KILLED_BY_DIVINE_WRATH;
+    else if (taux == TORMENT_MISCAST)
+        type = KILLED_BY_WILD_MAGIC;
+
+    const char *aux = "";
+
+    switch (taux)
     {
-        type = KILLED_BY_SOMETHING;
-        if (crawl_state.is_god_acting())
-            type = KILLED_BY_DIVINE_WRATH;
+    case TORMENT_CARDS:
+    case TORMENT_SPELL:
+        aux = "Symbol of Torment";
+        break;
 
-        switch (taux)
-        {
-        case TORMENT_CARDS:
-        case TORMENT_SPELL:
-            aux = "Symbol of Torment";
-            break;
+    case TORMENT_SPWLD:
+        // XXX: If we ever make any other weapon / randart eligible
+        // to torment, this will be incorrect.
+        aux = "Sceptre of Torment";
+        break;
 
-        case TORMENT_SPWLD:
-            // XXX: If we ever make any other weapon / randart eligible
-            // to torment, this will be incorrect.
-            aux = "Sceptre of Torment";
-            break;
+    case TORMENT_SCROLL:
+        aux = "a scroll of torment";
+        break;
 
-        case TORMENT_SCROLL:
-            aux = "a scroll of torment";
-            break;
+    case TORMENT_XOM:
+        type = KILLED_BY_XOM;
+        aux = "Xom's torment";
+        break;
 
-        case TORMENT_XOM:
-            type = KILLED_BY_XOM;
-            aux = "Xom's torment";
-            break;
+    case TORMENT_KIKUBAAQUDGHA:
+        aux = "Kikubaaqudgha's torment";
+        break;
 
-        case TORMENT_KIKUBAAQUDGHA:
-            aux = "Kikubaaqudgha's torment";
-            break;
-        }
+    case TORMENT_LURKING_HORROR:
+        type = KILLED_BY_SPORE;
+        aux = "an exploding lurking horror";
+
+    case TORMENT_MISCAST:
+        aux = "by torment";
+        break;
     }
 
     ouch(hploss, type, attacker? attacker->mid : MID_NOBODY, aux);
 
-    return 1;
+    return;
 }
 
-// torment_monsters() is called with power 0 because torment is
-// UNRESISTABLE except for having torment resistance!  Even if we used
-// maximum power of 1000, high level monsters and characters would save
-// too often.  (GDL)
-
-int torment_monsters(coord_def where, actor *attacker, int taux)
+static void _torment_stuff_at(coord_def where, actor *attacker,
+                       torment_source_type taux)
 {
-    int retval = 0;
-
     // Is the player in this cell?
     if (where == you.pos())
-        retval = torment_player(attacker, taux);
+        torment_player(attacker, taux);
+    // Don't return, since you could be standing on a monster.
 
     // Is a monster in this cell?
     monster* mons = monster_at(where);
-    if (mons == NULL)
-        return retval;
-
-    if (!mons->alive() || mons->res_torment())
-        return retval;
+    if (!mons || !mons->alive() || mons->res_torment())
+        return;
 
     int hploss = max(0, mons->hit_points * (50 - mons->res_negative_energy() * 5) / 100 - 1);
 
@@ -303,19 +302,12 @@ int torment_monsters(coord_def where, actor *attacker, int taux)
     }
 
     mons->hurt(attacker, hploss, BEAM_TORMENT_DAMAGE);
-
-    if (hploss)
-        retval = 1;
-
-    return retval;
 }
 
-int torment(actor *attacker, int taux, const coord_def& where)
+void torment(actor *attacker, torment_source_type taux, const coord_def& where)
 {
-    int r = 0;
     for (radius_iterator ri(where, LOS_NO_TRANS); ri; ++ri)
-        r += torment_monsters(*ri, attacker, taux);
-    return r;
+        _torment_stuff_at(*ri, attacker, taux);
 }
 
 void cleansing_flame(int pow, int caster, coord_def where,

crawl-ref/source/effects.h
@@ -50,9 +50,8 @@ void holy_word(int pow, holy_word_source_type source, const coord_def& where,
 void holy_word_monsters(coord_def where, int pow, holy_word_source_type source,
                         actor *attacker = NULL);
 
-int torment(actor *attacker, int taux, const coord_def& where);
-int torment_player(actor *attacker, int taux);
-int torment_monsters(coord_def where, actor *attacker, int taux);
+void torment(actor *attacker, torment_source_type taux, const coord_def& where);
+void torment_player(actor *attacker, torment_source_type taux);
 
 void cleansing_flame(int pow, int caster, coord_def where,
                      actor *attacker = NULL);

crawl-ref/source/enum.h
@@ -4292,13 +4292,14 @@ enum targeting_type
 
 enum torment_source_type
 {
-    TORMENT_GENERIC       = -1,
+    TORMENT_LURKING_HORROR= -1,
     TORMENT_CARDS         = -2,   // Symbol of torment
     TORMENT_SPWLD         = -3,   // Special wield torment
     TORMENT_SCROLL        = -4,
     TORMENT_SPELL         = -5,   // SPELL_SYMBOL_OF_TORMENT
     TORMENT_XOM           = -6,   // Xom effect
     TORMENT_KIKUBAAQUDGHA = -7,   // Kikubaaqudgha effect
+    TORMENT_MISCAST       = -8,
 };
 
 enum trap_type

crawl-ref/source/mon-cast.cc
@@ -5047,7 +5047,7 @@ void mons_cast(monster* mons, bolt &pbolt, spell_type spell_cast,
     }
 
     case SPELL_SYMBOL_OF_TORMENT:
-        torment(mons, mons->mindex(), mons->pos());
+        torment(mons, TORMENT_SPELL, mons->pos());
         return;
 
     case SPELL_MESMERISE:

crawl-ref/source/mon-death.cc
@@ -1349,7 +1349,7 @@ static bool _explode_monster(monster* mons, killer_type killer,
 
     // Explosion side-effects.
     if (type == MONS_LURKING_HORROR)
-        torment(mons, mons->mindex(), mons->pos());
+        torment(mons, TORMENT_LURKING_HORROR, mons->pos());
     else if (mons->has_ench(ENCH_INNER_FLAME))
     {
         for (adjacent_iterator ai(mons->pos(), false); ai; ++ai)

commit 400abd6f4b6b813bcd8d8f06a061ab01632bbac8

Fix necromancy miscasts tormenting LOS instead of only the miscast victim
Broken by 8a05d2b9773c8.

crawl-ref/source/spl-goditem.cc
@@ -1183,8 +1183,7 @@ void torment_player(actor *attacker, torment_source_type taux)
     return;
 }
 
-static void _torment_stuff_at(coord_def where, actor *attacker,
-                       torment_source_type taux)
+void torment_cell(coord_def where, actor *attacker, torment_source_type taux)
 {
     // Is the player in this cell?
     if (where == you.pos())
@@ -1215,7 +1214,7 @@ static void _torment_stuff_at(coord_def where, actor *attacker,
 void torment(actor *attacker, torment_source_type taux, const coord_def& where)
 {
     for (radius_iterator ri(where, LOS_NO_TRANS); ri; ++ri)
-        _torment_stuff_at(*ri, attacker, taux);
+        torment_cell(*ri, attacker, taux);
 }
 
 void setup_cleansing_flame_beam(bolt &beam, int pow, int caster,

crawl-ref/source/spl-goditem.h
@@ -33,6 +33,7 @@ void holy_word_monsters(coord_def where, int pow, holy_word_source_type source,
                         actor *attacker = nullptr);
 
 void torment(actor *attacker, torment_source_type taux, const coord_def& where);
+void torment_cell(coord_def where, actor *attacker, torment_source_type taux);
 void torment_player(actor *attacker, torment_source_type taux);
 
 void setup_cleansing_flame_beam(bolt &beam, int pow, int caster,

crawl-ref/source/spl-miscast.cc
@@ -1935,7 +1935,7 @@ void MiscastEffect::_necromancy(int severity)
                 do_msg();
             }
             else
-                torment(nullptr, TORMENT_MISCAST, target->pos());
+                torment_cell(target->pos(), nullptr, TORMENT_MISCAST);
             break;
 
         case 1: