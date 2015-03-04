6943d70b85b69d2f94c6ae061cbb203f5f7c7df9
Edgar A. Bering IV <trizor@gmail.com>
2015-03-03 20:17:23 -0600

    Check launcher inscription before firing using the launcher (#9512)
    Commiter's note: I simplified the condition and cleaned up the
    formatting.

--- crawl-ref/source/throw.cc
+++ crawl-ref/source/throw.cc
@@ -445,7 +445,10 @@ void fire_thing(int item)
     if (item == -1)
         return;
 
-    if (check_warning_inscriptions(you.inv[item], OPER_FIRE))
+    if (check_warning_inscriptions(you.inv[item], OPER_FIRE)
+        && (!you.weapon()
+            || is_launched(&you, you.weapon(), you.inv[item]) != LRET_LAUNCHED
+            || check_warning_inscriptions(*you.weapon(), OPER_FIRE)))
     {
         bolt beam;
         throw_it(beam, item, &target);
