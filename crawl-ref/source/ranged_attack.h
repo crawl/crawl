1b6081fea573dfc682838ee1aad3e1710a3af2b8
Chris Campbell <chriscampbell89@gmail.com>
2015-03-07 16:35:45 +0000

    Don't make phantasmal warrior melee pierce shields and AC
    They already have a fairly clear unique ability in their AF_VULN melee attack,
    in combination with placing alongside monsters with enchantments (vampire
    knights, and also now death knights with agony). Piercing AC and shields was a
    much less clear exception, with the AC-ignoring part in particular not being
    easily discoverable through regular gameplay.

--- crawl-ref/source/ranged_attack.h
+++ crawl-ref/source/ranged_attack.h
@@ -32,7 +32,7 @@ private:
     int weapon_damage();
     int calc_base_unarmed_damage();
     int calc_mon_to_hit_base();
-    int apply_damage_modifiers(int damage, int damage_max, bool &half_ac);
+    int apply_damage_modifiers(int damage, int damage_max);
     bool attack_ignores_shield(bool verbose);
     bool apply_damage_brand(const char *what = nullptr);
     special_missile_type random_chaos_missile_brand();
