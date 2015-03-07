2b346a28e228140b4c3919e48600ec9f0f1b48d2
Corin Buchanan-Howland <corin@buchananhowland.net>
2015-03-07 18:02:27 -0500

    Fix up randart generation a bit more
    Fix disparities between comments and code, and decrease the odds of wasting
    a positive property.

--- crawl-ref/source/artefact.cc
+++ crawl-ref/source/artefact.cc
@@ -828,11 +828,11 @@ static void _get_randart_properties(const item_def &item,
 {
     const object_class_type item_class = item.base_type;
 
-    // first figure out how good we want the artefact to be, range 1 to 6.
+    // first figure out how good we want the artefact to be, range 1 to 7.
     int quality = max(1, binomial(7, 30));
     // then consider adding bad properties. the better the artefact, the more
     // likely we add a bad property, up to a max of 2.
-    int bad = binomial(1 + div_rand_round(quality, 3), 35);
+    int bad = binomial(1 + div_rand_round(quality, 5), 30);
     // we start by assuming we'll allow one good property per quality level
     // and an additional one for each bad property.
     int good = quality + bad;
@@ -840,12 +840,12 @@ static void _get_randart_properties(const item_def &item,
     // things get spammy. Extra "good" properties will be used to enhance
     // properties only, not to add more distinct properties. There is still a
     // small chance of >4 properties.
-    int max_properties = 5 + one_chance_in(20) + one_chance_in(40);
+    int max_properties = 4 + one_chance_in(20) + one_chance_in(40);
     int enhance = 0;
     if (good + bad > max_properties)
     {
         enhance = good + bad - max_properties;
-        good = 5 - bad;
+        good = 4 - bad;
     }
 
     // initialize a vector of weighted artefact properties to pick from
@@ -886,7 +886,7 @@ static void _get_randart_properties(const item_def &item,
             const int max = artp_data[prop].max_dup;
             for (int i = 1;
                  good > 0 && item_props[prop] <= max &&
-                    (one_chance_in(i) || (enhance > 0 && i == 2));
+                    ((enhance > 0 && i > 1) || one_chance_in(i));
                  i += artp_data[prop].odds_inc)
             {
                 // Add one to the starting value for stat bonuses.
