crawl/crawl

commit fec06700d15bb7686c39167b0b9ef71005bf44d7

Improve grammar of several messages for the less-handed (#9371)
Your paw feels slithery!
The wandering mushroom's body twitches.

crawl-ref/source/godpassive.cc
@@ -299,13 +299,10 @@ string ash_describe_bondage(int flags, bool level)
         && you.bondage[ET_WEAPON] != -1)
     {
         if (you.bondage[ET_WEAPON] == you.bondage[ET_SHIELD])
-        {
-            desc = make_stringf("Your %s are %sbound.\n",
-                                you.hand_name(true).c_str(),
-                                you.bondage[ET_WEAPON] ? "" : "not ");
-        }
+            desc = you.hands_act("are", "bound.\n");
         else
         {
+            // FIXME: what if you sacrificed a hand?
             desc = make_stringf("Your %s %s is bound but not your %s %s.\n",
                                 you.bondage[ET_WEAPON] ? "weapon" : "shield",
                                 you.hand_name(false).c_str(),

crawl-ref/source/decks.cc
@@ -1996,21 +1996,29 @@ static void _blade_card(int power, deck_rarity_type rarity)
 {
     const int power_level = _get_power_level(power, rarity);
     const bool cleaving = you.duration[DUR_CLEAVE] > 0;
-    const item_def* const weapon = you.weapon();
-    const bool axe = weapon && item_attack_skill(*weapon) == SK_AXES;
-    const bool bladed = weapon && can_cut_meat(*weapon);
-    const bool plural = (weapon && weapon->quantity > 1) || !weapon;
-    string hand_part = "Your " + you.hand_name(true);
 
     you.increase_duration(DUR_CLEAVE, 10 + random2((power_level + 1) * 10));
 
     if (!cleaving)
     {
-        mprf(MSGCH_DURATION,
-             "%s look%s %ssharp%s",
-             (weapon) ? weapon->name(DESC_YOUR).c_str() : hand_part.c_str(),
-             (plural) ?  "" : "s", (bladed) ? "" : "oddly ",
-             (axe) ? " (like it always does)." : ".");
+        if (const item_def* const weapon = you.weapon())
+        {
+            // FIXME: it's not that odd for a dagger to look sharp...
+            const bool bladed = can_cut_meat(*weapon);
+            const bool axe = item_attack_skill(*weapon) == SK_AXES;
+            mprf(MSGCH_DURATION,
+                 "%s %s %ssharp%s", weapon->name(DESC_YOUR).c_str(),
+                 conjugate_verb("look", weapon->quantity > 1).c_str(),
+                 (bladed) ? "" : "oddly ",
+                 (axe) ? " (like it always does)." : ".");
+        }
+        else
+        {
+            // FIXME: no "oddly" for claws?
+            mprf(MSGCH_DURATION, "%s",
+                 you.hands_act("look", "oddly sharp.").c_str());
+        }
+
     }
     else
         mprf(MSGCH_DURATION, "Your cleaving ability is renewed.");

crawl-ref/source/item_use.cc
@@ -754,7 +754,7 @@ bool do_wear_armour(int item, bool quiet)
             if (you.species == SP_OCTOPODE)
                 mpr("You need the rest of your tentacles for walking.");
             else
-                mprf("You'd need another %s to do that!", you.hand_name(true).c_str());
+                mprf("You'd need another %s to do that!", you.hand_name(false).c_str());
         }
         return false;
     }
@@ -771,6 +771,7 @@ bool do_wear_armour(int item, bool quiet)
                 mpr("You need the rest of your tentacles for walking.");
             else
             {
+                // Singular hand should have already been handled above.
                 mprf("You'd need three %s to do that!",
                      you.hand_name(true).c_str());
             }
@@ -2306,8 +2307,8 @@ void random_uselessness(int scroll_slot)
         }
         else
         {
-            mprf("Your %s glow %s for a moment.",
-                 you.hand_name(true).c_str(), weird_glowing_colour().c_str());
+            mpr(you.hands_act("glow", weird_glowing_colour()
+                                      + " for a moment."));
         }
         break;
 

crawl-ref/source/melee_attack.cc
@@ -2179,7 +2179,7 @@ void melee_attack::attacker_sustain_passive_damage()
     else
     {
         if (attacker->is_player())
-            mprf("Your %s burn!", you.hand_name(true).c_str());
+            mpr(you.hands_act("burn", "!"));
         else
         {
             simple_monster_message(attacker->as_monster(),

crawl-ref/source/monster.cc
@@ -886,11 +886,17 @@ void monster::equip_weapon(item_def &item, int near, bool msg)
                 "random colours.");
             break;
         case SPWPN_PENETRATION:
-            mprf("%s %s briefly pass through it before %s manages to get a "
+        {
+            bool plural = true;
+            string hand = hand_name(true, &plural);
+            mprf("%s %s briefly %s through it before %s manages to get a "
                  "firm grip on it.",
                  pronoun(PRONOUN_POSSESSIVE).c_str(),
-                 hand_name(true).c_str(),
+                 hand.c_str(),
+                 // Not conj_verb: the monster isn't the subject.
+                 conjugate_verb("pass", plural).c_str(),
                  pronoun(PRONOUN_SUBJECTIVE).c_str());
+        }
             break;
         case SPWPN_REAPING:
             mpr("It is briefly surrounded by shifting shadows.");

crawl-ref/source/player-equip.cc
@@ -9,6 +9,7 @@
 #include "artefact.h"
 #include "art-enum.h"
 #include "delay.h"
+#include "english.h" // conjugate_verb
 #include "food.h"
 #include "goditem.h"
 #include "godpassive.h"
@@ -527,8 +528,7 @@ static void _equip_weapon_effect(item_def& item, bool showMsgs, bool unmeld)
                     break;
 
                 case SPWPN_SPEED:
-                    mprf("Your %s tingle!",
-                         you.hand_name(true).c_str());
+                    mpr(you.hands_act("tingle", "!"));
                     break;
 
                 case SPWPN_VAMPIRISM:
@@ -557,10 +557,17 @@ static void _equip_weapon_effect(item_def& item, bool showMsgs, bool unmeld)
                     break;
 
                 case SPWPN_PENETRATION:
-                    mprf("Your %s briefly pass through it before you manage "
+                {
+                    // FIXME: make hands_act take a pre-verb adverb so we can
+                    // use it here.
+                    bool plural = true;
+                    string hand = you.hand_name(true, &plural);
+
+                    mprf("Your %s briefly %s through it before you manage "
                          "to get a firm grip on it.",
-                         you.hand_name(true).c_str());
+                         hand.c_str(), conjugate_verb("pass", plural).c_str());
                     break;
+                }
 
                 case SPWPN_REAPING:
                     mpr("It is briefly surrounded by shifting shadows.");

crawl-ref/source/spl-summoning.cc
@@ -133,7 +133,7 @@ spret_type cast_sticks_to_snakes(int pow, god_type god, bool fail)
 {
     if (!you.weapon())
     {
-        mprf("Your %s feel slithery!", you.hand_name(true).c_str());
+        mpr(you.hands_act("feel", "slithery!"));
         return SPRET_ABORT;
     }
 
@@ -768,12 +768,16 @@ static bool _check_tukima_validity(const actor *target)
             return _fail_tukimas();
 
         if (target_is_player)
-            mprf("Your %s twitch.", you.hand_name(true).c_str());
+            mpr(you.hands_act("twitch", "."));
         else
         {
-            mprf("%s %s twitch.",
+            // FIXME: maybe move hands_act to class actor?
+            bool plural = true;
+            const string hand = target->hand_name(true, &plural);
+
+            mprf("%s %s %s.",
                  apostrophise(target->name(DESC_THE)).c_str(),
-                 target->hand_name(true).c_str());
+                 hand.c_str(), conjugate_verb("twitch", plural).c_str());
         }
         return false;
     }
@@ -3244,7 +3248,7 @@ spret_type cast_spectral_weapon(actor *agent, int pow, god_type god, bool fail)
                      wpn->quantity > 1 ? "" : "s");
             }
             else
-                mprf("Your %s twitch.", you.hand_name(true).c_str());
+                mpr(you.hands_act("twitch", "."));
         }
 
         return SPRET_ABORT;

crawl-ref/source/status.cc
@@ -293,8 +293,7 @@ bool fill_status_info(int status, status_info* inf)
 
     case DUR_CONFUSING_TOUCH:
     {
-        inf->long_text = string("Your ") + you.hand_name(true)
-                         + " are glowing red.";
+        inf->long_text = you.hands_act("are", "glowing red.");
         break;
     }
 

commit 40745be58be2b6e277ef2c3905758daac1825301

Fix Ash hand curse description (hy-on-github)
Would claim that your hands were bound if neither of them were.

Was broken by fec06700d15, last year.

crawl-ref/source/godpassive.cc
@@ -309,7 +309,12 @@ string ash_describe_bondage(int flags, bool level)
         && you.bondage[ET_WEAPON] != -1)
     {
         if (you.bondage[ET_WEAPON] == you.bondage[ET_SHIELD])
-            desc = you.hands_act("are", "bound.\n");
+        {
+            const string verb = make_stringf("are%s",
+                                             you.bondage[ET_WEAPON] ? ""
+                                                                    : " not");
+            desc = you.hands_act(verb, "bound.\n");
+        }
         else
         {
             // FIXME: what if you sacrificed a hand?