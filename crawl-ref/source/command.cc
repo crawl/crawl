f645e6cf7fe0cd601fdf957bd6a2ca138baf84d0
Jesse Luehrs <doy@tozt.net>
2015-04-01 02:29:04 -0400

    consistent messaging

--- crawl-ref/source/command.cc
+++ crawl-ref/source/command.cc
@@ -1026,7 +1026,7 @@ static void _add_formatted_keyhelp(column_composer &cols)
 
     _add_command(cols, 1, CMD_SAVE_GAME, "Save game and exit");
     _add_command(cols, 1, CMD_SAVE_GAME_NOW, "Save and exit without query");
-    _add_command(cols, 1, CMD_QUIT, "Suicide the current character");
+    _add_command(cols, 1, CMD_QUIT, "Abandon the current character");
     cols.add_formatted(1, "         and quit the game\n",
                        false, true, _cmdhelp_textfilter);
 
