707f55f986910406e3b7e4e711997e1b828a27d6
Shmuale Mark <shm.mark@gmail.com>
2015-04-08 10:47:15 -0400

    Be more paranoid about attributing boosts to Ash.

--- crawl-ref/source/skill_menu.cc
+++ crawl-ref/source/skill_menu.cc
@@ -548,7 +548,7 @@ string SkillMenuSwitch::get_help()
             vector<const char *> causes;
             if (you.duration[DUR_HEROISM])
                 causes.push_back("Heroism");
-            if (you_worship(GOD_ASHENZARI))
+            if (!you.skill_boost.empty() && in_good_standing(GOD_ASHENZARI, 3))
                 causes.push_back("Ashenzari's power");
             if (_any_crosstrained())
                 causes.push_back("cross-training");
