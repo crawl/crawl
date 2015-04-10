67d7a0a6d74fc139e12aeff742655065dca7c9c0
Chris Campbell <chriscampbell89@gmail.com>
2015-04-10 16:29:15 +0100

    Tone down caustic shrikes slightly
    Reduce their AC and HP a little, and make band sizes 3-5 instead of 3-6.

--- crawl-ref/source/mon-place.cc
+++ crawl-ref/source/mon-place.cc
@@ -2166,7 +2166,7 @@ static band_type _choose_band(monster_type mon_type, int &band_size,
 
     case MONS_CAUSTIC_SHRIKE:
         band = BAND_CAUSTIC_SHRIKE;
-        band_size = 2 + random2(4);
+        band_size = 2 + random2(3);
         break;
 
     case MONS_SHARD_SHRIKE:
