01bffe9f85f64cbacec3427af08512ee9b01f741
Shmuale Mark <shm.mark@gmail.com>
2015-04-17 08:37:57 -0400

    Document the flash_screen_message option and make it ignore use_animation.
    Since a player could certainly want to have it flash even if they don't
    want any other animations at all, and if they want to disable it as well
    it's just a matter of commenting it out in their rcfile (or adding
    `flash_screen_message =` to their rcfile it they were included from
    another one.
    
    Also, add some style fixes.

--- crawl-ref/source/options.h
+++ crawl-ref/source/options.h
@@ -594,6 +594,9 @@ enum use_animation_type
     UA_PLAYER           = (1 << 6),
     // animation when entering certain branches (abyss, zot, etc)
     UA_BRANCH_ENTRY     = (1 << 7),
+    // animations that can't be turned off (please don't use unless they can
+    // be turned off some other way)
+    UA_ALWAYS_ON        = (1 << 8),
 };
 
 #endif
