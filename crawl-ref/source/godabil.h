237d8f5f17f7aef8c727a19406778166f416d1ea
Chris Campbell <chriscampbell89@gmail.com>
2015-04-19 00:02:03 +0100

    Slightly increase the base price of Potion Petition
    Mainly just to make it a nicer, round number on the ability screen.

--- crawl-ref/source/godabil.h
+++ crawl-ref/source/godabil.h
@@ -27,7 +27,7 @@ const char * const GOZAG_SHOP_TYPE_KEY       = "gozag_shop_type_%d";
 const char * const GOZAG_SHOP_SUFFIX_KEY     = "gozag_shop_suffix_%d";
 const char * const GOZAG_SHOP_COST_KEY       = "gozag_shop_cost_%d";
 
-#define GOZAG_POTION_PETITION_AMOUNT 375
+#define GOZAG_POTION_PETITION_AMOUNT 400
 #define GOZAG_SHOP_BASE_MULTIPLIER 100
 #define GOZAG_SHOP_MOD_MULTIPLIER 25
 #define GOZAG_BRIBE_AMOUNT 3000
