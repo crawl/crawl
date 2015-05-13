0086549c04095d578a0b51b4e0100c9a1abe325c
Shmuale Mark <shm.mark@gmail.com>
2015-05-13 15:36:05 -0400

    Move species recommendations to data.

--- crawl-ref/source/job-data.h
+++ crawl-ref/source/job-data.h
@@ -3,6 +3,7 @@ struct job_def
     const char* abbrev; ///< Two-letter abbreviation
     const char* name; ///< Long name
     int s, i, d; ///< Starting Str, Dex, and Int
+    vector<species_type> recommended_species; ///< Which species are good at it
 };
 
 static const map<job_type, job_def> job_data =
@@ -11,146 +12,197 @@ static const map<job_type, job_def> job_data =
 { JOB_ABYSSAL_KNIGHT, {
     "AK", "Abyssal Knight",
     4, 4, 4,
+    { SP_HILL_ORC, SP_SPRIGGAN, SP_TROLL, SP_MERFOLK, SP_BASE_DRACONIAN,
+      SP_DEMONSPAWN, },
 } },
 
 { JOB_AIR_ELEMENTALIST, {
     "AE", "Air Elementalist",
     0, 7, 5,
+    { SP_HIGH_ELF, SP_DEEP_ELF, SP_TENGU, SP_BASE_DRACONIAN, SP_NAGA,
+      SP_VINE_STALKER, },
 } },
 
 { JOB_ARCANE_MARKSMAN, {
     "AM", "Arcane Marksman",
     3, 5, 4,
+    { SP_HIGH_ELF, SP_DEEP_ELF, SP_KOBOLD, SP_SPRIGGAN, SP_TROLL, SP_CENTAUR, },
 } },
 
 { JOB_ARTIFICER, {
     "Ar", "Artificer",
     3, 4, 5,
+    { SP_DEEP_DWARF, SP_HALFLING, SP_KOBOLD, SP_SPRIGGAN, SP_BASE_DRACONIAN,
+      SP_DEMONSPAWN, },
 } },
 
 { JOB_ASSASSIN, {
     "As", "Assassin",
     3, 3, 6,
+    { SP_TROLL, SP_HALFLING, SP_SPRIGGAN, SP_DEMONSPAWN, SP_VAMPIRE,
+      SP_VINE_STALKER, },
 } },
 
 { JOB_BERSERKER, {
     "Be", "Berserker",
     9, -1, 4,
+    { SP_HILL_ORC, SP_HALFLING, SP_OGRE, SP_MERFOLK, SP_MINOTAUR, SP_GARGOYLE,
+      SP_DEMONSPAWN, },
 } },
 
 { JOB_CHAOS_KNIGHT, {
     "CK", "Chaos Knight",
     4, 4, 4,
+    { SP_HILL_ORC, SP_TROLL, SP_CENTAUR, SP_MERFOLK, SP_MINOTAUR,
+      SP_BASE_DRACONIAN, SP_DEMONSPAWN, },
 } },
 
 { JOB_CONJURER, {
     "Cj", "Conjurer",
     0, 7, 5,
+    { SP_HIGH_ELF, SP_DEEP_ELF, SP_NAGA, SP_TENGU, SP_BASE_DRACONIAN,
+      SP_DEMIGOD, },
 } },
 
 { JOB_EARTH_ELEMENTALIST, {
     "EE", "Earth Elementalist",
     0, 7, 5,
+    { SP_DEEP_ELF, SP_DEEP_DWARF, SP_SPRIGGAN, SP_GARGOYLE, SP_DEMIGOD,
+      SP_GHOUL, SP_OCTOPODE, },
 } },
 
 { JOB_ENCHANTER, {
     "En", "Enchanter",
     0, 7, 5,
+    { SP_DEEP_ELF, SP_FELID, SP_KOBOLD, SP_SPRIGGAN, SP_NAGA, SP_VAMPIRE, },
 } },
 
 { JOB_FIGHTER, {
     "Fi", "Fighter",
     8, 0, 4,
+    { SP_DEEP_DWARF, SP_HILL_ORC, SP_TROLL, SP_MINOTAUR, SP_GARGOYLE,
+      SP_CENTAUR, },
 } },
 
 { JOB_FIRE_ELEMENTALIST, {
     "FE", "Fire Elementalist",
     0, 7, 5,
+    { SP_HIGH_ELF, SP_DEEP_ELF, SP_HILL_ORC, SP_NAGA, SP_TENGU, SP_DEMIGOD,
+      SP_GARGOYLE, },
 } },
 
 { JOB_GLADIATOR, {
     "Gl", "Gladiator",
     7, 0, 5,
+    { SP_DEEP_DWARF, SP_HILL_ORC, SP_MERFOLK, SP_MINOTAUR, SP_GARGOYLE,
+      SP_CENTAUR, },
 } },
 
 { JOB_HUNTER, {
     "Hu", "Hunter",
     4, 3, 5,
+    { SP_HIGH_ELF, SP_HILL_ORC, SP_HALFLING, SP_KOBOLD, SP_OGRE, SP_TROLL,
+      SP_CENTAUR, },
 } },
 
 { JOB_ICE_ELEMENTALIST, {
     "IE", "Ice Elementalist",
     0, 7, 5,
+    { SP_HIGH_ELF, SP_DEEP_ELF, SP_MERFOLK, SP_NAGA, SP_BASE_DRACONIAN,
+      SP_DEMIGOD, SP_GARGOYLE, },
 } },
 
 { JOB_MONK, {
     "Mo", "Monk",
     3, 2, 7,
+    { SP_HILL_ORC, SP_TROLL, SP_MINOTAUR, SP_GARGOYLE, SP_GHOUL, SP_MERFOLK,
+      SP_VAMPIRE, },
 } },
 
 { JOB_NECROMANCER, {
     "Ne", "Necromancer",
     0, 7, 5,
+    { SP_DEEP_ELF, SP_DEEP_DWARF, SP_HILL_ORC, SP_DEMONSPAWN, SP_MUMMY,
+      SP_VAMPIRE, },
 } },
 
 { JOB_SKALD, {
     "Sk", "Skald",
     4, 4, 4,
+    { SP_HIGH_ELF, SP_HALFLING, SP_CENTAUR, SP_MERFOLK, SP_BASE_DRACONIAN,
+      SP_VAMPIRE, },
 } },
 
 { JOB_SUMMONER, {
     "Su", "Summoner",
     0, 7, 5,
+    { SP_DEEP_ELF, SP_HILL_ORC, SP_VINE_STALKER, SP_MERFOLK, SP_TENGU,
+      SP_VAMPIRE, },
 } },
 
 { JOB_TRANSMUTER, {
     "Tm", "Transmuter",
     2, 5, 5,
+    { SP_NAGA, SP_MERFOLK, SP_BASE_DRACONIAN, SP_DEMIGOD, SP_DEMONSPAWN,
+      SP_TROLL, },
 } },
 
 { JOB_VENOM_MAGE, {
     "VM", "Venom Mage",
     0, 7, 5,
+    { SP_DEEP_ELF, SP_SPRIGGAN, SP_NAGA, SP_MERFOLK, SP_TENGU, SP_FELID,
+      SP_DEMONSPAWN, },
 } },
 
 { JOB_WANDERER, {
     "Wn", "Wanderer",
     0, 0, 0, // Randomised
+    { SP_HILL_ORC, SP_SPRIGGAN, SP_CENTAUR, SP_MERFOLK, SP_BASE_DRACONIAN,
+      SP_HUMAN, SP_DEMONSPAWN, },
 } },
 
 { JOB_WARPER, {
     "Wr", "Warper",
     3, 5, 4,
+    { SP_HALFLING, SP_HIGH_ELF, SP_DEEP_DWARF, SP_SPRIGGAN, SP_CENTAUR,
+      SP_BASE_DRACONIAN, },
 } },
 
 { JOB_WIZARD, {
     "Wz", "Wizard",
     -1, 10, 3,
+    { SP_HIGH_ELF, SP_DEEP_ELF, SP_NAGA, SP_BASE_DRACONIAN, SP_OCTOPODE,
+      SP_HUMAN, SP_MUMMY, },
 } },
 #if TAG_MAJOR_VERSION == 34
 { JOB_DEATH_KNIGHT, {
     "DK", "Death Knight",
     0, 0, 0,
+    { },
 } },
 
 { JOB_HEALER, {
     "He", "Healer",
     0, 0, 0,
+    { },
 } },
 
 { JOB_JESTER, {
     "Jr", "Jester",
     0, 0, 0,
+    { },
 } },
 
 { JOB_PRIEST, {
     "Pr", "Priest",
     0, 0, 0,
+    { },
 } },
 
 { JOB_STALKER, {
     "St", "Stalker",
     0, 0, 0,
+    { },
 } },
 #endif
 };
--- crawl-ref/source/jobs.h
+++ crawl-ref/source/jobs.h
@@ -6,6 +6,7 @@ job_type get_job_by_abbrev(const char *abbrev);
 const char *get_job_name(job_type which_job);
 job_type get_job_by_name(const char *name);
 void job_stat_init(job_type job);
+bool job_recommends_species(job_type job, species_type species);
 
 // Is the job valid for a new game?
 bool is_starting_job(job_type job);
