de8ef5ed7827dbd13d4f6e7105e86e4492da845c
Shmuale Mark <shm.mark@gmail.com>
2015-05-13 14:28:53 -0400

    Move job names to data.

--- crawl-ref/source/jobs.cc
+++ crawl-ref/source/jobs.cc
@@ -13,30 +13,6 @@ static const job_def& _job_def(job_type job)
     return job_data.at(job);
 }
 
-static const char * Job_Name_List[] =
-    { "Fighter", "Wizard",
-#if TAG_MAJOR_VERSION == 34
-      "Priest",
-#endif
-      "Gladiator", "Necromancer",
-      "Assassin", "Berserker", "Hunter", "Conjurer", "Enchanter",
-      "Fire Elementalist", "Ice Elementalist", "Summoner", "Air Elementalist",
-      "Earth Elementalist", "Skald",
-      "Venom Mage",
-      "Chaos Knight", "Transmuter",
-#if TAG_MAJOR_VERSION == 34
-      "Healer", "Stalker",
-#endif
-      "Monk", "Warper", "Wanderer", "Artificer", "Arcane Marksman",
-#if TAG_MAJOR_VERSION == 34
-      "Death Knight",
-#endif
-      "Abyssal Knight",
-#if TAG_MAJOR_VERSION == 34
-      "Jester",
-#endif
-};
-
 const char *get_job_abbrev(job_type which_job)
 {
     if (which_job == JOB_UNKNOWN)
@@ -53,36 +29,34 @@ job_type get_job_by_abbrev(const char *abbrev)
     return JOB_UNKNOWN;
 }
 
-const char *get_job_name(int which_job)
+const char *get_job_name(job_type which_job)
 {
     if (which_job == JOB_UNKNOWN)
         return "Unemployed";
-    COMPILE_CHECK(ARRAYSZ(Job_Name_List) == NUM_JOBS);
-    ASSERT_RANGE(which_job, 0, NUM_JOBS);
 
-    return Job_Name_List[which_job];
+    return _job_def(which_job).name;
 }
 
 job_type get_job_by_name(const char *name)
 {
-    job_type cl = JOB_UNKNOWN;
+    job_type job = JOB_UNKNOWN;
 
-    string low_name = lowercase_string(name);
+    const string low_name = lowercase_string(name);
 
-    for (int i = 0; i < NUM_JOBS; i++)
+    for (auto& entry : job_data)
     {
-        string low_job = lowercase_string(Job_Name_List[i]);
+        string low_job = lowercase_string(entry.second.name);
 
-        size_t pos = low_job.find(low_name);
+        const size_t pos = low_job.find(low_name);
         if (pos != string::npos)
         {
-            cl = static_cast<job_type>(i);
+            job = entry.first;
             if (!pos)  // prefix takes preference
                 break;
         }
     }
 
-    return cl;
+    return job;
 }
 
 // Determines if a job is valid for a new game.
