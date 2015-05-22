1dfc19394fa535fbbc959ba6c379323aad2ff8f7
gammafunk <gammafunk@gmail.com>
2015-05-21 19:28:06 -0500

    Properly count items in shops for Objstat
    A change in 1e85cf02 made unpurchased shop items have a 0 quantity in
    mitm, with the original item stored in shop_struct.stock. This wasn't
    accounted for in objstat's code, so it ommitted shop items in 0.16 and
    0.17. We now look explicitely for valid shops and count the valid items
    in stock.

--- crawl-ref/source/dbg-objstat.h
+++ crawl-ref/source/dbg-objstat.h
@@ -7,7 +7,7 @@
 #define DBGOBJSTAT_H
 
 #ifdef DEBUG_STATISTICS
-void objstat_record_item(item_def &item);
+void objstat_record_item(const item_def &item);
 void objstat_generate_stats();
 void objstat_record_monster(monster *mons);
 void objstat_iteration_stats();
