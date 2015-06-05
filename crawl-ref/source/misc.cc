b0a5d0994f206b36edf8bf9bdd6f69367363ed1f
Neil Moore <neil@s-z.org>
2015-06-05 00:23:13 -0400

    Correct another incorrect ballisto check.
    All non-ballistomycete monsters were being considered "always safe".
    
    <chequers> i managed to get to d:3 pressing only o and >

--- crawl-ref/source/misc.cc
+++ crawl-ref/source/misc.cc
@@ -158,7 +158,7 @@ static bool _mons_is_always_safe(const monster *mon)
     return mon->wont_attack()
         || mon->type == MONS_BUTTERFLY
         || mon->withdrawn()
-        || !mons_is_active_ballisto(mon);
+        || (mon->type == MONS_BALLISTOMYCETE && !mons_is_active_ballisto(mon));
 }
 
 bool mons_is_safe(const monster* mon, const bool want_move,
