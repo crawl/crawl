385ecc4fb8297afc61ea220ad4c71a1c7aa19036
Neil Moore <neil@s-z.org>
2015-06-05 16:04:17 -0400

    Allow walking into invisible neutrals.
    There was not even a message, because simple_monster_message requires
    the monster to be visible.  Rather than revealing the neutrality of
    the invisible monster, just let the attack happen if you couldn't see
    it.
    
    This is inconsistent with strict neutrals and good neutrals, where the
    player will swap with a monster they can't see.  Perhaps we should make
    all non-hostiles always visible instead?

--- crawl-ref/source/main.cc
+++ crawl-ref/source/main.cc
@@ -3310,7 +3310,8 @@ static void _move_player(coord_def move)
                 moving = false;
             }
         }
-        else if (targ_monst->temp_attitude() == ATT_NEUTRAL && !you.confused())
+        else if (targ_monst->temp_attitude() == ATT_NEUTRAL && !you.confused()
+                 && targ_monst->visible_to(&you))
         {
             simple_monster_message(targ_monst, " refuses to make way for you. "
                                              "(Use ctrl+direction to attack.)");
