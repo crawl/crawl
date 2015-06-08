8e67335bad107b226a6a23256261cf6eb1e1f675
Alan Malloy <alan@malloys.org>
2015-06-08 14:11:31 -0700

    Fix description of MR--- mutation
    [committer's note: closes #58 pull request. -nfm]
    
    [skip ci]

--- crawl-ref/source/mutation-data.h
+++ crawl-ref/source/mutation-data.h
@@ -372,7 +372,7 @@ static const mutation_def mut_data[] =
 
   {"You feel less resistant to hostile enchantments.",
    "You feel less resistant to hostile enchantments.",
-   "You feel vulnerable to magic hostile enchantments."},
+   "You feel vulnerable to hostile enchantments."},
 
    "magic resistance",
 },
