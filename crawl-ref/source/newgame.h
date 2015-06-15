1e4c0d9e8974e12ea0a92ad8f1e13785451cde19
Neil Moore <neil@s-z.org>
2015-06-14 22:31:47 -0400

    Don't claim JOB_UNKNOWN is a starting job (#9782)
    Among other things, this caused crashes when selecting "viable
    character".

--- crawl-ref/source/newgame.h
+++ crawl-ref/source/newgame.h
@@ -18,7 +18,6 @@ void choose_tutorial_character(newgame_def& ng_choice);
 bool choose_game(newgame_def& ng, newgame_def& choice,
                  const newgame_def& defaults);
 
-#define JOB_GROUP_SIZE 9
 /*
  * A structure for grouping backgrounds by category.
  */
@@ -27,7 +26,7 @@ struct job_group
     const char* name;   ///< Name of the group.
     coord_def position; ///< Relative coordinates of the title.
     int width;          ///< Column width.
-    job_type jobs[JOB_GROUP_SIZE]; ///< List of jobs in the group.
+    vector<job_type> jobs; ///< List of jobs in the group.
 
     /// A method to attach the group to a freeform.
     void attach(const newgame_def& ng, const newgame_def& defaults,
