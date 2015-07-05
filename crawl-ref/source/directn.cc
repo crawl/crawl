89e6e5aceb42c184894f721fd52e65e130649dd7
Chris Campbell <chriscampbell89@gmail.com>
2015-07-05 18:08:47 +0100

    Fix spelling
    Just in the player-visible instances, currently.

--- crawl-ref/source/directn.cc
+++ crawl-ref/source/directn.cc
@@ -3012,9 +3012,9 @@ string feature_description_at(const coord_def& where, bool covering,
         if (is_bloodcovered(where))
             covering_description = ", spattered with blood";
         else if (glowing_mold(where))
-            covering_description = ", covered with glowing mold";
+            covering_description = ", covered with glowing mould";
         else if (is_moldy(where))
-            covering_description = ", covered with mold";
+            covering_description = ", covered with mould";
     }
 
     // FIXME: remove desc markers completely; only Zin walls are left.
