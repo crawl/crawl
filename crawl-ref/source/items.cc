be3fe0050c055b8f5957e007f25876e7d2a5ace0
Chris Campbell <chriscampbell89@gmail.com>
2015-07-13 21:33:08 +0100

    Generate wizmode wands with their normal max charges
    Since giving them more results in them getting set to 0 charges on game load.

--- crawl-ref/source/items.cc
+++ crawl-ref/source/items.cc
@@ -4374,7 +4374,7 @@ bool get_item_by_name(item_def *item, const char* specs,
         break;
 
     case OBJ_WANDS:
-        item->plus = 24;
+        item->plus = wand_max_charges(item->sub_type);
         break;
 
     case OBJ_RODS:
