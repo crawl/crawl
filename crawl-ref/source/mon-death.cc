91d629c28094dbcf2ec3c8b197cd051754879a6d
Alan Malloy <alan@malloys.org>
2015-07-17 12:51:34 -0700

    Revert some checks back to mons_gives_xp (|amethyst)

--- crawl-ref/source/mon-death.cc
+++ crawl-ref/source/mon-death.cc
@@ -411,7 +411,7 @@ item_def* place_monster_corpse(const monster& mons, bool silent, bool force)
     // Temporary Tukima's Dance weapons stay as weapons (no free gold),
     // permanent dancing weapons turn to gold like other monsters.
     bool goldify = in_good_standing(GOD_GOZAG)
-                   && mons_class_is_threatening(mons.type)
+                   && mons_class_gives_xp(mons.type)
                    && !force;
 
     const bool no_coinflip =
