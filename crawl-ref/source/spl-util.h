774be198defa21987999d6eba107d47075564f65
Neil Moore <neil@s-z.org>
2015-08-01 17:45:28 -0400

    Pull *_LAST_EXPONENT out of their enums.
    Exponents are counts of bits, logically distinct from the things
    represented by those bits, and there is no reason to allow bitwise
    operations between the count and the bits being counted.
    
    It might be better to add this as an optional template parameter of
    enum_bitfield, so things like for-exponent loops can be abstracted
    into templated algorithms.

--- crawl-ref/source/spl-util.h
+++ crawl-ref/source/spl-util.h
@@ -25,10 +25,10 @@ enum spschool_flag_type
   SPTYP_POISON         = 1<<10,
   SPTYP_EARTH          = 1<<11,
   SPTYP_AIR            = 1<<12,
-  SPTYP_LAST_EXPONENT  = 12,
-  SPTYP_LAST_SCHOOL    = 1<<SPTYP_LAST_EXPONENT,
-  SPTYP_RANDOM         = 1<<(SPTYP_LAST_EXPONENT + 1),
+  SPTYP_LAST_SCHOOL    = SPTYP_AIR,
+  SPTYP_RANDOM         = SPTYP_LAST_SCHOOL << 1,
 };
+const int SPTYP_LAST_EXPONENT = 12;
 DEF_BITFIELD(spschools_type, spschool_flag_type);
 
 struct bolt;
