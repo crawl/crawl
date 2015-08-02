6ebb9d71f5ec40ef2268c2ec77ab4aedec86031a
Shmuale Mark <shm.mark@gmail.com>
2015-08-02 16:29:32 -0400

    Local tiles: Fix a missing item name (#9773).
    (cherry picked from commit 77655e490fdb14c3d0f8ab870e31c98ccf7df562)

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -2216,7 +2216,7 @@ void get_item_desc(const item_def &item, describe_info &inf)
     // Don't use verbose descriptions if the item contains spells,
     // so we can actually output these spells if space is scarce.
     const bool verbose = !item.has_spells();
-    inf.body << get_item_description(item, verbose, false, true);
+    inf.body << get_item_description(item, verbose);
 }
 
 // Returns true if spells can be shown to player.
