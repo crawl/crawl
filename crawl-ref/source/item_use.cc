72bfd7178ac58410308e4e309ac3b18b51acf798
olowin <aolowin@gmail.com>
2015-08-03 12:01:07 -0400

    The armour of Talos.

--- crawl-ref/source/item_use.cc
+++ crawl-ref/source/item_use.cc
@@ -611,6 +611,12 @@ bool can_wear_armour(const item_def &item, bool verbose, bool ignore_temporary)
 
     size_type player_size = you.body_size(PSIZE_TORSO, ignore_temporary);
     int bad_size = fit_armour_size(item, player_size);
+    if (is_unrandom_artefact(item, UNRAND_TALOS))
+    {
+        // adjust bad_size for the oversized plate armour
+        // negative means levels too small, positive means levels too large
+        bad_size = SIZE_LARGE - player_size;
+    }
 
     if (bad_size)
     {
