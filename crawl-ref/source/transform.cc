crawl/crawl

commit 7da1d818023c4b1e0a402cd4cca5651c0c5b4f11

Simplify potion-related checks and improve messaging
Refactors the PotionEffect and derived classes so there's a better
interface and no duplicated checks. PotionEffect::can_quaff() can be
called reliably to determine if the player can safely benefit from a
quaff. The ::quaff() method used when drinking calls this, and may
additionally prompt the player in cases where we want give the option to
quaff, but there's direct danger involved (a form change involving
0-stat transition), indirect danger (no appropriate melee weapon for
berserk), or temporarily no benefit (using invisibility while backlit).

Now invisibility pots allow quaffing through a prompt if the player is
temporarily backlit (i.e. not self-haloed) under any condition, as
opposed to only prompting when backlit by high contam. Aside from this
change, the logic of when a player is permitted to quaff or when they'll
receive a prompt is unchanged. Several messages printed when quaffing is
rejected by the interface have been made more specific.

crawl-ref/source/transform.cc
@@ -1524,8 +1524,12 @@ static mutation_type _beastly_appendage()
 }
 
 static bool _transformation_is_safe(transformation_type which_trans,
-                                    dungeon_feature_type feat, bool quiet)
+                                    dungeon_feature_type feat, bool quiet,
+                                    string *fail_reason = nullptr)
 {
+    string msg;
+    bool is_safe = true;
+
     if (which_trans == TRAN_TREE)
     {
         const cloud_type cloud = cloud_type_at(you.pos());
@@ -1535,29 +1539,28 @@ static bool _transformation_is_safe(transformation_type which_trans,
             && cloud != CLOUD_POISON
             && is_damaging_cloud(cloud, false))
         {
-            if (!quiet)
-            {
-                mprf("You can't transform into a tree while standing in a cloud of %s.",
-                     cloud_type_name(cloud).c_str());
-            }
-            return false;
+            msg = make_stringf("You can't transform into a tree while standing "
+                               "in a cloud of %s.",
+                               cloud_type_name(cloud).c_str());
+            is_safe = false;
         }
     }
 #if TAG_MAJOR_VERSION == 34
-
-    if (which_trans == TRAN_ICE_BEAST && you.species == SP_DJINNI)
-        return false; // melting is fatal...
+    else if (which_trans == TRAN_ICE_BEAST && you.species == SP_DJINNI)
+        is_safe = false; // melting is fatal...
 #endif
-
-    if (!feat_dangerous_for_form(which_trans, feat))
-        return true;
-
-    if (!quiet)
+    else if (feat_dangerous_for_form(which_trans, feat))
     {
-        mprf("You would %s in your new form.",
-             feat == DNGN_DEEP_WATER ? "drown" : "burn");
+        msg = make_stringf("You would %s in your new form.",
+                           feat == DNGN_DEEP_WATER ? "drown" : "burn");
+        is_safe = false;
     }
-    return false;
+
+    if (fail_reason)
+        *fail_reason = msg;
+    if (!is_safe && !quiet)
+        mpr(msg);
+    return is_safe;
 }
 
 /**
@@ -1690,10 +1693,12 @@ static bool _player_alive_enough_for(transformation_type which_trans)
  *                          to intervene. (That may be the only case.)
  */
 bool transform(int pow, transformation_type which_trans, bool involuntary,
-               bool just_check)
+               bool just_check, string *fail_reason)
 {
     const transformation_type previous_trans = you.form;
     const bool was_flying = you.airborne();
+    bool success = true;
+    string msg;
 
     // Zin's protection.
     if (!just_check && you_worship(GOD_ZIN)
@@ -1708,14 +1713,22 @@ bool transform(int pow, transformation_type which_trans, bool involuntary,
 
     if (you.transform_uncancellable)
     {
-        if (!involuntary)
-            mpr("You are stuck in your current form!");
-        return false;
+        msg = "You are stuck in your current form!";
+        success = false;
+    }
+    else if (!_transformation_is_safe(which_trans, env.grid(you.pos()),
+                                      involuntary, fail_reason))
+    {
+        success =  false;
     }
 
-    if (!_transformation_is_safe(which_trans, env.grid(you.pos()),
-        involuntary))
+    if (!success)
     {
+        // Message is not printed if we're updating fail_reason.
+        if (fail_reason)
+            *fail_reason = msg;
+        else if (!involuntary)
+            mpr(msg);
         return false;
     }
 
@@ -1757,25 +1770,20 @@ bool transform(int pow, transformation_type which_trans, bool involuntary,
     // the undead cannot enter most forms.
     if (!_player_alive_enough_for(which_trans))
     {
-        if (!involuntary)
-            mpr("Your unliving flesh cannot be transformed in this way.");
-        return false;
+        msg = "Your unliving flesh cannot be transformed in this way.";
+        success = false;
     }
-
-    if (which_trans == TRAN_LICH && you.duration[DUR_DEATHS_DOOR])
+    else if (which_trans == TRAN_LICH && you.duration[DUR_DEATHS_DOOR])
     {
-        if (!involuntary)
-            mpr("You cannot become a lich while in Death's Door.");
-        return false;
+        msg = "You cannot become a lich while in Death's Door.";
+        success = false;
     }
-
 #if TAG_MAJOR_VERSION == 34
-    if (you.species == SP_LAVA_ORC && !temperature_effect(LORC_STONESKIN)
-        && (which_trans == TRAN_ICE_BEAST || which_trans == TRAN_STATUE))
+    else if (you.species == SP_LAVA_ORC && !temperature_effect(LORC_STONESKIN)
+             && (which_trans == TRAN_ICE_BEAST || which_trans == TRAN_STATUE))
     {
-        if (!involuntary)
-            mpr("Your temperature is too high to benefit from that spell.");
-        return false;
+        msg =  "Your temperature is too high to benefit from that spell.";
+        success = false;
     }
 #endif
 
@@ -1789,9 +1797,8 @@ bool transform(int pow, transformation_type which_trans, bool involuntary,
         const mutation_type app = _beastly_appendage();
         if (app == NUM_MUTATIONS)
         {
-            if (!involuntary)
-                mpr("You have no appropriate body parts free.");
-            return false; // XXX: VERY dubious, since an untransform occurred
+            msg = "You have no appropriate body parts free.";
+            success = false; // XXX: VERY dubious, since an untransform occurred
         }
 
         if (!just_check)
@@ -1801,8 +1808,15 @@ bool transform(int pow, transformation_type which_trans, bool involuntary,
         }
     }
 
-    if (!involuntary && just_check && !check_form_stat_safety(which_trans))
+    if (!success)
+    {
+        // Message is not printed if we're updating fail_reason.
+        if (fail_reason)
+            *fail_reason = msg;
+        else if (!involuntary)
+            mpr(msg);
         return false;
+    }
 
     // If we're just pretending return now.
     if (just_check)

crawl-ref/source/actor.h
@@ -372,7 +372,7 @@ public:
     virtual int heat_radius() const = 0;
 #endif
 
-    virtual bool glows_naturally() const = 0;
+    virtual bool glows_naturally() const { return false; };
 
     virtual bool petrifying() const = 0;
     virtual bool petrified() const = 0;

crawl-ref/source/item_use.cc
@@ -1731,11 +1731,8 @@ void drink(int slot)
     if (player_under_penance(GOD_GOZAG) && one_chance_in(3))
     {
         zin_recite_interrupt();
-
         simple_god_message(" petitions for your drink to fail.", GOD_GOZAG);
-
         you.turn_is_over = true;
-
         return;
     }
 
@@ -1743,26 +1740,22 @@ void drink(int slot)
         return;
 
     zin_recite_interrupt();
-
     if (!alreadyknown && dangerous)
     {
         // Xom loves it when you drink an unknown potion and there is
         // a dangerous monster nearby...
         xom_is_stimulated(200);
     }
-
     if (is_blood_potion(potion))
     {
         // Always drink oldest potion.
         remove_oldest_perishable_item(potion);
     }
-
     dec_inv_item_quantity(slot, 1);
     count_action(CACT_USE, OBJ_POTIONS);
     auto_assign_item_slot(potion);
     you.turn_is_over = true;
-
-    // This got deferred from the it_use2 switch to prevent SIGHUP abuse.
+    // This got deferred from PotionExperience::effect to prevent SIGHUP abuse.
     if (pot_type == POT_EXPERIENCE)
         level_change();
 }
@@ -1948,7 +1941,6 @@ static item_def* _scroll_choose_weapon(bool alreadyknown, const string &pre_msg,
                                                 : "Enchant which weapon?",
                                        MT_INVLIST, selector,
                                        true, true, false);
-
         // The scroll is used up if we didn't know what it was originally.
         if (item_slot == PROMPT_NOTHING)
             return nullptr;
@@ -1967,19 +1959,16 @@ static item_def* _scroll_choose_weapon(bool alreadyknown, const string &pre_msg,
         }
 
         item_def* wpn = &you.inv[item_slot];
-
         if (!is_item_selected(*wpn, selector))
         {
             mpr("Choose a valid weapon, or Esc to abort.");
             more();
-
             continue;
         }
 
         // Now we're definitely using up the scroll.
         if (alreadyknown)
             mpr(pre_msg);
-
         return wpn;
     }
 }
@@ -1987,8 +1976,8 @@ static item_def* _scroll_choose_weapon(bool alreadyknown, const string &pre_msg,
 // Returns true if the scroll is used up.
 static bool _handle_brand_weapon(bool alreadyknown, const string &pre_msg)
 {
-    item_def* weapon = _scroll_choose_weapon(alreadyknown, pre_msg, SCR_BRAND_WEAPON);
-
+    item_def* weapon = _scroll_choose_weapon(alreadyknown, pre_msg,
+                                             SCR_BRAND_WEAPON);
     if (!weapon)
         return !alreadyknown;
 
@@ -2068,7 +2057,6 @@ static bool _identify(bool alreadyknown, const string &pre_msg)
         }
 
         item_def& item(you.inv[item_slot]);
-
         if (fully_identified(item)
             && (!is_deck(item) || top_card_is_known(item)))
         {

crawl-ref/source/player-act.cc
@@ -820,94 +820,49 @@ bool player::can_go_berserk() const
     return can_go_berserk(false);
 }
 
-bool player::can_go_berserk(bool intentional, bool potion, bool quiet) const
+bool player::can_go_berserk(bool intentional, bool potion, bool quiet,
+                            string *reason) const
 {
+    COMPILE_CHECK(HUNGER_STARVING - 100 + BERSERK_NUTRITION < HUNGER_VERY_HUNGRY);
     const bool verbose = (intentional || potion) && !quiet;
+    string msg;
+    bool success = false;
 
     if (berserk())
-    {
-        if (verbose)
-            mpr("You're already berserk!");
-        // or else you won't notice -- no message here.
-        return false;
-    }
-
-    if (duration[DUR_EXHAUSTED])
-    {
-        if (verbose)
-            mpr("You're too exhausted to go berserk.");
-        // or else they won't notice -- no message here
-        return false;
-    }
-
-    if (duration[DUR_DEATHS_DOOR])
-    {
-        if (verbose)
-            mpr("Your body is effectively dead; that's not a shape for a blood rage.");
-        return false;
-    }
-
-    if (beheld() && !player_equip_unrand(UNRAND_DEMON_AXE))
-    {
-        if (verbose)
-            mpr("You are too mesmerised to rage.");
-        // or else they won't notice -- no message here
-        return false;
-    }
-
-    if (afraid())
-    {
-        if (verbose)
-            mpr("You are too terrified to rage.");
-
-        return false;
-    }
-
+        msg = "You're already berserk!";
+    else if (duration[DUR_EXHAUSTED])
+         msg = "You're too exhausted to go berserk.";
+    else if (duration[DUR_DEATHS_DOOR])
+        msg = "Your body is effectively dead and in no shape for a blood rage.";
+    else if (beheld() && !player_equip_unrand(UNRAND_DEMON_AXE))
+        msg = "You are too mesmerised to rage.";
+    else if (afraid())
+        msg = "You are too terrified to rage.";
 #if TAG_MAJOR_VERSION == 34
-    if (you.species == SP_DJINNI)
-    {
-        if (verbose)
-            mpr("Only creatures of flesh and blood can berserk.");
-
-        return false;
-    }
-
+    else if (you.species == SP_DJINNI)
+        msg = "Only creatures of flesh and blood can berserk.";
 #endif
-    if (is_lifeless_undead())
+    else if (is_lifeless_undead())
+        msg = "You cannot raise a blood rage in your lifeless body.";
+    // Stasis for identified amulets; unided amulets will trigger when the
+    // player attempts to activate berserk.
+    else if (stasis(false))
+        msg = "You cannot go berserk while under stasis.";
+    else if (!intentional && !potion && clarity())
+        msg = "You're too calm and focused to rage.";
+    else if (hunger <= HUNGER_VERY_HUNGRY)
+        msg = "You're too hungry to go berserk.";
+    else
+        success = true;
+
+    if (!success)
     {
         if (verbose)
-            mpr("You cannot raise a blood rage in your lifeless body.");
-
-        return false;
+            mpr(msg);
+        if (reason)
+            *reason = msg;
     }
-
-    // Stasis, but only for identified amulets; unided amulets will
-    // trigger when the player attempts to activate berserk,
-    // auto-iding at that point, but also killing the berserk and
-    // wasting a turn.
-    if (stasis(false))
-    {
-        if (verbose)
-            mpr("You cannot go berserk while under stasis.");
-        return false;
-    }
-
-    if (!intentional && !potion && clarity())
-    {
-        if (verbose)
-            mpr("You're too calm and focused to rage.");
-        return false;
-    }
-
-    COMPILE_CHECK(HUNGER_STARVING - 100 + BERSERK_NUTRITION < HUNGER_VERY_HUNGRY);
-    if (hunger <= HUNGER_VERY_HUNGRY)
-    {
-        if (verbose)
-            mpr("You're too hungry to go berserk.");
-        return false;
-    }
-
-    return true;
+    return success;
 }
 
 bool player::berserk() const

crawl-ref/source/player.cc
@@ -826,10 +826,9 @@ bool berserk_check_wielded_weapon()
 {
     const item_def * const wpn = you.weapon();
     bool penance = false;
-    if (wpn && wpn->defined() && (!is_melee_weapon(*wpn)
-                                   || needs_handle_warning(*wpn,
-                                                           OPER_ATTACK,
-                                                           penance)))
+    if (wpn && wpn->defined()
+        && (!is_melee_weapon(*wpn)
+            || needs_handle_warning(*wpn, OPER_ATTACK, penance)))
     {
         string prompt = "Do you really want to go berserk while wielding "
                         + wpn->name(DESC_YOUR) + "?";
@@ -4905,56 +4904,72 @@ void dec_ambrosia_player(int delay)
         mpr("You feel less invigorated.");
 }
 
-bool invis_allowed(bool quiet)
+bool prompt_contam_invis()
 {
-    if (!you.backlit())
-        return true;
-
-    if (you.haloed() || you.glows_naturally())
-    {
-        if (!quiet)
-            mpr("You can't turn invisible.");
-        return false;
-    }
-    else if (!quiet
-             && get_contamination_level() > 1
-             && !yesno("Invisibility will do you no good right now; "
-                       "use anyway?", false, 'n'))
-    {
-        canned_msg(MSG_OK);
-        return false;
-    }
-
     return true;
 }
 
-bool flight_allowed(bool quiet)
+bool invis_allowed(bool quiet, string *fail_reason)
 {
+    string msg;
+    bool success = true;
+
+    if (you.haloed() && you.halo_radius() != -1)
+    {
+        msg = "Your halo prevents invisibility.";
+        success = false;
+    }
+    else if (you.backlit(false))
+    {
+        msg = "Invisibility will do you no good right now";
+        if (!quiet && !yesno((msg + "; use anyway?").c_str(), false, 'n'))
+        {
+            canned_msg(MSG_OK);
+            success = false;
+        }
+        msg += ".";
+    }
+
+    if (!success)
+    {
+        if (fail_reason)
+            *fail_reason = msg;
+        if (!quiet)
+            mpr(msg);
+    }
+    return success;
+}
+
+bool flight_allowed(bool quiet, string *fail_reason)
+{
+    string msg;
+    bool success = true;
+
     if (get_form()->forbids_flight())
     {
-        if (!quiet)
-        {
-            mpr(you.form == TRAN_TREE ? "Your roots keep you in place."
-                                      : "You can't fly in this form.");
-        }
-        return false;
+        msg = you.form == TRAN_TREE ? "Your roots keep you in place."
+            : "You can't fly in this form.";
+        success = false;
     }
-
-    if (you.liquefied_ground())
+    else if (you.liquefied_ground())
     {
-        if (!quiet)
-            mpr("You can't fly while stuck in liquid ground.");
-        return false;
+        msg = "You can't fly while stuck in liquid ground.";
+        success = false;
     }
-
-    if (you.duration[DUR_GRASPING_ROOTS])
+    else if (you.duration[DUR_GRASPING_ROOTS])
     {
-        if (!quiet)
-            mpr("The grasping roots prevent you from becoming airborne.");
-        return false;
+        msg = "The grasping roots prevent you from becoming airborne.";
+        success = false;
     }
 
-    return true;
+    if (!success)
+    {
+        if (fail_reason)
+            *fail_reason = msg;
+        if (!quiet)
+            mpr(msg);
+    }
+    return success;
 }
 
 void float_player()
@@ -7130,15 +7145,19 @@ bool player::visible_to(const actor *looker) const
             && (!invisible() || mon->can_see_invisible()));
 }
 
+/**
+ * Is the player backlit?
+ *
+ * @param self_halo If true, ignore the player's self-halo.
+ * @returns True if the player is backlit.
+*/
 bool player::backlit(bool self_halo) const
 {
-    if (get_contamination_level() > 1 || duration[DUR_CORONA]
-        || duration[DUR_LIQUID_FLAMES] || duration[DUR_QUAD_DAMAGE])
-    {
-        return true;
-    }
-
-    return !umbraed() && haloed() && (self_halo || halo_radius() == -1);
+    return get_contamination_level() > 1
+        || duration[DUR_CORONA]
+        || duration[DUR_LIQUID_FLAMES]
+        || duration[DUR_QUAD_DAMAGE]
+        || !umbraed() && haloed() && (self_halo || halo_radius() == -1);
 }
 
 bool player::umbra() const
@@ -7146,27 +7165,20 @@ bool player::umbra() const
     return !backlit() && umbraed() && !haloed();
 }
 
-bool player::glows_naturally() const
-{
-    return false;
-}
-
 // This is the imperative version.
 void player::backlight()
 {
     if (!duration[DUR_INVIS] && form != TRAN_SHADOW)
     {
-        if (duration[DUR_CORONA] || glows_naturally())
+        if (duration[DUR_CORONA])
             mpr("You glow brighter.");
         else
             mpr("You are outlined in light.");
-
         increase_duration(DUR_CORONA, random_range(15, 35), 250);
     }
     else
     {
         mpr("You feel strangely conspicuous.");
-
         increase_duration(DUR_CORONA, random_range(3, 5), 250);
     }
 }

crawl-ref/source/player.h
@@ -605,7 +605,7 @@ public:
     void attacking(actor *other, bool ranged = false);
     bool can_go_berserk() const;
     bool can_go_berserk(bool intentional, bool potion = false,
-                        bool quiet = false) const;
+                        bool quiet = false, string *reason = nullptr) const;
     bool go_berserk(bool intentional, bool potion = false);
     bool berserk() const;
     bool can_mutate() const;
@@ -715,7 +715,6 @@ public:
 #if TAG_MAJOR_VERSION == 34
     int heat_radius() const;
 #endif
-    bool glows_naturally() const;
     bool petrifying() const;
     bool petrified() const;
     bool liquefied_ground() const;
@@ -1043,8 +1042,9 @@ bool haste_player(int turns, bool rageext = false);
 void dec_haste_player(int delay);
 void dec_elixir_player(int delay);
 void dec_ambrosia_player(int delay);
-bool invis_allowed(bool quiet = false);
-bool flight_allowed(bool quiet = false);
+bool prompt_contam_invis();
+bool invis_allowed(bool quiet = false, string *fail_reason = nullptr);
+bool flight_allowed(bool quiet = false, string *fail_reason = nullptr);
 void fly_player(int pow, bool already_flying = false);
 void float_player();
 bool land_player(bool quiet = false);

crawl-ref/source/potion.h
@@ -13,8 +13,9 @@ private:
     DISALLOW_COPY_AND_ASSIGN(PotionEffect);
 protected:
     PotionEffect(potion_type);
+    bool check_known_quaff() const;
 public:
-    virtual bool can_quaff() const;
+    virtual bool can_quaff(string *reason = nullptr) const;
 
     /**
      * Elsewhere in the code there are things that can have the effect

commit 732e340ec7726b57633813a4d2327aef0560cb66

Properly check that quaffing lignification is safe
The change in 7da1d81 introduced a bug where the habitability was
skipped.

crawl-ref/source/transform.cc
@@ -1545,8 +1545,9 @@ static bool _transformation_is_safe(transformation_type which_trans,
             is_safe = false;
         }
     }
+
 #if TAG_MAJOR_VERSION == 34
-    else if (which_trans == TRAN_ICE_BEAST && you.species == SP_DJINNI)
+    if (which_trans == TRAN_ICE_BEAST && you.species == SP_DJINNI)
         is_safe = false; // melting is fatal...
 #endif
     else if (feat_dangerous_for_form(which_trans, feat))