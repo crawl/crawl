30fb260758fc8ebb82ddd32e60bc07204a7fb179
Shmuale Mark <shm.mark@gmail.com>
2015-08-31 13:25:47 -0400

    Refactor evolution code (gammafunk, |amethyst).

--- crawl-ref/source/godabil.h
+++ crawl-ref/source/godabil.h
@@ -113,8 +113,8 @@ int count_corpses_in_los(vector<stack_iterator> *positions);
 int fedhas_check_corpse_spores(bool quiet = false);
 int fedhas_corpse_spores(beh_type attitude = BEH_FRIENDLY);
 bool mons_is_evolvable(const monster* mon);
-bool fedhas_check_evolve_flora(bool quiet = false);
-void fedhas_evolve_flora();
+bool fedhas_check_evolve_flora(bool quiet);
+spret_type fedhas_evolve_flora(bool fail);
 
 void lugonu_bend_space();
 
