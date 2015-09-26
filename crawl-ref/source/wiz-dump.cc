fa89ac4d35260f3cac090095518ae7e0f1c12ac0
Neil Moore <neil@s-z.org>
2015-09-26 11:29:13 -0400

    Simplify and guard wiz-dump.h
    Move chardump_parser into the .cc file, since it is not used elsewhere.
    Protect the single remaining declaration (of wizard_load_dump_file) with
    an #ifdef WIZARD, so that trying to use it in a non-wizard build gives
    errors at compile time rather than link time.

--- crawl-ref/source/wiz-dump.cc
+++ crawl-ref/source/wiz-dump.cc
@@ -28,6 +28,32 @@
 #include "wiz-you.h"
 
 #ifdef WIZARD
+
+class chardump_parser
+{
+public:
+    chardump_parser(const string &f)
+        : filename(f), in_equipment(false), seen_skills(false) { }
+
+    bool parse();
+
+private:
+    bool _parse_from_file(const string &full_filename);
+
+    void _modify_character(const string &line);
+
+    bool _check_skill(const vector<string> &tokens);
+    bool _check_stats1(const vector<string> &tokens);
+    bool _check_stats2(const vector<string> &tokens);
+    bool _check_stats3(const vector<string> &tokens);
+    bool _check_char(const vector<string> &tokens);
+    bool _check_equipment(const vector<string> &tokens);
+
+    string filename;
+    bool in_equipment;
+    bool seen_skills;
+};
+
 static uint8_t _jewellery_type_from_artefact_prop(const string &s
 #if TAG_MAJOR_VERSION == 34
                                                   , bool is_amulet
