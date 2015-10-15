bb88bff1dd21ae825c1124f124f4e62c7a858788
olowin <aolowin@gmail.com>
2015-10-15 15:44:44 -0400

    Correctly account for tracers in action counts.

--- crawl-ref/source/actor.cc
+++ crawl-ref/source/actor.cc
@@ -333,7 +333,7 @@ int actor::spirit_shield(bool calc_unid, bool items) const
 }
 
 int actor::apply_ac(int damage, int max_damage, ac_type ac_rule,
-                    int stab_bypass) const
+                    int stab_bypass, bool for_real) const
 {
     int ac = max(armour_class() - stab_bypass, 0);
     int gdr = gdr_perc();
@@ -368,7 +368,7 @@ int actor::apply_ac(int damage, int max_damage, ac_type ac_rule,
     }
 
     saved = max(saved, min(gdr * max_damage / 100, ac / 2));
-    if ((saved >= damage) && is_player())
+    if (for_real && (damage > 0) && (saved >= damage) && is_player())
     {
         const item_def *body_armour = you.slot_item(EQ_BODY_ARMOUR);
         if (body_armour)
