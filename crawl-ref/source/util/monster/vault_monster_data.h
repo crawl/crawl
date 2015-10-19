c09e64592787b71d56397567c7c63b17c431c3cf
Shmuale Mark <shm.mark@gmail.com>
2015-10-19 12:35:02 -0400

    Add the contents of the monster repo to util/monster/.
    Up to date as of c9b677d.

---
+++ crawl-ref/source/util/monster/vault_monster_data.h
@@ -0,0 +1,12 @@
+/**
+ * @file vault_monster_data.h
+**/
+
+#ifndef __VAULT_MONSTER_DATA_H__
+#define __VAULT_MONSTER_DATA_H__
+
+#include "AppHdr.h"
+
+std::vector<std::string> get_vault_monsters ();
+
+#endif
