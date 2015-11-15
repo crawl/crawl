378fbfa8a7baca4cfefd349731c447df8330c267
Shmuale Mark <shm.mark@gmail.com>
2015-11-14 21:24:07 -0500

    oops (Grunt)

--- crawl-ref/source/invent.cc
+++ crawl-ref/source/invent.cc
@@ -242,7 +242,7 @@ void get_class_hotkeys(const int type, vector<char> &glyphs)
         glyphs.push_back('%');
         break;
     case OBJ_BOOKS:
-        glyphs.push_back('+');
+        glyphs.push_back(':');
         break;
     case OBJ_SCROLLS:
         glyphs.push_back('?');
