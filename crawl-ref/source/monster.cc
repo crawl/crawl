crawl/crawl

commit d63ab3c59aa384f4d63c7ca8d95804740299a244

Simplify monster intelligence
Squash I_REPTILE into I_ANIMAL, I_INSECT and I_PLANT into
I_BRAINLESS, and I_HIGH and I_NORMAL into I_HUMAN.

Effects:
- Some difference in tracking ranges/pathfinding/forgetfulness.
- Formerly high-intelligence monsters no longer have an
  extra-high chance to guess invisible players' locations or
  better MR estimation abilities, and no longer have shaft &
  drowning-while-confused immunity.
- Monsters that miscast noisily will no longer sometimes think
  someone else made the noise.
- If Kirke porkalates something, and then you kill Kirke, whether
  or not that monster becomes friendly no longer depends on it
  having >= human intelligence.

crawl-ref/source/monster.cc
@@ -699,7 +699,7 @@ bool monster::can_speak()
         return true;
 
     // Silent or non-sentient monsters can't use the original speech.
-    if (mons_intel(this) < I_NORMAL
+    if (mons_intel(this) < I_HUMAN
         || mons_shouts(type) == S_SILENT)
     {
         return false;
@@ -4355,7 +4355,7 @@ bool monster::poison(actor *agent, int amount, bool force)
 int monster::skill(skill_type sk, int scale, bool real, bool drained) const
 {
     // Let spectral weapons have necromancy skill for pain brand.
-    if (mons_intel(this) < I_NORMAL && !mons_is_avatar(type))
+    if (mons_intel(this) < I_HUMAN && !mons_is_avatar(type))
         return 0;
 
     const int hd = scale * get_hit_dice();
@@ -4829,7 +4829,7 @@ static int _estimated_trap_damage(trap_type trap)
  */
 bool monster::is_trap_safe(const coord_def& where, bool just_check) const
 {
-    const int intel = mons_intel(this);
+    const mon_intel_type intel = mons_intel(this);
 
     const trap_def *ptrap = find_trap(where);
     if (!ptrap)
@@ -4861,7 +4861,7 @@ bool monster::is_trap_safe(const coord_def& where, bool just_check) const
     }
 
     // Dumb monsters don't care at all.
-    if (intel == I_PLANT)
+    if (intel == I_BRAINLESS)
         return true;
 
     // Hostile monsters are not afraid of non-mechanical traps.
@@ -4915,7 +4915,7 @@ bool monster::is_trap_safe(const coord_def& where, bool just_check) const
 
     // Healthy monsters don't mind a little pain.
     if (mechanical && hit_points >= max_hit_points / 2
-        && (intel == I_ANIMAL
+        && (intel < I_HUMAN
             || hit_points > _estimated_trap_damage(trap.type)))
     {
         return true;
@@ -5291,7 +5291,7 @@ bool monster::can_go_frenzy() const
     if (mons_is_tentacle_or_tentacle_segment(type))
         return false;
 
-    if (mons_intel(this) == I_PLANT)
+    if (mons_intel(this) == I_BRAINLESS)
         return false;
 
     if (paralysed() || petrified() || petrifying() || asleep())

crawl-ref/source/beam.cc
@@ -5133,7 +5133,7 @@ bool ench_flavour_affects_monster(beam_type flavour, const monster* mon,
         rc = mon->holiness() == MH_NATURAL
              && mon->attitude != ATT_FRIENDLY
              && mons_can_be_zombified(mon)
-             && mons_intel(mon) >= I_NORMAL;
+             && mons_intel(mon) >= I_HUMAN;
         break;
 
     case BEAM_DISPEL_UNDEAD:

crawl-ref/source/butcher.cc
@@ -77,7 +77,7 @@ void finish_butchering(item_def& corpse, bool bottling)
     ASSERT(corpse.base_type == OBJ_CORPSES);
     ASSERT(corpse.sub_type == CORPSE_BODY);
     const bool was_holy = mons_class_holiness(corpse.mon_type) == MH_HOLY;
-    const bool was_intelligent = corpse_intelligence(corpse) >= I_NORMAL;
+    const bool was_intelligent = corpse_intelligence(corpse) >= I_HUMAN;
     const bool was_same_genus = is_player_same_genus(corpse.mon_type);
 
     if (bottling)

crawl-ref/source/cloud.cc
@@ -1499,7 +1499,7 @@ static bool _mons_avoids_cloud(const monster* mons, const cloud_struct& cloud,
 
         // This position could become deep water, and they might drown.
         if (grd(cloud.pos) == DNGN_SHALLOW_WATER
-            && mons_intel(mons) > I_PLANT)
+            && mons_intel(mons) > I_BRAINLESS)
         {
             return true;
         }
@@ -1527,7 +1527,7 @@ static bool _mons_avoids_cloud(const monster* mons, const cloud_struct& cloud,
     }
 
     // Exceedingly dumb creatures will wander into harmful clouds.
-    if (mons_intel(mons) == I_PLANT && !extra_careful)
+    if (mons_intel(mons) == I_BRAINLESS && !extra_careful)
         return false;
 
     // If we get here, the cloud is potentially harmful.

crawl-ref/source/dactions.cc
@@ -455,11 +455,7 @@ static void _daction_hog_to_human(monster *mon, bool in_transit)
         // * A monster's attitude shouldn't downgrade from friendly
         //   or good-neutral because you helped it. It'd suck to
         //   lose a permanent ally that way.
-        //
-        // * A monster has to be smart enough to realize that you
-        //   helped it.
-        if (mon->attitude == ATT_HOSTILE
-            && mons_intel(mon) >= I_NORMAL)
+        if (mon->attitude == ATT_HOSTILE)
         {
             mon->attitude = ATT_GOOD_NEUTRAL;
             mon->flags   |= MF_WAS_NEUTRAL;

crawl-ref/source/describe.cc
@@ -3888,15 +3888,10 @@ void get_monster_db_desc(const monster_info& mi, describe_info &inf,
         stair_use = true;
     }
 
-    if (mi.intel() <= I_PLANT)
+    if (mi.intel() <= I_BRAINLESS)
     {
         inf.body << uppercase_first(mi.pronoun(PRONOUN_SUBJECTIVE))
                  << " is mindless.\n";
-    }
-    else if (mi.intel() <= I_INSECT && you_worship(GOD_ELYVILON))
-    {
-        inf.body << uppercase_first(mi.pronoun(PRONOUN_SUBJECTIVE))
-                 << " is not intelligent enough to pacify.\n";
     }
 
 

crawl-ref/source/directn.cc
@@ -3328,7 +3328,7 @@ static string _get_monster_desc(const monster_info& mi)
     if (mi.is(MB_UMBRAED))
         text += pronoun + " is wreathed by an umbra.\n";
 
-    if (mi.intel() <= I_INSECT)
+    if (mi.intel() <= I_BRAINLESS)
         text += pronoun + " is mindless.\n";
 
     if (mi.is(MB_CHAOTIC))

crawl-ref/source/food.cc
@@ -1288,7 +1288,7 @@ bool is_forbidden_food(const item_def &food)
 
     // Specific handling for intelligent monsters like Gastronok and Xtahua
     // of a normally unintelligent class.
-    if (you_worship(GOD_ZIN) && corpse_intelligence(food) >= I_NORMAL)
+    if (you_worship(GOD_ZIN) && corpse_intelligence(food) >= I_HUMAN)
         return true;
 
     return god_hates_eating(you.religion, food.mon_type);

commit 02a7081604cefaadeaea8675edc0d7aec63da316

Allow insects to go berserk again.
This was broken by d63ab3c5. This fix was suggested by wheals, who also
suggested replacing this check by mons_is_object in trunk. Note that insects
being able to go berserk is quite important both with queen bees berserking
other bees and with moths of wrath in Spider.

crawl-ref/source/monster.cc
@@ -5209,7 +5209,10 @@ bool monster::can_go_frenzy() const
     if (mons_is_tentacle_or_tentacle_segment(type))
         return false;
 
-    if (mons_intel(this) == I_BRAINLESS)
+    // Insects are I_BRAINLESS and we want them to be able
+    // to berserk, so also check MH_NATURAL.
+    // This could maybe all be replaced by mons_is_object()?
+    if (mons_intel(this) == I_BRAINLESS && holiness() != MH_NATURAL)
         return false;
 
     if (paralysed() || petrified() || petrifying() || asleep())