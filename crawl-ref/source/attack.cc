253f78e25f9fa3dbaf687c7123b02abe9896a2ef
Alex Jurkiewicz <alex@jurkiewi.cz>
2015-11-24 23:00:00 +1100

    Remove blink frog healing from distortion attacks.

--- crawl-ref/source/attack.cc
+++ crawl-ref/source/attack.cc
@@ -444,27 +444,6 @@ void attack::alert_defender()
 
 bool attack::distortion_affects_defender()
 {
-    //jmf: blink frogs *like* distortion
-    // I think could be amended to let blink frogs "grow" like
-    // jellies do {dlb}
-    if (defender->is_monster()
-        && mons_genus(defender->type) == MONS_BLINK_FROG)
-    {
-        if (one_chance_in(5))
-        {
-            if (defender_visible)
-            {
-                special_damage_message =
-                    make_stringf("%s %s in the distortional energy.",
-                                 defender_name(false).c_str(),
-                                 defender->conj_verb("bask").c_str());
-            }
-
-            defender->heal(1 + random2avg(7, 2), true); // heh heh
-        }
-        return false;
-    }
-
     if (one_chance_in(3))
     {
         special_damage_message =
