50164069df6cbac3d0605377b3748b75721557c4
Jesse Luehrs <doy@tozt.net>
2015-11-28 23:20:32 -0500

    add a method to return what part of a string matched

--- crawl-ref/source/clua.cc
+++ crawl-ref/source/clua.cc
@@ -941,6 +941,17 @@ bool lua_text_pattern::matches(const string &s) const
     return clua.callbooleanfn(false, lua_fn_name.c_str(), "s", s.c_str());
 }
 
+pattern_match lua_text_pattern::match_location(const string &s) const
+{
+    // lua_text_pattern is only used if a special non-regex op is detected (^F
+    // for "armour && ego", for instance), and in those situations, it's
+    // unclear what exactly to use for the matched text here (especially in
+    // more complicated expressions that include things like <<>>, !!, etc).
+    return matches(s)
+        ? pattern_match::succeeded(s)
+        : pattern_match::failed(s);
+}
+
 void lua_text_pattern::pre_pattern(string &pat, string &fn) const
 {
     // Trim trailing spaces
