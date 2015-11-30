4d2330c39a808faf71f4ccc10e38ec7936a269f2
Jesse Luehrs <doy@tozt.net>
2015-11-30 03:12:53 -0500

    make the search highlight colour configurable

--- crawl-ref/source/options.h
+++ crawl-ref/source/options.h
@@ -414,7 +414,10 @@ public:
     int         rest_wait_percent; // Stop resting after restoring this
                                    // fraction of HP or MP
 
-    bool        regex_search; // whether to default to regex search for ^F
+    bool        regex_search;            // whether to default to regex search
+                                         // for ^F
+    int         search_highlight_colour; // colour to use when highlighting
+                                         // search results
 
     lang_t              language;         // Translation to use.
     const char*         lang_name;        // Database name of the language.
