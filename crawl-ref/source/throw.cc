d819f22e81df629f7b6e077605e35b905c102f7b
Shmuale Mark <shm.mark@gmail.com>
2015-12-01 14:26:56 -0500

    Display if a monster is immune when targetting nets.
    Closes #189 pull request.

--- crawl-ref/source/throw.cc
+++ crawl-ref/source/throw.cc
@@ -240,6 +240,13 @@ vector<string> fire_target_behaviour::get_monster_desc(const monster_info& mi)
     {
         if (get_ammo_brand(*item) == SPMSL_SILVER && mi.is(MB_CHAOTIC))
             descs.emplace_back("chaotic");
+        if (item->is_type(OBJ_MISSILES, MI_THROWING_NET)
+            && (mi.body_size() >= SIZE_GIANT
+                || mons_class_is_stationary(mi.type)
+                || mons_class_flag(mi.type, M_INSUBSTANTIAL)))
+        {
+            descs.emplace_back("immune to nets");
+        }
     }
     return descs;
 }
