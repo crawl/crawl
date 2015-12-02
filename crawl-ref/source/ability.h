3ae9bda4b7b0a9c49687e8d81498f04cb3b31bce
Shmuale Mark <shm.mark@gmail.com>
2015-12-02 11:06:20 -0500

    Move *_cost out of the header.

--- crawl-ref/source/ability.h
+++ crawl-ref/source/ability.h
@@ -11,48 +11,6 @@
 
 #include "enum.h"
 
-struct generic_cost
-{
-    int base, add, rolls;
-
-    generic_cost(int num)
-        : base(num), add(num == 0 ? 0 : (num + 1) / 2 + 1), rolls(1)
-    {
-    }
-    generic_cost(int num, int _add, int _rolls = 1)
-        : base(num), add(_add), rolls(_rolls)
-    {
-    }
-    static generic_cost fixed(int fixed)
-    {
-        return generic_cost(fixed, 0, 1);
-    }
-    static generic_cost range(int low, int high, int _rolls = 1)
-    {
-        return generic_cost(low, high - low + 1, _rolls);
-    }
-
-    int cost() const PURE;
-
-    operator bool () const { return base > 0 || add > 0; }
-};
-
-struct scaling_cost
-{
-    int value;
-
-    scaling_cost(int permille) : value(permille) {}
-
-    static scaling_cost fixed(int fixed)
-    {
-        return scaling_cost(-fixed);
-    }
-
-    int cost(int max) const;
-
-    operator bool () const { return value != 0; }
-};
-
 struct talent
 {
     ability_type which;
