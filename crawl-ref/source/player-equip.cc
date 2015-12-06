crawl/crawl

commit f50acb22cd3cdad6f136844c2f8d15def2d5a1cd

Add a new artefact property: Fragile
Fragile artefacts are permanently destroyed when unequipped.
Something of a different take on the Contam/Drain mechanic; the
only penalty is the loss of the item, but the item is *gone*.
Harmless for bad artefacts, but could result in fun choices for
an otherwise-strong ring or weapon.

crawl-ref/source/player-equip.cc
@@ -351,6 +351,13 @@ static void _unequip_artefact_effect(item_def &item,
         if (entry->world_reacts_func)
             you.unrand_reacts.set(slot, false);
     }
+
+    // this must be last!
+    if (proprt[ARTP_FRAGILE] && !meld)
+    {
+        mprf("%s crumbles to dust!", item.name(DESC_THE).c_str());
+        dec_inv_item_quantity(item.link, 1);
+    }
 }
 
 static void _equip_use_warning(const item_def& item)
@@ -625,15 +632,19 @@ static void _equip_weapon_effect(item_def& item, bool showMsgs, bool unmeld)
     }
 }
 
-static void _unequip_weapon_effect(item_def& item, bool showMsgs, bool meld)
+static void _unequip_weapon_effect(item_def& real_item, bool showMsgs,
+                                   bool meld)
 {
     you.wield_change = true;
     you.m_quiver.on_weapon_changed();
 
+    // Fragile artefacts may be destroyed, so make a copy
+    item_def item = real_item;
+
     // Call this first, so that the unrandart func can set showMsgs to
     // false if it does its own message handling.
     if (is_artefact(item))
-        _unequip_artefact_effect(item, &showMsgs, meld, EQ_WEAPON);
+        _unequip_artefact_effect(real_item, &showMsgs, meld, EQ_WEAPON);
 
     if (item.is_type(OBJ_MISCELLANY, MISC_LANTERN_OF_SHADOWS))
     {

crawl-ref/source/artefact.cc
@@ -640,6 +640,9 @@ static bool _artp_can_go_on_item(artefact_prop_type prop, const item_def &item,
                    || get_weapon_brand(item) != SPWPN_ANTIMAGIC;
         case ARTP_CONTAM:
             return !item.is_type(OBJ_JEWELLERY, AMU_DISMISSAL);
+            // not quite as interesting on armour, since you swap it less
+        case ARTP_FRAGILE:
+            return item_class != OBJ_ARMOUR;
         default:
             return true;
     }
@@ -771,6 +774,8 @@ static const artefact_prop_data artp_data[] =
     { "Drain", ARTP_VAL_BOOL, 25, // ARTP_DRAIN,
         nullptr, []() { return 1; }, 0, 0 },
     { "*Confuse", ARTP_VAL_BOOL, 25, // ARTP_CONFUSE,
+        nullptr, []() { return 1; }, 0, 0 },
+    { "Fragile", ARTP_VAL_BOOL, 25, // ARTP_FRAGILE,
         nullptr, []() { return 1; }, 0, 0 },
 };
 COMPILE_CHECK(ARRAYSZ(artp_data) == ARTP_NUM_PROPERTIES);

crawl-ref/source/describe.cc
@@ -187,6 +187,7 @@ static vector<string> _randart_propnames(const item_def& item,
         { ARTP_CORRODE,               PROPN_PLAIN },
         { ARTP_DRAIN,                 PROPN_PLAIN },
         { ARTP_CONFUSE,               PROPN_PLAIN },
+        { ARTP_FRAGILE,               PROPN_PLAIN },
 
         // Evokable abilities come second
         { ARTP_BLINK,                 PROPN_PLAIN },
@@ -458,6 +459,7 @@ static string _randart_descrip(const item_def &item)
         { ARTP_CORRODE, "It may corrode your equipment when you take damage.", false},
         { ARTP_DRAIN, "It causes draining when unequipped.", false},
         { ARTP_CONFUSE, "It may confuse you when you take damage.", false},
+        { ARTP_FRAGILE, "It will be destroyed if unequipped.", false },
     };
 
     // Give a short description of the base type, for base types with no

crawl-ref/source/enum.h
@@ -3941,6 +3941,7 @@ enum artefact_prop_type
     ARTP_CORRODE,
     ARTP_DRAIN,
     ARTP_CONFUSE,
+    ARTP_FRAGILE,
     ARTP_NUM_PROPERTIES
 };
 

crawl-ref/source/invent.cc
@@ -1605,8 +1605,11 @@ bool needs_handle_warning(const item_def &item, operation_types oper,
             return true;
         }
 
-        if (is_artefact(item) && artefact_property(item, ARTP_DRAIN))
+        if (is_artefact(item) && (artefact_property(item, ARTP_DRAIN)
+                                  || artefact_property(item, ARTP_FRAGILE)))
+        {
             return true;
+        }
     }
 
     if (oper == OPER_PUTON || oper == OPER_WEAR || oper == OPER_TAKEOFF
@@ -1622,8 +1625,11 @@ bool needs_handle_warning(const item_def &item, operation_types oper,
             return true;
         }
 
-        if (is_artefact(item) && artefact_property(item, ARTP_DRAIN))
+        if (is_artefact(item) && (artefact_property(item, ARTP_DRAIN)
+                                  || artefact_property(item, ARTP_FRAGILE)))
+        {
             return true;
+        }
         if (item.is_type(OBJ_JEWELLERY, AMU_DISMISSAL))
             return true;
         if (item.is_type(OBJ_JEWELLERY, AMU_REGENERATION)

crawl-ref/source/shopping.cc
@@ -1013,6 +1013,9 @@ int artefact_value(const item_def &item)
     if (prop[ ARTP_CONFUSE ])
         ret -= 8;
 
+    if (prop[ ARTP_FRAGILE ])
+        ret -= 8;
+
     // extremely good
     if (prop[ ARTP_FOG ])
         ret += 50;

crawl-ref/source/wiz-item.cc
@@ -1409,6 +1409,7 @@ static void _debug_rap_stats(FILE *ostat)
         "ARTP_CORRODE",
         "ARTP_DRAIN",
         "ARTP_CONFUSE",
+        "ARTP_FRAGILE",
     };
     COMPILE_CHECK(ARRAYSZ(rap_names) == ARTP_NUM_PROPERTIES);
 

commit 72fc8fbf4a5a41cdb3e218af3841a5f5779b9e24

Remove temporary brand on unwield, again
Broken by f50acb22cd3c. TY Medar!

crawl-ref/source/player-equip.cc
@@ -750,7 +750,8 @@ static void _unequip_weapon_effect(item_def& real_item, bool showMsgs,
 
             if (you.duration[DUR_WEAPON_BRAND])
             {
-                end_weapon_brand(item);
+                ASSERT(real_item.defined());
+                end_weapon_brand(real_item);
                 // We're letting this through even if hiding messages.
                 mpr("Your temporary branding evaporates.");
             }