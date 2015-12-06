7a29ed5b881c4667f07ceddc0dbd89487bf4a81e
Shmuale Mark <shm.mark@gmail.com>
2015-12-06 17:58:47 -0500

    Don't prompt if dazzling spray would hit an OoD (#10150).

--- crawl-ref/source/spl-damage.cc
+++ crawl-ref/source/spl-damage.cc
@@ -2686,9 +2686,7 @@ static bool _dazzle_can_hit(const actor *act)
         testbeam.thrower = KILL_YOU;
         zappy(ZAP_DAZZLING_SPRAY, 100, testbeam);
 
-        return !mons_is_avatar(mons->type)
-               && mons_species(mons->type) != MONS_BUSH
-               && !shoot_through_monster(testbeam, mons);
+        return testbeam.ignores_monster(mons);
     }
     else
         return false;
