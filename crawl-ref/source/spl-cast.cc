b5e021b590f862ec361c9f1110ef45aa1f63db2b
Shmuale Mark <shm.mark@gmail.com>
2015-12-06 16:17:12 -0500

    Standardise prompt texts.
    This also means they get coloured lightred by default.

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -801,7 +801,7 @@ bool cast_a_spell(bool check_range, spell_type spell)
         if (!yesno(god_loathes_spell(spell, you.religion) ?
             "<lightred>Casting this spell will cause instant excommunication!"
                 "</lightred> Really cast?" :
-            "Casting this spell will put you into penance. Really cast?",
+            "Casting this spell will place you under penance. Really cast?",
             true, 'n'))
         {
             canned_msg(MSG_OK);
