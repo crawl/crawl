dff1d40a2148ef9363b9680594a806768bea4941
Steve Melenchuk <smelenchuk@gmail.com>
2015-12-13 16:56:21 -0700

    Try to break this less.

--- .travis/deps.pl
+++ .travis/deps.pl
@@ -5,3 +5,23 @@ use warnings;
 if ($ENV{BUILD_ALL}) {
     retry(qw(git submodule update --init --recursive));
 }
+
+sub retry {
+    my (@cmd) = @_;
+
+    my $tries = 5;
+    my $sleep = 5;
+
+    my $ret;
+    while ($tries--) {
+        print "Running '@cmd'\n";
+        $ret = system(@cmd);
+        return if $ret == 0;
+        print "'@cmd' failed ($ret), retrying in $sleep seconds...\n";
+        sleep $sleep;
+    }
+
+    print "Failed to run '@cmd' ($ret)\n";
+    # 1 if there was a signal or system() failed, otherwise the exit status.
+    exit($ret & 127 ? 1 : $ret >> 8);
+}
