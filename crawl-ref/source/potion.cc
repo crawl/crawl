73bdc8ad3b8b7e30826245dae04c266ecbca69b2
Alan Malloy <alan@malloys.org>
2015-12-16 23:34:23 -0800

    Allow Fo to quaff !haste if they don't know what it is

--- crawl-ref/source/potion.cc
+++ crawl-ref/source/potion.cc
@@ -296,7 +296,7 @@ public:
 
     bool quaff(bool was_known) const override
     {
-        if (!check_known_quaff())
+        if (was_known && !check_known_quaff())
             return false;
 
         if (effect())
