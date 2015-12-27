28655ce3a9544a9bbbeb1f5e7ef961226d5dba82
Shmuale Mark <shm.mark@gmail.com>
2015-12-26 20:10:31 -0500

    Don't lower Natasha's HD on revival.
    This should make subsequent encounters less anticlimactic; she already has
    pretty low HP at HD:3.

--- crawl-ref/source/mon-death.cc
+++ crawl-ref/source/mon-death.cc
@@ -3288,9 +3288,7 @@ void mons_felid_revive(monster* mons)
         create_monster(
             mgen_data(type, (mons->has_ench(ENCH_CHARM) ? BEH_HOSTILE
                              : SAME_ATTITUDE(mons)),
-                      0, 0, 0, revive_place, mons->foe, 0, GOD_NO_GOD,
-                      MONS_NO_MONSTER, 0, COLOUR_INHERIT, PROX_ANYWHERE,
-                      level_id::current(), hd));
+                      0, 0, 0, revive_place, mons->foe));
 
     if (newmons)
     {
