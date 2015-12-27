crawl/crawl

commit 2cdb8dad07d0692118253dddd0e460d0b4682d15

Refactor cleaving further.
Now every melee_attack goes through cleave_setup, though cleave_targets
may end up empty. This allows for more extensible cleaving-like effects
(especially since they will only need to be added in one place, rather
than in actor::can_cleave as well). This also makes Gyre and Gimble
work with the Blade card (16 attacks!).

crawl-ref/source/melee_attack.cc
@@ -84,8 +84,6 @@ melee_attack::melee_attack(actor *attk, actor *defn,
     if (weapon && !using_weapon())
         wpn_skill = SK_FIGHTING;
 
-    can_cleave = attacker != defender && attacker->can_cleave(attack_number);
-
     attack_position = attacker->pos();
 }
 
@@ -133,7 +131,7 @@ bool melee_attack::handle_phase_attempted()
                 return false;
             }
         }
-        else if (can_cleave)
+        else if (!cleave_targets.empty())
         {
             targetter_cleave hitfunc(attacker, defender->pos());
             if (stop_attack_prompt(hitfunc, "attack"))
@@ -772,21 +770,21 @@ bool melee_attack::attack()
     if (attacker != defender && attacker->self_destructs())
         return did_hit = perceived_attack = true;
 
-    if (can_cleave && !cleaving)
+    if (!cleaving)
         cleave_setup();
 
     string dummy;
     const bool gyre = weapon && is_unrandom_artefact(*weapon, UNRAND_GYRE);
     if (gyre && !weapon->props.exists(ARTEFACT_NAME_KEY))
-       weapon->props[ARTEFACT_NAME_KEY].get_string() = "quick blades \"Gyre\" and \"Gimble\"";
+       set_artefact_name(*weapon, get_artefact_name(*weapon));
     unwind_var<string> gyre_name(gyre ? weapon->props[ARTEFACT_NAME_KEY].get_string()
                                       : dummy);
-    if (weapon && is_unrandom_artefact(*weapon, UNRAND_GYRE))
+    if (gyre)
     {
         if (!cleaving)
-            set_artefact_name(*attacker->weapon(), "quick blade \"Gyre\"");
+            set_artefact_name(*weapon, "quick blade \"Gyre\"");
         else
-            set_artefact_name(*attacker->weapon(), "quick blade \"Gimble\"");
+            set_artefact_name(*weapon, "quick blade \"Gimble\"");
     }
 
     // Attacker might have died from effects of cleaving handled prior to this
@@ -3636,20 +3634,19 @@ bool melee_attack::do_knockback(bool trample)
 }
 
 /**
- * Find the list of targets to cleave (after hitting the main target).
+ * Find the list of targets to cleave after hitting the main target.
  */
 void melee_attack::cleave_setup()
 {
-    if (cell_is_solid(defender->pos()))
-        return;
-
     // Don't cleave on a self-attack.
     if (attacker->pos() == defender->pos())
         return;
 
     // We need to get the list of the remaining potential targets now because
     // if the main target dies, its position will be lost.
-    get_cleave_targets(attacker, defender->pos(), cleave_targets);
+    get_cleave_targets(attacker, defender->pos(), cleave_targets, attack_number);
+    // We're already attacking this guy.
+    cleave_targets.pop_front();
 }
 
 // cleave damage modifier for additional attacks: 75% of base damage

crawl-ref/source/actor.cc
@@ -899,25 +899,3 @@ void actor::collide(coord_def newpos, const actor *agent, int pow)
          KILLED_BY_COLLISION, "",
          feature_description_at(newpos, false, DESC_A, false));
 }
-
-bool actor::can_cleave(int which_attack) const
-{
-    if (confused())
-        return false;
-
-    if (is_player()
-        && (you.form == TRAN_HYDRA && you.heads() > 1
-            || you.duration[DUR_CLEAVE]))
-    {
-        return true;
-    }
-
-    const item_def* weap = weapon(which_attack);
-    if (!weap)
-        return false;
-
-    if (is_unrandom_artefact(*weap, UNRAND_GYRE))
-        return true;
-
-    return item_attack_skill(*weap) == SK_AXES;
-}

crawl-ref/source/actor.h
@@ -101,7 +101,6 @@ public:
     {
         return weapon(0);
     }
-    bool can_cleave(int which_attack = -1) const;
     virtual random_var attack_delay(const item_def *weapon,
                                     const item_def *projectile = nullptr,
                                     bool random = true, bool scaled = true,

crawl-ref/source/fight.cc
@@ -492,62 +492,59 @@ static bool _dont_harm(const actor* attacker, const actor* defender)
 }
 
 /**
- * List potential cleave targets (adjacent hostile creatures), aside from the
- * targeted defender itself.
+ * List potential cleave targets (adjacent hostile creatures), including the
+ * defender itself.
  *
  * @param attacker[in]   The attacking creature.
  * @param def[in]        The location of the targeted defender.
  * @param targets[out]   A list to be populated with targets.
+ * @param which_attack   The attack_number (default -1, which uses the default weapon).
  */
 void get_cleave_targets(const actor* attacker, const coord_def& def,
-                        list<actor*> &targets)
+                        list<actor*> &targets, int which_attack)
 {
     // Prevent scanning invalid coordinates if the attacker dies partway through
     // a cleave (due to hitting explosive creatures, or perhaps other things)
     if (!attacker->alive())
         return;
 
-    if (attacker->weapon()
-        && is_unrandom_artefact(*attacker->weapon(), UNRAND_GYRE))
-    {
-        if (actor * target = actor_at(def))
-            targets.push_back(target);
-        return;
-    }
-
-    const coord_def atk = attacker->pos();
-    coord_def atk_vector = def - atk;
-    const int dir = coinflip() ? -1 : 1;
-
-    for (int i = 0; i < 7; ++i)
-    {
-        atk_vector = rotate_adjacent(atk_vector, dir);
-        if (cell_is_solid(atk + atk_vector))
-            continue;
-
-        actor * target = actor_at(atk + atk_vector);
-        if (target && !_dont_harm(attacker, target))
-            targets.push_back(target);
-    }
-}
-
-/**
- * List potential cleave targets (adjacent hostile creatures), including the
- * defender.
- *
- * @param attacker[in]   The attacking creature.
- * @param def[in]        The location of the targeted defender.
- * @param targets[out]   A list to be populated with targets.
- */
-void get_all_cleave_targets(const actor* attacker, const coord_def& def,
-                            list<actor*> &targets)
-{
-    if (cell_is_solid(def))
-        return;
-
     if (actor_at(def))
         targets.push_back(actor_at(def));
-    get_cleave_targets(attacker, def, targets);
+
+    const item_def* weap = attacker->weapon(which_attack);
+    if (attacker->confused() || !weap)
+        return;
+
+    if ((item_attack_skill(*weap) == SK_AXES
+         || attacker->is_player()
+            && (you.form == TRAN_HYDRA && you.heads() > 1
+                || you.duration[DUR_CLEAVE]))
+        && !attacker->confused())
+    {
+        const coord_def atk = attacker->pos();
+        coord_def atk_vector = def - atk;
+        const int dir = coinflip() ? -1 : 1;
+
+        for (int i = 0; i < 7; ++i)
+        {
+            atk_vector = rotate_adjacent(atk_vector, dir);
+
+            actor * target = actor_at(atk + atk_vector);
+            if (target && !_dont_harm(attacker, target))
+                targets.push_back(target);
+        }
+    }
+
+    if (is_unrandom_artefact(*weap, UNRAND_GYRE))
+    {
+        list<actor*> new_targets;
+        for (actor* targ : targets)
+        {
+            new_targets.push_back(targ);
+            new_targets.push_back(targ);
+        }
+        targets = new_targets;
+    }
 }
 
 /**

crawl-ref/source/fight.h
@@ -36,9 +36,7 @@ stab_type find_stab_type(const actor *attacker,
                          const actor *defender);
 
 void get_cleave_targets(const actor* attacker, const coord_def& def,
-                        list<actor*> &targets);
-void get_all_cleave_targets(const actor* attacker, const coord_def& def,
-                            list<actor*> &targets);
+                        list<actor*> &targets, int which_attack = -1);
 void attack_cleave_targets(actor* attacker, list<actor*> &targets,
                            int attack_number = 0,
                            int effective_attack_number = 0);

crawl-ref/source/main.cc
@@ -2691,8 +2691,7 @@ static void _swing_at_target(coord_def move)
     else
     {
         list<actor*> cleave_targets;
-        if (you.can_cleave())
-            get_all_cleave_targets(&you, target, cleave_targets);
+        get_cleave_targets(&you, target, cleave_targets);
 
         if (!cleave_targets.empty())
         {

crawl-ref/source/melee_attack.h
@@ -48,7 +48,6 @@ public:
     int       attack_number;
     int       effective_attack_number;
 
-    bool         can_cleave;
     list<actor*> cleave_targets;
     bool         cleaving;        // additional attack from cleaving
     coord_def attack_position;

crawl-ref/source/target.cc
@@ -483,7 +483,7 @@ targetter_cleave::targetter_cleave(const actor* act, coord_def target)
     origin = act->pos();
     aim = target;
     list<actor*> act_targets;
-    get_all_cleave_targets(act, target, act_targets);
+    get_cleave_targets(act, target, act_targets);
     while (!act_targets.empty())
     {
         targets.insert(act_targets.front()->pos());

commit d967c0aef3225395b7d30c3d872040858d2ad126

Fix attack prompts when cleaving (#9576).
Probably broken by 2cdb8dad07d0 (1 year ago!).

crawl-ref/source/melee_attack.cc
@@ -740,8 +740,12 @@ bool melee_attack::handle_phase_end()
  */
 bool melee_attack::attack()
 {
-    if (!cleaving && !handle_phase_attempted())
-        return false;
+    if (!cleaving)
+    {
+        cleave_setup();
+        if (!handle_phase_attempted())
+            return false;
+    }
 
     if (attacker != defender && attacker->is_monster()
         && mons_self_destructs(attacker->as_monster()))
@@ -749,9 +753,6 @@ bool melee_attack::attack()
         attacker->self_destruct();
         return did_hit = perceived_attack = true;
     }
-
-    if (!cleaving)
-        cleave_setup();
 
     string saved_gyre_name;
     if (weapon && is_unrandom_artefact(*weapon, UNRAND_GYRE))