77c8fc57243d13698a2ca7a30aaa8a6cd1b8b9ff
Nicholas Feinberg <pleasingfung@gmail.com>
2015-12-30 16:55:59 -0800

    Allow entering treeform in danger clouds (minmay)
    It sucks to drink un-id'd !lig in a freezing cloud generator, but,
    well, quite a lot of things suck to drink in a freezing cloud
    generator. Many of them will kill you. Not a special case worth
    its salt.
    
    :evergreen_tree::heart::cloud:

--- crawl-ref/source/transform.cc
+++ crawl-ref/source/transform.cc
@@ -1521,41 +1521,20 @@ static bool _transformation_is_safe(transformation_type which_trans,
                                     dungeon_feature_type feat, bool quiet,
                                     string *fail_reason = nullptr)
 {
-    string msg;
-    bool is_safe = true;
-
-    if (which_trans == TRAN_TREE)
-    {
-        const cloud_type cloud = cloud_type_at(you.pos());
-        if (cloud != CLOUD_NONE
-            // Tree form is immune to these two.
-            && cloud != CLOUD_MEPHITIC
-            && cloud != CLOUD_POISON
-            && is_damaging_cloud(cloud, false))
-        {
-            msg = make_stringf("You can't transform into a tree while standing "
-                               "in a cloud of %s.",
-                               cloud_type_name(cloud).c_str());
-            is_safe = false;
-        }
-    }
-
 #if TAG_MAJOR_VERSION == 34
     if (which_trans == TRAN_ICE_BEAST && you.species == SP_DJINNI)
-        is_safe = false; // melting is fatal...
+        return false; // melting is fatal...
 #endif
-    else if (feat_dangerous_for_form(which_trans, feat))
-    {
-        msg = make_stringf("You would %s in your new form.",
-                           feat == DNGN_DEEP_WATER ? "drown" : "burn");
-        is_safe = false;
-    }
+    if (!feat_dangerous_for_form(which_trans, feat))
+        return true;
 
+    const string msg = make_stringf("You would %s in your new form.",
+                                    feat == DNGN_DEEP_WATER ? "drown" : "burn");
     if (fail_reason)
         *fail_reason = msg;
-    if (!is_safe && !quiet)
+    if (!quiet)
         mpr(msg);
-    return is_safe;
+    return false;
 }
 
 /**
