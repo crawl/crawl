d5b4d0fe33224e6476c0dcebb4549e200ee2aeeb
Alan Malloy <alan@malloys.org>
2016-01-02 12:24:54 -0800

    Let the teleport message from "dismissal show up in ^p

--- crawl-ref/source/ouch.cc
+++ crawl-ref/source/ouch.cc
@@ -759,9 +759,9 @@ static void _maybe_dismiss(mid_t source)
             // 10% chance to teleport away monsters that harm you
             if (!mon->no_tele() && one_chance_in(10))
             {
-                mprf(MSGCH_EQUIPMENT, "The translocation field surrounding you"
-                                " vibrates and %s disappears!",
-                                mon->name(DESC_THE).c_str());
+                mprf("The translocation field surrounding you"
+                     " vibrates and %s disappears!",
+                     mon->name(DESC_THE).c_str());
                 place_cloud(CLOUD_TLOC_ENERGY, mon->pos(), 1 + random2(8), 0);
                 monster_teleport(mon, true, true);
             }
