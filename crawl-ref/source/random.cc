f2263e4c685f234e5cda456fbb5591a9a48a6331
Corin Buchanan-Howland <corin@buchananhowland.net>
2016-01-21 22:40:03 -0500

    Add rand_round (|amethyst)
    |amethyst put together this function to randomly round a double into an int
    because I needed it. Thanks!

--- crawl-ref/source/random.cc
+++ crawl-ref/source/random.cc
@@ -211,6 +211,12 @@ int div_rand_round(int num, int den)
         return num / den;
 }
 
+// Converts a double to an integer by randomly rounding.
+int rand_round(double x)
+{
+    return int(x) + decimal_chance(fmod(x, 1.0));
+}
+
 int div_round_up(int num, int den)
 {
     return num / den + (num % den != 0);
