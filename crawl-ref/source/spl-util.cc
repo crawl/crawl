241b07c99133d940c7cceecfe5ad60c7a2783075
Shmuale Mark <shm.mark@gmail.com>
2016-02-15 19:02:30 -0500

    Forbid memorising Spellforged Servitor with Sac Love (#10284).
    And any other future summoning spells.

--- crawl-ref/source/spl-util.cc
+++ crawl-ref/source/spl-util.cc
@@ -1275,15 +1275,11 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
             return "the dungeon can only cope with one malign gateway"
                     " at a time.";
         }
-        if (player_mutation_level(MUT_NO_LOVE))
-            return "you cannot coerce anything to answer your summons.";
         break;
 
     case SPELL_SUMMON_FOREST:
         if (temp && you.duration[DUR_FORESTED])
             return "you can only summon one forest at a time.";
-        if (player_mutation_level(MUT_NO_LOVE))
-            return "you cannot coerce anything to answer your summons.";
         break;
 
     case SPELL_ANIMATE_DEAD:
@@ -1296,29 +1292,16 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
             return "you cannot coerce anything to obey you.";
         break;
 
-    case SPELL_SUMMON_SMALL_MAMMAL:
-    case SPELL_SUMMON_HORRIBLE_THINGS:
-    case SPELL_HAUNT:
-    case SPELL_SUMMON_ICE_BEAST:
-    case SPELL_CALL_IMP:
-    case SPELL_SUMMON_GREATER_DEMON:
-    case SPELL_SHADOW_CREATURES:
-    case SPELL_CALL_CANINE_FAMILIAR:
-    case SPELL_SUMMON_DRAGON:
-    case SPELL_SUMMON_BUTTERFLIES:
-    case SPELL_MONSTROUS_MENAGERIE:
-    case SPELL_SUMMON_HYDRA:
-    case SPELL_SUMMON_MINOR_DEMON:
-    case SPELL_SUMMON_LIGHTNING_SPIRE:
-    case SPELL_SUMMON_GUARDIAN_GOLEM:
-    case SPELL_DRAGON_CALL:
-    case SPELL_SUMMON_MANA_VIPER:
-        if (player_mutation_level(MUT_NO_LOVE))
-            return "you cannot coerce anything to answer your summons.";
-        break;
-
     default:
         break;
+    }
+
+    if (get_spell_disciplines(spell) & SPTYP_SUMMONING
+        && spell != SPELL_AURA_OF_ABJURATION
+        && spell != SPELL_RECALL
+        && player_mutation_level(MUT_NO_LOVE))
+    {
+        return "you cannot coerce anything to answer your summons.";
     }
 
     return "";
