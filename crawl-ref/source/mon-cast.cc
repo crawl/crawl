728087f134b82bfe55f861e48f17431ed6e2d422
Nicholas Feinberg <pleasingfung@gmail.com>
2016-02-27 15:42:52 -0800

    Fix flash freeze's AUTOMATIC_HIT
    It now has ~the same to-hit as fire breath does on drakes, and
    very very high to-hit on later monsters (scales hard with power);
    can scale further if necessary.
    
    Should also fix a theoretical bug involving flash freeze & the
    warlock's mirror.

--- crawl-ref/source/mon-cast.cc
+++ crawl-ref/source/mon-cast.cc
@@ -937,7 +937,7 @@ bolt mons_spell_beam(monster* mons, spell_type spell_cast, int power,
         beam.damage   = dice_def(3, 7 + (power / 12));
         beam.colour   = WHITE;
         beam.flavour  = BEAM_ICE;
-        beam.hit      = AUTOMATIC_HIT;
+        beam.hit      = 20 + power / 6;
         break;
 
     case SPELL_SAP_MAGIC:
