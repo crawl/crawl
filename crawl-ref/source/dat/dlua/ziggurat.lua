crawl/crawl

commit 07949ec7b9c0b9583012be218f9b887257a10cd9

Replace Ziggurat fees with a 3-rune lock.
This way you need a good deal less scumming if you want to try multiple
Ziggurats -- completely or not.  The number of runes required comes from
historic data of people who left Ziggurats at particular urune count:
 0:   7
 1:   7
 2:  19
 3: 117 (earliest completion)
 4: 175
 5: 224
 6: 164
 7: 134
 8: 112
 9: 162
10: 177
11: 164
12: 106
13:  56
14: 100
15: 329

crawl-ref/source/dat/dlua/ziggurat.lua
@@ -10,8 +10,6 @@
 -- upvalues cannot (yet) be saved.
 ------------------------------------------------------------------------------
 
-require("dlua/lm_toll.lua")
-
 -- Deepest you can go in a ziggurat - at this point it's beyond
 -- obvious that we're not challenging the player, and one could hope
 -- she has enough loot by now.
@@ -19,7 +17,7 @@ ZIGGURAT_MAX = 27
 
 function zig()
   if not dgn.persist.ziggurat then
-    dgn.persist.ziggurat = { entry_fee = 0 }
+    dgn.persist.ziggurat = { }
     -- Initialise here to handle ziggurats accessed directly by &P.
     initialise_ziggurat(dgn.persist.ziggurat)
   end
@@ -35,10 +33,6 @@ function ziggurat_wall_colour()
 end
 
 function initialise_ziggurat(z, portal)
-  if portal then
-    z.entry_fee = portal.props.amount
-  end
-
   -- Any given ziggurat will use the same builder for all its levels,
   -- and the same colours for outer walls. Before choosing the builder,
   -- we specify a global excentricity. If zig_exc=0, then the ellipses
@@ -58,20 +52,6 @@ end
 
 -- Common setup for ziggurat entry vaults.
 function ziggurat_portal(e, spawnrange)
-  local d = crawl.roll_dice
-  local entry_fee =
-    10 * math.floor(200 + d(3,200) / 3 + d(10) * d(10) * d(10))
-
-  local function stair()
-    return toll_stair {
-      amount = entry_fee,
-      toll_desc = "to enter a ziggurat",
-      overview_note = "" .. entry_fee .. " gp",
-      floor = "stone_arch",
-      feat_tile = "dngn_portal_ziggurat_gone",
-      onclimb = "callback.ziggurat_initialiser"
-    }
-  end
 
   if spawnrange == "shallow" then
     e.tags("chance_shallow_zig extra")
@@ -84,7 +64,6 @@ function ziggurat_portal(e, spawnrange)
     e.chance("8%")
   end
 
-  e.lua_marker("O", stair)
   e.kfeat("O = enter_ziggurat")
 end
 
@@ -436,7 +415,7 @@ local function ziggurat_create_loot_at(c)
   -- affects the loot randomly (separatedly on each stage).
   local depth = you.depth()
   local nloot = depth
-  nloot = nloot + crawl.random2(math.floor(nloot * zig().entry_fee / 10000))
+  local nloot = depth + crawl.random2(math.floor(nloot * 0.5))
 
   local function find_free_space(nspaces)
     local spaces = { }

crawl-ref/source/dat/dlua/lm_timed.lua
@@ -5,7 +5,6 @@
 
 require('dlua/lm_tmsg.lua')
 require('dlua/lm_1way.lua')
-require('dlua/lm_toll.lua')
 
 TimedMarker = util.subclass(OneWayStair)
 TimedMarker.CLASS = "TimedMarker"
@@ -41,11 +40,6 @@ function TimedMarker:new(props)
     tmarker.dur_short = tmarker.dur
   end
   tmarker.msg = props.msg
-
-  if props.amount and props.amount > 0 then
-    tmarker.toll = TollStair:new(props)
-  end
-
   props.msg = nil
 
   return tmarker
@@ -57,14 +51,10 @@ function TimedMarker:activate(marker, verbose)
   dgn.register_listener(dgn.dgn_event_type('entered_level'), marker)
   dgn.register_listener(dgn.dgn_event_type('player_los'), marker, marker:pos())
   dgn.register_listener(dgn.dgn_event_type('turn'), marker)
-  if self.toll then
-    self.toll:activate(marker, verbose)
-  end
 end
 
 function TimedMarker:property(marker, pname)
-  return ((self.toll and self.toll:property(marker, pname))
-          or self.super.property(self, marker, pname))
+  return self.super.property(self, marker, pname)
 end
 
 function TimedMarker:start()
@@ -141,10 +131,6 @@ function TimedMarker:read(marker, th)
   self.dur = file.unmarshall_number(th)
   self.dur_short = file.unmarshall_number(th)
   self.msg  = lmark.unmarshall_marker(th)
-
-  if self.props.amount then
-    self.toll = TollStair:new(self.props)
-  end
 
   setmetatable(self, TimedMarker)
   return self

crawl-ref/source/dat/dlua/lm_toll.lua
@@ -1,6 +1,7 @@
 ------------------------------------------------------------------------------
 -- lm_toll.lua:
 -- One-way toll-stair marker.
+-- TAG_MAJOR_VERSION: please remove
 ------------------------------------------------------------------------------
 
 require("dlua/lm_1way.lua")
@@ -9,127 +10,25 @@ TollStair = util.subclass(OneWayStair)
 TollStair.CLASS = "TollStair"
 
 function TollStair:new(props)
-  local toll = OneWayStair.new(self, props)
-  if not props.amount or props.amount < 1 then
-    error("Bad toll amount: " .. props.amount)
-  end
-  toll.seen = 0
-  return toll
+  return OneWayStair.new(self, props)
 end
 
 function TollStair:activate(marker)
   self.super.activate(self, marker)
-
-  dgn.register_listener(dgn.dgn_event_type("v_leave_level"),
-                        marker, marker:pos())
-  dgn.register_listener(dgn.dgn_event_type("player_los"),
-                        marker, marker:pos())
-end
-
-function TollStair:check_shopping_list(marker, first_time)
-  local name = "Ziggurat"
-
-  if you.shopping_list_has(name, marker) then
-    return
-  end
-
-  local ask_name = name
-  if first_time then
-    ask_name = "" .. self.props.amount .. " gp " .. ask_name
-  end
-
-  crawl.redraw_screen()
-  if crawl.yesno("Add " .. ask_name .. " to shopping list? (y/N)",
-                 true, "n")
-  then
-    local verb = self.props.shop_verb or "enter"
-
-    you.shopping_list_add(name, verb, self.props.amount, marker)
-
-    crawl.mpr("You can access your shopping list by pressing <w>$</w>.")
-  end
 end
 
 function TollStair:event(marker, ev)
-  if ev:type() == dgn.dgn_event_type('player_los') then
-    local x, y = ev:pos()
-
-    -- Only prompt to add to shopping list the first time we are seen.
-    if self.seen == 1 then
-      return true
-    end
-
-    self.seen = 1
-
-    if self.props.amount <= you.gold() then
-      return true
-    end
-
-    return true
-  end
-
   return self.super.event(self, marker, ev)
 end
 
 function TollStair:check_veto(marker, pname)
-  -- Have we enough gold?
-  local gold = you.gold()
-  local needed = self.props.amount
-
-  if gold < needed then
-    crawl.mpr("This portal charges " .. needed .. " gold for entry; " ..
-              "you have only " .. gold .. " gold.")
-    if not you.in_branch("Pan") then
-      self:check_shopping_list(marker)
-    end
-    return "veto"
-  end
-
-  if pname == "veto_stair" then
-    -- Ok, ask if the player wants to spend the $$$.
-    if not crawl.yesno("This portal charges " .. needed ..
-                       " gold for entry. Pay?", true, "n") then
-      if not you.in_branch("Pan") then
-        self:check_shopping_list(marker)
-      end
-      return "veto"
-    end
-  elseif pname == "veto_level_change" then
-    local name = "Ziggurat"
-    if you.shopping_list_has(name, marker) then
-      crawl.mpr("Removing " .. name .. " from shopping list.")
-      you.shopping_list_del(name, marker)
-    end
-
-    -- Gold gold gold! Forget that gold!
-    you.gold(you.gold() - needed)
-
-    local toll_desc
-    if self.props.toll_desc then
-        toll_desc = self.props.toll_desc
-    else
-        toll_desc = "at " .. crawl.article_a(self.props.desc)
-    end
-    crawl.take_note("You paid a toll of " .. needed .. " gold " ..
-                    toll_desc .. ".")
-    return
-  end
 end
 
 function TollStair:property(marker, pname)
-  if pname == 'veto_stair' or pname == 'veto_level_change' then
-    return self:check_veto(marker, pname)
-  end
-
-  if pname == 'feature_description_long' then
-    return self:feature_description_long(marker)
-  end
-
   return self.super.property(self, marker, pname)
 end
 
 function TollStair:feature_description_long(marker)
-  return "The portal charges " .. self.props.amount .. " for entry.\n"
 end
 
 function TollStair:write(marker, th)

crawl-ref/source/dat/dlua/luamark.lua
@@ -20,6 +20,7 @@ require('dlua/lm_trig.lua')
 require('dlua/lm_pdesc.lua')
 require('dlua/lm_1way.lua')
 require('dlua/lm_timed.lua')
+require('dlua/lm_toll.lua') -- remove upon TAG_MAJOR_VERSION bump
 require('dlua/lm_fog.lua')
 require('dlua/lm_props.lua')
 require('dlua/lm_mon_prop.lua')

crawl-ref/source/stairs.cc
@@ -803,6 +803,20 @@ void down_stairs(dungeon_feature_type force_stair, bool force_known_shaft)
         you.opened_zot = true;
     }
 
+    if (stair_find == DNGN_ENTER_ZIGGURAT)
+    {
+        #define ZIG_RUNES 3
+        int nrune = 0;
+        for (int i = 0; i < NUM_RUNE_TYPES; i++)
+            if (you.runes[i])
+                nrune++;
+        if (nrune < ZIG_RUNES)
+        {
+            mprf("You need at least %d runes to enter this place.", ZIG_RUNES);
+            return;
+        }
+    }
+
     // Bail if any markers veto the move.
     if (_marker_vetoes_level_change())
         return;
@@ -848,10 +862,7 @@ void down_stairs(dungeon_feature_type force_stair, bool force_known_shaft)
     }
     const coord_def stair_pos = you.pos();
 
-    // XXX: Obsolete, now that labyrinth entrances are only placed via Lua
-    //      with timed markers. Leaving in to reduce the chance of an
-    //      accidental permanent labyrinth entry. [rob]
-    if (stair_find == DNGN_ENTER_LABYRINTH)
+    if (stair_find == DNGN_ENTER_LABYRINTH || stair_find == DNGN_ENTER_ZIGGURAT)
         dungeon_terrain_changed(you.pos(), DNGN_STONE_ARCH);
 
     if (stair_find == DNGN_ENTER_LABYRINTH

commit 2c957a37b42184ade10bd144fabb8086c46c4f6f

Fix all Ziggurats per-game having the same layout
Broken since 07949ec7, as it removed the call to ziggurat_initialiser when
entering a new Zig. Now checks depth in order to know when to reroll the
builder, since doing it with an onclimb trigger wouldn't work for Ziggurat
figurines.

crawl-ref/source/dat/dlua/ziggurat.lua
@@ -3,11 +3,6 @@
 --
 -- Code for ziggurats.
 --
--- Important notes:
--- ----------------
--- Functions that are attached to Lua markers' onclimb properties
--- cannot be closures, because Lua markers must be saved and closure
--- upvalues cannot (yet) be saved.
 ------------------------------------------------------------------------------
 
 function zig()
@@ -40,11 +35,6 @@ function initialise_ziggurat(z, portal)
   z.colour = ziggurat_wall_colour()
 end
 
-function callback.ziggurat_initialiser(portal)
-  dgn.persist.ziggurat = { }
-  initialise_ziggurat(dgn.persist.ziggurat, portal)
-end
-
 -- Common setup for ziggurat levels.
 function ziggurat_level(e)
   e.tags("allow_dup")
@@ -66,6 +56,10 @@ function ziggurat_awaken_all(mons)
 end
 
 function ziggurat_build_level(e)
+  if you.depth() == 1 then
+    dgn.persist.ziggurat = { }
+    initialise_ziggurat(dgn.persist.ziggurat, portal)
+  end
   local builder = zig().builder
 
   -- Deeper levels can have all monsters awake.