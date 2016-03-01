80e3b75b14cea6ec1bf45538d47ad9de5b2442f7
Alex Jurkiewicz <alex@jurkiewi.cz>
2016-03-01 23:00:00 +1100

    Don't let noise break mesmerisation.
    It's a spoilery mechanic which encourages carting around mostly-useless
    items (?noise) through a single branch of the game.

--- crawl-ref/source/behold.cc
+++ crawl-ref/source/behold.cc
@@ -118,20 +118,6 @@ void player::clear_beholders()
     you.duration[DUR_MESMERISE_IMMUNE] = random_range(21, 40);
 }
 
-// Possibly end mesmerisation if a loud noise happened.
-void player::beholders_check_noise(int loudness, bool axe)
-{
-    if (axe)
-        return;
-
-    if (loudness >= 20 && beheld())
-    {
-        mpr("For a moment, your mind becomes perfectly clear!");
-        clear_beholders();
-        _removed_beholder();
-    }
-}
-
 static void _removed_beholder_msg(const monster* mon)
 {
     if (!mon || !mon->alive() || !mons_is_siren_beholder(mon)
