bbaf034c5230874476d3e2bb9c84ac534c379a15
Nicholas Feinberg <pleasingfung@gmail.com>
2016-03-01 08:26:01 -0800

    Fix wizmode terrain change (|amethyst)
    Back up to six parameters...

--- crawl-ref/source/terrain.h
+++ crawl-ref/source/terrain.h
@@ -102,7 +102,8 @@ void dungeon_terrain_changed(const coord_def &pos,
                              dungeon_feature_type feat = DNGN_UNSEEN,
                              bool preserve_features = false,
                              bool preserve_items = false,
-                             bool temporary = false);
+                             bool temporary = false,
+                             bool wizmode = false);
 
 // Moves everything on the level at src to dst.
 void dgn_move_entities_at(coord_def src,
