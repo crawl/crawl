da238d44f56eac261136faa1ad2d2e8510e92ba3
Nicholas Feinberg <pleasingfung@gmail.com>
2016-04-10 14:09:15 -0700

    Fix a copy-paste error (wheals)

--- crawl-ref/source/hiscores.cc
+++ crawl-ref/source/hiscores.cc
@@ -907,7 +907,7 @@ enum old_species_type
     OLD_SP_SLUDGE_ELF = -7,
     OLD_SP_DJINNI = -8,
     OLD_SP_LAVA_ORC = -9,
-    NUM_OLD_SPECIES = OLD_SP_LAVA_ORC
+    NUM_OLD_SPECIES = -OLD_SP_LAVA_ORC
 };
 
 static string _species_name(int race)
@@ -953,7 +953,7 @@ static int _species_by_name(const string& name)
     if (race != SP_UNKNOWN)
         return race;
 
-    for (race = -1; race >= -NUM_OLD_JOBS; race--)
+    for (race = -1; race >= -NUM_OLD_SPECIES; race--)
         if (name == _species_name(race))
             return race;
 
