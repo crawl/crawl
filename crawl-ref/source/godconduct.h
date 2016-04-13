ce6d5cfd8b1596b2121beef0d7cec03254940383
Shmuale Mark <shm.mark@gmail.com>
2016-04-13 15:00:37 -0400

    Move cannibalism-hatred to data.

--- crawl-ref/source/godconduct.h
+++ crawl-ref/source/godconduct.h
@@ -39,6 +39,7 @@ void disable_attack_conducts(god_conduct_trigger conduct[3]);
 void god_conduct_turn_start();
 
 bool god_punishes_spell(spell_type spell, god_type god);
+bool god_hates_cannibalism(god_type god);
 
 string get_god_likes(god_type which_god);
 string get_god_dislikes(god_type which_god);
--- crawl-ref/source/religion.h
+++ crawl-ref/source/religion.h
@@ -88,7 +88,6 @@ int piety_rank(int piety = you.piety);
 int piety_scale(int piety_change);
 bool god_likes_your_god(god_type god, god_type your_god = you.religion);
 bool god_hates_your_god(god_type god, god_type your_god = you.religion);
-bool god_hates_cannibalism(god_type god);
 bool god_hates_killing(god_type god, const monster* mon);
 bool god_hates_eating(god_type god, monster_type mc);
 
