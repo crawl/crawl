crawl/crawl

commit e88cd6af2b87921f2a4eea2c249f155fc924418b

Add autofight_warning option.
The goal of this option is to be the counterpart of
tile_key_repeat_delay=0 for console mode.

Since we don't know if the player keeps the tab key down in console
mode, I keep track of time elapsed between two tab commands in
milliseconds (using std::chrono).  If that delta is less than the
autofight_warning option, we stop the user with the message "You should
not fight recklessly!".

The option has no effect if the tab command was issued by a macro.

[Committer's note: Squashed several commits.
 Closes #244 pull request. -nfm]

crawl-ref/source/misc.cc
@@ -790,10 +790,12 @@ int apply_chunked_AC(int dam, int ac)
     return hurt;
 }
 
-void handle_real_time(time_t t)
+void handle_real_time(std::chrono::milliseconds ms)
 {
-    you.real_time += min<time_t>(t - you.last_keypress_time, IDLE_TIME_CLAMP);
-    you.last_keypress_time = t;
+    you.real_time_delta = ms - you.last_keypress_time;
+    you.real_time_ms += you.real_time_delta;
+    you.real_time = floor(you.real_time_ms.count()/1000);
+    you.last_keypress_time = ms;
 }
 
 unsigned int breakpoint_rank(int val, const int breakpoints[],

crawl-ref/source/dungeon.cc
@@ -12,6 +12,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
 #include <list>
 #include <map>
 #include <set>

crawl-ref/source/externs.h
@@ -11,6 +11,7 @@
 #include <cstdint>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
 #include <deque>
 #include <list>
 #include <map>

crawl-ref/source/hiscores.cc
@@ -1690,7 +1690,7 @@ void scorefile_entry::init(time_t dt)
     birth_time = you.birth_time;     // start time of game
     death_time = (dt != 0 ? dt : time(nullptr)); // end time of game
 
-    handle_real_time(death_time);
+    handle_real_time(std::chrono::milliseconds(death_time * 1000));
     real_time = you.real_time;
 
     num_turns = you.num_turns;

crawl-ref/source/initfile.cc
@@ -802,6 +802,7 @@ void game_options::reset_options()
     easy_quit_item_prompts = true;
     allow_self_target      = CONFIRM_PROMPT;
     hp_warning             = 30;
+    autofight_warning      = 0;
     magic_point_warning    = 0;
     skill_focus            = SKM_FOCUS_ON;
     cloud_status           = !is_tiles();
@@ -2875,6 +2876,7 @@ void game_options::read_option_line(const string &str, bool runscript)
     else BOOL_OPTION(show_newturn_mark);
     else BOOL_OPTION(show_game_turns);
     else INT_OPTION(hp_warning, 0, 100);
+    else INT_OPTION(autofight_warning, 0, 1000);
     else INT_OPTION_NAMED("mp_warning", magic_point_warning, 0, 100);
     else LIST_OPTION(note_monsters);
     else LIST_OPTION(note_messages);

crawl-ref/source/main.cc
@@ -11,6 +11,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
+#include <chrono>
 #include <iostream>
 #include <list>
 #include <sstream>
@@ -2047,8 +2048,15 @@ void process_command(command_type cmd)
 
 #ifdef CLUA_BINDINGS
     case CMD_AUTOFIGHT:
-        if (!clua.callfn("hit_closest", 0, 0))
-            mprf(MSGCH_ERROR, "Lua error: %s", clua.error.c_str());
+        if (Options.autofight_warning > 0
+            && !is_processing_macro()
+            && you.real_time_delta <= std::chrono::milliseconds(Options.autofight_warning)
+            && crawl_state.prev_cmd == CMD_AUTOFIGHT)
+            mprf(MSGCH_DANGER, "You should not fight recklessly!");
+        else {
+            if (!clua.callfn("hit_closest", 0, 0))
+                mprf(MSGCH_ERROR, "Lua error: %s", clua.error.c_str());
+        }
         break;
     case CMD_AUTOFIGHT_NOMOVE:
         if (!clua.callfn("hit_closest_nomove", 0, 0))
@@ -2338,6 +2346,7 @@ void process_command(command_type cmd)
 
         break;
     }
+
 }
 
 static void _prep_input()

crawl-ref/source/menu.h
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <cstdio>
 #include <ctime>
+#include <chrono>
 #include <string>
 #include <vector>
 

crawl-ref/source/misc.h
@@ -67,7 +67,11 @@ void swap_with_monster(monster *mon_to_swap);
 
 int apply_chunked_AC(int dam, int ac);
 
-void handle_real_time(time_t t = time(0));
+void handle_real_time(
+    std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()
+        )
+    );
 unsigned int breakpoint_rank(int val, const int breakpoints[],
                              unsigned int num_breakpoints);
 

commit 15ddd17f425f8736f786281defaf0969f7fc702f

Fix idle time tracking (minmay, chequers)
Broken in e88cd6a.

crawl-ref/source/misc.cc
@@ -792,8 +792,11 @@ int apply_chunked_AC(int dam, int ac)
 
 void handle_real_time(chrono::time_point<chrono::system_clock> now)
 {
-    you.real_time_delta = chrono::duration_cast<chrono::milliseconds>(
-            now - you.last_keypress_time);
+    const chrono::milliseconds elapsed =
+     chrono::duration_cast<chrono::milliseconds>(now - you.last_keypress_time);
+    you.real_time_delta = min<chrono::milliseconds>(
+      elapsed,
+      (chrono::milliseconds)(IDLE_TIME_CLAMP * 1000));
     you.real_time_ms += you.real_time_delta;
     you.last_keypress_time = now;
 }