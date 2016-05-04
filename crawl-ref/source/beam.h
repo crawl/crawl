d6153c6f32184c7707f4b7a5560d17ca9b7cbe56
Nicholas Feinberg <pleasingfung@gmail.com>
2016-05-03 23:01:05 -0700

    Prompt for god-hated tree destruction (gammafunk)
    Warn before setting trees on fire with Dith, disintegrating them
    under Fedhas, etc.

--- crawl-ref/source/beam.h
+++ crawl-ref/source/beam.h
@@ -135,6 +135,7 @@ struct bolt
     bool chose_ray = false;       // do we want a specific ray?
     bool beam_cancelled = false;  // stop_attack_prompt() returned true
     bool dont_stop_player = false; // player answered self target prompt with 'y'
+    bool dont_stop_trees = false; // player answered tree-burning prompt with 'y'
 
     int       bounces = 0;        // # times beam bounced off walls
     coord_def bounce_pos = {0,0}; // position of latest wall bounce,
