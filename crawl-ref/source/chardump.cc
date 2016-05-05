crawl/crawl

commit 48885bca65be0117cb7c759dba5824e27105bad3

Don't claim to evoke hammers (WorldFamousW)
Wand/rod/deck evocations were being stored as club/whip/hammer (!)
evocations, respectively, for action count purposes. (For character
dumps.)

crawl-ref/source/chardump.cc
@@ -978,7 +978,7 @@ static bool _sort_by_first(pair<int, FixedVector<int, 28> > a,
     return false;
 }
 
-void count_action(caction_type type, int subtype)
+static void _count_action(caction_type type, int subtype)
 {
     pair<caction_type, int> pair(type, subtype);
     if (!you.action_count.count(pair))
@@ -995,7 +995,7 @@ void count_action(caction_type type, int subtype, int auxtype)
     ASSERT_RANGE(auxtype, -32768, 32768);
     int compound_subtype;
     compound_subtype = (auxtype << 16) | (subtype & 0xFFFF);
-    count_action(type, compound_subtype);
+    _count_action(type, compound_subtype);
 }
 
 /**

crawl-ref/source/chardump.h
@@ -40,8 +40,8 @@ void whereis_record(const char *status = "active");
 
 void record_turn_timestamp();
 
-void count_action(caction_type type, int subtype = 0);
-void count_action(caction_type type, int subtype, int auxtype);
+void count_action(caction_type type, int subtype = -32768,
+                  int auxtype = -32768);
 pair<int, int> caction_extract_types(int compound_subtype);
 
 #endif

commit 8690a2b252d4d91731e5ad5033bdf1b82d042d13

Fix [scrolls/potions]_used milestones
Broken by 48885bc.

Thanks to chequers for reporting, and to wheals for writing
every line of code in this commit. :P

crawl-ref/source/chardump.cc
@@ -980,6 +980,7 @@ static bool _sort_by_first(pair<int, FixedVector<int, 28> > a,
 
 static void _count_action(caction_type type, int subtype)
 {
+    dprf("counting %d", subtype);
     pair<caction_type, int> pair(type, subtype);
     if (!you.action_count.count(pair))
         you.action_count[pair].init(0);
@@ -993,9 +994,14 @@ void count_action(caction_type type, int subtype, int auxtype)
 {
     ASSERT_RANGE(subtype, -32768, 32768);
     ASSERT_RANGE(auxtype, -32768, 32768);
-    int compound_subtype;
-    compound_subtype = (auxtype << 16) | (subtype & 0xFFFF);
-    _count_action(type, compound_subtype);
+    _count_action(type, caction_compound(subtype, auxtype));
+}
+
+int caction_compound(int subtype, int auxtype)
+{
+    ASSERT_RANGE(subtype, -32768, 32768);
+    ASSERT_RANGE(auxtype, -32768, 32768);
+    return (auxtype << 16) | (subtype & 0xFFFF);
 }
 
 /**

crawl-ref/source/chardump.h
@@ -68,5 +68,6 @@ enum block_type    // auxtype for counted actions
 void count_action(caction_type type, int subtype = -32768,
                   int auxtype = -32768);
 pair<int, int> caction_extract_types(int compound_subtype);
+int caction_compound(int subtype = -32768, int auxtype = -32768);
 
 #endif

crawl-ref/source/hiscores.cc
@@ -1707,7 +1707,8 @@ void scorefile_entry::init(time_t dt)
     zigmax     = you.zig_max;
 
     scrolls_used = 0;
-    pair<caction_type, int> p(CACT_USE, OBJ_SCROLLS);
+    dprf("checking %d", caction_compound(OBJ_SCROLLS));
+    pair<caction_type, int> p(CACT_USE, caction_compound(OBJ_SCROLLS));
 
     const int maxlev = min<int>(you.max_level, 27);
     if (you.action_count.count(p))
@@ -1715,7 +1716,7 @@ void scorefile_entry::init(time_t dt)
             scrolls_used += you.action_count[p][i];
 
     potions_used = 0;
-    p = make_pair(CACT_USE, OBJ_POTIONS);
+    p = make_pair(CACT_USE, caction_compound(OBJ_POTIONS));
     if (you.action_count.count(p))
         for (int i = 0; i < maxlev; i++)
             potions_used += you.action_count[p][i];