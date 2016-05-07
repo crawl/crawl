60d392fe774abd4a1154e71b1dddd8ee8e8caa2a
Chris Campbell <chriscampbell89@gmail.com>
2016-05-07 17:10:14 +0100

    Apply corrosion slaying penalties to unarmed and aux attacks
    Since corrosion is otherwise fully uncoupled from actual equipment.

--- crawl-ref/source/attack.cc
+++ crawl-ref/source/attack.cc
@@ -1256,11 +1256,9 @@ int attack::player_apply_slaying_bonuses(int damage, bool aux)
 {
     int damage_plus = 0;
     if (!aux && using_weapon())
-    {
         damage_plus = get_weapon_plus();
-        if (you.duration[DUR_CORROSION])
-            damage_plus -= 4 * you.props["corrosion_amount"].get_int();
-    }
+    if (you.duration[DUR_CORROSION])
+        damage_plus -= 4 * you.props["corrosion_amount"].get_int();
     damage_plus += slaying_bonus(!weapon && wpn_skill == SK_THROWING
                                  || (weapon && is_range_weapon(*weapon)
                                             && using_weapon()));
