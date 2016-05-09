b0dbc27f6a74463d6d821c5d2669bd7d978bb772
Nicholas Feinberg <pleasingfung@gmail.com>
2016-05-09 10:00:19 -0700

    Mention rDamnation in monster descriptions

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -2924,6 +2924,8 @@ static const char* _get_resist_name(mon_resist_flags res_type)
         return "rotting";
     case MR_RES_NEG:
         return "negative energy";
+    case MR_RES_DAMNATION:
+        return "damnation";
     default:
         return "buggy resistance";
     }
@@ -3187,7 +3189,7 @@ static string _monster_stat_description(const monster_info& mi)
     {
         MR_RES_ELEC,    MR_RES_POISON, MR_RES_FIRE,
         MR_RES_STEAM,   MR_RES_COLD,   MR_RES_ACID,
-        MR_RES_ROTTING, MR_RES_NEG,
+        MR_RES_ROTTING, MR_RES_NEG,    MR_RES_DAMNATION,
     };
 
     vector<string> extreme_resists;
@@ -3202,6 +3204,8 @@ static string _monster_stat_description(const monster_info& mi)
         if (level != 0)
         {
             const char* attackname = _get_resist_name(rflags);
+            if (rflags == MR_RES_DAMNATION)
+                level = 3; // one level is immunity
             level = max(level, -1);
             level = min(level,  3);
             switch (level)
