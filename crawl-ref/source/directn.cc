fb5558230bbc5c627264bcd49e8a1652c1766312
Neil Moore <neil@s-z.org>
2016-05-25 16:49:14 -0400

    Describe spectral weapon brands/types when they enter view.
    We avoided this for dancing weapons, because there the monster name
    already includes the weapon type and brand.  But we also avoided
    it for spectral weapons, where that is not the case.  That is:
    
    Dancing weapon (both old and new behaviour):
      A trident of protection comes into view.
    
    Spectral weapon (old behaviour):
      A spectral weapon comes into view.
    
    Spectral weapon (new behaviour):
      A spectral weapon comes into view. It is a spectral dagger of
      electrocution.

--- crawl-ref/source/directn.cc
+++ crawl-ref/source/directn.cc
@@ -3362,8 +3362,12 @@ string get_monster_equipment_desc(const monster_info& mi,
 
     if (mi.type != MONS_DANCING_WEAPON && mi.type != MONS_SPECTRAL_WEAPON)
         weap = _describe_monster_weapon(mi, level == DESC_IDENTIFIED);
-    else if (level == DESC_IDENTIFIED || level == DESC_WEAPON_WARNING)
+    else if (level == DESC_IDENTIFIED || level == DESC_WEAPON_WARNING
+             // dancing weapons' names already include this information
+             || level == DESC_WEAPON && mi.type != MONS_DANCING_WEAPON)
+    {
         return " " + mi.full_name(DESC_A);
+    }
 
     // Print the rest of the equipment only for full descriptions.
     if (level == DESC_WEAPON || level == DESC_WEAPON_WARNING)
