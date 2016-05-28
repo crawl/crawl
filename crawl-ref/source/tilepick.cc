fc7d9a2e3ec6829bd28b1a509441305d7bdbc03d
Neil Moore <neil@s-z.org>
2016-05-28 05:19:55 -0400

    Don't crash on removed stones of tremors in tiles (#10453)

--- crawl-ref/source/tilepick.cc
+++ crawl-ref/source/tilepick.cc
@@ -2344,8 +2344,7 @@ static tileidx_t _tileidx_misc(const item_def &item)
 
 #if TAG_MAJOR_VERSION == 34
     case MISC_STONE_OF_TREMORS:
-        return evoker_is_charged(item) ? TILE_MISC_STONE_OF_TREMORS
-                                       : TILE_MISC_STONE_OF_TREMORS_INERT;
+        return TILE_MISC_STONE_OF_TREMORS_INERT;
 #endif
 
     case MISC_PHIAL_OF_FLOODS:
