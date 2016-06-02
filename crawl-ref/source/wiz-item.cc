crawl/crawl

commit 4a676c3a23cf0be8ea34ad8b12fe1fc0947c190f

Fix wiz-item staff acq stats, hackily
wiz-item had no idea how to handle rods or unrands being created
by staff acq, since it assumed all results would be OBJ_STAVES.
Better implementation welcomed.

crawl-ref/source/wiz-item.cc
@@ -800,7 +800,12 @@ static void _debug_acquirement_stats(FILE *ostat)
 
         acq_calls++;
         total_quant += item.quantity;
-        subtype_quants[item.sub_type] += item.quantity;
+        // hack alert: put rods & unrands into the end of staff acq
+        const int subtype_index
+            = item.base_type == OBJ_STAVES ? item.sub_type :
+              item.base_type == OBJ_RODS ? NUM_STAVES + item.sub_type :
+              NUM_STAVES + NUM_RODS; // an unrand, WPN_STAFF
+        subtype_quants[subtype_index] += item.quantity;
 
         max_plus    = max(max_plus, item.plus);
         total_plus += item.plus;
@@ -1185,7 +1190,24 @@ static void _debug_acquirement_stats(FILE *ostat)
             continue;
 
         item.sub_type = i;
-        string name = item.name(desc, terse, true);
+
+        if (type == OBJ_STAVES)
+        {
+            if (i == NUM_STAVES + NUM_RODS) // unrand
+            {
+                item.base_type = OBJ_WEAPONS;
+                item.sub_type = WPN_STAFF;
+            }
+            else if (i >= NUM_STAVES) // rod
+            {
+                item.base_type = OBJ_RODS;
+                item.sub_type = i - NUM_STAVES;
+            }
+            else
+                item.base_type = OBJ_STAVES; // actual staff
+        }
+
+        const string name = item.name(desc, terse, true);
 
         fprintf(ostat, format_str, name.c_str(),
                 (float) subtype_quants[i] * 100.0 / (float) total_quant);

commit 47c582d5e56a88e706a2058e8db6880da406b2d8

Unbreak acq stats
Broken by 4a676c3a23cf. I did say it was a hack!

crawl-ref/source/wiz-item.cc
@@ -802,7 +802,7 @@ static void _debug_acquirement_stats(FILE *ostat)
         total_quant += item.quantity;
         // hack alert: put rods & unrands into the end of staff acq
         const int subtype_index
-            = item.base_type == OBJ_STAVES ? item.sub_type :
+            = type != OBJ_STAVES || item.base_type == OBJ_STAVES ? item.sub_type :
               item.base_type == OBJ_RODS ? NUM_STAVES + item.sub_type :
               NUM_STAVES + NUM_RODS; // an unrand, WPN_STAFF
         subtype_quants[subtype_index] += item.quantity;