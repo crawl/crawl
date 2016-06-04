27345a758a47ea6660abbee2f470cc43067ec8e4
Nicholas Feinberg <pleasingfung@gmail.com>
2016-06-03 23:09:50 -0700

    Fix trog disabling spell training on join
    If you were in autotraining mode but had a spell skill set to
    train in manual mode (or vice versa), it wouldn't be disabled.
    Ditto if you hadn't yet gotten the skill to level 1.
    
    I wonder if this has ever affected anyone?

--- crawl-ref/source/religion.cc
+++ crawl-ref/source/religion.cc
@@ -3507,8 +3507,7 @@ static void _join_ru()
 static void _join_trog()
 {
     for (int sk = SK_SPELLCASTING; sk <= SK_LAST_MAGIC; ++sk)
-        if (you.skills[sk])
-            you.train[sk] = TRAINING_DISABLED;
+        you.train[sk] = you.train_alt[sk] = TRAINING_DISABLED;
 
     // When you start worshipping Trog, you make all non-hostile magic
     // users hostile.
