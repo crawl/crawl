35bb4dde54c3a680ffcaf9146bd81ee1eb0596fd
Neil Moore <neil@s-z.org>
2016-06-05 01:21:01 -0400

    Correct messages for losing Uskayaw 3* and 4*.
    God powers with capitalised messages do not add "You can
    {now,no longer}", so need distinct gain and loss messages.

--- crawl-ref/source/religion.cc
+++ crawl-ref/source/religion.cc
@@ -303,8 +303,10 @@ const vector<god_power> god_powers[NUM_GODS] =
     {
       { 1, ABIL_USKAYAW_STOMP, "stomp with the beat" },
       { 2, ABIL_USKAYAW_LINE_PASS, "pass through a line of other dancers" },
-      { 3, "Uskayaw will force your foes to helplessly watch your dance." },
-      { 4, "Uskayaw will force your foes to share their pain." },
+      { 3, "Uskayaw will force your foes to helplessly watch your dance.",
+           "Uskayaw will no longer force your foes to helplessly watch your dance."},
+      { 4, "Uskayaw will force your foes to share their pain.",
+           "Uskayaw will no longer force your foes to share their pain."},
       { 5, ABIL_USKAYAW_GRAND_FINALE, "merge with and destroy a victim" },
     },
 
