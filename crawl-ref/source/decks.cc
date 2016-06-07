10438618ed6fcb42fef6d70a9a44d6b17c7993a2
Chris Campbell <chriscampbell89@gmail.com>
2016-06-07 23:40:59 +0100

    Make the Wraith card ignore rN
    Undead still get a free pass on a couple of other Punishment cards (Famine and
    Swine).

--- crawl-ref/source/decks.cc
+++ crawl-ref/source/decks.cc
@@ -2698,7 +2698,7 @@ void card_effect(card_type which_card, deck_rarity_type rarity,
     case CARD_WARPWRIGHT:       _warpwright_card(power, rarity); break;
     case CARD_SHAFT:            _shaft_card(power, rarity); break;
     case CARD_TOMB:             entomb(10 + power/20 + random2(power/4)); break;
-    case CARD_WRAITH:           drain_player(power / 4, false); break;
+    case CARD_WRAITH:           drain_player(power / 4, false, true); break;
     case CARD_WRATH:            _godly_wrath(); break;
     case CARD_CRUSADE:          _crusade_card(power, rarity); break;
     case CARD_SUMMON_DEMON:     _summon_demon_card(power, rarity); break;
