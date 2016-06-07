crawl/crawl

commit a6dc20555d9a487245e9fcc00d491c10d7fa0fa6

Don't crash when a ranged attacker dies (elliptic)
During the attack.

There are probably more bugs lurking here.

crawl-ref/source/ranged_attack.cc
@@ -29,12 +29,14 @@ ranged_attack::ranged_attack(actor *attk, actor *defn, item_def *proj,
                              bool tele, actor *blame)
     : ::attack(attk, defn, blame), range_used(0), reflected(false),
       projectile(proj), teleport(tele), orig_to_hit(0),
-      should_alert_defender(true)
+      should_alert_defender(true), launch_type(LRET_FUMBLED)
 {
     init_attack(SK_THROWING, 0);
     kill_type = KILLED_BY_BEAM;
 
     string proj_name = projectile->name(DESC_PLAIN);
+    // init launch type early, so we can use it later in the constructor
+    launch_type = is_launched(attacker, weapon, *projectile);
 
     // [dshaligram] When changing bolt names here, you must edit
     // hiscores.cc (scorefile_entry::terse_missile_cause()) to match.
@@ -43,7 +45,7 @@ ranged_attack::ranged_attack(actor *attk, actor *defn, item_def *proj,
         kill_type = KILLED_BY_SELF_AIMED;
         aux_source = proj_name;
     }
-    else if (is_launched(attacker, weapon, *projectile) == LRET_LAUNCHED)
+    else if (launch_type == LRET_LAUNCHED)
     {
         aux_source = make_stringf("Shot with a%s %s by %s",
                  (is_vowel(proj_name[0]) ? "n" : ""), proj_name.c_str(),
@@ -298,8 +300,7 @@ bool ranged_attack::handle_phase_hit()
         }
     }
 
-    if (using_weapon()
-        || is_launched(attacker, weapon, *projectile) == LRET_THROWN)
+    if (using_weapon() || launch_type == LRET_THROWN)
     {
         if (using_weapon()
             && apply_damage_brand(projectile->name(DESC_THE).c_str()))
@@ -323,13 +324,12 @@ bool ranged_attack::handle_phase_hit()
 
 bool ranged_attack::using_weapon()
 {
-    return weapon
-           && is_launched(attacker, weapon, *projectile) == LRET_LAUNCHED;
+    return weapon && launch_type == LRET_LAUNCHED;
 }
 
 int ranged_attack::weapon_damage()
 {
-    if (is_launched(attacker, weapon, *projectile) == LRET_FUMBLED)
+    if (launch_type == LRET_FUMBLED)
         return 0;
 
     int dam = property(*projectile, PWPN_DAMAGE);
@@ -355,7 +355,7 @@ int ranged_attack::weapon_damage()
 int ranged_attack::calc_base_unarmed_damage()
 {
     // No damage bonus for throwing non-throwing weapons.
-    if (is_launched(attacker, weapon, *projectile) == LRET_FUMBLED)
+    if (launch_type == LRET_FUMBLED)
         return 0;
 
     int damage = you.skill_rdiv(wpn_skill);

crawl-ref/source/ranged_attack.h
@@ -57,6 +57,7 @@ private:
     bool teleport;
     int orig_to_hit;
     bool should_alert_defender;
+    launch_retval launch_type;
 };
 
 #endif

commit 599b5d79c63664ebfc9cc43e0af2bcb19e12532d

Count projectiles as launched correctly (Jafet)
Broken by a6dc205.

crawl-ref/source/itemprop-enum.h
@@ -210,6 +210,7 @@ enum jewellery_type
 
 enum launch_retval
 {
+    LRET_BUGGY = -1, // could be 0 maybe? TODO: test
     LRET_FUMBLED,
     LRET_LAUNCHED,
     LRET_THROWN,

crawl-ref/source/ranged_attack.cc
@@ -29,7 +29,7 @@ ranged_attack::ranged_attack(actor *attk, actor *defn, item_def *proj,
                              bool tele, actor *blame)
     : ::attack(attk, defn, blame), range_used(0), reflected(false),
       projectile(proj), teleport(tele), orig_to_hit(0),
-      should_alert_defender(true), launch_type(LRET_FUMBLED)
+      should_alert_defender(true), launch_type(LRET_BUGGY)
 {
     init_attack(SK_THROWING, 0);
     kill_type = KILLED_BY_BEAM;
@@ -324,7 +324,9 @@ bool ranged_attack::handle_phase_hit()
 
 bool ranged_attack::using_weapon()
 {
-    return weapon && launch_type == LRET_LAUNCHED;
+    return weapon && launch_type == LRET_LAUNCHED
+                     || (launch_type == LRET_BUGGY // not initialized
+                         && is_launched(attacker, weapon, *projectile));
 }
 
 int ranged_attack::weapon_damage()

crawl-ref/source/throw.cc
@@ -160,6 +160,7 @@ void fire_target_behaviour::set_prompt()
         case LRET_FUMBLED:  msg << "Tossing away "; break;
         case LRET_LAUNCHED: msg << "Firing ";             break;
         case LRET_THROWN:   msg << "Throwing ";           break;
+        case LRET_BUGGY:    msg << "Bugging "; break;
         }
     }
 
@@ -832,6 +833,9 @@ bool throw_it(bolt &pbolt, int throw_2, dist *target)
         break;
     case LRET_FUMBLED:
         practise(EX_WILL_THROW_OTHER);
+        break;
+    case LRET_BUGGY:
+        dprf("Unknown launch type for weapon."); // should never happen :)
         break;
     }
 