eac0f024db65402d1ef5b46b41431480fae4e9b3
Chris Campbell <chriscampbell89@gmail.com>
2016-06-09 21:19:05 +0100

    Reduce the effectiveness of amulets of magic regeneration
    It's still a fairly strong effect for those using MP heavily.

--- crawl-ref/source/player-reacts.cc
+++ crawl-ref/source/player-reacts.cc
@@ -970,7 +970,7 @@ static void _regenerate_hp_and_mp(int delay)
         if (player_mutation_level(MUT_MANA_REGENERATION))
             mp_regen_countup *= 2;
         if (you.wearing(EQ_AMULET, AMU_MANA_REGENERATION))
-            mp_regen_countup += div_rand_round(20 * delay, BASELINE_DELAY);
+            mp_regen_countup += div_rand_round(15 * delay, BASELINE_DELAY);
 
         you.magic_points_regeneration += mp_regen_countup;
     }
