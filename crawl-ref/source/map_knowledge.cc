crawl/crawl

commit 590be9041b806ce4c3b516214c6f8a988066850b

Insta-remove player clouds memory when outside LOS
So players can tell what's going on.

crawl-ref/source/map_knowledge.cc
@@ -220,3 +220,41 @@ map_feature get_cell_map_feature(const map_cell& cell)
         return MF_UNSEEN;
     return base_feature;
 }
+
+/// Iter over all known-but-unseen clouds & remove known-gone clouds.
+void update_cloud_knowledge()
+{
+    for (int x = X_BOUND_1; x <= X_BOUND_2; ++x)
+    {
+        for (int y = Y_BOUND_1; y <= Y_BOUND_2; ++y)
+        {
+            if (env.map_knowledge[x][y].update_cloud_state())
+            {
+#ifdef USE_TILE
+                tile_draw_map_cell({x, y}, true);
+#endif
+#ifdef USE_TILE_WEB
+                tiles.mark_for_redraw(gp);
+#endif
+            }
+        }
+    }
+}
+
+/// If there's a cloud in this cell that we know should be gone, remove it.
+/// Returns true if a cloud was removed.
+bool map_cell::update_cloud_state()
+{
+    if (visible())
+        return false; // we're already up-to-date
+
+    // player clouds vanish instantly out of los
+    if (_cloud && _cloud->killer == KILL_YOU_MISSILE)
+    {
+        clear_cloud();
+        return true;
+    }
+
+    // TODO: track decay & vanish appropriately (based on some worst case?)
+    return false;
+}

crawl-ref/source/cloud.cc
@@ -528,6 +528,8 @@ void manage_clouds()
 
         _dissipate_cloud(cloud);
     }
+
+    update_cloud_knowledge();
 }
 
 static void _maybe_leave_water(const coord_def pos)

crawl-ref/source/map_knowledge.h
@@ -269,6 +269,8 @@ struct map_cell
         }
     }
 
+    bool update_cloud_state();
+
     bool known() const
     {
         return !!(flags & MAP_GRID_KNOWN);
@@ -317,6 +319,8 @@ void set_terrain_visible(const coord_def c);
 void clear_terrain_visibility();
 
 int count_detected_mons();
+
+void update_cloud_knowledge();
 
 void clear_map(bool clear_items = true, bool clear_mons = true);
 

commit 2a448dbc0d068c8fb6b0392bf76030323f55ccfd

Fix WebTiles compilation
broken in 590be90.

crawl-ref/source/map_knowledge.cc
@@ -234,7 +234,7 @@ void update_cloud_knowledge()
                 tile_draw_map_cell({x, y}, true);
 #endif
 #ifdef USE_TILE_WEB
-                tiles.mark_for_redraw(gp);
+                tiles.mark_for_redraw({x, y});
 #endif
             }
         }