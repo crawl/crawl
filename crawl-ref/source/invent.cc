1f896ece557b55fb7d8a45fb19d1222413ad06f1
Nicholas Feinberg <pleasingfung@gmail.com>
2016-06-13 09:26:17 -0700

    Don't allow IDing identified decks (koboldina)

--- crawl-ref/source/invent.cc
+++ crawl-ref/source/invent.cc
@@ -996,7 +996,7 @@ bool item_is_selected(const item_def &i, int selector)
         return itype == OBJ_ARMOUR && item_is_equipped(i);
 
     case OSEL_UNIDENT:
-        return !fully_identified(i) || (is_deck(i) && !top_card_is_known(i));
+        return !fully_identified(i);
 
     case OBJ_MISSILES:
         return itype == OBJ_MISSILES || itype == OBJ_WEAPONS;
