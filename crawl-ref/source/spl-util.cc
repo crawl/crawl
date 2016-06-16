crawl/crawl

commit aa754e77b59fa631ace6e6e5f413e07c6e470f47

Move a bunch of spell aborts into spell_uselessness_reason.
Which fixes #10488.

crawl-ref/source/spl-util.cc
@@ -21,6 +21,7 @@
 #include "env.h"
 #include "godpassive.h"
 #include "godabil.h"
+#include "itemprop.h"
 #include "libutil.h"
 #include "message.h"
 #include "notes.h"
@@ -1196,8 +1197,10 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
         break;
 
     case SPELL_SWIFTNESS:
-        if (temp && !prevent)
+        if (temp)
         {
+            if (you.duration[DUR_SWIFTNESS])
+                return "this spell is already in effect.";
             if (player_movement_speed() <= FASTEST_PLAYER_MOVE_SPEED)
                 return "you're already traveling as fast as you can.";
             if (you.is_stationary())
@@ -1218,10 +1221,17 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
 
     case SPELL_REPEL_MISSILES:
         if (temp && (player_mutation_level(MUT_DISTORTION_FIELD) == 3
-                        || you.scan_artefacts(ARTP_RMSL, true)))
+                     || you.scan_artefacts(ARTP_RMSL, true)
+                     || you.attribute[ATTR_REPEL_MISSILES]
+                     || you.attribute[ATTR_DEFLECT_MISSILES]))
         {
             return "you're already repelling missiles.";
         }
+        break;
+
+    case SPELL_DEFLECT_MISSILES:
+        if (temp && you.attribute[ATTR_DEFLECT_MISSILES])
+            return "you're already deflecting missiles.";
         break;
 
     case SPELL_STATUE_FORM:
@@ -1254,6 +1264,14 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
     case SPELL_PORTAL_PROJECTILE:
     case SPELL_WARP_BRAND:
     case SPELL_EXCRUCIATING_WOUNDS:
+        if (temp
+            && (!you.weapon()
+                || you.weapon()->base_type != OBJ_WEAPONS
+                || !is_brandable_weapon(*you.weapon(), true)))
+        {
+            return "you aren't wielding an enchantable weapon.";
+        }
+        // intentional fallthrough
     case SPELL_SPECTRAL_WEAPON:
         if (you.species == SP_FELID)
             return "this spell is useless without hands.";
@@ -1261,8 +1279,8 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
 
     case SPELL_LEDAS_LIQUEFACTION:
         if (temp && (!you.stand_on_solid_ground()
-                        || you.duration[DUR_LIQUEFYING]
-                        || liquefied(you.pos())))
+                     || you.duration[DUR_LIQUEFYING]
+                     || liquefied(you.pos())))
         {
             return "you must stand on solid ground to cast this.";
         }
@@ -1274,7 +1292,19 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
         break;
 
     case SPELL_BORGNJORS_REVIVIFICATION:
+        if (temp && you.hp == you.hp_max)
+            return "you cannot be healed further.";
+        if (temp && you.hp_max < 21)
+            return "you lack the resilience to cast this spell.";
+        // Prohibited to all undead.
+        if (you.undead_state(temp))
+            return "you're too dead.";
+        break;
     case SPELL_DEATHS_DOOR:
+        if (temp && you.duration[DUR_EXHAUSTED])
+            return "you are too exhausted to enter Death's door!";
+        if (temp && you.duration[DUR_DEATHS_DOOR])
+            return "your appeal for an extension has been denied.";
         // Prohibited to all undead.
         if (you.undead_state(temp))
             return "you're too dead.";
@@ -1293,6 +1323,24 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
         {
             return "you can't be poisoned.";
         }
+        if (temp && !you.duration[DUR_POISONING])
+            return "you aren't poisoned right now.";
+        break;
+
+    case SPELL_OZOCUBUS_ARMOUR:
+        if (temp && player_effectively_in_light_armour())
+            return "your body armour is too heavy.";
+        if (temp && you.form == TRAN_STATUE)
+            return "the film of ice won't work on stone.";
+        if (temp && you.duration[DUR_FIRE_SHIELD])
+            return "your ring of flames would instantly melt the ice.";
+        break;
+
+    case SPELL_CIGOTUVIS_EMBRACE:
+        if (temp && you.form == TRAN_STATUE)
+            return "the corpses won't embrace your stony flesh.";
+        if (temp && you.duration[DUR_ICY_ARMOUR])
+            return "the corpses won't embrace your icy flesh.";
         break;
 
     case SPELL_SUBLIMATION_OF_BLOOD:
@@ -1327,6 +1375,11 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
     case SPELL_SUMMON_FOREST:
         if (temp && you.duration[DUR_FORESTED])
             return "you can only summon one forest at a time.";
+        break;
+
+    case SPELL_PASSWALL:
+        if (temp && you.is_stationary())
+            return "you can't move";
         break;
 
     case SPELL_ANIMATE_DEAD:

crawl-ref/source/spl-damage.cc
@@ -71,12 +71,6 @@
 //
 spret_type cast_delayed_fireball(bool fail)
 {
-    if (you.attribute[ATTR_DELAYED_FIREBALL])
-    {
-        mpr("You are already charged.");
-        return SPRET_ABORT;
-    }
-
     fail_check();
     // Okay, this message is weak but functional. - bwr
     mpr("You feel magically charged.");

crawl-ref/source/spl-other.cc
@@ -27,12 +27,6 @@
 
 spret_type cast_cure_poison(int pow, bool fail)
 {
-    if (!you.duration[DUR_POISONING])
-    {
-        canned_msg(MSG_NOTHING_HAPPENS);
-        return SPRET_ABORT;
-    }
-
     fail_check();
     reduce_player_poison((15 + roll_dice(3, pow / 2)) * 1000);
 
@@ -269,12 +263,6 @@ static bool _feat_is_passwallable(dungeon_feature_type feat)
 
 spret_type cast_passwall(const coord_def& delta, int pow, bool fail)
 {
-    if (you.is_stationary())
-    {
-        canned_msg(MSG_CANNOT_MOVE);
-        return SPRET_ABORT;
-    }
-
     coord_def dest;
     for (dest = you.pos() + delta;
          in_bounds(dest) && _feat_is_passwallable(grd(dest));
@@ -360,12 +348,6 @@ spret_type cast_intoxicate(int pow, bool fail)
 
 spret_type cast_darkness(int pow, bool fail)
 {
-    if (you.haloed())
-    {
-        mpr("It would have no effect in that bright light!");
-        return SPRET_ABORT;
-    }
-
     fail_check();
     if (you.duration[DUR_DARKNESS])
         mprf(MSGCH_DURATION, "It gets a bit darker.");

crawl-ref/source/spl-selfench.cc
@@ -42,30 +42,19 @@ int allowed_deaths_door_hp()
 
 spret_type cast_deaths_door(int pow, bool fail)
 {
-    if (you.undead_state())
-        mpr("You're already dead!");
-    else if (you.duration[DUR_EXHAUSTED])
-        mpr("You are too exhausted to enter Death's door!");
-    else if (you.duration[DUR_DEATHS_DOOR])
-        mpr("Your appeal for an extension has been denied.");
-    else
-    {
-        fail_check();
-        mpr("You stand defiantly in death's doorway!");
-        mprf(MSGCH_SOUND, "You seem to hear sand running through an hourglass...");
+    fail_check();
+    mpr("You stand defiantly in death's doorway!");
+    mprf(MSGCH_SOUND, "You seem to hear sand running through an hourglass...");
 
-        set_hp(allowed_deaths_door_hp());
-        deflate_hp(you.hp_max, false);
+    set_hp(allowed_deaths_door_hp());
+    deflate_hp(you.hp_max, false);
 
-        you.set_duration(DUR_DEATHS_DOOR, 10 + random2avg(13, 3)
-                                           + (random2(pow) / 10));
+    you.set_duration(DUR_DEATHS_DOOR, 10 + random2avg(13, 3)
+                                       + (random2(pow) / 10));
 
-        if (you.duration[DUR_DEATHS_DOOR] > 25 * BASELINE_DELAY)
-            you.duration[DUR_DEATHS_DOOR] = (23 + random2(5)) * BASELINE_DELAY;
-        return SPRET_SUCCESS;
-    }
-
-    return SPRET_ABORT;
+    if (you.duration[DUR_DEATHS_DOOR] > 25 * BASELINE_DELAY)
+        you.duration[DUR_DEATHS_DOOR] = (23 + random2(5)) * BASELINE_DELAY;
+    return SPRET_SUCCESS;
 }
 
 void remove_ice_armour()
@@ -77,24 +66,6 @@ void remove_ice_armour()
 
 spret_type ice_armour(int pow, bool fail)
 {
-    if (!player_effectively_in_light_armour())
-    {
-        mpr("Your body armour is too heavy.");
-        return SPRET_ABORT;
-    }
-
-    if (you.form == TRAN_STATUE)
-    {
-        mpr("The film of ice won't work on stone.");
-        return SPRET_ABORT;
-    }
-
-    if (you.duration[DUR_FIRE_SHIELD])
-    {
-        mpr("Your ring of flames would instantly melt the ice.");
-        return SPRET_ABORT;
-    }
-
     fail_check();
 
     if (you.duration[DUR_ICY_ARMOUR])
@@ -179,18 +150,6 @@ int harvest_corpses(const actor &harvester, bool dry_run)
  */
 spret_type corpse_armour(int pow, bool fail)
 {
-    if (you.form == TRAN_STATUE)
-    {
-        mpr("The corpses won't embrace your stony flesh.");
-        return SPRET_ABORT;
-    }
-
-    if (you.duration[DUR_ICY_ARMOUR])
-    {
-        mpr("The corpses won't embrace your icy flesh.");
-        return SPRET_ABORT;
-    }
-
     // Could check carefully to see if it's even possible that there are any
     // valid corpses/skeletons in LOS (any piles with stuff under them, etc)
     // before failing, but it's better to be simple + predictable from the
@@ -222,13 +181,6 @@ spret_type corpse_armour(int pow, bool fail)
 
 spret_type missile_prot(int pow, bool fail)
 {
-    if (you.attribute[ATTR_REPEL_MISSILES]
-        || you.attribute[ATTR_DEFLECT_MISSILES]
-        || player_equip_unrand(UNRAND_AIR))
-    {
-        mpr("You are already protected from missiles.");
-        return SPRET_ABORT;
-    }
     fail_check();
     you.attribute[ATTR_REPEL_MISSILES] = 1;
     mpr("You feel protected from missiles.");
@@ -237,11 +189,6 @@ spret_type missile_prot(int pow, bool fail)
 
 spret_type deflection(int pow, bool fail)
 {
-    if (you.attribute[ATTR_DEFLECT_MISSILES])
-    {
-        mpr("You are already deflecting missiles.");
-        return SPRET_ABORT;
-    }
     fail_check();
     you.attribute[ATTR_DEFLECT_MISSILES] = 1;
     mpr("You feel very safe from missiles.");
@@ -263,54 +210,27 @@ spret_type cast_regen(int pow, bool fail)
 
 spret_type cast_revivification(int pow, bool fail)
 {
-    if (you.hp == you.hp_max)
-        canned_msg(MSG_NOTHING_HAPPENS);
-    else if (you.hp_max < 21)
-        mpr("You lack the resilience to cast this spell.");
-    else
+    fail_check();
+    mpr("Your body is healed in an amazingly painful way.");
+
+    const int loss = 6 + binomial(9, 8, pow);
+    dec_max_hp(loss * you.hp_max / 100);
+    set_hp(you.hp_max);
+
+    if (you.duration[DUR_DEATHS_DOOR])
     {
-        fail_check();
-        mpr("Your body is healed in an amazingly painful way.");
-
-        const int loss = 6 + binomial(9, 8, pow);
-        dec_max_hp(loss * you.hp_max / 100);
-        set_hp(you.hp_max);
-
-        if (you.duration[DUR_DEATHS_DOOR])
-        {
-            mprf(MSGCH_DURATION, "Your life is in your own hands once again.");
-            // XXX: better cause name?
-            paralyse_player("Death's Door abortion", 5 + random2(5));
-            confuse_player(10 + random2(10));
-            you.duration[DUR_DEATHS_DOOR] = 0;
-            you.increase_duration(DUR_EXHAUSTED, roll_dice(1,3));
-        }
-        return SPRET_SUCCESS;
+        mprf(MSGCH_DURATION, "Your life is in your own hands once again.");
+        // XXX: better cause name?
+        paralyse_player("Death's Door abortion", 5 + random2(5));
+        confuse_player(10 + random2(10));
+        you.duration[DUR_DEATHS_DOOR] = 0;
+        you.increase_duration(DUR_EXHAUSTED, roll_dice(1,3));
     }
-
-    return SPRET_ABORT;
+    return SPRET_SUCCESS;
 }
 
 spret_type cast_swiftness(int power, bool fail)
 {
-    if (you.is_stationary())
-    {
-        canned_msg(MSG_CANNOT_MOVE);
-        return SPRET_ABORT;
-    }
-
-    if (!you.duration[DUR_SWIFTNESS] && player_movement_speed() <= 6)
-    {
-        mpr("You can't move any more quickly.");
-        return SPRET_ABORT;
-    }
-
-    if (you.duration[DUR_SWIFTNESS])
-    {
-        mpr("This spell is already in effect.");
-        return SPRET_ABORT;
-    }
-
     fail_check();
 
     if (you.in_liquid())
@@ -434,21 +354,6 @@ spret_type cast_silence(int pow, bool fail)
 
 spret_type cast_liquefaction(int pow, bool fail)
 {
-    if (!you.stand_on_solid_ground())
-    {
-        if (!you.ground_level())
-            mpr("You can't cast this spell without touching the ground.");
-        else
-            mpr("You need to be on clear, solid ground to cast this spell.");
-        return SPRET_ABORT;
-    }
-
-    if (you.duration[DUR_LIQUEFYING] || liquefied(you.pos()))
-    {
-        mpr("The ground here is already liquefied! You'll have to wait.");
-        return SPRET_ABORT;
-    }
-
     fail_check();
     flash_view_delay(UA_PLAYER, BROWN, 80);
     flash_view_delay(UA_PLAYER, YELLOW, 80);

crawl-ref/source/spl-wpnench.cc
@@ -185,22 +185,7 @@ static int _get_brand_duration(brand_type which_brand)
  */
 spret_type brand_weapon(brand_type which_brand, int power, bool fail)
 {
-    if (!you.weapon())
-    {
-        mpr("You aren't wielding a weapon.");
-        return SPRET_ABORT;
-    }
-
     item_def& weapon = *you.weapon();
-
-    if (!is_brandable_weapon(weapon, true))
-    {
-        if (weapon.base_type != OBJ_WEAPONS)
-            mpr("This isn't a weapon.");
-        else
-            mpr("You cannot enchant this weapon.");
-        return SPRET_ABORT;
-    }
 
     bool has_temp_brand = you.duration[DUR_WEAPON_BRAND];
     // No need to brand with a brand it's already branded with.

commit 937f5bdeb83a8ec4841653b0dd091defc0be98fd

De-invert an Ozo's condition (10507)
Broken by aa754e77b59fa631ace6e6e5f413e07c6e470f47.

crawl-ref/source/spl-util.cc
@@ -1328,7 +1328,7 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
         break;
 
     case SPELL_OZOCUBUS_ARMOUR:
-        if (temp && player_effectively_in_light_armour())
+        if (temp && !player_effectively_in_light_armour())
             return "your body armour is too heavy.";
         if (temp && you.form == TRAN_STATUE)
             return "the film of ice won't work on stone.";