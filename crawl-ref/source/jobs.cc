401aa941c4ef11d071c110094a443d026dacb7ab
Alex Jurkiewicz <alex@jurkiewi.cz>
2016-06-16 16:20:34 +1000

    Double starting skill for unarmed SK_WEAPON starts.
    It's not really a hard decision between starting with n weapon skill and
    n unarmed combat skill, so double the skill for picking UC.

--- crawl-ref/source/jobs.cc
+++ crawl-ref/source/jobs.cc
@@ -135,9 +135,11 @@ void give_job_skills(job_type job)
         {
             const item_def *weap = you.weapon();
             skill = weap ? item_attack_skill(*weap) : SK_UNARMED_COMBAT;
-            //XXX: WTF?
-            if (you.species == SP_FELID && job == JOB_FIGHTER)
-                amount += 2;
+            if (skill == SK_UNARMED_COMBAT)
+                if (!you.species == SP_FELID)
+                    amount *= 2;
+                else
+                    amount += 2;
             // Don't give throwing hunters Short Blades skill.
             if (job_gets_ranged_weapons(job) && !(weap && is_range_weapon(*weap)))
                 skill = SK_THROWING;
