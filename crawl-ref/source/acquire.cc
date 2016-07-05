b89c11b056d5214cafaaa6d31f41278b09dd354d
Nicholas Feinberg <pleasingfung@gmail.com>
2016-07-04 19:29:21 -0700

    Remove ammo ?acquirement
    It was almost never a good choice, and seemed unlikely to become
    an interesting one.
    
    Gifting gods (e.g. Oka) are unaffected.

--- crawl-ref/source/acquire.cc
+++ crawl-ref/source/acquire.cc
@@ -1542,12 +1542,11 @@ bool acquirement(object_class_type class_wanted, int agent,
         { OBJ_ARMOUR,     "Armour" },
         { OBJ_JEWELLERY,  "Jewellery" },
         { OBJ_BOOKS,      "Book" },
-        { OBJ_STAVES,     "Staff" },
+        { OBJ_STAVES,     "Staff " },
         { OBJ_WANDS,      "Wand" },
         { OBJ_MISCELLANY, "Misc. Evocable" },
         { OBJ_FOOD,       0 }, // amended below
         { OBJ_GOLD,       "Gold" },
-        { OBJ_MISSILES,   "Ammunition" },
     };
     ASSERT(acq_classes[7].type == OBJ_FOOD);
     acq_classes[7].name = you_worship(GOD_FEDHAS) ? "Fruit":
