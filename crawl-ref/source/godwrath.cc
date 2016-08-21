117139cce9d2eb26a5d0a36eff0d86dedbb322fb
Chris Campbell <chriscampbell89@gmail.com>
2016-08-21 20:27:53 +0100

    Adjust god descriptions

--- crawl-ref/source/godwrath.cc
+++ crawl-ref/source/godwrath.cc
@@ -1295,7 +1295,7 @@ static bool _nemelex_retribution()
     // card theme
     const god_type god = GOD_NEMELEX_XOBEH;
 
-    simple_god_message(" makes you draw from the Deck of Punishment.", god);
+    simple_god_message(" makes you draw from the deck of Punishment.", god);
     draw_from_deck_of_punishment();
     return true;
 }
