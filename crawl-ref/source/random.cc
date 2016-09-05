7db0936f4ece1cf1e95d792552b4934925808d8f
Alan Malloy <alan@malloys.org>
2016-09-05 00:24:30 -0700

    Remove char* overload for r_c_w now that it's templated right

--- crawl-ref/source/random.cc
+++ crawl-ref/source/random.cc
@@ -65,30 +65,6 @@ int random_range(int low, int high, int nrolls)
     return low + roll;
 }
 
-const char* random_choose_weighted(int weight, const char* first, ...)
-{
-    va_list args;
-    va_start(args, first);
-    const char* chosen = first;
-    int cweight = weight, nargs = 100;
-
-    while (nargs-- > 0)
-    {
-        const int nweight = va_arg(args, int);
-        if (!nweight)
-            break;
-
-        const char* choice = va_arg(args, const char*);
-        if (random2(cweight += nweight) < nweight)
-            chosen = choice;
-    }
-
-    va_end(args);
-    ASSERT(nargs > 0);
-
-    return chosen;
-}
-
 static int _random2(int max, int rng)
 {
     if (max <= 1)
