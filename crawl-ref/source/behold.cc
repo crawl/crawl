crawl/crawl

commit a8b2baea54554bf9aa8bff621be2bee964bbaccd

Make more functions take monster& instead of monster*

crawl-ref/source/behold.cc
@@ -15,24 +15,24 @@
 #include "state.h"
 
 // Add a monster to the list of beholders.
-void player::add_beholder(const monster* mon, bool axe)
+void player::add_beholder(const monster& mon, bool axe)
 {
     if (is_sanctuary(pos()) && !axe)
     {
         if (mons_is_siren_beholder(mon))
         {
-            if (can_see(*mon))
+            if (can_see(mon))
             {
                 mprf("%s's singing sounds muted, and has no effect on you.",
-                     mon->name(DESC_THE).c_str());
+                     mon.name(DESC_THE).c_str());
             }
             else
                 mpr("The melody is strangely muted, and has no effect on you.");
         }
         else
         {
-            if (can_see(*mon))
-                mprf("%s's is no longer quite as mesmerising!", mon->name(DESC_THE).c_str());
+            if (can_see(mon))
+                mprf("%s's is no longer quite as mesmerising!", mon.name(DESC_THE).c_str());
             else
                 mpr("Your mesmeriser suddenly seems less interesting!");
         }
@@ -43,18 +43,18 @@ void player::add_beholder(const monster* mon, bool axe)
     if (!duration[DUR_MESMERISED])
     {
         set_duration(DUR_MESMERISED, random_range(7, 15), 15);
-        beholders.push_back(mon->mid);
+        beholders.push_back(mon.mid);
         if (!axe)
         {
             mprf(MSGCH_WARN, "You are mesmerised by %s!",
-                             mon->name(DESC_THE).c_str());
+                             mon.name(DESC_THE).c_str());
         }
     }
     else
     {
         increase_duration(DUR_MESMERISED, random_range(5, 8), 15);
         if (!beheld_by(mon))
-            beholders.push_back(mon->mid);
+            beholders.push_back(mon.mid);
     }
 }
 
@@ -66,9 +66,9 @@ bool player::beheld() const
 }
 
 // Whether player is mesmerised by the given monster.
-bool player::beheld_by(const monster* mon) const
+bool player::beheld_by(const monster &mon) const
 {
-    return find(begin(beholders), end(beholders), mon->mid) != end(beholders);
+    return find(begin(beholders), end(beholders), mon.mid) != end(beholders);
 }
 
 // Checks whether a beholder keeps you from moving to
@@ -99,10 +99,10 @@ monster* player::get_any_beholder() const
 }
 
 // Removes a monster from the list of beholders if present.
-void player::remove_beholder(const monster* mon)
+void player::remove_beholder(const monster& mon)
 {
     for (unsigned int i = 0; i < beholders.size(); i++)
-        if (beholders[i] == mon->mid)
+        if (beholders[i] == mon.mid)
         {
             beholders.erase(beholders.begin() + i);
             _removed_beholder();
@@ -132,10 +132,10 @@ void player::beholders_check_noise(int loudness, bool axe)
     }
 }
 
-static void _removed_beholder_msg(const monster* mon)
+static void _removed_beholder_msg(const monster& mon)
 {
-    if (!mon || !mon->alive() || !mons_is_siren_beholder(mon)
-        || mon->submerged() || !you.see_cell(mon->pos()))
+    if (!mon.alive() || !mons_is_siren_beholder(mon)
+        || mon.submerged() || !you.see_cell(mon.pos()))
     {
         return;
     }
@@ -144,18 +144,18 @@ static void _removed_beholder_msg(const monster* mon)
     {
         if (mons_is_siren_beholder(mon))
         {
-            if (you.can_see(*mon))
+            if (you.can_see(mon))
             {
                 mprf("%s's singing becomes strangely muted.",
-                     mon->name(DESC_THE).c_str());
+                     mon.name(DESC_THE).c_str());
             }
             else
                 mpr("Something's singing becomes strangely muted.");
         }
         else
         {
-            if (you.can_see(*mon))
-                mprf("%s's is no longer quite as mesmerising!", mon->name(DESC_THE).c_str());
+            if (you.can_see(mon))
+                mprf("%s's is no longer quite as mesmerising!", mon.name(DESC_THE).c_str());
             else
                 mpr("Your mesmeriser suddenly seems less interesting!");
         }
@@ -163,14 +163,14 @@ static void _removed_beholder_msg(const monster* mon)
         return;
     }
 
-    if (you.can_see(*mon))
+    if (you.can_see(mon))
     {
-        if (silenced(you.pos()) || silenced(mon->pos()))
+        if (silenced(you.pos()) || silenced(mon.pos()))
         {
             if (mons_is_siren_beholder(mon))
             {
                 mprf("You can no longer hear %s's singing!",
-                     mon->name(DESC_THE).c_str());
+                     mon.name(DESC_THE).c_str());
             }
             else
                 mpr("The silence clears your mind.");
@@ -178,9 +178,9 @@ static void _removed_beholder_msg(const monster* mon)
         }
 
         if (mons_is_siren_beholder(mon))
-            mprf("%s stops singing.", mon->name(DESC_THE).c_str());
+            mprf("%s stops singing.", mon.name(DESC_THE).c_str());
         else
-            mprf("%s is no longer quite as mesmerising!", mon->name(DESC_THE).c_str());
+            mprf("%s is no longer quite as mesmerising!", mon.name(DESC_THE).c_str());
 
         return;
     }
@@ -209,7 +209,7 @@ void player::update_beholders()
             // printing any subsequent messages, or a --more-- can
             // crash (#6547).
             _removed_beholder(true);
-            _removed_beholder_msg(mon);
+            _removed_beholder_msg(*mon);
         }
     }
     if (removed)
@@ -228,7 +228,7 @@ void player::update_beholder(const monster* mon)
             // Do this dance to clear the duration before printing messages
             // (#8844), but still print all messages in the right order.
             _removed_beholder(true);
-            _removed_beholder_msg(mon);
+            _removed_beholder_msg(*mon);
             _removed_beholder();
             return;
         }
@@ -268,7 +268,7 @@ bool player::possible_beholder(const monster* mon) const
             && !mon->confused()
             && !mon->asleep() && !mon->cannot_move()
             && !mon->berserk_or_insane()
-            && !mons_is_fleeing(mon)
+            && !mons_is_fleeing(*mon)
             && !is_sanctuary(pos())
           || player_equip_unrand(UNRAND_DEMON_AXE));
 }

crawl-ref/source/areas.cc
@@ -333,7 +333,7 @@ bool remove_sanctuary(bool did_attack)
     // Now that the sanctuary is gone, monsters aren't afraid of it
     // anymore.
     for (monster_iterator mi; mi; ++mi)
-        mons_stop_fleeing_from_sanctuary(*mi);
+        mons_stop_fleeing_from_sanctuary(**mi);
 
     if (is_resting())
         stop_running();
@@ -449,12 +449,12 @@ void create_sanctuary(const coord_def& center, int time)
                 mon->behaviour = BEH_SEEK;
                 behaviour_event(mon, ME_EVAL, &you);
             }
-            else if (!mon->wont_attack() && mons_is_influenced_by_sanctuary(mon))
+            else if (!mon->wont_attack() && mons_is_influenced_by_sanctuary(*mon))
             {
-                mons_start_fleeing_from_sanctuary(mon);
+                mons_start_fleeing_from_sanctuary(*mon);
 
                 // Check to see that monster is actually fleeing.
-                if (mons_is_fleeing(mon) && you.can_see(*mon))
+                if (mons_is_fleeing(*mon) && you.can_see(*mon))
                 {
                     scare_count++;
                     seen_mon = mon;

crawl-ref/source/art-func.h
@@ -602,15 +602,17 @@ static monster* _find_nearest_possible_beholder()
 static void _DEMON_AXE_world_reacts(item_def *item)
 {
 
-    monster* closest = _find_nearest_possible_beholder();
+    monster* mon = _find_nearest_possible_beholder();
 
-    if (!closest)
+    if (!mon)
         return;
+
+    monster& closest = *mon;
 
     if (!you.beheld_by(closest))
     {
         mprf("Visions of slaying %s flood into your mind.",
-             closest->name(DESC_THE).c_str());
+             closest.name(DESC_THE).c_str());
 
         // The monsters (if any) currently mesmerising the player do not include
         // this monster. To avoid trapping the player, all other beholders

crawl-ref/source/attitude-change.cc
@@ -81,7 +81,7 @@ void beogh_follower_convert(monster* mons, bool orc_hit)
         && !testbits(mons->flags, MF_ATT_CHANGE_ATTEMPT)
         && !mons->friendly()
         && you.visible_to(mons) && !mons->asleep()
-        && !mons_is_confused(mons) && !mons->paralysed())
+        && !mons_is_confused(*mons) && !mons->paralysed())
     {
         mons->flags |= MF_ATT_CHANGE_ATTEMPT;
 
@@ -100,7 +100,7 @@ void beogh_follower_convert(monster* mons, bool orc_hit)
 
 void slime_convert(monster* mons)
 {
-    if (have_passive(passive_t::neutral_slimes) && mons_is_slime(mons)
+    if (have_passive(passive_t::neutral_slimes) && mons_is_slime(*mons)
         && !mons->is_shapeshifter()
         && !mons->neutral()
         && !mons->friendly()
@@ -225,7 +225,7 @@ bool beogh_followers_abandon_you()
 
             if (you.visible_to(mons)
                 && !mons->asleep()
-                && !mons_is_confused(mons)
+                && !mons_is_confused(*mons)
                 && !mons->cannot_act())
             {
                 const int hd = mons->get_experience_level();
@@ -366,7 +366,7 @@ static void _fedhas_neutralise_plant(monster* plant)
 static void _jiyva_convert_slime(monster* slime)
 {
     ASSERT(slime); // XXX: change to monster &slime
-    ASSERT(mons_is_slime(slime));
+    ASSERT(mons_is_slime(*slime));
 
     behaviour_event(slime, ME_ALERT);
 

crawl-ref/source/beam.cc
@@ -5128,7 +5128,7 @@ bool bolt::ignores_monster(const monster* mon) const
     // All kinds of beams go past orbs of destruction and friendly
     // battlespheres. We don't check mon->is_projectile() because that
     // check includes boulder beetles which should be hit.
-    if (mons_is_projectile(mon)
+    if (mons_is_projectile(*mon)
         || (mons_is_avatar(mon->type) && mons_aligned(agent(), mon)))
     {
         return true;
@@ -5582,7 +5582,7 @@ mon_resist_type bolt::apply_enchantment_to_monster(monster* mon)
         return MON_AFFECTED;
 
     case BEAM_SLEEP:
-        if (mons_just_slept(mon))
+        if (mons_just_slept(*mon))
             return MON_UNAFFECTED;
 
         mon->put_to_sleep(agent(), ench_power);

crawl-ref/source/dactions.cc
@@ -99,7 +99,7 @@ bool mons_matches_daction(const monster* mon, daction_type act)
     case DACT_ALLY_PLANT:
         // No check for friendliness since we pretend all plants became friendly
         // the moment you converted to Fedhas.
-        return mons_is_plant(mon);
+        return mons_is_plant(*mon);
     case DACT_ALLY_HEPLIAKLQANA:
     case DACT_UPGRADE_ANCESTOR:
         return mon->wont_attack() && mons_is_god_gift(mon, GOD_HEPLIAKLQANA);
@@ -124,7 +124,7 @@ bool mons_matches_daction(const monster* mon, daction_type act)
         return mons_enslaved_soul(*mon);
 
     case DACT_SLIME_NEW_ATTEMPT:
-        return mons_is_slime(mon);
+        return mons_is_slime(*mon);
 
     case DACT_KIRKE_HOGS:
         return (mon->type == MONS_HOG

crawl-ref/source/decks.cc
@@ -1768,9 +1768,9 @@ static void _elements_card(int power, deck_rarity_type rarity)
         monster hackmon;
 
         hackmon.type = mons_type;
-        mons_load_spells(&hackmon);
+        mons_load_spells(hackmon);
 
-        if (you_worship(GOD_DITHMENOS) && mons_is_fiery(&hackmon))
+        if (you_worship(GOD_DITHMENOS) && mons_is_fiery(hackmon))
         {
             _suppressed_card_message(you.religion, DID_FIRE);
             start++;

crawl-ref/source/fearmonger.cc
@@ -197,6 +197,6 @@ bool player::_possible_fearmonger(const monster* mon) const
         && !mon->asleep() && !mon->cannot_move()
         && !mon->wont_attack() && !mon->pacified()
         && !mon->berserk_or_insane()
-        && !mons_is_fleeing(mon)
+        && !mons_is_fleeing(*mon)
         && !is_sanctuary(pos());
 }

commit d50262598812aee4a4eca5c4c02904b95a5db05d

Fix nil beholder removal crash (cojito)
The root problem is that you can be mesmerized while climbing the
stairs, and it's not cleared when you get to the next level.

Crash introduced by a8b2baea54554bf9.

crawl-ref/source/behold.cc
@@ -132,8 +132,12 @@ void player::beholders_check_noise(int loudness, bool axe)
     }
 }
 
-static void _removed_beholder_msg(const monster& mon)
+static void _removed_beholder_msg(const monster *mons)
 {
+    if (!mons)
+        return;
+
+    const monster &mon = *mons;
     if (!mon.alive() || !mons_is_siren_beholder(mon)
         || mon.submerged() || !you.see_cell(mon.pos()))
     {
@@ -209,7 +213,7 @@ void player::update_beholders()
             // printing any subsequent messages, or a --more-- can
             // crash (#6547).
             _removed_beholder(true);
-            _removed_beholder_msg(*mon);
+            _removed_beholder_msg(mon);
         }
     }
     if (removed)
@@ -228,7 +232,7 @@ void player::update_beholder(const monster* mon)
             // Do this dance to clear the duration before printing messages
             // (#8844), but still print all messages in the right order.
             _removed_beholder(true);
-            _removed_beholder_msg(*mon);
+            _removed_beholder_msg(mon);
             _removed_beholder();
             return;
         }