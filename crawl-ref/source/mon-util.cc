crawl/crawl

commit 6871f4b479f2a5b157b6b1340b14d7e4e6ac0174

Dithmenos now gives piety for killing flaming dancing weapons
Signed-off-by: Naruni <kram321@gmail.com>

crawl-ref/source/mon-util.cc
@@ -821,11 +821,16 @@ bool cheibriados_thinks_mons_is_fast(const monster* mon)
     return cheibriados_monster_player_speed_delta(mon) > 0;
 }
 
-// Dithmenos also hates fire users and generally fiery beings.
+// Dithmenos also hates fire users, flaming weapons, and generally fiery beings.
 bool mons_is_fiery(const monster* mon)
 {
     if (mons_genus(mon->type) == MONS_DRACONIAN
         && draco_or_demonspawn_subspecies(mon) == MONS_RED_DRACONIAN)
+    {
+        return true;
+    }
+    if (mons_genus(mon->type) == MONS_DANCING_WEAPON
+        && mon->weapon()->brand == SPWPN_FLAMING)
     {
         return true;
     }

commit 0142e3fd4e6cf0db65267257d3fc3985d53ab0e4

Fix a Dith/Shadow Creatures crash (10666)
Broken by 6871f4b479f2a5b.

crawl-ref/source/mon-util.cc
@@ -828,7 +828,7 @@ bool mons_is_fiery(const monster& mon)
         return true;
     }
     if (mons_genus(mon.type) == MONS_DANCING_WEAPON
-        && mon.weapon()->brand == SPWPN_FLAMING)
+        && mon.weapon() && mon.weapon()->brand == SPWPN_FLAMING)
     {
         return true;
     }