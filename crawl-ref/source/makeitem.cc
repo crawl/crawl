crawl/crawl

commit 0f4b659dddc446658c1cb6387dfdd03bcb758db5

Breaks savefile compatibility.
Re-arranged book_type so that books you might find on the floor come first,
then books only given out by certain gods, and so on.  Added book types
BOOK_RANDART_LEVEL, BOOK_RANDART_THEME and BOOK_CARD_EFFECT.

Can now get randart books both from acquirement and shops/floor.  Acquirement
books have a chance of being a manual with a spell discipline skill.

Randart books have their own appearances now, and fixed level books their own
naming scheme.  Needs more entries.

Randart books aren't hilited in the menu like other randarts are; don't know
why.

Added some assertions to choose_random_weighted().


git-svn-id: https://crawl-ref.svn.sourceforge.net/svnroot/crawl-ref/trunk@7761 c06c8d41-db1a-0410-9941-cceddc491573

crawl-ref/source/makeitem.cc
@@ -2397,8 +2397,8 @@ static void _generate_scroll_item(item_def& item, int force_type,
     item.plus = 0;
 }
 
-static void _generate_book_item(item_def& item, int force_type,
-                                int item_level)
+static void _generate_book_item(item_def& item, int allow_uniques,
+                                int force_type, int item_level)
 {
     // determine special (description)
     item.special = random2(5);
@@ -2412,11 +2412,9 @@ static void _generate_book_item(item_def& item, int force_type,
     {
         do
         {
-            item.sub_type = random2(NUM_BOOKS);
+            item.sub_type = random2(NUM_NORMAL_BOOKS);
 
-            if (item.sub_type != BOOK_DESTRUCTION
-                && item.sub_type != BOOK_MANUAL
-                && book_rarity(item.sub_type) != 100
+            if (book_rarity(item.sub_type) != 100
                 && one_chance_in(10))
             {
                 item.sub_type = coinflip() ? BOOK_WIZARDRY : BOOK_POWER;
@@ -2428,9 +2426,7 @@ static void _generate_book_item(item_def& item, int force_type,
                 item.sub_type = BOOK_DESTRUCTION; // continue trying
             }
         }
-        while (item.sub_type == BOOK_DESTRUCTION
-               || item.sub_type == BOOK_MANUAL
-               || book_rarity(item.sub_type) == 100);
+        while (book_rarity(item.sub_type) == 100);
 
         // Tome of destruction: rare!
         if (item_level > 10 && x_chance_in_y(21 + item_level, 7000))
@@ -2455,6 +2451,36 @@ static void _generate_book_item(item_def& item, int force_type,
         }
         // Set number of reads possible before it "crumbles to dust".
         item.plus2 = 3 + random2(15);
+    }
+
+    // Manuals and books of destruction are rare enough without replacing
+    // them with randart books.
+    if (item.sub_type == BOOK_MANUAL || item.sub_type == BOOK_DESTRUCTION)
+        return;
+
+    // Only randomly generate randart books for OBJ_RANDOM, since randart
+    // spellbooks aren't merely of-the-same-type-but-better, but
+    // have an entirely different set of spells.
+    if (allow_uniques && item_level > 2 && force_type == OBJ_RANDOM
+        && x_chance_in_y(101 + item_level * 3, 4000))
+    {
+        // Same relative weights as acquirement
+        int choice = random_choose_weighted(
+            55, BOOK_RANDART_THEME,
+            12, BOOK_RANDART_LEVEL,
+             0);
+
+        item.sub_type = choice;
+    }
+
+    if (item.sub_type == BOOK_RANDART_THEME)
+        make_book_theme_randart(item, 0, 0, 7, 25);
+    else if (item.sub_type == BOOK_RANDART_LEVEL)
+    {
+        int max_level   = std::min( 9, std::max(1, item_level / 3) );
+        int spell_level = random_range(1, max_level);
+        int num_spells  = 7 - (spell_level + 1) / 2 + random_range(1, 2);
+        make_book_level_randart(item, spell_level, num_spells);
     }
 }
 
@@ -2756,7 +2782,7 @@ int items( int allow_uniques,       // not just true-false,
         break;
 
     case OBJ_BOOKS:
-        _generate_book_item(item, force_type, item_level);
+        _generate_book_item(item, allow_uniques, force_type, item_level);
         break;
 
     case OBJ_STAVES:

crawl-ref/source/acr.cc
@@ -433,6 +433,22 @@ static bool _item_type_can_be_artefact( int type)
             || type == OBJ_BOOKS);
 }
 
+static bool _make_book_randart(item_def &book)
+{
+    char type;
+
+    do
+    {
+        mpr("Make book fixed [t]heme or fixed [l]evel? ", MSGCH_PROMPT);
+        type = tolower(getch());
+    } while (type != 't' && type != 'l');
+
+    if (type == 'l')
+        return make_book_level_randart(book);
+    else
+        return make_book_theme_randart(book);
+}
+
 static void _do_wizard_command(int wiz_command, bool silent_fail)
 {
     ASSERT(you.wizard);
@@ -567,8 +583,16 @@ static void _do_wizard_command(int wiz_command, bool silent_fail)
                item.orig_monnum = -god;
             }
         }
- 
-        if (!make_item_randart( item ))
+
+        if (item.base_type == OBJ_BOOKS)
+        {
+            if (!_make_book_randart(item))
+            {
+                mpr("Failed to turn book into randart.");
+                break;
+            }
+        } 
+        else if (!make_item_randart( item ))
         {
             mpr("Failed to turn item into randart.");
             break;

crawl-ref/source/command.cc
@@ -1196,7 +1196,7 @@ static bool _append_books(std::string &desc, item_def &item, std::string key)
     std::vector<std::string> rods;
 
     item.base_type = OBJ_BOOKS;
-    for (int i = 0; i < NUM_BOOKS; i++)
+    for (int i = 0; i < NUM_FIXED_BOOKS; i++)
         for (int j = 0; j < 8; j++)
             if (which_spell_in_book(i, j) == type)
             {

crawl-ref/source/debug.cc
@@ -1121,7 +1121,7 @@ static bool _book_from_spell(const char* specs, item_def &item)
     if (type == SPELL_NO_SPELL)
         return (false);
 
-    for (int i = 0; i < NUM_BOOKS; i++)
+    for (int i = 0; i < NUM_FIXED_BOOKS; i++)
         for (int j = 0; j < 8; j++)
             if (which_spell_in_book(i, j) == type)
             {
@@ -1512,6 +1512,10 @@ bool get_item_by_name(item_def *item, char* specs,
             else
                 mpr( "Sorry, no books on that skill today." );
         }
+        else if (type_wanted == BOOK_RANDART_THEME)
+            make_book_theme_randart(*item);
+        else if (type_wanted == BOOK_RANDART_LEVEL)
+            make_book_level_randart(*item);
         break;
 
     case OBJ_WANDS:

crawl-ref/source/effects.cc
@@ -1262,11 +1262,11 @@ static int _find_acquirement_subtype(object_class_type class_wanted,
                 }
 
                 // If we don't have a book, try and get a new one.
-                if (type_wanted == NUM_BOOKS)
+                if (type_wanted > MAX_FIXED_BOOK)
                 {
                     do
                     {
-                        type_wanted = random2(NUM_BOOKS);
+                        type_wanted = random2(NUM_NORMAL_BOOKS);
                         if (one_chance_in(500))
                             break;
                     }
@@ -1274,12 +1274,8 @@ static int _find_acquirement_subtype(object_class_type class_wanted,
                 }
 
                 // If the book is invalid find any valid one.
-                while (book_rarity(type_wanted) == 100
-                       || type_wanted == BOOK_DESTRUCTION
-                       || type_wanted == BOOK_MANUAL)
-                {
-                    type_wanted = random2(NUM_BOOKS);
-                }
+                while (book_rarity(type_wanted) == 100)
+                    type_wanted = random2(NUM_NORMAL_BOOKS);
                 break;
 
             case OBJ_STAVES:
@@ -1421,6 +1417,93 @@ static int _find_acquirement_subtype(object_class_type class_wanted,
     return (type_wanted);
 }
 
+static void _do_book_acquirement(item_def &book, int agent)
+{
+    // items() shouldn't make book a randart for acquirement items.
+    ASSERT(!is_random_artefact(book));
+
+    // Non-normal books are rare enough without turning them into
+    // randart books.
+    if (book.sub_type > MAX_NORMAL_BOOK)
+        return;
+
+    int          level       = (you.skills[SK_SPELLCASTING] + 2) / 3;
+    unsigned int seen_levels = you.attribute[ATTR_RND_LVL_BOOKS];
+
+    if (agent == GOD_XOM)
+        level = random_range(1, 9);
+    else if (seen_levels & (1 << level))
+    {
+        // Give a book of a level not seen before, preferably one with
+        // spells of a low enough level for the player to cast, or the
+        // lowest aviable level if all levels which the player can cast
+        // have already been given.
+        int max_level = std::min(9, you.get_experience_level());
+       
+        std::vector<int> vec;
+        for (int i = 1; i <= 9 && (vec.empty() || i <= max_level); i++)
+        {
+            if (!(seen_levels & (1 << i)))
+                vec.push_back(i);
+        }
+        if (vec.size() > 0)
+            level = vec[random2(vec.size())];
+        else
+            level = -1;
+    }
+
+    int choice = random_choose_weighted(
+                               55, 0, // fixed themed
+                               24, 1, // leave alone
+             level == -1 ? 0 : 12, 2, // fixed level
+        agent == GOD_XOM ? 0 :  6, 3, // manual (too useful for Xom)
+                                0);
+
+    switch(choice)
+    {
+    case 0:
+        make_book_theme_randart(book, 0, 0, 7, 25);
+        break;
+
+    case 1:
+        // Leave alone
+        break;
+
+    case 2:
+    {
+        int num_spells = 7 - (level + 1) / 2 + random_range(1, 2);
+        make_book_level_randart(book, level, num_spells);
+        break;
+    }
+
+    // Spell discipline manual
+    case 3:
+    {
+        int weights[SK_POISON_MAGIC - SK_CONJURATIONS + 1];
+
+        for (int i = SK_CONJURATIONS; i <= SK_POISON_MAGIC; i++)
+        {
+            int w = std::max(0, 24 - you.skills[i])
+                * 100 / species_skills(i, you.species);
+
+            weights[i - SK_CONJURATIONS] = w;
+        }
+        int skill = choose_random_weighted(weights,
+                                           weights + ARRAYSZ(weights));
+        skill += SK_CONJURATIONS;
+
+        book.sub_type = BOOK_MANUAL;
+        book.plus = skill;
+        // Set number of reads possible before it "crumbles to dust".
+        book.plus2 = 3 + random2(15);
+        break;
+    }
+
+    default:
+        ASSERT(false);
+    }
+}
+
 bool acquirement(object_class_type class_wanted, int agent,
                  bool quiet, int* item_index)
 {
@@ -1450,9 +1533,18 @@ bool acquirement(object_class_type class_wanted, int agent,
         case 'f': class_wanted = OBJ_FOOD;       break;
         case 'g': class_wanted = OBJ_MISCELLANY; break;
         case 'h': class_wanted = OBJ_GOLD;       break;
-        default: break;
+        default:
+            // Lets wizards escape out of accidently choosing acquirement.
+            if (agent == AQ_WIZMODE)
+            {
+                canned_msg(MSG_OK);
+                return (false);
+            }
+            break;
         }
     }
+
+    const bool god_agent = agent > GOD_NO_GOD && agent < NUM_GODS;
 
     if (grid_destroys_items(grd(you.pos())))
     {
@@ -1460,7 +1552,7 @@ bool acquirement(object_class_type class_wanted, int agent,
         if (!silenced(you.pos()) && !quiet)
             mprf(MSGCH_SOUND, grid_item_destruction_message(grd(you.pos())));
 
-        if (agent > GOD_NO_GOD && agent < NUM_GODS)
+        if (god_agent)
         {
             if (agent == GOD_XOM)
                 simple_god_message(" snickers.", GOD_XOM);
@@ -1487,7 +1579,11 @@ bool acquirement(object_class_type class_wanted, int agent,
             if (you.species == SP_VAMPIRE && class_wanted == OBJ_FOOD)
                 class_wanted = OBJ_POTIONS;
 
-            thing_created = items( 1, class_wanted, type_wanted, true,
+            // Don't generate randart books in items(), we do that
+            // ourselves.
+            int want_arts = class_wanted == OBJ_BOOKS ? 0 : 1;
+
+            thing_created = items( want_arts, class_wanted, type_wanted, true,
                                    MAKE_GOOD_ITEM, MAKE_ITEM_RANDOM_RACE,
                                    0, 0, agent );
 
@@ -1577,7 +1673,10 @@ bool acquirement(object_class_type class_wanted, int agent,
         do_uncurse_item(thing);
 
         if (thing.base_type == OBJ_BOOKS)
+        {
+            _do_book_acquirement(thing, agent);
             mark_had_book(thing);
+        }
         else if (thing.base_type == OBJ_JEWELLERY)
         {
             switch (thing.sub_type)

crawl-ref/source/enum.h
@@ -183,76 +183,78 @@ enum quiver_type
 
 enum beam_type                  // beam[].flavour
 {
-    BEAM_NONE,
+    BEAM_NONE,                    // 0
 
     BEAM_MISSILE,
     BEAM_MMISSILE,                //    and similarly irresistible things
     BEAM_FIRE,
     BEAM_COLD,
-    BEAM_MAGIC,
+    BEAM_MAGIC,                   // 5
     BEAM_ELECTRICITY,
     BEAM_POISON,
     BEAM_NEG,
     BEAM_ACID,
-    BEAM_MIASMA,
+    BEAM_MIASMA,                  // 10
 
     BEAM_SPORE,
     BEAM_POISON_ARROW,
     BEAM_HELLFIRE,
     BEAM_NAPALM,
-    BEAM_STEAM,
+    BEAM_STEAM,                   // 15
     BEAM_HELLFROST,
     BEAM_ENERGY,
     BEAM_HOLY,
     BEAM_FRAG,
-    BEAM_LAVA,
+    BEAM_LAVA,                    // 20
     BEAM_ICE,
     BEAM_NUKE,
-    BEAM_RANDOM,                  // 25 - currently translates into FIRE..ACID
+    BEAM_RANDOM,                  // currently translates into FIRE..ACID
+    // 24
 
     // Enchantments
-    BEAM_FIRST_ENCHANTMENT,
+    BEAM_FIRST_ENCHANTMENT = 25,  // 25
     BEAM_SLOW = BEAM_FIRST_ENCHANTMENT,
     BEAM_HASTE,
     BEAM_HEALING,
     BEAM_PARALYSIS,
     BEAM_CONFUSION,
-    BEAM_INVISIBILITY,
+    BEAM_INVISIBILITY,            // 30
     BEAM_DIGGING,
     BEAM_TELEPORT,
     BEAM_POLYMORPH,
     BEAM_CHARM,
-    BEAM_BANISH,
+    BEAM_BANISH,                  // 35
     BEAM_DEGENERATE,
     BEAM_ENSLAVE_UNDEAD,
     BEAM_ENSLAVE_SOUL,
     BEAM_PAIN,
-    BEAM_DISPEL_UNDEAD,
+    BEAM_DISPEL_UNDEAD,           // 40
     BEAM_DISINTEGRATION,
     BEAM_ENSLAVE_DEMON,
     BEAM_BLINK,
     BEAM_PETRIFY,
-    BEAM_BACKLIGHT,
+    BEAM_BACKLIGHT,               // 45
     BEAM_SLEEP,
     BEAM_LAST_ENCHANTMENT = BEAM_SLEEP,
 
     // new beams for evaporate
-    BEAM_POTION_STINKING_CLOUD,
+    BEAM_POTION_STINKING_CLOUD,   // 47
     BEAM_POTION_POISON,
     BEAM_POTION_MIASMA,
-    BEAM_POTION_STEAM,
+    BEAM_POTION_STEAM,            // 50
     BEAM_POTION_FIRE,
     BEAM_POTION_COLD,
     BEAM_POTION_BLACK_SMOKE,
     BEAM_POTION_GREY_SMOKE,
-    BEAM_POTION_BLUE_SMOKE,
+    BEAM_POTION_BLUE_SMOKE,       // 55
     BEAM_POTION_PURP_SMOKE,
     BEAM_POTION_RANDOM,
 
     BEAM_TORMENT_DAMAGE,        // Pseudo-beam for damage flavour.
     BEAM_STEAL_FOOD,            // Pseudo-beam for harpyes stealing food.
 
-    BEAM_LINE_OF_SIGHT          // only used for checking monster LOS
+    BEAM_LINE_OF_SIGHT,         // 60 - only used for checking monster LOS
+    NUM_BEAMS
 };
 
 enum book_type
@@ -281,32 +283,49 @@ enum book_type
     BOOK_CLOUDS,
     BOOK_HEALING,                      //  XXX: not used
     BOOK_NECROMANCY,
-    BOOK_NECRONOMICON,
-    BOOK_CALLINGS,                     //   25
-    BOOK_CHARMS,
-    BOOK_DEMONOLOGY,
+    BOOK_CALLINGS,
+    BOOK_CHARMS,                       //   25
     BOOK_AIR,
     BOOK_SKY,
-    BOOK_DIVINATIONS,                  //   30
+    BOOK_DIVINATIONS,
     BOOK_WARP,
-    BOOK_ENVENOMATIONS,
-    BOOK_ANNIHILATIONS,
+    BOOK_ENVENOMATIONS,                //   30
     BOOK_UNLIFE,
-    BOOK_DESTRUCTION,                  //   35
     BOOK_CONTROL,
     BOOK_MUTATIONS,
     BOOK_TUKIMA,
-    BOOK_GEOMANCY,
-    BOOK_EARTH,                        //   40
-    BOOK_MANUAL,
+    BOOK_GEOMANCY,                     //   35
+    BOOK_EARTH,
     BOOK_WIZARDRY,
     BOOK_POWER,
     BOOK_CANTRIPS,                     //jmf: 04jan2000
-    BOOK_PARTY_TRICKS,           // 45 //jmf: 04jan2000
+    BOOK_PARTY_TRICKS,           // 40 //jmf: 04jan2000
     BOOK_BEASTS,
     BOOK_STALKING,         // renamed -- assassination was confusing  -- bwr
+    MAX_NORMAL_BOOK = BOOK_STALKING,
+
+    MIN_GOD_ONLY_BOOK,                      // 43
+    BOOK_ANNIHILATIONS = MIN_GOD_ONLY_BOOK, // 43
+    BOOK_DEMONOLOGY,
+    BOOK_NECRONOMICON,                      // 45
+    MAX_GOD_ONLY_BOOK = BOOK_NECRONOMICON,
+
+    MAX_FIXED_BOOK = MAX_GOD_ONLY_BOOK,
+
+    BOOK_RANDART_LEVEL,                     // 46
+    BOOK_RANDART_THEME,
+    BOOK_CARD_EFFECT,
+
+    MAX_MEMORISABLE_BOOK = BOOK_CARD_EFFECT,
+
+    BOOK_MANUAL,
+    BOOK_DESTRUCTION,                       // 50
     NUM_BOOKS
 };
+
+#define NUM_NORMAL_BOOKS     (MAX_NORMAL_BOOK + 1)
+#define NUM_FIXED_BOOKS      (MAX_FIXED_BOOK + 1)
+#define NUM_MEMORISABLE_BOOK (MAX_MEMORISABLE_BOOK + 1)
 
 enum branch_type                // you.where_are_you
 {

crawl-ref/source/externs.h
@@ -876,8 +876,8 @@ public:
   FixedVector<unsigned char, NUM_MUTATIONS> demon_pow;
   unsigned char magic_contamination;
 
-  FixedVector<bool, NUM_BOOKS>  had_book;
-  FixedVector<bool, NUM_SPELLS> seen_spell;
+  FixedVector<bool, NUM_FIXED_BOOKS> had_book;
+  FixedVector<bool, NUM_SPELLS>      seen_spell;
 
   unsigned char normal_vision;        // how far the species gets to see
   unsigned char current_vision;       // current sight radius (cells)

crawl-ref/source/invent.cc
@@ -116,7 +116,8 @@ const bool InvEntry::is_item_glowing() const
                 || (is_artefact(*item)
                     && (item->base_type == OBJ_WEAPONS
                         || item->base_type == OBJ_MISSILES
-                        || item->base_type == OBJ_ARMOUR))));
+                        || item->base_type == OBJ_ARMOUR
+                        || item->base_type == OBJ_BOOKS))));
 }
 
 const bool InvEntry::is_item_ego() const

commit 50cf0f481aefa021e30d5df88c502ed8e69241ca

Fix spellbook rarity (|amethyst)
Some spellbooks are supposed to be rarer than others - well, to
spawn less often at lower depths - but this functionality had
been broken since 0f4b659d, eight years ago.

Probably that means it's not very important, so I wouldn't be
surprised to see this removed again.

crawl-ref/source/makeitem.cc
@@ -1467,18 +1467,20 @@ static void _generate_book_item(item_def& item, bool allow_uniques,
         item.sub_type = force_type;
     else
     {
-        do
+        while (true)
         {
             item.sub_type = random2(NUM_FIXED_BOOKS);
-
-            if (!one_chance_in(100)
-                && x_chance_in_y(book_rarity(static_cast<book_type>(item.sub_type))-1, item_level+1))
-            {
-                // If this book is really rare for this depth, continue trying.
+            if (item_type_removed(OBJ_BOOKS, item.sub_type))
                 continue;
-            }
+
+            // If this book is really rare for this depth, continue trying.
+            const int rarity
+                = book_rarity(static_cast<book_type>(item.sub_type));
+            ASSERT(rarity != 100); // 'removed item' - ugh...
+
+            if (one_chance_in(100) || !x_chance_in_y(rarity-1, item_level+1))
+                break;
         }
-        while (book_rarity(static_cast<book_type>(item.sub_type)) == 100);
 
         // Skill manuals - rare.
         if (item_level > 6 && x_chance_in_y(21 + item_level, 4000))