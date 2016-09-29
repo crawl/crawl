958f1b56d7d776ff9811f4c702dd0025a7ef7f22
Alan Malloy <alan@malloys.org>
2016-09-29 11:20:26 -0700

    Don't call artefact amulets of nothing 'buggy' (simmarine)

--- crawl-ref/source/itemname.cc
+++ crawl-ref/source/itemname.cc
@@ -811,6 +811,7 @@ const char* jewellery_effect_name(int jeweltype, bool terse)
         case AMU_RAGE:                   return "+Rage";
         case AMU_REGENERATION:           return "Regen";
         case AMU_REFLECTION:             return "Reflect";
+        case AMU_NOTHING:                return ""
         default: return "buggy";
         }
     }
