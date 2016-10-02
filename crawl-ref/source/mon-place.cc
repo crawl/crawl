7c78f4c86778ed1af48357b743461576c6391660
regret-index <cloudofregret@live.com>
2016-10-01 21:59:51 -0400

    Exaggerate and diminish demonspawn monsters some more
    Demonspawn monsters might as well do something more while it takes months for
    given developers to bother to throw out the tangled code. Their defensive
    combination aspects blend together, after all. Extended's problems are
    indefinite due to its redundancies and length, but the monsters unique to it
    shouldn't fall to the same issues.
    
     * Standardize some minor variations in demonspawn health / damage stats.
     * Give the infernal and gelid demonspawn full elemental resistance. It's too
       obtuse and subtle flavour for them to have only two pips as part of symmetry
       with the possibility of player demonspawn having the corresponding rF+ / rC+
       scales + fascet. Likewise, bump up monstrous demonspawn aux damage and health,
       since the lack of a weapon in exchange for implied Robust is a mixed exchange,
       and provide torturous demonspawn rN+++ since they lost their hidden Augmentation
       / Powered By Pain effects (clearly, they're out of date).
    
       A reference list of what should be straightforward results that nobody
       can or will remember anyway:
        * Infernal [demonspawn] have rF+++ and AF_FIRE melee.
        * Gelid [demonspawn] have rC+++ and icemail:
          10 extra AC until they're hit by fire.
        * Torturous [demonspawn] have rN+++ and spines.
        * Monstrous [demonspawn] have doubled health, no weapon,
          and extra melee hits.
    
     * Give corrupters Corrupting Pulse (wretched stars) over the current unique
       temporary mutation hex Corrupt Body. They didn't originally get it because
       their current temporary mutation beam had a specific flavour group of
       mutations to apply (removed for being invisible and obscure) and because
       Corrupting Pulse used to apply to valid allies (removed for being a
       case-specific weakening interaction). One can argue that their summons
       blocking debilitative beams is nicely generous, but extended should be
       aggressive. They'll still cast it at half of the wretched star rate, anyway.
     * Trim Malign Offering off of black suns. They already have multiple
       non-unique spells for the sake of doing more than their allies-buff, and the
       spell is too weak for Pan- the other spells are cast more to compensate.
     * Tweak the bands to use less mixed un-classed demonspawn: one less base monster
       in classed demonspawn bands, and much more of a chance to stick to thematic
       or the same demons in the rare chances base ones spawn. The point of the base
       demonspawn is to emphasize and introduce those fascet attributes, but
       spawning too many of them mixed together muddies them in the long depths
       of Pan and Extended.
    
    Still left to do: replace the Sap Magic mechanic to resemble sifcasting so it
    can be cast more often by warmongers, not spawn two long demon-focused
    branches with overlapping themes in every game, probably just forcibly bond
    the base attributes to classes anyway.

--- crawl-ref/source/mon-place.cc
+++ crawl-ref/source/mon-place.cc
@@ -2236,10 +2236,10 @@ static const map<monster_type, band_set> bands_by_leader = {
     { MONS_GELID_DEMONSPAWN, { {2}, {{ BAND_GELID_DEMONSPAWN, {1, 3} }}}},
     { MONS_INFERNAL_DEMONSPAWN, { {2}, {{ BAND_INFERNAL_DEMONSPAWN, {1, 3} }}}},
     { MONS_TORTUROUS_DEMONSPAWN, {{2}, {{ BAND_TORTUROUS_DEMONSPAWN, {1, 3}}}}},
-    { MONS_BLOOD_SAINT,     { {}, {{ BAND_BLOOD_SAINT, {1, 5} }}}},
-    { MONS_WARMONGER,       { {}, {{ BAND_WARMONGER, {2, 5} }}}},
-    { MONS_CORRUPTER,       { {}, {{ BAND_CORRUPTER, {1, 5} }}}},
-    { MONS_BLACK_SUN,       { {}, {{ BAND_BLACK_SUN, {2, 5} }}}},
+    { MONS_BLOOD_SAINT,     { {}, {{ BAND_BLOOD_SAINT, {1, 4} }}}},
+    { MONS_WARMONGER,       { {}, {{ BAND_WARMONGER, {2, 4} }}}},
+    { MONS_CORRUPTER,       { {}, {{ BAND_CORRUPTER, {1, 4} }}}},
+    { MONS_BLACK_SUN,       { {}, {{ BAND_BLACK_SUN, {2, 4} }}}},
     { MONS_VASHNIA,         { {}, {{ BAND_VASHNIA, {3, 6}, true }}}},
     { MONS_ROBIN,           { {}, {{ BAND_ROBIN, {10, 13}, true }}}},
     { MONS_RAKSHASA,        { {2, 0, []() {
@@ -2752,7 +2752,7 @@ static monster_type _band_member(band_type band, int which,
                                           3, MONS_SKELETAL_WARRIOR);
 
     case BAND_MONSTROUS_DEMONSPAWN:
-        if (which == 1 || one_chance_in(5))
+        if (which == 1 || x_chance_in_y(2, 3))
         {
             return random_choose_weighted( 2, MONS_DEMONIC_CRAWLER,
                                            2, MONS_SIXFIRHY,
@@ -2761,7 +2761,7 @@ static monster_type _band_member(band_type band, int which,
         return random_demonspawn_monster_species();
 
     case BAND_GELID_DEMONSPAWN:
-        if (which == 1 || one_chance_in(5))
+        if (which == 1 || x_chance_in_y(2, 3))
         {
             return random_choose_weighted( 4, MONS_ICE_DEVIL,
                                            3, MONS_GELID_DEMONSPAWN);
@@ -2769,7 +2769,7 @@ static monster_type _band_member(band_type band, int which,
         return random_demonspawn_monster_species();
 
     case BAND_INFERNAL_DEMONSPAWN:
-        if (which == 1 || one_chance_in(5))
+        if (which == 1 || x_chance_in_y(2, 3))
         {
             return random_choose_weighted( 2, MONS_RED_DEVIL,
                                            2, MONS_SUN_DEMON,
@@ -2778,7 +2778,7 @@ static monster_type _band_member(band_type band, int which,
         return random_demonspawn_monster_species();
 
     case BAND_TORTUROUS_DEMONSPAWN:
-        if (which == 1 || one_chance_in(5))
+        if (which == 1 || x_chance_in_y(2, 3))
         {
             return random_choose_weighted( 2, MONS_ORANGE_DEMON,
                                            2, MONS_SIXFIRHY,
@@ -2787,9 +2787,9 @@ static monster_type _band_member(band_type band, int which,
         return random_demonspawn_monster_species();
 
     case BAND_BLOOD_SAINT:
-        if (which == 1 || which == 2 && one_chance_in(5))
+        if (which == 1 || which == 2 && one_chance_in(3))
         {
-            if (x_chance_in_y(3, 5))
+            if (x_chance_in_y(2, 3))
                 return coinflip() ? MONS_BALRUG : MONS_BLIZZARD_DEMON;
             else
                 return random_demonspawn_job();
@@ -2797,9 +2797,9 @@ static monster_type _band_member(band_type band, int which,
         return random_demonspawn_monster_species();
 
     case BAND_WARMONGER:
-        if (which == 1 || which == 2 && one_chance_in(5))
+        if (which == 1 || which == 2 && one_chance_in(3))
         {
-            if (x_chance_in_y(3, 5))
+            if (x_chance_in_y(2, 3))
                 return one_chance_in(4) ? MONS_EXECUTIONER : MONS_REAPER;
             else
                 return random_demonspawn_job();
@@ -2807,9 +2807,9 @@ static monster_type _band_member(band_type band, int which,
         return random_demonspawn_monster_species();
 
     case BAND_CORRUPTER:
-        if (which == 1 || which == 2 && one_chance_in(5))
+        if (which == 1 || which == 2 && one_chance_in(3))
         {
-            if (x_chance_in_y(3, 5))
+            if (x_chance_in_y(2, 3))
                 return one_chance_in(4) ? MONS_CACODEMON : MONS_SHADOW_DEMON;
             else
                 return random_demonspawn_job();
@@ -2817,9 +2817,9 @@ static monster_type _band_member(band_type band, int which,
         return random_demonspawn_monster_species();
 
     case BAND_BLACK_SUN:
-        if (which == 1 || which == 2 && one_chance_in(5))
+        if (which == 1 || which == 2 && one_chance_in(3))
         {
-            if (x_chance_in_y(3, 5))
+            if (x_chance_in_y(2, 3))
                 return one_chance_in(3) ? MONS_LOROCYPROCA : MONS_SOUL_EATER;
             else
                 return random_demonspawn_job();
