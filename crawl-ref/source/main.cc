2382e24a6aa83a6d02c4a2a21c4367afa88485d2
Chris Campbell <chriscampbell89@gmail.com>
2016-10-12 22:02:04 +0100

    Fix colouring of prayer messaging

--- crawl-ref/source/main.cc
+++ crawl-ref/source/main.cc
@@ -2326,7 +2326,8 @@ void process_command(command_type cmd)
 
         if (feat_is_altar(grd(you.pos())))
         {
-            mprf("Press %s or %s to pray at altars.",
+            // CMD_GO_UPSTAIRS needs to be escaped as "<<".
+            mprf("Press <w><%s</w> or <w>%s</w> to pray at altars.",
                  command_to_string(CMD_GO_UPSTAIRS).c_str(),
                  command_to_string(CMD_GO_DOWNSTAIRS).c_str());
         }
