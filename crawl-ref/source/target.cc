564c9e00a67f67dd9e011c851051c07bfe348ede
Neil Moore <neil@s-z.org>
2016-10-14 14:50:20 -0400

    Allow targetting mottled drac breath past range 1 (Brannock)
    Its range was increased, but the targetter didn't know that, so the UI
    implied range 1, and targetting further away required the !  key.
    
    We fix that problem, but leave another: the targetter doesn't actually
    check the path to the target, so it pretends to be a smite-targetted
    spell.  The targetter_splash class should be replaced by something based
    on targetter_beam instead, along the lines of the IMB targetter.

--- crawl-ref/source/target.cc
+++ crawl-ref/source/target.cc
@@ -700,7 +700,8 @@ aff_type targetter_cloud::is_affected(coord_def loc)
     return AFF_NO;
 }
 
-targetter_splash::targetter_splash(const actor* act)
+targetter_splash::targetter_splash(const actor* act, int ran)
+    : range(ran)
 {
     ASSERT(act);
     agent = act;
@@ -709,7 +710,7 @@ targetter_splash::targetter_splash(const actor* act)
 
 bool targetter_splash::valid_aim(coord_def a)
 {
-    if (agent && grid_distance(origin, a) > 1)
+    if (agent && grid_distance(origin, a) > range)
         return notify_fail("Out of range.");
     return true;
 }
