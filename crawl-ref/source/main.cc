fe8db68e3595e4445f2de1c7e37438c61b89195f
Isaac Sloat <isaac.sloat@gmail.com>
2016-10-17 12:29:24 -0500

    Fix religion screen flicker while atheist
    In webtiles.

--- crawl-ref/source/main.cc
+++ crawl-ref/source/main.cc
@@ -2184,7 +2184,8 @@ void process_command(command_type cmd)
     case CMD_DISPLAY_RELIGION:
     {
 #ifdef USE_TILE_WEB
-        tiles_crt_control show_as_menu(CRT_MENU, "describe_god");
+        if (!you_worship(GOD_NO_GOD))
+            tiles_crt_control show_as_menu(CRT_MENU, "describe_god");
 #endif
         describe_god(you.religion, true);
         redraw_screen();
