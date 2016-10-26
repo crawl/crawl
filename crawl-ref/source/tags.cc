f22b74db0ab9680eb7ea3b599f4f546477fa6a54
Isaac Sloat <isaac.sloat@gmail.com>
2016-10-26 14:53:32 -0500

    Save compatibility for mottled draconians
    Player mottled draconians get ABIL_BREATHE_FIRE to compensate for
    removal of sticky flame breath.

--- crawl-ref/source/tags.cc
+++ crawl-ref/source/tags.cc
@@ -2489,6 +2489,12 @@ static void tag_read_you(reader &th)
                 a -= 1;
         }
 
+        if (th.getMinorVersion() < TAG_MINOR_MOTTLED_REMOVAL)
+        {
+            if (a == ABIL_BREATHE_STICKY_FLAME)
+                a = ABIL_BREATHE_FIRE;
+        }
+
         // Bad offset from games transferred prior to 0.17-a0-2121-g4af814f.
         if (a == NUM_ABILITIES)
             a = ABIL_NON_ABILITY;
