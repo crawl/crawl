crawl/crawl

commit 59ea75df7436023d83c29fafa70fc573e594ff54

Warn against bad Fire Storm targetting.
This includes the usual warnings - catching oneself in the radius of the
blast, targetting friendly monsters and/or (for good gods) unaware
targets, etc.

crawl-ref/source/spl-damage.cc
@@ -128,6 +128,17 @@ spret_type cast_fire_storm(int pow, bolt &beam, bool fail)
     fail_check();
     setup_fire_storm(&you, pow, beam);
 
+    bolt tempbeam = beam;
+    tempbeam.ex_size = (pow > 76) ? 3 : 2;
+    tempbeam.is_tracer = true;
+
+    tempbeam.explode(false);
+    if (tempbeam.beam_cancelled)
+    {
+        canned_msg(MSG_OK);
+        return SPRET_ABORT;
+    }
+
     mpr("A raging storm of fire appears!");
 
     beam.explode(false);

commit 7c029487cb432699eea6e1677be6fcdbc3a9523a

Fix Fire Storm aborting
Bug introduced by 59ea75df (sort of): the warnings for "this beam
would hit you/a pal, are you sure?" triggered *after* spell
failure was checked.

crawl-ref/source/spl-damage.cc
@@ -116,7 +116,6 @@ spret_type cast_fire_storm(int pow, bolt &beam, bool fail)
         return SPRET_ABORT;
     }
 
-    fail_check();
     setup_fire_storm(&you, pow, beam);
 
     bolt tempbeam = beam;
@@ -126,6 +125,8 @@ spret_type cast_fire_storm(int pow, bolt &beam, bool fail)
     tempbeam.explode(false);
     if (tempbeam.beam_cancelled)
         return SPRET_ABORT;
+
+    fail_check();
 
     beam.refine_for_explosion();
     beam.explode(false);