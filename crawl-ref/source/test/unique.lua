6596e2e689dbb24fc95b0da730a1b907a7aeff0a
Chris Campbell <chriscampbell89@gmail.com>
2016-10-28 19:05:58 +0100

    Really(?) fix debug builds

--- crawl-ref/source/test/unique.lua
+++ crawl-ref/source/test/unique.lua
@@ -41,7 +41,7 @@ end
 local function run_unique_tests()
   test_uniques_branch("D", 15)
   test_uniques_branch("Depths", 5)
-  test_uniques_branch("Dis", 7)
+  test_uniques_branch("Dis", 4)
 
   for depth = 1, 15 do
     test_uniques_blank("D", depth, 3)
