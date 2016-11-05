3a282c79a5fac33457a8bbd9ce5e27cbe690f64e
Neil Moore <neil@s-z.org>
2016-11-05 01:41:14 -0400

    Always show max charges for wands of known type.
    In item names, e.g. "wand of confusion (?/48) {zapped:2}".
    
    It's a little ugly for "wand of confusion (?/48) {empty}" to differ from
    "wand of confusion (0/48)", but really the ugliness was already there,
    just less salient.

--- crawl-ref/source/itemname.cc
+++ crawl-ref/source/itemname.cc
@@ -1793,9 +1793,17 @@ string item_def::name_aux(description_level_type desc, bool terse, bool ident,
                  << " wand";
         }
 
-        if (know_pluses)
-            buff << " (" << charges << "/" << wand_max_charges(*this) << ")";
-        else if (!dbname && with_inscription)
+        if (know_type || know_pluses) // probably know_type is sufficient?
+        {
+            buff << " (";
+            if (know_pluses)
+                buff << charges;
+            else
+                buff << "?";
+            buff << "/" << wand_max_charges(*this) << ")";
+        }
+
+        if (!know_pluses && !dbname && with_inscription)
         {
             if (used_count == ZAPCOUNT_EMPTY)
                 buff << " {empty}";
