crawl/crawl

commit a43205b747151e4614bc3e110f0582e486c477c6

Remove priority from map CHANCE specifications
The previous specification allowed setting a priority using the form:

CHANCE: PRIORITY : NUMBER | PERCENT [(DEPTH)] [, ...]

The only time this was used in the current codebase was if priority was
0, in which it was considered a "dummy chance" that allowed the vault to
still be selected when looking for maps by DEPTH. Vaults specifying
CHANCE are usually processed seperately from non-CHANCE vaults so that
the former can have an absolute roll to decide their inclusion. For
dummy vaults, however, they are intended to balance selection of
non-CHANCE vaults, sometimes making the vault selector place
(effectively) no vault at all, and their CHANCE is actually a
specification of how often this is to happen. All non-zero priorities
were ignored in vault selection, so it only existed as a hack for dummy
vaults.

This commit removes priority from the chance specification and simply
looks for the dummy tag instead. All dummy vaults that specified
priority have had this removed, but will behave exactly as they did
before, selection-wise. The des syntax guide is updated appropriately.
The new CHANCE specification is simply:

CHANCE: NUMBER | PERCENT [(DEPTH)]

The version compat code in this commit discards all des cache made from
a previous version, causing the cache to be rebuilt.

crawl-ref/source/mapdef.cc
@@ -2079,7 +2079,7 @@ dlua_set_map::~dlua_set_map()
 
 string map_chance::describe() const
 {
-    return make_stringf("%d:%d", chance_priority, chance);
+    return make_stringf("%d", chance);
 }
 
 bool map_chance::roll() const
@@ -2089,13 +2089,11 @@ bool map_chance::roll() const
 
 void map_chance::write(writer &outf) const
 {
-    marshallInt(outf, chance_priority);
     marshallInt(outf, chance);
 }
 
 void map_chance::read(reader &inf)
 {
-    chance_priority = unmarshallInt(inf);
     chance = unmarshallInt(inf);
 }
 

crawl-ref/source/dat/des/branches/abyss.des
@@ -200,24 +200,24 @@ default-depth: Abyss
 # Dummy vaults
 NAME: abyss_furniture_dummy
 TAGS: dummy
-CHANCE: 0 : 90% (Abyss:1)
-CHANCE: 0 : 80% (Abyss:2)
-CHANCE: 0 : 70% (Abyss:3)
-CHANCE: 0 : 60% (Abyss:4)
-CHANCE: 0 : 50% (Abyss:5)
+CHANCE: 90% (Abyss:1)
+CHANCE: 80% (Abyss:2)
+CHANCE: 70% (Abyss:3)
+CHANCE: 60% (Abyss:4)
+CHANCE: 50% (Abyss:5)
 
 NAME: abyss_rune_dummy
 TAGS: abyss_rune unrand dummy
-CHANCE: 0 : 10%
+CHANCE: 10%
 
 # FIXME: the tag name is inaccurate with abyssal stairs placed by these vaults.
 NAME: abyss_exit_dummy
 TAGS: abyss_exit unrand dummy
-CHANCE: 0 : 95% (Abyss:1)
-CHANCE: 0 : 90% (Abyss:2)
-CHANCE: 0 : 85% (Abyss:3)
-CHANCE: 0 : 80% (Abyss:4)
-CHANCE: 0 : 75% (Abyss:5)
+CHANCE: 95% (Abyss:1)
+CHANCE: 90% (Abyss:2)
+CHANCE: 85% (Abyss:3)
+CHANCE: 80% (Abyss:4)
+CHANCE: 75% (Abyss:5)
 
 NAME:    abyss_fake_exit
 TAGS:    allow_dup

crawl-ref/source/dat/des/builder/dummy.des
@@ -14,9 +14,7 @@ NAME: primary_dummy
 TAGS: dummy
 DEPTH: 1-100, !Zig, !Lab, !Bazaar, !Trove, !Sewer, !Ossuary, !Bailey, !IceCv, !Volcano, !WizLab, !Desolation, !Depths:2-, !Pan
 ORIENT: float
-CHANCE: 0 : 8333
-CHANCE: 0 : 6666 (Zot)
-CHANCE: 0 : 10000 (Vaults)
+CHANCE: 8333, 6666 (Zot), 10000 (Vaults)
 MAP
 x
 ENDMAP
@@ -27,7 +25,7 @@ ENDMAP
 NAME: primary_mini_dummy
 TAGS: dummy
 DEPTH: 1-100, Pan, !Depths:1, !Abyss, !Lab
-CHANCE: 0 : 10000 (Vaults), 0 : 5000
+CHANCE: 10000 (Vaults), 5000
 MAP
 x
 ENDMAP

crawl-ref/source/l_dgn.cc
@@ -189,13 +189,9 @@ static int dgn_chance(lua_State *ls)
     MAP(ls, 1, map);
     if (lua_isnumber(ls, 2))
     {
-        const bool has_priority = lua_isnumber(ls, 3);
-        const int chance_priority =
-            has_priority? luaL_checkint(ls, 2) : DEFAULT_CHANCE_PRIORITY;
-        const int chance_par = 2 + has_priority;
-        const int chance = luaL_checkint(ls, chance_par);
-        _chance_magnitude_check(ls, chance_par, chance);
-        map->_chance.set_default(map_chance(chance_priority, chance));
+        const int chance = luaL_checkint(ls, 2);
+        _chance_magnitude_check(ls, 2, chance);
+        map->_chance.set_default(map_chance(chance));
     }
     return 0;
 }
@@ -204,15 +200,11 @@ static int dgn_depth_chance(lua_State *ls)
 {
     MAP(ls, 1, map);
     const string depth(luaL_checkstring(ls, 2));
-    const bool has_priority = lua_gettop(ls) == 4;
-    const int chance_priority =
-        has_priority? luaL_checkint(ls, 3) : DEFAULT_CHANCE_PRIORITY;
-    const int chance_par = 3 + has_priority;
-    const int chance = luaL_checkint(ls, chance_par);
-    _chance_magnitude_check(ls, chance_par, chance);
+    const int chance = luaL_checkint(ls, 3);
+    _chance_magnitude_check(ls, 3, chance);
     try
     {
-        map->_chance.add_range(depth, map_chance(chance_priority, chance));
+        map->_chance.add_range(depth, map_chance(chance));
     }
     catch (const bad_level_id &error)
     {

crawl-ref/source/mapdef.h
@@ -944,30 +944,17 @@ struct map_file_place
     }
 };
 
-const int DEFAULT_CHANCE_PRIORITY = 100;
 struct map_chance
 {
-    int chance_priority;
     int chance;
-    map_chance() : chance_priority(-1), chance(-1) { }
-    map_chance(int _priority, int _chance)
-        : chance_priority(_priority), chance(_chance) { }
-    map_chance(int _chance)
-        : chance_priority(DEFAULT_CHANCE_PRIORITY), chance(_chance) { }
-    bool valid() const { return chance_priority >= 0 && chance >= 0; }
-    bool dummy_chance() const { return chance_priority == 0 && chance >= 0; }
+    map_chance() : chance(-1) { }
+    map_chance(int _chance) : chance(_chance) { }
+    bool valid() const { return chance >= 0; }
     string describe() const;
     // Returns true if the vault makes the random CHANCE_ROLL.
     bool roll() const;
     void write(writer &) const;
     void read(reader &);
-};
-
-// For the bison parser's token union:
-struct map_chance_pair
-{
-    int priority;
-    int chance;
 };
 
 typedef vector<level_range> depth_ranges_v;

crawl-ref/source/maps.cc
@@ -860,7 +860,7 @@ bool map_selector::accept(const map_def &mapdef) const
         const map_chance chance(mapdef.chance(place));
         return mapdef.is_minivault() == mini
                && _is_extra_compatible(extra, mapdef.has_tag("extra"))
-               && (!chance.valid() || chance.dummy_chance())
+               && (!chance.valid() || mapdef.has_tag("dummy"))
                && depth_selectable(mapdef)
                && !mapdef.map_already_used();
     }
@@ -870,7 +870,7 @@ bool map_selector::accept(const map_def &mapdef) const
         const map_chance chance(mapdef.chance(place));
         // Only vaults with valid chance
         return chance.valid()
-               && !chance.dummy_chance()
+               && !mapdef.has_tag("dummy")
                && depth_selectable(mapdef)
                && _is_extra_compatible(extra, mapdef.has_tag("extra"))
                && !mapdef.map_already_used();
@@ -956,8 +956,7 @@ static bool _vault_chance_new(const map_def &map,
         // vault that want to be governed by one common
         // CHANCE. In this case each vault will use a
         // CHANCE, and a common chance_xxx tag. Pick the
-        // first such vault for the chance roll. Note that
-        // at this point we ignore chance_priority.
+        // first such vault for the chance roll.
         const string tag = vault_chance_tag(map);
         if (!chance_tags.count(tag))
         {
@@ -1291,8 +1290,8 @@ static bool _load_map_index(const string& cache, const string &base,
     }
 
 #if TAG_MAJOR_VERSION == 34
-    // Throw out pre-ORDER: indices entirely.
-    if (minor < TAG_MINOR_MAP_ORDER)
+    // Throw out indices that could have CHANCE priority entirely.
+    if (minor < TAG_MINOR_NO_PRIORITY)
         return false;
 #endif
 

crawl-ref/source/tag-version.h
@@ -197,6 +197,7 @@ enum tag_minor_version
     TAG_MINOR_GLOBAL_BR_INFO,      // move global branch info to a reserved location
     TAG_MINOR_SPIT_POISON_AGAIN,   // Make Naga poison spit a 2-level mutation.
     TAG_MINOR_HIDE_TO_SCALE,       // Rename dragon hides to scales.
+    TAG_MINOR_NO_PRIORITY,         // Remove CHANCE priority in map definitions.
 #endif
     NUM_TAG_MINORS,
     TAG_MINOR_VERSION = NUM_TAG_MINORS - 1

crawl-ref/source/util/levcomp.ypp
@@ -35,7 +35,6 @@ static NORETURN void yyerror(const char *e)
     int i;
     double f;
     const char *text;
-    map_chance_pair chance;
 }
 
 /* Two harmless shift/reduce conflicts */
@@ -52,7 +51,6 @@ static NORETURN void yyerror(const char *e)
 
 %token <text>   STRING MAP_LINE MONSTER_NAME ITEM_INFO
 %token <text>   LUA_LINE
-%type <chance>  chance_num
 %type <i>       chance_roll
 
 %%
@@ -599,32 +597,18 @@ chance_roll     : NUMBER PERC
                     $$ = $1;
                 }
 
-chance_num      : NUMBER COLON chance_roll
+chance_specifier : chance_roll STRING
                 {
-                    $$.priority = $1;
-                    $$.chance   = $3;
+                    lc_map.main.add(
+                        yylineno,
+                        make_stringf("depth_chance(\"%s\", %d)",
+                                     quote_lua_string($2).c_str(), (int)$1));
                 }
                 | chance_roll
                 {
-                    $$.priority = DEFAULT_CHANCE_PRIORITY;
-                    $$.chance   = $1;
-                }
-                ;
-
-chance_specifier : chance_num STRING
-                {
                     lc_map.main.add(
                         yylineno,
-                        make_stringf("depth_chance(\"%s\", %d, %d)",
-                                     quote_lua_string($2).c_str(),
-                                     $1.priority, $1.chance));
-                }
-                | chance_num
-                {
-                    lc_map.main.add(
-                        yylineno,
-                        make_stringf("chance(%d, %d)",
-                                     $1.priority, $1.chance));
+                        make_stringf("chance(%d)", (int)$1));
                 }
                 ;
 

commit 65ed1cb72bfa6a2f6b1de07922b6653d4c1a31d3

Fix save compat for all saves
Broken by a43205b747151e.

crawl-ref/source/mapdef.cc
@@ -2094,6 +2094,10 @@ void map_chance::write(writer &outf) const
 
 void map_chance::read(reader &inf)
 {
+#if TAG_MAJOR_VERSION == 34
+    if (inf.getMinorVersion() < TAG_MINOR_NO_PRIORITY)
+        unmarshallInt(inf); // was chance_priority
+#endif
     chance = unmarshallInt(inf);
 }
 