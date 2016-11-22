6ea533fcdc91354cd614fd04ee9715f2afe7ae47
Chris Campbell <chriscampbell89@gmail.com>
2016-11-22 16:14:47 +0000

    Remove the hat of the High Council
    It's one of a very large number of mechanically similar unrands that improve
    spellcasting in some way at a cost (robe of Folly, hat of Pondering, Majin-Bo,
    Maxwell's Etheric Cage) and probably has the least interesting downside
    of them (and orange crystal plate is also similar in practice, since it has
    Archmagi but reduces success by virtue of being CPA).

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -385,9 +385,6 @@ int raw_spell_fail(spell_type spell)
     chance2 += 4 * player_mutation_level(MUT_WILD_MAGIC);
     chance2 += 4 * player_mutation_level(MUT_ANTI_WIZARDRY);
 
-    if (player_equip_unrand(UNRAND_HIGH_COUNCIL))
-        chance2 += 7;
-
     if (you.props.exists(SAP_MAGIC_KEY))
         chance2 += you.props[SAP_MAGIC_KEY].get_int() * 12;
 
