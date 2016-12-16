575bbf731dbd63fb9d61813643da59ac9ed46dba
x0_000 <aqua_bass@hotmail.com>
2016-12-15 22:10:07 -0500

    Update some code to item_brand_known

--- crawl-ref/source/itemprop.cc
+++ crawl-ref/source/itemprop.cc
@@ -878,9 +878,6 @@ void do_curse_item(item_def &item, bool quiet)
     {
         if (!quiet)
         {
-            const bool was_known = is_artefact(item)
-                                 ? artefact_known_property(item, ARTP_BRAND)
-                                 : item_ident(item, ISFLAG_KNOW_TYPE);
             mprf("Your %s glows black briefly, but repels the curse.",
                  item.name(DESC_PLAIN).c_str());
             if (is_artefact(item))
@@ -888,7 +885,7 @@ void do_curse_item(item_def &item, bool quiet)
             else
                 set_ident_flags(item, ISFLAG_KNOW_TYPE);
 
-            if (!was_known)
+            if (!item_brand_known(item))
                 mprf_nocap("%s", item.name(DESC_INVENTORY_EQUIP).c_str());
         }
         return;
