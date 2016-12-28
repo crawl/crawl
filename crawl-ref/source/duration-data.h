crawl/crawl

commit 68c82e3cd3fb88c5d72c1c972f3ab9bc16c912e8

Invalidate agrid after silence ends (10636)
The invalidate_agrid() in player-reacts.cc would *usually* handle
this, but if anything checked the agrid between that invalidation
and the actual decrementing of the DUR_SILENCE to 0 (that is,
between lines 600 and 859), the agrid would still see DUR_SILENCE
as being in effect at your current position, and wouldn't update
until something else invalidated the agrid (e.g. another cast of
silence).

To replicate before this commit, have Darkness active along with
Silence; player-reacts.cc:773 will trigger the bug.

crawl-ref/source/duration-data.h
@@ -276,7 +276,7 @@ static const duration_def duration_data[] =
       MAGENTA, "Sil",
       "silence", "",
       "You radiate silence.", D_DISPELLABLE | D_EXPIRES,
-      {{ "Your hearing returns." }}, 5 },
+      {{ "Your hearing returns.", []() { invalidate_agrid(true); }}, 5 },
     { DUR_STEALTH,
       BLUE, "Stealth",
       "especially stealthy", "stealth",

commit 72753d0ed838ec59ffc3a5cf590fced33cd8fa20

Balance braces in an initialization
fixed the broken build in 68c82e3

crawl-ref/source/duration-data.h
@@ -276,7 +276,7 @@ static const duration_def duration_data[] =
       MAGENTA, "Sil",
       "silence", "",
       "You radiate silence.", D_DISPELLABLE | D_EXPIRES,
-      {{ "Your hearing returns.", []() { invalidate_agrid(true); }}, 5 },
+      {{ "Your hearing returns.", []() { invalidate_agrid(true); }}}, 5 },
     { DUR_STEALTH,
       BLUE, "Stealth",
       "especially stealthy", "stealth",