e9c2a71eba4ae088dc269dc987fc0b2e64c3ea87
Chris Campbell <chriscampbell89@gmail.com>
2016-12-28 17:17:18 +0000

    Remove an unused member of the bolt struct

--- crawl-ref/source/beam.h
+++ crawl-ref/source/beam.h
@@ -99,8 +99,6 @@ struct bolt
                                         // itself.
     bool   was_missile = false;   // For determining if this was SPMSL_FLAME /
                                   // FROST etc so that we can change mulch rate
-    bool   evoked = false;        // Was this beam evoked from a wand?
-
     // Do we draw animations?
     bool   animate = bool(Options.use_animations & UA_BEAM);
     ac_type ac_rule = AC_NORMAL;   // How defender's AC affects damage.
