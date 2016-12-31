e9910ba3f67c93ef8f7b17f8427d24bfae50fc44
Neil Moore <neil@s-z.org>
2016-12-31 10:29:12 -0500

    Fix dgl builds (doh)

--- crawl-ref/source/initfile.cc
+++ crawl-ref/source/initfile.cc
@@ -125,6 +125,10 @@ const vector<GameOption*> game_options::build_options_list()
 #else
         false;
 #endif
+#ifdef DGAMELAUNCH
+    UNUSED(USING_LOCAL_TILES);
+#endif
+
 #ifdef USE_TILE
     const bool USING_WEB_TILES =
 #if defined(USE_TILE_WEB)
@@ -285,8 +289,6 @@ const vector<GameOption*> game_options::build_options_list()
         new StringGameOption(SIMPLE_NAME(save_dir), _get_save_path("saves/")),
         new StringGameOption(SIMPLE_NAME(morgue_dir),
                              _get_save_path("morgue/")),
-#else
-        UNUSED(USING_LOCAL_TILES);
 #endif
 #ifdef USE_TILE
         new BoolGameOption(SIMPLE_NAME(tile_skip_title), false),
