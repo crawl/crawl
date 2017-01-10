0f062f23dd4830850738e25a352aaf93dd3f6773
Nicholas Feinberg <pleasingfung@gmail.com>
2017-01-09 19:34:11 -0800

    Lovely Barachian tiles (CanOfWorms)
    *ribbiting intensifies*

--- crawl-ref/source/tilepick-p.cc
+++ crawl-ref/source/tilepick-p.cc
@@ -632,6 +632,8 @@ tileidx_t tilep_species_to_base_tile(int sp, int level)
         return TILEP_BASE_FORMICID;
     case SP_VINE_STALKER:
         return TILEP_BASE_VINE_STALKER;
+    case SP_BARACHIAN:
+        return TILEP_BASE_BARACHIAN;
     default:
         return TILEP_BASE_HUMAN;
     }
