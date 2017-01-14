crawl/crawl

commit 768198ce1617d3edcf8e46d47edb593c770b4c0c

Make panlord AC & EV (mostly) inversely correlated.
Average AC+EV levels go up by 5, but the min & max remain the same and
variance is reduced.

crawl-ref/source/ghost.cc
@@ -200,8 +200,13 @@ void ghost_demon::init_pandemonium_lord()
     // hp - could be defined below (as could ev, AC, etc.). Oh well, too late:
     max_hp = 100 + roll_dice(3, 50);
 
-    ev = 5 + random2(20);
-    ac = 5 + random2(20);
+    // Panlord AC/EV should tend to be weighted towards one or the other.
+    int total_def = 10 + random2avg(40, 3);
+    int split = biased_random2(5, 2);
+    ac = div_rand_round(total_def * split, 10);
+    ev = total_def - ev;
+    if (coinflip())
+        swap(ac, ev);
 
     see_invis = true;
 

commit 4e2cedd21ea255cfcc26371600f8a1f6d5fa65bd

Fix random panlord defenses (Doesnty)
Caused by 768198ce16, intending to change them to be inversely related.

crawl-ref/source/ghost.cc
@@ -191,7 +191,7 @@ void ghost_demon::init_pandemonium_lord()
     int total_def = 10 + random2avg(40, 3);
     int split = biased_random2(5, 2);
     ac = div_rand_round(total_def * split, 10);
-    ev = total_def - ev;
+    ev = total_def - ac;
     if (coinflip())
         swap(ac, ev);
 