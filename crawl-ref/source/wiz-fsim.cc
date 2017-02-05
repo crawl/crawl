9b62066f3caded6040d0bf911eeb007a826b0cad
Neil Moore <neil@s-z.org>
2017-02-05 15:17:23 -0500

    Fix fsim defense mode with passive damage (advil, #10914)
    The passive damage could reduce the monster to zero damage, leading to
    messages about DEAD_MONSTER.
    
    (cherry picked from commit 60d86990370f76951c10ae81e1d10cd5e06f35ff)

--- crawl-ref/source/wiz-fsim.cc
+++ crawl-ref/source/wiz-fsim.cc
@@ -408,6 +408,7 @@ static fight_data _get_fight_data(monster &mon, int iter_limit, bool defend)
         for (int i = 0; i < iter_limit; i++)
         {
             you.hp = you.hp_max = 999; // again, arbitrary
+            mon.hit_points = mon.max_hit_points;
             bool did_hit = false;
             you.shield_blocks = 0; // no blocks this round
             fight_melee(&mon, &you, &did_hit, true);
