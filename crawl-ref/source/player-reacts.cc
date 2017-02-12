f830c9a6c3b4454de7a2edc565624f6645bb138f
Corin Buchanan-Howland <corin@buchananhowland.net>
2017-02-11 19:31:31 -0500

    Refactor player MP regen rate into its own method
    The place where rounding happens has changed a bit, but it shouldn't be
    significant.

--- crawl-ref/source/player-reacts.cc
+++ crawl-ref/source/player-reacts.cc
@@ -935,6 +935,7 @@ static void _regenerate_hp_and_mp(int delay)
     if (crawl_state.disables[DIS_PLAYER_REGEN])
         return;
 
+    // HP Regeneration
     if (!you.duration[DUR_DEATHS_DOOR])
     {
         const int base_val = player_regen();
@@ -958,19 +959,14 @@ static void _regenerate_hp_and_mp(int delay)
 
     update_regen_amulet_attunement();
 
+    // MP Regeneration
     if (!player_regenerates_mp())
         return;
 
     if (you.magic_points < you.max_magic_points)
     {
-        const int base_val = 7 + you.max_magic_points / 2;
+        const int base_val = player_mp_regen();
         int mp_regen_countup = div_rand_round(base_val * delay, BASELINE_DELAY);
-
-        if (player_mutation_level(MUT_MANA_REGENERATION))
-            mp_regen_countup *= 2;
-        if (you.props[MANA_REGEN_AMULET_ACTIVE].get_int() == 1)
-            mp_regen_countup += div_rand_round(15 * delay, BASELINE_DELAY);
-
         you.magic_points_regeneration += mp_regen_countup;
     }
 
