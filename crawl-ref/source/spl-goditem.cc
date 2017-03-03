00179d9f2c0deec772c957b24eee290a011a6ac3
Nicholas Feinberg <pleasingfung@gmail.com>
2017-03-03 07:26:13 -0800

    Fix /Random Effects (10960)
    Broken in... 4a616ededea7... :(

--- crawl-ref/source/spl-goditem.cc
+++ crawl-ref/source/spl-goditem.cc
@@ -1312,6 +1312,7 @@ spret_type cast_random_effects(int pow, bolt& beam, bool fail)
                                  ZAP_FIREBALL,
                                  ZAP_BOLT_OF_DRAINING,
                                  ZAP_VENOM_BOLT);
+    beam.origin_spell = SPELL_NO_SPELL; // let zapping reset this
 
     zapping(zap, pow, beam, false);
 
