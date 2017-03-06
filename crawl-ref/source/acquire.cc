crawl/crawl

commit 2c266a969726ab45ba4bda0773c98f2e66b9ded0

Don't print manuals' types twice when you drop them (minmay)

crawl-ref/source/items.cc
@@ -944,7 +944,7 @@ static bool _id_floor_item(item_def &item)
 {
     if (item.base_type == OBJ_BOOKS)
     {
-        if (fully_identified(item) && item.sub_type != BOOK_MANUAL)
+        if (fully_identified(item))
             return false;
 
         // fix autopickup for previously-unknown books (hack)

commit 27b705a98ada1795d38718895adc50b7a47d4291

Pre-identify manuals
Manual identification was broken in 2c266a969726 - step-over would
identify only the first manual in any given game, and all subsequent
manuals had to be picked up to identify their types.

Fixing this using the book/wand autoidentification approach causes much
more issues than it truly solves. So we take a different approach: all
manuals come pre-identified.

crawl-ref/source/acquire.cc
@@ -948,6 +948,9 @@ static bool _acquire_manual(item_def &book)
                     choose_random_weighted(weights, end(weights)));
     // Set number of bonus skill points.
     book.skill_points = random_range(2000, 3000);
+    // Identify.
+    set_ident_type(book, true);
+    set_ident_flags(book, ISFLAG_IDENT_MASK);
     return true;
 }
 

crawl-ref/source/items.cc
@@ -938,7 +938,7 @@ void item_check()
 }
 
 // Identify the object the player stepped on.
-// Books and manuals are fully identified.
+// Books are fully identified.
 // Wands are only type-identified.
 static bool _id_floor_item(item_def &item)
 {
@@ -967,6 +967,7 @@ static bool _id_floor_item(item_def &item)
             return true;
         }
     }
+
     return false;
 }
 

crawl-ref/source/makeitem.cc
@@ -1518,6 +1518,9 @@ static void _generate_book_item(item_def& item, bool allow_uniques,
         item.skill = _choose_manual_skill();
         // Set number of bonus skill points.
         item.skill_points = random_range(2000, 3000);
+        // Preidentify.
+        set_ident_type(item, true);
+        set_ident_flags(item, ISFLAG_IDENT_MASK);
         return; // rare enough without being replaced with randarts
     }
 