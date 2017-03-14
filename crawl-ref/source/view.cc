crawl/crawl

commit a321ce641457c03216f8b5bde19310af14b5a1ec

Make targeter/mesmerise code & rendering consistent across interfaces
This centralizes the code for mesmerise cell darkening (in view.cc,
where only tiles/webtiles were dealt with before) and makes it do
roughly the same thing at the same time for console/tiles.  Before,
console would only show the mesmerise range no matter the targeter, and
(web)tiles would show only the targeter range even when mesmerise was
active and it was a blink-like targeter.  Now, blink targeters will
appropriately color possible target cells if blinking to that cell is
impossible because of mesmerise.

This also fixes up a few other issues, such as the console blink
targeter not coloring appropriately for transparent walls.

crawl-ref/source/view.cc
@@ -1482,6 +1482,12 @@ void draw_cell(screen_cell_t *cell, const coord_def &gc,
 
     cell->flash_colour = BLACK;
 
+    // is this cell excluded from movement by mesmerise-related statuses?
+    // MAP_WITHHELD is set in `show.cc:_update_feat_at`.
+    bool mesmerise_excluded = ((gc != you.pos()) // for fungus form
+                               && (env.map_knowledge(gc).flags & MAP_WITHHELD)
+                               && !feat_is_solid(grd(gc)));
+
     // Alter colour if flashing the characters vision.
     if (flash_colour)
     {
@@ -1502,7 +1508,8 @@ void draw_cell(screen_cell_t *cell, const coord_def &gc,
     }
     else if (crawl_state.darken_range)
     {
-        if (!crawl_state.darken_range->valid_aim(gc))
+        if ((crawl_state.darken_range->obeys_mesmerise && mesmerise_excluded)
+            || (!crawl_state.darken_range->valid_aim(gc)))
         {
             cell->colour = DARKGREY;
 #ifdef USE_TILE
@@ -1528,25 +1535,23 @@ void draw_cell(screen_cell_t *cell, const coord_def &gc,
         if (!found)
             cell->colour = DARKGREY;
     }
+    else if (mesmerise_excluded) // but no range limits in place
+    {
+        cell->colour = DARKGREY;
+
 #ifdef USE_TILE_LOCAL
-    // Grey out grids that cannot be reached due to beholders.
-    else if (you.get_beholder(gc))
         cell->tile.bg |= TILE_FLAG_OOR;
-
-    else if (you.get_fearmonger(gc))
-        cell->tile.bg |= TILE_FLAG_OOR;
-
-    tile_apply_properties(gc, cell->tile);
 #elif defined(USE_TILE_WEB)
-    // For webtiles, we only grey out visible tiles
-    else if (you.get_beholder(gc) && you.see_cell(gc))
-        cell->tile.bg |= TILE_FLAG_OOR;
+        // For webtiles, we only grey out visible tiles
+        if (you.see_cell(gc))
+            cell->tile.bg |= TILE_FLAG_OOR;
+#endif
+    }
 
-    else if (you.get_fearmonger(gc) && you.see_cell(gc))
-        cell->tile.bg |= TILE_FLAG_OOR;
-
+#ifdef USE_TILE
     tile_apply_properties(gc, cell->tile);
 #endif
+
 #ifndef USE_TILE_LOCAL
     if ((_layers != LAYERS_ALL || Options.always_show_exclusions)
         && you.on_current_level

crawl-ref/source/god-abil.cc
@@ -6181,6 +6181,7 @@ bool ru_power_leap()
         args.self = CONFIRM_CANCEL;
         const int explosion_size = 1;
         targeter_smite tgt(&you, args.range, explosion_size, explosion_size);
+        tgt.obeys_mesmerise = true;
         args.hitfunc = &tgt;
         direction(beam, args);
         if (crawl_state.seen_hups)

crawl-ref/source/showsymb.cc
@@ -58,12 +58,6 @@ static unsigned short _cell_feat_show_colour(const map_cell& cell,
             return colour;
         }
     }
-    else if (!feat_is_solid(feat) && cell.flags & MAP_WITHHELD)
-    {
-        // Colour grids that cannot be reached due to beholders
-        // dark grey.
-        colour = DARKGREY;
-    }
     else if (!feat_is_solid(feat)
              && (cell.flags & (MAP_SANCTUARY_1 | MAP_SANCTUARY_2)))
     {

crawl-ref/source/spl-transloc.cc
@@ -250,6 +250,10 @@ void wizard_blink()
     direction_chooser_args args;
     args.restricts = DIR_TARGET;
     args.needs_path = false;
+    targeter_smite tgt(&you, LOS_RADIUS);
+    tgt.obeys_mesmerise = false;
+    args.hitfunc = &tgt;
+
     args.top_prompt = "Blink to where?";
     dist beam;
     direction(beam, args);
@@ -322,6 +326,7 @@ spret_type frog_hop(bool fail)
     const int hop_range = 2 + player_mutation_level(MUT_HOP) * 2; // 4-6
     coord_def target;
     targeter_smite tgt(&you, hop_range, 0, HOP_FUZZ_RADIUS);
+    tgt.obeys_mesmerise = true;
     while (true)
     {
         if (!_find_cblink_target(target, true, "hop", &tgt))
@@ -372,7 +377,9 @@ spret_type frog_hop(bool fail)
 spret_type controlled_blink(bool fail, bool safe_cancel)
 {
     coord_def target;
-    if (!_find_cblink_target(target, safe_cancel, "blink"))
+    targeter_smite tgt(&you, LOS_RADIUS);
+    tgt.obeys_mesmerise = true;
+    if (!_find_cblink_target(target, safe_cancel, "blink", &tgt))
         return SPRET_ABORT;
 
     fail_check();

crawl-ref/source/target.h
@@ -19,12 +19,14 @@ enum aff_type // sign and non-zeroness matters
 class targeter
 {
 public:
+    targeter() :  agent(nullptr), obeys_mesmerise(false) {};
     virtual ~targeter() {};
 
     coord_def origin;
     coord_def aim;
     const actor* agent;
     string why_not;
+    bool obeys_mesmerise; // whether the rendering of ranges should take into account mesmerise effects
 
     virtual bool set_aim(coord_def a);
     virtual bool valid_aim(coord_def a) = 0;

commit 11d4b8a0ea174d34042dadb23fee8b312b5113e9

Fix monsters being coloured grey under mesmerise/fear
Caused by a321ce6414.

crawl-ref/source/view.cc
@@ -1482,9 +1482,15 @@ void draw_cell(screen_cell_t *cell, const coord_def &gc,
 
     cell->flash_colour = BLACK;
 
-    // is this cell excluded from movement by mesmerise-related statuses?
+    // Don't hide important information by recolouring monsters.
+    monster_type mons = env.map_knowledge(gc).monster();
+    bool allow_mon_recolour = (mons == MONS_NO_MONSTER
+                               || mons_class_is_firewood(mons));
+
+    // Is this cell excluded from movement by mesmerise-related statuses?
     // MAP_WITHHELD is set in `show.cc:_update_feat_at`.
-    bool mesmerise_excluded = ((gc != you.pos()) // for fungus form
+    bool mesmerise_excluded = (gc != you.pos() // for fungus form
+                               && allow_mon_recolour
                                && map_bounds(gc)
                                && (env.map_knowledge(gc).flags & MAP_WITHHELD)
                                && !feat_is_solid(grd(gc)));
@@ -1498,12 +1504,8 @@ void draw_cell(screen_cell_t *cell, const coord_def &gc,
         else
             cell->colour = real_colour(flash_colour);
 #else
-        else if (gc != you.pos())
-        {
-            monster_type mons = env.map_knowledge(gc).monster();
-            if (mons == MONS_NO_MONSTER || mons_class_is_firewood(mons))
-                cell->colour = real_colour(flash_colour);
-        }
+        else if (gc != you.pos() && allow_mon_recolour)
+            cell->colour = real_colour(flash_colour);
 #endif
         cell->flash_colour = cell->colour;
     }