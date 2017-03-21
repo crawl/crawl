6d0956ec03516b528c58660b5e492389282127f6
gammafunk <gammafunk@gmail.com>
2017-03-21 02:59:11 -0500

    Remove item level transit code
    The last code that made items transit levels was removed in 9f24bd51.
    Add a minor version tag since we're removing an area of the save file.
    Leave the necessary unmarshalling code for purposes of save compat, but
    with TAG_MAJOR ifdef.

--- crawl-ref/source/tags.cc
+++ crawl-ref/source/tags.cc
@@ -295,7 +295,6 @@ static void tag_construct_you(writer &th);
 static void tag_construct_you_items(writer &th);
 static void tag_construct_you_dungeon(writer &th);
 static void tag_construct_lost_monsters(writer &th);
-static void tag_construct_lost_items(writer &th);
 static void tag_construct_companions(writer &th);
 static void tag_read_you(reader &th);
 static void tag_read_you_items(reader &th);
@@ -1155,8 +1154,6 @@ void tag_write(tag_type tagID, writer &outf)
         CANARY;
         tag_construct_lost_monsters(th);
         CANARY;
-        tag_construct_lost_items(th);
-        CANARY;
         tag_construct_companions(th);
         break;
     case TAG_LEVEL:
@@ -1243,9 +1240,12 @@ void tag_read(reader &inf, tag_type tag_id)
         EAT_CANARY;
         tag_read_lost_monsters(th);
         EAT_CANARY;
-        tag_read_lost_items(th);
-        EAT_CANARY;
 #if TAG_MAJOR_VERSION == 34
+        if (th.getMinorVersion() < TAG_MINOR_NO_ITEM_TRANSIT)
+        {
+            tag_read_lost_items(th);
+            EAT_CANARY;
+        }
         if (th.getMinorVersion() >= TAG_MINOR_COMPANION_LIST)
 #endif
         tag_read_companions(th);
@@ -1835,14 +1835,6 @@ static void marshall_follower_list(writer &th, const m_transit_list &mlist)
         marshall_follower(th, follower);
 }
 
-static void marshall_item_list(writer &th, const i_transit_list &ilist)
-{
-    marshallShort(th, ilist.size());
-
-    for (const auto &item : ilist)
-        marshallItem(th, item);
-}
-
 static m_transit_list unmarshall_follower_list(reader &th)
 {
     m_transit_list mlist;
@@ -1865,6 +1857,7 @@ static m_transit_list unmarshall_follower_list(reader &th)
     return mlist;
 }
 
+#if TAG_MAJOR_VERSION == 34
 static i_transit_list unmarshall_item_list(reader &th)
 {
     i_transit_list ilist;
@@ -1880,6 +1873,7 @@ static i_transit_list unmarshall_item_list(reader &th)
 
     return ilist;
 }
+#endif
 
 static void marshall_level_map_masks(writer &th)
 {
@@ -2135,12 +2129,6 @@ static void tag_construct_lost_monsters(writer &th)
 {
     marshallMap(th, the_lost_ones, marshall_level_id,
                  marshall_follower_list);
-}
-
-static void tag_construct_lost_items(writer &th)
-{
-    marshallMap(th, transiting_items, marshall_level_id,
-                 marshall_item_list);
 }
 
 static void tag_construct_companions(writer &th)
@@ -3974,13 +3962,15 @@ static void tag_read_lost_monsters(reader &th)
                   unmarshall_level_id, unmarshall_follower_list);
 }
 
+#if TAG_MAJOR_VERSION == 34
 static void tag_read_lost_items(reader &th)
 {
-    transiting_items.clear();
+    items_in_transit transiting_items;
 
     unmarshallMap(th, transiting_items,
                   unmarshall_level_id, unmarshall_item_list);
 }
+#endif
 
 static void tag_read_companions(reader &th)
 {
