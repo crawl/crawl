96c9018b16e2939d5efe93f323f5daab4f2b9c9f
Floodkiller <floodkiller3228@gmail.com>
2017-03-24 23:04:21 -0500

    Change Cyno recommended backgrounds.
    Change Cyno recommended backgrounds to remove Ice Elementalist and add
    Wanderer.  Recommended backgrounds for Cyno are currently just choosing
    any background that often prefer to spread their skills around over
    focusing on a single skill.  Despite Ice Elementalists having a lot of
    skill splashing, they are still heavily encouraged to focus Ice Magic;
    with the lower 'soft' skill cap, this may be less viable than it
    originally was.  Instead, it has been replaced by Wanderer, as Cyno should
    have an easier time adjusting to whatever starting gear it has been given
    over other species.
    
    If recommending Wanderer is not advised, the next suggested recommended
    backgrounds would be between Assassin, Abyssal Knight, and Monk.

--- crawl-ref/source/species-data.h
+++ crawl-ref/source/species-data.h
@@ -76,7 +76,7 @@ static const map<species_type, species_def> species_data =
     {"You have a short attention span."},
     {"short attention span"},
     { JOB_SKALD, JOB_WARPER, JOB_ARCANE_MARKSMAN, JOB_TRANSMUTER,
-      JOB_ICE_ELEMENTALIST },
+      JOB_WANDERER },
     { SK_SHORT_BLADES, SK_MACES_FLAILS, SK_POLEARMS, SK_LONG_BLADES, SK_STAVES,
       SK_BOWS, SK_CROSSBOWS, SK_SLINGS },
 } },
