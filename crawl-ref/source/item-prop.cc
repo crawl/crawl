1eccc6de330b52be30ecd3806ce0f80a7a955260
elliptic <hyperelliptical@gmail.com>
2017-03-29 10:17:33 -0400

    GSC base delay: 1.9 -> 1.8.
    So that they are more competitive with regular giant clubs, which have
    only a bit less base damage (20 vs 22) and require only 18 skill to reach
    min delay as opposed to 24 skill for GSC (before this commit).
    
    Note that ogres had a much higher maces apt when GC and GSC delays were last
    touched (1e3ea62). With the -1 apt that they have now, the cost of raising
    maces to 24 is much higher. Even after this change, I expect that many chars
    will prefer to use a GC than to raise maces from 18 to 22.

--- crawl-ref/source/item-prop.cc
+++ crawl-ref/source/item-prop.cc
@@ -440,7 +440,7 @@ static const weapon_def Weapon_prop[] =
     { WPN_GIANT_CLUB,        "giant club",         20, -6, 16,
         SK_MACES_FLAILS, SIZE_LARGE, NUM_SIZE_LEVELS, MI_NONE,
         DAMV_CRUSHING, 1, 10, 17, {} },
-    { WPN_GIANT_SPIKED_CLUB, "giant spiked club",  22, -7, 19,
+    { WPN_GIANT_SPIKED_CLUB, "giant spiked club",  22, -7, 18,
         SK_MACES_FLAILS, SIZE_LARGE, NUM_SIZE_LEVELS, MI_NONE,
         DAMV_CRUSHING | DAM_PIERCE, 1, 10, 19, {} },
 
