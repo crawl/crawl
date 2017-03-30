6f8bca2d117266f344eef02cf81a5cb8c4d73e1d
advil <rawlins@gmail.com>
2017-03-29 21:37:00 -0400

    Fix interbranch travel in wizmode (elliptic, 10984)
    This partially reverts some of the simplification part of 9217641b3915,
    with what is hopefully clearer logic at least.  The travel cache needs
    to be unupdated during wizmode interlevel travel commands, not during
    wizmode in general.  Thanks to elliptic for finding the bug and the
    culprit commit.

--- crawl-ref/source/stairs.h
+++ crawl-ref/source/stairs.h
@@ -11,11 +11,13 @@ level_id stair_destination(dungeon_feature_type feat, const string &dst,
 level_id stair_destination(coord_def pos, bool for_real = false);
 void floor_transition(dungeon_feature_type how,
                       const dungeon_feature_type whence, level_id whither,
-                      bool forced, bool going_up = false, bool shaft = false);
+                      bool forced, bool going_up = false, bool shaft = false,
+                      bool update_travel_cache = true);
 void take_stairs(dungeon_feature_type force_stair = DNGN_UNSEEN,
-                 bool going_up = false, bool force_known_shaft = false);
+                 bool going_up = false, bool force_known_shaft = false,
+                 bool update_travel_cache = true);
 void down_stairs(dungeon_feature_type force_stair = DNGN_UNSEEN,
-                 bool force_known_shaft = false);
-void up_stairs(dungeon_feature_type force_stair = DNGN_UNSEEN);
+                 bool force_known_shaft = false, bool update_travel_cache = true);
+void up_stairs(dungeon_feature_type force_stair = DNGN_UNSEEN, bool update_travel_cache = true);
 void new_level(bool restore = false);
 void leaving_level_now(dungeon_feature_type stair_used);
