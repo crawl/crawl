e43576ca91fc0391b14a47bc077bffa85efcc005
Chris Campbell <chriscampbell89@gmail.com>
2017-04-10 18:18:08 +0100

    Remove the cancel Song of Slaying ability
    The existence of the ability makes using it more fiddly in practice than just
    resting off the duration and dealing with the downside of the noise, since it
    requires manual cancellation once all visible enemies are killed.

--- crawl-ref/source/tilepick.cc
+++ crawl-ref/source/tilepick.cc
@@ -3206,8 +3206,6 @@ tileidx_t tileidx_ability(const ability_type ability)
         return TILEG_ABILITY_END_TRANSFORMATION;
     case ABIL_STOP_RECALL:
         return TILEG_ABILITY_STOP_RECALL;
-    case ABIL_STOP_SINGING:
-        return TILEG_ABILITY_STOP_SINGING;
     case ABIL_CANCEL_PPROJ:
         return TILEG_ABILITY_CANCEL_PPROJ;
 
