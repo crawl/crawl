abd2c1835a40db0ecd2a443068ffc9a586c2a360
Neil Moore <neil@s-z.org>
2017-05-04 09:49:49 -0400

    Fix cloud immunity brand in itemspec and debug acq stats.

--- crawl-ref/source/wiz-item.cc
+++ crawl-ref/source/wiz-item.cc
@@ -1157,6 +1157,7 @@ static void _debug_acquirement_stats(FILE *ostat)
             "jumping",
 #endif
             "repulsion",
+            "cloud immunity",
         };
 
         const int non_art = acq_calls - num_arts;
