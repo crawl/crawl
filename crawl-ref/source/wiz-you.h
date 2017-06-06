d0063caf0c197ff0841fb206911f5f88de005dfd
advil <rawlins@gmail.com>
2017-06-06 13:26:47 -0400

    Fix NOWIZARD builds
    It seems that this isn't widely used.

--- crawl-ref/source/wiz-you.h
+++ crawl-ref/source/wiz-you.h
@@ -37,7 +37,6 @@ void wizard_god_wrath();
 void wizard_god_mollify();
 void wizard_transform();
 void wizard_join_religion();
-species_type find_species_from_string(const string &species_str);
 job_type find_job_from_string(const string &job_str);
 void wizard_change_job_to(job_type job);
 void wizard_xom_acts();
