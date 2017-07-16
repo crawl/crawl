a32a064c6f5c9927d97120c6ce0004c45a5aeda1
gammafunk <gammafunk@gmail.com>
2017-07-16 17:32:56 -0500

    Slightly refactor a stat gain function (|amethyst)

--- crawl-ref/source/jobs.cc
+++ crawl-ref/source/jobs.cc
@@ -71,26 +71,22 @@ void job_stat_init(job_type job)
     you.hp_max_adj_perm = 0;
 
     // Gnolls don't get stats from background
-    if(!(you.species == SP_GNOLL))
+    if (you.species == SP_GNOLL)
+        return;
+
+    you.base_stats[STAT_STR] += _job_def(job).s;
+    you.base_stats[STAT_INT] += _job_def(job).i;
+    you.base_stats[STAT_DEX] += _job_def(job).d;
+
+    if (job == JOB_WANDERER)
     {
-        you.base_stats[STAT_STR] += _job_def(job).s;
-        you.base_stats[STAT_INT] += _job_def(job).i;
-        you.base_stats[STAT_DEX] += _job_def(job).d;
-
-        if (job == JOB_WANDERER)
+        for (int i = 0; i < 12; i++)
         {
-            for (int i = 0; i < 12; i++)
-            {
-                const stat_type stat = static_cast<stat_type>(random2(NUM_STATS));
-                // Stats that are already high will be chosen half as often.
-                if (you.base_stats[stat] > 17 && coinflip())
-                {
-                    i--;
-                    continue;
-                }
-
-                you.base_stats[stat]++;
-            }
+            const auto stat = random_choose_weighted(
+                    you.base_stats[STAT_STR] > 17 ? 1 : 2, STAT_STR,
+                    you.base_stats[STAT_INT] > 17 ? 1 : 2, STAT_INT,
+                    you.base_stats[STAT_DEX] > 17 ? 1 : 2, STAT_DEX);
+            you.base_stats[stat]++;
         }
     }
 }
