dd1ebd0fd3e6ca733d9d92d39b4a508ce10a5974
gammafunk <gammafunk@gmail.com>
2017-08-21 15:01:05 -0500

    Decrease the width of the xp_by_level morgue table
    This table is over 80 characters long, and due to a bug in the morgue
    display code, trying to display this in-game causes a crash. The table
    doesn't need as much padding as it currently has, so decrease this. The
    relevant display code will be fixed in a subsequent commit.

--- crawl-ref/source/chardump.cc
+++ crawl-ref/source/chardump.cc
@@ -457,7 +457,7 @@ static string _sdump_level_xp_info(LevelXPInfo xp_info, string name = "")
     f = TO_PERCENT(xp_info.spawn_count, total_count);
 
     out =
-        make_stringf("%14s | %7d | %7d | %5.1f | %7d | %7d | %5.1f | %7d\n",
+        make_stringf("%11s | %7d | %7d | %5.1f | %7d | %7d | %5.1f | %7d\n",
                      name.c_str(), xp_info.spawn_xp, xp_info.generated_xp,
                      c, xp_info.spawn_count, xp_info.generated_count, f,
                      xp_info.turns);
@@ -520,20 +520,20 @@ static void _sdump_xp_by_level(dump_params &par)
 " F = Spawn count percentage of total count\n"
 " G = Total turns spent on level\n\n";
 
-    text += "               ";
+    text += "            ";
     text += "     A         B        C        D         E        F        G    \n";
-    text += "               ";
+    text += "            ";
     text += "+---------+---------+-------+---------+---------+-------+---------\n";
 
     text += _sdump_level_xp_info(you.global_xp_info, "Total");
 
-    text += "               ";
+    text += "            ";
     text += "+---------+---------+-------+---------+---------+-------+---------\n";
 
     for (const LevelXPInfo &mi : all_info)
         text += _sdump_level_xp_info(mi);
 
-    text += "               ";
+    text += "            ";
     text += "+---------+---------+-------+---------+---------+-------+---------\n";
 
     text += "\n";
