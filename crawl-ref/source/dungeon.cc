728d0adb1858d0f1c0141d0cb0e084bda3fdd180
gammafunk <gammafunk@gmail.com>
2017-09-01 10:54:00 -0500

    Generate some monsters awake at level creation time on most levels
    Each level generation monster (i.e. any monster not generated from a
    vault) now has a 1/8 chance of being generated awake. These monsters can
    present the same kind of challenge to players negotiating seemingly
    cleared areas that spawns used to present. D:1 is excluded from
    generating these awake monsters, since the player doesn't have the
    ability to escape upwards and is particularly vulnerable in general.
    
    Portals, Abyss, and Pan are unaffected by this change as they either
    place all monsters through vaults (portals), or have their own monster
    generation rules: Abyss spawns monsters very frequently and Pan already
    generates all monsters awake.

--- crawl-ref/source/dungeon.cc
+++ crawl-ref/source/dungeon.cc
@@ -3763,7 +3763,6 @@ static void _builder_monsters()
     int mon_wanted = _num_mons_wanted();
 
     const bool in_shoals = player_in_branch(BRANCH_SHOALS);
-    const bool in_pan    = player_in_branch(BRANCH_PANDEMONIUM);
     if (in_shoals)
         dgn_shoals_generate_flora();
 
@@ -3777,12 +3776,20 @@ static void _builder_monsters()
     for (int i = 0; i < mon_wanted; i++)
     {
         mgen_data mg;
-        if (!in_pan)
+        // Chance to generate the monster awake, but away from level stairs.
+        if (player_in_connected_branch()
+            // Exclude D:1 from generating these awake monsters.
+            && (!player_in_branch(BRANCH_DUNGEON) || you.depth > 1)
+            && one_chance_in(8))
+        {
+            mg.proximity = PROX_AWAY_FROM_STAIRS;
+        }
+        // Pan monsters always generate awake.
+        else if (!player_in_branch(BRANCH_PANDEMONIUM))
             mg.behaviour = BEH_SLEEP;
         mg.flags    |= MG_PERMIT_BANDS;
         mg.map_mask |= MMT_NO_MONS;
         mg.preferred_grid_feature = preferred_grid_feature;
-
         place_monster(mg);
     }
 
