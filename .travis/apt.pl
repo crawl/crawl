3205e4252bd32345ec279d57e78dd9dfa0081dc1
Neil Moore <neil@s-z.org>
2017-09-01 15:51:14 -0400

    Try harder to fix travis BUILD_ALL builds.
    SDL *also* requires Xinerama.h.  Use xorg-dev to pull in all the X dev
    libraries.

--- .travis/apt.pl
+++ .travis/apt.pl
@@ -7,7 +7,7 @@ run(qw(sudo add-apt-repository ppa:zoogie/sdl2-snapshots -y));
 
 retry(qw(sudo apt-get update -qq));
 
-my @common_libs = qw(libxcursor-dev);
+my @common_libs = qw(xorg-dev);
 
 if ($ENV{CXX} eq "clang++") {
     retry(qw(sudo apt-get install -qq libstdc++6-4.7-dev), @common_libs);
