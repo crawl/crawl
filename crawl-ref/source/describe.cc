b23b10ae91e1c4cd5a3710432c7e11819cb59b25
Alan Malloy <alan@malloys.org>
2017-09-01 15:54:18 -0700

    Actually explain code instead of just saying it's a hack

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -3042,7 +3042,7 @@ static string _monster_attacks_description(const monster_info& mi)
         ++attack_counts[attack_info];
     }
 
-    // XXX: hack alert
+    // Hydrae have only one explicit attack, which is repeated for each head.
     if (mons_genus(mi.base_type) == MONS_HYDRA)
         for (auto &attack_count : attack_counts)
             attack_count.second = mi.num_heads;
