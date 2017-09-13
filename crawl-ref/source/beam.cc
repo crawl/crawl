7c7bb011eefbf3a1ba7eb9f5c2ad04efbace2e56
John Boyle <boylejm@gmail.com>
2017-09-13 00:50:12 -0700

    Discourage summoned creatures from Bolting their summoners (jwoodward48ss)

--- crawl-ref/source/beam.cc
+++ crawl-ref/source/beam.cc
@@ -4108,8 +4108,12 @@ void bolt::tracer_nonenchantment_affect_monster(monster* mon)
         }
         else
         {
-            friend_info.power
-                += 2 * final * mon->get_experience_level() / preac;
+            // Discourage summoned monsters firing on their summoner.
+            if (monster_by_mid(source_id)->summoner == mon->mid)
+                friend_info.power = 100;
+            else
+                friend_info.power
+                    += 2 * final * mon->get_experience_level() / preac;
             friend_info.count++;
         }
     }
