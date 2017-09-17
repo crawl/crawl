crawl/crawl

commit 44a4606457c614cd71b1c56c259aa7a0d801755e

Remove rods
Some rods have fun and unique effects, but the item class itself doesn't work
very well - it demands frequent item switching, and creates yet another
different item type that provides ranged attacks and summons (on top of
launchers, wands, regular evokables, ...). The iron rod and rod of clouds have
already been repurposed as high-tier wands. Rods of ignition and inaccuracy are
removed outright - they could in theory work as wands, but clouds and iron are
probably the better effects and there are already a large number of
direct-damage wands. Shadows is also removed outright, as it mostly duplicates
an existing scroll and spell (just with very slightly different rules).
Lightning rods are another neat effect but wouldn't function as a wand, so are
planned to be repurposed into some kind of evokable.

crawl-ref/source/target.cc
@@ -1167,61 +1167,6 @@ bool targeter_shadow_step::has_additional_sites(coord_def a)
     return temp_sites.size();
 }
 
-targeter_explosive_bolt::targeter_explosive_bolt(const actor *act, int pow, int r) :
-                          targeter_beam(act, r, ZAP_EXPLOSIVE_BOLT, pow, 0, 0)
-{
-}
-
-bool targeter_explosive_bolt::set_aim(coord_def a)
-{
-    if (!targeter_beam::set_aim(a))
-        return false;
-
-    bolt tempbeam = beam;
-    tempbeam.target = origin;
-    for (auto c : path_taken)
-    {
-        if (cell_is_solid(c))
-            break;
-
-        tempbeam.target = c;
-        if (anyone_there(c))
-        {
-            tempbeam.use_target_as_pos = true;
-            exp_map.init(INT_MAX);
-            tempbeam.determine_affected_cells(exp_map, coord_def(), 0,
-                                              1, true, true);
-        }
-    }
-
-    return true;
-}
-
-aff_type targeter_explosive_bolt::is_affected(coord_def loc)
-{
-    bool on_path = false;
-    for (auto c : path_taken)
-    {
-        if (cell_is_solid(c))
-            break;
-        if (c == loc)
-            on_path = true;
-        if (anyone_there(c)
-            && !beam.ignores_monster(monster_at(c))
-            && (loc - c).rdist() <= 9)
-        {
-            coord_def centre(9,9);
-            if (exp_map(loc - c + centre) < INT_MAX
-                && !cell_is_solid(loc))
-            {
-                return AFF_MAYBE;
-            }
-        }
-    }
-
-    return on_path ? AFF_TRACER : AFF_NO;
-}
-
 targeter_cone::targeter_cone(const actor *act, int r)
 {
     ASSERT(act);
@@ -1400,7 +1345,7 @@ aff_type targeter_shotgun::is_affected(coord_def loc)
 }
 
 targeter_monster_sequence::targeter_monster_sequence(const actor *act, int pow, int r) :
-                          targeter_beam(act, r, ZAP_EXPLOSIVE_BOLT, pow, 0, 0)
+                          targeter_beam(act, r, ZAP_DEBUGGING_RAY, pow, 0, 0)
 {
 }
 

commit 564118eadc1d2d9d6cce320fca9f7eb6cc738259

Fix line pass to not be cblink (11228)
This was broken in 44a4606457c614cd71b1c56c259aa7a0d801755e, by
replacing ZAP_EXPLOSIVE_BOLT with ZAP_DEBUGGING_RAY for
targeter_monster_sequence.  Z_D_R does not have can_beam set, and so
therefore leads to beam paths not being calculated in the way that
targeter_monster_sequence needs for its aiming functions.

crawl-ref/source/target.cc
@@ -1371,6 +1371,9 @@ aff_type targeter_shotgun::is_affected(coord_def loc)
 targeter_monster_sequence::targeter_monster_sequence(const actor *act, int pow, int r) :
                           targeter_beam(act, r, ZAP_DEBUGGING_RAY, pow, 0, 0)
 {
+    // for `path_taken` to be set properly, the beam needs to be piercing, and
+    // ZAP_DEBUGGING_RAY is not.
+    beam.pierce = true;
 }
 
 bool targeter_monster_sequence::set_aim(coord_def a)