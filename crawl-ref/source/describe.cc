7ea1c864dbf41c29bdf716a3acfc43a6b612b4cc
gammafunk <gammafunk@gmail.com>
2017-10-13 01:37:06 -0500

    Show wind immunity in monster info
    This was a hidden resistance that a few monsters have, and it effects
    player spells like Airstrike and Tornado, as well as the fan of gales,
    so it's better to show when a monster is immune to such things.

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -3060,6 +3060,8 @@ static const char* _get_resist_name(mon_resist_flags res_type)
         return "negative energy";
     case MR_RES_DAMNATION:
         return "damnation";
+    case MR_RES_WIND:
+        return "wind";
     default:
         return "buggy resistance";
     }
@@ -3492,6 +3494,7 @@ static string _monster_stat_description(const monster_info& mi)
         MR_RES_ELEC,    MR_RES_POISON, MR_RES_FIRE,
         MR_RES_STEAM,   MR_RES_COLD,   MR_RES_ACID,
         MR_RES_ROTTING, MR_RES_NEG,    MR_RES_DAMNATION,
+        MR_RES_WIND,
     };
 
     vector<string> extreme_resists;
@@ -3506,7 +3509,7 @@ static string _monster_stat_description(const monster_info& mi)
         if (level != 0)
         {
             const char* attackname = _get_resist_name(rflags);
-            if (rflags == MR_RES_DAMNATION)
+            if (rflags == MR_RES_DAMNATION || rflags == MR_RES_WIND)
                 level = 3; // one level is immunity
             level = max(level, -1);
             level = min(level,  3);
