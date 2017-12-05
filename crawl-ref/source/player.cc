04b8d97e730e2845214374931a016dbfdea8ec57
advil <rawlins@gmail.com>
2017-12-04 21:22:52 -0500

    Don't count closed door tiles as visible for items
    This prevents animate dead from working (Pinkbeast), and may have other
    effects. I think this works for all summoning, though I haven't checked
    monster simulacrum (which uses a different iterator).

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -5371,7 +5371,8 @@ bool player::can_water_walk() const
 
 int player::visible_igrd(const coord_def &where) const
 {
-    if (grd(where) == DNGN_LAVA
+    if (feat_is_closed_door(grd(where))
+        || grd(where) == DNGN_LAVA
         || (grd(where) == DNGN_DEEP_WATER
             && !species_likes_water(species)))
     {
