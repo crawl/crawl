b4d82c735bce8e33a4b12cdf802495e8c01c643d
Aidan Holm <aidanholm@gmail.com>
2017-12-14 01:35:31 +0800

    Don't show removed spells in ?/s menu (rchandra, #11287)

--- crawl-ref/source/lookup-help.cc
+++ crawl-ref/source/lookup-help.cc
@@ -413,6 +413,8 @@ static bool _spell_filter(string key, string body)
 
     if (spell == SPELL_NO_SPELL)
         return true;
+    if (spell_removed(spell))
+        return true;
 
     if (get_spell_flags(spell) & SPFLAG_TESTING)
         return !you.wizard;
