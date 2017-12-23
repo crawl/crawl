crawl/crawl

commit 5bb8f47916b7b8507622eaa91de6bb13ab602538

Refactor out an enum and actor property
The held_type enum was vestigial, since the previous uses are now
handled elsewhere. It's only used related to checks whether a monster's
constriction does damage and doesn't need to be marshalled. Remove this
property and add the necessary save compat to handle transferred games.
The constriction damage check is now handled via an actor method.

crawl-ref/source/tags.cc
@@ -721,7 +721,6 @@ static void _fix_missing_constrictions()
 
 static void _marshall_constriction(writer &th, const actor *who)
 {
-    _marshall_as_int(th, who->held);
     marshallInt(th, who->constricted_by);
     marshallInt(th, who->escape_attempts);
 
@@ -735,7 +734,10 @@ static void _marshall_constriction(writer &th, const actor *who)
 
 static void _unmarshall_constriction(reader &th, actor *who)
 {
-    who->held = unmarshall_int_as<held_type>(th);
+#if TAG_MAJOR_VERSION == 34
+    if (th.getMinorVersion() < TAG_MINOR_NO_ACTOR_HELD)
+        unmarshallInt(th);
+#endif
     who->constricted_by = unmarshallInt(th);
     who->escape_attempts = unmarshallInt(th);
 
@@ -5111,7 +5113,7 @@ void marshallMonster(writer &th, const monster& m)
     uint32_t parts = 0;
     if (mons_is_ghost_demon(m.type))
         parts |= MP_GHOST_DEMON;
-    if (m.held || m.constricting && m.constricting->size())
+    if (m.constricting && m.constricting->size())
         parts |= MP_CONSTRICTION;
     for (int i = 0; i < NUM_MONSTER_SLOTS; i++)
         if (m.inv[i] != NON_ITEM)

crawl-ref/source/MSVC/crawl.vcxproj
@@ -753,7 +753,6 @@ perl.exe "util/gen-cflg.pl" compflag.h "&lt;UNKNOWN&gt;" "&lt;UNKNOWN&gt;"
     <ClInclude Include="..\god-type.h" />
     <ClInclude Include="..\god-wrath.h" />
     <ClInclude Include="..\hash.h" />
-    <ClInclude Include="..\held-type.h" />
     <ClInclude Include="..\hints.h" />
     <ClInclude Include="..\hiscores.h" />
     <ClInclude Include="..\holy-word-source-type.h" />

crawl-ref/source/actor.cc
@@ -455,7 +455,6 @@ void actor::clear_clinging()
 void actor::clear_constricted()
 {
     constricted_by = 0;
-    held = HELD_NONE;
     escape_attempts = 0;
 }
 
@@ -565,7 +564,6 @@ void actor::start_constricting(actor &whom, int dur)
 
     (*constricting)[whom.mid] = dur;
     whom.constricted_by = mid;
-    whom.held = constriction_damage() ? HELD_CONSTRICTED : HELD_MONSTER;
 
     if (whom.is_player())
         you.redraw_evasion = true;

crawl-ref/source/actor.h
@@ -6,7 +6,6 @@
 #include "energy-use-type.h"
 #include "equipment-type.h"
 #include "god-type.h"
-#include "held-type.h"
 #include "item-prop-enum.h"
 #include "mon-holy-type.h"
 #include "random-var.h"
@@ -411,8 +410,6 @@ public:
 
     // Constriction stuff:
 
-    // What is holding us?  Not necessarily a monster.
-    held_type held;
     mid_t constricted_by;
     int escape_attempts;
 
@@ -438,6 +435,7 @@ public:
     int num_constricting() const;
     virtual bool has_usable_tentacle() const = 0;
     virtual int constriction_damage() const = 0;
+    virtual bool constriction_does_damage() const = 0;
     virtual bool clear_far_engulf() = 0;
 
     // Be careful using this, as it doesn't keep the constrictor in sync.

crawl-ref/source/held-type.h
@@ -1,10 +1,0 @@
-#pragma once
-
-enum held_type
-{
-    HELD_NONE = 0,
-    HELD_NET,         // currently unused
-    HELD_WEB,         // currently unused
-    HELD_MONSTER,     // but no damage
-    HELD_CONSTRICTED, // damaging
-};

crawl-ref/source/mon-info.cc
@@ -723,20 +723,18 @@ monster_info::monster_info(const monster* m, int milev)
     if (m->is_constricted())
     {
         const actor * const constrictor = actor_by_mid(m->constricted_by);
-        if (constrictor)
-        {
-            constrictor_name = (m->held == HELD_MONSTER ? "held by "
-                                                        : "constricted by ")
-                               + constrictor->name(_article_for(constrictor),
-                                                   true);
-        }
+        ASSERT(constrictor);
+        constrictor_name = (constrictor->constriction_does_damage() ?
+                            "held by " : "constricted by ")
+                           + constrictor->name(_article_for(constrictor),
+                                               true);
     }
 
     // names of what this monster is constricting, if any
     if (m->constricting)
     {
-        const char *gerund = m->constriction_damage() ? "constricting "
-                                                      : "holding ";
+        const char *gerund =
+            m->constriction_does_damage() ? "constricting " : "holding ";
         for (const auto &entry : *m->constricting)
         {
             if (const actor* const constrictee = actor_by_mid(entry.first))

crawl-ref/source/monster.cc
@@ -2961,6 +2961,11 @@ int monster::constriction_damage() const
     return -1;
 }
 
+bool monster::constriction_does_damage() const
+{
+    return constriction_damage();
+}
+
 /** Return true if the monster temporarily confused. False for butterflies, or
     other permanently confused monsters.
 */

crawl-ref/source/monster.h
@@ -446,6 +446,7 @@ public:
     bool has_attack_flavour(int flavour) const;
     bool has_damage_type(int dam_type);
     int constriction_damage() const override;
+    bool constriction_does_damage() const override;
 
     bool can_throw_large_rocks() const override;
     bool can_speak();

commit c60411f26562fa236c56766376ccf5ebdba71b93

Fix monster constriction data not getting marshalled
Broken in 5bb8f4791 as part of removing actor.held, we forgot to replace
a check with one of ::is_constricted().

A monster saved when it was constricted but not constricting something
itself wouldn't have its constriction data marshalled. So it would lose
all constriction state upon reload, causing a crash if the actor
constricting the monster attempted to constrict it again. In this case,
::is_constricted() would return false for the monster, yet it would
already be in its constrictor's constricting map.

This commit re-adds the constrictor check and uses the appropriate
methods to check constriction state instead of duplicating them.

crawl-ref/source/tags.cc
@@ -5113,7 +5113,7 @@ void marshallMonster(writer &th, const monster& m)
     uint32_t parts = 0;
     if (mons_is_ghost_demon(m.type))
         parts |= MP_GHOST_DEMON;
-    if (m.constricting && m.constricting->size())
+    if (m.is_constricted() || m.is_constricting())
         parts |= MP_CONSTRICTION;
     for (int i = 0; i < NUM_MONSTER_SLOTS; i++)
         if (m.inv[i] != NON_ITEM)