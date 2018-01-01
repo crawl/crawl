e575cb9ce86ff54d8ea3bdb959ad354ecd3c8371
Aidan Holm <aidanholm@gmail.com>
2018-01-02 00:15:51 +0800

    Postpone timed portal messages bofore level load (Yermak, #11279)
    TimedMessaging now ignores all turn events that occur before an
    ENTERED_LEVEL event, and the ENTERED_LEVEL event now also causes
    an elapsed time check.
    
    (cherry picked from commit e27d229730f958c901bd97600921c31e192e9853)

--- crawl-ref/source/dat/dlua/lm_timed.lua
+++ crawl-ref/source/dat/dlua/lm_timed.lua
@@ -108,6 +108,7 @@ function TimedMarker:event(marker, ev)
 
   if ev:type() == dgn.dgn_event_type('entered_level') then
     self:start()
+    self.msg:event(self, marker, ev)
   elseif ev:type() == dgn.dgn_event_type('player_los') then
     self:start_short(marker)
   elseif ev:type() == self.ticktype then
