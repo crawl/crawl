c0c383b3cde41077eccfe512133b419a2386c40c
gammafunk <gammafunk@gmail.com>
2018-01-01 15:03:54 -0600

    A batch of updated elven monster tiles (roctavian, #11340)
    Updates for all the elven monsters, including the uniques Dowan,
    Duvessa, and Fannar.
    
    Deep elf elementalists are supposed to have one of three tiles that
    cycles with each UI action as nice way to make them distinct and
    visualize their elemental nature, but this needs some additional code
    changes. Right now, using these in a cycle wouldn't display their
    wielded weapon for any tile except the first variant. I'm adding the
    variants, but only enabling use of the first one until said code changes
    can happen.

--- crawl-ref/source/tilepick.cc
+++ crawl-ref/source/tilepick.cc
@@ -3953,6 +3953,7 @@ void tile_init_props(monster* mon)
         case MONS_BLOCK_OF_ICE:
         case MONS_BUTTERFLY:
         case MONS_HUMAN:
+        case MONS_DEEP_ELF_ELEMENTALIST:
             break;
         default:
             return;
