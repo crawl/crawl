9658042124ba064ebae355b203bef8dc030089a3
gammafunk <gammafunk@gmail.com>
2018-01-03 14:31:30 -0600

    Have all forms of monster movement break indirect constriction (mibe)
    Previously monsters would continue to be indirectly constricted under
    Borgnjor's Vile Clutch if they were trampled, yanked with Lesser
    Beckoning, did a swoop attack, etc, since the move only caused
    direct constriction to be checked for validity.
    
    This commit unifies the checks for all forms of constrictions and
    simplifies the code to have the validity checks for both happen through
    a single method. Now any form of movement will cause the monster's
    zombie hands constriction to end.

--- crawl-ref/source/actor.h
+++ crawl-ref/source/actor.h
@@ -428,11 +428,8 @@ public:
     void stop_being_constricted(bool quiet = false);
 
     bool can_constrict(const actor* defender, bool direct) const;
-    bool has_invalid_direct_constrictor(const coord_def &where) const;
-    void clear_direct_constrictions_far_from(const coord_def &where);
-    bool has_invalid_indirect_constrictor() const;
-    void clear_invalid_indirect_constrictions();
-    void clear_invalid_constrictions();
+    bool has_invalid_constrictor(bool move = false) const;
+    void clear_invalid_constrictions(bool move = false);
     void accum_has_constricted();
     void handle_constriction();
     bool is_constricted() const;
