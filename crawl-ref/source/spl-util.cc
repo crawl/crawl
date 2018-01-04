002c4683ae4d4c83f5412c4db16aa869169bae8e
gammafunk <gammafunk@gmail.com>
2018-01-04 17:03:39 -0600

    Fix the smite targeting of Borgnjor's Vile Clutch (mibe, 11334)
    Currently the function to determine if susceptible monsters are in LOS
    considers this spell as a beam exploding spell. This means any monsters
    or other obstructions along the path to a valid target has the UI's
    tracer beam explode on the obstruction, causing the beam to not count
    the valid target as affected. The UI then incorrectly tells the player
    that no valid target is in view.
    
    This commit changes spell_no_hostile_in_range to handle the relevant
    tracer for smite-targeted spells directly through an explosion at the
    target. Now the UI correctly ignores obstructions for determining if
    hostiles are in range for Vile Clutch.
    
    TODO:
    
    1) I leave a comment noting how some solid and non-reachable targets
    should probably be bypassed for the hostile-in-range check.
    
    2) I leave another comment noting how explosion radii data for spells
    are not stored in any central place, and it would be good to do that.
    
    3) Make the necessary zap_data entries so smite-targeted spells can use
    the zap tracer loop in spell_no_hostile_in_range to properly determine
    when a susceptible monster is in view. Currently the UI will only
    consider range for smite-targeted spells without zap data. This means it
    will consider e.g. an rWind monster to be a valid hostile in range for
    Airstrike initially, only preventing the targeting later when the player
    finally accepts that target in the UI.
    
    4) I'm not actually sure that having to make zap entries for spells only
    so they can properly consider targets in range is a good idea. Might
    need to look at what the main intent of zap_data actually is.

--- crawl-ref/source/spl-util.cc
+++ crawl-ref/source/spl-util.cc
@@ -1446,12 +1446,39 @@ bool spell_no_hostile_in_range(spell_type spell)
 #ifdef DEBUG_DIAGNOSTICS
         beam.quiet_debug = true;
 #endif
+
+        const bool smite = testbits(flags, SPFLAG_TARGET);
+
         for (radius_iterator ri(you.pos(), range, C_SQUARE, LOS_DEFAULT);
              ri; ++ri)
         {
             tempbeam = beam;
             tempbeam.target = *ri;
-            tempbeam.fire();
+
+            // For smite-targeted spells that aren't LOS-range.
+            if (smite)
+            {
+                // XXX These are basic checks that might be applicable to
+                // non-smiting spells as well. For those, the loop currently
+                // relies mostly on results from the temp beam firing, but it
+                // may be valid to exclude solid and non-reachable targets for
+                // all spells. -gammafunk
+                if (cell_is_solid(*ri) || !you.see_cell_no_trans(*ri))
+                    continue;
+
+                // XXX Currently Vile Clutch is the only smite-targeted area
+                // spell that isn't LOS-range. Spell explosion radii are not
+                // stored anywhere, defaulting to 1 for non-smite-targeting
+                // spells through bolt::refine_for_explosions() or being set in
+                // setup functions for the smite targeted explosions. It would
+                // be good to move basic explosion radius info into spell_desc
+                // or possibly zap_data. -gammafunk
+                tempbeam.ex_size = tempbeam.is_explosion ? 1 : 0;
+                tempbeam.explode();
+            }
+            else
+                tempbeam.fire();
+
             if (tempbeam.foe_info.count > 0
                 || neutral && tempbeam.friend_info.count > 0)
             {
