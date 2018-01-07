crawl/crawl

commit 5bb8f47916b7b8507622eaa91de6bb13ab602538

Refactor out an enum and actor property
The held_type enum was vestigial, since the previous uses are now
handled elsewhere. It's only used related to checks whether a monster's
constriction does damage and doesn't need to be marshalled. Remove this
property and add the necessary save compat to handle transferred games.
The constriction damage check is now handled via an actor method.

crawl-ref/source/status.cc
@@ -417,9 +417,14 @@ bool fill_status_info(int status, status_info* inf)
     case STATUS_CONSTRICTED:
         if (you.is_constricted())
         {
+            const monster * const cstr = monster_by_mid(you.constricted_by);
+            ASSERT(cstr);
+            const bool damage =
+                cstr->constriction_does_damage();
+
             inf->light_colour = YELLOW;
-            inf->light_text   = you.held == HELD_MONSTER ? "Held" : "Constr";
-            inf->short_text   = you.held == HELD_MONSTER ? "held" : "constricted";
+            inf->light_text   = damage ? "Held" : "Constr";
+            inf->short_text   = damage ? "held" : "constricted";
         }
         break;
 

crawl-ref/source/MSVC/crawl.vcxproj
@@ -753,7 +753,6 @@ perl.exe "util/gen-cflg.pl" compflag.h "&lt;UNKNOWN&gt;" "&lt;UNKNOWN&gt;"
     <ClInclude Include="..\god-type.h" />
     <ClInclude Include="..\god-wrath.h" />
     <ClInclude Include="..\hash.h" />
-    <ClInclude Include="..\held-type.h" />
     <ClInclude Include="..\hints.h" />
     <ClInclude Include="..\hiscores.h" />
     <ClInclude Include="..\holy-word-source-type.h" />

crawl-ref/source/actor.cc
@@ -455,7 +455,6 @@ void actor::clear_clinging()
 void actor::clear_constricted()
 {
     constricted_by = 0;
-    held = HELD_NONE;
     escape_attempts = 0;
 }
 
@@ -565,7 +564,6 @@ void actor::start_constricting(actor &whom, int dur)
 
     (*constricting)[whom.mid] = dur;
     whom.constricted_by = mid;
-    whom.held = constriction_damage() ? HELD_CONSTRICTED : HELD_MONSTER;
 
     if (whom.is_player())
         you.redraw_evasion = true;

crawl-ref/source/actor.h
@@ -6,7 +6,6 @@
 #include "energy-use-type.h"
 #include "equipment-type.h"
 #include "god-type.h"
-#include "held-type.h"
 #include "item-prop-enum.h"
 #include "mon-holy-type.h"
 #include "random-var.h"
@@ -411,8 +410,6 @@ public:
 
     // Constriction stuff:
 
-    // What is holding us?  Not necessarily a monster.
-    held_type held;
     mid_t constricted_by;
     int escape_attempts;
 
@@ -438,6 +435,7 @@ public:
     int num_constricting() const;
     virtual bool has_usable_tentacle() const = 0;
     virtual int constriction_damage() const = 0;
+    virtual bool constriction_does_damage() const = 0;
     virtual bool clear_far_engulf() = 0;
 
     // Be careful using this, as it doesn't keep the constrictor in sync.

crawl-ref/source/held-type.h
@@ -1,10 +1,0 @@
-#pragma once
-
-enum held_type
-{
-    HELD_NONE = 0,
-    HELD_NET,         // currently unused
-    HELD_WEB,         // currently unused
-    HELD_MONSTER,     // but no damage
-    HELD_CONSTRICTED, // damaging
-};

crawl-ref/source/mon-info.cc
@@ -723,20 +723,18 @@ monster_info::monster_info(const monster* m, int milev)
     if (m->is_constricted())
     {
         const actor * const constrictor = actor_by_mid(m->constricted_by);
-        if (constrictor)
-        {
-            constrictor_name = (m->held == HELD_MONSTER ? "held by "
-                                                        : "constricted by ")
-                               + constrictor->name(_article_for(constrictor),
-                                                   true);
-        }
+        ASSERT(constrictor);
+        constrictor_name = (constrictor->constriction_does_damage() ?
+                            "held by " : "constricted by ")
+                           + constrictor->name(_article_for(constrictor),
+                                               true);
     }
 
     // names of what this monster is constricting, if any
     if (m->constricting)
     {
-        const char *gerund = m->constriction_damage() ? "constricting "
-                                                      : "holding ";
+        const char *gerund =
+            m->constriction_does_damage() ? "constricting " : "holding ";
         for (const auto &entry : *m->constricting)
         {
             if (const actor* const constrictee = actor_by_mid(entry.first))

crawl-ref/source/monster.cc
@@ -2961,6 +2961,11 @@ int monster::constriction_damage() const
     return -1;
 }
 
+bool monster::constriction_does_damage() const
+{
+    return constriction_damage();
+}
+
 /** Return true if the monster temporarily confused. False for butterflies, or
     other permanently confused monsters.
 */

crawl-ref/source/monster.h
@@ -446,6 +446,7 @@ public:
     bool has_attack_flavour(int flavour) const;
     bool has_damage_type(int dam_type);
     int constriction_damage() const override;
+    bool constriction_does_damage() const override;
 
     bool can_throw_large_rocks() const override;
     bool can_speak();

commit 614aadd2715bbbcd34603352e6178121f811295d

Fix a crash when attempting to print constriction status (dweomer)
Broken in 5bb8f479, we have an ASSERT when the player is constricted,
but we're unable to look up the corresponding monster. This commit uses
the proper check for a valid constrictor, returning false in the
relevant function if the constrictor is invalid for any reason, which
will prevent constriction being printed as a status.

crawl-ref/source/status.cc
@@ -405,8 +405,13 @@ bool fill_status_info(int status, status_info* inf)
     case STATUS_CONSTRICTED:
         if (you.is_constricted())
         {
+            // Our constrictor isn't, valid so don't report this status.
+            if (you.has_invalid_constrictor())
+                return false;
+
             const monster * const cstr = monster_by_mid(you.constricted_by);
             ASSERT(cstr);
+
             const bool damage =
                 cstr->constriction_does_damage(you.is_directly_constricted());
 