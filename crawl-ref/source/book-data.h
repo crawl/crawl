76daed49dd4e8b875119e2fd45f9bfb47f160300
Floodkiller <floodkiller3228@gmail.com>
2018-01-07 21:33:53 -0600

    Revert "Make Singularity a monster-only spell"
    This reverts commit e3e75563cf90702bce370c39fa33b12d774062ee.

--- crawl-ref/source/book-data.h
+++ crawl-ref/source/book-data.h
@@ -297,6 +297,7 @@ static const vector<spell_type> spellbook_templates[] =
     SPELL_MALIGN_GATEWAY,
     SPELL_DISJUNCTION,
     SPELL_CONTROLLED_BLINK,
+    SPELL_SINGULARITY,
 },
 
 {   // Book of Debilitation
