661249c9fd470940ee48f936d5be320eb89df32c
Aidan Holm <aidanholm@gmail.com>
2018-01-12 23:34:23 +0800

    Show tiles for more descriptions
    Skills, abilities, cards (just the generic card tile), and clouds

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -3250,7 +3250,12 @@ void describe_ability(ability_type ability)
     describe_info inf;
     inf.title = ability_name(ability);
     inf.body << get_ability_desc(ability, false);
+#ifdef USE_TILE
+    tile_def tile = tile_def(tileidx_ability(ability), TEX_GUI);
+    show_description(inf, &tile);
+#else
     show_description(inf);
+#endif
 }
 
 
@@ -4607,7 +4612,12 @@ void describe_skill(skill_type skill)
     describe_info inf;
     inf.title = skill_name(skill);
     inf.body << get_skill_description(skill, false);
+#ifdef USE_TILE
+    tile_def tile = tile_def(tileidx_skill(skill, TRAINING_ENABLED), TEX_GUI);
+    show_description(inf, &tile);
+#else
     show_description(inf);
+#endif
 }
 
 // only used in tiles
