db7e5fb388e0b1562973ff5ce682ef7f866d7f00
Alex Jurkiewicz <alex@jurkiewi.cz>
2018-01-23 11:38:00 +1100

    Tag a reference to MONS_BEETLE

--- crawl-ref/source/spl-summoning.cc
+++ crawl-ref/source/spl-summoning.cc
@@ -1701,7 +1701,9 @@ static bool _raise_remains(const coord_def &pos, int corps, beh_type beha,
         *motions_r |= DEAD_ARE_HOPPING;
     }
     else if (mons_genus(zombie_type)    == MONS_WORKER_ANT
+#if TAG_MAJOR_VERSION == 34
              || mons_genus(zombie_type) == MONS_BEETLE
+#endif
              || mons_base_char(zombie_type) == 's') // many genera
     {
         *motions_r |= DEAD_ARE_CRAWLING;
