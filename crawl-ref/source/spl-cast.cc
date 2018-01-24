07955c6c584410fe530dcc1db52a814f0677e048
Aidan Holm <aidanholm@gmail.com>
2018-01-25 03:53:02 +0800

    Omit very-dangerous message on 100% spell failure

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -1123,18 +1123,18 @@ static bool _spellcasting_aborted(spell_type spell, bool fake_spell)
         && !crawl_state.disables[DIS_CONFIRMATIONS]
         && !fake_spell)
     {
+        if (failure_rate_to_int(raw_spell_fail(spell)) == 100)
+        {
+            mprf(MSGCH_WARN, "It is impossible to cast this spell "
+                    "(100%% risk of failure)!");
+            return true;
+        }
+
         string prompt = make_stringf("The spell is %s to cast "
                                      "(%s risk of failure)%s",
                                      fail_severity_adjs[severity],
                                      failure_rate.c_str(),
                                      severity > 1 ? "!" : ".");
-
-        if (failure_rate_to_int(raw_spell_fail(spell)) == 100)
-        {
-            mprf(MSGCH_WARN, "%s", prompt.c_str());
-            mprf(MSGCH_WARN, "It is impossible to cast this spell!");
-            return true;
-        }
 
         prompt = make_stringf("%s Continue anyway?", prompt.c_str());
         if (!yesno(prompt.c_str(), false, 'n'))
