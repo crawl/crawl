crawl/crawl

commit 63fd1ff4893554aaee613dba326593781b505521

Make sure vampires in statue form get rPois+++ (papilio)

crawl-ref/source/player.cc
@@ -6180,15 +6180,13 @@ bool player::heal(int amount)
  */
 mon_holy_type player::holiness(bool temp) const
 {
-    mon_holy_type holi = MH_NATURAL;
+    mon_holy_type holi = undead_state(temp) ? MH_UNDEAD : MH_NATURAL;
+
     if (species == SP_GARGOYLE ||
         temp && (form == TRAN_STATUE || form == TRAN_WISP || petrified()))
     {
         holi = MH_NONLIVING;
     }
-
-    if (undead_state(temp))
-        holi = MH_UNDEAD;
 
     if (is_good_god(religion))
         holi |= MH_HOLY;

commit 307c7c3803888908571fd9314f1d79f27250220d

Allow lich form to set gargoyle holiness to undead (#688)
This was broken in 63fd1ff.

crawl-ref/source/player.cc
@@ -6035,11 +6035,20 @@ bool player::heal(int amount)
  */
 mon_holy_type player::holiness(bool temp) const
 {
-    mon_holy_type holi = undead_state(temp) ? MH_UNDEAD : MH_NATURAL;
+    mon_holy_type holi;
 
-    if (species == SP_GARGOYLE ||
-        temp && (form == transformation::statue
-                 || form == transformation::wisp || petrified()))
+    // Lich form takes precedence over a species' base holiness
+    if (undead_state(temp))
+        holi = MH_UNDEAD;
+    else if (species == SP_GARGOYLE)
+        holi = MH_NONLIVING;
+    else
+        holi = MH_NATURAL;
+
+    // Petrification takes precedence over base holiness and lich form
+    if (temp && (form == transformation::statue
+                 || form == transformation::wisp
+                 || petrified()))
     {
         holi = MH_NONLIVING;
     }