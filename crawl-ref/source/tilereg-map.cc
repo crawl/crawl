46506fa5f322bc9b28cced194d68ba242b29e893
wheals <shm.mark@gmail.com>
2018-01-29 21:59:37 -0500

    Fix local tiles minimap not properly showing transporters.

--- crawl-ref/source/tilereg-map.cc
+++ crawl-ref/source/tilereg-map.cc
@@ -65,6 +65,8 @@ void MapRegion::init_colours()
     m_colours[MF_EXCL_ROOT]     = Options.tile_excl_centre_col;
     m_colours[MF_EXCL]          = Options.tile_excluded_col;
     m_colours[MF_PLAYER]        = Options.tile_player_col;
+    m_colours[MF_TRANSPORTER]   = Options.tile_transporter_col;
+    m_colours[MF_TRANSPORTER_LANDING] = Options.tile_transporter_landing_col;
 }
 
 MapRegion::~MapRegion()
