83a29c404893b60d39f984589bdb734e059e539b
hellmonk <nldehaan91@gmail.com>
2018-02-03 13:20:16 -0600

    grue devour

--- crawl-ref/source/melee-attack.cc
+++ crawl-ref/source/melee-attack.cc
@@ -727,7 +727,7 @@ static void _hydra_consider_devouring(monster &defender)
  */
 bool melee_attack::handle_phase_killed()
 {
-    if (attacker->is_player() && you.form == transformation::hydra
+    if (attacker->is_player() && (you.species == SP_GRUE || you.form == transformation::hydra)
         && defender->is_monster() // better safe than sorry
         && defender->type != MONS_NO_MONSTER) // already reset
     {
