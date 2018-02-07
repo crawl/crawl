crawl/crawl

commit bc573f34565edbc8024cc9e61e9a541d477607a3

Let amulet of the acrobat work while waiting (Hellmonk)
Since otherwise people will be incentivized to move and forth while waiting.

Also, some light clean-up on the initial Acrobat commit and actually
enable the amulet to spawn.

crawl-ref/source/main.cc
@@ -1113,10 +1113,10 @@ static void _input()
         crawl_state.waiting_for_command = true;
         c_input_reset(true);
 
-        // Clear "last action was a move" flag.
-        if (you.props[LAST_ACTION_WAS_MOVE_KEY].get_bool())
+        // Clear "last action was a move or rest" flag.
+        if (you.props[LAST_ACTION_WAS_MOVE_OR_REST_KEY].get_bool())
         {
-            you.props[LAST_ACTION_WAS_MOVE_KEY] = false;
+            you.props[LAST_ACTION_WAS_MOVE_OR_REST_KEY] = false;
             you.redraw_evasion = true;
         }
 
@@ -1756,7 +1756,7 @@ void process_command(command_type cmd)
         break;
     }
 #endif
-    case CMD_REST:            _do_rest(); break;
+    case CMD_REST:           _do_rest(); break;
 
     case CMD_GO_UPSTAIRS:
     case CMD_GO_DOWNSTAIRS:
@@ -1822,7 +1822,7 @@ void process_command(command_type cmd)
             break;
         // else fall-through
     case CMD_WAIT:
-        you.turn_is_over = true;
+        update_acrobat_status();
         break;
 
     case CMD_PICKUP:
@@ -3335,16 +3335,9 @@ static void _move_player(coord_def move)
     if (you_worship(GOD_WU_JIAN) && !attacking)
         wu_jian_post_move_effects(did_wall_jump, initial_position);
 
-    // If you actually moved, then you are eligible for amulet of the acrobat.
-    if (you.props[ACROBAT_AMULET_ACTIVE].get_int() == 1
-        && !attacking
-        && moving
-        && !did_wall_jump)
-    {
-        you.duration[DUR_ACROBAT] = you.time_taken;
-        you.props[LAST_ACTION_WAS_MOVE_KEY] = true;
-        you.redraw_evasion = true;
-    }
+    // If you actually moved you are eligible for amulet of the acrobat.
+    if (!attacking && moving && !did_wall_jump)
+        update_acrobat_status();
 }
 
 static int _get_num_and_char(const char* prompt, char* buf, int buf_len)

crawl-ref/source/describe.cc
@@ -140,7 +140,7 @@ const char* jewellery_base_ability_string(int subtype)
     case AMU_HARM:                return "Harm";
     case AMU_MANA_REGENERATION:   return "RegenMP";
     case AMU_THE_GOURMAND:        return "Gourm";
-    case AMU_ACROBAT:           return "Acrobat";
+    case AMU_ACROBAT:             return "Acrobat";
 #if TAG_MAJOR_VERSION == 34
     case AMU_CONSERVATION:        return "Cons";
     case AMU_CONTROLLED_FLIGHT:   return "cFly";
@@ -387,7 +387,7 @@ static const char* _jewellery_base_ability_description(int subtype)
     case AMU_THE_GOURMAND:
         return "It allows you to eat raw meat even when not hungry.";
     case AMU_ACROBAT:
-        return "It helps you evade while moving.";
+        return "It helps you evade while moving and waiting.";
 #if TAG_MAJOR_VERSION == 34
     case AMU_CONSERVATION:
         return "It protects your inventory from destruction.";

crawl-ref/source/item-name.cc
@@ -825,6 +825,7 @@ const char* jewellery_effect_name(int jeweltype, bool terse)
         case AMU_RAGE:                   return "+Rage";
         case AMU_REGENERATION:           return "Regen";
         case AMU_REFLECTION:             return "Reflect";
+        case AMU_ACROBAT:                return "Acrobat";
         case AMU_NOTHING:                return "";
         default: return "buggy";
         }

crawl-ref/source/item-prop-enum.h
@@ -228,8 +228,6 @@ enum jewellery_type
     AMU_FIRST_AMULET = AMU_RAGE,
     AMU_HARM,
     AMU_ACROBAT,
-#if TAG_MAJOR_VERSION == 34
-#endif
     AMU_MANA_REGENERATION,
     AMU_THE_GOURMAND,
 #if TAG_MAJOR_VERSION == 34

crawl-ref/source/item-prop.cc
@@ -738,7 +738,6 @@ const set<pair<object_class_type, int> > removed_items =
 #if TAG_MAJOR_VERSION == 34
     { OBJ_JEWELLERY, AMU_CONTROLLED_FLIGHT },
     { OBJ_JEWELLERY, AMU_CONSERVATION },
-    { OBJ_JEWELLERY, AMU_ACROBAT },
     { OBJ_JEWELLERY, RING_REGENERATION },
     { OBJ_JEWELLERY, RING_SUSTAIN_ATTRIBUTES },
     { OBJ_JEWELLERY, RING_TELEPORT_CONTROL },

crawl-ref/source/output.cc
@@ -506,7 +506,7 @@ static bool _boosted_ev()
     return you.duration[DUR_AGILITY]
            || (you.wearing(EQ_AMULET, AMU_ACROBAT)
                 && (you.duration[DUR_ACROBAT]
-                   || you.props[LAST_ACTION_WAS_MOVE_KEY].get_bool()));
+                   || you.props[LAST_ACTION_WAS_MOVE_OR_REST_KEY].get_bool()));
 }
 
 static bool _boosted_sh()

crawl-ref/source/player.cc
@@ -2054,6 +2054,16 @@ bool player_is_shapechanged()
     return true;
 }
 
+void update_acrobat_status()
+{
+    if (you.props[ACROBAT_AMULET_ACTIVE].get_int() != 1)
+        return;
+
+    you.duration[DUR_ACROBAT] = you.time_taken;
+    you.props[LAST_ACTION_WAS_MOVE_OR_REST_KEY] = true;
+    you.redraw_evasion = true;
+}
+
 // An evasion factor based on the player's body size, smaller == higher
 // evasion size factor.
 static int _player_evasion_size_factor(bool base = false)
@@ -2070,6 +2080,7 @@ int player_shield_racial_factor()
     return max(1, 5 + (you.species == SP_FORMICID ? -2 // Same as trolls/centaurs/etc.
                                                   : _player_evasion_size_factor(true)));
 }
+
 
 // The total EV penalty to the player for all their worn armour items
 // with a base EV penalty (i.e. EV penalty as a base armour property,
@@ -2121,7 +2132,7 @@ static int _player_evasion_bonuses()
     // If you have an active amulet of the acrobat and just moved, get massive
     // EV bonus. We also display this bonus if the duration isn't in effect but
     // it was during the last move. It's a little hacky.
-    if (you.duration[DUR_ACROBAT] || you.props[LAST_ACTION_WAS_MOVE_KEY].get_bool())
+    if (you.duration[DUR_ACROBAT] || you.props[LAST_ACTION_WAS_MOVE_OR_REST_KEY].get_bool())
         evbonus += 15;
 
     return evbonus;

crawl-ref/source/player.h
@@ -51,7 +51,7 @@
 #define SAP_MAGIC_KEY "sap_magic_amount"
 #define TEMP_WATERWALK_KEY "temp_waterwalk"
 #define EMERGENCY_FLIGHT_KEY "emergency_flight"
-#define LAST_ACTION_WAS_MOVE_KEY "last_action_was_move"
+#define LAST_ACTION_WAS_MOVE_OR_REST_KEY "last_action_was_move_or_rest"
 
 // display/messaging breakpoints for penalties from Ru's MUT_HORROR
 #define HORROR_LVL_EXTREME  3
@@ -936,6 +936,8 @@ bool player_equip_unrand(int unrand_index);
 bool player_can_hear(const coord_def& p, int hear_distance = 999);
 
 bool player_is_shapechanged();
+
+void update_acrobat_status();
 
 bool is_effectively_light_armour(const item_def *item);
 bool player_effectively_in_light_armour();

commit 548d40651e771348404cf7746f7ab25832dff58b

Make waiting work again.
Broken in bc573f345, which made waiting take neither time nor a turn.

crawl-ref/source/main.cc
@@ -1823,6 +1823,7 @@ void process_command(command_type cmd)
         // else fall-through
     case CMD_WAIT:
         update_acrobat_status();
+        you.turn_is_over = true;
         break;
 
     case CMD_PICKUP: