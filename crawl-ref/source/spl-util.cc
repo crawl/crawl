86db44f4b3afe42cf42290c3a7d95ac1b1e18e33
Chris Campbell <chriscampbell89@gmail.com>
2018-02-20 15:36:55 +0000

    Mark PoG only temporarily useless when in Zot but without the orb (|amethyst)
    Since you could still leave and go elsewhere, so it should still be possible
    to memorise the spell.
    
    (cherry picked from commit 4386d7c10379cb1bdda37193e3f80c7d5eb57a65)

--- crawl-ref/source/spl-util.cc
+++ crawl-ref/source/spl-util.cc
@@ -1303,7 +1303,7 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
         break;
 
     case SPELL_GOLUBRIAS_PASSAGE:
-        if (orb_limits_translocation())
+        if (orb_limits_translocation(temp))
             return "the Orb prevents this spell from working.";
 
     default:
