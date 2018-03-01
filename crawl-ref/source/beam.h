47ba4cb28f6792185a3bbb8235124106e34f4ce8
Timothy Stiles <Patashu@hotmail.com>
2018-03-01 16:14:07 +1100

    Disintegration/IOOD/devastation bolts dig once more
    This reverts:
    https://github.com/crawl/crawl/commit/85f13f00f2816064f2b3d4dc3b800213a65987e2
    https://github.com/crawl/crawl/commit/40a51bc99568a3f660d561598d68db0217514645

--- crawl-ref/source/beam.h
+++ crawl-ref/source/beam.h
@@ -246,6 +246,7 @@ private:
     void affect_wall();
     void digging_wall_effect();
     void burn_wall_effect();
+    void destroy_wall_effect();
     void affect_ground();
     void affect_place_clouds();
     void affect_place_explosion_clouds();
