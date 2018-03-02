883c9907d064098ac1a3886ec1cce8679857a504
Timothy Stiles <Patashu@hotmail.com>
2018-03-02 19:27:58 +1100

    Fix failing test
    'Abyss:13: no valid skeletons in any parallel branch'
    So cap OODs to 12. Problem solved!

--- crawl-ref/source/mon-pick.cc
+++ crawl-ref/source/mon-pick.cc
@@ -29,6 +29,7 @@ int branch_ood_cap(branch_type branch)
         return 14;
     case BRANCH_VAULTS:
     case BRANCH_LAIR:
+    case BRANCH_ABYSS:
         return 12;
     case BRANCH_ELF:
         return 7;
