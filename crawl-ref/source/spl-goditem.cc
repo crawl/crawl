57864ee12feaaea81e5b7fe94ebc0beaa4a5030b
Aidan Holm <aidanholm@gmail.com>
2018-03-05 00:41:19 +0800

    Make entombment more consistent (NormalPerson7, #11405)
    This commit simplifies the set of features that the tomb card effect
    will convert to a wall. The feat_is_door() check allows closed doors
    to be filled; before this commit, they were left untouched and could
    be opened.

--- crawl-ref/source/spl-goditem.cc
+++ crawl-ref/source/spl-goditem.cc
@@ -837,18 +837,11 @@ static bool _do_imprison(int pow, const coord_def& where, bool zin)
             veto_spots.push_back(newpos);
         }
 
-        // Make sure we have a legitimate tile.
-        proceed = false;
-        if (!zin && !monster_at(*ai))
-        {
-            if (feat_is_trap(grd(*ai), true) || feat_is_stone_stair(grd(*ai))
-                || safe_tiles.count(grd(*ai)))
-            {
-                proceed = true;
-            }
-        }
-        else if (zin && !cell_is_solid(*ai))
-            proceed = true;
+        // closed doors are solid, but we don't want a behaviour difference
+        // between open and closed doors
+        proceed = !cell_is_solid(*ai) || feat_is_door(grd(*ai));
+        if (!zin && monster_at(*ai))
+            proceed = false;
 
         if (proceed)
         {
