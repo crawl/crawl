88af0b8d53e242a8348872a1792b1db184901133
Alex Jurkiewicz <alex@jurkiewi.cz>
2018-03-15 23:46:53 +1100

    Generate species data from YAML files
    This converts the following files from hand-written to auto-generated:
    
    * aptitudes.h
    * species-data.h
    * species-groups.h (was previously hardcoded in newgame.cc)
    * species-type.h
    
    Species definitions are found in `source/dat/species/*.yaml`.
    Documentation is in the (rewritten) `docs/develop/species_creation.md`.

---
+++ crawl-ref/docs/develop/species_creation.md
@@ -0,0 +1,47 @@
+# DCSS Species Definitions
+
+DCSS uses YAML files to define species, these files can be found in [source/dat/species](../../source/dat/species).
+
+To create a new species, you put an appropriate YAML file in there and compile DCSS. For any custom species behaviour which the YAML file can't express (for example, Octopode's eight ring slots), you'll also need custom C++ code added elsewhere.
+
+## Species Definition Format
+
+You can view existing files to get a sense of the format, it's quite straightforward.
+
+### Supported Keys
+
+| key  | type | required | description |
+| ---- | ---- | -------- | ----------- |
+| enum | `string` | **Yes** | Set the species enum. Must match the pattern `SP_[A-Z]+`. |
+| monster | `string` | **Yes** | Species' corresponding monster. Must match the pattern `MONS_[A-Z]+`. |
+| name | `string` | **Yes** | Species name, like 'Deep Dwarf' or 'Felid'. Required. Must be unique. |
+| short_name | `string` | No | Two letter species code. Must be unique (excepting special cases like Draconian sub-species). Defaults to the first two letters of the name. |
+| adjective | `string` | No | Species adjective, like 'Dwarven' or 'Feline'. Defaults to `$name`. |
+| genus | `string` | No | Species genus, like 'Dwarf' or 'Cat'. Defaults to `$name`.
+| difficulty | `string` | No | Which column the species is located in on the new game species select screen. Can be `Simple`, `Intermediate`, `Advanced`, or `false` (will not be shown on species select screen). Defaults to `Intermediate`. |
+| difficulty_priority | `int` | No | The priority of the species in the new game species select screen. Higher priorities are displayed first. Defaults to 0. |
+| species_flags | `array of strings` | No | Set a few species flags. Defaults to none. The available flags are: <!-- raw html is the only way to embed multi-line content in a markdown table eek --><ul><li> **hairless** - Species lacks hair (ensures they don't get hair-raising messages).</li><li>**small_torso** - Species' torso is a size smaller than their species size (eg Centaur, Naga). Allows for example large species to use medium armour.</li><li>**elven**, **orcish**, **draconian** - Marks the species as part of a particular racial family. Has various obscure effects.</li></ul> |
+| aptitudes | `map` | **Yes** | The available keys are:<ul><li>**xp**: `(int)` xp aptitude for the species. Required.</li><li>**hp**: `(int)` hp aptitude for the species. Required.</li><li>**mp_mod**: `(int)` starting mp modifier for the species. Required.</li><li>**mr**: `(int)` Magic Resistance gained per XL. Required.</li><li>All other species aptitudes can be specified here. For example: `fighting: 1`. If not specified, the aptitude defaults to 0. For skills the species cannot train, use `false`.</li></ul>
+| can_swim | `boolean` | No | If true, the monster can move in deep water. Defaults to false. |
+| undead | `str` | No | Undead type. Controls many things about how the species works, but most notably base resists and hunger. Defaults to US_ALIVE. The available settings are:<ul><li>**US_ALIVE** - Not undead</li><li>**US_HUNGRY_DEAD** - Ghouls</li><li>**US_UNDEAD** - Mummies</li><li>**US_SEMI_UNDEAD** - Vampires</li></ul> |
+| size | `str` | No | Species size. The available sizes are: tiny, little, small, medium, large, big, giant. Defaults to medium. |
+| str | `int` | **Yes** | Starting strength. Note: starting stats are modified by the background player chooses. |
+| int | `int` | **Yes** | Starting intelligence. |
+| dex | `int` | **Yes** | Starting dexterity. |
+| levelup_stat_frequency | `int` | **Yes** | How frequently (in XLs) the species' periodic stat-up occurs. You can specify `28` to ensure the species never gains stats on levelup. Note that all species gain a user-selectable stat every three levels. |
+| levelup_stats | `array of strings` | No | Which stats the species' periodic stat-up can randomly select from. Defaults to `[str, dex, int]`. |
+| mutations | `map {xl: {mut: amt}}` | No | Innate mutations for the species, and the XL it gains them. The map is keyed by XL, then each XL is another map containing the mutation enum name keying the number of levels to change. Look at Deep Dwarf for a good example. |
+| fake_mutations | `array of maps [{'long': str, 'short': str}, ...]` | No | A list of fake mutations the species starts with. Fake mutations do nothing except show up in the mutations list, and are used to communicate species-specific special behaviour. Each fake mutation has a short and long description, for the `%` and `A` screens respectively. Look at Barachi for a good example. Note that for a particular fake mutation, either the long or short description can be omitted (see White Draconian). |
+| recommended_jobs | `array of strings` | **Sometimes** | Backgrounds the species is recommended for. Backgrounds must match the pattern `JOB_[A-Z_]+`. For species with `difficulty` set to `false`, this parameter is irrelevant and therefore optional; otherwise it is required. |
+| recommended_weapons | `array of strings` | No | Weapons, identified by skill, recommended for the species. Entries must match a known weapon skill (`SK_LONG_BLADES`, etc). Defaults to all weapons except short blades and unarmed combat. |
+| walking_verb | `string` | No | What "verb" should be used to describe the species' movement style? The "verb" will have "er" or "ing" appended when used. Examples: "Slid" (Naga), "Glid" (Tengu). Defaults to "Walk". |
+| altar_action | `string` | No | When praying at an altar, print `You $altar_action the altar of foo.` for the species. Defaults to "kneel at". |
+
+### Advanced Keys
+
+You probably don't need or want to use these.
+
+| key  | type | required | description |
+| ---- | ---- | -------- | ----------- |
+| TAG_MAJOR_VERSION | `int` | No | Wrap the species in `#if TAG_MAJOR_VERSION == [...]` to deprecate it. See existing deprecated species for examples. |
+| create_enum | `boolean` | No | Set `true` if the species already has an enum in species-type.h. Any new species should not set this (and will fail to compile if you try). Defaults to `false`. |
---
+++ crawl-ref/source/util/species-gen.py
@@ -0,0 +1,375 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+import traceback
+import collections
+import re
+
+import yaml  # pip install pyyaml
+
+
+class Species(dict):
+    """Parser for YAML definition files.
+
+    If any YAML content is invalid, the relevant parser function below should
+    raise ValueError.
+    """
+    @staticmethod
+    def from_yaml(s):
+        species = Species()
+
+        # Pre-validation
+        if s.get('TAG_MAJOR_VERSION', None) is not None:
+            if not isinstance(s['TAG_MAJOR_VERSION'], int):
+                raise ValueError('TAG_MAJOR_VERSION must be an integer')
+        if not isinstance(s.get('fake_mutations', []), list):
+            raise ValueError('fake_mutations must be a list')
+        starting_species = s.get('difficulty', 'Intermediate') != False
+        has_recommended_jobs = bool(s.get('recommended_jobs'))
+        if starting_species != has_recommended_jobs:
+            raise ValueError('recommended_jobs must not be empty (or difficulty must be False)')
+
+        # Set attributes
+        species['enum'] = validate_string(s['enum'], 'enum', 'SP_[A-Z_]+$')
+        species['monster_name'] = validate_string(s['monster'], 'monster', 'MONS_[A-Z_]+$')
+        species['name'] = validate_string(s['name'], 'name', '..+')
+        species['short_name'] = s.get('short_name', s['name'][:2])
+        species['adjective'] = quote(s['adjective']) if 'adjective' in s else 'nullptr'
+        species['genus'] = quote(s['genus']) if 'genus' in s else 'nullptr'
+        species['species_flags'] = species_flags(s.get('species_flags', []))
+        species['xp'] = validate_int_range(s['aptitudes']['xp'], 'xp', -10, 10)
+        species['hp'] = validate_int_range(s['aptitudes']['hp'], 'hp', -10, 10)
+        species['mp'] = validate_int_range(s['aptitudes']['mp_mod'], 'mp_mod', -5, 20)
+        species['mr'] = validate_int_range(s['aptitudes']['mr'], 'mr', 0, 20)
+        species['aptitudes'] = aptitudes(s['aptitudes'])
+        species['habitat'] = 'HT_LAND' if not s.get('can_swim') else 'HT_WATER'
+        species['undead'] = undead_type(s.get('undead_type', 'US_ALIVE'))
+        species['size'] = size(s.get('size', 'medium'))
+        species['str'] = validate_int_range(s['str'], 'str', 1, 100)
+        species['int'] = validate_int_range(s['int'], 'int', 1, 100)
+        species['dex'] = validate_int_range(s['dex'], 'dex', 1, 100)
+        species['levelup_stats'] = levelup_stats(s.get('levelup_stats', []))
+        species['levelup_stat_frequency'] = validate_int_range(s['levelup_stat_frequency'], 'levelup_stat_frequency', 0, 28)
+        species['mutations'] = mutations(s.get('mutations', {}))
+        species['fake_mutations_long'] = fake_mutations_long(s.get('fake_mutations', []))
+        species['fake_mutations_short'] = fake_mutations_short(s.get('fake_mutations', []))
+        species['recommended_jobs'] = recommended_jobs(s.get('recommended_jobs', []))
+        species['recommended_weapons'] = recommended_weapons(s.get('recommended_weapons', []))
+        species['difficulty'] = difficulty(s.get('difficulty', 'Intermediate'))
+        species['difficulty_priority'] = validate_int_range(difficulty_priority(s.get('difficulty_priority', 0)), 'difficulty_priority', 0, 1000)
+        species['create_enum'] = validate_bool(s.get('create_enum', True), 'create_enum')
+        species['walking_verb'] = quote(s['walking_verb']) if 'walking_verb' in s else 'nullptr'
+        species['altar_action'] = quote(s['altar_action']) if 'altar_action' in s else 'nullptr'
+        species['tag_major_version_opener'] = '' if 'TAG_MAJOR_VERSION' not in s else "#if TAG_MAJOR_VERSION == %s" % s['TAG_MAJOR_VERSION']
+        species['tag_major_version_closer'] = '' if 'TAG_MAJOR_VERSION' not in s else "#endif"
+        return species
+
+
+SpeciesGroup = collections.namedtuple('SpeciesGroup', ['position', 'width', 'species'])
+SpeciesGroupEntry = collections.namedtuple('SpeciesGroupEntry', ['priority', 'enum'])
+SPECIES_GROUPS_TEMPLATE = {
+    'Simple': SpeciesGroup('coord_def(0, 0)', '20', []),
+    'Intermediate': SpeciesGroup('coord_def(25, 0)', '20', []),
+    'Advanced': SpeciesGroup('coord_def(50, 0)', '20', []),
+}
+SPECIES_GROUP_TEMPLATE = """
+    {{
+        "{name}",
+        {position},
+        {width},
+        {{ {species} }}
+    }},
+"""
+ALL_APTITUDES = ('fighting', 'short_blades', 'long_blades', 'axes',
+    'maces_and_flails', 'polearms', 'staves', 'slings', 'bows', 'crossbows',
+    'throwing', 'armour', 'dodging', 'stealth', 'shields', 'unarmed_combat',
+    'spellcasting', 'conjurations', 'hexes', 'charms', 'summoning',
+    'necromancy', 'transmutations', 'translocations', 'fire_magic',
+    'ice_magic', 'air_magic', 'earth_magic', 'poison_magic', 'invocations',
+    'evocations')
+UNDEAD_TYPES = ('US_ALIVE', 'US_HUNGRY_DEAD', 'US_UNDEAD', 'US_SEMI_UNDEAD')
+SIZES = ('SIZE_TINY', 'SIZE_LITTLE', 'SIZE_SMALL', 'SIZE_MEDIUM', 'SIZE_LARGE',
+    'SIZE_BIG', 'SIZE_GIANT')
+ALL_STATS = ('str', 'int', 'dex')
+ALL_WEAPON_SKILLS = ('SK_SHORT_BLADES', 'SK_LONG_BLADES', 'SK_AXES',
+    'SK_MACES_FLAILS', 'SK_POLEARMS', 'SK_STAVES', 'SK_SLINGS', 'SK_BOWS',
+    'SK_CROSSBOWS', 'SK_UNARMED_COMBAT')
+
+
+def recommended_jobs(jobs):
+    return ', '.join(validate_string(job, 'Job', 'JOB_[A-Z_]+') for job in jobs)
+
+
+def validate_string(val, name, pattern):
+    '''
+    Validate a string.
+
+    Note that re.match anchors to the start of the string, so you don't need to
+    prefix the pattern with '^'. But it doesn't require matching to the end, so
+    you'll probably want to suffix '$'.
+    '''
+    if not isinstance(val, str):
+        raise ValueError('%s isn\'t a string' % name)
+    if re.match(pattern, val):
+        return val
+    else:
+        raise ValueError('%s doesn\'t match pattern %s' % (val, pattern))
+    return val
+
+
+def validate_bool(val, name):
+    '''Validate a boolean.'''
+    if not isinstance(val, bool):
+        raise ValueError('%s isn\'t a boolean' % name)
+    return val
+
+
+def validate_int_range(val, name, min, max):
+    if not isinstance(val, int):
+        raise ValueError('%s isn\'t an integer' % name)
+    if not min <= val <= max:
+        raise ValueError('%s isn\'t between %s and %s' % (name, min, max))
+    return val
+
+
+def size(size):
+    val = "SIZE_%s" % size.upper()
+    if val not in SIZES:
+        raise ValueError('Size %s is invalid, pick one of tiny, little, small, medium, large, big, or giant')
+    return val
+
+
+def enumify(s):
+    return s.replace(' ', '_').upper()
+
+def quote(s):
+    if not isinstance(s, str):
+        raise ValueError('Expected a string but got %s' % repr(s))
+    return '"%s"' % s
+
+
+def species_flags(flags):
+    out = set()
+    for f in flags:
+        if f == 'elven':
+            out.add('SPF_ELVEN')
+        elif f == 'draconian':
+            out.add('SPF_DRACONIAN')
+        elif f == 'orcish':
+            out.add('SPF_ORCISH')
+        elif f == 'hairless':
+            out.add('SPF_NO_HAIR')
+        elif f == 'small_torso':
+            out.add('SPF_SMALL_TORSO')
+        else:
+            raise ValueError("Unknown species flag %s" % f)
+    if not out:
+        out.add('SPF_NONE')
+    return ' | '.join(out)
+
+
+def undead_type(type):
+    if type not in UNDEAD_TYPES:
+        raise ValueError('Unknown undead type %s' % type)
+    return type
+
+
+def levelup_stats(stats):
+    if not stats:
+        stats = ALL_STATS
+    else:
+        for s in stats:
+            if s not in ALL_STATS:
+                raise ValueError('Unknown stat %s' % s)
+    return ', '.join("STAT_%s" % s.upper() for s in stats)
+
+
+def mutations(mut_def):
+    out = []
+    for xl, muts in sorted(mut_def.items()):
+        validate_int_range(xl, 'Mutation Level', 1, 27)
+        if not isinstance(muts, dict):
+            raise ValueError('Mutation key %s doesn\'t seem to have a valid map of {name: amount} entries' % xl)
+        for mut_name, amt in sorted(muts.items()):
+            validate_string(mut_name, 'Mutation Name', 'MUT_[A-Z_]+')
+            validate_int_range(amt, 'Mutation Amount', -3, 3)
+            out.append("{{ {mut_name}, {amt}, {xl} }}".format(
+                mut_name=mut_name,
+                xl=xl,
+                amt=amt,
+            ))
+    return ', '.join(out)
+
+
+def fake_mutations_long(fmut_def):
+    return ', '.join(quote(m.get('long')) for m in fmut_def if m.get('long'))
+
+
+def fake_mutations_short(fmut_def):
+    return ', '.join(quote(m.get('short')) for m in fmut_def if m.get('short'))
+
+
+def recommended_weapons(weapons):
+    if not weapons:
+        weapons = list(ALL_WEAPON_SKILLS)
+        weapons.remove('SK_SHORT_BLADES')
+        weapons.remove('SK_UNARMED_COMBAT')
+    return ', '.join(validate_string(weap, 'Weapon Skill', 'SK_[A-Z_]+') for weap in weapons)
+
+
+def aptitudes(apts):
+    for apt, val in apts.items():
+        if apt not in ALL_APTITUDES and apt not in ('xp', 'hp', 'mp_mod', 'mr'):
+            raise ValueError("Unknown aptitude (typo?): %s" % apt)
+        validate_int_range(val, apt, -10, 10)
+    return apts
+
+
+def difficulty(d):
+    if d not in SPECIES_GROUPS_TEMPLATE.keys() and d is not False:
+        raise ValueError("Unknown difficulty: %s" % d)
+    return d
+
+
+def difficulty_priority(prio):
+    try:
+        return int(prio)
+    except ValueError:
+        raise ValueError('difficulty_priority value "%s" is not an integer' % prio)
+
+
+def generate_aptitudes_data(s, template):
+    """Convert a species in YAML representation to aptitudes.h format.
+
+    If any of the required data can't be loaded, ValueError is raised and
+    passed to the caller.
+    """
+    # Now generate the aptitudes block. The default is 0.
+    # Note: We have to differentiate between 0 and 'False' aptitudes specified
+    # in YAML. The latter is UNUSABLE_SKILL.
+    aptitudes = {apt: 0 for apt in ALL_APTITUDES}
+    for apt, val in s['aptitudes'].items():
+        if apt in ('xp', 'hp', 'mp_mod', 'mr'):
+            continue
+        if val is False:
+            aptitudes[apt] = 'UNUSABLE_SKILL'
+        else:
+            aptitudes[apt] = val
+    return template.format(enum = s['enum'], **aptitudes)
+
+
+def update_species_group(sg, s):
+    difficulty = s['difficulty']
+    if difficulty is False:
+        # Don't add this species to the species select screen
+        return sg
+    entry = SpeciesGroupEntry(s['difficulty_priority'], s['enum'])
+    sg[difficulty].species.append(entry)
+    return sg
+
+
+def generate_species_groups(sg):
+    out = ''
+    for name, group in sg.items():
+        out += SPECIES_GROUP_TEMPLATE.format(
+            name = name,
+            position = group.position,
+            width = group.width,
+            species = ', '.join(e.enum for e in reversed(sorted(group.species))),
+        )
+    return out
+
+
+def generate_species_type_data(s):
+    if s['create_enum'] == False:
+        return ''
+    else:
+        return '    %s,\n' % s['enum']
+
+
+def load_template(templatedir, name):
+    return open(os.path.join(templatedir, name)).read()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate species-data.h')
+    parser.add_argument('datadir', help='dat/species source dir')
+    parser.add_argument('templatedir', help='util/species-gen template source dir')
+    parser.add_argument('species_data', help='species-data.h output file path')
+    parser.add_argument('aptitudes', help='aptitudes.h output file path')
+    parser.add_argument('species_groups', help='species-groups.h output file path')
+    parser.add_argument('species_type', help='species-type.h output file path')
+    args = parser.parse_args()
+
+    # Validate args
+    if not os.path.isdir(args.datadir):
+        print('datadir isn\'t a directory')
+        sys.exit(1)
+    if not os.path.isdir(args.templatedir):
+        print('templatedir isn\'t a directory')
+        sys.exit(1)
+
+    # Load all species
+    all_species = []
+    for f_name in sorted(os.listdir(args.datadir)):
+        if not f_name.endswith('.yaml'):
+            continue
+        f_path = os.path.join(args.datadir, f_name)
+        try:
+            species_spec = yaml.load(open(f_path))
+        except yaml.YAMLError as e:
+            print("Failed to load %s: %s" % (f_name, e))
+            sys.exit(1)
+
+        try:
+            species = Species.from_yaml(species_spec)
+        except (ValueError, KeyError) as e:
+            print("Failed to load %s" % f_name)
+            traceback.print_exc()
+            sys.exit(1)
+        all_species.append(species)
+
+    # Generate code
+    species_data_out_text = load_template(args.templatedir, 'species-data-header.txt')
+    aptitudes_out_text = load_template(args.templatedir, 'aptitudes-header.txt')
+    species_type_out_text = load_template(args.templatedir, 'species-type-header.txt')
+
+    species_data_template = load_template(args.templatedir, 'species-data-species.txt')
+    aptitude_template = load_template(args.templatedir, 'aptitude-species.txt')
+    species_groups = SPECIES_GROUPS_TEMPLATE
+    for species in all_species:
+        # species-data.h
+        species_data_out_text += species_data_template.format(**species)
+        # aptitudes.h
+        aptitudes_out_text += generate_aptitudes_data(species, aptitude_template)
+        # species-type.h
+        species_type_out_text += generate_species_type_data(species)
+        # species-groups.h
+        species_groups = update_species_group(species_groups, species)
+
+    species_data_out_text += load_template(args.templatedir, 'species-data-deprecated-species.txt')
+    species_data_out_text += load_template(args.templatedir, 'species-data-footer.txt')
+    with open(args.species_data, 'w') as f:
+        f.write(species_data_out_text)
+
+    aptitudes_out_text += load_template(args.templatedir, 'aptitudes-deprecated-species.txt')
+    aptitudes_out_text += load_template(args.templatedir, 'aptitudes-footer.txt')
+    with open(args.aptitudes, 'w') as f:
+        f.write(aptitudes_out_text)
+
+    species_type_out_text += load_template(args.templatedir, 'species-type-footer.txt')
+    with open(args.species_type, 'w') as f:
+        f.write(species_type_out_text)
+
+    species_groups_out_text = ''
+    species_groups_out_text += load_template(args.templatedir, 'species-groups-header.txt')
+    species_groups_out_text += generate_species_groups(species_groups)
+    species_groups_out_text += load_template(args.templatedir, 'species-groups-footer.txt')
+    with open(args.species_groups, 'w') as f:
+        f.write(species_groups_out_text)
+
+
+if __name__ == '__main__':
+    main()
