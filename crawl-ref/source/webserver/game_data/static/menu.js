b06eeeebb3ea0a07f03a6a9817c677e920581fc8
advil <rawlins@gmail.com>
2018-04-22 21:43:38 -0400

    Ensure that close_all_menus syncs with webtiles clients
    This is one possible explanation for 11387, that somehow the javascript
    menu_stack gets out of sync with m_menu_stack on the client side. I
    haven't managed to reproduce this bug at all, but the two stacks getting
    out of sync via an extra open_menu immediately following init_menus
    would lead to the observed behavior, and explain why further menus act
    as submenus. This change ensures that the javascript stack is entirely
    empty when client-side close_all_menus is called, which is triggered on
    redraw. If this fix doesn't work, back to the drawing board.

--- crawl-ref/source/webserver/game_data/static/menu.js
+++ crawl-ref/source/webserver/game_data/static/menu.js
@@ -500,6 +500,12 @@ function ($, comm, client, enums, cr, util, options) {
         }
     }
 
+    function close_all_menus()
+    {
+        while (menu_stack.length > 0)
+            close_menu();
+    }
+
     function update_menu(data)
     {
         $.extend(menu, data);
@@ -558,6 +564,7 @@ function ($, comm, client, enums, cr, util, options) {
     comm.register_handlers({
         "menu": open_menu,
         "close_menu": close_menu,
+        "close_all_menus": close_all_menus,
         "update_menu": update_menu,
         "update_menu_items": update_menu_items,
         "menu_scroll": server_menu_scroll,
