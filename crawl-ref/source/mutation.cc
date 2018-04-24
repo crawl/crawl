6316c0843ef16b80b94c18284eddca0f91c4b014
gammafunk <gammafunk@gmail.com>
2018-04-24 18:04:02 -0500

    Allow the ignite blood demonspawn mutation under Dith (gameguard)
    Dith no longer hates fire, so this mutation should be allowed to work.
    Fixes bug 11455.

--- crawl-ref/source/mutation.cc
+++ crawl-ref/source/mutation.cc
@@ -373,9 +373,6 @@ mutation_activity_type mutation_activity_level(mutation_type mut)
         return mutation_activity_type::INACTIVE;
     }
 
-    if (you_worship(GOD_DITHMENOS) && mut == MUT_IGNITE_BLOOD)
-        return mutation_activity_type::INACTIVE;
-
     if ((you_worship(GOD_PAKELLAS) || player_under_penance(GOD_PAKELLAS))
          && (mut == MUT_MANA_LINK || mut == MUT_MANA_REGENERATION))
     {
