88c5a898d957c883c71655d19988cb5bf33d1464
advil <rawlins@gmail.com>
2018-05-02 22:19:27 -0400

    Add the ultimate enemy
    I got tired of wizmode test monsters inconveniently moving around and
    dying. (How did we not have one of these before?)

--- crawl-ref/source/monster-type.h
+++ crawl-ref/source/monster-type.h
@@ -862,6 +862,9 @@ enum monster_type                      // menv[].type
     MONS_ANCESTOR_HEXER,
 #endif
     MONS_TEST_SPAWNER,
+#if TAG_MAJOR_VERSION > 34
+    MONS_TEST_STATUE,
+#endif
 
     // Add new monsters here:
 #if TAG_MAJOR_VERSION == 34
@@ -1041,6 +1044,7 @@ enum monster_type                      // menv[].type
     MONS_FROG,
     MONS_BARACHI,
     MONS_BULTUNGIN,             // remove this someday
+    MONS_TEST_STATUE,
 #endif
 
     NUM_MONSTERS,               // used for polymorph
