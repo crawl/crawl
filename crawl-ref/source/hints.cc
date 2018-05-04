5e3be4255f43373f138d4069dcc8ca9753f41058
dplusplus <sulpsulpd@gmail.com>
2018-05-04 11:52:24 +0900

    fix typo

--- crawl-ref/source/hints.cc
+++ crawl-ref/source/hints.cc
@@ -1206,7 +1206,7 @@ void learned_something_new(hints_event_type seen_what, coord_def gc)
                 "<console>('<w>"
              << stringize_glyph(get_item_symbol(SHOW_ITEM_MISSILE))
              << "</w>') </console>"
-                "you've picked up. Missiles like tomahwaks and throwing nets "
+                "you've picked up. Missiles like tomahawks and throwing nets "
                 "can be thrown by hand, but other missiles like arrows and "
                 "needles require a launcher and training in using it to be "
                 "really effective. "
