cf060065ddc0166eec4390ca52d84f370e9555ad
Vajrapani <35668582+Vajrapani@users.noreply.github.com>
2018-05-08 16:00:39 +0100

    Part 4/4 of Deleting Slime Knight
    See desc. of Part 1. Completely finished the revert.

--- crawl-ref/source/ng-setup.cc
+++ crawl-ref/source/ng-setup.cc
@@ -275,18 +275,7 @@ static void _give_items_skills(const newgame_def& ng)
         else
             you.skills[SK_ARMOUR]++;
 
-        break;
-
- case JOB_SLIME_KNIGHT:
-        you.religion = GOD_JIYVA;
-        you.piety = 38;
-
-        if (species_apt(SK_ARMOUR) < species_apt(SK_DODGING))
-            you.skills[SK_DODGING]++;
-        else
-            you.skills[SK_ARMOUR]++;
-
-        break;            
+        break;          
 
     case JOB_WANDERER:
         create_wanderer();
