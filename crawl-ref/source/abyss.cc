a008dc0788dbece1c552aecff479fbc950756763
Edgar A. Bering IV <trizor@gmail.com>
2018-05-10 10:40:28 -0400

    Code cleanup
    Remove an unnecessary call to push_features_to_abyss from banished()

--- crawl-ref/source/abyss.cc
+++ crawl-ref/source/abyss.cc
@@ -395,7 +395,6 @@ static int _banished_depth(const int power)
 void banished(const string &who, const int power)
 {
     ASSERT(!crawl_state.game_is_arena());
-    push_features_to_abyss();
     if (brdepth[BRANCH_ABYSS] == -1)
         return;
 
