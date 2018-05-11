beac91de7af69ec8606f60f32a0e25869cc917ba
advil <rawlins@gmail.com>
2018-05-11 08:40:16 -0400

    Add gnolls to mutation test (Sergeek)

--- crawl-ref/source/test/mut_species.lua
+++ crawl-ref/source/test/mut_species.lua
@@ -110,7 +110,7 @@ end
 species = {"hill orc", "minotaur", "merfolk", "gargoyle", "draconian", "halfling", "troll", "ghoul",
             "human", "kobold", "centaur", "spriggan", "tengu", "deep elf", "ogre", "deep dwarf",
             "vine stalker", "vampire", "demigod", "formicid", "naga", "octopode", "felid", "barachi",
-            "mummy"}
+            "mummy", "gnoll"}
 
 test_random_mutations_species("demonspawn", ds_tries, ds_mut_iterations, chance_temporary, chance_clear)
 random_level_change("demonspawn", ds_tries, ds_mut_iterations, chance_temporary, chance_clear)
