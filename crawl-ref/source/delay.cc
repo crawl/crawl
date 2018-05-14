6cd201e5b5de85948f582f0c5dcace93e66f8d5a
advil <rawlins@gmail.com>
2018-05-14 18:10:29 -0400

    Handle just seen monsters better, especially fast ones (791)
    This improves the handling of several cases of fast monsters that are
    newly seen. First, it was possible for them to move multiple times in
    world_reacts with the following buggy pattern: they move into the
    players los, get announced, shout, and leave los -- this last step is
    supposed to be announced, but isn't unless it happens on the first move.
    Second, it was possible for fast monsters to move into and out of los
    without being announced at all, as long as they were wandering, and now
    they will be announced in such cases.
    
    However, there is an odd consequence of this, which comes from the fact
    that observing a wandering monster for the first time temporarily
    changes its behavior.  As long as the observation happens in the middle
    of world_reacts (not on the player's movement), and the monster has more
    energy, the monster subconsciously tries to stay in the player's los for
    at least one turn, so the player can get a real glimpse. Consequently,
    in cases where previously a fast monster would flicker in and out of the
    player's los with no one being the wiser, the monster will now be marked
    as SC_JUST_SEEN and most likely stay there for a turn. This probably
    adds an extra stealth check to such cases. I'm not sure how often this
    happens in practice, I had to jigger the wander behavior to get it to
    easily replicate.
    
    I thought that when I fixed the core bug, I could simplify things, but
    this was wrong -- I don't dare remove any code to do with previously
    unseen monsters.  This whole system is completely insane.

--- crawl-ref/source/delay.cc
+++ crawl-ref/source/delay.cc
@@ -42,6 +42,7 @@
 #include "libutil.h"
 #include "macro.h"
 #include "message.h"
+#include "mon-act.h"
 #include "mon-behv.h"
 #include "mon-tentacle.h"
 #include "mon-util.h"
@@ -1238,7 +1239,7 @@ static inline bool _monster_warning(activity_interrupt_type ai,
         {
             yell(mon);
         }
-        mon->seen_context = SC_JUST_SEEN;
+        mons_set_just_seen(mon);
     }
 
     if (crawl_state.game_is_hints())
