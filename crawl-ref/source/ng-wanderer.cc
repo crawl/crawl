5a2bd3f43c9fa496afced1ffd6f79db552e366e3
pubby <pubby8@gmail.com>
2018-06-01 18:22:02 -0500

    Improve wanderer assert (NormalPerson7)

--- crawl-ref/source/ng-wanderer.cc
+++ crawl-ref/source/ng-wanderer.cc
@@ -166,12 +166,12 @@ static skill_type _wanderer_role_skill_select(stat_type selected_role,
 
     if (selected_skill == NUM_SKILLS)
     {
+        ASSERT(you.species == SP_FELID || you.species == SP_OGRE);
+
         if (you.species == SP_FELID)
             selected_skill = SK_UNARMED_COMBAT;
-        else if(you.species == SP_OGRE)
+        else // Ogres
             selected_skill = SK_SPELLCASTING;
-        else
-            ASSERT(false);
     }
 
     return selected_skill;
