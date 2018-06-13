649425ede9a9ee2ea6eb14099a639e69cc05ce35
advil <rawlins@gmail.com>
2018-06-13 08:35:55 -0600

    Let `user_settings` be determined automatically
    So that a manual config.py change isn't required. This will by default
    use the same location as password_db.

--- crawl-ref/source/webserver/userdb.py
+++ crawl-ref/source/webserver/userdb.py
@@ -5,8 +5,18 @@ import os.path
 import logging
 import random
 
-from config import (max_passwd_length, nick_regex, password_db, settings_db,
+from config import (max_passwd_length, nick_regex, password_db,
                     crypt_algorithm, crypt_salt_length)
+
+def setup_settings_path():
+    global settings_db
+    try:
+        from config import settings_db as sdb_config
+        settings_db = sdb_config
+    except:
+        settings_db = os.path.join(os.path.dirname(password_db), "user_settings.db3")
+
+setup_settings_path()
 
 def ensure_settings_db_exists():
     if os.path.exists(settings_db):
