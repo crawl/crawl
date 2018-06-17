52c2948ca5864924dd8dc36ec3d181c5002fa9b5
Muffindrake <muffindrake@protonmail.ch>
2018-06-17 23:26:38 +0200

    Add and tweak some ghost vaults
    This is a squashed version of thirty commits from crawl:crawl/master
    which add many new simple and complex ghost vaults as well as tweak them
    slightly:
    
     6ea8e7e Add four ghost vaults for mainly Depths
     95e1b32 Adjust the vault muffindrake_ghost_dual_entry
     2547c9b Adjust the vault muffindrake_ghost_three_little_ghosts
     0da5cb0 Adjust the vault muffindrake_ghost_dead_mans_drink
     01493d7 A demon polymorph themed Depths ghost vault from Biasface
     52696f8 Two item themed ghost vaults
     c87f014 Updates to ebering_ghost_inner_flame
     f69a080 Updates to ebering_funeral_grove
     cd2a3d9 Bug fixes and code generalizations for gammafunk_ghost_hdyra_chop
     4d20840 An orcish armoury ghost vault for Dungeon and Orc (Biasface)
     bbc7872 Remove flame clouds from inner flame vault (jboons)
     76cc104 And icy themed ghost vault for Dungeon, Lair, and Depths (Biasface)
     d3eb0d8 Abstract loot scaling for single ghosts
     617fbbe Haunted rock garden ghost vault
     6fc7002 Reflecting pool ghost vault
     9374e73 Xom themed ghost vault
     577b210 Two sewer themed ghost vaults
     30f0393 Move a function to the ghost vault lua file and refactor
     7bfabb5 Updates for ebering_ghost_xom
     b51906a Updates to two ghost sewer vaults
     3b2bdd4 Ghost vault DES cleanups
     28ad1da Add a missing ghost setup function to a vault (ebering)
     9358b0b Add a missing ghost setup function to another vault (ebering)
     6e350e3 Some small ghost vault fixes
     921cf92 Consistently use depth breakpoints for ghost vault loot
     06062c5 Remove upweighting for gammafunk_ghost_grave
     d813392 Add good consumables or equipment to the loot in many ghost vaults
     1b94d8d A labyrinth themed ghost vault for Dungeon, Lair, and Orc from Maelrawn
     3e71e8a Updates to gammafunk_ghost_split
     fc6459a Updates to gammafunk_ghost_spectres.

--- crawl-ref/source/dat/dlua/ghost.lua
+++ crawl-ref/source/dat/dlua/ghost.lua
@@ -32,3 +32,166 @@ function vaults_ghost_setup(e)
     -- Vaults branch layout expects vaults_ghost as a tag selector.
     e.tags("vaults_ghost")
 end
+
+-- Make an item definition that will randomly choose from combinations of the
+-- given tables of weighted item types and optional egos.
+--
+-- @param items     A table with weapon names as keys and weights as values.
+-- @param egos      An optional table with ego names as keys and weights as
+--                  values.
+-- @param args      An optional string of arguments to use on every item entry.
+--                  Should not have leading or trailing whitespace.
+-- @param separator An optional separator to use between the item entries.
+--                  Defaults to '/', which is appropriate for ITEM statements.
+--                  Use '|' if making item statements for use with MONS.
+-- @returns A string containing the item definition.
+function random_item_def(items, egos, args, separator)
+    args = args ~= nil and " " .. args or ""
+    separator = separator ~= nil and separator or '/'
+    local item_def
+
+    for iname, iweight in pairs(items) do
+        -- If we have egos, define an item spec with all item+ego
+        -- combinations, each with weight scaled by item rarity and ego
+        -- rarity.
+        if egos ~= nil then
+            for ename, eweight in pairs(egos) do
+                if (not iname:find("demon") or ename ~= "holy_wrath")
+                   and (not make_arte or ename ~= "none") then
+                    def = iname .. args .. " ego:" .. ename .. " w:" ..
+                          math.floor(iweight * eweight)
+                    if item_def == nil then
+                         item_def = def
+                    else
+                         item_def = item_def .. " " .. separator .. " " .. def
+                    end
+                end
+            end
+        -- No egos, so define item spec with all item combinations, each with
+        -- weight scaled by item rarity.
+        else
+            def = iname .. args .. " w:" .. iweight
+            if item_def == nil then
+                 item_def = def
+            else
+                 item_def = item_def .. " " .. separator .. " " .. def
+            end
+        end
+    end
+    return item_def
+end
+
+function ghost_good_loot(e)
+    -- Possible loot items.
+    jewellery = "any jewellery"
+    good_jewellery = "any jewellery good_item"
+    randart_jewellery = "any jewellery randart"
+    aux = "cloak / scarf / helmet / hat / gloves / pair of boots"
+    good_aux = "cloak good_item / scarf good_item / helmet good_item / " ..
+        "hat good_item / gloves good_item / pair of boots good_item"
+    randart_aux = good_aux:gsub("good_item", "randart")
+    scrolls = "scroll of teleportation w:15 / scroll of fog w:15 / " ..
+        "scroll of fear w:15 / scroll of blinking w:10 / " ..
+        "scroll of enchant weapon w:5 / scroll of enchant armour w:5 / " ..
+        "scroll of brand weapon w:3 / scroll of magic mapping w:10 / " ..
+        "scroll of acquirement w:1"
+    potions = "potion of haste w:15 / potion of might w:10 / " ..
+        "potion of invisibility w:10 / potion of agility w:10 / " ..
+        "potion of brilliance w:5 / potion of magic w:10 / " ..
+        "potion of heal wounds w:15 / potion of mutation w:10 / " ..
+        "potion of cancelation w:10 / potion of resistance w:5 / " ..
+        "potion of experience w:1"
+
+    first_item = true
+    second_item = false
+    if you.in_branch("D") then
+        if you.depth() < 9 then
+            first_item = false
+        elseif you.depth() < 12 then
+            if crawl.coinflip() then
+                first_item = false
+            end
+        elseif you.depth() < 14 then
+            if crawl.coinflip() then
+                aux = good_aux
+                jewellery = good_jewellery
+            end
+            if crawl.one_chance_in(3) then
+                second_item = true
+            end
+        else
+            aux = good_aux
+            jewellery = good_jewellery
+            if crawl.one_chance_in(4) then
+               aux = randart_aux
+               jewellery = randart_jewellery
+            end
+            if crawl.coinflip() then
+                second_item = true
+            end
+         end
+    elseif you.in_branch("Lair") then
+        if crawl.one_chance_in(3) then
+            aux = good_aux
+            jewellery = good_jewellery
+        end
+        if crawl.one_chance_in(4) then
+            second_item = true
+        end
+    elseif you.in_branch("Orc") then
+        if crawl.coinflip() then
+            aux = good_aux
+            jewellery = good_jewellery
+        end
+        if crawl.one_chance_in(3) then
+            second_item = true
+        end
+    elseif you.in_branch("Shoals")
+      or you.in_branch("Snake")
+      or you.in_branch("Spider")
+      or you.in_branch("Swamp") then
+        aux = good_aux
+        jewellery = good_jewellery
+        if crawl.one_chance_in(3) then
+           aux = randart_aux
+           jewellery = randart_jewellery
+        end
+        second_item = true
+    elseif you.in_branch("Vaults") or you.in_branch("Elf") then
+        aux = good_aux
+        jewellery = good_jewellery
+        if crawl.coinflip() then
+           aux = randart_aux
+           jewellery = randart_jewellery
+        end
+        second_item = true
+    else
+        aux = randart_aux
+        jewellery = randart_jewellery
+        second_item = true
+    end
+
+    -- Define loot tables of potential item defs.
+    first_loot = { {name = "scrolls", def = scrolls, weight = 20},
+                   {name = "potions", def = potions, weight = 20},
+                   {name = "aux", def = aux, weight = 10},
+                   {name = "jewellery", def = jewellery, weight = 10},
+                   {name = "manual", def = "any manual", weight = 5} }
+    second_loot = { {name = "scrolls", def = scrolls, weight = 10},
+                    {name = "potions", def = potions, weight = 10} }
+
+    -- If we're upgrading the first item , choose a class, define the item
+    -- slot, otherwise the slot becomes the usual '|*' definition.
+    if first_item then
+        chosen = util.random_weighted_from("weight", first_loot)
+        e.item(chosen["def"])
+    else
+        e.item("superb_item / star_item")
+    end
+    if second_item then
+        chosen = util.random_weighted_from("weight", second_loot)
+        e.item(chosen["def"])
+    else
+        e.item("superb_item / star_item")
+    end
+end
