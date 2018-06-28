5980263529ddb096dfdc44c0ab16f68ae213bd07
gammafunk <gammafunk@gmail.com>
2018-06-28 15:44:43 -0500

    More ghost vaults
     b275bf3 Some ghost vault fixes and refactoring.:
    
    Make the good loot potion/scroll item lists available as variables for
    other vaults to use when they're handling their own loot placement. Fix
    a potion specification error ('cancellation', not 'cancelation') and
    clean up DES spacing.
    
     ff9aefc A potion ghost vault for Dungeon, Elf, and Depths (Ploomutoo):
    
    In ploomutoo_gammafunk_potion_laboratory, an alchemist with a collection
    of nice potions conducted a fatal experiment resulting in an explosion
    of mutagenic horrors. Now their ghost haunts a two-chambered lab,
    guarding loot that contains a number of potions of mutation, high
    quality random potions, and a piece of aux armour or a robe. Monsters in
    the first chamber are melee-oriented: ugly things, very ugly things,
    large abominations, and tentacled monstrosities are all used depending
    on the depth and branch, supplemented with the occasional glowing orange
    brain. In the second chamber is the ghost along with shape-shifters and
    malmutators (neqoxecs and later cacodemons).
    
    This vault was originally submitted by ploomutoo with the main layout
    and a basic set of monsters and loot for dungeon. I modified the monster
    sets to make the themes of the chambers distinct and to allow placement
    in other branches. I also scaled the number and composition of loot and
    monsters as depth increases. Finally, the layout was updated to better
    allow placement of mutagenic fog machines.
    
     270d1ae A happy afterlife ghost vault for Dungeon and Lair from hellmonk:
    
    In hellmonk_ghost_happy_afterlife, a ghost decides to not dwell on the
    failures they made in life, preferring to just relax next to a nice tree
    with a nice meal (a ration) and some nice drinks (1-2 potions). Of
    course, if you disturb its meal it will not hesitate to kill you, since
    ghosts like killing players more than they like meals they can no longer
    eat. The potions upgrade in quality and number as depth increases.

--- crawl-ref/source/dat/dlua/ghost.lua
+++ crawl-ref/source/dat/dlua/ghost.lua
@@ -81,6 +81,19 @@ function random_item_def(items, egos, args, separator)
     return item_def
 end
 
+-- Some scroll and potions with weights that are used as nice loot.
+ghost_loot_scrolls = "scroll of teleportation w:15 / scroll of fog w:15 / " ..
+    "scroll of fear w:15 / scroll of blinking w:10 / " ..
+    "scroll of enchant weapon w:5 / scroll of enchant armour w:5 / " ..
+    "scroll of brand weapon w:3 / scroll of magic mapping w:10 / " ..
+    "scroll of acquirement w:1"
+ghost_loot_potions = "potion of haste w:15 / potion of might w:10 / " ..
+    "potion of invisibility w:10 / potion of agility w:10 / " ..
+    "potion of brilliance w:5 / potion of magic w:10 / " ..
+    "potion of heal wounds w:15 / potion of mutation w:10 / " ..
+    "potion of cancellation w:10 / potion of resistance w:5 / " ..
+    "potion of experience w:1"
+
 function ghost_good_loot(e)
     -- Possible loot items.
     jewellery = "any jewellery"
@@ -90,17 +103,6 @@ function ghost_good_loot(e)
     good_aux = "cloak good_item / scarf good_item / helmet good_item / " ..
         "hat good_item / gloves good_item / pair of boots good_item"
     randart_aux = good_aux:gsub("good_item", "randart")
-    scrolls = "scroll of teleportation w:15 / scroll of fog w:15 / " ..
-        "scroll of fear w:15 / scroll of blinking w:10 / " ..
-        "scroll of enchant weapon w:5 / scroll of enchant armour w:5 / " ..
-        "scroll of brand weapon w:3 / scroll of magic mapping w:10 / " ..
-        "scroll of acquirement w:1"
-    potions = "potion of haste w:15 / potion of might w:10 / " ..
-        "potion of invisibility w:10 / potion of agility w:10 / " ..
-        "potion of brilliance w:5 / potion of magic w:10 / " ..
-        "potion of heal wounds w:15 / potion of mutation w:10 / " ..
-        "potion of cancelation w:10 / potion of resistance w:5 / " ..
-        "potion of experience w:1"
 
     first_item = true
     second_item = false
@@ -172,13 +174,13 @@ function ghost_good_loot(e)
     end
 
     -- Define loot tables of potential item defs.
-    first_loot = { {name = "scrolls", def = scrolls, weight = 20},
-                   {name = "potions", def = potions, weight = 20},
+    first_loot = { {name = "scrolls", def = ghost_loot_scrolls, weight = 20},
+                   {name = "potions", def = ghost_loot_potions, weight = 20},
                    {name = "aux", def = aux, weight = 10},
                    {name = "jewellery", def = jewellery, weight = 10},
                    {name = "manual", def = "any manual", weight = 5} }
-    second_loot = { {name = "scrolls", def = scrolls, weight = 10},
-                    {name = "potions", def = potions, weight = 10} }
+    second_loot = { {name = "scrolls", def = ghost_loot_scrolls, weight = 10},
+                    {name = "potions", def = ghost_loot_potions, weight = 10} }
 
     -- If we're upgrading the first item , choose a class, define the item
     -- slot, otherwise the slot becomes the usual '|*' definition.
