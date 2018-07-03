8507be72827d267845873bfb4638c6bfa17ec18f
gammafunk <gammafunk@gmail.com>
2018-07-02 21:31:05 -0500

    Remove unused monster casting for chain of chaos
    This spell used to be castable on a demonspawn monster, but that monster
    no longer exists. Chain of chaos isn't very good as a monster spell
    since it applies multiple chaos effects to the player per turn and at
    range. Chaos breath is ranged but only applies on effect per turn due to
    its use of clouds, so this is at least a better style of spell or breath
    to give monsters.
    
    This commit removes code used to let monsters cast chain of chaos
    directly. Monsters can still use the chain of chaos effect from wielding
    the mace of variability, since that triggers from a melee effect and
    doesn't need the usual monster spell setup functions.

--- crawl-ref/source/mon-cast.cc
+++ crawl-ref/source/mon-cast.cc
@@ -1851,7 +1851,6 @@ bool setup_mons_cast(const monster* mons, bolt &pbolt, spell_type spell_cast,
 #if TAG_MAJOR_VERSION == 34
     case SPELL_EPHEMERAL_INFUSION:
 #endif
-    case SPELL_CHAIN_OF_CHAOS:
     case SPELL_BLACK_MARK:
 #if TAG_MAJOR_VERSION == 34
     case SPELL_GRAND_AVATAR:
@@ -3224,11 +3223,6 @@ static bool _elec_vulnerable(actor* victim)
 static bool _mutation_vulnerable(actor* victim)
 {
     return victim->can_mutate();
-}
-
-static bool _dummy_vulnerable(actor* victim)
-{
-    return true;
 }
 
 static void _cast_black_mark(monster* agent)
@@ -6520,7 +6514,6 @@ void mons_cast(monster* mons, bolt pbolt, spell_type spell_cast,
     }
 
     case SPELL_CHAIN_LIGHTNING:
-    case SPELL_CHAIN_OF_CHAOS:
         cast_chain_spell(spell_cast, splpow, mons);
         return;
 
@@ -8187,8 +8180,6 @@ static bool _ms_waste_of_time(monster* mon, mon_spell_slot slot)
     case SPELL_CHAIN_LIGHTNING:
         return !_trace_los(mon, _elec_vulnerable)
                 || you.visible_to(mon) && friendly; // don't zap player
-    case SPELL_CHAIN_OF_CHAOS:
-        return !_trace_los(mon, _dummy_vulnerable);
     case SPELL_CORRUPTING_PULSE:
         return !_trace_los(mon, _mutation_vulnerable)
                || you.visible_to(mon)
