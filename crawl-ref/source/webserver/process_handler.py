crawl/crawl

commit caf04dffec4948451bd15925b20de72a47211919

Show mute status in spectator list

crawl-ref/source/webserver/process_handler.py
@@ -322,6 +322,7 @@ class CrawlProcessHandlerBase(object):
                             "Spectator '%s' has now been muted." % target)
         self.muted |= {target}
         self.save_mutelist(source)
+        self.update_watcher_description()
         return True
 
     def unmute(self, source, target):
@@ -341,6 +342,7 @@ class CrawlProcessHandlerBase(object):
             self.handle_notification(source, "You have cleared your mute list.")
             self.muted = set()
             self.save_mutelist(source)
+            self.update_watcher_description()
             return True
 
         if not target in self.muted:
@@ -351,6 +353,7 @@ class CrawlProcessHandlerBase(object):
         self.handle_notification(source, "You have unmuted '%s'." % target)
         self.muted -= {target}
         self.save_mutelist(source)
+        self.update_watcher_description()
         return True
 
     def show_mute_list(self, source):
@@ -378,11 +381,12 @@ class CrawlProcessHandlerBase(object):
                 class_type = 'player'
             else:
                 class_type = 'watcher'
+            n = watcher
+            if (watcher in self.muted):
+                n += " (muted)"
             if player_url is None:
-                return "<span class='{0}'>{1}</span>".format(class_type,
-                                                             watcher)
-            username = "<a href='{0}' target='_blank' class='{1}'>{2}</a>".format(config.player_url, class_type, watcher)
-            username = username.replace('%s', watcher.lower())
+                return "<span class='{0}'>{1}</span>".format(class_type, n)
+            username = "<a href='{0}' target='_blank' class='{1}'>{2}</a>".format(config.player_url, class_type, n)
             return username
 
         player_name, watchers = self.get_watchers(True)

commit 06ccb6e5e95bfff14217ea3eadc6285b22ae0742

Fix webtiles chat player urls (Cody Cutler)
Broken in caf04dffec4948451bd, because I didn't understand how these
were supposed to work.

crawl-ref/source/webserver/process_handler.py
@@ -373,9 +373,9 @@ class CrawlProcessHandlerBase(object):
 
     def update_watcher_description(self):
         try:
-            player_url = config.player_url
+            player_url_template = config.player_url
         except:
-            player_url = None
+            player_url_template = None
         def wrap_name(watcher, is_player=False):
             if is_player:
                 class_type = 'player'
@@ -384,9 +384,10 @@ class CrawlProcessHandlerBase(object):
             n = watcher
             if (watcher in self.muted):
                 n += " (muted)"
-            if player_url is None:
+            if player_url_template is None:
                 return "<span class='{0}'>{1}</span>".format(class_type, n)
-            username = "<a href='{0}' target='_blank' class='{1}'>{2}</a>".format(config.player_url, class_type, n)
+            player_url = player_url_template.replace("%s", watcher.lower())
+            username = "<a href='{0}' target='_blank' class='{1}'>{2}</a>".format(player_url, class_type, n)
             return username
 
         player_name, watchers = self.get_watchers(True)