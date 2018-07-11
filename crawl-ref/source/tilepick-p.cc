4a2a387fbb5e1ea4a37d6ee85c7023f253170846
Timothy Stiles <Timothy.Stiles@wisetechglobal.com>
2018-07-11 13:48:43 +1000

    Gnoll tiles (Giik)
    Made by Giik ( http://the-giik.tumblr.com/ )
    
    Committer's Note: Closes #811.

--- crawl-ref/source/tilepick-p.cc
+++ crawl-ref/source/tilepick-p.cc
@@ -628,6 +628,8 @@ tileidx_t tilep_species_to_base_tile(int sp, int level)
         return TILEP_BASE_VINE_STALKER;
     case SP_BARACHI:
         return TILEP_BASE_BARACHI;
+	case SP_GNOLL:
+		return TILEP_BASE_GNOLL;
     default:
         return TILEP_BASE_HUMAN;
     }
