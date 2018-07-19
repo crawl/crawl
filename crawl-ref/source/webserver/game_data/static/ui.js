63a12d5a5d0d8f72d36fb31a488aaa4b5f1280ac
Aidan Holm <aidanholm@gmail.com>
2018-07-20 00:00:15 +0800

    Fix webtiles font settings being ignored (Leszczynek, #11538)
    We continue to use the CRT font, even though webtiles barely uses the
    CRT emulation layer any more.

--- crawl-ref/source/webserver/game_data/static/ui.js
+++ crawl-ref/source/webserver/game_data/static/ui.js
@@ -1,5 +1,5 @@
-define(["jquery", "comm", "client"],
-function ($, comm, client) {
+define(["jquery", "comm", "client", "./options"],
+function ($, comm, client, options) {
     "use strict";
 
     function wrap_popup(elem, ephemeral)
@@ -88,6 +88,20 @@ function ($, comm, client) {
         if (new_ev.propagation_stopped)
             ev.stopImmediatePropagation();
     }
+
+    options.add_listener(function ()
+    {
+        var size = options.get("tile_font_crt_size");
+        $("#ui-stack").css("font-size", size === 0 ? "" : (size + "px"));
+
+        var family = options.get("tile_font_crt_family");
+        if (family !== "" && family !== "monospace")
+        {
+            family += ", monospace";
+            $("#ui-stack").css("font-family", family);
+        }
+    });
+
 
     $(document).off("game_init.ui")
         .on("game_init.ui", function () {
