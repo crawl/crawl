1f85d428485ad91c425e50c7023c78320f8776a8
advil <rawlins@gmail.com>
2018-07-30 12:15:47 -0400

    Fix messaging with cloud of scarf and qazlal (11500)
    This incorporates elements of both #799 and #804. Though apparently both
    MarvinPA and amalloy were against it for reasons I don't fully
    understand (see note in #804), I think it is preferable to show a
    message when you wear this scarf when it is completely useless. It's not
    exactly a lot of message spam, since this is an uncommon item, and this
    kind of information is useful to new players.
    
    Resolves #799, resolves #804
    
    (cherry picked from commit 33a98d2e1466f8a59b2e55a690287a62298a17b7)

--- crawl-ref/source/player-equip.cc
+++ crawl-ref/source/player-equip.cc
@@ -902,7 +902,12 @@ static void _equip_armour_effect(item_def& arm, bool unmeld,
             break;
 
         case SPARM_CLOUD_IMMUNE:
-            mpr("You feel immune to the effects of clouds.");
+            // player::cloud_immunity checks the scarf + passives, so can't
+            // call it here.
+            if (have_passive(passive_t::cloud_immunity))
+                mpr("Your immunity to the effects of clouds is unaffected.");
+            else
+                mpr("You feel immune to the effects of clouds.");
             break;
         }
     }
@@ -1068,7 +1073,8 @@ static void _unequip_armour_effect(item_def& item, bool meld,
         break;
 
     case SPARM_CLOUD_IMMUNE:
-        mpr("You feel vulnerable to the effects of clouds.");
+        if (!you.cloud_immune())
+            mpr("You feel vulnerable to the effects of clouds.");
         break;
 
     default:
