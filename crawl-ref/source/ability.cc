148a54496777b7e62c41afa634681763656c07c2
Aidan Holm <aidanholm@gmail.com>
2018-08-01 23:22:02 +0800

    Fix webtiles ability menu heading alignment
    (cherry picked from commit b09bef45d9d93b7d7537181aaba2ce3998f1c046)

--- crawl-ref/source/ability.cc
+++ crawl-ref/source/ability.cc
@@ -3125,7 +3125,7 @@ int choose_ability_menu(const vector<talent>& talents)
                                 "Cost                          Failure",
                                 "Ability - describe what?            "
                                 "Cost                          Failure",
-                                MEL_TITLE));
+                                MEL_TITLE), true, true);
 #endif
     abil_menu.set_tag("ability");
     abil_menu.add_toggle_key('!');
