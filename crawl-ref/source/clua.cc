f80aaed8d91f076c0aebd001b0048a5abcf0e5b0
Aidan Holm <aidanholm@gmail.com>
2018-08-23 02:14:21 +0800

    Fix require() only failing the first time (pibborn)
    Filenames were added to a list of already-loaded files before loading
    was actually attempted, which meant that only the first load failure
    returned an error, with subsequent require() calls returning true.
    
    (cherry picked from commit 497f4a5a2c8d6a30b310bb31026fcf27950afa49)

--- crawl-ref/source/clua.cc
+++ crawl-ref/source/clua.cc
@@ -326,13 +326,13 @@ int CLua::execfile(const char *filename, bool trusted, bool die_on_fail,
     if (!force && sourced_files.count(filename))
         return 0;
 
-    sourced_files.insert(filename);
-
     lua_State *ls = state();
     int err = loadfile(ls, filename, trusted || !managed_vm, die_on_fail);
     lua_call_throttle strangler(this);
     if (!err)
         err = lua_pcall(ls, 0, 0, 0);
+    if (!err)
+        sourced_files.insert(filename);
     set_error(err);
     if (die_on_fail && !error.empty())
     {
