71fb17221afc66fb6871ea4f5b51e36082e8af1d
advil <rawlins@gmail.com>
2018-08-30 20:55:36 -0400

    Don't use the full minor tag versioning for bones files
    Only use the subset that is relevant to reading and writing
    ghost_demons. The issue here is that upgrading a bones file on a server
    leads to it not being loadable by older saves, and it gets upgraded any
    time a new minor tag is introduced. A previous commit introduced a
    somewhat complicated backup system to deal with this, but that still had
    some corner cases, and leads to unnecessarily many bones backups on
    servers. The fact is that most save compat doesn't have much to do with
    bones files, and out of the current 199 minor tags, only 4 actually
    impacted bones. So, only use relevant minor tags for bones versions
    (which do need to be manually set). I'll leave the backup thing in
    place, but this change means that it only rarely will come into play.
    
    This doesn't impact ghost_demons in player saves, which inherit the
    versioning from the save.

--- crawl-ref/source/tags.cc
+++ crawl-ref/source/tags.cc
@@ -6867,6 +6867,7 @@ static void unmarshallSpells(reader &th, monster_spells &spells
 
 static void marshallGhost(writer &th, const ghost_demon &ghost)
 {
+    // save compat changes with minor tags here must be added to bones_minor_tags
     marshallString(th, ghost.name);
 
     marshallShort(th, ghost.species);
@@ -6894,6 +6895,7 @@ static void marshallGhost(writer &th, const ghost_demon &ghost)
 
 static ghost_demon unmarshallGhost(reader &th)
 {
+    // save compat changes with minor tags here must be added to bones_minor_tags
     ghost_demon ghost;
 
     ghost.name             = unmarshallString(th);
