7935be99ecd302266154e690b6777a97c4293c6a
gammafunk <gammafunk@gmail.com>
2018-09-05 17:11:57 -0500

    Give Jorgrun the Grasping Roots spell
    This will give Jorgrun a way to get in damage from anywhere and
    hopefully give him more chances to get in range for iron shot.
    
    This commit also re-adds Jorgrun to Swamp. He now has a full-range
    source of damage that doesn't depend on walls, which Swamp lacks.

--- crawl-ref/source/mon-spell.h
+++ crawl-ref/source/mon-spell.h
@@ -1721,6 +1721,7 @@ static const mon_spellbook mspell_list[] =
     {  MST_JORGRUN,
       {
        { SPELL_LRD, 16, MON_SPELL_WIZARD },
+       { SPELL_GRASPING_ROOTS, 16, MON_SPELL_WIZARD },
        { SPELL_IRON_SHOT, 16, MON_SPELL_WIZARD },
        { SPELL_PETRIFY, 16, MON_SPELL_WIZARD },
        { SPELL_DIG, 16, MON_SPELL_WIZARD },
