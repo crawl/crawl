3eb00d1601e79272feafa319bd0ab887b202c924
Aidan Holm <aidanholm@gmail.com>
2018-09-24 01:14:01 +0800

    Fix dry fountain tile not showing in ?/F

--- crawl-ref/source/terrain.cc
+++ crawl-ref/source/terrain.cc
@@ -1658,6 +1658,13 @@ dungeon_feature_type feat_by_desc(string desc)
     if (desc[desc.size() - 1] != '.')
         desc += ".";
 
+#if TAG_MAJOR_VERSION == 34
+    // hard-coded because all the dry fountain variants match this description,
+    // and they have a lower enum value, so the first is incorrectly returned
+    if (desc == "a dry fountain.")
+        return DNGN_DRY_FOUNTAIN;
+#endif
+
     return lookup(feat_desc_cache, desc, DNGN_UNSEEN);
 }
 
