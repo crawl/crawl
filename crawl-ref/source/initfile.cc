b49dddc8044bfa59870360867b6c9f7e73f30f1b
advil <rawlins@gmail.com>
2018-10-15 13:56:37 -0400

    Default `wall_jump_move` to false
    This change has been a long time coming.  In hindsight, the movement
    based implementation of this causes a lot of problems by turning what is
    normally an interface noop (moving into a solid thing) into something
    that can have annoying or bad tactical consequences.
    
    The option is (for now) still there, for those who find the movement
    version fun.

--- crawl-ref/source/initfile.cc
+++ crawl-ref/source/initfile.cc
@@ -211,7 +211,7 @@ const vector<GameOption*> game_options::build_options_list()
         new BoolGameOption(SIMPLE_NAME(rest_wait_both), false),
         new BoolGameOption(SIMPLE_NAME(cloud_status), !is_tiles()),
         new BoolGameOption(SIMPLE_NAME(wall_jump_prompt), false),
-        new BoolGameOption(SIMPLE_NAME(wall_jump_move), true),
+        new BoolGameOption(SIMPLE_NAME(wall_jump_move), false),
         new BoolGameOption(SIMPLE_NAME(darken_beyond_range), true),
         new BoolGameOption(SIMPLE_NAME(dump_book_spells), true),
         new BoolGameOption(SIMPLE_NAME(arena_dump_msgs), false),
