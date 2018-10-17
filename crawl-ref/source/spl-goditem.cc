0f038ff52e9e213b3a4cd5940764ca35cb55a0f1
gammafunk <gammafunk@gmail.com>
2018-10-17 12:39:47 -0500

    Fix a crash when death curses torment (geekosaur)
    The attacker can be nullptr for some sources of torment, so properly
    check for that case before dereferencing the attacker.

--- crawl-ref/source/spl-goditem.cc
+++ crawl-ref/source/spl-goditem.cc
@@ -1178,26 +1178,28 @@ void torment_player(actor *attacker, torment_source_type taux)
 
 void torment_cell(coord_def where, actor *attacker, torment_source_type taux)
 {
-    // Is the player in this cell? The Sceptre of Torment doesn't affect the
-    // wielder.
     if (where == you.pos()
-        && (!attacker->is_player() || taux != TORMENT_SCEPTRE))
+        // The Sceptre of Torment doesn't affect the  wielder.
+        && !(attacker && attacker->is_player() && taux == TORMENT_SCEPTRE))
     {
         torment_player(attacker, taux);
     }
     // Don't return, since you could be standing on a monster.
 
-    // Is a monster in this cell?
     monster* mons = monster_at(where);
     if (!mons
         || !mons->alive()
         || mons->res_torment()
-        || mons == attacker->as_monster() && taux == TORMENT_SCEPTRE)
+        // Monsters can't currently use the sceptre, but just in case.
+        || attacker
+           && mons == attacker->as_monster()
+           && taux == TORMENT_SCEPTRE)
     {
         return;
     }
 
-    int hploss = max(0, mons->hit_points * (50 - mons->res_negative_energy() * 5) / 100 - 1);
+    int hploss = max(0, mons->hit_points *
+                        (50 - mons->res_negative_energy() * 5) / 100 - 1);
 
     if (hploss)
     {
