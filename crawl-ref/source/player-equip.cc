crawl/crawl

commit 82e90586b2dc5d95895fca894a4503104906bce7

Fix transformation HP scaling with HP+ artefacts (11728)
When wearing faerie dragon scales with the HP artefact property and
entering or leaving a transformation that modifies HP, HP would be
scaled incorrectly. This was because:
 - On entering a transformation, equipment was melded after the
   transformation took effect, which meant the new max HP was used as
   the finishing value for the HP change from melding the scales
   (resulting in a much higher than expected final value).
 - On exiting a transformation, equipment was unmelded before the max HP
   was downscaled, which meant the old max HP was used as the starting
   value for the HP change from unmelding the scales (resulting in a
   much lower than expected final HP).

Hence, in transform(), meld equipment before changing the value of
you.form, and in untransform(), downscale HP from the transformation
ending before unmelding equipment.

crawl-ref/source/transform.cc
@@ -1729,12 +1729,12 @@ bool transform(int pow, transformation which_trans, bool involuntary,
     // Give the transformation message.
     mpr(get_form(which_trans)->transform_message(previous_trans));
 
+    _remove_equipment(rem_stuff);
+
     // Update your status.
     you.form = which_trans;
     you.set_duration(DUR_TRANSFORMATION, _transform_duration(which_trans, pow));
     update_player_symbol();
-
-    _remove_equipment(rem_stuff);
 
     you.props[TRANSFORM_POW_KEY] = pow;
 
@@ -1959,6 +1959,24 @@ void untransform(bool skip_move)
     if (dex_mod)
         notify_stat_change(STAT_DEX, -dex_mod, true);
 
+    if (hp_downscale != 10 && you.hp != you.hp_max)
+    {
+        int hp = you.hp * 10 / hp_downscale;
+        if (hp < 1)
+            hp = 1;
+        else if (hp > you.hp_max)
+            hp = you.hp_max;
+        set_hp(hp);
+    }
+    calc_hp();
+
+    if (you.hp <= 0)
+    {
+        ouch(0, KILLED_BY_FRAILTY, MID_NOBODY,
+             make_stringf("losing the %s form",
+                          transform_name(old_form)).c_str());
+    }
+
     _unmeld_equipment(melded);
 
     if (!skip_move)
@@ -2003,24 +2021,6 @@ void untransform(bool skip_move)
         const item_def *armour = you.slot_item(EQ_BODY_ARMOUR, false);
         mprf(MSGCH_DURATION, "%s cracks your icy armour.",
              armour->name(DESC_YOUR).c_str());
-    }
-
-    if (hp_downscale != 10 && you.hp != you.hp_max)
-    {
-        int hp = you.hp * 10 / hp_downscale;
-        if (hp < 1)
-            hp = 1;
-        else if (hp > you.hp_max)
-            hp = you.hp_max;
-        set_hp(hp);
-    }
-    calc_hp();
-
-    if (you.hp <= 0)
-    {
-        ouch(0, KILLED_BY_FRAILTY, MID_NOBODY,
-             make_stringf("losing the %s form",
-                          transform_name(old_form)).c_str());
     }
 
     // Stop being constricted if we are now too large.

commit b49ccaa70fcdb0ec537ccf58463956cf76476d24

Fix transformation HP scaling with HP+ artefacts (11728) (take 2)
The previous attempt at fixing this issue, 82e9058,
caused problems with forcibly unwielding equipment instead of melding
equipment, because _remove_equipment was being called before the
transformation took effect. The second part of this original commit was
fine and is retained in this commit; that is, downscaling HP before
unmelding equipment in untransform.

To fix the double-scaling of HP when transforming, add a new parameter
to the functions recalc_and_scale_hp, calc_hp and get_real_hp, called
ignore_transformations, with a single purpose: when recalc_and_scale_hp
is called from _calc_hp_artefact, don't include any the effects of any
current transformations in the max HP calculated in get_real_hp.

Additionally, the get_real_hp function is confusing, so add a properly
formatted docstring, and rename the parameter trans to temp_effects.
This parameter was incorrectly described as only affecting whether to
include berserk/transformation HP bonuses, when it also includes
equipment bonuses.

crawl-ref/source/player-equip.cc
@@ -45,7 +45,7 @@ static void _mark_unseen_monsters();
  */
 static void _calc_hp_artefact()
 {
-    recalc_and_scale_hp();
+    recalc_and_scale_hp(true);
     if (you.hp_max <= 0) // Borgnjor's abusers...
         ouch(0, KILLED_BY_DRAINING);
 }

crawl-ref/source/player.cc
@@ -2713,13 +2713,13 @@ static void _gain_and_note_hp_mp()
 /**
  * Calculate max HP changes and scale current HP accordingly.
  */
-void recalc_and_scale_hp()
+void recalc_and_scale_hp(bool ignore_transformations)
 {
     // Rounding must be down or Deep Dwarves would abuse certain values.
     // We can reduce errors by a factor of 100 by using partial hp we have.
     int old_max = you.hp_max;
     int hp = you.hp * 100 + you.hit_points_regeneration;
-    calc_hp();
+    calc_hp(ignore_transformations);
     int new_max = you.hp_max;
     hp = hp * new_max / old_max;
     if (hp < 100)
@@ -3632,10 +3632,10 @@ int player::scan_artefacts(artefact_prop_type which_property,
     return retval;
 }
 
-void calc_hp()
+void calc_hp(bool ignore_transformations)
 {
     int oldhp = you.hp, oldmax = you.hp_max;
-    you.hp_max = get_real_hp(true, false);
+    you.hp_max = get_real_hp(true, false, ignore_transformations);
     deflate_hp(you.hp_max, false);
     if (oldhp != you.hp || oldmax != you.hp_max)
         dprf("HP changed: %d/%d -> %d/%d", oldhp, oldmax, you.hp, you.hp_max);
@@ -3922,10 +3922,18 @@ void set_mp(int new_amount)
     you.redraw_magic_points = true;
 }
 
-// If trans is true, being berserk and/or transformed is taken into account
-// here. Else, the base hp is calculated. If rotted is true, calculate the
-// real max hp you'd have if the rotting was cured.
-int get_real_hp(bool trans, bool rotted)
+
+/**
+ * Get the player's max HP
+ * @param temp_effects   Whether to include transformations, berserk and
+ *                       other temporary HP-altering effects
+ * @param rotted         Whether to calculate the base or rotted max HP
+ * @param ignore_forms   Whether to ignore transformations --
+ *                       This parameter is necessary to avoid double-scaling
+ *                       when melding HP artefacts {NormalPerson7}
+ * @return               The player's calculated max HP.
+ */
+int get_real_hp(bool temp_effects, bool rotted, bool ignore_forms)
 {
     int hitp;
 
@@ -3955,18 +3963,19 @@ int get_real_hp(bool trans, bool rotted)
     if (!rotted)
         hitp += you.hp_max_adj_temp;
 
-    if (trans)
+    if (temp_effects)
         hitp += you.scan_artefacts(ARTP_HP);
 
     // Being berserk makes you resistant to damage. I don't know why.
-    if (trans && you.berserk())
+    if (temp_effects && you.berserk())
         hitp = hitp * 3 / 2;
 
-    if (trans) // Some transformations give you extra hp.
+    // Some transformations give you extra hp.
+    if (temp_effects && !ignore_forms)
         hitp = hitp * form_hp_mod() / 10;
 
 #if TAG_MAJOR_VERSION == 34
-    if (trans && player_equip_unrand(UNRAND_ETERNAL_TORMENT))
+    if (temp_effects && player_equip_unrand(UNRAND_ETERNAL_TORMENT))
         hitp = hitp * 4 / 5;
 #endif
 

crawl-ref/source/player.h
@@ -1048,9 +1048,9 @@ bool player_has_feet(bool temp = true, bool include_mutations = true);
 bool enough_hp(int minimum, bool suppress_msg, bool abort_macros = true);
 bool enough_mp(int minimum, bool suppress_msg, bool abort_macros = true);
 
-void calc_hp();
+void calc_hp(bool ignore_transformations = false);
 void calc_mp();
-void recalc_and_scale_hp();
+void recalc_and_scale_hp(bool ignore_transformations = false);
 
 void dec_hp(int hp_loss, bool fatal, const char *aux = nullptr);
 void dec_mp(int mp_loss, bool silent = false);
@@ -1071,7 +1071,8 @@ void dec_max_hp(int hp_loss);
 void deflate_hp(int new_level, bool floor);
 void set_hp(int new_amount);
 
-int get_real_hp(bool trans, bool rotted = false);
+int get_real_hp(bool temp_effects, bool rotted = false,
+                bool ignore_forms = false);
 int get_real_mp(bool include_items);
 
 int get_contamination_level();

crawl-ref/source/transform.cc
@@ -1959,6 +1959,24 @@ void untransform(bool skip_move)
     if (dex_mod)
         notify_stat_change(STAT_DEX, -dex_mod, true);
 
+    if (hp_downscale != 10 && you.hp != you.hp_max)
+    {
+        int hp = you.hp * 10 / hp_downscale;
+        if (hp < 1)
+            hp = 1;
+        else if (hp > you.hp_max)
+            hp = you.hp_max;
+        set_hp(hp);
+    }
+    calc_hp();
+
+    if (you.hp <= 0)
+    {
+        ouch(0, KILLED_BY_FRAILTY, MID_NOBODY,
+             make_stringf("losing the %s form",
+                          transform_name(old_form)).c_str());
+    }
+
     _unmeld_equipment(melded);
 
     if (!skip_move)
@@ -2003,24 +2021,6 @@ void untransform(bool skip_move)
         const item_def *armour = you.slot_item(EQ_BODY_ARMOUR, false);
         mprf(MSGCH_DURATION, "%s cracks your icy armour.",
              armour->name(DESC_YOUR).c_str());
-    }
-
-    if (hp_downscale != 10 && you.hp != you.hp_max)
-    {
-        int hp = you.hp * 10 / hp_downscale;
-        if (hp < 1)
-            hp = 1;
-        else if (hp > you.hp_max)
-            hp = you.hp_max;
-        set_hp(hp);
-    }
-    calc_hp();
-
-    if (you.hp <= 0)
-    {
-        ouch(0, KILLED_BY_FRAILTY, MID_NOBODY,
-             make_stringf("losing the %s form",
-                          transform_name(old_form)).c_str());
     }
 
     // Stop being constricted if we are now too large.