fcd4bc0d70d8a3bff6dbdd38ccd5475a00bbfeb7
root <root@app.webzook.net>
2018-11-05 01:05:53 +0000

    fix i18n-format build bug

--- crawl-ref/source/i18n-format.h
+++ crawl-ref/source/i18n-format.h
@@ -1,9 +1,9 @@
 #pragma once
 
-//#include <stdio.h>
-//#include <stdarg.h>
-//#include <string.h>
-//#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
 
 //typedef int bool;
 #define true 1
