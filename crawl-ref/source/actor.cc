fc52f6bd83ace086316b85cca8477fe20bfee048
gammafunk <gammafunk@gmail.com>
2018-11-24 18:28:42 -0600

    Add more checks for invalid constrictions
    Usually monster death handles the necessary constriction cleanup, but
    there are cases involving e.g. summons where a summoner's death can lead
    the constriction code to attempt to constrict a summoned monster that's
    been reset.
    
    This commit adds aliveness checks to the methods that check for invalid
    constrictors and constrictees to unconstrict and also requires aliveness
    before attempting to apply damage.
    
    (cherry picked from commit 6f6daefbf7dfc89422366fc25d6742fefb1c6ff1)

--- crawl-ref/source/actor.cc
+++ crawl-ref/source/actor.cc
@@ -544,7 +544,9 @@ void actor::stop_constricting_all(bool intentional, bool quiet)
 
 static bool _invalid_constricting_map_entry(const actor *constrictee)
 {
-    return !constrictee || !constrictee->is_constricted();
+    return !constrictee
+        || !constrictee->alive()
+        || !constrictee->is_constricted();
 }
 
 /**
@@ -608,8 +610,8 @@ bool actor::has_invalid_constrictor(bool move) const
     if (!is_constricted())
         return false;
 
-    const actor* const attacker = actor_by_mid(constricted_by);
-    if (!attacker)
+    const actor* const attacker = actor_by_mid(constricted_by, true);
+    if (!attacker || !attacker->alive())
         return true;
 
     // Direct constriction (e.g. by nagas and octopode players or AT_CONSTRICT)
@@ -642,7 +644,7 @@ void actor::clear_invalid_constrictions(bool move)
     vector<mid_t> need_cleared;
     for (const auto &entry : *constricting)
     {
-        const actor * const constrictee = actor_by_mid(entry.first);
+        const actor * const constrictee = actor_by_mid(entry.first, true);
         if (_invalid_constricting_map_entry(constrictee)
             || constrictee->has_invalid_constrictor())
         {
@@ -829,7 +831,7 @@ void actor::handle_constriction()
     {
         actor* const defender = actor_by_mid(i.first);
         const int duration = i.second;
-        if (defender)
+        if (defender && defender->alive())
             constriction_damage_defender(*defender, duration);
     }
 
