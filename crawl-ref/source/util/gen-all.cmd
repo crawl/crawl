e781e4a546b06d4c4961ed8056db85e4970be737
stenella <stenellad@gmail.com>
2018-11-27 10:50:56 -0800

    MSVC support cont.
    Update gen-all.cmd to be up to date with the current makefile,
    namely adding aptitudes and crawl_manual.txt, which stops
    MSVC builds from crashing when they can't find the files.
    
    Add some more MSVC build outputs to gitignore
    Copy CREDITS.txt from the root directory to docs/CREDITS.txt
    to match the makefile/release versions; add it to gitignore.
    
    Change MSVC/stdint.h to match MSVC/include/stdint.h , which
    we changed earlier in the first MSVC compatability commits.

--- crawl-ref/source/util/gen-all.cmd
+++ crawl-ref/source/util/gen-all.cmd
@@ -1,8 +1,31 @@
+:: 1139-1141 in Makefile
+
+:: docs/FAQ.html This file needs 
+:: to be run from inside /util, 
+:: or it can't find FAQ.txt
+perl FAQ2html.pl
+
 cd ..
-perl util/art-data.pl
+:: art-enum.h ,art-data.h
+perl util/art-data.pl 
+:: mon-mst.h
 perl util/gen-mst.pl
+:: cmd-name.h
 perl util/cmd-name.pl
+:: dat/dlua/tags.lua
 perl util/gen-luatags.pl
+:: mi-enum.h
 perl util/gen-mi-enum
+:: docs/aptitudes.txt
+perl util/gen-apt.pl ../docs/aptitudes.txt ../docs/template/apt-tmpl.txt species-data.h aptitudes.h
+:: docs/aptitudes-wide.txt
+perl util/gen-apt.pl ../docs/aptitudes-wide.txt ../docs/template/apt-tmpl-wide.txt species-data.h aptitudes.h
+
+::Change encoding to UTF-8
+::which cuts the size in half
+chcp 65001
+:: crawl_manual.txt
+perl util/unrest.pl ../docs/crawl_manual.rst > ../docs/crawl_manual.txt
+
 cd util
 pause
