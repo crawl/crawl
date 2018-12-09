4fe995b51871d2c606f93ac70b7a2a9326918ded
NormalPerson7 <cmrn.stphnsn@hotmail.com>
2018-12-09 20:51:54 +0000

    Clean up some trap choosing code

--- crawl-ref/source/dungeon.cc
+++ crawl-ref/source/dungeon.cc
@@ -5876,18 +5876,18 @@ static void _place_specific_trap(const coord_def& where, trap_spec* spec,
     }
 
     // find an appropriate trap for TRAP_RANDOM
-    while (spec_type >= NUM_TRAPS
-#if TAG_MAJOR_VERSION == 34
-           || spec_type == TRAP_DART || spec_type == TRAP_GAS
-           || spec_type == TRAP_SHADOW || spec_type == TRAP_SHADOW_DORMANT
-#endif
-           || !is_valid_shaft_level() && spec_type == TRAP_SHAFT)
+    if (spec_type == TRAP_RANDOM)
     {
         do
         {
             spec_type = static_cast<trap_type>(random2(NUM_TRAPS));
         }
-        while (!is_regular_trap(spec_type));
+        while (!is_regular_trap(spec_type)
+#if TAG_MAJOR_VERSION == 34
+               || spec_type == TRAP_DART || spec_type == TRAP_GAS
+               || spec_type == TRAP_SHADOW || spec_type == TRAP_SHADOW_DORMANT
+#endif
+               || !is_valid_shaft_level() && spec_type == TRAP_SHAFT);
     }
 
     trap_def t;
