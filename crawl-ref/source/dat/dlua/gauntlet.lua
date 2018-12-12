53ffe6b153467403b86d69314a41123184c5561e
gammafunk <gammafunk@gmail.com>
2018-12-12 16:24:50 -0600

    Add support for custom Gauntlet arena terrain
    This commit adds support for arena monster sets to define thematically
    appropriate types of liquid and plants. For instance, necromancy-themed
    sets might want withered plants, and demonic-themed ones might want
    demonic plants. Likewise a simulacra set wouldn't want deep water
    replaced with lava and a fire themed set would want lava instead of deep
    water.
    
    These preferences are defined via keys described in the comments of
    tier1_arena_monsters in gauntlet.lua. The monster sets have been updated
    to give these preferences where appropriate.
    
    Also realign the arena variables to consistent spacing.

--- crawl-ref/source/dat/dlua/gauntlet.lua
+++ crawl-ref/source/dat/dlua/gauntlet.lua
@@ -131,14 +131,58 @@ function gauntlet_arena_item_setup(e, other_loot)
             e.item("any jewellery good_item")
         end
         e.nsubst("d = " .. d_first_nsubst .. " / ef / |* / .")
+
+-- Arena subvault terrain setup. Handles randomization of rock, liquids, trap
+-- placement, and plants.
+-- subvault.
+-- @param e              Lua environment.
+-- @param rock_unchanged If true, don't randomize 'x' glyphs to other wall
+--                       types. Defaults to false.
+-- @param liquid         Either nil or one of the strings "lava" or "water". If
+--                       nil, 'l' is unchanged and 'w' has a chance to become
+--                       lava. If "lava", all 'w' glyphs becomes lava. If
+--                       "water", all 'l' glyphs become deep water.
+-- @param plant          Either nil or one of the strings "demonic" or
+--                       "withered". If nil, 'p' becomes a mix of mostly
+--                       plants with some fungus. If "withered" or "demonic",
+--                       'p' becomes withered or demonic plants, respectively.
+function gauntlet_arena_terrain_setup(e, rock_unchanged, liquid, plant)
+    if not rock_unchanged then
+        local glyphs = "xxc"
+        if gauntlet_arena_tier == 2 then
+            glyphs = "cccvvb"
+        end
+        e.subst("x : " .. glyphs)
+    end
+
+    if not liquid then liquid = "default" end
+    if liquid == "lava" then
+        e.subst("w = l")
+    elseif liquid == "water" then
+        e.subst("l = w")
+    elseif liquid == "default" then
+        e.subst("w : wwwl")
+    else
+        error("Unknown liquid type: " .. liquid)
+    end
+
+    if not plant then plant = "default" end
+    if plant == "demonic" then
+        e.kmons("p = demonic plant")
+    elseif plant == "withered" then
+        e.kmons("p = withered plant")
+    elseif plant == "default" then
+        e.kmons("p = plant w:30 / fungus")
+    else
+        error("Unknown plant type: " .. plant)
     end
 end
 
 -- Arena subvault main setup. See the comments in the arena subvault section of
 -- gauntlet.des for details.
 -- @param e              Lua environment.
--- @param rock_unchanged If true, replace all 'x' with a wall type appropriate
---                       for the given tier. Defaults to true.
+-- @param rock_unchanged If true, don't randomize 'x' glyphs to other wall
+--                       types. Defaults to false.
 function gauntlet_arena_setup(e, rock_unchanged)
     gauntlet_arena_transporter_setup(e)
 
@@ -150,152 +194,179 @@ function gauntlet_arena_setup(e, rock_unchanged)
     gauntlet_arena_mons_setup(e, entry["second"], "2")
     gauntlet_arena_mons_setup(e, entry["third"], "3")
 
-    if not rock_unchanged then
-        local glyphs = "xxc"
-        if gauntlet_arena_tier == 2 then
-            glyphs = "cccvvb"
-        end
-        e.subst("x : " .. glyphs)
-    end
+    gauntlet_arena_terrain_setup(e, rock_unchanged, entry["liquid"],
+                                 entry["plant"])
 end
 
 -- Arena monster sets used for the first vault choice. The monster entries are
--- in the keys 'first', 'second', and 'third' in decreasing difficulty with
--- each entry - giving the monster to place on the glyphs '1', '2', and '3',
--- respectively.
+-- in the keys 'first', 'second', and 'third' giving the monsters to place on
+-- the glyphs '1', '2', and '3', respectively. See the comments in arena
+-- subvault section of gauntlet.des for more details.
 --
--- Each entry must contain a 'mons' key with the definition, and 'min' and 'max
--- keys. The number of monsters that place for that glyph will then be random
--- uniform on [min, max].
+-- Each monster entry must contain a "mons" key with the definition, and "min"
+-- and "max" keys. The number of monsters that place for that glyph will then
+-- be random uniform on [min, max].
 --
--- Set the weight for the entry by adding a 'weight' key. The default weight is
--- 10. Set a a key of 'loot' with a valid item def to define a custom item.
--- Exactly one instance of this item will always place as loot.
+-- Other supported keys:
+-- "weight" : Choice weight for the arena set, defaults to 10.
+-- "liquid" : Either "lava" or "water" will force the arena vault to use
+--            only that liquid wherever it would place 'w' or 'l'.
+-- "plant"  : Either "demonic" or "withered" will have the arena use that type
+--            of plant whenever it would place a plant.
+-- "loot"   : Should contain a valid item definition for a custom loot item.
+--            Exactly one of this item will always place in the arena.
 tier1_gauntlet_arenas = {
   {
-    first   = {mons = "spiny frog simulacrum / wyvern simulacrum " ..
+    first  = {mons = "spiny frog simulacrum / wyvern simulacrum " ..
                       "/ hornet simulacrum", min = 1, max = 1},
-    second  = {mons = "simulacrum place:D:12", min = 2, max = 4},
+    second = {mons = "simulacrum place:D:12", min = 2, max = 4},
+    liquid = "water",
+    plant  = "withered",
   },
   {
-    second  = {mons = "raiju", min = 1, max = 1},
-    third   = {mons = "steam dragon", min = 1, max = 2},
+    second = {mons = "raiju", min = 1, max = 1},
+    third  = {mons = "steam dragon", min = 1, max = 2},
+    liquid = "water",
   },
   {
-    second  = {mons = "slime creature", min = 2, max = 4},
-    third   = {mons = "jelly", min = 1, max = 3},
+    second = {mons = "slime creature", min = 2, max = 4},
+    third  = {mons = "jelly", min = 1, max = 3},
+    liquid = "water",
+    plant  = "demonic",
   },
   {
-    first   = {mons = "death ooze", min = 1, max = 1},
+    first  = {mons = "death ooze", min = 1, max = 1},
+    plant  = "withered",
   },
   {
     first  = {mons = "sixfirhy", min = 1, max = 1},
     second = {mons = "ufetubus", min = 2, max = 4},
+    liquid = "water",
+    plant  = "demonic",
   },
   {
-    first   = {mons = "ice devil", min = 1, max = 1},
-    second  = {mons = "white imp", min = 2, max = 4},
+    first  = {mons = "ice devil", min = 1, max = 1},
+    second = {mons = "white imp", min = 2, max = 4},
+    liquid = "water",
+    plant  = "demonic",
   },
   {
-    first   = {mons = "soul eater", min = 1, max = 1},
-    second  = {mons = "shadow imp", min = 2, max = 4},
+    first  = {mons = "soul eater", min = 1, max = 1},
+    second = {mons = "shadow imp", min = 2, max = 4},
+    liquid = "water",
+    plant  = "withered",
   },
   {
-    first   = {mons = "ynoxinul", min = 1, max = 1},
-    second  = {mons = "rust devil", min = 0, max = 1},
-    third   = {mons = "iron imp", min = 2, max = 4},
+    first  = {mons = "ynoxinul", min = 1, max = 1},
+    second = {mons = "rust devil", min = 0, max = 1},
+    third  = {mons = "iron imp", min = 2, max = 4},
+    plant  = "demonic",
   },
   {
-    first   = {mons = "smoke demon", min = 1, max = 1},
-    second  = {mons = "red devil", min = 0, max = 1},
-    third   = {mons = "crimson imp", min = 2, max = 4},
+    first  = {mons = "smoke demon", min = 1, max = 1},
+    second = {mons = "red devil", min = 0, max = 1},
+    third  = {mons = "crimson imp", min = 2, max = 4},
+    liquid = "lava",
+    plant  = "demonic",
   },
   {
-    first   = {mons = "neqoxec", min = 1, max = 1},
-    second  = {mons = "chaos spawn", min = 1, max = 3},
+    first  = {mons = "neqoxec", min = 1, max = 1},
+    second = {mons = "chaos spawn", min = 1, max = 3},
+    plant  = "demonic",
   },
   {
-    second  = {mons = "shadow", min = 2, max = 4},
+    second = {mons = "shadow", min = 2, max = 4},
+    liquid = "water",
+    plant  = "withered",
   },
   {
-    first   = {mons = "catoblepas", min = 1, max = 1},
-    second  = {mons = "gargoyle", min = 1, max = 2},
+    first  = {mons = "catoblepas", min = 1, max = 1},
+    second = {mons = "gargoyle", min = 1, max = 2},
   },
   {
-    first   = {mons = "torpor snail", min = 1, max = 1},
-    second  = {mons = "yak band / wolf band / elephant band", min = 1,
+    first  = {mons = "torpor snail", min = 1, max = 1},
+    second = {mons = "yak band / wolf band / elephant band", min = 1,
                max = 1},
   },
   {
-    first   = {mons = "fire crab", min = 1, max = 1},
-    second  = {mons = "fire bat", min = 2, max = 4},
+    first  = {mons = "fire crab", min = 1, max = 1},
+    second = {mons = "fire bat", min = 2, max = 4},
+    liquid = "lava",
   },
   {
-    second  = {mons = "shapeshifter", min = 1, max = 3},
+    second = {mons = "shapeshifter", min = 1, max = 3},
   },
   {
-    first   = {mons = "flying skull band", min = 1, max = 1},
+    first  = {mons = "flying skull band", min = 1, max = 1},
+    plant  = "withered",
   },
   {
-    first   = {mons = "queen ant", min = 1, max = 1},
-    second  = {mons = "soldier ant", min = 3, max = 6},
+    first  = {mons = "queen ant", min = 1, max = 1},
+    second = {mons = "soldier ant", min = 3, max = 6},
   },
   {
-    second  = {mons = "hornet", min = 2, max = 3},
+    second = {mons = "hornet", min = 2, max = 3},
   },
   {
-    first   = {mons = "queen bee", min = 1, max = 1},
-    second  = {mons = "killer bee band", min = 1, max = 1},
+    first  = {mons = "queen bee", min = 1, max = 1},
+    second = {mons = "killer bee band", min = 1, max = 1},
   },
   {
-    first   = {mons = "moth of wrath", min = 1, max = 1},
-    second  = {mons = "wolf band", min = 1, max = 1},
+    first  = {mons = "moth of wrath", min = 1, max = 1},
+    second = {mons = "wolf band", min = 1, max = 1},
   },
   {
-    first   = {mons = "worldbinder", min = 1, max = 1},
-    second  = {mons = "boggart", min = 1, max = 3},
+    first  = {mons = "worldbinder", min = 1, max = 1},
+    second = {mons = "boggart", min = 1, max = 3},
+    plant  = "demonic",
   },
   {
-    first   = {mons = "death scarab", min = 1, max = 1},
-    second  = {mons = "spectre place:Lair:6", min = 1, max = 1},
+    first  = {mons = "death scarab", min = 1, max = 1},
+    second = {mons = "spectre place:Lair:6", min = 1, max = 1},
+    liquid = "water",
+    plant  = "withered",
   },
   {
-    first   = {mons = "shining eye", min = 1, max = 1},
-    second  = {mons = "floating eye / golden eye", min = 1, max = 2},
-    loot    = "potion of mutation ident:type",
-    weight  = 5,
+    first  = {mons = "shining eye", min = 1, max = 1},
+    second = {mons = "floating eye / golden eye", min = 1, max = 2},
+    loot   = "potion of mutation ident:type",
+    plant  = "demonic",
+    weight = 5,
   },
   {
-    first   = {mons = "eye of devastation", min = 1, max = 1},
-    second  = {mons = "floating eye / golden eye", min = 1, max = 2},
-    weight  = 5,
+    first  = {mons = "eye of devastation", min = 1, max = 1},
+    second = {mons = "floating eye / golden eye", min = 1, max = 2},
+    weight = 5,
   },
   {
-    first   = {mons = "tengu warrior", min = 1, max = 1},
-    second  = {mons = "tengu conjurer", min = 1, max = 2},
+    first  = {mons = "tengu warrior", min = 1, max = 1},
+    second = {mons = "tengu conjurer", min = 1, max = 2},
   },
   {
-    first   = {mons = "wizard", min = 1, max = 1},
-    second  = {mons = "white imp / shadow imp", min = 1, max = 3},
+    first  = {mons = "wizard", min = 1, max = 1},
+    second = {mons = "white imp / shadow imp", min = 1, max = 3},
   },
   {
-    second  = {mons = "large abomination", min = 0, max = 2},
-    third   = {mons = "small abomination", min = 3, max = 6},
+    second = {mons = "large abomination", min = 0, max = 2},
+    third  = {mons = "small abomination", min = 3, max = 6},
+    plant  = "demonic",
   },
   {
-    first   = {mons = "ice statue", min = 1, max = 1},
-    second  = {mons = "rime drake", min = 1, max = 2},
+    first  = {mons = "ice statue", min = 1, max = 1},
+    second = {mons = "rime drake", min = 1, max = 2},
+    liquid = "water",
   },
   {
-    first   = {mons = "oklob plant", min = 1, max = 1},
-    second  = {mons = "acid dragon", min = 1, max = 2},
+    first  = {mons = "oklob plant", min = 1, max = 1},
+    second = {mons = "acid dragon", min = 1, max = 2},
   },
   {
     first  = {mons = "necromancer", min = 1, max = 1},
     second = {mons = "wolf band", min = 1, max = 1},
+    plant  = "withered",
   },
   {
-    second  = {mons = "swamp worm", min = 2, max = 3},
+    second = {mons = "swamp worm", min = 2, max = 3},
   },
 }
 
@@ -303,9 +374,11 @@ tier1_gauntlet_arenas = {
 -- tier1_gauntlet_arenas.
 tier2_gauntlet_arenas = {
   {
-    first   = {mons = "hydra simulacrum / harpy simulacrum " ..
+    first  = {mons = "hydra simulacrum / harpy simulacrum " ..
                       "/ ice dragon simulacrum", min = 1, max = 1},
-    second  = {mons = "simulacrum place:D:15", min = 2, max = 4},
+    second = {mons = "simulacrum place:D:15", min = 2, max = 4},
+    liquid = "water",
+    plant  = "withered",
   },
   {
     first  = {mons = "dire elephant", min = 1, max = 1},
@@ -315,65 +388,87 @@ tier2_gauntlet_arenas = {
     first  = {mons = "catoblepas", min = 1, max = 1},
     second = {mons = "molten gargoyle", min = 1, max = 2},
     third  = {mons = "gargoyle", min = 1, max = 2},
+    liquid = "lava",
   },
   {
     second = {mons = "shadow", min = 3, max = 6},
+    liquid = "water",
+    plant  = "withered",
   },
   {
     first  = {mons = "fire crab", min = 1, max = 1},
     second = {mons = "hell hound", min = 1, max = 3},
     third  = {mons = "fire bat", min = 3, max = 6},
+    liquid = "lava",
   },
   {
     first  = {mons = "efreet", min = 1, max = 1},
     second = {mons = "hell hound", min = 1, max = 3},
     third  = {mons = "fire elemental", min = 1, max = 3},
+    liquid = "lava",
+    plant  = "demonic",
   },
   {
     first  = {mons = "hell hog", min = 1, max = 1},
     second = {mons = "hell hound", min = 1, max = 3},
     third  = {mons = "fire bat", min = 2, max = 4},
+    liquid = "lava",
+    plant  = "demonic",
   },
   {
     second = {mons = "death ooze", min = 1, max = 1},
     third  = {mons = "jelly w:5 / slime creature", min = 2, max = 4},
+    liquid = "water",
   },
   {
     second = {mons = "sixfirhy", min = 2, max = 4},
+    liquid = "water",
+    plant  = "demonic",
   },
   {
     first  = {mons = "hell beast", min = 1, max = 1},
     second = {mons = "hell hound", min = 0, max = 2},
+    liquid = "lava",
+    plant  = "demonic",
   },
   {
     first  = {mons = "green death", min = 1, max = 1},
     second = {mons = "orange demon", min = 0, max = 2},
+    plant  = "demonic",
   },
   {
     first  = {mons = "hellion", min = 1, max = 1},
     second = {mons = "sun demon / smoke demon / red devil", min = 0, max = 1},
     third  = {mons = "hell hound", min = 0, max = 2},
+    liquid = "lava",
+    plant  = "demonic",
     weight = 5
   },
   {
     first  = {mons = "balrug", min = 1, max = 1},
     third  = {mons = "sun demon", min = 0, max = 1},
     third  = {mons = "hell hound", min = 0, max = 2},
+    liquid = "lava",
+    plant  = "demonic",
     weight = 5
   },
   {
     first  = {mons = "cacodemon", min = 1, max = 1},
     second = {mons = "neqoxec", min = 0, max = 3},
     loot   = "potion of mutation ident:type",
+    plant  = "demonic",
     weight = 5
   },
   {
     first  = {mons = "blizzard demon", min = 1, max = 1},
     second = {mons = "white imp / ice devil", min = 0, max = 3},
+    liquid = "water",
   },
   {
     first  = {mons = "reaper", min = 1, max = 1},
     second = {mons = "hellwing / shadow imp", min = 1, max = 3},
+    liquid = "water",
+    plant  = "withered",
   },
   {
     first  = {mons = "glowing shapeshifter", min = 1, max = 1},
@@ -381,20 +476,26 @@ tier2_gauntlet_arenas = {
   },
   {
     first  = {mons = "apocalypse crab", min = 1, max = 1},
+    plant  = "demonic",
   },
   {
     second = {mons = "flying skull band", min = 2, max = 3},
+    plant  = "withered",
   },
   {
     first  = {mons = "death drake", min = 1, max = 1},
     second = {mons = "zombie place:D:15 / spectre place:D:15", min = 2,
               max = 4},
+    liquid = "water",
+    plant  = "withered"
   },
   {
     first  = {mons = "ghost crab", min = 1, max = 1},
+    liquid = "water",
+    plant  = "withered",
   },
   {
-    first = {mons = "meliai band", min = 1, max = 1},
+    first  = {mons = "meliai band", min = 1, max = 1},
   },
   {
     first  = {mons = "torpor snail", min = 1, max = 1},
@@ -404,32 +505,39 @@ tier2_gauntlet_arenas = {
     first  = {mons = "entropy weaver", min = 1, max = 1},
     second = {mons = "wolf spider", min = 0, max = 1},
     third  = {mons = "redback / jumping spider", min = 1, max = 2},
+    liquid = "water",
   },
   {
     first  = {mons = "moth of wrath", min = 1, max = 1},
     second = {mons = "wolf spider / nothing", min = 0, max = 1},
     third  = {mons = "redback", min = 2, max = 4},
+    liquid = "water",
   },
   {
     second = {mons = "thrashing horror", min = 1, max = 2},
     third  = {mons = "small abomination", min = 2, max = 4},
+    plant  = "demonic",
   },
   {
     first  = {mons = "great orb of eyes", min = 1, max = 1},
     second = {mons = "ugly thing", min = 1, max = 2},
+    plant  = "demonic",
   },
   {
     second = {mons = "glowing orange brain", min = 1, max = 2},
     third  = {mons = "ugly thing", min = 1, max = 2},
+    plant  = "demonic",
   },
   {
     second = {mons = "large abomination", min = 2, max = 3},
     third  = {mons = "small abomination", min = 3, max = 6},
+    plant  = "demonic",
   },
   {
     first  = {mons = "wretched star", min = 1, max = 1},
     second = {mons = "neqoxec / small abomination", min = 1, max = 3},
     loot   = "potion of mutation ident:type",
+    plant  = "demonic",
   },
   {
     second = {mons = "wizard", min = 2, max = 2},
@@ -438,16 +546,22 @@ tier2_gauntlet_arenas = {
   {
     first  = {mons = "necromancer", min = 1, max = 1},
     second = {mons = "death yak band", min = 1, max = 1},
+    liquid = "water",
+    plant  = "withered",
   },
   {
     second = {mons = "death scarab", min = 2, max = 2},
     third  = {mons = "spectre place:Lair:6", min = 2, max = 4},
+    liquid = "water",
+    plant  = "withered",
   },
   {
     first  = {mons = "orange crystal statue", min = 1, max = 1},
   },
   {
     first  = {mons = "obsidian statue", min = 1, max = 1},
+    liquid = "water",
+    plant  = "withered",
   },
   {
     -- Guarantee that one places in the central location.
@@ -456,8 +570,8 @@ tier2_gauntlet_arenas = {
     third  = {mons = "raiju", min = 1, max = 2},
   },
   {
-    first   = {mons = "tyrant leech", min = 1, max = 1},
-    second  = {mons = "swamp worm", min = 2, max = 4},
+    first  = {mons = "tyrant leech", min = 1, max = 1},
+    second = {mons = "swamp worm", min = 2, max = 4},
   },
 }
 
