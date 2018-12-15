f63cd86e0c8c143ac51ee46031eef15d51b73c6e
Edgar A. Bering IV <trizor@gmail.com>
2018-12-15 12:06:13 -0500

    Don't place monsters on traps
    For dispersal traps this creates the lingering question "how did they
    get there"; for alarm traps and Zot traps it prevents an interface screw
    in console. (It can still happen that a player enters LOS and a
    wandering awake monster is standing on certain kinds of traps.)

--- crawl-ref/source/mon-place.cc
+++ crawl-ref/source/mon-place.cc
@@ -418,13 +418,7 @@ bool can_place_on_trap(monster_type mon_type, trap_type trap)
     if (mon_type == MONS_FULMINANT_PRISM || mon_type == MONS_LIGHTNING_SPIRE)
         return true;
 
-    if (trap == TRAP_TELEPORT || trap == TRAP_TELEPORT_PERMANENT
-        || trap == TRAP_SHAFT)
-    {
-        return false;
-    }
-
-    return true;
+    return false;
 }
 
 bool drac_colour_incompatible(int drac, int colour)
