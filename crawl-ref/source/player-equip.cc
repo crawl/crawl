b41e856eae3359b69e61ab92559d21998b36b308
bjobae <bjobae@gmail.com>
2018-12-17 00:39:39 +0100

    Nerf Amulet of the Acrobat
    The amulet doesn't boost EV if player is constricted or held.

--- crawl-ref/source/player-equip.cc
+++ crawl-ref/source/player-equip.cc
@@ -1184,7 +1184,9 @@ bool acrobat_boost_visible()
     // it was during the last move. It's a little hacky.
     return you.props[ACROBAT_AMULET_ACTIVE].get_int() == 1
            && (you.duration[DUR_ACROBAT]
-               || you.props[LAST_ACTION_WAS_MOVE_OR_REST_KEY].get_bool());
+               || you.props[LAST_ACTION_WAS_MOVE_OR_REST_KEY].get_bool())
+           && (!you.caught())
+           && (!you.is_constricted());
 }
 
 static void _equip_amulet_of_mana_regeneration()
