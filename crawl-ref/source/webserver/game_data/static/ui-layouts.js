b-crawl/bcrawl

Iconography and gui elements for decks
Move old deck icons to UNUSED. Recolour and relocate the remaining ones
to fit the colour scheme of Nemelex's other abilities.

crawl-ref/source/webserver/game_data/static/ui-layouts.js
@@ -188,7 +188,7 @@ function ($, comm, client, ui, enums, cr, util, scroller, main, gui, player) {
     {
         var $popup = $(".templates > .describe-cards").clone();
         var $card_tmpl = $(".templates > .describe-generic");
-        var t = main.MISC_CARD, tex = enums.texture.DEFAULT;
+        var t = main.NEMELEX_CARD, tex = enums.texture.DEFAULT;
         desc.cards.forEach(function (card) {
             var $card = $card_tmpl.clone().removeClass("hidden").addClass("describe-card");
             $card.find(".header > span").html(card.name);

crawl/crawl

crawl-ref/source/webserver/game_data/static/ui-layouts.js
@@ -188,7 +188,7 @@ function ($, comm, client, ui, enums, cr, util, scroller, main, gui, player) {
     {
         var $popup = $(".templates > .describe-cards").clone();
         var $card_tmpl = $(".templates > .describe-generic");
-        var t = main.MISC_CARD, tex = enums.texture.DEFAULT;
+        var t = gui.NEMELEX_CARD, tex = enums.texture.GUI;
         desc.cards.forEach(function (card) {
             var $card = $card_tmpl.clone().removeClass("hidden").addClass("describe-card");
             $card.find(".header > span").html(card.name);