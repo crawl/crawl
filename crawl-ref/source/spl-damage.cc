640b738662c3ec8c815d89b0f6f15d81100eed2f
NormalPerson7 <cmrn.stphnsn@hotmail.com>
2019-01-16 18:10:11 +0000

    Fix monsters not being woken when hit by animated trees (11819)
    This commit adds a call to behaviour_event to, among other things, wake
    sleeping monsters, when they are attacked by an animated tree from
    summon forest and the attack hits (regardless of any damage dealt).

--- crawl-ref/source/spl-damage.cc
+++ crawl-ref/source/spl-damage.cc
@@ -2371,6 +2371,8 @@ void forest_damage(const actor *mon)
                             "@foe@ @is@ scraped by a branch!",
                             "A tree reaches out and scrapes @foe@!",
                             "A root barely touches @foe@ from below.");
+                    if (foe->is_monster())
+                        behaviour_event(foe->as_monster(), ME_WHACK);    
                 }
                 else
                 {
@@ -2378,6 +2380,8 @@ void forest_damage(const actor *mon)
                         "@foe@ @is@ hit by a branch!",
                         "A tree reaches out and hits @foe@!",
                         "A root smacks @foe@ from below.");
+                    if (foe->is_monster())
+                        behaviour_event(foe->as_monster(), ME_WHACK);    
                 }
 
                 msg = replace_all(replace_all(msg,
