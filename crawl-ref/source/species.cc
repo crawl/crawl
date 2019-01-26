dc03e01af6270c8ad31b42f997966e0998785fa6
gammafunk <gammafunk@gmail.com>
2019-01-25 18:16:50 -0600

    Don't give brown as an XL7 draconian colour (blorx1, ebering)
    The random_draconian_colour() function merged as part of the datafiles
    branch didn't properly exclude base draconians as a colour to award at
    XL7. Rewrite this function in a way that uses random_range() for the
    non-base colours, but so that it can also exclude removed/unviable
    colours.

--- crawl-ref/source/species.cc
+++ crawl-ref/source/species.cc
@@ -512,18 +512,24 @@ bool is_starting_species(species_type species)
         && !get_species_def(species).recommended_jobs.empty();
 }
 
+// Check that we can give this draconian species to players as a color.
+static bool _is_viable_draconian(species_type species)
+{
+#if TAG_MAJOR_VERSION == 34
+    if (species == SP_MOTTLED_DRACONIAN)
+        return false;
+#endif
+    return true;
+}
+
+// A random non-base draconian colour appropriate for the player.
 species_type random_draconian_colour()
 {
-    vector<species_type> dracs;
-    for (int drac = 0; drac < static_cast<int>(NUM_SPECIES); ++drac)
-        if (species_is_draconian(static_cast<species_type>(drac))
-#if TAG_MAJOR_VERSION == 34
-            && static_cast<species_type>(drac) != SP_MOTTLED_DRACONIAN
-#endif
-            )
-        {
-            dracs.push_back(static_cast<species_type>(drac));
-        }
-    auto choice = random_iterator(dracs);
-    return *choice;
+  species_type species;
+  do {
+      species =
+          static_cast<species_type>(random_range(SP_FIRST_NONBASE_DRACONIAN,
+                                                 SP_LAST_NONBASE_DRACONIAN));
+  } while (!_is_viable_draconian(species));
+  return species;
 }
