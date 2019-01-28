0a62fede96339ba7b056a6c009ad09b263f350ed
NormalPerson7 <cmrn.stphnsn@hotmail.com>
2019-01-28 18:37:55 +0000

    Use keys for paralysed_by and petrified_by player props (ebering)

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -6706,7 +6706,7 @@ void player::paralyse(actor *who, int str, string source)
     {
         take_note(Note(NOTE_PARALYSIS, str, 0, source));
         // use the real name here even for invisible monsters
-        props["paralysed_by"] = use_actor_name ? who->name(DESC_A, true)
+        props[PARALYSED_BY_KEY] = use_actor_name ? who->name(DESC_A, true)
                                                : source;
     }
 
@@ -6754,7 +6754,7 @@ void player::petrify(actor *who, bool force)
     duration[DUR_PETRIFYING] = 3 * BASELINE_DELAY;
 
     if (who)
-        props["petrified_by"] = who->name(DESC_A, true);
+        props[PETRIFIED_BY_KEY] = who->name(DESC_A, true);
 
     redraw_evasion = true;
     mprf(MSGCH_WARN, "You are slowing down.");
