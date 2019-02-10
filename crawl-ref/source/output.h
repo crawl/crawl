eca9499c9d91cdc62de88a91636d7acb802a895d
Alex Jurkiewicz <alex@jurkiewi.cz>
2019-02-10 17:17:56 +1100

    Expose MR and Stealth to Lua
    Two new functions return the number of pips for each of these
    resistances.
    
    (cherry picked from commit db70d5e40e67d5362310165970ab0c9ed0a707a9)

--- crawl-ref/source/output.h
+++ crawl-ref/source/output.h
@@ -29,6 +29,8 @@ int update_monster_pane();
 const char *equip_slot_to_name(int equip);
 int equip_name_to_slot(const char *s);
 
+int stealth_breakpoint(int stealth);
+
 void print_overview_screen();
 
 string mutation_overview();
