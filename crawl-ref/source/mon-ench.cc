e140485246c40f86f40a5567cb418fac4ac19ab8
gammafunk <gammafunk@gmail.com>
2019-02-16 23:04:20 -0600

    Tidy up a monster class method
    In monster::is_summoned(), remove checks for derived undead spells that
    no longer apply, since those use fake abjuration now, and clean up
    comments.
    
    (cherry picked from commit 53b51606c3e9e82d1c48a31361246907f715bc3a)

--- crawl-ref/source/mon-ench.cc
+++ crawl-ref/source/mon-ench.cc
@@ -1906,6 +1906,21 @@ void monster::mark_summoned(int longevity, bool mark_items, int summon_type, boo
             ii->flags |= ISFLAG_SUMMONED;
 }
 
+/* Is the monster temporarily summoned?
+ *
+ * Monsters must have ENCH_ABJ (giving how long they last) to be considered
+ * summons. If they additionally set ENCH_SUMMON, which gives how they were
+ * derived, this must not be from certain spells or "monster summoning types"
+ * we don't consider actual summons. Temporary monsters with
+ * ENCH_FAKE_ABJURATION also aren't summons, and durably summoned monsters
+ * aren't temporary.
+ *
+ * @param[out] duration    The monster's summon duration in aut.
+ * @param[out] summon_type The monster's means of summoning. If positive, this
+ *                         is a spell_type, if negative, it's a
+ *                         mon_summon_type.
+ * @returns True if the monster is a temporary summon, false otherwise.
+ */
 bool monster::is_summoned(int* duration, int* summon_type) const
 {
     const mon_enchant abj = get_ench(ENCH_ABJ);
@@ -1932,19 +1947,16 @@ bool monster::is_summoned(int* duration, int* summon_type) const
     if (summon_type != nullptr)
         *summon_type = summ.degree;
 
+    // Conjured things (fire vortices, ball lightning, IOOD) are not summoned.
     if (mons_is_conjured(type))
         return false;
 
+    // Certain spells or monster summon types that set abjuration but aren't
+    // considered summons.
     switch (summ.degree)
     {
     // Temporarily dancing weapons are really there.
     case SPELL_TUKIMAS_DANCE:
-
-    // A corpse/skeleton which was temporarily animated.
-    case SPELL_ANIMATE_DEAD:
-    case SPELL_ANIMATE_SKELETON:
-
-    // Conjured stuff (fire vortices, ball lightning, IOOD) is handled above.
 
     // Clones aren't really summoned (though their equipment might be).
     case MON_SUMM_CLONE:
