crawl/crawl

commit f44d0c5ad20cc88c73c694df44ac0a645adc09d0

Fix dancing weapon description when multiple monsters enter LOS (11843)
When multiple enemies entered line of sight at once including at least
one dancing weapon, the message "There is." was being printed. This is
because there was an unnecessary check for enemies not being dancing
weapons in get_monster_equipment_desc. Remove this check, and add a
special case to the helper function _describe_monster_weapon to prevent
these messages being printed as "There is wielding a +4 trident."

These messages should now properly print as e.g.
"There is a +4 executioner's axe of electrocution."

crawl-ref/source/directn.cc
@@ -3001,6 +3001,8 @@ static string _describe_monster_weapon(const monster_info& mi, bool ident)
 
     if (mi.type == MONS_PANDEMONIUM_LORD)
         desc += " armed with ";
+    else if (mi.type == MONS_DANCING_WEAPON)
+        desc += " ";
     else
         desc += " wielding ";
     desc += name1;
@@ -3297,7 +3299,7 @@ string get_monster_equipment_desc(const monster_info& mi,
 
     string weap = "";
 
-    if (mi.type != MONS_DANCING_WEAPON && mi.type != MONS_SPECTRAL_WEAPON)
+    if (mi.type != MONS_SPECTRAL_WEAPON)
         weap = _describe_monster_weapon(mi, level == DESC_IDENTIFIED);
 
     // Print the rest of the equipment only for full descriptions.

commit 8afefd03de73d48d8dcf591056bba925e3d0e5b3

Fix dancing weapon description on hover (11887)
Previously the dancing weapon's name was being duplicated because there
weren't proper checks for dancing weapons in the second half of
get_monster_equipment_desc. This was broken in f44d0c5.

crawl-ref/source/directn.cc
@@ -3297,10 +3297,7 @@ string get_monster_equipment_desc(const monster_info& mi,
         }
     }
 
-    string weap = "";
-
-    if (mi.type != MONS_SPECTRAL_WEAPON)
-        weap = _describe_monster_weapon(mi, level == DESC_IDENTIFIED);
+    string weap = _describe_monster_weapon(mi, level == DESC_IDENTIFIED);
 
     // Print the rest of the equipment only for full descriptions.
     if (level == DESC_WEAPON || level == DESC_WEAPON_WARNING)
@@ -3342,8 +3339,13 @@ string get_monster_equipment_desc(const monster_info& mi,
 
     vector<string> item_descriptions;
 
-    if (!weap.empty())
+    // Dancing weapons have all their weapon information in their full_name, so
+    // we don't need to add another weapon description here (see Mantis 11887).
+    if (!weap.empty()
+        && mi.type != MONS_DANCING_WEAPON && mi.type != MONS_SPECTRAL_WEAPON)
+    {
         item_descriptions.push_back(weap.substr(1)); // strip leading space
+    }
 
     if (mon_arm)
     {