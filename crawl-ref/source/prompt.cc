70d21d577ef650efbc6ac2f9f77ebe044fa1c3ce
advil <rawlins@gmail.com>
2019-03-12 09:17:31 -0400

    Fix build try 2
    (cherry picked from commit 4d902318472b732ae930f8d934103d35724b2abd)

--- crawl-ref/source/prompt.cc
+++ crawl-ref/source/prompt.cc
@@ -4,7 +4,7 @@
  **/
 
 #include "AppHdr.h"
-#include <math.h> // isnan
+#include <cmath> // isnan
 
 #include "prompt.h"
 
@@ -346,7 +346,7 @@ double prompt_for_float(const char* prompt)
     char *end;
     double ret = strtod(specs, &end);
 
-    if (ret == 0 && end == specs || ::isnan(ret))
+    if (ret == 0 && end == specs || std::isnan(ret))
         ret = -1;
 
     return ret;
