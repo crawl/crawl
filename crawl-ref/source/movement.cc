0d52ccf8f5b0280e7c7045b3a733428c0790a01e
NormalPerson7 <cmrn.stphnsn@hotmail.com>
2019-03-30 22:26:05 +0000

    Fix spurious message on moving into door with wall_jump_move (11940)
    When attempting to move into a closed door with the wall_jump_move
    option set to true, the function wu_jian_can_wall_jump was being called,
    which meant spurious messages describing why wall jumping was impossible
    in the current situation were being printed to the message log.
    
    To fix this, move the check for moving into a closed door (a situation
    where it is not possible to wall jump by moving) to inside the
    definition of bool can_wall_jump, before the function
    wu_jian_can_wall_jump() is called.
    
    Also remove an unnecessary variable (targ_grid) and replace it with
    grd(targ) everywhere.

--- crawl-ref/source/movement.cc
+++ crawl-ref/source/movement.cc
@@ -537,8 +537,13 @@ void move_player_action(coord_def move)
 
     const coord_def targ = you.pos() + move;
     string wall_jump_err;
-    bool can_wall_jump = Options.wall_jump_move &&
-                            wu_jian_can_wall_jump(targ, wall_jump_err);
+    // Don't allow wall jump against close doors via movement -- need to use
+    // the ability. Also, if moving into a closed door, don't call
+    // wu_jian_can_wall_jump, to avoid printing a spurious message (see 11940).
+    bool can_wall_jump = Options.wall_jump_move
+                         && (!in_bounds(targ)
+                             || !feat_is_closed_door(grd(targ)))
+                         && wu_jian_can_wall_jump(targ, wall_jump_err);
     bool did_wall_jump = false;
     // You can't walk out of bounds!
     if (!in_bounds(targ) && !can_wall_jump)
@@ -548,13 +553,6 @@ void move_player_action(coord_def move)
             mpr("This wall is too hard to dig through.");
         return;
     }
-
-    const dungeon_feature_type targ_grid = grd(targ);
-
-    // don't allow wall jump against close doors via movement -- need to use
-    // the ability
-    if (can_wall_jump && feat_is_closed_door(targ_grid))
-        can_wall_jump = false;
 
     const string walkverb = you.airborne()                     ? "fly"
                           : you.swimming()                     ? "swim"
@@ -821,7 +819,7 @@ void move_player_action(coord_def move)
     // BCR - Easy doors single move
     if ((Options.travel_open_doors || !you.running)
         && !attacking
-        && feat_is_closed_door(targ_grid))
+        && feat_is_closed_door(grd(targ)))
     {
         open_door_action(move);
         move.reset();
