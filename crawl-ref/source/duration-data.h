crawl/crawl

commit 1406302debe1752e4e119b44c7eb044399fa33dc

Simplify HP calculations (11728)
Combine calc_hp() and recalc_and_scale_hp() into a single function.

Remove the default for the rotting parameter to get_real_hp() and
require it be explicit in each usage.

Add a check to get_real_hp() for the presence of hp changing
transmutations, and handle those through calc_hp instead of some special
cases. Sequence equipment melding and HP calculation appropraitely.
(This fixes 11728.)

[Committer's note: Closes #875, #877. Checked whitespace, revised the
 commit message to reflect the final state of the patch. Simplified a
 check.]

crawl-ref/source/duration-data.h
@@ -273,6 +273,7 @@ static const duration_def duration_data[] =
       "You are standing in death's doorway.", D_EXPIRES,
       {{ "Your life is in your own hands again!", []() {
             you.duration[DUR_DEATHS_DOOR_COOLDOWN] = random_range(10, 30);
+            calc_hp();
       }}, { "Your time is quickly running out!", 5 }}, 10},
     { DUR_DEATHS_DOOR_COOLDOWN,
       YELLOW, "-DDoor",

crawl-ref/source/player.cc
@@ -63,6 +63,7 @@
 #include "shout.h"
 #include "skills.h"
 #include "spl-damage.h"
+#include "spl-selfench.h"
 #include "spl-transloc.h"
 #include "spl-util.h"
 #include "sprint.h"
@@ -2692,7 +2693,7 @@ static void _gain_and_note_hp_mp()
     const int old_maxmp = you.max_magic_points;
 
     // recalculate for game
-    recalc_and_scale_hp();
+    calc_hp(true, false);
     calc_mp();
 
     set_mp(old_maxmp > 0 ? old_mp * you.max_magic_points / old_maxmp
@@ -2700,7 +2701,7 @@ static void _gain_and_note_hp_mp()
 
     // Get "real" values for note-taking, i.e. ignore Berserk,
     // transformations or equipped items.
-    const int note_maxhp = get_real_hp(false, false);
+    const int note_maxhp = get_real_hp(false, true);
     const int note_maxmp = get_real_mp(false);
 
     char buf[200];
@@ -2713,19 +2714,34 @@ static void _gain_and_note_hp_mp()
 /**
  * Calculate max HP changes and scale current HP accordingly.
  */
-void recalc_and_scale_hp()
+void calc_hp(bool scale, bool set)
 {
     // Rounding must be down or Deep Dwarves would abuse certain values.
     // We can reduce errors by a factor of 100 by using partial hp we have.
+    int oldhp = you.hp;
     int old_max = you.hp_max;
-    int hp = you.hp * 100 + you.hit_points_regeneration;
-    calc_hp();
-    int new_max = you.hp_max;
-    hp = hp * new_max / old_max;
-    if (hp < 100)
-        hp = 100;
-    set_hp(min(hp / 100, you.hp_max));
-    you.hit_points_regeneration = hp % 100;
+
+    you.hp_max = get_real_hp(true, true);
+
+    if (scale) {
+        int hp = you.hp * 100 + you.hit_points_regeneration;
+        int new_max = you.hp_max;
+        hp = hp * new_max / old_max;
+        if (hp < 100)
+            hp = 100;
+        set_hp(min(hp / 100, you.hp_max));
+        you.hit_points_regeneration = hp % 100;
+    }
+    if (set)
+        you.hp = you.hp_max;
+
+    you.hp = min(you.hp, you.hp_max);
+
+    if (oldhp != you.hp || old_max != you.hp_max)
+    {
+        dprf("HP changed: %d/%d -> %d/%d", oldhp, old_max, you.hp, you.hp_max);
+        you.redraw_hit_points = true;
+    }
 }
 
 int xp_to_level_diff(int xp, int scale)
@@ -3632,16 +3648,6 @@ int player::scan_artefacts(artefact_prop_type which_property,
     return retval;
 }
 
-void calc_hp()
-{
-    int oldhp = you.hp, oldmax = you.hp_max;
-    you.hp_max = get_real_hp(true, false);
-    deflate_hp(you.hp_max, false);
-    if (oldhp != you.hp || oldmax != you.hp_max)
-        dprf("HP changed: %d/%d -> %d/%d", oldhp, oldmax, you.hp, you.hp_max);
-    you.redraw_hit_points = true;
-}
-
 void dec_hp(int hp_loss, bool fatal, const char *aux)
 {
     ASSERT(!crawl_state.game_is_arena());
@@ -3817,7 +3823,7 @@ void rot_hp(int hp_loss)
     const int initial_rot = you.hp_max_adj_temp;
     you.hp_max_adj_temp -= hp_loss;
     // don't allow more rot than you have normal mhp
-    you.hp_max_adj_temp = max(-(get_real_hp(false, true) - 1),
+    you.hp_max_adj_temp = max(-(get_real_hp(false, false) - 1),
                               you.hp_max_adj_temp);
     if (initial_rot == you.hp_max_adj_temp)
         return;
@@ -3880,20 +3886,6 @@ void dec_max_hp(int hp_loss)
     you.redraw_hit_points = true;
 }
 
-// Use of floor: false = hp max, true = hp min. {dlb}
-void deflate_hp(int new_level, bool floor)
-{
-    ASSERT(!crawl_state.game_is_arena());
-
-    if (floor && you.hp < new_level)
-        you.hp = new_level;
-    else if (!floor && you.hp > new_level)
-        you.hp = new_level;
-
-    // Must remain outside conditional, given code usage. {dlb}
-    you.redraw_hit_points = true;
-}
-
 void set_hp(int new_amount)
 {
     ASSERT(!crawl_state.game_is_arena());
@@ -3922,9 +3914,13 @@ void set_mp(int new_amount)
     you.redraw_magic_points = true;
 }
 
-// If trans is true, being berserk and/or transformed is taken into account
-// here. Else, the base hp is calculated. If rotted is true, calculate the
-// real max hp you'd have if the rotting was cured.
+/**
+ * Get the player's max HP
+ * @param trans          Whether to include transformations, berserk,
+ *                       items etc.
+ * @param rotted         Whether to include the effects of rotting.
+ * @return               The player's calculated max HP.
+ */
 int get_real_hp(bool trans, bool rotted)
 {
     int hitp;
@@ -3952,7 +3948,7 @@ int get_real_hp(bool trans, bool rotted)
 
     hitp /= 100;
 
-    if (!rotted)
+    if (rotted)
         hitp += you.hp_max_adj_temp;
 
     if (trans)
@@ -3962,13 +3958,17 @@ int get_real_hp(bool trans, bool rotted)
     if (trans && you.berserk())
         hitp = hitp * 3 / 2;
 
-    if (trans) // Some transformations give you extra hp.
+    // Some transformations give you extra hp.
+    if (trans)
         hitp = hitp * form_hp_mod() / 10;
 
 #if TAG_MAJOR_VERSION == 34
     if (trans && player_equip_unrand(UNRAND_ETERNAL_TORMENT))
         hitp = hitp * 4 / 5;
 #endif
+
+    if (trans && you.duration[DUR_DEATHS_DOOR] > 0)
+        hitp = allowed_deaths_door_hp();
 
     return max(1, hitp);
 }
@@ -8156,9 +8156,8 @@ void player_end_berserk()
 
     slow_player(dur);
 
-    // 1KB: No berserk healing.
-    set_hp((you.hp + 1) * 2 / 3);
-    calc_hp();
+    //Un-apply Berserk's +50% Current/Max HP
+    calc_hp(true, false);
 
     learned_something_new(HINT_POSTBERSERK);
     Hints.hints_events[HINT_YOU_ENCHANTED] = hints_slow;

crawl-ref/source/spl-selfench.cc
@@ -44,11 +44,10 @@ spret_type cast_deaths_door(int pow, bool fail)
     mpr("You stand defiantly in death's doorway!");
     mprf(MSGCH_SOUND, "You seem to hear sand running through an hourglass...");
 
-    set_hp(allowed_deaths_door_hp());
-    deflate_hp(you.hp_max, false);
-
     you.set_duration(DUR_DEATHS_DOOR, 10 + random2avg(13, 3)
                                        + (random2(pow) / 10));
+
+    calc_hp(false, true);
 
     if (you.duration[DUR_DEATHS_DOOR] > 25 * BASELINE_DELAY)
         you.duration[DUR_DEATHS_DOOR] = (23 + random2(5)) * BASELINE_DELAY;

crawl-ref/source/ghost.cc
@@ -277,7 +277,7 @@ void ghost_demon::init_player_ghost(bool actual_ghost)
     unwind_var<bool> fishtail(you.fishtail, false);
 
     name   = you.your_name;
-    max_hp = min(get_real_hp(false), MAX_GHOST_HP);
+    max_hp = min(get_real_hp(false, false), MAX_GHOST_HP);
     ev     = min(you.evasion(EV_IGNORE_HELPLESS), MAX_GHOST_EVASION);
     ac     = you.armour_class();
     dprf("ghost ac: %d, ev: %d", ac, ev);

crawl-ref/source/hiscores.cc
@@ -1718,7 +1718,7 @@ void scorefile_entry::init(time_t dt)
 
     final_hp         = you.hp;
     final_max_hp     = you.hp_max;
-    final_max_max_hp = get_real_hp(true, true);
+    final_max_max_hp = get_real_hp(true, false);
 
     final_mp          = you.magic_points;
     final_max_mp      = you.max_magic_points;

crawl-ref/source/output.cc
@@ -753,7 +753,7 @@ static void _print_stats_mp(int x, int y)
 
 static void _print_stats_hp(int x, int y)
 {
-    int max_max_hp = get_real_hp(true, true);
+    int max_max_hp = get_real_hp(true, false);
 
     // Calculate colour
     short hp_colour = HUD_VALUE_COLOUR;
@@ -765,7 +765,7 @@ static void _print_stats_hp(int x, int y)
     else
     {
         const int hp_percent =
-            (you.hp * 100) / get_real_hp(true, false);
+            (you.hp * 100) / get_real_hp(true, true);
 
         for (const auto &entry : Options.hp_colour)
             if (hp_percent <= entry.first)
@@ -2172,7 +2172,7 @@ static vector<formatted_string> _get_overview_stats()
 
     entry.cprintf("%d/%d", you.hp, you.hp_max);
     if (player_rotted())
-        entry.cprintf(" (%d)", get_real_hp(true, true));
+        entry.cprintf(" (%d)", get_real_hp(true, false));
 
     cols.add_formatted(0, entry.to_colour_string(), false);
     entry.clear();

crawl-ref/source/player-act.cc
@@ -740,10 +740,8 @@ bool player::go_berserk(bool intentional, bool potion)
 
     you.increase_duration(DUR_BERSERK, berserk_duration);
 
-    calc_hp();
-    set_hp(you.hp * 3 / 2);
-
-    deflate_hp(you.hp_max, false);
+    //Apply Berserk's +50% Current/Max HP
+    calc_hp(true, false);
 
     if (!you.duration[DUR_MIGHT])
         notify_stat_change(STAT_STR, 5, true);

crawl-ref/source/player-equip.cc
@@ -45,7 +45,7 @@ static void _mark_unseen_monsters();
  */
 static void _calc_hp_artefact()
 {
-    recalc_and_scale_hp();
+    calc_hp();
     if (you.hp_max <= 0) // Borgnjor's abusers...
         ouch(0, KILLED_BY_DRAINING);
 }

commit 4517bf713c99b7565a801d8f8562389a07eae0c9

Don't change max HP during DDoor (#11933, #11976)
Changed in 1406302de, which broke DDoor's interaction with Revivification and
HP-costing abilities.

crawl-ref/source/duration-data.h
@@ -273,7 +273,6 @@ static const duration_def duration_data[] =
       "You are standing in death's doorway.", D_EXPIRES,
       {{ "Your life is in your own hands again!", []() {
             you.duration[DUR_DEATHS_DOOR_COOLDOWN] = random_range(10, 30);
-            calc_hp();
       }}, { "Your time is quickly running out!", 5 }}, 10},
     { DUR_DEATHS_DOOR_COOLDOWN,
       YELLOW, "-DDoor",

crawl-ref/source/player.cc
@@ -3929,7 +3929,7 @@ int get_real_hp(bool trans, bool rotted)
     hitp /= 10;
 
     const bool hep_frail = have_passive(passive_t::frail)
-                          || player_under_penance(GOD_HEPLIAKLQANA);
+                           || player_under_penance(GOD_HEPLIAKLQANA);
 
     // Mutations that increase HP by a percentage
     hitp *= 100 + (you.get_mutation_level(MUT_ROBUST) * 10)
@@ -3959,9 +3959,6 @@ int get_real_hp(bool trans, bool rotted)
     if (trans && player_equip_unrand(UNRAND_ETERNAL_TORMENT))
         hitp = hitp * 4 / 5;
 #endif
-
-    if (trans && you.duration[DUR_DEATHS_DOOR] > 0)
-        hitp = allowed_deaths_door_hp();
 
     return max(1, hitp);
 }

crawl-ref/source/spl-selfench.cc
@@ -47,7 +47,7 @@ spret cast_deaths_door(int pow, bool fail)
     you.set_duration(DUR_DEATHS_DOOR, 10 + random2avg(13, 3)
                                        + (random2(pow) / 10));
 
-    calc_hp(false, true);
+    set_hp(allowed_deaths_door_hp());
 
     if (you.duration[DUR_DEATHS_DOOR] > 25 * BASELINE_DELAY)
         you.duration[DUR_DEATHS_DOOR] = (23 + random2(5)) * BASELINE_DELAY;