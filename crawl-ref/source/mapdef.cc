crawl/crawl

commit 80c1907ebbf68b0f6e40264dfb70bfa6b7c4ed38

Use unordered_set for tags, instead of set
f1fd2d0243764f converted the tags API and internal representation from
pure strings (that got split every access) into set<string>s, to deal
with order issues that were leading to seed divergence. However, it
turns out that `has_tag` is called many, many more times than a sorted
tag list is ever needed, and since `set` usually uses something like a binary
tree internally, the costs of a search were substantially outweighing
any gains from storing the tags in a sorted form.

This commit converts to using unordered_set for better lookup time,
which cuts about 50% of the time spent checking for tags. On a fulldebug
build, this shaves at least 1s off a typical total pregen time. There is
probably more to be gained as even with this change, the `count` call in
has_tag is *still* about 1s total over a full pregen run. I've also
slightly clarified some function names in this API.

The cost of `get_tags` is negligable, about 1ms total.

crawl-ref/source/mapdef.cc
@@ -2732,7 +2732,7 @@ string map_def::validate_map_placeable()
     // Ok, the map wants to be placed by tag. In this case it should have
     // at least one tag that's not a map flag.
     bool has_selectable_tag = false;
-    for (const string &piece : get_tags())
+    for (const string &piece : tags)
     {
         if (_map_tag_is_selectable(piece))
         {
@@ -3245,21 +3245,10 @@ void map_def::fixup()
     }
 }
 
-bool map_def::has_tags(const set<string> &tagswanted) const
+bool map_def::has_all_tags(const string &tagswanted) const
 {
-    if (tags.empty() || tagswanted.size() == 0)
-        return false;
-
-    for (const string &tag : tagswanted)
-        if (!tags.count(tag))
-            return false;
-
-    return true;
-}
-
-bool map_def::has_tags(const string &tagswanted) const
-{
-    return has_tags(parse_tags(tagswanted));
+    const auto &tags_set = parse_tags(tagswanted);
+    return has_all_tags(tags_set.begin(), tags_set.end());
 }
 
 bool map_def::has_tag(const string &tagwanted) const
@@ -3287,9 +3276,18 @@ bool map_def::has_tag_suffix(const string &suffix) const
     return false;
 }
 
-const set<string> map_def::get_tags() const
+const unordered_set<string> map_def::get_tags_unsorted() const
 {
     return tags;
+}
+
+const vector<string> map_def::get_tags() const
+{
+    // this might seem inefficient, but get_tags is not called very much; the
+    // hotspot revealed by profiling is actually has_tag checks.
+    vector<string> result(tags.begin(), tags.end());
+    sort(result.begin(), result.end());
+    return result;
 }
 
 void map_def::add_tags(const string &tag)

crawl-ref/source/dungeon.cc
@@ -684,7 +684,7 @@ static void _set_grd(const coord_def &c, dungeon_feature_type feat)
     grd(c) = feat;
 }
 
-static void _dgn_register_vault(const string &name, const set<string> &tags)
+static void _dgn_register_vault(const string &name, const unordered_set<string> &tags)
 {
     if (!tags.count("allow_dup"))
         you.uniq_map_names.insert(name);
@@ -692,7 +692,10 @@ static void _dgn_register_vault(const string &name, const set<string> &tags)
     if (tags.count("luniq"))
         env.level_uniq_maps.insert(name);
 
-    for (const string &tag : tags)
+    vector<string> sorted_tags(tags.begin(), tags.end());
+    sort(sorted_tags.begin(), sorted_tags.end());
+
+    for (const string &tag : sorted_tags)
     {
         if (starts_with(tag, "uniq_"))
             you.uniq_map_tags.insert(tag);
@@ -703,7 +706,7 @@ static void _dgn_register_vault(const string &name, const set<string> &tags)
 
 static void _dgn_register_vault(const map_def &map)
 {
-    _dgn_register_vault(map.name, map.get_tags());
+    _dgn_register_vault(map.name, map.get_tags_unsorted());
 }
 
 static void _dgn_register_vault(const string &name, string &spaced_tags)
@@ -716,7 +719,7 @@ static void _dgn_unregister_vault(const map_def &map)
     you.uniq_map_names.erase(map.name);
     env.level_uniq_maps.erase(map.name);
 
-    for (const string &tag : map.get_tags())
+    for (const string &tag : map.get_tags_unsorted())
     {
         if (starts_with(tag, "uniq_"))
             you.uniq_map_tags.erase(tag);
@@ -1065,9 +1068,7 @@ dgn_register_place(const vault_placement &place, bool register_vault)
     }
 
     // Find tags matching properties.
-    set<string> tags = place.map.get_tags();
-
-    for (const auto &tag : tags)
+    for (const auto &tag : place.map.get_tags_unsorted())
     {
         const feature_property_type prop = str_to_fprop(tag);
         if (prop == FPROP_NONE)
@@ -2567,9 +2568,9 @@ static bool _vault_can_use_layout(const map_def *vault, const map_def *layout)
 
     ASSERT(layout->has_tag_prefix("layout_type_"));
 
-    const set<string> tags = layout->get_tags();
-
-    for (auto &tag : tags)
+    // in principle, tag order can matter here, even though that is probably
+    // a vault designer error
+    for (const auto &tag : layout->get_tags())
     {
         if (starts_with(tag, "layout_type_"))
         {

crawl-ref/source/libutil.cc
@@ -218,11 +218,11 @@ int strip_number_tag(string &s, const string &tagprefix)
     return x;
 }
 
-set<string> parse_tags(const string &tags)
+unordered_set<string> parse_tags(const string &tags)
 {
     vector<string> split_tags = split_string(" ", tags);
-    set<string> ordered_tags(split_tags.begin(), split_tags.end());
-    return ordered_tags;
+    unordered_set<string> tags_set(split_tags.begin(), split_tags.end());
+    return tags_set;
 }
 
 bool parse_int(const char *s, int &i)

crawl-ref/source/libutil.h
@@ -9,6 +9,7 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 #include "enum.h"
 
@@ -74,7 +75,7 @@ int strip_number_tag(string &s, const string &tagprefix);
 vector<string> strip_multiple_tag_prefix(string &s, const string &tagprefix);
 string strip_tag_prefix(string &s, const string &tagprefix);
 const string tag_without_prefix(const string &s, const string &tagprefix);
-set<string> parse_tags(const string &tags);
+unordered_set<string> parse_tags(const string &tags);
 bool parse_int(const char *s, int &i);
 
 // String 'descriptions'

crawl-ref/source/mapdef.h
@@ -17,6 +17,7 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 #include "dlua.h"
 #include "enum.h"
@@ -1151,7 +1152,7 @@ public:
     vector<subvault_place> subvault_places;
 
 private:
-    set<string>     tags;
+    unordered_set<string>     tags;
     // This map has been loaded from an index, and not fully realised.
     bool            index_only;
     mutable long    cache_offset;
@@ -1248,10 +1249,20 @@ public:
     bool is_minivault() const;
     bool is_overwritable_layout() const;
     bool has_tag(const string &tagwanted) const;
-    bool has_tags(const string &tagswanted) const;
-    bool has_tags(const set<string> &tagswanted) const;
     bool has_tag_prefix(const string &tag) const;
     bool has_tag_suffix(const string &suffix) const;
+
+    template <typename TagIterator>
+    bool has_all_tags(TagIterator begin, TagIterator end) const
+    {
+        if (tags.empty() || begin == end) // legacy behavior for empty case
+            return false;
+        for ( ; begin != end; ++begin)
+            if (!has_tag(*begin))
+                return false;
+        return true;
+    }
+    bool has_all_tags(const string &tagswanted) const;
 
     template <typename TagIterator>
     bool has_any_tag(TagIterator begin, TagIterator end) const
@@ -1262,7 +1273,8 @@ public:
         return false;
     }
 
-    const set<string> get_tags() const;
+    const vector<string> get_tags() const;
+    const unordered_set<string> get_tags_unsorted() const;
     void add_tags(const string &tag);
     void set_tags(const string &tag);
     bool remove_tags(const string &tag);

crawl-ref/source/maps.cc
@@ -730,11 +730,11 @@ mapref_vector find_maps_for_tag(const string &tag,
 {
     mapref_vector maps;
     level_id place = level_id::current();
-    set<string> tag_set = parse_tags(tag);
+    unordered_set<string> tag_set = parse_tags(tag);
 
     for (const map_def &mapdef : vdefs)
     {
-        if (mapdef.has_tags(tag_set)
+        if (mapdef.has_all_tags(tag_set.begin(), tag_set.end())
             && !mapdef.has_tag("dummy")
             && (!check_depth || !mapdef.has_depth()
                 || mapdef.is_usable_in(place))
@@ -888,7 +888,7 @@ bool map_selector::accept(const map_def &mapdef) const
     }
 
     case TAG:
-        return mapdef.has_tags(tag) // allow multiple tags, for temple overflow vaults
+        return mapdef.has_all_tags(tag) // allow multiple tags, for temple overflow vaults
                && (!check_depth
                    || !mapdef.has_depth()
                    || mapdef.is_usable_in(place))

crawl-ref/source/ng-init.cc
@@ -142,7 +142,7 @@ void initialise_temples()
         if (main_temple->has_tag("temple_variable"))
         {
             vector<int> sizes;
-            for (auto &tag : main_temple->get_tags())
+            for (const auto &tag : main_temple->get_tags())
             {
                 if (starts_with(tag, "temple_altars_"))
                 {
@@ -287,7 +287,7 @@ void initialise_temples()
             break;
         unsigned int num = 0;
         vector<god_type> this_temple_gods;
-        for (auto &tag : map->get_tags())
+        for (const auto &tag : map->get_tags())
         {
             if (!starts_with(tag, "temple_overflow_"))
                 continue;

commit 8f2d45b691dc0e18d645d593e43fdf87dfd8f4cd

Sort tags_string() again
This is a regression from 80c1907ebbf6.

crawl-ref/source/mapdef.cc
@@ -3378,7 +3378,8 @@ void map_def::set_tags(const string &tag)
 
 string map_def::tags_string() const
 {
-    return join_strings(tags.begin(), tags.end());
+    auto sorted_tags = get_tags();
+    return join_strings(sorted_tags.begin(), sorted_tags.end());
 }
 
 keyed_mapspec *map_def::mapspec_at(const coord_def &c)