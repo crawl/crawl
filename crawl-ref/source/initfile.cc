c6fd4e61657c468868a7e22272abb3d390f07c1c
Alex Jurkiewicz <alex@jurkiewi.cz>
2019-07-10 12:09:24 +1000

    Default show_more to false in debug builds
    Debug builds unsurprisingly spew large numbers of messages, so
    repeatedly showing a more prompt is very inconvenient.

--- crawl-ref/source/initfile.cc
+++ crawl-ref/source/initfile.cc
@@ -194,7 +194,11 @@ const vector<GameOption*> game_options::build_options_list()
         new BoolGameOption(SIMPLE_NAME(note_chat_messages), false),
         new BoolGameOption(SIMPLE_NAME(note_dgl_messages), true),
         new BoolGameOption(SIMPLE_NAME(clear_messages), false),
+#ifdef DEBUG
+        new BoolGameOption(SIMPLE_NAME(show_more), false),
+#else
         new BoolGameOption(SIMPLE_NAME(show_more), !USING_TOUCH),
+#endif
         new BoolGameOption(SIMPLE_NAME(small_more), false),
         new BoolGameOption(SIMPLE_NAME(pickup_thrown), true),
         new BoolGameOption(SIMPLE_NAME(show_travel_trail), USING_DGL),
