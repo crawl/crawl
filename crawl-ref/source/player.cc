crawl/crawl

commit ece4408007701a2614cf13ff3611b8bd120c697f

Turn off skill targets when aptitudes change from level up (11943)
Add a call to check_training_targets() into the function
check_skill_level_change(). Also, update an inaccurate comment about
this function.

crawl-ref/source/player.cc
@@ -2912,7 +2912,7 @@ void level_change(bool skip_attribute_increase)
                     // do_level_up = false) but save the old values; then when
                     // we want the messages later, we restore the old skill
                     // levels and call check_skill_level_change() again, this
-                    // time passing do_update = true.
+                    // time passing do_level_up = true.
 
                     uint8_t saved_skills[NUM_SKILLS];
                     for (skill_type sk = SK_FIRST_SKILL; sk < NUM_SKILLS; ++sk)

crawl-ref/source/skills.cc
@@ -399,6 +399,8 @@ void check_skill_level_change(skill_type sk, bool do_level_up)
         else
             you.skills[sk] = new_level;
     }
+
+    check_training_targets();
 }
 
 // Fill a queue in random order with the values of the array.

commit 5a03d68f59656b7a87f1a9b57388edfddc017b24

Ensure train targets turn off once reached with !experience (#11993)
This bug was caused by commit ece4408. I have tested the bug fixed by
ece4408 (#11943) to ensure this commit did not cause a regression.

crawl-ref/source/player.cc
@@ -2882,6 +2882,11 @@ void level_change(bool skip_attribute_increase)
                         check_skill_level_change(sk);
                     }
 
+                    // It's possible we passed a training target due to
+                    // skills being rescaled to new aptitudes. Thus, we must
+                    // check the training targets.
+                    check_training_targets();
+
                     // Tell the player about their new species
                     for (auto &mut : fake_mutations(you.species, false))
                         mprf(MSGCH_INTRINSIC_GAIN, "%s", mut.c_str());

crawl-ref/source/skills.cc
@@ -399,8 +399,6 @@ void check_skill_level_change(skill_type sk, bool do_level_up)
         else
             you.skills[sk] = new_level;
     }
-
-    check_training_targets();
 }
 
 // Fill a queue in random order with the values of the array.