c172519d7723b142d40dde890059127c762a2cff
advil <rawlins@gmail.com>
2019-08-11 11:26:06 -0400

    Fix some non-working code in `your_talents`
    I'm still not sure under what natural circumstances this code can get
    triggered, but it seems to be happening in this megazig character:
    
        http://crawl.akrasiac.org/rawdata/malcoda/crash-malcoda-20190811-143529.txt
    
    This bit of code for doing backup hotkey assignment for abilities has
    apparently never worked, but it also is hard to get to, so no one has
    noticed before.
    
    (cherry picked from commit 2d01d4ab0a7cd92fbdf502c7aa80bf80fc4145f9)

--- crawl-ref/source/ability.cc
+++ crawl-ref/source/ability.cc
@@ -3534,7 +3534,7 @@ vector<talent> your_talents(bool check_confused, bool include_unusable)
         }
 
         // Try to find a free hotkey for i, starting from Z.
-        for (int k = 51; k >= 0; ++k)
+        for (int k = 51; k >= 0; --k)
         {
             const int kkey = index_to_letter(k);
             bool good_key = true;
@@ -3549,7 +3549,7 @@ vector<talent> your_talents(bool check_confused, bool include_unusable)
 
             if (good_key)
             {
-                tal.hotkey = k;
+                tal.hotkey = kkey;
                 you.ability_letter_table[k] = tal.which;
                 break;
             }
