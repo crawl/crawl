crawl/crawl

commit 80d7c3d5c14ee4473920a087932203a28c2f47fe

Fix ungenerated branches in wizmode + pregen (12071)
These were falling through the cracks of _branch_pregenerates, but not
generating as part of the generation order. This commit just moves you
directly to them without any pregeneration (which needless to say will
break the seed, but if you're doing this you probably don't care).

crawl-ref/source/files.cc
@@ -1580,6 +1580,8 @@ static bool _branch_pregenerates(branch_type b)
 {
     if (!you.deterministic_levelgen)
         return false;
+    if (!brentry[b].is_valid() && is_random_subbranch(b))
+        return false;
     return count(branch_generation_order.begin(),
         branch_generation_order.end(), b) > 0;
 }
@@ -1597,7 +1599,10 @@ static bool _branch_pregenerates(branch_type b)
 */
 bool pregen_dungeon(const level_id &stopping_point)
 {
-    if (stopping_point.is_valid())
+    // TODO: the is_valid() check here doesn't look quite right to me, but so
+    // far I can't get it to break anything...
+    if (stopping_point.is_valid()
+        || is_random_subbranch(stopping_point.branch) && you.wizard)
     {
         if (you.save->has_chunk(stopping_point.describe()))
             return false;

commit b6411803c1cc08ac3c05ddab2346746a002484da

Fix a crash with full pregen
This was introduced in 80d7c3d5c

crawl-ref/source/files.cc
@@ -1580,7 +1580,7 @@ static bool _branch_pregenerates(branch_type b)
 {
     if (!you.deterministic_levelgen)
         return false;
-    if (!brentry[b].is_valid() && is_random_subbranch(b))
+    if (b == NUM_BRANCHES || !brentry[b].is_valid() && is_random_subbranch(b))
         return false;
     return count(branch_generation_order.begin(),
         branch_generation_order.end(), b) > 0;
@@ -1602,7 +1602,8 @@ bool pregen_dungeon(const level_id &stopping_point)
     // TODO: the is_valid() check here doesn't look quite right to me, but so
     // far I can't get it to break anything...
     if (stopping_point.is_valid()
-        || is_random_subbranch(stopping_point.branch) && you.wizard)
+        || stopping_point.branch != NUM_BRANCHES &&
+           is_random_subbranch(stopping_point.branch) && you.wizard)
     {
         if (you.save->has_chunk(stopping_point.describe()))
             return false;