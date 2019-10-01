3348362083ca7e9968c8929f581d998b843b2a2e
Richard Wardin <shalmezad@gmail.com>
2019-10-01 19:30:23 -0400

    Fix being able to pick out barbs while incapacitated (12065)
    Carefully pulling out barbs now requires that
    you can move (not paralyzed or petrified), aren't confused,
    and aren't asleep.
    
    [Committer's note: Closes #1162. Tweaked berserk message.]

--- crawl-ref/source/player-reacts.cc
+++ crawl-ref/source/player-reacts.cc
@@ -829,8 +829,15 @@ static void _decrement_durations()
         doom_howl(min(delay, you.duration[DUR_DOOM_HOWL]));
 
     dec_elixir_player(delay);
-    extract_manticore_spikes("You carefully extract the barbed spikes from "
-                             "your body.");
+
+    if (!you.cannot_move()
+        && !you.confused()
+        && !you.asleep())
+    {
+        extract_manticore_spikes(
+            make_stringf("You %s the barbed spikes from your body.",
+                you.berserk() ? "rip and tear" : "carefully extract").c_str());
+    }
 
     if (!env.sunlight.empty())
         process_sunlights();
