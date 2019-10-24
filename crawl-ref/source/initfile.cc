9c56bcba7025d672565a42afd8ec1305c020d4be
advil <rawlins@gmail.com>
2019-10-24 09:13:43 -0400

    Add vaults to stable version morgues
    We'll see if players think this is too spammy or too obscure, but for
    0.24.0 at least this will be helpful for testing any seed issues that
    come up.

--- crawl-ref/source/initfile.cc
+++ crawl-ref/source/initfile.cc
@@ -1156,10 +1156,8 @@ void game_options::reset_options()
     dump_order.clear();
     new_dump_fields("header,hiscore,stats,misc,inventory,"
                     "skills,spells,overview,mutations,messages,"
-                    "screenshot,monlist,kills,notes");
-    if (Version::ReleaseType == VER_ALPHA)
-        new_dump_fields("vaults");
-    new_dump_fields("skill_gains,action_counts");
+                    "screenshot,monlist,kills,notes,vaults",
+                    "skill_gains,action_counts");
     // Currently enabled by default for testing in trunk.
     if (Version::ReleaseType == VER_ALPHA)
         new_dump_fields("xp_by_level");
