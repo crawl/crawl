ce263d24b725d9bf8605765226b68618b794a676
Aidan Holm <aidanholm@gmail.com>
2019-11-14 12:09:59 +0800

    Add textbox paste from clipboard function

--- crawl-ref/source/ui.h
+++ crawl-ref/source/ui.h
@@ -1110,6 +1110,9 @@ protected:
         void killword();
         void kill_to_begin();
         void kill_to_end();
+#ifdef USE_TILE_LOCAL
+        void clipboard_paste();
+#endif
 
         bool is_wordchar(char32_t c);
 
