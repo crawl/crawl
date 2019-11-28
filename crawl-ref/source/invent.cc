b449462f3f8cce445758471141f6933ce014560f
Nikolai Lavsky <3328424+nlavsky@users.noreply.github.com>
2019-11-28 14:06:07 +0300

    Remove an inventory hotkey for corpses and skeletons
    Although the player cannot pickup carrion anymore, the pickup menu
    still displays a hotkey for it. The hotkey already does nothing
    because corpses and skeletons are stationary objects (62bc8482).

--- crawl-ref/source/invent.cc
+++ crawl-ref/source/invent.cc
@@ -275,9 +275,6 @@ void get_class_hotkeys(const int type, vector<char> &glyphs)
     case OBJ_MISCELLANY:
         glyphs.push_back('}');
         break;
-    case OBJ_CORPSES:
-        glyphs.push_back('&');
-        break;
     default:
         break;
     }
