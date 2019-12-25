5f8687bca5ed173a039bdfd0014589c418b5cf6d
Brendan Hickey <brendan.m.hickey@gmail.com>
2019-12-24 20:15:22 -0500

    Changing 'slaying song' to 'sleighing song'

--- crawl-ref/source/spl-selfench.cc
+++ crawl-ref/source/spl-selfench.cc
@@ -190,7 +190,7 @@ spret cast_song_of_slaying(int pow, bool fail)
     if (you.duration[DUR_SONG_OF_SLAYING])
         mpr("You start a new song!");
     else
-        mpr("You start singing a song of slaying.");
+        mpr("You start singing a sleighing song.");
 
     you.set_duration(DUR_SONG_OF_SLAYING, 20 + random2avg(pow, 2));
 
