d6c695c3ee9ea8ba20d3ace344cfc2ed7b9189f6
gammafunk <gammafunk@gmail.com>
2020-02-05 12:03:12 -0600

    Remove an unused variable (geekosaur)

--- crawl-ref/source/show.cc
+++ crawl-ref/source/show.cc
@@ -171,7 +171,6 @@ static void _update_feat_at(const coord_def &gp)
 
     // We want to give non-solid terrain and the icy walls themselves MAP_ICY
     // so we can properly recolor both.
-    const bool is_wall = feat_is_wall(feat);
     if (env.level_state & LSTATE_ICY_WALL
         && (is_icecovered(gp)
             || !feat_is_wall(feat) && count_adjacent_icy_walls(gp)))
