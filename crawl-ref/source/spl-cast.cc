6f036f9bbcaa87707f65dd1af5c9c5797aa42a6e
gammafunk <gammafunk@gmail.com>
2020-02-09 17:16:08 -0600

    Fix Dith's Shadow Mimic for Teleport Other (joemaro)
    Dithmenos currently shadow mimics Teleport Other unconditionally, like
    it does for other hexes. This creates a problem where a player trying to
    teleport (or remove teleport from) a monster has their work potentially
    undone by the shadow mimic if their initial cast was successful.
    
    This commit has Dith not shadow mimic if Teleport Other hits its
    intended target and successfully changes its teleportation state on the
    first cast. If the Teleport Other was unsuccessful, Dith still has the
    usual chance to shadow mimic the spell. Closes #1273.

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -1488,7 +1488,9 @@ spret your_spells(spell_type spell, int powc, bool allow_fail,
     if (you.props.exists("battlesphere") && allow_fail)
         aim_battlesphere(&you, spell, powc, beam);
 
-    const bool old_target = actor_at(beam.target);
+    const auto orig_target = monster_at(beam.target);
+    const bool self_target = you.pos() == beam.target;
+    const bool had_tele = orig_target && orig_target->has_ench(ENCH_TP);
 
     spret cast_result = _do_cast(spell, powc, spd, beam, god, fail);
 
@@ -1498,7 +1500,8 @@ spret your_spells(spell_type spell, int powc, bool allow_fail,
     {
         if (you.props.exists("battlesphere") && allow_fail)
             trigger_battlesphere(&you, beam);
-        actor* victim = actor_at(beam.target);
+
+        const auto victim = monster_at(beam.target);
         if (will_have_passive(passive_t::shadow_spells)
             && allow_fail
             && !god_hates_spell(spell, you.religion, !allow_fail)
@@ -1506,7 +1509,16 @@ spret your_spells(spell_type spell, int powc, bool allow_fail,
             && !(flags & spflag::neutral)
             && (beam.is_enchantment()
                 || battlesphere_can_mirror(spell))
-            && (!old_target || (victim && !victim->is_player())))
+            // Must have a target, but that can't be the player.
+            && !self_target
+            && orig_target
+            // For teleport other, only mimic if the spell hit who we
+            // originally targeted and if we failed to change the target's
+            // teleport status. This way the mimic won't just undo the effect
+            // of a successful cast.
+            && (spell != SPELL_TELEPORT_OTHER
+                || (orig_target == victim
+                    && had_tele == victim->has_ench(ENCH_TP))))
         {
             dithmenos_shadow_spell(&beam, spell);
         }
