b8697bfa384bdd50ff988f68ff05872d8e2e8290
Aidan Holm <aidanholm@gmail.com>
2020-02-13 21:31:55 +0800

    Disable lua bytecode loading
    (cherry picked from commit 768f60da87a3fa0b5561da5ade9309577c176d04)
    (cherry picked from commit 2791043dd8ecdf641e8b1b542d00dea30014df52)

--- crawl-ref/source/clua.cc
+++ crawl-ref/source/clua.cc
@@ -729,6 +729,20 @@ bool CLua::callfn(const char *fn, int nargs, int nret)
     return !err;
 }
 
+static int lua_loadstring(lua_State *ls)
+{
+    const auto lua = luaL_checkstring(ls, 1);
+    if (lua[0] == 0x1b)
+        abort();
+    lua_settop(ls, 0);
+    if (luaL_loadstring(ls, lua))
+    {
+        lua_pushnil(ls);
+        lua_insert(ls, 1);
+    }
+    return lua_gettop(ls);
+}
+
 void CLua::init_lua()
 {
     if (_state)
@@ -784,6 +798,9 @@ void CLua::init_lua()
         lua_pushstring(_state, l.first.c_str());
         lua_call(_state, 1, 0);
     }
+
+    lua_pushcfunction(_state, lua_loadstring);
+    lua_setglobal(_state, "loadstring");
 
     // Open Crawl bindings
     cluaopen_kills(_state);
