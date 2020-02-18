crawl/crawl

commit 4dc3416460316b005cbaab37228a816d8c1e0b38

Fix an incorrect message (#10516).

crawl-ref/source/player-equip.cc
@@ -831,7 +831,7 @@ static void _equip_armour_effect(item_def& arm, bool unmeld,
                     float_player();
                 }
             }
-            if (!unmeld)
+            if (!unmeld && !player_mutation_level(MUT_NO_ARTIFICE))
             {
                 if (player_mutation_level(MUT_NO_ARTIFICE))
                     mpr("Take it off to stop flying.");

commit 11ec75b3b8d2927dff4afa90cbc74c6c53f6fe97

Fix a flight message not displaying
Appears to have been broken by 4dc341646031.

crawl-ref/source/player-equip.cc
@@ -853,16 +853,19 @@ static void _equip_armour_effect(item_def& arm, bool unmeld,
                     float_player();
                 }
             }
-            if (!unmeld && !you.has_mutation(MUT_NO_ARTIFICE))
+            if (!unmeld)
             {
                 if (you.has_mutation(MUT_NO_ARTIFICE))
-                    mpr("Take it off to stop flying.");
+                {
+                    mprf("Take your %s off to stop flying.",
+                         arm.name(DESC_BASENAME).c_str());
+                }
                 else
                 {
-                mprf("(use the <w>%s</w>bility menu to %s flying)",
-                     command_to_string(CMD_USE_ABILITY).c_str(),
-                     you.attribute[ATTR_LAST_FLIGHT_STATUS]
-                         ? "stop or start" : "start or stop");
+                    mprf("(use the <w>%s</w>bility menu to %s flying)",
+                         command_to_string(CMD_USE_ABILITY).c_str(),
+                         you.attribute[ATTR_LAST_FLIGHT_STATUS]
+                             ? "stop or start" : "start or stop");
                 }
             }
 