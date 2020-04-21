crawl/crawl

commit 00990ba7645445f3baeb5d67a05f98950c96f666

Fix compilation with TAG_MAJOR_VERSION 35
Several latent issues have crept into the codebase over time, this
commit fixes all of them in C++ files.

There are additional changes that have to be made to text files, see
util/tag-35-upgrade.py for a solution for those.

crawl-ref/source/describe.cc
@@ -2140,29 +2140,6 @@ string get_item_description(const item_def &item, bool verbose,
         }
         break;
 
-    case OBJ_POTIONS:
-#ifdef DEBUG_BLOOD_POTIONS
-        // List content of timer vector for blood potions.
-        if (!dump && is_blood_potion(item))
-        {
-            item_def stack = static_cast<item_def>(item);
-            CrawlHashTable &props = stack.props;
-            if (!props.exists("timer"))
-                description << "\nTimers not yet initialized.";
-            else
-            {
-                CrawlVector &timer = props["timer"].get_vector();
-                ASSERT(!timer.empty());
-
-                description << "\nQuantity: " << stack.quantity
-                            << "        Timer size: " << (int) timer.size();
-                description << "\nTimers:\n";
-                for (const CrawlStoreValue& store : timer)
-                    description << store.get_int() << "  ";
-            }
-        }
-#endif
-
     case OBJ_SCROLLS:
     case OBJ_ORBS:
     case OBJ_GOLD:

crawl-ref/source/describe-spells.cc
@@ -656,8 +656,12 @@ static void _write_book(const spellbook_contents &book,
                         make_stringf("%d%%", hex_chance(spell, hd)));
         }
 
+#if TAG_MAJOR_VERSION == 34
         string schools = (source_item && source_item->base_type == OBJ_RODS) ?
                 "Evocations" : _spell_schools(spell);
+#else
+        string schools = _spell_schools(spell);
+#endif
         tiles.json_write_string("schools", schools);
         tiles.json_write_int("level", spell_difficulty(spell));
         tiles.json_close_object();

crawl-ref/source/enchant-type.h
@@ -154,8 +154,8 @@ enum enchant_type
     ENCH_GOLD_LUST,
     ENCH_DRAINED,
     ENCH_REPEL_MISSILES,
-    ENCH_DEFLECT_MISSILES,
 #if TAG_MAJOR_VERSION == 34
+    ENCH_DEFLECT_MISSILES,
     ENCH_NEGATIVE_VULN,
     ENCH_CONDENSATION_SHIELD,
 #endif

crawl-ref/source/feature-data.h
@@ -263,7 +263,9 @@ static feature_def feat_defs[] =
     FFT_TRAP, MF_TRAP,\
 }
 
+#if TAG_MAJOR_VERSION == 34
 TRAP(DNGN_TRAP_MECHANICAL, "mechanical trap", "trap_mechanical", LIGHTCYAN),
+#endif
 TRAP(DNGN_TRAP_ARROW, "arrow trap", "trap_mechanical", LIGHTCYAN),
 TRAP(DNGN_TRAP_SPEAR, "spear trap", "trap_mechanical", LIGHTCYAN),
 TRAP(DNGN_TRAP_BLADE, "blade trap", "trap_mechanical", LIGHTCYAN),

crawl-ref/source/god-item.cc
@@ -159,8 +159,10 @@ bool is_evil_item(const item_def& item, bool calc_unid)
 
     switch (item.base_type)
     {
+#if TAG_MAJOR_VERSION == 34
     case OBJ_POTIONS:
         return is_blood_potion(item);
+#endif
     case OBJ_SCROLLS:
         return item.sub_type == SCR_TORMENT;
     case OBJ_STAVES:

crawl-ref/source/item-name.cc
@@ -3353,12 +3353,14 @@ string item_prefix(const item_def &item, bool temp)
         break;
 
     case OBJ_POTIONS:
+#if TAG_MAJOR_VERSION == 34
         if (is_good_god(you.religion) && item_type_known(item)
             && is_blood_potion(item))
         {
             prefixes.push_back("evil_eating");
             prefixes.push_back("forbidden");
         }
+#endif
         if (is_preferred_food(item))
         {
             prefixes.push_back("preferred");

crawl-ref/source/item-prop-enum.h
@@ -477,8 +477,8 @@ enum special_missile_type // to separate from weapons in general {dlb}
     SPMSL_PENETRATION,
 #endif
     SPMSL_DISPERSAL,
+    SPMSL_EXPLODING,                   // Only used by Damnation crossbow
 #if TAG_MAJOR_VERSION == 34
-    SPMSL_EXPLODING,
     SPMSL_STEEL,
 #endif
     SPMSL_SILVER,
@@ -489,8 +489,8 @@ enum special_missile_type // to separate from weapons in general {dlb}
     SPMSL_CONFUSION,
     SPMSL_SICKNESS,
 #endif
-    SPMSL_FRENZY,
-    SPMSL_BLINDING,
+    SPMSL_FRENZY,                      // Datura
+    SPMSL_BLINDING,                    // Atropa
     NUM_REAL_SPECIAL_MISSILES,
     NUM_SPECIAL_MISSILES = NUM_REAL_SPECIAL_MISSILES,
 };

crawl-ref/source/items.cc
@@ -3959,9 +3959,9 @@ colour_t item_def::miscellany_colour() const
 #endif
         case MISC_HORN_OF_GERYON:
             return LIGHTRED;
+#if TAG_MAJOR_VERSION == 34
         case MISC_LAMP_OF_FIRE:
             return YELLOW;
-#if TAG_MAJOR_VERSION == 34
         case MISC_SACK_OF_SPIDERS:
             return WHITE;
         case MISC_BUGGY_LANTERN_OF_SHADOWS:
@@ -4438,6 +4438,7 @@ bool get_item_by_name(item_def *item, const char* specs,
 
     case OBJ_POTIONS:
         item->quantity = 12;
+#if TAG_MAJOR_VERSION == 34
         if (is_blood_potion(*item))
         {
             const char* prompt;
@@ -4449,6 +4450,7 @@ bool get_item_by_name(item_def *item, const char* specs,
                 age = -1;
             init_perishable_stack(*item, age);
         }
+#endif
         break;
 
     case OBJ_FOOD:

commit 55863b0b92b119d746b382dc43bcb77421228a59

Don't crash when trying to describe potions
Broken in 00990ba764544.

crawl-ref/source/describe.cc
@@ -2140,6 +2140,7 @@ string get_item_description(const item_def &item, bool verbose,
         }
         break;
 
+    case OBJ_POTIONS:
     case OBJ_SCROLLS:
     case OBJ_ORBS:
     case OBJ_GOLD: