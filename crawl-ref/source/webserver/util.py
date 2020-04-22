65e1c1da0b104da44e27397c12e35c2a856e8041
Alex Jurkiewicz <alex@jurkiewi.cz>
2020-04-23 07:54:40 +1000

    Fix lint errors

--- crawl-ref/source/webserver/util.py
+++ crawl-ref/source/webserver/util.py
@@ -121,14 +121,14 @@ def send_email(to_address, subject, body_plaintext, body_html):
     if not to_address:
         return
 
-    logging.info("Sending email to '%s' with subject '%s'" %
-                                                (to_address, subject))
+    logging.info("Sending email to '%s' with subject '%s'", to_address, subject)
     connected = False
     try:
         # establish connection
         # TODO: this should not be a blocking call at all...
         if config.smtp_use_ssl:
-            email_server = smtplib.SMTP_SSL(config.smtp_host, config.smtp_port)  # type: smtplib.SMTP
+            email_server = smtplib.SMTP_SSL(
+                config.smtp_host, config.smtp_port)  # type: smtplib.SMTP
         else:
             email_server = smtplib.SMTP(config.smtp_host, config.smtp_port)
         connected = True
@@ -153,16 +153,23 @@ def send_email(to_address, subject, body_plaintext, body_html):
         email_server.sendmail(config.smtp_from_addr, to_address, msg.as_string())
     finally:
         # end connection
-        if connected: email_server.quit()
+        if connected:
+            email_server.quit()
+
 
 def validate_email_address(address):  # type: (str) -> Optional[str]
     # Returns an error string describing the problem, or None.
     # NOTE: a blank email address validates successfully.
-    if not address: return None
-    if " " in address: return "Email address can't contain a space"
-    if "@" not in address: return "Expected email address to contain the @ symbol"
-    if not re.match(r"[^@]+@[^@]+\.[^@]+", address): return "Invalid email address"
-    if len(address) >= 80: return "Email address can't be more than 80 characters"
+    if not address:
+        return None
+    if " " in address:
+        return "Email address can't contain a space"
+    if "@" not in address:
+        return "Expected email address to contain the @ symbol"
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", address):
+        return "Invalid email address"
+    if len(address) >= 80:
+        return "Email address can't be more than 80 characters"
     return None
 
 
