e12560f0e4c8723b0c35cb19510fe3a9381f44c4
kimjoy2002 <kimjoy2002@naver.com>
2020-05-01 21:24:01 +0900

    Fixed missing HE aptitudes by #70

--- crawl-ref/source/util/gen-apt.pl
+++ crawl-ref/source/util/gen-apt.pl
@@ -125,7 +125,6 @@ sub aptitude_table
     for my $sp (sort_species(@SPECIES))
     {
         next if $sp eq 'Mottled Draconian';
-        next if $sp eq 'High Elf';
         next if $sp eq 'Sludge Elf';
 
         my $line = '';
