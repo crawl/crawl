31286d223f32c27ea68df9e2e22875642b777f46
Aidan Holm <aidanholm@gmail.com>
2020-05-11 19:20:17 +0800

    Fix unordered_map<> on Mac OS

--- crawl-ref/source/xp-evoker-data.h
+++ crawl-ref/source/xp-evoker-data.h
@@ -15,7 +15,7 @@ struct evoker_data
     int max_charges;
 };
 
-static const unordered_map<misc_item_type, evoker_data> xp_evoker_data = {
+static const unordered_map<misc_item_type, evoker_data, std::hash<int>> xp_evoker_data = {
     { MISC_PHIAL_OF_FLOODS, { "phial_debt", 10, 1 } },
     { MISC_HORN_OF_GERYON, { "horn_debt", 10, 1 } },
     { MISC_LIGHTNING_ROD,  { "rod_debt", 3, LIGHTNING_MAX_CHARGE } },
