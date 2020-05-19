93ebf739aaaf216c03a055e3406440e6d5f26ee3
Alex Jurkiewicz <alex@jurkiewi.cz>
2020-05-20 08:51:48 +1000

    Require upper-case Y to forget Vehumet gifts
    Resolves #1418

--- crawl-ref/source/spl-selfench.cc
+++ crawl-ref/source/spl-selfench.cc
@@ -128,6 +128,7 @@ int cast_selective_amnesia(const string &pre_msg)
         spell = get_spell_by_letter(keyin);
         slot = get_spell_slot_by_letter(keyin);
 
+        const bool in_library = you.spell_library[spell];
         if (spell != SPELL_NO_SPELL)
         {
             const string prompt = make_stringf(
@@ -135,10 +136,9 @@ int cast_selective_amnesia(const string &pre_msg)
                     spell_title(spell), spell_levels_required(spell),
                     spell_levels_required(spell) != 1 ? "s" : "",
                     player_spell_levels() + spell_levels_required(spell),
-                    you.spell_library[spell] ? "" :
-                    " This spell is not in your library!");
+                    in_library ? "" : " This spell is not in your library!");
 
-            if (yesno(prompt.c_str(), true, 'n', false))
+            if (yesno(prompt.c_str(), in_library, 'n', false))
             {
                 if (!pre_msg.empty())
                     mpr(pre_msg);
