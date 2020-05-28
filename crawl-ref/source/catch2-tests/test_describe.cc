ece1f79024fc4c768aaf256f26565eb755eb60f1
Elliot Dronebarger <edon22@gmail.com>
2020-05-28 13:19:25 -0500

    The linter always gets me

--- crawl-ref/source/catch2-tests/test_describe.cc
+++ crawl-ref/source/catch2-tests/test_describe.cc
@@ -10,24 +10,6 @@
 
 #include "monster-type.h"
 
-
-/**TEST_CASE("mons_is_removed() returns correct values", "[single-file]" ) {
-
-    init_monsters();
-
-    SECTION ("mons_is_removed() returns true for removed monster") {
-        bool removed = mons_is_removed(MONS_BUMBLEBEE);
-
-        REQUIRE(removed == true);
-    }
-
-    SECTION ("mons_is_removed() returns false for current monster") {
-        bool removed = mons_is_removed(MONS_BUTTERFLY);
-
-        REQUIRE(removed == false);
-    }
-}**/
-
 TEST_CASE("_describe_monster_habitat outputs correct descriptions", "[single-file") {
 
     init_monsters();
