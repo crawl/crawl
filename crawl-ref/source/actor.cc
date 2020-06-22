4a33e3d59c9a8bc6257bbdbb5c6bad1a320c2e61
Goratrix <goratrix@chello.sk>
2020-06-22 16:59:32 +0200

    Fix scarf of harm not working
    Commit 19c8d55 added harm to scarves, but used the 'wearing' function,
    which is only valid for getting jewellery subtypes. Need to use
    'wearing_ego' to get the scarf ego instead.
    
    (cherry picked from commit ea8168b82ca800ed68677786aa9987e05db3d308)

--- crawl-ref/source/actor.cc
+++ crawl-ref/source/actor.cc
@@ -280,7 +280,7 @@ bool actor::reflection(bool calc_unid, bool items) const
 bool actor::extra_harm(bool calc_unid, bool items) const
 {
     return items &&
-           (wearing(EQ_CLOAK, SPARM_HARM, calc_unid)
+           (wearing_ego(EQ_CLOAK, SPARM_HARM, calc_unid)
             || scan_artefacts(ARTP_HARM, calc_unid));
 }
 
