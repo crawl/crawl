ea324acf0aa6fba6c63fed895f2e1f22489b029e
Byrel Mitchell <brm@siteenhancementservices.com>
2020-06-30 10:05:41 -0400

    Fixed TSO stabbing comment
    TSO blocks all stabs, not just on intelligent monsters.

--- crawl-ref/source/fight.cc
+++ crawl-ref/source/fight.cc
@@ -346,7 +346,7 @@ stab_type find_stab_type(const actor *attacker,
 {
     const monster* def = defender.as_monster();
 
-    // Stabbing intelligent monsters is unchivalric, and disabled under TSO!
+    // Stabbing monsters is unchivalric, and disabled under TSO!
     // When just checking for display purposes, still indicate when monsters
     // are sleeping/paralysed etc.
     if (actual && attacker && attacker->is_player()
