6b68d4d4d6cf2975e206260a11cde1d0a02882bc
advil <rawlins@gmail.com>
2020-07-01 09:49:32 -0400

    Set cursor region on fakecursorxy
    This was apparently a major factor in "invalid" cursor positions.
    Basically, as far as I can tell, write_char_at leads to changes in
    wherex() and wherey(), even without a cgotoxy call, and so all the other
    checks will flag the results as invalid unless the region is updated.
    This also makes the region value consistent after cursorxy regardless of
    options.

--- crawl-ref/source/libconsole.h
+++ crawl-ref/source/libconsole.h
@@ -51,7 +51,8 @@ struct save_cursor_pos
     save_cursor_pos()
         : region(get_cursor_region()), pos(cgetpos(region))
     {
-        ASSERT(valid_cursor_pos(pos.x, pos.y, region));
+        ASSERTM(valid_cursor_pos(pos.x, pos.y, region),
+            "invalid cursor position %d,%d in region %d", pos.x, pos.y, region);
     };
     ~save_cursor_pos()
     {
