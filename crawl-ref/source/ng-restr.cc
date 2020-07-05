38d6b57793ffaeba302e8c55ef5a4dbb4ed08e9a
kimjoy2002 <kimjoy2002@naver.com>
2020-07-05 11:30:23 +0900

    fixed demigod melted knight issued by #186

--- crawl-ref/source/ng-restr.cc
+++ crawl-ref/source/ng-restr.cc
@@ -40,6 +40,7 @@ static bool _banned_combination(job_type job, species_type species)
         if (job == JOB_BERSERKER
             || job == JOB_CHAOS_KNIGHT
             || job == JOB_ABYSSAL_KNIGHT
+            || job == JOB_MELTED_KNIGHT
             || job == JOB_MONK)
         {
             return true;
