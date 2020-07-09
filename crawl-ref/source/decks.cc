daab700853c7e0e96a6439fc59d4f6c86a26012d
Nicholas Feinberg <pleasingfung@gmail.com>
2020-07-08 18:03:41 -0700

    Make Storm card electric (WiseOldWolf)
    Broken in 86364c1113e.

--- crawl-ref/source/decks.cc
+++ crawl-ref/source/decks.cc
@@ -1494,6 +1494,7 @@ static void _storm_card(int power)
     for (auto p : targets)
     {
         bolt beam;
+        beam.flavour           = BEAM_ELECTRICITY;
         beam.is_tracer         = false;
         beam.is_explosion      = true;
         beam.glyph             = dchar_glyph(DCHAR_FIRED_BURST);

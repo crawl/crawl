crawl/crawl

commit 86364c1113ef6329b67f8a7d428392bf0fcf43e9

Revise storm card (minmay, ebering)
Storm's old effect (wind blast + storm cloud ring + air elemental)
didn't fit in well with the rest of the deck of desctruction, and wasn't
that great anyway. Revise to be more purely about AoE electrical damage.

New effects are: wind blast plus up to nine electrical explosions
(depending on power).

The explosion targets are chosen randomly at a distance so that they
don't damage the player, firing only one explosion per cell. As a
result, using this card in a corridor or near a corner will likely not
hit the intended target---particularly at low power.

In terms of balance, the explosions do slightly less damage than power
zero conjure ball lightnings at low power, scaling to be comprable to
high power CBL at high power. This isn't too out of line with the other
destruction cards, considering the terrain limitations and noise; but
will need some playtesting feedback to be certain.

[Committer's notes: re-wrote the patch to use random sampling of targets
 based on a radius iterator; adjusted damage formulas; added flavour
 messaging. Closes #942]

crawl-ref/source/decks.cc
@@ -11,12 +11,14 @@
 #include <iostream>
 #include <iterator>
 #include <sstream>
+#include <unordered_set>
 
 #include "ability.h"
 #include "abyss.h"
 #include "act-iter.h"
 #include "artefact.h"
 #include "attitude-change.h"
+#include "beam.h"
 #include "cloud.h"
 #include "coordit.h"
 #include "dactions.h"
@@ -75,6 +77,7 @@
 #include "uncancel.h"
 #include "unicode.h"
 #include "view.h"
+#include "viewchar.h"
 #include "xom.h"
 
 using namespace ui;
@@ -94,11 +97,11 @@ deck_archetype deck_of_escape =
 deck_archetype deck_of_destruction =
 {
     { CARD_VITRIOL,    5 },
-    { CARD_STORM,      5 },
     { CARD_PAIN,       5 },
     { CARD_ORB,        5 },
     { CARD_DEGEN,      3 },
     { CARD_WILD_MAGIC, 5 },
+    { CARD_STORM,      5 },
 };
 
 deck_archetype deck_of_summoning =
@@ -1536,29 +1539,69 @@ static void _storm_card(int power)
 {
     const int power_level = _get_power_level(power);
 
-    _friendly(MONS_AIR_ELEMENTAL, 3);
+    wind_blast(&you, (power_level + 1) * 66, coord_def(), true);
+    redraw_screen(); // Update monster positions
 
-    wind_blast(&you, (power_level == 0) ? 100 : 200, coord_def(), true);
-
-    for (radius_iterator ri(you.pos(), 4, C_SQUARE, LOS_SOLID); ri; ++ri)
+    // 1-3, 4-6, 7-9
+    const int max_explosions = random_range((power_level * 3) + 1, (power_level + 1) * 3);
+    // Select targets based on simultaneously running max_explosions resivoir
+    // samples from the radius iterator over valid targets.
+    //
+    // Once the possible targets are drawn, the result is deduplicated into a
+    // set of targets.
+    vector<coord_def> target_draws (12, you.pos());
+    int valid_targets = 0;
+    for (radius_iterator ri(you.pos(), LOS_NO_TRANS, true); ri; ++ri)
     {
-        monster *mons = monster_at(*ri);
-
-        if (adjacent(*ri, you.pos()))
-            continue;
-
-        if (mons && mons->wont_attack())
-            continue;
-
-        if ((feat_has_solid_floor(grd(*ri))
-             || grd(*ri) == DNGN_DEEP_WATER)
-            && !cloud_at(*ri))
+        if (grid_distance(*ri, you.pos()) > 3 && !cell_is_solid(*ri))
         {
-            place_cloud(CLOUD_STORM, *ri,
-                        5 + (power_level + 1) * random2(10), & you);
+            ++valid_targets;
+            for (int i = 0; i < max_explosions; ++i)
+            {
+                if (one_chance_in(valid_targets))
+                    target_draws[i] = *ri;
+            }
         }
     }
 
+    unordered_set<coord_def> targets (target_draws.begin(), target_draws.end());
+    targets.erase(you.pos());
+
+    bool heard = false;
+    for (auto p : targets)
+    {
+        bolt beam;
+        beam.is_tracer         = false;
+        beam.is_explosion      = true;
+        beam.glyph             = dchar_glyph(DCHAR_FIRED_BURST);
+        beam.name              = "electrical discharge";
+        beam.aux_source        = "the storm";
+        beam.explode_noise_msg = "You hear a clap of thunder!";
+        beam.real_flavour      = beam.flavour;
+        beam.colour            = LIGHTCYAN;
+        beam.source_id         = MID_PLAYER;
+        beam.thrower           = KILL_YOU;
+        beam.is_explosion      = true;
+        beam.ex_size           = 3;
+        beam.damage            = dice_def(3, 9 + 9 * power_level);
+        beam.source = p;
+        beam.target = p;
+        beam.explode();
+        heard = heard || player_can_hear(p);
+    }
+    // Lots of loud bangs, even if everything is silenced get a message.
+    // Thunder comes after the animation runs.
+    if (targets.size() > 0)
+    {
+        vector<string> thunder_adjectives = { "mighty",
+                                              "violent",
+                                              "cataclysmic" };
+        mprf("You %s %s%s peal%s of thunder!",
+              heard ? "hear" : "feel",
+              targets.size() > 1 ? "" : "a ",
+              thunder_adjectives[power_level].c_str(),
+              targets.size() > 1 ? "s" : "");
+    }
 }
 
 static void _illusion_card(int power)
@@ -1673,6 +1716,8 @@ static void _torment_card()
 
 // Punishment cards don't have their power adjusted depending on Nemelex piety,
 // and are based on experience level instead of invocations skill.
+// Max power = 200 * (2700+2500) / 2700 + 243 + 300 = 928
+// Min power = 1 * 2501 / 2700 + 1 + 0 = 2
 static int _card_power(bool punishment)
 {
     if (punishment)

commit daab700853c7e0e96a6439fc59d4f6c86a26012d

Make Storm card electric (WiseOldWolf)
Broken in 86364c1113e.

crawl-ref/source/decks.cc
@@ -1494,6 +1494,7 @@ static void _storm_card(int power)
     for (auto p : targets)
     {
         bolt beam;
+        beam.flavour           = BEAM_ELECTRICITY;
         beam.is_tracer         = false;
         beam.is_explosion      = true;
         beam.glyph             = dchar_glyph(DCHAR_FIRED_BURST);