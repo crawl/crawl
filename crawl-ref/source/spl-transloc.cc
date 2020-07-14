6162d163db6c0b05cfab622accee6f206a9fd3fe
Nicholas Feinberg <pleasingfung@gmail.com>
2020-07-14 08:41:40 -0700

    Make charge sometimes trigger SW
    It still seems to work poorly at longer ranges, but I'm not sure
    why exactly.

--- crawl-ref/source/spl-transloc.cc
+++ crawl-ref/source/spl-transloc.cc
@@ -38,6 +38,7 @@
 #include "prompt.h"
 #include "religion.h"
 #include "shout.h"
+#include "spl-summoning.h" // trigger_spectral_weapon for palentonga charge
 #include "spl-util.h"
 #include "stash.h"
 #include "state.h"
@@ -578,6 +579,8 @@ spret palentonga_charge(bool fail)
     melee_attack charge_atk(&you, target_mons);
     charge_atk.roll_dist = grid_distance(initial_pos, you.pos());
     charge_atk.attack();
+    if (you.props.exists("spectral_weapon"))
+        trigger_spectral_weapon(&you, target_mons);
 
     // Normally this is 10 aut (times haste, etc), but slow weapons
     // take longer. Most relevant for low-skill players and Dark Maul.
