crawl/crawl

commit cef5c758032e6977ec2480526abbb29aad841a72

Insert a cap in spell failure calculations
It's possible to cause an overflow in the spell failure calculations with
extreme encumbrance values, achieved by having 0 or 1 strength and wearing
CPA. This commit sets the cap to a value slightly lower than where the
overflow should theoretically occur to avoid this.

This should be considered a temporary solution until something more elegant
is done with the spell failure calculations.

crawl-ref/source/spl-cast.cc
@@ -338,6 +338,13 @@ int raw_spell_fail(spell_type spell)
     const int spell_level = spell_difficulty(spell);
     ASSERT_RANGE(spell_level, 0, (int) ARRAYSZ(difficulty_by_level));
     chance += difficulty_by_level[spell_level]; // between 0 and 330
+    
+    // since chance is passed through a 3rd degree polynomial, cap the
+    // value to avoid any overflow issues. The value that causes an
+    // overflow seems to be slightly higher than 1400, so the cap will
+    // be set there. This value is only reachable through gameplay by
+    // extreme scenarios, e.g. a 1 str character wearing CPA.
+    chance = min(chance, 1400);
 
     // This polynomial is a smoother approximation of a breakpoint-based
     // calculation that originates pre-DCSS, mapping `chance` at this point to

commit 20a70addb10e953c393e1a4e6057ead572b74c8c

Lower the spell fail polynomial cap
This was introduced in cef5c758032e to deal with potential overflows in
low strength / heavy armour scenarios. However, this commit didn't take
into account the fact that `chance2` is signed, so while I'm not sure if
an overflow was possible in practical scenarios, it was possible in
principle. (CanOfWorms and I discussed this at the time but it looks
like neither of us ever made the commit.) This commit lowers this cap
substantially so that this polynomial is always dealing with pretty sane
values.

Resolves #1420

crawl-ref/source/spl-cast.cc
@@ -338,11 +338,11 @@ int raw_spell_fail(spell_type spell)
     chance += difficulty_by_level[spell_level]; // between 0 and 330
 
     // since chance is passed through a 3rd degree polynomial, cap the
-    // value to avoid any overflow issues. The value that causes an
-    // overflow seems to be slightly higher than 1400, so the cap will
-    // be set there. This value is only reachable through gameplay by
-    // extreme scenarios, e.g. a 1 str character wearing CPA.
-    chance = min(chance, 1400);
+    // value to avoid any overflow issues. We choose 210 by solving for chance2
+    // = 200 in the polynomial -- it gets capped at 100 ultimately, but we
+    // need a bunch of headroom in case some later calculations lower the value
+    // below 100 after this.
+    chance = min(chance, 210);
 
     // This polynomial is a smoother approximation of a breakpoint-based
     // calculation that originates pre-DCSS, mapping `chance` at this point to