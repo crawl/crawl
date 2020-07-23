9bf6f1401de0176e0e695ad85b3e9fc7e42da3ab
Edgar A. Bering IV <trizor@gmail.com>
2020-07-23 09:16:54 -0400

    Remove some vestigal eating options

--- crawl-ref/source/initfile.cc
+++ crawl-ref/source/initfile.cc
@@ -164,8 +164,6 @@ const vector<GameOption*> game_options::build_options_list()
         new BoolGameOption(SIMPLE_NAME(easy_door), true),
         new BoolGameOption(SIMPLE_NAME(warn_hatches), false),
         new BoolGameOption(SIMPLE_NAME(enable_recast_spell), true),
-        new BoolGameOption(SIMPLE_NAME(auto_butcher), true),
-        new BoolGameOption(SIMPLE_NAME(auto_eat), true),
         new BoolGameOption(SIMPLE_NAME(auto_hide_spells), false),
         new BoolGameOption(SIMPLE_NAME(blink_brightens_background), false),
         new BoolGameOption(SIMPLE_NAME(bold_brightens_foreground), false),
