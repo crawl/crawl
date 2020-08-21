ec200485ca3436d404326c2d2175ebdacda11c67
wheals <whealswithinwheals@gmail.com>
2020-08-21 17:13:18 +0300

    Let Palentongas get poisonous tails.

--- crawl-ref/source/mutation.cc
+++ crawl-ref/source/mutation.cc
@@ -1247,9 +1247,9 @@ bool physiology_mutation_conflict(mutation_type mutat)
     if (_is_covering(mutat) && _body_covered() >= 3)
         return true;
 
-    // Only Nagas and Draconians can get this one.
+    // Only species that already have tails can get this one.
     if (you.species != SP_NAGA && !species_is_draconian(you.species)
-        && mutat == MUT_STINGER)
+        && you.species != SP_PALENTONGA && mutat == MUT_STINGER)
     {
         return true;
     }
