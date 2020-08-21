afce7a58c0dece93c5e302ec653eee14ae9f374a
wheals <whealswithinwheals@gmail.com>
2020-08-21 12:47:49 +0300

    Hide Charms aptitude on ?%.

--- crawl-ref/source/util/gen-apt.pl
+++ crawl-ref/source/util/gen-apt.pl
@@ -241,6 +241,7 @@ sub load_aptitudes
                 my $skill = skill_name($2);
                 next if $skill eq "Stabbing";
                 next if $skill eq "Traps";
+                next if $skill eq "Charms";
                 die "$skillfile:$.: Unknown skill: $skill\n"
                     unless $SKILL_ABBR{$skill};
                 die "$skillfile:$.: Repeated skill def $1 for $species.\n"
