crawl/crawl

commit 4531babe98175c10995a28d2272e43e1d1ae8af4

Add glyph mode customization options for webtiles
This commit adds two options `glyph_mode_font` and
`glyph_mode_font_size` that allow customization of the font and its size
used to render the dungeon view in the webtiles glyph mode. If set to
monospace (default) or the font is not available, the browser's default
monospaced font will be used.

While implementing these options, this commit also follows the glyph
metrics exactly as prescribed by the font. With the change, the dungeon
view of the console version and the webtiles glyph mode should be more
similar and the box-drawing characters will no longer appears to be
unconnected, provided that the monospaced font used by the browser is
properly authored in the first place.

[ Committer's note: Tweaked, squashed, and rebased. Closes #1512 ]

crawl-ref/source/webserver/game_data/static/cell_renderer.js
@@ -7,12 +7,7 @@ function ($, view_data, main, tileinfo_player, icons, dngn, enums,
 
     function DungeonCellRenderer()
     {
-        // see also, renderer_settings in game.js. In fact, do these values
-        // do anything?
-        var ratio = window.devicePixelRatio;
         this.set_cell_size(32, 32);
-        this.glyph_mode_font_size = 24 * ratio;
-        this.glyph_mode_font = "monospace";
     }
 
     var fg_term_colours, bg_term_colours;
@@ -109,6 +104,59 @@ function ($, view_data, main, tileinfo_player, icons, dngn, enums,
 
             return (Math.floor(this.glyph_mode_font_size * glyph_scale / 100)
                 + "px " + this.glyph_mode_font);
+        },
+
+        glyph_mode_update_font_metrics: function ()
+        {
+            this.ctx.font = this.glyph_mode_font_name();
+
+            // Glyph used here does not matter because fontBoundingBoxAscent
+            // and fontBoundingBoxDescent are specific to the font whereas all
+            // glyphs in a monospaced font will have the same width
+            var metrics = this.ctx.measureText('@');
+            this.glyph_mode_font_width = metrics.width;
+
+            // Currently, fontBoundingBoxAscent/Descent are still
+            // experimental for most web browsers and may be unavailable.
+            if (metrics.fontBoundingBoxAscent)
+            {
+                this.glyph_mode_baseline = metrics.fontBoundingBoxAscent;
+                this.glyph_mode_line_height = metrics.fontBoundingBoxAscent
+                                            + metrics.fontBoundingBoxDescent;
+            }
+            else
+            {   // Inspired by https://stackoverflow.com/q/1134586/
+                var body = document.body;
+                var ref_glyph = document.createElement("span");
+                var ref_block = document.createElement("div");
+                var div = document.createElement("div");
+
+                ref_glyph.innerHTML = '@';
+                ref_glyph.style.font = this.ctx.font;
+
+                ref_block.style.display = "inline-block";
+                ref_block.style.width = "1px";
+                ref_block.style.height = "0px";
+
+                div.style.visibility = "hidden";
+                div.appendChild(dmy_glyph);
+                div.appendChild(ref_block);
+                body.appendChild(div);
+
+                try
+                {
+                    ref_block.style["vertical-align"] = "baseline";
+                    this.glyph_mode_baseline = ref_block.offsetTop
+                                                - ref_glyph.offsetTop;
+                    ref_block.style["vertical-align"] = "bottom";
+                    this.glyph_mode_line_height = ref_block.offsetTop
+                                                    - ref_glyph.offsetTop;
+                }
+                finally
+                {
+                    document.body.removeChild(div);
+                }
+            }
         },
 
         render_cursors: function(cx, cy, x, y)
@@ -473,7 +521,6 @@ function ($, view_data, main, tileinfo_player, icons, dngn, enums,
             this.ctx.fillStyle = fg_term_colours[col.fg];
             this.ctx.font = prefix + this.glyph_mode_font_name();
             this.ctx.textAlign = "center";
-            this.ctx.textBaseline = "middle";
 
             this.ctx.save();
 
@@ -483,8 +530,18 @@ function ($, view_data, main, tileinfo_player, icons, dngn, enums,
                 this.ctx.rect(x, y, this.cell_width, this.cell_height);
                 this.ctx.clip();
 
-                this.ctx.fillText(map_cell.g,
-                                  x + this.cell_width/2, y + this.cell_height/2);
+                if (options.get("tile_display_mode") == "hybrid")
+                {
+                    this.ctx.textAlign = "center";
+                    this.ctx.textBaseline = "middle";
+                    this.ctx.fillText(map_cell.g, x + this.cell_width/2,
+                                        y + this.cell_height/2);
+                }
+                else
+                {
+                    this.ctx.fillText(map_cell.g, x,
+                                        y + this.glyph_mode_baseline);
+                }
             }
             finally
             {

crawl-ref/source/webserver/game_data/static/game.js
@@ -106,6 +106,7 @@ function ($, comm, client, dungeon_renderer, display, minimap, enums, messages,
             layout(layout_parameters, true);
         display.invalidate(true);
         display.display();
+        glyph_mode_font_init();
     });
 
     function toggle_full_window_dungeon_view(full)
@@ -225,13 +226,25 @@ function ($, comm, client, dungeon_renderer, display, minimap, enums, messages,
         document.title = data.text;
     }
 
-    var device_ratio = window.devicePixelRatio;
-    var renderer_settings = {
-        glyph_mode_font_size: 24 * device_ratio,
-        glyph_mode_font: "monospace"
-    };
+    function glyph_mode_font_init()
+    {
+        if (options.get("tile_display_mode") == "tiles") return;
 
-    $.extend(dungeon_renderer, renderer_settings);
+        var device_ratio = window.devicePixelRatio;
+        var glyph_font, glyph_size;
+
+        glyph_size = options.get("glyph_mode_font_size") * device_ratio;
+        glyph_font = options.get("glyph_mode_font");
+
+        if (!document.fonts.check(glyph_size + "px " + glyph_font))
+            glyph_font = "monospace";
+
+        var renderer_settings = {
+            glyph_mode_font_size: glyph_size,
+            glyph_mode_font: glyph_font
+        };
+        $.extend(dungeon_renderer, renderer_settings);
+    }
 
     $(document).ready(function () {
         $(window)

crawl-ref/source/initfile.cc
@@ -393,6 +393,8 @@ const vector<GameOption*> game_options::build_options_list()
         new StringGameOption(SIMPLE_NAME(tile_font_msg_family), "monospace"),
         new StringGameOption(SIMPLE_NAME(tile_font_stat_family), "monospace"),
         new StringGameOption(SIMPLE_NAME(tile_font_lbl_family), "monospace"),
+        new StringGameOption(SIMPLE_NAME(glyph_mode_font), "monospace"),
+        new IntGameOption(SIMPLE_NAME(glyph_mode_font_size), 24, 8, 144),
 #endif
 #ifdef USE_FT
         new BoolGameOption(SIMPLE_NAME(tile_font_ft_light), false),
@@ -4556,6 +4558,9 @@ void game_options::write_webtiles_options(const string& name)
     tiles.json_write_int("tile_font_stat_size", Options.tile_font_stat_size);
     tiles.json_write_int("tile_font_msg_size", Options.tile_font_msg_size);
     tiles.json_write_int("tile_font_lbl_size", Options.tile_font_lbl_size);
+
+    tiles.json_write_string("glyph_mode_font", Options.glyph_mode_font);
+    tiles.json_write_int("glyph_mode_font_size", Options.glyph_mode_font_size);
 
     tiles.json_write_bool("show_game_time", Options.show_game_time);
 

crawl-ref/source/options.h
@@ -536,6 +536,8 @@ public:
     string      tile_font_msg_family;
     string      tile_font_stat_family;
     string      tile_font_lbl_family;
+    string      glyph_mode_font;
+    int         glyph_mode_font_size;
 #endif
     int         tile_font_crt_size;
     int         tile_font_msg_size;

crawl-ref/source/webserver/game_data/static/dungeon_renderer.js
@@ -251,13 +251,9 @@ function ($, cr, map_knowledge, options, dngn, util, view_data, enums) {
 
             if (options.get("tile_display_mode") == "glyphs")
             {
-                // font size ratio: handled in cell_renderer.js
-                var margin = 2 * ratio;
-                this.ctx.font = this.glyph_mode_font_name();
-                var metrics = this.ctx.measureText("@");
-                this.set_cell_size(metrics.width + margin,
-                        Math.floor(this.glyph_mode_font_size * scale / 100)
-                        + margin);
+                this.glyph_mode_update_font_metrics();
+                this.set_cell_size(this.glyph_mode_font_width,
+                                    this.glyph_mode_line_height);
             }
             else if ((min_diameter * cell_size.w / ratio > width)
                 || (min_diameter * cell_size.h / ratio > height))

commit f92a7954ca93683e18bbd1b8bf97399cad445a13

Handle dynamic DPI in webtiles glyphs mode
This commit fixes handling of dynamic DPI situations in webtiles glyphs
mode that was accidentlly broken in 4531bab.

crawl-ref/source/webserver/game_data/static/cell_renderer.js
@@ -96,11 +96,11 @@ function ($, view_data, main, tileinfo_player, icons, dngn, enums,
 
         glyph_mode_font_name: function ()
         {
-            var glyph_scale;
+            var glyph_scale = window.devicePixelRatio;
             if (this.ui_state == enums.ui.VIEW_MAP)
-                glyph_scale = options.get("tile_map_scale");
+                glyph_scale *= options.get("tile_map_scale");
             else
-                glyph_scale = options.get("tile_viewport_scale");
+                glyph_scale *= options.get("tile_viewport_scale");
 
             return (Math.floor(this.glyph_mode_font_size * glyph_scale / 100)
                 + "px " + this.glyph_mode_font);

crawl-ref/source/webserver/game_data/static/game.js
@@ -230,10 +230,8 @@ function ($, comm, client, dungeon_renderer, display, minimap, enums, messages,
     {
         if (options.get("tile_display_mode") == "tiles") return;
 
-        var device_ratio = window.devicePixelRatio;
         var glyph_font, glyph_size;
-
-        glyph_size = options.get("glyph_mode_font_size") * device_ratio;
+        glyph_size = options.get("glyph_mode_font_size");
         glyph_font = options.get("glyph_mode_font");
 
         if (!document.fonts.check(glyph_size + "px " + glyph_font))