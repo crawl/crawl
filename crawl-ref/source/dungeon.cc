82a079e3609e827c98d347d0184d0f4f9b499ce6
kimjoy2002 <kimjoy2002@naver.com>
2020-09-20 22:33:11 +0900

    more pipe

--- crawl-ref/source/dungeon.cc
+++ crawl-ref/source/dungeon.cc
@@ -5679,8 +5679,8 @@ static int _make_delicious_corpse()
 static int _make_mercenary()
 {
     pair<object_class_type, int> create_obj = random_choose_weighted(
-        85, make_pair<object_class_type, int>(OBJ_MISCELLANY, MISC_MERCENARY),
-        5, make_pair<object_class_type, int>(OBJ_MISCELLANY, MISC_PIPE),
+        80, make_pair<object_class_type, int>(OBJ_MISCELLANY, MISC_MERCENARY),
+        10, make_pair<object_class_type, int>(OBJ_MISCELLANY, MISC_PIPE),
         5, make_pair<object_class_type, int>(OBJ_MISCELLANY, MISC_HEALING_MIST),
         2, make_pair<object_class_type, int>(OBJ_BOOKS, BOOK_AID),
         1, make_pair<object_class_type, int>(OBJ_WANDS, WAND_HASTING),
