fe262e0a26737be36829dbf297bccc14780e3552
Nicholas Feinberg <pleasingfung@gmail.com>
2020-09-22 18:43:32 -0700

    Remove guaranteed early Nemelex deck
    As with Gozag, Nemelex granting a guaranteed early deck (before the
    player even reached 1* of piety!) was part of what made them remarkably
    strong. Shift that early guaranteed deck and all other deck gifting to
    after the player reaches 1* of piety, to require a little work by the
    player before they get their reward. (This decreases deck gifting somewhat
    in general, though less so at higher piety.)

--- crawl-ref/source/religion.cc
+++ crawl-ref/source/religion.cc
@@ -203,6 +203,9 @@ const vector<god_power> god_powers[NUM_GODS] =
     // Nemelex
     {
       { 0, "draw from decks of power" },
+      { 1, "Nemelex will now gift you decks of power as you gain piety.",
+           "Nemelex will no longer gift you decks.",
+           "Nemelex will gift you decks of power as you gain piety." },
       { 3, ABIL_NEMELEX_TRIPLE_DRAW, "choose one out of three cards" },
       { 4, ABIL_NEMELEX_DEAL_FOUR, "deal four cards at a time" },
       { 5, ABIL_NEMELEX_STACK_FIVE, "stack five cards from your decks",
@@ -995,29 +998,39 @@ static bool _want_missile_gift()
            && x_chance_in_y(1 + you.skills[sk], 12);
 }
 
-static bool _give_nemelex_gift(bool forced = false)
+static bool _want_nemelex_gift()
 {
+    if (you.piety < piety_breakpoint(0))
+        return false;
+    const int piety_over_one_star = you.piety - piety_breakpoint(0);
+
     // Nemelex will give at least one gift early.
-    if (forced
-        || !you.num_total_gifts[GOD_NEMELEX_XOBEH]
-           && x_chance_in_y(you.piety + 1, piety_breakpoint(1))
-        || one_chance_in(3) && x_chance_in_y(you.piety + 1, MAX_PIETY))
+    if (!you.num_total_gifts[GOD_NEMELEX_XOBEH]
+        && x_chance_in_y(piety_over_one_star + 1, piety_breakpoint(1)))
     {
-        if (gift_cards())
-        {
-            simple_god_message(" deals you some cards!");
-            mprf(MSGCH_GOD, "You now have %s.", deck_summary().c_str());
-        }
-        else
-            simple_god_message(" goes to deal, but finds you have enough cards.");
-        _inc_gift_timeout(5 + random2avg(9, 2));
-        you.num_current_gifts[you.religion]++;
-        you.num_total_gifts[you.religion]++;
-        take_note(Note(NOTE_GOD_GIFT, you.religion));
         return true;
     }
 
-    return false;
+    return one_chance_in(3) && x_chance_in_y(piety_over_one_star + 1, MAX_PIETY);
+}
+
+static bool _give_nemelex_gift(bool forced = false)
+{
+    if (!forced && !_want_nemelex_gift())
+        return false;
+
+    if (gift_cards())
+    {
+        simple_god_message(" deals you some cards!");
+        mprf(MSGCH_GOD, "You now have %s.", deck_summary().c_str());
+    }
+    else
+        simple_god_message(" goes to deal, but finds you have enough cards.");
+    _inc_gift_timeout(5 + random2avg(9, 2));
+    you.num_current_gifts[you.religion]++;
+    you.num_total_gifts[you.religion]++;
+    take_note(Note(NOTE_GOD_GIFT, you.religion));
+    return true;
 }
 
 #if TAG_MAJOR_VERSION == 34
