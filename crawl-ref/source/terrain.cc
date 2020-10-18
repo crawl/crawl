crawl/crawl

commit b5bd6537f432e9b79925e8281a2dec72b504e765

Remove references to globals across files
I found three separate instances of functions reusing a global variable
exposed by travel.cc for their own scratch space, all for mild
variations on the same BFS algorithm. This commit replaces that with a
local variable, and one of a more sensible type as well; only a boolean
visited / not visited state needs to be tracked.

I don't quite have the energy to properly extract these right now, but
these would possibly be a good candidate for unit testing.

crawl-ref/source/terrain.cc
@@ -942,12 +942,14 @@ static coord_def _dgn_find_nearest_square(
     function<bool (const coord_def &)> acceptable,
     function<bool (const coord_def &)> traversable = nullptr)
 {
-    memset(travel_point_distance, 0, sizeof(travel_distance_grid_t));
+    bool visited[GXM][GYM];
+    memset(&visited, 0, sizeof(visited));
 
     vector<coord_def> points[2];
     int iter = 0;
     points[iter].push_back(pos);
 
+    // TODO: Deduplicate this BFS code (see commit for other two instances).
     while (!points[iter].empty())
     {
         // Iterate each layer of BFS in random order to avoid bias.
@@ -957,22 +959,18 @@ static coord_def _dgn_find_nearest_square(
             if (p != pos && acceptable(p))
                 return p;
 
-            travel_point_distance[p.x][p.y] = 1;
-            for (int yi = -1; yi <= 1; ++yi)
-                for (int xi = -1; xi <= 1; ++xi)
-                {
-                    if (!xi && !yi)
-                        continue;
+            visited[p.x][p.y] = true;
+            for (adjacent_iterator ai(p); ai; ++ai)
+            {
+                const coord_def np = p + coord_def(ai->x, ai->y);
+                if (!in_bounds(np) || visited[np.x][np.y])
+                    continue;
 
-                    const coord_def np = p + coord_def(xi, yi);
-                    if (!in_bounds(np) || travel_point_distance[np.x][np.y])
-                        continue;
+                if (traversable && !traversable(np))
+                    continue;
 
-                    if (traversable && !traversable(np))
-                        continue;
-
-                    points[!iter].push_back(np);
-                }
+                points[!iter].push_back(np);
+            }
         }
 
         points[iter].clear();

crawl-ref/source/files.cc
@@ -1225,7 +1225,9 @@ static void _grab_followers()
         }
     }
 
-    memset(travel_point_distance, 0, sizeof(travel_distance_grid_t));
+    bool visited[GXM][GYM];
+    memset(&visited, 0, sizeof(visited));
+
     vector<coord_def> places[2] = { { you.pos() }, {} };
     int place_set = 0;
     while (!places[place_set].empty())
@@ -1234,10 +1236,10 @@ static void _grab_followers()
         {
             for (adjacent_iterator ai(p); ai; ++ai)
             {
-                if (travel_point_distance[ai->x][ai->y])
+                if (visited[ai->x][ai->y])
                     continue;
 
-                travel_point_distance[ai->x][ai->y] = 1;
+                visited[ai->x][ai->y] = true;
                 if (_grab_follower_at(*ai, can_follow))
                     places[!place_set].push_back(*ai);
             }

crawl-ref/source/mon-transit.cc
@@ -406,8 +406,10 @@ void handle_followers(const coord_def &from,
     vector<coord_def> places[2];
     int place_set = 0;
 
+    bool visited[GXM][GYM];
+    memset(&visited, 0, sizeof(visited));
+
     places[place_set].push_back(from);
-    memset(travel_point_distance, 0, sizeof(travel_distance_grid_t));
     while (!places[place_set].empty())
     {
         for (const coord_def p : places[place_set])
@@ -415,11 +417,11 @@ void handle_followers(const coord_def &from,
             for (adjacent_iterator ai(p); ai; ++ai)
             {
                 if ((*ai - from).rdist() > radius
-                    || travel_point_distance[ai->x][ai->y])
+                    || visited[ai->x][ai->y])
                 {
                     continue;
                 }
-                travel_point_distance[ai->x][ai->y] = 1;
+                visited[ai->x][ai->y] = true;
 
                 bool real_follower = false;
                 if (handler(*ai, from, real_follower))

crawl-ref/source/travel.cc
@@ -116,7 +116,7 @@ static string trans_travel_dest;
 travel_distance_grid_t travel_point_distance;
 
 // Apply slime wall checks when checking if squares are travelsafe.
-bool g_Slime_Wall_Check = true;
+static bool g_Slime_Wall_Check = true;
 
 static uint8_t curr_waypoints[GXM][GYM];
 

commit 578ab304f51f697f72252c47a712bbfc6f60ea42

Fix out-of-bounds access (wheals)
This bug was introduced in b5bd653, which changed some code to use an
adjacent_iterator in passing, but neglected to adjust the expression
used to derive the adjacent coord_def.

crawl-ref/source/terrain.cc
@@ -962,7 +962,7 @@ static coord_def _dgn_find_nearest_square(
             visited[p.x][p.y] = true;
             for (adjacent_iterator ai(p); ai; ++ai)
             {
-                const coord_def np = p + coord_def(ai->x, ai->y);
+                const coord_def np = coord_def(ai->x, ai->y);
                 if (!in_bounds(np) || visited[np.x][np.y])
                     continue;
 