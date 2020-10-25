a43efd798ecf2c90f7c138e3b5f1a464725829cf
Nicholas Feinberg <pleasingfung@gmail.com>
2020-10-25 12:25:55 -0700

    Force Swamp generation for testing

--- crawl-ref/source/catch2-tests/test_ng-init-branches.cc
+++ crawl-ref/source/catch2-tests/test_ng-init-branches.cc
@@ -21,19 +21,17 @@ TEST_CASE( "create_brentry_validity", "[single-file]" ) {
     }
 }
 
-TEST_CASE( "disable_exactly_one_of_each_lair_branch", "[single-file]" ) {
+TEST_CASE( "disable_exactly_one_non_swamp_lair_branch", "[single-file]" ) {
     FixedVector<level_id, NUM_BRANCHES> candidate_brentry;
 
     for (int i=0; i < 100; i++)
     {
         candidate_brentry = create_brentry();
 
-        // != functions as xor here, testing if *exactly* one branch is
-        // valid
-        REQUIRE(candidate_brentry[BRANCH_SWAMP].is_valid()
-                != candidate_brentry[BRANCH_SHOALS].is_valid());
+        REQUIRE(candidate_brentry[BRANCH_SWAMP].is_valid());
 
         REQUIRE(candidate_brentry[BRANCH_SPIDER].is_valid()
-                != candidate_brentry[BRANCH_SNAKE].is_valid());
+                + candidate_brentry[BRANCH_SNAKE].is_valid()
+                + candidate_brentry[BRANCH_SHOALS].is_valid() == 1);
     }
 }
