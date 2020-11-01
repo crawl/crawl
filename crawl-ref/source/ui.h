a1a73b7a55c18c448106d9c4d7cc0cab5279ce10
advil <rawlins@gmail.com>
2020-11-01 11:19:54 -0500

    Improve Text wrap recalculation checks
    This function is *extremely* slow for large text boxes on local tiles,
    and this check eliminates some level of re-calling when doing initial
    widget sizing. I think there's still something wrong with this call
    somewhere deeper in that I haven't figured out, but this helps somewhat
    at a global level.

--- crawl-ref/source/ui.h
+++ crawl-ref/source/ui.h
@@ -123,6 +123,8 @@ public:
     explicit constexpr Size(int v) : width(v), height(v) {}
     constexpr Size(int w, int h) : width(w), height(h) {}
 
+    constexpr bool is_valid() const { return width >= 0 && height >= 0; }
+
     constexpr bool operator <= (const Size& other) const
     {
         return width <= other.width && height <= other.height;
@@ -781,6 +783,7 @@ protected:
     COLOURS m_bg_colour = BLACK;
 #endif
     Size m_wrapped_size = Size{-1};
+    Size m_wrapped_sizereq = Size{-1};
     string hl_pat;
     bool hl_line;
 };
