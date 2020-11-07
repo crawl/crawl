060e6a62af71360d55555127ee2f5ebc5578e8de
Nicholas Feinberg <pleasingfung@gmail.com>
2020-11-07 09:51:39 -0800

    Allow rebranding weapons to spectral (hellmonk)
    Seems fun. Weights chosen fairly arbitrarily.
    
    I also considered allowing SPWPN_ACID, but it'd be a bit dicey code-wise.

--- crawl-ref/source/item-use.cc
+++ crawl-ref/source/item-use.cc
@@ -2385,13 +2385,14 @@ static void _rebrand_weapon(item_def& wpn)
         else
         {
             new_brand = random_choose_weighted(
-                                    30, SPWPN_FLAMING,
-                                    30, SPWPN_FREEZING,
-                                    25, SPWPN_VORPAL,
-                                    20, SPWPN_VENOM,
-                                    15, SPWPN_DRAINING,
-                                    15, SPWPN_ELECTROCUTION,
-                                    12, SPWPN_PROTECTION,
+                                    28, SPWPN_FLAMING,
+                                    28, SPWPN_FREEZING,
+                                    23, SPWPN_VORPAL,
+                                    18, SPWPN_VENOM,
+                                    14, SPWPN_DRAINING,
+                                    14, SPWPN_ELECTROCUTION,
+                                    11, SPWPN_PROTECTION,
+                                    11, SPWPN_SPECTRAL,
                                     8, SPWPN_VAMPIRISM,
                                     3, SPWPN_CHAOS);
         }
@@ -2467,6 +2468,11 @@ static void _brand_weapon(item_def &wpn)
     case SPWPN_ACID:
         flash_colour = ETC_SLIME;
         mprf("%s oozes corrosive slime.", itname.c_str());
+        break;
+
+    case SPWPN_SPECTRAL:
+        flash_colour = BLUE;
+        mprf("%s acquires a faint afterimage.", itname.c_str());
         break;
 
     default:
