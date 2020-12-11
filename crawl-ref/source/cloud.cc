368d90fffdfadd77c1e5298e59cdf97a51697a67
advil <rawlins@gmail.com>
2020-12-11 15:50:47 -0500

    Also protect unbreathing creatures from miasma
    (following e1f20c3b445d4e)

--- crawl-ref/source/cloud.cc
+++ crawl-ref/source/cloud.cc
@@ -958,7 +958,7 @@ bool actor_cloud_immune(const actor &act, cloud_type type)
         case CLOUD_STEAM:
             return act.res_steam() > 0;
         case CLOUD_MIASMA:
-            return act.res_rotting() > 0;
+            return act.res_rotting() > 0 || act.is_unbreathing();
         case CLOUD_PETRIFY:
             return act.res_petrify();
         case CLOUD_SPECTRAL:
