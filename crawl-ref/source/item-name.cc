07df540b9138a44b098813b0aa7e0130babf264c
AdamPG <adampgardner@gmail.com>
2020-12-14 10:18:13 -0330

    Restore ghoul's ability to use potions
    The recent removal of US_HUNGRY_UNDEAD made ghouls unable to use potions (like mummies and lich form). This commit fixes this unintended interaction and removes some lingering references to US_HUNGRY_UNDEAD.

--- crawl-ref/source/item-name.cc
+++ crawl-ref/source/item-name.cc
@@ -2968,7 +2968,7 @@ bool is_useless_item(const item_def &item, bool temp, bool ident)
     case OBJ_POTIONS:
     {
         // Mummies can't use potions.
-        if (you.undead_state(temp) == US_UNDEAD)
+        if (you.undead_state(temp) == US_UNDEAD && you.species != SP_GHOUL)
             return true;
 
         if (!ident && !item_type_known(item))
