crawl/crawl

commit 4140fda2ff28bc70663d9f798f7a364ca0cbf572

Make described spell damage deterministic (Midn8)
For player and monster spells.

crawl-ref/source/spl-damage.cc
@@ -1265,10 +1265,10 @@ void shillelagh(actor *wielder, coord_def where, int pow)
         _shatter_player(pow, wielder, true);
 }
 
-dice_def irradiate_damage(int pow)
+dice_def irradiate_damage(int pow, bool random)
 {
     const int dice = 6;
-    const int max_dam = 30 + div_rand_round(pow, 2);
+    const int max_dam = 30 + random ? div_rand_round(pow, 2) : pow / 2;
     return calc_dice(dice, max_dam);
 }
 

crawl-ref/source/beam.cc
@@ -357,7 +357,7 @@ template<typename T>
 class power_deducer
 {
 public:
-    virtual T operator()(int pow) const = 0;
+    virtual T operator()(int pow, bool random = true) const = 0;
     virtual ~power_deducer() {}
 };
 
@@ -367,7 +367,7 @@ template<int adder, int mult_num = 0, int mult_denom = 1>
 class tohit_calculator : public tohit_deducer
 {
 public:
-    int operator()(int pow) const override
+    int operator()(int pow, bool /*random*/) const override
     {
         return adder + pow * mult_num / mult_denom;
     }
@@ -379,7 +379,7 @@ template<int numdice, int adder, int mult_num, int mult_denom>
 class dicedef_calculator : public dam_deducer
 {
 public:
-    dice_def operator()(int pow) const override
+    dice_def operator()(int pow, bool /*random*/) const override
     {
         return dice_def(numdice, adder + pow * mult_num / mult_denom);
     }
@@ -389,9 +389,9 @@ template<int numdice, int adder, int mult_num, int mult_denom>
 class calcdice_calculator : public dam_deducer
 {
 public:
-    dice_def operator()(int pow) const override
+    dice_def operator()(int pow, bool random) const override
     {
-        return calc_dice(numdice, adder + pow * mult_num / mult_denom);
+        return calc_dice(numdice, adder + pow * mult_num / mult_denom, random);
     }
 };
 
@@ -463,14 +463,14 @@ int zap_to_hit(zap_type z_type, int power, bool is_monster)
     return hit;
 }
 
-dice_def zap_damage(zap_type z_type, int power, bool is_monster)
+dice_def zap_damage(zap_type z_type, int power, bool is_monster, bool random)
 {
     const zap_info* zinfo = _seek_zap(z_type);
     if (!zinfo)
         return dice_def(0,0);
     const dam_deducer* dam_calc = is_monster ? zinfo->monster_damage
                                              : zinfo->player_damage;
-    return dam_calc ? (*dam_calc)(power) : dice_def(0,0);
+    return dam_calc ? (*dam_calc)(power, random) : dice_def(0,0);
 }
 
 colour_t zap_colour(zap_type z_type)

crawl-ref/source/beam.h
@@ -346,7 +346,7 @@ bool zap_explodes(zap_type ztype);
 bool zap_is_enchantment(zap_type ztype);
 int zap_ench_power(zap_type z_type, int pow, bool is_monster);
 int zap_to_hit(zap_type z_type, int power, bool is_monster);
-dice_def zap_damage(zap_type z_type, int power, bool is_monster);
+dice_def zap_damage(zap_type z_type, int power, bool is_monster, bool random = true);
 colour_t zap_colour(zap_type z_type);
 
 void zappy(zap_type z_type, int power, bool is_monster, bolt &pbolt);

crawl-ref/source/describe-spells.cc
@@ -504,7 +504,7 @@ static dice_def _spell_damage(spell_type spell, int hd)
     if (zap == NUM_ZAPS)
         return dice_def(0,0);
 
-    return zap_damage(zap, pow, true);
+    return zap_damage(zap, pow, true, false);
 }
 
 static int _spell_hd(spell_type spell, const monster_info &mon_owner)

crawl-ref/source/random.cc
@@ -286,7 +286,7 @@ int dice_def::roll() const
     return roll_dice(num, size);
 }
 
-dice_def calc_dice(int num_dice, int max_damage)
+dice_def calc_dice(int num_dice, int max_damage, bool random)
 {
     dice_def ret(num_dice, 0);
 
@@ -300,8 +300,10 @@ dice_def calc_dice(int num_dice, int max_damage)
         ret.num  = max_damage;
         ret.size = 1;
     }
-    else
+    else if (random)
         ret.size = div_rand_round(max_damage, num_dice);
+    else
+        ret.size = max_damage / num_dice; // round down
 
     return ret;
 }

crawl-ref/source/random.h
@@ -222,7 +222,7 @@ struct dice_def
 
 constexpr dice_def CONVENIENT_NONZERO_DAMAGE{42, 1};
 
-dice_def calc_dice(int num_dice, int max_damage);
+dice_def calc_dice(int num_dice, int max_damage, bool random = true);
 
 // I must be a random-access iterator.
 template <typename I>

crawl-ref/source/spl-cast.cc
@@ -2502,7 +2502,7 @@ static dice_def _spell_damage(spell_type spell)
         case SPELL_IOOD:
             return iood_damage(power, INFINITE_DISTANCE);
         case SPELL_IRRADIATE:
-            return irradiate_damage(power);
+            return irradiate_damage(power, false);
         case SPELL_SHATTER:
             return shatter_damage(power);
         case SPELL_BATTLESPHERE:
@@ -2513,7 +2513,7 @@ static dice_def _spell_damage(spell_type spell)
     const zap_type zap = spell_to_zap(spell);
     if (zap == NUM_ZAPS)
         return dice_def(0,0);
-    return zap_damage(zap, power, false);
+    return zap_damage(zap, power, false, false);
 }
 
 string spell_damage_string(spell_type spell)

crawl-ref/source/spl-damage.h
@@ -34,7 +34,7 @@ spret cast_airstrike(int pow, const dist &beam, bool fail);
 spret cast_shatter(int pow, bool fail);
 dice_def shatter_damage(int pow, monster *mons = nullptr);
 spret cast_irradiate(int powc, actor* who, bool fail);
-dice_def irradiate_damage(int powc);
+dice_def irradiate_damage(int powc, bool random = true);
 bool ignite_poison_affects(const actor* act);
 spret cast_ignite_poison(actor *agent, int pow, bool fail,
                               bool tracer = false);

commit 4c0aae810e2a691c212666ebe89be4fefdd72898

Fix Irradiate damage (Midn8)
Broken in 4140fda2ff28.

crawl-ref/source/spl-damage.cc
@@ -1268,7 +1268,7 @@ void shillelagh(actor *wielder, coord_def where, int pow)
 dice_def irradiate_damage(int pow, bool random)
 {
     const int dice = 6;
-    const int max_dam = 30 + random ? div_rand_round(pow, 2) : pow / 2;
+    const int max_dam = 30 + (random ? div_rand_round(pow, 2) : pow / 2);
     return calc_dice(dice, max_dam);
 }
 