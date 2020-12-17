e913e73dd248cea1aac1e2e18a6ec3d44bdc937f
Nicholas Feinberg <pleasingfung@gmail.com>
2020-12-16 16:57:35 -0800

    Add power_perc to lua table (advil)

--- crawl-ref/source/l-spells.cc
+++ crawl-ref/source/l-spells.cc
@@ -359,6 +359,7 @@ static const struct luaL_reg spells_clib[] =
     { "fail_severity" , l_spells_fail_severity },
     { "power"         , l_spells_power },
     { "max_power"     , l_spells_max_power },
+    { "power_perc"    , l_spells_power_perc },
     { "dir_or_target" , l_spells_dir_or_target },
     { "target"        , l_spells_target },
     { "dir"           , l_spells_dir },
