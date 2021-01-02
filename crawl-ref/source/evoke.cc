105bc0f7bc0538ce0380fe774c2a35860f9f7c17
advil <rawlins@gmail.com>
2021-01-02 18:26:21 -0500

    Revert "Improve tremorstone + summons warning checks (12409)"
    This reverts commit 91735a3152ac1321536d30bc7ca0037caf38fc6b.

--- crawl-ref/source/evoke.cc
+++ crawl-ref/source/evoke.cc
@@ -835,14 +835,6 @@ static spret _phantom_mirror(dist *target)
     return spret::success;
 }
 
-static bool _valid_tremorstone_target(monster &m)
-{
-    return !mons_is_firewood(*monster_at(*ri))
-        && !(have_passive(passive_t::shoot_through_plants)
-                                && fedhas_protects(act->as_monster()))
-        && !always_shoot_through_monster(&you, *act->as_monster());
-}
-
 /**
  * Find the cell at range 3 closest to the center of mass of monsters in range,
  * or a random range 3 cell if there are none.
@@ -859,7 +851,7 @@ static coord_def _find_tremorstone_target(bool& see_targets)
 
     for (radius_iterator ri(you.pos(), LOS_NO_TRANS); ri; ++ri)
     {
-        if (monster_at(*ri) && _valid_tremorstone_target(*monster_at(*ri)))
+        if (monster_at(*ri) && !mons_is_firewood(*monster_at(*ri)))
         {
             com += *ri;
             see_targets = see_targets || you.can_see(*monster_at(*ri));
@@ -961,7 +953,8 @@ static spret _tremorstone()
     targeter_radius hitfunc(&you, LOS_NO_TRANS);
     auto vulnerable = [](const actor *act) -> bool
     {
-        return act && _valid_tremorstone_target(*act->as_monster());
+        return !(have_passive(passive_t::shoot_through_plants)
+                 && fedhas_protects(act->as_monster()));
     };
     if ((!see_target
         && !yesno("You can't see anything, throw a tremorstone anyway?",
