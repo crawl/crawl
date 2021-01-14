9debfc2063acdef14acd5c54f99fb2ec6650b6a9
wheals <whealswithinwheals@gmail.com>
2021-01-14 23:24:49 +0200

    The future
    (cherry picked from commit 8aa0e2c87f6091a12041dd3dfc7a966e616a35c2)

--- crawl-ref/source/ng-input.cc
+++ crawl-ref/source/ng-input.cc
@@ -18,7 +18,7 @@ formatted_string opening_screen()
 {
     string msg =
     "<yellow>Hello, welcome to " CRAWL " " + string(Version::Long) + "!</yellow>\n"
-    "<brown>(c) Copyright 1997-2002 Linley Henzell, 2002-2020 Crawl DevTeam";
+    "<brown>(c) Copyright 1997-2002 Linley Henzell, 2002-2021 Crawl DevTeam";
 
     return formatted_string::parse_string(msg);
 }
