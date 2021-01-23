crawl/crawl

commit 3dd439832fe0f5fe295caad1a2fe3e479a757cda

Turn monster explosions into fineffs
Explosions firing immediately causes a pretty classic set of issues -
we're invalidating the old state of the world in surprising ways. For
example, if a ball lightning explodes and kills an attacker midway
through the resolution of a multi-target attack (e.g. a javelin toss),
the game just has no way to figure out how to resolve the rest of the
attack afterward - it can't generate new ranged_attacks without the
original attacker, so it just crashes.

Instead, let's wait until the *end* of any ongoing melee attacks or
beams before triggering death explosions. The timing here is slightly
different for attack-triggered fineffs - we trigger them a little later
now melee attacks, in fight.cc - but hopefully that's ok.

Overall, this code is very bad and probably buggy. I'm sorry.

crawl-ref/source/beam.cc
@@ -31,6 +31,7 @@
 #include "english.h"
 #include "exercise.h"
 #include "fight.h"
+#include "fineff.h"
 #include "god-conduct.h"
 #include "god-item.h"
 #include "god-passive.h" // passive_t::convert_orcs
@@ -945,6 +946,7 @@ int bolt::range_used(bool leg_only) const
 void bolt::finish_beam()
 {
     extra_range_used = BEAM_STOP;
+    fire_final_effects();
 }
 
 void bolt::affect_wall()

crawl-ref/source/mon-explode.cc
@@ -13,6 +13,7 @@
 #include "dungeon-char-type.h"
 #include "english.h"
 #include "env.h"
+#include "fineff.h"
 #include "fprop.h"
 #include "message.h"
 #include "monster.h"
@@ -20,8 +21,9 @@
 #include "mon-place.h"
 #include "mon-util.h"
 #include "mpr.h"
-#include "state.h"
 #include "spl-goditem.h"
+#include "state.h"
+#include "stringutil.h"
 #include "target.h"
 #include "terrain.h"
 #include "torment-source-type.h"
@@ -177,7 +179,9 @@ bool explode_monster(monster* mons, killer_type killer,
     bolt beam;
     const monster_type type = mons->type;
     string sanct_msg = "";
-    actor* agent = mons;
+    string boom_msg = make_stringf("%s explodes!", mons->full_name(DESC_THE).c_str());
+    actor* agent = nullptr;
+    bool inner_flame = false;
 
     auto it = explosions.find(type);
     if (it != explosions.end())
@@ -191,6 +195,8 @@ bool explode_monster(monster* mons, killer_type killer,
         sanct_msg = string("By Zin's power, ") +
                     apostrophise(mons->name(DESC_THE)) + " " +
                     effect + ".";
+        if (type == MONS_BENNU)
+            boom_msg = make_stringf("%s blazes out!", mons->full_name(DESC_THE).c_str());
     } else {
         if (!mons->has_ench(ENCH_INNER_FLAME))
         {
@@ -212,6 +218,7 @@ bool explode_monster(monster* mons, killer_type killer,
         mons->flags    |= MF_EXPLODE_KILL;
         sanct_msg       = "By Zin's power, the fiery explosion is contained.";
         beam.aux_source = "exploding inner flame";
+        inner_flame = true;
     }
 
     if (beam.aux_source.empty())
@@ -232,34 +239,15 @@ bool explode_monster(monster* mons, killer_type killer,
         }
     }
 
-    bool saw = false;
-    if (you.can_see(*mons))
+    if (is_sanctuary(mons->pos()))
     {
-        saw = true;
-        viewwindow();
-        update_screen();
-        if (is_sanctuary(mons->pos()))
+        if (you.can_see(*mons))
             mprf(MSGCH_GOD, "%s", sanct_msg.c_str());
-        else if (type == MONS_BENNU)
-            mprf(MSGCH_MONSTER_DAMAGE, MDAM_DEAD, "%s blazes out!",
-                 mons->full_name(DESC_THE).c_str());
-        else
-            mprf(MSGCH_MONSTER_DAMAGE, MDAM_DEAD, "%s explodes!",
-                 mons->full_name(DESC_THE).c_str());
+        return false;
     }
 
-    if (is_sanctuary(mons->pos()))
-        return false;
-
-    // Explosion side-effects.
     if (type == MONS_LURKING_HORROR)
         torment(mons, TORMENT_LURKING_HORROR, mons->pos());
-    else if (mons->has_ench(ENCH_INNER_FLAME))
-    {
-        for (adjacent_iterator ai(mons->pos(), false); ai; ++ai)
-            if (!cell_is_solid(*ai) && !cloud_at(*ai) && !one_chance_in(5))
-                place_cloud(CLOUD_FIRE, *ai, 10 + random2(10), agent);
-    }
 
     // Detach monster from the grid first, so it doesn't get hit by
     // its own explosion. (GDL)
@@ -275,11 +263,6 @@ bool explode_monster(monster* mons, killer_type killer,
 
     // Exploding kills the monster a bit earlier than normal.
     mons->hit_points = -16;
-    if (saw)
-    {
-        viewwindow();
-        update_screen();
-    }
 
     // FIXME: show_more == you.see_cell(mons->pos())
     if (type == MONS_LURKING_HORROR)
@@ -288,7 +271,7 @@ bool explode_monster(monster* mons, killer_type killer,
         flash_view_delay(UA_MONSTER, DARKGRAY, 300, &hitfunc);
     }
     else
-        beam.explode();
+        explosion_fineff::schedule(beam, boom_msg, sanct_msg, inner_flame, agent);
 
     // Monster died in explosion, so don't re-attach it to the grid.
     return true;

crawl-ref/source/attack.cc
@@ -136,9 +136,6 @@ bool attack::handle_phase_killed()
 
 bool attack::handle_phase_end()
 {
-    // This may invalidate both the attacker and defender.
-    fire_final_effects();
-
     return true;
 }
 

crawl-ref/source/fineff.cc
@@ -8,6 +8,7 @@
 
 #include "fineff.h"
 
+#include "beam.h"
 #include "bloodspatter.h"
 #include "coordit.h"
 #include "dactions.h"
@@ -485,6 +486,26 @@ void shock_serpent_discharge_fineff::fire()
                                         "a shock serpent", "electric aura");
     if (amount)
         oppressor.expose_to_element(beam.flavour, amount);
+}
+
+void explosion_fineff::fire()
+{
+    if (is_sanctuary(beam.target))
+    {
+        if (you.see_cell(beam.target))
+            mprf(MSGCH_GOD, "%s", sanctuary_message.c_str());
+        return;
+    }
+
+    if (you.see_cell(beam.target))
+        mprf(MSGCH_MONSTER_DAMAGE, MDAM_DEAD, "%s", boom_message.c_str());
+
+    if (inner_flame)
+        for (adjacent_iterator ai(beam.target, false); ai; ++ai)
+            if (!cell_is_solid(*ai) && !cloud_at(*ai) && !one_chance_in(5))
+                place_cloud(CLOUD_FIRE, *ai, 10 + random2(10), flame_agent);
+
+    beam.explode();
 }
 
 void delayed_action_fineff::fire()

crawl-ref/source/fineff.h
@@ -10,6 +10,8 @@
 #include "mon-util.h"
 #include "mgen-data.h"
 
+struct bolt;
+
 class final_effect
 {
 public:
@@ -238,6 +240,34 @@ protected:
     coord_def position;
     int power;
     mon_attitude_type attitude;
+};
+
+class explosion_fineff : public final_effect
+{
+public:
+    // One explosion at a time, please.
+    bool mergeable(const final_effect &) const override { return false; }
+    void fire() override;
+
+    static void schedule(bolt &beam, string boom, string sanct,
+                         bool inner_flame, const actor* flame_agent)
+    {
+        final_effect::schedule(new explosion_fineff(beam, boom, sanct,
+                                                    inner_flame, flame_agent));
+    }
+protected:
+    explosion_fineff(const bolt &beem, string boom, string sanct,
+                     bool flame, const actor* agent)
+        : final_effect(0, 0, coord_def()), beam(beem),
+          boom_message(boom), sanctuary_message(sanct),
+          inner_flame(flame), flame_agent(agent)
+    {
+    }
+    bolt beam;
+    string boom_message;
+    string sanctuary_message;
+    bool inner_flame;
+    const actor* flame_agent;
 };
 
 // A fineff that triggers a daction; otherwise the daction

commit 96f8838c2c92253b666e72aa743336c103652ca2

Fix death explosion messages
Broken by 3dd439832fe0f5fe295caad1a2fe3e479a757cda - we'd claim that
players were 'hit from afar by a bloated husk' instead of 'killed by
an exploding bloated husk'. Oops!

From that commit's final line:
> Overall, this code is very bad and probably buggy. I'm sorry.

crawl-ref/source/beam.cc
@@ -2727,7 +2727,7 @@ void bolt::internal_ouch(int dam)
              aux_source.empty() ? nullptr : aux_source.c_str(), true,
              source_name.empty() ? nullptr : source_name.c_str());
     }
-    else if (monst && mon_explodes_on_death(monst->type))
+    else if (is_death_effect)
     {
         ouch(dam, KILLED_BY_SPORE, source_id,
              aux_source.c_str(), true,

crawl-ref/source/beam.h
@@ -94,6 +94,7 @@ struct bolt
     bool   pierce = false;        // Can the beam pass through a target and
                                   // hit another target behind the first?
     bool   is_explosion = false;
+    bool   is_death_effect = false; // effect of e.g. ballistomycete spore
     bool   aimed_at_spot = false; // aimed at (x, y), should not cross
     string aux_source = "";       // source of KILL_MISC beams
 

crawl-ref/source/mon-explode.cc
@@ -34,6 +34,7 @@ static void _setup_base_explosion(bolt & beam, const monster& origin)
 {
     beam.is_tracer    = false;
     beam.is_explosion = true;
+    beam.is_death_effect = true;
     beam.source_id    = origin.mid;
     beam.glyph        = dchar_glyph(DCHAR_FIRED_BURST);
     beam.source       = origin.pos();