55eedbe7eb50b959a066d7e77f5c81bac1ba6718
Nicholas Feinberg <pleasingfung@gmail.com>
2021-01-23 08:04:37 -0800

    Fix death explosion messages
    Broken by 3dd439832fe0f5fe295caad1a2fe3e479a757cda - we'd claim that
    players were 'hit from afar by a bloated husk' instead of 'killed by
    an exploding bloated husk'. Oops!
    
    From that commit's final line:
    > Overall, this code is very bad and probably buggy. I'm sorry.

--- crawl-ref/source/mon-explode.cc
+++ crawl-ref/source/mon-explode.cc
@@ -34,6 +34,7 @@ static void _setup_base_explosion(bolt & beam, const monster& origin)
 {
     beam.is_tracer    = false;
     beam.is_explosion = true;
+    beam.is_death_effect = true;
     beam.source_id    = origin.mid;
     beam.glyph        = dchar_glyph(DCHAR_FIRED_BURST);
     beam.source       = origin.pos();
