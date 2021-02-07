9fa89c534f9dd3d6f91c52a6b5fe9394f0e5aa8d
kimjoy2002 <kimjoy2002@naver.com>
2021-02-07 20:49:37 +0900

    fixed crystal spear of spectral crash bug

--- crawl-ref/source/makeitem.cc
+++ crawl-ref/source/makeitem.cc
@@ -335,7 +335,8 @@ bool is_weapon_brand_ok(int type, int brand, bool /*strict*/)
     case SPWPN_SPECTRAL:
         if (is_range_weapon(item)
             || (basic_hands_reqd(item, SIZE_MEDIUM) == HANDS_ONE
-                && type != WPN_CLUB))
+                && type != WPN_CLUB
+                && type != WPN_CRYSTAL_SPEAR))
         {
             // only on two-handers and clubs
             return false;
