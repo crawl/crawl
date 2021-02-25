crawl/crawl

commit e143a0ad5d2d1f367574c6fc699c19bce55c5231

Convert gaze ability attacks to spells.
...since they all duplicate spells right now anyway.

This is a very slight buff to giant eyeballs in that their paralysis can
last a turn or two longer than previously, and a very slight nerf to
eyes of draining and ghost moths in that they don't recover MP from
their drain gaze now. If so inclined, someone can go and special-case
these back in, but the code to let the respective spells be irresistible
is special-casey enough already.

Also contains a fix for messaging for spells which don't generate noise,
and the removal of the no-longer-used MSG_BEING_WATCHED (which is in the
database now).

crawl-ref/source/mon-cast.cc
@@ -6466,7 +6466,7 @@ void mons_cast_noise(monster* mons, const bolt &pbolt,
          mons->friendly()    ? MSGCH_FRIEND_SPELL
                              : MSGCH_MONSTER_SPELL);
 
-    if (silent)
+    if (silent || noise == 0)
         mons_speaks_msg(mons, msg, chan, true);
     else if (noisy(noise, mons->pos(), mons->mindex()) || !unseen)
     {

crawl-ref/source/beam.cc
@@ -5088,6 +5088,12 @@ bool bolt::has_saving_throw() const
         return false;
     case BEAM_VULNERABILITY:
         return !one_chance_in(3);  // Ignores MR 1/3 of the time
+    case BEAM_PARALYSIS:        // Giant eyeball paralysis is irresistible
+        return !(agent() && agent()->type == MONS_GIANT_EYEBALL);
+    case BEAM_DRAIN_MAGIC:
+        return !agent()
+               || (agent()->type != MONS_EYE_OF_DRAINING
+                   && agent()->type != MONS_GHOST_MOTH);
     default:
         return true;
     }

crawl-ref/source/enum.h
@@ -676,7 +676,6 @@ enum canned_message_type
     MSG_CALL_DEAD,
     MSG_ANIMATE_REMAINS,
     MSG_DECK_EXHAUSTED,
-    MSG_BEING_WATCHED,
     MSG_CANNOT_MOVE,
     MSG_YOU_DIE,
 };

crawl-ref/source/message.cc
@@ -1664,9 +1664,6 @@ void canned_msg(canned_message_type which_message)
         case MSG_DECK_EXHAUSTED:
             mpr("The deck of cards disappears in a puff of smoke.");
             break;
-        case MSG_BEING_WATCHED:
-            mpr("You feel you are being watched by something.");
-            break;
         case MSG_CANNOT_MOVE:
             mpr("You cannot move.");
             break;

crawl-ref/source/mon-abil.cc
@@ -1543,17 +1543,6 @@ bool mon_special_ability(monster* mons, bolt & beem)
     return used;
 }
 
-// Combines code for eyeball-type monsters, etc. to reduce clutter.
-static bool _eyeball_will_use_ability(monster* mons)
-{
-    return coinflip()
-       && !mons_is_confused(mons)
-       && !mons_is_wandering(mons)
-       && !mons_is_fleeing(mons)
-       && !mons->pacified()
-       && !player_or_mon_in_sanct(mons);
-}
-
 //---------------------------------------------------------------
 //
 // mon_nearby_ability
@@ -1603,100 +1592,6 @@ void mon_nearby_ability(monster* mons)
     case MONS_KILLER_KLOWN:
         // Choose random colour.
         mons->colour = random_colour();
-        break;
-
-    case MONS_GOLDEN_EYE:
-        if (_eyeball_will_use_ability(mons)
-            && mons->see_cell_no_trans(foe->pos()))
-        {
-            const bool can_see = you.can_see(mons);
-            if (can_see && you.can_see(foe))
-            {
-                mprf("%s blinks at %s.",
-                     mons->name(DESC_THE).c_str(),
-                     foe->name(DESC_THE).c_str());
-            }
-
-            int confuse_power = 2 + random2(3);
-
-            if (foe->is_player() && !can_see)
-            {
-                canned_msg(MSG_BEING_WATCHED);
-                interrupt_activity(AI_MONSTER_ATTACKS, mons);
-            }
-
-            int res_margin = foe->check_res_magic((mons->get_hit_dice() * 5)
-                             * confuse_power);
-            if (res_margin > 0)
-            {
-                if (foe->is_player())
-                    canned_msg(MSG_YOU_RESIST);
-                else if (foe->is_monster())
-                {
-                    const monster* foe_mons = foe->as_monster();
-                    simple_monster_message(foe_mons,
-                           mons_resist_string(foe_mons, res_margin));
-                }
-                break;
-            }
-
-            foe->confuse(mons, 2 + random2(3));
-        }
-        break;
-
-    case MONS_GIANT_EYEBALL:
-        if (_eyeball_will_use_ability(mons)
-            && mons->see_cell_no_trans(foe->pos()))
-        {
-            const bool can_see = you.can_see(mons);
-            if (can_see && you.can_see(foe))
-            {
-                mprf("%s stares at %s.",
-                     mons->name(DESC_THE).c_str(),
-                     foe->name(DESC_THE).c_str());
-            }
-
-            if (foe->is_player() && !can_see)
-                canned_msg(MSG_BEING_WATCHED);
-
-            // Subtly different from old paralysis behaviour, but
-            // it'll do.
-            foe->paralyse(mons, 2 + random2(3));
-        }
-        break;
-
-    case MONS_EYE_OF_DRAINING:
-    case MONS_GHOST_MOTH:
-        if (_eyeball_will_use_ability(mons)
-            && mons->see_cell_no_trans(foe->pos())
-            && foe->is_player())
-        {
-            if (you.can_see(mons))
-                simple_monster_message(mons, " stares at you.");
-            else
-                canned_msg(MSG_BEING_WATCHED);
-
-            interrupt_activity(AI_MONSTER_ATTACKS, mons);
-
-            int mp = 5 + random2avg(13, 3);
-#if TAG_MAJOR_VERSION == 34
-            if (you.species != SP_DJINNI)
-                mp = min(mp, you.magic_points);
-            else
-            {
-                // Cap draining somehow, otherwise a ghost moth will heal
-                // itself every turn.  Other races don't have such a problem
-                // because they drop to 0 mp nearly immediately.
-                mp = mp * (you.hp_max - you.duration[DUR_ANTIMAGIC] / 3)
-                        / you.hp_max;
-            }
-#else
-            mp = min(mp, you.magic_points);
-#endif
-            drain_mp(mp);
-
-            mons->heal(mp, true); // heh heh {dlb}
-        }
         break;
 
     case MONS_AIR_ELEMENTAL:

crawl-ref/source/mon-data.h
@@ -2445,7 +2445,7 @@ DUMMY(MONS_MOTH, 'y', WHITE, "moth")
     { {AT_HIT, AF_DRAIN_STAT, 18}, {AT_HIT, AF_DRAIN_STAT, 18},
       {AT_STING, AF_POISON_STRONG, 12}, AT_NO_ATK },
     { 13, 3, 5, 0 },
-    8, 10, MST_NO_SPELLS, CE_MUTAGEN, Z_BIG, S_SILENT,
+    8, 10, MST_DRAIN_GAZE, CE_MUTAGEN, Z_BIG, S_SILENT,
     I_INSECT, HT_LAND, FL_WINGED, 12, DEFAULT_ENERGY,
     MONUSE_NOTHING, MONEAT_NOTHING, SIZE_LARGE, MON_SHAPE_INSECT_WINGED
 },
@@ -3033,7 +3033,7 @@ DUMMY(MONS_ELEMENTAL, 'E', LIGHTGREY, "elemental")
     0, 10, MONS_GIANT_EYEBALL, MONS_GIANT_EYEBALL, MH_NATURAL, 10,
     { AT_NO_ATK, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
     { 3, 3, 5, 0 },
-    0, 1, MST_NO_SPELLS, CE_CLEAN, Z_NOZOMBIE, S_SILENT,
+    0, 1, MST_GIANT_EYEBALL, CE_CLEAN, Z_NOZOMBIE, S_SILENT,
     I_PLANT, HT_LAND, FL_LEVITATE, 3, DEFAULT_ENERGY,
     MONUSE_NOTHING, MONEAT_NOTHING, SIZE_MEDIUM, MON_SHAPE_ORB
 },
@@ -3045,7 +3045,7 @@ DUMMY(MONS_ELEMENTAL, 'E', LIGHTGREY, "elemental")
     0, 10, MONS_GIANT_EYEBALL, MONS_EYE_OF_DRAINING, MH_NATURAL, MAG_IMMUNE,
     { AT_NO_ATK, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
     { 7, 3, 5, 0 },
-    3, 1, MST_NO_SPELLS, CE_POISONOUS, Z_NOZOMBIE, S_SILENT,
+    3, 1, MST_DRAIN_GAZE, CE_POISONOUS, Z_NOZOMBIE, S_SILENT,
     I_PLANT, HT_LAND, FL_LEVITATE, 5, DEFAULT_ENERGY,
     MONUSE_NOTHING, MONEAT_NOTHING, SIZE_MEDIUM, MON_SHAPE_ORB
 },
@@ -3106,7 +3106,7 @@ DUMMY(MONS_ELEMENTAL, 'E', LIGHTGREY, "elemental")
     0, 17, MONS_GIANT_EYEBALL, MONS_GOLDEN_EYE, MH_NATURAL, MAG_IMMUNE,
     { AT_NO_ATK, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
     { 6, 1, 2, 0 },
-    0, 20, MST_BLINKER, CE_NOCORPSE, Z_NOZOMBIE, S_SILENT,
+    0, 20, MST_GOLDEN_EYE, CE_NOCORPSE, Z_NOZOMBIE, S_SILENT,
     I_PLANT, HT_LAND, FL_LEVITATE, 13, DEFAULT_ENERGY,
     MONUSE_NOTHING, MONEAT_NOTHING, SIZE_TINY, MON_SHAPE_ORB
 },

crawl-ref/source/mon-spll.h
@@ -2320,4 +2320,26 @@ static const mon_spellbook mspell_list[] =
        END_OF_MONS_BOOK
       }
     },
+
+    {  MST_GIANT_EYEBALL,
+      {
+       { SPELL_PARALYSE, 100, MON_SPELL_NATURAL | MON_SPELL_INSTANT },
+       END_OF_MONS_BOOK
+      }
+    },
+
+    {  MST_DRAIN_GAZE,
+      {
+       { SPELL_DRAIN_MAGIC, 100, MON_SPELL_NATURAL | MON_SPELL_INSTANT },
+       END_OF_MONS_BOOK
+      }
+    },
+
+    {  MST_GOLDEN_EYE,
+      {
+       { SPELL_CONFUSE, 100, MON_SPELL_NATURAL | MON_SPELL_INSTANT },
+       { SPELL_BLINK, 29, MON_SPELL_NATURAL },
+       END_OF_MONS_BOOK
+      }
+    },
 };

commit 7a858468532a2ed152b29ac8fcd902bc3e69ca7f

Fix @player_only@ messages (#9201)
When printing monster spell messages, we never checked to see if we
they were valid. As a result, unseen ghost moths casting Draining Gaze
on other monsters would print the literal string "You feel something
watching you. @player_only@".

This bug was introduced in e143a0ad5d2 (October 2014) and first reported
by johlstei a month later. I managed to reproduce it in March 2015 but was
unable to figure out the root cause, because the monster speech code is
absolutely insane. But this is it!

crawl-ref/source/mon-cast.cc
@@ -6589,7 +6589,8 @@ static void _speech_keys(vector<string>& key_list,
     }
 }
 
-static string _speech_message(const vector<string>& key_list,
+static string _speech_message(const monster &mon,
+                              const vector<string>& key_list,
                               bool silent, bool unseen)
 {
     string prefix;
@@ -6607,12 +6608,14 @@ static string _speech_message(const vector<string>& key_list,
 #endif
 
         msg = getSpeakString(prefix + key);
+
         if (msg == "__NONE")
         {
             msg = "";
             break;
         }
-        else if (!msg.empty())
+
+        if (!msg.empty() && !invalid_msg(mon, msg))
             break;
 
         // If we got no message and we're using the silent prefix, then
@@ -6836,7 +6839,7 @@ void mons_cast_noise(monster* mons, const bolt &pbolt,
     vector<string> key_list;
     _speech_keys(key_list, mons, pbolt, spell_cast, slot_flags, targeted);
 
-    string msg = _speech_message(key_list, silent, unseen);
+    string msg = _speech_message(*mons, key_list, silent, unseen);
 
     if (msg.empty())
     {

crawl-ref/source/mon-speak.cc
@@ -398,6 +398,17 @@ void maybe_mons_speaks(monster* mons)
     // Okay then, don't speak.
 }
 
+static actor* _get_foe(const monster &mon)
+{
+    if (!crawl_state.game_is_arena()
+        && mon.wont_attack()
+        && invalid_monster_index(mon.foe))
+    {
+        return &you;
+    }
+    return mon.get_foe();
+}
+
 // Returns true if something is said.
 bool mons_speaks(monster* mons)
 {
@@ -482,9 +493,7 @@ bool mons_speaks(monster* mons)
     if (mons->props.exists("speech_prefix"))
         prefixes.push_back(mons->props["speech_prefix"].get_string());
 
-    const actor*    foe   = (!crawl_state.game_is_arena() && mons->wont_attack()
-                                && invalid_monster_index(mons->foe)) ?
-                                    &you : mons->get_foe();
+    const actor*    foe  = _get_foe(*mons);
     const monster* m_foe = foe ? foe->as_monster() : nullptr;
 
     if (!foe || foe->is_player() || mons->wont_attack())
@@ -774,6 +783,26 @@ bool mons_speaks(monster* mons)
     }
 
     return mons_speaks_msg(mons, msg, MSGCH_TALK, silence);
+}
+
+bool invalid_msg(const monster &mon, string msg)
+{
+    const actor*    foe    = _get_foe(mon);
+    const monster* m_foe   = foe ? foe->as_monster() : nullptr;
+    // TODO: dedup with mons_speaks()
+    const bool no_foe      = (foe == nullptr);
+    const bool no_player   = crawl_state.game_is_arena()
+                             || (!mon.wont_attack()
+                                 && (!foe || !foe->is_player()));
+    const bool mon_foe     = (m_foe != nullptr);
+    const bool no_god      = no_foe || (mon_foe && foe->deity() == GOD_NO_GOD);
+    const bool named_foe   = !no_foe && (!mon_foe || (m_foe->is_named()
+                                && m_foe->type != MONS_ROYAL_JELLY));
+    const bool no_foe_name = !named_foe
+                             || (mon_foe && (m_foe->flags & MF_NAME_MASK));
+    const bool unseen = !you.can_see(mon);
+    return _invalid_msg(msg, no_player, no_foe, no_foe_name, no_god, unseen);
+
 }
 
 bool mons_speaks_msg(monster* mons, const string &msg,

crawl-ref/source/mon-speak.h
@@ -12,3 +12,4 @@ bool mons_speaks(monster* mons);
 bool mons_speaks_msg(monster* mons, const string &msg,
                      const msg_channel_type def_chan = MSGCH_TALK,
                      const bool silence = false);
+bool invalid_msg(const monster &mon, string msg);