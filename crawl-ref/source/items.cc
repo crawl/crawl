crawl/crawl

commit bb19ded6d30e696c234aeda7060a468f3fc7a340

Turn wear-id into floor-id
Following much the same reasoning as in f6d4cf81.

Especially now that curses are gone (RIP), there's very little incentive not
to wear-ID items. (This was mostly true before too, but it's extra obvious
now.) Instead, just identify equippable items when you step on them. This does
remove certain categories of surprise from randarts (it's *Contam!), but
players will live.

No comp for this in terms of ?id drops, since that item was better spent on
consumables for the period of the game in which ?id is in finite supply.
(So no difference.)

Closes #1770.

crawl-ref/source/items.cc
@@ -944,6 +944,8 @@ void item_check()
 // Identify the object the player stepped on.
 // Books are fully identified.
 // Wands are only type-identified.
+// Equipment items are fully identified,
+// but artefact equipment skips some type-id checks.
 static bool _id_floor_item(item_def &item)
 {
     if (item.base_type == OBJ_BOOKS)
@@ -969,6 +971,30 @@ static bool _id_floor_item(item_def &item)
                 set_item_autopickup(item, AP_FORCE_OFF);
             return true;
         }
+    }
+    else if (item_type_is_equipment(item.base_type))
+    {
+        if (fully_identified(item))
+            return false;
+
+        // autopickup hack for previously-unknown items
+        if (item_needs_autopickup(item))
+            item.props["needs_autopickup"] = true;
+
+        if (is_artefact(item))
+        {
+            if (!(item.flags & ISFLAG_NOTED_ID))
+            {
+                item.flags |= ISFLAG_NOTED_ID;
+
+                // Make a note of it.
+                take_note(Note(NOTE_ID_ITEM, 0, 0, item.name(DESC_A),
+                               origin_desc(item)));
+            }
+        }
+
+        set_ident_flags(item, ISFLAG_IDENT_MASK);
+        return true;
     }
 
     return false;

crawl-ref/source/delay.cc
@@ -656,11 +656,6 @@ void JewelleryOnDelay::finish()
 void EquipOnDelay::finish()
 {
     const unsigned int old_talents = your_talents(false).size();
-
-    set_ident_flags(equip, ISFLAG_IDENT_MASK);
-    if (is_artefact(equip))
-        equip.flags |= ISFLAG_NOTED_ID;
-
     const bool is_amulet = equip.base_type == OBJ_JEWELLERY;
     const equipment_type eq_slot = is_amulet ? EQ_AMULET :
                                                get_armour_slot(equip);

crawl-ref/source/item-prop.h
@@ -243,6 +243,12 @@ static inline bool is_weapon(const item_def &item)
     return item.base_type == OBJ_WEAPONS || item.base_type == OBJ_STAVES;
 }
 
+inline constexpr bool item_type_is_equipment(object_class_type base_type)
+{
+        return base_type == OBJ_WEAPONS || base_type == OBJ_ARMOUR
+               || base_type == OBJ_JEWELLERY || base_type == OBJ_STAVES;
+}
+
 void remove_whitespace(string &str);
 
 void auto_id_inventory();

crawl-ref/source/player-equip.cc
@@ -220,8 +220,6 @@ static void _equip_artefact_effect(item_def &item, bool *show_msgs, bool unmeld,
             you.unrand_reacts.set(slot);
     }
 
-    const bool alreadyknown = item_type_known(item);
-    const bool dangerous    = player_in_a_dangerous_place();
     const bool msg          = !show_msgs || *show_msgs;
 
     artefact_properties_t  proprt;
@@ -265,25 +263,12 @@ static void _equip_artefact_effect(item_def &item, bool *show_msgs, bool unmeld,
     if (proprt[ARTP_RAMPAGING] && msg && !unmeld)
         mpr("You feel ready to rampage towards enemies.");
 
-    if (!alreadyknown && dangerous)
-    {
-        // Xom loves it when you use an unknown random artefact and
-        // there is a dangerous monster nearby...
-        xom_is_stimulated(100);
-    }
-
     if (proprt[ARTP_HP])
         _calc_hp_artefact();
 
     // Let's try this here instead of up there.
     if (proprt[ARTP_MAGICAL_POWER])
         calc_mp();
-
-    if (!fully_identified(item))
-    {
-        set_ident_type(item, true);
-        set_ident_flags(item, ISFLAG_IDENT_MASK);
-    }
 }
 
 /**
@@ -447,8 +432,6 @@ static void _equip_weapon_effect(item_def& item, bool showMsgs, bool unmeld)
     {
     case OBJ_STAVES:
     {
-        set_ident_flags(item, ISFLAG_IDENT_MASK);
-        set_ident_type(OBJ_STAVES, item.sub_type, true);
         _wield_cursed(item, unmeld);
         break;
     }
@@ -460,25 +443,10 @@ static void _equip_weapon_effect(item_def& item, bool showMsgs, bool unmeld)
         if (artefact)
             _equip_artefact_effect(item, &showMsgs, unmeld, EQ_WEAPON);
 
-        const bool was_known      = item_type_known(item);
-
-        set_ident_flags(item, ISFLAG_IDENT_MASK);
-
         special = item.brand;
 
         if (artefact)
-        {
             special = artefact_property(item, ARTP_BRAND);
-
-            if (!was_known && !(item.flags & ISFLAG_NOTED_ID))
-            {
-                item.flags |= ISFLAG_NOTED_ID;
-
-                // Make a note of it.
-                take_note(Note(NOTE_ID_ITEM, 0, 0, item.name(DESC_A),
-                               origin_desc(item)));
-            }
-        }
 
         if (special != SPWPN_NORMAL)
         {
@@ -600,28 +568,8 @@ static void _equip_weapon_effect(item_def& item, bool showMsgs, bool unmeld)
                 }
             }
 
-            // effect second
-            switch (special)
-            {
-            case SPWPN_DISTORTION:
-                if (!was_known)
-                {
-                    // Xom loves it when you ID a distortion weapon this way,
-                    // and even more so if he gifted the weapon himself.
-                    if (origin_as_god_gift(item) == GOD_XOM)
-                        xom_is_stimulated(200);
-                    else
-                        xom_is_stimulated(100);
-                }
-                break;
-
-            case SPWPN_ANTIMAGIC:
+            if (special == SPWPN_ANTIMAGIC)
                 calc_mp();
-                break;
-
-            default:
-                break;
-            }
         }
 
         _wield_cursed(item, unmeld);
@@ -1250,20 +1198,10 @@ static void _equip_jewellery_effect(item_def &item, bool unmeld,
         break;
     }
 
-    bool new_ident = false;
-    // Artefacts have completely different appearance than base types
-    // so we don't allow them to make the base types known.
     if (artefact)
     {
         bool show_msgs = true;
         _equip_artefact_effect(item, &show_msgs, unmeld, slot);
-
-        set_ident_flags(item, ISFLAG_KNOW_PROPERTIES);
-    }
-    else
-    {
-        new_ident = set_ident_type(item, true);
-        set_ident_flags(item, ISFLAG_IDENT_MASK);
     }
 
     if (item.cursed() && !unmeld)
@@ -1277,8 +1215,6 @@ static void _equip_jewellery_effect(item_def &item, bool unmeld,
 
     if (!unmeld)
         mprf_nocap("%s", item.name(DESC_INVENTORY_EQUIP).c_str());
-    if (new_ident)
-        auto_assign_item_slot(item);
 }
 
 static void _deactivate_regeneration_item(const item_def &item, bool meld)

crawl-ref/source/shopping.cc
@@ -820,7 +820,8 @@ static bool _purchase(shop_struct& shop, const level_pos& pos, int index)
 
     origin_purchased(item);
 
-    if (shoptype_identifies_stock(shop.type))
+    if (shoptype_identifies_stock(shop.type)
+        || item_type_is_equipment(item.base_type))
     {
         // Identify the item and its type.
         // This also takes the ID note if necessary.

commit 5110eb77c4cdb4d8880e23280d69f544c6c1d886

Fix ID notes (dinky)
Broken in bb19ded6d30e696c234aeda7060a468f3fc7a340 .

crawl-ref/source/items.cc
@@ -981,19 +981,16 @@ static bool _id_floor_item(item_def &item)
         if (item_needs_autopickup(item))
             item.props["needs_autopickup"] = true;
 
-        if (is_artefact(item))
-        {
-            if (!(item.flags & ISFLAG_NOTED_ID))
-            {
-                item.flags |= ISFLAG_NOTED_ID;
-
-                // Make a note of it.
-                take_note(Note(NOTE_ID_ITEM, 0, 0, item.name(DESC_A),
-                               origin_desc(item)));
-            }
-        }
-
         set_ident_flags(item, ISFLAG_IDENT_MASK);
+
+        if (is_artefact(item) && !(item.flags & ISFLAG_NOTED_ID))
+        {
+            item.flags |= ISFLAG_NOTED_ID;
+
+            // Make a note of it.
+            take_note(Note(NOTE_ID_ITEM, 0, 0, item.name(DESC_A),
+                           origin_desc(item)));
+        }
         return true;
     }
 