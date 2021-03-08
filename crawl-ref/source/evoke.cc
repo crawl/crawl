c41e867b77275f225b1056c5d0dd2e62a99f8412
Kate <601195+semitonal@users.noreply.github.com>
2021-03-08 06:38:27 +0000

    Give black draconians increased breath power in dragon form
    Other draconian breath weapons already get a breath power boost, and the
    spell description also claimed that this applied for black draconians too.

--- crawl-ref/source/evoke.cc
+++ crawl-ref/source/evoke.cc
@@ -180,8 +180,9 @@ static bool _lightning_rod(dist *preselect)
 void black_drac_breath()
 {
     const int num_shots = roll_dice(2, 1 + you.experience_level / 7);
-    const int range = you.experience_level / 3 + 5; // 5--14
-    const int power = 25 + you.experience_level; // 25-52
+    const int range = you.experience_level / 3 + 5; // 5-14
+    const int power = 25 + (you.form == transformation::dragon
+                            ? 2 * you.experience_level : you.experience_level);
     for (int i = 0; i < num_shots; ++i)
         _spray_lightning(range, power);
 }
