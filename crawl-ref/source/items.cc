crawl/crawl

commit 83025b51d3308daaaf0f8057c1135c7b796a8623

Fix equipment identification
bb19ded didn't properly identify items that were already in the player's
inventory. This commit restores equip identify behavior, to fix this
and handle any unforeseen cases of items being placed directly into the
player's inventory.

Closes #1777.

crawl-ref/source/items.cc
@@ -941,6 +941,20 @@ void item_check()
     }
 }
 
+void identify_item(item_def& item)
+{
+    set_ident_flags(item, ISFLAG_IDENT_MASK);
+
+    if (is_artefact(item) && !(item.flags & ISFLAG_NOTED_ID))
+    {
+        item.flags |= ISFLAG_NOTED_ID;
+
+        // Make a note of it.
+        take_note(Note(NOTE_ID_ITEM, 0, 0, item.name(DESC_A),
+                       origin_desc(item)));
+    }
+}
+
 // Identify the object the player stepped on.
 // Books are fully identified.
 // Wands are only type-identified.
@@ -980,17 +994,7 @@ static bool _id_floor_item(item_def &item)
         // autopickup hack for previously-unknown items
         if (item_needs_autopickup(item))
             item.props["needs_autopickup"] = true;
-
-        set_ident_flags(item, ISFLAG_IDENT_MASK);
-
-        if (is_artefact(item) && !(item.flags & ISFLAG_NOTED_ID))
-        {
-            item.flags |= ISFLAG_NOTED_ID;
-
-            // Make a note of it.
-            take_note(Note(NOTE_ID_ITEM, 0, 0, item.name(DESC_A),
-                           origin_desc(item)));
-        }
+        identify_item(item);
         return true;
     }
 

crawl-ref/source/items.h
@@ -91,6 +91,7 @@ void lose_item_stack(const coord_def& where);
 
 string item_message(vector<const item_def *> const &items);
 void item_check();
+void identify_item(item_def& item);
 void request_autopickup(bool do_pickup = true);
 void id_floor_items();
 

crawl-ref/source/player-equip.cc
@@ -170,6 +170,8 @@ void equip_effect(equipment_type slot, int item_slot, bool unmeld, bool msg)
 
     const interrupt_block block_unmeld_interrupts(unmeld);
 
+    identify_item(item);
+
     if (slot == EQ_WEAPON)
         _equip_weapon_effect(item, msg, unmeld);
     else if (slot >= EQ_CLOAK && slot <= EQ_BODY_ARMOUR)

crawl-ref/source/shopping.cc
@@ -825,8 +825,7 @@ static bool _purchase(shop_struct& shop, const level_pos& pos, int index)
     {
         // Identify the item and its type.
         // This also takes the ID note if necessary.
-        set_ident_type(item, true);
-        set_ident_flags(item, ISFLAG_IDENT_MASK);
+        identify_item(item);
     }
 
     // Shopkeepers will place goods you can't carry outside the shop.

commit 6d67f77588c66d07f2197d56470a509974f6a09b

Fix shop purchase potion/scroll stacking
This was broken in 83025b5. It turns out that items_stack() has some fairly
particular conditions that don't necessarily match ISFLAG_IDENT_MASK, due
to a potential lua info leak. For some background on this, see:
crawl-dev-20151123.lg

If this commit somehow breaks identify again, I apologize in advance.

crawl-ref/source/items.cc
@@ -943,6 +943,11 @@ void item_check()
 
 void identify_item(item_def& item)
 {
+    // items_stack() has strict flag conditions to prevent a shop info leak,
+    // so we need set_ident_type() here to permit stacking shop purchases.
+    if (is_stackable_item(item))
+        set_ident_type(item, true);
+
     set_ident_flags(item, ISFLAG_IDENT_MASK);
 
     if (is_artefact(item) && !(item.flags & ISFLAG_NOTED_ID))