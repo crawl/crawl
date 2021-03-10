crawl/crawl

commit efe1b4415710e1faf530dd6caca231af667cf6ee

Refactor some eq slot checks
This abstracts hard-coded species-based eq slot checks (that don't
involve a mutation) to species.cc, and generally tries to clean up
various handling of ring/arm counts. There was a lot of special casing
around MUT_MISSING_HAND, custom octopus code, etc., that should at least
be better now.

crawl-ref/source/output.cc
@@ -586,7 +586,7 @@ static void _print_stats_equip(int x, int y)
 {
     CGOTOXY(x, y, GOTO_STAT);
     textcolour(HUD_CAPTION_COLOUR);
-    cprintf((you.species == SP_OCTOPODE) ? "Eq: " : "Equip: ");
+    cprintf((species_arm_count(you.species) > 2) ? "Eq: " : "Equip: ");
     textcolour(LIGHTGREY);
     for (equipment_type eqslot : e_order)
     {
@@ -1916,14 +1916,15 @@ static void _print_overview_screen_equip(column_composer& cols,
 
     for (equipment_type eqslot : e_order)
     {
-        if (you.species == SP_OCTOPODE
+        // leave space for all the ring slots
+        if (species_arm_count(you.species) > 2
             && eqslot != EQ_WEAPON
             && !you_can_wear(eqslot))
         {
             continue;
         }
 
-        if (you.species != SP_OCTOPODE
+        if (species_arm_count(you.species) <= 2
             && eqslot >= EQ_RING_ONE && eqslot <= EQ_RING_EIGHT)
         {
             continue;
@@ -2591,11 +2592,14 @@ string mutation_overview()
                                             !form_likes_water()));
     }
 
-    if (you.species == SP_OCTOPODE)
+    if (species_arm_count(you.species) > 2)
     {
         mutations.push_back(_annotate_form_based(
-            make_stringf("%d rings", you.has_tentacles(false)),
+            make_stringf("%d rings", you.arm_count()),
             !get_form()->slot_available(EQ_RING_EIGHT)));
+    }
+    if (you.species == SP_OCTOPODE)
+    {
         mutations.push_back(_annotate_form_based(
             make_stringf("constrict %d", you.has_tentacles(false)),
             !form_keeps_mutations()));

crawl-ref/source/acquire.cc
@@ -556,9 +556,7 @@ static int _acquirement_jewellery_subtype(bool /*divine*/, int & /*quantity*/,
     int result = 0;
 
     // Rings are (number of usable rings) times as common as amulets.
-    // XXX: unify this with the actual check for ring slots
-    const int ring_num = (you.species == SP_OCTOPODE ? 8 : 2)
-                       - (you.get_mutation_level(MUT_MISSING_HAND) ? 1 : 0);
+    const int ring_num = you.arm_count();
 
     // Try ten times to give something the player hasn't seen.
     for (int i = 0; i < 10; i++)

crawl-ref/source/command.cc
@@ -299,7 +299,7 @@ void list_jewellery()
 {
     string jstr;
     int cols = get_number_of_cols() - 1;
-    bool split = you.species == SP_OCTOPODE && cols > 84;
+    bool split = species_arm_count(you.species) > 2 && cols > 84;
 
     for (int j = EQ_LEFT_RING; j < NUM_EQUIP; j++)
     {
@@ -346,7 +346,8 @@ void list_jewellery()
                            split && i > EQ_AMULET ? (cols - 1) / 2 : cols);
         item = colour_string(item, colour);
 
-        if (i == EQ_RING_SEVEN && you.species == SP_OCTOPODE &&
+        // doesn't handle arbitrary arm counts
+        if (i == EQ_RING_SEVEN && species_arm_count(you.species) == 8 &&
                 you.get_mutation_level(MUT_MISSING_HAND))
         {
             mprf(MSGCH_EQUIPMENT, "%s", item.c_str());

crawl-ref/source/god-abil.cc
@@ -4325,12 +4325,10 @@ static void _extra_sacrifice_code(ability_type sac)
     const sacrifice_def &sac_def = _get_sacrifice_def(sac);
     if (sac_def.sacrifice == ABIL_RU_SACRIFICE_HAND)
     {
-        equipment_type ring_slot;
-
-        if (you.species == SP_OCTOPODE)
-            ring_slot = EQ_RING_EIGHT;
-        else
-            ring_slot = EQ_LEFT_RING;
+        auto ring_slots = species_ring_slots(you.species);
+        // the last one gets sacrificed: either EQ_LEFT_RING or EQ_RING_EIGHT
+        equipment_type ring_slot = ring_slots.back();
+        ring_slots.pop_back();
 
         item_def* const shield = you.slot_item(EQ_SHIELD, true);
         item_def* const weapon = you.slot_item(EQ_WEAPON, true);
@@ -4360,25 +4358,13 @@ static void _extra_sacrifice_code(ability_type sac)
         // And one ring
         if (ring != nullptr)
         {
-            if (you.species == SP_OCTOPODE)
-            {
-                for (int eq = EQ_RING_ONE; eq <= EQ_RING_SEVEN; eq++)
-                {
-                    if (!you.slot_item(static_cast<equipment_type>(eq), true))
-                    {
-                        open_ring_slot = true;
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                if (!you.slot_item(static_cast<equipment_type>(
-                    EQ_RIGHT_RING), true))
+            // XX does not handle an open slot on the finger amulet
+            for (const auto &eq : ring_slots)
+                if (!you.slot_item(eq, true))
                 {
                     open_ring_slot = true;
+                    break;
                 }
-            }
 
             mprf("You can no longer wear %s!",
                 ring->name(DESC_YOUR).c_str());
@@ -4387,7 +4373,7 @@ static void _extra_sacrifice_code(ability_type sac)
             {
                 mprf("You put %s back on %s %s!",
                      ring->name(DESC_YOUR).c_str(),
-                     (you.species == SP_OCTOPODE ? "another" : "your other"),
+                     (ring_slots.size() > 1 ? "another" : "your other"),
                      you.hand_name(true).c_str());
                 puton_ring(ring_inv_slot, false);
             }

crawl-ref/source/god-passive.cc
@@ -601,53 +601,32 @@ void ash_check_bondage()
     for (int j = EQ_FIRST_EQUIP; j < NUM_EQUIP; j++)
     {
         const equipment_type i = static_cast<equipment_type>(j);
-        // Missing hands mean fewer rings
-        if (you.species != SP_OCTOPODE && i == EQ_LEFT_RING
-                 && you.get_mutation_level(MUT_MISSING_HAND))
-        {
+
+        // handles missing hand, octopode ring slots, finger necklace, species
+        // armour restrictions, etc. Finger necklace slot counts.
+        if (you_can_wear(i) == MB_FALSE)
             continue;
-        }
-        // Octopodes don't count these slots:
-        else if (you.species == SP_OCTOPODE
-                 && ((i == EQ_LEFT_RING || i == EQ_RIGHT_RING)
-                     || (i == EQ_RING_EIGHT
-                         && you.get_mutation_level(MUT_MISSING_HAND))))
-        {
-            continue;
-        }
-        // *Only* octopodes count these slots:
-        else if (you.species != SP_OCTOPODE
-                 && i >= EQ_RING_ONE && i <= EQ_RING_EIGHT)
-        {
-            continue;
-        }
-        // The macabre finger necklace's extra slot does count if equipped.
-        else if (!player_equip_unrand(UNRAND_FINGER_AMULET)
-                 && i == EQ_RING_AMULET)
-        {
-            continue;
-        }
 
         // transformed away slots are still considered to be possibly bound
-        if (you_can_wear(i))
+        num_slots++;
+        if (you.equip[i] != -1)
         {
-            num_slots++;
-            if (you.equip[i] != -1)
+            const item_def& item = you.inv[you.equip[i]];
+            if (item.cursed() && (i != EQ_WEAPON || is_weapon(item)))
             {
-                const item_def& item = you.inv[you.equip[i]];
-                if (item.cursed() && (i != EQ_WEAPON || is_weapon(item)))
+                if (i == EQ_WEAPON && _two_handed())
+                    num_cursed += 2;
+                else
                 {
-                    if (i == EQ_WEAPON && _two_handed())
-                        num_cursed += 2;
-                    else
+                    num_cursed++;
+                    if (i == EQ_BODY_ARMOUR
+                        && is_unrandom_artefact(item, UNRAND_LEAR))
                     {
-                        num_cursed++;
-                        if (i == EQ_BODY_ARMOUR && is_unrandom_artefact(item, UNRAND_LEAR))
-                            num_cursed += 3;
+                        num_cursed += 3;
                     }
-                    if (!item_is_melded(item))
-                        _curse_boost_skills(item);
                 }
+                if (!item_is_melded(item))
+                    _curse_boost_skills(item);
             }
         }
     }

crawl-ref/source/item-use.cc
@@ -859,7 +859,7 @@ bool can_wear_armour(const item_def &item, bool verbose, bool ignore_temporary)
     if (base_type != OBJ_ARMOUR || you.has_mutation(MUT_NO_ARMOUR))
     {
         if (verbose)
-            mpr("You can't wear that.");
+            mpr("You can't wear that!");
 
         return false;
     }
@@ -867,19 +867,22 @@ bool can_wear_armour(const item_def &item, bool verbose, bool ignore_temporary)
     const int sub_type = item.sub_type;
     const equipment_type slot = get_armour_slot(item);
 
-    if (you.species == SP_OCTOPODE && slot != EQ_HELMET && slot != EQ_SHIELD)
-    {
-        if (verbose)
-            mpr("You can't wear that!");
-        return false;
-    }
-
-    if (species_is_draconian(you.species) && slot == EQ_BODY_ARMOUR)
+    if (species_bans_eq(you.species, slot))
     {
         if (verbose)
         {
-            mprf("Your wings%s won't fit in that.", you.has_mutation(MUT_BIG_WINGS)
-                 ? "" : ", even vestigial as they are,");
+            // *if* the species bans body armour, then blame it on wings.
+            // (But don't unconditionally ban armour with this mut.)
+            // XX turn this mut into a two-level mutation?
+            if (slot == EQ_BODY_ARMOUR
+                && species_mutation_level(you.species, MUT_BIG_WINGS))
+            {
+                mprf("Your wings%s won't fit in that.",
+                    you.has_mutation(MUT_BIG_WINGS)
+                        ? "" : ", even vestigial as they are,");
+            }
+            else
+                mpr("You can't wear that!");
         }
         return false;
     }
@@ -898,7 +901,7 @@ bool can_wear_armour(const item_def &item, bool verbose, bool ignore_temporary)
     {
         if (verbose)
         {
-            if (you.species == SP_OCTOPODE)
+            if (you.species == SP_OCTOPODE) // XX ugly hardcoding -- but Op locomotion is unique
                 mpr("You need the rest of your tentacles for walking.");
             else
                 mprf("You'd need another %s to do that!", you.hand_name(false).c_str());
@@ -1368,31 +1371,16 @@ bool takeoff_armour(int item)
 // Returns a list of possible ring slots.
 static vector<equipment_type> _current_ring_types()
 {
-    vector<equipment_type> ret;
-    if (you.species == SP_OCTOPODE)
-    {
-        for (int i = 0; i < 8; ++i)
-        {
-            const equipment_type slot = (equipment_type)(EQ_RING_ONE + i);
-
-            if (you.get_mutation_level(MUT_MISSING_HAND)
-                && slot == EQ_RING_EIGHT)
-            {
-                continue;
-            }
-
-            if (get_form()->slot_available(slot))
-                ret.push_back(slot);
-        }
-    }
-    else
-    {
-        if (you.get_mutation_level(MUT_MISSING_HAND) == 0)
-            ret.push_back(EQ_LEFT_RING);
-        ret.push_back(EQ_RIGHT_RING);
-    }
+    vector<equipment_type> ret = species_ring_slots(you.species);
+    if (you.has_mutation(MUT_MISSING_HAND))
+        ret.pop_back();
     if (player_equip_unrand(UNRAND_FINGER_AMULET))
         ret.push_back(EQ_RING_AMULET);
+
+    erase_if(ret, [](const equipment_type &e)
+        {
+            return !get_form()->slot_available(e);
+        });
     return ret;
 }
 

crawl-ref/source/mutation-data.h
@@ -844,7 +844,7 @@ static const mutation_def mut_data[] =
    "The barb on your tail seems less sharp."},
 },
 
-// Draconian only
+// Draconian/gargoyle only
 { MUT_BIG_WINGS, 4, 1, mutflag::good, true,
   "big wings",
 

crawl-ref/source/mutation.cc
@@ -679,14 +679,18 @@ string describe_mutations(bool drop_title)
     }
     // XX if there is ever a giant player species, check it here
 
-    if (you.species == SP_OCTOPODE)
+    if (species_arm_count(you.species) > 2)
     {
-        const string num_tentacles =
-               number_in_words(you.has_usable_tentacles(false));
+        const string num_arms = number_in_words(you.arm_count());
         result += _annotate_form_based(
             make_stringf("You can wear up to %s rings at the same time.",
-                         num_tentacles.c_str()),
+                         num_arms.c_str()),
             !get_form()->slot_available(EQ_RING_EIGHT));
+    }
+
+    if (you.species == SP_OCTOPODE)
+    {
+        const string num_tentacles = number_in_words(you.has_tentacles(false));
         result += _annotate_form_based(
             make_stringf("You can use your tentacles to constrict %s enemies at once.",
                          num_tentacles.c_str()),

commit f99593c837f11c4b3d2d65ae705e1ef558ead745

Swap L/R ring in % display order
efe1b44 accidentally changed which finger the first ring goes on. This
is actually good, because it resolves 11879 -- the first ring and the
sacrifice hand are drawn from a consistent order where they are always
different. However, now the order in % with only one ring looks odd.
This prints the right ring first. (Sorry lefties..)

crawl-ref/source/output.cc
@@ -576,7 +576,7 @@ static int _count_digits(int val)
 static const equipment_type e_order[] =
 {
     EQ_WEAPON, EQ_SHIELD, EQ_BODY_ARMOUR, EQ_HELMET, EQ_CLOAK,
-    EQ_GLOVES, EQ_BOOTS, EQ_AMULET, EQ_LEFT_RING, EQ_RIGHT_RING,
+    EQ_GLOVES, EQ_BOOTS, EQ_AMULET, EQ_RIGHT_RING, EQ_LEFT_RING,
     EQ_RING_ONE, EQ_RING_TWO, EQ_RING_THREE, EQ_RING_FOUR,
     EQ_RING_FIVE, EQ_RING_SIX, EQ_RING_SEVEN, EQ_RING_EIGHT,
     EQ_RING_AMULET,