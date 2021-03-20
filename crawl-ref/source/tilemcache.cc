f5bc2b81cb128df785ef33a8f8946f51c274c642
Nicholas Feinberg <pleasingfung@gmail.com>
2021-03-20 16:22:41 -0700

    Show weapons on new Balrug tile
    ??oops

--- crawl-ref/source/tilemcache.cc
+++ crawl-ref/source/tilemcache.cc
@@ -372,6 +372,7 @@ bool mcache_monster::get_weapon_offset(tileidx_t mon_tile,
     case TILEP_MONS_TENGU_WARRIOR:
     case TILEP_MONS_TENGU_REAVER:
     case TILEP_MONS_SOJOBO:
+    case TILEP_MONS_BALRUG:
         *ofs_x = -2;
         *ofs_y = 0;
         break;
