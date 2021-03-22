0b40708614ad4b09fe87efaf589ddcb4328ac3fb
Kate <601195+semitonal@users.noreply.github.com>
2021-03-22 01:28:16 +0000

    Staticify some functions

--- crawl-ref/source/mutation.cc
+++ crawl-ref/source/mutation.cc
@@ -637,7 +637,7 @@ void validate_mutations(bool debug_msg)
     ASSERT(total_temp == you.attribute[ATTR_TEMP_MUTATIONS]);
 }
 
-string _terse_mut_name(mutation_type mut)
+static string _terse_mut_name(mutation_type mut)
 {
     const int current_level = you.get_mutation_level(mut);
     const int base_level = you.get_base_mutation_level(mut);
@@ -689,7 +689,7 @@ static bool _is_appendage_mutation(mutation_type mut)
     return false;
 }
 
-vector<string> _get_mutations(bool terse)
+static vector<string> _get_mutations(bool terse)
 {
     vector<string> result;
 
