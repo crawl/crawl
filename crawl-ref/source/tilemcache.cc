37402fc473c1ba680e8afbe17472d67476fed1ec
Nicholas Feinberg <pleasingfung@gmail.com>
2021-03-25 08:32:35 -0700

    Add good dogs (Sastrei)
    New tiles for hound, wolf, gnoll sergeant, gnoll shaman, and Grum.

--- crawl-ref/source/tilemcache.cc
+++ crawl-ref/source/tilemcache.cc
@@ -348,9 +348,6 @@ bool mcache_monster::get_weapon_offset(tileidx_t mon_tile,
         *ofs_y = 0;
         break;
     // Shift to the left.
-    case TILEP_MONS_GNOLL_SHAMAN:
-    case TILEP_MONS_GNOLL_SERGEANT:
-    case TILEP_MONS_GRUM:
     case TILEP_MONS_CRAZY_YIUF:
     case TILEP_MONS_DEEP_ELF_DEATH_MAGE:
         *ofs_x = -1;
@@ -358,6 +355,7 @@ bool mcache_monster::get_weapon_offset(tileidx_t mon_tile,
         break;
     case TILEP_MONS_HOBGOBLIN:
     case TILEP_MONS_ROBIN:
+    case TILEP_MONS_GNOLL_SERGEANT:
     case TILEP_MONS_TIAMAT:
     case TILEP_MONS_TIAMAT_1:
     case TILEP_MONS_TIAMAT_2:
@@ -381,6 +379,8 @@ bool mcache_monster::get_weapon_offset(tileidx_t mon_tile,
     case TILEP_MONS_EFREET:
     case TILEP_MONS_VAMPIRE_MAGE:
     case TILEP_MONS_GNOLL:
+    case TILEP_MONS_GRUM:
+    case TILEP_MONS_GNOLL_SHAMAN:
         *ofs_x = -3;
         *ofs_y = 0;
         break;
