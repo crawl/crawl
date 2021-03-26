6bb3289e1e0e37f7f60b977e9adf4b9cb21f3ff6
gammafunk <gammafunk@gmail.com>
2021-03-26 00:16:58 -0500

    checkwhite player.cc

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -2606,7 +2606,7 @@ static void _gain_innate_spells()
         auto spindex = find(begin(you.spells), end(you.spells), spell);
         if (spindex != end(you.spells))
             continue; // already learned that one
-        
+
         mprf("The power to cast %s wells up from within.", spell_title(spell));
         add_spell_to_memory(spell);
     }
