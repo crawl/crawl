41e92d2c55444cfbd6ba5968babec2286685af09
Edgar A. Bering IV <trizor@gmail.com>
2021-03-29 08:06:20 +0300

    Restore the gnoll net equip rate
    Before the addition of ironbound beastmasters it was 1 in 20;
    after, because of fallthrough, it became 1 in 200.

--- crawl-ref/source/mon-gear.cc
+++ crawl-ref/source/mon-gear.cc
@@ -1479,7 +1479,7 @@ static void _give_ammo(monster* mon, int level, bool mons_summoned)
 
         case MONS_DRACONIAN_KNIGHT:
         case MONS_GNOLL:
-            if (!level || !one_chance_in(20))
+            if (!level || coinflip())
                 break;
             // deliberate fall-through
         case MONS_IRONBOUND_BEASTMASTER:
