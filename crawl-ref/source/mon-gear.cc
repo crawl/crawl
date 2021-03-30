39f900b3a3e3aadc112d7d638113ec4f20d8ce0f
Nicholas Feinberg <pleasingfung@gmail.com>
2021-03-30 12:41:00 -0700

    Remove D:1 halberd gnolls
    In line with removing d:1 gnoll bands and gnolls with nets, give players
    a little longer before experiencing the true, unadulterated terror of
    Gnoll. (Unless they're playing gnollcrawl.)

--- crawl-ref/source/mon-gear.cc
+++ crawl-ref/source/mon-gear.cc
@@ -1043,6 +1043,10 @@ int make_mons_weapon(monster_type type, int level, bool melee_only)
         }
         break;
 
+    case MONS_GNOLL:
+        if (!level && item.is_type(OBJ_WEAPONS, WPN_HALBERD))
+            item.sub_type = WPN_CLUB;
+
     case MONS_WIGHT:
         if (coinflip())
         {
