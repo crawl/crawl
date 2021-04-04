dc75bf111db0d0839d05504a72f15b19978b5045
Nikolai Lavsky <3328424+nlavsky@users.noreply.github.com>
2021-04-04 21:14:10 +0300

    Fix web clients not getting updated about the state of XP evokers
    Whenever an inventory item's item_def changes, the game sends an update
    to the web client(s). Because information about XP evokers is stored
    outside of item_defs, there are no updates when XP evokers recharge
    or lose charges.
    
    Fix this by caching the number of charges in the item.plus field
    (similar to how wand charges are stored) and sending updates when
    necessary.

--- crawl-ref/source/tileweb.cc
+++ crawl-ref/source/tileweb.cc
@@ -1123,6 +1123,7 @@ void TilesFramework::_send_item(item_def& current, const item_def& next,
                                 bool force_full)
 {
     bool changed = false;
+    bool xp_evoker_changed = false;
     bool defined = next.defined();
 
     if (force_full || current.base_type != next.base_type)
@@ -1144,8 +1145,19 @@ void TilesFramework::_send_item(item_def& current, const item_def& next,
 
     changed |= _update_int(force_full, current.sub_type, next.sub_type,
                            "sub_type", false);
-    changed |= _update_int(force_full, current.plus, next.plus,
-                           PLUS_KEY, false);
+    if (is_xp_evoker(next))
+    {
+        short int next_charges = evoker_charges(next.sub_type);
+        xp_evoker_changed = _update_int(force_full, current.plus,
+                                        next_charges,
+                                        PLUS_KEY, false);
+        changed |= xp_evoker_changed;
+    }
+    else
+    {
+        changed |= _update_int(force_full, current.plus, next.plus,
+                               PLUS_KEY, false);
+    }
     changed |= _update_int(force_full, current.plus2, next.plus2,
                            "plus2", false);
     changed |= _update_int(force_full, current.flags, next.flags,
@@ -1161,8 +1173,11 @@ void TilesFramework::_send_item(item_def& current, const item_def& next,
     if (changed && defined)
     {
         string name = next.name(DESC_A, true, false, true);
-        if (force_full || current.name(DESC_A, true, false, true) != name)
+        if (force_full || current.name(DESC_A, true, false, true) != name
+            || xp_evoker_changed)
+        {
             json_write_string("name", name);
+        }
 
         const string prefix = item_prefix(next);
         const int prefcol = menu_colour(next.name(DESC_INVENTORY), prefix);
@@ -1178,7 +1193,7 @@ void TilesFramework::_send_item(item_def& current, const item_def& next,
         }
 
         tileidx_t tile = tileidx_item(next);
-        if (force_full || tileidx_item(current) != tile)
+        if (force_full || tileidx_item(current) != tile || xp_evoker_changed)
         {
             json_open_array("tile");
             tileidx_t base_tile = tileidx_known_base_item(tile);
@@ -1189,6 +1204,8 @@ void TilesFramework::_send_item(item_def& current, const item_def& next,
         }
 
         current = next;
+        if (is_xp_evoker(current))
+            current.plus = evoker_charges(current.sub_type);
     }
 }
 
