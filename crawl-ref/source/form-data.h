08c2d0c9631fa2c2aec2f480533cfa3b1bfe67e0
dilly <65288602+dillytin@users.noreply.github.com>
2021-06-09 17:22:53 +0900

    Remove -Wiz from blade hand form
    Blade hand is now level 6 spell and has become much harder to find. You can only find it from a book of Transfiguration which contains statue form and dragon form. Blade hand is now not decent as before when it was level 5 and in a book of Changes or not attactive, compared to statue form which gives you a good deal of AC and resistance and makes you free from ER from body armour so that you can cast spells easily(and still you can wear shields!). Therefore, I suggest Blade hand form should have no -wiz penalty. It already requires a lot of exp and stats to stabilise. I want to make blade hand attractive as the same level spell: statue form.

--- crawl-ref/source/form-data.h
+++ crawl-ref/source/form-data.h
@@ -89,7 +89,7 @@ static const form_entry formdata[] =
     "",
     EQF_HANDS, MR_NO_FLAGS,
     FormDuration(10, PS_SINGLE, 100), 0, 0, SIZE_CHARACTER, 10,
-    0, 0, 0, true, 20, true, 22,
+    0, 0, 0, true, 0, true, 22,
     SPWPN_NORMAL, RED, "", { "hit", "slash", "slice", "shred" },
     FC_DEFAULT, FC_DEFAULT, FC_DEFAULT, true,
     "", 0, "", "", "", "",
