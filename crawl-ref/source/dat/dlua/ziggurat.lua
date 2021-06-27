14994ecaaf4b7c377a5fcc03de740d40b6dc3504
Kate <601195+semitonal@users.noreply.github.com>
2021-06-27 20:27:43 +0100

    Adjust Slime floor zig scaling
    To replace some of the azure jelly weight with rockslimes and quicksilver
    oozes. They're unlikely to be especially worrying threats, but they do
    probably at least deserve some additional weight as the new top-end Slime
    monsters.

--- crawl-ref/source/dat/dlua/ziggurat.lua
+++ crawl-ref/source/dat/dlua/ziggurat.lua
@@ -249,7 +249,8 @@ end))
 mset(spec_fn(function ()
   local d = 5 + 5 * you.zigs_completed()
   return "place:Slime:$ w:1500 / floating eye w:" .. d .. " / " ..
-         "azure jelly w:" .. d * 5 .. " / acid blob w:" .. d * 14
+         "azure jelly w:" .. d .. " / quicksilver ooze w:" .. d .. " / " ..
+         "rockslime w:" .. d * 3 .. " / acid blob w:" .. d * 14
 end))
 
 mset(spec_fn(function ()
