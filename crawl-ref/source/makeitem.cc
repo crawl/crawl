96bc1138baa82ce58550d9a6e9f325009f9786a4
Nicholas Feinberg <pleasingfung@gmail.com>
2021-07-03 16:17:24 -0700

    One more book freq tweak
    Per objstat, this gets average spells generated in Lair and D back
    to the same levels they were at pre-recent-changes.

--- crawl-ref/source/makeitem.cc
+++ crawl-ref/source/makeitem.cc
@@ -1829,12 +1829,12 @@ int items(bool allow_uniques,
     else
     {
         ASSERT(force_type == OBJ_RANDOM);
-        // Total weight: 1810
+        // Total weight: 1795
         item.base_type = random_choose_weighted(
                                     10, OBJ_STAVES,
                                     45, OBJ_JEWELLERY,
+                                    60, OBJ_BOOKS,
                                     70, OBJ_WANDS,
-                                    75, OBJ_BOOKS,
                                    212, OBJ_ARMOUR,
                                    212, OBJ_WEAPONS,
                                    176, OBJ_POTIONS,
