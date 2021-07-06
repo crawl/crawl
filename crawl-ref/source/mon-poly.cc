dd0959613029cc62119412e88740ee14c8ab0e77
Edgar A. Bering IV <trizor@gmail.com>
2021-07-06 23:48:45 +0300

    fix: end engulf upon polymorph
    Closes #1935. Polymorph is described as the monster evaporating and
    reforming, so the lore solution to this bug of "you are now engulfed by
    flesh", while terrifying, doesn't quite fit.

--- crawl-ref/source/mon-poly.cc
+++ crawl-ref/source/mon-poly.cc
@@ -411,6 +411,7 @@ void change_monster_type(monster* mons, monster_type targetc)
     // evaporating and reforming justifies this behaviour.
     mons->stop_constricting_all();
     mons->stop_being_constricted();
+    mons->clear_far_engulf();
 }
 
 // Is the new monster able to live in *any* habitat that the original
