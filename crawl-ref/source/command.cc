d41220b2a51eeedbfed3638612a6da8e56ad5798
Edgar A. Bering IV <trizor@gmail.com>
2021-07-06 18:29:17 +0300

    text: adjust a command description

--- crawl-ref/source/command.cc
+++ crawl-ref/source/command.cc
@@ -910,7 +910,7 @@ static void _add_formatted_keyhelp(column_composer &cols)
     _add_command(cols, 1, CMD_QUIVER_ITEM, "select action to be Quivered", 2);
     _add_command(cols, 1, CMD_SWAP_QUIVER_RECENT, "swap between most recent quiver actions", 2);
     _add_command(cols, 1, CMD_QUAFF, "Quaff a potion", 2);
-    _add_command(cols, 1, CMD_READ, "Read a scroll (or book on floor)", 2);
+    _add_command(cols, 1, CMD_READ, "Read a scroll", 2);
     _add_command(cols, 1, CMD_WIELD_WEAPON, "Wield an item (<w>-</w> for none)", 2);
     _add_command(cols, 1, CMD_WEAPON_SWAP, "wield item a, or switch to b", 2);
 
