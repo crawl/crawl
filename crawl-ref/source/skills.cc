5dd80122531a7252015baefc20bfd966f9c3501f
Nicholas Feinberg <pleasingfung@gmail.com>
2021-07-09 07:41:10 -0700

    New Holy Roller title (PF (particleface))
    Palentonga Invo 27 with any good god.

--- crawl-ref/source/skills.cc
+++ crawl-ref/source/skills.cc
@@ -1725,6 +1725,8 @@ string skill_title_by_rank(skill_type best_skill, uint8_t skill_rank,
                 result = "Blood Saint";
             else if (species == SP_PALENTONGA && skill_rank == 5 && god == GOD_QAZLAL)
                 result = "Rolling Thunder";
+            else if (species == SP_PALENTONGA && skill_rank == 5 && is_good_god(god))
+                result = "Holy Roller";
             else if (species == SP_MUMMY && skill_rank == 5 && god == GOD_NEMELEX_XOBEH)
                 result = "Forbidden One";
             else if (species == SP_VINE_STALKER && skill_rank == 5 && god == GOD_NEMELEX_XOBEH)
