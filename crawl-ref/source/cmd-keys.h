crawl/crawl

commit 1715c4e68f8d19e1d8e3bfa059267a9559dcc3b3

feat: try to map more currency symbols to CMD_LIST_GOLD
I am not sure whether this will (a) build on every platform, or (b) work
on every build target. When testing on a US keyboard layout on mac, I
can consistently get these to work on console, but not on the other two.
However, I think this is because I need to enter them as complex chorded
keystrokes involving number keys, where local tiles number key
processing interferes. Not sure what's happening in webtiles at all, but
on a US layout these definitely don't work for me. We'll see what, if
anything, happens on other layouts.

crawl-ref/source/cmd-keys.h
@@ -104,6 +104,10 @@
 {'?', CMD_DISPLAY_COMMANDS},
 {'!', CMD_ANNOTATE_LEVEL},
 {'$', CMD_LIST_GOLD},
+{u'€', CMD_LIST_GOLD},
+{u'£', CMD_LIST_GOLD},
+{u'¥', CMD_LIST_GOLD},
+{u'₩', CMD_LIST_GOLD},
 {CONTROL('D'), CMD_MACRO_ADD},
 {'~', CMD_MACRO_ADD},
 #ifdef WIZARD

commit d5c625afe77c71fc25375012f3990ed5eb2daa9c

fix: reorder currency symbols so that `$` shows in help
Bug introduced in 1715c4e68; the help menu can't handle unicode
keybindings and shows a number, and `$` is probably the best one to show
anyways.

crawl-ref/source/cmd-keys.h
@@ -103,11 +103,11 @@
 {'=', CMD_ADJUST_INVENTORY},
 {'?', CMD_DISPLAY_COMMANDS},
 {'!', CMD_ANNOTATE_LEVEL},
-{'$', CMD_LIST_GOLD},
 {u'€', CMD_LIST_GOLD},
 {u'£', CMD_LIST_GOLD},
 {u'¥', CMD_LIST_GOLD},
 {u'₩', CMD_LIST_GOLD},
+{'$', CMD_LIST_GOLD},
 {CONTROL('D'), CMD_MACRO_MENU},
 {CONTROL('E'), CMD_MACRO_ADD},
 {'~', CMD_GAME_MENU},