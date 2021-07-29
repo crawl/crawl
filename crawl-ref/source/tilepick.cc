40d131073a73bcd40423f0577f10aa0ff1ce4601
Edgar A. Bering IV <trizor@gmail.com>
2021-07-29 18:00:14 +0300

    tile: Word of Chaos ability icon (CanOfWorms)
    (cherry picked from commit 0740493359141c00c6986e47fe40e4601178bda3)

--- crawl-ref/source/tilepick.cc
+++ crawl-ref/source/tilepick.cc
@@ -3445,7 +3445,7 @@ tileidx_t tileidx_ability(const ability_type ability)
     case ABIL_DAMNATION:
         return TILEG_ABILITY_HURL_DAMNATION;
     case ABIL_WORD_OF_CHAOS:
-        return TILEG_ERROR; // TODO
+        return TILEG_ABILITY_WORD_OF_CHAOS;
     // Vampires
     case ABIL_TRAN_BAT:
         return TILEG_ABILITY_BAT_FORM;
