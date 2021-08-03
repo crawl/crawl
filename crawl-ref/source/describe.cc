9707f7362f617ad0b53f7dfd6ce56dd436d4c4d4
advil <rawlins@gmail.com>
2021-08-03 12:33:36 -0400

    fix: mark ENCH_SHORT_LIVED in monster descriptions
    Indicate that briar patches will crumble away.

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -4741,7 +4741,9 @@ void get_monster_db_desc(const monster_info& mi, describe_info &inf,
             inf.body << "\nIt is quickly melting away.\n";
         break;
 
+    case MONS_BRIAR_PATCH: // death msg uses "crumbling"
     case MONS_PILLAR_OF_SALT:
+        // XX why are these "quick" here but "slow" elsewhere??
         if (mi.is(MB_SLOWLY_DYING))
             inf.body << "\nIt is quickly crumbling away.\n";
         break;
