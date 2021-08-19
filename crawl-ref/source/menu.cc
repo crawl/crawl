crawl/crawl

commit da2e87d06967aee713c8032367f989cd630df5ba

fix: off-by-one error in menu calculations
This was causing javascript exceptions in webtiles with 1-item menus.
I'm not sure it was otherwise very consequential, maybe visual glitches
when spectating? I'm actually still not sure that this is being
calculated right (based on some output debugging) but it isn't causing
invalid menu jump_to targets any more, that I can find.

crawl-ref/source/menu.cc
@@ -1484,7 +1484,7 @@ int Menu::get_first_visible() const
         // why does this use y2? It can lead to partially visible items in tiles
         int item_y2;
         m_ui.menu->get_item_region(i, nullptr, &item_y2);
-        if (item_y2 > y)
+        if (item_y2 >= y)
             return i;
     }
     return items.size();

commit 298df9dbad5ca674615179f8b034e7716c86723c

fix: refix something that is *not* an off-by-one-error
da2e87d06967ae turns out not to have been an off-by-one error, but
rather the result of bad initial calculations when the menu is not yet
displayed, resulting in invalid scroll values. In addition,
da2e87d06967ae broke scrolling in menus that don't use a hover. This
wasn't noticed before now partly because webtiles still works, the bug
seems to be a bit erratic, and in the words of one dev, "oh so that key
is actually broke and its not my broken keyboard".

crawl-ref/source/menu.cc
@@ -1503,12 +1503,12 @@ void Menu::deselect_all(bool update_view)
 int Menu::get_first_visible() const
 {
     int y = m_ui.scroller->get_scroll();
-    for (int i = 0; i < (int)items.size(); i++)
+    for (int i = 0; i < (int) items.size(); i++)
     {
         // why does this use y2? It can lead to partially visible items in tiles
         int item_y2;
         m_ui.menu->get_item_region(i, nullptr, &item_y2);
-        if (item_y2 >= y)
+        if (item_y2 > y)
             return i;
     }
     return items.size();
@@ -2189,6 +2189,9 @@ bool Menu::snap_in_page(int index)
 {
     if (index < 0 || index >= static_cast<int>(items.size()))
         return false;
+    const int vph = m_ui.scroller->get_region().height;
+    if (vph == 0) // ui not yet set up
+        return false;
 
     int y1, y2;
     m_ui.menu->get_item_region(index, &y1, &y2);
@@ -2197,8 +2200,7 @@ bool Menu::snap_in_page(int index)
     // important when the first item in a menu is a header
     if (index >= 1 && items[index - 1]->level != MEL_ITEM)
         m_ui.menu->get_item_region(index - 1, &y1, nullptr);
-    int vph = m_ui.scroller->get_region().height;
-    int vpy = m_ui.scroller->get_scroll();
+    const int vpy = m_ui.scroller->get_scroll();
 
     if (y2 >= vpy + vph)
         m_ui.scroller->set_scroll(y2 - vph