e2b30c500c89bc6e6742a274b68062011ef8d27c
John Stahara <john.stahara@gmail.com>
2021-08-19 18:14:11 -0700

    "Okay, then" on saying 'n' to warn_hatches prompts

--- crawl-ref/source/main.cc
+++ crawl-ref/source/main.cc
@@ -1468,9 +1468,22 @@ static bool _prompt_stairs(dungeon_feature_type ygrd, bool down, bool shaft)
     if (Options.warn_hatches)
     {
         if (feat_is_escape_hatch(ygrd))
-            return yesno("Really go through this one-way escape hatch?", true, 'n');
+        {
+            if (!yesno("Really go through this one-way escape hatch?", true, 'n'))
+            {
+                canned_msg(MSG_OK);
+                return false;
+            }
+        }
+
         if (down && shaft) // voluntary shaft usage
-            return yesno("Really dive through this shaft in the floor?", true, 'n');
+        {
+            if (!yesno("Really dive through this shaft in the floor?", true, 'n'))
+            {
+                canned_msg(MSG_OK);
+                return false;
+            }
+        }
     }
 
     return true;
