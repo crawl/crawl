36ea832cf17e4b1ba76f3108118dad86836cb23a
John Stahara <john.stahara@gmail.com>
2021-08-23 12:31:56 -0700

    Don't ever consider books evil
    Unfortunate from a flavour perspective, but in gameplay terms it's never
    detrimental to read a book.  This allows autopickup to read books, even
    if your current god forbids you from casting the spells contained
    within.  It isn't the knowledge that's harmful, after all, but how you
    use it!

--- crawl-ref/source/god-item.cc
+++ crawl-ref/source/god-item.cc
@@ -159,8 +159,6 @@ bool is_evil_item(const item_def& item, bool calc_unid)
         return item.sub_type == SCR_TORMENT;
     case OBJ_STAVES:
         return item.sub_type == STAFF_DEATH;
-    case OBJ_BOOKS:
-        return _is_book_type(item, is_evil_spell);
     case OBJ_MISCELLANY:
         return item.sub_type == MISC_HORN_OF_GERYON;
     default:
