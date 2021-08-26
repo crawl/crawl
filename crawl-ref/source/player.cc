crawl/crawl

commit 14faa605fdf6afe43d831ae06f5ba015b416325d

Check for monsters, objects & features as soon as you move. (#2041)
Check for things after the player moves, rather than waiting for the start of the next turn.
Improve the accuracy of a comment in check_for_interesting_features().

Co-authored-by: Aliscans <crawl@thimk.info>

crawl-ref/source/player.cc
@@ -527,6 +527,10 @@ void moveto_location_effects(dungeon_feature_type old_feat,
 
     if (stepped)
         _moveto_maybe_repel_stairs();
+
+    update_monsters_in_view();
+    if (check_for_interesting_features() && you.running.is_explore())
+        stop_running();
 }
 
 // Use this function whenever the player enters (or lands and thus re-enters)

crawl-ref/source/travel.cc
@@ -4986,7 +4986,7 @@ bool check_for_interesting_features()
     // Scan through the shadow map, compare it with the actual map, and if
     // there are any squares of the shadow map that have just been
     // discovered and contain an item, or have an interesting dungeon
-    // feature, stop exploring.
+    // feature, announce the discovery and return true.
     explore_discoveries discoveries;
     for (vision_iterator ri(you); ri; ++ri)
     {

commit a0e16d178bf249a7c1cec821e92aeeac9fad7f6b

Fix explore being interrupted by autopickupable items
Caused by 14faa60; check_for_interesting_features uses the stash to
determine whether to stop, but it was being called without actually
updating the stash.

crawl-ref/source/player.cc
@@ -529,6 +529,7 @@ void moveto_location_effects(dungeon_feature_type old_feat,
         _moveto_maybe_repel_stairs();
 
     update_monsters_in_view();
+    maybe_update_stashes();
     if (check_for_interesting_features() && you.running.is_explore())
         stop_running();
 }