2e5a6a2853da1e1f5df4837a58c2cced35a951aa
MainiacJoe <mainiacjoe@gmail.com>
2021-09-08 09:28:57 -0400

    Take note when player prays at an ecumenical altar In the morgue, it will be referred to as, "the altar of an unknown god," since I've discovered that some players think the term, "ecumenical altar" refers to an altar in the Ecumenical Temple.

--- crawl-ref/source/god-prayer.cc
+++ crawl-ref/source/god-prayer.cc
@@ -82,6 +82,8 @@ static bool _pray_ecumenical_altar()
             unwind_var<int> fakepoor(you.attribute[ATTR_GOLD_GENERATED], 0);
 
             god_type altar_god = _altar_identify_ecumenical_altar();
+            take_note(Note(NOTE_MESSAGE, 0, 0,
+                    "Prayed at the altar of an unknown god."));
             mprf(MSGCH_GOD, "%s accepts your prayer!",
                             god_name(altar_god).c_str());
             you.turn_is_over = true;
