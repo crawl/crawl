b337ad3f7faecda3cd6123a63e8bfbc80143e229
Perry Fraser <perryprog@users.noreply.github.com>
2021-09-11 11:45:04 -0400

    Use have_passive(wall jump) for get_god_abilities silence logic
    Equivalent to what was there, just better.
    
    Co-authored-by: gammafunk <gammafunk@gmail.com>

--- crawl-ref/source/ability.cc
+++ crawl-ref/source/ability.cc
@@ -3944,7 +3944,7 @@ vector<ability_type> get_god_abilities(bool ignore_silence, bool ignore_piety,
 
     if (!ignore_silence && silenced(you.pos()))
     {
-        if (you_worship(GOD_WU_JIAN) && piety_rank() >= 2)
+        if (have_passive(passive_t::wu_jian_wall_jump))
             abilities.push_back(ABIL_WU_JIAN_WALLJUMP);
         return abilities;
     }
