0d7ab7f379262240f30a1f251a7fbcaf1fa234d4
Nicholas Feinberg <pleasingfung@gmail.com>
2021-09-21 16:32:20 -0700

    An immobile stone gathers much moss (Sastreii)
    New stone wall tiles for Swamp and Sewer.

--- crawl-ref/source/tileview.cc
+++ crawl-ref/source/tileview.cc
@@ -1304,6 +1304,11 @@ void apply_variations(const tile_flavour &flv, tileidx_t *bg,
     {
         if (orig == TILE_DNGN_STONE_WALL)
             orig = TILE_STONE_WALL_SPIDER;
+    } else if (player_in_branch(BRANCH_SWAMP)
+               || player_in_branch(BRANCH_SEWER))
+    {
+        if (orig == TILE_DNGN_STONE_WALL)
+            orig = TILE_WALL_STONE_MOSSY;
     }
 
     if (orig == TILE_FLOOR_NORMAL)
