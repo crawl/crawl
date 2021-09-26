a3b98fb556cb25ab2c8850891c51fa841ffcb7db
Gopall <gopala2003@hotmail.com>
2021-09-25 22:14:14 -0400

    More zotdef stuff

--- crawl-ref/source/webserver/game_data/static/player.js
+++ crawl-ref/source/webserver/game_data/static/player.js
@@ -370,6 +370,17 @@ function ($, comm, enums, map_knowledge, messages, options, util) {
         for (var i = 0; i < simple_stats.length; ++i)
             $("#stats_" + simple_stats[i]).text(player[simple_stats[i]]);
 
+        
+		//zotdef
+		if (player.zp !== null)
+        {
+            $(".stats_zot_defense").show();
+            $("#stats_zp").text(player.zp);
+        }
+        else
+            $(".stats_zot_defense").hide();
+
+
         $("#stats_hpline > .stats_caption").text(
             (player.real_hp_max != player.hp_max) ? "HP:" : "Health:");
 
@@ -513,6 +524,7 @@ function ($, comm, enums, map_knowledge, messages, options, util) {
                 mp: 0, mp_max: 0, dd_real_mp_max: 0,
                 ac: 0, ev: 0, sh: 0,
                 xl: 0, progress: 0,
+                zp: null,
                 time: 0, time_delta: 0,
                 gold: 0,
                 str: 0, int: 0, dex: 0,
