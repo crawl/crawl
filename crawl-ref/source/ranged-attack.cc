485a8aac0bfa5594f1cfa76f8e472064a3904b66
Santiago Acosta <santiagoacosta777@gmail.com>
2021-09-28 10:38:34 -0300

    Remove archer monsters hidden bonus damage
    This commit removes the HD * 4/3 bonus damage from monsters with the archer tag. This bonus, besides being hidden, lacks an analogous bonus in melee monsters (those with the fighter tag). The end result of this is rather unintuitive from the player's perspective, since when the player uses ranged weapons, they usually hit for a little bit less than their melee counterparts, and this is dramatically different from monsters (a merfolk javelineer, for example, hits for a little bit more than an impaler with a +9 demon trident).

--- crawl-ref/source/ranged-attack.cc
+++ crawl-ref/source/ranged-attack.cc
@@ -349,11 +349,6 @@ int ranged_attack::calc_mon_to_hit_base()
 int ranged_attack::apply_damage_modifiers(int damage)
 {
     ASSERT(attacker->is_monster());
-    if (attacker->as_monster()->is_archer())
-    {
-        const int bonus = attacker->get_hit_dice() * 4 / 3;
-        damage += random2avg(bonus, 2);
-    }
     return damage;
 }
 
