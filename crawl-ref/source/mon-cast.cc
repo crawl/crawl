crawl/crawl

commit 98ec5579a34455a245d23d38b6272956ca151b1f

Make allies avoid shooting you (Torax)
Use aimed_at_spot to have allies avoid hitting the player when they
miss their actual target. Keep enemy & neutral behavior the same.

It seems polite.

crawl-ref/source/mon-cast.cc
@@ -1397,6 +1397,9 @@ bolt mons_spell_beam(const monster* mons, spell_type spell_cast, int power,
     beam.origin_spell = real_spell;
     beam.source_id = mons->mid;
     beam.source_name = mons->name(DESC_A, true);
+    // Avoid overshooting and potentially hitting the player.
+    if (mons->temp_attitude() == ATT_FRIENDLY)
+        beam.aimed_at_spot = true;
 
     if (!mons_spell_is_spell(real_spell))
         power = mons_power_for_hd(real_spell, mons->get_hit_dice());

crawl-ref/source/throw.cc
@@ -928,6 +928,8 @@ bool mons_throw(monster* mons, bolt &beam, int msl, bool teleport)
         return false;
 
     beam.aimed_at_spot |= returning;
+    // Avoid overshooting and potentially hitting the player.
+    beam.aimed_at_spot |= mons->temp_attitude() == ATT_FRIENDLY;
 
     const launch_retval projected =
         is_launched(mons, mons->mslot_item(MSLOT_WEAPON),

crawl-ref/source/beam.cc
@@ -2147,6 +2147,7 @@ void bolt_parent_init(const bolt &parent, bolt &child)
     child.attitude       = parent.attitude;
 
     child.pierce         = parent.pierce ;
+    child.aimed_at_spot  = parent.aimed_at_spot;
     child.is_explosion   = parent.is_explosion;
     child.ex_size        = parent.ex_size;
     child.foe_ratio      = parent.foe_ratio;

commit 98e7288b7fd90c5ae50b6fa101a9500862cf13c5

Fix piercing allies (dilly)
98ec5579a34455a245d inadvertently disabled piercing for all allies
with beam-type attacks (lightning spires, merfolk javelineers, etc).

crawl-ref/source/mon-cast.cc
@@ -1397,9 +1397,6 @@ bolt mons_spell_beam(const monster* mons, spell_type spell_cast, int power,
     beam.origin_spell = real_spell;
     beam.source_id = mons->mid;
     beam.source_name = mons->name(DESC_A, true);
-    // Avoid overshooting and potentially hitting the player.
-    if (mons->temp_attitude() == ATT_FRIENDLY)
-        beam.aimed_at_spot = true;
 
     if (!mons_spell_is_spell(real_spell))
         power = mons_power_for_hd(real_spell, mons->get_hit_dice());
@@ -1681,6 +1678,11 @@ bolt mons_spell_beam(const monster* mons, spell_type spell_cast, int power,
         beam.glyph = 0;
         beam.name = "";
     }
+
+    // Avoid overshooting and potentially hitting the player.
+    // Piercing beams' tracers already account for this.
+    if (mons->temp_attitude() == ATT_FRIENDLY && !beam.pierce)
+        beam.aimed_at_spot = true;
 
     return beam;
 }

crawl-ref/source/throw.cc
@@ -929,7 +929,9 @@ bool mons_throw(monster* mons, bolt &beam, int msl, bool teleport)
 
     beam.aimed_at_spot |= returning;
     // Avoid overshooting and potentially hitting the player.
-    beam.aimed_at_spot |= mons->temp_attitude() == ATT_FRIENDLY;
+    // Piercing beams' tracers already account for this.
+    beam.aimed_at_spot |= mons->temp_attitude() == ATT_FRIENDLY
+                          && !beam.pierce;
 
     const launch_retval projected =
         is_launched(mons, mons->mslot_item(MSLOT_WEAPON),