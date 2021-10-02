c20955654e95f046d01c06ff82ebef2e971ceded
Nicholas Feinberg <pleasingfung@gmail.com>
2021-10-02 13:33:06 -0700

    Remove random energy
    Random energy has a long and dismal history. It was invented as a
    means to discourage repetitive and boring play patterns (pillar-dancing,
    door-dancing, break-dancing, etc), but signally failed to do so.
    Instead, it obfuscated game mechanics, added situations that felt
    'unfair', and generally kludged everything up.
    
    It was a bold experiment, but it's been clear for many years now
    that it's failed. Let's move on.

--- crawl-ref/source/monster.cc
+++ crawl-ref/source/monster.cc
@@ -5672,12 +5672,6 @@ void monster::lose_energy(energy_use_type et, int div, int mult)
     if ((et == EUT_MOVE || et == EUT_SWIM) && has_ench(ENCH_FROZEN))
         energy_loss += 4;
 
-    // Randomize movement cost slightly, to make it less predictable,
-    // and make pillar-dancing not entirely safe.
-    // No randomization for allies following you to avoid traffic jam
-    if ((et == EUT_MOVE || et == EUT_SWIM) && (!friendly() || foe != MHITYOU))
-        energy_loss += random2(3) - 1;
-
     speed_increment -= energy_loss;
 }
 
