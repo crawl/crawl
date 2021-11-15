crawl/crawl

commit 6ee961360c27e6260473338b3577d2b0b8d54b0f

Despoiler /poly (gammafunk)
Pre-choose a set of 3 monsters that any given monster can turn into
(filtering by holiness & habitat and weighting by HD and demon tier),
and display them on the /polymorph targeter. This should make it much
more possible to make tactical choices about the use of /polymorph.

This does not affect other sources of polymorphing, such as Xom or Nem.

crawl-ref/source/monster.cc
@@ -4129,11 +4129,7 @@ bool monster::airborne() const
 {
     // For dancing weapons, this function can get called before their
     // ghost_demon is created, so check for a nullptr ghost. -cao
-    return mons_is_ghost_demon(type) && ghost && ghost->flies
-           // check both so spectral humans and zombified dragons both fly
-           || mons_class_flag(mons_base_type(*this), M_FLIES)
-           || mons_class_flag(type, M_FLIES)
-           || has_facet(BF_BAT)
+    return monster_inherently_flies(*this)
            || scan_artefacts(ARTP_FLY) > 0
            || mslot_item(MSLOT_ARMOUR)
               && mslot_item(MSLOT_ARMOUR)->base_type == OBJ_ARMOUR
@@ -5374,7 +5370,7 @@ bool monster::polymorph(poly_power_type power)
         return true;
     }
 
-    return monster_polymorph(this, RANDOM_MONSTER, power);
+    return monster_polymorph(this, RANDOM_POLYMORPH_MONSTER, power);
 }
 
 static bool _mons_is_icy(int mc)

crawl-ref/source/mon-abil.cc
@@ -666,7 +666,7 @@ bool slime_creature_polymorph(monster& slime, poly_power_type power)
         }
     }
 
-    return monster_polymorph(&slime, RANDOM_MONSTER, power);
+    return monster_polymorph(&slime, RANDOM_POLYMORPH_MONSTER, power);
 }
 
 static bool _starcursed_split(monster* mon)

crawl-ref/source/mon-info.cc
@@ -214,7 +214,8 @@ static bool _is_public_key(string key)
      || key == MON_GENDER_KEY
      || key == SEEN_SPELLS_KEY
      || key == KNOWN_MAX_HP_KEY
-     || key == VAULT_HD_KEY )
+     || key == VAULT_HD_KEY
+     || key == POLY_SET_KEY)
     {
         return true;
     }

crawl-ref/source/mon-place.cc
@@ -748,6 +748,8 @@ monster* place_monster(mgen_data mg, bool force_pos, bool dont_place)
     if (!mon)
         return nullptr;
 
+    init_poly_set(mon);
+
     if (mg.props.exists(MAP_KEY))
         mon->set_originating_map(mg.props[MAP_KEY].get_string());
 

crawl-ref/source/mon-poly.cc
@@ -99,7 +99,7 @@ void monster_drop_things(monster* mons,
     }
 }
 
-static bool _valid_morph(monster* mons, monster_type new_mclass)
+static bool _valid_type_morph(const monster* mons, monster_type new_mclass)
 {
     // Shapeshifters cannot polymorph into glowing shapeshifters or
     // vice versa.
@@ -107,15 +107,6 @@ static bool _valid_morph(monster* mons, monster_type new_mclass)
              && mons->has_ench(ENCH_SHAPESHIFTER))
          || (new_mclass == MONS_SHAPESHIFTER
              && mons->has_ench(ENCH_GLOWING_SHAPESHIFTER)))
-    {
-        return false;
-    }
-
-    // [hm] Lower base draconian chances since there are nine of them,
-    // and they shouldn't each count for a full chance.
-    if (mons_genus(new_mclass) == MONS_DRACONIAN
-        && new_mclass != MONS_DRACONIAN
-        && !one_chance_in(9))
     {
         return false;
     }
@@ -153,6 +144,25 @@ static bool _valid_morph(monster* mons, monster_type new_mclass)
         || (new_mclass == MONS_HUMAN
             && (mons->type == MONS_PRINCE_RIBBIT
                 || mons->mname == "Prince Ribbit")))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+
+
+static bool _valid_morph(monster* mons, monster_type new_mclass)
+{
+    if (!_valid_type_morph(mons, new_mclass))
+        return false;
+
+    // [hm] Lower base draconian chances since there are nine of them,
+    // and they shouldn't each count for a full chance.
+    if (mons_genus(new_mclass) == MONS_DRACONIAN
+        && new_mclass != MONS_DRACONIAN
+        && !one_chance_in(9))
     {
         return false;
     }
@@ -386,6 +396,9 @@ void change_monster_type(monster* mons, monster_type targetc)
         if (could_see && shifter.ench != ENCH_NONE)
             discover_shifter(*mons);
     }
+    // generate a new polymorph set
+    mons->props.erase(POLY_SET_KEY);
+    init_poly_set(mons);
 
     if (old_mon_caught)
         check_net_will_hold_monster(mons);
@@ -397,6 +410,110 @@ void change_monster_type(monster* mons, monster_type targetc)
     // evaporating and reforming justifies this behaviour.
     mons->stop_constricting_all();
     mons->stop_being_constricted();
+}
+
+// Is the new monster able to live in *any* habitat that the original
+// monster could?
+// NOTE: this doesn't check for item-based flight, so if we
+// poly an item-using flying monster into a non-item-using,
+// non-flying monster, they could be in trouble. Not much way
+// around this and it's a very very niche case.
+// Also assumes the monster is not a derived (zombie etc) monster.
+static bool _habitat_matches(bool orig_flies, habitat_type orig_hab,
+                             monster_type new_type)
+{
+    if (monster_class_flies(new_type))
+        return true;
+    if (orig_flies)
+        return false;
+
+    const habitat_type new_hab = mons_habitat_type(new_type, new_type, false);
+    switch (orig_hab)
+    {
+        case HT_AMPHIBIOUS:
+        case HT_AMPHIBIOUS_LAVA:
+            return new_hab == orig_hab;
+        case HT_WATER:
+            return new_hab == orig_hab || new_hab == HT_AMPHIBIOUS;
+        case HT_LAVA:
+            return new_hab == orig_hab || new_hab == HT_AMPHIBIOUS_LAVA;
+        case HT_LAND:
+            return new_hab == orig_hab
+                || new_hab == HT_AMPHIBIOUS
+                || new_hab == HT_AMPHIBIOUS_LAVA;
+        case NUM_HABITATS:
+            break;
+    }
+    return false; // should never happen
+}
+
+void init_poly_set(monster *mons)
+{
+    if (mons->props.exists(POLY_SET_KEY))
+        return;
+
+    const int orig_tier = mons_demon_tier(mons->type);
+    if (orig_tier == -1)
+    {
+        // panlords & hell lords get poly immunity. why? unclear to me
+        // TODO: allow polying panlords into other random panlords, ha
+        return;
+    }
+
+    const int orig_hd = mons_power(mons->type);
+    const bool orig_flies = monster_inherently_flies(*mons);
+    const habitat_type orig_hab
+        = mons_habitat_type(mons->type, mons_base_type(*mons), false);
+
+    map<monster_type, int> weights;
+    for (monster_type mt = MONS_0; mt < NUM_MONSTERS; ++mt)
+    {
+        if (invalid_monster_type(mt))
+            continue; // no polying into bugs
+
+        const monster_type species = mons_species(mt);
+        if (weights.find(species) != weights.end())
+            continue; // already saw this one
+        if (!_valid_type_morph(mons, species))
+            continue; // no polying into statues, same species, etc
+
+        if (!_habitat_matches(orig_flies, orig_hab, species))
+            continue;
+
+        // OK, we're good. Let's look at weights.
+        const int new_tier = mons_demon_tier(species);
+        const int new_hd = mons_power(species);
+        // make HD upgrades less likely.
+        const int hd_delta = abs(orig_hd - new_hd) * (new_hd > orig_hd ? 2 : 1);
+        const int tier_delta = abs(orig_tier - new_tier);
+        const int total_delta = hd_delta + tier_delta;
+        const int max_delta = 8;
+        if (total_delta > max_delta)
+            continue;
+        // halve weight for each HD of difference and each demon tier level apart
+        const int weight = 1 << (max_delta - total_delta) ;
+        weights[species] = weight;
+    }
+
+    CrawlVector &set = mons->props[POLY_SET_KEY];
+    for (int i = 0; i < 3; i++)
+    {
+        if (weights.size() <= 0)
+            return; // can't choose any more
+        const monster_type *chosen = random_choose_weighted<map<monster_type,int>>(weights);
+        ASSERT(chosen);
+        set.push_back(*chosen);
+        weights.erase(*chosen);
+    }
+}
+
+static monster_type _poly_from_set(monster *mons)
+{
+    ASSERT(mons->props.exists(POLY_SET_KEY));
+    const CrawlVector &set = mons->props[POLY_SET_KEY];
+    if (set.size() <= 0)
+        return MONS_NO_MONSTER;
+    return (monster_type)set[random2(set.size())].get_int();
 }
 
 // If targetc == RANDOM_MONSTER, then relpower indicates the desired
@@ -454,6 +571,9 @@ bool monster_polymorph(monster* mons, monster_type targetc,
                            || _is_poly_power_unsuitable(power, source_power,
                                                         target_power, relax)));
     }
+
+    if (targetc == RANDOM_POLYMORPH_MONSTER)
+        targetc = _poly_from_set(mons);
 
     bool could_see = you.can_see(*mons);
     bool need_note = could_see && mons_is_notable(*mons);

crawl-ref/source/mon-poly.h
@@ -12,6 +12,7 @@ item_def* item_mimic_at(const coord_def &c);
 bool mimic_at(const coord_def &c);
 
 #define ORIGINAL_TYPE_KEY "original_type"
+const string POLY_SET_KEY = "poly_set";
 
 enum poly_power_type
 {
@@ -26,6 +27,7 @@ void monster_drop_things(
     bool (*suitable)(const item_def& item) = always_true<const item_def &>);
 
 void change_monster_type(monster* mons, monster_type targetc);
+void init_poly_set(monster *mons);
 bool monster_polymorph(monster* mons, monster_type targetc,
                        poly_power_type power = PPT_SAME);
 

crawl-ref/source/mon-util.cc
@@ -105,6 +105,20 @@ static int _mons_exp_mod(monster_type mclass);
 
 /* ******************** BEGIN PUBLIC FUNCTIONS ******************** */
 
+bool monster_class_flies(monster_type mc)
+{
+    return mons_class_flag(mc, M_FLIES);
+}
+
+bool monster_inherently_flies(const monster &mons)
+{
+    // check both so spectral humans and zombified dragons both fly
+    return monster_class_flies(mons.type)
+        || monster_class_flies(mons_base_type(mons))
+        || mons_is_ghost_demon(mons.type) && mons.ghost && mons.ghost->flies
+        || mons.has_facet(BF_BAT);
+}
+
 static habitat_type _grid2habitat(dungeon_feature_type grid)
 {
     if (feat_is_watery(grid))

crawl-ref/source/mon-util.h
@@ -307,6 +307,9 @@ mon_energy_usage mons_energy(const monster& mon);
 int mons_class_zombie_base_speed(monster_type zombie_base_mc);
 int mons_base_speed(const monster& mon, bool known = false);
 
+bool monster_class_flies(monster_type mc);
+bool monster_inherently_flies(const monster &mons);
+
 bool mons_class_can_regenerate(monster_type mc);
 bool mons_can_regenerate(const monster& mon);
 bool mons_class_fast_regen(monster_type mc);

commit 58c29a6370127a90d3efe4aa661531872fc57dc5

Fix Degeneration & Xom polymorph
These were broken in 6ee961360c27e and functioned identically to /poly.

crawl-ref/source/monster.cc
@@ -5225,7 +5225,9 @@ bool monster::polymorph(poly_power_type power)
         return true;
     }
 
-    return monster_polymorph(this, RANDOM_POLYMORPH_MONSTER, power);
+    const monster_type targ = power == PPT_SAME ? RANDOM_POLYMORPH_MONSTER
+                                                : RANDOM_MONSTER;
+    return monster_polymorph(this, targ, power);
 }
 
 static bool _mons_is_icy(int mc)