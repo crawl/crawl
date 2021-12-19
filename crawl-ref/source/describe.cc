1e6f1b227945815424707e0aacbc104adf29027f
advil <rawlins@gmail.com>
2021-12-19 10:20:57 -0500

    lint

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -3130,7 +3130,7 @@ static level_id _item_level_id(const item_def &item)
     {
         loc = level_id::parse_level_id(item.props["level_id"].get_string());
     }
-    catch(const bad_level_id &err)
+    catch (const bad_level_id &err)
     {
         // die?
     }
