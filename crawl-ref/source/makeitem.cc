crawl/crawl

commit 850bfe61e39c27fcdc3dcf3f7b28b0b55a6c1371

Fix an interaction of fallback randarts and acquirement (12277)
I hadn't fully understood one of the ways in which acquired objects
could interfere with unrand generation. After this commit, unrands that
place outside of levelgen (acquirement) or in the abyss or a trove
should always cause later instances of that unrand to simply be replaced
with a fallback randart, and have no other effect on item generation
during levelgen. Before this commit, unrands that were forced by vaults
would be correctly replaced with a fallback if they had already been
acquired, but unrands that were placed randomly (e.g. by | in a vault;
Slime:5 was a good place to check) could be affected by previous
acquirements in a way that had unpredictable results for those items.
I'm pretty sure that this bug had no impact on item generation (or
levelgen more broadly) outside of those specific grids.

This commit may change some seeds as I'm adding a subgenerator the will
impact certain rare cases of unrand placement (mainly for uniques who
can come with a special unrand). These uniques may need further testing.

No save compat for this, so upgrading will lead to the same behavior as
games from before this commit would have.

crawl-ref/source/makeitem.cc
@@ -25,6 +25,11 @@
 #include "stepdown.h"
 #include "stringutil.h"
 
+static void _setup_fallback_randart(const int unrand_id,
+                                    item_def &item,
+                                    int &force_type,
+                                    int &item_level);
+
 int create_item_named(string name, coord_def pos, string *error)
 {
     trim_string(name);
@@ -182,7 +187,7 @@ static weapon_type _determine_weapon_subtype(int item_level)
     }
 }
 
-static bool _try_make_item_unrand(item_def& item, int force_type, int agent)
+static bool _try_make_item_unrand(item_def& item, int &force_type, int agent)
 {
     if (player_in_branch(BRANCH_PANDEMONIUM) && agent == NO_AGENT)
         return false;
@@ -190,6 +195,15 @@ static bool _try_make_item_unrand(item_def& item, int force_type, int agent)
     int idx = find_okay_unrandart(item.base_type, force_type,
                                   player_in_branch(BRANCH_ABYSS)
                                       && agent == NO_AGENT);
+
+    // if an idx was found that exists, the unrand was generated via
+    // acquirement or similar; replace it with a fallback randart.
+    if (idx != -1 && get_unique_item_status(idx))
+    {
+        int item_level;
+        _setup_fallback_randart(idx, item, force_type, item_level);
+        return false;
+    }
 
     if (idx != -1 && make_item_unrandart(item, idx))
         return true;
@@ -219,6 +233,10 @@ static bool _try_make_weapon_artefact(item_def& item, int force_type,
         {
             if (_try_make_item_unrand(item, force_type, agent))
                 return true;
+            // update sub type for fallback randarts (otherwise, the call will
+            // not have changed this value)
+            if (force_type != OBJ_RANDOM)
+                item.sub_type = force_type;
         }
 
         if (_weapon_disallows_randart(item.sub_type))
@@ -1547,7 +1565,9 @@ static void _generate_staff_item(item_def& item, bool allow_uniques,
     {
         // Temporarily fix the base_type to get enhancer staves
         item.base_type = OBJ_WEAPONS;
-        if (_try_make_item_unrand(item, WPN_STAFF, agent))
+        // TODO: how does this relate to force_type?
+        int tmp_force_type = WPN_STAFF;
+        if (_try_make_item_unrand(item, tmp_force_type, agent))
             return;
         item.base_type = OBJ_STAVES;
     }

crawl-ref/source/artefact.cc
@@ -303,9 +303,22 @@ unique_item_status_type get_unique_item_status(int art)
     return you.unique_items[art - UNRAND_START];
 }
 
-static void _set_unique_item_status(int art, unique_item_status_type status)
+static void _set_unique_item_status(int art, bool exists)
 {
     ASSERT_RANGE(art, UNRAND_START + 1, UNRAND_LAST);
+
+    const unique_item_status_type status = !exists
+        ? UNIQ_NOT_EXISTS
+        : !crawl_state.generating_level
+                // treat unrands that generate in these branches as if they
+                // were acquired. TODO: there's a potential bug here if every
+                // octopus king ring generates and the last is acquired. Also,
+                // I suspect that these getting lost in the abyss isn't handled
+                // right
+                || level_id::current().branch == BRANCH_TROVE
+                || level_id::current().branch == BRANCH_ABYSS
+            ? UNIQ_EXISTS_NONLEVELGEN
+            : UNIQ_EXISTS;
     you.unique_items[art - UNRAND_START] = status;
 }
 
@@ -1328,8 +1341,11 @@ int find_okay_unrandart(uint8_t aclass, uint8_t atype, bool in_abyss)
 {
     int ret = -1;
 
-    // Pick randomly among not-yet-existing unrandarts with the proper
-    // base_type and sub_type.
+    // Pick randomly among unrandarts with the proper
+    // base_type and sub_type. This will rule out unrands that have already
+    // placed as part of levelgen, but may find unrands that have been acquired.
+    // because of this, the caller needs to properly set up a fallback randart
+    // in some cases: see makeitem.cc:_setup_fallback_randart.
     for (int i = 0, count = 0; i < NUM_UNRANDARTS; i++)
     {
         const int              index = i + UNRAND_START;
@@ -1343,7 +1359,11 @@ int find_okay_unrandart(uint8_t aclass, uint8_t atype, bool in_abyss)
             get_unique_item_status(index);
 
         if (in_abyss && status != UNIQ_LOST_IN_ABYSS
-            || !in_abyss && status != UNIQ_NOT_EXISTS)
+            || !in_abyss && status != UNIQ_NOT_EXISTS
+               // for acquired items, ignore them in the random calculations
+               // here and let fallback artefacts replace them.
+               // TODO: abyss? double check trove
+               && status != UNIQ_EXISTS_NONLEVELGEN)
         {
             continue;
         }
@@ -1733,12 +1753,14 @@ static void _make_octoring(item_def &item)
 
     // If there are any types left, unset the 'already found' flag
     if (you.octopus_king_rings != 0xff)
-        _set_unique_item_status(UNRAND_OCTOPUS_KING_RING, UNIQ_NOT_EXISTS);
+        _set_unique_item_status(UNRAND_OCTOPUS_KING_RING, false);
 }
 
 bool make_item_unrandart(item_def &item, int unrand_index)
 {
     ASSERT_RANGE(unrand_index, UNRAND_START + 1, (UNRAND_START + NUM_UNRANDARTS));
+    rng::subgenerator item_rng; // for safety's sake, this is sometimes called
+                                // directly
 
     item.unrand_idx = unrand_index;
 
@@ -1755,7 +1777,7 @@ bool make_item_unrandart(item_def &item, int unrand_index)
     ASSERT(!item.props.exists(ARTEFACT_APPEAR_KEY));
     item.props[ARTEFACT_APPEAR_KEY].get_string() = unrand->unid_name;
 
-    _set_unique_item_status(unrand_index, UNIQ_EXISTS);
+    _set_unique_item_status(unrand_index, true);
 
     if (unrand_index == UNRAND_FAERIE)
         _make_faerie_armour(item);

crawl-ref/source/unique-item-status-type.h
@@ -5,4 +5,5 @@ enum unique_item_status_type
     UNIQ_NOT_EXISTS = 0,
     UNIQ_EXISTS = 1,
     UNIQ_LOST_IN_ABYSS = 2,
+    UNIQ_EXISTS_NONLEVELGEN = 3,
 };

commit 77e77742e6c78934465219541914bef038a60ace

Fix unrands being able to reappear in the Abyss
This was broken in 850bfe61e39, a year and a half ago. Probably no
one noticed because it's almost impossible to encounter this case
to begin with.

crawl-ref/source/makeitem.cc
@@ -178,7 +178,7 @@ static bool _try_make_item_unrand(item_def& item, int &force_type, int agent)
     // if an idx was found that exists, the unrand was generated via
     // acquirement or similar; replace it with a fallback randart.
     // Choosing a new unrand could break seed stability.
-    if (get_unique_item_status(idx))
+    if (get_unique_item_status(idx) == UNIQ_EXISTS_NONLEVELGEN)
     {
         int item_level;
         _setup_fallback_randart(idx, item, force_type, item_level);