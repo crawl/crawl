crawl/crawl

commit 95685b5d4a526e94ea562b9df2ea7fce36eb05bc

Rename mid_msg -> expire_msg, etc (Pinkbeast)
5c2cb368445 (2015) continued the misleading naming used by
_decrement_a_duration() in which effects for the 'expiration point'
(an arbitrary # of turns before the end of a duration at which the
player is warned that it's nearly done) were claimed to occur at
the 'midpoint' (50% of the way through the duration.) Fix this.

crawl-ref/source/player-reacts.cc
@@ -121,37 +121,37 @@
 
 static bool _decrement_a_duration(duration_type dur, int delay,
                                  const char* endmsg = nullptr,
-                                 int midloss = 0,
-                                 const char* midmsg = nullptr,
+                                 int exploss = 0,
+                                 const char* expmsg = nullptr,
                                  msg_channel_type chan = MSGCH_DURATION)
 {
     ASSERT(you.duration[dur] >= 0);
     if (you.duration[dur] == 0)
         return false;
 
-    ASSERT(!midloss || midmsg != nullptr);
-    const int midpoint = duration_expire_point(dur);
-    ASSERTM(!midloss || midloss * BASELINE_DELAY < midpoint,
-            "midpoint delay loss %d not less than duration midpoint %d",
-            midloss * BASELINE_DELAY, midpoint);
+    ASSERT(!exploss || expmsg != nullptr);
+    const int exppoint = duration_expire_point(dur);
+    ASSERTM(!exploss || exploss * BASELINE_DELAY < exploss,
+            "expiration delay loss %d not less than duration expiration point %d",
+            exploss * BASELINE_DELAY, exppoint);
 
     const int old_dur = you.duration[dur];
     you.duration[dur] -= delay;
 
-    // If we cross the midpoint, handle midloss and print the midpoint message.
-    if (you.duration[dur] <= midpoint && old_dur > midpoint)
+    // If we start expiring, handle exploss and print the exppoint message.
+    if (you.duration[dur] <= exppoint && old_dur > exppoint)
     {
-        you.duration[dur] -= midloss * BASELINE_DELAY;
-        if (midmsg)
+        you.duration[dur] -= exploss * BASELINE_DELAY;
+        if (expmsg)
         {
             // Make sure the player has a turn to react to the midpoint
             // message.
             if (you.duration[dur] <= 0)
                 you.duration[dur] = 1;
             if (need_expiration_warning(dur))
-                mprf(MSGCH_DANGER, "Careful! %s", midmsg);
+                mprf(MSGCH_DANGER, "Careful! %s", expmsg);
             else
-                mprf(chan, "%s", midmsg);
+                mprf(chan, "%s", expmsg);
         }
     }
 
@@ -540,9 +540,9 @@ static void _try_to_respawn_ancestor()
 static void _decrement_simple_duration(duration_type dur, int delay)
 {
     if (_decrement_a_duration(dur, delay, duration_end_message(dur),
-                             duration_mid_offset(dur),
-                             duration_mid_message(dur),
-                             duration_mid_chan(dur)))
+                             duration_expire_offset(dur),
+                             duration_expire_message(dur),
+                             duration_expire_chan(dur)))
     {
         duration_end_effect(dur);
     }

crawl-ref/source/duration-data.h
@@ -68,7 +68,7 @@ enum duration_flags : uint32_t
 };
 
 /// A description of the behaviour when a duration begins 'expiring'.
-struct midpoint_msg
+struct expiring_msg
 {
     /// What message should be printed when the duration begins expiring?
     const char* msg;
@@ -96,8 +96,8 @@ struct decrement_rules
 {
     /// What happens when the duration ends?
     end_effect end;
-    /// What happens when a duration hits 50% remaining.
-    midpoint_msg mid_msg;
+    /// What happens when a duration passes its expire_threshold.
+    expiring_msg expire_msg;
     /// Should the message be MSGCH_RECOVERY instead of MSGCH_DURATION?
     bool recovery;
 };

crawl-ref/source/status.cc
@@ -1037,26 +1037,27 @@ const char *duration_end_message(duration_type dur)
 }
 
 /**
- * What message should a given duration print when it reaches 50%, if any?
+ * What message should a given duration print when it passes its
+ * expiring threshold, if any?
  *
  * @param dur   The duration in question (e.g. DUR_PETRIFICATION).
- * @return      A message to print for the duration when it hits 50%.
+ * @return      A message to print.
  */
-const char *duration_mid_message(duration_type dur)
+const char *duration_expire_message(duration_type dur)
 {
-    return _lookup_duration(dur)->decr.mid_msg.msg;
+    return _lookup_duration(dur)->decr.expire_msg.msg;
 }
 
 /**
- * How much should the duration be decreased by when it hits the midpoint (to
- * fuzz the remaining time), if at all?
+ * How much should the duration be decreased by when it passes its
+ * expiring threshold (to fuzz the remaining time), if at all?
  *
  * @param dur   The duration in question (e.g. DUR_PETRIFICATION).
  * @return      A random value to reduce the remaining duration by; may be 0.
  */
-int duration_mid_offset(duration_type dur)
+int duration_expire_offset(duration_type dur)
 {
-    return _lookup_duration(dur)->decr.mid_msg.offset();
+    return _lookup_duration(dur)->decr.expire_msg.offset();
 }
 
 /**
@@ -1078,7 +1079,7 @@ int duration_expire_point(duration_type dur)
  * @param dur   The duration in question (e.g. DUR_PETRIFICATION).
  * @return      The appropriate message channel, e.g. MSGCH_RECOVERY.
  */
-msg_channel_type duration_mid_chan(duration_type dur)
+msg_channel_type duration_expire_chan(duration_type dur)
 {
     return _lookup_duration(dur)->decr.recovery ? MSGCH_RECOVERY
                                                 : MSGCH_DURATION;

crawl-ref/source/status.h
@@ -79,7 +79,7 @@ void init_duration_index();
 bool duration_decrements_normally(duration_type dur);
 const char *duration_end_message(duration_type dur);
 void duration_end_effect(duration_type dur);
-const char *duration_mid_message(duration_type dur);
-int duration_mid_offset(duration_type dur);
+const char *duration_expire_message(duration_type dur);
+int duration_expire_offset(duration_type dur);
 int duration_expire_point(duration_type dur);
-msg_channel_type duration_mid_chan(duration_type dur);
+msg_channel_type duration_expire_chan(duration_type dur);

commit 1c9ffb4daf049ca629999cb561f630085b792161

Fix a duration crash (skeral)
Introduced by 95685b5d4a526e94ea562b9df2ea7fce36eb05bc .

crawl-ref/source/player-reacts.cc
@@ -131,7 +131,7 @@ static bool _decrement_a_duration(duration_type dur, int delay,
 
     ASSERT(!exploss || expmsg != nullptr);
     const int exppoint = duration_expire_point(dur);
-    ASSERTM(!exploss || exploss * BASELINE_DELAY < exploss,
+    ASSERTM(!exploss || exploss * BASELINE_DELAY < exppoint,
             "expiration delay loss %d not less than duration expiration point %d",
             exploss * BASELINE_DELAY, exppoint);
 