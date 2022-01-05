crawl/crawl

commit 44cf67ff301134fefa0a766109bb7c39141622db

Squarelos.
This commit changes many aspects of Crawl (LOS, spell/ability ranges,
explosions, haloes, etc) to use squares instead of circles. This makes sense
because it takes the same amount of time to move one cell diagonally as to
move one cell horizontally or vertically, so a square region of radius r is
actually the area that can be reached in r steps. Switching to the use of
squares fixes many unfortunate features of the old system; I'll list a few
of the more major ones here.

First, suppose that you find a sleeping monster with a ranged attack in an
open area and you want to close to melee it (or stab it). If you approach
it horizontally or vertically then it has 8 turns to wake up and start
attacking you, but if you approach it diagonally then it only has 5 turns.
Similarly, if you find a sleeping melee-based monster and you want to use
ranged attacks on it yourself, you should approach it horizontally or
vertically rather than diagonally.

Another variant of this is if you want to lead a dangerous monster (e.g. an
orc priest) elsewhere on the level -- in this case it is much safer to
enter the edge of its vision horizontally or vertically from it rather
than diagonally. If you start leading it away diagonally, then you will be
only 5 steps ahead of it and it will get several attacks for free if you
both turn into a corridor.

Spells of lower range having circular ranges also causes weird behavior. For
instance, suppose you ('@') have Stone Arrow (range 5) and want to use it on an
troll ('T'). You encounter the ogre with it 5 squares diagonally from you:

.....@.....
...........
...........
...........
...........
..........T

If you just wait until the troll enters range of your spell, you will have to
wait two turns and then will only get two casts before the troll is in melee
range of you. But if instead you move SW a few steps, you can reach this
position:

...........
...........
...........
..@....T...
...........
...........

crawl-ref/source/player.cc
@@ -2506,9 +2506,9 @@ void forget_map(bool rot)
 
         if (rot)
         {
-            const int dist = distance2(you.pos(), p);
+            const int dist = grid_distance(you.pos(), p);
             int chance = pow(geometric_chance,
-                             max(1, (dist - radius) / 40)) * scalar;
+                             max(1, (dist * dist - radius) / 40)) * scalar;
             if (x_chance_in_y(chance, scalar))
                 continue;
         }
@@ -3573,7 +3573,7 @@ void display_char_status()
     else
         mpr("You are alive.");
 
-    const int halo_size = you.halo_radius2();
+    const int halo_size = you.halo_radius();
     if (halo_size >= 0)
     {
         if (halo_size > 37)
@@ -7269,7 +7269,7 @@ bool player::backlit(bool self_halo) const
         return true;
     }
 
-    return !umbraed() && haloed() && (self_halo || halo_radius2() == -1);
+    return !umbraed() && haloed() && (self_halo || halo_radius() == -1);
 }
 
 bool player::umbra() const

commit 2cf8304be81ab186db79fb072b5d58a4eb1a9843

Fix halo descriptions on '@'
Broken since 44cf67ff301, oops.

crawl-ref/source/player.cc
@@ -3263,9 +3263,9 @@ void display_char_status()
     const int halo_size = you.halo_radius();
     if (halo_size >= 0)
     {
-        if (halo_size > 37)
+        if (halo_size > 5)
             mpr("You are illuminated by a large divine halo.");
-        else if (halo_size > 10)
+        else if (halo_size > 3)
             mpr("You are illuminated by a divine halo.");
         else
             mpr("You are illuminated by a small divine halo.");