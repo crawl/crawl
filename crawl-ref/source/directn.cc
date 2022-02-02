74f3203ff42b4cf85b5995dcb22cc51dd8f43d6c
Edgar A. Bering IV <trizor@gmail.com>
2022-02-02 13:13:10 -0600

    fix: account for guile in ctouch display (dilly)
    (cherry picked from commit 9d6eb31067244a2964301293fdc205f27082a572)

--- crawl-ref/source/directn.cc
+++ crawl-ref/source/directn.cc
@@ -3545,9 +3545,10 @@ static vector<string> _get_monster_desc_vector(const monster_info& mi)
     if (you.duration[DUR_CONFUSING_TOUCH])
     {
         const int pow = you.props[CONFUSING_TOUCH_KEY].get_int();
+        const int wl  = you.wearing_ego(EQ_ALL_ARMOUR, SPARM_GUILE) ?
+            guile_adjust_willpower(mi.willpower()) : mi.willpower();
         descs.emplace_back(make_stringf("chance to confuse on hit: %d%%",
-                                        hex_success_chance(mi.willpower(),
-                                                           pow, 100)));
+                                        hex_success_chance(wl, pow, 100)));
     }
     else if (you.form == transformation::fungus
              && !mons_is_unbreathing(mi.type))
