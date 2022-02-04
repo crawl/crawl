448c0e43879e96d2ecc0b347a80b7d4d59f91b04
Nicholas Feinberg <pleasingfung@gmail.com>
2022-02-04 08:16:33 -0800

    Add Oozemancy ability tile (Sastreii)
    (cherry picked from commit 2e6bf50d99e45a8599540b35d0eb8fc3ee5f8dd8)

--- crawl-ref/source/tilepick.cc
+++ crawl-ref/source/tilepick.cc
@@ -3677,6 +3677,8 @@ tileidx_t tileidx_ability(const ability_type ability)
     case ABIL_BEOGH_RESURRECTION:
         return TILEG_ABILITY_BEOGH_RESURRECTION;
     // Jiyva
+    case ABIL_JIYVA_OOZEMANCY:
+        return TILEG_ABILITY_JIYVA_OOZEMANCY;
     case ABIL_JIYVA_SLIMIFY:
         return TILEG_ABILITY_JIYVA_SLIMIFY;
     // Fedhas
