abffb2d2b2ae4bdebd4207ad2a5038eda789bcbf
advil <rawlins@gmail.com>
2022-02-18 09:28:53 -0500

    feat: add a CLO to prevent writing player bones
    This is not really intended as a player facing feature (it doesn't
    prevent ghosts from being generated or anything like that), but as an
    option to support community management features on webtiles.
    Implementing as a separate commit to make it easier to backport.

--- crawl-ref/source/initfile.cc
+++ crawl-ref/source/initfile.cc
@@ -2116,7 +2116,8 @@ void read_options(const string &s, bool runscript, bool clear_aliases)
 
 game_options::game_options()
     : seed(0), seed_from_rc(0),
-    no_save(false), language(lang_t::EN), lang_name(nullptr),
+    no_save(false), no_player_bones(false), language(lang_t::EN),
+    lang_name(nullptr),
     prefs_dirty(false)
 {
     reset_options();
@@ -4360,6 +4361,7 @@ enum commandline_option_type
     CLO_WIZARD,
     CLO_EXPLORE,
     CLO_NO_SAVE,
+    CLO_NO_PLAYER_BONES,
     CLO_GDB,
     CLO_NO_GDB, CLO_NOGDB,
     CLO_THROTTLE,
@@ -4385,9 +4387,9 @@ static const char *cmd_ops[] =
     "objstat", "iters", "force-map", "arena", "dump-maps", "test", "script",
     "builddb", "help", "version", "seed", "pregen", "save-version", "sprint",
     "extra-opt-first", "extra-opt-last", "sprint-map", "edit-save",
-    "print-charset", "tutorial", "wizard", "explore", "no-save", "gdb",
-    "no-gdb", "nogdb", "throttle", "no-throttle", "playable-json",
-    "branches-json", "save-json", "gametypes-json", "bones",
+    "print-charset", "tutorial", "wizard", "explore", "no-save",
+    "no-player-bones", "gdb", "no-gdb", "nogdb", "throttle", "no-throttle",
+    "playable-json", "branches-json", "save-json", "gametypes-json", "bones",
 #ifdef USE_TILE_WEB
     "webtiles-socket", "await-connection", "print-webtiles-options",
 #endif
@@ -5594,6 +5596,11 @@ bool parse_args(int argc, char **argv, bool rc_only)
         case CLO_NO_SAVE:
             if (!rc_only)
                 Options.no_save = true;
+            break;
+
+        case CLO_NO_PLAYER_BONES:
+            if (!rc_only)
+                Options.no_player_bones = true;
             break;
 
 #ifdef USE_TILE_WEB
