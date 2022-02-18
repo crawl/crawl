crawl/crawl

commit 58ad9d9fa3b3adf3be215d9f702ce3ab16133a8c

Quiet down Irradiate (#12446)
I'm guessing, based on loudness being set to 0, that it was never
expected to have the explosion noise. However, explode() reset the
loudness anyway, so setting it manually had no effect.

crawl-ref/source/spl-damage.cc
@@ -1360,7 +1360,6 @@ spret cast_irradiate(int powc, actor* who, bool fail)
     beam.source = you.pos();
     beam.target = you.pos();
     beam.hit = AUTOMATIC_HIT;
-    beam.loudness = 0;
     beam.explode(true, true);
 
     apply_random_around_square([powc, who] (coord_def where) {

crawl-ref/source/beam.cc
@@ -5918,7 +5918,7 @@ bool bolt::explode(bool show_more, bool hole_in_the_middle)
     }
 #endif
 
-    if (!is_tracer)
+    if (!is_tracer && flavour != BEAM_VISUAL)
     {
         loudness = explosion_noise(r);
 

commit 99c160cdfe8cc31f2c08a6fb9878817a5e5fcaa5

Fix Shilleiagh noise (frdfg)
When Devastator was added in 9cc3ee3f91d, it was intended to make 7
noise with every hit. However, since it was an explosion, it accidentally
made 15 noise instead, and hence the wiki noted it as making 'fireball-level
noise'. This continued for 10 years until 58ad9d9fa3b, which accidentally
muted it while trying to make Irradiate silent.

This commit restores Devastator to its intended 7 noise per hit.

crawl-ref/source/spl-damage.cc
@@ -1676,8 +1676,11 @@ void shillelagh(actor *wielder, coord_def where, int pow)
     beam.source = wielder->pos();
     beam.target = where;
     beam.hit = AUTOMATIC_HIT;
-    beam.loudness = 7;
     beam.explode();
+
+    // We need to do this separately from the beam, since explosions reset
+    // noise values & visual beams don't cause noise regardless.
+    noisy(7, where);
 
     counted_monster_list affected_monsters;
     for (adjacent_iterator ai(where, false); ai; ++ai)