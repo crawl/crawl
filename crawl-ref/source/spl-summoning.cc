097e55589aa21286b0261eecc7d90d24005db34d
Nicholas Feinberg <pleasingfung@gmail.com>
2022-02-18 12:20:20 -0800

    Fix quiver/summon warnings (CarefulOdds)
    Don't check for interactions with ongoing AOE attacks (OTR, Polar
    Vortex, &c) when you're just quivering a summon spell. Only check
    on cast.
    
    Closes #2429.

--- crawl-ref/source/spl-summoning.cc
+++ crawl-ref/source/spl-summoning.cc
@@ -1149,7 +1149,7 @@ void create_malign_gateway(coord_def point, beh_type beh, string cause,
 spret cast_malign_gateway(actor * caster, int pow, god_type god,
                           bool fail, bool test)
 {
-    if (caster->is_player() && stop_summoning_prompt(MR_RES_POISON))
+    if (!test && caster->is_player() && stop_summoning_prompt(MR_RES_POISON))
         return spret::abort;
 
     coord_def point = find_gateway_location(caster);
