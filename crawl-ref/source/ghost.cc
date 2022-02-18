e1c2155d811078928c0f3b21b5343cc7d58f6845
advil <rawlins@gmail.com>
2022-02-18 09:28:53 -0500

    feat: add a CLO to prevent writing player bones
    This is not really intended as a player facing feature (it doesn't
    prevent ghosts from being generated or anything like that), but as an
    option to support community management features on webtiles.
    Implementing as a separate commit to make it easier to backport.
    
    (cherry picked from commit abffb2d2b2ae4bdebd4207ad2a5038eda789bcbf)
    (cherry picked from commit 4d5d836560838efc031c05e6f08dc6392d05ca49)

--- crawl-ref/source/ghost.cc
+++ crawl-ref/source/ghost.cc
@@ -22,6 +22,7 @@
 #include "mon-transit.h"
 #include "mpr.h"
 #include "ng-input.h"
+#include "options.h"
 #include "skills.h"
 #include "spl-util.h"
 #include "state.h"
@@ -809,6 +810,8 @@ spell_type ghost_demon::translate_spell(spell_type spell) const
 const vector<ghost_demon> ghost_demon::find_ghosts(bool include_player)
 {
     vector<ghost_demon> gs;
+    if (Options.no_player_bones)
+        include_player = false;
 
     if (include_player && you.undead_state(false) == US_ALIVE)
     {
