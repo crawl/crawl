crawl/crawl

commit 4e44252f3f24252e461ee3764b93ec7b8ad007b5

refactor: better organization for animated objects take 2
These are still a bit all over the place, but this at least gives a
slightly more unified API and uses it; small UI cleanups happened as a
consequence, e.g. better labeling in ctrl-x, animated armour gets the
dancing icon (I realize it isn't technically "dancing" but it's still
useful I think). Hopefully haven't missed too much here.

This reverts commit f925c23bd1a27674721db3c623a4d332f2cfaff7.

crawl-ref/source/hiscores.cc
@@ -1383,8 +1383,11 @@ void scorefile_entry::init_death_cause(int dam, mid_t dsrc,
 
             // Setting this is redundant for dancing weapons, however
             // we do care about the above indentification. -- bwr
-            if (mons->type != MONS_DANCING_WEAPON)
-                auxkilldata = env.item[mons->inv[MSLOT_WEAPON]].name(DESC_A);
+            if (!mons_class_is_animated_weapon(mons->type)
+                && mons->get_defining_object())
+            {
+                auxkilldata = mons->get_defining_object()->name(DESC_A);
+            }
         }
 
         const bool death = (you.hp <= 0 || death_type == KILLED_BY_DRAINING);

crawl-ref/source/directn.cc
@@ -3357,7 +3357,7 @@ static string _describe_monster_weapon(const monster_info& mi, bool ident)
 
     if (mi.type == MONS_PANDEMONIUM_LORD)
         desc += " armed with ";
-    else if (mi.type == MONS_DANCING_WEAPON)
+    else if (mons_class_is_animated_weapon(mi.type))
         desc += " ";
     else
         desc += " wielding ";
@@ -3643,13 +3643,15 @@ string get_monster_equipment_desc(const monster_info& mi,
             string str = comma_separated_line(attributes.begin(),
                                               attributes.end());
 
-            if (mi.type == MONS_DANCING_WEAPON
+            if (mons_class_is_animated_weapon(mi.type)
                 || mi.type == MONS_PANDEMONIUM_LORD
                 || mi.type == MONS_PLAYER_GHOST)
             {
                 if (!str.empty())
                     str += " ";
 
+                // animated armour is has "animated" in its name already,
+                // spectral weapons have "spectral".
                 if (mi.type == MONS_DANCING_WEAPON)
                     str += "dancing weapon";
                 else if (mi.type == MONS_PANDEMONIUM_LORD)
@@ -3703,11 +3705,8 @@ string get_monster_equipment_desc(const monster_info& mi,
 
     // Dancing weapons have all their weapon information in their full_name, so
     // we don't need to add another weapon description here (see Mantis 11887).
-    if (!weap.empty()
-        && mi.type != MONS_DANCING_WEAPON && mi.type != MONS_SPECTRAL_WEAPON)
-    {
+    if (!weap.empty() && !mons_class_is_animated_weapon(mi.type))
         item_descriptions.push_back(weap.substr(1)); // strip leading space
-    }
 
     // as with dancing weapons, don't claim animated armours 'wear' their armour
     if (mon_arm && mi.type != MONS_ANIMATED_ARMOUR)

crawl-ref/source/dungeon.cc
@@ -4988,6 +4988,7 @@ static void _dgn_give_mon_spec_items(mons_spec &mspec, monster *mon)
     }
 
     // Make dancing launchers less pathetic.
+    // XX this should be done on creation, not placement?
     if (mons_class_is_animated_weapon(mon->type))
         _give_animated_weapon_ammo(*mon);
 }

crawl-ref/source/mapdef.cc
@@ -4237,7 +4237,7 @@ mons_list::mons_spec_slot mons_list::parse_mons_spec(string spec)
                 return slot;
             }
             else if (mons_class_itemuse(type) < MONUSE_STARTING_EQUIPMENT
-                     && (!mons_class_is_animated_weapon(type)
+                     && (!mons_class_is_animated_object(type)
                          || mspec.items.size() > 1)
                      && (type != MONS_ZOMBIE && type != MONS_SKELETON
                          || invalid_monster_type(mspec.monbase)
@@ -4248,7 +4248,7 @@ mons_list::mons_spec_slot mons_list::parse_mons_spec(string spec)
                 error = make_stringf("Monster '%s' can't use items.",
                     mon_str.c_str());
             }
-            else if (mons_class_is_animated_weapon(type))
+            else if (mons_class_is_animated_object(type))
             {
                 auto item = mspec.items.get_item(0);
                 const auto *unrand = item.ego < SP_FORBID_EGO
@@ -4258,9 +4258,13 @@ mons_list::mons_spec_slot mons_list::parse_mons_spec(string spec)
                 const auto sub = unrand && unrand->base_type != OBJ_UNASSIGNED
                     ? unrand->sub_type : item.sub_type;
 
-                if (get_item_slot(base, sub) != EQ_WEAPON)
+                const auto def_slot = mons_class_is_animated_weapon(type)
+                    ? EQ_WEAPON
+                    : EQ_BODY_ARMOUR;
+
+                if (get_item_slot(base, sub) != def_slot)
                 {
-                    error = make_stringf("Monster '%s' needs a weapon.",
+                    error = make_stringf("Monster '%s' needs a defining item.",
                                          mon_str.c_str());
                 }
             }

crawl-ref/source/melee-attack.cc
@@ -2288,7 +2288,7 @@ string melee_attack::mons_attack_desc()
         ret = " from afar";
     }
 
-    if (weapon && attacker->type != MONS_DANCING_WEAPON && attacker->type != MONS_SPECTRAL_WEAPON)
+    if (weapon && !mons_class_is_animated_weapon(attacker->type))
         ret += " with " + weapon->name(DESC_A);
 
     return ret;

crawl-ref/source/menu.cc
@@ -1591,18 +1591,19 @@ bool MonsterMenuEntry::get_tiles(vector<tile_def>& tileset) const
 
     if (m->type == MONS_DANCING_WEAPON)
     {
-        // For fake dancing weapons, just use a generic long sword, since
-        // fake monsters won't have a real item equipped.
+        // other animated objects use regular monster tiles (TODO: animate
+        // armour's seems broken in this menu)
         item_def item;
-        if (fake)
+
+        if (!fake && m->inv[MSLOT_WEAPON])
+            item = *m->inv[MSLOT_WEAPON];
+
+        if (fake || !item.defined())
         {
             item.base_type = OBJ_WEAPONS;
             item.sub_type  = WPN_LONG_SWORD;
             item.quantity  = 1;
         }
-        else
-            item = *m->inv[MSLOT_WEAPON];
-
         tileset.emplace_back(tileidx_item(item));
         tileset.emplace_back(TILEI_ANIMATED_WEAPON);
     }

crawl-ref/source/mon-act.cc
@@ -1158,7 +1158,7 @@ bool handle_throw(monster* mons, bolt & beem, bool teleport, bool check_only)
 
     if (mons_itemuse(*mons) < MONUSE_STARTING_EQUIPMENT
         && mons->type != MONS_SPECTRAL_THING
-        && !mons_class_is_animated_weapon(mons->type))
+        && !mons_class_is_animated_object(mons->type))
     {
         return false;
     }

crawl-ref/source/mon-death.cc
@@ -1696,6 +1696,8 @@ item_def* monster_die(monster& mons, killer_type killer,
     }
     else if (mons.type == MONS_DANCING_WEAPON)
     {
+        // TODO: does any of the following ever need to happen for other
+        // animated objects?
         if (!hard_reset)
         {
             if (killer == KILL_RESET)

commit a21e45360d95a0d1f88339dfca0561ab4f8c8b13

Fix kaux/ckaux
Broken in 4e44252f3f24252e461ee3764b93ec7b8ad007b5, 9 months ago.

crawl-ref/source/hiscores.cc
@@ -1388,11 +1388,13 @@ void scorefile_entry::init_death_cause(int dam, mid_t dsrc,
 
             // Setting this is redundant for dancing weapons, however
             // we do care about the above indentification. -- bwr
-            if (!mons_class_is_animated_weapon(mons->type)
-                && mons->get_defining_object())
+            if (mons_class_is_animated_weapon(mons->type))
             {
-                auxkilldata = mons->get_defining_object()->name(DESC_A);
-            }
+                const item_def* weap = mons->get_defining_object();
+                if (weap)
+                    auxkilldata = weap->name(DESC_A);
+            } else
+                auxkilldata = env.item[mons->inv[MSLOT_WEAPON]].name(DESC_A);
         }
 
         const bool death = (you.hp <= 0 || death_type == KILLED_BY_DRAINING);