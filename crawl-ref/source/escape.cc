d70920a61ce698edca5fd8562c467180bd7f7511
Rosstin <rmurphy@nianticlabs.com>
2022-03-11 12:54:36 -0800

    Add epilogue strings to miscname.txt.

--- crawl-ref/source/escape.cc
+++ crawl-ref/source/escape.cc
@@ -14,6 +14,7 @@
 
 #include "AppHdr.h"
 
+#include "database.h"
 #include "describe.h"
 #include "english.h"
 #include "enum.h"
@@ -72,7 +73,7 @@ std::string win_messages_religion(god_type god){
 		case GOD_IGNIS:
 			return "";
 		case GOD_JIYVA:
-			return "You spread the dominion of slime across the world, returning all beings to the shapeless ooze from which we all sprang.";
+			return getMiscString("epilogue_jiyva");
 		case GOD_KIKUBAAQUDGHA:
 			return "";
 		case GOD_LUGONU:
@@ -104,8 +105,8 @@ std::string win_messages_religion(god_type god){
 		case GOD_ZIN:
 			return "You spread the word of Zin far and wide.";
 		case GOD_SHINING_ONE:
-			return "You spread the word of Zin far and wide.";
-		case GOD_NO_GOD: // hmm
+			return "You spread the holy wrath of the Shining One throughout the world.";
+		case GOD_NO_GOD:
 		default:
 			return "Your name is spread far and wide as the retriever of the Orb.";
 			break;
@@ -116,65 +117,54 @@ std::string win_messages_demigod(skill_type sk){
 	switch(sk){
 		// fighting and melee weapons
 		case SK_FIGHTING:
-			return "You shed your mortal half and ascend to Godhood, wresting victory over Okawaru to become the new God of Battle.";
+			return getMiscString("epilogue_demigod_fighting");
 		case SK_SHORT_BLADES:
-			return "You shed your mortal half and ascend to Godhood, backstabbing Dithmenos to become the new God of Knives.";
+			return getMiscString("epilogue_demigod_short_blades");
 		case SK_LONG_BLADES:
-			return "With your skill with the sword, you best Okawaru in a duel, becoming the new God of Blades.";
+			return getMiscString("epilogue_demigod_long_blades");
 		case SK_AXES:
-			return "With your mighty axe, you slaughter Trog, becoming the new God of Axes.";
+			return getMiscString("epilogue_demigod_axes");
 		case SK_MACES_FLAILS:
-			return "With your skill with the mace, you crush Okawaru, becoming the God of Maces.";
+			return getMiscString("epilogue_demigod_maces_flails");
 		case SK_POLEARMS:
-			return "With your skill with the polearm, you skewer Okawaru from a distance, becoming the God of Polearms.";
-		case SK_STAVES:
-			return "With your skill with staves, you batter Okawaru into submission, becoming the God of Staves.";
+			return getMiscString("epilogue_demigod_polearms");
+		case SK_STAVES: 
+			// check if your stave is magical? use your equipped/inventory weapon info in some way?
+			return getMiscString("epilogue_demigod_staves");
 		case SK_UNARMED_COMBAT: // Usk, Wu Jian
 			if(you.strength() >= you.dex()){
-				return "With your strong body, you outdance even Uskayaw, becoming the God of Wrestling.";
+				return getMiscString("epilogue_demigod_unarmed_strength");
 			}
 			else{
-				return "With the skills of your body, you crush the Wu Jian Council, becoming the God of Martial Arts.";
+				return getMiscString("epilogue_demigod_unarmed_dex");
 			}
-			break;
 		// ranged weapons
 		case SK_SLINGS:
-			return "With your singular sling, you smite down Trog, becoming the God of Slings.";
-		/*
+			return getMiscString("epilogue_demigod_slings");
 		case SK_BOWS:
-			mprf("With your trusty bow, you snipe down Okawaru, becoming the God of Bows.");
-			break;
+			return getMiscString("epilogue_demigod_bows");
 		case SK_CROSSBOWS:
-			mprf("With your skill with the Crossbow, you pierce Okawaru's defenses, becoming the God of Crossbows.");
-			break;
+			return getMiscString("epilogue_demigod_crossbows");
 		case SK_THROWING:
-			mprf("With your mighty projectiles, you cast down Okawaru, becoming the new God of Throwing.");
-			break;
+			return getMiscString("epilogue_demigod_throwing");
 		// defensive skills
-		case SK_ARMOUR: // Chei? Ash?
-			mprf("With your insurmountable defenses, you gradually unseat Cheibriados, becoming the God of Armour.");
-			//mprf("With your mastery of sartorial defenses, you supplant Ashenzari, becoming the God of Armour.");
-			//TSO?
-			break;
-		case SK_DODGING: // Usk?
-			mprf("With your untouchable moves, you outdance even Uskayaw, becoming the God of Dodging.");
-			// wu jian?
-			break;
+		case SK_ARMOUR:
+			return getMiscString("epilogue_demigod_armour");
+		case SK_DODGING: 
+			return getMiscString("epilogue_demigod_dodging");
 		case SK_STEALTH:
-			mprf("You slip into the darkness and dispatch Dithmenos, becoming the new God of Shadows.");
-			break;
-		case SK_SHIELDS: // Chei, TSO?
-			mprf("With your unparallelled mastery of the shield, you block the Shining One's efforts to thwart you, and become the God of Shields.");
-			break;
+			return getMiscString("epilogue_demigod_stealth");
+		case SK_SHIELDS:
+			return getMiscString("epilogue_demigod_shields");
 		// spellcasting skills
-		case SK_SPELLCASTING: // Sif
-			mprf("With your overwhelming knowledge of spells, your magical knowledge surpasses even Sif Muna, and you become the new God of Magic.");
+		case SK_SPELLCASTING: 
+			return getMiscString("epilogue_demigod_spellcasting");
 			break;
 		case SK_CONJURATIONS: // Veh
 			mprf("With your skill with destructive magic, you best Vehumet, becoming the new God of Conjurations.");
 			break;
 		case SK_HEXES: // Ru
-			mprf("With your clever maledictions, you beguile Ru, becoming the God of Hexes.");
+			mprf("With your clever maledictions, you beguile even Ru, becoming the God of Hexes.");
 			break;
 		case SK_SUMMONINGS: // Makh
 			mprf("With your skill at calling otherworldly beings, you supplant Makhleb, becoming the God of Bindings.");
@@ -200,7 +190,7 @@ std::string win_messages_demigod(skill_type sk){
 			break;
 		case SK_AIR_MAGIC:
 			mprf("With your mastery of the air, you supplant Qazlal Stormbringer, becoming the new God of Storms.");
-			break;*/
+			break;
 		case SK_EARTH_MAGIC:
 			return "With the very firmament supporting you, you ascend to the throne of The Iron City of Dis, becoming the God of Earth.";
 		case SK_POISON_MAGIC: // jivya or fedhas
