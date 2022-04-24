aff69085e68e929c1ec90f9d877d2b01ed1b5add
Brian Power <powerbf.it@gmail.com>
2022-04-25 00:37:54 +1000

    i18n: Handle monclone.h/monclone.cc

--- crawl-ref/source/util/i18n/extract-text.py
+++ crawl-ref/source/util/i18n/extract-text.py
@@ -129,7 +129,7 @@ SKIP_FILES = [
     # keys/properties
     'defines.h', 'dgn-layouts.h', 'dgn-layouts.cc', 'god-abil.h', 'initfile.cc',
     'libunix.h', 'libunix.cc', 'libutil.h', 'libutil.cc', 'mgen-data.h',
-    'mi-enum.h', 'mon-abil.h',
+    'mi-enum.h', 'mon-abil.h', 'mon-clone.h',
     # debug/test stuff
     'debug.h', 'ctest.h', 'ctest.cc', 'fake-main.cc', 'coord-def.h',
     'crash.h', 'crash.cc', 'errors.h', 'errors.cc',
