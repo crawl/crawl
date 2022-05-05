b2816e9c68af1c27bf785215e3b5420cf55cc74e
Nicholas Feinberg <pleasingfung@gmail.com>
2022-05-05 07:05:13 -0700

    Cap Zenata summons (Ge0ff)

--- crawl-ref/source/spl-summoning.cc
+++ crawl-ref/source/spl-summoning.cc
@@ -2628,6 +2628,7 @@ static const map<spell_type, summon_cap> summonsdata =
     { SPELL_SUMMON_TZITZIMITL,        { 0, 3 } },
     { SPELL_SUMMON_HELL_SENTINEL,     { 0, 3 } },
     { SPELL_CONJURE_LIVING_SPELLS,    { 0, 6 } },
+    { SPELL_SHEZAS_DANCE,             { 0, 6 } },
 };
 
 bool summons_are_capped(spell_type spell)
