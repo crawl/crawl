crawl/crawl

commit 18a14b5c600103b97690df5d470f12d585c0c213

Weaken early D OODs
Removal of chaff from the D spawn pool made OODs much nastier than
they were in the past. Let's have no more D:2 water mocassins or
steam dragons.

crawl-ref/source/mon-place.cc
@@ -227,6 +227,21 @@ bool monster_can_submerge(const monster* mon, dungeon_feature_type feat)
         return false;
 }
 
+static int _ood_fuzzspan(level_id &place)
+{
+    if (place.branch != BRANCH_DUNGEON || place.depth >= 5)
+        return 5;
+
+    // Literally any OOD is too nasty for D:1.
+    if (place.depth == 1)
+        return 0;
+
+    // In early D, since player and enemy strength scale so rapidly
+    // with depth, spawn OODs from much closer depths.
+    // Only up to D:4 on D:2, up to D:6 on D:3, and D:8 on D:4.
+    return place.depth;
+}
+
 static void _apply_ood(level_id &place)
 {
     // OODs do not apply to any portal vaults, any 1-level branches, Zot and
@@ -243,23 +258,14 @@ static void _apply_ood(level_id &place)
     level_id old_place = place;
 #endif
 
-    // The OOD fuzz roll is not applied on D:1, and is applied slightly less
-    // often (0.75*0.14) on D:2. All other levels have a straight 14% chance of
-    // moderate OOD fuzz for each monster at level generation.
-    if (place.branch == BRANCH_DUNGEON
-        && (place.depth == 1
-            || place.depth == 2 && one_chance_in(4)))
+    const int fuzzspan = _ood_fuzzspan(place);
+    if (fuzzspan && x_chance_in_y(14, 100))
     {
-        return;
-    }
-
-    if (x_chance_in_y(14, 100))
-    {
-        const int fuzzspan = 5;
-        const int fuzz = max(0, random_range(-fuzzspan, fuzzspan, 2));
-
-        // Quite bizarre logic: why should we fail in >50% cases here?
-        if (fuzz)
+        // We want a left-weighted distribution; slight fuzzing should be much
+        // more common than the full depth fuzz. This does mean that OODs are closer
+        // to a 6% chance than the 14% implied above, which is a bit silly.
+        const int fuzz = random_range(-fuzzspan, fuzzspan);
+        if (fuzz > 0)
         {
             place.depth += fuzz;
             dprf("Monster level fuzz: %d (old: %s, new: %s)",

commit aae9a6c91728f777a9a4475a8fc79449d54ce1cf

Fix OOD fuzzing
Broken in 18a14b5c600 (almost exactly a year ago) to *not* weight
away from the edges. Oops!

crawl-ref/source/mon-place.cc
@@ -264,7 +264,7 @@ static void _apply_ood(level_id &place)
         // We want a left-weighted distribution; slight fuzzing should be much
         // more common than the full depth fuzz. This does mean that OODs are closer
         // to a 6% chance than the 14% implied above, which is a bit silly.
-        const int fuzz = random_range(-fuzzspan, fuzzspan);
+        const int fuzz = random_range(-fuzzspan, fuzzspan, 2);
         if (fuzz > 0)
         {
             place.depth += fuzz;