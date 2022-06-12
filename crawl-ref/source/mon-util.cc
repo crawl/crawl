crawl/crawl

commit 2ca5787500ec8efac2b55a44e937fbcb8f280d80

Don't freeze jelly spawns' souls (ebering)
Casting simulac on the royal jelly's buddlylings didn't even work.

crawl-ref/source/mon-util.cc
@@ -1696,6 +1696,7 @@ bool mons_can_be_spectralised(const monster& mon, bool divine)
 {
     return mon.holiness() & (MH_NATURAL | MH_DEMONIC | MH_HOLY)
            && !mon.is_summoned()
+           && !testbits(mon.flags, MF_NO_REWARD)
            && mon.type != MONS_PANDEMONIUM_LORD
            && (mons_has_attacks(mon, true)
                || divine && mon.has_spells());

commit bc6e321ce9896732971ba3283bc161250ace7a83

Fix simulac/dchan + wretches (dilly)
Broken by 2ca5787500ec8efac2b55a44e937fbcb8f280d80.

crawl-ref/source/mon-util.cc
@@ -1696,7 +1696,8 @@ bool mons_can_be_spectralised(const monster& mon, bool divine)
 {
     return mon.holiness() & (MH_NATURAL | MH_DEMONIC | MH_HOLY)
            && !mon.is_summoned()
-           && !testbits(mon.flags, MF_NO_REWARD)
+           && (!testbits(mon.flags, MF_NO_REWARD)
+               || mon.props.exists(KIKU_WRETCH_KEY))
            && mon.type != MONS_PANDEMONIUM_LORD
            && (mons_has_attacks(mon, true)
                || divine && mon.has_spells());