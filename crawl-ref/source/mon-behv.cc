crawl/crawl

commit 7f122b9c1a71e8b19da7abb5570002bd38a7d26e

Add Vault Sentinels
These are individuals charged with patroling the Vaults branch and keeping
it alert for intruders. They have two abilities:

They can blow on a signal horn, which generates a lot of noise and thus will
wake creatures up and possibly attract them to the sentinel's location.

They can cast Sentinel's Mark, which is a hex that makes all awake creatures
on a level (especially those of normal or better intelligence) aware of the
player's current location at all times. Which means that, no matter where you
are, the level will come hunting for you.

Stat-wise, they are fairly unremarkable fighters with average equipment,
but have a 1 in 3 chance of also spawning with a crossbow.

crawl-ref/source/mon-behv.cc
@@ -466,6 +466,15 @@ void handle_behaviour(monster* mon)
                      && mon->is_travelling()
                      && mon->travel_target == MTRAV_PLAYER))
             {
+                // If their foe is marked, the monster always knows exactly
+                // where they are.
+                if (mons_foe_is_marked(mon))
+                {
+                    mon->target = you.pos();
+                    try_pathfind(mon);
+                    break;
+                }
+
                 // Maybe the foe is just invisible.
                 if (mon->target.origin() && afoe && mon->near_foe())
                 {
@@ -691,8 +700,18 @@ void handle_behaviour(monster* mon)
             // Batty monsters don't automatically reseek so that
             // they'll flitter away, we'll reset them just before
             // they get movement in handle_monsters() instead. -- bwr
-            if (proxFoe && !mons_is_batty(mon))
+            if (proxFoe && !mons_is_batty(mon) || mons_foe_is_marked(mon))
             {
+                new_beh = BEH_SEEK;
+                break;
+            }
+
+            // Intelligent creatures not currently pursuing another foe are
+            // altered by a sentinel's mark
+            if (mon->foe == MHITNOT && !isFriendly
+                && you.duration[DUR_SENTINEL_MARK] && isSmart)
+            {
+                new_foe = MHITYOU;
                 new_beh = BEH_SEEK;
                 break;
             }
@@ -706,6 +725,7 @@ void handle_behaviour(monster* mon)
             // leave the level, in case their current choice is blocked.
             if (!proxFoe && mon->foe != MHITNOT
                    && one_chance_in(isSmart ? 60 : 20)
+                   && !mons_foe_is_marked(mon)
                 || isPacified && one_chance_in(isSmart ? 40 : 120))
             {
                 new_foe = MHITNOT;

crawl-ref/source/beam.cc
@@ -3459,6 +3459,11 @@ void bolt::affect_player_enchantment()
         obvious_effect = true;
         break;
 
+    case BEAM_SENTINEL_MARK:
+        you.sentinel_mark();
+        obvious_effect = true;
+        break;
+
     default:
         // _All_ enchantments should be enumerated here!
         mpr("Software bugs nibble your toes!");
@@ -5859,6 +5864,7 @@ static string _beam_type_name(beam_type type)
     case BEAM_PETRIFYING_CLOUD:      return "calcifying dust";
     case BEAM_BOLT_OF_ZIN:           return "silver light";
     case BEAM_ENSNARE:               return "magic web";
+    case BEAM_SENTINEL_MARK:         return "sentinel's mark";
 
     case NUM_BEAMS:                  die("invalid beam type");
     }

crawl-ref/source/enum.h
@@ -368,7 +368,8 @@ enum beam_type                  // bolt::flavour
     BEAM_BERSERK,
     BEAM_SLEEP,
     BEAM_INNER_FLAME,
-    BEAM_LAST_ENCHANTMENT = BEAM_INNER_FLAME,
+    BEAM_SENTINEL_MARK,
+    BEAM_LAST_ENCHANTMENT = BEAM_SENTINEL_MARK,
 
     BEAM_MEPHITIC,
     BEAM_GLOOM,
@@ -1470,6 +1471,7 @@ enum duration_type
 #if TAG_MAJOR_VERSION == 34
     DUR_BATTLESPHERE,
 #endif
+    DUR_SENTINEL_MARK,
     NUM_DURATIONS
 };
 
@@ -2558,6 +2560,8 @@ enum monster_type                      // menv[].type
     MONS_DEEP_TROLL_SHAMAN,
     MONS_DIAMOND_OBELISK,
 
+    MONS_VAULT_SENTINEL,
+
     NUM_MONSTERS,               // used for polymorph
 
     // MONS_NO_MONSTER can get put in savefiles, so it shouldn't change
@@ -3344,6 +3348,7 @@ enum spell_type
     SPELL_FORCE_LANCE,
     SPELL_MALMUTATE,
     SPELL_MIGHT_OTHER,
+    SPELL_SENTINEL_MARK,
     NUM_SPELLS
 };
 

crawl-ref/source/main.cc
@@ -2847,6 +2847,9 @@ static void _decrement_durations()
                           0,
                           "Your shroud begins to fray at the edges.");
 
+    _decrement_a_duration(DUR_SENTINEL_MARK, delay,
+                          "The sentinel's mark upon you fades away.");
+
     if (!env.sunlight.empty())
         process_sunlights();
 }

crawl-ref/source/mon-abil.cc
@@ -3056,6 +3056,31 @@ bool mon_special_ability(monster* mons, bolt & beem)
             _starcursed_scream(mons, actor_at(beem.target));
         break;
 
+
+    case MONS_VAULT_SENTINEL:
+        if (mons->has_ench(ENCH_CONFUSION))
+            break;
+
+        if (!silenced(mons->pos()) && !mons->has_ench(ENCH_BREATH_WEAPON)
+                && one_chance_in(4))
+        {
+            if (you.can_see(mons))
+            {
+                simple_monster_message(mons, " blows on a signal horn!");
+                noisy(25, mons->pos());
+            }
+            else
+                noisy(25, mons->pos(), "You hear a note blown loudly on a horn!");
+
+            // This is probably coopting the enchant for something beyond its
+            // intended purpose, but the message does match....
+            mon_enchant breath_timeout =
+                mon_enchant(ENCH_BREATH_WEAPON, 1, mons, (4 +  random2(9)) * 10);
+            mons->add_ench(breath_timeout);
+            used = true;
+        }
+        break;
+
     default:
         break;
     }

crawl-ref/source/mon-cast.cc
@@ -904,6 +904,12 @@ bolt mons_spell_beam(monster* mons, spell_type spell_cast, int power,
         beam.flavour  = BEAM_MMISSILE;
         break;
 
+    case SPELL_SENTINEL_MARK:
+        beam.ench_power = 125; //Difficult to resist
+        beam.flavour    = BEAM_SENTINEL_MARK;
+        beam.is_beam    = true;
+        break;
+
     default:
         if (check_validity)
         {

crawl-ref/source/mon-data.h
@@ -4641,6 +4641,19 @@ static monsterentry mondata[] = {
     MONUSE_WEAPONS_ARMOUR, MONEAT_NOTHING, SIZE_SMALL
 },
 
+{
+    MONS_VAULT_SENTINEL, '@', LIGHTBLUE, "vault sentinel",
+    M_FIGHTER | M_WARM_BLOOD | M_SEE_INVIS | M_SPEAKS
+        | M_SPELLCASTER | M_ACTUAL_SPELLS,
+    MR_NO_FLAGS,
+    550, 12, MONS_HUMAN, MONS_HUMAN, MH_NATURAL, -3,
+    { {AT_HIT, AF_PLAIN, 15}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
+    { 6, 3, 5, 0 },
+    1, 13, MST_VAULT_SENTINEL, CE_CONTAMINATED, Z_NOZOMBIE, S_SHOUT,
+    I_NORMAL, HT_LAND, FL_NONE, 10, DEFAULT_ENERGY,
+    MONUSE_WEAPONS_ARMOUR, MONEAT_NOTHING, SIZE_MEDIUM
+},
+
 // mimics
 {
     MONS_INEPT_ITEM_MIMIC, 'X', BLACK, "inept item mimic",

crawl-ref/source/mon-gear.cc
@@ -724,6 +724,23 @@ static item_make_species_type _give_weapon(monster* mon, int level,
         break;
     }
 
+    case MONS_VAULT_SENTINEL:
+        if (!melee_only && one_chance_in(3))
+        {
+            item.base_type = OBJ_WEAPONS;
+            item.sub_type  = WPN_CROSSBOW;
+            break;
+        }
+
+        item.base_type = OBJ_WEAPONS;
+
+        item.sub_type = random_choose_weighted(
+            5, WPN_LONG_SWORD,   4, WPN_FALCHION,
+            3, WPN_WAR_AXE,      3, WPN_MORNINGSTAR,
+            0);
+
+        break;
+
     case MONS_CYCLOPS:
     case MONS_STONE_GIANT:
         item.base_type = OBJ_MISSILES;
@@ -1944,6 +1961,11 @@ static void _give_armour(monster* mon, int level, bool spectral_orcs)
         item.base_type = OBJ_ARMOUR;
         item.sub_type  = random_choose(ARM_CHAIN_MAIL,   ARM_SPLINT_MAIL,
                                        ARM_PLATE_ARMOUR, -1);
+        break;
+
+    case MONS_VAULT_SENTINEL:
+        item.base_type = OBJ_ARMOUR;
+        item.sub_type  = random_choose(ARM_RING_MAIL,   ARM_SCALE_MAIL, -1);
         break;
 
     case MONS_FREDERICK:

commit e82337ba6aab7ff1d71a8637a8f50531d7f349d3

Fix batty monsters + Mark (dilly)
Don't make Mark turn batty monsters into unstoppable killing machines.

This has been broken since 7f122b9c1a7 (2013), when Vault Sentinels
and hence Mark were first added.

crawl-ref/source/mon-behv.cc
@@ -675,7 +675,7 @@ void handle_behaviour(monster* mon)
             // Batty monsters don't automatically reseek so that
             // they'll flitter away, we'll reset them just before
             // they get movement in handle_monsters() instead. -- bwr
-            if (proxFoe && !mons_is_batty(*mon) || mons_foe_is_marked(*mon))
+            if ((proxFoe || mons_foe_is_marked(*mon)) && !mons_is_batty(*mon))
             {
                 new_beh = BEH_SEEK;
                 break;