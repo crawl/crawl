crawl/crawl

commit 437f8d1d3d7ac52eafa3a8e7bdbfce506b4eac7f

Add a new unrand: the lochaber axe
Long long ago, bardiches used to be known as lochaber axes, until they
were renamed in commit 8b14a9, on December 13, 2007, presumably so
that players wouldn't think they were some kind of axe.

The lochaber axe was gone... but not forgotten...

It's back, reimplemented as an unrand, and this time, there's no problem
if players think it's some kind of axe. Because it is. The lochaber
axe has both reach and cleaving, and combines them both into a sort of
"super-cleave", aka "reave", which attacks everybody in a radius of 2
with each swing.

Additionally, due to its nature as a combination of polearm and axe,
either skill can be used to wield it -- the higher of your Polearms or
Axes skill will be used to determine its damage and delay.

Currently, it's statted out as a +5 bardiche with -1 to base accuracy
and +1 to base damage and base delay. Currently uses the old tile for
lochaber axes, although it will probably need something more menacing
before it's ready for prime-time.

Much thanks to PleasingFungus for coding assistance, and gammafunk for
discussions, the lochaber axe tile, and not immediately reverting this.

crawl-ref/source/fight.cc
@@ -770,7 +770,8 @@ bool force_player_cleave(coord_def target)
 
     if (!cleave_targets.empty())
     {
-        targeter_cleave hitfunc(&you, target);
+        const int range = player_equip_unrand(UNRAND_LOCHABER_AXE) ? 2 : 1;
+        targeter_cleave hitfunc(&you, target, range);
         if (stop_attack_prompt(hitfunc, "attack"))
             return true;
 
@@ -784,12 +785,20 @@ bool force_player_cleave(coord_def target)
 
 bool attack_cleaves(const actor &attacker, int which_attack)
 {
-    const item_def* weap = attacker.weapon(which_attack);
+    if (attacker.is_player()
+        && (you.form == transformation::storm || you.duration[DUR_CLEAVE]))
+    {
+        return true;
+    }
 
-    return weap && item_attack_skill(*weap) == SK_AXES
-        || attacker.is_player()
-            && (you.form == transformation::storm
-                || you.duration[DUR_CLEAVE]);
+    const item_def* weap = attacker.weapon(which_attack);
+    return weap && weapon_cleaves(*weap);
+}
+
+bool weapon_cleaves(const item_def &weap)
+{
+    return item_attack_skill(weap) == SK_AXES
+           || is_unrandom_artefact(weap, UNRAND_LOCHABER_AXE);
 }
 
 /**
@@ -812,25 +821,28 @@ void get_cleave_targets(const actor &attacker, const coord_def& def,
     if (actor_at(def))
         targets.push_back(actor_at(def));
 
+    const item_def* weap = attacker.weapon(which_attack);
+
     if (attack_cleaves(attacker, which_attack))
     {
         const coord_def atk = attacker.pos();
-        coord_def atk_vector = def - atk;
-        const int dir = random_choose(-1, 1);
+        const bool lochaber = weap && is_unrandom_artefact(*weap, UNRAND_LOCHABER_AXE);
+        const int cleave_radius = lochaber ? 2 : 1;
 
-        for (int i = 0; i < 7; ++i)
+        for (distance_iterator di(atk, true, true, cleave_radius); di; ++di)
         {
-            atk_vector = rotate_adjacent(atk_vector, dir);
-
-            actor *target = actor_at(atk + atk_vector);
-            if (target && !_dont_harm(attacker, *target))
-                targets.push_back(target);
+            if (*di == def) continue; // no double jeopardy
+            actor *target = actor_at(*di);
+            if (!target || _dont_harm(attacker, *target))
+                continue;
+            if (di.radius() == 2 && !can_reach_attack_between(atk, *di, REACH_TWO))
+                continue;
+            targets.push_back(target);
         }
     }
 
     // fake cleaving: gyre and gimble's extra attacks are implemented as
     // cleaving attacks on enemies already in `targets`
-    const item_def* weap = attacker.weapon(which_attack);
     if (weap && is_unrandom_artefact(*weap, UNRAND_GYRE))
     {
         list<actor*> new_targets;
@@ -856,10 +868,9 @@ void attack_cleave_targets(actor &attacker, list<actor*> &targets,
                            wu_jian_attack_type wu_jian_attack,
                            bool is_projected)
 {
+    const item_def* weap = attacker.weapon(attack_number);
     if (attacker.is_player())
     {
-        const item_def* weap = attacker.weapon(attack_number);
-
         if ((wu_jian_attack == WU_JIAN_ATTACK_WHIRLWIND
              || wu_jian_attack == WU_JIAN_ATTACK_WALL_JUMP
              || wu_jian_attack == WU_JIAN_ATTACK_TRIGGERED_AUX)
@@ -870,13 +881,15 @@ void attack_cleave_targets(actor &attacker, list<actor*> &targets,
             // worshiping Wu they'll be able to cleave their Wu attacks.
         }
     }
-
+    const bool reaching = weap && weapon_reach(*weap) > REACH_NONE;
     while (attacker.alive() && !targets.empty())
     {
         actor* def = targets.front();
 
         if (def && def->alive() && !_dont_harm(attacker, *def)
-            && (is_projected || adjacent(attacker.pos(), def->pos())))
+            && (is_projected
+                || adjacent(attacker.pos(), def->pos())
+                || reaching))
         {
             melee_attack attck(&attacker, def, attack_number,
                                ++effective_attack_number, true);

crawl-ref/source/attack.cc
@@ -35,6 +35,7 @@
 #include "nearby-danger.h"
 #include "pronoun-type.h"
 #include "religion.h"
+#include "skills.h"
 #include "spl-util.h"
 #include "state.h"
 #include "stepdown.h"
@@ -375,6 +376,7 @@ void attack::init_attack(skill_type unarmed_skill, int attack_number)
     ASSERT(attacker);
 
     wpn_skill       = weapon ? item_attack_skill(*weapon) : unarmed_skill;
+
     if (attacker->is_player() && you.form_uses_xl())
         wpn_skill = SK_FIGHTING; // for stabbing, mostly
 

crawl-ref/source/dat/clua/stash.lua
@@ -52,7 +52,11 @@ function ch_stash_search_annotate_item(it)
 
   local skill = it.weap_skill
   if skill then
-    annot = annot .. "{" .. skill .. "} "
+    local skills = crawl.split(skill, ",")
+    for i = 1, #skills, 1
+    do
+        annot = annot .. "{" .. skills[i] .. "} "
+    end
     local hands = it.hands
     local hands_adj
     if hands == 2 then

crawl-ref/source/describe.cc
@@ -1381,6 +1381,27 @@ static string _handedness_string(const item_def &item)
 
 }
 
+static string _category_string(const item_def &item)
+{
+    if (is_unrandom_artefact(item, UNRAND_LOCHABER_AXE))
+        return ""; // handled in art-data DBRAND
+
+    string description = "";
+    description += "This ";
+    if (is_unrandom_artefact(item))
+        description += get_artefact_base_name(item);
+    else
+        description += "weapon";
+    description += " falls into the";
+
+    const skill_type skill = item_attack_skill(item);
+
+    description +=
+        make_stringf(" '%s' category. ",
+                     skill == SK_FIGHTING ? "buggy" : skill_name(skill));
+    return description;
+}
+
 static string _describe_weapon(const item_def &item, bool verbose, bool monster)
 {
     string description;
@@ -1400,7 +1421,7 @@ static string _describe_weapon(const item_def &item, bool verbose, bool monster)
                           ? get_weapon_brand(item) : SPWPN_NORMAL;
     const int damtype = get_vorpal_type(item);
 
-    if (verbose)
+    if (verbose && !is_unrandom_artefact(item, UNRAND_LOCHABER_AXE))
     {
         switch (item_attack_skill(item))
         {
@@ -1584,18 +1605,7 @@ static string _describe_weapon(const item_def &item, bool verbose, bool monster)
 
     if (verbose)
     {
-        description += "\n\nThis ";
-        if (is_unrandom_artefact(item))
-            description += get_artefact_base_name(item);
-        else
-            description += "weapon";
-        description += " falls into the";
-
-        const skill_type skill = item_attack_skill(item);
-
-        description +=
-            make_stringf(" '%s' category. ",
-                         skill == SK_FIGHTING ? "buggy" : skill_name(skill));
+        description += "\n\n" + _category_string(item);
 
         // XX this is shown for felids, does that actually make sense?
         description += _handedness_string(item);

crawl-ref/source/fight.h
@@ -48,6 +48,7 @@ int stab_bonus_denom(stab_type stab);
 
 bool force_player_cleave(coord_def target);
 bool attack_cleaves(const actor &attacker, int which_attack = -1);
+bool weapon_cleaves(const item_def &item);
 void get_cleave_targets(const actor &attacker, const coord_def& def,
                         list<actor*> &targets, int which_attack = -1);
 void attack_cleave_targets(actor &attacker, list<actor*> &targets,

crawl-ref/source/item-prop.cc
@@ -1756,6 +1756,18 @@ bool is_brandable_weapon(const item_def &wpn, bool allow_ranged, bool divine)
 }
 
 /**
+ * Which skill should the lochaber axe use?
+ *
+ * @return The higher of your Polearms and Axes skills.
+ */
+static skill_type _lochaber_skill()
+{
+    return you.skill(SK_AXES, 100, false, true) >
+           you.skill(SK_POLEARMS, 100, false, true) ?
+               SK_AXES : SK_POLEARMS;
+}
+
+/**
  * Returns the skill used by the given item to attack.
  *
  * @param item  The item under consideration.
@@ -1767,6 +1779,8 @@ skill_type item_attack_skill(const item_def &item)
     switch (item.base_type)
     {
     case OBJ_WEAPONS:
+        if (is_unrandom_artefact(item, UNRAND_LOCHABER_AXE))
+            return _lochaber_skill();
         return Weapon_prop[ Weapon_index[item.sub_type] ].skill;
     case OBJ_STAVES:
         return SK_STAVES;
@@ -1883,6 +1897,12 @@ bool item_skills(const item_def &item, set<skill_type> &skills)
     const skill_type sk = item_attack_skill(item);
     if (sk != SK_FIGHTING)
         skills.insert(sk);
+
+    if (is_unrandom_artefact(item, UNRAND_LOCHABER_AXE))
+    {
+        skills.insert(SK_POLEARMS);
+        skills.insert(SK_AXES);
+    }
 
     return !skills.empty();
 }
@@ -2024,8 +2044,11 @@ reach_type weapon_reach(const item_def &item)
 {
     if (is_unrandom_artefact(item, UNRAND_RIFT))
         return REACH_THREE;
-    if (item_attack_skill(item) == SK_POLEARMS)
+    if (item_attack_skill(item) == SK_POLEARMS
+        || is_unrandom_artefact(item, UNRAND_LOCHABER_AXE))
+    {
         return REACH_TWO;
+    }
     return REACH_NONE;
 }
 

crawl-ref/source/l-item.cc
@@ -10,6 +10,7 @@
 
 #include "adjust.h"
 #include "artefact.h"
+#include "art-enum.h"
 #include "cluautil.h"
 #include "colour.h"
 #include "coord.h"
@@ -569,7 +570,14 @@ IDEF(weap_skill)
     if (skill == SK_FIGHTING)
         return 0;
 
-    lua_pushstring(ls, skill_name(skill));
+    if (is_unrandom_artefact(*item, UNRAND_LOCHABER_AXE))
+    {
+        lua_pushstring(ls, make_stringf("%s,%s",
+                                        skill_name(SK_POLEARMS),
+                                        skill_name(SK_AXES)).c_str());
+    }
+    else
+        lua_pushstring(ls, skill_name(skill));
     lua_pushnumber(ls, skill);
     return 2;
 }

crawl-ref/source/melee-attack.cc
@@ -186,7 +186,8 @@ bool melee_attack::handle_phase_attempted()
         }
         else if (!cleave_targets.empty())
         {
-            targeter_cleave hitfunc(attacker, defender->pos());
+            const int range = player_equip_unrand(UNRAND_LOCHABER_AXE) ? 2 : 1;
+            targeter_cleave hitfunc(attacker, defender->pos(), range);
             if (stop_attack_prompt(hitfunc, "attack"))
             {
                 cancel_attack = true;

commit 87332871fa3a1ec3ace19dae7ff7ea9569b833e3

Don't crash when a dead monster cleaves
E.g. if they're killed by minotaur retaliation midway through an axe
swing.

Broken in 437f8d1d3d7ac52eafa3a8e7bdbfce506b4eac7f.

crawl-ref/source/fight.cc
@@ -868,6 +868,8 @@ void attack_cleave_targets(actor &attacker, list<actor*> &targets,
                            wu_jian_attack_type wu_jian_attack,
                            bool is_projected)
 {
+    if (!attacker.alive())
+        return;
     const item_def* weap = attacker.weapon(attack_number);
     if (attacker.is_player())
     {