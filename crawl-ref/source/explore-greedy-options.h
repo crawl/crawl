1bddeb3ff1359ec3a78c0badbff5511c30b24b40
Rytis Petronis <petronisrytis@gmail.com>
2022-06-25 23:03:57 +0300

    remove spaces at eol

--- crawl-ref/source/explore-greedy-options.h
+++ crawl-ref/source/explore-greedy-options.h
@@ -3,10 +3,10 @@
 enum class explore_greedy_options
 {
     // Default, same as before the change, greedy visits piles of items
-    EG_PILE, 
+    EG_PILE,
     // Greedy explore now visits enchanted items both in stacks and not,
     // ignores piles of mundane items
-    EG_ENCHANTED, 
+    EG_ENCHANTED,
     // Greedy explore visits both piles and single enchanted items
     EG_BOTH,
 };
