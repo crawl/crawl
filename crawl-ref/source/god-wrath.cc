00251e6216e2981d970e0e4732fccf5651da91e1
riverfiend <63566628+riverfiend@users.noreply.github.com>
2022-07-09 12:57:38 -0500

    fix a paren

--- crawl-ref/source/god-wrath.cc
+++ crawl-ref/source/god-wrath.cc
@@ -1021,7 +1021,7 @@ static void _lugonu_minion_retribution()
     // if this is major wrath, summon a few minions; 0 below xl9, 0-3 at xl 27.
     // otherwise, summon around 1 + xl/7 minions, maxing at 6 at xl 21.
     const int how_many = (major ? random2(you.experience_level / 9 + 1)
-                                : max(random2(3) + you.experience_level / 7),
+                                : max(random2(3) + you.experience_level / 7,
                                       1));
 
     // did we successfully summon any minions? (potentially set true below)
