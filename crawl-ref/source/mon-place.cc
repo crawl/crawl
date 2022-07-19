crawl/crawl

commit 74bf5736c0002df9636fc6333b837f5770b722dd

Remove env.spawn_random_rate (riverfiend, acrobat)
This field was extremely useless and mostly misleadingly misused.

This might fix a bug with orb spawns not spawning in Temple. At any
rate, they spawn there now.

crawl-ref/source/mon-place.cc
@@ -274,14 +274,30 @@ static void _apply_ood(level_id &place)
     }
 }
 
+/**
+ * Rate at which random monsters spawn, with lower numbers making
+ * them spawn more often (5 or less causes one to spawn about every
+ * 5 turns). 0 stops random generation.
+ */
+static int _get_monster_spawn_rate()
+{
+    if (player_in_branch(BRANCH_ABYSS))
+    {
+        if (player_in_starting_abyss())
+            return 50;
+        return 5 * (have_passive(passive_t::slow_abyss) ? 2 : 1);
+    }
+
+    if (player_on_orb_run())
+        return have_passive(passive_t::slow_orb_run) ? 36 : 18;
+
+    return 50;
+}
+
 //#define DEBUG_MON_CREATION
 
 /**
  * Spawn random monsters.
-
- * The spawn rate defaults to the current env.spawn_random_rate for the branch,
- * but is modified by whether the player is in the abyss and on what level, as
- * well as whether the player has the orb.
  */
 void spawn_random_monsters()
 {
@@ -301,32 +317,14 @@ void spawn_random_monsters()
 #ifdef DEBUG_MON_CREATION
     mprf(MSGCH_DIAGNOSTICS, "in spawn_random_monsters()");
 #endif
-    int rate = env.spawn_random_rate;
-    if (!rate)
-    {
-#ifdef DEBUG_MON_CREATION
-        mprf(MSGCH_DIAGNOSTICS, "random monster gen turned off");
-#endif
-        return;
-    }
 
-    if (player_on_orb_run())
-        rate = have_passive(passive_t::slow_orb_run) ? 36 : 18;
-
-    if (player_in_branch(BRANCH_ABYSS))
-    {
-        if (!player_in_starting_abyss())
-            rate = 5;
-        if (have_passive(passive_t::slow_abyss))
-            rate *= 2;
-    }
-
+    const int rate = _get_monster_spawn_rate();
     if (!x_chance_in_y(5, rate))
         return;
 
     // Orb spawns. Don't generate orb spawns in Abyss to show some mercy to
     // players that get banished there on the orb run.
-    // ...except in the deep Abyss!
+    // ...but in the deep abyss, do spawn nearby monsters sometimes!
     if (player_on_orb_run() && !player_in_branch(BRANCH_ABYSS)
         || player_in_branch(BRANCH_ABYSS)
            && you.depth > 5

crawl-ref/source/dungeon.cc
@@ -1539,28 +1539,6 @@ void dgn_reset_level(bool enable_random_maps)
     // Lose all listeners.
     dungeon_events.clear();
 
-    // Set default random monster generation rate (smaller is more often,
-    // except that 0 == no random monsters).
-    if (player_in_branch(BRANCH_TEMPLE)
-        && !player_on_orb_run() // except for the Orb run
-        || crawl_state.game_is_tutorial())
-    {
-        // No random monsters in tutorial or ecu temple
-        env.spawn_random_rate = 0;
-    }
-    else if (player_in_connected_branch()
-             || (player_on_orb_run() && !player_in_branch(BRANCH_ABYSS)))
-        env.spawn_random_rate = 240;
-    else if (player_in_branch(BRANCH_ABYSS)
-             || player_in_branch(BRANCH_PANDEMONIUM))
-    {
-        // Abyss spawn rate is set for those characters that start out in the
-        // Abyss; otherwise the number is ignored in the Abyss.
-        env.spawn_random_rate = 50;
-    }
-    else
-        // No random monsters in portal vaults if we don't have the orb.
-        env.spawn_random_rate = 0;
     env.density = 0;
     env.forest_awoken_until = 0;
 

crawl-ref/source/env.h
@@ -73,11 +73,6 @@ struct crawl_environment
     // Sort of like player::attribute
     CrawlHashTable properties;
 
-    // Rate at which random monsters spawn, with lower numbers making
-    // them spawn more often (5 or less causes one to spawn about every
-    // 5 turns). Set to 0 to stop random generation.
-    int spawn_random_rate;
-
     // Time when level was saved (hence we write out you.elapsed_time
     // (but load it back to env.elapsed_time); used during level load
     int elapsed_time;

crawl-ref/source/tag-version.h
@@ -274,6 +274,7 @@ enum tag_minor_version
     TAG_MINOR_REMOVE_CT_SKILLS,    // Remove the very long-unused ct skills.
     TAG_MINOR_MERGE_RANGED,        // Merge all ranged weapon skills together.
     TAG_MINOR_RECOMPRESS_BADMUTS,  // Reduce some more mutations to 2 levels.
+    TAG_MINOR_SPAWN_RATE,          // Remove the env.spawn_random_rate field.
 #endif
     NUM_TAG_MINORS,
     TAG_MINOR_VERSION = NUM_TAG_MINORS - 1

crawl-ref/source/tags.cc
@@ -4816,8 +4816,6 @@ static void _tag_construct_level(writer &th)
     marshallCoord(th, env.sanctuary_pos);
     marshallByte(th, env.sanctuary_time);
 
-    marshallInt(th, env.spawn_random_rate);
-
     env.markers.write(th);
     env.properties.write(th);
 
@@ -6410,7 +6408,10 @@ static void _tag_read_level(reader &th)
     env.sanctuary_pos  = unmarshallCoord(th);
     env.sanctuary_time = unmarshallByte(th);
 
-    env.spawn_random_rate = unmarshallInt(th);
+#if TAG_MAJOR_VERSION == 34
+    if (th.getMinorVersion() < TAG_MINOR_SPAWN_RATE)
+        unmarshallInt(th); // was env.spawn_random_rate
+#endif
 
     env.markers.read(th);
 #if TAG_MAJOR_VERSION == 34

crawl-ref/source/wiz-mon.cc
@@ -303,40 +303,6 @@ void debug_list_monsters()
     }
 }
 
-void wizard_spawn_control()
-{
-    // 50 spots are reserved for non-wandering monsters.
-    int max_spawn = MAX_MONSTERS - 50;
-    for (monster_iterator mi; mi; ++mi)
-        if (mi->alive())
-            max_spawn--;
-
-    if (max_spawn <= 0)
-    {
-        mprf(MSGCH_PROMPT, "Level already filled with monsters, "
-                           "get rid of some of them first.");
-        return;
-    }
-
-    mprf(MSGCH_PROMPT, "Spawn how many random monsters (max %d)? ",
-         max_spawn);
-
-    if (cancellable_get_line(specs, sizeof(specs)))
-    {
-        canned_msg(MSG_OK);
-        return;
-    }
-
-    const int num = min(atoi(specs), max_spawn);
-    if (num <= 0)
-        return;
-
-    mprf("Spawning %i monster%s.", num, num == 1 ? "" : "s");
-
-    for (int i = 0; i < num; ++i)
-        spawn_random_monsters();
-}
-
 static const char* ht_names[] =
 {
     "land",

crawl-ref/source/wiz-mon.h
@@ -8,7 +8,6 @@
 #ifdef WIZARD
 
 void wizard_create_spec_monster_name();
-void wizard_spawn_control();
 void wizard_detect_creatures();
 void wizard_dismiss_all_monsters(bool force_all = false);
 void debug_list_monsters();

crawl-ref/source/wizard.cc
@@ -139,7 +139,7 @@ static void _do_wizard_command(int wiz_command)
     case CONTROL('Q'): wizard_toggle_dprf(); break;
 
     case 'r': wizard_change_species(); break;
-    case 'R': wizard_spawn_control(); break;
+    case 'R':
     case CONTROL('R'): wizard_recreate_level(); break;
 
     case 's':
@@ -442,7 +442,7 @@ int list_wizard_commands(bool do_redraw_screen)
                        "<yellow>Builder debugging</yellow>\n"
                        "<w>L</w>      place a vault by name\n"
                        "<w>P</w>      create a level based on a vault\n"
-                       "<w>Ctrl-R</w> regenerate current level\n"
+                       "<w>R</w> regenerate current level\n"
                        "<w>Ctrl-A</w> generate new Abyss area\n"
                        "<w>K</w>      mark all vaults as unused\n"
                        "<w>:</w>      find branches and overflow\n"
@@ -465,7 +465,6 @@ int list_wizard_commands(bool do_redraw_screen)
                        "<w>e</w>      trigger explore traps\n"
                        "<w>Ctrl-B</w> banish yourself to the Abyss\n"
                        "<w>Ctrl-S</w> change Abyss speed\n"
-                       "<w>R</w>      change monster spawn rate\n"
                        "<w>Ctrl-W</w> change Shoals' tide speed\n",
                        true);
 

commit ee4989fb639b303d8b4878e2fcb01efce70561f5

Fix: don't spawn monsters in portals (CarefulOdds, Goratrix)
Broken in 74bf5736c0002df9636fc6333b837f5770b722dd .

crawl-ref/source/mon-place.cc
@@ -304,12 +304,12 @@ void spawn_random_monsters()
     if (crawl_state.disables[DIS_SPAWNS])
         return;
 
-    if (crawl_state.game_is_arena()
-        || (crawl_state.game_is_sprint()
-            && player_in_connected_branch()
-            && you.chapter == CHAPTER_ORB_HUNTING)
-        // Spawns no longer occur outside the Orb run in connected branches.
-        || !player_on_orb_run() && player_in_connected_branch())
+    if (crawl_state.game_is_arena())
+        return;
+
+    if (!player_on_orb_run()
+        && !player_in_branch(BRANCH_ABYSS)
+        && !player_in_branch(BRANCH_PANDEMONIUM))
     {
         return;
     }