crawl/crawl

commit 29c30a7bfa8049fde98852a123a78d82baa4abe0

fix: explicit default foreground for to_colour_string
When the crawl binary renders colour strings, it uses lightgray as the
default foreground color. However, this is not explicitly part of the
string, so the webtiles color parsing code was not inserting a lightgray
span, leading to artifacts. This commit ensures that this default is
present in all autogenerated colour strings; it may or may not have some
side effects I haven't noticed. (N.b. trying to change this on the
webtiles side breaks a *lot* of formatting, unfortunately.)

crawl-ref/source/format.cc
@@ -356,7 +356,9 @@ string formatted_string::tostring(int s, int e) const
 
 string formatted_string::to_colour_string() const
 {
-    string st;
+    // this is necessary because of the default of LIGHTGRAY in last_colour.
+    // (If this were changed to write closing tags, it might not be?)
+    string st = "<lightgray>";
     const int size = ops.size();
     for (int i = 0; i < size; ++i)
     {

commit f9470816a2fd6e15fb311452da0659f001cb82e3

fix: correct formatted_string::trim implementation
The first pass at this function only trimmed whitespace outside of color
tags, which was previously reasonable for the main use cases. However,
29c30a7bfa8 broke that assumption, leading to dead space in some popups.
This commit is a full formatted_string::trim implementation that trims
whitespace regardless of color tags.

crawl-ref/source/format.cc
@@ -485,7 +485,14 @@ formatted_string formatted_string::substr_bytes(int pos, int length) const
 
 formatted_string formatted_string::trim() const
 {
-    return parse_string(trimmed_string(to_colour_string()));
+    string nocolor = tostring();
+    auto left_trim = nocolor.find_first_not_of(" \t\n\r");
+    // in principle this should preserve the colors I guess, but it's a lot
+    // easier not to do that...
+    if (left_trim == string::npos)
+        return formatted_string();
+    auto right_trim = nocolor.find_last_not_of(" \t\n\r");
+    return substr_bytes(left_trim, right_trim - left_trim + 1);
 }
 
 void formatted_string::del_char()