crawl/crawl

commit bdabc8c6d3d15eae00cd2e427f43c04c37675bc5

Fix: allow autoexplore with inhibited regen (various)
Yet another in a very long series of issues with i_feel_safe(), a876b224025
fixed various issues with resting while inhibited regen prevented the
player from healing. However, it unintentionally broke many other things
that depend on i_feel_safe(), such as autoexplore, running, interlevel
travel, spell memorization, etc etc. It also gave a very confusing message -
it was not at all clear that inhibited regen was at fault.

This refactors the code to split out the relevant check into a new can_rest_here()
function. (Maybe that function should also call i_feel_safe()?) It will almost
certainly cause new issues.

This closes #2595.

crawl-ref/source/main.cc
@@ -1725,7 +1725,7 @@ static void _do_rest()
         return;
     }
 
-    if (i_feel_safe())
+    if (i_feel_safe() && can_rest_here())
     {
         if (you.is_sufficiently_rested(true) && ancestor_full_hp())
         {
@@ -2112,7 +2112,7 @@ void process_command(command_type cmd, command_type prev_cmd)
     case CMD_ADJUST_INVENTORY: adjust(); break;
 
     case CMD_SAFE_WAIT:
-        if (!i_feel_safe(true))
+        if (!i_feel_safe(true) && can_rest_here())
             break;
         // else fall-through
     case CMD_WAIT:

crawl-ref/source/nearby-danger.cc
@@ -148,10 +148,6 @@ bool mons_is_safe(const monster* mon, const bool want_move,
                            // monsters capable of throwing or zapping wands.
                            || !mons_can_hurt_player(mon)));
 
-    // is this overkill? The main goal here is to disable or interrupt rests
-    // and waits when there is an inhibiting monster in view
-    is_safe = is_safe && !regeneration_is_inhibited(mon);
-
     if (consider_user_options)
     {
         bool moving = you_are_delayed()
@@ -172,6 +168,42 @@ bool mons_is_safe(const monster* mon, const bool want_move,
     }
 
     return is_safe;
+}
+
+static string _seen_monsters_announcement(const vector<monster*> &visible,
+                                          bool sensed_monster)
+{
+    // Announce the presence of monsters (Eidolos).
+    if (visible.size() == 1)
+    {
+        const monster& m = *visible[0];
+        return make_stringf("%s is nearby!", m.name(DESC_A).c_str());
+    }
+    if (visible.size() > 1)
+        return "There are monsters nearby!";
+    if (sensed_monster)
+        return "There is a strange disturbance nearby!";
+    return "";
+}
+
+static void _announce_monsters(string announcement, vector<monster*> &visible)
+{
+    mprf(MSGCH_WARN, "%s", announcement.c_str());
+
+    if (Options.use_animations & UA_MONSTER_IN_SIGHT)
+    {
+        static bool tried = false; // !!??!!
+
+        if (visible.size() && tried)
+        {
+            monster_view_annotator flasher(&visible);
+            delay(100);
+        }
+        else if (visible.size())
+            tried = true;
+        else
+            tried = false;
+    }
 }
 
 // Return all nearby monsters in range (default: LOS) that the player
@@ -292,46 +324,39 @@ bool i_feel_safe(bool announce, bool want_move, bool just_monsters,
     vector<monster* > visible;
     copy_if(monsters.begin(), monsters.end(), back_inserter(visible),
             [](const monster *mon){ return mon->visible_to(&you); });
+    const bool sensed = any_of(monsters.begin(), monsters.end(),
+                   [](const monster *mon){
+                       return env.map_knowledge(mon->pos()).flags & MAP_INVISIBLE_MONSTER;
+                   });
 
-    const bool sensed_monster = any_of(monsters.begin(), monsters.end(),
-            [](const monster *mon){
-                return env.map_knowledge(mon->pos()).flags & MAP_INVISIBLE_MONSTER;
-            });
+    const string announcement = _seen_monsters_announcement(visible, sensed);
+    if (!announce || announcement.empty())
+        return announcement.empty();
+    _announce_monsters(announcement, visible);
+    return false;
+}
 
-    // Announce the presence of monsters (Eidolos).
-    string msg;
-    if (visible.size() == 1)
+bool can_rest_here()
+{
+    // XXX: consider doing a check for whether your regen is *ever* inhibited
+    // before iterating over each monster.
+    vector<monster*> visible;
+    bool sensed = false;
+    for (monster_near_iterator mi(you.pos(), LOS_NO_TRANS); mi; ++mi)
     {
-        const monster& m = *visible[0];
-        msg = make_stringf("%s is nearby!", m.name(DESC_A).c_str());
+        if (!regeneration_is_inhibited(*mi))
+            continue;
+        if (mi->visible_to(&you))
+            visible.push_back(*mi);
+        else if (env.map_knowledge(mi->pos()).flags & MAP_INVISIBLE_MONSTER)
+            sensed = true;
     }
-    else if (visible.size() > 1)
-        msg = "There are monsters nearby!";
-    else if (sensed_monster)
-        msg = "There is a strange disturbance nearby!";
-    else
+
+    const string announcement = _seen_monsters_announcement(visible, sensed);
+    if (announcement.empty())
         return true;
 
-    if (announce)
-    {
-        mprf(MSGCH_WARN, "%s", msg.c_str());
-
-        if (Options.use_animations & UA_MONSTER_IN_SIGHT)
-        {
-            static bool tried = false;
-
-            if (visible.size() && tried)
-            {
-                monster_view_annotator flasher(&visible);
-                delay(100);
-            }
-            else if (visible.size())
-                tried = true;
-            else
-                tried = false;
-        }
-    }
-
+    _announce_monsters(announcement, visible);
     return false;
 }
 

crawl-ref/source/nearby-danger.h
@@ -33,6 +33,8 @@ bool i_feel_safe(bool announce = false, bool want_move = false,
                  bool just_monsters = false, bool check_dist = true,
                  int range = -1);
 
+bool can_rest_here();
+
 bool there_are_monsters_nearby(bool dangerous_only = false,
                                bool require_visible = true,
                                bool consider_user_options = false);

crawl-ref/source/tilereg-cmd.cc
@@ -179,6 +179,7 @@ bool tile_command_not_applicable(const command_type cmd, bool safe)
     switch (cmd)
     {
     case CMD_REST:
+        return !safe || !can_rest_here();
     case CMD_EXPLORE:
     case CMD_INTERLEVEL_TRAVEL:
     case CMD_MEMORISE_SPELL:

commit 9d01ccc61525b2fafc67f39e68efa8a73d04aa1d

Don't spam messages in local tiles
When there are invisible monsters nearby.

Broken by bdabc8c6d3d15eae00cd2e427f43c04c37675bc5 .

crawl-ref/source/main.cc
@@ -1725,7 +1725,7 @@ static void _do_rest()
         return;
     }
 
-    if (i_feel_safe() && can_rest_here())
+    if (i_feel_safe() && can_rest_here(true))
     {
         if (you.is_sufficiently_rested(true) && ancestor_full_hp())
         {
@@ -2123,7 +2123,7 @@ void process_command(command_type cmd, command_type prev_cmd)
     case CMD_ADJUST_INVENTORY: adjust(); break;
 
     case CMD_SAFE_WAIT:
-        if (!i_feel_safe(true) && can_rest_here())
+        if (!i_feel_safe(true) && can_rest_here(true))
             break;
         // else fall-through
     case CMD_WAIT:

crawl-ref/source/nearby-danger.cc
@@ -336,7 +336,7 @@ bool i_feel_safe(bool announce, bool want_move, bool just_monsters,
     return false;
 }
 
-bool can_rest_here()
+bool can_rest_here(bool announce)
 {
     // XXX: consider doing a check for whether your regen is *ever* inhibited
     // before iterating over each monster.
@@ -356,7 +356,8 @@ bool can_rest_here()
     if (announcement.empty())
         return true;
 
-    _announce_monsters(announcement, visible);
+    if (announce)
+        _announce_monsters(announcement, visible);
     return false;
 }
 

crawl-ref/source/nearby-danger.h
@@ -33,7 +33,7 @@ bool i_feel_safe(bool announce = false, bool want_move = false,
                  bool just_monsters = false, bool check_dist = true,
                  int range = -1);
 
-bool can_rest_here();
+bool can_rest_here(bool announce = false);
 
 bool there_are_monsters_nearby(bool dangerous_only = false,
                                bool require_visible = true,