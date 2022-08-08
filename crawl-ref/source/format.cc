crawl/crawl

commit 29c30a7bfa8049fde98852a123a78d82baa4abe0

fix: explicit default foreground for to_colour_string
When the crawl binary renders colour strings, it uses lightgray as the
default foreground color. However, this is not explicitly part of the
string, so the webtiles color parsing code was not inserting a lightgray
span, leading to artifacts. This commit ensures that this default is
present in all autogenerated colour strings; it may or may not have some
side effects I haven't noticed. (N.b. trying to change this on the
webtiles side breaks a *lot* of formatting, unfortunately.)

crawl-ref/source/format.cc
@@ -356,7 +356,9 @@ string formatted_string::tostring(int s, int e) const
 
 string formatted_string::to_colour_string() const
 {
-    string st;
+    // this is necessary because of the default of LIGHTGRAY in last_colour.
+    // (If this were changed to write closing tags, it might not be?)
+    string st = "<lightgray>";
     const int size = ops.size();
     for (int i = 0; i < size; ++i)
     {

commit 0638b6a9f9a82127f072bb086e2417791719be5d

fix: don't generate empty extra religion screens in webtiles
More generally, don't insert a default <lightgray> in colour strings
unless the first op is a text op. This bug is continued fallout from
29c30a7. I think this fix probably subsumes edc2efc2671c but not
f9470816a (since blank colour strings will still get a default tag).

crawl-ref/source/format.cc
@@ -358,8 +358,8 @@ string formatted_string::to_colour_string() const
 {
     // this is necessary because of the default of LIGHTGRAY in last_colour.
     // (If this were changed to write closing tags, it might not be?)
-    string st = "<lightgray>";
     const int size = ops.size();
+    string st = size > 0 && ops[0].type == FSOP_TEXT ? "<lightgray>" : "";
     for (int i = 0; i < size; ++i)
     {
         switch (ops[i].type)