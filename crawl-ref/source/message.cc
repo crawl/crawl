crawl/crawl

commit ae15d2e64f5e317357f4e054f0cfd535b6741deb

Fix coloured channels && auto-capitalisation in mpr().

crawl-ref/source/message.cc
@@ -1090,6 +1090,11 @@ void mpr(std::string text, msg_channel_type channel, int param, bool nojoin, boo
     bool domore = check_more(text, channel);
     bool join = !domore && !nojoin && check_join(text, channel);
 
+    // Must do this before converting to formatted string and back;
+    // that doesn't preserve close tags!
+    std::string col = colour_to_str(colour_msg(colour));
+    text = "<" + col + ">" + text + "</" + col + ">"; // XXX
+
     if (you.duration[DUR_QUAD_DAMAGE])
     {
         // No sound, so we simulate the reverb with all caps.
@@ -1105,8 +1110,6 @@ void mpr(std::string text, msg_channel_type channel, int param, bool nojoin, boo
         text = fs.to_colour_string();
     }
 
-    std::string col = colour_to_str(colour_msg(colour));
-    text = "<" + col + ">" + text + "</" + col + ">"; // XXX
     message_item msg = message_item(text, channel, param, join);
     messages.add(msg);
     _last_msg_turn = msg.turn;

commit 6ae1a4a2c7a7bd7ed943c1e942a9eaecf9ab6f5c

fix: handle color in "Found..." messages correctly
This had two parts. First: the use of a closing tag in message.cc is
simply incorrect, on the current state of things -- colour strings are
valid without closing tags, though closing tags must match if present,
so wrapping an open and a closing tag around an arbitrary colour string
therefore does not guarantee a valid colour string (bug introduced in
ae15d2e64f).  This at long last resolves #1224. Second: using
`formatted_mpr` in the travel.cc for some "Found..." messages is
incompatible with consistent coloring given the current state of things,
because it eliminates any sense of a default color. So even with the
first fix, messages with sub-coloring were not affected correctly by a
`message_colour` rc line (e.g. when an un-id'd scroll was in the same
message as something else).

N.b. if `formatted_string` was refactored to use a more stack-like
representation internally, as in #1249, then the second fix at least
wouldn't be necessary.  However, that PR has been languishing for a long
time, and is a bit overkill for this bug in any case, given that the use
of formatted_mpr is extremely rare (maybe should be eliminated).

crawl-ref/source/message.cc
@@ -1548,7 +1548,10 @@ static void _mpr(string text, msg_channel_type channel, int param, bool nojoin,
     msg_colour_type colour = prepare_message(text, channel, param);
 
     string col = colour_to_str(colour_msg(colour));
-    text = "<" + col + ">" + text + "</" + col + ">"; // XXX
+    // lack of a closing tag is intentional: this is a valid color string and
+    // makes fewer assumptions about `text` this way.
+    // TODO: this doesn't override any opening color in `text`...
+    text = "<" + col + ">" + text; // XXX
 
     msg::_append_to_tees(text + "\n", channel);
 

crawl-ref/source/travel.cc
@@ -4899,13 +4899,13 @@ template <class C> void explore_discoveries::say_any(
         return;
     }
 
-    const auto message = formatted_string::parse_string("Found " +
-                           comma_separated_line(coll.begin(), coll.end()) + ".");
+    const auto message = "Found " +
+                           comma_separated_line(coll.begin(), coll.end()) + ".";
 
-    if (message.width() >= get_number_of_cols())
+    if (formatted_string::parse_string(message).width() >= get_number_of_cols())
         mprf("Found %s %s.", number_in_words(size).c_str(), category);
     else
-        formatted_mpr(message);
+        mpr(message);
 }
 
 vector<string> explore_discoveries::apply_quantities(