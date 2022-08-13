de8b1eb0999c2ca9baf963ebf16fb0398dda0e7e
Nicholas Feinberg <pleasingfung@gmail.com>
2022-08-13 12:37:00 -0700

    Tweak Ozocubu's Refrigeration damage further
    To try to make the positional aspect of the spell's damage more
    noticable, make the adjacency penalty for damage 2/3rds / 1/3rd for
    1 / 2 adjacent creatures (was 3/4ths / 1/2).

--- crawl-ref/source/spl-damage.cc
+++ crawl-ref/source/spl-damage.cc
@@ -668,12 +668,12 @@ static int _ozo_adj_dam(int base_dam, int adj_actors, bool actual)
         return base_dam;
     case 1:
         if (actual)
-            return div_rand_round(3 * base_dam, 4);
-        return 3 * base_dam / 4;
+            return div_rand_round(2 * base_dam, 3);
+        return 2 * base_dam / 3;
     default:
         if (actual)
-            return div_rand_round(base_dam, 2);
-        return base_dam / 2;
+            return div_rand_round(base_dam, 3);
+        return base_dam / 3;
     }
 }
 
