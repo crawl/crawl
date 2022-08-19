crawl/crawl

commit 45d3ea808a7ee420d3fe589bc412818b5bcfa033

feat: various account management enhancements
Nick restriction improvements:
* Allow defining a custom name check function in config. Some things are
  a bit easier to do this way than with a regex.

Two restricted registration modes:
* set `new_accounts_disabled` to disable new account creation
  altogether. This is a bit quick and dirty, and users currently don't
  get an error until they hit the submit button.
* set `new_accounts_hold` to allow new account creation, but mark any
  new accounts with an "account hold" flag. This flag:
    - prevents login via ssh, where relevant. (It just sets the ban
      flag, so messaging to players may be confusing..)
    - prevents the account's games from appearing in the lobby or being
      spectated.
    - prevents the account from using chat.
    - prevents the account from spectating altogether. (Of course they
      can log out and spectate as anonymous.)
    - Players with this flag can still play games.
    - This flag is implemented as a new dgamelaunch db flag. Since dgl
      itself hasn't added any flags in like 15 years, hopefully this is
      safe.

The account hold flags can be set and cleared via the wtutil.py command
line tool. I'd like to add an admin panel interface too, but that isn't
done. Ths flag, if set, has an effect even when the new account hold
mode isn't enabled, so it can also be used as a kind of soft ban if
someone is causing trouble in chat but for whatever reason isn't judged
to merit an outright ban.

crawl-ref/source/webserver/webtiles/userdb.py
@@ -86,18 +86,22 @@ def set_mutelist(username, mutelist):  # type: (str, Optional[str]) -> None
 # from dgamelaunch.h
 DGLACCT_ADMIN = 1
 DGLACCT_LOGIN_LOCK = 2
-DGLACCT_PASSWD_LOCK = 4 # unused by crawl
-DGLACCT_EMAIL_LOCK = 8  # unused by crawl
+DGLACCT_PASSWD_LOCK = 4 # used by webtiles only for account holds
+DGLACCT_EMAIL_LOCK = 8  # used by webtiles only for account holds
 
+# not used by dgamelaunch, used by webtiles
+DGLACCT_ACCOUNT_HOLD = 16
 
 def dgl_is_admin(flags):  # type: (int) -> bool
     return bool(flags & DGLACCT_ADMIN)
 
 
 def dgl_is_banned(flags):  # type: (int) -> bool
-    return bool(flags & DGLACCT_LOGIN_LOCK)
+    return bool(flags & DGLACCT_LOGIN_LOCK) and not dgl_account_hold(flags)
 
-# TODO: something with other lock flags?
+
+def dgl_account_hold(flags):  # type: (int) -> bool
+    return bool(flags & DGLACCT_ACCOUNT_HOLD)
 
 
 def get_user_info(username):  # type: (str) -> Optional[Tuple[int, str, int]]
@@ -199,6 +203,10 @@ def encrypt_pw(passwd):  # type: (str) -> str
 
 def register_user(username, passwd, email):  # type: (str, str, str) -> Optional[str]
     """Returns an error message or None on success."""
+    if config.get('new_accounts_disabled'):
+        # XX show a message before they enter form data...
+        return "New account creation is disabled."
+
     if passwd == "":
         return "The password can't be empty!"
     if email:  # validate the email only if it is provided
@@ -207,7 +215,9 @@ def register_user(username, passwd, email):  # type: (str, str, str) -> Optional
             return result
     username = username.strip()
     if not re.match(config.get('nick_regex'), username):
-        return "Invalid username!"
+        return "Account creation failed."
+    if config.get('nick_check_fun') and not config.get('nick_check_fun')(username):
+        return "Account creation failed."
 
     crypted_pw = encrypt_pw(passwd)
 
@@ -219,14 +229,19 @@ def register_user(username, passwd, email):  # type: (str, str, str) -> Optional
     if result:
         return "User already exists!"
 
+    flags = 0
+    if config.get('new_accounts_hold'):
+        flags = (flags | DGLACCT_LOGIN_LOCK | DGLACCT_EMAIL_LOCK
+                       | DGLACCT_PASSWD_LOCK | DGLACCT_ACCOUNT_HOLD)
+
     with crawl_db(config.get('password_db')) as db:
         query = """
             INSERT INTO dglusers
                 (username, email, password, flags, env)
             VALUES
-                (?, ?, ?, 0, '')
+                (?, ?, ?, ?, '')
         """
-        db.c.execute(query, (username, email, crypted_pw))
+        db.c.execute(query, (username, email, crypted_pw, flags))
         db.conn.commit()
 
     return None
@@ -238,6 +253,19 @@ def change_password(userid, passwd):
     crypted_pw = encrypt_pw(passwd)
 
     with crawl_db(config.get('password_db')) as db:
+        query = """
+            SELECT id, flags
+            FROM dglusers
+            WHERE username=?
+            COLLATE NOCASE
+        """
+        db.c.execute(query, (username,))
+        result = db.c.fetchone()  # type: Optional[Tuple[int, str, int]]
+        if not result:
+            return "Invalid username!"
+        if (result[1] & DGLACCT_PASSWD_LOCK):
+            return "Account has a password lock!"
+
         db.c.execute("update dglusers set password=? where id=?",
                      (crypted_pw, userid))
         # invalidate any recovery tokens that might exist, even for normal
@@ -255,6 +283,18 @@ def change_email(user_id, email):  # type: (str, str) -> Optional[str]
         return result
 
     with crawl_db(config.get('password_db')) as db:
+        query = """
+            SELECT id, flags
+            FROM dglusers
+            WHERE username=?
+            COLLATE NOCASE
+        """
+        db.c.execute(query, (username,))
+        result = db.c.fetchone()  # type: Optional[Tuple[int, str, int]]
+        if not result:
+            return "Invalid username!"
+        if result[1] & DGLACCT_EMAIL_LOCK:
+            return "Account has an email lock!"
         db.c.execute("update dglusers set email=? where id=?", (email, user_id))
         db.conn.commit()
 
@@ -272,6 +312,7 @@ def set_ban(username, banned=True):
         result = db.c.fetchone()  # type: Optional[Tuple[int, str, int]]
         if not result:
             return "Invalid username!"
+
         if banned and dgl_is_banned(result[1]):
             return "User '%s' is already banned." % username
         elif not banned and not dgl_is_banned(result[1]):
@@ -279,10 +320,55 @@ def set_ban(username, banned=True):
         if banned and dgl_is_admin(result[1]):
             return "Remove admin flag before banning."
 
+        # this is more of a sanity check than anything, the cli clears both
+        if not banned and (result[1] & DGLACCT_ACCOUNT_HOLD):
+            return "Clear account hold to clear ban flag"
+
         if banned:
-            new_flags = result[1] | DGLACCT_LOGIN_LOCK
+            # escalate from an account hold to a ban if necessary
+            new_flags = result[1] & ~DGLACCT_ACCOUNT_HOLD
+            new_flags = new_flags | DGLACCT_LOGIN_LOCK
         else:
-            new_flags = result[1] & ~DGLACCT_LOGIN_LOCK
+            # clean up any lingering lock flags
+            new_flags = result[1] & ~(DGLACCT_LOGIN_LOCK
+                                        | DGLACCT_EMAIL_LOCK
+                                        | DGLACCT_PASSWD_LOCK)
+        db.c.execute("UPDATE dglusers SET flags=? WHERE id=?",
+                    (new_flags, result[0]))
+        db.c.execute("DELETE FROM recovery_tokens WHERE user_id=?",
+                     (result[0],))
+        db.conn.commit()
+    return None
+
+
+def set_account_hold(username, hold=True):
+    with crawl_db(config.get('password_db')) as db:
+        query = """
+            SELECT id, flags
+            FROM dglusers
+            WHERE username=?
+            COLLATE NOCASE
+        """
+        db.c.execute(query, (username,))
+        result = db.c.fetchone()  # type: Optional[Tuple[int, str, int]]
+        if not result:
+            return "Invalid username!"
+        # this will override bans
+        if hold and dgl_account_hold(result[1]):
+            return "User '%s' is already on hold." % username
+        elif not hold and not dgl_account_hold(result[1]):
+            return "User '%s' isn't currently on hold." % username
+        if hold and dgl_is_admin(result[1]):
+            return "Remove admin flag before setting an account hold."
+
+        # account holds set the ban flag so that dgamelaunch can't be used to
+        # get around the restrictions. XX dgamelaunch messaging wording?
+        hold_flags = (DGLACCT_LOGIN_LOCK | DGLACCT_EMAIL_LOCK
+                        | DGLACCT_PASSWD_LOCK | DGLACCT_ACCOUNT_HOLD)
+        if hold:
+            new_flags = result[1] | hold_flags
+        else:
+            new_flags = result[1] & ~hold_flags
         db.c.execute("UPDATE dglusers SET flags=? WHERE id=?",
                     (new_flags, result[0]))
         db.c.execute("DELETE FROM recovery_tokens WHERE user_id=?",
@@ -301,7 +387,9 @@ def get_bans():
         """
         db.c.execute(query, (DGLACCT_LOGIN_LOCK,))
         result = db.c.fetchall()
-        return [x[1] for x in result]
+        bans = [x[1] for x in result if dgl_is_banned(x[2])]
+        holds = [x[1] for x in result if dgl_account_hold(x[2])]
+        return (bans, holds)
 
 
 def find_recovery_token(token):

crawl-ref/source/webserver/webtiles/ws_handler.py
@@ -51,11 +51,14 @@ def do_lobby_updates():
         if not game.process:
             # handled immediately in `remove_in_lobbys`, ignore here
             continue
+        admin_only = game.account_restricted()
 
         game_entry = game.lobby_entry()
         # Queue up the collected lobby changes in each socket. This loop is
         # synchronous.
         for socket in lobby_sockets:
+            if admin_only and not socket.is_admin():
+                continue
             socket.queue_message("lobby_entry", **game_entry)
 
     # ...and finally, flush all the updates. This loop may be asynchronous.
@@ -95,7 +98,7 @@ def write_dgl_status_file():
                              str(socket.process.idle_time()),
                              str(socket.process.watcher_count()))
                         for socket in list(sockets)
-                        if socket.username and socket.is_running()]
+                        if socket.username and socket.show_in_lobby()]
     try:
         status_target = config.get('dgl_status_file')
         status_dir = os.path.dirname(status_target)
@@ -341,13 +344,26 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
     def is_in_lobby(self):
         return not self.is_running() and self.watched_game is None
 
-    def send_lobby(self):
+    def send_lobby_data(self):
         self.queue_message("lobby_clear")
         from webtiles.process_handler import processes
         for process in list(processes.values()):
+            if process.account_restricted() and not self.is_admin():
+                continue
             self.queue_message("lobby_entry", **process.lobby_entry())
         self.send_message("lobby_complete")
+
+    def send_lobby(self):
+        self.send_lobby_data()
         self.send_lobby_html()
+
+    def account_restricted(self):
+        return not self.username or userdb.dgl_account_hold(self.user_flags)
+
+    def show_in_lobby(self):
+        return (self.is_running()
+            and self.process.process
+            and not self.account_restricted())
 
     def send_announcement(self, text):
         # TODO: something in lobby?
@@ -654,8 +670,12 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
 
             self.queue_message("login_success", username=username,
                                admin=self.is_admin())
+            if self.account_restricted():
+                self.queue_message("set_account_hold")
             if self.watched_game:
                 self.watched_game.update_watcher_description()
+            elif self.is_admin():
+                self.send_lobby()
             else:
                 self.send_lobby_html()
 
@@ -765,11 +785,20 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
         if self.is_running():
             self.process.stop()
 
+        if self.account_restricted():
+            self.send_message("login_fail", reason="Account restricted; spectating unavailable")
+            self.go_lobby()
+            return
+
         from webtiles.process_handler import processes
         procs = [process for process in list(processes.values())
                  if process.username.lower() == username.lower()]
         if len(procs) >= 1:
             process = procs[0]
+            r = process.get_primary_receiver()
+            if r and r.account_restricted():
+                self.go_lobby()
+                return
             if self.watched_game:
                 if self.watched_game == process:
                     return
@@ -794,9 +823,13 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
             receiver = self.watched_game
 
         if receiver:
+            if self.account_restricted():
+                self.send_message("chat",
+                        content='Account restricted; chat unavailable')
+                return
             if self.username is None:
-                self.send_message("chat", content
-                                  = 'You need to log in to send messages!')
+                self.send_message("chat",
+                        content='You need to log in to send messages!')
                 return
 
             if not receiver.handle_chat_command(self, text):
@@ -818,7 +851,13 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
 
     def change_password(self, cur_password, new_password):
         if self.username is None:
-            self.send_message("change_password_fail", reason = "You need to log in to change your password.")
+            self.send_message("change_password_fail",
+                    reason="You need to log in to change your password.")
+            return
+
+        if self.account_restricted():
+            self.send_message("change_password_fail",
+                    reason="Account restricted; change password unavailable.")
             return
 
         if not userdb.user_passwd_match(self.username, cur_password):
@@ -842,6 +881,10 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
     def change_email(self, email):
         if self.username is None:
             self.send_message("change_email_fail", reason = "You need to log in to change your email")
+            return
+        if self.account_restricted():
+            self.send_message("change_email_fail",
+                    reason="Account restricted; change email unavailable.")
             return
         error = userdb.change_email(self.user_id, email)
         if error is None:

crawl-ref/source/webserver/config.py
@@ -151,8 +151,13 @@ ssl_port = 8081
 
 # kill_timeout = 10 # Seconds until crawl is killed after HUP is sent
 
+# a nick is allowed if it matches this regex
 # nick_regex = r"^[a-zA-Z0-9]{3,20}$"
 # max_passwd_length = 20
+
+# or you can define a function that returns true on an acceptable nick:
+# def nick_check_fun(s):
+#     return s != "plog"
 
 # Set to True to allow users to request a password reset email. Some settings
 # must be properly configured for this to work:
@@ -211,6 +216,13 @@ pidfile = None
 # For example: "http://crawl.akrasiac.org/scoring/players/%s.html"
 # Set to None to disable player page hyperlinks
 player_url = None
+
+# set one of these for various moderation modes. Disabled preempts hold. In
+# account hold mode, new accounts cannot use chat, cannot spectate, and do
+# not appear in the lobby until explicitly approved by an admin. They can still
+# play. (Of course, they can still log out and spectate as anon.)
+# new_accounts_disabled = True
+# new_accounts_hold = True
 
 # Only for development:
 # This is insecure; do not set development_mode = True in production!

crawl-ref/source/webserver/static/scripts/client.js
@@ -1061,6 +1061,16 @@ function (exports, $, key_conversion, chat, comm) {
         $("#admin_panel").show();
     }
 
+    function set_account_hold()
+    {
+        $("#account_restricted").show();
+    }
+
+    function clear_account_hold()
+    {
+        $("#account_restricted").hide();
+    }
+
     function login_required(data)
     {
         cleanup();
@@ -1195,7 +1205,10 @@ function (exports, $, key_conversion, chat, comm) {
 
     function make_watch_link(data)
     {
-        return "<a href='#watch-" + data.username + "'></a>";
+        if (data.username.startsWith("[account hold]"))
+            return "<b></b>"; // don't linkify, watching disabled
+        else
+            return "<a href='#watch-" + data.username + "'></a>";
     }
 
     function format_duration(seconds)
@@ -1503,6 +1516,8 @@ function (exports, $, key_conversion, chat, comm) {
 
         "go_lobby": go_lobby,
         "go_admin": go_admin,
+        "set_account_hold": set_account_hold,
+        "clear_account_hold": clear_account_hold,
         "login_required": login_required,
         "game_started": crawl_started,
         "game_ended": crawl_ended,

crawl-ref/source/webserver/templates/client.html
@@ -162,6 +162,10 @@
 
       <div id="banner">{% include banner.html %}</div>
 
+      <div id="account_restricted" style="display:none;">
+        This account is being held for administrator approval. Until approved,
+        community features are restricted.
+      </div>
       <div>
         <div id="admin_panel_button" style="display:none;">
           <span><a href="javascript:">Toggle admin panel</a></span>

crawl-ref/source/webserver/webtiles/process_handler.py
@@ -549,10 +549,20 @@ class CrawlProcessHandlerBase(object):
         except (OSError, IOError):
             pass
 
+    def account_restricted(self):
+        # convenience function to check the ws_handler data.
+        # this won't work with dgl games, but an account hold sets a dgl ban
+        # flag so they can't log in that way
+        r = self.get_primary_receiver()
+        return r and r.account_restricted()
+
     def lobby_entry(self):
+        u = self.username
+        if self.account_restricted():
+            u = "[account hold] " + u
         entry = {
             "id": self.id,
-            "username": self.username,
+            "username": u,
             "spectator_count": self.watcher_count(),
             "idle_time": (self.idle_time() if self.is_idle() else 0),
             "game_id": self.game_params["id"],

crawl-ref/source/webserver/webtiles/server.py
@@ -368,23 +368,54 @@ def reset_token_commands(args):
 
 def ban_commands(args):
     if args.list:
-        banned = userdb.get_bans()
+        banned, held = userdb.get_bans()
         if len(banned):
             print("Banned users: " + ", ".join(banned))
         else:
             print("No banned users!")
+        if len(held):
+            print("Account holds: " + ", ".join(held))
+        else:
+            print("No account holds!")
+    elif args.clear_holds:
+        banned, held = userdb.get_bans()
+        if len(held):
+            cleared = []
+            for u in held:
+                err = userdb.set_account_hold(u, False)
+                if err:
+                    print(err, file=sys.stderr)
+                else:
+                    cleared.append(u)
+            if len(cleared):
+                print("Holds cleared for: " + ", ".join(cleared))
+            else:
+                print("No holds cleared.")
+        else:
+            print("No holds to clear.")
     elif args.add:
         err = userdb.set_ban(args.add, True)
         if err:
             print(err, file=sys.stderr)
         else:
             print("'%s' is now banned." % args.add)
-    elif args.clear:
-        err = userdb.set_ban(args.clear, False)
+    elif args.hold:
+        err = userdb.set_account_hold(args.hold, True)
         if err:
             print(err, file=sys.stderr)
         else:
-            print("'%s' is no longer banned." % args.clear)
+            print("Account hold set for '%s'." % args.hold)
+    elif args.clear:
+        # XX is there any use for fine-grained clear commands?
+        err1 = userdb.set_account_hold(args.clear, False)
+        if err1:
+            err2 = userdb.set_ban(args.clear, False)
+            if err2:
+                print(err2, file=sys.stderr)
+            else:
+                print("'%s' is no longer banned." % args.clear)
+        else:
+            print("Account hold cleared for '%s'." % args.clear)
 
 
 def parse_args_util():
@@ -403,18 +434,21 @@ def parse_args_util():
     parser_pw.add_argument('--clear-reset', type=str,
         help='Clear any password reset tokens for the specified username.')
     parser_ban = subparsers.add_parser('ban',
-        help="Set and clear bans by username.",
-        description="Set and clear bans by username.")
-    parser_ban.add_argument('--add', type=str, help='Set a ban for a user.')
-    parser_ban.add_argument('--clear', type=str, help='Clear a ban for a user.')
+        help="Set and clear bans and account holds by username.",
+        description="Set and clear bans and account holds by username.")
+    parser_ban.add_argument('--add', type=str, help='Set an account ban for a user. Replaces any account holds.')
+    parser_ban.add_argument('--hold', type=str, help='Set an account hold for a user. Replaces any ban flags.')
+    parser_ban.add_argument('--clear', type=str, help='Clear a ban/hold for a user.')
     parser_ban.add_argument('--list', action='store_true',
-            help='List current banned users.')
+            help='List current banned users and account holds.')
+    parser_ban.add_argument ('--clear-holds', action='store_true',
+            help='Clear all current account holds')
     result = parser.parse_args()
     # XX is there a better way to do this
     if result.mode == "password" and not result.reset and not result.clear_reset:
         parser_pw.print_help()
         sys.exit()
-    elif result.mode == "ban" and not result.add and not result.clear and not result.list:
+    elif result.mode == "ban" and not result.add and not result.clear and not result.list and not result.hold and not result.clear_holds:
         parser_ban.print_help()
         sys.exit()
 

commit c9032a9cacfcd9ca1549a25e5ed0cbf5e945c197

fix: unbreak change email/pw
This has apparently been broken since 45d3ea808a7ee. Possibly most
webtiles servers haven't been restarted since then? Anyways, I only got
a report about this today.

crawl-ref/source/webserver/webtiles/userdb.py
@@ -122,7 +122,8 @@ def get_user_info(username):  # type: (str) -> Optional[Tuple[int, str, int]]
 
 
 def user_passwd_match(username, passwd):  # type: (str, str) -> Optional[Tuple[str, str]]
-    """Returns the correctly cased username and a reason for failure."""
+    """Returns whether the check succeeded, the correctly cased username and a
+    reason for failure."""
     passwd = passwd[0:config.get('max_passwd_length')]
 
     # TODO: this all gets recollected when get_user_info is called shortly
@@ -140,12 +141,11 @@ def user_passwd_match(username, passwd):  # type: (str, str) -> Optional[Tuple[s
 
     # a banned player should never return true for the password check
     if result and dgl_is_banned(result[2]):
-        return result[0], 'Account is disabled.'
+        return False, result[0], 'Account is disabled.'
     elif result and crypt.crypt(passwd, result[1]) == result[1]:
-        return result[0], None
+        return True, result[0], None
     else:
-        return None, None
-
+        return False, None, None
 
 def ensure_user_db_exists():  # type: () -> None
     if os.path.exists(config.get('password_db')):
@@ -246,7 +246,7 @@ def register_user(username, passwd, email):  # type: (str, str, str) -> Optional
 
     return None
 
-def change_password(userid, passwd):
+def change_password(user_id, passwd):
     if passwd == "":
         return "The password can't be empty."
 
@@ -254,24 +254,24 @@ def change_password(userid, passwd):
 
     with crawl_db(config.get('password_db')) as db:
         query = """
-            SELECT id, flags
+            SELECT flags
             FROM dglusers
-            WHERE username=?
+            WHERE id=?
             COLLATE NOCASE
         """
-        db.c.execute(query, (username,))
+        db.c.execute(query, (user_id,))
         result = db.c.fetchone()  # type: Optional[Tuple[int, str, int]]
         if not result:
-            return "Invalid username!"
-        if (result[1] & DGLACCT_PASSWD_LOCK):
+            return "Invalid account!"
+        if (result[0] & DGLACCT_PASSWD_LOCK):
             return "Account has a password lock!"
 
         db.c.execute("update dglusers set password=? where id=?",
-                     (crypted_pw, userid))
+                     (crypted_pw, user_id))
         # invalidate any recovery tokens that might exist, even for normal
         # password changes:
         db.c.execute("delete from recovery_tokens where user_id=?",
-                     (userid,))
+                     (user_id,))
         db.conn.commit()
 
     return None
@@ -284,16 +284,16 @@ def change_email(user_id, email):  # type: (str, str) -> Optional[str]
 
     with crawl_db(config.get('password_db')) as db:
         query = """
-            SELECT id, flags
+            SELECT flags
             FROM dglusers
-            WHERE username=?
+            WHERE id=?
             COLLATE NOCASE
         """
-        db.c.execute(query, (username,))
+        db.c.execute(query, (user_id,))
         result = db.c.fetchone()  # type: Optional[Tuple[int, str, int]]
         if not result:
-            return "Invalid username!"
-        if result[1] & DGLACCT_EMAIL_LOCK:
+            return "Invalid account!"
+        if result[0] & DGLACCT_EMAIL_LOCK:
             return "Account has an email lock!"
         db.c.execute("update dglusers set email=? where id=?", (email, user_id))
         db.conn.commit()

crawl-ref/source/webserver/webtiles/ws_handler.py
@@ -725,8 +725,8 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
         self.init_user(login_callback)
 
     def login(self, username, password):
-        real_username, fail_reason = userdb.user_passwd_match(username, password)
-        if real_username and fail_reason is None:
+        success, real_username, fail_reason = userdb.user_passwd_match(username, password)
+        if success and real_username:
             self.logger.info("User %s logging in from %s.",
                                         real_username, self.request.remote_ip)
             self.do_login(real_username)
@@ -735,7 +735,7 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
                             real_username, self.request.remote_ip, fail_reason)
             self.send_message("login_fail", reason = fail_reason)
         else:
-            self.logger.warning("Failed login for user %s  (IP: %s).", username,
+            self.logger.warning("Failed login for user %s (IP: %s).", username,
                             self.request.remote_ip)
             self.send_message("login_fail")
 
@@ -914,7 +914,7 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
                     reason="Account restricted; change password unavailable.")
             return
 
-        if not userdb.user_passwd_match(self.username, cur_password):
+        if not userdb.user_passwd_match(self.username, cur_password)[0]:
             self.send_message("change_password_fail", reason = "Your password didn't match.")
             self.logger.info("Non-matching current password during password change for %s", self.username)
             return