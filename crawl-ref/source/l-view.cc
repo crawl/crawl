8ab06856ece8d2b64ecfeeb06017598dd831ebec
mgdelmonte <mgdelmonte@gmail.com>
2022-08-19 13:01:06 -0400

    Add lua function view.get_map
    This is significantly more efficient than querying each cell
    from lua. It also provides more and better data, but never more
    than is visible to a normal player. See function comments for
    a complete description.
    
    [Committer's note: did some minor refactoring.]
    
    Closes #2675.

--- crawl-ref/source/l-view.cc
+++ crawl-ref/source/l-view.cc
@@ -21,6 +21,9 @@
 #include "terrain.h"
 #include "travel.h"
 #include "view.h"
+#include "map-knowledge.h"
+#include "coordit.h"
+#include "stash.h"
 
 /*** What is the feature here?
  * @tparam int x
@@ -204,6 +207,109 @@ LUAFN(view_cell_see_cell)
     return 1;
 }
 
+/*** Returns the entire known map as a table of cell objects.
+ * Cells have x, y, feature; optional monster (object);
+ * optional cloud (name); and optional flags [visible, mapped,
+ * traversable, solid, door, undiggable, frontier, excluded, item,
+ * unvisited, unsafe, invisible_monster, detected_item, detected_monster].
+ * The frontier flag indicates that the cell is adjacent to an unknown cell.
+ * The key for each cell is 40000*(100+x) + (100+y), which is
+ * a reversible composition of the x,y coordinates (much more efficient
+ * as a key for lua than other options).
+ * @treturn table[cell]
+ * @function get_map
+ */
+LUAFN(view_get_map)
+{
+    lua_createtable(ls, env.map_knowledge.size(), 0);
+    for (rectangle_iterator ri(BOUNDARY_BORDER - 1); ri; ++ri)
+    {
+        const coord_def p = *ri;
+        map_cell& cell = env.map_knowledge(p);
+        if (!cell.known())
+            continue;
+        auto pc = grid2player(p);
+        lua_createtable(ls, 4, 0);
+        LUA_PUSHINT("x", pc.x);
+        LUA_PUSHINT("y", pc.y)
+        auto feat = cell.feat();
+        LUA_PUSHSTRING("feature", dungeon_feature_name(feat));
+        auto visible = cell.visible();
+        if (visible)
+            LUA_PUSHBOOL("visible", true);
+        if (cell.mapped() && !cell.seen())
+            LUA_PUSHBOOL("mapped", true);
+        if (!feat_is_solid(feat) || feat_is_door(feat))
+        {
+            for (adjacent_iterator ai(p); ai; ++ai)
+            {
+                if (!env.map_knowledge(*ai).known())
+                {
+                    LUA_PUSHBOOL("frontier", true);
+                    break;
+                }
+            }
+        }
+        if (feat_is_traversable_now(feat))
+            LUA_PUSHBOOL("traversable", true);
+        if (feat_is_solid(feat))
+        {
+            LUA_PUSHBOOL("solid", true);
+            if (feat_is_door(feat))
+                LUA_PUSHBOOL("door", true);
+            if (!feat_is_diggable(feat))
+                LUA_PUSHBOOL("undiggable", true);
+            lua_rawseti(ls, -2, 40000*(100+pc.x) + (100+pc.y));
+            continue;
+        }
+
+        if (is_excluded(p))
+            LUA_PUSHBOOL("excluded", true);
+        if (feat == DNGN_ENTER_SHOP)
+        {
+            LevelStashes *lev = StashTrack.find_current_level();
+            if (lev && lev->shop_needs_visit(p))
+                LUA_PUSHBOOL("unvisited", true);
+        }
+        if (cell.item() && cell.item()->defined())
+            LUA_PUSHBOOL("item", true)
+        if (cell.monsterinfo())
+        {
+            lua_push_moninf(ls, cell.monsterinfo());
+            lua_setfield(ls, -2, "monster");
+        }
+        else if (cell.invisible_monster())
+            LUA_PUSHBOOL("invisible_monster", true);
+        bool unsafe = false;
+        if (cell.cloud() != CLOUD_NONE)
+        {
+            LUA_PUSHSTRING("cloud", cloud_type_name(cell.cloud()).c_str());
+            auto killer = cell.cloudinfo()->killer;
+            if (is_damaging_cloud(cell.cloud(), true, YOU_KILL(killer)))
+                unsafe = true;
+        }
+        if (!unsafe && cell.trap() != TRAP_UNASSIGNED)
+        {
+            trap_def trap;
+            trap.type = cell.trap();
+            trap.ammo_qty = 1;
+            if (!trap.is_safe())
+                unsafe = true;
+        }
+        if (unsafe)
+            LUA_PUSHBOOL("unsafe", true);
+        if (!visible)
+        {
+            if (cell.detected_item())
+                LUA_PUSHBOOL("detected_item", true);
+            if (cell.detected_monster())
+                LUA_PUSHBOOL("detected_monster", true);
+        }
+        lua_rawseti(ls, -2, 40000*(100+pc.x) + (100+pc.y));
+    }
+    return 1;
+}
+
 /**
  * @brief Are the given coordinates in the minimal bounding box of the known
  * map?
@@ -237,6 +343,7 @@ static const struct luaL_reg view_lib[] =
     { "invisible_monster", view_invisible_monster },
     { "cell_see_cell", view_cell_see_cell },
     { "in_known_map_bounds", view_in_known_map_bounds },
+    { "get_map", view_get_map },
 
     { "update_monsters", view_update_monsters },
 
