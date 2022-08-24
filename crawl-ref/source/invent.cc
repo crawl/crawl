3c20cbc1e557f4cb5a3822bc505f72360354a14f
advil <rawlins@gmail.com>
2022-08-24 13:03:39 -0400

    fix: don't crash on ' in pickup
    sigh
    
    (cherry picked from commit acc37e2db082a082f87124e5131f9d8dbc731ba6)

--- crawl-ref/source/invent.cc
+++ crawl-ref/source/invent.cc
@@ -416,10 +416,20 @@ bool InvMenu::examine_index(int i)
 {
     if (on_examine)
         return Menu::examine_index(i);
-    // default behavior: examine inv item. You must override or use on_examine
-    // if your items come from somewhere else, or this will cause crashes!
-    if (i >= 0 && i < static_cast<int>(items.size()) && items[i]->hotkeys.size())
+    else if (type == menu_type::pickup)
     {
+        auto ie = dynamic_cast<InvEntry *>(items[i]);
+        if (ie)
+        {
+            auto desc_tgt = const_cast<item_def*>(ie->item);
+            ASSERT(desc_tgt);
+            return describe_item(*desc_tgt);
+        }
+    }
+    else if (i >= 0 && i < static_cast<int>(items.size()) && items[i]->hotkeys.size())
+    {
+        // default behavior: examine inv item. You must override or use on_examine
+        // if your items come from somewhere else, or this will cause crashes!
         unsigned char select = items[i]->hotkeys[0];
         const int invidx = letter_to_index(select);
         ASSERT(you.inv[invidx].defined());
