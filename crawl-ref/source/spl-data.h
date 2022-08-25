557434ed058bb63115eeaec7522e06fc49d85951
Nicholas Feinberg <pleasingfung@gmail.com>
2022-08-24 22:23:32 -0700

    Cap Static Discharge power at 50 (hellmonk)
    100 power is a lot for a level 2 spell.

--- crawl-ref/source/spl-data.h
+++ crawl-ref/source/spl-data.h
@@ -1146,7 +1146,7 @@ static const struct spell_desc spelldata[] =
     spschool::conjuration | spschool::air,
     spflag::area,
     2,
-    100,
+    50,
     1, 1,
     3, 0,
     TILEG_STATIC_DISCHARGE,
