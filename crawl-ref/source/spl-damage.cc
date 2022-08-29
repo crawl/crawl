crawl/crawl

commit aed3a0c8d8a132d27718dec265e5da983d5dda97

Experimental: redesign Ozocubu's Refrigeration
In DCSS, spells are most exciting when they're context sensitive.
You cast Fireball or Freezing Cloud when the terrain and enemy
positions suit it. It's fine to have strong spells, but it's not
very fun to have one spell which is so universally good that you can
repeatedly cast it to clear the great majority of fights. Ozo's Fridge
fell in this category.

Let's experiment with making it a bit more situation-dependent. This
commit increases its damage by ~10% and removes the -Potion gimmick, but
reduces damage against creatures that are adjacent to others (including
the player). The current formula reduces damage by 25% if there's one
creature adjacent and by 50% if there are two or more, but this should
be considered very much a first draft.

Concerns I've seen about this design:
1. It'll cause players to lure enemies so you can fight one-on-one. This
   is generally wise, but I don't think a screen-wide AOE particularly
   pushes players to do this, even if it's worse against clumps of enemies.
2. It'll be hard for players to tell what's happening. I've done some
   work on the targeter to try to show how much enemies are affected, and
   probably some work could be done on printed messages if necessary.

crawl-ref/source/spl-damage.cc
@@ -648,6 +648,30 @@ static int _los_spell_damage_actor(const actor* agent, actor &target,
     return hurted;
 }
 
+static int _count_adj_actors(coord_def pos)
+{
+    int adj_count = 0;
+    for (adjacent_iterator ai(pos); ai; ++ai)
+        if (actor_at(*ai))
+            ++adj_count;
+    return adj_count;
+}
+
+static int _ozo_adj_dam(int base_dam, int adj_actors, bool actual)
+{
+    switch (adj_actors) {
+    case 0:
+        return base_dam;
+    case 1:
+        if (actual)
+            return div_rand_round(3 * base_dam, 4);
+        return 3 * base_dam / 4;
+    default:
+        if (actual)
+            return div_rand_round(base_dam, 2);
+        return base_dam / 2;
+    }
+}
 
 static spret _cast_los_attack_spell(spell_type spell, int pow,
                                          const actor* agent, bool actual,
@@ -681,8 +705,6 @@ static spret _cast_los_attack_spell(spell_type spell, int pow,
             prompt_verb = "refrigerate";
             vulnerable = [](const actor *caster, const actor *act) {
                 return act != caster
-                       // Players don't get immunity with rC+++.
-                       && (act->is_player() || act->res_cold() < 3)
                        && !god_protects(caster, act->as_monster());
             };
             break;
@@ -734,12 +756,6 @@ static spret _cast_los_attack_spell(spell_type spell, int pow,
 
             mpr(player_msg);
             flash_view_delay(UA_PLAYER, beam.colour, 300, &hitfunc);
-
-            if (spell == SPELL_OZOCUBUS_REFRIGERATION)
-            {
-                mpr("You feel very cold.");
-                you.increase_duration(DUR_NO_POTIONS, 7 + random2(9), 15);
-            }
         }
         else
         {
@@ -756,19 +772,27 @@ static spret _cast_los_attack_spell(spell_type spell, int pow,
     }
 
     vector<actor *> affected_actors;
+    map<actor*, int> ozo_adj_count;
 
     for (actor_near_iterator ai((agent ? agent : &you)->pos(), LOS_NO_TRANS);
          ai; ++ai)
     {
         if (!actual && !agent->can_see(**ai))
             continue;
+        if (!(*vulnerable)(agent, *ai))
+            continue;
 
         if ((*vulnerable)(agent, *ai))
             affected_actors.push_back(*ai);
+
+        // For perf, don't count when running tracers.
+        if (spell == SPELL_OZOCUBUS_REFRIGERATION)
+            ozo_adj_count[*ai] = actual ? _count_adj_actors(ai->pos()) : 0;
     }
 
     const int avg_damage = (1 + beam.damage.num * beam.damage.size) / 2;
     int total_damage = 0;
+    const int base_dam_size = beam.damage.size;
 
     for (auto a : affected_actors)
     {
@@ -776,6 +800,9 @@ static spret _cast_los_attack_spell(spell_type spell, int pow,
         // a bunch of ballistomycete spores that blow each other up.
         if (!a->alive())
             continue;
+
+        if (spell == SPELL_OZOCUBUS_REFRIGERATION)
+            beam.damage.size = _ozo_adj_dam(base_dam_size, ozo_adj_count[a], actual);
 
         int this_damage = _los_spell_damage_actor(agent, *a, beam, actual);
         total_damage += this_damage;

crawl-ref/source/spl-cast.cc
@@ -1315,6 +1315,7 @@ unique_ptr<targeter> find_spell_targeter(spell_type spell, int pow, int range)
 
     // LOS radius:
     case SPELL_OZOCUBUS_REFRIGERATION:
+        return make_unique<targeter_refrig>(&you);
     case SPELL_OLGREBS_TOXIC_RADIANCE:
         return make_unique<targeter_maybe_radius>(&you, LOS_NO_TRANS,
                                                   LOS_RADIUS, 0, 1);

crawl-ref/source/target.cc
@@ -1577,6 +1577,29 @@ bool targeter_shadow_step::has_additional_sites(coord_def a)
     return temp_sites.size();
 }
 
+aff_type targeter_refrig::is_affected(coord_def loc)
+{
+    if (!targeter_radius::is_affected(loc))
+        return AFF_NO;
+    const actor* act = actor_at(loc);
+    if (!act || act->is_player())
+        return AFF_NO;
+
+    int adj = 0;
+    for (adjacent_iterator ai(loc); ai; ++ai)
+        if (actor_at(*ai))
+            ++adj;
+    switch (adj)
+    {
+    case 0:
+        return AFF_MULTIPLE;
+    case 1:
+        return AFF_YES;
+    default:
+        return AFF_MAYBE;
+    }
+}
+
 targeter_cone::targeter_cone(const actor *act, int r)
 {
     ASSERT(act);

crawl-ref/source/target.h
@@ -237,6 +237,16 @@ public:
     }
 };
 
+class targeter_refrig : public targeter_radius
+{
+public:
+    targeter_refrig(actor *agent)
+        : targeter_radius(agent, LOS_NO_TRANS, LOS_RADIUS, 0, 1)
+    { }
+
+    aff_type is_affected(coord_def loc) override;
+};
+
 class targeter_flame_wave : public targeter_radius
 {
 public:

crawl-ref/source/zap-data.h
@@ -1650,9 +1650,9 @@ _mon_hex_zap(ZAP_DRAIN_MAGIC, BEAM_DRAIN_MAGIC),
     ZAP_REFRIGERATE,
     "refrigeration",
     200,
-    new calcdice_calculator<4, 30, 4, 10>,
+    new calcdice_calculator<4, 30, 4, 9>,
     new tohit_calculator<AUTOMATIC_HIT>,
-    new calcdice_calculator<4, 30, 4, 10>,
+    new calcdice_calculator<4, 30, 4, 9>,
     new tohit_calculator<AUTOMATIC_HIT>,
     LIGHTCYAN,
     false,

commit b3c18b1f4e05f9bf615ef91ba830286c68feaab5

Fix: make rC+++ monsters immune to Refrig again
Broken in aed3a0c8d8a132d27718dec265e5da983d5dda97 . I forgot how
monster resists worked.

crawl-ref/source/spl-damage.cc
@@ -713,6 +713,8 @@ static spret _cast_los_attack_spell(spell_type spell, int pow,
             prompt_verb = "refrigerate";
             vulnerable = [](const actor *caster, const actor *act) {
                 return act != caster
+                       // Players don't get immunity with rC+++.
+                       && (act->is_player() || act->res_cold() < 3)
                        && !god_protects(caster, act->as_monster());
             };
             break;