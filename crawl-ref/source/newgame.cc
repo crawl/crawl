0feb890e1c940332584fedee679650cf1fb54404
advil <rawlins@gmail.com>
2022-09-11 20:49:01 -0400

    fix: fully remove AK from job selection
    It was still possible to choose AK by doing jobs first, but then you
    could only pick species randomly and got converted to a monk on game
    start. This commit will also fix a tag_upgrade build failure.

--- crawl-ref/source/newgame.cc
+++ crawl-ref/source/newgame.cc
@@ -1098,7 +1098,7 @@ static job_group jobs_order[] =
     {
         "Zealot",
         coord_def(1, 0), 25,
-        { JOB_BERSERKER, JOB_ABYSSAL_KNIGHT, JOB_CINDER_ACOLYTE,
+        { JOB_BERSERKER, JOB_CINDER_ACOLYTE,
           JOB_CHAOS_KNIGHT }
     },
     {
