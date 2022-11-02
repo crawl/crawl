372ae3aaf5a56f29bdfc41e311dbda9d42d81675
scarf <greenscarf005@gmail.com>
2022-11-03 00:31:13 +0900

    feat: put cloud of electricity for ball lightning

--- crawl-ref/source/mon-act.cc
+++ crawl-ref/source/mon-act.cc
@@ -3570,6 +3570,9 @@ static bool _monster_move(monster* mons)
         if (mons->has_ench(ENCH_ROLLING))
             place_cloud(CLOUD_DUST, mons->pos(), 2, mons);
 
+        if (mons->type == MONS_BALL_LIGHTNING)
+            place_cloud(CLOUD_ELECTRICITY, mons->pos(), 2 + random2(2), mons);
+
         if (mons->type == MONS_FOXFIRE)
             check_place_cloud(CLOUD_FLAME, mons->pos(), 2, mons);
 
