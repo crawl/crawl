c6323d94246fee88e99261ef54716e66fcf0cc9b
Nikolai Lavsky <3328424+nlavsky@users.noreply.github.com>
2022-12-06 22:08:31 +0300

    fix: add more uselessness checks for consumables
    There are several situations when the game doesn't let you use an item,
    but the item is not marked as useless. For example, scrolls are not
    considered useless in Gehenna or when you are confused or berserk.
    Scrolls of teleportation are greyed out in the Gauntlet, but not when
    you are under Stuck or -Tele status. Scrolls of enchant weapon/armour
    look exactly the same in your inventory when you have something to
    enchant and when there are no such items.
    
    This commit adds several uselessness checks for consumables. This
    affects not only the action panel, but also the `i`, `r`, `q`, and `v`
    menus and the quiver.

--- crawl-ref/source/item-name.cc
+++ crawl-ref/source/item-name.cc
@@ -2825,6 +2825,9 @@ bool is_useless_item(const item_def &item, bool temp, bool ident)
         // all cases.
     }
 
+    if (temp && you.cannot_act())
+        return true;
+
     switch (item.base_type)
     {
     case OBJ_WEAPONS:
@@ -2901,8 +2904,8 @@ bool is_useless_item(const item_def &item, bool temp, bool ident)
         return false;
 
     case OBJ_SCROLLS:
-        if (temp && silenced(you.pos()))
-            return true; // can't use scrolls while silenced
+        if (temp && cannot_read_item_reason(&item).size())
+            return true;
 
         if (!ident && !item_type_known(item))
             return false;
@@ -2913,12 +2916,7 @@ bool is_useless_item(const item_def &item, bool temp, bool ident)
 
         switch (item.sub_type)
         {
-        case SCR_TELEPORTATION:
-            return you.stasis()
-                   || crawl_state.game_is_sprint()
-                   || temp && player_in_branch(BRANCH_GAUNTLET);
-        case SCR_BLINKING:
-            return you.stasis();
+        // Checks for other subtypes are in cannot_read_item_reason().
         case SCR_AMNESIA:
             return you_worship(GOD_TROG) || you.has_mutation(MUT_INNATE_CASTER);
 #if TAG_MAJOR_VERSION == 34
@@ -2929,9 +2927,6 @@ bool is_useless_item(const item_def &item, bool temp, bool ident)
         case SCR_ENCHANT_ARMOUR:
         case SCR_BRAND_WEAPON:
             return you.has_mutation(MUT_NO_GRASPING);
-        case SCR_SUMMONING:
-        case SCR_BUTTERFLIES:
-            return you.allies_forbidden();
         case SCR_FOG:
         case SCR_POISON:
             return temp && (env.level_state & LSTATE_STILL_WINDS);
@@ -2944,6 +2939,9 @@ bool is_useless_item(const item_def &item, bool temp, bool ident)
 
     case OBJ_WANDS:
         if (you.get_mutation_level(MUT_NO_ARTIFICE))
+            return true;
+
+        if (temp && (you.confused() || you.berserk()))
             return true;
 
 #if TAG_MAJOR_VERSION == 34
@@ -2963,6 +2961,12 @@ bool is_useless_item(const item_def &item, bool temp, bool ident)
         // Mummies and liches can't use potions.
         if (!you.can_drink(temp))
             return true;
+
+        if (temp && (player_in_branch(BRANCH_COCYTUS)
+                     || you.berserk()))
+        {
+            return true;
+        }
 
         if (!ident && !item_type_known(item))
             return false;
@@ -3079,19 +3083,25 @@ bool is_useless_item(const item_def &item, bool temp, bool ident)
         return true;
 
     case OBJ_MISCELLANY:
+        // The figurine can always be used.
+        if (item.sub_type == MISC_ZIGGURAT)
+            return false;
+
+        if (temp && (you.confused() || you.berserk()))
+            return true;
+
+        if (temp && is_xp_evoker(item) && !evoker_charges(item.sub_type))
+            return true;
+
         switch (item.sub_type)
         {
 #if TAG_MAJOR_VERSION == 34
         case MISC_BUGGY_EBONY_CASKET:
             return item_type_known(item);
-#endif
-        // These can always be used.
-#if TAG_MAJOR_VERSION == 34
+        // It can always be used.
         case MISC_BUGGY_LANTERN_OF_SHADOWS:
-#endif
-        case MISC_ZIGGURAT:
             return false;
-
+#endif
         // Purely summoning misc items don't work w/ sac love
         case MISC_BOX_OF_BEASTS:
         case MISC_HORN_OF_GERYON:
