crawl/crawl

commit 9e567866badc7cfc8ca387d2bf0da53c9c40d3d4

fix: nerf throwing
This removes a pretty good tech reported in #2874 by acrobat. Via
quiver, and also throw with the "toss anything" submenu, all species
could do damage throws with any throwing ammo. I think this even
probably prevented mulching. The bug originates in a8e9c987b86ad, which
replaced a specific fumble check in ranged-attack.cc with an item type
check.

Resolves #2874

crawl-ref/source/ranged-attack.cc
@@ -338,6 +338,9 @@ bool ranged_attack::using_weapon() const
 
 bool ranged_attack::clumsy_throwing() const
 {
+    if (attacker->is_player())
+        return !is_throwable(&you, *projectile);
+    // probably unreachable?
     return projectile->base_type != OBJ_MISSILES;
 }
 

commit 03f49bf3ed92263efdb7ec09be30a886eda065cb

fix: unnerf launching
Bug introduced in 9e567866badc7; silly me for thinking that
`clumsy_throwing` would only be checked for throwing.

Resolves https://crawl.develz.org/mantis/view.php?id=12714

crawl-ref/source/ranged-attack.cc
@@ -338,6 +338,8 @@ bool ranged_attack::using_weapon() const
 
 bool ranged_attack::clumsy_throwing() const
 {
+    if (!throwing())
+        return false;
     if (attacker->is_player())
         return !is_throwable(&you, *projectile);
     // probably unreachable?