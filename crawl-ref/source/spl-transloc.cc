ae655ce16752476e11221319961dedeea0a8d993
Nicholas Feinberg <pleasingfung@gmail.com>
2023-01-06 19:31:16 -0800

    Fix an abyssal Vhi's crash (erf)
    Very weird.

--- crawl-ref/source/spl-transloc.cc
+++ crawl-ref/source/spl-transloc.cc
@@ -586,7 +586,7 @@ bool find_charge_target(vector<coord_def> &target_path, int max_range,
 
 static void _charge_cloud_trail(const coord_def pos)
 {
-    if (!apply_cloud_trail(pos))
+    if (!cell_is_solid(pos) && !apply_cloud_trail(pos))
         place_cloud(CLOUD_DUST, pos, 2 + random2(3), &you);
 }
 
