crawl/crawl

commit 86a8295270962016579da244f3382e678c83a075

Maybe fix the famous serpent's lash mimic bug
Check for mimics on entering a space, not at end of turn. Is this right?
Who knows.

crawl-ref/source/player-reacts.cc
@@ -1029,10 +1029,6 @@ void player_reacts()
     if (you.duration[DUR_POISONING])
         handle_player_poison(you.time_taken);
 
-    // Reveal adjacent mimics.
-    for (adjacent_iterator ai(you.pos(), false); ai; ++ai)
-        discover_mimic(*ai);
-
     // Player stealth check.
     seen_monsters_react(stealth);
 

crawl-ref/source/player.cc
@@ -536,6 +536,10 @@ void moveto_location_effects(dungeon_feature_type old_feat,
     if (stepped)
         _moveto_maybe_repel_stairs();
 
+    // Reveal adjacent mimics.
+    for (adjacent_iterator ai(you.pos(), false); ai; ++ai)
+        discover_mimic(*ai);
+
     bool was_running = you.running;
 
     update_monsters_in_view();

commit f493690b3a5b08d42310919754122fd3b084d885

Fix apportation + mimics (dilly, acrobat)
Broken in 86a8295270962016579da244f3382e678c83a075.

crawl-ref/source/player-reacts.cc
@@ -1027,6 +1027,10 @@ void player_reacts()
     if (you.duration[DUR_POISONING])
         handle_player_poison(you.time_taken);
 
+    // safety first: make absolutely sure that there's no mimic underfoot.
+    // (this can happen with e.g. apport.)
+    discover_mimic(you.pos());
+
     // Player stealth check.
     seen_monsters_react(stealth);
 