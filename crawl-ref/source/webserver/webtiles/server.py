crawl/crawl

commit dc488dc59d06da635a692737f074e4db40eb212b

refactor: rewrite webtiles db code
Since the very beginning (82b49a24b7ed1), webtiles has accessed its user
databases by opening and then closing a connection on each access. I
haven't been able to figure out any reason why it should do things this
way (sqlite3 can certainly handle concurrency), and there are a bunch of
reasons why this is bad: (a) it increases overhead on each call (an
fopen is necessary each time, probably among other things), and (b) it
leads to a bunch of bad code to minimize the number of db opens per each
function.

This commit rewrites the db access code to have a persistent connection
to both of the sqlite3 databases. Because of this change, I was also
able to majorly refactor most of the db calls so that there is much less
duplicate code. This commit also changes (arguably) a bunch of stylistic
stuff in this code, and improves aspects of the API. Along the way, I
wrote unit tests for the major functions of this module.

I'm not sure how significant the speedup is, but it isn't nothing; on
a SSD in local testing this about halved the time of db calls (though
from ~20ms to ~10ms, so may or may not be impactful). But I suspect on
a server with a spinning disk (aka CAO), this will be quite noticeable.

crawl-ref/source/webserver/webtiles/server.py
@@ -351,9 +351,10 @@ def reset_token_commands(args):
         username = args.reset
 
     user_info = userdb.get_user_info(username)
-
     if not user_info:
         err_exit("Reset/clear password failed; invalid user: %s" % username)
+
+    username = user_info.username # canonicalize
 
     # don't crash on the default config
     if config.get('lobby_url') is None:
@@ -371,10 +372,10 @@ def reset_token_commands(args):
         if not ok:
             err_exit("Error generating password reset token for %s: %s" % (username, msg))
         else:
-            if not user_info[1]:
+            if not user_info.email:
                 logging.warning("No email set for account '%s', use caution!" % username)
             print("Setting a password reset token on account '%s'." % username)
-            print("Email: %s\nMessage body to send to user:\n%s\n" % (user_info[1], msg))
+            print("Email: %s\nMessage body to send to user:\n%s\n" % (username, msg))
             return True
     return False
 
@@ -402,10 +403,10 @@ def show_flags(username):
     if not r:
         err_exit("Unknown user '%s'!" % username)
     # XX would be nice to normalize username
-    if not r[2]:
-        print("User '%s' (id %d) has no flags set." % (username, r[0]))
+    if not r.flags:
+        print("User '%s' (id %d) has no flags set." % (r.username, r.id))
     else:
-        print("Flags for '%s' (id %d): %s" % (username, r[0], userdb.flag_description(r[2])))
+        print("Flags for '%s' (id %d): %s" % (r.username, r.id, userdb.flag_description(r.flags)))
     return True
 
 
@@ -428,7 +429,7 @@ def flag_commands(args):
             print("No matching users.")
         else:
             print("Users with flag '%s': %s" %
-                (userdb.flag_description(flag), ", ".join([u[1] for u in l])))
+                (userdb.flag_description(flag), ", ".join([u.username for u in l])))
         return True
     elif args.set:
         r = userdb.set_flags(args.set, flag, mask=flag)
@@ -453,7 +454,7 @@ def ban_commands(args):
     if args.check_config_bans or args.run_config_bans:
         # potentially very heavy commands on old servers...
         all_users = userdb.get_all_users()
-        affected = [n[1] for n in all_users if not config.check_name(n[1])]
+        affected = [n.username for n in all_users if not config.check_name(n.username)]
         if not affected:
             print("No affected users.")
             return True
@@ -605,10 +606,7 @@ def run_util():
 
     init_logging(config.get('logging_config'))
 
-    if config.get('dgl_mode'):
-        userdb.ensure_user_db_exists()
-        userdb.upgrade_user_db()
-    userdb.ensure_settings_db_exists()
+    userdb.init_db_connections()
 
     mode_fun = None
 
@@ -694,10 +692,7 @@ def run():
         # is this ever set to False by anyone in practice?
         dgl_mode = config.get('dgl_mode')
 
-        if dgl_mode:
-            userdb.ensure_user_db_exists()
-            userdb.upgrade_user_db()
-        userdb.ensure_settings_db_exists()
+        userdb.init_db_connections()
 
         signal.signal(signal.SIGUSR1, usr1_handler)
 

.github/workflows/ci.yml
@@ -464,6 +464,9 @@ jobs:
     - name: Unit Tests (pytest)
       working-directory: crawl-ref/source/webserver
       run: pytest
+    - name: Unit Tests (unittest)
+      working-directory: crawl-ref/source/webserver
+      run: python -m unittest webtiles.userdb
 
   build_headers:
     name: Build headers ${{ matrix.compiler }} ${{ matrix.build_opts }} ${{ matrix.debug }}

crawl-ref/source/webserver/webtiles/ws_handler.py
@@ -349,12 +349,14 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
         if not user_info:
             self.send_message("admin_pw_reset_done", error="Invalid user")
             return
-        ok, msg = userdb.generate_forgot_password(username)
+        ok, msg = userdb.generate_forgot_password(user_info.username)
         if not ok:
             self.send_message("admin_pw_reset_done", error=msg)
         else:
-            self.logger.info("Admin user '%s' set a password token on account '%s'", self.username, username)
-            self.send_message("admin_pw_reset_done", email_body=msg, username=username, email=user_info[1])
+            self.logger.info("Admin user '%s' set a password token on account '%s'",
+                self.username, user_info.username)
+            self.send_message("admin_pw_reset_done", email_body=msg,
+                username=user_info.username, email=user_info.email)
 
     @admin_required
     def admin_pw_reset_clear(self, username):
@@ -559,7 +561,7 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
                     # Binary doesn't support save info, print a warning so that
                     # the admin can see there is a misconfiguration
                     # XX it should be possible to check for this case on startup
-                    logging.warn("Save info check for '%s' failed" % b)
+                    logging.warning("Save info check for '%s' failed" % b)
                     data = None # force error case in update_save_info call
 
                 self.update_save_info(b, data)
@@ -648,11 +650,17 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
             self.reset_timeout()
 
     def update_db_info(self):
+        """update self's user info from the user database, including updating
+        any flag changes that may have happened in the db."""
         if not self.username:
             return True # caller needs to check for anon if necessary
         # won't detect a change in hold state on first login...
         old_restriction = self.user_flags is not None and self.account_restricted()
-        self.user_id, self.user_email, self.user_flags = userdb.get_user_info(self.username)
+        u = userdb.get_user_info(self.username)
+        self.username = u.username # canonicalize
+        self.user_id = u.id
+        self.email = u.email
+        self.user_flags = u.flags
         self.logger.extra["username"] = self.username
         if userdb.dgl_is_banned(self.user_flags):
             return False

commit 1ff12e1e11577aac4164773593ebd262f92999fa

fix: show email in password reset CLI (gammafunk)
Broken in dc488dc59d06da635

crawl-ref/source/webserver/webtiles/server.py
@@ -421,7 +421,7 @@ def reset_token_commands(args):
             if not user_info.email:
                 logging.warning("No email set for account '%s', use caution!" % username)
             print("Setting a password reset token on account '%s'." % username)
-            print("Email: %s\nMessage body to send to user:\n%s\n" % (username, msg))
+            print("Email: %s\nMessage body to send to user:\n%s\n" % (user_info.email, msg))
             return True
     return False
 