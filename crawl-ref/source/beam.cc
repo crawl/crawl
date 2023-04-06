crawl/crawl

commit abb061897f7b570d6fe8f0abf22fc0f8a34d7bbc

Extract beam::good_to_fire()
It's a surprise tool that will help us later.

crawl-ref/source/beam.cc
@@ -6760,6 +6760,27 @@ bool bolt::can_pull(const actor &act, int dam) const
     return origin_spell == SPELL_HARPOON_SHOT && dam;
 }
 
+ai_action::goodness bolt::good_to_fire() const
+{
+    // Use of foeRatio:
+    // The higher this number, the more monsters will _avoid_ collateral
+    // damage to their friends.
+    // Setting this to zero will in fact have all monsters ignore their
+    // friends when considering collateral damage.
+
+    // Quick check - did we in fact hit anything?
+    if (foe_info.count == 0)
+        return ai_action::neutral();
+
+    // Only fire if they do acceptably low collateral damage.
+    if (!friend_info.power
+        || foe_info.power >= div_round_up(foe_ratio * friend_info.power, 100))
+    {
+        return ai_action::good();
+    }
+    return ai_action::bad();
+}
+
 void clear_zap_info_on_exit()
 {
     for (const zap_info &zap : zap_data)

crawl-ref/source/beam.h
@@ -12,6 +12,7 @@
 #include "enchant-type.h"
 #include "externs.h"
 #include "killer-type.h"
+#include "mon-ai-action.h"
 #include "mon-attitude-type.h"
 #include "random.h"
 #include "ray.h"
@@ -317,6 +318,7 @@ private:
     bool fuzz_invis_tracer();
 public:
     void choose_ray();
+    ai_action::goodness good_to_fire() const;
 };
 
 int mons_adjust_flavoured(monster* mons, bolt &pbolt, int hurted,

crawl-ref/source/mon-util.cc
@@ -3615,25 +3615,7 @@ bool mons_should_fire(bolt &beam, bool ignore_good_idea)
             return false;
     }
 
-    // Use of foeRatio:
-    // The higher this number, the more monsters will _avoid_ collateral
-    // damage to their friends.
-    // Setting this to zero will in fact have all monsters ignore their
-    // friends when considering collateral damage.
-
-    // Quick check - did we in fact get any foes?
-    if (beam.foe_info.count == 0)
-        return false;
-
-    // If we hit no friends, fire away.
-    if (beam.friend_info.count == 0)
-        return true;
-
-    // Only fire if they do acceptably low collateral damage.
-    return beam.foe_info.power >=
-           div_round_up(beam.foe_ratio *
-                        (beam.foe_info.power + beam.friend_info.power),
-                        100);
+    return beam.good_to_fire() >= ai_action::good();
 }
 
 /**

commit fb0d676c022131b80f9d2d95bfe82bb79937114f

Fix friendly fire
Broken in abb061897f7b570d6fe8f0abf22fc0f8a34d7bbc.

crawl-ref/source/beam.cc
@@ -6772,9 +6772,10 @@ ai_action::goodness bolt::good_to_fire() const
     if (foe_info.count == 0)
         return ai_action::neutral();
 
+    const int total_pow = foe_info.power + friend_info.power;
     // Only fire if they do acceptably low collateral damage.
     if (!friend_info.power
-        || foe_info.power >= div_round_up(foe_ratio * friend_info.power, 100))
+        || foe_info.power >= div_round_up(foe_ratio * total_pow, 100))
     {
         return ai_action::good();
     }