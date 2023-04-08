df59a09ada343e061e0471525cb1c012432030f7
advil <rawlins@gmail.com>
2023-04-08 10:08:59 -0400

    fix: throttle some webtiles error spam
    When the server gets into a bad state and the socket buffers start to
    get filled up, on recovery it often emits a very large number of logged
    warnings in `on_message` (usually just many "Socket buffer full"
    messages). This itself can be pretty IO intensive.  This commit
    suppresses all but the first one in a sequence of errors.
    
    Also, hide a common harmless warning that can happen on a disconnect.

--- crawl-ref/source/webserver/webtiles/ws_handler.py
+++ crawl-ref/source/webserver/webtiles/ws_handler.py
@@ -308,7 +308,8 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
         self.compressed_bytes_sent = 0
         self.uncompressed_bytes_sent = 0
         self.message_queue = []  # type: List[str]
-        self.failed_messages = 0
+        self.failed_messages = 0 # messages to webtiles
+        self.failed_on_messages = 0 # messages from webtiles
 
         self.subprotocol = None
 
@@ -1214,7 +1215,9 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
                 handler(**obj)
             elif self.process:
                 self.process.handle_input(message)
-            elif not self.watched_game and obj["msg"] != 'ui_state_sync':
+            elif (not self.watched_game
+                    and obj["msg"] != 'ui_state_sync'
+                    and obj["msg"] != 'key'):
                 # ui_state_sync can get queued by the js client just before
                 # shutdown, and have its sending delayed by enough that the
                 # process has stopped. I do not currently think there's a
@@ -1225,28 +1228,35 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
                 self.logger.warning("Didn't know how to handle msg (user %s): %s",
                                     self.username and self.username or "[Anon]",
                                     obj["msg"])
+            if self.failed_on_messages > 1:
+                self.logger.warning("%d more on_message errors...", self.failed_on_messages)
+            self.failed_on_messages = 0
         except OSError as e:
             # maybe should throw a custom exception from the socket call rather
             # than rely on these cases?
-            excerpt = message[:50] if len(message) > 50 else message
-            trunc = "..." if len(message) > 50 else ""
-            if e.errno == errno.EAGAIN or e.errno == errno.ENOBUFS:
-                # errno is different on mac vs linux, maybe also depending on
-                # python version
-                self.logger.warning(
-                    "Socket buffer full; skipping JSON message ('%r%s')!",
-                                excerpt, trunc)
-            else:
-                self.logger.warning(
-                                "Error while handling JSON message ('%r%s')!",
-                                excerpt, trunc,
-                                exc_info=True)
+            if self.failed_on_messages == 0:
+                excerpt = message[:50] if len(message) > 50 else message
+                trunc = "..." if len(message) > 50 else ""
+                if e.errno == errno.EAGAIN or e.errno == errno.ENOBUFS:
+                    # errno is different on mac vs linux, maybe also depending on
+                    # python version
+                    self.logger.warning(
+                        "Socket buffer full; skipping JSON message ('%r%s')!",
+                                    excerpt, trunc)
+                else:
+                    self.logger.warning(
+                                    "Error while handling JSON message ('%r%s')!",
+                                    excerpt, trunc,
+                                    exc_info=True)
+            self.failed_on_messages += 1
         except Exception:
-            excerpt = message[:50] if len(message) > 50 else message
-            trunc = "..." if len(message) > 50 else ""
-            self.logger.warning("Error while handling JSON message ('%r%s')!",
-                                excerpt, trunc,
-                                exc_info=True)
+            if self.failed_on_messages == 0:
+                excerpt = message[:50] if len(message) > 50 else message
+                trunc = "..." if len(message) > 50 else ""
+                self.logger.warning("Error while handling JSON message ('%r%s')!",
+                                    excerpt, trunc,
+                                    exc_info=True)
+            self.failed_on_messages += 1
 
     def _encode_for_send(self, msg, deflate):
         try:
@@ -1400,8 +1410,8 @@ class CrawlWebSocket(tornado.websocket.WebSocketHandler):
             comp_ratio = 100 - 100 * (self.compressed_bytes_sent + self.uncompressed_bytes_sent) / self.total_message_bytes
             comp_ratio = round(comp_ratio, 2)
 
-        if self.failed_messages > 0:
-            failed_msg = ", %d failed messages" % self.failed_messages
+        if self.failed_messages > 0 or self.failed_on_messages > 0:
+            failed_msg = ", %d (client) + %d (process) failed messages" % (self.failed_messages, self.failed_on_messages)
         else:
             failed_msg = ""
 
