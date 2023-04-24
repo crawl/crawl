d85ba11b5aacf36ab71133eea2bdb3b60bcea0e3
advil <rawlins@gmail.com>
2023-04-24 07:16:41 -0400

    fix: redo disable some errors on dgl builds
    Restore c8ec674a0ca, with some missing #ifdefs.
    
    This reverts commit 2cc9ac7196178b7a7cd06e2ba3a27825e5ef34d3.

--- crawl-ref/source/initfile.cc
+++ crawl-ref/source/initfile.cc
@@ -272,8 +272,6 @@ const vector<GameOption*> game_options::build_options_list()
     vector<GameOption*> options = {
 #if !defined(DGAMELAUNCH) || defined(DGL_REMEMBER_NAME)
         new StringGameOption(NEWGAME_NAME(name), "", true),
-#else
-        new DisabledGameOption({"name"}),
 #endif
         new BoolGameOption(NEWGAME_NAME(fully_random), false),
         new StringGameOption(NEWGAME_NAME(arena_teams), ""),
@@ -719,7 +717,7 @@ const vector<GameOption*> game_options::build_options_list()
 #ifdef DGAMELAUNCH
         new DisabledGameOption(
             {"name_bypasses_menu", "restart_after_save", "newgame_after_quit",
-             "map_file_name", "morgue_dir", "type"}),
+             "map_file_name", "morgue_dir"}),
 #else
         new MultipleChoiceGameOption<game_type>(NEWGAME_NAME(type),
             GAME_TYPE_NORMAL,
@@ -2307,7 +2305,9 @@ newgame_def read_startup_prefs()
     temp.basefilename = filename;
     temp.read_options(fl, false);
 
+#ifndef DGAMELAUNCH
     const bool manual_game_type = Options["type"].was_loaded();
+#endif
 
     // !!side effect warning!!
     {
@@ -2319,9 +2319,11 @@ newgame_def read_startup_prefs()
         Options.merge(temp);
     }
 
+#ifndef DGAMELAUNCH
     // override the startup prefs file with this
     if (manual_game_type)
         temp.game.type = Options.game.type;
+#endif
 
     if (!temp.game.allowed_species.empty())
         temp.game.species = temp.game.allowed_species[0];
