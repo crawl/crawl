f69bf04e7ac477d9b293eef0b694bcd8d393f85f
Nikolai Lavsky <3328424+nlavsky@users.noreply.github.com>
2023-04-27 16:38:32 +0300

    fix: make horn of Geryon's summons default to attacking
    Currently, summoned hell beasts often just follow the player and ignore
    nearby enemies until they attack first. This commit makes these summons
    more aggressive, similar to how mutant beasts behave.

--- crawl-ref/source/evoke.cc
+++ crawl-ref/source/evoke.cc
@@ -93,7 +93,7 @@ static bool _evoke_horn_of_geryon()
 
         if (random2(adjusted_power) > 7)
             beh = BEH_FRIENDLY;
-        mgen_data mg(MONS_HELL_BEAST, beh, you.pos(), MHITYOU, MG_FORCE_BEH);
+        mgen_data mg(MONS_HELL_BEAST, beh, you.pos(), MHITYOU, MG_AUTOFOE);
         mg.set_summoned(&you, 3, SPELL_NO_SPELL);
         mg.set_prox(PROX_CLOSE_TO_PLAYER);
         mon = create_monster(mg);
