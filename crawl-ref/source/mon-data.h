crawl/crawl

commit 9651f06ebd3838f041cdbc27029221a095e864f8

fix wizlab wizards "come into view" message (Flugkiller)
Wizlab wizards did not have the M_UNIQUE flag, which caused them to
get displayed in "come into view" messsages as "a <name>" instead
of "<name>". this only happened when they came into view along with
other monsters.

This fixes the issue by giving them the M_UNIQUE flag.

Resolves #2738

crawl-ref/source/mon-data.h
@@ -5336,7 +5336,7 @@ DUMMY(MONS_SNAKE, 'S', LIGHTGREEN, "snake", TILEP_MONS_ADDER)
 // Semi-unique humans ('@')
 {
     MONS_HELLBINDER, '@', ETC_FIRE, "Hellbinder",
-    M_SPEAKS | M_WARM_BLOOD | M_SEE_INVIS,
+    M_SPEAKS | M_WARM_BLOOD | M_SEE_INVIS | M_UNIQUE,
     MR_NO_FLAGS,
     10, MONS_HUMAN, MONS_HUMAN, MH_NATURAL, 100,
     { {AT_HIT, AF_PLAIN, 6}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
@@ -5349,7 +5349,7 @@ DUMMY(MONS_SNAKE, 'S', LIGHTGREEN, "snake", TILEP_MONS_ADDER)
 
 {
     MONS_CLOUD_MAGE, '@', ETC_SILVER, "Cloud Mage",
-    M_SPEAKS | M_WARM_BLOOD | M_SEE_INVIS | M_FLIES,
+    M_SPEAKS | M_WARM_BLOOD | M_SEE_INVIS | M_FLIES | M_UNIQUE,
     MR_NO_FLAGS,
     10, MONS_HUMAN, MONS_HUMAN, MH_NATURAL, 100,
     { {AT_HIT, AF_PLAIN, 6}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
@@ -5362,7 +5362,7 @@ DUMMY(MONS_SNAKE, 'S', LIGHTGREEN, "snake", TILEP_MONS_ADDER)
 
 {
     MONS_HEADMASTER, '@', ETC_IRON, "Head Instructor",
-    M_WARM_BLOOD | M_FIGHTER | M_TWO_WEAPONS | M_SPEAKS | M_SEE_INVIS,
+    M_WARM_BLOOD | M_FIGHTER | M_TWO_WEAPONS | M_SPEAKS | M_SEE_INVIS | M_UNIQUE,
     MR_NO_FLAGS,
     10, MONS_HUMAN, MONS_HUMAN, MH_NATURAL, 100,
     { {AT_HIT, AF_PLAIN, 30}, {AT_HIT, AF_PLAIN, 30}, AT_NO_ATK, AT_NO_ATK },

commit 1548c4f2a5f8428e51dff291bc8d79d177c85ede

Re-fix wizlab wizard names (elliptic)
9651f06ebd3838 caused hellbinders, etc to be incorrectly emitted
as uniq milestones.

Re-fixes #2738.

crawl-ref/source/mon-data.h
@@ -5393,7 +5393,7 @@ DUMMY(MONS_SNAKE, 'S', LIGHTGREEN, "snake", TILEP_MONS_ADDER)
 // Semi-unique humans ('@')
 {
     MONS_HELLBINDER, '@', ETC_FIRE, "Hellbinder",
-    M_SPEAKS | M_WARM_BLOOD | M_SEE_INVIS | M_UNIQUE,
+    M_SPEAKS | M_ALWAYS_NAMED | M_WARM_BLOOD | M_SEE_INVIS,
     MR_NO_FLAGS,
     10, MONS_HUMAN, MONS_HUMAN, MH_NATURAL, 100,
     { {AT_HIT, AF_PLAIN, 6}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
@@ -5406,7 +5406,7 @@ DUMMY(MONS_SNAKE, 'S', LIGHTGREEN, "snake", TILEP_MONS_ADDER)
 
 {
     MONS_CLOUD_MAGE, '@', ETC_SILVER, "Cloud Mage",
-    M_SPEAKS | M_WARM_BLOOD | M_SEE_INVIS | M_FLIES | M_UNIQUE,
+    M_SPEAKS | M_ALWAYS_NAMED | M_WARM_BLOOD | M_SEE_INVIS | M_FLIES,
     MR_NO_FLAGS,
     10, MONS_HUMAN, MONS_HUMAN, MH_NATURAL, 100,
     { {AT_HIT, AF_PLAIN, 6}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
@@ -5419,7 +5419,7 @@ DUMMY(MONS_SNAKE, 'S', LIGHTGREEN, "snake", TILEP_MONS_ADDER)
 
 {
     MONS_HEADMASTER, '@', ETC_IRON, "Head Instructor",
-    M_WARM_BLOOD | M_FIGHTER | M_TWO_WEAPONS | M_SPEAKS | M_SEE_INVIS | M_UNIQUE,
+    M_WARM_BLOOD | M_FIGHTER | M_TWO_WEAPONS | M_SPEAKS | M_ALWAYS_NAMED | M_SEE_INVIS,
     MR_NO_FLAGS,
     10, MONS_HUMAN, MONS_HUMAN, MH_NATURAL, 100,
     { {AT_HIT, AF_PLAIN, 30}, {AT_HIT, AF_PLAIN, 30}, AT_NO_ATK, AT_NO_ATK },

crawl-ref/source/mon-flags.h
@@ -118,7 +118,8 @@ enum monclass_flag_type : uint64_t
     /// An ancestor granted by Hepliaklqana
     M_ANCESTOR          = BIT(35),
 
-                        //BIT(36), // was M_ALWAYS_CORPSE
+    /// Gets a special name, like the Hellbinder
+    M_ALWAYS_NAMED      = BIT(36),
 
     /// prefer ranged attacks over melee
     M_PREFER_RANGED     = BIT(37),

crawl-ref/source/mon-util.cc
@@ -1483,6 +1483,13 @@ bool mons_is_or_was_unique(const monster& mon)
               && mons_is_unique((monster_type) mon.props[ORIGINAL_TYPE_KEY].get_int());
 }
 
+/// This monster isn't a unique per se, but it gets a name anyway.
+/// E.g., the Hellbinder.
+bool mons_is_specially_named(monster_type mc)
+{
+    return mons_class_flag(mc, M_ALWAYS_NAMED);
+}
+
 /**
  * Is the given type one of Hepliaklqana's granted ancestors?
  *

crawl-ref/source/mon-util.h
@@ -230,6 +230,7 @@ bool mons_can_shout(monster_type mclass);
 bool mons_is_ghost_demon(monster_type mc);
 bool mons_is_unique(monster_type mc);
 bool mons_is_or_was_unique(const monster& mon);
+bool mons_is_specially_named(monster_type mc);
 bool mons_is_pghost(monster_type mc);
 bool mons_is_draconian_job(monster_type mc);
 bool mons_is_hepliaklqana_ancestor(monster_type mc);

crawl-ref/source/view.cc
@@ -447,7 +447,7 @@ static string _describe_monsters_from_species(const vector<details> &species)
         [] (const details &det)
         {
             string name = det.name;
-            if (mons_is_unique(det.mon->type))
+            if (mons_is_unique(det.mon->type) || mons_is_specially_named(det.mon->type))
                 return name;
             else if (det.count > 1 && det.genus)
             {