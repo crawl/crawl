crawl/crawl

commit 5f0c076935360da4785604ba0cbf58fd1f90324d

Semi-fix a weird Hep crash (#3138, jturbzz)
I don't understand what would cause a Hep hexer ancestor to drop
its quickblade on the floor and also to somehow turn it into a
mangled randart, but that seems to have happened. Ew.

As the commit notes, see also https://crawl.develz.org/mantis/view.php?id=11756 .

crawl-ref/source/artefact.cc
@@ -1934,4 +1934,16 @@ void artefact_fixup_props(item_def &item)
 
     if (props.exists(KNOWN_PROPS_KEY))
         artefact_pad_store_vector(props[KNOWN_PROPS_KEY], false);
+
+    // As of 0.30, it seems like there is some rare circumstance that can
+    // cause a Hepliaklqana ancestor's weapon to become a half-baked artefact -
+    // ISFLAG_RANDART set, but ARTEFACT_PROPS_KEY not. Until we understand
+    // what's happening, fix things here to salvage broken saves.
+    // (This seems to be related to
+    // https://crawl.develz.org/mantis/view.php?id=11756 - see also abyss.cc.
+    if ((item.flags & (ISFLAG_SUMMONED | ISFLAG_RANDART))
+        && !item.props.exists(ARTEFACT_PROPS_KEY))
+    {
+        item.flags &= ~ISFLAG_RANDART;
+    }
 }

commit be637cbdd6b991db40107e63cc8be35861416954

Fix broken artefact book names (#3144)
Broken in 5f0c07693536 (#3138).

crawl-ref/source/artefact.cc
@@ -1941,7 +1941,8 @@ void artefact_fixup_props(item_def &item)
     // what's happening, fix things here to salvage broken saves.
     // (This seems to be related to
     // https://crawl.develz.org/mantis/view.php?id=11756 - see also abyss.cc.
-    if ((item.flags & (ISFLAG_SUMMONED | ISFLAG_RANDART))
+    if (item.base_type == OBJ_WEAPONS
+        && (item.flags & (ISFLAG_SUMMONED | ISFLAG_RANDART))
         && !item.props.exists(ARTEFACT_PROPS_KEY))
     {
         item.flags &= ~ISFLAG_RANDART;