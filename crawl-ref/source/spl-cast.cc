crawl/crawl

commit 39cede6f937497c50376fd57df79c91e5373edbe

Make Djinn use all skills (elliptic)
Because Djinn don't choose what spells they get, they need to have
unified spell school training. That ensures that, if they choose to
train magic, they can use whichever spells they get.

I initially implemented this by making Djinn *only* use Spellcasting
for magic. This was simple, which is good! But it had several issues:

- It prevented use of pain weapons & elemental staff melee, which
  was a recurring source of confusion for players.
- The low apt for Spellcasting felt bad, even though Djinn were still
  very strong.
- It meant that many types of items, like manuals of Fire Magic, were
  useless.
- It made Ashenzari Introspection curses ludicrously dominant.

Instead, let's make Djinn able to use all magic skills, as usual.
Furthermore, let's give them a ludicrous +11 apt in all spell schools
and Spellcasting, enough to let them train every one at once at the
same rate the old Spellcasting apt provided. However! The player can
only enable *all* magic skills or *none* - magic skills can't be
trained separately.

In practice, this is mostly equivalent to the old system, with the
exception of fixing the problems listed above and a few other caveats.
(For example, Ru Arcana sacrifices now work and 'waste' XP, like
sacrificing skills on a gnoll.) I'm hopeful it should feel much better.
(It's a slight buff to dj spell starts, due to how skills round.)

This needs playtesting both for bugs and to make sure the starting
skill setup works right. I'm quite scared. Fingers crossed...

crawl-ref/source/spl-cast.cc
@@ -376,7 +376,7 @@ static int _stepdown_spellpower(int power)
 
 static int _skill_power(spell_type spell)
 {
-    int power = 0;
+    int power = you.skill(SK_SPELLCASTING, 50);
 
     const spschools_type disciplines = get_spell_disciplines(spell);
     const int skillcount = count_bits(disciplines);
@@ -387,10 +387,6 @@ static int _skill_power(spell_type spell)
                 power += you.skill(spell_type2skill(bit), 200);
         power /= skillcount;
     }
-
-    // Innate casters use spellcasting for every spell school.
-    const int splcast_mult = you.has_mutation(MUT_INNATE_CASTER) ? 250 : 50;
-    power += you.skill(SK_SPELLCASTING, splcast_mult);
     return power;
 }
 

crawl-ref/source/dat/species/djinni.yaml
@@ -17,19 +17,19 @@ aptitudes:
   staves: -1
   ranged weapons: -2
   throwing: -2
-  spellcasting: -3
+  spellcasting: 11
+  fire_magic: 11
+  ice_magic: 11
+  air_magic: 11
+  earth_magic: 11
+  poison_magic: 11
+  conjurations: 11
+  hexes: 11
+  summoning: 11
+  necromancy: 11
+  translocations: 11
+  transmutations: 11
   dodging: 1
-  fire_magic: False
-  ice_magic: False
-  air_magic: False
-  earth_magic: False
-  poison_magic: False
-  conjurations: False
-  hexes: False
-  summoning: False
-  necromancy: False
-  translocations: False
-  transmutations: False
   stealth: -1
 str: 7
 int: 9

crawl-ref/source/describe.cc
@@ -3473,7 +3473,7 @@ void target_item(item_def &item)
     you.set_training_target(skill, target, true);
     // ensure that the skill is at least enabled
     if (you.train[skill] == TRAINING_DISABLED)
-        you.train[skill] = TRAINING_ENABLED;
+        set_training_status(skill, TRAINING_ENABLED);
     you.train_alt[skill] = you.train[skill];
     reset_training();
 }

crawl-ref/source/l-you.cc
@@ -1122,9 +1122,9 @@ LUAFN(you_train_skill)
     skill_type sk = str_to_skill_safe(luaL_checkstring(ls, 1));
     if (lua_gettop(ls) >= 2 && can_enable_skill(sk))
     {
-        you.train[sk] = min(max((training_status)luaL_safe_checkint(ls, 2),
+        set_training_status(sk, min(max((training_status)luaL_safe_checkint(ls, 2),
                                                  TRAINING_DISABLED),
-                                             TRAINING_FOCUSED);
+                                             TRAINING_FOCUSED));
         reset_training();
     }
 

crawl-ref/source/ng-setup.cc
@@ -207,20 +207,6 @@ static void _give_job_spells(job_type job)
     }
 }
 
-static void _cleanup_innate_magic_skills()
-{
-    // could use a reference here, but seems surprising to the reader
-    int spcasting = you.skills[SK_SPELLCASTING];
-    for (skill_type sk = SK_FIRST_MAGIC_SCHOOL; sk <= SK_LAST_MAGIC; sk++)
-    {
-        const int lvl = you.skills[sk];
-        if (lvl > spcasting)
-            spcasting = lvl;
-        you.skills[sk] = 0;
-    }
-    you.skills[SK_SPELLCASTING] = spcasting;
-}
-
 void give_items_skills(const newgame_def& ng)
 {
     create_wanderer();
@@ -291,9 +277,6 @@ void give_items_skills(const newgame_def& ng)
 
     if (you.has_mutation(MUT_NO_ARMOUR))
         you.skills[SK_SHIELDS] = 0;
-
-    if (you.has_mutation(MUT_INNATE_CASTER))
-        _cleanup_innate_magic_skills();
 
     if (!you_worship(GOD_NO_GOD))
     {

crawl-ref/source/ng-wanderer.cc
@@ -222,9 +222,6 @@ static skill_type _wanderer_role_skill_select(bool defense)
 
     if (defense)
         skill = _apt_weighted_choice(defense_skills, defense_size);
-    // give Djinn some help since they only have one magic apt
-    else if (you.has_mutation(MUT_INNATE_CASTER) && coinflip())
-        skill = SK_SPELLCASTING;
     // reduce the chance of a spell felid a bit
     else if (you.has_mutation(MUT_NO_GRASPING) && one_chance_in(3))
         skill = _apt_weighted_choice(physical_skills, physical_size);
@@ -253,12 +250,7 @@ static void _setup_starting_skills(skill_type sk1, skill_type sk2,
         if (sk <= SK_LAST_MUNDANE)
             martial++;
         else if (sk > SK_LAST_MUNDANE && sk <= SK_LAST_MAGIC)
-        {
-            // handle Djinn
-            if (you.has_mutation(MUT_INNATE_CASTER))
-                sk = SK_SPELLCASTING;
             magical++;
-        }
         if (sk != SK_NONE)
         {
             you.skills[sk]++;
@@ -579,13 +571,6 @@ static vector<spell_type> _wanderer_good_equipment(skill_type & skill)
     if (skill == SK_FIGHTING)
         skill =  _apt_weighted_choice(combined_weapon_skills, total_weapons);
 
-    // handle Djinn
-    if (you.has_mutation(MUT_INNATE_CASTER) && skill == SK_SPELLCASTING)
-    {
-        skill = (skill_type)(SK_SPELLCASTING + random2(SK_LAST_MAGIC
-                    - SK_SPELLCASTING + 1));
-    }
-
     switch (skill)
     {
     case SK_MACES_FLAILS:
@@ -702,13 +687,6 @@ static vector<spell_type> _wanderer_decent_equipment(skill_type & skill,
         && (skill == SK_STAVES || skill == SK_RANGED_WEAPONS))
     {
         skill = SK_FIGHTING;
-    }
-
-    // handle Djinn
-    if (you.has_mutation(MUT_INNATE_CASTER) && skill == SK_SPELLCASTING)
-    {
-        skill = (skill_type)(SK_SPELLCASTING + random2(SK_LAST_MAGIC
-                    - SK_SPELLCASTING + 1));
     }
 
     // Don't give a gift from the same skill twice; just default to

crawl-ref/source/religion.cc
@@ -3748,8 +3748,7 @@ static void _join_ru()
 void join_trog_skills()
 {
     if (!you.has_mutation(MUT_DISTRIBUTED_TRAINING))
-        for (int sk = SK_SPELLCASTING; sk <= SK_LAST_MAGIC; ++sk)
-            you.train[sk] = you.train_alt[sk] = TRAINING_DISABLED;
+        set_magic_training(TRAINING_DISABLED);
     you.skills_to_hide.insert(SK_SPELLCASTING);
 }
 

crawl-ref/source/skill-menu.cc
@@ -310,7 +310,9 @@ void SkillMenuEntry::set_aptitude()
     else
         text += make_stringf(" %d", apt);
 
-    text += "</white> ";
+    text += "</white>";
+    if (apt < 10 && apt > -10)
+        text += " ";
 
     if (manual)
     {
@@ -1527,15 +1529,20 @@ void SkillMenu::toggle_practise(skill_type sk, int keyn)
     if (keyn >= 'A' && keyn <= 'Z')
         you.train.init(TRAINING_DISABLED);
     if (get_state(SKM_DO) == SKM_DO_PRACTISE)
-        you.train[sk] = (you.train[sk] ? TRAINING_DISABLED : TRAINING_ENABLED);
+        set_training_status(sk, you.train[sk] ? TRAINING_DISABLED : TRAINING_ENABLED);
     else if (get_state(SKM_DO) == SKM_DO_FOCUS)
-    {
-        you.train[sk]
-            = (training_status)((you.train[sk] + 1) % NUM_TRAINING_STATUSES);
-    }
+        set_training_status(sk, (training_status)((you.train[sk] + 1) % NUM_TRAINING_STATUSES));
     else
         die("Invalid state.");
     reset_training();
+    if (is_magic_skill(sk) && you.has_mutation(MUT_INNATE_CASTER))
+    {
+        // This toggles every single magic skill, so let's just regenerate the display.
+        refresh_display();
+        return;
+    }
+
+    // Otherwise, only toggle the affected skill button.
     SkillMenuEntry* skme = find_entry(sk);
     skme->set_name(true);
     const vector<int> hotkeys = skme->get_name_item()->get_hotkeys();

commit a1596ed5d586ac103c69090d8983db06292737a9

Fix multischool spell success/power (purplemustard)
Broken by 39cede6f937497c.

crawl-ref/source/spl-cast.cc
@@ -376,8 +376,7 @@ static int _stepdown_spellpower(int power)
 
 static int _skill_power(spell_type spell)
 {
-    int power = you.skill(SK_SPELLCASTING, 50);
-
+    int power = 0;
     const spschools_type disciplines = get_spell_disciplines(spell);
     const int skillcount = count_bits(disciplines);
     if (skillcount)
@@ -387,7 +386,7 @@ static int _skill_power(spell_type spell)
                 power += you.skill(spell_type2skill(bit), 200);
         power /= skillcount;
     }
-    return power;
+    return power + you.skill(SK_SPELLCASTING, 50);
 }
 
 