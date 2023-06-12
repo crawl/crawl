crawl/crawl

commit 0e04eb73d5ab99656dc566e4e7979a961e0404e9

Allow Death Channel to work on demons and holies
To allow necromancy some further application in areas where it's generally
weaker, and for thematic consistency with Yredelemnul's Enslave Soul, allow
it to create spectral demons and holies, and to also work on living
creatures that don't leave a corpse.

crawl-ref/source/mon-util.cc
@@ -1671,6 +1671,13 @@ bool mons_can_be_zombified(const monster& mon)
            && !mons_enslaved_body_and_soul(mon);
 }
 
+bool mons_can_be_spectralised(const monster& mon)
+{
+    return mon.holiness() & (MH_NATURAL | MH_DEMONIC | MH_HOLY)
+           && !mon.is_summoned()
+           && mon.type != MONS_PANDEMONIUM_LORD;
+}
+
 bool mons_class_can_use_stairs(monster_type mc)
 {
     return (!mons_class_is_zombified(mc) || mc == MONS_SPECTRAL_THING)

crawl-ref/source/mon-util.h
@@ -317,6 +317,7 @@ bool mons_class_leaves_hide(monster_type mc);
 bool mons_is_zombified(const monster& mons);
 bool mons_class_can_be_zombified(monster_type mc);
 bool mons_can_be_zombified(const monster& mon);
+bool mons_can_be_spectralised(const monster& mon);
 bool mons_class_can_use_stairs(monster_type mc);
 bool mons_class_can_use_transporter(monster_type mc);
 bool mons_can_use_stairs(const monster& mon,

crawl-ref/source/god-abil.cc
@@ -1678,14 +1678,10 @@ bool yred_injury_mirror()
 
 bool yred_can_enslave_soul(monster* mon)
 {
-    return (mon->holiness() & MH_NATURAL
-            || mon->holiness() & MH_DEMONIC
-            || mon->holiness() & MH_HOLY)
-           && !mon->is_summoned()
+    return mons_can_be_spectralised(*mon)
            && !mons_enslaved_body_and_soul(*mon)
            && mon->attitude != ATT_FRIENDLY
-           && mons_intel(*mon) >= I_HUMAN
-           && mon->type != MONS_PANDEMONIUM_LORD;
+           && mons_intel(*mon) >= I_HUMAN;
 }
 
 void yred_make_enslaved_soul(monster* mon, bool force_hostile)

crawl-ref/source/mon-death.cc
@@ -1119,7 +1119,11 @@ static string _killer_type_name(killer_type killer)
  */
 static void _make_derived_undead(monster* mons, bool quiet, bool bound_soul)
 {
-    if (mons->holiness() & MH_NATURAL && mons_can_be_zombified(*mons))
+    bool valid_mons = bound_soul ? mons->holiness() & MH_NATURAL
+                                   && mons_can_be_zombified(*mons)
+                                 : mons_can_be_spectralised(*mons);
+
+    if (valid_mons)
     {
         // Use the original monster type as the zombified type here, to
         // get the proper stats from it.

commit bf13549d7c2a334ff38fb0051bddd08410055114

Fix monster specs for simulacrums/spectres
Broken by 0e04eb73d5a (2021), when we allowed demonic and holy ghosts.

This also fixes the 'monster' bot's output.

crawl-ref/source/mapdef.cc
@@ -4365,24 +4365,26 @@ void mons_list::get_zombie_type(string s, mons_spec &spec) const
     if (base_monster.props.exists(MGEN_NUM_HEADS))
         spec.props[MGEN_NUM_HEADS] = base_monster.props[MGEN_NUM_HEADS];
 
-    const int zombie_size = mons_zombie_size(spec.monbase);
-    if (!zombie_size)
+    spec.type = zombie_montypes[mod];
+    switch (spec.type)
     {
-        spec.type = MONS_PROGRAM_BUG;
-        return;
-    }
-    if (mod == 1 && mons_class_flag(spec.monbase, M_NO_ZOMBIE))
-    {
-        spec.type = MONS_PROGRAM_BUG;
-        return;
-    }
-    if (mod == 2 && mons_class_flag(spec.monbase, M_NO_SKELETON))
-    {
-        spec.type = MONS_PROGRAM_BUG;
-        return;
+    case MONS_SIMULACRUM:
+    case MONS_SPECTRAL_THING:
+        if (mons_class_can_be_spectralised(spec.monbase))
+            return;
+        break;
+    case MONS_SKELETON:
+        if (!mons_skeleton(spec.monbase))
+            break;
+        // fallthrough to MONS_ZOMBIE
+    case MONS_ZOMBIE:
+    default:
+        if (mons_class_can_be_zombified(spec.monbase))
+            return;
+        break;
     }
 
-    spec.type = zombie_montypes[mod];
+    spec.type = MONS_PROGRAM_BUG;
 }
 
 mons_spec mons_list::get_hydra_spec(const string &name) const

crawl-ref/source/mon-util.cc
@@ -1734,16 +1734,24 @@ bool mons_can_be_zombified(const monster& mon)
            && mons_has_attacks(mon, true);
 }
 
+bool mons_class_can_be_spectralised(monster_type mzc, bool divine)
+{
+    monster_type mc = mons_species(mzc);
+    ASSERT_smc();
+    return mons_class_holiness(mzc) & (MH_NATURAL | MH_DEMONIC | MH_HOLY)
+        && mc != MONS_PANDEMONIUM_LORD
+        && (!divine || smc->attack[0].type != AT_NONE); // i.e. has_attack
+}
+
 // Does this monster have a soul that can be used for necromancy (Death
 // Channel, Simulacrum, Yredelemnul's Bind Soul)? For Bind Soul, allow
 // monsters with no attacks if they have some spells to use.
 bool mons_can_be_spectralised(const monster& mon, bool divine)
 {
-    return mon.holiness() & (MH_NATURAL | MH_DEMONIC | MH_HOLY)
+    return mons_class_can_be_spectralised(mon.type, divine)
            && !mon.is_summoned()
            && (!testbits(mon.flags, MF_NO_REWARD)
                || mon.props.exists(KIKU_WRETCH_KEY))
-           && mon.type != MONS_PANDEMONIUM_LORD
            && (mons_has_attacks(mon, true)
                || divine && mon.has_spells());
 }

crawl-ref/source/mon-util.h
@@ -325,6 +325,7 @@ bool mons_class_leaves_organ(monster_type mc);
 bool mons_is_zombified(const monster& mons);
 bool mons_class_can_be_zombified(monster_type mc);
 bool mons_can_be_zombified(const monster& mon);
+bool mons_class_can_be_spectralised(monster_type mc, bool divine = false);
 bool mons_can_be_spectralised(const monster& mon, bool divine = false);
 bool mons_class_can_use_stairs(monster_type mc);
 bool mons_class_can_use_transporter(monster_type mc);