crawl/crawl

commit be6b1f94854b4c053629de70abcc4c16dea3323d

Make Lucy's wrath protection trigger on Gozag/Ely
Specifically, on Gozag's incite effect and on Ely's enemy lifesaving.
Lucy wouldn't trigger at all otherwise, since those gods don't have
'active' wrath.

Don't trigger on Gozag potion drinking blocking - feels abusable.

crawl-ref/source/god-wrath.cc
@@ -406,8 +406,11 @@ static bool _cheibriados_retribution()
     return true;
 }
 
-static void _banish_foes_nearby()
+void lucy_check_meddling()
 {
+    if (!have_passive(passive_t::wrath_banishment))
+        return;
+
     vector<monster*> potential_banishees;
     for (monster_near_iterator mi(you.pos(), LOS_NO_TRANS); mi; ++mi)
     {
@@ -2251,8 +2254,7 @@ bool divine_retribution(god_type god, bool no_bonus, bool force)
         return false;
     }
 
-    if (have_passive(passive_t::wrath_banishment))
-        _banish_foes_nearby();
+    lucy_check_meddling();
 
     if (no_bonus)
         return true;
@@ -2370,5 +2372,6 @@ void gozag_incite(monster *mon)
     {
         mon->add_ench(ENCH_GOZAG_INCITE);
         view_update_at(mon->pos());
+        lucy_check_meddling();
     }
 }

crawl-ref/source/mon-death.cc
@@ -32,6 +32,7 @@
 #include "god-companions.h"
 #include "god-conduct.h"
 #include "god-passive.h" // passive_t::bless_followers, share_exp, convert_orcs
+#include "god-wrath.h" // lucy_check_meddling
 #include "hints.h"
 #include "hiscores.h"
 #include "item-name.h"
@@ -770,6 +771,8 @@ static bool _ely_heal_monster(monster* mons, killer_type killer, int i)
              mons->hit_points * 2 <= mons->max_hit_points ? "." : "!");
 
     god_speaks(god, msg.c_str());
+
+    lucy_check_meddling();
 
     return true;
 }

crawl-ref/source/god-wrath.h
@@ -11,5 +11,4 @@ bool divine_retribution(god_type god, bool no_bonus = false, bool force = false)
 void reduce_xp_penance(god_type god, int amount);
 
 void gozag_incite(monster *mon);
-
-bool drain_wands();
+void lucy_check_meddling();

commit 2cbb21b3561248aaac5b5642b1927fdaff4b5705

Fix Lucy wrath protection crashes (#3173)
Introduced by be6b1f94854b4c053629de70abcc4c16dea3323d.

Resolves #3173.

crawl-ref/source/fineff.cc
@@ -19,6 +19,7 @@
 #include "env.h"
 #include "fight.h"
 #include "god-abil.h"
+#include "god-wrath.h" // lucy_check_meddling
 #include "libutil.h"
 #include "losglobal.h"
 #include "melee-attack.h"
@@ -895,6 +896,10 @@ void spectral_weapon_fineff::fire()
     mons->summoner = atkr->mid;
     mons->behaviour = BEH_SEEK; // for display
     atkr->props[SPECTRAL_WEAPON_KEY].get_int() = mons->mid;
+}
+
+void lugonu_meddle_fineff::fire() {
+    lucy_check_meddling();
 }
 
 // Effects that occur after all other effects, even if the monster is dead.

crawl-ref/source/fineff.h
@@ -502,4 +502,17 @@ protected:
     }
 };
 
+class lugonu_meddle_fineff : public final_effect
+{
+public:
+    bool mergeable(const final_effect &) const override { return true; };
+    void fire() override;
+
+    static void schedule() {
+        final_effect::schedule(new lugonu_meddle_fineff());
+    }
+protected:
+    lugonu_meddle_fineff() : final_effect(nullptr, nullptr, coord_def()) { }
+};
+
 void fire_final_effects();

crawl-ref/source/god-wrath.cc
@@ -22,6 +22,7 @@
 #include "death-curse.h"
 #include "decks.h"
 #include "env.h"
+#include "fineff.h"
 #include "ghost.h"
 #include "god-abil.h"
 #include "god-passive.h" // shadow_monster
@@ -431,7 +432,12 @@ void lucy_check_meddling()
     const int to_banish = roll_dice(2, 3);
     shuffle_array(begin(potential_banishees), end(potential_banishees));
     for (int i = 0; i < to_banish && i < (int)potential_banishees.size(); ++i)
-        potential_banishees[i]->banish(&you);
+    {
+        // We might have banished a summoner and poofed its summons, etc.
+        monster* mon = potential_banishees[i];
+        if (!invalid_monster(mon) && mon->alive())
+            mon->banish(&you);
+    }
 }
 
 static void _spell_retribution(monster* avatar, spell_type spell, god_type god,
@@ -2372,6 +2378,6 @@ void gozag_incite(monster *mon)
     {
         mon->add_ench(ENCH_GOZAG_INCITE);
         view_update_at(mon->pos());
-        lucy_check_meddling();
+        lugonu_meddle_fineff::schedule();
     }
 }

crawl-ref/source/mon-death.cc
@@ -32,7 +32,6 @@
 #include "god-companions.h"
 #include "god-conduct.h"
 #include "god-passive.h" // passive_t::bless_followers, share_exp, convert_orcs
-#include "god-wrath.h" // lucy_check_meddling
 #include "hints.h"
 #include "hiscores.h"
 #include "item-name.h"
@@ -772,7 +771,7 @@ static bool _ely_heal_monster(monster* mons, killer_type killer, int i)
 
     god_speaks(god, msg.c_str());
 
-    lucy_check_meddling();
+    lugonu_meddle_fineff::schedule();
 
     return true;
 }