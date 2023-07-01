141be24249c34efa1ffe9e879d83841cbc1717fe
Nicholas Feinberg <pleasingfung@gmail.com>
2023-07-01 13:17:34 -0700

    Fix transmuter starting form

--- crawl-ref/source/ng-setup.cc
+++ crawl-ref/source/ng-setup.cc
@@ -536,7 +536,10 @@ static void _setup_generic(const newgame_def& ng,
     }
 
     if (you.char_class == JOB_TRANSMUTER)
+    {
+        you.default_form = transformation::beast;
         set_form(transformation::beast, 1); // hacky...
+    }
 
     reassess_starting_skills();
     init_skill_order();
