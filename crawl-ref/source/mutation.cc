6babd820d02f08c7ec7f4a6e1f293ad7aa8e6dc6
Nicholas Feinberg <pleasingfung@gmail.com>
2023-07-15 21:42:43 -0700

    Fix the tag upgrade build?

--- crawl-ref/source/mutation.cc
+++ crawl-ref/source/mutation.cc
@@ -200,8 +200,10 @@ static const int conflict[][3] =
     { MUT_COLD_RESISTANCE,     MUT_COLD_VULNERABILITY,     -1},
     { MUT_SHOCK_RESISTANCE,    MUT_SHOCK_VULNERABILITY,    -1},
     { MUT_STRONG_WILLED,       MUT_WEAK_WILLED,            -1},
+#if TAG_MAJOR_VERSION == 34
     { MUT_NO_REGENERATION,     MUT_INHIBITED_REGENERATION, -1},
     { MUT_NO_REGENERATION,     MUT_REGENERATION,           -1},
+#endif
     { MUT_HP_CASTING,          MUT_HIGH_MAGIC,             -1},
     { MUT_HP_CASTING,          MUT_LOW_MAGIC,              -1},
 };
