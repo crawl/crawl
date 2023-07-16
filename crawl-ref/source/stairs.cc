crawl/crawl

commit 4d237caa85e2dd7638bbde1e443ca14111cf8674

Let formicids shaft through Tomb
I believe this was only forbidden by accident; shafts can't generate
as random malevolence effects in Tomb, which incidentally broke Formicid
self-shaft.

crawl-ref/source/stairs.cc
@@ -561,7 +561,7 @@ static level_id _travel_destination(const dungeon_feature_type how,
     level_id dest;
     if (shaft)
     {
-        if (!is_valid_shaft_level())
+        if (!is_valid_shaft_level(false))
         {
             if (known_shaft)
                 mpr("The shaft disappears in a puff of logic!");

crawl-ref/source/player.cc
@@ -7445,7 +7445,7 @@ bool player::can_do_shaft_ability(bool quiet) const
 
     if (feat_is_shaftable(env.grid(pos())))
     {
-        if (!is_valid_shaft_level())
+        if (!is_valid_shaft_level(false))
         {
             if (!quiet)
                 mpr("You can't shaft yourself on this level.");

crawl-ref/source/traps.cc
@@ -1022,9 +1022,10 @@ dungeon_feature_type trap_feature(trap_type type)
 /***
  * Can a shaft be placed on the current level?
  *
+ * @param respect_brflags Whether brflag::no_shafts should be factored in.
  * @returns true if such a shaft can be placed.
  */
-bool is_valid_shaft_level()
+bool is_valid_shaft_level(bool respect_brflags)
 {
     // Important: We are sometimes called before the level has been loaded
     // or generated, so should not depend on properties of the level itself,
@@ -1038,7 +1039,7 @@ bool is_valid_shaft_level()
 
     const Branch &branch = branches[place.branch];
 
-    if (branch.branch_flags & brflag::no_shafts)
+    if (respect_brflags && branch.branch_flags & brflag::no_shafts)
         return false;
 
     // Don't allow shafts from the bottom of a branch.

crawl-ref/source/traps.h
@@ -38,7 +38,7 @@ void destroy_trap(const coord_def& pos);
 trap_def* trap_at(const coord_def& where);
 trap_type get_trap_type(const coord_def& where);
 
-bool is_valid_shaft_level();
+bool is_valid_shaft_level(bool respect_brflags = true);
 void set_shafted();
 void roll_trap_effects();
 void do_trap_effects();

commit ce7730605ecaf9ae477703a6f83a922257d4344f

Fix Tomb Fo shaft warning (WizardIke)
Broken in 4d237caa85e2dd7638bbde1e443ca14111cf8674.

Closes #3181.

crawl-ref/source/stairs.cc
@@ -994,7 +994,7 @@ void floor_transition(dungeon_feature_type how,
 
     // Warn Formicids if they cannot shaft here
     if (player_has_ability(ABIL_SHAFT_SELF, true)
-                                && !is_valid_shaft_level())
+                                && !is_valid_shaft_level(false))
     {
         mpr("Beware, you cannot shaft yourself on this level.");
     }