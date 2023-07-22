crawl/crawl

commit 8e7229f42b641c003bf5ab767e04083e393a620f

refactor: generalize some popup code
Previously there was (at least as far as I know) not really any single
standard way to throw up a popup with a short (or long) message in it,
leading to a bunch of ad hoc `ui::Popup` + `ui::Text` + CRT mode rendering calls
that were all similar, but not quite identical, and also not very
general. This replaces these with a single code path:

* Use the formatted_scroller class so that we automatically get a bunch
  of features that just plain `ui::Text` doesn't have. Also, if someone
  wants a quick way to show longer text, this easily provides that.
  (There's some code that could be refactored to use this capability,
  but I haven't done that.
* Wrap class instantiation in some convenience calls in the ui
  namespace.
* Standardize various aspects of this across calls

As part of this I fixed a bunch of formatted_scroller bugs:
* webtiles spacing fixes
* implement title in webtiles (this was just missing)
* if easy_exit is set, allow clicking out of scroller popups
* refactor so that a scroller can correctly handle subclassing key
  overrides, regular scroller key handling, and easy exit all at the
  same time. (Slightly complicates the api from the standard
  process_key).

crawl-ref/source/end.cc
@@ -91,6 +91,8 @@ static void _clear_globals_on_exit()
     destroy_abyss();
 }
 
+// game is dying / crashing, potentially quite early in the startup process.
+// do our best to provide this information to the user somehow.
 bool fatal_error_notification(string error_msg)
 {
     if (error_msg.empty())
@@ -102,13 +104,25 @@ bool fatal_error_notification(string error_msg)
     // initialisation process, and cio_init should be fairly safe.
 #ifndef USE_TILE_LOCAL
     if (!ui::is_available() && !msg::uses_stderr(MSGCH_ERROR))
-        cio_init(); // this, however, should be fairly safe
+        cio_init();
 #endif
 
-    mprf(MSGCH_ERROR, "%s", error_msg.c_str());
+    const bool ui_available =
+#if (!defined(DGAMELAUNCH)) || defined(DGL_PAUSE_AFTER_ERROR)
+        // possibly changed by the cio_init call
+        ui::is_available() && !msg::uses_stderr(MSGCH_ERROR);
+#else
+        false;
+#endif
 
-    if (!ui::is_available() || msg::uses_stderr(MSGCH_ERROR))
+    if (!ui_available || crawl_state.seen_hups)
+    {
+        // in these cases we don't want to attempt the ui popup for the error
+        // message, just print the error to the log. In the non-crash case,
+        // this will also echo to stderr.
+        mprf(MSGCH_ERROR, "%s", error_msg.c_str());
         return false;
+    }
 
     // do the linebreak here so webtiles has it, but it's needed below as well
     linebreak_string(error_msg, 79);
@@ -116,46 +130,7 @@ bool fatal_error_notification(string error_msg)
     tiles.send_exit_reason("error", error_msg);
 #endif
 
-    // this is a bit heavy to continue past here in the face of a real crash.
-    if (crawl_state.seen_hups)
-        return false;
-
-#if (!defined(DGAMELAUNCH)) || defined(DGL_PAUSE_AFTER_ERROR)
-#ifdef USE_TILE_WEB
-    tiles_crt_popup show_as_popup;
-    tiles.set_ui_state(UI_CRT);
-#endif
-
-    // TODO: better formatting, maybe use a formatted_scroller?
-    // Escape '<'.
-    // NOTE: This assumes that the error message doesn't contain
-    //       any formatting!
-    error_msg = string("Fatal error:\n\n<lightred>")
-                       + replace_all(error_msg, "<", "<<");
-    error_msg += "</lightred>\n\n<cyan>Hit any key to exit, "
-                 "ctrl-p for the full log.</cyan>";
-    linebreak_string(error_msg, cgetsize(GOTO_CRT).x - 1);
-
-    auto prompt_ui =
-                make_shared<Text>(formatted_string::parse_string(error_msg));
-    auto popup = make_shared<ui::Popup>(prompt_ui);
-    bool done = false;
-
-    popup->on_hotkey_event([&](const KeyEvent& ev) {
-        if (ev.key() == CONTROL('P'))
-        {
-            replay_messages();
-            return true;
-        }
-        return false;
-    });
-
-    popup->on_keydown_event([&](const KeyEvent&) { return done = true; });
-
-    mouse_control mc(MOUSE_MODE_MORE);
-    ui::run_layout(move(popup), done);
-#endif
-
+    ui::error(error_msg, "Fatal error:", true);
     return true;
 }
 
@@ -249,16 +224,7 @@ NORETURN void screen_end_game(string text)
     delete_files();
 
     if (!text.empty())
-    {
-        auto prompt_ui = make_shared<Text>(
-                formatted_string::parse_string(text));
-        auto popup = make_shared<ui::Popup>(prompt_ui);
-        bool done = false;
-        popup->on_keydown_event([&](const KeyEvent&) { return done = true; });
-
-        mouse_control mc(MOUSE_MODE_MORE);
-        ui::run_layout(move(popup), done);
-    }
+        ui::message(text);
 
     game_ended(game_exit::abort); // TODO: is this the right exit condition?
 }

crawl-ref/source/prompt.cc
@@ -14,6 +14,7 @@
 #include "menu.h"
 #include "message.h"
 #include "options.h"
+#include "scroller.h"
 #include "state.h"
 #include "stringutil.h"
 #ifdef USE_TILE
@@ -21,6 +22,83 @@
 #endif
 #include "viewchar.h"
 #include "ui.h"
+
+namespace ui
+{
+    class message_scroller : public formatted_scroller
+    {
+    public:
+        message_scroller()
+            : formatted_scroller(FS_EASY_EXIT)
+        { }
+
+        message_scroller(string text, string title="", string prompt="")
+            : message_scroller()
+        {
+            if (title.size())
+                set_title(formatted_string::parse_string(title));
+
+            if (prompt.size())
+                set_more(formatted_string::parse_string(prompt));
+
+            add_formatted_string(formatted_string::parse_string(text));
+        }
+
+    protected:
+        maybe_bool process_key(int ch) override
+        {
+            // general keycode that is not hinted by default; it is mostly
+            // useful for debugging. If you want it to be apparent to players,
+            // just set a different `more`. (See e.g. the arena call to
+            // the wrapper functions for an example.)
+            if (ch == CONTROL('P'))
+            {
+                if (crawl_state.game_started)
+                    replay_messages();
+                else
+                    replay_messages_during_startup(); // show a title; less cryptic for this case
+                return MB_TRUE;
+            }
+            return formatted_scroller::process_key(ch);
+        }
+    };
+
+    // Convenience wrapper for `message_scroller`.
+    // somewhat general popup code for short messages. This accepts color
+    // formatting for the optional `title` and `prompt`, but not `msg`.
+    int message(string msg, string title, string prompt)
+    {
+        message_scroller ms(replace_all(msg, "<", "<<"), // XX probably ok to relax?
+            title, prompt);
+        ms.show();
+        return ms.get_lastch();
+    }
+
+    // slightly smarter error msging than just `mprf(MSGCH_ERROR, ...)`: if
+    // there are ui elements on the stack, show a popup on top of them; either
+    // way, log the error to MSGCH_ERROR.
+    int error(string err, string title, bool force_popup)
+    {
+        // unlike `message` above, does not accept color formatting in title,
+        // so that it can auto-wrap it in an angry red
+        // XX direct errorf version of this fn?
+        mprf(MSGCH_ERROR, "%s", err.c_str());
+        // assumes UI init! See end.cc::fatal_error_notification for a wrapped
+        // version that does not.
+        // XX unify the two?
+        if (force_popup || has_layout())
+        {
+            if (title.size())
+            {
+                title = string("<lightred>")
+                        + replace_all(title, "<", "<<")
+                        + "</lightred>";
+            }
+            return message(err, title);
+        }
+        return 0; // no popup used
+    }
+};
 
 // Like yesno, but requires a full typed answer.
 // Unlike yesno, prompt should have no trailing space.

crawl-ref/source/prompt.h
@@ -5,6 +5,13 @@
 
 #pragma once
 
+namespace ui
+{
+    int error(string err, string title="Error!", bool force_popup=false);
+    int message(string msg,
+        string title="", string prompt="<cyan>Hit any key to continue...</cyan>");
+}
+
 bool yes_or_no(PRINTF(0, ));
 typedef map<int, int> explicit_keymap;
 int yesno(const char * str, bool allow_lowercase, int default_answer,

crawl-ref/source/arena.cc
@@ -30,6 +30,7 @@
 #include "mon-tentacle.h"
 #include "newgame-def.h"
 #include "ng-init.h"
+#include "prompt.h"
 #include "spl-miscast.h"
 #include "state.h"
 #include "stringutil.h"
@@ -118,44 +119,6 @@ namespace msg
 }
 
 extern void world_reacts();
-
-static void _results_popup(string msg, bool error=false)
-{
-    // TODO: shared code here with end.cc
-    linebreak_string(msg, 79);
-
-#ifdef USE_TILE_WEB
-    tiles_crt_popup show_as_popup;
-    tiles.set_ui_state(UI_CRT);
-#endif
-
-    if (error)
-    {
-        msg = string("Arena error:\n\n<lightred>")
-                       + replace_all(msg, "<", "<<");
-        msg += "</lightred>";
-    }
-    else
-        msg = string("Arena results:\n\n") + msg;
-
-    msg += "\n\n<cyan>Hit any key to continue, "
-                 "ctrl-p for the full log.</cyan>";
-
-    auto prompt_ui = make_shared<Text>(
-            formatted_string::parse_string(msg));
-    bool done = false;
-    prompt_ui->on_hotkey_event([&](const KeyEvent& ev) {
-        if (ev.key() == CONTROL('P'))
-            replay_messages();
-        else
-            done = true;
-        return done;
-    });
-
-    mouse_control mc(MOUSE_MODE_MORE);
-    auto popup = make_shared<ui::Popup>(prompt_ui);
-    ui::run_layout(move(popup), done);
-}
 
 namespace arena
 {
@@ -1112,7 +1075,10 @@ namespace arena
 
             mpr("---- Contest finished ----\n" + outcome);
             if (!skipped_arena_ui)
-                _results_popup(outcome);
+            {
+                ui::message(outcome, "Arena results:",
+                    "<cyan>Hit any key to continue, ctrl-p for the full log.</cyan>");
+            }
         }
 
         ui::pop_layout();
@@ -1582,8 +1548,7 @@ NORETURN void run_arena(const newgame_def& choice, const string &default_arena_t
             }
             else
             {
-                mprf(MSGCH_ERROR, "%s", error.what());
-                _results_popup(error.what(), true);
+                ui::error(error.what());
                 last_teams = arena_choice.arena_teams;
                 arena_choice.arena_teams = "";
                 // fallthrough

crawl-ref/source/command.cc
@@ -1370,7 +1370,7 @@ public:
         process_key(key);
     };
 private:
-    bool process_key(int ch) override
+    maybe_bool process_key(int ch) override
     {
         int key = toalower(ch);
 
@@ -1385,8 +1385,11 @@ private:
         switch (key)
         {
             case CK_ESCAPE: case ':': case '#': case '/': case 'q': case 'v': case '!':
-                return false;
+                // exit the UI, these help screens are activated outside of
+                // the scroller popup
+                return MB_FALSE;
             default:
+                // try to process help section hotkeys
                 if (!(page = _get_help_section(key, header_text, help_text, scroll)))
                     break;
                 if (page != prev_page)
@@ -1397,7 +1400,7 @@ private:
                 }
                 scroll = scroll ? (scroll-2)*line_height : 0;
                 set_scroll(scroll);
-                return true;
+                return MB_TRUE;
         }
 
         return formatted_scroller::process_key(ch);

crawl-ref/source/dgn-overview.cc
@@ -692,13 +692,13 @@ class dgn_overview : public formatted_scroller
 public:
     dgn_overview(const string& text = "") : formatted_scroller(FS_PREWRAPPED_TEXT, text) {};
 
-private:
-    bool process_key(int ch) override
+protected:
+    maybe_bool process_key(int ch) override
     {
         // We handle these after exiting dungeon overview window
         // to prevent menus from stacking on top of each other.
         if (ch == 'G' || ch == '_' || ch == '$' || ch =='!')
-            return false;
+            return MB_FALSE;
         else
             return formatted_scroller::process_key(ch);
     }

crawl-ref/source/output.cc
@@ -2624,14 +2624,12 @@ public:
     overview_popup() {};
     vector<char> equip_chars;
 private:
-    bool process_key(int ch) override
+    maybe_bool process_key(int ch) override
     {
         if (find(equip_chars.begin(), equip_chars.end(), ch) != equip_chars.end())
         {
             item_def& item = you.inv[letter_to_index(ch)];
-            if (!describe_item(item))
-                return false;
-            return true;
+            return frombool(describe_item(item));
         }
         return formatted_scroller::process_key(ch);
     };

crawl-ref/source/scroller.cc
@@ -111,7 +111,8 @@ int formatted_scroller::show()
     bool done = false;
     popup->on_keydown_event([&done, &text, this](const KeyEvent& ev) {
         m_lastch = ev.key();
-        done = !process_key(m_lastch);
+        const maybe_bool pkey_result = process_key(m_lastch);
+        // done = !process_key(m_lastch);
         if (m_contents_dirty)
         {
             m_contents_dirty = false;
@@ -128,12 +129,17 @@ int formatted_scroller::show()
             m_scroll_dirty = false;
             m_scroller->set_scroll(m_scroll);
         }
-        if (done)
-            return true;
-        if (m_scroller->on_event(ev))
-            return true;
-        if (m_flags & FS_EASY_EXIT)
+        if (pkey_result == MB_FALSE)
             return done = true;
+        else if (pkey_result == MB_MAYBE)
+        {
+            if (m_scroller->on_event(ev))
+                return true;
+            // only check easy exit if `process_key` hasn't intercepted the key
+            if (m_flags & FS_EASY_EXIT)
+                return done = true;
+        }
+        // key either not processed at all, or `process_key` returned MB_TRUE
         return true;
     });
 
@@ -143,6 +149,8 @@ int formatted_scroller::show()
     tiles.json_write_string("text", contents.to_colour_string(LIGHTGRAY));
     tiles.json_write_string("highlight", highlight);
     tiles.json_write_string("more", m_more.to_colour_string(LIGHTGRAY));
+    tiles.json_write_string("title", m_title.to_colour_string(LIGHTGRAY));
+    tiles.json_write_bool("easy_exit", m_flags & FS_EASY_EXIT);
     tiles.json_write_bool("start_at_end", m_flags & FS_START_AT_END);
     tiles.push_ui_layout("formatted-scroller", 2);
     popup->on_layout_pop([](){ tiles.pop_ui_layout(); });
@@ -164,10 +172,16 @@ int formatted_scroller::show()
     return m_lastch;
 }
 
-bool formatted_scroller::process_key(int ch)
+// MB_TRUE: key was processed, keep UI open
+// MB_FALSE: key was processed, close UI
+// MB_MAYBE: key was not processed
+// (the polarity is awkwardly opposite how this is used and derived here, but
+// we keep it this way because all process_key functions in crawl have the
+// same meanings for T/F)
+maybe_bool formatted_scroller::process_key(int ch)
 {
     // most keyhandling is in the scroller event handling, not here
-    return !ui::key_exits_popup(ch, true);
+    return ui::key_exits_popup(ch, true) ? MB_FALSE : MB_MAYBE;
 }
 
 void formatted_scroller::set_scroll(int y)

commit b41548654ea18992421ffe39400ed60f8ff86abe

fix: several tutorial end issues
* The tutorial summary popups have had broken formatting since
  8e7229f42b641c00. This lets ui::message take color formatting
  optionally and uses it for this case.
* Give tutorial summary popups titles, and remove a manually formatted
  prompt that was hardcoded in the database, now that the popup is more
  standardized. Also reword the prompt for exit popups.
* tutorial exit conditions were also broken; this mostly shouldn't
  matter but may affect a popup on webtiles
* convert the tutorial death message to a popup with text from the
  database, rather than a hard-coded popup

crawl-ref/source/end.cc
@@ -215,7 +215,7 @@ void delete_files()
     you.save = 0;
 }
 
-NORETURN void screen_end_game(string text)
+NORETURN void screen_end_game(string text, game_exit exit)
 {
 #ifdef USE_TILE_WEB
     tiles.send_exit_reason("quit");
@@ -224,9 +224,9 @@ NORETURN void screen_end_game(string text)
     delete_files();
 
     if (!text.empty())
-        ui::message(text);
+        ui::message(text, "", "<cyan>Hit any key to exit...</cyan>", true);
 
-    game_ended(game_exit::abort); // TODO: is this the right exit condition?
+    game_ended(exit); // TODO: is this the right exit condition?
 }
 
 static game_exit _kill_method_to_exit(kill_method_type kill)

crawl-ref/source/end.h
@@ -15,7 +15,7 @@ NORETURN void end(int exit_code, bool print_err = false, PRINTF(2, = nullptr));
 NORETURN void end_game(scorefile_entry &se);
 NORETURN void game_ended(game_exit exit, const string &message = "");
 NORETURN void game_ended_with_error(const string &message);
-NORETURN void screen_end_game(string text);
+NORETURN void screen_end_game(string text, game_exit exit);
 void cio_cleanup();
 bool fatal_error_notification(string error_msg);
 

crawl-ref/source/hints.cc
@@ -2866,8 +2866,10 @@ void tutorial_msg(const char *key, bool end)
     _replace_static_tags(text);
     text = untag_tiles_console(text);
 
+    // n.b. leaving the dungeon counts as "winning" here (and pops up the
+    // tutorial summary)
     if (end)
-        screen_end_game(text);
+        screen_end_game(text, you.hp <= 0 ? game_exit::death : game_exit::win);
 
     // "\n" to preserve indented parts, the rest is unwrapped, or split into
     // paragraphs by "\n\n", split_string() will ignore the empty line.

crawl-ref/source/l-crawl.cc
@@ -1271,7 +1271,6 @@ static int crawl_get_command(lua_State *ls)
     return 1;
 }
 
-LUAWRAP(crawl_endgame, screen_end_game(luaL_checkstring(ls, 1)))
 LUAWRAP(crawl_tutorial_skill, set_tutorial_skill(luaL_checkstring(ls, 1), luaL_safe_checkint(ls, 2)))
 LUAWRAP(crawl_tutorial_hint, tutorial_init_hint(luaL_checkstring(ls, 1)))
 LUAWRAP(crawl_print_hint, print_hint(luaL_checkstring(ls, 1), luaL_optstring(ls, 2, ""), luaL_optstring(ls, 3, "")))
@@ -1507,7 +1506,6 @@ static const struct luaL_reg crawl_clib[] =
     { "is_webtiles",        crawl_is_webtiles },
     { "err_trace",          crawl_err_trace },
     { "get_command",        crawl_get_command },
-    { "endgame",            crawl_endgame },
     { "tutorial_msg",       crawl_tutorial_msg },
     { "dump_char",          crawl_dump_char },
 #ifdef WIZARD

crawl-ref/source/ouch.cc
@@ -1128,7 +1128,8 @@ void ouch(int dam, kill_method_type death_type, mid_t source, const char *aux,
         if (!non_death)
             tutorial_death_message();
 
-        screen_end_game("");
+        // only quitting can lead to this?
+        screen_end_game("", game_exit::quit);
     }
 
     // Okay, so you're dead.

crawl-ref/source/prompt.cc
@@ -67,9 +67,11 @@ namespace ui
     // Convenience wrapper for `message_scroller`.
     // somewhat general popup code for short messages. This accepts color
     // formatting for the optional `title` and `prompt`, but not `msg`.
-    int message(string msg, string title, string prompt)
+    int message(string msg, string title, string prompt, bool format_msg)
     {
-        message_scroller ms(replace_all(msg, "<", "<<"), // XX probably ok to relax?
+        // XX could this just always accept a format string msg?
+        message_scroller ms(
+            format_msg ? msg : replace_all(msg, "<", "<<"),
             title, prompt);
         ms.show();
         return ms.get_lastch();

crawl-ref/source/prompt.h
@@ -11,7 +11,8 @@ namespace ui
 {
     int error(string err, string title="Error!", bool force_popup=false);
     int message(string msg,
-        string title="", string prompt="<cyan>Hit any key to continue...</cyan>");
+        string title="", string prompt="<cyan>Hit any key to continue...</cyan>",
+        bool format_msg=false);
 }
 
 bool yes_or_no(PRINTF(0, ));

crawl-ref/source/tutorial.cc
@@ -47,14 +47,11 @@ void tutorial_init_hint(const char* hintstr)
         Hints.hints_events[hint] = true;
 }
 
+// noreturn, but tutorial_msg doesn't guarantee it
 void tutorial_death_message()
 {
     canned_msg(MSG_YOU_DIE);
-    mpr_nojoin(MSGCH_TUTORIAL,
-               "In Crawl, death is a sad but common occurrence. "
-               "Note that there's usually something you could have done to "
-               "survive, for example by using some kind of item, running away, "
-               "resting between fights, or by avoiding combat entirely. "
-               "Keep trying, eventually you'll prevail!");
     more();
+ 
+    tutorial_msg("tutorial death", true);
 }