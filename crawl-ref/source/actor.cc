crawl/crawl

commit 57a6a0d306af5fede1b6d10abc967cf4a8703c84

Allow BVC/roots to work on unseen foes
Simplify. This was not intended for /roots, and it's not needed for
BVC. We can nerf BVC again in other ways if need be.

crawl-ref/source/actor.cc
@@ -587,15 +587,13 @@ bool actor::has_invalid_constrictor(bool move) const
 
     // Direct constriction (e.g. by nagas and octopode players or AT_CONSTRICT)
     // must happen between adjacent squares.
-    if (get_constrict_type() == CONSTRICT_MELEE)
+    const auto typ = get_constrict_type();
+    if (typ == CONSTRICT_MELEE)
         return !ignoring_player && !adjacent(attacker->pos(), pos());
 
     // Indirect constriction requires the defender not to move.
     return move
-        // Indirect constriction requires reachable ground.
-        || !feat_has_solid_floor(env.grid(pos()))
-        // Constriction doesn't work out of LOS.
-        || !ignoring_player && !attacker->see_cell(pos());
+        || typ == CONSTRICT_BVC && !feat_has_solid_floor(env.grid(pos()));
 }
 
 /**
@@ -695,7 +693,7 @@ bool actor::can_engulf(const actor &defender) const
 
 bool actor::can_constrict(const actor &defender, constrict_type typ) const
 {
-    if (defender.is_constricted())
+    if (defender.is_constricted() || defender.res_constrict() >= 3)
         return false;
 
     if (typ == CONSTRICT_MELEE)
@@ -704,10 +702,8 @@ bool actor::can_constrict(const actor &defender, constrict_type typ) const
             && (!is_constricting() || has_usable_tentacle());
     }
 
-    return can_see(defender)
-        && defender.res_constrict() < 3
-        // All current indrect forms of constriction require reachable ground.
-        && feat_has_solid_floor(env.grid(defender.pos()));
+    return typ != CONSTRICT_BVC
+           || feat_has_solid_floor(env.grid(defender.pos()));
 }
 
 #ifdef DEBUG_DIAGNOSTICS

commit 9dd5d42eca33f928bd4e2c8b24486beb4a3efc67

Fix: make /roots require solid ground again (bobross420-1)
Broken in 57a6a0d306af5fede1b6d10abc967cf4a8703c84.

Closes #3152.

crawl-ref/source/actor.cc
@@ -595,7 +595,7 @@ bool actor::has_invalid_constrictor(bool move) const
     return move
         // Constriction doesn't work out of LOS, to avoid sauciness.
         || !ignoring_player && !attacker->see_cell(pos())
-        || typ == CONSTRICT_BVC && !feat_has_solid_floor(env.grid(pos()));
+        || !feat_has_solid_floor(env.grid(pos()));
 }
 
 /**
@@ -708,8 +708,7 @@ bool actor::can_constrict(const actor &defender, constrict_type typ) const
     if (!see_cell_no_trans(defender.pos()))
         return false;
 
-    return typ != CONSTRICT_BVC
-           || feat_has_solid_floor(env.grid(defender.pos()));
+    return feat_has_solid_floor(env.grid(defender.pos()));
 }
 
 #ifdef DEBUG_DIAGNOSTICS