62dffe198b4f3ee454d82e9d42a5d5f7b583fba0
Nicholas Feinberg <pleasingfung@gmail.com>
2023-08-20 14:48:53 -0700

    Simplify Siphon Essence
    Death Form's Siphon Essence was originally intended to provide an
    exciting payoff for using the form in extended, where many enemies
    are nonliving and hence immune to torment. However, Death Form's
    resistances are already very exciting in extended, and it was
    surprising and counterintuitive to players that Siphon Essence's
    torment effect applied to non-tormentable foes.
    
    Make the effect match Torment by only applying to living foes. For
    now, the melee 'blight' Slow and Weak status effects can still apply
    to non-nonliving enemies, but that may change.

--- crawl-ref/source/spl-damage.cc
+++ crawl-ref/source/spl-damage.cc
@@ -4495,7 +4495,7 @@ int siphon_essence_range() { return 2; }
 bool siphon_essence_affects(const monster &m)
 {
     return !m.wont_attack()
-        && !(m.holiness() & MH_NONLIVING)
+        && !m.res_torment()
         && !mons_is_conjured(m.type) // redundant?
         && !mons_is_tentacle_or_tentacle_segment(m.type); // dubious
         // intentionally allowing firewood, i guess..?
