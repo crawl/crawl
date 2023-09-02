39726597b9d330587d631f323ca564a3be4d8024
Nicholas Feinberg <pleasingfung@gmail.com>
2023-09-02 14:07:45 -0700

    Give Flux talismans a glow-up (Sastreii)
    New item tile.

--- crawl-ref/source/tilepick.cc
+++ crawl-ref/source/tilepick.cc
@@ -2726,6 +2726,7 @@ static tileidx_t _tileidx_talisman(const item_def &item)
     switch (item.sub_type)
     {
     case TALISMAN_BEAST:    return TILE_TALISMAN_BEAST;
+    case TALISMAN_FLUX:    return TILE_TALISMAN_FLUX;
     case TALISMAN_SERPENT:  return TILE_TALISMAN_SNAKE;
     case TALISMAN_MAW:      return TILE_TALISMAN_MAW;
     case TALISMAN_BLADE:    return TILE_TALISMAN_BLADE;
@@ -2733,7 +2734,6 @@ static tileidx_t _tileidx_talisman(const item_def &item)
     case TALISMAN_DRAGON:   return TILE_TALISMAN_DRAGON;
     case TALISMAN_STORM:    return TILE_TALISMAN_STORM;
     case TALISMAN_DEATH:    return TILE_TALISMAN_DEATH;
-    case TALISMAN_FLUX:
     default: return TILE_ERROR;
     }
 }
