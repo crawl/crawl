c7603755dd3771a057496df5faf65eac721f999c
elliptic <hyperelliptical@gmail.com>
2023-09-09 15:34:16 -0400

    Nerf unwielding (0.3 -> 0.5).
    Other weapon swaps (wielding, switching from one weapon to another) take 0.5.
    This is a nerf to repeatedly wielding and unwielding distortion in an
    emergency, but that doesn't seem like enough of a reason to have a weird
    duration on this action.

--- crawl-ref/source/item-use.cc
+++ crawl-ref/source/item-use.cc
@@ -1545,13 +1545,10 @@ static bool _do_wield_weapon(item_def *to_wield, bool adjust_time_taken)
 #endif
             canned_msg(MSG_EMPTY_HANDED_NOW);
 
-            // Switching to bare hands is extra fast.
+            // Switching to bare hands is the same speed as other weapon swaps.
             you.turn_is_over = true;
             if (adjust_time_taken)
-            {
-                you.time_taken *= 3;
-                you.time_taken /= 10;
-            }
+                you.time_taken /= 2;
         }
         else
             canned_msg(MSG_EMPTY_HANDED_ALREADY);
