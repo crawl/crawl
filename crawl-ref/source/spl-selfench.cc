11e2366a1b5c4c43146480de3b956c42d2094196
Monkooky <buisinessmonkey@gmail.com>
2023-09-10 09:41:27 -0500

    Adjust increase duration, and apply a cap to jinxbite will-
    Removes a behaviour where increase duration could lower durations when passed a cap.
    
    Grants jinxbite a cap on lowered will duration

--- crawl-ref/source/spl-selfench.cc
+++ crawl-ref/source/spl-selfench.cc
@@ -253,9 +253,10 @@ spret cast_jinxbite(int pow, bool fail)
          you.duration[DUR_JINXBITE] ? "more" : "some");
 
     int dur = random_range(9, 15) + div_rand_round(pow, 4);
+    int max = 15 + div_rand_round(pow, 4);
 
     you.increase_duration(DUR_JINXBITE, dur);
-    you.increase_duration(DUR_LOWERED_WL, dur * 2, 0, "You feel your willpower being sapped.");
+    you.increase_duration(DUR_LOWERED_WL, dur * 2, max * 2, "You feel your willpower being sapped.");
 
     return spret::success;
 }
