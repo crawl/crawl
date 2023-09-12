crawl/crawl

commit cb44df74be13a448663c7820f163aac4c4ef74a6

Tweak Marshlight appearance
Make will-o-the-wisps' foxfires match their colouring, and tweak
messaging. This should hopefully make players more scared of them.
(At least in tiles, sorry!)

Some argument for giving the marshlights a different name and/or a
fully separate monster type.

This may break save compat.

crawl-ref/source/tilepick.cc
@@ -1852,7 +1852,7 @@ static tileidx_t _tileidx_monster_no_props(const monster_info& mon)
         return _tileidx_monster_zombified(mon);
 
     if (mon.props.exists(MONSTER_TILE_KEY))
-        return mon.props[MONSTER_TILE_KEY].get_short();
+        return mon.props[MONSTER_TILE_KEY].get_int();
 
     int tile_num = 0;
     if (mon.props.exists(TILE_NUM_KEY))

crawl-ref/source/dungeon.cc
@@ -5275,7 +5275,7 @@ monster* dgn_place_monster(mons_spec &mspec, coord_def where,
     if (mspec.props.exists(MONSTER_TILE_KEY))
     {
         mons->props[MONSTER_TILE_KEY] =
-            mspec.props[MONSTER_TILE_KEY].get_short();
+            mspec.props[MONSTER_TILE_KEY].get_int();
     }
     if (mspec.props.exists(MONSTER_TILE_NAME_KEY))
     {

crawl-ref/source/mapdef.cc
@@ -4068,7 +4068,7 @@ mons_list::mons_spec_slot mons_list::parse_mons_spec(string spec)
             }
             // Store name along with the tile.
             mspec.props[MONSTER_TILE_NAME_KEY].get_string() = tile;
-            mspec.props[MONSTER_TILE_KEY] = short(index);
+            mspec.props[MONSTER_TILE_KEY] = int(index);
         }
 
         string dbname = strip_tag_prefix(mon_str, "dbname:");

crawl-ref/source/mon-cast.cc
@@ -4959,7 +4959,7 @@ static void _branch_summon_helper(monster* mons, spell_type spell_cast)
 static void _cast_marshlight(monster &mons, mon_spell_slot, bolt&)
 {
     const int pow = mons_spellpower(mons, SPELL_MARSHLIGHT);
-    cast_foxfire(mons, pow, GOD_NO_GOD, false);
+    cast_foxfire(mons, pow, GOD_NO_GOD, false, true);
 }
 
 static void _cast_flay(monster &caster, mon_spell_slot, bolt&)

crawl-ref/source/spl-summoning.cc
@@ -2593,7 +2593,7 @@ void kiku_unearth_wretches()
 }
 
 static bool _create_foxfire(const actor &agent, coord_def pos,
-                            god_type god, int pow)
+                            god_type god, int pow, bool marshlight = false)
 {
     const auto att = agent.is_player() ? BEH_FRIENDLY
                                        : SAME_ATTITUDE(agent.as_monster());
@@ -2612,6 +2612,8 @@ static bool _create_foxfire(const actor &agent, coord_def pos,
 
     foxfire->add_ench(ENCH_SHORT_LIVED);
     foxfire->steps_remaining = you.current_vision + 2;
+    if (marshlight)
+        foxfire->props[MONSTER_TILE_KEY] = TILEP_MONS_MARSHLIGHT;
 
     // Avoid foxfire without targets always moving towards (0,0)
     if (!foxfire->get_foe()
@@ -2622,7 +2624,7 @@ static bool _create_foxfire(const actor &agent, coord_def pos,
     return true;
 }
 
-spret cast_foxfire(actor &agent, int pow, god_type god, bool fail)
+spret cast_foxfire(actor &agent, int pow, god_type god, bool fail, bool marshlight)
 {
     bool see_space = false;
     for (adjacent_iterator ai(agent.pos()); ai; ++ai)
@@ -2647,7 +2649,7 @@ spret cast_foxfire(actor &agent, int pow, god_type god, bool fail)
 
     for (fair_adjacent_iterator ai(agent.pos()); ai; ++ai)
     {
-        if (!_create_foxfire(agent, *ai, god, pow))
+        if (!_create_foxfire(agent, *ai, god, pow, marshlight))
             continue;
         ++created;
         if (created == 2)
@@ -2656,9 +2658,10 @@ spret cast_foxfire(actor &agent, int pow, god_type god, bool fail)
 
     if (created && you.see_cell(agent.pos()))
     {
-        mprf("%s conjure%s some foxfire!",
+        mprf("%s conjure%s some %s!",
              agent.name(DESC_THE).c_str(),
-             agent.is_monster() ? "s" : "");
+             agent.is_monster() ? "s" : "",
+             marshlight ? "marshlight" : "foxfire");
     }
     else if (agent.is_player())
         canned_msg(MSG_NOTHING_HAPPENS);

crawl-ref/source/spl-summoning.h
@@ -102,7 +102,8 @@ spret fedhas_grow_oklob(const coord_def& target, bool fail);
 
 void kiku_unearth_wretches();
 
-spret cast_foxfire(actor &agent, int pow, god_type god, bool fail);
+spret cast_foxfire(actor &agent, int pow, god_type god, bool fail,
+                   bool marshlight = false);
 spret foxfire_swarm();
 bool summon_spider(const actor &agent, coord_def pos, god_type god,
                         spell_type spell, int pow);

crawl-ref/source/tags.cc
@@ -7166,7 +7166,7 @@ void unmarshallMonster(reader &th, monster& m)
                 m.props.erase(MONSTER_TILE_KEY);
         }
         else // Update monster tile.
-            m.props[MONSTER_TILE_KEY] = short(index);
+            m.props[MONSTER_TILE_KEY] = int(index);
     }
 
 #if TAG_MAJOR_VERSION == 34

commit be3b949103aec40a331ffba86029e046962bd761

Fix save compat?
Very unclear to me what's going on here. (This was caused by
cb44df74be13a44.)

crawl-ref/source/tilepick.cc
@@ -1852,7 +1852,14 @@ static tileidx_t _tileidx_monster_no_props(const monster_info& mon)
         return _tileidx_monster_zombified(mon);
 
     if (mon.props.exists(MONSTER_TILE_KEY))
+    {
+#if TAG_MAJOR_VERSION == 34
+        // XXX: how can this happen? Can we fix this in tags.cc?
+        if (mon.props[MONSTER_TILE_KEY].get_type() == SV_SHORT)
+            return mon.props[MONSTER_TILE_KEY].get_short();
+#endif
         return mon.props[MONSTER_TILE_KEY].get_int();
+    }
 
     int tile_num = 0;
     if (mon.props.exists(TILE_NUM_KEY))