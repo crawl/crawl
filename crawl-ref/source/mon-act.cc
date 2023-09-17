6a5aa610a2447b2f902487ccca93687088902949
Nicholas Feinberg <pleasingfung@gmail.com>
2023-09-17 11:51:25 -0700

    Give boulders a flarhgunnstow (elliptic)
    Experimentally, to push Brom's Barrelling Boulder toward an appropriate
    power level for a start-book spell, add a 1/3 chance per move of the
    boulder wobbling to one side while rolling. This makes it fairly
    prone to hitting the sides of corridors, for example.

--- crawl-ref/source/mon-act.cc
+++ crawl-ref/source/mon-act.cc
@@ -993,12 +993,28 @@ static void _setup_boulder_explosion(monster& boulder, bolt& beam)
     beam.damage       = boulder_damage(pow, true);
 }
 
+static coord_def _wobble_dir(coord_def dir)
+{
+    // Computer, can I get a boulder wobble?
+    if (dir.x && dir.y)
+    {
+        if (coinflip())
+            return coord_def(dir.x, 0);
+        return coord_def(0, dir.y);
+    }
+    if (dir.y)
+        return coord_def(coinflip() ? 1 : -1, dir.y);
+    return coord_def(dir.x, coinflip() ? 1 : -1);
+}
+
 static void _handle_boulder_movement(monster& boulder)
 {
     place_cloud(CLOUD_DUST, boulder.pos(), 2 + random2(3), &boulder);
 
     // First, find out where we intend to move next
     coord_def dir = boulder.props[BOULDER_DIRECTION_KEY].get_coord();
+    if (one_chance_in(3))
+        dir = _wobble_dir(dir);
     coord_def targ = boulder.pos() + dir;
 
     // If our summoner is the player, and they cannot see us, silently crumble
@@ -1015,7 +1031,7 @@ static void _handle_boulder_movement(monster& boulder)
     {
         if (you.can_see(boulder))
         {
-            mprf("%s slams into the %s and explodes in a flurry of rock fragments!",
+            mprf("%s slams into a %s and explodes!",
                  boulder.name(DESC_THE).c_str(),
                  feat_type_name(env.grid(targ)));
         }
