8ad8ed16fb755595de53cde02a0e1ce483916ac8
Implojin <implosionwow@gmail.com>
2023-09-25 01:16:23 -0500

    fix: Don't steal mons throwables with Tukimas (Darby)
    Previously, it was possible to pull throwables out of monster inventories by
    casting Tukima's Dance at them, iff they were wielding a ranged weapon.
    
    This behavior seems to date back to when launchers had ammo and seems buggy;
    I think we're ok to just remove it.

--- crawl-ref/source/spl-summoning.cc
+++ crawl-ref/source/spl-summoning.cc
@@ -758,19 +758,6 @@ static void _animate_weapon(int pow, actor* target)
     montarget->unequip(*(montarget->mslot_item(wp_slot)), false, true);
     montarget->inv[wp_slot] = NON_ITEM;
 
-    // Also steal ammo for launchers.
-    if (is_range_weapon(*wpn))
-    {
-        const int ammo = montarget->inv[MSLOT_MISSILE];
-        if (ammo != NON_ITEM)
-        {
-            ASSERT(mons->inv[MSLOT_MISSILE] == NON_ITEM);
-            mons->inv[MSLOT_MISSILE] = ammo;
-            montarget->inv[MSLOT_MISSILE] = NON_ITEM;
-            env.item[ammo].set_holding_monster(*mons);
-        }
-    }
-
     // Find out what our god thinks before killing the item.
     conduct_type why = god_hates_item_handling(*wpn);
 
