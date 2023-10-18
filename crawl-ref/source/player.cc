crawl/crawl

commit 902d8d2af8465b7100e5316c3aed4c799b10dcde

Don't give accuracy to Me foes (elliptic, acrobat)
Make flux form and meteoran backlighting work the same way as
self-halo (from TSO, orb of light, etc), by not providing an accuracy
bonus to foes. It still disables invisibility and ruins stealth.

Also, remove the flux form EV bonus added to compensate for this.

This reverts commit 7f215ca21b984ff96d35925c7007df7b865050c2.

crawl-ref/source/player.cc
@@ -7203,8 +7203,8 @@ bool player::backlit(bool self_halo, bool temp) const
                     || duration[DUR_QUAD_DAMAGE]
                     || !umbraed() && haloed()
                        && (self_halo || halo_radius() == -1))
-           || you.has_mutation(MUT_GLOWING)
-           || you.form == transformation::flux;
+           || self_halo && (you.has_mutation(MUT_GLOWING)
+                            || you.form == transformation::flux);
 }
 
 bool player::umbra() const

crawl-ref/source/dat/species/meteoran.yaml
@@ -19,10 +19,10 @@ aptitudes:
   staves: 2
   ranged weapons: 1
   throwing: 2
-  armour: 2
-  dodging: 2
+  armour: 1
+  dodging: 1
   stealth: -2
-  shields: 2
+  shields: 1
   unarmed_combat: 2
   spellcasting: 2
   conjurations: 2

crawl-ref/source/transform.cc
@@ -604,14 +604,9 @@ private:
 public:
     static const FormFlux &instance() { static FormFlux inst; return inst; }
 
-    int contam_dam(bool random = true, bool max = false) const override
+    int contam_dam(bool random = true, bool max = false) const
     {
         return divided_scaling(FormScaling().Base(30).Scaling(20), random, max, 100);
-    }
-
-    int ev_bonus(bool max) const override
-    {
-        return scaling_value(FormScaling().Base(4).Scaling(4), false, max);
     }
 };
 

commit 84ea7605eb8ff50413b9cc662d26088f5f10e708

Mark !invis useless again when perma-backlit (CarefulOdds, elliptic)
This was broken by 902d8d2af84.

!invis is now marked useless in flux form. This is not really ideal
(autopickup should really take it anyway) - someone should fix it.

crawl-ref/source/player.cc
@@ -4792,7 +4792,7 @@ bool invis_allowed(bool quiet, string *fail_reason, bool temp)
     string msg;
     bool success = true;
 
-    if (you.backlit(false, false))
+    if (you.backlit(true, false))
     {
         msg = "Your body glows too brightly to become invisible.";
         success = false;
@@ -4823,7 +4823,7 @@ bool invis_allowed(bool quiet, string *fail_reason, bool temp)
             success = false;
         }
     }
-    else if (you.backlit(false, temp))
+    else if (you.backlit(true, temp))
     {
         msg = "You are backlit; invisibility will do you no good right now";
         if (quiet)
@@ -7241,6 +7241,9 @@ bool player::backlit(bool self_halo, bool temp) const
                        && (self_halo || halo_radius() == -1))
            || self_halo && (you.has_mutation(MUT_GLOWING)
                             || you.form == transformation::flux);
+    // TODO: find some way to mark !invis for autopickup while
+    // fluxing while still marking it temp-useless (and while
+    // marking it perma-useless for meteors)
 }
 
 bool player::umbra() const