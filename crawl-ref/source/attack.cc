crawl/crawl

commit 9c3d1c461af9f556f8621018d14cb0a2a1dce9a6

Make Trog block pain brand
Rather than forbidding it, make Trog disable pain brand while
worshipped, as if the weapon was brandless. This should avoid some
random annoyances when e.g. finding an early enchanted pain weapon.

crawl-ref/source/attack.cc
@@ -1451,7 +1451,8 @@ bool attack::apply_damage_brand(const char *what)
         break;
     }
     case SPWPN_PAIN:
-        pain_affects_defender();
+        if (!you_worship(GOD_TROG))
+            pain_affects_defender();
         break;
 
     case SPWPN_DISTORTION:

crawl-ref/source/describe.cc
@@ -1239,6 +1239,10 @@ static string _desc_attack_delay(const item_def &item)
 static string _describe_brand(brand_type brand)
 {
     switch (brand) {
+    case SPWPN_PAIN:
+        if (you_worship(GOD_TROG))
+            return "";
+        // fallthrough to description
     case SPWPN_ACID:
     case SPWPN_CHAOS:
     case SPWPN_DISTORTION:
@@ -1246,7 +1250,6 @@ static string _describe_brand(brand_type brand)
     case SPWPN_ELECTROCUTION:
     case SPWPN_FLAMING:
     case SPWPN_FREEZING:
-    case SPWPN_PAIN:
     case SPWPN_VENOM:
     {
         const string brand_name = uppercase_first(brand_type_name(brand, true));
@@ -1582,6 +1585,8 @@ static string _describe_weapon_brand(const item_def &item)
         {
             string desc = "In the hands of one skilled in necromantic "
                  "magic, it inflicts extra damage on living creatures.";
+            if (you_worship(GOD_TROG))
+                return desc + " Trog prevents you from unleashing this effect.";
             if (!is_useless_skill(SK_NECROMANCY))
                 return desc;
             return desc + " Your inability to study Necromancy prevents "

crawl-ref/source/god-item.cc
@@ -305,8 +305,7 @@ bool is_hasty_item(const item_def& item, bool calc_unid)
 bool is_wizardly_item(const item_def& item, bool calc_unid)
 {
     if ((calc_unid || item_brand_known(item))
-        && (get_weapon_brand(item) == SPWPN_PAIN
-           || get_armour_ego_type(item) == SPARM_ENERGY))
+        && get_armour_ego_type(item) == SPARM_ENERGY)
     {
         return true;
     }

crawl-ref/source/player-equip.cc
@@ -533,7 +533,12 @@ static void _equip_weapon_effect(item_def& item, bool showMsgs, bool unmeld)
                 case SPWPN_PAIN:
                 {
                     const string your_arm = you.arm_name(false);
-                    if (you.skill(SK_NECROMANCY) == 0)
+                    if (you_worship(GOD_TROG))
+                    {
+                        mprf(MSGCH_GOD, "Trog suppresses %s necromantic effect.",
+                             apostrophise(item_name).c_str());
+                    }
+                    else if (you.skill(SK_NECROMANCY) == 0)
                         mpr("You have a feeling of ineptitude.");
                     else if (you.skill(SK_NECROMANCY) <= 6)
                     {

commit 1d0c90dd83da9e5c800cc986f08e1816a70ec2b3

Fix pain ego with Trog (Implojin)
Don't block monsters from using pain ego if the player worships Trog
(broken in 9c3d1c461af). Do block monsters which worship Trog from
using pain ego.

Goodbug.

crawl-ref/source/attack.cc
@@ -1452,8 +1452,13 @@ bool attack::apply_damage_brand(const char *what)
         break;
     }
     case SPWPN_PAIN:
-        if (!you_worship(GOD_TROG))
-            pain_affects_defender();
+        if (attacker->is_player() && you_worship(GOD_TROG)
+            || !attacker->is_player() && attacker->as_monster()->god == GOD_TROG)
+        {
+            break;
+        }
+
+        pain_affects_defender();
         break;
 
     case SPWPN_DISTORTION: