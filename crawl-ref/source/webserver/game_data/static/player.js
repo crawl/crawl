b336c40ecfa40ebc62dbc37cb2261bfd5f112e1a
Rytis Petronis <petronisrytis@gmail.com>
2023-11-25 17:44:13 +0200

    change the else if to else to restore regular behavior

--- crawl-ref/source/webserver/game_data/static/player.js
+++ crawl-ref/source/webserver/game_data/static/player.js
@@ -158,7 +158,7 @@ function ($, comm, client, enums, map_knowledge, messages, options, util) {
             return String.fromCharCode("a".charCodeAt(0) + index);
         else if (index < 52)
             return String.fromCharCode("A".charCodeAt(0) + index - 26);
-        else if (index < 62)
+        else
             return String.fromCharCode("0".charCodeAt(0) + index - 52);
     }
     player.index_to_letter = index_to_letter;
