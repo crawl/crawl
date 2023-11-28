crawl/crawl

commit 750b6846b9c0d0aaa30a239c7b29dc46c16a5eca

feat: unusual monster item threat highlighting
This commit adds a new game option: unusual_monster_items.

When set, this option highlights monsters carrying 'unusual' items,
similar to the existing tiles highlighting for 'nasty' monsters.

This is a list option that matches regexes against each monster's inventory,
so players can customize this in a very granular way. You could, for example,
highlight any monster carrying snakebite.

This works in both tiles and console through a pair of associated options,
tile_show_threat_levels = unusual , and unusual_highlight = hi:magenta .
Please see options_guide.txt for details.

I've setup the default highlighting here based on what felt reasonable to me
in playtesting: Future adjustments likely, feedback welcome.

crawl-ref/source/mon-info.cc
@@ -1395,6 +1395,11 @@ void monster_info::to_string(int count, string& desc, int& desc_colour,
         colour_type = MLC_NEUTRAL;
         break;
     case ATT_HOSTILE:
+        if (has_unusual_items())
+        {
+            colour_type = MLC_UNUSUAL;
+            break;
+        }
         switch (threat)
         {
         case MTHRT_TRIVIAL: colour_type = MLC_TRIVIAL; break;
@@ -1650,6 +1655,39 @@ bool monster_info::fellow_slime() const {
     return attitude == ATT_GOOD_NEUTRAL
         && have_passive(passive_t::neutral_slimes)
         && mons_class_is_slime(type);
+}
+
+vector<string> monster_info::get_unusual_items() const
+{
+    vector<string> names;
+    const auto &patterns = Options.unusual_monster_items;
+
+    for (unsigned i = 0; i <= MSLOT_LAST_VISIBLE_SLOT; ++i)
+    {
+        if (!inv[i])
+            continue;
+
+        const item_def* item = inv[i].get();
+        const string name = item->name(DESC_A, false, false, true, false);
+        const brand_type brand = get_weapon_brand(*item);
+
+        if (any_of(begin(patterns), end(patterns),
+                   [&](const text_pattern &p) -> bool
+                   { return p.matches(name)
+                            || p.matches("artefact") && is_artefact(*item)
+                            || p.matches("vulnerable")
+                               && you.is_brand_vulnerable(brand); }))
+        {
+            names.push_back(name);
+        }
+    }
+
+    return names;
+}
+
+bool monster_info::has_unusual_items() const
+{
+    return attitude == ATT_HOSTILE && !get_unusual_items().empty();
 }
 
 // Only checks for spells from preset monster spellbooks.

crawl-ref/source/view.cc
@@ -277,6 +277,8 @@ static bool _is_mon_equipment_worth_listing(const monster_info &mi)
         return true;
     if (mi.inv[MSLOT_WAND])
         return true;
+    if (mi.has_unusual_items())
+        return true;
 
     return _is_item_worth_listing(mi.inv[MSLOT_SHIELD])
         || _is_item_worth_listing(mi.inv[MSLOT_ARMOUR])

crawl-ref/source/defines.h
@@ -234,6 +234,7 @@ typedef uint8_t colour_t;
 
 #define COLFLAG_FRIENDLY_MONSTER         0x0100
 #define COLFLAG_NEUTRAL_MONSTER          0x0200
+#define COLFLAG_UNUSUAL_MASK             0x0300
 #define COLFLAG_WILLSTAB                 0x0400
 #define COLFLAG_MAYSTAB                  0x0800
 #define COLFLAG_ITEM_HEAP                0x1000

crawl-ref/source/describe.cc
@@ -5308,6 +5308,21 @@ static string _monster_stat_description(const monster_info& mi, bool mark_spells
                << _get_threat_desc(mi.threat) << ".\n";
     }
 
+    if (mi.has_unusual_items())
+    {
+        const vector<string> unusual_items = mi.get_unusual_items();
+
+        result << uppercase_first(pronoun) << " ";
+        (!mons_class_is_animated_weapon(mi.type) ?
+            result << conjugate_verb("have", plural)
+                   << " an unusual item: "
+                   << comma_separated_line(unusual_items.begin(),
+                                           unusual_items.end()) :
+            result << conjugate_verb("are", plural)
+                   << " an unusual item")
+               << ".\n";
+    }
+
     if (!resist_descriptions.empty())
     {
         result << uppercase_first(pronoun) << " "

crawl-ref/source/initfile.cc
@@ -149,7 +149,7 @@ static monster_list_colour_type _str_to_mlc(const string &s)
     static const char * const _monster_list_colour_names[NUM_MLC] =
     {
         "friendly", "neutral", "good_neutral",
-        "trivial", "easy", "tough", "nasty",
+        "trivial", "easy", "tough", "nasty", "unusual",
     };
     for (int i = 0; i < NUM_MLC; ++i)
         if (s == _monster_list_colour_names[i])
@@ -542,6 +542,9 @@ const vector<GameOption*> game_options::build_options_list()
             false
 #endif
             ),
+
+        new ListGameOption<text_pattern>(SIMPLE_NAME(unusual_monster_items), {}, true),
+
         new BoolGameOption(SIMPLE_NAME(arena_dump_msgs), false),
         new BoolGameOption(SIMPLE_NAME(arena_dump_msgs_all), false),
         new BoolGameOption(SIMPLE_NAME(arena_list_eq), false),
@@ -570,6 +573,8 @@ const vector<GameOption*> game_options::build_options_list()
                              CHATTR_HILITE | (GREEN << 8)),
         new CursesGameOption(SIMPLE_NAME(neutral_highlight),
                              CHATTR_HILITE | (LIGHTGREY << 8)),
+        new CursesGameOption(SIMPLE_NAME(unusual_highlight),
+                             CHATTR_HILITE | (MAGENTA << 8)),
         new CursesGameOption(SIMPLE_NAME(stab_highlight),
                              CHATTR_HILITE | (BLUE << 8)),
         new CursesGameOption(SIMPLE_NAME(may_stab_highlight),
@@ -660,7 +665,8 @@ const vector<GameOption*> game_options::build_options_list()
              {MLC_TRIVIAL, DARKGREY},
              {MLC_EASY, LIGHTGREY},
              {MLC_TOUGH, YELLOW},
-             {MLC_NASTY, LIGHTRED}
+             {MLC_NASTY, LIGHTRED},
+             {MLC_UNUSUAL, LIGHTMAGENTA}
             },
             false,
             [this]()
@@ -781,7 +787,7 @@ const vector<GameOption*> game_options::build_options_list()
         new BoolGameOption(SIMPLE_NAME(tile_show_minimagicbar), true),
         new BoolGameOption(SIMPLE_NAME(tile_show_demon_tier), false),
         new BoolGameOption(SIMPLE_NAME(tile_grinch), false),
-        new StringGameOption(SIMPLE_NAME(tile_show_threat_levels), "nasty"),
+        new StringGameOption(SIMPLE_NAME(tile_show_threat_levels), "nasty, unusual"),
         new StringGameOption(SIMPLE_NAME(tile_show_items), "!?/=([)}:|"),
         // disabled by default due to performance issues
         new BoolGameOption(SIMPLE_NAME(tile_water_anim), !USING_WEB_TILES),
@@ -2568,6 +2574,7 @@ void game_options::reset_aliases(bool clear)
     // Backwards compatibility:
     Options.add_alias("friend_brand", "friend_highlight");
     Options.add_alias("neutral_brand", "neutral_highlight");
+    Options.add_alias("unusual_brand", "unusual_highlight");
     Options.add_alias("stab_brand", "stab_highlight");
     Options.add_alias("may_stab_brand", "may_stab_highlight");
     Options.add_alias("heap_brand", "heap_highlight");

crawl-ref/source/libunix.cc
@@ -1094,7 +1094,9 @@ bool is_cursor_enabled()
 
 static inline unsigned get_highlight(int col)
 {
-    return (col & COLFLAG_FRIENDLY_MONSTER) ? Options.friend_highlight :
+    return ((col & COLFLAG_UNUSUAL_MASK) == COLFLAG_UNUSUAL_MASK) ?
+                                              Options.unusual_highlight :
+           (col & COLFLAG_FRIENDLY_MONSTER) ? Options.friend_highlight :
            (col & COLFLAG_NEUTRAL_MONSTER)  ? Options.neutral_highlight :
            (col & COLFLAG_ITEM_HEAP)        ? Options.heap_highlight :
            (col & COLFLAG_WILLSTAB)         ? Options.stab_highlight :

crawl-ref/source/libw32c.cc
@@ -616,7 +616,9 @@ static unsigned short _dos_highlight(unsigned short colour, unsigned highlight)
 // XX code duplication
 static inline unsigned get_highlight(int col)
 {
-    return (col & COLFLAG_FRIENDLY_MONSTER) ? Options.friend_highlight :
+    return ((col & COLFLAG_UNUSUAL_MASK) == COLFLAG_UNUSUAL_MASK) ?
+                                              Options.unusual_highlight :
+           (col & COLFLAG_FRIENDLY_MONSTER) ? Options.friend_highlight :
            (col & COLFLAG_NEUTRAL_MONSTER)  ? Options.neutral_highlight :
            (col & COLFLAG_ITEM_HEAP)        ? Options.heap_highlight :
            (col & COLFLAG_WILLSTAB)         ? Options.stab_highlight :

crawl-ref/source/menu.cc
@@ -2403,6 +2403,9 @@ bool MonsterMenuEntry::get_tiles(vector<tile_def>& tileset) const
         tileset.emplace_back(TILE_HALO_GD_NEUTRAL);
     else if (m->neutral())
         tileset.emplace_back(TILE_HALO_NEUTRAL);
+    else if (Options.tile_show_threat_levels.find("unusual") != string::npos
+             && m->has_unusual_items())
+        tileset.emplace_back(TILE_THREAT_UNUSUAL);
     else
         switch (m->threat)
         {

commit e172a6e21a281f86f540f599892ab073e1703803

fix: refactor some unusual items monster messaging
750b6846b9 introduced a bug that could cause monster bands to come into view
with messages like "A gnoll is ." if one of the mons in the band was carrying
an unusual item that was neither branded nor artefact, like a throwing net.

Here I've refactored and unified the relevant code to be able to handle
messaging for arbitrary unusual items, but `get_monster_equipment_desc`
is still pretty fragile, and has had many similar messaging bugs over the
years (e.g. 7046ed1b37, f44d0c5ad2, 1e14066ec4).

Fingers crossed that this doesn't break anything else.

crawl-ref/source/directn.cc
@@ -3843,8 +3843,8 @@ string get_monster_equipment_desc(const monster_info& mi,
     item_def* mon_wnd = mi.inv[MSLOT_WAND].get();
     item_def* mon_rng = mi.inv[MSLOT_JEWELLERY].get();
 
-#define uninteresting(x) (x && !item_is_branded(*x) && !is_artefact(*x))
-    // For "comes into view" msgs, only care about branded stuff and artefacts
+#define uninteresting(x) (x && !item_is_worth_listing(*x))
+    // For "comes into view" msgs, only list interesting items
     if (level == DESC_IDENTIFIED)
     {
         if (uninteresting(mon_arm))

crawl-ref/source/items.cc
@@ -772,6 +772,34 @@ bool item_is_branded(const item_def& item)
     }
 }
 
+bool item_is_unusual(const item_def& item)
+{
+    const auto &patterns = Options.unusual_monster_items;
+    const string name = item.name(DESC_A, false, false, true, false);
+
+    return any_of(begin(patterns), end(patterns),
+                  [&](const text_pattern &p) -> bool
+                  { return p.matches(name); });
+}
+
+bool item_is_worth_listing(const item_def& item)
+{
+    if (item_is_unusual(item))
+        return true;
+
+    switch (item.base_type)
+    {
+    case OBJ_STAVES:
+    case OBJ_WANDS:
+        return true;
+    case OBJ_WEAPONS:
+        return is_unrandom_artefact(item)
+               || get_weapon_brand(item) != SPWPN_NORMAL;
+    default:
+        return item_is_branded(item) || is_artefact(item);
+    }
+}
+
 // 2 - artefact, 1 - glowing/runed, 0 - mundane
 static int _item_name_specialness(const item_def& item)
 {

crawl-ref/source/items.h
@@ -97,6 +97,8 @@ void pickup_menu(int item_link);
 void pickup(bool partial_quantity = false);
 
 bool item_is_branded(const item_def& item);
+bool item_is_unusual(const item_def& item);
+bool item_is_worth_listing(const item_def& item);
 vector<item_def*> item_list_on_square(int obj);
 vector<const item_def*> const_item_list_on_square(int obj);
 

crawl-ref/source/mon-info.cc
@@ -24,6 +24,7 @@
 #include "god-passive.h" // passive_t::neutral_slimes
 #include "item-prop.h"
 #include "item-status-flag-type.h"
+#include "items.h" // item_is_unusual
 #include "libutil.h"
 #include "los.h"
 #include "message.h"
@@ -1677,24 +1678,15 @@ bool monster_info::fellow_slime() const {
 vector<string> monster_info::get_unusual_items() const
 {
     vector<string> names;
-    const auto &patterns = Options.unusual_monster_items;
-
-    for (unsigned i = 0; i <= MSLOT_LAST_VISIBLE_SLOT; ++i)
+    for (unsigned int i = 0; i <= MSLOT_LAST_VISIBLE_SLOT; ++i)
     {
         if (!inv[i])
             continue;
 
         const item_def* item = inv[i].get();
-        const string name = item->name(DESC_A, false, false, true, false);
-
-        if (any_of(begin(patterns), end(patterns),
-                   [&](const text_pattern &p) -> bool
-                   { return p.matches(name); }))
-        {
-            names.push_back(name);
-        }
+        if (item_is_unusual(*item))
+            names.push_back(item->name(DESC_A, false, false, true, false));
     }
-
     return names;
 }
 

crawl-ref/source/view.cc
@@ -250,40 +250,17 @@ static void _genus_factoring(map<const string, details> &types,
     types[name] = {mon, name, num, true};
 }
 
-static bool _is_weapon_worth_listing(const unique_ptr<item_def> &wpn)
-{
-    return wpn && (wpn->base_type == OBJ_STAVES
-                   || is_unrandom_artefact(*wpn.get())
-                   || get_weapon_brand(*wpn.get()) != SPWPN_NORMAL);
-}
-
-static bool _is_item_worth_listing(const unique_ptr<item_def> &item)
-{
-    return item && (item_is_branded(*item.get())
-                    || is_artefact(*item.get()));
-}
-
 static bool _is_mon_equipment_worth_listing(const monster_info &mi)
 {
+    for (unsigned int i = 0; i <= MSLOT_LAST_VISIBLE_SLOT; ++i)
+    {
+        if (!mi.inv[i])
+            continue;
 
-    if (_is_weapon_worth_listing(mi.inv[MSLOT_WEAPON]))
-        return true;
-    const unique_ptr<item_def> &alt_weap = mi.inv[MSLOT_ALT_WEAPON];
-    if (mi.wields_two_weapons() && _is_weapon_worth_listing(alt_weap))
-        return true;
-    // can a wand be in the alt weapon slot? get_monster_equipment_desc seems to
-    // think so, so we'll check
-    if (alt_weap && alt_weap->base_type == OBJ_WANDS)
-        return true;
-    if (mi.inv[MSLOT_WAND])
-        return true;
-    if (mi.has_unusual_items())
-        return true;
-
-    return _is_item_worth_listing(mi.inv[MSLOT_SHIELD])
-        || _is_item_worth_listing(mi.inv[MSLOT_ARMOUR])
-        || _is_item_worth_listing(mi.inv[MSLOT_JEWELLERY])
-        || _is_item_worth_listing(mi.inv[MSLOT_MISSILE]);
+        if (item_is_worth_listing(*mi.inv[i].get()))
+            return true;
+    }
+    return false;
 }
 
 /// Return whether or not monster_info::_core_name() describes the inventory