7208ddb293eecf3a7057d9c741de0eb78cd53905
Nicholas Feinberg <pleasingfung@gmail.com>
2023-12-18 15:54:15 -0800

    Align colons
    In monster stat blocks.
    
    Boy, what a bother! :)

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -100,7 +100,7 @@ using namespace ui;
 static command_type _get_action(int key, vector<command_type> actions);
 static void _print_bar(int value, int scale, const string &name,
                        ostringstream &result, int base_value = INT_MAX);
-static string _padded(string str, int pad_to);
+static string _padded(string str, int pad_to, bool prepend = false);
 
 static void _describe_mons_to_hit(const monster_info& mi, ostringstream &result);
 static string _describe_weapon_brand(const item_def &item);
@@ -5219,10 +5219,14 @@ static int _codepoints(string str)
     return len;
 }
 
-static string _padded(string str, int pad_to)
+static string _padded(string str, int pad_to, bool prepend)
 {
     const int padding = pad_to - _codepoints(str);
-    if (padding > 0)
+    if (padding <= 0)
+        return str;
+    if (prepend)
+        str.insert(0, string(padding, ' '));
+    else
         str.append(padding, ' ');
     return str;
 }
@@ -5248,44 +5252,6 @@ static string _build_bar(int value, int scale)
 }
 
 /**
- * Returns a description of a given monster's max HP.
- *
- * @param mi[in]            Player-visible info about the monster in question.
- */
-static string _describe_monster_hp(const monster_info& mi)
-{
-    return "Max HP: " + mi.get_max_hp_desc();
-}
-
-/**
- * Returns a description of a given monster's AC.
- *
- * @param mi[in]            Player-visible info about the monster in question.
- */
-static string _describe_monster_ac(const monster_info& mi)
-{
-    return "AC: " + _build_bar(mi.ac, 5);
-}
-
-/**
- * Returns a description of a given monster's EV.
- *
- * @param mi[in]            Player-visible info about the monster in question.
- */
-static string _describe_monster_ev(const monster_info& mi)
-{
-    return "EV: " + _build_bar(mi.base_ev, 5);
-}
-
-static string _describe_monster_sh(const monster_info& mi)
-{
-    const int sh = mi.sh / 2; // rescale to match player SH
-    if (sh <= 0)
-        return "";
-    return "SH: " + _build_bar(sh, 5);
-}
-
-/**
  * Returns a description of a given monster's WL.
  *
  * @param mi[in]            Player-visible info about the monster in question.
@@ -5296,10 +5262,10 @@ static string _describe_monster_wl(const monster_info& mi)
     if (will == WILL_INVULN)
     {
         if (Options.char_set == CSET_ASCII)
-            return "Will: inf";
-        return "Will: ∞";
+            return "inf";
+        return "∞";
     }
-    return "Will: " + _build_bar(will, WL_PIP);
+    return _build_bar(will, WL_PIP);
 }
 
 /**
@@ -5423,7 +5389,7 @@ static void _add_speed_desc(const monster_info &mi, ostringstream &result)
     if (!unusual_speed && !unusual_energy && !travel_delay_diff)
         return;
 
-    result << "\nSpeed: " << speed * 10 << "%";
+    result << "Speed: " << speed * 10 << "%";
 
     vector<string> unusuals;
 
@@ -5450,7 +5416,68 @@ static void _add_speed_desc(const monster_info &mi, ostringstream &result)
         // It would be interesting to qualify this with 'on land',
         // if appropriate, but sort of annoying to get player swim speed.
     }
+
+    result << "\n";
 }
+
+struct TableCell
+{
+    string   label;
+    string   value;
+    colour_t colour;
+};
+
+class TablePrinter
+{
+private:
+    vector<vector<TableCell>> rows;
+
+public:
+    void AddRow()
+    {
+        rows.push_back({});
+    }
+
+    void AddCell(string label = "", string value = "", colour_t colour = LIGHTGREY)
+    {
+        rows[rows.size() - 1].push_back({label, value, colour});
+    }
+
+    void Print(ostringstream &result)
+    {
+        vector<int> labels_lengths_by_col;
+        for (size_t row = 0; row < rows.size(); ++row)
+        {
+            for (size_t col = 0; col < rows[row].size(); ++col)
+            {
+                const int label_len = _codepoints(rows[row][col].label);
+                if (col == labels_lengths_by_col.size())
+                    labels_lengths_by_col.push_back(label_len);
+                else
+                    labels_lengths_by_col[col] = max(labels_lengths_by_col[col], label_len);
+            }
+        }
+        const int cell_len = 80 / labels_lengths_by_col.size();
+
+        for (const auto &row : rows)
+        {
+            for (size_t col = 0; col < row.size(); ++col)
+            {
+                const TableCell &cell = row[col];
+                if (cell.label.empty())
+                    continue; // padding
+
+                const int label_len = labels_lengths_by_col[col];
+                const string label = _padded(cell.label, label_len, true);
+                const string body = make_stringf("%s: %s",
+                                                 label.c_str(),
+                                                 cell.value.c_str());
+                result << colourize_str(_padded(body, cell_len), cell.colour);
+            }
+            result << "\n";
+        }
+    }
+};
 
 // Converts a numeric resistance to its symbolic counterpart.
 // Can handle any maximum level. The default is for single level resistances
@@ -5505,19 +5532,16 @@ static string _res_name(mon_resist_flags res)
     }
 }
 
-static string _desc_mon_resist(resists_t resist_set, mon_resist_flags res)
+static void _desc_mon_resist(TablePrinter &pr,
+                             resists_t resist_set, mon_resist_flags res)
 {
     const int level = get_resist(resist_set, res);
     const int max = (res == MR_RES_POISON || res == MR_RES_ELEC) ? 1 : 3; // lies
     const string desc = desc_resist(level, max, level == 3, false);
-    const string prefixed_desc =
-        _padded(make_stringf("%s: %s", _res_name(res).c_str(), desc.c_str()), 16);
-    if (level)
-        return prefixed_desc;
-    return make_stringf("<darkgrey>%s</darkgrey>", prefixed_desc.c_str());
+    pr.AddCell(_res_name(res), desc, level ? LIGHTGREY : DARKGREY);
 }
 
-static void _add_resist_desc(resists_t resist_set, ostringstream &result)
+static void _add_resist_desc(TablePrinter &pr, resists_t resist_set)
 {
     const mon_resist_flags common_resists[] =
         { MR_RES_FIRE, MR_RES_COLD, MR_RES_POISON, MR_RES_NEG, MR_RES_ELEC };
@@ -5529,9 +5553,9 @@ static void _add_resist_desc(resists_t resist_set, ostringstream &result)
     if (!found)
         return;
 
-    result << "\n";
+    pr.AddRow();
     for (mon_resist_flags rflags : common_resists)
-        result << _desc_mon_resist(resist_set, rflags);
+        _desc_mon_resist(pr, resist_set, rflags);
 }
 
 // Describe a monster's (intrinsic) resistances, speed and a few other
@@ -5543,16 +5567,20 @@ static string _monster_stat_description(const monster_info& mi, bool mark_spells
 
     ostringstream result;
 
-    // These padding values are set to line up defenses with common resists.
-    result << _padded(_describe_monster_hp(mi), 16);  // worst case is "Max HP: ~9999"
-                                                      // len 13, then 3 spaces after
-    result << _padded(_describe_monster_wl(mi), 16);  // "Will: +++++" 11
-    result << _padded(_describe_monster_ac(mi), 16);  // "AC: ++++ ++++" 13 again
-    result << _padded(_describe_monster_ev(mi), 16);  // "EV*: ++++ ++++" 14
-    result << _describe_monster_sh(mi);
+    TablePrinter pr;
+
+    pr.AddRow();
+    pr.AddCell("Max HP", mi.get_max_hp_desc());
+    pr.AddCell("Will", _describe_monster_wl(mi));
+    pr.AddCell("AC", _build_bar(mi.ac, 5));
+    pr.AddCell("EV", _build_bar(mi.base_ev, 5));
+    if (mi.sh / 2 > 0)  // rescale to match player SH
+        pr.AddCell("SH", _build_bar(mi.sh / 2, 5));
+    else
+        pr.AddCell(); // ensure alignment
 
     const resists_t resist = mi.resists();
-    _add_resist_desc(resist, result);
+    _add_resist_desc(pr, resist);
 
     // Less important common properties. Arguably should be lower down.
     const size_type sz = mi.body_size();
@@ -5560,19 +5588,22 @@ static string _monster_stat_description(const monster_info& mi, bool mark_spells
     const auto holiness = mons_class_holiness(mi.type);
     const string holi = holiness == MH_NONLIVING ? "Nonliv."
                                                  : single_holiness_description(holiness);
-    result << "\n";
-    result << _padded(make_stringf("Threat: %s", _get_threat_desc(mi.threat)), 16);
-    result << _padded(make_stringf("Class: %s", uppercase_first(holi).c_str()), 16);
-    result << _padded(make_stringf("Size: %s", size_desc.c_str()), 16);
-    result << _padded(make_stringf("Int: %s", _get_int_desc(mi.intel())), 16);
+    pr.AddRow();
+    pr.AddCell("Threat", _get_threat_desc(mi.threat));
+    pr.AddCell("Class", uppercase_first(holi).c_str());
+    pr.AddCell("Size", size_desc.c_str());
+    pr.AddCell("Int", _get_int_desc(mi.intel()));
     if (mi.is(MB_SICK) || mi.is(MB_NO_REGEN))
-        result << "Regen: None";
+        pr.AddCell("Regen", "None");
     else if (mons_class_fast_regen(mi.type) || mi.is(MB_REGENERATION))
-        result << "Regen: " << mi.regen_rate(1) << "/turn"; // (Wait, what's a 'turn'?)
+        pr.AddCell("Regen", make_stringf("%d/turn", mi.regen_rate(1)));
+                                        // (Wait, what's a 'turn'?)
+
+    pr.Print(result);
 
     _add_speed_desc(mi, result);
 
-    result << "\n\n";
+    result << "\n";
 
     if (crawl_state.game_started)
     {
