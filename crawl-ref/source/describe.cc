dfd56e322c51b89cc19b76c92a1294c12731cac1
Nicholas Feinberg <pleasingfung@gmail.com>
2023-12-18 15:09:19 -0800

    Tweak repulsion ego description
    The old one was unclear to some, helping contribute to the perennial
    repulsion-reflection mix-up. Maybe this will help?

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -2122,7 +2122,7 @@ static const char* _item_ego_desc(special_armour_type ego)
         return "it improves its wearer's accuracy and damage with "
                "thrown weapons, such as rocks and javelins (Slay +4).";
     case SPARM_REPULSION:
-        return "it protects its wearer by repelling missiles.";
+        return "it helps its wearer evade missiles.";
 #if TAG_MAJOR_VERSION == 34
     case SPARM_CLOUD_IMMUNE:
         return "it does nothing special.";
