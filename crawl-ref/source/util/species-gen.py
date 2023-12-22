f40074dcd01f5230e4629573850765f8c7aec9a9
Nicholas Feinberg <pleasingfung@gmail.com>
2023-12-22 13:43:46 -0800

    Add kittens
    For a Hep low-piety invo title.
    
    The naming here is a little iffy - 'feline kitten' feels silly.
    Should maybe special case that.
    
    Also a bit leery of 'tengu chick' but whatever, we can roll that
    back later.

--- crawl-ref/source/util/species-gen.py
+++ crawl-ref/source/util/species-gen.py
@@ -42,7 +42,7 @@ class Species(MutableMapping):
             'undead_type', 'size', 'str', 'int', 'dex', 'levelup_stats',
             'levelup_stat_frequency', 'recommended_jobs', 'recommended_weapons',
             'difficulty', 'difficulty_priority', 'create_enum', 'walking_verb',
-            'altar_action', 'mutations'}
+            'altar_action', 'mutations', 'child_name'}
 
     def __init__(self, yaml_dict):
         self.backing_dict = dict()
@@ -148,6 +148,7 @@ class Species(MutableMapping):
                                     s.get('create_enum', False), 'create_enum')
         self['walking_verb'] = quote_or_nullptr('walking_verb', s)
         self['altar_action'] = quote_or_nullptr('altar_action', s)
+        self['child_name']   = quote_or_nullptr('child_name', s)
 
         if 'TAG_MAJOR_VERSION' in s:
             self['tag_major_version_opener'] = (
