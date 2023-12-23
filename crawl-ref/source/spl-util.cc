c72055979819dfb092cc0debdea905271eb22396
DracoOmega <draco.omega.dcss.dev@gmail.com>
2023-12-23 12:47:12 -0330

    Don't require shift+z to cast Mercury Vapours at enemies still in the AoE
    It doesn't seem an uncommon situation that the player would want to cast
    early for the Weak, so let's not make them see an extra prompt.

--- crawl-ref/source/spl-util.cc
+++ crawl-ref/source/spl-util.cc
@@ -1591,7 +1591,11 @@ bool spell_no_hostile_in_range(spell_type spell)
         }
         return true;
 
+    // Check slightly beyond our target range, in case someone wants to catch
+    // something in the AoE at the edge of range.
     case SPELL_MERCURY_VAPOURS:
+        return find_near_hostiles(range + 1, false).empty();
+
     case SPELL_SCORCH:
         return find_near_hostiles(range, false).empty();
 
