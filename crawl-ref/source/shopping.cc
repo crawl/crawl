903e00c026d529044c139298f200e36455b391cc
Nicholas Feinberg <pleasingfung@gmail.com>
2023-12-25 23:23:41 -0800

    Revert "Fix default shop ordering" (gammafunk)
    Just broke more things.
    
    This reverts commit d22453358fbc46b1444d6a61a651476891020241.

--- crawl-ref/source/shopping.cc
+++ crawl-ref/source/shopping.cc
@@ -839,10 +839,8 @@ static bool _purchase(shop_struct& shop, const level_pos& pos, int index)
 
 enum shopping_order
 {
-    ORDER_ORIGINAL,
-    // default order must be original (stock) order, or index_to_letter breaks
-    ORDER_DEFAULT = ORDER_ORIGINAL,
     ORDER_TYPE,
+    ORDER_DEFAULT = ORDER_TYPE,
     ORDER_PRICE,
     ORDER_ALPHABETICAL,
     NUM_ORDERS
@@ -850,8 +848,7 @@ enum shopping_order
 
 static const char * const shopping_order_names[NUM_ORDERS] =
 {
-    // XXX no name longer than 6 letters
-    "none", "type", "price", "name"
+    "type", "price", "name"
 };
 
 static shopping_order operator++(shopping_order &x)
@@ -1241,9 +1238,6 @@ void ShopMenu::resort()
 {
     switch (order)
     {
-    case ORDER_ORIGINAL:
-        // noop
-        break;
     case ORDER_TYPE:
     {
         const bool id = shoptype_identifies_stock(shop.type);
