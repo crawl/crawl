crawl/crawl

commit b92c8fe61fd27051bdea34e1b4cbf590aca69b95

Turn off quiver autoswitching (CarefulOdds, advil)
Switching to a new quivered item when you ran out of the thing you
were shooting made sense back when folks were juggling +2 elvish arrows
and +1 arrows of flame. These days, though, it's pretty uncommon that
the player wants to keep chucking stuff when their quiver runs empty,
and much more likely to be disastrous if e.g. the quiver switches from
boomerangs to scrolls of fear.

Let's disable this and see if anyone complains.

crawl-ref/source/items.cc
@@ -447,7 +447,7 @@ void inc_inv_item_quantity(int obj, int amount)
 
     you.inv[obj].quantity += amount;
     if (you.inv[obj].quantity == amount) // not currently possible?
-        quiver::on_actions_changed(true);
+        quiver::on_actions_changed();
 }
 
 void inc_mitm_item_quantity(int obj, int amount)
@@ -2085,7 +2085,7 @@ static int _place_item_in_free_slot(item_def &it, int quant_got,
         taken_new_item(item.base_type);
 
     you.last_pickup[item.link] = quant_got;
-    quiver::on_actions_changed(true);
+    quiver::on_actions_changed();
     item_skills(item, you.skills_to_show);
 
     if (const item_def* newitem = auto_assign_item_slot(item))

crawl-ref/source/quiver.cc
@@ -933,8 +933,7 @@ namespace quiver
             // Update the legacy quiver history data structure
             // TODO: eliminate this? History should be stored per quiver, not
             // globally
-            you.m_quiver_history.on_item_fired(you.inv[item_slot],
-                    !target.fire_context || !target.fire_context->autoswitched);
+            you.m_quiver_history.on_item_fired(you.inv[item_slot]);
         }
 
         virtual formatted_string quiver_description(bool short_desc) const override
@@ -2011,7 +2010,7 @@ namespace quiver
     }
 
     action_cycler::action_cycler(shared_ptr<action> init)
-        : autoswitched(false), current(init)
+        : current(init)
     { }
 
     // by default, initialize as invalid, not empty
@@ -2096,14 +2095,13 @@ namespace quiver
      * @param new_act the action to fill in. nullptr is safe.
      * @return whether the action changed as a result of the call.
      */
-    bool action_cycler::set(const shared_ptr<action> new_act, bool _autoswitched)
+    bool action_cycler::set(const shared_ptr<action> new_act)
     {
         auto n = new_act ? new_act : make_shared<action>();
 
         const bool diff = *n != *get();
         auto old = move(current);
         current = move(n);
-        autoswitched = _autoswitched;
 
         if (diff)
         {
@@ -2120,12 +2118,9 @@ namespace quiver
             // side effects, ugh. Update the fire history, and play a sound
             // if needed. TODO: refactor so this is less side-effect-y
             // somehow?
-            if (!autoswitched)
-            {
-                const int item_slot = get()->get_item();
-                if (item_slot >= 0 && you.inv[item_slot].defined())
-                    you.m_quiver_history.set_quiver(you.inv[item_slot]);
-            }
+            const int item_slot = get()->get_item();
+            if (item_slot >= 0 && you.inv[item_slot].defined())
+                you.m_quiver_history.set_quiver(you.inv[item_slot]);
 #ifdef USE_SOUND
             parse_sound(CHANGE_QUIVER_SOUND);
 #endif
@@ -2146,7 +2141,6 @@ namespace quiver
         // don't use regular set: avoid all the side effects when importing
         // from another action cycler. (Used in targeting.)
         current = other.get();
-        autoswitched = false;
         set_needs_redraw();
         return diff;
     }
@@ -2317,27 +2311,8 @@ namespace quiver
         return set(next(dir, allow_disabled));
     }
 
-    void action_cycler::on_actions_changed(bool check_autoswitch)
+    void action_cycler::on_actions_changed()
     {
-        if (!get()->is_valid())
-        {
-            // XX should find_replacement respect +f?
-            auto r = get()->find_replacement();
-            if (r && r->is_valid()
-                && _fireorder_inscription_ok(r->get_item(), false) != false)
-            {
-                set(r, true);
-            }
-        }
-        else if (check_autoswitch && autoswitched)
-        {
-            auto r = ammo_to_action(you.m_quiver_history.get_last_ammo());
-            if (r && r->is_valid()
-                && _fireorder_inscription_ok(r->get_item(), false) != false)
-            {
-                set(r);
-            }
-        }
         set_needs_redraw();
     }
 
@@ -3153,9 +3128,9 @@ namespace quiver
     }
 #endif
 
-    void on_actions_changed(bool check_autoswitch)
+    void on_actions_changed()
     {
-        you.quiver_action.on_actions_changed(check_autoswitch);
+        you.quiver_action.on_actions_changed();
     }
 
     void set_needs_redraw()

crawl-ref/source/quiver.h
@@ -136,13 +136,13 @@ namespace quiver
 
         shared_ptr<action> next(int dir = 0, bool allow_disabled=true) const;
 
-        virtual bool set(const shared_ptr<action> n, bool _autoswitched=false);
+        virtual bool set(const shared_ptr<action> n);
         bool set(const action_cycler &other);
         bool replace(const shared_ptr<action> new_act);
         bool set_from_slot(int slot);
         bool cycle(int dir = 0, bool allow_disabled=true);
         bool clear();
-        void on_actions_changed(bool check_autoswitch=false);
+        void on_actions_changed();
         virtual void set_needs_redraw();
         shared_ptr<action> find_last_valid();
 
@@ -150,8 +150,6 @@ namespace quiver
         shared_ptr<action> do_target();
         virtual string fire_key_hints() const;
         virtual bool targeter_handles_key(command_type c) const;
-
-        bool autoswitched;
     protected:
         action_cycler(shared_ptr<action> init);
 
@@ -162,7 +160,7 @@ namespace quiver
 
     void choose(action_cycler &cur_quiver, bool allow_empty=true);
     bool set_to_quiver(shared_ptr<quiver::action> s, action_cycler &cur_quiver);
-    void on_actions_changed(bool check_autoswitch=false);
+    void on_actions_changed();
     void on_weapon_changed();
     void on_newchar();
 

crawl-ref/source/throw.cc
@@ -441,7 +441,7 @@ void fire_item_no_quiver(dist *target)
     //   more general (though right now this generality is mostly unused).
     // * Ensure that the regular fire history isn't affected by this call.
     ammo_only_action_cycler q;
-    q.set(a, true);
+    q.set(a);
     if (target->needs_targeting())
         q.target();
     else

commit 978718064758f410bec845dbda0147ed0847073a

Autoquiver throwables on pickup again (advil)
Broken in b92c8fe61fd27051bdea34e1b4cbf590aca69b95.

Resolves #3344.

crawl-ref/source/items.cc
@@ -446,10 +446,7 @@ void inc_inv_item_quantity(int obj, int amount)
 {
     if (you.equip[EQ_WEAPON] == obj)
         you.wield_change = true;
-
     you.inv[obj].quantity += amount;
-    if (you.inv[obj].quantity == amount) // not currently possible?
-        quiver::on_actions_changed();
 }
 
 void inc_mitm_item_quantity(int obj, int amount)
@@ -2144,6 +2141,7 @@ static int _place_item_in_free_slot(item_def &it, int quant_got,
         taken_new_item(item.base_type);
 
     you.last_pickup[item.link] = quant_got;
+    quiver::on_item_pickup(freeslot);
     quiver::on_actions_changed();
     item_skills(item, you.skills_to_show);
 

crawl-ref/source/quiver.cc
@@ -2302,6 +2302,26 @@ namespace quiver
         return set(next(dir, allow_disabled));
     }
 
+    void action_cycler::on_item_pickup(int slot)
+    {
+        if (get()->is_valid()
+            || _fireorder_inscription_ok(slot, false) == false)
+        {
+            return;
+        }
+        const item_def &item = you.inv[slot];
+        for (unsigned int i_flags = 0; i_flags < Options.fire_order.size();
+             i_flags++)
+        {
+            const fire_type ftyp = (fire_type)Options.fire_order[i_flags];
+            if (_item_matches(item, ftyp, false))
+            {
+                set(make_shared<ammo_action>(slot));
+                return;
+            }
+        }
+    }
+
     void action_cycler::on_actions_changed()
     {
         set_needs_redraw();
@@ -3122,6 +3142,11 @@ namespace quiver
     void on_actions_changed()
     {
         you.quiver_action.on_actions_changed();
+    }
+
+    void on_item_pickup(int slot)
+    {
+        you.quiver_action.on_item_pickup(slot);
     }
 
     void set_needs_redraw()

crawl-ref/source/quiver.h
@@ -142,6 +142,7 @@ namespace quiver
         bool cycle(int dir = 0, bool allow_disabled=true);
         bool clear();
         void on_actions_changed();
+        void on_item_pickup(int slot);
         virtual void set_needs_redraw();
         shared_ptr<action> find_last_valid();
 
@@ -161,6 +162,7 @@ namespace quiver
     bool set_to_quiver(shared_ptr<quiver::action> s, action_cycler &cur_quiver);
     void on_actions_changed();
     void on_weapon_changed();
+    void on_item_pickup(int slot);
     void on_newchar();
 
     // TODO: perhaps this should be rolled into action_cycler?