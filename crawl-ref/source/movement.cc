crawl/crawl

commit 7bd419a0e568c3c84d0be9204f26b31b41b7d7ed

Don't lungepage recklessly (Hal)
If you rampage toward a monster and then kill it with lunge, don't
continue by moving into its tile. Even if we prevented moving into
deep water or lava, this could still result in the player moving
into traps, dangerous clouds, or exclusions, all of which seem like
potentially nasty surprises. Let's settle on a destination tile when
we start the move and then stick to it.

crawl-ref/source/movement.cc
@@ -864,6 +864,7 @@ void move_player_action(coord_def move)
 
     if (you.rampaging())
     {
+        const monster *rampage_targ = get_rampage_target(move);
         switch (_rampage_forward(move))
         {
             // Check the player's position again; rampage may have moved us.
@@ -876,10 +877,16 @@ void move_player_action(coord_def move)
 
             case spret::success:
                 rampaged = true;
-                if (you_worship(GOD_WU_JIAN))
+                if (you_worship(GOD_WU_JIAN)
+                    && wu_jian_post_move_effects(false, initial_position))
                 {
-                    did_wu_jian_attack
-                        = wu_jian_post_move_effects(false, initial_position);
+                    did_wu_jian_attack = true;
+                    // If you kill something with a lunge, don't continue
+                    // rampaging into its space. That could be a nasty surprise
+                    // for players who land on traps, clouds, exclusions, etc,
+                    // even if we prevented moving into solid terrain or lava.
+                    if (rampage_targ && !rampage_targ->alive())
+                        moving = false;
                 }
                 // If we've rampaged, reset initial_position for the second
                 // move.

commit ec2d92a32c3302389850c69901d24765efb774bc

fix: Apply attack delay to WJC rampage kills (Undo)
7bd419a0e5 introduced a bug where if the player killed their rampage target
during WJC postmoves, that move would take zero delay and fail to end the
turn. Using this bug, the player could equip seven-league and a high damage
weapon like the lochaber axe or dark maul, and bounce around the screen
making zero delay WJC attack moves.

move_player_action implicitly assumes that either an attack or a movement
will be attempted if it's being called: In this case, after 7bd419a0e5,
its movement attempt was being blocked, and there was no target monster
left to attack, so it eventually just returned without applying time_taken
or ending the turn.

This commit hopefully fixes the bug, by applying attack delay and ending
the turn if our rampage target dies during WJC postmoves.

crawl-ref/source/movement.cc
@@ -886,7 +886,13 @@ void move_player_action(coord_def move)
                     // for players who land on traps, clouds, exclusions, etc,
                     // even if we prevented moving into solid terrain or lava.
                     if (rampage_targ && !rampage_targ->alive())
-                        moving = false;
+                    {
+                        you.time_taken = you.attack_delay().roll();
+                        you.turn_is_over = true;
+                        you.berserk_penalty = 0;
+                        wu_jian_trigger_serpents_lash(false, initial_position);
+                        return;
+                    }
                 }
                 // If we've rampaged, reset initial_position for the second
                 // move.