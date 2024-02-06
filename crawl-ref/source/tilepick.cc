crawl/crawl

commit 5b44be1ddb685f4f29a3c56d8fed4afaf7c9e86f

Move monsters into YAML
For hopefully better readability and editability, without having
to cross-reference obscure fields.

crawl-ref/source/dat/mons/boulder.yaml
@@ -1,0 +1,17 @@
+name: "boulder"
+glyph: {char: "*", colour: brown}
+flags: [no_exp_gain, no_poly_to, conjured]
+resists: {elec: 1, fire: 1, cold: 1}
+xp_mult: 0
+holiness: [nonliving]
+will: invuln
+attacks:
+hd: 10
+hp_10x: 270
+ac: 10
+ev: 0
+intelligence: brainless
+speed: 20
+size: giant
+shape: orb
+tile: boulder_beetle

crawl-ref/source/tilepick.cc
@@ -1967,7 +1967,7 @@ static tileidx_t _tileidx_monster_no_props(const monster_info& mon)
 
         case MONS_BUSH:
             if (env.map_knowledge(mon.pos).cloud() == CLOUD_FIRE)
-                return TILEP_MONS_BUSH_BURNING;
+                return TILEP_MONS_BURNING_BUSH;
             return base;
 
         case MONS_BOULDER_BEETLE:

commit cc018136db8d63087ccf63d8df784bc85423c044

Fix placeholder BB boulder tile (SentientSupper)
5b44be1 accidentally used the base boulder beetle tile instead of the
rolling one. We still could use an actual distinct tile for this in the
style of its spell icon (something more geometric), but for now, this both
makes it properly spherical and also now animates it.

crawl-ref/source/dat/mons/boulder.yaml
@@ -14,4 +14,4 @@ intelligence: brainless
 speed: 20
 size: giant
 shape: orb
-tile: boulder_beetle
+tile: boulder_beetle_rolling

crawl-ref/source/tilepick.cc
@@ -1975,6 +1975,9 @@ static tileidx_t _tileidx_monster_no_props(const monster_info& mon)
                    ? _mon_random(TILEP_MONS_BOULDER_BEETLE_ROLLING, mon.number)
                    : base;
 
+        case MONS_BOULDER:
+            return _mon_random(TILEP_MONS_BOULDER_BEETLE_ROLLING, mon.number);
+
         case MONS_DANCING_WEAPON:
         {
             // Use item tile.