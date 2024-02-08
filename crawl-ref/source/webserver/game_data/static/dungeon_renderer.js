crawl/crawl

commit aef1140ae215b706d85166a49a4ace05fe4683b3

New decorations: magic conduits, bedeviled crystal
These are both highly variable pieces, meant to reflect spell schools and
demonic planes, to serve as magical varieties of decoration beyond the
usual emphasis of clouds or longer lengths of plain crystal walls.

The former are all further metal statue types of miscellaneous magic
collecting devices, with a slightly different orb, different palette, and
names each for all the spell schools. (Translocations is grouped together
with Summoning, as we have few workable translocations themes for vaults.)
These conduits help a couple of magical portals and branches beyond the
recent occasional presence of bookcases- Ice Caves, Volcanoes, Wizlabs,
Snake, Elf, Crypt, and Zot. They also provide some more workable flavour
for the spell-gifting gods who don't give spellbooks, Kiku and Vehumet,
plus a small number of particularly magic-focused vaults deeper on- across
Vaults and Depths.

The latter is a different tile and description for crystal walls, and serve
as links to the various demonic planes of regular games and extended for
a multitude demonic vaults without all of them leaning too hard on demonic
vegetation or lava (as clashes with quite a few other themes). They don't
actually spawn inside Hell / Pan / Abyss themselves, but there's a large
number of demonic vaults across D, Depths, Zot, and so on that can
reasonably fit a little window into Cocytus or Pan or the Abyss.

The conduits are a mix of snw-0's first spell icon for Battlesphere, the
rltiles original iron golem, and a variety of subtle overlays using
ontoclasm's item type icons. The bedeviled crystal combines quite a few
parts- roctavian's old lugonu altar, snw-0's grey crystal recolouring,
jpeg's original strange machine for Tukima's wizlab, and depending on
the crystal: dd's lava, snw-0's recolouring of roctavian's floor_frozen,
Denzi's cobalt_stone + wall_bars + floor_demonic, roctavian and due's edits

crawl-ref/source/webserver/game_data/static/dungeon_renderer.js
@@ -19,6 +19,7 @@ function ($, comm, cr, map_knowledge, options, dngn, util, view_data, enums,
         if (base_bg >= dngn.DNGN_LAVA && base_bg < dngn.FLOOR_MAX)
             return options.get("tile_water_anim");
         else if (base_bg >= dngn.DNGN_ENTER_ZOT_CLOSED && base_bg < dngn.BLOOD
+                (base_bg >= dngn.DNGN_SILVER_STATUE && base_bg < ARCANE_CONDUIT)
                  || is_torch(base_bg))
             return options.get("tile_misc_anim");
         else
@@ -29,11 +30,13 @@ function ($, comm, cr, map_knowledge, options, dngn, util, view_data, enums,
     {
         var base_bg = dngn.basetile(cell.bg.value);
         if (base_bg == dngn.DNGN_PORTAL_WIZARD_LAB
+            || base_bg >= dngn.ARCANE_CONDUIT && base_bg < dngn.STORM_CONDUIT
             || is_torch(base_bg))
         {
             cell.bg.value = base_bg + (cell.bg.value - base_bg + 1) % dngn.tile_count(base_bg);
         }
-        else if (base_bg > dngn.DNGN_LAVA && base_bg < dngn.BLOOD)
+        else if ((base_bg > dngn.DNGN_LAVA && base_bg < dngn.BLOOD) ||
+                 (base_bg >= dngn.DNGN_SILVER_STATUE && base_bg < ARCANE_CONDUIT))
         {
             cell.bg.value = base_bg + Math.floor(Math.random() * dngn.tile_count(base_bg))
         }

commit 3e06421dc0973c063c375cb6e2508e31e8bfbaa3

Fix webtiles tile animations
Broke in aef1140 adding more animating tiles, properly handled now.

crawl-ref/source/webserver/game_data/static/dungeon_renderer.js
@@ -18,10 +18,13 @@ function ($, comm, cr, map_knowledge, options, dngn, util, view_data, enums,
         var base_bg = dngn.basetile(cell.bg.value);
         if (base_bg >= dngn.DNGN_LAVA && base_bg < dngn.FLOOR_MAX)
             return options.get("tile_water_anim");
-        else if (base_bg >= dngn.DNGN_ENTER_ZOT_CLOSED && base_bg < dngn.BLOOD
-                (base_bg >= dngn.DNGN_SILVER_STATUE && base_bg < ARCANE_CONDUIT)
+        else if ((base_bg >= dngn.DNGN_ENTER_ZOT_CLOSED && base_bg < dngn.BLOOD)
+                 || (base_bg >= dngn.DNGN_SILVER_STATUE && base_bg < dngn.ARCANE_CONDUIT)
+                 || (base_bg >= dngn.ARCANE_CONDUIT && base_bg < dngn.STORM_CONDUIT)
                  || is_torch(base_bg))
+        {
             return options.get("tile_misc_anim");
+        }
         else
             return false;
     }
@@ -36,7 +39,7 @@ function ($, comm, cr, map_knowledge, options, dngn, util, view_data, enums,
             cell.bg.value = base_bg + (cell.bg.value - base_bg + 1) % dngn.tile_count(base_bg);
         }
         else if ((base_bg > dngn.DNGN_LAVA && base_bg < dngn.BLOOD) ||
-                 (base_bg >= dngn.DNGN_SILVER_STATUE && base_bg < ARCANE_CONDUIT))
+                 (base_bg >= dngn.DNGN_SILVER_STATUE && base_bg < dngn.ARCANE_CONDUIT))
         {
             cell.bg.value = base_bg + Math.floor(Math.random() * dngn.tile_count(base_bg))
         }