crawl/crawl

commit 71bd2ada001b83a62f405e83654961836e002c83

Allow followers to enter/be recall into portal vaults
The original veto for this appears to long predate interlevel recall and
I can't help but wonder if it was an attempt to prevent followers being
lost forever inside portals. In any case, that time has long passed and I
see no good reason to forbid a player from using apostles or bound souls
in portal levels.

crawl-ref/source/files.cc
@@ -1187,17 +1187,13 @@ static void _clear_env_map()
     env.map_forgotten.reset();
 }
 
-static bool _grab_follower_at(const coord_def &pos, bool can_follow)
+static bool _grab_follower_at(const coord_def &pos)
 {
     if (pos == you.pos())
         return false;
 
     monster* fol = monster_at(pos);
     if (!fol || !fol->alive() || fol->incapacitated())
-        return false;
-
-    // only H's ancestors can follow into portals & similar.
-    if (!can_follow && !mons_is_hepliaklqana_ancestor(fol->type))
         return false;
 
     // The monster has to already be tagged in order to follow.
@@ -1227,8 +1223,6 @@ static bool _grab_follower_at(const coord_def &pos, bool can_follow)
 
 static void _grab_followers()
 {
-    const bool can_follow = branch_allows_followers(you.where_are_you);
-
     int non_stair_using_allies = 0;
     int non_stair_using_undead = 0;
     int non_stair_using_summons = 0;
@@ -1280,7 +1274,7 @@ static void _grab_followers()
             duvessa->flags &= ~MF_TAKING_STAIRS;
     }
 
-    if (can_follow && non_stair_using_allies > 0)
+    if (non_stair_using_allies > 0)
     {
         // Summons won't follow and will time out.
         if (non_stair_using_summons > 0)
@@ -1313,7 +1307,7 @@ static void _grab_followers()
                     continue;
 
                 visited[ai->x][ai->y] = true;
-                if (_grab_follower_at(*ai, can_follow))
+                if (_grab_follower_at(*ai))
                     places[!place_set].push_back(*ai);
             }
         }

crawl-ref/source/place.cc
@@ -33,11 +33,6 @@ int absdungeon_depth(branch_type branch, int subdepth)
     return branches[branch].absdepth + subdepth - 1;
 }
 
-bool branch_allows_followers(branch_type branch)
-{
-    return is_connected_branch(branch) || branch == BRANCH_PANDEMONIUM;
-}
-
 vector<level_id> all_dungeon_ids()
 {
     vector<level_id> out;

crawl-ref/source/place.h
@@ -26,8 +26,6 @@ int absdungeon_depth(branch_type branch, int depth);
 
 bool single_level_branch(branch_type branch);
 
-bool branch_allows_followers(branch_type branch);
-
 vector<level_id> all_dungeon_ids();
 
 bool is_level_on_stack(level_id id);

crawl-ref/source/spl-other.cc
@@ -152,20 +152,17 @@ void do_player_recall(recall_t type)
     }
 
     // Then search for recallable companions on any floor
-    if (branch_allows_followers(you.where_are_you))
+    for (auto &entry : companion_list)
     {
-        for (auto &entry : companion_list)
+        const int mid = entry.first;
+        if (companion_is_elsewhere(mid, true))
         {
-            const int mid = entry.first;
-            if (companion_is_elsewhere(mid, true))
-            {
-                // Recall can't pull monsters out of the Abyss
-                if (entry.second.level.branch == BRANCH_ABYSS)
-                    continue;
+            // Recall can't pull monsters out of the Abyss
+            if (entry.second.level.branch == BRANCH_ABYSS)
+                continue;
 
-                if (try_recall(mid))
-                    did_recall = true;
-            }
+            if (try_recall(mid))
+                did_recall = true;
         }
     }
 

commit 817a28345ea709c5de4a80715fbc6f602b9eebf9

Fix hostiles beyond able to follow the player through portals (various)
This was broken by 71bd2ada001b which was only intended to let you take
FRIENDLIES with you, rather than losing the old enemy avoidance behaviour.

(It's possible this was responsible for a bug that caused multiple monsters
to follow the player into the arena after using Okawaru's Duel ability,
but I've not been able to reproduce that myself.)

crawl-ref/source/files.cc
@@ -1196,6 +1196,13 @@ static bool _grab_follower_at(const coord_def &pos)
     if (!fol || !fol->alive() || fol->incapacitated())
         return false;
 
+    // Only friendlies can follow the player through portals
+    if (!fol->friendly() && (!is_connected_branch(you.where_are_you)
+                            || you.where_are_you == BRANCH_PANDEMONIUM))
+    {
+        return false;
+    }
+
     // The monster has to already be tagged in order to follow.
     if (!testbits(fol->flags, MF_TAKING_STAIRS))
         return false;