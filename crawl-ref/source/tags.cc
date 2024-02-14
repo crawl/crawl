bfd232f637dae8faebce36c1aca7e711e6a6c517
DracoOmega <draco.omega.dcss.dev@gmail.com>
2024-02-13 22:15:13 -0330

    Fix apostles still getting player Vhi's instead of monster Vhi's
    Seems to have gotten messed up in the rebase to master.

--- crawl-ref/source/tags.cc
+++ crawl-ref/source/tags.cc
@@ -7042,6 +7042,11 @@ void unmarshallMonster(reader &th, monster& m)
             slot.spell = _fixup_soh_breath(m.type);
             m.spells.push_back(slot);
         }
+        else if (slot.spell == SPELL_ELECTRIC_CHARGE)
+        {
+            slot.spell = SPELL_ELECTROLUNGE;
+            m.spells.push_back(slot);
+        }
 #if TAG_MAJOR_VERSION == 34
         else if (slot.spell != SPELL_DELAYED_FIREBALL
                  && slot.spell != SPELL_MELEE
