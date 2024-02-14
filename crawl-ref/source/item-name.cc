crawl/crawl

commit 4224600aead45df7c84102127e7103837e200c84

Let evil players wield good weapons
Blocking undead and demonic players from wielding weapons of holy
wrath was very fiddly - lots of annoying corner case code around
entering and leaving Death Form.

Achieve the same flavour with less annoyance by just disabling the
brand while players are dead, rather than blocking wielding
entirely.

crawl-ref/source/item-name.cc
@@ -3328,28 +3328,8 @@ bool is_useless_item(const item_def &item, bool temp, bool ident)
     switch (item.base_type)
     {
     case OBJ_WEAPONS:
-        if (you.has_mutation(MUT_NO_GRASPING))
-            return true;
-
-        if (!you.could_wield(item, true, !temp)
-            && !is_throwable(&you, item))
-        {
-            // Weapon is too large (or small) to be wielded and cannot
-            // be thrown either.
-            return true;
-        }
-
-        if (you.undead_or_demonic() && is_holy_item(item, false))
-        {
-            if (!temp && you.form == transformation::death
-                && you.species != SP_DEMONSPAWN)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        return false;
+        return !you.has_mutation(MUT_NO_GRASPING)
+            && !you.could_wield(item, true, !temp);
 
     case OBJ_MISSILES:
         // All missiles are useless for felids.

crawl-ref/source/acquire.cc
@@ -1210,6 +1210,9 @@ static string _why_reject(const item_def &item, int agent)
         return "Destroying pain weapon after Necro sac!";
     }
 
+    if (you.undead_or_demonic(false) && is_holy_item(item))
+        return "Destroying holy weapon for evil player!";
+
     return ""; // all OK
 }
 

crawl-ref/source/attack.cc
@@ -1407,6 +1407,9 @@ bool attack::apply_damage_brand(const char *what)
         break;
 
     case SPWPN_HOLY_WRATH:
+        if (attacker->undead_or_demonic())
+            break; // No holy wrath for thee!
+
         if (defender->holy_wrath_susceptible())
             special_damage = 1 + (random2(damage_done * 15) / 10);
 

crawl-ref/source/describe.cc
@@ -1744,7 +1744,7 @@ static string _describe_weapon_brand(const item_def &item)
     case SPWPN_HOLY_WRATH:
         return "It has been blessed by the Shining One, dealing an additional "
                "three-quarters of any damage that pierces undead and demons' "
-               "armour.";
+               "armour. Undead and demons cannot use this.";
     case SPWPN_ELECTROCUTION:
         return "It sometimes electrocutes victims (1/4 chance, 8-20 damage).";
     case SPWPN_VENOM:

crawl-ref/source/item-use.cc
@@ -1257,7 +1257,7 @@ static bool _safe_to_remove_or_wear(const item_def &item,
 // Rather messy - we've gathered all the can't-wield logic from wield_weapon()
 // here.
 bool can_wield(const item_def *weapon, bool say_reason,
-               bool ignore_temporary_disability, bool unwield, bool only_known)
+               bool ignore_temporary_disability, bool unwield)
 {
 #define SAY(x) {if (say_reason) { x; }}
     if (you.melded[EQ_WEAPON] && unwield)
@@ -1325,34 +1325,6 @@ bool can_wield(const item_def *weapon, bool say_reason,
         }
         else
             return true;
-    }
-
-    bool id_brand = false;
-
-    if (you.undead_or_demonic() && is_holy_item(*weapon)
-        && (item_type_known(*weapon) || !only_known))
-    {
-        if (say_reason)
-        {
-            mprf(MSGCH_PROMPT, "This weapon is holy and will not allow you to wield it.");
-            id_brand = true;
-        }
-        else
-            return false;
-    }
-    if (id_brand)
-    {
-        auto wwpn = const_cast<item_def*>(weapon);
-        if (!is_artefact(*weapon) && !is_blessed(*weapon)
-            && !item_type_known(*weapon))
-        {
-            set_ident_flags(*wwpn, ISFLAG_KNOW_TYPE);
-            if (in_inventory(*weapon))
-                mprf_nocap("%s", weapon->name(DESC_INVENTORY_EQUIP).c_str());
-        }
-        else if (is_artefact(*weapon) && !item_type_known(*weapon))
-            artefact_learn_prop(*wwpn, ARTP_BRAND);
-        return false;
     }
 
     if (!ignore_temporary_disability && is_shield_incompatible(*weapon))
@@ -1572,10 +1544,6 @@ static bool _do_wield_weapon(item_def &new_wpn, const item_def *old_wpn)
 
     // Ensure wieldable
     if (!can_wield(&new_wpn, true))
-        return false;
-
-    // Really ensure wieldable, even unknown brand
-    if (!can_wield(&new_wpn, true, false, false, false))
         return false;
 
     // At this point, we know it's possible to equip this item. However, there

crawl-ref/source/item-use.h
@@ -43,8 +43,7 @@ bool wear_armour(int slot);
 bool can_wear_armour(const item_def &item, bool verbose, bool ignore_temporary);
 
 bool can_wield(const item_def *weapon, bool say_why = false,
-               bool ignore_temporary_disability = false, bool unwield = false,
-               bool only_known = true);
+               bool ignore_temporary_disability = false, bool unwield = false);
 
 bool auto_wield();
 bool wield_weapon(int slot);

crawl-ref/source/monster.cc
@@ -638,6 +638,7 @@ bool monster::could_wield(const item_def &item, bool ignore_brand,
     {
         // Undead and demonic monsters and monsters that are
         // gifts/worshippers of Yredelemnul won't use holy weapons.
+        // They could, they just don't want to.
         if ((undead_or_demonic() || god == GOD_YREDELEMNUL)
             && is_holy_item(item))
         {

crawl-ref/source/player-act.cc
@@ -441,11 +441,10 @@ bool player::can_wield(const item_def& item, bool ignore_curse,
  * what they're currently wielding, transformed into, or any other state.
  *
  * @param item              The item to wield.
- * @param ignore_brand      Whether to disregard the weapon's brand.
  * @return                  Whether the player could potentially wield the
  *                          item.
  */
-bool player::could_wield(const item_def &item, bool ignore_brand,
+bool player::could_wield(const item_def &item, bool /*ignore_brand*/,
                          bool ignore_transform, bool quiet) const
 {
     // Some lingering flavor from the days where sandblast ammo was wielded.
@@ -496,14 +495,6 @@ bool player::could_wield(const item_def &item, bool ignore_brand,
     if (get_mutation_level(MUT_MISSING_HAND)
         && you.hands_reqd(item) == HANDS_TWO)
     {
-        return false;
-    }
-
-    // don't let undead/demonspawn wield holy weapons/scrolls (out of spite)
-    if (!ignore_brand && undead_or_demonic() && is_holy_item(item))
-    {
-        if (!quiet)
-            mpr("This weapon is holy and will not allow you to wield it.");
         return false;
     }
 

commit 1d7162e751315d0d9bbe2f5bbaf7cca68dfae46b

Fix: felids can't wield artefact weapons
Broken in 4224600aead45df7c84102127e7103837e200c84.

crawl-ref/source/item-name.cc
@@ -3328,8 +3328,8 @@ bool is_useless_item(const item_def &item, bool temp, bool ident)
     switch (item.base_type)
     {
     case OBJ_WEAPONS:
-        return !you.has_mutation(MUT_NO_GRASPING)
-            && !you.could_wield(item, true, !temp);
+        return you.has_mutation(MUT_NO_GRASPING)
+            || !you.could_wield(item, true, !temp);
 
     case OBJ_MISSILES:
         // All missiles are useless for felids.