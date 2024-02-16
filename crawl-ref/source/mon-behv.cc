crawl/crawl

commit 0a82c8690a01462b55615d2cbd8e7497dc3b9d6c

Refactor/rename summon_can_attack()
This function (used to prevent player summons and other allies from
attacking outside the player's line of sight) has gotten a laundry list of
specifiers attached to it over time, as more and more things are added to
it (and more oversights are corrected). The name is also somewhat
misleading, since many of those things are not summons!

With the Beogh revamp removing one of the last cases of a perma-friendly
player ally being able to attack outside of los, I have simplified the
check greatly: now it looks entirely at whether the monster is permanently
friendly, with no other qualifiers. This means that charmed/bribed monsters
can still fight out of sight (since their 'true' attitude is not friendly)
but essentially of the existing cases in _mons_attacks_outside_los should
be subsumed more cleanly.

I've also renamed these functions, to be more clear with what they're doing
now: monster_needs_los() and monster_los_is_valid().

I also removed a couple special-cases that don't seem necessary (spectral
weapons are already banned from manual attacks elsewhere, for instance)

Fun fact: due to the exact specifiers in _mons_attacks_outside_los, charmed
death knights and profane servitors would previously refuse to attack out
of los because they worshipped Yred....

crawl-ref/source/mon-behv.cc
@@ -68,7 +68,7 @@ static void _mon_check_foe_invalid(monster* mon)
         if (actor *foe = mon->get_foe())
         {
             const monster* foe_mons = foe->as_monster();
-            if (foe_mons->alive() && summon_can_attack(mon, foe)
+            if (foe_mons->alive() && monster_los_is_valid(mon, foe)
                 && (mon->has_ench(ENCH_FRENZIED)
                     || mon->friendly() != foe_mons->friendly()
                     || mon->neutral() != foe_mons->neutral()))
@@ -360,8 +360,9 @@ void handle_behaviour(monster* mon)
         set_nearest_monster_foe(mon);
     }
 
-    // Friendly summons will come back to the player if they go out of sight.
-    if (!summon_can_attack(mon))
+    // Friendly allies will come back to the player if they go out of sight
+    // (and haven't been commanded to run away).
+    if (isFriendly && monster_needs_los(mon) && mon->behaviour != BEH_WITHDRAW)
         mon->target = you.pos();
 
     // Monsters do not attack themselves. {dlb}
@@ -906,7 +907,7 @@ static bool _mons_check_foe(monster* mon, const coord_def& p,
                || neutral && !foe->neutral()
                || mon->has_ench(ENCH_FRENZIED))
            && !mons_is_projectile(*foe)
-           && summon_can_attack(mon, p)
+           && monster_los_is_valid(mon, p)
            && (friendly || !is_sanctuary(p))
            && !mons_is_firewood(*foe)
            && !foe->props.exists(KIKU_WRETCH_KEY)
@@ -1424,53 +1425,31 @@ void make_mons_leave_level(monster* mon)
     }
 }
 
-// Given an adjacent monster, returns true if the monster can hit it
-bool monster_can_hit_monster(monster* mons, const monster* targ)
+// Summons and perma-friendly allies can't attack out of the player's LOS
+bool monster_needs_los(const monster* mons)
 {
-    return summon_can_attack(mons, targ);
+    return !crawl_state.game_is_arena()
+           && mons->attitude == ATT_FRIENDLY;
 }
 
-static bool _mons_attacks_outside_los(const monster &mon)
-{
-    return !mon.is_summoned()
-        && !mon.has_ench(ENCH_FAKE_ABJURATION)
-        && !mon.has_ench(ENCH_PORTAL_PACIFIED)
-        && mon.god != GOD_YREDELEMNUL
-        && !mons_is_hepliaklqana_ancestor(mon.type)
-        && !mon.props.exists(ANIMATE_DEAD_KEY);
-}
-
-// Friendly summons can't attack out of the player's LOS, it's too abusable.
-bool summon_can_attack(const monster* mons)
-{
-    return crawl_state.game_is_arena()
-        || !mons->friendly()
-        || _mons_attacks_outside_los(*mons)
-        || you.see_cell_no_trans(mons->pos());
-}
-
-bool summon_can_attack(const monster* mons, const coord_def &p)
+// Check whether the player has line of sight to both the attacker and defender,
+// if this is a monster that requires that to attack.
+//
+// Returns true if the monster can attack the specified target.
+bool monster_los_is_valid(const monster* mons, const coord_def &p)
 {
     if (crawl_state.game_is_arena())
         return true;
 
-    // Spectral weapons only attack their target
-    if (mons->type == MONS_SPECTRAL_WEAPON)
-        return false;
-
-    if (!mons->friendly()
-        // XXX: can we merge foxfire in?
-        || _mons_attacks_outside_los(*mons) && mons->type != MONS_FOXFIRE)
-    {
+    if (!monster_needs_los(mons))
         return true;
-    }
 
     return you.see_cell_no_trans(mons->pos()) && you.see_cell_no_trans(p);
 }
 
-bool summon_can_attack(const monster* mons, const actor* targ)
+bool monster_los_is_valid(const monster* mons, const actor* targ)
 {
-    return summon_can_attack(mons, targ->pos());
+    return monster_los_is_valid(mons, targ->pos());
 }
 
 vector<monster *> find_allies_targeting(const actor &a)

crawl-ref/source/mon-movetarget.cc
@@ -721,7 +721,7 @@ void set_random_target(monster* mon)
         if (!in_bounds(newtarget))
             continue;
 
-        if (!summon_can_attack(mon, newtarget))
+        if (!monster_los_is_valid(mon, newtarget))
             continue;
 
         if (!mon->is_location_safe(newtarget))

crawl-ref/source/fineff.cc
@@ -763,18 +763,13 @@ void make_derived_undead_fineff::fire()
 
     if (mg.god != GOD_YREDELEMNUL)
     {
-        if (undead->type == MONS_ZOMBIE)
-            undead->props[ANIMATE_DEAD_KEY] = true;
-        else
-        {
-            int dur = undead->type == MONS_SKELETON ? 3 : 5;
+        int dur = undead->type == MONS_SKELETON ? 3 : 5;
 
-            // Sculpt Simulacrum has a shorter duration than Bind Soul simulacra
-            if (spell == SPELL_SIMULACRUM)
-                dur = 3;
+        // Sculpt Simulacrum has a shorter duration than Bind Soul simulacra
+        if (spell == SPELL_SIMULACRUM)
+            dur = 3;
 
-            undead->add_ench(mon_enchant(ENCH_FAKE_ABJURATION, dur));
-        }
+        undead->add_ench(mon_enchant(ENCH_FAKE_ABJURATION, dur));
     }
     if (!agent.empty())
         mons_add_blame(undead, "animated by " + agent);

crawl-ref/source/melee-attack.cc
@@ -3473,7 +3473,7 @@ void melee_attack::do_spines()
             // Dithmenos' shadow can't take damage, don't spam.
             || attacker->type == MONS_PLAYER_SHADOW
             // Don't let spines kill things out of LOS.
-            || !summon_can_attack(defender->as_monster(), attacker))
+            || !monster_los_is_valid(defender->as_monster(), attacker))
         {
             return;
         }

crawl-ref/source/mon-act.cc
@@ -1998,7 +1998,7 @@ void handle_monster_move(monster* mons)
             && mons->behaviour != BEH_WITHDRAW
             && (!(mons_aligned(mons, targ) || targ->type == MONS_FOXFIRE)
                 || mons->has_ench(ENCH_FRENZIED))
-            && monster_can_hit_monster(mons, targ))
+            && monster_los_is_valid(mons, targ))
         {
             // Maybe they can swap places?
             if (_swap_monsters(*mons, *targ))
@@ -2995,7 +2995,7 @@ bool mon_can_move_to_pos(const monster* mons, const coord_def& delta,
             return false; // blocks square
         }
 
-        if (!summon_can_attack(mons, targ))
+        if (!monster_los_is_valid(mons, targ))
             return false;
 
         // Cut down plants only when no alternative, or they're

crawl-ref/source/mon-behv.h
@@ -44,11 +44,9 @@ void make_mons_stop_fleeing(monster* mon);
 
 void make_mons_leave_level(monster* mon);
 
-bool monster_can_hit_monster(monster* mons, const monster* targ);
-
-bool summon_can_attack(const monster* mons);
-bool summon_can_attack(const monster* mons, const coord_def &p);
-bool summon_can_attack(const monster* mons, const actor* targ);
+bool monster_needs_los(const monster* mons);
+bool monster_los_is_valid(const monster* mons, const coord_def &p);
+bool monster_los_is_valid(const monster* mons, const actor* targ);
 
 void shake_off_monsters(const actor* target);
 

crawl-ref/source/monster.cc
@@ -4998,7 +4998,7 @@ bool monster::near_foe() const
 {
     const actor *afoe = get_foe();
     return afoe && see_cell_no_trans(afoe->pos())
-           && summon_can_attack(this, afoe);
+           && monster_los_is_valid(this, afoe);
 }
 
 /**

crawl-ref/source/monster.h
@@ -37,7 +37,6 @@ using std::vector;
 /// has a given hound already used up its howl?
 #define DOOM_HOUND_HOWLED_KEY "doom_hound_howled"
 #define KIKU_WRETCH_KEY "kiku_wretch"
-#define ANIMATE_DEAD_KEY "animate_dead"
 
 #define DROPPER_MID_KEY "dropper_mid"
 

commit 42642787d0c310ba7f2bf97858144666965a7164

Unbreak telling allies to guard an area
This was broken by 0a82c8690a01462b55615d2cbd8e7497dc3b9d6c changing the
LoS check for allies to return to you from 'Does this ally currently have
LoS to the player' to 'Does this ally ever need LoS to the player to
attack', resulting in all perma-allies attempting to return to the player
every single turn - whether they could see you or not.

I've also carved out an additional exception to not return to you if you
told them to guard an area, since this is probably what the player wants
in that case.

Additionally, telling allies to follow you, or recalling them, should fully
end their travel behaviour. (Previously - and unrelated to the linked
commit - telling them to guard an area after you'd previously told them to
guard a different area would often result in them trying to run back to
the first one or something outright being stuck in place, since they would
still be trying to use pathfinding info from the previous time they did
pathfinding)

I am 100% positive travel_path is still not being cleared in all situations
that it should be, so it's possible that the minor change in
_handle_monster_travelling will uncover some additional bugs, but the code
currently seems to assume that the monster's target will be travel_path[0],
if it's travelling, and thus if anything sets its target directly, it can
cause it to perpetually freeze in place by assuming it's moving to its next
waypoint when it's not actually moving anywhere at all. Probably better to
find any other bugs as they come up rather than leave this one here.

(Probably better to refactor a lot of monster movement / behaviour code,
but HOO BOY is that a tall ask...)

crawl-ref/source/mon-behv.cc
@@ -362,9 +362,14 @@ void handle_behaviour(monster* mon)
     }
 
     // Friendly allies will come back to the player if they go out of sight
-    // (and haven't been commanded to run away).
-    if (isFriendly && monster_needs_los(mon) && mon->behaviour != BEH_WITHDRAW)
+    // (and haven't been commanded to run away or guard an area).
+    if (isFriendly && monster_needs_los(mon)
+        && mon->behaviour != BEH_WITHDRAW
+        && !mon->is_patrolling()
+        && !you.can_see(*mon))
+    {
         mon->target = you.pos();
+    }
 
     // Monsters do not attack themselves. {dlb}
     if (mon->foe == mon->mindex())

crawl-ref/source/mon-movetarget.cc
@@ -532,8 +532,9 @@ static bool _handle_monster_travelling(monster* mon)
         }
     }
 
-    // Else, we can see the next waypoint and are making good progress.
-    // Carry on, then!
+    // We can see the next waypoint and should be able to walk there.
+    // (Reset our target to this, just in case)
+    mon->target = mon->travel_path[0];
     return false;
 }
 

crawl-ref/source/shout.cc
@@ -465,6 +465,7 @@ static void _set_allies_patrol_point(bool clear = false)
             mi->behaviour = BEH_WANDER;
         else
             mi->behaviour = BEH_SEEK;
+        mi->travel_path.clear();
     }
 }
 

crawl-ref/source/spl-other.cc
@@ -175,6 +175,7 @@ void recall_orders(monster *mons)
 
     // Don't patrol
     mons->patrol_point = coord_def(0, 0);
+    mons->travel_path.clear();
 
     // Don't wander
     mons->behaviour = BEH_SEEK;