crawl/crawl

commit 0a82c8690a01462b55615d2cbd8e7497dc3b9d6c

Refactor/rename summon_can_attack()
This function (used to prevent player summons and other allies from
attacking outside the player's line of sight) has gotten a laundry list of
specifiers attached to it over time, as more and more things are added to
it (and more oversights are corrected). The name is also somewhat
misleading, since many of those things are not summons!

With the Beogh revamp removing one of the last cases of a perma-friendly
player ally being able to attack outside of los, I have simplified the
check greatly: now it looks entirely at whether the monster is permanently
friendly, with no other qualifiers. This means that charmed/bribed monsters
can still fight out of sight (since their 'true' attitude is not friendly)
but essentially of the existing cases in _mons_attacks_outside_los should
be subsumed more cleanly.

I've also renamed these functions, to be more clear with what they're doing
now: monster_needs_los() and monster_los_is_valid().

I also removed a couple special-cases that don't seem necessary (spectral
weapons are already banned from manual attacks elsewhere, for instance)

Fun fact: due to the exact specifiers in _mons_attacks_outside_los, charmed
death knights and profane servitors would previously refuse to attack out
of los because they worshipped Yred....

crawl-ref/source/fineff.cc
@@ -763,18 +763,13 @@ void make_derived_undead_fineff::fire()
 
     if (mg.god != GOD_YREDELEMNUL)
     {
-        if (undead->type == MONS_ZOMBIE)
-            undead->props[ANIMATE_DEAD_KEY] = true;
-        else
-        {
-            int dur = undead->type == MONS_SKELETON ? 3 : 5;
+        int dur = undead->type == MONS_SKELETON ? 3 : 5;
 
-            // Sculpt Simulacrum has a shorter duration than Bind Soul simulacra
-            if (spell == SPELL_SIMULACRUM)
-                dur = 3;
+        // Sculpt Simulacrum has a shorter duration than Bind Soul simulacra
+        if (spell == SPELL_SIMULACRUM)
+            dur = 3;
 
-            undead->add_ench(mon_enchant(ENCH_FAKE_ABJURATION, dur));
-        }
+        undead->add_ench(mon_enchant(ENCH_FAKE_ABJURATION, dur));
     }
     if (!agent.empty())
         mons_add_blame(undead, "animated by " + agent);

crawl-ref/source/melee-attack.cc
@@ -3473,7 +3473,7 @@ void melee_attack::do_spines()
             // Dithmenos' shadow can't take damage, don't spam.
             || attacker->type == MONS_PLAYER_SHADOW
             // Don't let spines kill things out of LOS.
-            || !summon_can_attack(defender->as_monster(), attacker))
+            || !monster_los_is_valid(defender->as_monster(), attacker))
         {
             return;
         }

crawl-ref/source/mon-act.cc
@@ -1998,7 +1998,7 @@ void handle_monster_move(monster* mons)
             && mons->behaviour != BEH_WITHDRAW
             && (!(mons_aligned(mons, targ) || targ->type == MONS_FOXFIRE)
                 || mons->has_ench(ENCH_FRENZIED))
-            && monster_can_hit_monster(mons, targ))
+            && monster_los_is_valid(mons, targ))
         {
             // Maybe they can swap places?
             if (_swap_monsters(*mons, *targ))
@@ -2995,7 +2995,7 @@ bool mon_can_move_to_pos(const monster* mons, const coord_def& delta,
             return false; // blocks square
         }
 
-        if (!summon_can_attack(mons, targ))
+        if (!monster_los_is_valid(mons, targ))
             return false;
 
         // Cut down plants only when no alternative, or they're

crawl-ref/source/mon-behv.cc
@@ -68,7 +68,7 @@ static void _mon_check_foe_invalid(monster* mon)
         if (actor *foe = mon->get_foe())
         {
             const monster* foe_mons = foe->as_monster();
-            if (foe_mons->alive() && summon_can_attack(mon, foe)
+            if (foe_mons->alive() && monster_los_is_valid(mon, foe)
                 && (mon->has_ench(ENCH_FRENZIED)
                     || mon->friendly() != foe_mons->friendly()
                     || mon->neutral() != foe_mons->neutral()))
@@ -360,8 +360,9 @@ void handle_behaviour(monster* mon)
         set_nearest_monster_foe(mon);
     }
 
-    // Friendly summons will come back to the player if they go out of sight.
-    if (!summon_can_attack(mon))
+    // Friendly allies will come back to the player if they go out of sight
+    // (and haven't been commanded to run away).
+    if (isFriendly && monster_needs_los(mon) && mon->behaviour != BEH_WITHDRAW)
         mon->target = you.pos();
 
     // Monsters do not attack themselves. {dlb}
@@ -906,7 +907,7 @@ static bool _mons_check_foe(monster* mon, const coord_def& p,
                || neutral && !foe->neutral()
                || mon->has_ench(ENCH_FRENZIED))
            && !mons_is_projectile(*foe)
-           && summon_can_attack(mon, p)
+           && monster_los_is_valid(mon, p)
            && (friendly || !is_sanctuary(p))
            && !mons_is_firewood(*foe)
            && !foe->props.exists(KIKU_WRETCH_KEY)
@@ -1424,53 +1425,31 @@ void make_mons_leave_level(monster* mon)
     }
 }
 
-// Given an adjacent monster, returns true if the monster can hit it
-bool monster_can_hit_monster(monster* mons, const monster* targ)
+// Summons and perma-friendly allies can't attack out of the player's LOS
+bool monster_needs_los(const monster* mons)
 {
-    return summon_can_attack(mons, targ);
+    return !crawl_state.game_is_arena()
+           && mons->attitude == ATT_FRIENDLY;
 }
 
-static bool _mons_attacks_outside_los(const monster &mon)
-{
-    return !mon.is_summoned()
-        && !mon.has_ench(ENCH_FAKE_ABJURATION)
-        && !mon.has_ench(ENCH_PORTAL_PACIFIED)
-        && mon.god != GOD_YREDELEMNUL
-        && !mons_is_hepliaklqana_ancestor(mon.type)
-        && !mon.props.exists(ANIMATE_DEAD_KEY);
-}
-
-// Friendly summons can't attack out of the player's LOS, it's too abusable.
-bool summon_can_attack(const monster* mons)
-{
-    return crawl_state.game_is_arena()
-        || !mons->friendly()
-        || _mons_attacks_outside_los(*mons)
-        || you.see_cell_no_trans(mons->pos());
-}
-
-bool summon_can_attack(const monster* mons, const coord_def &p)
+// Check whether the player has line of sight to both the attacker and defender,
+// if this is a monster that requires that to attack.
+//
+// Returns true if the monster can attack the specified target.
+bool monster_los_is_valid(const monster* mons, const coord_def &p)
 {
     if (crawl_state.game_is_arena())
         return true;
 
-    // Spectral weapons only attack their target
-    if (mons->type == MONS_SPECTRAL_WEAPON)
-        return false;
-
-    if (!mons->friendly()
-        // XXX: can we merge foxfire in?
-        || _mons_attacks_outside_los(*mons) && mons->type != MONS_FOXFIRE)
-    {
+    if (!monster_needs_los(mons))
         return true;
-    }
 
     return you.see_cell_no_trans(mons->pos()) && you.see_cell_no_trans(p);
 }
 
-bool summon_can_attack(const monster* mons, const actor* targ)
+bool monster_los_is_valid(const monster* mons, const actor* targ)
 {
-    return summon_can_attack(mons, targ->pos());
+    return monster_los_is_valid(mons, targ->pos());
 }
 
 vector<monster *> find_allies_targeting(const actor &a)

crawl-ref/source/mon-behv.h
@@ -44,11 +44,9 @@ void make_mons_stop_fleeing(monster* mon);
 
 void make_mons_leave_level(monster* mon);
 
-bool monster_can_hit_monster(monster* mons, const monster* targ);
-
-bool summon_can_attack(const monster* mons);
-bool summon_can_attack(const monster* mons, const coord_def &p);
-bool summon_can_attack(const monster* mons, const actor* targ);
+bool monster_needs_los(const monster* mons);
+bool monster_los_is_valid(const monster* mons, const coord_def &p);
+bool monster_los_is_valid(const monster* mons, const actor* targ);
 
 void shake_off_monsters(const actor* target);
 

crawl-ref/source/mon-movetarget.cc
@@ -721,7 +721,7 @@ void set_random_target(monster* mon)
         if (!in_bounds(newtarget))
             continue;
 
-        if (!summon_can_attack(mon, newtarget))
+        if (!monster_los_is_valid(mon, newtarget))
             continue;
 
         if (!mon->is_location_safe(newtarget))

crawl-ref/source/monster.cc
@@ -4998,7 +4998,7 @@ bool monster::near_foe() const
 {
     const actor *afoe = get_foe();
     return afoe && see_cell_no_trans(afoe->pos())
-           && summon_can_attack(this, afoe);
+           && monster_los_is_valid(this, afoe);
 }
 
 /**

crawl-ref/source/monster.h
@@ -37,7 +37,6 @@ using std::vector;
 /// has a given hound already used up its howl?
 #define DOOM_HOUND_HOWLED_KEY "doom_hound_howled"
 #define KIKU_WRETCH_KEY "kiku_wretch"
-#define ANIMATE_DEAD_KEY "animate_dead"
 
 #define DROPPER_MID_KEY "dropper_mid"
 

commit 213d36726f0033a8e4beb488114cccfd39a8a707

Fix Animate Dead zombies being temporary (CarefulOdds)
Accidentally broken by 0a82c8690a01462b55615d2cbd8e7497dc3b9d6c

Sorry!

crawl-ref/source/fineff.cc
@@ -763,14 +763,9 @@ void make_derived_undead_fineff::fire()
     if (!mg.mname.empty())
         name_zombie(*undead, mg.base_type, mg.mname);
 
-    if (mg.god != GOD_YREDELEMNUL)
+    if (mg.god != GOD_YREDELEMNUL && undead->type != MONS_ZOMBIE)
     {
-        int dur = undead->type == MONS_SKELETON ? 3 : 5;
-
-        // Sculpt Simulacrum has a shorter duration than Bind Soul simulacra
-        if (spell == SPELL_SIMULACRUM)
-            dur = 3;
-
+        int dur = (undead->type == MONS_SKELETON || spell == SPELL_SIMULACRUM) ? 3 : 5;
         undead->add_ench(mon_enchant(ENCH_FAKE_ABJURATION, dur));
     }
     if (!agent.empty())