d0048a8123a3f19ceca18216cfa3820d848f885d
Nicholas Feinberg <pleasingfung@gmail.com>
2024-02-17 16:58:32 -0800

    Increase Foxfire Swarm damage
    Moving it from 1d6 to 1d8 might make it a bit more competitive with
    Ignis's other active. Elliptic suggested reducing the max number of
    foxfires from 24 to 8 (ie going from radius 2 to 1), which *would*
    make it easier to balance, but would be a terrible loss to the
    spectacle of the effect. I'd rather hold off.

--- crawl-ref/source/spl-summoning.cc
+++ crawl-ref/source/spl-summoning.cc
@@ -2757,7 +2757,7 @@ spret foxfire_swarm()
     bool unknown_unseen = false;
     for (radius_iterator ri(you.pos(), 2, C_SQUARE, LOS_NO_TRANS); ri; ++ri)
     {
-        if (_create_foxfire(you, *ri, GOD_NO_GOD, 10))
+        if (_create_foxfire(you, *ri, GOD_NO_GOD, 20))
         {
             created = true;
             continue;
