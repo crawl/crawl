b849e8b480befd319def5947a4085b211a494e50
David Lawrence Ramsey <pooka109@gmail.com>
2024-02-20 17:31:01 -0600

    Add a few missing foul-flame-related bits.
    It can rarely show up in the chaos brand, so it should be displayed
    then. Also, like holy wrath, it needs to do damage to function, even if
    nothing is currently immune to it.

--- crawl-ref/source/attack.cc
+++ crawl-ref/source/attack.cc
@@ -623,6 +623,7 @@ brand_type attack::random_chaos_brand()
     case SPWPN_VAMPIRISM:       brand_name += "vampirism"; break;
     case SPWPN_HOLY_WRATH:      brand_name += "holy wrath"; break;
     case SPWPN_ANTIMAGIC:       brand_name += "antimagic"; break;
+    case SPWPN_FOUL_FLAME:      brand_name += "foul flame"; break;
     default:                    brand_name += "BUGGY"; break;
     }
 
@@ -1171,8 +1172,8 @@ bool attack::apply_damage_brand(const char *what)
 
     if (!damage_done
         && (brand == SPWPN_FLAMING || brand == SPWPN_FREEZING
-            || brand == SPWPN_HOLY_WRATH || brand == SPWPN_ANTIMAGIC
-            || brand == SPWPN_VAMPIRISM))
+            || brand == SPWPN_HOLY_WRATH || brand == SPWPN_FOUL_FLAME
+            || brand == SPWPN_ANTIMAGIC || brand == SPWPN_VAMPIRISM))
     {
         // These brands require some regular damage to function.
         return false;
