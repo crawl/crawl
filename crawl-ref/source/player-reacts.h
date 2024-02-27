0d0dddbf9d6a864bdd6341e7e4e6805cbf33805c
David Lawrence Ramsey <pooka109@gmail.com>
2024-02-27 15:59:21 -0600

    extract_manticore_spikes() -> extract_barbs().
    Since barbs aren't exclusive to manticores.

--- crawl-ref/source/player-reacts.h
+++ crawl-ref/source/player-reacts.h
@@ -4,5 +4,5 @@ void player_reacts();
 void player_reacts_to_monsters();
 
 // Only function other than decrement_duratons() which uses decrement_a_duration()
-void extract_manticore_spikes(const char* endmsg);
+void extract_barbs(const char* endmsg);
 int current_horror_level(); // XXX: move?
