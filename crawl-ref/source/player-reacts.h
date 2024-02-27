a61f84b8bde00b1c40388e1b42465f523922443d
David Lawrence Ramsey <pooka109@gmail.com>
2024-02-27 16:01:25 -0600

    Fix comment typo.

--- crawl-ref/source/player-reacts.h
+++ crawl-ref/source/player-reacts.h
@@ -3,6 +3,6 @@
 void player_reacts();
 void player_reacts_to_monsters();
 
-// Only function other than decrement_duratons() which uses decrement_a_duration()
+// Only function other than decrement_durations() which uses decrement_a_duration()
 void extract_barbs(const char* endmsg);
 int current_horror_level(); // XXX: move?
