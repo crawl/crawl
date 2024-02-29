crawl/crawl

commit 4e4bab2fd2a8b753dc4cfd1b8ffeccd21831bb09

Add ARTP_MANA_REGENERATION
Functions the same as the amulet. Amulet of Vitality is now a plain amulet
with 2 levels of ARTP_MANA_REGENERATION instead of being a RegenMP amulet
that was special-cased to count twice.

Amulets of regeneration and magic regeneration used entirely different
methods to track whether the player was attuned to them or not, and this
unifies them, as well as refactoring a whole lot of code related to this.
(This also improves messaging a couple of cases, such as Amulet of Vitality
implying it doesn't work for Djinn at all when you first wear it). I also
reduced some needless scanning of inventory artifact props whenever the
player's regen ticks while they are already at full hp/mp.

There is one functional change here, which is that items which grant both
hp and mp regeneration (currently ONLY the Amulet of Vitality) will no
longer provide either until BOTH of the player's hp and mp are full
simultaneously. This irks me a little, but it seems minor enough that all
of the extra infrastructure required to avoid it (while still allowing
more than a single source of RegenMP to ever function at once) seems
unnecessary.

crawl-ref/source/player-equip.cc
@@ -184,8 +184,8 @@ static void _equip_jewellery_effect(item_def &item, bool unmeld,
 static void _unequip_jewellery_effect(item_def &item, bool mesg, bool meld,
                                       equipment_type slot);
 static void _equip_use_warning(const item_def& item);
-static void _equip_regeneration_item(const item_def& item);
-static void _deactivate_regeneration_item(const item_def& item, bool meld);
+static void _handle_regen_item_equip(const item_def& item);
+static void _deactivate_item(const item_def& item);
 
 static void _assert_valid_slot(equipment_type eq, equipment_type slot)
 {
@@ -230,6 +230,9 @@ void equip_effect(equipment_type slot, int item_slot, bool unmeld, bool msg)
         _equip_armour_effect(item, unmeld, slot);
     else if (slot >= EQ_FIRST_JEWELLERY && slot <= EQ_LAST_JEWELLERY)
         _equip_jewellery_effect(item, unmeld, slot);
+
+    if (!unmeld)
+        _handle_regen_item_equip(item);
 }
 
 void unequip_effect(equipment_type slot, int item_slot, bool meld, bool msg)
@@ -297,16 +300,6 @@ void equip_artefact_effect(item_def &item, bool *show_msgs, bool unmeld,
     {
         canned_msg(proprt[ARTP_MAGICAL_POWER] > 0 ? MSG_MANA_INCREASE
                                                   : MSG_MANA_DECREASE);
-    }
-
-    if (proprt[ARTP_REGENERATION]
-        && !unmeld
-        // If regen is an intrinsic property too, don't double print messages.
-        && !item.is_type(OBJ_JEWELLERY, AMU_REGENERATION)
-        && (item.base_type != OBJ_ARMOUR
-            || !armour_type_prop(item.sub_type, ARMF_REGENERATION)))
-    {
-        _equip_regeneration_item(item);
     }
 
     // Modify ability scores.
@@ -412,8 +405,8 @@ void unequip_artefact_effect(item_def &item,  bool *show_msgs, bool meld,
     if (proprt[ARTP_SEE_INVISIBLE])
         _mark_unseen_monsters();
 
-    if (proprt[ARTP_REGENERATION])
-        _deactivate_regeneration_item(item, meld);
+    if (proprt[ARTP_REGENERATION] && !meld)
+        _deactivate_item(item);
 
     if (is_unrandom_artefact(item))
     {
@@ -895,9 +888,6 @@ static void _equip_armour_effect(item_def& arm, bool unmeld,
 
     }
 
-    if (armour_type_prop(arm.sub_type, ARMF_REGENERATION) && !unmeld)
-        _equip_regeneration_item(arm);
-
     if (is_artefact(arm))
     {
         bool show_msgs = true;
@@ -1028,8 +1018,8 @@ static void _unequip_armour_effect(item_def& item, bool meld,
         break;
     }
 
-    if (armour_type_prop(item.sub_type, ARMF_REGENERATION))
-        _deactivate_regeneration_item(item, meld);
+    if (armour_type_prop(item.sub_type, ARMF_REGENERATION) && !meld)
+        _deactivate_item(item);
 
     if (is_artefact(item))
         unequip_artefact_effect(item, nullptr, meld, slot, false);
@@ -1064,10 +1054,24 @@ static void _remove_amulet_of_faith(item_def &item)
     lose_piety(piety_loss);
 }
 
-static void _equip_regeneration_item(const item_def &item)
+static void _handle_regen_item_equip(const item_def& item)
 {
+    const bool regen_hp = (is_artefact(item)
+                           && artefact_property(item, ARTP_REGENERATION))
+                          || item.is_type(OBJ_JEWELLERY, AMU_REGENERATION)
+                          || armour_type_prop(item.sub_type, ARMF_REGENERATION);
+
+    const bool regen_mp = (is_artefact(item)
+                            && artefact_property(item, ARTP_MANA_REGENERATION))
+                          || item.is_type(OBJ_JEWELLERY, AMU_MANA_REGENERATION);
+
+    if (!regen_hp && !regen_mp)
+        return;
+
+    // The item is providing at least one of these two things, so let's check
+    // attunement status.
+
     equipment_type eq_slot = item_equip_slot(item);
-    // currently regen is only on amulets and armour
     bool plural = (eq_slot == EQ_BOOTS && item.sub_type != ARM_BARDING)
                   || eq_slot == EQ_GLOVES;
     string item_name = is_artefact(item) ? get_artefact_name(item)
@@ -1078,22 +1082,46 @@ static void _equip_regeneration_item(const item_def &item)
                                          : item_slot_name(eq_slot);
 
 #if TAG_MAJOR_VERSION == 34
-    if (you.get_mutation_level(MUT_NO_REGENERATION))
+    if (regen_hp && !regen_mp && you.get_mutation_level(MUT_NO_REGENERATION))
     {
         mprf("The %s feel%s cold and inert.", item_name.c_str(),
              plural ? "" : "s");
         return;
     }
 #endif
-    if (you.hp == you.hp_max)
+    if (regen_mp && !regen_hp && !player_regenerates_mp())
     {
-        mprf("The %s throb%s to your uninjured body.", item_name.c_str(),
-             plural ? " as they attune themselves" : "s as it attunes itself");
+        mprf("The %s feel%s cold and inert.", item_name.c_str(),
+             plural ? "" : "s");
+
+        return;
+    }
+
+    bool activate = false;
+    if (regen_hp && you.hp == you.hp_max
+        && (!regen_mp || you.magic_points == you.max_magic_points))
+    {
+        activate = true;
+    }
+    else if (regen_mp && you.magic_points == you.max_magic_points
+             && (!regen_hp || you.hp == you.hp_max))
+    {
+        activate = true;
+    }
+
+    if (activate)
+    {
+        mprf("The %s throb%s to your%s body.", item_name.c_str(),
+             plural ? " as they attune themselves" : "s as it attunes itself",
+             regen_hp ? " uninjured" : "");
         you.activated.set(eq_slot);
         return;
     }
-    mprf("The %s cannot attune %s to your injured body.", item_name.c_str(),
-         plural ? "themselves" : "itself");
+
+    mprf("The %s cannot attune %s to your%s body.", item_name.c_str(),
+         plural ? "themselves" : "itself",
+         you.hp < you.hp_max ? " injured" : " exhausted");
+
     you.activated.set(eq_slot, false);
     return;
 }
@@ -1104,19 +1132,6 @@ bool acrobat_boost_active()
            && you.duration[DUR_ACROBAT]
            && (!you.caught())
            && (!you.is_constricted());
-}
-
-static void _equip_amulet_of_mana_regeneration()
-{
-    if (!player_regenerates_mp())
-        mpr("The amulet feels cold and inert.");
-    else if (you.magic_points == you.max_magic_points)
-        you.props[MANA_REGEN_AMULET_ACTIVE] = 1;
-    else
-    {
-        mpr("The amulet cannot attune itself to your exhausted body.");
-        you.props[MANA_REGEN_AMULET_ACTIVE] = 0;
-    }
 }
 
 static void _equip_amulet_of_reflection()
@@ -1204,21 +1219,11 @@ static void _equip_jewellery_effect(item_def &item, bool unmeld,
 
         break;
 
-    case AMU_REGENERATION:
-        if (!unmeld)
-            _equip_regeneration_item(item);
-        break;
-
     case AMU_ACROBAT:
         if (you.has_mutation(MUT_TENGU_FLIGHT))
             mpr("You feel no more acrobatic than usual.");
         else
             mpr("You feel ready to tumble and roll out of harm's way.");
-        break;
-
-    case AMU_MANA_REGENERATION:
-        if (!unmeld)
-            _equip_amulet_of_mana_regeneration();
         break;
 
     case AMU_REFLECTION:
@@ -1228,6 +1233,8 @@ static void _equip_jewellery_effect(item_def &item, bool unmeld,
     case AMU_GUARDIAN_SPIRIT:
         _spirit_shield_message(unmeld);
         break;
+
+    // Regeneration and Magic Regeneration amulets handled elsewhere.
     }
 
     if (artefact)
@@ -1240,10 +1247,9 @@ static void _equip_jewellery_effect(item_def &item, bool unmeld,
         mprf_nocap("%s", item.name(DESC_INVENTORY_EQUIP).c_str());
 }
 
-static void _deactivate_regeneration_item(const item_def &item, bool meld)
+static void _deactivate_item(const item_def& item)
 {
-    if (!meld)
-        you.activated.set(get_item_slot(item), false);
+    you.activated.set(get_item_slot(item), false);
 }
 
 static void _unequip_jewellery_effect(item_def &item, bool mesg, bool meld,
@@ -1265,7 +1271,8 @@ static void _unequip_jewellery_effect(item_def &item, bool mesg, bool meld,
         break;
 
     case AMU_REGENERATION:
-        _deactivate_regeneration_item(item, meld);
+        if (!meld)
+            _deactivate_item(item);
         break;
 
     case RING_SEE_INVISIBLE:
@@ -1322,7 +1329,7 @@ static void _unequip_jewellery_effect(item_def &item, bool mesg, bool meld,
 
     case AMU_MANA_REGENERATION:
         if (!meld)
-            you.props[MANA_REGEN_AMULET_ACTIVE] = 0;
+            _deactivate_item(item);
         break;
     }
 

crawl-ref/source/artefact-prop-type.h
@@ -87,5 +87,6 @@ enum artefact_prop_type
     ARTP_ENHANCE_EARTH,
     ARTP_ENHANCE_ALCHEMY,
     ARTP_ACROBAT,
+    ARTP_MANA_REGENERATION,
     ARTP_NUM_PROPERTIES
 };

crawl-ref/source/artefact.cc
@@ -381,6 +381,7 @@ struct jewellery_fake_artp
 
 static map<jewellery_type, vector<jewellery_fake_artp>> jewellery_artps = {
     { AMU_REGENERATION, { { ARTP_REGENERATION, 1 } } },
+    { AMU_MANA_REGENERATION, { { ARTP_MANA_REGENERATION, 1} } },
     { AMU_REFLECTION, { { ARTP_SHIELDING, AMU_REFLECT_SH / 2} } },
 
     { RING_MAGICAL_POWER, { { ARTP_MAGICAL_POWER, 9 } } },
@@ -850,6 +851,8 @@ static const artefact_prop_data artp_data[] =
 
     { "Acrobat", ARTP_VAL_BOOL, 0, // ARTP_ACROBAT,
         []() {return 1;}, nullptr, 0, 0},
+    { "RegenMP", ARTP_VAL_BOOL, 0,   // ARTP_MANA_REGENERATION,
+        []() { return 1; }, nullptr, 0, 0 },
 };
 COMPILE_CHECK(ARRAYSZ(artp_data) == ARTP_NUM_PROPERTIES);
 // weights sum to 1000

crawl-ref/source/describe.cc
@@ -445,6 +445,9 @@ static const vector<property_descriptor> & _get_all_artp_desc_data()
         { ARTP_ACROBAT,
             "It increases your evasion after moving or waiting.",
             prop_note::plain },
+        { ARTP_MANA_REGENERATION,
+            "It increases your rate of magic regeneration.",
+            prop_note::symbolic },
     };
     return data;
 }
@@ -509,6 +512,7 @@ static vector<string> _randart_propnames(const item_def& item,
         ARTP_NEGATIVE_ENERGY,
         ARTP_WILLPOWER,
         ARTP_REGENERATION,
+        ARTP_MANA_REGENERATION,
         ARTP_RMUT,
         ARTP_RCORR,
 
@@ -772,7 +776,8 @@ void desc_randart_props(const item_def &item, vector<string> &lines)
             sdesc = replace_all(sdesc, "%d", make_stringf("%+d", stval));
 
         if (desc.display_type == prop_note::symbolic
-            && desc.property != ARTP_REGENERATION) // symbolic, but no text modification
+            && desc.property != ARTP_REGENERATION // symbolic, but no text modification
+            && desc.property != ARTP_MANA_REGENERATION)
         {
             // for symbolic props, desc.desc is just the resist name, need
             // to fill in to get a complete sentence

crawl-ref/source/player-reacts.cc
@@ -910,31 +910,51 @@ static void _handle_emergency_flight()
     }
 }
 
-// Regen equipment only begins to function when full health is reached.
-static void _update_equipment_attunement_by_health()
+static bool _is_regen_item(const item_def& item)
 {
-    if (you.hp != you.hp_max
-#if TAG_MAJOR_VERSION == 34
-        || you.get_mutation_level(MUT_NO_REGENERATION)
-#endif
-        )
-    {
+    return is_artefact(item) && artefact_property(item, ARTP_REGENERATION)
+            || item.base_type == OBJ_ARMOUR
+              && armour_type_prop(item.sub_type, ARMF_REGENERATION)
+            || item.is_type(OBJ_JEWELLERY, AMU_REGENERATION);
+}
+
+static bool _is_mana_regen_item(const item_def& item)
+{
+    return is_artefact(item) && artefact_property(item, ARTP_MANA_REGENERATION)
+            || item.is_type(OBJ_JEWELLERY, AMU_MANA_REGENERATION);
+}
+
+// Regeneration and Magic Regeneration items only work if the player has reached
+// max hp/mp while they are being worn. This scans and updates such items when
+// the player refills their hp/mp.
+static void _maybe_attune_items(bool attune_regen, bool attune_mana_regen)
+{
+    if (!attune_regen && !attune_mana_regen)
         return;
-    }
 
     vector<string> eq_list;
     bool plural = false;
+
+    bool gained_regen = false;
+    bool gained_mana_regen = false;
 
     for (int slot = EQ_MIN_ARMOUR; slot <= EQ_MAX_WORN; ++slot)
     {
         if (you.melded[slot] || you.equip[slot] == -1 || you.activated[slot])
             continue;
         const item_def &arm = you.inv[you.equip[slot]];
-        if (is_artefact(arm) && artefact_property(arm, ARTP_REGENERATION)
-            || arm.base_type == OBJ_ARMOUR
-               && armour_type_prop(arm.sub_type, ARMF_REGENERATION)
-            || arm.is_type(OBJ_JEWELLERY, AMU_REGENERATION))
+
+        if ((attune_regen && _is_regen_item(arm)
+             && (you.magic_points == you.max_magic_points || !_is_mana_regen_item(arm)))
+            || (attune_mana_regen && _is_mana_regen_item(arm)
+                && (you.hp == you.hp_max || !_is_regen_item(arm))))
         {
+            // Track which properties we should notify the player they have gained.
+            if (!gained_regen && _is_regen_item(arm))
+                gained_regen = true;
+            if (!gained_mana_regen && _is_mana_regen_item(arm))
+                gained_mana_regen = true;
+
             eq_list.push_back(is_artefact(arm) ? get_artefact_name(arm) :
                 slot == EQ_AMULET ? "amulet" :
                 slot != EQ_BODY_ARMOUR ?
@@ -953,29 +973,14 @@ static void _update_equipment_attunement_by_health()
     if (eq_list.empty())
         return;
 
+    const char* msg = (gained_regen && gained_mana_regen) ? " health and magic"
+                       : (gained_regen ? "" : " magic");
+
     plural = plural || eq_list.size() > 1;
     string eq_str = comma_separated_line(eq_list.begin(), eq_list.end());
-    mprf("Your %s attune%s to your body as you begin to regenerate "
-         "more quickly.", eq_str.c_str(), plural ? " themselves" : "s itself");
-}
-
-// Amulet of magic regeneration needs to be worn while at full magic before it
-// begins to function.
-static void _update_mana_regen_amulet_attunement()
-{
-    if (you.wearing(EQ_AMULET, AMU_MANA_REGENERATION)
-        && player_regenerates_mp())
-    {
-        if (you.magic_points == you.max_magic_points
-            && you.props[MANA_REGEN_AMULET_ACTIVE].get_int() == 0)
-        {
-            you.props[MANA_REGEN_AMULET_ACTIVE] = 1;
-            mpr("Your amulet attunes itself to your body and you begin to "
-                "regenerate magic more quickly.");
-        }
-    }
-    else if (!you.melded[EQ_AMULET])
-        you.props[MANA_REGEN_AMULET_ACTIVE] = 0;
+    mprf("Your %s attune%s to your body and you begin to regenerate%s "
+         "more quickly.", eq_str.c_str(), plural ? " themselves" : "s itself",
+         msg);
 }
 
 // cjo: Handles player hp and mp regeneration. If the counter
@@ -989,6 +994,9 @@ static void _regenerate_hp_and_mp(int delay)
 {
     if (crawl_state.disables[DIS_PLAYER_REGEN])
         return;
+
+    const int old_hp = you.hp;
+    const int old_mp = you.magic_points;
 
     // HP Regeneration
     if (!you.duration[DUR_DEATHS_DOOR])
@@ -1012,28 +1020,29 @@ static void _regenerate_hp_and_mp(int delay)
 
     ASSERT_RANGE(you.hit_points_regeneration, 0, 100);
 
-    _update_equipment_attunement_by_health();
-
     // MP Regeneration
-    if (!player_regenerates_mp())
-        return;
-
-    if (you.magic_points < you.max_magic_points)
+    if (player_regenerates_mp())
     {
-        const int base_val = player_mp_regen();
-        int mp_regen_countup = div_rand_round(base_val * delay, BASELINE_DELAY);
-        you.magic_points_regeneration += mp_regen_countup;
+        if (you.magic_points < you.max_magic_points)
+        {
+            const int base_val = player_mp_regen();
+            int mp_regen_countup = div_rand_round(base_val * delay, BASELINE_DELAY);
+            you.magic_points_regeneration += mp_regen_countup;
+        }
+
+        while (you.magic_points_regeneration >= 100)
+        {
+            inc_mp(1);
+            you.magic_points_regeneration -= 100;
+        }
+
+        ASSERT_RANGE(you.magic_points_regeneration, 0, 100);
     }
 
-    while (you.magic_points_regeneration >= 100)
-    {
-        inc_mp(1);
-        you.magic_points_regeneration -= 100;
-    }
-
-    ASSERT_RANGE(you.magic_points_regeneration, 0, 100);
-
-    _update_mana_regen_amulet_attunement();
+    // Update attunement of regeneration items if our hp/mp has refilled.
+    _maybe_attune_items(you.hp != old_hp && you.hp == you.hp_max,
+                        you.magic_points != old_mp
+                        && you.magic_points == you.max_magic_points);
 }
 
 static void _handle_fugue(int delay)

crawl-ref/source/player.cc
@@ -1292,13 +1292,16 @@ int player_mp_regen()
     if (you.get_mutation_level(MUT_MANA_REGENERATION))
         regen_amount *= 2;
 
-    if (you.wearing(EQ_AMULET, AMU_MANA_REGENERATION)
-        && you.props[MANA_REGEN_AMULET_ACTIVE].get_int() == 1)
+    // Amulets and artefacts.
+    for (int slot = EQ_MIN_ARMOUR; slot <= EQ_MAX_WORN; ++slot)
     {
-        regen_amount += 40;
-        // grants a second pip on top of its base type
-        if (player_equip_unrand(UNRAND_VITALITY))
+        if (you.melded[slot] || you.equip[slot] == -1 || !you.activated[slot])
+            continue;
+        const item_def &arm = you.inv[you.equip[slot]];
+        if (arm.is_type(OBJ_JEWELLERY, AMU_MANA_REGENERATION))
             regen_amount += 40;
+        if (is_artefact(arm))
+            regen_amount += 40 * artefact_property(arm, ARTP_MANA_REGENERATION);
     }
 
     // Rampage healing grants a variable regen boost while active.

crawl-ref/source/player.h
@@ -45,7 +45,6 @@
 #define POWERED_BY_DEATH_KEY "powered_by_death_strength"
 #define FUGUE_KEY "fugue_of_the_fallen_bonus"
 #define FORCE_MAPPABLE_KEY "force_mappable"
-#define MANA_REGEN_AMULET_ACTIVE "mana_regen_amulet_active"
 #define TEMP_WATERWALK_KEY "temp_waterwalk"
 #define EMERGENCY_FLIGHT_KEY "emergency_flight"
 #define PARALYSED_BY_KEY "paralysed_by"

crawl-ref/source/util/art-data.pl
@@ -72,6 +72,7 @@ my %field_type = (
     RANDAPP  => "bool",
     RCORR    => "bool",
     REGEN    => "num",
+    REGEN_MP => "num",
     RMSL     => "bool",
     RMUT     => "bool",
     SEEINV   => "bool",
@@ -550,7 +551,7 @@ my @art_order = (
     "SH", "HARM", "RAMPAGE", "ARCHMAGI", "ENH_CONJ", "ENH_HEXES", "\n",
     "ENH_SUMM", "ENH_NECRO", "ENH_TLOC", "unused", "ENH_FIRE", "\n",
     "ENH_ICE", "ENH_AIR", "ENH_EARTH", "ENH_ALCH", "\n",
-    "ACROBAT",
+    "ACROBAT", "REGEN_MP",
     "}",
 # end TAG_MAJOR_VERSION
 # start TAG_MAJOR_VERSION == 35
@@ -565,7 +566,7 @@ my @art_order = (
 #     "SH", "HARM", "RAMPAGE", "ARCHMAGI", "ENH_CONJ", "ENH_HEXES", "\n",
 #     "ENH_SUMM", "ENH_NECRO", "ENH_TLOC", "ENH_FIRE", "\n",
 #     "ENH_ICE", "ENH_AIR", "ENH_EARTH", "ENH_ALCH", "\n",
-#     "ACROBAT",
+#     "ACROBAT", "REGEN_MP",
 #     "}",
 # end TAG_MAJOR_VERSION
 

commit 0d51e52c06fb14b85f866fa6261f96136ed3b4ed

Prevent crash on equip
prevents several crashes introduced  in 4e4bab2fd2a8b753dc4cfd1b8ffeccd21831bb09

crawl-ref/source/player-equip.cc
@@ -1059,7 +1059,8 @@ static void _handle_regen_item_equip(const item_def& item)
     const bool regen_hp = (is_artefact(item)
                            && artefact_property(item, ARTP_REGENERATION))
                           || item.is_type(OBJ_JEWELLERY, AMU_REGENERATION)
-                          || armour_type_prop(item.sub_type, ARMF_REGENERATION);
+                          || (item.base_type == OBJ_ARMOUR
+                              && (item.sub_type, ARMF_REGENERATION));
 
     const bool regen_mp = (is_artefact(item)
                             && artefact_property(item, ARTP_MANA_REGENERATION))