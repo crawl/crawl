69a5a376f162f9918cea96d3238b5f21cde13fc8
DracoOmega <draco.omega.dcss.dev@gmail.com>
2024-03-02 03:57:35 -0330

    Properly label gizmos on the " screen
    They did actually show up there, but the slot was labelled 'Unknown'.

--- crawl-ref/source/command.cc
+++ crawl-ref/source/command.cc
@@ -350,7 +350,8 @@ void list_jewellery()
                  (i == EQ_RING_SIX)    ? "6th ring" :
                  (i == EQ_RING_SEVEN)  ? "7th ring" :
                  (i == EQ_RING_EIGHT)  ? "8th ring" :
-                 (i == EQ_RING_AMULET) ? "Amulet ring"
+                 (i == EQ_RING_AMULET) ? "Amulet ring" :
+                 (i == EQ_GIZMO)       ? "Gizmo"
                                        : "unknown";
 
         string item;
