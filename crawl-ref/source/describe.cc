9348d4545b1f0e99e779ed8520b5e0b6a753634a
David Lawrence Ramsey <pooka109@gmail.com>
2024-03-05 00:55:50 -0600

    Clarify foul flame weapon brand description.
    Good god worshippers can't use foul flame-branded weapons, just as holy
    beings can't.

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -1760,7 +1760,8 @@ static string _describe_weapon_brand(const item_def &item)
         return "It has been infused with foul flame, dealing an additional "
                "three-quarters of damage to holy beings, an additional quarter "
                "damage to undead and demons, and an additional half damage to "
-               "all others. Holy beings cannot use this.";
+               "all others. Holy beings and good god worshippers cannot use "
+               "this.";
     case SPWPN_ELECTROCUTION:
         return "It sometimes electrocutes victims (1/4 chance, 8-20 damage).";
     case SPWPN_VENOM:
