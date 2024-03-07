2954f7671007c04e9378af2c1ab8d7ef04c5bd39
David Lawrence Ramsey <pooka109@gmail.com>
2024-03-06 20:37:41 -0600

    No jewellery acquirement if useless, redux.
    _find_acquirement_subtype() now accounts for the no-jewellery
    mutation, and won't generate jewellery with it.

--- crawl-ref/source/acquire.cc
+++ crawl-ref/source/acquire.cc
@@ -788,7 +788,8 @@ static int _find_acquirement_subtype(object_class_type &class_wanted,
     ASSERT(class_wanted != OBJ_RANDOM);
 
     if (class_wanted == OBJ_ARMOUR && !player_can_use_armour()
-        || class_wanted == OBJ_WEAPONS && you.has_mutation(MUT_NO_GRASPING))
+        || class_wanted == OBJ_WEAPONS && you.has_mutation(MUT_NO_GRASPING)
+        || class_wanted == OBJ_JEWELLERY && you.has_mutation(MUT_NO_JEWELLERY))
     {
         return OBJ_RANDOM;
     }
