bc5cafa0a1a79d9131010d6f608875ed6ed94f69
SentientSupper <trollinterminator@gmail.com>
2024-03-10 12:46:41 +0800

    New tile for slug shots
    Based on the iron shot tile but with less outline, and colors taken from
    enchanted boomerangs. Feels weird for the hand cannon description to say
    it shoots metal slugs while the actual tiles look like sling stones.
    
    Closes #3641.

--- crawl-ref/source/tilepick.cc
+++ crawl-ref/source/tilepick.cc
@@ -3083,6 +3083,9 @@ tileidx_t tileidx_item_throw(const item_def &item, int dx, int dy)
             case MI_THROWING_NET:
                 ch = TILE_MI_THROWING_NET0;
                 break;
+            case MI_SLUG:
+                ch = TILE_MI_SLUG0;
+                break;
             default:
                 break;
         }
@@ -3095,7 +3098,6 @@ tileidx_t tileidx_item_throw(const item_def &item, int dx, int dy)
             case MI_STONE:
                 ch = TILE_MI_STONE0;
                 break;
-            case MI_SLUG:
             case MI_SLING_BULLET:
                 switch (item.brand)
                 {
