crawl/crawl

commit 5fa0bccb637462735179f4649d3c13a62652af78

Remove AC, EV, SH breakpoints
Previously raising Armour, Dodging, or Shields skill purely for the
defensive benefits (as opposed to decreasing penalties of armour/shields)
did nothing except at discrete points where it suddenly gave you one extra
point of AC (or EV etc). With most skills we try to avoid this sort of
breakpoint so that players gain less benefit from micromanaging skill levels.

Although AC, EV, SH were all already computed to high precision, an internal
value of 8.37 AC was rounded to 8 AC before being used for anything. This
commit changes that - now a value of 8.37 AC means that 8 AC will be used
63% of the time and 9 AC will be used 37% of the time. This type of random
rounding is already used to smooth out the effects of skills in various
other points, including the effect of weapon skill on weapon delay.

The precise internal value of 8.37 AC is currently only visible to the
player when examining an equipment item and seeing how swapping it in will
change their AC. Previously the game might have said "Your AC would decrease
by 1 (8 -> 7)" whereas now it might say "Your AC would decrease by 0.61
(8.37 -> 7.76)". The display of player AC/EV/SH everywhere else is rounded
down to an integer, similar to how weapon delay is displayed rounded down
to a multiple of 0.1. I think there's a decent argument that both weapon
delay and AC/EV/SH (after this commit) should be displayed with one extra
digit of precision "normally", but that can be a separate discussion.

A note on balance: in general I expect this to only have very minor effects.
Previously AC was rounded down to an integer, EV was rounded up,
and SH was rounded to the nearest integer. This means that for a "random"
character not paying attention to the breakpoints, on average this commit
will increase their AC by 0.5 and decrease their EV by 0.5, while leaving
SH unchanged. For a player paying close attention to both AC and EV

crawl-ref/source/fight.cc
@@ -154,29 +154,40 @@ int mon_shield_bypass(int hd)
 }
 
 /**
- * Return the odds of a monster attack with the given to-hit bonus hitting the given ev with the
- * given EV, rounded to the nearest percent.
+ * Return the odds of a monster attack with the given to-hit bonus hitting the given ev (scaled by 100),
+ * rounded to the nearest percent.
  *
  * TODO: deduplicate with to_hit_pct().
  *
  * @return                  To-hit percent between 0 and 100 (inclusive).
  */
-int mon_to_hit_pct(int to_land, int ev)
+int mon_to_hit_pct(int to_land, int scaled_ev)
 {
     if (to_land >= AUTOMATIC_HIT)
         return 100;
 
-    if (ev <= 0)
+    if (scaled_ev <= 0)
         return 100 - MIN_HIT_MISS_PERCENTAGE / 2;
 
     ++to_land; // per calc_to_hit()
 
-    int hits = 0;
+    const int ev = scaled_ev/100;
+
+    // EV is random-rounded, so the actual value might be either ev or ev+1.
+    // We repeat the calculation below once for each case
+    int hits_lower = 0;
     for (int ev1 = 0; ev1 < ev; ev1++)
         for (int ev2 = 0; ev2 < ev; ev2++)
-            hits += max(0, to_land - (ev1 + ev2));
+            hits_lower += max(0, to_land - (ev1 + ev2));
+    double hit_chance_lower = ((double)hits_lower) / (to_land * ev * ev);
 
-    double hit_chance = ((double)hits) / (to_land * ev * ev);
+    int hits_upper = 0;
+    for (int ev1 = 0; ev1 < ev+1; ev1++)
+        for (int ev2 = 0; ev2 < ev+1; ev2++)
+            hits_upper += max(0, to_land - (ev1 + ev2));
+    double hit_chance_upper = ((double)hits_upper) / (to_land * (ev+1) * (ev+1));
+
+    double hit_chance = ((100 - (scaled_ev % 100)) * hit_chance_lower + (scaled_ev % 100) * hit_chance_upper) / 100;
 
     // Apply Bayes Theorem to account for auto hit and miss.
     hit_chance = hit_chance * (1 - MIN_HIT_MISS_PERCENTAGE / 200.0)
@@ -185,24 +196,44 @@ int mon_to_hit_pct(int to_land, int ev)
     return (int)(hit_chance*100);
 }
 
-int mon_beat_sh_pct(int bypass, int sh)
+int mon_beat_sh_pct(int bypass, int scaled_sh)
 {
-    if (sh <= 0)
+    if (scaled_sh <= 0)
         return 100;
 
-    sh *= 2; // per shield_bonus()
+    int sh = scaled_sh/100;
 
-    int hits = 0;
+    // SH is random-rounded, so the actual value might be either sh or sh+1.
+    // We repeat the calculation below once for each case
+    sh *= 2; // per shield_bonus()
+    int hits_lower = 0;
     for (int sh1 = 0; sh1 < sh; sh1++)
     {
         for (int sh2 = 0; sh2 < sh; sh2++)
         {
             int adj_sh = (sh1 + sh2) / (3*2) - 1;
-            hits += max(0, bypass - adj_sh);
+            hits_lower += max(0, bypass - adj_sh);
         }
     }
-    const int denom = sh * sh * bypass;
-    return hits * 100 / denom;
+    const int denom_lower = sh * sh * bypass;
+    double hit_chance_lower = ((double)hits_lower * 100) / denom_lower;
+
+    sh += 2; // since we already multiplied by 2
+    int hits_upper = 0;
+    for (int sh1 = 0; sh1 < sh; sh1++)
+    {
+        for (int sh2 = 0; sh2 < sh; sh2++)
+        {
+            int adj_sh = (sh1 + sh2) / (3*2) - 1;
+            hits_upper += max(0, bypass - adj_sh);
+        }
+    }
+    const int denom_upper = sh * sh * bypass;
+    double hit_chance_upper = ((double)hits_upper * 100) / denom_upper;
+
+    double hit_chance = ((100 - (scaled_sh % 100)) * hit_chance_lower + (scaled_sh % 100) * hit_chance_upper) / 100;
+
+    return (int)(hit_chance*100);
 }
 
 /**

crawl-ref/source/describe.cc
@@ -1825,11 +1825,11 @@ static string _describe_weapon_brand(const item_def &item)
     }
 }
 
-static string _describe_point_change(int points)
+static string _describe_point_change(float points)
 {
     string point_diff_description;
 
-    point_diff_description += make_stringf("%s by %d",
+    point_diff_description += make_stringf("%s by %.2f",
                                            points > 0 ? "increase" : "decrease",
                                            abs(points));
 
@@ -1837,20 +1837,20 @@ static string _describe_point_change(int points)
 }
 
 static string _describe_point_diff(int original,
-                                   int changed)
+                                   int changed, int scale = 100)
 {
     string description;
 
-    int difference = changed - original;
-
-    if (difference == 0)
+    if (original == changed)
         return "remain unchanged";
+
+    float difference = ((float)(changed - original)) / scale;
 
     description += _describe_point_change(difference);
     description += " (";
-    description += to_string(original);
+    description += make_stringf("%.2f", ((float)(original)) / scale);
     description += " -> ";
-    description += to_string(changed);
+    description += make_stringf("%.2f", ((float)(changed)) / scale);
     description += ")";
 
     return description;
@@ -1881,9 +1881,9 @@ static string _equipment_switchto_string(const item_def &item)
 static string _equipment_ac_ev_change_description(const item_def &item, bool remove = false)
 {
     // First, test if there is any EV change at all.
-    const int cur_ev = you.evasion(true);
-    const int new_ev = remove ? you.evasion_without_specific_item(item)
-                              : you.evasion_with_specific_item(item);
+    const int cur_ev = you.evasion_scaled(100, true);
+    const int new_ev = remove ? you.evasion_without_specific_item(100, item)
+                              : you.evasion_with_specific_item(100, item);
 
     // If we're previewing non-armour and there is no EV change, print no
     // extra description at all (since almost all items of these types will
@@ -1914,12 +1914,12 @@ static string _equipment_ac_ev_change_description(const item_def &item, bool rem
     {
         description += "\nYour AC would ";
 
-        const int new_ac = remove ? you.armour_class_with_one_removal(item)
-                                  : you.armour_class_with_one_sub(item);
-        description += _describe_point_diff(you.armour_class(), new_ac) + ".";
+        const int new_ac = remove ? you.armour_class_with_one_removal(100, item)
+                                  : you.armour_class_with_one_sub(100, item);
+        description += _describe_point_diff(you.armour_class_scaled(100), new_ac) + ".";
     }
 
-    description += "\nYour EV would " + _describe_point_diff(cur_ev, new_ev) + ".";
+    description += "\nYour EV would " + _describe_point_diff(cur_ev, new_ev, 100) + ".";
 
     return description;
 }
@@ -2114,8 +2114,13 @@ static string _describe_ammo(const item_def &item)
 
 static string _warlock_mirror_reflect_desc()
 {
-    const int SH = crawl_state.need_save ? player_shield_class() : 0;
-    const int reflect_chance = 100 * SH / omnireflect_chance_denom(SH);
+    const int scaled_SH = crawl_state.need_save ? player_shield_class(100, false) : 0;
+    const int SH = scaled_SH / 100;
+    // We use random-rounded SH, so take a weighted average of the
+    // chances with SH and SH+1.
+    const int reflect_chance_numer = (100 - (scaled_SH % 100)) * SH * omnireflect_chance_denom(SH+1) + (scaled_SH % 100) * (SH+1) * omnireflect_chance_denom(SH);
+    const int reflect_chance_denom = omnireflect_chance_denom(SH) * omnireflect_chance_denom(SH+1);
+    const int reflect_chance = reflect_chance_numer / reflect_chance_denom;
     return "\n\nWith your current SH, it has a " + to_string(reflect_chance) +
            "% chance to reflect attacks against your willpower and other "
            "normally unblockable effects.";
@@ -5283,16 +5288,16 @@ static void _describe_mons_to_hit(const monster_info& mi, ostringstream &result)
     // We ignore the EV penalty for not being able to see an enemy because, if you
     // can't see an enemy, you can't get a monster description for them. (Except through
     // ?/M, but let's neglect that for now.)
-    const int ev = you.evasion();
+    const int scaled_ev = you.evasion_scaled(100);
 
     const int to_land = weapon && is_unrandom_artefact(*weapon, UNRAND_SNIPER) ? AUTOMATIC_HIT :
                                                                 total_base_hit + post_roll_modifiers;
-    const int beat_ev_chance = mon_to_hit_pct(to_land, ev);
+    const int beat_ev_chance = mon_to_hit_pct(to_land, scaled_ev);
 
-    const int shield_class = player_shield_class();
+    const int scaled_sh = player_shield_class(100, false);
     const int shield_bypass = mon_shield_bypass(mi.hd);
     // ignore penalty for unseen attacker, as with EV above
-    const int beat_sh_chance = mon_beat_sh_pct(shield_bypass, shield_class);
+    const int beat_sh_chance = mon_beat_sh_pct(shield_bypass, scaled_sh);
 
     const int hit_chance = beat_ev_chance * beat_sh_chance / 100;
     result << uppercase_first(mi.pronoun(PRONOUN_SUBJECTIVE)) << " "

crawl-ref/source/fight.h
@@ -69,9 +69,9 @@ void attack_multiple_targets(actor &attacker, list<actor*> &targets,
 class attack;
 int to_hit_pct(const monster_info& mi, attack &atk, bool melee);
 int mon_to_hit_base(int hd, bool skilled);
-int mon_to_hit_pct(int to_land, int ev);
+int mon_to_hit_pct(int to_land, int scaled_ev);
 int mon_shield_bypass(int hd);
-int mon_beat_sh_pct(int shield_bypass, int shield_class);
+int mon_beat_sh_pct(int bypass, int scaled_sh);
 
 int weapon_min_delay_skill(const item_def &weapon);
 int weapon_min_delay(const item_def &weapon, bool check_speed = true);

crawl-ref/source/hiscores.cc
@@ -1749,8 +1749,8 @@ void scorefile_entry::init(time_t dt)
     intel = you.stat(STAT_INT, false);
     dex   = you.stat(STAT_DEX, false);
 
-    ac    = you.armour_class();
-    ev    = you.evasion();
+    ac    = you.armour_class_scaled(1);
+    ev    = you.evasion_scaled(1);
     sh    = player_displayed_shield_class();
 
     god = you.religion;

crawl-ref/source/l-you.cc
@@ -150,12 +150,12 @@ LUARET1(you_base_mp, number, get_real_mp(false))
  * @treturn int
  * @function ac
  */
-LUARET1(you_ac, number, you.armour_class())
+LUARET1(you_ac, number, you.armour_class_scaled(1))
 /*** Evasion.
  * @treturn int
  * @function ev
  */
-LUARET1(you_ev, number, you.evasion())
+LUARET1(you_ev, number, you.evasion_scaled(1))
 /*** Shield class.
  * @treturn int
  * @function sh

crawl-ref/source/output.cc
@@ -620,12 +620,12 @@ static bool _boosted_mp()
 
 static bool _boosted_ac()
 {
-    return you.armour_class() > you.base_ac(1);
+    return you.armour_class_scaled(100) > you.base_ac(100);
 }
 
 static bool _boosted_ev()
 {
-    return you.evasion() > you.evasion(true);
+    return you.evasion_scaled(100) > you.evasion_scaled(100, true);
 }
 
 static bool _boosted_sh()
@@ -1031,7 +1031,7 @@ static void _print_stats_ac(int x, int y)
     else if (you.corrosion_amount())
         text_col = RED;
 
-    string ac = make_stringf("%2d ", you.armour_class());
+    string ac = make_stringf("%2d ", you.armour_class_scaled(1));
 #ifdef WIZARD
     if (you.wizard && !_is_using_small_layout())
         ac += make_stringf("(%d%%) ", you.gdr_perc());
@@ -1060,11 +1060,11 @@ static void _print_stats_ev(int x, int y)
     CGOTOXY(x+4, y, GOTO_STAT);
 
     // Color EV based on whether temporary effects are raising or lowering it
-    const int bonus = you.evasion() - you.evasion(true);
+    const int bonus = you.evasion_scaled(100) - you.evasion_scaled(100, true);
     textcolour(bonus < 0 ? RED
                          : bonus > 0 ? LIGHTBLUE
                                      : HUD_VALUE_COLOUR);
-    CPRINTF("%2d ", you.evasion());
+    CPRINTF("%2d ", you.evasion_scaled(1));
 
     you.redraw_evasion = false;
 }
@@ -2332,7 +2332,7 @@ static vector<formatted_string> _get_overview_stats()
     else
         entry.textcolour(HUD_VALUE_COLOUR);
 
-    entry.cprintf("%2d", you.armour_class());
+    entry.cprintf("%2d", you.armour_class_scaled(1));
 
     cols.add_formatted(1, entry.to_colour_string(), false);
     entry.clear();
@@ -2345,7 +2345,7 @@ static vector<formatted_string> _get_overview_stats()
     else
         entry.textcolour(HUD_VALUE_COLOUR);
 
-    entry.cprintf("%2d", you.evasion());
+    entry.cprintf("%2d", you.evasion_scaled(1));
 
     cols.add_formatted(1, entry.to_colour_string(), false);
     entry.clear();

crawl-ref/source/player.cc
@@ -2190,7 +2190,7 @@ static int _player_armour_adjusted_dodge_bonus(int scale)
 }
 
 // Total EV for player
-static int _player_evasion(bool ignore_temporary)
+static int _player_evasion(int final_scale, bool ignore_temporary)
 {
     const int size_factor = _player_evasion_size_factor();
     const int scale = 100;
@@ -2207,7 +2207,7 @@ static int _player_evasion(bool ignore_temporary)
 
     // Everything below this are transient modifiers
     if (ignore_temporary)
-        return unscale_round_up(natural_evasion, scale);
+        return (natural_evasion * final_scale) / scale;
 
     // Apply temporary bonuses, penalties, and multipliers
     int final_evasion =
@@ -2223,7 +2223,7 @@ static int _player_evasion(bool ignore_temporary)
                             final_evasion);
     }
 
-    return unscale_round_up(final_evasion, scale);
+    return (final_evasion * final_scale) / scale;
 }
 
 // Returns the spellcasting penalty (increase in spell failure) for the
@@ -2291,7 +2291,7 @@ static int _sh_from_shield(const item_def &item)
  * Exactly twice the value displayed to players, for legacy reasons.
  * @return      The player's current SH value.
  */
-int player_shield_class()
+int player_shield_class(int scale, bool random)
 {
     int shield = 0;
 
@@ -2319,7 +2319,7 @@ int player_shield_class()
     shield += you.wearing(EQ_AMULET, AMU_REFLECTION) * AMU_REFLECT_SH * 100;
     shield += you.scan_artefacts(ARTP_SHIELDING) * 200;
 
-    return (shield + 50) / 100;
+    return random ? div_rand_round(shield * scale, 100) : ((shield * scale) / 100);
 }
 
 /**
@@ -2330,7 +2330,7 @@ int player_shield_class()
  */
 int player_displayed_shield_class()
 {
-    return player_shield_class() / 2;
+    return player_shield_class(1, false) / 2;
 }
 
 /**
@@ -6442,18 +6442,23 @@ int player::base_ac(int scale) const
 
 int player::armour_class() const
 {
-    return armour_class_with_specific_items(get_armour_items());
+    return div_rand_round(armour_class_scaled(100), 100);
 }
 
-int player::armour_class_with_one_sub(item_def sub) const
+int player::armour_class_scaled(int scale) const
 {
-    return armour_class_with_specific_items(
+    return armour_class_with_specific_items(scale, get_armour_items());
+}
+
+int player::armour_class_with_one_sub(int scale, item_def sub) const
+{
+    return armour_class_with_specific_items(scale,
                             get_armour_items_one_sub(sub));
 }
 
-int player::armour_class_with_one_removal(item_def removed) const
+int player::armour_class_with_one_removal(int scale, item_def removed) const
 {
-    return armour_class_with_specific_items(
+    return armour_class_with_specific_items(scale,
                             get_armour_items_one_removal(removed));
 }
 
@@ -6482,10 +6487,10 @@ static int _meek_bonus()
     return min(max(0, (scale_top - you.hp) / hp_per_ac), max_ac);
 }
 
-int player::armour_class_with_specific_items(vector<const item_def *> items) const
+int player::armour_class_with_specific_items(int scale,
+                                 vector<const item_def *> items) const
 {
-    const int scale = 100;
-    int AC = base_ac_with_specific_items(scale, items);
+    int AC = base_ac_with_specific_items(100, items);
 
     if (duration[DUR_ICY_ARMOUR])
     {
@@ -6497,7 +6502,7 @@ int player::armour_class_with_specific_items(vector<const item_def *> items) con
         AC += 100 * player_icemail_armour_class();
 
     if (duration[DUR_FIERY_ARMOUR])
-        AC += 7 * scale;
+        AC += 700;
 
     if (duration[DUR_QAZLAL_AC])
         AC += 300;
@@ -6506,7 +6511,7 @@ int player::armour_class_with_specific_items(vector<const item_def *> items) con
     {
         AC += 700;
         if (player_equip_unrand(UNRAND_MEEK))
-            AC += _meek_bonus() * scale;
+            AC += _meek_bonus() * 100;
     }
 
     if (you.wearing_ego(EQ_GIZMO, SPGIZMO_PARRYREV))
@@ -6516,13 +6521,13 @@ int player::armour_class_with_specific_items(vector<const item_def *> items) con
     }
 
     if (you.props.exists(PASSWALL_ARMOUR_KEY))
-        AC += you.props[PASSWALL_ARMOUR_KEY].get_int() * scale;
+        AC += you.props[PASSWALL_ARMOUR_KEY].get_int() * 100;
 
     AC -= 100 * corrosion_amount();
 
     AC += sanguine_armour_bonus();
 
-    return AC / scale;
+    return AC * scale / 100;
 }
 
 void player::refresh_rampage_hints()
@@ -6562,7 +6567,7 @@ int player::gdr_perc() const
  */
 int player::evasion(bool ignore_temporary, const actor* act) const
 {
-    int base_evasion = _player_evasion(ignore_temporary);
+    int base_evasion = div_rand_round(_player_evasion(100, ignore_temporary), 100);
 
     const bool attacker_invis = act && !act->visible_to(this);
     const int invis_penalty
@@ -6571,9 +6576,20 @@ int player::evasion(bool ignore_temporary, const actor* act) const
     return base_evasion - invis_penalty;
 }
 
+int player::evasion_scaled(int scale, bool ignore_temporary, const actor* act) const
+{
+    int base_evasion = _player_evasion(scale, ignore_temporary);
+
+    const bool attacker_invis = act && !act->visible_to(this);
+    const int invis_penalty
+        = attacker_invis && !ignore_temporary ? 10 : 0;
+
+    return base_evasion - invis_penalty * scale;
+}
+
 // What would our natural EV be if we wore a given piece of armour instead of
 // whatever might be in that slot currently (if anything)?
-int player::evasion_with_specific_item(const item_def& new_item) const
+int player::evasion_with_specific_item(int scale, const item_def& new_item) const
 {
     // Since there are a lot of things which can affect the calculation of EV,
     // including artifact properties on either the item we're equipped or the
@@ -6613,7 +6629,7 @@ int player::evasion_with_specific_item(const item_def& new_item) const
     }
 
     // Now, simply calculate evasion without temporary boosts.
-    int ret = evasion(true);
+    int ret = evasion_scaled(scale, true);
 
     // Restore old item and clear out any item copies, just in case.
     you.equip[slot] = old_item;
@@ -6622,7 +6638,7 @@ int player::evasion_with_specific_item(const item_def& new_item) const
     return ret;
 }
 
-int player::evasion_without_specific_item(const item_def& item_to_remove) const
+int player::evasion_without_specific_item(int scale, const item_def& item_to_remove) const
 {
     int slot = get_equip_slot(&item_to_remove);
 
@@ -6632,7 +6648,7 @@ int player::evasion_without_specific_item(const item_def& item_to_remove) const
 
     // Briefly remove item, calculate EV, then put it back on
     you.equip[slot] = -1;
-    int ret = evasion(true);
+    int ret = evasion_scaled(scale, true);
     you.equip[slot] = item_to_remove.link;
 
     return ret;

crawl-ref/source/player.h
@@ -848,6 +848,8 @@ public:
     int gdr_perc() const override;
     int evasion(bool ignore_temporary = false,
                 const actor *attacker = nullptr) const override;
+    int evasion_scaled(int scale, bool ignore_temporary = false,
+                const actor *attacker = nullptr) const;
 
     int stat_hp() const override     { return hp; }
     int stat_maxhp() const override  { return hp_max; }
@@ -865,8 +867,8 @@ public:
     int adjusted_shield_penalty(int scale = 1) const;
 
     // Calculates total permanent EV if the player was/wasn't wearing a given item
-    int evasion_with_specific_item(const item_def& new_item) const;
-    int evasion_without_specific_item(const item_def& item_to_remove) const;
+    int evasion_with_specific_item(int scale, const item_def& new_item) const;
+    int evasion_without_specific_item(int scale, const item_def& item_to_remove) const;
 
     bool wearing_light_armour(bool with_skill = false) const;
     int  skill(skill_type skill, int scale = 1, bool real = false,
@@ -942,9 +944,11 @@ public:
 
     bool clear_far_engulf(bool force = false, bool moved = false) override;
 
-    int armour_class_with_one_sub(item_def sub) const;
+    int armour_class_scaled(int scale) const;
 
-    int armour_class_with_one_removal(item_def sub) const;
+    int armour_class_with_one_sub(int scale, item_def sub) const;
+
+    int armour_class_with_one_removal(int scale, item_def sub) const;
 
     int ac_changes_from_mutations() const;
     vector<const item_def *> get_armour_items() const;
@@ -952,7 +956,7 @@ public:
     vector<const item_def *> get_armour_items_one_removal(const item_def& sub) const;
     int base_ac_with_specific_items(int scale,
                                     vector<const item_def *> armour_items) const;
-    int armour_class_with_specific_items(
+    int armour_class_with_specific_items(int scale,
                                 vector<const item_def *> items) const;
 
 protected:
@@ -1057,7 +1061,7 @@ int player_res_poison(bool allow_random = true, bool temp = true,
                       bool items = true);
 int player_willpower(bool temp = true);
 
-int player_shield_class();
+int player_shield_class(int scale = 1, bool random = true);
 int player_displayed_shield_class();
 bool player_omnireflects();
 

commit a2195036411c0d9ed7231185c14486741db67dda

Don't display monster to hit with 100x the chance
When examining a monster when your sh > 0. Hopefully the math isn't
wrong. Fixes an issue introduced in 5fa0bccb63.

crawl-ref/source/fight.cc
@@ -216,7 +216,7 @@ int mon_beat_sh_pct(int bypass, int scaled_sh)
         }
     }
     const int denom_lower = sh * sh * bypass;
-    double hit_chance_lower = ((double)hits_lower * 100) / denom_lower;
+    double hit_chance_lower = ((double)hits_lower) / denom_lower;
 
     sh += 2; // since we already multiplied by 2
     int hits_upper = 0;
@@ -229,7 +229,7 @@ int mon_beat_sh_pct(int bypass, int scaled_sh)
         }
     }
     const int denom_upper = sh * sh * bypass;
-    double hit_chance_upper = ((double)hits_upper * 100) / denom_upper;
+    double hit_chance_upper = ((double)hits_upper) / denom_upper;
 
     double hit_chance = ((100 - (scaled_sh % 100)) * hit_chance_lower + (scaled_sh % 100) * hit_chance_upper) / 100;
 