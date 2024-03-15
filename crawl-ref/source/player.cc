128e7171b3c8aa3136df48b8a84cb4f7f027275a
SentientSupper <trollinterminator@gmail.com>
2024-03-16 00:16:41 +0800

    Don't evolve while undead
    Closes #3535

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -2634,7 +2634,7 @@ void apply_exp()
     _handle_god_wrath(exp_gained);
 
     // evolution mutation timer
-    if (you.attribute[ATTR_EVOL_XP] > 0)
+    if (you.attribute[ATTR_EVOL_XP] > 0 && you.can_safely_mutate(true))
         you.attribute[ATTR_EVOL_XP] -= exp_gained;
 
     // modified experience due to sprint inflation
