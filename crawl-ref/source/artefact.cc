crawl/crawl

commit 55136d7841389a13275f47e37acfe5727eb7597f

Begin data-ifying staves

crawl-ref/source/item-name.cc
@@ -1147,23 +1147,6 @@ static const char* staff_primary_string(uint32_t p)
     return primary_strings[p % ARRAYSZ(primary_strings)];
 }
 
-static const char* staff_type_name(int stafftype)
-{
-    switch ((stave_type)stafftype)
-    {
-    case STAFF_FIRE:        return "fire";
-    case STAFF_COLD:        return "cold";
-    case STAFF_ALCHEMY:     return "alchemy";
-    case STAFF_DEATH:       return "death";
-    case STAFF_CONJURATION: return "conjuration";
-    case STAFF_AIR:         return "air";
-    case STAFF_EARTH:       return "earth";
-    default:                return item_type_removed(OBJ_STAVES, stafftype)
-                                ? "removedness"
-                                : "bugginess";
-    }
-}
-
 const char *base_type_string(const item_def &item)
 {
     return base_type_string(item.base_type);
@@ -1206,6 +1189,7 @@ string sub_type_string(const item_def &item, bool known)
     {
     case OBJ_WEAPONS:  // deliberate fall through, as XXX_prop is a local
     case OBJ_MISSILES: // variable to item-prop.cc.
+    case OBJ_STAVES:
     case OBJ_ARMOUR:
         return item_base_name(type, sub_type);
     case OBJ_WANDS: return _wand_type_name(sub_type);
@@ -1267,7 +1251,6 @@ string sub_type_string(const item_def &item, bool known)
             return string("book of ") + _book_type_name(sub_type);
         }
     }
-    case OBJ_STAVES: return staff_type_name(static_cast<stave_type>(sub_type));
 #if TAG_MAJOR_VERSION == 34
     case OBJ_RODS:   return "removed rod";
 #endif
@@ -1910,7 +1893,7 @@ string item_def::name_aux(description_level_type desc, bool terse, bool ident,
             buff << "staff";
         }
         else
-            buff << "staff of " << staff_type_name(item_typ);
+            buff << "staff of " << item_base_name(OBJ_STAVES, item_typ);
 
         if (cursed() && terse && !dbname && !qualname)
             buff << " (curse)";

crawl-ref/source/item-prop.cc
@@ -767,6 +767,40 @@ static const weapon_def Weapon_prop[] =
 
 };
 
+struct staff_def
+{
+    stave_type id;
+    const char* name;
+    skill_type skill;
+    beam_type resist;
+    int damage_mult;
+};
+static int Staff_index[NUM_STAVES];
+static const staff_def Staff_prop[] =
+{
+#if TAG_MAJOR_VERSION == 34
+    { STAFF_WIZARDRY,    "wizardry" },
+    { STAFF_POWER,       "power" },
+#endif
+    { STAFF_FIRE,        "fire",        SK_FIRE_MAGIC,   BEAM_FIRE,        50 },
+    { STAFF_COLD,        "cold",        SK_ICE_MAGIC,    BEAM_COLD,        50 },
+    { STAFF_ALCHEMY,     "alchemy",     SK_ALCHEMY,      BEAM_POISON,      50 },
+#if TAG_MAJOR_VERSION == 34
+    { STAFF_ENERGY,      "energy" },
+#endif
+    { STAFF_DEATH,       "death",       SK_NECROMANCY,   BEAM_NEG,         50 },
+    { STAFF_CONJURATION, "conjuration", SK_CONJURATIONS, BEAM_MMISSILE,    50 },
+#if TAG_MAJOR_VERSION == 34
+    { STAFF_ENCHANTMENT, "enchantment" },
+    { STAFF_SUMMONING,   "summoning" },
+#endif
+    { STAFF_AIR,         "air",         SK_AIR_MAGIC,    BEAM_ELECTRICITY, 50 },
+    { STAFF_EARTH,       "earth",       SK_EARTH_MAGIC,  BEAM_MMISSILE,    67 },
+#if TAG_MAJOR_VERSION == 34
+    { STAFF_CHANNELING,  "channeling" },
+#endif
+};
+
 struct missile_def
 {
     int         id;
@@ -890,6 +924,7 @@ void init_properties()
     // about too few, so we check this ourselves.
     COMPILE_CHECK(NUM_ARMOURS   == ARRAYSZ(Armour_prop));
     COMPILE_CHECK(NUM_WEAPONS   == ARRAYSZ(Weapon_prop));
+    COMPILE_CHECK(NUM_STAVES    == ARRAYSZ(Staff_prop));
     COMPILE_CHECK(NUM_MISSILES  == ARRAYSZ(Missile_prop));
     COMPILE_CHECK(NUM_GEM_TYPES == ARRAYSZ(Gem_prop));
 #if TAG_MAJOR_VERSION == 34
@@ -901,6 +936,9 @@ void init_properties()
 
     for (int i = 0; i < NUM_WEAPONS; i++)
         Weapon_index[ Weapon_prop[i].id ] = i;
+
+    for (int i = 0; i < NUM_STAVES; i++)
+        Staff_index[ Staff_prop[i].id ] = i;
 
     for (int i = 0; i < NUM_MISSILES; i++)
         Missile_index[ Missile_prop[i].id ] = i;
@@ -3147,6 +3185,8 @@ string item_base_name(object_class_type type, int sub_type)
     {
     case OBJ_WEAPONS:
         return Weapon_prop[Weapon_index[sub_type]].name;
+    case OBJ_STAVES:
+        return Staff_prop[Staff_index[sub_type]].name;
     case OBJ_MISSILES:
         return Missile_prop[Missile_index[sub_type]].name;
     case OBJ_ARMOUR:

crawl-ref/source/item-prop.h
@@ -100,6 +100,8 @@ brand_type get_weapon_brand(const item_def &item) PURE;
 special_armour_type get_armour_ego_type(const item_def &item) PURE;
 special_missile_type get_ammo_brand(const item_def &item) PURE;
 
+// staff functions:
+
 // armour functions:
 bool armour_is_enchantable(const item_def &item) PURE;
 int armour_max_enchant(const item_def &item) PURE;

commit 5aa55ea52e65156e2bd2694bee996a9289cdb732

Fix randart staff names
Fixes an issue introduced in 55136d7

crawl-ref/source/artefact.cc
@@ -1432,7 +1432,7 @@ string make_artefact_name(const item_def &item, bool appearance)
     // get base type
     lookup += _get_artefact_type(item, appearance);
 
-    const string base_name = _base_name(item);
+    const string base_name = item.base_type == OBJ_STAVES ? "" : _base_name(item);
     if (appearance)
     {
         string appear = getRandNameString(lookup, " appearance");

crawl-ref/source/item-name.cc
@@ -1876,7 +1876,7 @@ string item_def::name_aux(description_level_type desc, bool terse, bool ident,
         if (is_artefact(*this) && !dbname)
         {
             if (know_type)
-                buff << "staff";
+                buff << item_base_name(OBJ_STAVES, item_typ) << " staff";
             // TODO: crop long artefact names when not controlled by webtiles
             buff << get_artefact_name(*this, ident);
             if (!know_type)