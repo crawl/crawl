crawl/crawl

commit 926e5b6e4c1ff7608bc4a4bdb60678fc7a80099a

Revamp Draconian breath weapons
There's been some desire to revamp how draconian breath weapons work for
a while now, so this is a serious attempt at doing so. The general idea is
to make pressing the button less common (ie: avoid it being 'correct' to
breathe at the start of every latergame fight, even when the breath does
very little) while also making the breath effects flashier and more
impactful when you do.

All breath weapons can now store up to 3 charges, and are on an XP-based
recharge system. (You get 2 charges immediately upon hitting level 7 and
upgraded saves above xl 7 will be given 3). All breaths have been adjusted
somewhat - with some being overhauled more significantly - both to be more
distinctive from each other and to scale better throughout the length of
the game.

Red Draconians: Combustion Breath

Borrowing functionality from old Explosive Bolt, Combusion Breath causes a
fireball to strike every target in a line. It does less single target
damage than most breaths, but can do significant AoE damage if you line up
a good shot. (The user is immune to damage from these explosions).

Green Draconians: Noxious Breath

Very similar to their old breath, but the clouds last longer (especially at
higher power) and it has a power-based chance to spawn additional clouds
along the sides of the main beam as you rise in xl.

Pale Draconians: Steam Breath

Again, very similar to their old breath, but the initial hit now does
relevant damage, and the density/duration of the steam clouds improves with
xl. (Keep in mind, using it is now much less free than it used to be.)

Yellow Draconians: Caustic Breath

Fires a penetrating acid bolt that leaves acidic fog clouds on all
unoccupied spaces it passes over. These are rarely seen clouds (which
yellow draconians are conveniently immune to!). Early on, these can serve
as obstructions to enemies, and later on a reliable damage source that few
things are immune to.

Black Draconians: Galvanic Breath

crawl-ref/source/ability.cc
@@ -65,6 +65,7 @@
 #include "religion.h"
 #include "rltiles/tiledef-icons.h"
 #include "skills.h"
+#include "species.h"
 #include "spl-book.h"
 #include "spl-cast.h"
 #include "spl-clouds.h"
@@ -324,26 +325,34 @@ static vector<ability_def> &_get_ability_list()
             abflag::breath | abflag::dir_or_target },
         { ABIL_BREATHE_FIRE, "Breathe Fire",
             0, 0, 0, 5, {fail_basis::xl, 30, 1},
-            abflag::breath |  abflag::dir_or_target },
-        { ABIL_BREATHE_FROST, "Breathe Frost",
-            0, 0, 0, 5, {fail_basis::xl, 30, 1},
             abflag::breath | abflag::dir_or_target },
+        { ABIL_COMBUSTION_BREATH, "Combustion Breath",
+            0, 0, 0, 5, {fail_basis::xl, 30, 1},
+            abflag::drac_charges },
+        { ABIL_GLACIAL_BREATH, "Glacial Breath",
+            0, 0, 0, 5, {fail_basis::xl, 30, 1},
+            abflag::drac_charges },
         { ABIL_BREATHE_POISON, "Breathe Poison Gas",
             0, 0, 0, 6, {fail_basis::xl, 30, 1},
             abflag::breath | abflag::dir_or_target },
-        { ABIL_BREATHE_MEPHITIC, "Breathe Noxious Fumes",
+        { ABIL_NOXIOUS_BREATH, "Noxious Breath",
             0, 0, 0, 6, {fail_basis::xl, 30, 1},
-            abflag::breath | abflag::dir_or_target },
-        { ABIL_BREATHE_LIGHTNING, "Breathe Lightning",
-            0, 0, 0, LOS_MAX_RANGE, {fail_basis::xl, 30, 1}, abflag::breath },
-        { ABIL_BREATHE_POWER, "Breathe Dispelling Energy",
+            abflag::drac_charges },
+        { ABIL_GALVANIC_BREATH, "Galvanic Breath",
             0, 0, 0, LOS_MAX_RANGE, {fail_basis::xl, 30, 1},
-            abflag::breath | abflag::dir_or_target | abflag::not_self },
-        { ABIL_BREATHE_STEAM, "Breathe Steam",
+            abflag::drac_charges },
+        { ABIL_NULLIFYING_BREATH, "Nullifying Breath",
+            0, 0, 0, LOS_MAX_RANGE, {fail_basis::xl, 30, 1},
+            abflag::drac_charges },
+        { ABIL_STEAM_BREATH, "Steam Breath",
             0, 0, 0, 6, {fail_basis::xl, 20, 1},
-            abflag::breath | abflag::dir_or_target },
-        { ABIL_BREATHE_ACID, "Breathe Acid",
-            0, 0, 0, 3, {fail_basis::xl, 30, 1}, abflag::breath },
+            abflag::drac_charges },
+        { ABIL_CAUSTIC_BREATH, "Caustic Breath",
+            0, 0, 0, 3, {fail_basis::xl, 30, 1},
+            abflag::drac_charges },
+        { ABIL_MUD_BREATH, "Mud Breath",
+            0, 0, 0, 3, {fail_basis::xl, 30, 1},
+            abflag::drac_charges },
         { ABIL_TRAN_BAT, "Bat Form",
             2, 0, 0, -1, {fail_basis::xl, 45, 2}, abflag::none },
         { ABIL_EXSANGUINATE, "Exsanguinate",
@@ -761,13 +770,12 @@ static int _ability_zap_pow(ability_type abil)
     {
         case ABIL_SPIT_POISON:
             return 10 + you.experience_level;
-        case ABIL_BREATHE_ACID:
+        case ABIL_CAUSTIC_BREATH:
         case ABIL_BREATHE_FIRE:
-        case ABIL_BREATHE_FROST:
+        case ABIL_GLACIAL_BREATH:
         case ABIL_BREATHE_POISON:
-        case ABIL_BREATHE_POWER:
-        case ABIL_BREATHE_STEAM:
-        case ABIL_BREATHE_MEPHITIC:
+        case ABIL_NULLIFYING_BREATH:
+        case ABIL_NOXIOUS_BREATH:
             return you.form == transformation::dragon
                                  ? 2 * you.experience_level
                                  : you.experience_level;
@@ -919,6 +927,13 @@ const string make_cost_description(ability_type ability)
     if (abil.flags & abflag::breath)
         ret += ", Breath";
 
+    if (abil.flags & abflag::drac_charges)
+    {
+        ret += make_stringf(", %d/%d uses available",
+                    draconian_breath_uses_available(),
+                    MAX_DRACONIAN_BREATH);
+    }
+
     if (abil.flags & abflag::delay)
         ret += ", Delay";
 
@@ -1060,6 +1075,9 @@ static const string _detailed_cost_description(ability_type ability)
         ret << ".";
     }
 
+    if (abil.flags & abflag::drac_charges)
+        ret << "\nGaining experience will replenish charges of this ability.";
+
 #if TAG_MAJOR_VERSION == 34
     if (abil.ability == ABIL_HEAL_WOUNDS)
     {
@@ -1180,12 +1198,13 @@ static int _adjusted_failure_chance(ability_type ability, int base_chance)
     switch (ability)
     {
     case ABIL_BREATHE_FIRE:
-    case ABIL_BREATHE_FROST:
-    case ABIL_BREATHE_ACID:
-    case ABIL_BREATHE_LIGHTNING:
-    case ABIL_BREATHE_POWER:
-    case ABIL_BREATHE_MEPHITIC:
-    case ABIL_BREATHE_STEAM:
+    case ABIL_GLACIAL_BREATH:
+    case ABIL_CAUSTIC_BREATH:
+    case ABIL_GALVANIC_BREATH:
+    case ABIL_NULLIFYING_BREATH:
+    case ABIL_NOXIOUS_BREATH:
+    case ABIL_STEAM_BREATH:
+    case ABIL_MUD_BREATH:
         if (you.form == transformation::dragon)
             return base_chance - 20;
         return base_chance;
@@ -1904,15 +1923,25 @@ static bool _check_ability_possible(const ability_def& abil, bool quiet = false)
         return true;
     }
 
-    case ABIL_SPIT_POISON:
+    case ABIL_COMBUSTION_BREATH:
+    case ABIL_GLACIAL_BREATH:
+    case ABIL_GALVANIC_BREATH:
+    case ABIL_CAUSTIC_BREATH:
+    case ABIL_NULLIFYING_BREATH:
+    case ABIL_STEAM_BREATH:
+    case ABIL_NOXIOUS_BREATH:
+    case ABIL_MUD_BREATH:
+        if (draconian_breath_uses_available() <= 0)
+        {
+            if (!quiet)
+                mpr("You have exhausted your breath weapon. Dive deeper!");
+            return false;
+        }
+        return true;
+
     case ABIL_BREATHE_FIRE:
-    case ABIL_BREATHE_FROST:
+    case ABIL_SPIT_POISON:
     case ABIL_BREATHE_POISON:
-    case ABIL_BREATHE_LIGHTNING:
-    case ABIL_BREATHE_ACID:
-    case ABIL_BREATHE_POWER:
-    case ABIL_BREATHE_STEAM:
-    case ABIL_BREATHE_MEPHITIC:
         if (you.duration[DUR_BREATH_WEAPON])
         {
             if (!quiet)
@@ -2278,9 +2307,6 @@ unique_ptr<targeter> find_ability_targeter(ability_type ability)
         return make_unique<targeter_siphon_essence>();
 
     // Full LOS:
-    case ABIL_BREATHE_LIGHTNING: // Doesn't account for bounces/explosions
-                                 // hitting areas behind glass/statues.
-        return make_unique<targeter_maybe_radius>(&you, LOS_SOLID, LOS_RADIUS);
     case ABIL_KIKU_TORMENT:
     case ABIL_CHEIBRIADOS_SLOUCH:
     case ABIL_QAZLAL_DISASTER_AREA: // Doesn't account for explosions hitting
@@ -2489,21 +2515,6 @@ bool activate_talent(const talent& tal, dist *target)
     }
 }
 
-static bool _acid_breath_can_hit(const actor *act)
-{
-    if (act->is_monster())
-    {
-        const monster* mons = act->as_monster();
-        bolt testbeam;
-        testbeam.thrower = KILL_YOU;
-        zappy(ZAP_BREATHE_ACID, 100, false, testbeam);
-
-        return !testbeam.ignores_monster(mons);
-    }
-    else
-        return false;
-}
-
 /// If the player is stationary, print 'You cannot move.' and return true.
 static bool _abort_if_stationary()
 {
@@ -2694,6 +2705,30 @@ static spret _siphon_essence(bool fail)
     return spret::success;
 }
 
+static spret _do_draconian_breath(const ability_def& abil, dist *target, bool fail)
+{
+    spret result = spret::abort;
+
+    static map<ability_type, spell_type> breath_to_spell =
+        {
+            { ABIL_COMBUSTION_BREATH, SPELL_COMBUSTION_BREATH },
+            { ABIL_GLACIAL_BREATH, SPELL_GLACIAL_BREATH },
+            { ABIL_NULLIFYING_BREATH, SPELL_NULLIFYING_BREATH },
+            { ABIL_STEAM_BREATH, SPELL_STEAM_BREATH },
+            { ABIL_NOXIOUS_BREATH, SPELL_NOXIOUS_BREATH },
+            { ABIL_CAUSTIC_BREATH, SPELL_CAUSTIC_BREATH },
+            { ABIL_MUD_BREATH, SPELL_MUD_BREATH },
+            { ABIL_GALVANIC_BREATH, SPELL_GALVANIC_BREATH },
+        };
+    result = your_spells(breath_to_spell[abil.ability], you.experience_level,
+                            false, nullptr, target, fail);
+
+    if (result == spret::success)
+        you.props[DRACONIAN_BREATH_USES_KEY].get_int() -= 1;
+
+    return result;
+}
+
 /*
  * Use an ability.
  *
@@ -2788,43 +2823,18 @@ static spret _do_ability(const ability_def& abil, bool fail, dist *target,
         break;
     }
 
-    case ABIL_BREATHE_ACID:       // Draconian acid splash
-    {
-        int pow = (you.form == transformation::dragon) ?
-            2 * you.experience_level : you.experience_level;
-        targeter_splash hitfunc(&you, beam.range, pow);
-        direction_chooser_args args;
-        args.mode = TARG_HOSTILE;
-        args.hitfunc = &hitfunc;
-        args.get_desc_func = bind(desc_beam_hit_chance, placeholders::_1, &hitfunc);
-        if (!spell_direction(*target, beam, &args))
-            return spret::abort;
-
-        if (stop_attack_prompt(hitfunc, "spit acid", _acid_breath_can_hit))
-            return spret::abort;
-
-        fail_check();
-        zapping(ZAP_BREATHE_ACID, pow, beam, false, "You spit a glob of acid.");
-
-        you.increase_duration(DUR_BREATH_WEAPON,
-                          3 + random2(10) + random2(30 - you.experience_level));
-        break;
-    }
-
-    case ABIL_BREATHE_LIGHTNING:
-        fail_check();
-        mpr("You breathe a wild blast of lightning!");
-        black_drac_breath();
-        you.increase_duration(DUR_BREATH_WEAPON,
-                          3 + random2(10) + random2(30 - you.experience_level));
-        break;
+    case ABIL_COMBUSTION_BREATH:
+    case ABIL_GLACIAL_BREATH:
+    case ABIL_NULLIFYING_BREATH:
+    case ABIL_STEAM_BREATH:
+    case ABIL_NOXIOUS_BREATH:
+    case ABIL_CAUSTIC_BREATH:
+    case ABIL_GALVANIC_BREATH:
+    case ABIL_MUD_BREATH:
+        return _do_draconian_breath(abil, target, fail);
 
     case ABIL_BREATHE_FIRE:
-    case ABIL_BREATHE_FROST:
     case ABIL_BREATHE_POISON:
-    case ABIL_BREATHE_POWER:
-    case ABIL_BREATHE_STEAM:
-    case ABIL_BREATHE_MEPHITIC:
     {
         spret result = spret::abort;
         int cooldown = 3 + random2(10) + random2(30 - you.experience_level);
@@ -2832,11 +2842,7 @@ static spret _do_ability(const ability_def& abil, bool fail, dist *target,
         static map<ability_type, string> breath_message =
         {
             { ABIL_BREATHE_FIRE, "You breathe a blast of fire." },
-            { ABIL_BREATHE_FROST, "You exhale a wave of freezing cold." },
             { ABIL_BREATHE_POISON, "You exhale a blast of poison gas." },
-            { ABIL_BREATHE_POWER, "You breathe a bolt of dispelling energy." },
-            { ABIL_BREATHE_STEAM, "You exhale a blast of scalding steam." },
-            { ABIL_BREATHE_MEPHITIC, "You exhale a blast of noxious fumes." },
         };
 
         result = zapping(ability_to_zap(abil.ability),
@@ -3136,7 +3142,7 @@ static spret _do_ability(const ability_def& abil, bool fail, dist *target,
                               ZAP_LIGHTNING_BOLT,
                               ZAP_BOLT_OF_MAGMA,
                               ZAP_BOLT_OF_DRAINING,
-                              ZAP_CORROSIVE_BOLT);
+                              ZAP_BREATHE_ACID);
             zapping(ztype, power, beam);
         }
         break;
@@ -3946,13 +3952,15 @@ vector<talent> your_talents(bool check_confused, bool include_unusable, bool ign
             ABIL_HOP,
             ABIL_SPIT_POISON,
             ABIL_BREATHE_FIRE,
-            ABIL_BREATHE_FROST,
+            ABIL_COMBUSTION_BREATH,
+            ABIL_GLACIAL_BREATH,
             ABIL_BREATHE_POISON,
-            ABIL_BREATHE_LIGHTNING,
-            ABIL_BREATHE_POWER,
-            ABIL_BREATHE_STEAM,
-            ABIL_BREATHE_MEPHITIC,
-            ABIL_BREATHE_ACID,
+            ABIL_GALVANIC_BREATH,
+            ABIL_NULLIFYING_BREATH,
+            ABIL_STEAM_BREATH,
+            ABIL_NOXIOUS_BREATH,
+            ABIL_CAUSTIC_BREATH,
+            ABIL_MUD_BREATH,
             ABIL_TRAN_BAT,
             ABIL_REVIVIFY,
             ABIL_EXSANGUINATE,

crawl-ref/source/ability-type.h
@@ -8,16 +8,14 @@ enum ability_type
     // Innate abilities and (Demonspawn) mutations.
     ABIL_SPIT_POISON = 1,
     ABIL_BREATHE_FIRE,
-    ABIL_BREATHE_FROST,
+    ABIL_GLACIAL_BREATH,
     ABIL_BREATHE_POISON,
-    ABIL_BREATHE_LIGHTNING,
-    ABIL_BREATHE_POWER,
-#if TAG_MAJOR_VERSION == 34
-    ABIL_BREATHE_STICKY_FLAME,
-#endif
-    ABIL_BREATHE_STEAM,
-    ABIL_BREATHE_MEPHITIC,
-    ABIL_BREATHE_ACID,
+    ABIL_GALVANIC_BREATH,
+    ABIL_NULLIFYING_BREATH,
+    ABIL_COMBUSTION_BREATH,
+    ABIL_STEAM_BREATH,
+    ABIL_NOXIOUS_BREATH,
+    ABIL_CAUSTIC_BREATH,
     // Others
 #if TAG_MAJOR_VERSION == 34
     ABIL_BLINK,
@@ -64,6 +62,7 @@ enum ability_type
     ABIL_SIPHON_ESSENCE,
     // Talismans
     ABIL_BEGIN_UNTRANSFORM,
+    ABIL_MUD_BREATH,
 
     // Note: this is getting dangerously close to ABIL_EVOKE_BERSERK! be careful
 

crawl-ref/source/ability.h
@@ -39,6 +39,7 @@ enum class abflag
     berserk_ok          = 0x00002000, // can use even if berserk
     card                = 0x00004000, // deck drawing (Nemelex)
     torchlight          = 0x00008000, // costs torchlight (Yred)
+    drac_charges        = 0x00010000, // consumes draconian breath charges
 
     // targeting flags
     dir_or_target       = 0x10000000, // uses DIR_NONE targeting

crawl-ref/source/beam.cc
@@ -659,6 +659,28 @@ static beam_type _chaos_beam_flavour(bolt* beam)
     return flavour;
 }
 
+static void _combustion_breath_explode(bolt *parent, coord_def pos)
+{
+    bolt beam;
+
+    bolt_parent_init(*parent, beam);
+    beam.name         = "fiery explosion";
+    beam.aux_source   = "combustion breath";
+    beam.damage       = dice_def(3, 3 + div_rand_round(parent->ench_power * 2, 3));
+    beam.colour       = RED;
+    beam.flavour      = BEAM_FIRE;
+    beam.is_explosion = true;
+    beam.source       = pos;
+    beam.target       = pos;
+    beam.origin_spell = SPELL_COMBUSTION_BREATH;
+    beam.refine_for_explosion();
+    beam.explode();
+    parent->friend_info += beam.friend_info;
+    parent->foe_info    += beam.foe_info;
+    if (beam.is_tracer && beam.beam_cancelled)
+        parent->beam_cancelled = true;
+}
+
 bool bolt::visible() const
 {
     return !is_tracer && glyph != 0 && !is_enchantment();
@@ -2162,7 +2184,8 @@ void fire_tracer(const monster* mons, bolt &pbolt, bool explode_only,
 set<coord_def> create_feat_splash(coord_def center,
                                 int radius,
                                 int number,
-                                int duration)
+                                int duration,
+                                dungeon_feature_type new_feat)
 {
     set<coord_def> splash_coords;
 
@@ -2174,7 +2197,7 @@ set<coord_def> create_feat_splash(coord_def center,
         {
             number--;
             int time = random_range(duration, duration * 3 / 2) - (di.radius() * 20);
-            temp_change_terrain(*di, DNGN_SHALLOW_WATER, time,
+            temp_change_terrain(*di, new_feat, time,
                                 TERRAIN_CHANGE_FLOOD);
             splash_coords.insert(*di);
         }
@@ -2601,6 +2624,16 @@ void bolt::affect_endpoint()
             place_cloud(CLOUD_FIRE, pos(), 5 + random2(5), agent());
         break;
 
+    case SPELL_MUD_BREATH:
+    {
+        mpr("The mud splashes down.");
+        const int num = 3 + div_rand_round(ench_power * 3, 7) + random2(4);
+        const int dur = (div_rand_round(ench_power * 1, 3) + 15) * BASELINE_DELAY;
+        const int rad = (num > 20) ? 3 : (num > 9) ? 2 : 1;
+        create_feat_splash(pos(), rad, num, dur, DNGN_MUD);
+    }
+    break;
+
     default:
         break;
     }
@@ -2775,15 +2808,18 @@ void bolt::affect_place_clouds()
     if (flavour == BEAM_MIASMA)
         place_cloud(CLOUD_MIASMA, p, random2(5) + 2, agent());
 
-    //XXX: these use the name for a gameplay effect.
-    if (name == "ball of steam")
-        place_cloud(CLOUD_STEAM, p, random2(5) + 2, agent());
 
+    if (flavour == BEAM_STEAM)
+    {
+        if (origin_spell == SPELL_STEAM_BREATH)
+            place_cloud(CLOUD_STEAM, p, div_rand_round(ench_power, 3) + 3, agent());
+        else
+            place_cloud(CLOUD_STEAM, p, random2(5) + 2, agent());
+    }
+
+    //XXX: these use the name for a gameplay effect.
     if (name == "poison gas")
         place_cloud(CLOUD_POISON, p, random2(4) + 3, agent());
-
-    if (name == "blast of choking fumes")
-        place_cloud(CLOUD_MEPHITIC, p, random2(4) + 3, agent());
 
     if (name == "trail of fire")
         place_cloud(CLOUD_FIRE, p, random2(ench_power) + ench_power, agent());
@@ -2796,6 +2832,34 @@ void bolt::affect_place_clouds()
 
     if (origin_spell == SPELL_DEATH_RATTLE)
         place_cloud(CLOUD_MIASMA, p, random2(4) + 4, agent());
+
+    if (origin_spell == SPELL_CAUSTIC_BREATH)
+    {
+        if (!actor_at(p))
+        {
+            place_cloud(CLOUD_ACID, p, 3 + random_range(ench_power / 2,
+                                                        ench_power * 4 / 5),
+                        agent());
+        }
+    }
+
+    if (origin_spell == SPELL_NOXIOUS_BREATH)
+    {
+        place_cloud(CLOUD_MEPHITIC, p,
+                    2 + random_range(ench_power / 2, ench_power * 4 / 5), agent());
+
+        for (adjacent_iterator ai(pos()); ai; ++ai)
+        {
+            if (feat_is_solid(env.grid(*ai)))
+                continue;
+
+            if (x_chance_in_y(max(0, ench_power - 10), ench_power - 5))
+            {
+                place_cloud(CLOUD_MEPHITIC, *ai,
+                        2 + random_range(ench_power / 3, ench_power / 2), agent());
+            }
+        }
+    }
 
     // Only place clouds on creatures who are immune to them
     if (origin_spell == SPELL_MOURNING_WAIL)
@@ -3034,6 +3098,12 @@ bool bolt::harmless_to_player() const
 
     if (you.cloud_immune() && is_big_cloud())
         return true;
+
+    if (origin_spell == SPELL_COMBUSTION_BREATH
+        || origin_spell == SPELL_NULLIFYING_BREATH)
+    {
+        return true;
+    }
 
     switch (flavour)
     {
@@ -4167,6 +4237,17 @@ void bolt::affect_player()
     if (origin_spell == SPELL_QUICKSILVER_BOLT)
         debuff_player();
 
+    if (origin_spell == SPELL_NULLIFYING_BREATH)
+    {
+        debuff_player();
+        int amount = min(you.magic_points, random2avg(ench_power, 3));
+        if (amount > 0)
+        {
+            mprf(MSGCH_WARN, "You feel your power leaking away.");
+            drain_mp(amount);
+        }
+    }
+
     if (origin_spell == SPELL_THROW_PIE && final_dam > 0)
     {
         const pie_effect effect = _random_pie_effect(you);
@@ -4231,6 +4312,12 @@ bool bolt::ignores_player() const
     // Digging -- don't care.
     if (flavour == BEAM_DIGGING)
         return true;
+
+    if (origin_spell == SPELL_COMBUSTION_BREATH
+        || origin_spell == SPELL_NULLIFYING_BREATH)
+    {
+        return true;
+    }
 
     if (agent() && agent()->is_monster()
         && mons_is_hepliaklqana_ancestor(agent()->as_monster()->type))
@@ -4549,6 +4636,9 @@ void bolt::tracer_nonenchantment_affect_monster(monster* mon)
 
     // Either way, we could hit this monster, so update range used.
     extra_range_used += range_used_on_hit();
+
+    if (origin_spell == SPELL_COMBUSTION_BREATH && !in_explosion_phase)
+        _combustion_breath_explode(this, mon->pos());
 }
 
 void bolt::tracer_affect_monster(monster* mon)
@@ -4758,8 +4848,11 @@ void bolt::handle_petrify_chaining(coord_def centre)
 static void _print_pre_death_message(const monster &mon, bool goldify,
                                      spell_type origin_spell)
 {
-    if (mon.is_insubstantial() || origin_spell != SPELL_GLACIATE)
+    if (mon.is_insubstantial()
+        || (origin_spell != SPELL_GLACIATE && origin_spell != SPELL_GLACIAL_BREATH))
+    {
         return;
+    }
     if (goldify)
         simple_monster_message(mon, " shatters and turns to gold!");
     else
@@ -4804,7 +4897,7 @@ void bolt::kill_monster(monster &mon)
 
     item_def *corpse = monster_die(mon, ref_killer, kindex);
 
-    if (origin_spell != SPELL_GLACIATE)
+    if (origin_spell != SPELL_GLACIATE && origin_spell != SPELL_GLACIAL_BREATH)
         return;
 
     if (corpse)
@@ -4817,35 +4910,17 @@ void bolt::kill_monster(monster &mon)
                                                    MG_FORCE_PLACE).set_base(species),
                                          false))
     {
+        // Make glacial breath ice blocks more sturdy, since they're supposed
+        // to make useful obstacles
+        if (origin_spell == SPELL_GLACIAL_BREATH)
+        {
+            pillar->max_hit_points = 15 + div_rand_round(ench_power * 5, 2);
+            pillar->hit_points = pillar->max_hit_points;
+        }
+
         const int time_left = random_range(7, 17) * BASELINE_DELAY;
         mon_enchant temp_en(ENCH_SLOWLY_DYING, 1, 0, time_left);
         pillar->add_ench(temp_en);
-    }
-}
-
-static void _acid_splash_monsters(const bolt& beam, monster* mon, actor* agent)
-{
-    for (adjacent_iterator ai(mon->pos()); ai; ++ai)
-    {
-        if (actor *victim = actor_at(*ai))
-        {
-            if (victim == agent)
-                continue;
-
-            if (victim->is_monster()
-                && beam.ignores_monster(victim->as_monster()))
-            {
-                continue;
-            }
-
-            if (you.see_cell(*ai))
-            {
-                mprf("The acid splashes onto %s!",
-                     victim->name(DESC_THE).c_str());
-            }
-
-            victim->splash_with_acid(agent);
-        }
     }
 }
 
@@ -4889,14 +4964,28 @@ void bolt::monster_post_hit(monster* mon, int dmg)
         sticky_flame_monster(mon, agent(), 3 + random2(ench_power / 20));
     }
 
-    // purple draconian breath
     if (origin_spell == SPELL_QUICKSILVER_BOLT)
         debuff_monster(*mon);
+
+    if (origin_spell == SPELL_NULLIFYING_BREATH)
+    {
+        debuff_monster(*mon);
+        if (mon->antimagic_susceptible())
+        {
+            const int dur = (5 + random_range(div_rand_round(ench_power, 2),
+                                             div_rand_round(ench_power * 3, 4) + 1))
+                            * BASELINE_DELAY;
+            mon->add_ench(mon_enchant(ENCH_ANTIMAGIC, 0, agent(), dur));
+        }
+    }
 
     if (dmg)
         beogh_follower_convert(mon, true);
 
     knockback_actor(mon, dmg);
+
+    if (origin_spell == SPELL_COMBUSTION_BREATH && !in_explosion_phase)
+        _combustion_breath_explode(this, mon->pos());
 
     if (origin_spell == SPELL_FLASH_FREEZE
              || name == "blast of ice"
@@ -4971,8 +5060,6 @@ static int _knockback_dist(spell_type origin, int pow)
     case SPELL_FORCE_LANCE:
     case SPELL_ISKENDERUNS_MYSTIC_BLAST:
         return 2 + div_rand_round(pow, 50);
-    case SPELL_CHILLING_BREATH:
-        return 2;
     default:
         return 1;
     }
@@ -4991,9 +5078,7 @@ void bolt::knockback_actor(actor *act, int dam)
                      : 17;
     const int dist = binomial(max_dist, roll - weight, roll); // This is silly! -- PF
 
-    const string push_type =
-        origin_spell == SPELL_CHILLING_BREATH ? "freezing wind" : name;
-    act->knockback(*agent(), dist, ench_power, push_type);
+    act->knockback(*agent(), dist, ench_power, name);
 }
 
 void bolt::pull_actor(actor *act, int dam)
@@ -5354,6 +5439,10 @@ void bolt::affect_monster(monster* mon)
     // Apply flavoured specials.
     mons_adjust_flavoured(mon, *this, postac, true);
 
+    // Arc out from the original target AFTER damage done to the main target
+    if (origin_spell == SPELL_GALVANIC_BREATH)
+        do_galvanic_jolt(*agent(), pos(), damage);
+
     // mons_adjust_flavoured may kill the monster directly.
     if (mon->alive())
     {
@@ -5366,9 +5455,6 @@ void bolt::affect_monster(monster* mon)
             const int blood = min(postac/2, mon->hit_points);
             bleed_onto_floor(mon->pos(), mon->type, blood, true);
         }
-        // Acid splash from yellow draconians.
-        if (origin_spell == SPELL_ACID_SPLASH)
-            _acid_splash_monsters(*this, mon, agent());
         // Now hurt monster.
         mon->hurt(agent(), final, flavour, KILLED_BY_BEAM, "", "", false);
     }
@@ -6380,6 +6466,14 @@ const map<spell_type, explosion_sfx> spell_explosions = {
         "Space twists violently!",
         "shrill echo",
     } },
+    { SPELL_COMBUSTION_BREATH, {
+        "The embers explode!",
+        "a fiery explosion",
+    } },
+    { SPELL_NULLIFYING_BREATH, {
+        "The antimagic surges outward!",
+        "a quiet echo",
+    } },
 };
 
 // Takes a bolt and refines it for use in the explosion function.
@@ -6425,6 +6519,11 @@ void bolt::refine_for_explosion()
     {
         colour     = LIGHTCYAN;
         ex_size    = 2;
+    }
+    else if (origin_spell = SPELL_NULLIFYING_BREATH)
+    {
+        colour  = MAGENTA;
+        ex_size = 2;
     }
 
     if (!is_tracer && !seeMsg.empty() && !hearMsg.empty())
@@ -6533,7 +6632,8 @@ bool bolt::explode(bool show_more, bool hole_in_the_middle)
 
         // Not an "explosion", but still a bit noisy at the target location.
         if (origin_spell == SPELL_INFESTATION
-            || origin_spell == SPELL_DAZZLING_FLASH)
+            || origin_spell == SPELL_DAZZLING_FLASH
+            || origin_spell == SPELL_NULLIFYING_BREATH)
         {
             loudness = spell_effect_noise(origin_spell);
         }
@@ -7135,8 +7235,8 @@ bool bolt::can_knockback(int dam) const
     switch (origin_spell)
     {
     case SPELL_PRIMAL_WAVE:
-        return flavour == BEAM_WATER;
-    case SPELL_CHILLING_BREATH:
+    case SPELL_MUD_BREATH:
+        return true;
     case SPELL_FORCE_LANCE:
     case SPELL_ISKENDERUNS_MYSTIC_BLAST:
         return dam != 0;

crawl-ref/source/beam.h
@@ -359,7 +359,8 @@ spret zapping(zap_type ztype, int power, bolt &pbolt,
                    bool fail = false);
 bool player_tracer(zap_type ztype, int power, bolt &pbolt, int range = 0);
 
-set<coord_def> create_feat_splash(coord_def center, int radius, int num, int dur);
+set<coord_def> create_feat_splash(coord_def center, int radius, int num, int dur,
+                                  dungeon_feature_type new_feat = DNGN_SHALLOW_WATER);
 
 void init_zap_index();
 void clear_zap_info_on_exit();

crawl-ref/source/dat/species/draconian-black.yaml
@@ -36,7 +36,7 @@ mutations:
   14:
     MUT_BIG_WINGS: 1
 fake_mutations:
-  - long: You can breathe wild blasts of lightning.
-    short: breathe lightning
+  - long: You can breathe arcing electricity.
+    short: galvanic breath
 
 orc_name: Black Orconian

crawl-ref/source/dat/species/draconian-green.yaml
@@ -36,6 +36,6 @@ mutations:
     MUT_STINGER: 1
 fake_mutations:
   - long: You can breathe blasts of noxious fumes.
-    short: breathe noxious fumes
+    short: noxious breath
 
 orc_name: Green Orconian

crawl-ref/source/dat/species/draconian-grey.yaml
@@ -29,6 +29,11 @@ str: 10
 int: 8
 dex: 6
 levelup_stat_frequency: 4
+fake_mutations:
+  - long: You can breathe torrents of mud.
+  - short: mud breath
+  - long: You are unimpeded by mud.
+    short: mud-swimmer
 mutations:
   1:
     MUT_COLD_BLOODED: 1

commit 8179dd6ec30e1f0049ee61f48391db763f73abef

Fix makh major destruction firing acid globs
Fixes an issue introduced by 926e5b6
Closes #3678

crawl-ref/source/ability.cc
@@ -3164,7 +3164,7 @@ static spret _do_ability(const ability_def& abil, bool fail, dist *target,
                               ZAP_LIGHTNING_BOLT,
                               ZAP_BOLT_OF_MAGMA,
                               ZAP_BOLT_OF_DRAINING,
-                              ZAP_BREATHE_ACID);
+                              ZAP_CORROSIVE_BOLT);
             zapping(ztype, power, beam);
         }
         break;