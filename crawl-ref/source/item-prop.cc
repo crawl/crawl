crawl/crawl

commit 95338c156ce86d7b4b3cb89f98aa10beac2d877e

Fix randart stave names (various)
Partially reverts 55136d7.

Closes #3685.

crawl-ref/source/item-prop.cc
@@ -2091,6 +2091,12 @@ bool staff_uses_evocations(const item_def &item)
     return item.base_type == OBJ_STAVES;
 }
 
+const char* staff_type_name(stave_type s)
+{
+    ASSERT_RANGE(s, 0, NUM_STAVES);
+    return Staff_prop[Staff_index[s]].name;
+}
+
 skill_type staff_skill(stave_type s)
 {
     ASSERT_RANGE(s, 0, NUM_STAVES);
@@ -3195,8 +3201,6 @@ string item_base_name(object_class_type type, int sub_type)
     {
     case OBJ_WEAPONS:
         return Weapon_prop[Weapon_index[sub_type]].name;
-    case OBJ_STAVES:
-        return Staff_prop[Staff_index[sub_type]].name;
     case OBJ_MISSILES:
         return Missile_prop[Missile_index[sub_type]].name;
     case OBJ_ARMOUR:

crawl-ref/source/item-name.cc
@@ -1189,13 +1189,13 @@ string sub_type_string(const item_def &item, bool known)
     {
     case OBJ_WEAPONS:  // deliberate fall through, as XXX_prop is a local
     case OBJ_MISSILES: // variable to item-prop.cc.
-    case OBJ_STAVES:
     case OBJ_ARMOUR:
         return item_base_name(type, sub_type);
     case OBJ_WANDS: return _wand_type_name(sub_type);
     case OBJ_SCROLLS: return scroll_type_name(sub_type);
     case OBJ_JEWELLERY: return jewellery_type_name(sub_type);
     case OBJ_POTIONS: return potion_type_name(sub_type);
+    case OBJ_STAVES: return staff_type_name(static_cast<stave_type>(sub_type));
     case OBJ_BOOKS:
     {
         switch (sub_type)
@@ -1893,7 +1893,7 @@ string item_def::name_aux(description_level_type desc, bool terse, bool ident,
             buff << "staff";
         }
         else
-            buff << "staff of " << item_base_name(OBJ_STAVES, item_typ);
+            buff << "staff of " << staff_type_name(static_cast<stave_type>(item_typ));
 
         if (cursed() && terse && !dbname && !qualname)
             buff << " (curse)";

crawl-ref/source/item-prop.h
@@ -103,10 +103,11 @@ special_armour_type get_armour_ego_type(const item_def &item) PURE;
 special_missile_type get_ammo_brand(const item_def &item) PURE;
 
 // staff functions:
-skill_type staff_skill(stave_type staff);
-beam_type staff_damage_type(stave_type staff);
-int staff_damage_mult(stave_type staff);
-ac_type staff_ac_check(stave_type staff);
+const char* staff_type_name(stave_type staff) PURE;
+skill_type staff_skill(stave_type staff) PURE;
+beam_type staff_damage_type(stave_type staff) PURE;
+int staff_damage_mult(stave_type staff) PURE;
+ac_type staff_ac_check(stave_type staff) PURE;
 
 // armour functions:
 bool armour_is_enchantable(const item_def &item) PURE;

commit 54de21ab3573ef318df3e5bb85e652e1645e5097

Don't crash on \
Broken in 95338c156ce86d7b4b3cb89f98aa10beac2d877e etc.

crawl-ref/source/item-prop.cc
@@ -2093,6 +2093,8 @@ bool staff_uses_evocations(const item_def &item)
 
 const char* staff_type_name(stave_type s)
 {
+    if (s == NUM_STAVES)
+        return "bugginess"; // used for known items
     ASSERT_RANGE(s, 0, NUM_STAVES);
     return Staff_prop[Staff_index[s]].name;
 }