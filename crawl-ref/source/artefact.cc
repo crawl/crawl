40b700cd640ffc95f112116aa479e72e8371d3fa
David Lawrence Ramsey <pooka109@gmail.com>
2024-04-02 19:17:21 -0500

    Limit Ignis' artefact weapons to flaming brand.

--- crawl-ref/source/artefact.cc
+++ crawl-ref/source/artefact.cc
@@ -150,6 +150,12 @@ static bool _god_fits_artefact(const god_type which_god, const item_def &item,
             return false;
         break;
 
+    case GOD_IGNIS:
+        // Fire god.
+        if (item.base_type == OBJ_WEAPONS && brand != SPWPN_FLAMING)
+            return false;
+        break;
+
     default:
         break;
     }
