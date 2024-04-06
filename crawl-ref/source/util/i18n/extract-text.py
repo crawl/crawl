34075e384f05d3a992bd2f21f04a6e4578160688
Brian Power <powerbf.it@gmail.com>
2024-04-06 20:27:24 +1000

    i18n: extract-text.py: Move constants to start

--- crawl-ref/source/util/i18n/extract-text.py
+++ crawl-ref/source/util/i18n/extract-text.py
@@ -30,6 +30,7 @@ import sys
 # handles escaped double-quotes
 STRING_PATTERN = r'"(\\|\"|[^"])*"'
 
+# strings to ignore
 IGNORE_STRINGS = [
     'the', 'the ', ' the ', 'its ',
     'a', 'a ', 'an', 'an ',
@@ -39,6 +40,71 @@ IGNORE_STRINGS = [
     #text colour tags
     'lightgrey', 'darkgrey', 'lightgreen', 'darkgreen', 'lightcyan', 'darkcyan',
     'lightred', 'darkred', 'lightmagenta', 'darkmagenta', 'lightyellow', 'darkyellow'
+]
+
+# These files need special handling because they define data structures
+# containing strings (normally names of things)
+# (stringutil.h is an exception - it contains strings related to list building)
+SPECIAL_FILES = [
+    'stringutil.h', 'mon-data.h',
+    'spl-data.h', 'zap-data.h', 'feature-data.h',
+    'item-prop.cc', 'item-name.cc', 'art-data.h',
+    'species-data.h', 'job-data.h', 'form-data.h', 'variant-msg.cc'
+]
+
+# These files are evaluated differently. We ignore all strings unless we have a reason to extract them,
+# as opposed to extracting all strings unless we have a reason to ignore them.
+LAZY_FILES = [
+    'dgn-overview.cc', 'end.cc', 'files.cc','fineff.cc', 'god-passive.cc',
+    'god-prayer.cc', 'macro.cc', 'main.cc', 'tilereg-dgn.cc'
+]
+
+SKIP_FILES = [
+    # covered in a way that doesn't use the literal strings from the file
+    'mutant-beast.h',
+    # these just contain a bunch of compile flags, etc.
+    'AppHdr.h', 'AppHdr.cc',
+    'build.h', 'compflag.h',
+    'version.h', 'version.cc',
+    # json tags should not be translated
+    'branch-data-json.h', 'branch-data-json.cc',
+    'json.h', 'json.cc', 'json-wrapper.h',
+    'tileweb.h', 'tileweb.cc', 'tileweb-text.h', 'tileweb-text.cc',
+    # nor other tags
+    'colour.h', 'colour.cc', 'format.h', 'format.cc',
+    # keys/properties
+    'defines.h', 'dgn-layouts.h', 'dgn-layouts.cc', 'god-abil.h', 'initfile.cc',
+    'libunix.h', 'libunix.cc', 'libutil.h', 'libutil.cc', 'mgen-data.h',
+    'mi-enum.h', 'mon-abil.h', 'mon-clone.h', 'mon-speak.cc', 'monster.h',
+    'religion-enum.h',
+    # debug/test stuff
+    'debug.h', 'ctest.h', 'ctest.cc', 'fake-main.cc', 'coord-def.h',
+    'crash.h', 'crash.cc', 'errors.h', 'errors.cc',
+    # English grammar
+    'english.h', 'english.cc',
+    # files related to the translation process itself
+    'xlate.h', 'xlate.cc',
+    'localise.h', 'localise.cc',
+    'database.h', 'database.cc', 'sqldbm.cc',
+    # stuff related to morgue file is not translated
+    # (because if we run this on a server, we want all the morgues in English)
+    'chardump.h', 'chardump.cc', 'kills.h', 'kills.cc', 'notes.h', 'notes.cc',
+    # lua scripting stuff
+    'clua.h', 'clua.cc', 'cluautil.h', 'cluautil.cc', 'dlua.h', 'dlua.cc',
+    # internal logic
+    'domino.h', 'domino.cc', 'dungeon.h', 'dungeon.cc', 'mapdef.h', 'mapdef.cc',
+    'mapmark.h', 'mapmark.cc', 'maps.h', 'maps.cc', 'mon-gear.h', 'mon-gear.cc',
+    'mon-pathfind.cc', 'ng-init.cc', 'ng-input.cc', 'precision-menu.h',
+    'precision-menu.cc', 'randbook.h', 'randbook.cc', 'sound.h', 'sound.cc',
+    'tilepick.cc', 'viewchar.h', 'viewchar.cc',
+    # error messages that probably aren't worth translating
+    'fontwrapper-ft.cc', 'game-options.h', 'game-options.cc', 'glwrapper-ogl.cc',
+    'libw32c.h', 'libw32c.cc', 'package.cc', 'windowmanager-sdl.cc',
+    # utils
+    'stringutil.cc', 'syscalls.h', 'syscalls.cc', 'ui.h', 'ui.cc',
+    'unicode.h', 'unicode.cc',
+    # nonsense
+    'lang-fake.h', 'lang-fake.cc'
 ]
 
 
@@ -613,70 +679,6 @@ def is_unique_monster(string):
     else:
         return True
 
-
-SKIP_FILES = [
-    # covered in a way that doesn't use the literal strings from the file
-    'mutant-beast.h',
-    # these just contain a bunch of compile flags, etc.
-    'AppHdr.h', 'AppHdr.cc',
-    'build.h', 'compflag.h',
-    'version.h', 'version.cc',
-    # json tags should not be translated
-    'branch-data-json.h', 'branch-data-json.cc',
-    'json.h', 'json.cc', 'json-wrapper.h',
-    'tileweb.h', 'tileweb.cc', 'tileweb-text.h', 'tileweb-text.cc',
-    # nor other tags
-    'colour.h', 'colour.cc', 'format.h', 'format.cc',
-    # keys/properties
-    'defines.h', 'dgn-layouts.h', 'dgn-layouts.cc', 'god-abil.h', 'initfile.cc',
-    'libunix.h', 'libunix.cc', 'libutil.h', 'libutil.cc', 'mgen-data.h',
-    'mi-enum.h', 'mon-abil.h', 'mon-clone.h', 'mon-speak.cc', 'monster.h',
-    'religion-enum.h',
-    # debug/test stuff
-    'debug.h', 'ctest.h', 'ctest.cc', 'fake-main.cc', 'coord-def.h',
-    'crash.h', 'crash.cc', 'errors.h', 'errors.cc',
-    # English grammar
-    'english.h', 'english.cc',
-    # files related to the translation process itself
-    'xlate.h', 'xlate.cc',
-    'localise.h', 'localise.cc',
-    'database.h', 'database.cc', 'sqldbm.cc',
-    # stuff related to morgue file is not translated
-    # (because if we run this on a server, we want all the morgues in English)
-    'chardump.h', 'chardump.cc', 'kills.h', 'kills.cc', 'notes.h', 'notes.cc',
-    # lua scripting stuff
-    'clua.h', 'clua.cc', 'cluautil.h', 'cluautil.cc', 'dlua.h', 'dlua.cc',
-    # internal logic
-    'domino.h', 'domino.cc', 'dungeon.h', 'dungeon.cc', 'mapdef.h', 'mapdef.cc',
-    'mapmark.h', 'mapmark.cc', 'maps.h', 'maps.cc', 'mon-gear.h', 'mon-gear.cc',
-    'mon-pathfind.cc', 'ng-init.cc', 'ng-input.cc', 'precision-menu.h',
-    'precision-menu.cc', 'randbook.h', 'randbook.cc', 'sound.h', 'sound.cc',
-    'tilepick.cc', 'viewchar.h', 'viewchar.cc',
-    # error messages that probably aren't worth translating
-    'fontwrapper-ft.cc', 'game-options.h', 'game-options.cc', 'glwrapper-ogl.cc',
-    'libw32c.h', 'libw32c.cc', 'package.cc', 'windowmanager-sdl.cc',
-    # utils
-    'stringutil.cc', 'syscalls.h', 'syscalls.cc', 'ui.h', 'ui.cc',
-    'unicode.h', 'unicode.cc',
-    # nonsense
-    'lang-fake.h', 'lang-fake.cc'
-]
-
-# These files define data structures containing strings (normally names of things)
-# (stringutil.h is an exception - it contains strings related to list building)
-SPECIAL_FILES = [
-    'stringutil.h', 'mon-data.h',
-    'spl-data.h', 'zap-data.h', 'feature-data.h',
-    'item-prop.cc', 'item-name.cc', 'art-data.h',
-    'species-data.h', 'job-data.h', 'form-data.h', 'variant-msg.cc'
-]
-
-# These files are evaluated differently. We ignore all strings unless we have a reason to extract them,
-# as opposed to extracting all strings unless we have a reason to ignore them.
-LAZY_FILES = [
-    'dgn-overview.cc', 'end.cc', 'files.cc','fineff.cc', 'god-passive.cc',
-    'god-prayer.cc', 'macro.cc', 'main.cc', 'tilereg-dgn.cc'
-]
 
 files = []
 if len(sys.argv) > 1:
