256e25cf8df39be89b8f54777ca1e3c52f0edec6
NormalPerson7 <cmrn.stphnsn@hotmail.com>
2024-04-11 15:29:33 +0100

    Fix wanderer evoker starts from allowing duplicate evokers
    Previously, if your wanderer rolled a misc evoker, you could still
    get the same misc evoker generate in your game. Since they're meant
    to essentially be unrands, fix this.
    
    [Sidenote: misc evokers are generally pretty unbalanced for starting
    equipment. The lightning rod in particular is insanely strong, with
    4 charges of (2+)d15 unavoidable lightning damage. Some rebalancing
    may be desirable in future.]

--- crawl-ref/source/ng-setup.cc
+++ crawl-ref/source/ng-setup.cc
@@ -181,6 +181,9 @@ item_def* newgame_make_item(object_class_type base,
     if (item.base_type == OBJ_MISSILES)
         _autopickup_ammo(static_cast<missile_type>(item.sub_type));
 
+    if (item.base_type == OBJ_MISCELLANY)
+        handle_generated_misc(static_cast<misc_item_type>(item.sub_type));
+
     origin_set_startequip(item);
     if (item.base_type == OBJ_WEAPONS && you.species == SP_COGLIN)
         name_weapon(item);
