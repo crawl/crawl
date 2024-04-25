e3f94467a25470c8f57e6d1f8a137c3c7d7da517
Monkooky <buisinessmonkey@gmail.com>
2024-04-24 20:32:22 -0500

    Describe gastronomic expanse

--- crawl-ref/source/duration-data.h
+++ crawl-ref/source/duration-data.h
@@ -729,8 +729,8 @@ static const duration_def duration_data[] =
       {{ "Your bloodlust subsides." },
        { "You feel your bloodlust ebbing." }}, 6},
     //TODO this needs rewrite
-    { DUR_GASTRONOMIC, YELLOW, "Gut", "", "can recruit",
-      "You are stuff", D_NO_FLAGS,},
+    { DUR_GASTRONOMIC, YELLOW, "Gut", "transposing a stomach", "gastronomic expanse",
+      "You are transposing a stomach with part of the dungeon.", D_DISPELLABLE,},
 
     // The following are visible in wizmode only, or are handled
     // specially in the status lights and/or the % or @ screens.
