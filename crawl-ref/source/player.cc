crawl/crawl

commit 7d2b172a6eb8d1e3f10bd94dd1949236c0338d7d

Change how rCorr interacts with corrosion stacks on the player
Protecting yourself against corrosion feels bad because of how
inconsistent it is.

Each stack of corrosion on the players reduces AC and slay by 1.
Quadrupled most sources of corrosion to compensate. Having corrosion
resistance will halve the number of stacks applied to the player
instead of a coinflip chance to prevent the status entirely.

crawl-ref/source/player.cc
@@ -6304,7 +6304,7 @@ int player::corrosion_amount() const
         corrosion += slime_wall_corrosion(&you);
 
     if (player_in_branch(BRANCH_DIS))
-        corrosion += 2;
+        corrosion += 8;
 
     return corrosion;
 }
@@ -6348,7 +6348,7 @@ int player::armour_class_with_specific_items(vector<const item_def *> items) con
     if (you.props.exists(PASSWALL_ARMOUR_KEY))
         AC += you.props[PASSWALL_ARMOUR_KEY].get_int() * scale;
 
-    AC -= 400 * corrosion_amount();
+    AC -= 100 * corrosion_amount();
 
     AC += sanguine_armour_bonus();
 
@@ -6924,16 +6924,6 @@ bool player::resists_dislodge(string event) const
 
 bool player::corrode_equipment(const char* corrosion_source, int degree)
 {
-    // rCorr protects against 50% of corrosion.
-    if (res_corr())
-    {
-        degree = binomial(degree, 50);
-        if (!degree)
-        {
-            dprf("rCorr protects.");
-            return false;
-        }
-    }
     // always increase duration, but...
     increase_duration(DUR_CORROSION, 10 + roll_dice(2, 4), 50,
                       make_stringf("%s corrodes you!",
@@ -6941,12 +6931,13 @@ bool player::corrode_equipment(const char* corrosion_source, int degree)
 
     // the more corrosion you already have, the lower the odds of more
     // Static environmental corrosion doesn't factor in
+    // Reduce corrosion amount by 50% if you have resistance
     int prev_corr = props[CORROSION_KEY].get_int();
     bool did_corrode = false;
     for (int i = 0; i < degree; i++)
-        if (!x_chance_in_y(prev_corr, prev_corr + 7))
+        if (!x_chance_in_y(prev_corr, prev_corr + 28))
         {
-            props[CORROSION_KEY].get_int()++;
+            props[CORROSION_KEY].get_int() += res_corr() ? 2 : 4;
             prev_corr++;
             did_corrode = true;
         }

crawl-ref/source/attack.cc
@@ -1104,7 +1104,7 @@ int attack::player_apply_slaying_bonuses(int damage, bool aux)
                         || (weapon && is_range_weapon(*weapon)
                                    && using_weapon());
     damage_plus += slaying_bonus(throwing);
-    damage_plus -= 4 * you.corrosion_amount();
+    damage_plus -= you.corrosion_amount();
 
     // XXX: should this also trigger on auxes?
     if (!aux && !ranged)

crawl-ref/source/output.cc
@@ -1115,7 +1115,7 @@ static void _print_stats_wp(int y)
         item_def wpn = *you.weapon(); // copy
 
         if (you.corrosion_amount() && wpn.base_type == OBJ_WEAPONS)
-            wpn.plus -= 4 * you.corrosion_amount();
+            wpn.plus -= you.corrosion_amount();
 
         text = wpn.name(DESC_PLAIN, true, false, true);
     }

crawl-ref/source/status.cc
@@ -201,7 +201,7 @@ bool fill_status_info(int status, status_info& inf)
         // Intentional fallthrough
     case DUR_CORROSION:
         inf.light_text = make_stringf("Corr (%d)",
-                          (-4 * you.corrosion_amount()));
+                          (-1 * you.corrosion_amount()));
         break;
 
     case DUR_FLAYED:

crawl-ref/source/terrain.cc
@@ -886,7 +886,7 @@ int slime_wall_corrosion(actor* act)
     if (actor_slime_wall_immune(act))
         return 0;
 
-    return count_adjacent_slime_walls(act->pos());
+    return count_adjacent_slime_walls(act->pos()) * 4;
 }
 
 // slime wall damage under Jiyva's oozemancy; this should only affect monsters

commit a9ac73bae21a2bd218fb73628b7d68bca2c0009c

fix corrosion chances for high degree
likely broken in 7d2b172

not likely relevant since only ever applies to wu jian wrath

crawl-ref/source/player.cc
@@ -7211,7 +7211,7 @@ bool player::corrode_equipment(const char* corrosion_source, int degree)
         if (!x_chance_in_y(prev_corr, prev_corr + 28))
         {
             props[CORROSION_KEY].get_int() += res_corr() ? 2 : 4;
-            prev_corr++;
+            prev_corr = props[CORROSION_KEY].get_int();
             did_corrode = true;
         }
 