9e921f76444d449bb155a3858839f22a839809cd
Monkooky <buisinessmonkey@gmail.com>
2024-05-05 11:01:36 -0500

    Update status colour

--- crawl-ref/source/duration-data.h
+++ crawl-ref/source/duration-data.h
@@ -729,7 +729,7 @@ static const duration_def duration_data[] =
       {{ "Your bloodlust subsides." },
        { "You feel your bloodlust ebbing." }}, 6},
     //TODO this needs rewrite
-    { DUR_GASTRONOMIC, YELLOW, "Gut", "transposing a stomach", "gastronomic expanse",
+    { DUR_GASTRONOMIC, MAGENTA, "Gut", "transposing a stomach", "gastronomic expanse",
       "You are transposing a stomach with part of the dungeon.", D_DISPELLABLE,},
 
     // The following are visible in wizmode only, or are handled
