crawl/crawl

commit 3dd439832fe0f5fe295caad1a2fe3e479a757cda

Turn monster explosions into fineffs
Explosions firing immediately causes a pretty classic set of issues -
we're invalidating the old state of the world in surprising ways. For
example, if a ball lightning explodes and kills an attacker midway
through the resolution of a multi-target attack (e.g. a javelin toss),
the game just has no way to figure out how to resolve the rest of the
attack afterward - it can't generate new ranged_attacks without the
original attacker, so it just crashes.

Instead, let's wait until the *end* of any ongoing melee attacks or
beams before triggering death explosions. The timing here is slightly
different for attack-triggered fineffs - we trigger them a little later
now melee attacks, in fight.cc - but hopefully that's ok.

Overall, this code is very bad and probably buggy. I'm sorry.

crawl-ref/source/fineff.cc
@@ -8,6 +8,7 @@
 
 #include "fineff.h"
 
+#include "beam.h"
 #include "bloodspatter.h"
 #include "coordit.h"
 #include "dactions.h"
@@ -485,6 +486,26 @@ void shock_serpent_discharge_fineff::fire()
                                         "a shock serpent", "electric aura");
     if (amount)
         oppressor.expose_to_element(beam.flavour, amount);
+}
+
+void explosion_fineff::fire()
+{
+    if (is_sanctuary(beam.target))
+    {
+        if (you.see_cell(beam.target))
+            mprf(MSGCH_GOD, "%s", sanctuary_message.c_str());
+        return;
+    }
+
+    if (you.see_cell(beam.target))
+        mprf(MSGCH_MONSTER_DAMAGE, MDAM_DEAD, "%s", boom_message.c_str());
+
+    if (inner_flame)
+        for (adjacent_iterator ai(beam.target, false); ai; ++ai)
+            if (!cell_is_solid(*ai) && !cloud_at(*ai) && !one_chance_in(5))
+                place_cloud(CLOUD_FIRE, *ai, 10 + random2(10), flame_agent);
+
+    beam.explode();
 }
 
 void delayed_action_fineff::fire()

crawl-ref/source/mon-explode.cc
@@ -13,6 +13,7 @@
 #include "dungeon-char-type.h"
 #include "english.h"
 #include "env.h"
+#include "fineff.h"
 #include "fprop.h"
 #include "message.h"
 #include "monster.h"
@@ -20,8 +21,9 @@
 #include "mon-place.h"
 #include "mon-util.h"
 #include "mpr.h"
-#include "state.h"
 #include "spl-goditem.h"
+#include "state.h"
+#include "stringutil.h"
 #include "target.h"
 #include "terrain.h"
 #include "torment-source-type.h"
@@ -177,7 +179,9 @@ bool explode_monster(monster* mons, killer_type killer,
     bolt beam;
     const monster_type type = mons->type;
     string sanct_msg = "";
-    actor* agent = mons;
+    string boom_msg = make_stringf("%s explodes!", mons->full_name(DESC_THE).c_str());
+    actor* agent = nullptr;
+    bool inner_flame = false;
 
     auto it = explosions.find(type);
     if (it != explosions.end())
@@ -191,6 +195,8 @@ bool explode_monster(monster* mons, killer_type killer,
         sanct_msg = string("By Zin's power, ") +
                     apostrophise(mons->name(DESC_THE)) + " " +
                     effect + ".";
+        if (type == MONS_BENNU)
+            boom_msg = make_stringf("%s blazes out!", mons->full_name(DESC_THE).c_str());
     } else {
         if (!mons->has_ench(ENCH_INNER_FLAME))
         {
@@ -212,6 +218,7 @@ bool explode_monster(monster* mons, killer_type killer,
         mons->flags    |= MF_EXPLODE_KILL;
         sanct_msg       = "By Zin's power, the fiery explosion is contained.";
         beam.aux_source = "exploding inner flame";
+        inner_flame = true;
     }
 
     if (beam.aux_source.empty())
@@ -232,34 +239,15 @@ bool explode_monster(monster* mons, killer_type killer,
         }
     }
 
-    bool saw = false;
-    if (you.can_see(*mons))
+    if (is_sanctuary(mons->pos()))
     {
-        saw = true;
-        viewwindow();
-        update_screen();
-        if (is_sanctuary(mons->pos()))
+        if (you.can_see(*mons))
             mprf(MSGCH_GOD, "%s", sanct_msg.c_str());
-        else if (type == MONS_BENNU)
-            mprf(MSGCH_MONSTER_DAMAGE, MDAM_DEAD, "%s blazes out!",
-                 mons->full_name(DESC_THE).c_str());
-        else
-            mprf(MSGCH_MONSTER_DAMAGE, MDAM_DEAD, "%s explodes!",
-                 mons->full_name(DESC_THE).c_str());
+        return false;
     }
 
-    if (is_sanctuary(mons->pos()))
-        return false;
-
-    // Explosion side-effects.
     if (type == MONS_LURKING_HORROR)
         torment(mons, TORMENT_LURKING_HORROR, mons->pos());
-    else if (mons->has_ench(ENCH_INNER_FLAME))
-    {
-        for (adjacent_iterator ai(mons->pos(), false); ai; ++ai)
-            if (!cell_is_solid(*ai) && !cloud_at(*ai) && !one_chance_in(5))
-                place_cloud(CLOUD_FIRE, *ai, 10 + random2(10), agent);
-    }
 
     // Detach monster from the grid first, so it doesn't get hit by
     // its own explosion. (GDL)
@@ -275,11 +263,6 @@ bool explode_monster(monster* mons, killer_type killer,
 
     // Exploding kills the monster a bit earlier than normal.
     mons->hit_points = -16;
-    if (saw)
-    {
-        viewwindow();
-        update_screen();
-    }
 
     // FIXME: show_more == you.see_cell(mons->pos())
     if (type == MONS_LURKING_HORROR)
@@ -288,7 +271,7 @@ bool explode_monster(monster* mons, killer_type killer,
         flash_view_delay(UA_MONSTER, DARKGRAY, 300, &hitfunc);
     }
     else
-        beam.explode();
+        explosion_fineff::schedule(beam, boom_msg, sanct_msg, inner_flame, agent);
 
     // Monster died in explosion, so don't re-attach it to the grid.
     return true;

crawl-ref/source/attack.cc
@@ -136,9 +136,6 @@ bool attack::handle_phase_killed()
 
 bool attack::handle_phase_end()
 {
-    // This may invalidate both the attacker and defender.
-    fire_final_effects();
-
     return true;
 }
 

crawl-ref/source/beam.cc
@@ -31,6 +31,7 @@
 #include "english.h"
 #include "exercise.h"
 #include "fight.h"
+#include "fineff.h"
 #include "god-conduct.h"
 #include "god-item.h"
 #include "god-passive.h" // passive_t::convert_orcs
@@ -945,6 +946,7 @@ int bolt::range_used(bool leg_only) const
 void bolt::finish_beam()
 {
     extra_range_used = BEAM_STOP;
+    fire_final_effects();
 }
 
 void bolt::affect_wall()

crawl-ref/source/fineff.h
@@ -10,6 +10,8 @@
 #include "mon-util.h"
 #include "mgen-data.h"
 
+struct bolt;
+
 class final_effect
 {
 public:
@@ -238,6 +240,34 @@ protected:
     coord_def position;
     int power;
     mon_attitude_type attitude;
+};
+
+class explosion_fineff : public final_effect
+{
+public:
+    // One explosion at a time, please.
+    bool mergeable(const final_effect &) const override { return false; }
+    void fire() override;
+
+    static void schedule(bolt &beam, string boom, string sanct,
+                         bool inner_flame, const actor* flame_agent)
+    {
+        final_effect::schedule(new explosion_fineff(beam, boom, sanct,
+                                                    inner_flame, flame_agent));
+    }
+protected:
+    explosion_fineff(const bolt &beem, string boom, string sanct,
+                     bool flame, const actor* agent)
+        : final_effect(0, 0, coord_def()), beam(beem),
+          boom_message(boom), sanctuary_message(sanct),
+          inner_flame(flame), flame_agent(agent)
+    {
+    }
+    bolt beam;
+    string boom_message;
+    string sanctuary_message;
+    bool inner_flame;
+    const actor* flame_agent;
 };
 
 // A fineff that triggers a daction; otherwise the daction

commit 10a096165b8dc30e2a897c0210fbf363e9dedc2d

Properly ascribe blame for deaths due to exploding monsters
This was broken since 3dd439832fe0f5fe295caad1a2fe3e479a757cda which caused
all ikiller information for deaths caused by exploders (ie: ball lightning
or fulminant prisms) to be lost. This isn't conceptually trivial to fix,
as now the monster responsible for the explosion will already be cleaned
up by the time the explosion happens (and the entire blame chain was stored
inside that monster).

The fix I've settled on is to maintain a very transient cache for copies of
monsters that are about to explode. If a scorefile_entry tries to look up
a monster by mid and fails, it will look at this cache instead so that it
can find the dead monster's info as it was when the final_effect was
first scheduled.

(This cache is cleared as soon as fineffs are done processing, so there
shouldn't be any need to worry about its state.)

It's possible there is some other class of fineff that could benefit from
using it, for similar reasons, but this wasn't immediately obvious to me.
However, this should once again let us see whose fault these deaths are.

(Fun fact: I'd thought that Xak'krixis was wildly underperforming, but
apparently a full 90% of their kills weren't being attributed to them. Poor
Lom also lost out on ~30% or so of their kills since the original commit).

crawl-ref/source/env.h
@@ -104,6 +104,10 @@ struct crawl_environment
 
     // Things to happen when the current attack/etc finishes.
     vector<final_effect *> final_effects;
+    // Copies of monsters cached so they can be looked up during a final_effect
+    // that will be processed after their death. Used mainly to assign proper
+    // blame for dead exploders. (Cleared every time final_effects is)
+    vector<monster> final_effect_monster_cache;
 
     // A stack that accumulates subvaults being placed. A failure may pop a
     // part of the stack before retrying.

crawl-ref/source/files.cc
@@ -2191,6 +2191,7 @@ bool load_level(dungeon_feature_type stair_taken, load_mode_type load_mode,
 
     // Shouldn't happen, but this is too unimportant to assert.
     deleteAll(env.final_effects);
+    env.final_effect_monster_cache.clear();
 
     los_changed();
 

crawl-ref/source/fineff.cc
@@ -990,4 +990,7 @@ void fire_final_effects()
         env.final_effects.pop_back();
         eff->fire();
     }
+
+    // Clear all cached monster copies
+    env.final_effect_monster_cache.clear();
 }

crawl-ref/source/hiscores.cc
@@ -1345,7 +1345,10 @@ void scorefile_entry::init_death_cause(int dam, mid_t dsrc,
     death_type   = dtype;
     damage       = dam;
 
-    const monster *source_monster = monster_by_mid(death_source);
+    // Try searching for both a living monster and a dead-but-cached monster
+    const monster *source_monster = monster_by_mid(death_source)
+                                     ? monster_by_mid(death_source)
+                                     : cached_monster_copy_by_mid(death_source);
     if (source_monster)
         killer_map = source_monster->originating_map();
 
@@ -1375,9 +1378,9 @@ void scorefile_entry::init_death_cause(int dam, mid_t dsrc,
             || death_type == KILLED_BY_BEING_THROWN
             || death_type == KILLED_BY_COLLISION
             || death_type == KILLED_BY_CONSTRICTION)
-        && monster_by_mid(death_source))
+        && source_monster)
     {
-        const monster* mons = monster_by_mid(death_source);
+        const monster* mons = source_monster;
         ASSERT(mons);
 
         // Previously the weapon was only used for dancing weapons,

crawl-ref/source/mon-explode.cc
@@ -53,6 +53,9 @@ static void _setup_base_explosion(bolt & beam, const monster& origin)
 
     beam.aux_source.clear();
     beam.attitude = origin.attitude;
+
+    // Cache a copy of the exploding monster so we can look up blame info after it dies.
+    env.final_effect_monster_cache.push_back(origin);
 }
 
 static int _inferno_power(int /*hd*/)

crawl-ref/source/mon-util.cc
@@ -4813,6 +4813,17 @@ monster *monster_by_mid(mid_t m, bool require_valid)
     return 0;
 }
 
+monster *cached_monster_copy_by_mid(mid_t m)
+{
+    for (size_t i = 0; i < env.final_effect_monster_cache.size(); ++i)
+    {
+        if (env.final_effect_monster_cache[i].mid == m)
+            return &env.final_effect_monster_cache[i];
+    }
+
+    return nullptr;
+}
+
 void init_anon()
 {
     monster &mon = env.mons[ANON_FRIENDLY_MONSTER];

crawl-ref/source/mon-util.h
@@ -496,6 +496,7 @@ int get_dist_to_nearest_monster();
 bool monster_nearby();
 actor *actor_by_mid(mid_t m, bool require_valid = false);
 monster *monster_by_mid(mid_t m, bool require_valid = false);
+monster *cached_monster_copy_by_mid(mid_t m);
 bool mons_is_recallable(const actor* caller, const monster& targ);
 void init_anon();
 actor *find_agent(mid_t m, kill_category kc);