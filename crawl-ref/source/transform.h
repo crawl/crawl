12f0f65bdd24010e368ab20ce2ee605b8af3f23a
Nicholas Feinberg <pleasingfung@gmail.com>
2024-05-11 07:36:17 -0700

    Remove unused function in header

--- crawl-ref/source/transform.h
+++ crawl-ref/source/transform.h
@@ -312,7 +312,6 @@ bool form_can_wield(transformation form = you.form);
 bool form_can_wear(transformation form = you.form);
 bool form_can_fly(transformation form = you.form);
 bool form_can_swim(transformation form = you.form);
-bool form_likes_water(transformation form = you.form);
 bool form_changed_physiology(transformation form = you.form);
 bool form_can_bleed(transformation form = you.form);
 // Does the form keep the benefits of resistance, scale, and aux mutations?
