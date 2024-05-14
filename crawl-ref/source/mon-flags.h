crawl/crawl

commit 672eff3212572a203b58ffd4f1e2474d8a40fcfc

Refactor more band leader code

crawl-ref/source/mon-flags.h
@@ -283,8 +283,7 @@ enum monster_flag_type : uint64_t
     /// possibly got piety with TSO
     MF_TSO_SEEN           = BIT(38),
 
-    /// Created as the leader of a band
-    MF_BAND_LEADER        = BIT(40)
+    // MF_BAND_LEADER      = BIT(40) // Created as the leader of a band
 };
 DEF_BITFIELD(monster_flags_t, monster_flag_type);
 

crawl-ref/source/mon-place.cc
@@ -774,9 +774,6 @@ monster* place_monster(mgen_data mg, bool force_pos, bool dont_place)
     if (mon->mindex() >= MAX_MONSTERS - 30)
         return mon;
 
-    if (band_size > 1)
-        mon->flags |= MF_BAND_LEADER;
-
     const bool priest = mon->is_priest();
 
     mgen_data band_template = mg;

crawl-ref/source/mon-util.cc
@@ -3974,11 +3974,9 @@ bool monster_senior(const monster& m1, const monster& m2, bool fleeing)
     }
 
     // Band leaders can displace followers regardless of type considerations.
-    // -cao
-    if (m1.is_band_leader_of(m2))
-        return true;
     // And prevent followers to displace the leader to avoid constant swapping.
-    else if (m1.is_band_follower_of(m2))
+    // -cao
+    if (m1.is_band_leader_of(m2) || m1.is_band_follower_of(m2))
         return false;
 
     // Monsters smart enough to use stairs can push past monsters too stupid

crawl-ref/source/monster.cc
@@ -6405,32 +6405,21 @@ bool monster::angered_by_attacks() const
 
 bool monster::is_band_follower_of(const monster& leader) const
 {
-    if (!testbits(flags, MF_BAND_FOLLOWER)
-        || !props.exists(BAND_LEADER_KEY))
-    {
+    if (!testbits(flags, MF_BAND_FOLLOWER) || !props.exists(BAND_LEADER_KEY))
         return false;
-    }
 
     return leader.mid == static_cast<mid_t>(props[BAND_LEADER_KEY].get_int());
 }
 
 bool monster::is_band_leader_of(const monster& follower) const
 {
-    // Check if we're a leader of anyone at all
-    if (!testbits(flags, MF_BAND_LEADER))
-        return false;
-
     return follower.is_band_follower_of(*this);
 }
 
 monster* monster::get_band_leader() const
 {
-    // Not a band follower
-    if (!testbits(flags, MF_BAND_FOLLOWER)
-        || !props.exists(BAND_LEADER_KEY))
-    {
+    if (!testbits(flags, MF_BAND_FOLLOWER) || !props.exists(BAND_LEADER_KEY))
         return nullptr;
-    }
 
     // Return our leader (if they still exist on this floor)
     mid_t leader_mid = static_cast<mid_t>(props[BAND_LEADER_KEY].get_int());

commit a4d4a8453ae54f5ca65411c8d6681d9b04b1e365

Experiment with making bands a little harder to separate
Monsters in bands that have a natural leader try to stay within a moderate
distance of this leader while wandering about, so that if the leader is
chasing after the player, the rest of the band tends to follow (whether
they've yet noticed the player themselves or not). But no such protections
between band followers and each other exist, which sometimes results in it
being straightforward to pick off the members of a band individually
without alerting most of them - often without much deliberate effort, if
they're already awake.

In past, there have been some issues with the functionality of bands that
rely on each other for synergy, so I'm curious to try making them a little
bit stickier.

This commit makes it so that when a band leader would pick a new wander
target, if one of their followers is out of sight and seeking a target,
they will wander in the direction of that band member instead of randomly.
And since band followers already vaguely follow the leader, the band as a
whole will tend to move in that direction.

There is still a bit of lag time here. The leader still finishes moving
wherever they were wandering first, and gets no special awareness of the
player (or whatever else their follower was chasing). But it increases the
*tendancy* for the rest of a band to follow on a slight delay from the
player engaging any part of it.

I'm not certain if this change will be impactful enough to be clear to
players, or that it will be a mechanical improvement if it is, but things
of this nature are hard to know without playtesting, so let's playtest!

(This commit also independently fixes a bug introduced by 672eff3212572a203b58ffd4f1e2474d8a40fcfc
which made band leader unable to push past their own followers)

crawl-ref/source/mon-flags.h
@@ -288,7 +288,8 @@ enum monster_flag_type : uint64_t
     /// Part of an orc apostle band
     MF_APOSTLE_BAND       = BIT(39),
 
-    // MF_BAND_LEADER      = BIT(40) // Created as the leader of a band
+    // Created as the leader of a band
+    MF_BAND_LEADER        = BIT(40),
 
     /// Will prefer not to approach enemies it's already in spellcast range of
     MF_CAUTIOUS           = BIT(41),

crawl-ref/source/mon-movetarget.cc
@@ -789,6 +789,42 @@ static bool _band_wander_target(monster * mon)
     return false;
 }
 
+// Makes a band leader with nothing better to do wander in the general direction
+// of a band follower that has moved away to chase after something.
+//
+// Return true if a target still needs to be set. If returns false, mon->target
+// was set.
+static bool _leader_wander_target(monster * mon)
+{
+    for (monster_iterator mi; mi; ++mi)
+    {
+        if (mi->is_band_follower_of(*mon)
+            && mi->behaviour == BEH_SEEK
+            && (grid_distance(mi->pos(), mon->pos()) > LOS_DEFAULT_RANGE
+               || !mon->see_cell(mi->pos())))
+        {
+            monster_pathfind mp;
+            mp.set_range(1000);
+
+            if (mp.init_pathfind(mon, mi->pos()))
+            {
+                mon->travel_path = mp.calc_waypoints();
+                if (!mon->travel_path.empty())
+                {
+                    // Okay then, we found a path. Let's use it!
+                    mon->target = mon->travel_path[0];
+                    mon->travel_target = MTRAV_PATROL;
+                    return false;
+                }
+
+                // If we can't find a path to them, look through the rest of our followers
+            }
+        }
+    }
+
+    return true;
+}
+
 // Returns true if a movement target still needs to be set
 static bool _herd_wander_target(monster * mon)
 {
@@ -924,6 +960,9 @@ void check_wander_target(monster* mon, bool isPacified)
 
         if (need_target && band_leader != nullptr)
             need_target = _band_wander_target(mon);
+
+        if (need_target && testbits(mon->flags, MF_BAND_LEADER))
+            need_target = _leader_wander_target(mon);
 
         // XXX: This is really dumb wander behaviour... instead of
         // changing the goal square every turn, better would be to

crawl-ref/source/mon-place.cc
@@ -759,6 +759,9 @@ monster* place_monster(mgen_data mg, bool force_pos, bool dont_place)
     if (mon->mindex() >= MAX_MONSTERS - 30)
         return mon;
 
+    if (band_size > 1)
+        mon->flags |= MF_BAND_LEADER;
+
     const bool priest = mon->is_priest();
 
     mgen_data band_template = mg;

crawl-ref/source/mon-util.cc
@@ -4034,9 +4034,11 @@ bool monster_senior(const monster& m1, const monster& m2, bool fleeing)
     }
 
     // Band leaders can displace followers regardless of type considerations.
+    if (m1.is_band_leader_of(m2))
+        return true;
     // And prevent followers to displace the leader to avoid constant swapping.
     // -cao
-    if (m1.is_band_leader_of(m2) || m1.is_band_follower_of(m2))
+    else if (m1.is_band_follower_of(m2))
         return false;
 
     // Monsters smart enough to use stairs can push past monsters too stupid

crawl-ref/source/monster.cc
@@ -6495,6 +6495,10 @@ bool monster::is_band_follower_of(const monster& leader) const
 
 bool monster::is_band_leader_of(const monster& follower) const
 {
+    // Check if we're a leader of anyone at all
+    if (!testbits(flags, MF_BAND_LEADER))
+        return false;
+
     return follower.is_band_follower_of(*this);
 }
 