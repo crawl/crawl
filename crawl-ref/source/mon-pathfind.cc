crawl/crawl

commit 323ad4e9fb1dbb588b479de76b9c62a829cc0646

Fix clinging monsters breaking autoexplore (shalmezad, 11924)
Clinging monsters in vaults could pathfind their way through the runed
door, and were therefore marked as not safe, preventing autoexplore.
This was caused by mons_traversable() sidestepping checks for runed
cells and uninhabitable cells in mons_can_traverse() if the monster can
cling. I've just hoisted them out here.

In addition, cell_can_cling_to() only checks physical location,
and so couldn't check whether a feature is passable by a given monster.

Last but not least, the call to cell_is_clingable() previously passed
the wrong parameter.

crawl-ref/source/mon-pathfind.cc
@@ -433,9 +433,15 @@ bool monster_pathfind::traversable(const coord_def& p)
 // its preferred habit and capability of flight or opening doors.
 bool monster_pathfind::mons_traversable(const coord_def& p)
 {
+    if (cell_is_runed(p))
+        return false;
+    if (!mons->is_habitable(p))
+        return false;
+
     return mons_can_traverse(*mons, p, traverse_in_sight)
             || mons->can_cling_to_walls()
-               && cell_is_clingable(pos)
+               && cell_is_clingable(p)
+               && mons->can_pass_through_feat(grd(p))
                && cell_can_cling_to(pos, p);
 }
 

commit 99eaaefb1226f274f97fb1ad5e8d26641ef0cc66

Remind monsters how to open doors
A fairly old bug seemingly introduced in 323ad4e. Monsters would open doors under the simplest circumstances (standing right next to them, and/or with an otherwise unobstructed view to the player) but as soon as waypoint pathfinding kicked in, the is_habitable check was happening too early and they never got to check if they were able to traverse the door. Closed doors are not considered habitable (probably since 323ad4e).

These checks were introduced to solve an issue with clinging but were not purged in 53d2a09 when clinging was removed. Both checks were duplicated in mons_can_traverse but in the correct sequence to also account for doors.

crawl-ref/source/mon-pathfind.cc
@@ -432,11 +432,6 @@ bool monster_pathfind::traversable(const coord_def& p)
 // its preferred habit and capability of flight or opening doors.
 bool monster_pathfind::mons_traversable(const coord_def& p)
 {
-    if (cell_is_runed(p))
-        return false;
-    if (!mons->is_habitable(p))
-        return false;
-
     return mons_can_traverse(*mons, p, traverse_in_sight);
 }
 