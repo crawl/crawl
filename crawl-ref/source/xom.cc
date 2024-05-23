5e7fbf1b49724b40b0c87aac92729ba20ed5c6d1
David Lawrence Ramsey <pooka109@gmail.com>
2024-05-23 14:48:07 -0500

    Add Xom helmet slot messages to the database.

--- crawl-ref/source/xom.cc
+++ crawl-ref/source/xom.cc
@@ -1990,11 +1990,18 @@ static void _xom_pseudo_miscast(int /*sever*/)
 
     if (item_def* item = you.slot_item(EQ_HELMET))
     {
-        string str = "Your ";
-        str += item->name(DESC_BASENAME, false, false, false);
-        str += " leaps into the air, briefly spins, then lands back on "
-               "your head!";
-        messages.push_back(str);
+        string name = item->name(DESC_YOUR, false, false, false);
+        string str = _get_xom_speech("helmet slot");
+
+        if (!str.empty())
+        {
+            str = maybe_pick_random_substring(str);
+
+            str = replace_all(str, "@Your_item@", uppercase_first(name));
+            str = replace_all(str, "@your_item@", name);
+
+            messages.push_back(str);
+        }
     }
 
     if (item_def* item = you.slot_item(EQ_BOOTS))
