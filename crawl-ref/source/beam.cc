crawl/crawl

commit e42ff28ed44e11e251d365e9ea717092fa1d2136

Alert monsters hit by enchantments *after* the enchantment effect
This is aimed primarily to avoid the relatively-common situation of casting
Ensorcelled Hibernation on a wandering monster that is unaware of you, and
this causing it to shout and wake up other monsters immediately, even when
the spell successfully puts it to sleep. Now (like non-enchantment beams),
the monster is alerted only *after* the effect happens - and being asleep
will stop them from shouting.

This technically buffs a similar situation with paralysis wands and maybe
a few others, but that seems fine.

I am slightly concerned this might have unforseen consequences (since it's
not obvious if there was a *reason* behavior events were fired early in
this function), but I guess we'll see them if we see them.

crawl-ref/source/beam.cc
@@ -4759,8 +4759,6 @@ void bolt::enchantment_affect_monster(monster* mon)
 
     bool hit_woke_orc = false;
 
-    // Nasty enchantments will annoy the monster, and are considered
-    // naughty (even if a monster might resist).
     if (nasty_to(mon))
     {
         if (YOU_KILL(thrower))
@@ -4776,13 +4774,7 @@ void bolt::enchantment_affect_monster(monster* mon)
             if (flavour != BEAM_HIBERNATION)
                 you.pet_target = mon->mindex();
         }
-
-        // Don't wake a target that just got slept by Hibernation
-        if (flavour != BEAM_SHADOW_TORPOR || !mon->asleep())
-            behaviour_event(mon, ME_ANNOY, agent());
     }
-    else if (flavour != BEAM_HIBERNATION || !mon->asleep())
-        behaviour_event(mon, ME_ALERT, agent());
 
     // Doing this here so that the player gets to see monsters
     // "flicker and vanish" when turning invisible....
@@ -4835,6 +4827,17 @@ void bolt::enchantment_affect_monster(monster* mon)
     handle_petrify_chaining(mon->pos());
 
     extra_range_used += range_used_on_hit();
+
+    // Nasty enchantments will annoy the monster, and are considered
+    // naughty (even if a monster resisted).
+    if (nasty_to(mon))
+    {
+        // Don't wake a target that just got slept by Hibernation
+        if (flavour != BEAM_SHADOW_TORPOR || !mon->asleep())
+            behaviour_event(mon, ME_ANNOY, agent());
+    }
+    else if (flavour != BEAM_HIBERNATION || !mon->asleep())
+        behaviour_event(mon, ME_ALERT, agent());
 }
 
 static void _add_petrify_chain_candidates(const bolt& beam, coord_def pos,

commit eb6dff901acaa13301db90c81364b208179a6596

Fix Charming breaking immediately (Undo)
Broken by e42ff28ed44e11e251d firing ME_ANNOY after the enchantment effect
instead of before.

I opted to solve this by just not considering BEAM_CHARM to be nasty to a
monster you're aligned with, which seems to work well enough.

crawl-ref/source/beam.cc
@@ -5850,7 +5850,8 @@ bool ench_flavour_affects_monster(actor *agent, beam_type flavour,
     // These are special allies whose loyalty can't be so easily bent
     case BEAM_CHARM:
         rc = !(god_protects(*mon)
-               || testbits(mon->flags, MF_DEMONIC_GUARDIAN));
+               || testbits(mon->flags, MF_DEMONIC_GUARDIAN))
+             && !mons_aligned(agent, mon);
         break;
 
     case BEAM_MINDBURST:
@@ -7149,6 +7150,7 @@ bool bolt::nasty_to(const monster* mon) const
         case BEAM_VAMPIRIC_DRAINING:
         case BEAM_ENFEEBLE:
         case BEAM_RIMEBLIGHT:
+        case BEAM_CHARM:
             return ench_flavour_affects_monster(agent(), flavour, mon);
         case BEAM_TUKIMAS_DANCE:
             return tukima_affects(*mon); // XXX: move to ench_flavour_affects?