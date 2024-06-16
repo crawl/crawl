9cb92693c652dbf66a2a5cc106055e1f82d7ccf0
David Lawrence Ramsey <pooka109@gmail.com>
2024-06-16 18:23:35 -0500

    Set monster gods in YAML.
    _place_monster_set_god() is now place_monster_maybe_override_god(). It
    can override the YAML-set god (orc Beogh-worshippers and unbelievers,
    Xom-adopted holies in the Abyss), and gives any priestly monsters with
    no god set the nameless god, just like the old code, except without the
    explicit genus checks. The explicit genus checks were redundant anyway,
    since they all fell through to the default case.
    
    The old code included a comment that all (non-priestly) angels and
    daevas worship TSO, except for Mennas. However, the actual code made all
    (non-priestly) holies worship TSO except for Mennas and apises (Elyvilon
    worshippers); seraphim were explicitly set as TSO worshippers. I've made
    the new code give angels and daevas who normally worship TSO a chance to
    worship Xom in the Abyss; doing that for all holy beings who worship TSO
    would override seraphim.
    
    Also, make orc apostles explicitly worship Beogh, instead of leaving it
    as a 6-out-of-7 chance for the non-priestly ones. It doesn't make sense
    for the latter to be unbelievers.

--- crawl-ref/source/mon-place.cc
+++ crawl-ref/source/mon-place.cc
@@ -869,96 +869,30 @@ static void _place_twister_clouds(monster *mon)
     polar_vortex_damage(mon, -10);
 }
 
-static void _place_monster_set_god(monster *mon, monster_type cls,
-                                   level_id place)
+static void _place_monster_maybe_override_god(monster *mon, monster_type cls,
+                                              level_id place)
 {
-    // Give priestly monsters a god.
+    // [ds] Vault defs can request priest monsters of unusual types.
     if (mon->is_priest())
     {
-        // Berserkers belong to Trog.
-        if (cls == MONS_SPRIGGAN_BERSERKER)
-            mon->god = GOD_TROG;
-        // Death knights belong to Yredelemnul.
-        else if (cls == MONS_DEATH_KNIGHT)
-            mon->god = GOD_YREDELEMNUL;
-        // Seraphim follow the Shining One.
-        else if (cls == MONS_SERAPH)
-            mon->god = GOD_SHINING_ONE;
-        // Draconian stormcallers worship Qazlal.
-        else if (cls == MONS_DRACONIAN_STORMCALLER)
-            mon->god = GOD_QAZLAL;
-        // Demonspawn blood saints, hell knights, and Asterion worship Makhleb.
-        else if (cls == MONS_DEMONSPAWN_BLOOD_SAINT
-                 || cls == MONS_HELL_KNIGHT
-                 || cls == MONS_ASTERION)
-        {
-            mon->god = GOD_MAKHLEB;
-        }
-        else if (cls == MONS_DEMONSPAWN_BLACK_SUN
-                 || cls == MONS_BURIAL_ACOLYTE)
-        {
-            mon->god = GOD_KIKUBAAQUDGHA;
-        }
-        else if (cls == MONS_DEMONSPAWN_CORRUPTER
-                 || cls == MONS_MLIOGLOTL)
-        {
-            mon->god = GOD_LUGONU;
-        }
-        else
-        {
-            switch (mons_genus(cls))
-            {
-            case MONS_ORC:
-                mon->god = GOD_BEOGH;
-                break;
-            case MONS_JELLY:
-                mon->god = GOD_JIYVA;
-                break;
-            case MONS_MUMMY:
-            case MONS_DRACONIAN:
-            case MONS_ELF:
-                // [ds] Vault defs can request priest monsters of unusual types.
-            default:
-                mon->god = GOD_NAMELESS;
-                break;
-            }
-        }
-
+        if (mon->god == GOD_NO_GOD)
+            mon->god = GOD_NAMELESS;
         return;
     }
 
-    // The Royal Jelly belongs to Jiyva.
-    if (cls == MONS_ROYAL_JELLY)
-        mon->god = GOD_JIYVA;
-    // Mennas belongs to Zin.
-    else if (cls == MONS_MENNAS)
-        mon->god = GOD_ZIN;
-    // Yiuf is a faithful Xommite.
-    else if (cls == MONS_CRAZY_YIUF)
-        mon->god = GOD_XOM;
-    // Grinder and Ignacio belong to Makhleb.
-    else if (cls == MONS_GRINDER
-             || cls == MONS_IGNACIO)
-    {
-        mon->god = GOD_MAKHLEB;
-    }
-    // 1 out of 7 non-priestly orcs are unbelievers.
-    else if (mons_genus(cls) == MONS_ORC)
+    // 1 out of 7 non-priestly orcs who are unbelievers stay that way,
+    // and the others follow Beogh's word.
+    if (mons_genus(cls) == MONS_ORC && mon->god == GOD_NO_GOD)
     {
         if (!one_chance_in(7))
             mon->god = GOD_BEOGH;
     }
-    else if (cls == MONS_APIS)
-        mon->god = GOD_ELYVILON;
-    else if (cls == MONS_PROFANE_SERVITOR)
-        mon->god = GOD_YREDELEMNUL;
-    // Angels (other than Mennas) and daevas belong to TSO, but 1 out of
-    // 7 in the Abyss are adopted by Xom.
-    else if (mons_class_holiness(cls) == MH_HOLY)
+    // 1 out of 7 angels or darvas who normally worship TSO are adopted
+    // by Xom if they're in the Abyss.
+    else if ((cls == MONS_ANGEL || cls == MONS_DAEVA)
+              && mon->god == GOD_SHINING_ONE)
     {
-        if (place != BRANCH_ABYSS || !one_chance_in(7))
-            mon->god = GOD_SHINING_ONE;
-        else
+        if (one_chance_in(7) && place == BRANCH_ABYSS)
             mon->god = GOD_XOM;
     }
 }
@@ -1124,7 +1058,10 @@ static monster* _place_monster_aux(const mgen_data &mg, const monster *leader,
         mons_make_god_gift(*mon, mg.god);
     // Not a god gift. Give the monster a god.
     else
-        _place_monster_set_god(mon, mg.cls, mg.place);
+    {
+        mon->god = m_ent->god;
+        _place_monster_maybe_override_god(mon, mg.cls, mg.place);
+    }
 
     // Monsters that need halos/silence auras/umbras.
     if ((mon->holiness() & MH_HOLY)
