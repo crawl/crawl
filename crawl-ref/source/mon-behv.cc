13f425ce8bc9bacc932d98c43cfa80b562c81ac3
DracoOmega <draco.omega.dcss.dev@gmail.com>
2024-06-25 22:44:09 -0230

    Fix a weird infinite loop with ally retreat orders (robotcentaur)
    Due to an oversight, after ally retreat behavior was refactored by 6316ba047ae8947d194a466133b87ee628c4d497
    it didn't actually properly end once they'd reached their destination.
    Their behavior and patrol points would be set properly to end it, and then
    handle_behavior would silently reset their behavior back. I am frankly
    astounded that no one else reported this problem before now.
    
    But even worse: because it would set their patrol point to (0, 0) WITHOUT
    actually changing their behavior to not seek it out, anything that reached
    the end of where it was going would immediately attempt to travel to
    (0, 0). And apparently if you were on a level where the ally in question
    could get near to the level boundary (eg: Shoals), this would cause an
    infinite loop. I am not sure the specific cause of this, since it went away
    when the first bug was fixed; I assume it was because handle_behavior kept
    getting called to give the monster somewhere valid to move and it kept on
    insisting it should move to (0, 0).
    
    Anyway, it seems to work properly now.

--- crawl-ref/source/mon-behv.cc
+++ crawl-ref/source/mon-behv.cc
@@ -850,7 +850,13 @@ void handle_behaviour(monster* mon)
             }
 
             if (stop_retreat)
+            {
                 mons_end_withdraw_order(*mon);
+                // XXX: The above function already sets this, but otherwise they
+                //      with be ignored just below. Ugh.
+                new_beh = BEH_SEEK;
+                new_foe = MHITYOU;
+            }
             else
                 mon->props[LAST_POS_KEY] = mon->pos();
 
