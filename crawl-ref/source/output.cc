2fd60d507a7252c1037bcfaa8ce9427427393395
fbwer <dlawoqja123@gmail.com>
2024-06-26 12:29:12 +0900

    test 2

--- crawl-ref/source/output.cc
+++ crawl-ref/source/output.cc
@@ -753,7 +753,7 @@ static void _print_stats_noise(int x, int y)
     if (have_passive(passive_t::dampen_noise) || player_equip_unrand(UNRAND_THIEF))
     {
         textcolour(LIGHTMAGENTA);
-        CPRINTF("Noise/2: ");
+        CPRINTF("Noi/2: ");
     }
     else 
     {
