df7db63c151637a7d29a2f93725d40e9f5ffb22d
David Lawrence Ramsey <pooka109@gmail.com>
2024-07-09 00:19:24 -0500

    Remove another reference to ears.

--- crawl-ref/source/orb.cc
+++ crawl-ref/source/orb.cc
@@ -64,7 +64,7 @@ void orb_pickup_noise(const coord_def& where, int loudness, const char* msg, con
         if (msg)
             mprf(MSGCH_ORB, "%s", msg);
         else
-            mprf(MSGCH_ORB, "The Orb lets out an ear-shattering shriek!");
+            mprf(MSGCH_ORB, "The Orb lets out an agonising shriek!");
     }
     else
     {
