8408ca8fbfe92b941435760bc2e462d1531cd9d9
Monkooky <buisinessmonkey@gmail.com>
2024-07-31 03:38:53 -0500

    Update Charlatan's Orb
    The Charlatan's Orb can currently break the 27 skill limit for evocations, and this does not behave correctly for some misc evokers.
    The orb was already probably an incorrect choice for a significant majority of characters, and simply capping the boost at 27 would have made this worse.
    
    Instead, we give the orb a more unusual effect of directly boosting skills based on unmodified evocations.

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -6220,11 +6220,14 @@ int player::skill(skill_type sk, int scale, bool real, bool temp) const
     else if (ash_has_skill_boost(sk))
             level = ash_skill_boost(sk, scale);
 
-    if (player_equip_unrand(UNRAND_CHARLATANS_ORB) && sk == SK_EVOCATIONS)
-        level = div_rand_round(level * 3, 2);
+    if (player_equip_unrand(UNRAND_CHARLATANS_ORB) && sk != SK_EVOCATIONS)
+        level += skill(SK_EVOCATIONS, 10, true, false) * scale / 50;
 
     if (temp && duration[DUR_HEROISM] && sk <= SK_LAST_MUNDANE)
-        level = min(level + 5 * scale, MAX_SKILL_LEVEL * scale);
+        level += 5 * scale;
+
+    if (level > MAX_SKILL_LEVEL * scale)
+        level = MAX_SKILL_LEVEL * scale;
 
     return level;
 }
