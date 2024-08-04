crawl/crawl

commit 258319946a6387f947fb8e9a2748ec4143870134

Add code framework for gaining Infernal Marks
This adds the basic backend for gaining Makhleb's new capstone gift - one
of a selection of Makhleb-specific mutations that grant powerful passive
effects and/or change how your Makhleb invocations work.

At 6*, the player will be offered a choice of 3 different Marks (out of
however many exist - currently planned at 9). Selecting one of these will
permanently brand the player with it (injuring them greatly in the
process...) and give them its effect for as long as they worship Makhleb.
The mark will persist as a mutation, even if they abandon the god, but will
not actually function unless they start worshipping them again (this
prevents any possibility of abandoning to reroll your mutation
possibilities, and it's also good flavour that you have to live with the
consequences of your actions here.) Forms which normally suppress mutations
will not suppress these.

(They also cause silver vulnerability - despite otherwise being
perma-mutations, they are clearly demonic in origin and ought to count.)

None of the marks included in this commit *do* anything yet; that will come
in future commits.

crawl-ref/source/ability.cc
@@ -471,6 +471,12 @@ static vector<ability_def> &_get_ability_list()
             abflag::dir_or_target },
         { ABIL_MAKHLEB_INFERNAL_SERVANT, "Infernal Servant",
             0, scaling_cost::fixed(8), 4, -1, {fail_basis::invo, 40, 5, 20}},
+        { ABIL_MAKHLEB_BRAND_SELF_1, "Brand Self #1",
+            0, 0, 0, -1, {fail_basis::invo}, abflag::none },
+        { ABIL_MAKHLEB_BRAND_SELF_2, "Brand Self #2",
+            0, 0, 0, -1, {fail_basis::invo}, abflag::none },
+        { ABIL_MAKHLEB_BRAND_SELF_3, "Brand Self #3",
+            0, 0, 0, -1, {fail_basis::invo}, abflag::none },
 
         // Sif Muna
         { ABIL_SIF_MUNA_CHANNEL_ENERGY, "Channel Magic",
@@ -1258,6 +1264,21 @@ talent get_talent(ability_type ability, bool check_confused)
     return result;
 }
 
+static mutation_type _makhleb_ability_to_mutation(ability_type abil)
+{
+    // XXX: The list of marks the player will be offered is generated as soon
+    //      as the player first joins Makhleb, but internally their ability
+    //      keybinds are assigned before god-specific code is run, and it will
+    //      try to pull the names of mutations that aren't asigned yet. Use a
+    //      placeholder to stop a crash on conversion.
+    if (!you.props.exists(MAKHLEB_OFFERED_MARKS_KEY))
+        return MUT_NON_MUTATION;
+
+    return (mutation_type)you.props[MAKHLEB_OFFERED_MARKS_KEY]
+                            .get_vector()[abil - ABIL_MAKHLEB_BRAND_SELF_1].get_int();
+}
+
+
 string ability_name(ability_type ability, bool dbname)
 {
     // Special-case some dynamic names
@@ -1286,6 +1307,17 @@ string ability_name(ability_type ability, bool dbname)
                 return "Dismiss Apostle";
             else
                 return "Dismiss " + get_apostle_name(3, true);
+
+        case ABIL_MAKHLEB_BRAND_SELF_1:
+        case ABIL_MAKHLEB_BRAND_SELF_2:
+        case ABIL_MAKHLEB_BRAND_SELF_3:
+            if (dbname)
+                return "Brand Self";
+            else
+            {
+                return make_stringf("Accept %s",
+                                    mutation_name(_makhleb_ability_to_mutation(ability)));
+            }
 
         default:
             return get_ability_def(ability).name;
@@ -1497,15 +1529,33 @@ string get_ability_desc(const ability_type ability, bool need_title)
     if (lookup.empty()) // Nothing found?
         lookup = "No description found.\n";
 
-    if (ability == ABIL_ASHENZARI_CURSE)
-        lookup += _curse_desc();
+    switch (ability)
+    {
+        case ABIL_ASHENZARI_CURSE:
+            lookup += _curse_desc();
+            break;
 
-    if (ability == ABIL_BEOGH_DISMISS_APOSTLE_1)
-        lookup += "\n" + apostle_short_description(1) + "\n";
-    else if (ability == ABIL_BEOGH_DISMISS_APOSTLE_2)
-        lookup += "\n" + apostle_short_description(2) + "\n";
-    else if (ability == ABIL_BEOGH_DISMISS_APOSTLE_3)
-        lookup += "\n" + apostle_short_description(3) + "\n";
+        case ABIL_BEOGH_DISMISS_APOSTLE_1:
+        case ABIL_BEOGH_DISMISS_APOSTLE_2:
+        case ABIL_BEOGH_DISMISS_APOSTLE_3:
+        {
+            const int index = ability - ABIL_BEOGH_DISMISS_APOSTLE_1;
+            lookup += "\n" + apostle_short_description(index) + "\n";
+        }
+        break;
+
+        case ABIL_MAKHLEB_BRAND_SELF_1:
+        case ABIL_MAKHLEB_BRAND_SELF_2:
+        case ABIL_MAKHLEB_BRAND_SELF_3:
+        {
+            const mutation_type mut = _makhleb_ability_to_mutation(ability);
+            lookup += "\n" + get_mutation_desc(mut);
+        }
+        break;
+
+        default:
+        break;
+    }
 
     if (testbits(get_ability_def(ability).flags, abflag::sacrifice))
         lookup += _sacrifice_desc(ability);
@@ -3336,6 +3386,12 @@ static spret _do_ability(const ability_def& abil, bool fail, dist *target,
     case ABIL_MAKHLEB_INFERNAL_SERVANT:
         fail_check();
         makhleb_infernal_servant();
+        break;
+
+    case ABIL_MAKHLEB_BRAND_SELF_1:
+    case ABIL_MAKHLEB_BRAND_SELF_2:
+    case ABIL_MAKHLEB_BRAND_SELF_3:
+        makhleb_inscribe_mark(_makhleb_ability_to_mutation(abil.ability));
         break;
 
     case ABIL_TROG_BERSERK:

crawl-ref/source/ability-type.h
@@ -157,6 +157,9 @@ enum ability_type
     ABIL_MAKHLEB_MAJOR_DESTRUCTION,
     ABIL_MAKHLEB_GREATER_SERVANT_OF_MAKHLEB,
 #endif
+    ABIL_MAKHLEB_BRAND_SELF_1,
+    ABIL_MAKHLEB_BRAND_SELF_2,
+    ABIL_MAKHLEB_BRAND_SELF_3,
     // Sif Muna
     ABIL_SIF_MUNA_CHANNEL_ENERGY = 1070,
     ABIL_SIF_MUNA_FORGET_SPELL,

crawl-ref/source/god-abil.cc
@@ -7002,3 +7002,17 @@ void makhleb_infernal_servant()
         }
     }
 }
+
+void makhleb_inscribe_mark(mutation_type mark)
+{
+    mprf("You utter a prayer to Makhleb and carve the %s into yourself.",
+         mutation_name(mark));
+
+    const int hploss = min(you.hp - 1, you.hp * 2 / 3);
+    blood_spray(you.pos(), MONS_PLAYER, 50);
+    ouch(hploss, KILLED_BY_SELF_AIMED, MID_PLAYER);
+
+    perma_mutate(mark, 1, " inscribed by the player.");
+
+    you.one_time_ability_used.set(GOD_MAKHLEB);
+}

crawl-ref/source/god-abil.h
@@ -11,6 +11,7 @@
 #include "god-type.h"
 #include "item-prop-enum.h" // brand_type
 #include "los-type.h"
+#include "mutation-type.h"
 #include "random.h"
 #include "recite-eligibility.h"
 #include "recite-type.h"
@@ -83,6 +84,8 @@ const char * const GOZAG_SHOP_COST_KEY       = "gozag_shop_cost_%d";
 
 #define NIGHTFALL_INITIAL_DUR_KEY "nightfall_initial_dur"
 #define DITHMENOS_MARIONETTE_SPELLS_KEY "marionette_spells_valid"
+
+#define MAKHLEB_OFFERED_MARKS_KEY "makhleb_offered_marks"
 
 struct bolt;
 class stack_iterator;
@@ -244,3 +247,4 @@ void jiyva_end_oozemancy();
 
 spret makhleb_unleash_destruction(int power, bolt& beam, bool fail);
 void makhleb_infernal_servant();
+void makhleb_inscribe_mark(mutation_type mark);

crawl-ref/source/mutation-data.h
@@ -2629,6 +2629,46 @@ static const mutation_def mut_data[] =
   {"", "", ""},
 },
 
+{ MUT_MAHKLEB_MARK_HEMOCLASM, 0, 1, mutflag::makhleb, false,
+  "Mark of Hemoclasm",
+
+  {"You bear the Mark of Hemoclasm.", "", ""},
+  {"You feel some sort of feeling.", "", ""},
+  {"", "", ""},
+},
+
+{ MUT_MAKHLEB_MARK_LEGION, 0, 1, mutflag::makhleb, false,
+  "Mark of the Legion",
+
+  {"You bear the Mark of the Legion.", "", ""},
+  {"You feel some sort of feeling.", "", ""},
+  {"", "", ""},
+},
+
+{ MUT_MAKHLEB_MARK_SHRIKE, 0, 1, mutflag::makhleb, false,
+  "Mark of the Shrike",
+
+  {"You bear the Mark of the Shrike.", "", ""},
+  {"You feel some sort of feeling.", "", ""},
+  {"", "", ""},
+},
+
+{ MUT_MAKHLEB_MARK_EXECUTION, 0, 1, mutflag::makhleb, false,
+  "Mark of Execution",
+
+  {"You bear the Mark of Execution.", "", ""},
+  {"You feel some sort of feeling.", "", ""},
+  {"", "", ""},
+},
+
+{ MUT_MAKHLEB_MARK_FIEND, 0, 1, mutflag::makhleb, false,
+  "Mark of the Fiend",
+
+  {"You bear the Mark of the Fiend.", "", ""},
+  {"You feel some sort of feeling.", "", ""},
+  {"", "", ""},
+},
+
 };
 
 static const mutation_category_def category_mut_data[] =

crawl-ref/source/mutation-type.h
@@ -309,6 +309,11 @@ enum mutation_type
     MUT_MAKHLEB_COC_ALIGNED,
     MUT_MAKHLEB_TAR_ALIGNED,
     MUT_MAKHLEB_DIS_ALIGNED,
+    MUT_MAHKLEB_MARK_HEMOCLASM,
+    MUT_MAKHLEB_MARK_LEGION,
+    MUT_MAKHLEB_MARK_SHRIKE,
+    MUT_MAKHLEB_MARK_EXECUTION,
+    MUT_MAKHLEB_MARK_FIEND,
     NUM_MUTATIONS,
     CATEGORY_MUTATIONS,
     RANDOM_MUTATION = CATEGORY_MUTATIONS,

crawl-ref/source/mutation.cc
@@ -81,10 +81,11 @@ enum class mutflag
     bad     = 1 << 1, // used by malmut etc
     jiyva   = 1 << 2, // jiyva-only muts
     qazlal  = 1 << 3, // qazlal wrath
+    makhleb = 1 << 4, // makhleb capstone marks
 
-    last    = qazlal
+    last    = makhleb
 };
-DEF_BITFIELD(mutflags, mutflag, 3);
+DEF_BITFIELD(mutflags, mutflag, 4);
 COMPILE_CHECK(mutflags::exponent(mutflags::last_exponent) == mutflag::last);
 
 #include "mutation-data.h"
@@ -328,6 +329,16 @@ bool is_body_facet(mutation_type mut)
  */
 mutation_activity_type mutation_activity_level(mutation_type mut)
 {
+    // Makhleb mutations are active in all forms, but only while worshipping.
+    const mutation_def& mdef = _get_mutation_def(mut);
+    if (_mut_has_use(mdef, mutflag::makhleb))
+    {
+        if (you_worship(GOD_MAKHLEB))
+            return mutation_activity_type::FULL;
+        else
+            return mutation_activity_type::INACTIVE;
+    }
+
     // First make sure the player's form permits the mutation.
     if (!form_keeps_mutations())
     {
@@ -1602,6 +1613,11 @@ static mutation_type _get_random_slime_mutation()
 bool is_slime_mutation(mutation_type mut)
 {
     return _mut_has_use(mut_data[mut_index[mut]], mutflag::jiyva);
+}
+
+bool is_makhleb_mark(mutation_type mut)
+{
+    return _mut_has_use(mut_data[mut_index[mut]], mutflag::makhleb);
 }
 
 static mutation_type _get_random_qazlal_mutation()
@@ -3382,7 +3398,11 @@ int player::how_mutated(bool innate, bool levels, bool temp) const
     {
         if (you.mutation[i])
         {
-            const int mut_level = get_base_mutation_level(static_cast<mutation_type>(i), innate, temp);
+            // Infernal Marks should count for silver vulnerability despite
+            // being permanent mutations.
+            const bool check_innate = innate || is_makhleb_mark(static_cast<mutation_type>(i));
+            const int mut_level = get_base_mutation_level(static_cast<mutation_type>(i),
+                                                          check_innate, temp);
 
             if (levels)
                 result += mut_level;

crawl-ref/source/mutation.h
@@ -43,6 +43,7 @@ bool is_bad_mutation(mutation_type mut);
 bool is_good_mutation(mutation_type mut);
 bool is_body_facet(mutation_type mut);
 bool is_slime_mutation(mutation_type mut);
+bool is_makhleb_mark(mutation_type mut);
 
 bool mutate(mutation_type which_mutation, const string &reason,
             bool failMsg = true,

commit c3173615b286836edd8b64e25738eac9eb93e0ef

Fix off-by-one error in Dismiss Apostle descriptions (Mattlistener)
Accidentally broken by 258319946a6387f947fb8e9a2748ec4143870134

It was printing the stat block for the incorrect apostle (including, more
confusingly to players, the last apostle that was available to be
recruited, whether they had actually taken them or not!)

crawl-ref/source/ability.cc
@@ -1581,7 +1581,7 @@ string get_ability_desc(const ability_type ability, bool need_title)
         case ABIL_BEOGH_DISMISS_APOSTLE_2:
         case ABIL_BEOGH_DISMISS_APOSTLE_3:
         {
-            const int index = ability - ABIL_BEOGH_DISMISS_APOSTLE_1;
+            const int index = ability - ABIL_BEOGH_DISMISS_APOSTLE_1 + 1;
             lookup += "\n" + apostle_short_description(index) + "\n";
         }
         break;