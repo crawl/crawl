crawl/crawl

commit 23c40133775a1abd3d966f680c3459ae1234e16e

Indicate whether forms have bones.
And make the various bones checks use it properly.

crawl-ref/source/mutation.cc
@@ -1974,7 +1974,7 @@ bool physiology_mutation_conflict(mutation_type mutat)
     }
 
     // No bones for thin skeletal structure or horns.
-    if (!species::has_bones(you.species)
+    if (!player_has_bones()
         && (mutat == MUT_THIN_SKELETAL_STRUCTURE || mutat == MUT_HORNS))
     {
         return true;

crawl-ref/source/death-curse.cc
@@ -139,7 +139,7 @@ static void _curse_message(actor& target, actor* /*source*/,
     if (!silenced(you.pos()))
         messages.push_back("You hear strange and distant voices.");
 
-    if (species::has_bones(you.species))
+    if (player_has_bones())
         messages.push_back("Your bones ache.");
 
     mpr(*random_iterator(messages));

crawl-ref/source/form-data.h
@@ -50,6 +50,7 @@ struct form_entry
 
     // Row 9:
     form_capability has_hair;
+    form_capability has_bones;
 
     // Row 10:
     const char *shout_verb;
@@ -78,7 +79,7 @@ static const form_entry formdata[] =
     {}, true, {},
     SPWPN_NORMAL, LIGHTGREY, "", DEFAULT_VERBS,
     FC_DEFAULT, FC_DEFAULT, FC_DEFAULT, true, false,
-    FC_DEFAULT,
+    FC_DEFAULT, FC_DEFAULT,
     "", 0, "", "", "", "",
     {},
     {}
@@ -93,7 +94,7 @@ static const form_entry formdata[] =
     {}, true, {},
     SPWPN_VENOM, LIGHTGREEN, "Fangs", ANIMAL_VERBS,
     FC_DEFAULT, FC_FORBID, FC_FORBID, false, true,
-    FC_FORBID,
+    FC_FORBID, FC_FORBID,
     "hiss", -4, "front pincers", "", "crawl onto", "flesh",
     { {"venomous fangs", "You have venomous fangs."},
       {"", "You are tiny and dextrous."} // short-form "tiny" is automatically added
@@ -110,7 +111,7 @@ static const form_entry formdata[] =
     {}, true, FormScaling().Base(12).Scaling(8),
     SPWPN_NORMAL, RED, "", { "hit", "slash", "slice", "shred" },
     FC_DEFAULT, FC_DEFAULT, FC_DEFAULT, true, false,
-    FC_DEFAULT,
+    FC_DEFAULT, FC_DEFAULT,
     "", 0, "", "", "", "",
     {},
     {}
@@ -124,7 +125,7 @@ static const form_entry formdata[] =
     FormScaling().Base(27).Scaling(11), true, FormScaling().Base(9),
     SPWPN_NORMAL, LIGHTGREY, "", DEFAULT_VERBS,
     FC_DEFAULT, FC_DEFAULT, FC_FORBID, true, true,
-    FC_FORBID,
+    FC_FORBID, FC_FORBID,
     "", 0, "", "", "place yourself before", "stone",
     { { "powerful", "Your melee attacks are powerful." },
       { "torment resistance 1", "You are resistant to unholy torment." } // same as MUT_TORMENT_RESISTANCE
@@ -140,7 +141,7 @@ static const form_entry formdata[] =
     FormScaling().Base(9).Scaling(6), true, FormScaling().Base(7),
     SPWPN_NORMAL, LIGHTGREY, "", { "hit", "lash", "body-slam", "crush" },
     FC_DEFAULT, FC_ENABLE, FC_ENABLE, false, true,
-    FC_FORBID,
+    FC_FORBID, FC_ENABLE,
     "hiss", -2, "", "", "coil in front of", "flesh",
     { { "constrict", "You have a powerful constriction melee attack."} },
     // cold-blooded and amphibious are added separately
@@ -156,7 +157,7 @@ static const form_entry formdata[] =
     FormScaling().Base(12).Scaling(6), true, FormScaling().Base(15).Scaling(9),
     SPWPN_NORMAL, GREEN, "Teeth and claws", { "hit", "claw", "bite", "maul" },
     FC_ENABLE, FC_FORBID, FC_ENABLE, false, true,
-    FC_FORBID,
+    FC_FORBID, FC_ENABLE,
     "roar", 6, "foreclaw", "", "bow your head before", "flesh",
     { { "dragon claw", "You have a powerful clawing attack." },
       { "dragon scales", "Your giant scaled body is strong and resilient, but less evasive." },
@@ -173,7 +174,7 @@ static const form_entry formdata[] =
     {}, true, FormScaling().Base(6),
     SPWPN_DRAINING, MAGENTA, "", DEFAULT_VERBS,
     FC_DEFAULT, FC_DEFAULT, FC_FORBID, true, true,
-    FC_FORBID,
+    FC_FORBID, FC_DEFAULT,
     "", 0, "", "", "", "bone",
     { { "vile attack", "Your melee attacks drain, slow and weaken victims." },
       { "siphon essence", "You can torment nearby foes to heal from their wounds." },
@@ -191,7 +192,7 @@ static const form_entry formdata[] =
     {}, false, FormScaling().Base(-2),
     SPWPN_NORMAL, LIGHTGREY, "Teeth", ANIMAL_VERBS,
     FC_ENABLE, FC_FORBID, FC_ENABLE, false, true,
-    FC_ENABLE,
+    FC_ENABLE, FC_ENABLE,
     "squeak", -8, "foreclaw", "", "perch on", "flesh",
     { { "extremely fast", "You cover ground extremely quickly." },
       { "", "You are tiny, dextrous, and very evasive." } // short-form "tiny" is automatically added
@@ -210,7 +211,7 @@ static const form_entry formdata[] =
     {}, false, FormScaling().XLBased(),
     SPWPN_NORMAL, LIGHTGREY, "Teeth", ANIMAL_VERBS,
     FC_DEFAULT, FC_FORBID, FC_ENABLE, false, true,
-    FC_ENABLE,
+    FC_ENABLE, FC_ENABLE,
     "squeal", 0, "front trotter", "trotter", "bow your head before", "flesh",
     { { "very fast", "You cover ground very quickly." } },
     { { "weak attacks", "Your unarmed attacks are very weak." },
@@ -228,7 +229,7 @@ static const form_entry formdata[] =
     {}, true, {},
     SPWPN_NORMAL, LIGHTGREY, "", DEFAULT_VERBS,
     FC_DEFAULT, FC_DEFAULT, FC_DEFAULT, true, false,
-    FC_DEFAULT,
+    FC_DEFAULT, FC_DEFAULT,
     "", 0, "", "", "", "",
     {},
     {}
@@ -244,7 +245,7 @@ static const form_entry formdata[] =
     FormScaling().Base(20).Scaling(14).XLBased(), true, FormScaling().Base(9),
     SPWPN_NORMAL, BROWN, "Branches", { "hit", "smack", "pummel", "thrash" },
     FC_FORBID, FC_FORBID, FC_FORBID, false, true,
-    FC_FORBID,
+    FC_FORBID, FC_FORBID,
     "creak", 0, "branch", "root", "sway towards", "wood",
     { { "resilient", "Your bark is very hard, but your evasion is minimal." },
       { "branches", "Your unarmed attacks smack enemies forcefully with your branches." },
@@ -267,7 +268,7 @@ static const form_entry formdata[] =
     {}, false, FormScaling().XLBased(),
     SPWPN_NORMAL, LIGHTGREY, "Teeth", ANIMAL_VERBS,
     FC_DEFAULT, FC_FORBID, FC_ENABLE, false, true,
-    FC_ENABLE,
+    FC_ENABLE, FC_ENABLE,
     "squeak", -8, "front leg", "", "curl into a sanctuary of spikes before", "flesh",
     {},
     { { "no casting", "You cannot cast spells." }, }
@@ -286,7 +287,7 @@ static const form_entry formdata[] =
     SPWPN_NORMAL, LIGHTGREY, "Misty tendrils", { "touch", "touch",
                                                  "engulf", "engulf" },
     FC_ENABLE, FC_FORBID, FC_FORBID, false, true,
-    FC_FORBID,
+    FC_FORBID, FC_FORBID,
     "whoosh", -8, "misty tendril", "strand", "swirl around", "vapour",
     { { "", "You are tiny and evasive." },
       { "insubstantial", "You are insubstantial and cannot be petrified, ensnared, or set on fire." },
@@ -306,7 +307,7 @@ static const form_entry formdata[] =
     {}, false, FormScaling().XLBased(),
     SPWPN_NORMAL, LIGHTGREY, "", DEFAULT_VERBS,
     FC_DEFAULT, FC_FORBID, FC_FORBID, false, true,
-    FC_FORBID,
+    FC_FORBID, FC_FORBID,
     "", 0, "", "", "", "",
     {},
     { { "no casting", "You cannot cast spells." },
@@ -323,7 +324,7 @@ static const form_entry formdata[] =
     FormScaling().Base(12), false, FormScaling().Base(9).XLBased(),
     SPWPN_CONFUSE, BROWN, "Spores", FormAttackVerbs("release spores at"),
     FC_DEFAULT, FC_FORBID, FC_FORBID, false, true,
-    FC_FORBID,
+    FC_FORBID, FC_FORBID,
     "sporulate", -8, "hypha", "", "release spores on", "flesh",
     { { "", "You are tiny and evasive." },
       { "spores", "Your melee attacks release spores that confuse breathing creatures." },
@@ -344,7 +345,7 @@ static const form_entry formdata[] =
     {}, true, {},
     SPWPN_NORMAL, MAGENTA, "", DEFAULT_VERBS,
     FC_DEFAULT, FC_FORBID, FC_FORBID, true, true,
-    FC_FORBID,
+    FC_FORBID, FC_FORBID,
     "", 0, "", "", "", "shadow",
     { { "half damage", "Damage taken is halved, but you are drained when taking damage."},
       { "bleed smoke", "You bleed smoke when taking damage." },
@@ -368,7 +369,7 @@ static const form_entry formdata[] =
     {}, true, {},
     SPWPN_NORMAL, GREEN, "", { "nip at", "bite", "gouge", "chomp" },
     FC_DEFAULT, FC_ENABLE, FC_ENABLE, false, true,
-    FC_FORBID,
+    FC_FORBID, FC_ENABLE,
     "roar", 4, "foreclaw", "", "bow your heads before", "flesh",
     { { "fast swimmer", "You swim very quickly." },
       { "devour", "You can devour living enemies to heal." }
@@ -386,7 +387,7 @@ static const form_entry formdata[] =
     FormScaling().Base(12).Scaling(3), true, FormScaling().Base(24).Scaling(6),
     SPWPN_ELECTROCUTION, LIGHTCYAN, "", { "hit", "buffet", "batter", "blast" },
     FC_ENABLE, FC_DEFAULT, FC_FORBID, false, true,
-    FC_FORBID,
+    FC_FORBID, FC_FORBID,
     "bellow", 0, "", "", "place yourself before", "air",
     { { "electrical cleaving", "Your electrical attacks strike out in all directions at once." },
       { "evasive", "You are incredibly evasive." },
@@ -405,7 +406,7 @@ static const form_entry formdata[] =
     {}, true, {},
     SPWPN_NORMAL, LIGHTGREY, "", DEFAULT_VERBS,
     FC_DEFAULT, FC_DEFAULT, FC_DEFAULT, true, false,
-    FC_ENABLE,
+    FC_ENABLE, FC_DEFAULT,
     "", 0, "", "", "", "",
     {}, // slaying bonus added separately
     {}
@@ -420,7 +421,7 @@ static const form_entry formdata[] =
     {}, true, FormScaling().Base(2),
     SPWPN_NORMAL, GREEN, "", DEFAULT_VERBS,
     FC_DEFAULT, FC_DEFAULT, FC_DEFAULT, true, false,
-    FC_DEFAULT,
+    FC_DEFAULT, FC_DEFAULT,
     "shout twice", 0, "", "", "", "",
     { { "devouring maw", "Your midsection houses a second, enormous mouth." },
       { "", "You can devour living enemies to heal." }
@@ -437,7 +438,7 @@ static const form_entry formdata[] =
     {}, true, {},
     SPWPN_NORMAL, CYAN, "", DEFAULT_VERBS,
     FC_DEFAULT, FC_DEFAULT, FC_DEFAULT, true, false,
-    FC_DEFAULT,
+    FC_DEFAULT, FC_DEFAULT,
     "", 0, "", "", "", "",
     { { "contaminating", "Foes you strike become dangerously contaminated with magical radiation." }, },
     { { "glow", "You glow with magical radiation, making you easy to see." } }

crawl-ref/source/player-act.cc
@@ -711,7 +711,7 @@ string player::arm_name(bool plural, bool *can_plural) const
     string adj;
     if (form == transformation::death)
     {
-        if (species::has_bones(you.species))
+        if (player_has_bones())
             adj = "bony";
         else
             adj = "fossilised";

crawl-ref/source/player.cc
@@ -960,6 +960,14 @@ bool player_has_hair(bool temp, bool include_mutations)
     return species::has_hair(you.species);
 }
 
+bool player_has_bones(bool temp)
+{
+    if (temp && form_has_bones(you.form))
+        return true;
+
+    return species::has_bones(you.species);
+}
+
 bool player_has_feet(bool temp, bool include_mutations)
 {
     if (you.has_innate_mutation(MUT_CONSTRICTING_TAIL)

crawl-ref/source/player.h
@@ -1115,6 +1115,7 @@ maybe_bool you_can_wear(equipment_type eq, bool temp = false);
 bool player_can_use_armour();
 
 bool player_has_hair(bool temp = true, bool include_mutations = true);
+bool player_has_bones(bool temp = true);
 bool player_has_feet(bool temp = true, bool include_mutations = true);
 
 bool enough_hp(int minimum, bool suppress_msg, bool abort_macros = true);

crawl-ref/source/transform.cc
@@ -122,7 +122,7 @@ Form::Form(const form_entry &fe)
       can_bleed(fe.can_bleed),
       keeps_mutations(fe.keeps_mutations),
       changes_physiology(fe.changes_physiology),
-      has_hair(fe.has_hair),
+      has_hair(fe.has_hair), has_bones(fe.has_bones),
       shout_verb(fe.shout_verb),
       shout_volume_modifier(fe.shout_volume_modifier),
       hand_name(fe.hand_name), foot_name(fe.foot_name),
@@ -1219,6 +1219,24 @@ bool form_has_hair(transformation form)
         return false;
     else
         return species::has_hair(you.species);
+}
+
+/**
+ * Does this form have bones?
+ *
+ * @param form      The form in question.
+ * @return          Whether the form has bones.
+ */
+bool form_has_bones(transformation form)
+{
+    form_capability result = get_form(form)->has_bones;
+
+    if (result == FC_ENABLE)
+        return true;
+    else if (result == FC_FORBID)
+        return false;
+    else
+        return species::has_bones(you.species);
 }
 
 static set<equipment_type>

crawl-ref/source/transform.h
@@ -228,6 +228,8 @@ public:
 
     /// Does this form have hair?
     const form_capability has_hair;
+    /// Does this form have bones?
+    const form_capability has_bones;
 
     /// what verb does the player use when shouting in this form?
     const string shout_verb;
@@ -320,6 +322,7 @@ bool form_can_bleed(transformation form = you.form);
 // Does the form keep the benefits of resistance, scale, and aux mutations?
 bool form_keeps_mutations(transformation form = you.form);
 bool form_has_hair(transformation form = you.form);
+bool form_has_bones(transformation form = you.form);
 
 bool feat_dangerous_for_form(transformation which_trans,
                              dungeon_feature_type feat);

commit 01112393972fb22360e1e9a294cfd4c7340b90fc

fix: Don't reject bones muts in tempforms (acrobat, Ge0ff)
23c4013377 introduced a bug where demonspawn could miss a horns levelup mut
(with a blank demonic ancestry message) by gaining an XL while in treeform,
stormform, wispform, etc. We really don't want to be rejecting mutations
based on temporary forms here.

crawl-ref/source/mutation.cc
@@ -2035,7 +2035,7 @@ bool physiology_mutation_conflict(mutation_type mutat)
     }
 
     // No bones for thin skeletal structure or horns.
-    if (!you.has_bones()
+    if (!you.has_bones(false)
         && (mutat == MUT_THIN_SKELETAL_STRUCTURE || mutat == MUT_HORNS))
     {
         return true;