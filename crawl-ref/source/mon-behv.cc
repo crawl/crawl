b3f2be6f2865462e14507bee8210875ca55b1f7c
Nicholas Feinberg <pleasingfung@gmail.com>
2024-08-10 08:36:19 -0700

    Remove an antique bit of monster AI (Monkooky)
    Doesn't seem helpful to make low HP monsters avoid targeting the
    player. This logic dates back to pre-Stone Soup days.

--- crawl-ref/source/mon-behv.cc
+++ crawl-ref/source/mon-behv.cc
@@ -269,7 +269,6 @@ void handle_behaviour(monster* mon)
         proxPlayer = false;
 
     bool proxFoe;
-    bool isHealthy  = (mon->hit_points > mon->max_hit_points / 2);
     bool isSmart    = (mons_intel(*mon) >= I_HUMAN);
     bool isScared   = mon->has_ench(ENCH_FEAR);
     bool isPacified = mon->pacified();
@@ -398,7 +397,6 @@ void handle_behaviour(monster* mon)
         && !mons_is_avatar(mon->type)
         && mon->foe != MHITYOU && mon->foe != MHITNOT
         && proxPlayer && !mon->berserk_or_frenzied()
-        && isHealthy
         && !one_chance_in(3))
     {
         mon->foe = MHITYOU;
