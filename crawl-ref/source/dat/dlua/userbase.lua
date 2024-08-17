crawl/crawl

commit 60c0b14602b623ef3867852943f44a184c42de05

feat: clua api support for inventory scrolls
This commit adds the clua hooks c_choose_identify,
c_choose_enchant_weapon, c_choose_brand_weapon, and
c_choose_enchant_armour, which allows users to choose targets for the
corresponding scrolls without a menu. These hooks all return an
inventory letter to indicate the target. If the target item isn't valid
for the scroll, the usual menu happens.

Add documentation for these and also document the
c_choose_okawaru_weapon and c_choose_okawaru_armour hooks.

crawl-ref/source/dat/dlua/userbase.lua
@@ -418,10 +418,62 @@ function c_message(text, channel) end
 --
 -- This hook can be defined to execute lua when an acquirement scroll is read.
 --
--- The hook should call @{items.acquirement_items} to get an array of
--- @{items.Item} representations of the offered items. If it returns a valid
--- index in this array, the given item will be acquired without the usual
--- acquirement menu.
+-- The hook should call @{items.acquirement_items} with an argument of 1 to get
+-- an array of @{items.Item} representations of the offered items. If it
+-- returns a valid index in this array, the given item will be acquired without
+-- the usual acquirement menu.
 --
 -- @treturn int An index between 1 and the number of offered items.
 -- @function c_choose_acquirement
+
+--- Okawaru weapon and armour acquirement hooks.
+--
+-- These hooks can be defined to execute lua when an Okawaru's Recieve Weapon
+-- or Recieve Armour capstone abilities are used.
+--
+-- The hook should call @{items.acquirement_items} with an argument of 2
+-- (for weapons) or 3 (for armour) to get an array of @{items.Item}
+-- representations of the offered items. If it returns a valid index in this
+-- array, the given item will be acquired without the usual acquirement menu.
+--
+-- @function c_choose_okawaru_weapon
+-- @treturn int An index between 1 and the number of offered items.
+--
+-- @function c_choose_okawaru_armour
+-- @treturn int An index between 1 and the number of offered items.
+
+-- Identify scroll hook.
+--
+-- This hook can be defined to execute lua when an identify scroll is read. If
+-- the item with the returned inventory letter is not identified, the scroll
+-- will target this item without the usual menu.
+--
+-- @treturn int A string with a slot letter (i.e. a-z or A-Z).
+-- @function c_choose_identify
+
+-- Enchant weapon scroll hook.
+--
+-- This hook can be defined to execute lua when an enchant weapon scroll is
+-- read. If the item with the returned inventory letter is an enchantable
+-- weapon, the scroll will target this weapon without the usual menu.
+--
+-- @treturn int A string with a slot letter (i.e. a-z or A-Z).
+-- @function c_choose_enchant_weapon
+
+-- Brand weapon scroll hook.
+--
+-- This hook can be defined to execute lua when a brand weapon scroll is read.
+-- If the item with the returned inventory letter is a brandable weapon, the
+-- scroll will target this weapon without the usual menu.
+--
+-- @treturn int A string with a slot letter (i.e. a-z or A-Z).
+-- @function c_choose_brand_weapon
+
+-- Enchant armour scroll hook.
+--
+-- This hook can be defined to execute lua when an enchant armour scroll is
+-- read. If the item with the returned inventory letter is enchantable armour,
+-- the scroll will target this armour without the usual menu.
+--
+-- @treturn int A string with a slot letter (i.e. a-z or A-Z).
+-- @function c_choose_enchant_armour

crawl-ref/source/item-prop.cc
@@ -1676,6 +1676,14 @@ bool is_enchantable_armour(const item_def &arm, bool unknown)
     return true;
 }
 
+bool is_enchantable_weapon(const item_def &weapon, bool unknown)
+{
+    return weapon.base_type == OBJ_WEAPONS
+       && !is_artefact(weapon)
+       && (unknown && !item_ident(weapon, ISFLAG_KNOW_PLUSES)
+           || weapon.plus < MAX_WPN_ENCHANT);
+}
+
 //
 // Weapon information and checking functions:
 //

crawl-ref/source/item-prop.h
@@ -124,6 +124,7 @@ int wand_charge_value(int type, int item_level = 1) PURE;
 bool is_known_empty_wand(const item_def &item) PURE;
 #endif
 bool is_offensive_wand(const item_def &item) PURE;
+bool is_enchantable_weapon(const item_def &weapon, bool unknown = false) PURE;
 bool is_enchantable_armour(const item_def &arm, bool unknown = false) PURE;
 
 bool is_shield(const item_def &item) PURE;

crawl-ref/source/item-use.cc
@@ -3267,8 +3267,23 @@ static item_def* _scroll_choose_weapon(bool alreadyknown, const string &pre_msg,
 // Returns true if the scroll is used up.
 static bool _handle_brand_weapon(bool alreadyknown, const string &pre_msg)
 {
-    item_def* weapon = _scroll_choose_weapon(alreadyknown, pre_msg,
-                                             SCR_BRAND_WEAPON);
+    item_def* weapon = nullptr;
+    string letter = "";
+    if (!clua.callfn("c_choose_brand_weapon", ">s", &letter))
+    {
+        if (!clua.error.empty())
+            mprf(MSGCH_ERROR, "Lua error: %s", clua.error.c_str());
+    }
+    else if (!letter.empty())
+    {
+        item_def &item = you.inv[letter_to_index(letter.c_str()[0])];
+        if (item.defined() && is_brandable_weapon(item, true))
+            weapon = &item;
+    }
+
+    if (!weapon)
+        weapon = _scroll_choose_weapon(alreadyknown, pre_msg, SCR_BRAND_WEAPON);
+
     if (!weapon)
         return !alreadyknown;
 
@@ -3318,8 +3333,25 @@ bool enchant_weapon(item_def &wpn, bool quiet)
  */
 static bool _identify(bool alreadyknown, const string &pre_msg, int &link)
 {
-    item_def* itemp = _choose_target_item_for_scroll(alreadyknown, OSEL_UNIDENT,
-                       "Identify which item? (\\ to view known items)");
+    item_def* itemp = nullptr;
+    string letter = "";
+    if (!clua.callfn("c_choose_identify", ">s", &letter))
+    {
+        if (!clua.error.empty())
+            mprf(MSGCH_ERROR, "Lua error: %s", clua.error.c_str());
+    }
+    else if (!letter.empty())
+    {
+        item_def &item = you.inv[letter_to_index(letter.c_str()[0])];
+        if (item.defined() && !fully_identified(item))
+            itemp = &item;
+    }
+
+    if (!itemp)
+    {
+        itemp = _choose_target_item_for_scroll(alreadyknown, OSEL_UNIDENT,
+            "Identify which item? (\\ to view known items)");
+    }
 
     if (!itemp)
         return !alreadyknown;
@@ -3355,8 +3387,26 @@ static bool _identify(bool alreadyknown, const string &pre_msg, int &link)
 
 static bool _handle_enchant_weapon(bool alreadyknown, const string &pre_msg)
 {
-    item_def* weapon = _scroll_choose_weapon(alreadyknown, pre_msg,
-                                             SCR_ENCHANT_WEAPON);
+    item_def* weapon = nullptr;
+    string letter = "";
+    if (!clua.callfn("c_choose_enchant_weapon", ">s", &letter))
+    {
+        if (!clua.error.empty())
+            mprf(MSGCH_ERROR, "Lua error: %s", clua.error.c_str());
+    }
+    else if (!letter.empty())
+    {
+        item_def &item = you.inv[letter_to_index(letter.c_str()[0])];
+        if (item.defined() && is_enchantable_weapon(item, true))
+            weapon = &item;
+    }
+
+    if (!weapon)
+    {
+        weapon = _scroll_choose_weapon(alreadyknown, pre_msg,
+                                       SCR_ENCHANT_WEAPON);
+    }
+
     if (!weapon)
         return !alreadyknown;
 
@@ -3397,8 +3447,25 @@ bool enchant_armour(int &ac_change, bool quiet, item_def &arm)
 
 static int _handle_enchant_armour(bool alreadyknown, const string &pre_msg)
 {
-    item_def* target = _choose_target_item_for_scroll(alreadyknown, OSEL_ENCHANTABLE_ARMOUR,
-                                                      "Enchant which item?");
+    item_def* target= nullptr;
+    string letter = "";
+    if (!clua.callfn("c_choose_enchant_armour", ">s", &letter))
+    {
+        if (!clua.error.empty())
+            mprf(MSGCH_ERROR, "Lua error: %s", clua.error.c_str());
+    }
+    else if (!letter.empty())
+    {
+        item_def &item = you.inv[letter_to_index(letter.c_str()[0])];
+        if (item.defined() && is_enchantable_armour(item, true))
+            target = &item;
+    }
+
+    if (!target)
+    {
+        target = _choose_target_item_for_scroll(alreadyknown,
+            OSEL_ENCHANTABLE_ARMOUR, "Enchant which item?");
+    }
 
     if (!target)
         return alreadyknown ? -1 : 0;

commit f3f77184821e8b3f3ed0117cb24466b6335ff5f2

fix: clua api ldoc generation
This has apparently been broken since 60c0b14602 (mid-0.31), as mentioned
in previous commits on this ldoc remains pretty fragile.

To regenerate these, run `make api`.

crawl-ref/source/dat/dlua/userbase.lua
@@ -426,10 +426,10 @@ function c_message(text, channel) end
 -- @treturn int An index between 1 and the number of offered items.
 -- @function c_choose_acquirement
 
---- Okawaru weapon and armour acquirement hooks.
+--- Okawaru weapon acquirement hook.
 --
--- These hooks can be defined to execute lua when an Okawaru's Recieve Weapon
--- or Recieve Armour capstone abilities are used.
+-- This hook can be defined to execute lua when Okawaru's Recieve Weapon
+-- capstone ability is used.
 --
 -- The hook should call @{items.acquirement_items} with an argument of 2
 -- (for weapons) or 3 (for armour) to get an array of @{items.Item}
@@ -438,6 +438,16 @@ function c_message(text, channel) end
 --
 -- @function c_choose_okawaru_weapon
 -- @treturn int An index between 1 and the number of offered items.
+
+--- Okawaru armour acquirement hook.
+--
+-- This hook can be defined to execute lua when Okawaru's Recieve Armour
+-- capstone ability is used.
+--
+-- The hook should call @{items.acquirement_items} with an argument of 2
+-- (for weapons) or 3 (for armour) to get an array of @{items.Item}
+-- representations of the offered items. If it returns a valid index in this
+-- array, the given item will be acquired without the usual acquirement menu.
 --
 -- @function c_choose_okawaru_armour
 -- @treturn int An index between 1 and the number of offered items.