crawl/crawl

commit d9015ae29d17d9fb551e92c09c08022577a67ff7

Fix inner flame explosion message order for summons
This was incorrect, even before the recent refactoring. If an abjurable
summon exploded from inner flame, the message log would print something
like: "The wolf spider disappears in a puff of smoke. The wolf spider
explodes!". Now it will properly print a summon poof message afterward,
instead (by saving it in the explosion_fineff itself).

crawl-ref/source/spl-clouds.cc
@@ -97,7 +97,7 @@ void explode_blastmotes_at(coord_def p)
     const string boom  = "The cloud of blastmotes explodes!";
     const string sanct = "By Zin's power, the fiery explosion is contained.";
     explosion_fineff::schedule(beam, boom, sanct, EXPLOSION_FINEFF_CONCUSSION,
-                               nullptr);
+                               nullptr, nullptr);
 }
 
 cloud_type spell_to_cloud(spell_type spell)

crawl-ref/source/fineff.cc
@@ -633,6 +633,9 @@ void explosion_fineff::fire()
                                         actor_to_death_source(beam.agent()));
         }
     }
+
+    if (you.see_cell(beam.target) && !poof_message.empty())
+        mpr(poof_message.c_str());
 }
 
 void delayed_action_fineff::fire()

crawl-ref/source/fineff.h
@@ -276,17 +276,19 @@ public:
     void fire() override;
 
     static void schedule(bolt &beam, string boom, string sanct,
-                         explosion_fineff_type typ, const actor* flame_agent)
+                         explosion_fineff_type typ, const actor* flame_agent,
+                         string poof)
     {
         final_effect::schedule(new explosion_fineff(beam, boom, sanct,
-                                                    typ, flame_agent));
+                                                    typ, flame_agent, poof));
     }
 protected:
     explosion_fineff(const bolt &beem, string boom, string sanct,
-                     explosion_fineff_type _typ, const actor* agent)
+                     explosion_fineff_type _typ, const actor* agent,
+                     string poof)
         : final_effect(0, 0, coord_def()), beam(beem),
           boom_message(boom), sanctuary_message(sanct),
-          typ(_typ), flame_agent(agent)
+          typ(_typ), flame_agent(agent), poof_message(poof)
     {
     }
     bolt beam;
@@ -294,6 +296,7 @@ protected:
     string sanctuary_message;
     explosion_fineff_type typ;
     const actor* flame_agent;
+    string poof_message;
 };
 
 // A fineff that triggers a daction; otherwise the daction

crawl-ref/source/mon-death.cc
@@ -3000,10 +3000,12 @@ item_def* monster_die(monster& mons, killer_type killer,
         }
     }
 
-    if (!silent && !mons_reset)
+    // Poof messages for summoned things exploded are stored in explosion_fineff
+    // to print later.
+    if (!silent && !mons_reset && !exploded)
         _print_summon_poof_message(mons, real_death);
 
-    if (!was_banished && !mons_reset)
+    if (!was_banished && !mons_reset && !exploded)
         _monster_die_cloud(mons, real_death);
 
     item_def* corpse = nullptr;

crawl-ref/source/mon-explode.cc
@@ -327,6 +327,10 @@ bool explode_monster(monster* mons, killer_type killer, bool pet_kill)
     actor* agent = nullptr;
     bool inner_flame = false;
 
+    string poof_msg = "";
+    if (mons->is_abjurable())
+        poof_msg = "The " + mons->name(DESC_PLAIN) + " residue " + summoned_poof_msg(*mons) + ".";
+
     auto it = explosions.find(type);
     if (it != explosions.end())
     {
@@ -412,7 +416,7 @@ bool explode_monster(monster* mons, killer_type killer, bool pet_kill)
     {
         const auto typ = inner_flame ? EXPLOSION_FINEFF_INNER_FLAME
                                      : EXPLOSION_FINEFF_GENERIC;
-        explosion_fineff::schedule(beam, boom_msg, sanct_msg, typ, agent);
+        explosion_fineff::schedule(beam, boom_msg, sanct_msg, typ, agent, poof_msg);
     }
 
     // Monster died in explosion, so don't re-attach it to the grid.

commit 4194ddcd6649d0b5e9c96bb3ac5f06bd0078deb4

Fix crash with blastmotes clouds exploding
Accidentally broken by d9015ae29d17d9

crawl-ref/source/spl-clouds.cc
@@ -97,7 +97,7 @@ void explode_blastmotes_at(coord_def p)
     const string boom  = "The cloud of blastmotes explodes!";
     const string sanct = "By Zin's power, the fiery explosion is contained.";
     explosion_fineff::schedule(beam, boom, sanct, EXPLOSION_FINEFF_CONCUSSION,
-                               nullptr, nullptr);
+                               nullptr, "");
 }
 
 cloud_type spell_to_cloud(spell_type spell)