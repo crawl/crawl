211d9555a860180e107074d0dc0ca5f79af43fa5
Brian Power <powerbf.it@gmail.com>
2024-10-08 02:31:58 +1000

    i18n: dbm-check.py: Start with an empty dict for each file

--- crawl-ref/source/util/i18n/dbm-check.py
+++ crawl-ref/source/util/i18n/dbm-check.py
@@ -16,8 +16,8 @@ if len(sys.argv) < 2:
     sys.stderr.write("Usage: dbm-check.py <files>\n")
     sys.exit(1)
 
-data = {}
 for file_name in sys.argv[1:]:
+    data = {}
     dbm.read_dbm(file_name, data)
     empties = [key for key, value in data.items() if value == ""]
     print("{}: {} entries, {} empty values".format(file_name, len(data), len(empties)))
