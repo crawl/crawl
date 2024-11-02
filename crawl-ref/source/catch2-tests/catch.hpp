c5051a7168cefb0e1f01312457a7b8507e99271f
Brian Power <powerbf.it@gmail.com>
2024-11-02 23:33:06 +1000

    Fix catch2-tests compile error in github workflow

--- crawl-ref/source/catch2-tests/catch.hpp
+++ crawl-ref/source/catch2-tests/catch.hpp
@@ -10714,7 +10714,7 @@ namespace Catch {
 
     // 32kb for the alternate stack seems to be sufficient. However, this value
     // is experimentally determined, so that's not guaranteed.
-    static constexpr std::size_t sigStackSize = 32768 >= MINSIGSTKSZ ? 32768 : MINSIGSTKSZ;
+    static const std::size_t sigStackSize = 32768 >= MINSIGSTKSZ ? 32768 : MINSIGSTKSZ;
 
     static SignalDefs signalDefs[] = {
         { SIGINT,  "SIGINT - Terminal interrupt signal" },
