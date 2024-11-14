crawl/crawl

commit 02fa12cfa58bf5b15c769780ce23e0ab14f10ce6

Adjust player draconian color balance a little
Draconians have long been considered a strong species, but after the breath
rework of 926e5b6e4c1ff7608bc4a4bdb60678fc7a80099a , multiple players
thought they had become even stronger. This wasn't my intention and I had
considered nerfing their aptitudes in weapon/magic skills a little in
compensation, but after further investigation into player data, it appears
like draconians may benefit from being played by experienced players to a
noticably greater degree than most other species do - in other words, they
are only an outlier in species strength when including experienced players
and seem closer to middle-of-the-pack otherwise. I am theorizing that this
is because benefiting from the new breath mechanics involves active
resource management moreso than most species' strengths do. At any rate, I
am reluctant to broadly nerf a species that may be 'fine' for the larger
chunk of Crawl's playerbase, especially since one of the ways in which they
are strong is varied, interactive, and unique.

However, balance *between* colors could use some adjustment. To this end, I
am making the following changes:

-Grey draconians get their +5 AC bonus at XL 14 instead of XL 7
-Red draconian breath damage is significantly increased and can no longer
 miss.
-Black draconian breath damage is slightly reduced.
-Yellow draconian breath range is increased by 1. (A tweak to acid bite may
 also be coming in a future commit)
-Purple draconians no longer have increased willpower scaling with XL, but
 get the Strong Will mutation at XL 7 and at XL 14 gain 'Inviolate Magic'
 which prevents their magical effects from being unravelled and reduces
 hostile MP drain by 2/3rd.

More detailed reasoning:

-Grey draconians are clearly ahead of the pack at the moment. 5 AC may be
 debatably weaker than a key resist in lategame, but 5 AC is a lot when you
 get it all at once at XL 7. To that end, move their +5 AC to XL 14

crawl-ref/source/mutation.cc
@@ -985,8 +985,7 @@ static vector<string> _get_fakemuts(bool terse)
         {
             // XX generalize this code somehow?
             const string scale_clause = string(species::scale_type(you.species))
-                  + " scales are "
-                  + (you.species == SP_GREY_DRACONIAN ? "very " : "") + "hard";
+                  + " scales are hard";
 
             result.push_back(_annotate_form_based(
                         make_stringf("Your %s. (AC +%d)", you.species == SP_NAGA

crawl-ref/source/beam.cc
@@ -670,9 +670,9 @@ static beam_type _chaos_beam_flavour(bolt* beam)
 dice_def combustion_breath_damage(int pow, bool allow_random)
 {
     if (allow_random)
-        return dice_def(3, 3 + div_rand_round(pow * 2, 3));
+        return dice_def(3, 4 + div_rand_round(pow * 10, 9));
     else
-        return dice_def(3, 3 + pow * 2 / 3);
+        return dice_def(3, 4 + pow * 10 / 9);
 }
 
 static void _combustion_breath_explode(bolt *parent, coord_def pos)
@@ -3906,7 +3906,8 @@ void bolt::affect_player_enchantment(bool resistible)
         if (!player_is_debuffable())
             break;
 
-        debuff_player();
+        // If the player is unravelling themselves voluntarily, allow it to work.
+        debuff_player(agent() && agent()->is_player());
         _unravelling_explode(*this);
         obvious_effect = true;
         break;

crawl-ref/source/dat/species/draconian-grey.yaml
@@ -38,5 +38,7 @@ fake_mutations:
 mutations:
   1:
     MUT_COLD_BLOODED: 1
+  14:
+    MUT_IRON_FUSED_SCALES: 1
 
 orc_name: Grey Orconian

crawl-ref/source/dat/species/draconian-purple.yaml
@@ -12,7 +12,7 @@ aptitudes:
   xp: -1
   hp: 1
   mp_mod: 0
-  wl: 6
+  wl: 3
   fighting: 1
   ranged weapons: -1
   throwing: -1
@@ -31,6 +31,10 @@ levelup_stat_frequency: 4
 mutations:
   1:
     MUT_COLD_BLOODED: 1
+  7:
+    MUT_STRONG_WILLED: 1
+  14:
+    MUT_INVIOLATE_MAGIC: 1
 fake_mutations:
   - long: You can breathe blasts of antimagic.
     short: nullifying breath

crawl-ref/source/mutation-data.h
@@ -28,6 +28,26 @@ struct mutation_category_def
 static const mutation_def mut_data[] =
 {
 
+{ MUT_IRON_FUSED_SCALES, 0, 1, mutflag::good, true,
+  "iron-fused scales",
+
+  {"Your scales are fused with iron. (AC + 5)", "", ""},
+  {"Iron fuses itself to your scales.", "", ""},
+  {"The iron flakes away from your scales.", "", ""},
+  TILEG_MUT_GENERIC_GOOD_MUTATION,
+  {"Iron will fuse itself to your scales (AC + 5)"}
+},
+
+{ MUT_INVIOLATE_MAGIC, 0, 1, mutflag::good, false,
+  "inviolate magic",
+
+  {"Your magical power and effects resist distruption.", "", ""},
+  {"Your magical power grows resistant to distruption.", "", ""},
+  {"Your magical power loses its resistance to disruption.", "", ""},
+  TILEG_MUT_GENERIC_GOOD_MUTATION,
+  {"Your magical power will grow resistant to disruption."}
+},
+
 { MUT_TOUGH_SKIN, 0, 3, mutflag::good, true,
   "tough skin",
 

crawl-ref/source/mutation-type.h
@@ -318,6 +318,8 @@ enum mutation_type
     MUT_MAKHLEB_MARK_EXECUTION,
     MUT_MAKHLEB_MARK_ATROCITY,
     MUT_MAKHLEB_MARK_FANATIC,
+    MUT_IRON_FUSED_SCALES,
+    MUT_INVIOLATE_MAGIC,
     NUM_MUTATIONS,
     CATEGORY_MUTATIONS,
     RANDOM_MUTATION = CATEGORY_MUTATIONS,

crawl-ref/source/ouch.cc
@@ -1083,7 +1083,7 @@ void ouch(int dam, kill_method_type death_type, mid_t source, const char *aux,
             mp = min(mp, you.magic_points);
 
             dam -= mp;
-            drain_mp(mp);
+            drain_mp(mp, true);
 
             // Wake players who took fatal damage exactly equal to current HP,
             // but had it reduced below fatal threshold by spirit shield.

crawl-ref/source/player-equip.cc
@@ -750,7 +750,7 @@ static void _spirit_shield_message(bool unmeld)
 #if TAG_MAJOR_VERSION == 34
         if (you.species == SP_DEEP_DWARF)
         {
-            drain_mp(you.magic_points);
+            drain_mp(you.magic_points, true);
             mpr("Now linked to your health, your magic stops regenerating.");
         }
 #endif

commit be89f894c6b6857ce1d0504840bc40d35689cf34

Fix a purple draconian Ru sac will crash
https://underhound.eu/crawl/morgue/ZingZang1235/crash-ZingZang1235-20241112-184040.txt

What happened here was that first, the player took Ru sacrifice will,
granting an innate MUT_WEAK_WILLED. Then, at XL7, they became a purple
draconian, and the levelup mutation code tried to grant them an innate
MUT_STRONG_WILLED. This caused the loop in perma_mutate() to assert,
because there was an implicit assumption there that we wouldn't try
to grant conflicting innate mutations.

To workaround this, here we prevent granting a species mutation on
levelup (!) if a prior conflicting innate mutation exists.

This really shouldn't be happening, but as we've seen, continued
expansion of Ru sacrifices and species mutations (and now Makhleb marks)
over time makes it increasingly likely that someone will inadvertently
add conflicting innate muts which will need to be handled.

These conditions could have theoretically triggered an assert for a
long time, but as far as I can tell, the conditions to actually
trigger this bug were introduced in 02fa12cfa58b when purple Dr gained
strong-willed. This commit therefore shouldn't need to be backported
further than 0.32.

I've also added an explicit assert here to maybe save some future
devtime in tracking this down, if it reoccurs through an alternate
(non-levelup) call to perma_mutate() in the future.

crawl-ref/source/mutation.cc
@@ -3383,6 +3383,7 @@ void roll_demonspawn_mutations()
 bool perma_mutate(mutation_type which_mut, int how_much, const string &reason)
 {
     ASSERT(_is_valid_mutation(which_mut));
+    ASSERT(!mut_check_conflict(which_mut, true));
 
     int cap = get_mutation_cap(which_mut);
     how_much = min(how_much, cap);

crawl-ref/source/species.cc
@@ -720,6 +720,11 @@ void give_level_mutations(species_type species, int xp_level)
     for (const auto& lum : get_species_def(species).level_up_mutations)
         if (lum.xp_level == xp_level)
         {
+            // XX: perma_mutate() doesn't handle prior conflicting innate muts,
+            // so we skip this mut if this occurs, e.g. through a Ru sacrifice.
+            if (mut_check_conflict(lum.mut, true))
+                continue;
+
             perma_mutate(lum.mut, lum.mut_level,
                          species::name(species) + " growth");
         }