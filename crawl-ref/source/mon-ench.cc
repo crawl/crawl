crawl/crawl

commit fb19e97205a53957dc0dd5bdbeb20ada8cb9d718

Refactor backend handling of summons / created monsters (Part 1)
Crawl's handling of summoned and not-technically-summoned-but-close
monsters is a complicated and somewhat untidy thing that makes it harder to
generalize behavior between conventional summons and other monsters that
share many of their properties, without considerable special cases and
bespoke code in various places. For instance, monsters created by the
player can use one of *4* different enchantments for their timer, which can
result in those monsters expiring with with 4 different killer_types (but
not even on a 1-to-1 basis with which timer is used!) that results in
sometimes-unclear differences in behavior. It's also needlessly verbose to
ask if a given monster was created by a given spell, easy to forget to
check for all 4 different ways a monster can be temporary here, and many
other issues that have resulted in various bugs over time (several of which
were outstanding at the time of this commit.)

This is an attempt to significantly clean up and better-document this
entire system, as well as make it easier to interact with and fix a number
of bugs in the process.

Major changes:

-ENCH_ABJ, ENCH_FAKE_ABJURATION, and ENCH_SHORT_LIVED are all folded into
 a single timer: ENCH_SUMMON_TIMER. (Save-compat code will convert
 deprecated enchantments into the new standard timer).

-Rather than differentiate summon behavior by which timer is used, two
 monster flags are added (MF_ACTUAL_SUMMON and MF_PERSISTS) to cover the
 most clear and actually-meaningful differences that already existed.
 -MF_ACTUAL_SUMMON indicates the a monster is abjurable, will produce a
  poof message (and possibly clouds) when it dies - regardless of whether
  this was by timeout or being directly killed - and will not produce other
  death effects (like simulacrum freezing clouds) when it times out.
 -MF_PERSISTS indicates that a monster should not be removed when its

crawl-ref/source/mon-ench.cc
@@ -634,29 +634,17 @@ void monster::remove_enchantment_effect(const mon_enchant &me, bool quiet)
         monster_web_cleanup(*this, true);
         break;
     }
-    case ENCH_FAKE_ABJURATION:
-        if (type == MONS_BATTLESPHERE)
-            return end_battlesphere(this, false);
-    case ENCH_ABJ:
+
+    case ENCH_SUMMON_TIMER:
         if (type == MONS_SPECTRAL_WEAPON)
             return end_spectral_weapon(this, false);
-        // Set duration to -1 so that monster_die() and any of its
-        // callees can tell that the monster ran out of time or was
-        // abjured.
-        add_ench(mon_enchant(
-            (me.ench != ENCH_FAKE_ABJURATION) ?
-                ENCH_ABJ : ENCH_FAKE_ABJURATION, 0, 0, -1));
+        else if (type == MONS_BATTLESPHERE)
+            return end_battlesphere(this, false);
 
         if (berserk())
             simple_monster_message(*this, " is no longer berserk.");
 
-        monster_die(*this, (me.ench == ENCH_FAKE_ABJURATION) ? KILL_MISC :
-                            (quiet) ? KILL_DISMISSED : KILL_RESET, NON_MONSTER);
-        break;
-    case ENCH_SHORT_LIVED:
-        // Conjured ball lightnings explode when they time out.
-        suicide();
-        monster_die(*this, KILL_TIMEOUT, NON_MONSTER);
+        monster_die(*this, quiet ? KILL_RESET : KILL_TIMEOUT, NON_MONSTER);
         break;
 
     case ENCH_SOUL_RIPE:
@@ -750,10 +738,10 @@ void monster::remove_enchantment_effect(const mon_enchant &me, bool quiet)
     case ENCH_HAUNTING:
         if (type != MONS_SOUL_WISP)
         {
-            mon_enchant abj = get_ench(ENCH_ABJ);
-            abj.degree = 1;
-            abj.duration = min(5 + random2(30), abj.duration);
-            update_ench(abj);
+            mon_enchant timer = get_ench(ENCH_SUMMON_TIMER);
+            timer.degree = 1;
+            timer.duration = min(5 + random2(30), timer.duration);
+            update_ench(timer);
         }
         break;
 
@@ -1284,7 +1272,7 @@ static void _merfolk_avatar_song(monster* mons)
                 monster* soul = create_monster(
                     mgen_data(MONS_DROWNED_SOUL, SAME_ATTITUDE(mons),
                               deep_water[i], mons->foe, MG_FORCE_PLACE)
-                    .set_summoned(mons, 1, SPELL_NO_SPELL));
+                    .set_summoned(mons, SPELL_NO_SPELL, summ_dur(1)));
 
                 // Scale down drowned soul damage for low level merfolk avatars
                 if (soul)
@@ -1338,7 +1326,7 @@ void monster::apply_enchantment(const mon_enchant &me)
     case ENCH_SICK:
     case ENCH_CORONA:
     case ENCH_CONTAM:
-    case ENCH_ABJ:
+    case ENCH_SUMMON_TIMER:
     case ENCH_CHARM:
     case ENCH_SLEEP_WARY:
     case ENCH_LOWERED_WL:
@@ -1350,7 +1338,6 @@ void monster::apply_enchantment(const mon_enchant &me)
     case ENCH_LIFE_TIMER:
     case ENCH_FLIGHT:
     case ENCH_DAZED:
-    case ENCH_FAKE_ABJURATION:
     case ENCH_RECITE_TIMER:
     case ENCH_INNER_FLAME:
     case ENCH_MUTE:
@@ -1501,12 +1488,6 @@ void monster::apply_enchantment(const mon_enchant &me)
         decay_enchantment(en, true);
         break;
     }
-
-    case ENCH_SHORT_LIVED:
-        // This should only be used for ball lightning -- bwr
-        if (decay_enchantment(en))
-            suicide();
-        break;
 
     case ENCH_SLOWLY_DYING:
         // If you are no longer dying, you must be dead.
@@ -1910,83 +1891,61 @@ void monster::apply_enchantment(const mon_enchant &me)
     }
 }
 
-void monster::mark_summoned(int longevity, bool mark_items, int summon_type, bool abj)
+void monster::mark_summoned(int summon_type, int longevity, bool mark_items)
 {
-    if (abj)
-        add_ench(mon_enchant(ENCH_ABJ, longevity));
-    if (summon_type != 0)
-        add_ench(mon_enchant(ENCH_SUMMON, summon_type, 0, INT_MAX));
+    if (longevity > 0)
+        add_ench(mon_enchant(ENCH_SUMMON_TIMER, 1, 0, longevity));
+
+    add_ench(mon_enchant(ENCH_SUMMON, summon_type, 0, INT_MAX));
 
     if (mark_items)
         for (mon_inv_iterator ii(*this); ii; ++ii)
             ii->flags |= ISFLAG_SUMMONED;
 }
 
-/* Is the monster temporarily summoned?
+/* Is the monster created by a spell or effect (generally temporary)?
  *
- * Monsters must have ENCH_ABJ (giving how long they last) to be considered
- * summons. If they additionally set ENCH_SUMMON, which gives how they were
- * derived, this must not be from certain spells or "monster summoning types"
- * we don't consider actual summons. Temporary monsters with
- * ENCH_FAKE_ABJURATION also aren't summons, and durably summoned monsters
- * aren't temporary.
- *
- * @param[out] duration    The monster's summon duration in aut.
- * @param[out] summon_type The monster's means of summoning. If negative, this
- *                         is a mon_summon_type, otherwise it's a spell_type.
- * @returns True if the monster is a temporary summon, false otherwise.
+ * Monsters with ENCH_SUMMON are considered summoned for this purpose, whether
+ * or not they are 'actual' abjurable summons or non-abjurable magical creations
+ * like battlespheres or hoarfrost cannons.
+
+ * @returns True if the monster is a temporary summon/creation, false otherwise.
  */
-bool monster::is_summoned(int* duration, int* summon_type) const
+bool monster::is_summoned() const
 {
-    // Not everything with a 'summon source' counts as a temporary summon, but
-    // we should at least return their source if one exists
-    const mon_enchant summ = get_ench(ENCH_SUMMON);
-    if (summ.ench == ENCH_NONE)
-    {
-        if (summon_type != nullptr)
-            *summon_type = 0;
-    }
-    else if (summon_type != nullptr)
-        *summon_type = summ.degree;
+    return has_ench(ENCH_SUMMON);
+}
 
-    const mon_enchant abj = get_ench(ENCH_ABJ);
-    if (abj.ench == ENCH_NONE)
-    {
-        if (duration != nullptr)
-            *duration = -1;
-
-        return false;
-    }
-    if (duration != nullptr)
-        *duration = abj.duration;
-
-    // Conjured things (fire vortices, ball lightning, IOOD) are not summoned.
-    if (mons_is_conjured(type))
+bool monster::is_summoned_by(int summon_type) const
+{
+    if (!is_summoned())
         return false;
 
-    // No summon type specified, but we otherwise seem to be temporary.
-    if (summ.ench == ENCH_NONE)
+    mon_enchant summ = get_ench(ENCH_SUMMON);
+    return summ.degree == summon_type;
+}
+
+bool monster::is_summoned_by(const actor& _summoner, int summon_type) const
+{
+    if (!is_summoned())
+        return false;
+
+    if (summoner != _summoner.mid)
+        return false;
+
+    // If we didn't specify a specific summon type to check for, any summon
+    // type will do.
+    if (summon_type == SPELL_NO_SPELL)
         return true;
 
-    // Certain spells or monster summon types that set abjuration but aren't
-    // considered summons.
-    switch (summ.degree)
-    {
-    // Temporarily dancing weapons are really there.
-    case SPELL_TUKIMAS_DANCE:
+    // Otherwise check that the creating spell/effect matches the query.
+    mon_enchant summ = get_ench(ENCH_SUMMON);
+    return summ.degree == summon_type;
+}
 
-    // Clones aren't really summoned (though their equipment might be).
-    case MON_SUMM_CLONE:
-
-    // Nor are body parts.
-    case SPELL_CREATE_TENTACLES:
-
-    // Some object which was animated, and thus not really summoned.
-    case MON_SUMM_ANIMATE:
-        return false;
-    }
-
-    return true;
+bool monster::is_abjurable() const
+{
+    return is_summoned() && testbits(flags, MF_ACTUAL_SUMMON);
 }
 
 bool monster::is_perm_summoned() const
@@ -2030,13 +1989,13 @@ static const char *enchant_names[] =
 #if TAG_MAJOR_VERSION == 34
     "rot",
 #endif
-    "summon", "abj", "corona",
+    "summon", "summon_timer", "corona",
     "charm", "sticky_flame", "glowing_shapeshifter", "shapeshifter", "tp",
     "sleep_wary",
 #if TAG_MAJOR_VERSION == 34
-    "submerged",
+    "submerged", "short_lived",
 #endif
-    "short_lived", "paralysis", "sick",
+    "paralysis", "sick",
 #if TAG_MAJOR_VERSION == 34
     "sleepy",
 #endif
@@ -2072,7 +2031,10 @@ static const char *enchant_names[] =
 #if TAG_MAJOR_VERSION == 34
     "withdrawn", "attached",
 #endif
-    "guardian_timer", "flight", "liquefying", "polar_vortex", "fake_abjuration",
+    "guardian_timer", "flight", "liquefying", "polar_vortex",
+#if TAG_MAJOR_VERSION == 34
+    "fake_abjuration",
+#endif
     "dazed", "mute", "blind", "dumb", "mad", "silver_corona", "recite timer",
     "inner_flame",
 #if TAG_MAJOR_VERSION == 34
@@ -2229,7 +2191,7 @@ void mon_enchant::cap_degree()
 
     // Hard cap to simulate old enum behaviour, we should really throw this
     // out entirely.
-    const int max = (ench == ENCH_ABJ || ench == ENCH_FAKE_ABJURATION) ?
+    const int max = (ench == ENCH_SUMMON_TIMER) ?
             MAX_ENCH_DEGREE_ABJURATION : MAX_ENCH_DEGREE_DEFAULT;
     if (degree > max)
         degree = max;
@@ -2351,9 +2313,6 @@ int mon_enchant::calc_duration(const monster* mons,
             cturn = 1000 * (deg - 1) / _mod_speed(200, mons->speed);
         cturn += 1000 / _mod_speed(100, mons->speed);
         break;
-    case ENCH_SHORT_LIVED:
-        cturn = 1200 / _mod_speed(200, mons->speed);
-        break;
     case ENCH_SLOWLY_DYING:
     {
         // This may be a little too direct but the randomization at the end
@@ -2377,8 +2336,7 @@ int mon_enchant::calc_duration(const monster* mons,
         cturn = 30 * 10 / _mod_speed(10, mons->speed);
         break;
 
-    case ENCH_FAKE_ABJURATION:
-    case ENCH_ABJ:
+    case ENCH_SUMMON_TIMER:
         // The duration is:
         // deg = 1     90 aut
         // deg = 2    180 aut
@@ -2467,4 +2425,19 @@ void mon_enchant::set_duration(const monster* mons, const mon_enchant *added)
 
     if (duration > maxduration)
         maxduration = duration;
+}
+
+// Converts a given summon duration category into a duration in aut (with random
+// fuzzing).
+int summ_dur(int degree)
+{
+    int aut = 0;
+
+    if (degree >= 6)
+        aut = 1000;
+    if (degree >= 5)
+        aut += 500;
+    aut += 100 * min(4, degree);
+
+    return fuzz_value(aut, 60, 40);
 }

crawl-ref/source/abyss.cc
@@ -1857,7 +1857,7 @@ static bool _spawn_corrupted_servant_near(const coord_def &pos)
         if (!monster_habitable_grid(mons, env.grid(p)))
             continue;
         mgen_data mg(mons, BEH_NEUTRAL, p);
-        mg.set_summoned(0, 5, 0).set_non_actor_summoner("Lugonu's corruption");
+        mg.set_summoned(0, 0, summ_dur(5)).set_non_actor_summoner("Lugonu's corruption");
         mg.place = BRANCH_ABYSS;
         return create_monster(mg);
     }
@@ -1882,7 +1882,7 @@ static void _spawn_corrupted_servant_near_monster(const monster &who)
         if (!monster_habitable_grid(mons, env.grid(p)))
             continue;
         mgen_data mg(mons, BEH_COPY, p);
-        mg.set_summoned(&who, 3, 0);
+        mg.set_summoned(&who, 0, summ_dur(3));
         mg.place = BRANCH_ABYSS;
         if (create_monster(mg))
             return;

crawl-ref/source/actor.h
@@ -46,8 +46,10 @@ public:
     virtual bool      alive() const = 0;
 
     // Should return false for perma-summoned things.
-    virtual bool is_summoned(int* duration = nullptr,
-                             int* summon_type = nullptr) const = 0;
+    virtual bool is_summoned() const = 0;
+    virtual bool is_summoned_by(int summon_type) const = 0;
+    virtual bool is_summoned_by(const actor& summoner,
+                                int summon_type = SPELL_NO_SPELL) const = 0;
 
     virtual bool is_perm_summoned() const = 0;
 

crawl-ref/source/arena.cc
@@ -1145,7 +1145,7 @@ bool arena_veto_place_monster(const mgen_data &mg, bool first_band_member,
     // If the first band member makes it past the summon throttle cut,
     // let all of the rest of its band in too regardless of the summon
     // throttle.
-    if (mg.abjuration_duration > 0 && first_band_member)
+    if (mg.summon_duration > 0 && first_band_member)
     {
         if (mg.behaviour == BEH_FRIENDLY
             && arena::faction_a.active_members > arena::summon_throttle)
@@ -1222,7 +1222,7 @@ void arena_placed_monster(monster* mons)
         // Real summons drop corpses and items.
         if (arena::real_summons)
         {
-            mons->del_ench(ENCH_ABJ, true, false);
+            mons->del_ench(ENCH_SUMMON_TIMER, true, false);
             for (mon_inv_iterator ii(*mons); ii; ++ii)
                 ii->flags &= ~ISFLAG_SUMMONED;
         }

crawl-ref/source/art-func.h
@@ -564,7 +564,7 @@ static void _DEMON_AXE_melee_effects(item_def* /*item*/, actor* attacker,
             create_monster(
                 mgen_data(summon_any_demon(RANDOM_DEMON_COMMON),
                           SAME_ATTITUDE(mons), mons->pos(), mons->foe)
-                .set_summoned(mons, 6, SPELL_SUMMON_DEMON));
+                .set_summoned(mons, SPELL_SUMMON_DEMON, summ_dur(6)));
         }
         else if (!you.allies_forbidden())
         {
@@ -572,7 +572,7 @@ static void _DEMON_AXE_melee_effects(item_def* /*item*/, actor* attacker,
                                                   : random_demon_by_tier(4));
             if (create_monster(
                     mgen_data(type, BEH_COPY, you.pos(), MHITYOU, MG_FORCE_BEH | MG_AUTOFOE)
-                    .set_summoned(&you, 6, SPELL_SUMMON_DEMON, GOD_NO_GOD)))
+                    .set_summoned(&you, SPELL_SUMMON_DEMON, summ_dur(6))))
             {
                 mpr("A gate to Pandemonium opens briefly!");
             }
@@ -1814,7 +1814,7 @@ static void _ASMODEUS_melee_effects(item_def* /*weapon*/, actor* attacker,
 
         mgen_data mg(demon, BEH_FRIENDLY, you.pos(), MHITYOU,
                      MG_FORCE_BEH | MG_AUTOFOE);
-        mg.set_summoned(&you, 4, SPELL_FIRE_SUMMON);
+        mg.set_summoned(&you, SPELL_FIRE_SUMMON, summ_dur(4));
 
         if (create_monster(mg))
         {

crawl-ref/source/attitude-change.cc
@@ -147,7 +147,7 @@ void dismiss_god_summons(god_type god)
     for (monster_iterator mi; mi; ++mi)
     {
         if (is_follower(**mi)
-            && mi->has_ench(ENCH_ABJ)
+            && mi->is_summoned()
             && mons_is_god_gift(**mi, god))
         {
             // The monster disappears.

crawl-ref/source/beam.cc
@@ -2704,7 +2704,7 @@ void bolt::affect_endpoint()
                                             SAME_ATTITUDE(agent(true)->as_monster()),
                                             spot, agent(true)->as_monster()->foe,
                                             MG_FORCE_PLACE)
-                            .set_summoned(agent(true), 1, SPELL_FLASHING_BALESTRA, GOD_NO_GOD));
+                            .set_summoned(agent(true), SPELL_FLASHING_BALESTRA, summ_dur(1)));
 
             if (blade)
                 blade->add_ench(ENCH_MIGHT);
@@ -2735,7 +2735,7 @@ void bolt::affect_endpoint()
             bool obviousness; // dummy argument
             monster *mirror = clone_mons(blitzer, true, &obviousness,
                                          blitzer->attitude, spot);
-            mirror->mark_summoned(2, true, SPELL_PHANTOM_BLITZ);
+            mirror->mark_summoned(SPELL_PHANTOM_BLITZ, 2);
             mirror->summoner = blitzer->mid;
             mirror->foe = blitzer->foe;
             mirror->hit_points = blitzer->max_hit_points;
@@ -3075,7 +3075,7 @@ void bolt::affect_place_explosion_clouds()
 
             actor* summ = agent();
             mgen_data mg(MONS_FIRE_VORTEX, att, p, MHITNOT, MG_NONE, god);
-            mg.set_summoned(summ, 1, SPELL_FIRE_STORM);
+            mg.set_summoned(summ, SPELL_FIRE_STORM, summ_dur(1), false, false);
 
             // Spell-summoned monsters need to have a live summoner.
             if (summ == nullptr || !summ->alive())
@@ -5846,9 +5846,7 @@ bool bolt::ignores_monster(const monster* mon) const
     if ((flavour == BEAM_HAEMOCLASM || flavour == BEAM_BLOODRITE) && mon->friendly())
         return true;
 
-    int summon_type = 0;
-    mon->is_summoned(nullptr, &summon_type);
-    if (flavour == BEAM_QAZLAL && summon_type == MON_SUMM_AID)
+    if (flavour == BEAM_QAZLAL && mon->is_summoned_by(you, MON_SUMM_AID))
         return true;
 
     if (origin_spell == SPELL_UPHEAVAL && agent() && agent() == mon)
@@ -6664,7 +6662,7 @@ mon_resist_type bolt::apply_enchantment_to_monster(monster* mon)
         if (!monster_can_be_unravelled(*mon))
             return MON_UNAFFECTED;
 
-        if (mon->is_summoned())
+        if (mon->is_abjurable())
         {
             mprf("The magic binding %s to this plane unravels!",
                  mon->name(DESC_THE).c_str());

crawl-ref/source/cloud.cc
@@ -617,8 +617,8 @@ static void _handle_spectral_cloud(const cloud_struct& cloud)
                              (agent ? agent->foe : short{MHITYOU}),
                              MG_FORCE_PLACE)
                     .set_base(basetype)
-                    .set_summoned(actor_by_mid(cloud.source), 1,
-                                  SPELL_SPECTRAL_CLOUD));
+                    .set_summoned(actor_by_mid(cloud.source),
+                                  SPELL_SPECTRAL_CLOUD, summ_dur(1)));
 }
 
 void manage_clouds()
@@ -1005,10 +1005,8 @@ bool actor_cloud_immune(const actor &act, const cloud_struct &cloud)
         return true;
     }
 
-    int summon_type = 0;
-    act.is_summoned(nullptr, &summon_type);
     if (!player && have_passive(passive_t::cloud_immunity)
-        && (act.as_monster()->friendly() && summon_type == MON_SUMM_AID))
+        && act.is_summoned_by(MON_SUMM_AID))
     {
         return true;
     }
@@ -1959,7 +1957,7 @@ static const vector<chaos_effect> chaos_effects = {
 
             // The player shouldn't get new permanent followers from cloning.
             if (clone->attitude == ATT_FRIENDLY && !clone->is_summoned())
-                clone->mark_summoned(6, true, MON_SUMM_CLONE);
+                clone->mark_summoned(MON_SUMM_CLONE, 6);
             else
                 clone->flags |= (MF_NO_REWARD | MF_HARD_RESET);
 

commit 30e30eea1891777bf5e383d296fd7631fb9257a3

Unbreak Vanquished Vanguard / Summon Mortal Champion summon cap (Ge0FF)
Broken since fb19e97205a5 which made ENCH_SUMMON get set even when
mark_summoned is called with a summon type of 0. Both of these spells call
mark_summoned() a second time, after the monster is created, to properly
mark their manually-created gear as summoned, but this was trashing the
record of what spell had created them, causing their summon cap to be
ignored.

crawl-ref/source/mon-ench.cc
@@ -1875,7 +1875,12 @@ void monster::mark_summoned(int summon_type, int longevity, bool mark_items,
     if (longevity > 0)
         add_ench(mon_enchant(ENCH_SUMMON_TIMER, 1, 0, longevity));
 
-    add_ench(mon_enchant(ENCH_SUMMON, summon_type, 0, INT_MAX));
+    // Fully replace any existing summon source, if we're giving a new one.
+    if (has_ench(ENCH_SUMMON) && summon_type != 0)
+        del_ench(ENCH_SUMMON);
+
+    if (!has_ench(ENCH_SUMMON))
+        add_ench(mon_enchant(ENCH_SUMMON, summon_type, 0, INT_MAX));
 
     if (mark_items)
         for (mon_inv_iterator ii(*this); ii; ++ii)