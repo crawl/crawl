1bdb9b73b775f4095a03e70a3670208321074c66
Implojin <implosionwow@gmail.com>
2024-11-20 11:30:05 -0600

    Shorten Forgecraft's artefact property string (gammafunk)
    Using the full name here was a bit unwieldly.

--- crawl-ref/source/artefact.cc
+++ crawl-ref/source/artefact.cc
@@ -876,7 +876,7 @@ static const artefact_prop_data artp_data[] =
         []() { return 1; }, nullptr, 0, 0 },
     { "Wiz", ARTP_VAL_BOOL, 0,   // ARTP_WIZARDRY,
         []() { return 1; }, nullptr, 0, 0 },
-    { "Forgecraft", ARTP_VAL_BOOL, 20, // ARTP_ENHANCE_FORGECRAFT,
+    { "Forge", ARTP_VAL_BOOL, 20, // ARTP_ENHANCE_FORGECRAFT,
         []() {return 1;}, nullptr, 0, 0},
 };
 COMPILE_CHECK(ARRAYSZ(artp_data) == ARTP_NUM_PROPERTIES);
