crawl/crawl

commit 1dcf16d5f2f0bb8b4fbd7f36cd11368bcbb95fe0

Refactor god_protects, shoot_through_monster, and related functions
A number of different types of ally are intended to either never be harmed
by player actions, or outright be fired through (eg: battlespheres, Hep
ancestors, demonic guardians). But many of these different catergories are
handled by different functions, and one category or another being
overlooked from some attack is a frequent source of bugs.

This refactors essentially all such cases to go through a pair of
functions: never_harm_monster() and shoot_through_monster(). The former is
for all monsters that should never be negatively affected by any actions
caused the a given agent (usually the player). shoot_through_monster() is
mostly the exact same list, except additionally includes Jiyva jellies
(where the player cannot harm them, but also cannot shoot through them.)

(I think some of this Jiyva behavior is a bit strange and incorrect, but
I've not attempt to change its current functionality in this commit.)

never_harm_monster() shoud be used in most cases, with
shoot_through_monster() specifically for projectiles.

This commit also fixes a handful of existing bugs, such as it being
possible to affect a demonic guardian with Irradiate or slow any
normally-unharmable ally with Metabolic Englaciate.

This also fixes #3985, and half of #4083

crawl-ref/source/beam.cc
@@ -1839,8 +1839,7 @@ static bool _monster_resists_mass_enchantment(monster* mons,
     // of "is unaffected" messages. --Eino
     if (mons->is_firewood())
         return true;
-    // Jiyva protects from mass enchantments.
-    if (have_passive(passive_t::neutral_slimes) && god_protects(*mons))
+    if (never_harm_monster(&you, mons))
         return true;
 
     switch (wh_enchant)
@@ -5014,7 +5013,7 @@ static void _add_chain_candidates(const bolt& beam, coord_def pos,
             continue;
 
         monster *mon = act->as_monster();
-        if (mon && (shoot_through_monster(beam, mon)
+        if (mon && (shoot_through_monster(beam.agent(), mon)
                     || mon->is_peripheral()))
         {
             continue;
@@ -5121,20 +5120,6 @@ void bolt::kill_monster(monster &mon)
     _print_pre_death_message(mon, goldify, origin_spell);
 
     int kindex = actor_to_death_source(agent());
-    // Prevent spore explosions killing plants from being registered
-    // as a Fedhas misconduct. Deaths can trigger the ally dying or
-    // plant dying conducts, but spore explosions shouldn't count
-    // for either of those.
-    //
-    // FIXME: Should be a better way of doing this. For now, we are
-    // just falsifying the death report... -cao
-    if (flavour == BEAM_SPORE && god_protects(mon) && fedhas_protects(mon))
-    {
-        if (mon.attitude == ATT_FRIENDLY)
-            mon.attitude = ATT_HOSTILE;
-        monster_die(mon, KILL_MON, kindex);
-        return;
-    }
 
     killer_type ref_killer = thrower;
     if (!YOU_KILL(thrower) && reflector == MID_PLAYER)
@@ -5510,7 +5495,7 @@ bool bolt::at_blocking_monster() const
     if (!pierce && !ignores_monster(mon) && mon->is_firewood())
         return true;
     if (have_passive(passive_t::neutral_slimes)
-        && god_protects(agent(), *mon, true)
+        && never_harm_monster(agent(), mon)
         && flavour != BEAM_VILE_CLUTCH)
     {
         return true;
@@ -5530,7 +5515,7 @@ void bolt::affect_monster(monster* mon)
     if (agent()
         && flavour != BEAM_VILE_CLUTCH
         && have_passive(passive_t::neutral_slimes)
-        && god_protects(agent(), *mon, true))
+        && mons_is_slime(*mon))
     {
         if (!is_tracer && you.see_cell(mon->pos()))
         {
@@ -5543,16 +5528,9 @@ void bolt::affect_monster(monster* mon)
         return;
     }
 
-    if (shoot_through_monster(*this, mon) && !is_tracer)
-    {
-        if (you.see_cell(mon->pos()))
-        {
-            if (testbits(mon->flags, MF_DEMONIC_GUARDIAN))
-                mpr("Your demonic guardian avoids your attack.");
-            else
-                god_protects(agent(), *mon, false); // messaging
-        }
-    }
+    // Print messages for monsters avoiding attacks.
+    if (!is_tracer)
+        shoot_through_monster(agent(), mon, true);
 
     if (flavour == BEAM_WATER && mon->type == MONS_WATER_ELEMENTAL && !is_tracer)
     {
@@ -5854,16 +5832,14 @@ bool bolt::ignores_monster(const monster* mon) const
         return true;
     }
 
-    // All kinds of beams go past orbs of destruction and friendly
-    // battlespheres.
-    if (always_shoot_through_monster(agent(), *mon))
+    // All kinds of beams go past orbs of destruction, the player can shoot
+    // through friendly battlespheres, their own ancestor, plants if they're
+    // worshipping Fedhas, etc.
+    if (shoot_through_monster(agent(), *mon))
         return true;
 
     // Missiles go past bushes and briar patches, unless aimed directly at them
     if (bush_immune(*mon))
-        return true;
-
-    if (shoot_through_monster(*this, mon))
         return true;
 
     // Fire storm creates these, so we'll avoid affecting them.
@@ -6062,11 +6038,9 @@ bool ench_flavour_affects_monster(actor *agent, beam_type flavour,
     }
         break;
 
-    // These are special allies whose loyalty can't be so easily bent
+    // Special allies whose loyalty can't be so easily bent
     case BEAM_CHARM:
-        rc = !(god_protects(*mon)
-               || testbits(mon->flags, MF_DEMONIC_GUARDIAN))
-             && !mons_aligned(agent, mon);
+        rc = !(never_harm_monster(agent, mon));
         break;
 
     case BEAM_MINDBURST:
@@ -7761,29 +7735,6 @@ int _ench_pow_to_dur(int pow)
     int base_dur = stepdown(pow * BASELINE_DELAY, 70);
 
     return div_rand_round(base_dur, 2) + random2(1 + base_dur);
-}
-
-// Do all beams skip past a particular monster?
-// see also shoot_through_monsters
-// can these be consolidated? Some checks there don't need a bolt arg
-bool always_shoot_through_monster(const actor *originator, const monster &victim)
-{
-    return mons_is_projectile(victim)
-        || (mons_is_avatar(victim.type)
-            && originator && mons_aligned(originator, &victim));
-}
-
-// Can a particular beam go through a particular monster?
-// Fedhas worshipers can shoot through non-hostile plants,
-// and players can shoot through their demonic guardians.
-bool shoot_through_monster(const bolt& beam, const monster* victim)
-{
-    actor *originator = beam.agent();
-    if (!victim || !originator)
-        return false;
-    return god_protects(originator, *victim)
-           || (originator->is_player()
-               && testbits(victim->flags, MF_DEMONIC_GUARDIAN));
 }
 
 /**

crawl-ref/source/ability.cc
@@ -2941,8 +2941,7 @@ static bool _abort_if_stationary()
 static bool _cleansing_flame_affects(const actor *act)
 {
     return act->res_holy_energy() < 3
-           && (!act->is_monster()
-               || !always_shoot_through_monster(&you, *act->as_monster()));
+           && !never_harm_monster(&you, act->as_monster());
 }
 
 static string _vampire_str_int_info_blurb(string stats_affected)

crawl-ref/source/actor.cc
@@ -21,6 +21,7 @@
 #include "message.h"
 #include "mon-behv.h"
 #include "mon-death.h"
+#include "mon-util.h"
 #include "religion.h"
 #include "spl-damage.h"
 #include "stepdown.h"
@@ -971,8 +972,8 @@ void actor::collide(coord_def newpos, const actor *agent, int damage)
 {
     actor *other = actor_at(newpos);
     // TODO: should the first of these check agent?
-    const bool god_prot = god_protects(agent, as_monster());
-    const bool god_prot_other = other && god_protects(agent, other->as_monster());
+    const bool immune = never_harm_monster(agent, as_monster());
+    const bool immune_other = never_harm_monster(agent, other->as_monster());
 
     ASSERT(this != other);
     ASSERT(alive());
@@ -983,7 +984,7 @@ void actor::collide(coord_def newpos, const actor *agent, int damage)
         return;
     }
 
-    if (is_monster() && !god_prot)
+    if (is_monster() && !immune)
         behaviour_event(as_monster(), ME_WHACK, agent);
 
     const int dam = apply_ac(damage);
@@ -998,26 +999,26 @@ void actor::collide(coord_def newpos, const actor *agent, int damage)
                  conj_verb("collide").c_str(),
                  other->name(DESC_THE).c_str(),
                  attack_strength_punctuation((dam + damother) / 2).c_str());
-            // OK, now do the messaging for god protection.
-            if (god_prot)
-                god_protects(agent, *as_monster(), false);
-            if (god_prot_other)
-                god_protects(agent, *other->as_monster(), false);
+            // OK, now do the messaging for protected monsters.
+            if (immune)
+                never_harm_monster(agent, *as_monster(), true);
+            if (immune_other)
+                never_harm_monster(agent, *other->as_monster(), true);
         }
 
-        if (other->is_monster() && !god_prot_other)
+        if (other->is_monster() && !immune_other)
             behaviour_event(other->as_monster(), ME_WHACK, agent);
 
         const string thisname = name(DESC_A, true);
         const string othername = other->name(DESC_A, true);
-        if (other->alive() && !god_prot_other)
+        if (other->alive() && !immune_other)
         {
             other->hurt(agent, damother, BEAM_MISSILE, KILLED_BY_COLLISION,
                         othername, thisname);
             if (damother && other->is_monster())
                 print_wounds(*other->as_monster());
         }
-        if (alive() && !god_prot)
+        if (alive() && !immune)
         {
             hurt(agent, dam, BEAM_MISSILE, KILLED_BY_COLLISION,
                  thisname, othername);
@@ -1046,11 +1047,11 @@ void actor::collide(coord_def newpos, const actor *agent, int damage)
                  attack_strength_punctuation(dam).c_str());
         }
 
-        if (god_prot)
-            god_protects(agent, *as_monster(), false); // messaging
+        if (immune)
+            never_harm_monster(agent, *as_monster(), true); // messaging
     }
 
-    if (!god_prot)
+    if (!immune)
     {
         hurt(agent, dam, BEAM_MISSILE, KILLED_BY_COLLISION,
              "", feature_description_at(newpos));

crawl-ref/source/beam.h
@@ -381,9 +381,6 @@ void bolt_parent_init(const bolt &parent, bolt &child);
 
 int explosion_noise(int rad);
 
-bool always_shoot_through_monster(const actor *agent, const monster &mon);
-bool shoot_through_monster(const bolt& beam, const monster* victim);
-
 int omnireflect_chance_denom(int SH);
 
 void glaciate_freeze(monster* mon, killer_type englaciator,

crawl-ref/source/cloud.cc
@@ -784,15 +784,6 @@ void place_cloud(cloud_type cl_type, const coord_def& ctarget, int cl_range,
 
     const monster * const mons = monster_at(ctarget);
 
-    // Fedhas et al protect their chosen ones from damaging clouds.
-    // XX demonic guardians? This logic mostly doesn't apply because protected
-    // monsters are also cloud immune, mostly
-    if (god_protects(agent, mons)
-        && !actor_cloud_immune(*mons, cl_type))
-    {
-        return;
-    }
-
     ASSERT(!cell_is_solid(ctarget));
 
     god_conduct_trigger conducts[3];
@@ -995,9 +986,7 @@ bool actor_cloud_immune(const actor &act, const cloud_struct &cloud)
 
     const bool player = act.is_player();
 
-    if (!player
-        && (god_protects(*act.as_monster())
-            || testbits(act.as_monster()->flags, MF_DEMONIC_GUARDIAN))
+    if (!player && never_harm_monster(&you, act.as_monster())
         && (cloud.whose == KC_YOU || cloud.whose == KC_FRIENDLY)
         && (act.as_monster()->friendly() || act.as_monster()->neutral())
         && (cloud.whose == KC_YOU || cloud.whose == KC_FRIENDLY))

crawl-ref/source/evoke.cc
@@ -706,8 +706,7 @@ static spret _phantom_mirror(dist *target)
 static bool _valid_tremorstone_target(const monster &m)
 {
     return !m.is_firewood()
-        && !god_protects(m)
-        && !always_shoot_through_monster(&you, m);
+        && !never_harm_monster(&you, m);
 }
 
 /**

crawl-ref/source/fight.cc
@@ -952,7 +952,7 @@ bool player_unrand_bad_attempt(const item_def &weapon,
         return stop_attack_prompt(hitfunc, "attack",
                                   [](const actor *act)
                                   {
-                                      return !god_protects(act->as_monster());
+                                      return !never_harm_monster(&you, act->as_monster());
                                   }, nullptr, defender->as_monster(),
                                   check_only);
     }
@@ -965,7 +965,7 @@ bool player_unrand_bad_attempt(const item_def &weapon,
         return stop_attack_prompt(hitfunc, "attack",
                                [](const actor *act)
                                {
-                                   return !god_protects(act->as_monster());
+                                   return !never_harm_monster(&you, act->as_monster());
                                }, nullptr, defender->as_monster(),
                                check_only);
     }
@@ -977,7 +977,7 @@ bool player_unrand_bad_attempt(const item_def &weapon,
                                [] (const actor *m)
                                {
                                    return !m->res_torment()
-                                       && !god_protects(m->as_monster());
+                                       && !never_harm_monster(&you, m->as_monster());
                                },
                                   nullptr, defender->as_monster(),
                                 check_only);
@@ -996,7 +996,7 @@ bool player_unrand_bad_attempt(const item_def &weapon,
         return stop_attack_prompt(hitfunc, "attack",
                                [](const actor *act)
                                {
-                                   return !god_protects(act->as_monster());
+                                   return !never_harm_monster(&you, act->as_monster());
                                }, nullptr, defender->as_monster(),
                                check_only);
     }
@@ -1018,7 +1018,7 @@ bool dont_harm(const actor &attacker, const actor &defender)
         return true;
 
     if (defender.is_monster())
-        return god_protects(&attacker, defender.as_monster(), false);
+        return never_harm_monster(&attacker, defender.as_monster(), true);
 
     if (defender.is_player())
         return attacker.wont_attack();
@@ -1278,7 +1278,7 @@ bool bad_attack(const monster *mon, string& adj, string& suffix,
             return false;
 
         // If we cannot hurt an ally anyway, don't bother warning as if we could
-        if (god_protects(mon))
+        if (never_harm_monster(&you, mon))
             return false;
 
         if (god_hates_attacking_friend(you.religion, *mon))
@@ -1499,11 +1499,8 @@ bool warn_about_bad_targets(const char* source_name, vector<coord_def> targets,
     for (coord_def p : targets)
     {
         const monster* mon = monster_at(p);
-        if (!mon || god_protects(&you, *mon)
-            || always_shoot_through_monster(&you, *mon))
-        {
+        if (!mon || never_harm_monster(&you, mon))
             continue;
-        }
 
         if (should_ignore && should_ignore(*mon))
             continue;

crawl-ref/source/fineff.cc
@@ -603,7 +603,7 @@ void explosion_fineff::fire()
             actor *act = actor_at(*ai);
             if (!act
                 || act->is_stationary()
-                || act->is_monster() && god_protects(*act->as_monster()))
+                || act->is_monster() && never_harm_monster(&you, *act->as_monster()))
             {
                 continue;
             }

commit 1c27a4ad6190d5b5b610cc47672d6194148ad8b8

Fix Charm instantly breaking (not_that_greg)
Accidentally broken by 1dcf16d5f2f0bb8b4fbd7f36cd11368bcbb95fe0

crawl-ref/source/beam.cc
@@ -6040,7 +6040,8 @@ bool ench_flavour_affects_monster(actor *agent, beam_type flavour,
 
     // Special allies whose loyalty can't be so easily bent
     case BEAM_CHARM:
-        rc = !(never_harm_monster(agent, mon));
+        rc = !never_harm_monster(agent, mon)
+             && !mons_aligned(agent, mon);
         break;
 
     case BEAM_MINDBURST: