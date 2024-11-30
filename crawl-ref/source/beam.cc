crawl/crawl

commit 33284afee520d1fe114d1a0560f5795f086bd044

Fix enemies sometimes getting incorrectly hit twice by allied beams
When allies fire a piercing beam at an enemy, and the player happens to be
standing in the path of that beam (and could be harmed by it), they will
cut their shot short immediately before it would hit the player. But this
is done by 'rewinding' the beam as soon as they test whether it can harm
the player, and code designed to allow piercing beams to hit monsters
standing at the player's location (ie: Fedhas plants) allows a beam to try
hitting a monster immediately after trying to affect the player. But the
beam has already rewound at that point, resulting in hitting the monster in
the space immediately *prior* a second time.

Now try a bit harder to actually end the beam processing when we mean to.

crawl-ref/source/beam.cc
@@ -1148,6 +1148,11 @@ void bolt::affect_cell()
             return;
         if (hit == AUTOMATIC_HIT && !pierce)
             finish_beam();
+        // XXX: If an ally stopped a piercing beam short to avoid hitting the
+        //      player on this cell, don't attempt to hit a monster on the
+        //      cell immediately before them again.
+        if (extra_range_used >= BEAM_STOP)
+            return;
     }
 
     // Stop single target beams from affecting a monster if they already

commit d50fa5e7cccdb40151f69b45c1ab9f0509cad7a2

Don't fail to place explosion clouds beneath the player (grumposus)
Broken by 33284afee520d1fe114d1a0560f5795f086bd044 .

(The method used there apparently gets tripped for all explosions, causing
them to skip the part where they leave clouds on the player's tile. This
should hopefully be a bit more foolproof, even if it's slightly ugly.)

crawl-ref/source/beam.cc
@@ -1144,6 +1144,7 @@ void bolt::affect_cell()
     if (hit_player && can_affect_actor(&you))
     {
         const int prev_reflections = reflections;
+        const coord_def old_pos = pos();
         affect_player();
         if (reflections != prev_reflections)
             return;
@@ -1152,7 +1153,7 @@ void bolt::affect_cell()
         // XXX: If an ally stopped a piercing beam short to avoid hitting the
         //      player on this cell, don't attempt to hit a monster on the
         //      cell immediately before them again.
-        if (extra_range_used >= BEAM_STOP)
+        if (pos() != old_pos)
             return;
     }
 