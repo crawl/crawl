crawl/crawl

commit 1f3234b322e4218ed56ee57fdd052a89523954fb

Fix Searing Ray charging an extra 1 MP on its first turn active
If you only had 2 MP, it would still work, but would eat up 3 MP if you had
it available.

If you were a djinn at 3 HP, though, it would just crash.

crawl-ref/source/spl-damage.cc
@@ -3892,7 +3892,7 @@ bool handle_searing_ray(actor& agent, int turn)
     beam.fire();
     trigger_battlesphere(&agent);
 
-    if (agent.is_player())
+    if (agent.is_player() && turn > 1)
     {
         pay_mp(1);
         finalize_mp_cost();

commit f8f82a2dde9e028c2c93dc1f3d749131d0568028

Fix Searing Ray crashing when cast
Broken by 1f3234b. I should have paid more attention to the 'else' below
that...

Fixes #4162

crawl-ref/source/spl-damage.cc
@@ -3892,10 +3892,13 @@ bool handle_searing_ray(actor& agent, int turn)
     beam.fire();
     trigger_battlesphere(&agent);
 
-    if (agent.is_player() && turn > 1)
+    if (agent.is_player())
     {
-        pay_mp(1);
-        finalize_mp_cost();
+        if (turn > 1)
+        {
+            pay_mp(1);
+            finalize_mp_cost();
+        }
 
         if (turn > 3)
         {