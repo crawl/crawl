crawl/crawl

commit 008097b4cb438ba1d480b8f6a88f94df0e265dea

Squash all ID flags into one, refactor heavily
Crawl's identification game has grown more and more simple over the years,
but much of the *backend* for its old complexity remained in the code:
there were flags for individually identifying an item's type, plusses, and
even each individual artprop of every artefact. At one point in time, these
all served a purpose, but most of them are redundant or unnecessary now.

This replaces all existing ID flags with a single one, ISFLAG_IDENTIFIED,
and refactors a ton of code to be simpler and more clear; items are now
either fully identified or unidentified.

Multiple completely unnused functions have been removed, a few more have
been renamed for clarity, and identification checks for situations where
un-ID'd items should be impossible (ie: checking the ID status of equipment
the player is currently wearing) are also removed. Old uses of
set_ident_flags() and set_ident_type() are both folded into identify_item()
as appropriate.

get_item_known_info() is removed entirely. With item identification being
binary and all monster equipment being pre-identified, this felt like
overkill for most current uses. Instead, we just hide unknown properties of
items in player-facing description, rather than making a copy of every item
with all unknown properties removed (in fact, most uses of this function
effectively did so already.) ID checks were added to a few clue item
functions, but the only place that required any special handling (as far as
I noticed, at least) was calculating attack delay with an unidentified
weapon with a speed or heavy brand, and that can be handled more simply.

The stanard way to check whether an item is identified is now
item_def::is_identified(), which checks not only the ISFLAG_IDENTIFIED on
the item itself, but whether the item *type* is known. (A less obvious
effect of get_item_known_info was that the item copy could be *more*
identified than the original in many cases - in fact, this was seemingly

crawl-ref/source/tileweb.cc
@@ -1230,7 +1230,7 @@ void TilesFramework::_send_player(bool force_full)
     for (unsigned int i = 0; i < ENDOFPACK; ++i)
     {
         json_open_object(to_string(i));
-        item_def item = get_item_known_info(you.inv[i]);
+        item_def item = you.inv[i];
         if (((char)i == you.equip[EQ_WEAPON] && is_weapon(item)
              || (char)i == you.equip[EQ_OFFHAND] && you.offhand_weapon())
             && you.corrosion_amount())
@@ -1283,7 +1283,7 @@ static int _useful_consumable_order(const item_def &item, const string &name)
 
     if (item.quantity < 1
         || order == base_types.end() // covers the empty case
-        || (!Options.action_panel_show_unidentified && !fully_identified(item)))
+        || (!Options.action_panel_show_unidentified && !item.is_identified()))
     {
         return -1;
     }
@@ -1697,18 +1697,10 @@ void TilesFramework::_send_cell(const coord_def &gc,
                     minfo.props[MONSTER_TILE_KEY] =
                         int(last_player_doll.parts[TILEP_PART_BASE]);
                     item_def *item;
-                    if (you.slot_item(EQ_WEAPON))
-                    {
-                        item = new item_def(
-                            get_item_known_info(*you.slot_item(EQ_WEAPON)));
+                    if (item = you.slot_item(EQ_WEAPON))
                         minfo.inv[MSLOT_WEAPON].reset(item);
-                    }
-                    if (you.slot_item(EQ_OFFHAND))
-                    {
-                        item = new item_def(
-                            get_item_known_info(*you.slot_item(EQ_OFFHAND)));
+                    if (item = you.slot_item(EQ_OFFHAND))
                         minfo.inv[MSLOT_SHIELD].reset(item);
-                    }
                     tileidx_t mcache_idx = mcache.register_monster(minfo);
                     mcache_entry *entry = mcache.get(mcache_idx);
                     if (entry)

commit 577656edd5ca13fc7996b112417a9d79dadeba95

Fix a webtiles crash with monster player tiles (gammafunk)
008097b accidentally gave the monster_info the player's actual weapon and
shield instead of a copy of them, leading to deleting a pointer that was
still in use.

crawl-ref/source/tileweb.cc
@@ -1698,9 +1698,15 @@ void TilesFramework::_send_cell(const coord_def &gc,
                         int(last_player_doll.parts[TILEP_PART_BASE]);
                     item_def *item;
                     if (item = you.slot_item(EQ_WEAPON))
+                    {
+                        item = new item_def(*you.slot_item(EQ_WEAPON));
                         minfo.inv[MSLOT_WEAPON].reset(item);
+                    }
                     if (item = you.slot_item(EQ_OFFHAND))
+                    {
+                        item = new item_def(*you.slot_item(EQ_OFFHAND));
                         minfo.inv[MSLOT_SHIELD].reset(item);
+                    }
                     tileidx_t mcache_idx = mcache.register_monster(minfo);
                     mcache_entry *entry = mcache.get(mcache_idx);
                     if (entry)