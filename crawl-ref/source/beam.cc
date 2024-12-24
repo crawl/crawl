crawl/crawl

commit afcf47756822c0054e31f1b0fdfddaf4394db83a

Refactor how tile-picking for beams works
Tile selection for beams used to be handled by tileidx_bolt(), which was
organized in an... archaic manner. Tile selection was done via a
combination of beam color and string-matching against the name of the
projectile being fired. This was clunky and also error-prone in several
ways: projectile text could be edited elsewhere (which would silently cause
their custom tiles to disappear), tile overrides for removed spells could
remain long after the spell was gone (since it's not obvious which string
corresponds to what spell). In fact, both of those things were true for at
least one or two spells immediately prior to this commit!

This removes that function entirely and instead allows any custom tiles to
be specified as a simple member of zap_info (or manually-specified via
beam.tile_beam and beam.tile_explosion, in cases where zaps are not used.)
If neither is specified, it will still fall back on the default
color-to-tile mapping done by tileidx_zap().

This also allows the tile for the path of a beam, and the explosion at the
*end* of that beam, to use different tiles easily. This has been applied to
iceblast (and related spells) and fireball at present, but could easily be
extended to move things. (I think it is a cleaner visual, in general, if
the non-damaging trail and damaging explosion of an effect use different
tiles, though not all effects have such obviously useful tiles
pre-existing.)

Tile directional orientation is now also handled automatically by
vary_bolt_tile() and I renamed the tile enums for the 'default' color-based
bolts to be clearer.

Aside from the aforementioned animation tweaks to iceblast/fireball, there
should be no other functional change.

crawl-ref/source/beam.cc
@@ -410,6 +410,8 @@ struct zap_info
     dungeon_char_type glyph;
     bool can_beam;
     bool is_explosion;
+    tileidx_t tile_beam;
+    tileidx_t tile_explode;
 };
 
 #include "zap-data.h"
@@ -587,6 +589,9 @@ void zappy(zap_type z_type, int power, bool is_monster, bolt &pbolt)
 
     if (pbolt.loudness == 0)
         pbolt.loudness = _zap_loudness(z_type, pbolt.origin_spell);
+
+    pbolt.tile_beam = zinfo->tile_beam;
+    pbolt.tile_explode = zinfo->tile_explode;
 }
 
 bool bolt::can_affect_actor(const actor *act) const
@@ -860,14 +865,10 @@ void bolt::draw(const coord_def& p, bool force_refresh)
         return;
 
 #ifdef USE_TILE
-    if (tile_beam == -1)
-        tile_beam = tileidx_bolt(*this);
-
-    if (tile_beam != -1)
-    {
-        int dist = (p - source).rdist();
-        view_add_tile_overlay(p, vary_bolt_tile(tile_beam, dist));
-    }
+    // Set default value if none specified.
+    if (tile_beam == 0)
+        tile_beam = tileidx_zap(colour);
+    view_add_tile_overlay(p, vary_bolt_tile(tile_beam, source, target));
 #endif
     const unsigned short c = colour == BLACK ? random_colour(true)
                                              : element_colour(colour);
@@ -1248,14 +1249,15 @@ void bolt::do_fire()
     cursor_control coff(false);
 
 #ifdef USE_TILE
-    tile_beam = -1;
-
-    if (item && !is_tracer && (flavour == BEAM_MISSILE
-                               || flavour == BEAM_VISUAL))
+    // Set up uninitialized / item-based beam tile, if we're actually firing.
+    if (!is_tracer)
     {
-        const coord_def diff = target - source;
-        tile_beam = tileidx_item_throw(
-                                get_item_known_info(*item), diff.x, diff.y);
+        if (item && (flavour == BEAM_MISSILE || flavour == BEAM_VISUAL))
+        {
+            const coord_def diff = target - source;
+            tile_beam = tileidx_item_throw(
+                                    get_item_known_info(*item), diff.x, diff.y);
+        }
     }
 #endif
 
@@ -2297,6 +2299,8 @@ void bolt_parent_init(const bolt &parent, bolt &child)
     child.origin_spell   = parent.origin_spell;
     child.glyph          = parent.glyph;
     child.colour         = parent.colour;
+    child.tile_beam      = parent.tile_beam;
+    child.tile_explode   = parent.tile_explode;
 
     child.flavour        = parent.flavour;
 
@@ -7186,9 +7190,15 @@ bool bolt::explosion_draw_cell(const coord_def& p)
         if (in_los_bounds_v(drawpos))
         {
 #ifdef USE_TILE
-            int dist = (p - source).rdist();
-            tileidx_t tile = tileidx_bolt(*this);
-            view_add_tile_overlay(p, vary_bolt_tile(tile, dist));
+            // Use default value if none specified.
+            if (tile_explode == 0)
+            {
+                if (tile_beam != 0)
+                    tile_explode = tile_beam;
+                else
+                    tile_explode = tileidx_zap(colour);
+            }
+            view_add_tile_overlay(p, vary_bolt_tile(tile_explode, source, target));
 #endif
             const unsigned short c = colour == BLACK ?
                     random_colour(true) : element_colour(colour, false, p);

crawl-ref/source/tilepick.cc
@@ -3439,168 +3439,45 @@ tileidx_t tileidx_cloud(const cloud_info &cl)
 }
 
 #ifdef USE_TILE
-tileidx_t tileidx_bolt(const bolt &bolt)
+tileidx_t vary_bolt_tile(tileidx_t tile, const coord_def& origin, const coord_def& target)
 {
-    const int col = bolt.colour;
-    const coord_def diff = bolt.target - bolt.source;
+    const coord_def diff = target - origin;
     const int dir = _tile_bolt_dir(diff.x, diff.y);
+    const int dist = diff.rdist();
 
-    switch (col)
-    {
-    case WHITE:
-        if (bolt.origin_spell == SPELL_ICEBLAST ||
-            bolt.origin_spell == SPELL_SERACFALL ||
-            bolt.origin_spell == SPELL_GLACIATE)
-        {
-            return TILE_BOLT_ICEBLAST;
-        }
-        else if (bolt.name == "crystal spear")
-            return TILE_BOLT_CRYSTAL_SPEAR + dir;
-        else if (bolt.name == "puff of frost")
-            return TILE_BOLT_FROST;
-        else if (bolt.name == "shard of ice"
-                 || bolt.name == "shard of alchemical ice"
-                 || bolt.name == "salvo of alchemical ice"
-                    && !bolt.in_explosion_phase)
-        {
-            return TILE_BOLT_ICICLE + dir;
-        }
-        else if (bolt.name == "salvo of icicles")
-            return TILE_BOLT_ICICLE_SALVO + dir;
-        else if (bolt.name == "searing ray")
-            return TILE_BOLT_SEARING_RAY;
-        else if (bolt.name == "bolt of light"
-                 || bolt.name == "blinding ray")
-        {
-            return TILE_BOLT_LIGHT + dir;
-        }
-        else if (bolt.name == "fortress blast")
-            return TILE_BOLT_FORTRESS_BLAST;
-        break;
-
-    case LIGHTCYAN:
-        if (bolt.name == "iron shot")
-            return TILE_BOLT_IRON_SHOT + dir;
-        else if (bolt.name == "crystallizing shot")
-            return TILE_BOLT_CRYSTAL_SPEAR + dir;
-        else if (bolt.name == "zap")
-            return TILE_BOLT_ZAP + dir % tile_main_count(TILE_BOLT_ZAP);
-        break;
-
-    case RED:
-        if (bolt.name == "puff of flame")
-            return TILE_BOLT_FLAME;
-        else if (bolt.origin_spell == SPELL_SPIT_LAVA ||
-                 bolt.origin_spell == SPELL_BOLT_OF_MAGMA)
-        {
-            return TILE_BOLT_MAGMA;
-        }
-        else if (bolt.origin_spell == SPELL_FIRE_STORM)
-            return TILE_BOLT_FIRE_STORM;
-        else if (bolt.name == "rain of gore")
-            return TILE_BOLT_HAEMOCLASM;
-        break;
-
-    case LIGHTRED:
-        if (bolt.name.find("damnation") != string::npos)
-            return TILE_BOLT_DAMNATION;
-        if (bolt.name == "blood arrow")
-            return TILE_BOLT_BLOOD_ARROW + dir;
-        break;
-
-    case LIGHTMAGENTA:
-        if (bolt.name == "magic dart")
-            return TILE_BOLT_MAGIC_DART;
-        else if (bolt.origin_spell == SPELL_WARP_SPACE)
-            return TILE_BOLT_WARP_SPACE;
-        break;
-
-    case BROWN:
-        if (bolt.name == "blast of sand")
-            return TILE_BOLT_SANDBLAST;
-        else if (bolt.name == "volley of thorns" ||
-                 bolt.name == "spray of wooden splinters")
-            return TILE_BOLT_SPLINTERSPRAY + dir;
-        else if (bolt.origin_spell == SPELL_THROW_BOLAS)
-            return TILE_BOLT_BOLAS;
-        else if (bolt.name == "klown pie")
-            return TILE_BOLT_PIE + dir;
-        break;
-
-    case GREEN:
-        if (bolt.name == "sting")
-            return TILE_BOLT_STING;
-        break;
-
-    case LIGHTGREEN:
-        if (bolt.name == "poison arrow")
-            return TILE_BOLT_POISON_ARROW + dir;
-        break;
-
-    case LIGHTGREY:
-        if (bolt.name == "stone arrow" || bolt.name == "stone bullet")
-            return TILE_BOLT_STONE_ARROW + dir;
-        break;
-
-    case DARKGREY:
-        if (bolt.name == "bolt of negative energy")
-            return TILE_BOLT_DRAIN;
-        break;
-
-    case MAGENTA:
-        if (bolt.origin_spell == SPELL_SHADOW_BEAM)
-            return TILE_BOLT_SHADOW_BEAM + dir % tile_main_count(TILE_BOLT_SHADOW_BEAM);
-        if (bolt.origin_spell == SPELL_SHADOW_BALL
-            || bolt.origin_spell == SPELL_CREEPING_SHADOW
-            || bolt.origin_spell == SPELL_SHADOW_TEMPEST
-            || bolt.origin_spell == SPELL_SHADOW_PRISM)
-        {
-            return TILE_BOLT_SHADOW_BLAST;
-        }
-        if (bolt.origin_spell == SPELL_SHADOW_SHARD)
-            return TILE_BOLT_SHADOW_SHARD;
-        if (bolt.origin_spell == SPELL_SHADOW_SHOT)
-            return TILE_BOLT_SHADOW_SHOT + dir;
-        break;
-
-    case CYAN:
-        if (bolt.name == "slug dart")
-            return TILE_BOLT_STONE_ARROW + dir;
-        else if (bolt.name == "lance of force")
-            return TILE_BOLT_FORCE_LANCE + dir;
-        else if (bolt.name == "harpoon shot")
-            return TILE_BOLT_HARPOON_SHOT + dir;
-        else if (bolt.name == "spray of metal splinters")
-            return TILE_BOLT_METAL_SPLINTERS + dir;
-        else if (bolt.name == "ghostly fireball")
-            return TILE_BOLT_GHOSTLY_FIREBALL;
-        else if (bolt.name == "umbral torchlight")
-            return TILE_BOLT_UMBRAL_TORCHLIGHT;
-        else if (bolt.name == "phantom echo")
-            return TILE_BOLT_PHANTOM_BLITZ;
-        break;
-
-    case ETC_MUTAGENIC:
-        if (bolt.name == "irradiate" || bolt.name == "unravelling"
-            || bolt.name == "burst of quintessence")
-        {
-            return TILE_BOLT_IRRADIATE;
-        }
-        break;
-    }
-
-    return tileidx_zap(col);
+    return vary_bolt_tile(tile, dir, dist);
 }
 
-tileidx_t vary_bolt_tile(tileidx_t tile, int dist)
+tileidx_t vary_bolt_tile(tileidx_t tile, int dir, int dist)
 {
     switch (tile)
     {
+    case TILE_BOLT_STONE_ARROW:
+    case TILE_BOLT_CRYSTAL_SPEAR:
+    case TILE_BOLT_ICICLE:
+    case TILE_BOLT_ICICLE_SALVO:
+    case TILE_BOLT_LIGHT:
+    case TILE_BOLT_IRON_SHOT:
+    case TILE_BOLT_BLOOD_ARROW:
+    case TILE_BOLT_SPLINTERSPRAY:
+    case TILE_BOLT_PIE:
+    case TILE_BOLT_POISON_ARROW:
+    case TILE_BOLT_SHADOW_SHOT:
+    case TILE_BOLT_FORCE_LANCE:
+    case TILE_BOLT_HARPOON_SHOT:
+    case TILE_BOLT_METAL_SPLINTERS:
+        return tile + dir;
+
+    case TILE_BOLT_ZAP:
+    case TILE_BOLT_SHADOW_BEAM:
+        return tile + dir % tile_main_count(tile);
+
     case TILE_BOLT_FROST:
     case TILE_BOLT_MAGIC_DART:
     case TILE_BOLT_SANDBLAST:
     case TILE_BOLT_STING:
         return tile + (dist - 1) % tile_main_count(tile);
+
     case TILE_BOLT_FLAME:
     case TILE_BOLT_MAGMA:
     case TILE_BOLT_ICEBLAST:
@@ -3612,8 +3489,10 @@ tileidx_t vary_bolt_tile(tileidx_t tile, int dist)
     case TILE_BOLT_MANIFOLD_ASSAULT:
     case TILE_BOLT_PARAGON_TEMPEST:
         return tile + ui_random(tile_main_count(tile));
+
     case TILE_MI_BOOMERANG0:
         return tile + ui_random(4);
+
     default:
         return tile;
     }

crawl-ref/source/tilepick.h
@@ -49,7 +49,9 @@ tileidx_t tileidx_known_base_item(tileidx_t label);
 
 tileidx_t tileidx_cloud(const cloud_info &cl);
 tileidx_t tileidx_bolt(const bolt &bolt);
-tileidx_t vary_bolt_tile(tileidx_t tile, int dist);
+tileidx_t vary_bolt_tile(tileidx_t tile, const coord_def& origin,
+                                         const coord_def& target);
+tileidx_t vary_bolt_tile(tileidx_t tile, int dir = 0, int dist = 0);
 tileidx_t tileidx_zap(int colour);
 tileidx_t tileidx_spell(const spell_type spell);
 tileidx_t tileidx_skill(const skill_type skill, int train);

crawl-ref/source/art-func.h
@@ -54,6 +54,7 @@
 #include "spl-transloc.h"  // For Autumn Katana's Manifold Assault
 #include "tag-version.h"
 #include "terrain.h"       // For storm bow
+#include "rltiles/tiledef-main.h"
 #include "unwind.h"        // For autumn katana
 #include "view.h"          // For arc blade's discharge effect
 
@@ -904,6 +905,7 @@ static void _DAMNATION_launch(bolt* beam)
     expl->is_explosion = true;
     expl->damage = dice_def(2, 14);
     expl->name   = "damnation";
+    expl->tile_explode = TILE_BOLT_DAMNATION;
 
     beam->special_explosion = expl;
 }

crawl-ref/source/beam.h
@@ -171,7 +171,9 @@ struct bolt
 
     ray_def     ray;             // shoot on this specific ray
 
-    int         tile_beam; // only used if USE_TILE is defined
+    // only used if USE_TILE is defined
+    tileidx_t tile_beam = 0;
+    tileidx_t tile_explode = 0;
 
 private:
     bool can_see_invis = false;

crawl-ref/source/god-abil.cc
@@ -3959,9 +3959,6 @@ spret qazlal_upheaval(coord_def target, bool quiet, bool fail, dist *player_targ
     beam.hit         = AUTOMATIC_HIT;
     beam.glyph       = dchar_glyph(DCHAR_EXPLOSION);
     beam.loudness    = 10;
-#ifdef USE_TILE
-    beam.tile_beam = -1;
-#endif
 
     if (target.origin())
     {

crawl-ref/source/mon-cast.cc
@@ -6236,9 +6236,6 @@ static void _mons_upheaval(monster& mons, actor& /*foe*/, bool randomize)
     beam.hit         = AUTOMATIC_HIT;
     beam.glyph       = dchar_glyph(DCHAR_EXPLOSION);
     beam.loudness    = 10;
-#ifdef USE_TILE
-    beam.tile_beam   = -1;
-#endif
     beam.draw_delay  = 0;
     beam.target = mons.target;
     string message = "";

crawl-ref/source/mon-explode.cc
@@ -27,6 +27,7 @@
 #include "stringutil.h"
 #include "target.h"
 #include "terrain.h"
+#include "rltiles/tiledef-main.h"
 #include "torment-source-type.h"
 #include "view.h"
 #include "viewchar.h"
@@ -170,6 +171,7 @@ static void _setup_shadow_prism_explosion(bolt& beam, const monster& origin)
     beam.damage  = prism_damage(origin.get_hit_dice(), origin.prism_charge == 2);
     beam.name    = "blast of shadow";
     beam.colour  = MAGENTA;
+    beam.tile_explode = TILE_BOLT_SHADOW_BLAST;
     beam.ex_size = origin.prism_charge;
     beam.origin_spell = SPELL_SHADOW_PRISM;
 }
@@ -218,6 +220,7 @@ static void _setup_haemoclasm_explosion(bolt& beam, const monster& origin)
     beam.name        = "rain of gore";
     beam.hit_verb    = "batters";
     beam.colour      = RED;
+    beam.tile_explode= TILE_BOLT_HAEMOCLASM;
     beam.ex_size     = 1;
     beam.source_name = origin.name(DESC_PLAIN, true);
     beam.thrower     = KILL_YOU_MISSILE;

commit a33de6ed3933e7a47fa782fcc317e33cd109b62d

Fix beam tiles for beams whose tile varies with distance travelled (r-i)
Accidentally broken by afcf47756822c0054e31f1b0fdfddaf4394db83a

crawl-ref/source/beam.cc
@@ -868,7 +868,7 @@ void bolt::draw(const coord_def& p, bool force_refresh)
     // Set default value if none specified.
     if (tile_beam == 0)
         tile_beam = tileidx_zap(colour);
-    view_add_tile_overlay(p, vary_bolt_tile(tile_beam, source, target));
+    view_add_tile_overlay(p, vary_bolt_tile(tile_beam, source, target, p));
 #endif
     const unsigned short c = colour == BLACK ? random_colour(true)
                                              : element_colour(colour);
@@ -7231,7 +7231,7 @@ bool bolt::explosion_draw_cell(const coord_def& p)
                 else
                     tile_explode = tileidx_zap(colour);
             }
-            view_add_tile_overlay(p, vary_bolt_tile(tile_explode, source, target));
+            view_add_tile_overlay(p, vary_bolt_tile(tile_explode, source, target, p));
 #endif
             const unsigned short c = colour == BLACK ?
                     random_colour(true) : element_colour(colour, false, p);

crawl-ref/source/tilepick.cc
@@ -3448,11 +3448,12 @@ tileidx_t tileidx_cloud(const cloud_info &cl)
 }
 
 #ifdef USE_TILE
-tileidx_t vary_bolt_tile(tileidx_t tile, const coord_def& origin, const coord_def& target)
+tileidx_t vary_bolt_tile(tileidx_t tile, const coord_def& origin,
+                         const coord_def& target, const coord_def& pos)
 {
     const coord_def diff = target - origin;
     const int dir = _tile_bolt_dir(diff.x, diff.y);
-    const int dist = diff.rdist();
+    const int dist = (pos - origin).rdist();
 
     return vary_bolt_tile(tile, dir, dist);
 }

crawl-ref/source/tilepick.h
@@ -50,7 +50,7 @@ tileidx_t tileidx_known_base_item(tileidx_t label);
 tileidx_t tileidx_cloud(const cloud_info &cl);
 tileidx_t tileidx_bolt(const bolt &bolt);
 tileidx_t vary_bolt_tile(tileidx_t tile, const coord_def& origin,
-                                         const coord_def& target);
+                         const coord_def& target, const coord_def& pos);
 tileidx_t vary_bolt_tile(tileidx_t tile, int dir = 0, int dist = 0);
 tileidx_t tileidx_zap(int colour);
 tileidx_t tileidx_spell(const spell_type spell);