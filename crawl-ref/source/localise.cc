46aee8db83ecbd2be8ab4755441c17fbc87af0e2
Brian Power <powerbf.it@gmail.com>
2024-12-30 00:48:07 +1000

    i18n: Clean up isalpha/isdigit usage

--- crawl-ref/source/localise.cc
+++ crawl-ref/source/localise.cc
@@ -37,7 +37,25 @@ static string _localise_list(const string context, const string& value);
 static string _localise_player_species_job(const string& s);
 static string _localise_player_title(const string& context, const string& text);
 
-// returns true only for 0-9 (unlike isdigit() which is affected by locale)
+// returns true only for a-z (not affected by locale like std::islower())
+static inline bool _is_ascii_lower(char c)
+{
+    return c >= 'a' && c <= 'z';
+}
+
+// returns true only for A-Z (not affected by locale like std::isupper())
+static inline bool _is_ascii_upper(char c)
+{
+    return c >= 'A' && c <= 'Z';
+}
+
+// returns true only for a-z and A-Z (not affected by locale like std::isalpha())
+static inline bool _is_ascii_alpha(char c)
+{
+    return _is_ascii_lower(c) || _is_ascii_upper(c);
+}
+
+// returns true only for 0-9 (std::isdigit() has a bunch of annoying caveats)
 static inline bool _is_ascii_digit(char c)
 {
     return c >= '0' && c <= '9';
@@ -311,7 +329,7 @@ static bool _starts_with_menu_id(const string& s)
     if (s.length() < 4)
         return false;
 
-    return isalpha(s[0]) && s.substr(1,3) == " - ";
+    return _is_ascii_alpha(s[0]) && s.substr(1,3) == " - ";
 }
 
 static string _strip_menu_id(const string& s, string& id)
@@ -355,7 +373,6 @@ static bool _starts_with_count(const string& s)
         char ch = s[i];
         if (ch == ' ' && i > 0)
             return true;
-        // note: isdigit() can be affected by locale
         else if (!_is_ascii_digit(ch))
             return false;
     }
@@ -537,7 +554,7 @@ static string _localise_annotation_element(const string& s)
     if (!prefix.empty() || !suffix.empty())
         return prefix + _localise_annotation_element(rest) + suffix;
 
-    if (isdigit(s[0]))
+    if (_is_ascii_digit(s[0]))
     {
         result =  _localise_counted_string("", s);
         return result.empty() ? s : result;
@@ -557,7 +574,7 @@ static string _localise_annotation_element(const string& s)
         int last_alpha_pos = -1;
         for (int k = (int)tok.length() - 1; k >= 0; k--)
         {
-            if (isalpha(tok[k]))
+            if (_is_ascii_alpha(tok[k]))
             {
                 last_alpha_pos = k;
                 break;
@@ -908,7 +925,7 @@ static bool _check_adjectives(const vector<string>& adjs)
             return false;
         else if (adj == "shaped")
             contains_shaped = true;
-        else if (adj[0] >= 'A' && adj[0] <= 'Z' && adj != "Lernaean")
+        else if (_is_ascii_upper(adj[0]) && adj != "Lernaean")
             contains_name = true;
 
     if (contains_name && !contains_shaped)
@@ -1094,7 +1111,7 @@ static string _localise_unidentified_scroll(const string& context, const string&
     rest += "%s";
 
     string result;
-    if (isdigit(rest[0]))
+    if (_is_ascii_digit(rest[0]))
     {
         int count;
         string plural = _strip_count(rest, count);
@@ -2754,7 +2771,7 @@ string localise(const string& text_in, const map<string, string>& params_in, boo
                         nested_calls--;
                     }
                 }
-                if (!key.empty() && isupper(key[0]))
+                if (!key.empty() && _is_ascii_upper(key[0]))
                     value = uppercase_first(value);
                 res << value;
             }
@@ -2863,7 +2880,7 @@ static string _localise_player_title(const string& context, const string& text)
         rest = text;
 
     // all titles start with a capital letter
-    if (rest.empty() || rest[0] < 'A' || rest[0] > 'Z')
+    if (rest.empty() || !_is_ascii_upper(rest[0]))
         return "";
 
     TRACE("context='%s', value='%s'", context.c_str(), text.c_str());
