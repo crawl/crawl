crawl/crawl

commit 9283c139d45942595a4af26bba009607fd00a784

Reroll items that conflict with any fixed artprops
The artprops keyword in an item_spec induces an item to be a random
artefact, but with the specified artefact properties always present. A
problem arises when the base type or sub type of the item is randomized,
since specific base/sub types can conflict with the specified
properties. For example, "any ring artprops:rF:1" will conflict with the
intrinsic rC- of a ring of ice, causing the rF+ artprop to be ignored
when that item type is rolled. For vaults that are trying to define
thematic items, having some randomly generated items ignore the
specified properties won't work. The alternative is to specify an
explicit list of acceptable subtypes in the vault, but this is onerous
and more difficult to maintain.

This commit implements rerolled random types whenever there's a conflict
between the specific fixed artprops and the item's intrinsic properties.
This rerolling is limited to 1000 attempts to avoid an infinite loop, a
scheme already in use before this commit for weapons and jewellery. We
just extend it to staves and talismans and trigger rerolls for all types
whenever there's an artprop conflict. Now the result of an item_def of
the form "any TYPE artprops:..." will always produce an item of TYPE
with a random subtype where all specified artprops can be applied. If
this proves to be impossible, for example because the specified artprops
could never be compatible with TYPE, item of TYPE with random subtype is
still generated, but with any offending artprops ignored. An error
message is printed for each conflicting artprop in this case.

crawl-ref/source/makeitem.cc
@@ -362,16 +362,22 @@ bool is_weapon_brand_ok(int type, int brand, bool /*strict*/)
     return true;
 }
 
+// Choose a random weapon type compatible with any supplied brand and fixed
+// artprops. If no weapon type is compatible with both of these, use the last
+// chosen type, set no brand, and ignore the fixed artprops.
 static void _roll_weapon_type(item_def& item, int item_level)
 {
     for (int i = 0; i < 1000; ++i)
     {
         item.sub_type = _determine_weapon_subtype(item_level);
-        if (is_weapon_brand_ok(item.sub_type, item.brand, true))
+        if (is_weapon_brand_ok(item.sub_type, item.brand, true)
+            && are_fixed_props_ok(item))
+        {
             return;
+        }
     }
 
-    item.brand = SPWPN_NORMAL; // fall back to no brand
+    item.brand = SPWPN_NORMAL;
 }
 
 /// Plusses for a non-artefact weapon with positive plusses.
@@ -988,8 +994,11 @@ static void _generate_armour_item(item_def& item, bool allow_uniques,
         for (int i = 0; i < 1000; ++i)
         {
             item.sub_type = _get_random_armour_type(item_level);
-            if (is_armour_brand_ok(item.sub_type, item.brand, true))
+            if (is_armour_brand_ok(item.sub_type, item.brand, true)
+                && are_fixed_props_ok(item))
+            {
                 break;
+            }
         }
     }
 
@@ -1359,14 +1368,26 @@ static void _generate_book_item(item_def& item, bool allow_uniques,
 
 static stave_type _get_random_stave_type()
 {
-    stave_type r;
-    do
-    {
-        r = static_cast<stave_type>(random2(NUM_STAVES));
-    }
-    while (item_type_removed(OBJ_STAVES, r));
+    static vector<stave_type> valid_types;
+    if (valid_types.empty())
+        for (int i = STAFF_FIRST_STAFF; i < NUM_STAVES; i++)
+            if (!item_type_removed(OBJ_STAVES, (stave_type)i))
+                valid_types.push_back((stave_type)i);
 
-    return r;
+    return *random_iterator(valid_types);
+}
+
+// Choose a random stave type compatible with any fixed artprops. If no stave
+// type is compatible, we use the last chosen type and ignore the fixed
+// artprops.
+static void _roll_stave_type(item_def& item)
+{
+    for (int i = 0; i < 1000; ++i)
+    {
+        item.sub_type = _get_random_stave_type();
+        if (are_fixed_props_ok(item))
+            return;
+    }
 }
 
 static void _try_make_staff_artefact(item_def& item, bool allow_uniques,
@@ -1402,7 +1423,7 @@ static void _generate_staff_item(item_def& item, bool allow_uniques,
                                  int force_type, int item_level, int agent)
 {
     if (force_type == OBJ_RANDOM)
-        item.sub_type = _get_random_stave_type();
+        _roll_stave_type(item);
     else
         item.sub_type = force_type;
 
@@ -1433,6 +1454,26 @@ static void _generate_gem_item(item_def& item, int force_type)
 {
     ASSERT_RANGE(force_type, 0, NUM_GEM_TYPES);
     item.sub_type = force_type;
+}
+
+jewellery_type get_random_amulet_type()
+{
+    static vector<jewellery_type> valid_types;
+    if (valid_types.empty())
+        for (int i = AMU_FIRST_AMULET; i < NUM_JEWELLERY; i++)
+            if (!item_type_removed(OBJ_JEWELLERY, (jewellery_type)i))
+                valid_types.push_back((jewellery_type)i);
+    return *random_iterator(valid_types);
+}
+
+jewellery_type get_random_ring_type()
+{
+    static vector<jewellery_type> valid_types;
+    if (valid_types.empty())
+        for (int i = RING_FIRST_RING; i < NUM_RINGS; i++)
+            if (!item_type_removed(OBJ_JEWELLERY, (jewellery_type)i))
+                valid_types.push_back((jewellery_type)i);
+    return *random_iterator(valid_types);
 }
 
 static bool _try_make_jewellery_unrandart(item_def& item, int force_type,
@@ -1489,6 +1530,33 @@ static short _determine_ring_plus(int subtype)
     return _good_jewellery_plus(subtype);
 }
 
+// Choose a random ring type compatible with any fixed artprops. If no ring
+// type is compatible, we use the last chosen type and ignore the fixed
+// artprops.
+static void _roll_ring_type(item_def& item)
+{
+    for (int i = 0; i < 1000; ++i)
+    {
+        item.sub_type = get_random_ring_type();
+        if (are_fixed_props_ok(item))
+            return;
+    }
+}
+
+// Choose a random amulet type compatible with any fixed artprops. If no amulet
+// type is compatible, we use the last chosen type and ignore the fixed
+// artprops.
+static void _roll_amulet_type(item_def& item)
+{
+    for (int i = 0; i < 1000; ++i)
+    {
+        item.sub_type = get_random_amulet_type();
+        if (are_fixed_props_ok(item))
+            return;
+    }
+
+}
+
 static void _generate_jewellery_item(item_def& item, bool allow_uniques,
                                      int force_type, int item_level,
                                      int agent)
@@ -1500,23 +1568,20 @@ static void _generate_jewellery_item(item_def& item, bool allow_uniques,
     }
 
     // Determine subtype.
-    // Note: removed double probability reduction for some subtypes
     if (force_type != OBJ_RANDOM
+        // These values request a random ring or a random amulet, respectively.
         && force_type != NUM_RINGS
         && force_type != NUM_JEWELLERY)
     {
         item.sub_type = force_type;
     }
-    else
+    else if (force_type == NUM_RINGS
+             || force_type == OBJ_RANDOM && one_chance_in(4))
     {
-        if (force_type == NUM_RINGS)
-            item.sub_type = get_random_ring_type();
-        else if (force_type == NUM_JEWELLERY)
-            item.sub_type = get_random_amulet_type();
-        else
-            item.sub_type = (one_chance_in(4) ? get_random_amulet_type()
-                                              : get_random_ring_type());
+        _roll_ring_type(item);
     }
+    else
+        _roll_amulet_type(item);
 
     item.plus = _determine_ring_plus(item.sub_type);
 
@@ -1577,19 +1642,30 @@ static int _talisman_level(int item_level)
     }
 }
 
-static int _pick_talisman_type(int force_type, int lvl)
+// Choose a random talisman type compatible with any fixed artprops. If no
+// talisman type is compatible, we use the last chosen type and ignore the
+// fixed artprops.
+static void _roll_talisman_type(item_def &item, int lvl)
 {
-    if (force_type != OBJ_RANDOM)
-        return force_type;
-
     random_picker<talisman_type, NUM_TALISMANS * 3 /*ew*/> picker;
-    return picker.pick(talisman_weights, lvl, NUM_TALISMANS);
+
+    for (int i = 0; i < 1000; ++i)
+    {
+        item.sub_type = picker.pick(talisman_weights, lvl, NUM_TALISMANS);
+        if (are_fixed_props_ok(item))
+            return;
+    }
 }
 
 static void _generate_talisman_item(item_def& item, int force_type, int item_level)
 {
     const int lvl = _talisman_level(item_level);
-    item.sub_type = _pick_talisman_type(force_type, lvl);
+
+    if (force_type != OBJ_RANDOM)
+        item.sub_type = force_type;
+    else
+        _roll_talisman_type(item, lvl);
+
     if (item_level == ISPEC_RANDART || x_chance_in_y(lvl, 270))
         make_item_randart(item);
 }
@@ -2060,27 +2136,6 @@ static bool _armour_is_visibly_special(const item_def &item)
         return true;
 
     return false;
-}
-
-
-jewellery_type get_random_amulet_type()
-{
-    static vector<jewellery_type> valid_types;
-    if (valid_types.empty())
-        for (int i = AMU_FIRST_AMULET; i < NUM_JEWELLERY; i++)
-            if (!item_type_removed(OBJ_JEWELLERY, (jewellery_type)i))
-                valid_types.push_back((jewellery_type)i);
-    return *random_iterator(valid_types);
-}
-
-jewellery_type get_random_ring_type()
-{
-    static vector<jewellery_type> valid_types;
-    if (valid_types.empty())
-        for (int i = RING_FIRST_RING; i < NUM_RINGS; i++)
-            if (!item_type_removed(OBJ_JEWELLERY, (jewellery_type)i))
-                valid_types.push_back((jewellery_type)i);
-    return *random_iterator(valid_types);
 }
 
 // Sets item appearance to match brands, if any.

crawl-ref/source/artefact.cc
@@ -704,6 +704,37 @@ static bool _artp_can_go_on_item(artefact_prop_type prop, int prop_val,
     }
 }
 
+bool are_fixed_props_ok(item_def& item)
+{
+    if (!item.props.exists(FIXED_PROPS_KEY))
+        return true;
+
+    artefact_properties_t intrinsic_props;
+    intrinsic_props.init(0);
+    _populate_item_intrinsic_artps(item, intrinsic_props);
+
+    artefact_properties_t props;
+    props.init(0);
+
+    CrawlHashTable const *fixed_props;
+    fixed_props = &item.props[FIXED_PROPS_KEY].get_table();
+    for (auto const &kv : *fixed_props)
+    {
+        const auto prop = artp_type_from_name(kv.first);
+        const auto &final_val = kv.second.get_int();
+        const auto prop_val = final_val - intrinsic_props[prop];
+        if (!_artp_can_go_on_item(prop, prop_val, item, intrinsic_props,
+                                  props))
+        {
+            return false;
+        }
+
+        props[prop] = prop_val;
+    }
+
+    return true;
+}
+
 /// Generation info for a type of artefact property.
 struct artefact_prop_data
 {

crawl-ref/source/artefact.h
@@ -145,3 +145,5 @@ void unrand_reacts();
 void unrand_death_effects(monster* mons, killer_type killer);
 
 bool item_type_can_be_artefact(object_class_type typ);
+
+bool are_fixed_props_ok(item_def& item);

crawl-ref/source/item-prop-enum.h
@@ -570,6 +570,7 @@ enum stave_type
     STAFF_POWER,
 #endif
     STAFF_FIRE,
+    STAFF_FIRST_STAFF = STAFF_FIRE,
     STAFF_COLD,
     STAFF_ALCHEMY,
 #if TAG_MAJOR_VERSION == 34

commit b60e247eb4dafc9d0a3a18b078440513962df170

Don't generate substantially more amulets than rings (various)
Broken by 9283c13 accidentally inverting from one_chance_in_(4) while it
was rearranging the item type code.

crawl-ref/source/makeitem.cc
@@ -1576,7 +1576,7 @@ static void _generate_jewellery_item(item_def& item, bool allow_uniques,
         item.sub_type = force_type;
     }
     else if (force_type == NUM_RINGS
-             || force_type == OBJ_RANDOM && one_chance_in(4))
+             || force_type == OBJ_RANDOM && !one_chance_in(4))
     {
         _roll_ring_type(item);
     }