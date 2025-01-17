574e4ebf1702066b91a9ab6b94b7d12dfcf7b15d
DracoOmega <draco.omega.dcss.dev@gmail.com>
2025-01-17 12:36:29 -0330

    Fix a crash with " (list jewellery) on octopode on a narrow window

--- crawl-ref/source/command.cc
+++ crawl-ref/source/command.cc
@@ -347,7 +347,7 @@ static void _list_equipment(equipment_slot first_slot, equipment_slot last_slot)
             {
                 // XXX: Must strip color tags out to get the proper actual string
                 //      length to pad.
-                int pad = width - formatted_string::parse_string(entries[i]).tostring().length();
+                int pad = max(0, (int)(width - formatted_string::parse_string(entries[i]).tostring().length()));
                 mprf(MSGCH_EQUIPMENT, "%s%s", (entries[i].c_str() + string(pad, ' ')).c_str(),
                                               entries[i+1].c_str());
             }
