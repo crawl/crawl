599c4360dd1e9687022806266f332cc5b98491c1
Cgettys <3117273+Cgettys@users.noreply.github.com>
2025-01-18 15:01:11 -0800

    fix: overflow of overlays with Qazlal's Disaster area (prawnwizard)
    In rare cases, you can exceed the 20 overlay limit.
    
    Primarily due to duplicate overlays. This leads to a crash.
    
    To fix this:
    * Deduplicate all overlays, not just the most recent
    * Reduce overlay cap to 16 instead of 20 for vectorization reasons
    * Make packed_cell fit into 196 bytes, because why not + cache alignment
    * Make add_overlay an instance method for convenience
    
    Modern clang versions auto-vectorize and unroll this std::find nicely.
    GCC doesn't do quite as well, but at least clones/unrolls it
    4 wide. We'll see if it's a win or not.

--- crawl-ref/source/view.cc
+++ crawl-ref/source/view.cc
@@ -1630,12 +1630,7 @@ static void add_overlays(const coord_def& gc, screen_cell_t* cell)
            && tile_overlays[tile_overlay_i].gc == gc)
     {
         const auto &overlay = tile_overlays[tile_overlay_i];
-        if (cell->tile.num_dngn_overlay == 0
-            || cell->tile.dngn_overlay[cell->tile.num_dngn_overlay - 1]
-                                            != static_cast<int>(overlay.tile))
-        {
-            cell->tile.dngn_overlay[cell->tile.num_dngn_overlay++] = overlay.tile;
-        }
+        cell->tile.add_overlay(overlay.tile);
         tile_overlay_i++;
     }
 #endif
