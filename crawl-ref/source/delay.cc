crawl/crawl

commit 2dbd7c1ffd74f34fc6c2de16f9470e8d4fb2abf5

Completely rewrite how player equipment slots are handled
The system Crawl used to handle its equipment slots was conceptually
simple, but inflexible; every possible place an item could be equipped
had a unique enum entry in equipment_type. This resulted in things like
2 different enums for left ring and right ring, 8 additional ones for
octopode rings, another specifically for the ring granted by the Macabre
Finger (and a bunch of code to compute a list of which of these slots
was available at the moment - with Ru's Sac Hand removing a different
enum depending on whether you were an octopode or not!)

This made adding additional slots for different body plans non-trivial
(even Coglins had to duplicate a lot of code to handle letting the
player equip more than one weapon - something that was very
ring-specific up to that point). Items like Lear's, which occupy more
than one equipment slot at once, also required a lot of brittle
special-casing in many different places.

This commit rewrites player equipment handling from the ground up,
allowing much more flexibility with items, species, or mutations that
add new slots, as well as simplifying items that occupy multiple slots.
It also fixes a handful of existing bugs and improves QoL in a number of
common cases.

An incomplete list of major changes:

[Backend]:

-All information related to player equipment is now contained in the new
 player_equipment_info class (replacing you.equip, you.melded,
 you.activated, and you.unrand_reacts) and all querying/changing of
 equipment is done through its methods.

-Equipment slots are now defined by *type* rather than instance. ie: there
 is only one SLOT_RING, but the player can possess an arbitrary number of
 them (or of any other slot) and this number can be dynamically modified by
 mutations, equipment, or anything else one likes.

-As such, all slots of a type are now equivalent. You don't have a 'left
 ring' and a 'right ring', but '2 rings' (minorly affects inventory

crawl-ref/source/delay.cc
@@ -207,7 +207,7 @@ bool EquipOffDelay::try_interrupt(bool force)
     {
         const bool is_armour = equip.base_type == OBJ_ARMOUR
                                // Shields and orbs aren't clothes.
-                               && get_armour_slot(equip) != EQ_OFFHAND;
+                               && get_armour_slot(equip) != SLOT_OFFHAND;
         const char* verb = is_armour ? "disrobing" : "removing your equipment";
         const string prompt = make_stringf("Keep %s?", verb);
         if (!crawl_state.disables[DIS_CONFIRMATIONS]
@@ -686,14 +686,6 @@ void MultidropDelay::tick()
     items.erase(items.begin());
 }
 
-void JewelleryOnDelay::tick()
-{
-    // This is a 1-turn delay where the time cost is handled
-    // in finish().
-    // FIXME: get rid of this hack!
-    you.time_taken = 0;
-}
-
 void DropItemDelay::tick()
 {
     // This is a 1-turn delay where the time cost is handled
@@ -757,32 +749,6 @@ void handle_delay()
     delay->handle();
 }
 
-void JewelleryOnDelay::finish()
-{
-    // Recheck -Tele here, since our condition may have changed since starting
-    // the amulet swap process.
-    // Just breaking here is okay because swapping jewellery is a one-turn
-    // action, so conceptually there is nothing to interrupt - in other words,
-    // this is equivalent to if the user took off the previous amulet and was
-    // affected by tele other before putting the -Tele amulet on as a separate
-    // action on the next turn.
-    // XXX: duplicates a check in invent.cc:check_warning_inscriptions()
-    if (!crawl_state.disables[DIS_CONFIRMATIONS]
-        && needs_notele_warning(jewellery, OPER_PUTON))
-    {
-        string prompt = "Really put on ";
-        prompt += jewellery.name(DESC_INVENTORY);
-        prompt += " while about to teleport?";
-        if (!yesno(prompt.c_str(), false, 'n'))
-            return;
-    }
-
-#ifdef USE_SOUND
-    parse_sound(WEAR_JEWELLERY_SOUND);
-#endif
-    puton_ring(jewellery, false, false, true);
-}
-
 bool EquipOnDelay::invalidated()
 {
     return !equip.defined();
@@ -790,30 +756,12 @@ bool EquipOnDelay::invalidated()
 
 void EquipOnDelay::finish()
 {
-    const unsigned int old_talents = your_talents(false).size();
-    const bool is_amulet = equip.base_type == OBJ_JEWELLERY;
-    const equipment_type eq_slot = is_amulet ?      EQ_AMULET :
-                                   primary_weapon ? EQ_WEAPON :
-                                                    get_armour_slot(equip);
-
-#ifdef USE_SOUND
-    if (is_weapon(equip))
-        parse_sound(WIELD_WEAPON_SOUND);
-    else if (!is_amulet)
-        parse_sound(EQUIP_ARMOUR_SOUND);
-#endif
     mprf("You finish %s %s.", get_verb(), equip.name(DESC_YOUR).c_str());
 
-    equip_item(eq_slot, equip.link);
-
-    check_item_hint(equip, old_talents);
-
-    if (is_weapon(equip))
-    {
+    if (is_weapon(equip) && you.has_mutation(MUT_SLOW_WIELD))
         maybe_name_weapon(equip);
-        you.wield_change  = true;
-        quiver::on_weapon_changed();
-    }
+
+    equip_item(slot, equip.link);
 }
 
 bool EquipOffDelay::invalidated()
@@ -823,30 +771,8 @@ bool EquipOffDelay::invalidated()
 
 void EquipOffDelay::finish()
 {
-    const bool is_amu = equip.base_type == OBJ_JEWELLERY;
-    const equipment_type slot = is_amu ?         EQ_AMULET :
-                                primary_weapon ? EQ_WEAPON :
-                                                 get_armour_slot(equip);
-    ASSERTM(you.equip[slot] == equip.link,
-        "Mismatched link in EquipOffDelay::finish: slot is %d with link %d, link is %d",
-        slot, you.equip[slot], equip.link);
-
-#ifdef USE_SOUND
-    parse_sound(is_amu ?           REMOVE_JEWELLERY_SOUND :
-                is_weapon(equip) ? WIELD_NOTHING_SOUND :
-                                   DEQUIP_ARMOUR_SOUND);
-#endif
-    if (is_weapon(equip))
-        say_farewell_to_weapon(equip);
-
     mprf("You finish %s %s.", get_verb(), equip.name(DESC_YOUR).c_str());
-    unequip_item(slot);
-
-    if (is_weapon(equip))
-    {
-        you.wield_change  = true;
-        quiver::on_weapon_changed();
-    }
+    unequip_item(equip);
 }
 
 void MemoriseDelay::finish()
@@ -1010,8 +936,8 @@ void TransformDelay::finish()
 {
     if (form == transformation::none)
     {
-        untransform();
         unset_default_form();
+        untransform();
         return;
     }
 

crawl-ref/source/transform.h
@@ -87,9 +87,8 @@ private:
 protected:
     Form(transformation tran);
 public:
-    bool slot_available(int slot) const;
-    bool can_wield() const { return slot_available(EQ_WEAPON); }
-    virtual bool can_wear_item(const item_def& item) const;
+    bool slot_is_blocked(equipment_slot slot) const;
+    bool can_wield() const { return !slot_is_blocked(SLOT_WEAPON); }
 
     int get_duration(int pow) const;
 
@@ -131,7 +130,7 @@ public:
     virtual string get_transform_description() const { return description; }
 
     virtual string get_description(bool past_tense = false) const;
-    virtual string transform_message(bool was_flying) const;
+    virtual string transform_message() const;
     virtual string get_untransform_message() const;
 
     virtual int res_fire() const;
@@ -204,8 +203,8 @@ public:
     const int dex_mod;
 
     /// Equipment types unusable in this form.
-    /** A bitfield representing a union of (1 << equipment_type) values for
-     * equipment types that are unusable in this form.
+    /** A bitfield representing a union of (1 << equipment_slot) values for
+     * equipment slots that are melded in this form.
      */
     const int blocked_slots; // XX check enum size at compile time?
     /// size of the form
@@ -273,9 +272,6 @@ protected:
                         bool max = false, int scale = 1) const;
 
 private:
-    bool all_blocked(int slotflags) const;
-
-private:
     /// Can this form fly?
     /** Whether the form enables, forbids, or does nothing to the player's
      * ability to fly.
@@ -331,14 +327,15 @@ bool form_has_feet(transformation form = you.form);
 bool form_has_ears(transformation form = you.form);
 
 bool feat_dangerous_for_form(transformation which_trans,
-                             dungeon_feature_type feat);
-
-bool check_form_stat_safety(transformation new_form, bool quiet = false);
+                             dungeon_feature_type feat,
+                             const item_def* talisman = nullptr);
 
 string cant_transform_reason(transformation which_trans, bool involuntary = false,
                              bool temp = true);
-bool check_transform_into(transformation which_trans, bool involuntary = false);
-bool transform(int pow, transformation which_trans, bool involuntary = false);
+bool check_transform_into(transformation which_trans, bool involuntary = false,
+                          const item_def* talisman = nullptr);
+bool transform(int pow, transformation which_trans, bool involuntary = false,
+               bool using_talisman = false);
 
 // skip_move: don't make player re-enter current cell
 void untransform(bool skip_move = false);
@@ -348,10 +345,6 @@ void set_default_form(transformation t, const item_def *source);
 
 void set_form(transformation which_trans, int dur);
 void return_to_default_form();
-
-void remove_one_equip(equipment_type eq, bool meld = true,
-                      bool mutation = false);
-void unmeld_one_equip(equipment_type eq);
 
 monster_type transform_mons();
 string blade_parts(bool terse = false);

commit 503d60fff3008cbfc41f4f292e5185fb4452da3a

Fix HP scaling when entering/exiting talisman forms (CanOfWorms)
When the player's max HP is adjusted by changing form via talisman, their
current HP is supposed to be left alone. This is to discourage things such
as shifting into a form with -90% max hp and then immediately leaving it
for a big heal.

But the logic for this was subtly broken by 2dbd7c1 which changed which
point in the complicated transformation sequence the player's 'default
form' was set - resulting in the code thinking you were merely returning to
that form after polymorph (which *is* supposed to scale your HP, so that
you get bonus HP from drinking !lig, for instance.)

But apparently, even in 0.32, this logic was partially broken. It was
possibly to instantly heal by shifting into an underskilled talisman, and
then directly into a talisman that you *did* have skill for (eg: beast
talisman).

Rather than rely on the finicky behavior of comparing current and default
form, HP scaling is now controlled by a new argument passed to
transformation functions, so that it should hopefully reliably happen where
we want it to, and not when we don't.

crawl-ref/source/delay.cc
@@ -947,7 +947,7 @@ void TransformDelay::finish()
     if (form == transformation::none)
     {
         unset_default_form();
-        untransform();
+        untransform(false, false);
         return;
     }
 

crawl-ref/source/transform.cc
@@ -1568,9 +1568,8 @@ static void _on_enter_form(transformation which_trans)
     }
 }
 
-void set_form(transformation which_trans, int dur)
+void set_form(transformation which_trans, int dur, bool scale_hp)
 {
-    const transformation old_form = you.form;
     you.form = which_trans;
     you.duration[DUR_TRANSFORMATION] = dur * BASELINE_DELAY;
     update_player_symbol();
@@ -1584,17 +1583,6 @@ void set_form(transformation which_trans, int dur)
     if (dex_mod)
         notify_stat_change(STAT_DEX, dex_mod, true);
 
-    // Don't scale HP when going from nudity to a talisman form
-    // or vice versa. This is to discourage regenerating in a -90%
-    // underskilled talisman form and scaling back up to full, or
-    // leaving a +HP form to regen.
-    // Do scale HP when entering or leaving eg tree form, regardless
-    // of whether you're going from a talisman form or not.
-    const bool leaving_default = you.default_form == old_form
-                                 && which_trans == transformation::none;
-    const bool entering_default = you.default_form == which_trans
-                                 && old_form == transformation::none;
-    const bool scale_hp = !entering_default && !leaving_default;
     calc_hp(scale_hp);
 
     you.redraw_evasion      = true;
@@ -1603,7 +1591,7 @@ void set_form(transformation which_trans, int dur)
     quiver::set_needs_redraw();
 }
 
-static void _enter_form(int pow, transformation which_trans)
+static void _enter_form(int pow, transformation which_trans, bool scale_hp = true)
 {
     const bool was_flying = you.airborne();
 
@@ -1618,7 +1606,7 @@ static void _enter_form(int pow, transformation which_trans)
     // changes) before adjusting the player to be transformed.
     you.equipment.meld_equipment(get_form(which_trans)->blocked_slots);
 
-    set_form(which_trans, _transform_duration(which_trans, pow));
+    set_form(which_trans, _transform_duration(which_trans, pow), scale_hp);
 
     if (you.digging && !form_keeps_mutations(which_trans))
     {
@@ -1763,9 +1751,9 @@ bool transform(int pow, transformation which_trans, bool involuntary,
     }
 
     if (you.form != transformation::none)
-        untransform(true);
+        untransform(true, !using_talisman);
 
-    _enter_form(pow, which_trans);
+    _enter_form(pow, which_trans, !using_talisman);
 
     return true;
 }
@@ -1775,8 +1763,13 @@ bool transform(int pow, transformation which_trans, bool involuntary,
  * form.
  * @param skip_move      If true, skip any move that was in progress before
  *                       the transformation ended.
+ * @param scale_hp       Whether keep the player's HP percentage the same if
+ *                       their max HP changes. (Should be false for
+ *                       talisman-related shapeshifting, to prevent exploits
+ *                       such as instantly healing via entering a -90% HP form
+ *                       and then leaving it again immediately.)
  */
-void untransform(bool skip_move)
+void untransform(bool skip_move, bool scale_hp)
 {
     const transformation old_form = you.form;
 
@@ -1787,7 +1780,7 @@ void untransform(bool skip_move)
     if (!message.empty())
         mprf(MSGCH_DURATION, "%s", message.c_str());
 
-    set_form(transformation::none, 0);
+    set_form(transformation::none, 0, scale_hp);
 
     const int str_mod = get_form(old_form)->str_mod;
     const int dex_mod = get_form(old_form)->dex_mod;
@@ -1876,14 +1869,14 @@ void untransform(bool skip_move)
 void return_to_default_form()
 {
     if (you.default_form == transformation::none)
-        untransform();
+        untransform(false, false);
     else
     {
         // Forcibly break out of forced forms at this point (since this should
         // only be called in situations where those should end and transform()
         // will refuse to do that on its own)
         if (you.transform_uncancellable)
-            untransform(true);
+            untransform(true, false);
         transform(0, you.default_form, true, true);
     }
     ASSERT(you.form == you.default_form);

crawl-ref/source/transform.h
@@ -338,12 +338,12 @@ bool transform(int pow, transformation which_trans, bool involuntary = false,
                bool using_talisman = false);
 
 // skip_move: don't make player re-enter current cell
-void untransform(bool skip_move = false);
+void untransform(bool skip_move = false, bool scale_hp = true);
 
 void unset_default_form();
 void set_default_form(transformation t, const item_def *source);
 
-void set_form(transformation which_trans, int dur);
+void set_form(transformation which_trans, int dur, bool scale_hp = true);
 void return_to_default_form();
 
 monster_type transform_mons();