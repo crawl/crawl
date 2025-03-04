crawl/crawl

commit 31604a805092c86b6d8add61cdd7f55ecea294fd

Fix grow oklob not placing under the player (LiciTheCrawler)
There was a bug that made plants hostile while being placed and Fedhas
worshippers cannot be on top of hostile oklob plants (but can be on top
of other hostile plants). This allowed growing ballistomycetes under
the player but not oklobs.

Fixes #4048

crawl-ref/source/mon-place.cc
@@ -971,14 +971,6 @@ static monster* _place_monster_aux(const mgen_data &mg, const monster *leader,
     mon->base_monster = mg.base_type;
     mon->xp_tracking  = mg.xp_tracking;
 
-    // Set pos and link monster into monster grid.
-    if (!dont_place && !mon->move_to_pos(fpos))
-    {
-        env.mid_cache.erase(mon->mid);
-        mon->reset();
-        return 0;
-    }
-
     // Pick the correct Serpent of Hell.
     if (mon->type == MONS_SERPENT_OF_HELL)
     {
@@ -1300,6 +1292,16 @@ static monster* _place_monster_aux(const mgen_data &mg, const monster *leader,
         }
 
         mon->behaviour = BEH_WANDER;
+    }
+
+    // Set pos and link monster into monster grid.
+    // This must be done after setting the monster's attitude as `move_to_pos`
+    // might check it.
+    if (!dont_place && !mon->move_to_pos(fpos))
+    {
+        env.mid_cache.erase(mon->mid);
+        mon->reset();
+        return 0;
     }
 
     if (mg.is_summoned())

commit b2a08d023c6c6fa7c9433a3ccb0396da98a5e77c

Fix shapeshifter monsters spawning in their natural form (Ge0FF)
Broken somewhat subtly by 31604a8. monster_polymorph will fail if the
monster isn't in a valid position (since the space they're on isn't
habitable for anything they could turn into), so moving when in
_place_monster_aux the monster is actually put on the grid caused them to
always spawn in their native, unshifted form.

I have moved the polymorph lower in the function. I am a little concerned
that there may be other non-obvious consequences to the change done in
31604a8, but a cursory inspection didn't turn up anything else that seemed
dependent on the monster having a real position.

crawl-ref/source/mon-place.cc
@@ -1191,17 +1191,6 @@ static monster* _place_monster_aux(const mgen_data &mg, const monster *leader,
 
     mon->flags |= MF_JUST_SUMMONED;
 
-    // Don't leave shifters in their starting shape.
-    if (mg.cls == MONS_SHAPESHIFTER || mg.cls == MONS_GLOWING_SHAPESHIFTER)
-    {
-        msg::suppress nm;
-        monster_polymorph(mon, RANDOM_MONSTER);
-
-        // It's not actually a known shapeshifter if it happened to be
-        // placed in LOS of the player.
-        mon->flags &= ~MF_KNOWN_SHIFTER;
-    }
-
     if (mons_class_is_animated_weapon(mg.cls))
     {
         if (mg.props.exists(TUKIMA_WEAPON))
@@ -1302,6 +1291,19 @@ static monster* _place_monster_aux(const mgen_data &mg, const monster *leader,
         env.mid_cache.erase(mon->mid);
         mon->reset();
         return 0;
+    }
+
+    // Don't leave shifters in their starting shape.
+    // (This must be done after they are moved into a real position, or
+    // monster_polymorph will veto any possible thing they could turn into.)
+    if (mg.cls == MONS_SHAPESHIFTER || mg.cls == MONS_GLOWING_SHAPESHIFTER)
+    {
+        msg::suppress nm;
+        monster_polymorph(mon, RANDOM_MONSTER);
+
+        // It's not actually a known shapeshifter if it happened to be
+        // placed in LOS of the player.
+        mon->flags &= ~MF_KNOWN_SHIFTER;
     }
 
     if (mg.is_summoned())