2246e8532c493afc546beef8e9e9bfacb9c842d8
mumra <pete.hurst@gmail.com>
2025-03-10 02:19:21 +0000

    fix CORS config in dev server

--- crawl-ref/source/web/vite.config.ts
+++ crawl-ref/source/web/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [crossOriginIsolation, react()],
+  plugins: [crossOriginIsolation(), react()],
 });
