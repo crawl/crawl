34610d1a4b6b215d44ff31599f41c877849298a0
SentientSupper <146337911+SentientSupper@users.noreply.github.com>
2025-03-10 12:52:16 +0800

    Allow aux attacks to receive lash bonuses (#3695)
    WJC's lash claims to work on melee attacks and doesn't specifically
    mention not benefiting aux attacks.
    
    Closes #3298
    
    ---------
    
    Co-authored-by: Hellmonk <nldehaan91@gmail.com>

--- crawl-ref/source/melee-attack.cc
+++ crawl-ref/source/melee-attack.cc
@@ -1961,9 +1961,12 @@ bool melee_attack::player_aux_test_hit()
         if (x_chance_in_y(player_blind_miss_chance(1), 100))
             to_hit = -1;
     }
-
-    if (to_hit >= evasion || auto_hit)
+  
+    if (to_hit >= evasion || auto_hit
+        || wu_jian_has_momentum(wu_jian_attack))
+    {
         return true;
+    }
 
     mprf("Your %s misses %s.", aux_attack.c_str(),
          defender->name(DESC_THE).c_str());
