8eb92faf5a41b4db18f926fe50ac9de98d80bc7c
DracoOmega <draco.omega.dcss.dev@gmail.com>
2025-03-17 12:09:49 -0230

    Don't let Okawaru gift reaping weapons (cool3)
    Oka doesn't technically hate you for using them, but the brand does nothing
    while worshipping them, so it feels they should never gift it.

--- crawl-ref/source/acquire.cc
+++ crawl-ref/source/acquire.cc
@@ -1162,6 +1162,10 @@ static string _why_reject(const item_def &item, int agent)
     if (agent == GOD_TROG && is_unrandom_artefact(item, UNRAND_TROG))
         return "Destroying Trog-gifted Wrath of Trog!";
 
+    // Oka does not gift reaping weapons.
+    if (agent == GOD_OKAWARU && get_weapon_brand(item) == SPWPN_REAPING)
+        return "Destroying Oka-gifted reaping weapon.";
+
     // Pain brand is useless if you've sacrificed Necromacy.
     if (you.get_mutation_level(MUT_NO_NECROMANCY_MAGIC)
         && get_weapon_brand(item) == SPWPN_PAIN)
