85c0d3feac165453702f603b7de9a660ffce363d
DracoOmega <draco.omega.dcss.dev@gmail.com>
2025-03-18 19:57:40 -0230

    Fix a Nobody / phantom mirror crash
    timeout_enchantments was also deleting its summon timer in the middle of
    reviving it (causing it to die again before the avoided_death_fineff had
    fired, resulting in an assert).
    
    Really would be nice to have that 'just remove harmful status effects'
    function....

--- crawl-ref/source/mon-abil.cc
+++ crawl-ref/source/mon-abil.cc
@@ -1336,8 +1336,13 @@ bool pyrrhic_recollection(monster& nobody)
     nobody.heal(nobody.max_hit_points);
 
     // If this was a phantom mirror copy, allow it to revive, but don't wipe out
-    // its summoner timer at the same time, to keep it from having unlimited duration.
-    mon_enchant summon_timer = nobody.get_ench(ENCH_SUMMON_TIMER);
+    // its summon timer at the same time, to keep it from having unlimited duration.
+    mon_enchant summon_timer;
+    if (nobody.has_ench(ENCH_SUMMON_TIMER))
+    {
+        summon_timer = nobody.get_ench(ENCH_SUMMON_TIMER);
+        nobody.del_ench(ENCH_SUMMON_TIMER, true, false);
+    }
     // Also, manually delete the previous enkindle without effect, in case
     // Nobody's taken a big enough hit to go from healthy to dead in one action,
     // or we'll immediately wipe out the spells we just gave them.
