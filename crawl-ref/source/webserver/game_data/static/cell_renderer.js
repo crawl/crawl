7ce8407eeac1e53fec410130ad2df50686b75a8a
Nikolai Lavsky <3328424+nlavsky@users.noreply.github.com>
2025-03-20 18:14:38 +0300

    fix: display the chaos-laced status icon in webtiles
    This got missed in 4b739738.

--- crawl-ref/source/webserver/game_data/static/cell_renderer.js
+++ crawl-ref/source/webserver/game_data/static/cell_renderer.js
@@ -1116,6 +1116,7 @@ function ($, view_data, gui, main, tileinfo_player, icons, dngn, enums,
                 case icons.FULLY_CHARGED:
                 case icons.VITRIFIED:
                 case icons.CONFUSED:
+                case icons.LACED_WITH_CHAOS:
                     this.draw_icon(idx, x, y, ofsx, ofsy, img_scale);
                     return 6;
                 case icons.CONC_VENOM:
