crawl/crawl

commit fb19e97205a53957dc0dd5bdbeb20ada8cb9d718

Refactor backend handling of summons / created monsters (Part 1)
Crawl's handling of summoned and not-technically-summoned-but-close
monsters is a complicated and somewhat untidy thing that makes it harder to
generalize behavior between conventional summons and other monsters that
share many of their properties, without considerable special cases and
bespoke code in various places. For instance, monsters created by the
player can use one of *4* different enchantments for their timer, which can
result in those monsters expiring with with 4 different killer_types (but
not even on a 1-to-1 basis with which timer is used!) that results in
sometimes-unclear differences in behavior. It's also needlessly verbose to
ask if a given monster was created by a given spell, easy to forget to
check for all 4 different ways a monster can be temporary here, and many
other issues that have resulted in various bugs over time (several of which
were outstanding at the time of this commit.)

This is an attempt to significantly clean up and better-document this
entire system, as well as make it easier to interact with and fix a number
of bugs in the process.

Major changes:

-ENCH_ABJ, ENCH_FAKE_ABJURATION, and ENCH_SHORT_LIVED are all folded into
 a single timer: ENCH_SUMMON_TIMER. (Save-compat code will convert
 deprecated enchantments into the new standard timer).

-Rather than differentiate summon behavior by which timer is used, two
 monster flags are added (MF_ACTUAL_SUMMON and MF_PERSISTS) to cover the
 most clear and actually-meaningful differences that already existed.
 -MF_ACTUAL_SUMMON indicates the a monster is abjurable, will produce a
  poof message (and possibly clouds) when it dies - regardless of whether
  this was by timeout or being directly killed - and will not produce other
  death effects (like simulacrum freezing clouds) when it times out.
 -MF_PERSISTS indicates that a monster should not be removed when its

crawl-ref/source/mgen-data.h
@@ -32,13 +32,13 @@ struct mgen_data
     beh_type        behaviour;
 
     // Who summoned this monster?  Important to know for death accounting
-    // and the summon cap, if and when it goes in. nullptr is no summoner.
+    // and the summon cap. nullptr is no summoner.
     const actor*    summoner;
 
     // For summoned monsters, this is a measure of how long the summon will
-    // hang around, on a scale of 1-6, 6 being longest. Use 0 for monsters
-    // that aren't summoned.
-    int             abjuration_duration;
+    // hang around, measured in aut. Use 0 for monsters that aren't summoned
+    // or those which are summoned but do not expire on their.
+    int             summon_duration;
 
     // For summoned monsters this is their type of summoning, either the
     // spell which summoned them or one of the values of the enumeration
@@ -124,7 +124,7 @@ struct mgen_data
               mgen_flags genflags = MG_NONE,
               god_type which_god = GOD_NO_GOD)
 
-        : cls(mt), behaviour(beh), summoner(nullptr), abjuration_duration(0),
+        : cls(mt), behaviour(beh), summoner(nullptr), summon_duration(0),
           summon_type(0), pos(p), foe(mfoe), flags(genflags), god(which_god),
           base_type(MONS_NO_MONSTER), colour(COLOUR_INHERIT),
           proximity(PROX_ANYWHERE), place(level_id::current()), hd(0), hp(0),
@@ -162,8 +162,9 @@ struct mgen_data
         return *this;
     }
 
-    mgen_data &set_summoned(const actor* _summoner, int abjuration_dur,
-                            int _summon_type, god_type _god = GOD_NO_GOD)
+    mgen_data &set_summoned(const actor* _summoner, int _summon_type,
+                            int duration = 0, bool abjurable = true,
+                            bool dependent = true)
     {
         // Hijack any summons created by player shadows or marionettes to belong
         // to the player (your shadow is too emphemeral to keep them from
@@ -187,30 +188,23 @@ struct mgen_data
         }
         else
             summoner = _summoner;
-        abjuration_duration = abjuration_dur;
+        summon_duration = duration;
         summon_type = _summon_type;
-        if (_god != GOD_NO_GOD)
-            god = _god;
 
-        ASSERT(summon_type == 0 || abjuration_dur >= 1 && abjuration_dur <= 6
-               || cls == MONS_BALL_LIGHTNING || cls == MONS_ORB_OF_DESTRUCTION
-               || cls == MONS_BATTLESPHERE || cls == MONS_BALLISTOMYCETE_SPORE
-               || cls == MONS_BOULDER
-               || cls == MONS_HOARFROST_CANNON
-               || cls == MONS_PILE_OF_DEBRIS
-               || cls == MONS_HELLFIRE_MORTAR
-               || cls == MONS_GLOBE_OF_ANNIHILATION
-               || summon_type == SPELL_ANIMATE_DEAD
-               || summon_type == SPELL_DEATH_CHANNEL
-               || summon_type == SPELL_BIND_SOULS
-               || summon_type == SPELL_SIMULACRUM
-               || summon_type == SPELL_AWAKEN_VINES
-               || summon_type == SPELL_FULMINANT_PRISM
-               || summon_type == SPELL_SHADOW_PRISM
-               || summon_type == SPELL_INFESTATION
-               || summon_type == SPELL_FOXFIRE
-               || summon_type == SPELL_MARSHLIGHT
-               || summon_type == MON_SUMM_AID);
+        // It doesn't make sense to have an abjurable summon with no duration.
+        if (duration = 0)
+            abjurable = false;
+
+        if (abjurable)
+            extra_flags |= MF_ACTUAL_SUMMON;
+        else
+            extra_flags &= ~MF_ACTUAL_SUMMON;
+
+        if (!dependent)
+            extra_flags |= MF_PERSISTS;
+        else
+            extra_flags &= ~MF_PERSISTS;
+
         return *this;
     }
 
@@ -227,7 +221,10 @@ struct mgen_data
     // when placing the monster?
     bool use_position() const { return in_bounds(pos); };
 
-    bool summoned() const { return abjuration_duration > 0; }
+    // XXX: The summoner field is used in normal band placement to temporarily
+    //      designate a band member's leader, so we need to rule that out.
+    bool is_summoned() const { return summon_type != SPELL_NO_SPELL
+                                      || (summoner != nullptr && !(flags & MG_BAND_MINION)); }
 
     static mgen_data sleeper_at(monster_type what,
                                 const coord_def &where,
@@ -238,9 +235,11 @@ struct mgen_data
 
     static mgen_data hostile_at(monster_type mt,
                                 bool alert = false,
-                                const coord_def &p = coord_def(-1, -1))
+                                const coord_def &p = coord_def(-1, -1),
+                                god_type god = GOD_NO_GOD)
 
     {
-        return mgen_data(mt, BEH_HOSTILE, p, alert ? MHITYOU : MHITNOT);
+        return mgen_data(mt, BEH_HOSTILE, p, alert ? MHITYOU : MHITNOT,
+                         MG_NONE, god);
     }
 };

crawl-ref/source/abyss.cc
@@ -1857,7 +1857,7 @@ static bool _spawn_corrupted_servant_near(const coord_def &pos)
         if (!monster_habitable_grid(mons, env.grid(p)))
             continue;
         mgen_data mg(mons, BEH_NEUTRAL, p);
-        mg.set_summoned(0, 5, 0).set_non_actor_summoner("Lugonu's corruption");
+        mg.set_summoned(0, 0, summ_dur(5)).set_non_actor_summoner("Lugonu's corruption");
         mg.place = BRANCH_ABYSS;
         return create_monster(mg);
     }
@@ -1882,7 +1882,7 @@ static void _spawn_corrupted_servant_near_monster(const monster &who)
         if (!monster_habitable_grid(mons, env.grid(p)))
             continue;
         mgen_data mg(mons, BEH_COPY, p);
-        mg.set_summoned(&who, 3, 0);
+        mg.set_summoned(&who, 0, summ_dur(3));
         mg.place = BRANCH_ABYSS;
         if (create_monster(mg))
             return;

crawl-ref/source/actor.h
@@ -46,8 +46,10 @@ public:
     virtual bool      alive() const = 0;
 
     // Should return false for perma-summoned things.
-    virtual bool is_summoned(int* duration = nullptr,
-                             int* summon_type = nullptr) const = 0;
+    virtual bool is_summoned() const = 0;
+    virtual bool is_summoned_by(int summon_type) const = 0;
+    virtual bool is_summoned_by(const actor& summoner,
+                                int summon_type = SPELL_NO_SPELL) const = 0;
 
     virtual bool is_perm_summoned() const = 0;
 

crawl-ref/source/arena.cc
@@ -1145,7 +1145,7 @@ bool arena_veto_place_monster(const mgen_data &mg, bool first_band_member,
     // If the first band member makes it past the summon throttle cut,
     // let all of the rest of its band in too regardless of the summon
     // throttle.
-    if (mg.abjuration_duration > 0 && first_band_member)
+    if (mg.summon_duration > 0 && first_band_member)
     {
         if (mg.behaviour == BEH_FRIENDLY
             && arena::faction_a.active_members > arena::summon_throttle)
@@ -1222,7 +1222,7 @@ void arena_placed_monster(monster* mons)
         // Real summons drop corpses and items.
         if (arena::real_summons)
         {
-            mons->del_ench(ENCH_ABJ, true, false);
+            mons->del_ench(ENCH_SUMMON_TIMER, true, false);
             for (mon_inv_iterator ii(*mons); ii; ++ii)
                 ii->flags &= ~ISFLAG_SUMMONED;
         }

crawl-ref/source/art-func.h
@@ -564,7 +564,7 @@ static void _DEMON_AXE_melee_effects(item_def* /*item*/, actor* attacker,
             create_monster(
                 mgen_data(summon_any_demon(RANDOM_DEMON_COMMON),
                           SAME_ATTITUDE(mons), mons->pos(), mons->foe)
-                .set_summoned(mons, 6, SPELL_SUMMON_DEMON));
+                .set_summoned(mons, SPELL_SUMMON_DEMON, summ_dur(6)));
         }
         else if (!you.allies_forbidden())
         {
@@ -572,7 +572,7 @@ static void _DEMON_AXE_melee_effects(item_def* /*item*/, actor* attacker,
                                                   : random_demon_by_tier(4));
             if (create_monster(
                     mgen_data(type, BEH_COPY, you.pos(), MHITYOU, MG_FORCE_BEH | MG_AUTOFOE)
-                    .set_summoned(&you, 6, SPELL_SUMMON_DEMON, GOD_NO_GOD)))
+                    .set_summoned(&you, SPELL_SUMMON_DEMON, summ_dur(6))))
             {
                 mpr("A gate to Pandemonium opens briefly!");
             }
@@ -1814,7 +1814,7 @@ static void _ASMODEUS_melee_effects(item_def* /*weapon*/, actor* attacker,
 
         mgen_data mg(demon, BEH_FRIENDLY, you.pos(), MHITYOU,
                      MG_FORCE_BEH | MG_AUTOFOE);
-        mg.set_summoned(&you, 4, SPELL_FIRE_SUMMON);
+        mg.set_summoned(&you, SPELL_FIRE_SUMMON, summ_dur(4));
 
         if (create_monster(mg))
         {

crawl-ref/source/attitude-change.cc
@@ -147,7 +147,7 @@ void dismiss_god_summons(god_type god)
     for (monster_iterator mi; mi; ++mi)
     {
         if (is_follower(**mi)
-            && mi->has_ench(ENCH_ABJ)
+            && mi->is_summoned()
             && mons_is_god_gift(**mi, god))
         {
             // The monster disappears.

crawl-ref/source/beam.cc
@@ -2704,7 +2704,7 @@ void bolt::affect_endpoint()
                                             SAME_ATTITUDE(agent(true)->as_monster()),
                                             spot, agent(true)->as_monster()->foe,
                                             MG_FORCE_PLACE)
-                            .set_summoned(agent(true), 1, SPELL_FLASHING_BALESTRA, GOD_NO_GOD));
+                            .set_summoned(agent(true), SPELL_FLASHING_BALESTRA, summ_dur(1)));
 
             if (blade)
                 blade->add_ench(ENCH_MIGHT);
@@ -2735,7 +2735,7 @@ void bolt::affect_endpoint()
             bool obviousness; // dummy argument
             monster *mirror = clone_mons(blitzer, true, &obviousness,
                                          blitzer->attitude, spot);
-            mirror->mark_summoned(2, true, SPELL_PHANTOM_BLITZ);
+            mirror->mark_summoned(SPELL_PHANTOM_BLITZ, 2);
             mirror->summoner = blitzer->mid;
             mirror->foe = blitzer->foe;
             mirror->hit_points = blitzer->max_hit_points;
@@ -3075,7 +3075,7 @@ void bolt::affect_place_explosion_clouds()
 
             actor* summ = agent();
             mgen_data mg(MONS_FIRE_VORTEX, att, p, MHITNOT, MG_NONE, god);
-            mg.set_summoned(summ, 1, SPELL_FIRE_STORM);
+            mg.set_summoned(summ, SPELL_FIRE_STORM, summ_dur(1), false, false);
 
             // Spell-summoned monsters need to have a live summoner.
             if (summ == nullptr || !summ->alive())
@@ -5846,9 +5846,7 @@ bool bolt::ignores_monster(const monster* mon) const
     if ((flavour == BEAM_HAEMOCLASM || flavour == BEAM_BLOODRITE) && mon->friendly())
         return true;
 
-    int summon_type = 0;
-    mon->is_summoned(nullptr, &summon_type);
-    if (flavour == BEAM_QAZLAL && summon_type == MON_SUMM_AID)
+    if (flavour == BEAM_QAZLAL && mon->is_summoned_by(you, MON_SUMM_AID))
         return true;
 
     if (origin_spell == SPELL_UPHEAVAL && agent() && agent() == mon)
@@ -6664,7 +6662,7 @@ mon_resist_type bolt::apply_enchantment_to_monster(monster* mon)
         if (!monster_can_be_unravelled(*mon))
             return MON_UNAFFECTED;
 
-        if (mon->is_summoned())
+        if (mon->is_abjurable())
         {
             mprf("The magic binding %s to this plane unravels!",
                  mon->name(DESC_THE).c_str());

crawl-ref/source/cloud.cc
@@ -617,8 +617,8 @@ static void _handle_spectral_cloud(const cloud_struct& cloud)
                              (agent ? agent->foe : short{MHITYOU}),
                              MG_FORCE_PLACE)
                     .set_base(basetype)
-                    .set_summoned(actor_by_mid(cloud.source), 1,
-                                  SPELL_SPECTRAL_CLOUD));
+                    .set_summoned(actor_by_mid(cloud.source),
+                                  SPELL_SPECTRAL_CLOUD, summ_dur(1)));
 }
 
 void manage_clouds()
@@ -1005,10 +1005,8 @@ bool actor_cloud_immune(const actor &act, const cloud_struct &cloud)
         return true;
     }
 
-    int summon_type = 0;
-    act.is_summoned(nullptr, &summon_type);
     if (!player && have_passive(passive_t::cloud_immunity)
-        && (act.as_monster()->friendly() && summon_type == MON_SUMM_AID))
+        && act.is_summoned_by(MON_SUMM_AID))
     {
         return true;
     }
@@ -1959,7 +1957,7 @@ static const vector<chaos_effect> chaos_effects = {
 
             // The player shouldn't get new permanent followers from cloning.
             if (clone->attitude == ATT_FRIENDLY && !clone->is_summoned())
-                clone->mark_summoned(6, true, MON_SUMM_CLONE);
+                clone->mark_summoned(MON_SUMM_CLONE, 6);
             else
                 clone->flags |= (MF_NO_REWARD | MF_HARD_RESET);
 

commit 804bde46941465643b824d9a463d80711bde7c6d

Fix monsters from Lugonu's Corruption lasting forever (Ge0FF)
Likely broken since fb19e97205a5 this was possibly the only place in the
code where summoned monsters were made with no summoner *or* summon type
(that weren't also *intended* to be permanent).

But in general, it feels like giving something a summon timer ought to
actually apply its summon timer, so modifying the core function like this
makes sense to me.

crawl-ref/source/mgen-data.h
@@ -236,6 +236,7 @@ struct mgen_data
     // XXX: The summoner field is used in normal band placement to temporarily
     //      designate a band member's leader, so we need to rule that out.
     bool is_summoned() const { return summon_type != SPELL_NO_SPELL
+                                      || summon_duration > 0
                                       || (summoner != nullptr && !(flags & MG_BAND_MINION)); }
 
     static mgen_data sleeper_at(monster_type what,