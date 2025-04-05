crawl/crawl

commit d67f6492281a5b8d07af0ef746d709d4451a2aae

Don't attack an exponential number of times with cleaving quick blades
(ie: if you have the Cleave status from Xom)

The second attack against each target would initiate an entire new series
of attacks against every adjacent enemy, which grew *wildly* out of hand
with many nearby enemies. If you were sufficiently bad at killing them
(maybe only possible in wizmode), this could even crash the game.

Instead, only determine cleave targets from the first attack.

crawl-ref/source/melee-attack.cc
@@ -1209,7 +1209,7 @@ bool melee_attack::run_attack_set()
  */
 bool melee_attack::attack()
 {
-    if (!cleaving && !never_cleave)
+    if (!cleaving && !never_cleave && !is_multihit)
     {
         cleave_setup();
         if (!handle_phase_attempted())

commit 4491eb6210ac593ecfe8bf15293937416abdc91e

Don't skip a variety of attack effects on quick blade's second attack
This was broken by d67f6492281a5b8d0 which fixed a major bug, but had
several unintended side-effects. For instance, the second attack of a
quick blade set:
 -Couldn't fumble in water
 -Wouldn't flinch away from a monster you're afraid of
 -Didn't get the accuracy bonus of Manifold Assault or Kinetic Grapnel
  (or Grapnel's slaying bonus, either)
 -Couldn't trigger berserkitis (arguably okay)
 -Didn't add to attack action counts (not actually sure which of the two
  behaviours makes more sense here)

Now specifically skip the cleaving target phase, while running the rest of
the code in handle_phase_atempted()

crawl-ref/source/melee-attack.cc
@@ -1458,9 +1458,10 @@ bool melee_attack::run_attack_set()
  */
 bool melee_attack::attack()
 {
-    if (!cleaving && !never_cleave && !is_multihit)
+    if (!cleaving && !never_cleave)
     {
-        cleave_setup();
+        if (!is_multihit)
+            cleave_setup();
         if (!handle_phase_attempted())
             return false;
 