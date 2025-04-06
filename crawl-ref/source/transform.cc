5b40c49a21cc0523862600afd8797ac2ec7f1c23
sdynet <sdynet76@gmail.com>
2025-04-07 05:12:47 +0900

    Change Kiku and Zin passive to max at ****** (160) piety. (#4052)
    Change the chance of passive power to max percent out at 160 piety,
    like other gods:
     * Kiku's torment protection
     * Zin's polymorph protection

--- crawl-ref/source/transform.cc
+++ crawl-ref/source/transform.cc
@@ -1947,7 +1947,7 @@ bool transform(int dur, transformation which_trans, bool involuntary,
 {
     // Zin's protection.
     if (have_passive(passive_t::resist_polymorph)
-        && x_chance_in_y(you.piety, MAX_PIETY)
+        && x_chance_in_y(you.piety, piety_breakpoint(5))
         && which_trans != transformation::none)
     {
         simple_god_message(" protects your body from unnatural transformation!");
