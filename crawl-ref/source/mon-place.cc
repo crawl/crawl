crawl/crawl

commit 238f98a3a70114a0d73f19358f64e72e65131f2f

Allow mons_class_habitat to return HT_FLYER
Instead of having multiple different callers of this function manually
check flying status afterward and add HT_FLYER onto the return value
themselves.

It's now in the same category as 'considering giant monsters amphibious for
most purposes' (with the ability to ignore this to look at 'core' habitat,
if desired.)

This also fixes an issue with f2a3531c259 where the checks for flying
summons did not properly account for them flying.

I am... not 100% sure this doesn't have unexpected effects on some part of
code that doesn't expect flying monsters to have HT_LAVA, but I didn't see
anything obvious upon looking into it, and it feels like a cleaner way to
use our new habitat bitflags.

crawl-ref/source/mon-place.cc
@@ -165,11 +165,7 @@ bool monster_habitable_feat(const monster* mon, dungeon_feature_type feat)
  */
 bool monster_habitable_feat(monster_type mt, dungeon_feature_type feat)
 {
-    habitat_type ht = mons_class_habitat(mt);
-    if (mons_class_flag(mt, M_FLIES))
-        ht = (habitat_type)(ht | HT_FLYER);
-
-    return _habitable_feat(ht, feat);
+    return _habitable_feat(mons_class_habitat(mt), feat);
 }
 
 bool monster_habitable_grid(const monster* mon, const coord_def& pos)

crawl-ref/source/mon-util.cc
@@ -3321,35 +3321,38 @@ mon_intel_type mons_intel(const monster& m)
     return mons_class_intel(mon.type);
 }
 
-habitat_type mons_class_habitat(monster_type mc, bool real_amphibious)
+// What habitats can this monster effectively occupy?
+// If core_only is true (defaults to false), ignore flight and large size, and
+// consider only 'native' habitat.
+habitat_type mons_class_habitat(monster_type mc, bool core_only)
 {
     const monsterentry *me = get_monster_data(mc);
-    habitat_type ht = (me ? me->habitat
-                          : get_monster_data(MONS_PROGRAM_BUG)->habitat);
-    if (!real_amphibious)
+    habitat_type ht = (me ? me->habitat : HT_LAND);
+    if (!core_only)
     {
         // XXX: No class equivalent of monster::body_size(PSIZE_BODY)!
-        size_type st = (me ? me->size
-                           : get_monster_data(MONS_PROGRAM_BUG)->size);
+        size_type st = (me ? me->size : SIZE_GIANT);
         if (ht == HT_LAND && st >= SIZE_GIANT)
             ht = HT_AMPHIBIOUS;
+        if (me && monster_class_flies(mc))
+            ht = HT_FLYER;
     }
     return ht;
 }
 
 habitat_type mons_habitat_type(monster_type t, monster_type base_t,
-                               bool real_amphibious)
+                               bool core_only)
 {
     return mons_class_habitat(fixup_zombie_type(t, base_t),
-                              real_amphibious);
+                              core_only);
 }
 
-habitat_type mons_habitat(const monster& mon, bool real_amphibious)
+habitat_type mons_habitat(const monster& mon, bool core_only)
 {
     const monster_type type = mons_is_draconian_job(mon.type)
         ? draconian_subspecies(mon) : mon.type;
 
-    return mons_habitat_type(type, mons_base_type(mon), real_amphibious);
+    return mons_habitat_type(type, mons_base_type(mon), core_only);
 }
 
 int mons_power(monster_type mc)

crawl-ref/source/mon-movetarget.cc
@@ -29,8 +29,6 @@ static void _mark_neighbours_target_unreachable(monster* mon)
 {
     const mon_intel_type intel = mons_intel(*mon);
     habitat_type habitat = mons_habitat(*mon);
-    if (mon->airborne())
-        habitat = (habitat_type)(habitat | HT_FLYER);
 
     for (radius_iterator ri(mon->pos(), 2, C_SQUARE); ri; ++ri)
     {
@@ -52,8 +50,6 @@ static void _mark_neighbours_target_unreachable(monster* mon)
             continue;
 
         habitat_type other_habitat = mons_habitat(*m);
-        if (m->airborne())
-            other_habitat = (habitat_type)(other_habitat | HT_FLYER);
 
         // Monsters of less restrictive habitats might prefer different routes.
         if ((habitat & other_habitat) != other_habitat)

crawl-ref/source/mon-util.h
@@ -290,9 +290,9 @@ mon_intel_type mons_intel(const monster& mon);
 // Use mons_habitat() wherever possible, since the class variants do not
 // handle zombies correctly.
 habitat_type mons_habitat_type(monster_type t, monster_type base_t,
-                               bool real_amphibious = false);
-habitat_type mons_class_habitat(monster_type t, bool real_amphibious = false);
-habitat_type mons_habitat(const monster& mon, bool real_amphibious = false);
+                               bool core_only = false);
+habitat_type mons_class_habitat(monster_type t, bool core_only = false);
+habitat_type mons_habitat(const monster& mon, bool core_only = false);
 
 bool mons_skeleton(monster_type mc);
 

crawl-ref/source/monster.cc
@@ -298,7 +298,7 @@ bool monster::floundering_at(const coord_def p) const
     const dungeon_feature_type grid = env.grid(p);
     return (liquefied(p)
             || (feat_is_water(grid)
-                // Use real_amphibious to detect giant non-water monsters in
+                // Use core_only to detect giant non-water monsters in
                 // deep water, who flounder despite being treated as amphibious.
                 && !(mons_habitat(*this, true) & HT_DEEP_WATER)
                 && !extra_balanced_at(p)))

commit 0d869b6020a6afd7523c59c315ee37c7c84cd467

Fix a few things ignoring item-based/temporary monster flight (WizardIke)
An accidental oversight caused by 238f98a3a70114a0d7. Now, mons_habitat
will automatically include any source of flight.

(And prune another bit of code that should now be unnecessary.)

crawl-ref/source/mon-place.cc
@@ -90,11 +90,6 @@ static monster* _place_pghost_aux(const mgen_data &mg, const monster *leader,
 
 static int _fill_apostle_band(monster& mons, monster_type* band);
 
-static bool _hab_requires_mon_flight(dungeon_feature_type g)
-{
-    return g == DNGN_LAVA || g == DNGN_DEEP_WATER;
-}
-
 static bool _habitable_feat(habitat_type ht, dungeon_feature_type feat)
 {
     if ((ht & HT_MALIGN_GATEWAY) && feat == DNGN_MALIGN_GATEWAY)
@@ -149,10 +144,7 @@ habitat_type habitat_for_any(const vector<monster_type>& mon_types)
  */
 bool monster_habitable_feat(const monster* mon, dungeon_feature_type feat)
 {
-    habitat_type ht = mons_habitat(*mon);
-
-    bool type_safe = _habitable_feat(ht, feat);
-    return type_safe || (_hab_requires_mon_flight(feat) && mon->airborne());
+    return _habitable_feat(mons_habitat(*mon), feat);
 }
 
 /**

crawl-ref/source/mon-util.cc
@@ -3363,7 +3363,12 @@ habitat_type mons_habitat(const monster& mon, bool core_only)
     const monster_type type = mons_is_draconian_job(mon.type)
         ? draconian_subspecies(mon) : mon.type;
 
-    return mons_habitat_type(type, mons_base_type(mon), core_only);
+    habitat_type ret = mons_habitat_type(type, mons_base_type(mon), core_only);
+    // This includes item-based and temporary flight, which mons_class_habitat cannot!
+    if (!core_only && mon.airborne())
+        ret = (habitat_type)(ret | HT_FLYER);
+
+    return ret;
 }
 
 int mons_power(monster_type mc)