01fb971b632e0069fad14bcc99f7268009df8233
DracoOmega <draco.omega.dcss.dev@gmail.com>
2025-04-17 17:34:23 -0230

    Don't make a note about every runed door that the player encounters
    While it makes some sense to list runed doors seen in the dungeon overview
    (and this commit does not change that), the game would make an entry in the
    dump about *every single runed door tile* the player ever observed. Just
    encountering doorvault could flood a whole page of it (and necropolis also
    is full of them).
    
    This doesn't seem particularly worth noting, and nobody objected when I
    asked about removing this.

--- crawl-ref/source/feature-data.h
+++ crawl-ref/source/feature-data.h
@@ -52,7 +52,7 @@ static feature_def feat_defs[] =
     DNGN_RUNED_DOOR, "runed door", "runed_door",
     DCHAR_DOOR_CLOSED, NUM_DCHAR_TYPES,
     COLOUR_AND_MAP(LIGHTBLUE),
-    FFT_OPAQUE | FFT_SOLID | FFT_NOTABLE, MF_DOOR,
+    FFT_OPAQUE | FFT_SOLID, MF_DOOR,
 },
 
 {
@@ -80,7 +80,7 @@ static feature_def feat_defs[] =
     DNGN_RUNED_CLEAR_DOOR, "runed translucent door", "runed_clear_door",
     DCHAR_DOOR_CLOSED, NUM_DCHAR_TYPES,
     COLOUR_AND_MAP(LIGHTCYAN),
-    FFT_SOLID | FFT_NOTABLE, MF_DOOR,
+    FFT_SOLID, MF_DOOR,
 },
 
 {
