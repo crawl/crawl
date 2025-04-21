crawl/crawl

commit 08901def8ca821a23f7aa51e6e6069c1cf3496e0

Fix monsters being alerted at the end of Passwall (dilly)
In fixing an issue with distortion unwield banishment effects being delayed
by one turn for Coglins, 0c5ade9 introduced at least one other (and
probably more) by calling world_reacts() on all 0-turn delays.

Rather than attempt to untangle exactly which parts of world_reacts() make
sense to call on 0-aut turns (ie: bookkeeping) and which do not (ie:
changes of world state), I am rolling back this particular fix and making
EquipOffDelay check for banishment specifically (PasswallDelay already
does much the same thing for traps, for instance).

A more comprehensive refactoring could make sense, but doesn't seem
appropriate after feature freeze, since the risk of subtle bugs seems high.

crawl-ref/source/delay.cc
@@ -72,6 +72,8 @@
 #include "xom.h"
 #include "zot.h" // ZOT_CLOCK_PER_FLOOR
 
+extern void check_banished();
+
 int interrupt_block::interrupts_blocked = 0;
 
 static const char *_activity_interrupt_name(activity_interrupt ai);
@@ -743,6 +745,10 @@ void EquipOffDelay::finish()
 {
     mprf("You finish %s %s.", get_verb(), equip.name(DESC_YOUR).c_str());
     unequip_item(equip);
+
+    // Banishment via coglin distortion unwield might not happen until the turn
+    // after unwielding, otherwise.
+    check_banished();
 }
 
 void MemoriseDelay::finish()

crawl-ref/source/main.cc
@@ -216,6 +216,8 @@ static void _take_starting_note();
 static void _startup_hints_mode();
 static void _set_removed_types_as_identified();
 
+void check_banished();
+
 static void _startup_asserts()
 {
     for (int i = 0; i < NUM_BRANCHES; ++i)
@@ -1163,15 +1165,12 @@ static void _input()
 #endif
 
         // Some delays reset you.time_taken.
-        if (!time_is_frozen && (you.time_taken || you.turn_is_over)
-            && you.berserk())
+        if (!time_is_frozen && (you.time_taken || you.turn_is_over))
         {
-            _do_berserk_no_combat_penalty();
+            if (you.berserk())
+                _do_berserk_no_combat_penalty();
+            world_reacts();
         }
-
-        // Call this even if we took no time - we might have been banished
-        // by an unwield effect.
-        world_reacts();
 
         if (!you_are_delayed())
             update_can_currently_train();
@@ -2497,7 +2496,7 @@ static void _prep_input()
     }
 }
 
-static void _check_banished()
+void check_banished()
 {
     if (you.banished)
     {
@@ -2608,7 +2607,7 @@ void world_reacts()
     }
 #endif
 
-    _check_banished();
+    check_banished();
     _check_sanctuary();
     _check_trapped();
     check_spectral_weapon(you);
@@ -2626,7 +2625,7 @@ void world_reacts()
     // (mostly by exploding)
     fire_final_effects();
 
-    _check_banished();
+    check_banished();
 
     ASSERT(you.time_taken >= 0);
     you.elapsed_time += you.time_taken;

commit 0ed8941409ee46d5efcdf741cc871f809b701794

Fix monster compilation (dolorous, WizardIke)
Broken by 08901def8ca, so let's pull this function out into a proper header
(and abyss.h seems appropriate enough.)

crawl-ref/source/abyss.cc
@@ -457,6 +457,23 @@ void banished(const string &who, const int power)
     }
 }
 
+void check_banished()
+{
+    if (you.banished)
+    {
+        you.banished = false;
+        ASSERT(brdepth[BRANCH_ABYSS] != -1);
+        if (!player_in_branch(BRANCH_ABYSS))
+            mprf(MSGCH_BANISHMENT, "You are cast into the Abyss!");
+        else if (you.depth < brdepth[BRANCH_ABYSS])
+            mprf(MSGCH_BANISHMENT, "You are cast deeper into the Abyss!");
+        else
+            mprf(MSGCH_BANISHMENT, "The Abyss bends around you!");
+        // these are included in default force_more_message
+        banished(you.banished_by, you.banished_power);
+    }
+}
+
 void push_features_to_abyss()
 {
     abyssal_features.clear();

crawl-ref/source/abyss.h
@@ -37,6 +37,7 @@ extern abyss_state abyssal_state;
 void abyss_morph();
 
 void banished(const string &who = "", const int power = 0);
+void check_banished();
 void push_features_to_abyss();
 
 void clear_abyssal_rune_knowledge();

crawl-ref/source/delay.cc
@@ -10,6 +10,7 @@
 #include <cstdio>
 #include <cstring>
 
+#include "abyss.h"
 #include "ability.h"
 #include "areas.h"
 #include "artefact.h"
@@ -71,8 +72,6 @@
 #include "travel.h"
 #include "xom.h"
 #include "zot.h" // ZOT_CLOCK_PER_FLOOR
-
-extern void check_banished();
 
 int interrupt_block::interrupts_blocked = 0;
 

crawl-ref/source/main.cc
@@ -216,8 +216,6 @@ static void _take_starting_note();
 static void _startup_hints_mode();
 static void _set_removed_types_as_identified();
 
-void check_banished();
-
 static void _startup_asserts()
 {
     for (int i = 0; i < NUM_BRANCHES; ++i)
@@ -2493,23 +2491,6 @@ static void _prep_input()
             mprf(MSGCH_GOD, "You have a vision of multiple gates.");
 
         you.seen_portals = 0;
-    }
-}
-
-void check_banished()
-{
-    if (you.banished)
-    {
-        you.banished = false;
-        ASSERT(brdepth[BRANCH_ABYSS] != -1);
-        if (!player_in_branch(BRANCH_ABYSS))
-            mprf(MSGCH_BANISHMENT, "You are cast into the Abyss!");
-        else if (you.depth < brdepth[BRANCH_ABYSS])
-            mprf(MSGCH_BANISHMENT, "You are cast deeper into the Abyss!");
-        else
-            mprf(MSGCH_BANISHMENT, "The Abyss bends around you!");
-        // these are included in default force_more_message
-        banished(you.banished_by, you.banished_power);
     }
 }
 