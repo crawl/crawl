81cb54cb1ca19d2451483d09cd1b62eb593fa61e
DracoOmega <draco.omega.dcss.dev@gmail.com>
2025-04-30 21:36:45 -0230

    End a few effects when exiting forms
    Rime yak walls will disappear and werewolf loses its slaying bonus.

--- crawl-ref/source/transform.cc
+++ crawl-ref/source/transform.cc
@@ -2049,6 +2049,13 @@ void untransform(bool skip_move, bool scale_hp)
             mprf(MSGCH_DURATION, "Your tiny sun winks out.");
         }
     }
+    else if (old_form == transformation::rime_yak)
+    {
+        you.duration[DUR_RIME_YAK_AURA] = 0;
+        end_terrain_change(TERRAIN_CHANGE_RIME_YAK);
+    }
+    else if (old_form == transformation::werewolf)
+        you.duration[DUR_WEREFURY] = 0;
 
     // If the player is no longer be eligible to equip some of the items that
     // they were wearing (possibly due to losing slots from their default form
