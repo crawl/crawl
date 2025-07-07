3d2785031b75921f7f712d21b4c71ee7fbf4b713
DracoOmega <draco.omega.dcss.dev@gmail.com>
2025-07-07 03:26:21 -0230

    Don't prevent using fuzzed ?blinking with LoS <= 2 (Darby)
    Not that they're very useful with that small an LoS, but they technically
    do more than nothing and so shouldn't be effectively forbidden.

--- crawl-ref/source/spl-transloc.cc
+++ crawl-ref/source/spl-transloc.cc
@@ -898,7 +898,7 @@ spret controlled_blink(bool safe_cancel, dist *target)
     // Fuzz blinking by 2 tiles while in Zot or on the orb run.
     if (orb_limits_translocation())
     {
-        targeter_hop tgt(you.current_vision - 2, false);
+        targeter_hop tgt(max(1, you.current_vision - 2), false);
         if (!_find_cblink_target(*target, safe_cancel, "blink", &tgt))
             return spret::abort;
         target->target = _fuzz_blink_destination(target->target);
