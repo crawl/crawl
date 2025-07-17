crawl/crawl

commit 733a6eaeeb0b8ec7d363dfdf06752e1fbc20489b

Simplify LOS attack effect logic
Simplify and unify code for damaging players and monsters with Drain
Life, Ozocubu's Refrigeration, and the Singing Sword. Remove the
fairly redundant message announcing everything you hit.

crawl-ref/source/spl-damage.cc
@@ -591,63 +591,8 @@ static bool _drain_lifeable(const actor* agent, const actor* act)
              || mons && m && mons_atts_aligned(mons->attitude, m->attitude));
 }
 
-static void _los_spell_pre_damage_monsters(const actor* agent,
-                                           vector<monster *> affected_monsters,
-                                           const char *verb)
-{
-    // Filter out affected monsters that we don't know for sure are there
-    vector<monster*> seen_monsters;
-    for (monster *mon : affected_monsters)
-        if (you.can_see(*mon))
-            seen_monsters.push_back(mon);
-
-    if (!seen_monsters.empty())
-    {
-        counted_monster_list mons_list =
-            counted_monster_list(seen_monsters);
-        const string message = make_stringf("%s %s %s.",
-                mons_list.describe(DESC_THE).c_str(),
-                conjugate_verb("be", mons_list.count() > 1).c_str(), verb);
-        if (strwidth(message) < get_number_of_cols() - 2)
-            mpr(message);
-        else
-        {
-            // Exclamation mark to suggest that a lot of creatures were
-            // affected.
-            mprf("The monsters around %s are %s!",
-                agent && agent->is_monster() && you.can_see(*agent)
-                ? agent->as_monster()->name(DESC_THE).c_str()
-                : "you", verb);
-        }
-    }
-}
-
-static int _los_spell_damage_player(const actor* agent, bolt &beam,
-                                    bool actual)
-{
-    int hurted = actual ? beam.damage.roll()
-                        // Monsters use the average for foe calculations.
-                        : (1 + beam.damage.num * beam.damage.size) / 2;
-    hurted = check_your_resists(hurted, beam.flavour, beam.name, 0,
-            // Drain life doesn't apply drain effects.
-            actual && beam.origin_spell != SPELL_DRAIN_LIFE);
-    if (actual && hurted > 0)
-    {
-
-        if (agent && !agent->is_player())
-        {
-            ouch(hurted, KILLED_BY_BEAM, agent->mid,
-                 make_stringf("by %s", beam.name.c_str()).c_str(), true,
-                 agent->as_monster()->name(DESC_A).c_str());
-            you.expose_to_element(beam.flavour, 5);
-        }
-    }
-
-    return hurted;
-}
-
-static int _los_spell_damage_monster(const actor* agent, monster &target,
-                                     bolt &beam, bool actual)
+static int _los_spell_damage_actor(const actor* agent, actor &target,
+                                   bolt &beam, bool actual)
 {
 
     beam.thrower = (agent && agent->is_player()) ? KILL_YOU :
@@ -656,23 +601,26 @@ static int _los_spell_damage_monster(const actor* agent, monster &target,
 
     // Set conducts here. The monster needs to be alive when this is done, and
     // mons_adjust_flavoured() could kill it.
+    monster *mon_targ = target.as_monster();
     god_conduct_trigger conducts[3];
-    if (actual && YOU_KILL(beam.thrower))
-        set_attack_conducts(conducts, target, you.can_see(target));
+    if (actual && YOU_KILL(beam.thrower) && mon_targ)
+        set_attack_conducts(conducts, *mon_targ, you.can_see(target));
 
     int hurted = actual ? beam.damage.roll()
                         // Tracers use the average for damage calculations.
                         : (1 + beam.damage.num * beam.damage.size) / 2;
-    hurted = mons_adjust_flavoured(&target, beam, hurted,
-                 // Drain life doesn't apply drain effects.
-                 actual && beam.origin_spell != SPELL_DRAIN_LIFE);
+    const bool doFlavour = actual && beam.origin_spell != SPELL_DRAIN_LIFE;
+    if (mon_targ)
+        hurted = mons_adjust_flavoured(mon_targ, beam, hurted, doFlavour);
+    else
+        hurted = check_your_resists(hurted, beam.flavour, beam.name, 0, doFlavour);
     dprf("damage done: %d", hurted);
 
     if (actual)
     {
-        if (YOU_KILL(beam.thrower))
+        if (YOU_KILL(beam.thrower) && mon_targ)
         {
-            _player_hurt_monster(target, hurted, beam.flavour, false);
+            _player_hurt_monster(*mon_targ, hurted, beam.flavour, false);
             if (target.alive())
                 you.pet_target = target.mindex();
         }
@@ -709,7 +657,7 @@ static spret _cast_los_attack_spell(spell_type spell, int pow,
 
     const char *player_msg = nullptr, *global_msg = nullptr,
                *mons_vis_msg = nullptr, *mons_invis_msg = nullptr,
-               *verb = nullptr, *prompt_verb = nullptr;
+               *prompt_verb = nullptr;
     bool (*vulnerable)(const actor *, const actor *) = nullptr;
 
     switch (spell)
@@ -720,7 +668,6 @@ static spret _cast_los_attack_spell(spell_type spell, int pow,
             mons_vis_msg = " drains the heat from the surrounding"
                            " environment!";
             mons_invis_msg = "The ambient heat is drained!";
-            verb = "frozen";
             prompt_verb = "refrigerate";
             vulnerable = [](const actor *caster, const actor *act) {
                 return act != caster
@@ -736,7 +683,6 @@ static spret _cast_los_attack_spell(spell_type spell, int pow,
                          " surroundings.";
             mons_vis_msg = " draws from the surrounding life force!";
             mons_invis_msg = "The surrounding life force dissipates!";
-            verb = "drained of life";
             prompt_verb = "drain life";
             vulnerable = &_drain_lifeable;
             break;
@@ -746,7 +692,6 @@ static spret _cast_los_attack_spell(spell_type spell, int pow,
             global_msg = "Something sends a blast of sound all around you.";
             mons_vis_msg = " sends a blast of sound all around you!";
             mons_invis_msg = "Sound blasts the surrounding area!";
-            verb = "blasted";
             // prompt_verb = "sing" The singing sword prompts in melee-attack
             vulnerable = [](const actor *caster, const actor *act) {
                 return act != caster
@@ -800,8 +745,7 @@ static spret _cast_los_attack_spell(spell_type spell, int pow,
         }
     }
 
-    bool affects_you = false;
-    vector<monster *> affected_monsters;
+    vector<actor *> affected_actors;
 
     for (actor_near_iterator ai((agent ? agent : &you)->pos(), LOS_NO_TRANS);
          ai; ++ai)
@@ -810,76 +754,39 @@ static spret _cast_los_attack_spell(spell_type spell, int pow,
             continue;
 
         if ((*vulnerable)(agent, *ai))
-        {
-            if (ai->is_player())
-                affects_you = true;
-            else if (*ai != agent)
-                affected_monsters.push_back(ai->as_monster());
-        }
+            affected_actors.push_back(*ai);
     }
 
     const int avg_damage = (1 + beam.damage.num * beam.damage.size) / 2;
     int total_damage = 0;
-    // XXX: This ordering is kind of broken; it's to preserve the message
-    // order from the original behaviour in the case of refrigerate.
-    if (affects_you)
-    {
-        total_damage = _los_spell_damage_player(agent, beam, actual);
-        if (!actual && mons)
-        {
-            if (mons->wont_attack())
-            {
-                beam.friend_info.count++;
-                beam.friend_info.power +=
-                    (you.get_experience_level() * total_damage / avg_damage);
-            }
-            else
-            {
-                beam.foe_info.count++;
-                beam.foe_info.power +=
-                    (you.get_experience_level() * total_damage / avg_damage);
-            }
-        }
-    }
 
-    if (actual && !affected_monsters.empty())
-        _los_spell_pre_damage_monsters(agent, affected_monsters, verb);
-
-    for (auto m : affected_monsters)
+    for (auto a : affected_actors)
     {
         // Watch out for invalidation. Example: Ozocubu's refrigeration on
         // a bunch of ballistomycete spores that blow each other up.
-        if (!m->alive())
+        if (!a->alive())
             continue;
 
-        int this_damage = _los_spell_damage_monster(agent, *m, beam, actual);
+        int this_damage = _los_spell_damage_actor(agent, *a, beam, actual);
         total_damage += this_damage;
 
-        if (!actual)
+        if (actual || !this_damage)
+            continue;
+
+        if (a->is_monster() && !mons_is_threatening(*a->as_monster()))
+            continue;
+
+        if (!agent || !mons_aligned(a, agent))
         {
-            if (mons)
-            {
-                if (mons_atts_aligned(m->attitude, mons->attitude))
-                {
-                    beam.friend_info.count++;
-                    beam.friend_info.power +=
-                        (m->get_hit_dice() * this_damage / avg_damage);
-                }
-                else
-                {
-                    beam.foe_info.count++;
-                    beam.foe_info.power +=
-                        (m->get_hit_dice() * this_damage / avg_damage);
-                }
-            }
-            else if (player_caster
-                     && this_damage
-                     && !m->wont_attack()
-                     && mons_is_threatening(*m))
-            {
-                beam.foe_info.count++;
-                beam.foe_info.power++;
-            }
+            beam.foe_info.count++;
+            beam.foe_info.power +=
+                (a->get_hit_dice() * this_damage / avg_damage);
+        }
+        else
+        {
+            beam.friend_info.count++;
+            beam.friend_info.power +=
+                (a->get_hit_dice() * this_damage / avg_damage);
         }
     }
 

commit 758b7540862a2d61abc67fb4616358f35387d03c

Fix broken kauxes for LoS attack spells (gammafunk)
Apparently broken since 733a6ea, causing Ozocubu's Refrigeration to produce
death messages suggesting it was caused by the monster hitting you in melee
instead (which is rather more noticable now that it is cast by Orbs of
Winter, which have no attack...)

This also simultaneously gives a unique kaux for dying to the Singing
Sword's sonic wave, which I'm not sure it had before. Who will be the first
to die to it, I wonder? (Will anyone?)

crawl-ref/source/spl-damage.cc
@@ -603,7 +603,17 @@ static int _los_spell_damage_actor(const actor* agent, actor &target,
                 you.pet_target = target.mindex();
         }
         else if (hurted)
-            target.hurt(agent, hurted, beam.flavour);
+        {
+            kill_method_type ktype = KILLED_BY_BEAM;
+            if (beam.origin_spell == SPELL_DRAIN_LIFE)
+                ktype = KILLED_BY_DRAINING;
+            else if (beam.origin_spell == SPELL_OZOCUBUS_REFRIGERATION)
+                ktype = KILLED_BY_FREEZING;
+
+            string aux = beam.origin_spell == SPELL_SONIC_WAVE ? "sonic wave" : "";
+
+            target.hurt(agent, hurted, beam.flavour, ktype, "", aux);
+        }
 
         // Cold-blooded creatures can be slowed.
         if (beam.origin_spell == SPELL_OZOCUBUS_REFRIGERATION