crawl/crawl

commit fb19e97205a53957dc0dd5bdbeb20ada8cb9d718

Refactor backend handling of summons / created monsters (Part 1)
Crawl's handling of summoned and not-technically-summoned-but-close
monsters is a complicated and somewhat untidy thing that makes it harder to
generalize behavior between conventional summons and other monsters that
share many of their properties, without considerable special cases and
bespoke code in various places. For instance, monsters created by the
player can use one of *4* different enchantments for their timer, which can
result in those monsters expiring with with 4 different killer_types (but
not even on a 1-to-1 basis with which timer is used!) that results in
sometimes-unclear differences in behavior. It's also needlessly verbose to
ask if a given monster was created by a given spell, easy to forget to
check for all 4 different ways a monster can be temporary here, and many
other issues that have resulted in various bugs over time (several of which
were outstanding at the time of this commit.)

This is an attempt to significantly clean up and better-document this
entire system, as well as make it easier to interact with and fix a number
of bugs in the process.

Major changes:

-ENCH_ABJ, ENCH_FAKE_ABJURATION, and ENCH_SHORT_LIVED are all folded into
 a single timer: ENCH_SUMMON_TIMER. (Save-compat code will convert
 deprecated enchantments into the new standard timer).

-Rather than differentiate summon behavior by which timer is used, two
 monster flags are added (MF_ACTUAL_SUMMON and MF_PERSISTS) to cover the
 most clear and actually-meaningful differences that already existed.
 -MF_ACTUAL_SUMMON indicates the a monster is abjurable, will produce a
  poof message (and possibly clouds) when it dies - regardless of whether
  this was by timeout or being directly killed - and will not produce other
  death effects (like simulacrum freezing clouds) when it times out.
 -MF_PERSISTS indicates that a monster should not be removed when its

crawl-ref/source/timed-effects.cc
@@ -536,7 +536,7 @@ void monster::timeout_enchantments(int levels)
         switch (entry.first)
         {
         case ENCH_POISON: case ENCH_CORONA: case ENCH_CONTAM:
-        case ENCH_STICKY_FLAME: case ENCH_ABJ: case ENCH_SHORT_LIVED:
+        case ENCH_STICKY_FLAME: case ENCH_SUMMON_TIMER:
         case ENCH_HASTE: case ENCH_MIGHT: case ENCH_FEAR:
         case ENCH_CHARM: case ENCH_SLEEP_WARY: case ENCH_SICK:
         case ENCH_PARALYSIS: case ENCH_PETRIFYING:
@@ -544,7 +544,7 @@ void monster::timeout_enchantments(int levels)
         case ENCH_LOWERED_WL: case ENCH_SOUL_RIPE: case ENCH_ANTIMAGIC:
         case ENCH_REGENERATION: case ENCH_STRONG_WILLED:
         case ENCH_MIRROR_DAMAGE: case ENCH_LIQUEFYING:
-        case ENCH_SILVER_CORONA: case ENCH_DAZED: case ENCH_FAKE_ABJURATION:
+        case ENCH_SILVER_CORONA: case ENCH_DAZED:
         case ENCH_BREATH_WEAPON: case ENCH_WRETCHED:
         case ENCH_SCREAMED: case ENCH_BLIND: case ENCH_WORD_OF_RECALL:
         case ENCH_INJURY_BOND: case ENCH_FLAYED: case ENCH_BARBS:
@@ -836,8 +836,9 @@ void timeout_malign_gateways(int duration)
                                          mmark->behaviour,
                                          mmark->pos,
                                          MHITNOT,
-                                         MG_FORCE_PLACE);
-                mg.set_summoned(caster, 0, 0, mmark->god);
+                                         MG_FORCE_PLACE,
+                                         mmark->god);
+                mg.set_summoned(caster, 0);
                 if (!is_player)
                     mg.non_actor_summoner = mmark->summoner_string;
 

crawl-ref/source/abyss.cc
@@ -1857,7 +1857,7 @@ static bool _spawn_corrupted_servant_near(const coord_def &pos)
         if (!monster_habitable_grid(mons, env.grid(p)))
             continue;
         mgen_data mg(mons, BEH_NEUTRAL, p);
-        mg.set_summoned(0, 5, 0).set_non_actor_summoner("Lugonu's corruption");
+        mg.set_summoned(0, 0, summ_dur(5)).set_non_actor_summoner("Lugonu's corruption");
         mg.place = BRANCH_ABYSS;
         return create_monster(mg);
     }
@@ -1882,7 +1882,7 @@ static void _spawn_corrupted_servant_near_monster(const monster &who)
         if (!monster_habitable_grid(mons, env.grid(p)))
             continue;
         mgen_data mg(mons, BEH_COPY, p);
-        mg.set_summoned(&who, 3, 0);
+        mg.set_summoned(&who, 0, summ_dur(3));
         mg.place = BRANCH_ABYSS;
         if (create_monster(mg))
             return;

crawl-ref/source/actor.h
@@ -46,8 +46,10 @@ public:
     virtual bool      alive() const = 0;
 
     // Should return false for perma-summoned things.
-    virtual bool is_summoned(int* duration = nullptr,
-                             int* summon_type = nullptr) const = 0;
+    virtual bool is_summoned() const = 0;
+    virtual bool is_summoned_by(int summon_type) const = 0;
+    virtual bool is_summoned_by(const actor& summoner,
+                                int summon_type = SPELL_NO_SPELL) const = 0;
 
     virtual bool is_perm_summoned() const = 0;
 

crawl-ref/source/arena.cc
@@ -1145,7 +1145,7 @@ bool arena_veto_place_monster(const mgen_data &mg, bool first_band_member,
     // If the first band member makes it past the summon throttle cut,
     // let all of the rest of its band in too regardless of the summon
     // throttle.
-    if (mg.abjuration_duration > 0 && first_band_member)
+    if (mg.summon_duration > 0 && first_band_member)
     {
         if (mg.behaviour == BEH_FRIENDLY
             && arena::faction_a.active_members > arena::summon_throttle)
@@ -1222,7 +1222,7 @@ void arena_placed_monster(monster* mons)
         // Real summons drop corpses and items.
         if (arena::real_summons)
         {
-            mons->del_ench(ENCH_ABJ, true, false);
+            mons->del_ench(ENCH_SUMMON_TIMER, true, false);
             for (mon_inv_iterator ii(*mons); ii; ++ii)
                 ii->flags &= ~ISFLAG_SUMMONED;
         }

crawl-ref/source/art-func.h
@@ -564,7 +564,7 @@ static void _DEMON_AXE_melee_effects(item_def* /*item*/, actor* attacker,
             create_monster(
                 mgen_data(summon_any_demon(RANDOM_DEMON_COMMON),
                           SAME_ATTITUDE(mons), mons->pos(), mons->foe)
-                .set_summoned(mons, 6, SPELL_SUMMON_DEMON));
+                .set_summoned(mons, SPELL_SUMMON_DEMON, summ_dur(6)));
         }
         else if (!you.allies_forbidden())
         {
@@ -572,7 +572,7 @@ static void _DEMON_AXE_melee_effects(item_def* /*item*/, actor* attacker,
                                                   : random_demon_by_tier(4));
             if (create_monster(
                     mgen_data(type, BEH_COPY, you.pos(), MHITYOU, MG_FORCE_BEH | MG_AUTOFOE)
-                    .set_summoned(&you, 6, SPELL_SUMMON_DEMON, GOD_NO_GOD)))
+                    .set_summoned(&you, SPELL_SUMMON_DEMON, summ_dur(6))))
             {
                 mpr("A gate to Pandemonium opens briefly!");
             }
@@ -1814,7 +1814,7 @@ static void _ASMODEUS_melee_effects(item_def* /*weapon*/, actor* attacker,
 
         mgen_data mg(demon, BEH_FRIENDLY, you.pos(), MHITYOU,
                      MG_FORCE_BEH | MG_AUTOFOE);
-        mg.set_summoned(&you, 4, SPELL_FIRE_SUMMON);
+        mg.set_summoned(&you, SPELL_FIRE_SUMMON, summ_dur(4));
 
         if (create_monster(mg))
         {

crawl-ref/source/attitude-change.cc
@@ -147,7 +147,7 @@ void dismiss_god_summons(god_type god)
     for (monster_iterator mi; mi; ++mi)
     {
         if (is_follower(**mi)
-            && mi->has_ench(ENCH_ABJ)
+            && mi->is_summoned()
             && mons_is_god_gift(**mi, god))
         {
             // The monster disappears.

crawl-ref/source/beam.cc
@@ -2704,7 +2704,7 @@ void bolt::affect_endpoint()
                                             SAME_ATTITUDE(agent(true)->as_monster()),
                                             spot, agent(true)->as_monster()->foe,
                                             MG_FORCE_PLACE)
-                            .set_summoned(agent(true), 1, SPELL_FLASHING_BALESTRA, GOD_NO_GOD));
+                            .set_summoned(agent(true), SPELL_FLASHING_BALESTRA, summ_dur(1)));
 
             if (blade)
                 blade->add_ench(ENCH_MIGHT);
@@ -2735,7 +2735,7 @@ void bolt::affect_endpoint()
             bool obviousness; // dummy argument
             monster *mirror = clone_mons(blitzer, true, &obviousness,
                                          blitzer->attitude, spot);
-            mirror->mark_summoned(2, true, SPELL_PHANTOM_BLITZ);
+            mirror->mark_summoned(SPELL_PHANTOM_BLITZ, 2);
             mirror->summoner = blitzer->mid;
             mirror->foe = blitzer->foe;
             mirror->hit_points = blitzer->max_hit_points;
@@ -3075,7 +3075,7 @@ void bolt::affect_place_explosion_clouds()
 
             actor* summ = agent();
             mgen_data mg(MONS_FIRE_VORTEX, att, p, MHITNOT, MG_NONE, god);
-            mg.set_summoned(summ, 1, SPELL_FIRE_STORM);
+            mg.set_summoned(summ, SPELL_FIRE_STORM, summ_dur(1), false, false);
 
             // Spell-summoned monsters need to have a live summoner.
             if (summ == nullptr || !summ->alive())
@@ -5846,9 +5846,7 @@ bool bolt::ignores_monster(const monster* mon) const
     if ((flavour == BEAM_HAEMOCLASM || flavour == BEAM_BLOODRITE) && mon->friendly())
         return true;
 
-    int summon_type = 0;
-    mon->is_summoned(nullptr, &summon_type);
-    if (flavour == BEAM_QAZLAL && summon_type == MON_SUMM_AID)
+    if (flavour == BEAM_QAZLAL && mon->is_summoned_by(you, MON_SUMM_AID))
         return true;
 
     if (origin_spell == SPELL_UPHEAVAL && agent() && agent() == mon)
@@ -6664,7 +6662,7 @@ mon_resist_type bolt::apply_enchantment_to_monster(monster* mon)
         if (!monster_can_be_unravelled(*mon))
             return MON_UNAFFECTED;
 
-        if (mon->is_summoned())
+        if (mon->is_abjurable())
         {
             mprf("The magic binding %s to this plane unravels!",
                  mon->name(DESC_THE).c_str());

crawl-ref/source/cloud.cc
@@ -617,8 +617,8 @@ static void _handle_spectral_cloud(const cloud_struct& cloud)
                              (agent ? agent->foe : short{MHITYOU}),
                              MG_FORCE_PLACE)
                     .set_base(basetype)
-                    .set_summoned(actor_by_mid(cloud.source), 1,
-                                  SPELL_SPECTRAL_CLOUD));
+                    .set_summoned(actor_by_mid(cloud.source),
+                                  SPELL_SPECTRAL_CLOUD, summ_dur(1)));
 }
 
 void manage_clouds()
@@ -1005,10 +1005,8 @@ bool actor_cloud_immune(const actor &act, const cloud_struct &cloud)
         return true;
     }
 
-    int summon_type = 0;
-    act.is_summoned(nullptr, &summon_type);
     if (!player && have_passive(passive_t::cloud_immunity)
-        && (act.as_monster()->friendly() && summon_type == MON_SUMM_AID))
+        && act.is_summoned_by(MON_SUMM_AID))
     {
         return true;
     }
@@ -1959,7 +1957,7 @@ static const vector<chaos_effect> chaos_effects = {
 
             // The player shouldn't get new permanent followers from cloning.
             if (clone->attitude == ATT_FRIENDLY && !clone->is_summoned())
-                clone->mark_summoned(6, true, MON_SUMM_CLONE);
+                clone->mark_summoned(MON_SUMM_CLONE, 6);
             else
                 clone->flags |= (MF_NO_REWARD | MF_HARD_RESET);
 

commit 6e62d30407f3d2f2094e51038adbf5e2e92438a6

Don't dismiss eldritch tentacles on player attacks (Acrobat)
The previous behavior of 'getting angry at the player' was seemingly broken
by fb19e97 and unnoticed by me until now. More than most summons, the fact
that you may have to deal with a hostile monster in a random position is a
deliberate part of this spell and being able to avoid it by hitting the
tentacle first (perhaps to 'reroll' its placement), feels like incorrect
behavior. (Although, in most cases, this is unlikey to matter much.)

crawl-ref/source/mon-behv.cc
@@ -1109,6 +1109,7 @@ void behaviour_event(monster* mon, mon_event_type event, const actor *src,
         if (src == &you && mon->angered_by_attacks())
         {
             if (mon->attitude == ATT_FRIENDLY && mon->is_summoned()
+                && mon->type != MONS_ELDRITCH_TENTACLE
                 && !mon->is_child_monster() && !mons_is_tentacle_segment(mon->type))
             {
                 summon_dismissal_fineff::schedule(mon);

crawl-ref/source/timed-effects.cc
@@ -987,7 +987,7 @@ void timeout_malign_gateways(int duration)
                                          MHITNOT,
                                          MG_FORCE_PLACE,
                                          mmark->god);
-                mg.set_summoned(caster, 0);
+                mg.set_summoned(caster, SPELL_MALIGN_GATEWAY, 0, false, false);
                 if (!is_player)
                     mg.non_actor_summoner = mmark->summoner_string;
 