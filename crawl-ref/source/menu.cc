crawl/crawl

commit 7fc1e07ca948639001fe0f9a4a99276aec31b3a0

Change hotkey behavior in non-inventory menus
This restores something close to the pre-0.34 behavior for menus besides
non-paged 'i', with a hotkey selecting the closest matching entry, rather
than the first entry of the current category (the latter of which makes
sense when there are multiple categories with overlapping letters, but not
when there's a single category with so many letters that they can never be
all on screen at the same time, such as memorizing from a very large
library or examining many monsters on the screen at once).

This fixes #4700

crawl-ref/source/menu.cc
@@ -2180,8 +2180,7 @@ int Menu::get_first_visible(bool skip_init_headers, int col) const
 
 bool Menu::is_hotkey(int i, int key)
 {
-    bool ishotkey = items[i]->is_hotkey(key);
-    return ishotkey && (!is_set(MF_SELECT_BY_PAGE) || in_page(i));
+    return items[i]->is_hotkey(key);
 }
 
 /// find the first item (if any) that has hotkey `key`.
@@ -2192,39 +2191,66 @@ int Menu::hotkey_to_index(int key, bool primary_only)
     // Process all items, in case user hits hotkey for an
     // item not on the current page.
 
-    // We first check for the first matching hotkey, starting from the top of
+    // Depending on flags, we have one of two behaviors:
+    //
+    // If MF_SELECT_BY_CATEGORY is set (used for single-page inventory screen),
+    // we first check for the first matching hotkey, starting from the top of
     // the current menu subsection the cursor is in. If no match is found, we
     // next check from the start of the menu to the current position. (This
     // means that in cases where a menu has multiple entries with the same
     // letter, we will select one within the current subsection, if one exists,
     // and then check elsewhere if not.)
+    //
+    // If it is not, we simply select the nearest entry with a matching hotkey.
 
-    // First, determine the top of our current section.
-    int top = 0;
-    for (int i = last_hovered; i >= 0; --i)
+    if (is_set(MF_SELECT_BY_CATEGORY))
     {
-        if (items[i]->level != MEL_ITEM)
+        // First, determine the top of our current section.
+        int top = 0;
+        for (int i = last_hovered; i >= 0; --i)
         {
-            top = i;
-            break;
+            if (items[i]->level != MEL_ITEM)
+            {
+                top = i;
+                break;
+            }
+        }
+
+        for (int i = top; i < final; ++i)
+        {
+            if (is_hotkey(i, key)
+                && (!primary_only || items[i]->hotkeys[0] == key))
+            {
+                return i;
+            }
+        }
+        for (int i = 0; i < top; ++i)
+        {
+            if (is_hotkey(i, key)
+                && (!primary_only || items[i]->hotkeys[0] == key))
+            {
+                return i;
+            }
         }
     }
+    else
+    {
+        int nearest_index = -1;
+        int nearest_dist = INT_MAX;
+        for (int i = 0; i < (int)items.size(); ++i)
+        {
+            if (is_hotkey(i, key))
+            {
+                int dist = abs(i - last_hovered);
+                if (dist < nearest_dist)
+                {
+                    nearest_dist = dist;
+                    nearest_index = i;
+                }
+            }
+        }
 
-    for (int i = top; i < final; ++i)
-    {
-        if (is_hotkey(i, key)
-            && (!primary_only || items[i]->hotkeys[0] == key))
-        {
-            return i;
-        }
-    }
-    for (int i = 0; i < top; ++i)
-    {
-        if (is_hotkey(i, key)
-            && (!primary_only || items[i]->hotkeys[0] == key))
-        {
-            return i;
-        }
+        return nearest_index;
     }
 
     return -1;

crawl-ref/source/directn.cc
@@ -496,7 +496,7 @@ namespace
     public:
         DescMenu()
             : InvMenu(MF_SINGLESELECT | MF_ANYPRINTABLE
-                            | MF_ALLOW_FORMATTING | MF_SELECT_BY_PAGE
+                            | MF_ALLOW_FORMATTING
                             | MF_INIT_HOVER)
         { }
 

crawl-ref/source/invent.cc
@@ -1474,6 +1474,8 @@ void display_inventory()
     int flags = MF_SINGLESELECT | MF_ALLOW_FORMATTING | MF_SECONDARY_SCROLL;
     if (Options.show_paged_inventory)
         flags |= MF_PAGED_INVENTORY;
+    else
+        flags |= MF_SELECT_BY_CATEGORY;
 
     InvMenu menu(flags);
     menu.load_inv_items(Options.show_paged_inventory ? OSEL_GEAR : OSEL_ANY, -1);

crawl-ref/source/menu.h
@@ -261,8 +261,10 @@ enum MenuFlag
     MF_SELECT_QTY       = 0x00008,   ///< Allow partial selections by quantity
     MF_ANYPRINTABLE     = 0x00010,   ///< Any printable character is valid, and
                                      ///< closes the menu.
-    MF_SELECT_BY_PAGE   = 0x00020,   ///< Allow selections to occur only on
-                                     ///< currently-visible page.
+    MF_SELECT_BY_CATEGORY = 0x00020, ///< Hotkeys will select the first matching
+                                     ///< key within the currently-selected
+                                     ///< menu sub-category, rather than nearest
+                                     ///< entry with that hotkey.
     MF_INIT_HOVER       = 0x00040,   ///< Show the hover on initial display
     MF_WRAP             = 0x00080,   ///< Paging past the end will wrap back.
     MF_ALLOW_FILTER     = 0x00100,   ///< Control-F will ask for regex and

commit 15649fa8370902db03c70548e2954602c8ff44ca

Fix category multidrop hotkeys no longer working (Ge0FF)
Broken by 7fc1e07

crawl-ref/source/menu.cc
@@ -2239,7 +2239,8 @@ int Menu::hotkey_to_index(int key, bool primary_only)
         int nearest_dist = INT_MAX;
         for (int i = 0; i < (int)items.size(); ++i)
         {
-            if (is_hotkey(i, key))
+            if (is_hotkey(i, key)
+                && (!primary_only || items[i]->hotkeys[0] == key))
             {
                 int dist = abs(i - last_hovered);
                 if (dist < nearest_dist)