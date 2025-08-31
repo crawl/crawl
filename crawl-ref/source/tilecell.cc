a0c3d34337454a108dcf28c5d6be8d0a0f9b836d
Memoria <memoria.dcss@gmail.com>
2025-08-31 07:50:01 -0500

    Correctly draw shadows in levels with slimy walls
    Whenever there was at least one slimy wall anywhere in the level, all
    wall shadows everywhere would fail to draw, when the intent was to only
    remove shadows around the slimy walls to draw their corrosive border
    tiles instead. Besides being a visual bug also seen after Oozemancy, it
    also leaked information to tiles players on whether a Slime entry vault
    was on Lair:4 or Lair:5.
    
    It now uses similar logic to the visually similar Frozen Ramparts tile
    effects.
    
    Resolves #2668.

--- crawl-ref/source/tilecell.cc
+++ crawl-ref/source/tilecell.cc
@@ -522,7 +522,7 @@ void pack_cell_overlays(const coord_def &gc, crawl_view_buffer &vbuf)
         _pack_default_waves(gc, vbuf);
 
     if (env.level_state & LSTATE_SLIMY_WALL
-        && cell.map_knowledge.feat() != DNGN_SLIMY_WALL)
+        && cell.map_knowledge.flags & MAP_CORRODING)
     {
         _add_directional_overlays(gc, vbuf, TILE_SLIME_OVERLAY,
                                   _is_seen_slimy_wall);
