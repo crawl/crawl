500a117a725f580345567be3ece9d83acf7af41b
regret-index <cloudofregret@hotmail.com>
2025-09-14 22:13:37 -0230

    Vault review, latest earth monsters edition
    Alongside many minor changes and some Sprint adjustments, this commit also
    handles three new monsters' distributions:
    
     * Rock fish are placed somewhat sparingly in vaults, as there was not
       a lot of depth-appropriate vaults deploying rock worms or interesting
       vaults using big fish / giant goldfish in the first place. Most
       notably, they have a few Sewer appearances to help nudge the ends of
       difficulty over again. New vaults will most likely be needed to
       really explore the possibilities of this return of wall-walking
       monsters.
    
       * Note that for now, vaults directly placing wall-walking monsters
         inside walls must have non-solid features next to one of those tiles
         for the monster to place, though there are still plenty of
         possibilities to use in doing so.
    
     * Kobold geomancers appear in a few kobold vaults, mage vaults, and
       earth vaults alike.
    
     * Mountainshells mostly just appear in Lair vaults with earthen themes,
       as well as a few Lair vaults looking for the highest-tier monsters
       native to the branch. They also get some placements in Earth zigs
       (though without any zig-stacking scaling) over boulder beetles, since
       they're much more capable of being relevant at that point.
    
    The main other bit of note here is nerfing one of the Necropolis vaults
    using moths of wrath.

--- crawl-ref/source/dat/dlua/ziggurat.lua
+++ crawl-ref/source/dat/dlua/ziggurat.lua
@@ -376,7 +376,7 @@ mset(with_props(spec_fn(function ()
   local d = 20 + you.zigs_completed() * 5
   local e = 20 + you.zigs_completed() * 8
   local f = 20 + you.zigs_completed() * you.zigs_completed() * 3
-  return "gargoyle w:20 / earth elemental w:20 / boulder beetle w:20 / " ..
+  return "gargoyle w:20 / earth elemental w:20 / mountainshell w:20 / " ..
          "torpor snail w:" .. d .. " / iron golem w:" .. d .. " / " ..
          "war gargoyle w:" .. d .. " / stone giant w:" .. d .. " / " ..
          "caustic shrike w:" .. d .. " / entropy weaver w:" .. d .. " / " ..
