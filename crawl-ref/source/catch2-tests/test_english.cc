12e3b08ebcaa00e0911cf98a03c0474f327e1101
David Lawrence Ramsey <pooka109@gmail.com>
2025-09-14 21:48:49 -0500

    Update pluralisation tests.
    Orc skeletons are no longer a thing, and neither are cockroaches.

--- crawl-ref/source/catch2-tests/test_english.cc
+++ crawl-ref/source/catch2-tests/test_english.cc
@@ -10,10 +10,10 @@ TEST_CASE( "English stuff works", "[single-file]" ) {
 
 TEST_CASE( "pluralise_monsters", "[single-file]" ) {
     REQUIRE( pluralise_monster("orc") == "orcs" );
-    REQUIRE( pluralise_monster("orc skeleton") == "orc skeletons" );
+    REQUIRE( pluralise_monster("orc zombie") == "orc zombies" );
     REQUIRE( pluralise_monster("merfolk") == "merfolk" );
     REQUIRE( pluralise_monster("jelly") == "jellies" );
-    REQUIRE( pluralise_monster("cockroach") == "cockroaches" );
+    REQUIRE( pluralise_monster("clockroach") == "clockroaches" );
     REQUIRE( pluralise_monster("bush") == "bushes" );
     REQUIRE( pluralise_monster("guardian sphinx") == "guardian sphinxes" );
     REQUIRE( pluralise_monster("elf") == "elves" );
