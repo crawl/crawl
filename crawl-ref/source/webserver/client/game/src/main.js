e2bb4dc2b526a4a913937b61777037a14c194341
mumra <pete.hurst@gmail.com>
2025-10-19 09:06:12 +0100

    Move static JavaScript to /src folders for bundling
    This moves all the static .js from webtiles into the appropriate
    project src file. This commit literally just moves the files so that
    git can track the history of the files and (hopefully) help resolve
    conflicts if any other developers have touched these files.

---
+++ crawl-ref/source/webserver/client/game/src/main.js
@@ -0,0 +1,26 @@
+// Bundling styles didn't work, because there's no good way to *unload* them
+// when we switch game client. This could probably be handled properly but for
+// now including them with plain old html is the path of least resistance.
+// import "./style.css";
+// import "./simplebar.css";
+import "./game";
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+//     </a>
+//     <h1>Vite + TypeScript</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite and TypeScript logos to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
