crawl/crawl

commit b2c792946cc853f54898f332c9bb498ebb7c14be

Remove a no longer needed tile cache
We had a cache for tiles of squares in view and another one for all
other tiles. This allowed us to easily set tiles to unseen as they
exited view in the Labyrinth branch by never writing them to the out of
view tile cache (Labyrinth no longer exists), replace mcache reference
tiles for monsters out of view with generic ones (but the bug that
caused us to do this is gone and we no longer do this), and support the
clean_map option (which no longer exists). As none of these reasons for
keeping the two separate caches exist anymore it should be fine to just
use one cache for all tiles.

The cache for tiles in view stored tiles within a radius of 8 from the
player and was indexed with coordinates that were relative to the
player. This meant it would become invalid after the player moved if it
wasn't updated and was sometimes leading to a crash in webtiles when
drawing parchments. Using a single cache indexed by grid coordinates
should fix this.

crawl-ref/source/tileview.cc
@@ -917,19 +917,23 @@ static tileidx_t _get_floor_bg(const coord_def& gc)
 
 void tile_draw_floor()
 {
-    for (int cy = 0; cy < tile_env.fg.height(); cy++)
-        for (int cx = 0; cx < tile_env.fg.width(); cx++)
+    tile_env.icons.clear();
+
+    for (int cy = 0; cy < ENV_SHOW_DIAMETER; cy++)
+        for (int cx = 0; cx < ENV_SHOW_DIAMETER; cx++)
         {
             const coord_def ep(cx, cy);
             const coord_def gc = show2grid(ep);
 
+            if (!map_bounds(gc))
+                continue;
+
             tileidx_t bg = _get_floor_bg(gc);
 
             // init tiles
-            tile_env.bg(ep) = bg;
-            tile_env.fg(ep) = 0;
-            tile_env.cloud(ep) = 0;
-            tile_env.icons.erase(ep);
+            tile_env.bk_bg(gc) = bg;
+            tile_env.bk_fg(gc) = 0;
+            tile_env.bk_cloud(gc) = 0;
         }
 }
 
@@ -951,43 +955,18 @@ static void _tile_place_item(const coord_def &gc, const item_def &item,
     if (more_items)
         t |= TILE_FLAG_S_UNDER;
 
-    if (you.see_cell(gc))
-    {
-        const coord_def ep = crawl_view.grid2show(gc);
-        if (tile_env.fg(ep))
-            return;
+    tile_env.bk_fg(gc) = t;
 
-        tile_env.fg(ep) = t;
-
-        if (item_needs_autopickup(item))
-            tile_env.bg(ep) |= TILE_FLAG_CURSOR3;
-    }
-    else
-    {
-        tile_env.bk_fg(gc) = t;
-
-        if (item_needs_autopickup(item))
-            tile_env.bk_bg(gc) |= TILE_FLAG_CURSOR3;
-    }
+    if (item_needs_autopickup(item))
+        tile_env.bk_bg(gc) |= TILE_FLAG_CURSOR3;
 }
 
 static void _tile_place_item_marker(const coord_def &gc, const item_def &item)
 {
-    if (you.see_cell(gc))
-    {
-        const coord_def ep = crawl_view.grid2show(gc);
-        tile_env.fg(ep) |= TILE_FLAG_S_UNDER;
+    tile_env.bk_fg(gc) = ((tileidx_t)tile_env.bk_fg(gc)) | TILE_FLAG_S_UNDER;
 
-        if (item_needs_autopickup(item))
-            tile_env.bg(ep) |= TILE_FLAG_CURSOR3;
-    }
-    else
-    {
-        tile_env.bk_fg(gc) = ((tileidx_t) tile_env.bk_fg(gc)) | TILE_FLAG_S_UNDER;
-
-        if (item_needs_autopickup(item))
-            tile_env.bk_bg(gc) |= TILE_FLAG_CURSOR3;
-    }
+    if (item_needs_autopickup(item))
+        tile_env.bk_bg(gc) |= TILE_FLAG_CURSOR3;
 }
 
 /**
@@ -1004,12 +983,9 @@ static void _tile_place_invisible_monster(const coord_def &gc)
     // see tileidx_feature
     // That tile is hidden by clouds though
     if (cell.feat() != DNGN_SHALLOW_WATER || cell.cloud() != CLOUD_NONE)
-    {
-        if (you.see_cell(gc))
-            tile_env.fg(ep) = TILE_UNSEEN_MONSTER;
-        else
-            tile_env.bk_fg(gc) = TILE_UNSEEN_MONSTER;
-    }
+        tile_env.bk_fg(gc) = TILE_UNSEEN_MONSTER;
+    else
+        tile_env.bk_fg(gc) = 0;
 
     if (env.map_knowledge(gc).item())
         _tile_place_item_marker(gc, *env.map_knowledge(gc).item());
@@ -1017,8 +993,6 @@ static void _tile_place_invisible_monster(const coord_def &gc)
 
 static void _tile_place_monster(const coord_def &gc, const monster_info& mon)
 {
-    const coord_def ep = grid2show(gc);
-
     tileidx_t t    = tileidx_monster(mon);
     tileidx_t t0   = t & TILE_FLAG_MASK;
     tileidx_t flag = t & (~TILE_FLAG_MASK);
@@ -1032,12 +1006,7 @@ static void _tile_place_monster(const coord_def &gc, const monster_info& mon)
             t |= TILE_FLAG_S_UNDER;
 
             if (item_needs_autopickup(*env.map_knowledge(gc).item()))
-            {
-                if (you.see_cell(gc))
-                    tile_env.bg(ep) |= TILE_FLAG_CURSOR3;
-                else
-                    tile_env.bk_bg(gc) |= TILE_FLAG_CURSOR3;
-            }
+                tile_env.bk_bg(gc) |= TILE_FLAG_CURSOR3;
         }
     }
     else
@@ -1046,15 +1015,12 @@ static void _tile_place_monster(const coord_def &gc, const monster_info& mon)
         t = flag | (mcache_idx ? mcache_idx : t0);
     }
 
+    tile_env.bk_fg(gc) = t;
     if (!you.see_cell(gc))
-    {
-        tile_env.bk_fg(gc) = t;
         return;
-    }
-    tile_env.fg(ep) = t;
     set<tileidx_t> status_icons = status_icons_for(mon);
     if (!status_icons.empty())
-        tile_env.icons[ep] = std::move(status_icons);
+        tile_env.icons[gc] = std::move(status_icons);
 
     // Add name tags.
     if (!mons_class_gives_xp(mon.type))
@@ -1097,13 +1063,7 @@ void tile_reset_fg(const coord_def &gc)
 
 static void _tile_place_cloud(const coord_def &gc, const cloud_info &cl)
 {
-    if (you.see_cell(gc))
-    {
-        const coord_def ep = grid2show(gc);
-        tile_env.cloud(ep) = tileidx_cloud(cl);
-    }
-    else
-        tile_env.bk_cloud(gc) = tileidx_cloud(cl);
+    tile_env.bk_cloud(gc) = tileidx_cloud(cl);
 }
 
 void tile_draw_map_cell(const coord_def& gc, bool foreground_only)
@@ -1112,12 +1072,7 @@ void tile_draw_map_cell(const coord_def& gc, bool foreground_only)
         tile_env.bk_bg(gc) = _get_floor_bg(gc);
 
     if (you.see_cell(gc))
-    {
-        const coord_def ep = grid2show(gc);
-        tile_env.fg(ep) = 0;
-        tile_env.cloud(ep) = 0;
-        tile_env.icons.erase(ep);
-    }
+        tile_env.icons.erase(gc);
 
     const map_cell& cell = env.map_knowledge(gc);
 

crawl-ref/source/tile-env.h
@@ -15,15 +15,9 @@ struct crawl_tile_environment
     FixedArray<tile_fg_store, GXM, GYM> bk_fg;
     FixedArray<tileidx_t, GXM, GYM> bk_bg;
     FixedArray<tileidx_t, GXM, GYM> bk_cloud;
-#endif
-    FixedArray<tile_flavour, GXM, GYM> flv;
-    // indexed by (show-1) coords
-#ifdef USE_TILE // TODO: separate out this stuff from crawl_environment
-    FixedArray<tileidx_t, ENV_SHOW_DIAMETER, ENV_SHOW_DIAMETER> fg;
-    FixedArray<tileidx_t, ENV_SHOW_DIAMETER, ENV_SHOW_DIAMETER> bg;
-    FixedArray<tileidx_t, ENV_SHOW_DIAMETER, ENV_SHOW_DIAMETER> cloud;
     map<coord_def, set<tileidx_t>> icons;
 #endif
+    FixedArray<tile_flavour, GXM, GYM> flv;
     tile_flavour default_flavour;
     std::vector<std::string> names;
 };

crawl-ref/source/tilepick.cc
@@ -1789,8 +1789,7 @@ static void _add_tentacle_overlay(const coord_def pos,
     if (!in_bounds(next))
         return;
 
-    const coord_def next_showpos(grid2show(next));
-    if (!show_bounds(next_showpos))
+    if (!show_bounds(grid2show(next)))
         return;
 
     tile_flags flag;
@@ -1802,7 +1801,7 @@ static void _add_tentacle_overlay(const coord_def pos,
         case main_dir::west: flag = TILE_FLAG_TENTACLE_SE; break;
         default: die("invalid direction");
     }
-    tile_env.bg(next_showpos) |= flag;
+    tile_env.bk_bg(next) |= flag;
 
     switch (type)
     {
@@ -1814,7 +1813,7 @@ static void _add_tentacle_overlay(const coord_def pos,
         case tentacle_type::spectral_kraken: flag = TILE_FLAG_TENTACLE_SPECTRAL_KRAKEN; break;
         default: flag = TILE_FLAG_TENTACLE_KRAKEN;
     }
-    tile_env.bg(next_showpos) |= flag;
+    tile_env.bk_bg(next) |= flag;
 }
 
 static void _handle_tentacle_overlay(const coord_def pos,

crawl-ref/source/tilereg-dgn.cc
@@ -760,7 +760,8 @@ bool DungeonRegion::update_tip_text(string &tip)
                 tip += make_stringf("HEIGHT(%d)\n", dgn_height_at(gc));
 
             tip += "\n";
-            tip += tile_debug_string(tile_env.fg(ep), tile_env.bg(ep), ' ');
+            tip += tile_debug_string(tile_env.bk_fg(gc), tile_env.bk_bg(gc),
+                                     ' ');
             tip += "\n";
         }
         else

crawl-ref/source/tilereg-mon.cc
@@ -171,15 +171,14 @@ void MonsterRegion::pack_buffers()
             if (mon)
             {
                 const coord_def gc = mon->pos;
-                const coord_def ep = grid2show(gc);
 
                 if (crawl_view.in_los_bounds_g(gc))
                 {
                     packed_cell cell;
-                    cell.fg = tile_env.fg(ep);
-                    cell.bg = tile_env.bg(ep);
+                    cell.fg = tile_env.bk_fg(gc);
+                    cell.bg = tile_env.bk_bg(gc);
                     cell.flv = tile_env.flv(gc);
-                    if (set<tileidx_t>* icons = map_find(tile_env.icons, ep))
+                    if (set<tileidx_t>* icons = map_find(tile_env.icons, gc))
                         cell.icons = *icons;
                     tile_apply_properties(gc, cell);
 

crawl-ref/source/view.cc
@@ -1136,16 +1136,6 @@ static update_flags player_view_update_at(const coord_def &gc)
         env.pgrid(gc) |= FPROP_SEEN_OR_NOEXP;
     }
 
-#ifdef USE_TILE
-    const coord_def ep = grid2show(gc);
-
-    // We remove any references to mcache when
-    // writing to the background.
-    tile_env.bk_fg(gc) = tile_env.fg(ep);
-    tile_env.bk_bg(gc) = tile_env.bg(ep);
-    tile_env.bk_cloud(gc) = tile_env.cloud(ep);
-#endif
-
     return ret;
 }
 
@@ -1187,8 +1177,7 @@ static void _draw_out_of_bounds(screen_cell_t *cell)
 #endif
 }
 
-static void _draw_outside_los(screen_cell_t *cell, const coord_def &gc,
-                                    const coord_def &ep)
+static void _draw_outside_los(screen_cell_t *cell, const coord_def &gc)
 {
     // Outside the env.show area.
     cglyph_t g = get_cell_glyph(gc);
@@ -1198,16 +1187,14 @@ static void _draw_outside_los(screen_cell_t *cell, const coord_def &gc,
 #ifdef USE_TILE
     // this is just for out-of-los rays, but I don't see a more efficient way..
     if (in_bounds(gc))
-        cell->tile.bg = tile_env.bg(ep);
+        cell->tile.bg = tile_env.bk_bg(gc);
 
     tileidx_out_of_los(&cell->tile.fg, &cell->tile.bg, &cell->tile.cloud, gc);
-#else
-    UNUSED(ep);
 #endif
 }
 
 static void _draw_player(screen_cell_t *cell,
-                         const coord_def &gc, const coord_def &ep,
+                         const coord_def &gc,
                          bool anim_updates)
 {
     // Player overrides everything in cell.
@@ -1228,19 +1215,19 @@ static void _draw_player(screen_cell_t *cell,
     cell->colour = real_colour(cell->colour);
 
 #ifdef USE_TILE
-    cell->tile.fg = tile_env.fg(ep) = tileidx_player();
-    cell->tile.bg = tile_env.bg(ep);
-    cell->tile.cloud = tile_env.cloud(ep);
+    cell->tile.fg = tileidx_player();
+    cell->tile.bg = tile_env.bk_bg(gc);
+    cell->tile.cloud = tile_env.bk_cloud(gc);
     cell->tile.icons = status_icons_for_player();
     if (anim_updates)
         tile_apply_animations(cell->tile.bg, &tile_env.flv(gc));
 #else
-    UNUSED(ep, anim_updates);
+    UNUSED(anim_updates);
 #endif
 }
 
 static void _draw_los(screen_cell_t *cell,
-                      const coord_def &gc, const coord_def &ep,
+                      const coord_def &gc,
                       bool anim_updates)
 {
     cglyph_t g = get_cell_glyph(gc);
@@ -1248,15 +1235,15 @@ static void _draw_los(screen_cell_t *cell,
     cell->colour = g.col;
 
 #ifdef USE_TILE
-    cell->tile.fg = tile_env.fg(ep);
-    cell->tile.bg = tile_env.bg(ep);
-    cell->tile.cloud = tile_env.cloud(ep);
-    if (set<tileidx_t>* icons = map_find(tile_env.icons, ep))
+    cell->tile.fg = tile_env.bk_fg(gc);
+    cell->tile.bg = tile_env.bk_bg(gc);
+    cell->tile.cloud = tile_env.bk_cloud(gc);
+    if (set<tileidx_t>* icons = map_find(tile_env.icons, gc))
         cell->tile.icons = *icons;
     if (anim_updates)
         tile_apply_animations(cell->tile.bg, &tile_env.flv(gc));
 #else
-    UNUSED(ep, anim_updates);
+    UNUSED(anim_updates);
 #endif
 }
 
@@ -1728,23 +1715,22 @@ void draw_cell(screen_cell_t *cell, const coord_def &gc,
 #ifdef USE_TILE
     cell->tile.clear();
 #endif
-    const coord_def ep = grid2show(gc);
 
     if (!map_bounds(gc))
         _draw_out_of_bounds(cell);
     else if (!crawl_view.in_los_bounds_g(gc))
-        _draw_outside_los(cell, gc, coord_def());
+        _draw_outside_los(cell, gc);
     else if (gc == you.pos() && you.on_current_level
              && _layers & Layer::PLAYER
              && !crawl_state.game_is_arena()
              && !crawl_state.arena_suspended)
     {
-        _draw_player(cell, gc, ep, anim_updates);
+        _draw_player(cell, gc, anim_updates);
     }
     else if (you.see_cell(gc))
-        _draw_los(cell, gc, ep, anim_updates);
+        _draw_los(cell, gc, anim_updates);
     else
-        _draw_outside_los(cell, gc, ep); // in los bounds but not visible
+        _draw_outside_los(cell, gc); // in los bounds but not visible
 
 #ifdef USE_TILE
     cell->tile.map_knowledge = map_bounds(gc) ? env.map_knowledge(gc) : map_cell();

commit 87cdf11214e9d7e29150c914f4cee2779781a510

Fix clearing some remembered foreground tiles
Remembered monster and cloud tiles would be incorrectly cleared for
squares in LOS range that weren't in LOS. This was broken in
commit b2c7929

crawl-ref/source/tileview.cc
@@ -925,7 +925,7 @@ void tile_draw_floor()
             const coord_def ep(cx, cy);
             const coord_def gc = show2grid(ep);
 
-            if (!map_bounds(gc))
+            if (!you.see_cell(gc))
                 continue;
 
             tileidx_t bg = _get_floor_bg(gc);