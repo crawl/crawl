crawl/crawl

commit c485ba435ccddd4165712bbd07a31c45092752b2

Refactor and simplify how monsters follow the player across stairs
The code that handled determining which monsters would follow with the
player when they took stairs or transporters had some opaque and
complicated behavior which may have once served an important purpose, but
enough of the game has changed around it that I think a simpler and more
intuitive method could be better.

Namely: friendly monsters could follow the player from up to 5 tiles away
from the staircase, but only so long as they were connected in a contiguous
chain, and *only* if there was not a single non-friendly monster adjacent
to the player at the same time (at which point the max distance allies
could be pulled from was 1). Note that 'non-friendly monster' in this case
included PLANTS. So merely being near a plant would cause you to leave
allies behind which you'd normally expect to follow you.

Note also that this chain didn't care about line of sight, so you could
pull along things around multiple corners, just so long as they were all
touching each other.

There was additional behavior of flagging monsters as MF_TAKING_STAIRS even
when we knew they *couldn't* take stairs, just so they could count as links
in that chain to allow access to stair-taking allies who might be behind
them, and needing to juggle separate return values for whether that flag
*really* meant they were taking the stairs or not. (Also, if one of them
broke the chain during transit, the rest of your allies wouldn't arrive...)

The original stated purpose of this code was to prevent zombies and summons
from blocking perma-allies behind them from travelling with the player, but
in the time since it was written, interlevel-recall was added and most
perma-allies removed from the game. There just isn't a compelling need for
this additional complexity anymore, in my opinion.

The new logic is: stair-using allies can follow from 3 tiles away (so long
as the player can see them) and adjacencies don't matter. MF_TAKING_STAIRS

crawl-ref/source/files.cc
@@ -1187,34 +1187,8 @@ static void _clear_env_map()
     env.map_forgotten.reset();
 }
 
-static bool _grab_follower_at(const coord_def &pos)
+static void _grab_follower(monster* fol)
 {
-    if (pos == you.pos())
-        return false;
-
-    monster* fol = monster_at(pos);
-    if (!fol || !fol->alive() || fol->incapacitated())
-        return false;
-
-    // Only friendlies can follow the player through portals
-    if (!fol->friendly() && (!is_connected_branch(you.where_are_you)
-                            || you.where_are_you == BRANCH_PANDEMONIUM))
-    {
-        return false;
-    }
-
-    // The monster has to already be tagged in order to follow.
-    if (!testbits(fol->flags, MF_TAKING_STAIRS))
-        return false;
-
-    // If a monster that can't use stairs was marked as a follower,
-    // it's because it's an ally and there might be another ally
-    // behind it that might want to push through.
-    // This means we don't actually send it on transit, but we do
-    // return true, so adjacent real followers are handled correctly. (jpeg)
-    if (!mons_can_use_stairs(*fol))
-        return true;
-
     level_id dest = level_id::current();
 
     dprf("%s is following to %s.", fol->name(DESC_THE, true).c_str(),
@@ -1224,11 +1198,39 @@ static bool _grab_follower_at(const coord_def &pos)
     fol->destroy_inventory();
     monster_cleanup(fol);
     if (could_see)
-        view_update_at(pos);
-    return true;
+        view_update_at(fol->pos());
 }
 
-static void _grab_followers()
+// Expire all friendly summons / zombies / etc. when the player is leaving a floor.
+static void _expire_temporary_allies()
+{
+    for (auto &mons : menv_real)
+    {
+        if (!mons.alive())
+            continue;
+
+        if (mons.type == MONS_BATTLESPHERE)
+            end_battlesphere(&mons, false);
+        else if (mons.type == MONS_SPECTRAL_WEAPON)
+            end_spectral_weapon(&mons, false);
+        else if (mons.type == MONS_INUGAMI)
+            check_canid_farewell(mons, false);
+        else if (mons.friendly()
+                && (mons.is_summoned() || mons.has_ench(ENCH_FAKE_ABJURATION))
+                    && !mons.is_perm_summoned())
+        {
+            monster_die(mons, KILL_RESET, NON_MONSTER, true);
+        }
+        // Yred & animate dead zombies crumble on floor change
+        else if (mons.has_ench(ENCH_SUMMON)
+                    && mons.get_ench(ENCH_SUMMON).degree == SPELL_ANIMATE_DEAD)
+        {
+            monster_die(mons, KILL_RESET, NON_MONSTER, true);
+        }
+    }
+}
+
+static void _grab_followers_and_expire_summons()
 {
     int non_stair_using_allies = 0;
     int non_stair_using_undead = 0;
@@ -1237,12 +1239,23 @@ static void _grab_followers()
     monster* dowan = nullptr;
     monster* duvessa = nullptr;
 
-    // Handle some hacky cases
-    for (adjacent_iterator ai(you.pos()); ai; ++ai)
+    // Do a first pass to account for Dowan/Duvessa refusing to leave a floor
+    // without the other, as well as printing a message if the player is leaving
+    // summoned allies behind.
+    for (radius_iterator ri(you.pos(), 3, C_SQUARE, LOS_NO_TRANS, true); ri; ++ri)
     {
-        monster* fol = monster_at(*ai);
+        monster* fol = monster_at(*ri);
         if (fol == nullptr)
             continue;
+
+        // Hostile monsters can only follow while adjacent and incapacitated
+        // monsters cannot follow at all. (We already checked this when flagging
+        // them, but it may have changed in the meantime)
+        if (!fol->alive() || fol->incapacitated()
+            || (!fol->friendly() && grid_distance(*ri, you.pos()) > 1))
+        {
+            fol->flags &= ~MF_TAKING_STAIRS;
+        }
 
         if (mons_is_mons_class(fol, MONS_DUVESSA) && fol->alive())
             duvessa = fol;
@@ -1250,7 +1263,8 @@ static void _grab_followers()
         if (mons_is_mons_class(fol, MONS_DOWAN) && fol->alive())
             dowan = fol;
 
-        if (fol->wont_attack() && !mons_can_use_stairs(*fol))
+        // Note friendlies that are being left behind because they can't take stairs.
+        if (fol->friendly() && !mons_can_use_stairs(*fol))
         {
             if (!mons_is_conjured(fol->type))
                 non_stair_using_allies++;
@@ -1300,27 +1314,22 @@ static void _grab_followers()
         }
     }
 
-    bool visited[GXM][GYM];
-    memset(&visited, 0, sizeof(visited));
+    // Kill friendly summons before moving off-floor.
+    // (Helps with bookkeeping in some cases).
+    _expire_temporary_allies();
 
-    vector<coord_def> places[2] = { { you.pos() }, {} };
-    int place_set = 0;
-    while (!places[place_set].empty())
+    for (radius_iterator ri(you.pos(), 3, C_SQUARE, LOS_NO_TRANS, true); ri; ++ri)
     {
-        for (const coord_def &p : places[place_set])
-        {
-            for (adjacent_iterator ai(p); ai; ++ai)
-            {
-                if (visited[ai->x][ai->y])
-                    continue;
+        monster* fol = monster_at(*ri);
+        if (!fol)
+            continue;
 
-                visited[ai->x][ai->y] = true;
-                if (_grab_follower_at(*ai))
-                    places[!place_set].push_back(*ai);
-            }
-        }
-        places[place_set].clear();
-        place_set = !place_set;
+        // Only grab monsters who were flagged when the player started their
+        // stair delay.
+        if (!testbits(fol->flags, MF_TAKING_STAIRS))
+            continue;
+
+        _grab_follower(fol);
     }
 
     // Clear flags of monsters that didn't follow.
@@ -1330,35 +1339,6 @@ static void _grab_followers()
             continue;
 
         mons.flags &= ~MF_TAKING_STAIRS;
-    }
-}
-
-// Expire all friendly summons / zombies / etc. when the player is leaving a floor.
-static void _expire_temporary_allies()
-{
-    for (auto &mons : menv_real)
-    {
-        if (!mons.alive())
-            continue;
-
-        if (mons.type == MONS_BATTLESPHERE)
-            end_battlesphere(&mons, false);
-        else if (mons.type == MONS_SPECTRAL_WEAPON)
-            end_spectral_weapon(&mons, false);
-        else if (mons.type == MONS_INUGAMI)
-            check_canid_farewell(mons, false);
-        else if (mons.friendly()
-                && (mons.is_summoned() || mons.has_ench(ENCH_FAKE_ABJURATION))
-                    && !mons.is_perm_summoned())
-        {
-            monster_die(mons, KILL_RESET, NON_MONSTER, true);
-        }
-        // Yred & animate dead zombies crumble on floor change
-        else if (mons.has_ench(ENCH_SUMMON)
-                    && mons.get_ench(ENCH_SUMMON).degree == SPELL_ANIMATE_DEAD)
-        {
-            monster_die(mons, KILL_RESET, NON_MONSTER, true);
-        }
     }
 }
 
@@ -2164,10 +2144,7 @@ bool load_level(dungeon_feature_type stair_taken, load_mode_type load_mode,
         if (old_level.depth != -1)
         {
             if (!crawl_state.game_is_descent())
-            {
-                _grab_followers();
-                _expire_temporary_allies();
-            }
+                _grab_followers_and_expire_summons();
 
             if (env.level_state & LSTATE_DELETED)
                 delete_level(old_level), dprf("<lightmagenta>Deleting level.</lightmagenta>");

crawl-ref/source/mon-transit.cc
@@ -383,14 +383,6 @@ void follower::read_from_prop(CrawlVector& vec)
     transit_start_time = vec[NUM_MONSTER_SLOTS + 1].get_int();
 }
 
-static bool _is_religious_follower(const monster &mon)
-{
-    return (you_worship(GOD_YREDELEMNUL)
-            || will_have_passive(passive_t::convert_orcs)
-            || you_worship(GOD_FEDHAS))
-                && is_follower(mon);
-}
-
 static bool _mons_can_follow_player_from(const monster &mons,
                                          const coord_def from,
                                          bool within_level = false)
@@ -421,23 +413,18 @@ static bool _mons_can_follow_player_from(const monster &mons,
     if (!mons.friendly() && (mons.pos() - from).rdist() > 1)
         return false;
 
-    // Monsters that can't use stairs can still be marked as followers
-    // (though they'll be ignored for transit), so any adjacent real
-    // follower can follow through. (jpeg)
+    // Finally, check whether it is possible for the monster to actually use
+    // the same method of transit the player is.
     if (within_level && !mons_class_can_use_transporter(mons.type)
         || !within_level && !mons_can_use_stairs(mons, env.grid(from)))
     {
-        if (_is_religious_follower(mons))
-            return true;
-
         return false;
     }
     return true;
 }
 
 // Tag any monster following the player
-static bool _tag_follower_at(const coord_def &pos, const coord_def &from,
-                             bool &real_follower)
+static bool _tag_follower_at(const coord_def &pos, const coord_def &from)
 {
     if (!in_bounds(pos) || pos == from)
         return false;
@@ -449,7 +436,6 @@ static bool _tag_follower_at(const coord_def &pos, const coord_def &from,
     if (!_mons_can_follow_player_from(*fol, from))
         return false;
 
-    real_follower = true;
     fol->flags |= MF_TAKING_STAIRS;
 
     // Clear patrolling/travel markers.
@@ -462,69 +448,27 @@ static bool _tag_follower_at(const coord_def &pos, const coord_def &from,
     return true;
 }
 
-static int _follower_tag_radius(const coord_def &from)
-{
-    // If only friendlies are adjacent, we set a max radius of 5, otherwise
-    // only adjacent friendlies may follow.
-    for (adjacent_iterator ai(from); ai; ++ai)
-    {
-        if (const monster* mon = monster_at(*ai))
-            if (!mon->friendly())
-                return 1;
-    }
-
-    return 5;
-}
-
 /**
- * Handle movement of adjacent player followers from a given location. This is
- * used when traveling through stairs or a transporter.
+ * Handle movement of monsters following the player across stairs or transporters
  *
  * @param from       The location from which the player moved.
  * @param handler    A handler function that does movement of the actor to the
- *                   destination, returning true if the actor was friendly. The
- *                   `real` argument tracks whether the actor was an actual
- *                   follower that counts towards the follower limit.
+ *                   destination, returning true if the actor was (or will be)
+ *                   moved.
  **/
 void handle_followers(const coord_def &from,
                       bool (*handler)(const coord_def &pos,
-                                      const coord_def &from, bool &real))
+                                      const coord_def &from))
 {
-    const int radius = _follower_tag_radius(from);
     int n_followers = 18;
-
-    vector<coord_def> places[2];
-    int place_set = 0;
-
-    bool visited[GXM][GYM];
-    memset(&visited, 0, sizeof(visited));
-
-    places[place_set].push_back(from);
-    while (!places[place_set].empty())
+    for (radius_iterator ri(from, 3, C_SQUARE, LOS_NO_TRANS, true); ri; ++ri)
     {
-        for (const coord_def &p : places[place_set])
+        if (handler(*ri, from))
         {
-            for (adjacent_iterator ai(p); ai; ++ai)
-            {
-                if ((*ai - from).rdist() > radius
-                    || visited[ai->x][ai->y])
-                {
-                    continue;
-                }
-                visited[ai->x][ai->y] = true;
-
-                bool real_follower = false;
-                if (handler(*ai, from, real_follower))
-                {
-                    // If we've run out of our follower allowance, bail.
-                    if (real_follower && --n_followers <= 0)
-                        return;
-                    places[!place_set].push_back(*ai);
-                }
-            }
+            // If we've run out of our follower allowance, bail.
+            if (--n_followers <= 0)
+                return;
         }
-        places[place_set].clear();
-        place_set = !place_set;
     }
 }
 
@@ -545,8 +489,7 @@ void untag_followers()
         mons.flags &= ~MF_TAKING_STAIRS;
 }
 
-static bool _transport_follower_at(const coord_def &pos, const coord_def &from,
-                                   bool &real_follower)
+static bool _transport_follower_at(const coord_def &pos, const coord_def &from)
 {
     if (!in_bounds(pos) || pos == from)
         return false;
@@ -564,12 +507,13 @@ static bool _transport_follower_at(const coord_def &pos, const coord_def &from,
     // Guantlet and Beogh in particular.
     if (fol->find_place_to_live(true, fol->friendly()))
     {
-        real_follower = true;
         env.map_knowledge(pos).clear_monster();
         dprf("%s is transported.", fol->name(DESC_THE, true).c_str());
+
+        return true;
     }
 
-    return true;
+    return false;
 }
 
 /**

crawl-ref/source/mon-transit.h
@@ -60,7 +60,7 @@ void place_transiting_monsters();
 void place_followers();
 void handle_followers(const coord_def &from,
                       bool (*handler)(const coord_def &pos,
-                                      const coord_def &from, bool &real));
+                                      const coord_def &from));
 void tag_followers();
 void untag_followers();
 void transport_followers_from(const coord_def &from);

crawl-ref/source/mon-util.cc
@@ -1832,6 +1832,15 @@ bool mons_can_use_stairs(const monster& mon, dungeon_feature_type stair)
         return false;
     }
 
+    // If this is the entrance to a portal vault (or another region of Pandemonium)
+    // only friendly monsters can traverse this.
+    if (!mon.friendly()
+        && (feat_is_portal_entrance(stair) || stair == DNGN_TRANSIT_PANDEMONIUM
+                                           || stair == DNGN_ENTER_PANDEMONIUM))
+    {
+        return false;
+    }
+
     // Everything else is fine
     return true;
 }

commit a6dda02a3e1e309ab83abc8b4ddf6ce22808e03e

Don't leave afterimages of monsters that followed the player across stairs
Accidentally broken by c485ba435cc , memories of monsters that followed
the player between floors would still be displayed if you peeked back at
the floor they'd followed you from.

crawl-ref/source/files.cc
@@ -1200,12 +1200,13 @@ static void _grab_follower(monster* fol)
 
     dprf("%s is following to %s.", fol->name(DESC_THE, true).c_str(),
          dest.describe().c_str());
-    bool could_see = you.can_see(*fol);
+    const bool could_see = you.can_see(*fol);
+    const coord_def old_pos = fol->pos();
     fol->set_transit(dest);
     fol->destroy_inventory();
     monster_cleanup(fol);
     if (could_see)
-        view_update_at(fol->pos());
+        view_update_at(old_pos);
 }
 
 // Expire all friendly summons / zombies / etc. when the player is leaving a floor.