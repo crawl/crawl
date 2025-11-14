crawl/crawl

commit 04fd3a23e9b4f217fdb77d59f1d425bc43b55ed4

Fix wall monster pathfinding considering walls opaque (gammafunk, various)
This only affected certain types of pathfinding, but significantly that
included the one done for the danger check used to control autoexplore.
This meant that autoexplore would consider rock fish on the other side of
glass to be unable to reach the player until they actually crossed over to
the other side of the wall.

(I've also removed a misleading comment.)

crawl-ref/source/mon-pathfind.cc
@@ -437,23 +437,26 @@ bool monster_pathfind::is_reachable(const coord_def& p)
 
 bool monster_pathfind::traversable(const coord_def& p)
 {
-    if (!traverse_unmapped && env.grid(p) == DNGN_UNSEEN)
+    const dungeon_feature_type feat = env.grid(p);
+    if (!traverse_unmapped && feat == DNGN_UNSEEN)
         return false;
 
     if (traverse_no_actors && actor_at(p))
         return false;
 
-    // XXX: Hack to be somewhat consistent with uses of
-    //      opc_immob elsewhere in pathfinding.
-    //      All of this should eventually be replaced by
-    //      giving the monster a proper pathfinding LOS.
-    if (opc_immob(p) == OPC_OPAQUE && !feat_is_closed_door(env.grid(p)))
+    if (feat_is_solid(feat) && (!mons || !mons->can_pass_through_feat(feat)))
+        return false;
+
+    if (monster* mon_at = monster_at(p))
     {
+        // Try to path around immobile monsters.
+        if (mon_at->is_stationary())
+            return false;
+
         // XXX: Ugly hack to make thorn hunters use their briars for defensive
         //      cover instead of just pathing around them.
         if (mons && mons->type == MONS_THORN_HUNTER
-            && monster_at(p)
-            && monster_at(p)->type == MONS_BRIAR_PATCH)
+            && mon_at->type == MONS_BRIAR_PATCH)
         {
             return true;
         }
@@ -464,13 +467,10 @@ bool monster_pathfind::traversable(const coord_def& p)
     if (mons)
         return mons_traversable(p);
 
-    if (traverse_doors && feat_is_closed_door(env.grid(p))
-        && !cell_is_runed(p))
-    {
+    if (traverse_doors && feat_is_closed_door(feat) && !cell_is_runed(p))
         return true;
-    }
 
-    return feat_has_solid_floor(env.grid(p));
+    return feat_has_solid_floor(feat);
 }
 
 // Checks whether a given monster can pass over a certain position, respecting

crawl-ref/source/nearby-danger.cc
@@ -67,7 +67,7 @@ static bool _mons_has_path_to_player(const monster* mon)
     // like sensed monsters via ash (which will get set as known unreachable
     // on detection).
     if (mon->travel_target == MTRAV_KNOWN_UNREACHABLE
-                                        && !you.see_cell_no_trans(mon->pos()))
+                                        && !you.see_cell(mon->pos()))
     {
         return false;
     }

commit 6ed2157ae40adfe3c0db995c722062c6baaab4bf

Fix a few monster pathfinding issues
Broken by 04fd3a2 , monsters would consider all other monsters impassible
and also often fail to realize they could open doors.

Some players attributed the former behavior to giving monsters more
interesting 'flanking' behavior, but in my testing it did not actually seem
to result in that very much more often than the previous code (but would
sometimes get monsters stuck in place, especially with groups of allies).

crawl-ref/source/mon-pathfind.cc
@@ -443,15 +443,14 @@ bool monster_pathfind::traversable(const coord_def& p)
     if (traverse_no_actors && actor_at(p))
         return false;
 
-    if (feat_is_solid(feat) && (!mons || !mons->can_pass_through_feat(feat)))
+    if (feat_is_solid(feat) && !feat_is_closed_door(feat)
+        && (!mons || !mons->can_pass_through_feat(feat)))
+    {
         return false;
+    }
 
     if (monster* mon_at = monster_at(p))
     {
-        // Try to path around immobile monsters.
-        if (mon_at->is_stationary())
-            return false;
-
         // XXX: Ugly hack to make thorn hunters use their briars for defensive
         //      cover instead of just pathing around them.
         if (mons && mons->type == MONS_THORN_HUNTER
@@ -460,7 +459,9 @@ bool monster_pathfind::traversable(const coord_def& p)
             return true;
         }
 
-        return false;
+        // Try to path around immobile monsters.
+        if (mon_at->is_stationary())
+            return false;
     }
 
     if (mons)