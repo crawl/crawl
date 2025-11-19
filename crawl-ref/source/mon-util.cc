crawl/crawl

commit 7ec163d275af49e8ffb0d7ff38afe8005f288982

Refactor mons_has_ranged_attack() to fix some profound pathfinding issues
mon_has_ranged_attack() was used for a number of significant
movement-related checks, but its logic effectively equated 'has an action
that can reach beyond 1 tile' with 'has an action that can reach anywhere
in LoS'. This had numerous negative consequences such as monsters with
polearms not bothering to pathfind around simple obstacles, nor retreat
from unreachable players pelting them with ranged attacks (since they
falsely believed they were already in range to strike back). And also some
more minor consequences like monsters sidestepping to make room for
polearms users in the distance behind them to... fire at the player?
(Despite the fact that they could reach over their head already, even if
they were in range.)

This commit replaces that method (and many other related ones) with
monster::threat_range() which can be used to approximate the range at which
a monster can perform some threatening action. Pathfinding now uses this
information to calculate a 'fallback' movement target if a monster's
desired target is not directly reachable, by instead looking for some
position close enough to at least threaten their foe. This information also
affects retreating behavior, and in general should make monsters appear to
act more sensibly in many situations - closing to useful positions instead
of just seeming to visibly give up.

This also affects the clua API slightly, exposing threat_range() to
replace monster_info::range(), which performed a similar function, and
removing references to the old and bad mons_has_ranged_attack().

Also minorly affects logic for what monsters will pick up launchers or
throwing weapons to attempt to ban them on monsters with attack spells
rather than specifically those with conjurations (and removing the ban on
summoners picking up launchers; they may end up with their LoF blocked, but
they're not actively counterproductive for them to have).

crawl-ref/source/mon-util.cc
@@ -3673,51 +3673,55 @@ bool mons_should_fire(const bolt& beam, const targeting_tracer &tracer,
 }
 
 /**
- * Can monsters use the given spell effectively from range? (If a monster has
- * the given spell, should it try to keep its distance from its enemies?)
+ * Is a given spell capable of offensive action (including summoning something
+ * that might be)?
  *
- * @param monspell      The spell in question.
- * @param attack_only   Whether to only count spells which directly harm
- *                      enemies (ie: do damage). Overrides ench_too.
- * @param ench_too      Whether to count temporary debilitating effects (Slow,
- *                      etc).
- * @return              Whether the given spell should be considered 'ranged'.
+ * This is only used for some monster AI logic, and so even if it's a bit of an
+ * approximation in some cases, it's hopefully good enough.
+ *
+ * @param spell         The spell in question.
+ * @param needs_lof     Whether the spell is allowed to need direct line of fire
+ *                      or not.
+ * @param damage_only   Whether to exlude hexes and summoning effects.
+ *                      (This is very approximate.)
  */
-bool ms_ranged_spell(spell_type monspell, bool attack_only, bool ench_too)
+bool is_offensive_spell(spell_type spell, maybe_bool needs_lof, bool damage_only)
 {
-    // summoning spells are usable from ranged, but not direct attacks.
-    if (spell_typematch(monspell, spschool::summoning))
-        return !attack_only;
+    const spell_flags flags = get_spell_flags(spell);
+    const spschools_type schools = get_spell_disciplines(spell);
 
-    const spell_flags flags = get_spell_flags(monspell);
-
-    // buffs & escape spells aren't considered 'ranged'.
-    if (testbits(flags, spflag::selfench)
-        || testbits(flags, spflag::escape)
-        || monspell == SPELL_BLINK_OTHER_CLOSE)
-    {
-        return false;
-    }
-
-    // conjurations are attacks.
-    if (spell_typematch(monspell, spschool::conjuration))
-        return true;
-
-    // hexes that aren't conjurations or summons are enchantments.
-    if (spell_typematch(monspell, spschool::hexes))
-        return !attack_only && ench_too;
-
-    switch (monspell)
-    {
-    case SPELL_NO_SPELL:
-    case SPELL_CANTRIP:
-    case SPELL_BLINK_CLOSE:
+    // These are not offensive spells.
+    if (flags & (spflag::escape | spflag::helpful | spflag::selfench))
         return false;
 
-    default:
-        // Everything else is probably some kind of attack, hopefully.
-        return true;
+    // Spells in these schools may be directly targeted, but are rarely direct
+    // damage.
+    if (damage_only && (schools && (spschool::hexes | spschool::summoning | spschool::forgecraft)))
+    {
+        switch (spell)
+        {
+            // The only monster-castable exceptions as of now.
+            // If there are more in future, perhaps we could add a flag.
+            case SPELL_BRAIN_BITE:
+            case SPELL_DOOM_BOLT:
+            case SPELL_CRYSTALLISING_SHOT:
+                return true;
+
+            default:
+                return false;
+        }
     }
+
+    // Assume that spflag::target and spflag::harms_area both ignore line of
+    // fire, while spflag::dir_or_target requires it (regardless of whether it
+    // penetrates or not).
+
+    if (needs_lof == true && !(flags & (spflag::dir_or_target)))
+        return false;
+    else if (needs_lof == false && (flags & (spflag::dir_or_target)))
+        return false;
+
+    return true;
 }
 
 // Returns true if the monster has an ability that can affect the target
@@ -3728,16 +3732,11 @@ bool mons_has_los_ability(monster_type mon_type)
            || mon_type == MONS_STARCURSED_MASS;
 }
 
-bool mons_has_ranged_spell(const monster& mon, bool attack_only,
-                           bool ench_too)
+bool mons_has_ranged_damage_spell(const monster& mon)
 {
-    // Monsters may have spell-like abilities.
-    if (mons_has_los_ability(mon.type))
-        return true;
-
     for (const mon_spell_slot &slot : mon.spells)
     {
-        if (ms_ranged_spell(slot.spell, attack_only, ench_too)
+        if (is_offensive_spell(slot.spell, maybe_bool::maybe, true)
             // Assume spells with no defined range are always effective at
             // range.
             && spell_range(slot.spell, &mon) != 1)
@@ -3747,39 +3746,6 @@ bool mons_has_ranged_spell(const monster& mon, bool attack_only,
     }
 
     return false;
-}
-
-static bool _mons_has_usable_ranged_weapon(const monster* mon)
-{
-    return mon->launcher() != nullptr;
-}
-
-static bool _mons_has_attack_wand(const monster& mon)
-{
-    const item_def *wand = mon.mslot_item(MSLOT_WAND);
-
-    return wand && is_offensive_wand(*wand);
-}
-
-bool mons_has_ranged_attack(const monster& mon)
-{
-    return mons_has_ranged_spell(mon, true)
-           || _mons_has_usable_ranged_weapon(&mon)
-           || mon.missiles()
-           || mon.reach_range() > 1
-           || _mons_has_attack_wand(mon);
-}
-
-bool mons_can_attack(const monster& mon)
-{
-    const actor* foe = mon.get_foe();
-    if (!foe || !mon.can_see(*foe))
-        return false;
-
-    if (mons_has_ranged_attack(mon) && mon.see_cell_no_trans(foe->pos()))
-        return true;
-
-    return adjacent(mon.pos(), foe->pos());
 }
 
 /**

crawl-ref/source/mon-util.h
@@ -358,13 +358,10 @@ bool mons_should_fire(const bolt &beam, const targeting_tracer& tracer,
                       bool ignore_good_idea = false);
 
 bool mons_has_los_ability(monster_type mon_type);
-bool ms_ranged_spell(spell_type monspell, bool attack_only = false,
-                     bool ench_too = true);
-bool mons_has_ranged_spell(const monster& mon, bool attack_only = false,
-                           bool ench_too = true);
-bool mons_has_ranged_attack(const monster& mon);
+bool is_offensive_spell(spell_type spell, maybe_bool needs_lof = maybe_bool::maybe,
+                        bool damage_only = false);
+bool mons_has_ranged_damage_spell(const monster& mon);
 bool _mons_has_smite_attack(const monster* mons);
-bool mons_can_attack(const monster& mon);
 
 gender_type mons_class_gender(monster_type mc);
 const char *mons_pronoun(monster_type mon_type, pronoun_type variant,

crawl-ref/source/monster.cc
@@ -60,6 +60,7 @@
 #include "notes.h"
 #include "ouch.h"
 #include "religion.h"
+#include "spl-book.h"
 #include "spl-clouds.h" // explode_blastmotes_at
 #include "spl-monench.h"
 #include "spl-other.h"
@@ -670,12 +671,9 @@ void monster::bind_melee_flags()
 
 static bool _needs_ranged_attack(const monster* mon)
 {
-    // Prevent monsters that have conjurations from grabbing missiles.
-    if (mon->has_spell_of_type(spschool::conjuration))
-        return false;
-
-    // Same for summonings, but make an exception for friendlies.
-    if (!mon->friendly() && mon->has_spell_of_type(spschool::summoning))
+    // Prevent monsters that have other sources of direct ranged damage
+    // from grabbing missiles or launchers.
+    if (mons_has_ranged_damage_spell(*mon))
         return false;
 
     // Blademasters don't want to throw stuff.
@@ -1869,11 +1867,6 @@ bool monster::pickup_missile(item_def &item, bool msg, bool force)
         }
         else // None of these exceptions hold for throwing nets.
         {
-            // Spellcasters should not waste time with ammunition.
-            // Neither summons nor debuffs are counted for this purpose.
-            if (!force && mons_has_ranged_spell(*this, true, false))
-                return false;
-
             // Monsters in a fight will only pick up missiles if doing so
             // is worthwhile.
             if (!mons_is_wandering(*this)
@@ -6744,4 +6737,64 @@ bool monster::is_firewood() const
 bool monster::is_peripheral() const
 {
     return mons_class_is_peripheral(type);
+}
+
+/**
+ * How many tiles away is the maximum distance this monster can do something
+ * harmful to another entity? We include summoning as 'something harmful', in
+ * this context.
+ *
+ * @param include_lof_requiring   Whether to consider actions that require
+ *                                direct line of fire (ie: launchers, spell
+ *                                projectiles).
+ *
+ * @param include_lof_ignoring  Whether to consider actions that don't require
+ *                              direct line of fire (like smite-targeted spells
+ *                              or reaching weapons).
+ *
+ * @return  The maximum distance of any considered option that the monster has
+ *          to harm someone.
+ */
+int monster::threat_range(bool include_lof_requiring, bool include_lof_ignoring) const
+{
+    if (include_lof_requiring && (launcher() || missiles()))
+        return LOS_RADIUS;
+
+    if (include_lof_ignoring && mons_has_los_ability(type))
+        return LOS_RADIUS;
+
+    int range = 1;
+
+    if (include_lof_ignoring)
+        range = reach_range();
+
+    if (include_lof_requiring)
+    {
+        item_def *wand = mslot_item(MSLOT_WAND);
+        if (wand && is_offensive_wand(*wand))
+            range = max(range, spell_range(spell_in_wand(static_cast<wand_type>(wand->sub_type)), this));
+    }
+
+    // Test spells for appropriate LoF properties
+    maybe_bool needs_lof = maybe_bool::maybe;
+    if (include_lof_requiring && !include_lof_ignoring)
+        needs_lof = true;
+    else if (!include_lof_requiring && include_lof_ignoring)
+        needs_lof = false;
+
+    for (const mon_spell_slot &slot : spells)
+    {
+        if (is_offensive_spell(slot.spell, needs_lof))
+        {
+            const int sp_range = spell_range(slot.spell, this);
+
+            // Assume spells with no defined range can affect all of LoS.
+            if (sp_range == -1)
+                return LOS_RADIUS;
+            else
+                range = max(range, sp_range);
+        }
+    }
+
+    return range;
 }

crawl-ref/source/l-moninf.cc
@@ -137,14 +137,6 @@ MIRET1(number, base_type, base_type)
  * @function number
  */
 MIRET1(number, number, number)
-/*** Does this monster have a ranged attack we know about?
- * A monster is considered to have a ranged attack if it has any of the
- * following: a reach attack, a throwable missile, a launcher weapon, an
- * attack wand, or an attack spell with a range greater than 1.
- * @treturn boolean
- * @function has_ranged_attack
- */
-MIRET1(boolean, has_known_ranged_attack, is(MB_RANGED_ATTACK))
 /*** A string describing monster speed.
  * Possible values are: "very slow", "slow", "normal", "fast", "very fast", and
  * "extremely fast".
@@ -735,7 +727,7 @@ LUAFN(moninf_get_range)
 {
     MONINF(ls, 1, mi);
 
-    lua_pushnumber(ls, mi->range());
+    lua_pushnumber(ls, mi->threat_range);
     return 1;
 }
 
@@ -929,7 +921,6 @@ static const struct luaL_reg moninf_lib[] =
     MIREG(desc),
     MIREG(status),
     MIREG(name),
-    MIREG(has_known_ranged_attack),
     MIREG(speed_description),
     MIREG(spells),
     MIREG(res_poison),

crawl-ref/source/mon-act.cc
@@ -310,8 +310,9 @@ static bool _unfriendly_or_impaired(const monster& mon)
 
 // Check up to eight grids in the given direction for whether there's a
 // monster of the same alignment as the given monster that happens to
-// have a ranged attack. If this is true for the first monster encountered,
-// returns true. Otherwise returns false.
+// have a line-of-fire-requiring ranged attack. If this is true for the first
+// monster encountered (and they are sufficiently close), returns true.
+// Otherwise returns false.
 static bool _ranged_ally_in_dir(monster* mon, coord_def p)
 {
     coord_def pos = mon->pos();
@@ -328,13 +329,9 @@ static bool _ranged_ally_in_dir(monster* mon, coord_def p)
 
         if (mons_aligned(mon, ally))
         {
-            // Hostile monsters of normal intelligence only move aside for
-            // monsters of the same type.
-            if (_unfriendly_or_impaired(*mon)
-                && mons_genus(mon->type) != mons_genus(ally->type))
-            {
+            // Hostile monsters only move aside for monsters of the same type.
+            if (mon->wont_attack() && mons_genus(mon->type) != mons_genus(ally->type))
                 return false;
-            }
 
             // XXX: Sometimes the player wants allies in front of them to stay
             // out of LOF. However use of allies for cover is extremely common,
@@ -343,7 +340,10 @@ static bool _ranged_ally_in_dir(monster* mon, coord_def p)
             // both cases, have allies move out of the way only for other
             // monsters.
             if (ally->is_monster())
-                return mons_has_ranged_attack(*(ally->as_monster()));
+            {
+                return grid_distance(mon->pos(), ally->pos())
+                            <= ally->as_monster()->threat_range(true, false);
+            }
         }
         break;
     }

crawl-ref/source/mon-behv.cc
@@ -88,7 +88,7 @@ static void _mon_check_foe_invalid(monster* mon)
 static bool _mon_tries_regain_los(monster* mon)
 {
     // Only intelligent monsters with ranged attack will try to regain LOS.
-    if (mons_intel(*mon) < I_HUMAN || !mons_has_ranged_attack(*mon))
+    if (mons_intel(*mon) < I_HUMAN || mon->threat_range() <= 1)
         return false;
 
     // Any special case should go here.
@@ -779,11 +779,8 @@ void handle_behaviour(monster* mon)
             // try to attack. The chance is low to prevent the player from
             // dancing in and out of the water.
             try_pathfind(mon);
-            if (one_chance_in(10) && !target_is_unreachable(mon)
-                || mons_can_attack(*mon))
-            {
+            if (one_chance_in(10) && !target_is_unreachable(mon))
                 new_beh = BEH_SEEK;
-            }
             else if (!proxPlayer && one_chance_in(5))
                 new_beh = BEH_WANDER;
             else if (proxPlayer)
@@ -1089,10 +1086,10 @@ void behaviour_event(monster* mon, mon_event_type event, const actor *src,
         if (src_idx != ANON_FRIENDLY_MONSTER)
             mon->foe = src_idx;
 
-        // If the monster can't reach its target and can't attack it
-        // either, retreat.
+        // If the monster can't reach its target (even just to get into attack
+        // range), retreat.
         try_pathfind(mon);
-        if (mons_intel(*mon) > I_BRAINLESS && !mons_can_attack(*mon)
+        if (mons_intel(*mon) > I_BRAINLESS
             && target_is_unreachable(mon) && !mons_just_slept(*mon))
         {
             mon->behaviour = BEH_RETREAT;

crawl-ref/source/mon-info.cc
@@ -838,9 +838,6 @@ monster_info::monster_info(const monster* m, int milev)
     if (!mons_has_attacks(*m))
         mb.set(MB_NO_ATTACKS);
 
-    if (mons_has_ranged_attack(*m))
-        mb.set(MB_RANGED_ATTACK);
-
     if (is_ally_target(*m))
         mb.set(MB_ALLY_TARGET);
 
@@ -881,6 +878,8 @@ monster_info::monster_info(const monster* m, int milev)
         if (m->is_firewood())
             mb.set(MB_FIREWOOD);
     }
+
+    threat_range = m->threat_range();
 
     client_id = m->get_client_id();
     last_seen_at_turn = you.num_turns;
@@ -1697,31 +1696,6 @@ bool monster_info::wields_two_weapons() const
 bool monster_info::can_regenerate() const
 {
     return !is(MB_NO_REGEN);
-}
-
-int monster_info::range() const
-{
-    int range = reach_range(true);
-    // wielding ranged weapon?
-    const item_def *weapon = inv[MSLOT_WEAPON].get();
-    if (weapon && is_range_weapon(*weapon))
-        range = LOS_DEFAULT_RANGE;
-    // quivering something?
-    const item_def *missile = inv[MSLOT_MISSILE].get();
-    if (missile)
-        range = LOS_DEFAULT_RANGE;
-    // ranged attack spells?
-    const vector<mon_spell_slot> &unique_slots = get_unique_spells(*this);
-    for (const auto& slot : unique_slots)
-        if (ms_ranged_spell(slot.spell, true, true))
-            range = max(range, mons_spell_range_for_hd(slot.spell, hd));
-    // has attack wand?
-    const item_def *wand = inv[MSLOT_WAND].get();
-    if (wand && is_offensive_wand(*wand)) {
-        const spell_type spell = spell_in_wand(static_cast<wand_type>(wand->sub_type));
-        range = max(range, calc_spell_range(spell, spell_power_cap(spell), false, true));
-    }
-    return range;
 }
 
 int monster_info::reach_range(bool items) const

crawl-ref/source/mon-info.h
@@ -119,7 +119,9 @@ enum monster_info_flags
     MB_SUPPRESSED,
 #endif
     MB_ROLLING,
+#if TAG_MAJOR_VERSION == 34
     MB_RANGED_ATTACK,
+#endif
     MB_NO_NAME_TAG,
 #if TAG_MAJOR_VERSION == 34
     MB_MAGIC_ARMOUR,
@@ -310,6 +312,7 @@ struct monster_info_base
     bool backlit;
     bool umbraed;
     int last_seen_at_turn;
+    int threat_range;
 
     mid_t client_id;
     mid_t summoner_id;
@@ -441,7 +444,6 @@ struct monster_info : public monster_info_base
 
     bool wields_two_weapons() const;
     bool can_regenerate() const;
-    int range() const;
     int reach_range(bool items = true) const;
 
     size_type body_size() const;

commit c5498a261d27de6a527dbf8a8bb13709f15b7a85

Remove some broken logic regarding monsters picking up launchers (Acrobat)
Monsters that had damage-dealing spells were supposed to not bother picking
up ranged weapons. This was ancient behavior that ended up broken by
7ec163d (due to the new function using an && instead of an & in one
place...)

However, on reflection, perhaps there is not a sufficiently compelling
reason that conjurer monsters *not* pick up launchers. The original idea
(I presume) was to avoid needless overlap between their spells and another
means of firing damaging projectiles, but we don't stop conjurers from
picking up *wands* and those are even more similar. A handful of monsters
with direct damage spells already can even spawn *with* launchers (eg:
kobold blastminers, deep elf knights), so in the rare chance this happens,
it seems probably fine?

So I am simply removing all logic related to this rather than do a more
exhaustive cleanup of what spells do damage or not. If in future we want
to limit this a bit moreso, there are other ways.

crawl-ref/source/mon-util.cc
@@ -3570,35 +3570,14 @@ bool mons_should_fire(const bolt& beam, const targeting_tracer &tracer,
  * @param spell         The spell in question.
  * @param needs_lof     Whether the spell is allowed to need direct line of fire
  *                      or not.
- * @param damage_only   Whether to exlude hexes and summoning effects.
- *                      (This is very approximate.)
  */
-bool is_offensive_spell(spell_type spell, maybe_bool needs_lof, bool damage_only)
+bool is_offensive_spell(spell_type spell, maybe_bool needs_lof)
 {
     const spell_flags flags = get_spell_flags(spell);
-    const spschools_type schools = get_spell_disciplines(spell);
 
     // These are not offensive spells.
     if (flags & (spflag::escape | spflag::helpful | spflag::selfench))
         return false;
-
-    // Spells in these schools may be directly targeted, but are rarely direct
-    // damage.
-    if (damage_only && (schools && (spschool::hexes | spschool::summoning | spschool::forgecraft)))
-    {
-        switch (spell)
-        {
-            // The only monster-castable exceptions as of now.
-            // If there are more in future, perhaps we could add a flag.
-            case SPELL_BRAIN_BITE:
-            case SPELL_DOOM_BOLT:
-            case SPELL_CRYSTALLISING_SHOT:
-                return true;
-
-            default:
-                return false;
-        }
-    }
 
     // Assume that spflag::target and untagged spells both ignore line of
     // fire, while spflag::dir_or_target requires it (regardless of whether it
@@ -3618,22 +3597,6 @@ bool mons_has_los_ability(monster_type mon_type)
 {
     return mons_is_siren_beholder(mon_type)
            || mon_type == MONS_STARCURSED_MASS;
-}
-
-bool mons_has_ranged_damage_spell(const monster& mon)
-{
-    for (const mon_spell_slot &slot : mon.spells)
-    {
-        if (is_offensive_spell(slot.spell, maybe_bool::maybe, true)
-            // Assume spells with no defined range are always effective at
-            // range.
-            && spell_range(slot.spell, &mon) != 1)
-        {
-            return true;
-        }
-    }
-
-    return false;
 }
 
 /**

crawl-ref/source/mon-util.h
@@ -357,9 +357,7 @@ bool mons_should_fire(const bolt &beam, const targeting_tracer& tracer,
                       bool ignore_good_idea = false);
 
 bool mons_has_los_ability(monster_type mon_type);
-bool is_offensive_spell(spell_type spell, maybe_bool needs_lof = maybe_bool::maybe,
-                        bool damage_only = false);
-bool mons_has_ranged_damage_spell(const monster& mon);
+bool is_offensive_spell(spell_type spell, maybe_bool needs_lof = maybe_bool::maybe);
 bool _mons_has_smite_attack(const monster* mons);
 
 gender_type mons_class_gender(monster_type mc);

crawl-ref/source/monster.cc
@@ -674,11 +674,6 @@ void monster::bind_melee_flags()
 
 static bool _needs_ranged_attack(const monster* mon)
 {
-    // Prevent monsters that have other sources of direct ranged damage
-    // from grabbing missiles or launchers.
-    if (mons_has_ranged_damage_spell(*mon))
-        return false;
-
     // Blademasters don't want to throw stuff.
     if (mon->type == MONS_DEEP_ELF_BLADEMASTER)
         return false;