d0b26c84e2f753d6ee2ba5acca12f396806b0c46
gammafunk <gammafunk@gmail.com>
2025-11-29 17:28:48 -0600

    Lua 5.4: DES cache regeneration
    Add a minor version check for any DES bytecode generated with Lua 5.1
    and discard it. Also remove some unused variables.

--- crawl-ref/source/tag-version.h
+++ crawl-ref/source/tag-version.h
@@ -340,6 +340,7 @@ enum tag_minor_version
     TAG_MINOR_TRACK_ORIGIN_LEVEL,  // Track the original level on which a monster was generated
     TAG_MINOR_FIX_POLAR_VORTEX_INFO_LEAK, // Don't leak whether the polar vortex caster has moved or gone
     TAG_MINOR_CONSTRICTED_TYPE,    // Refactor saving of constriction type on actors
+    TAG_MINOR_LUA_5_4,             // Upgrade from Lua 5.1 to Lua 5.4.
 #endif
     NUM_TAG_MINORS,
     TAG_MINOR_VERSION = NUM_TAG_MINORS - 1
