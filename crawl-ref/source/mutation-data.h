crawl/crawl

commit e68fdf0d6e36da7cfc586f35d546c639b9b2eb04

New bad mutation: Spiteful Blood
This mutation gives a chance that the player's own blood rises up to fight
them when enough of it is spilled. When falling below 50% max HP, an
erythrospite may spawn adjacent to you - an angry mass of spilled blood
with HP and attack damage that scales with a factor of the player's XL
(and vampiric attacks, but poor defenses).

If one spawns, a second will not be able to spawn until the player heals
fully (and then gets significant reinjured).

At the second level of this mutation, two erythrospites may spawn at the
same time.

Damage, spawn chance, and other details are provisional, but this is a
further attempt at trying new angles for more interesting badmuts (that are
also not intended to be backbreaking).

crawl-ref/source/mutation-data.h
@@ -2410,6 +2410,18 @@ static const mutation_def mut_data[] =
   TILEG_MUT_ACCURSED,
 },
 
+{ MUT_SPITEFUL_BLOOD, 2, 2, mutflag::bad | mutflag::need_blood,
+  "spiteful blood",
+
+  {"Your shed blood may rise up to attack you.",
+   "Your shed blood may rise up to attack you as a group.", ""},
+  {"You feel a willful anger gathering in your blood.",
+   "You feel your blood grow even more spiteful.", ""},
+  {"Your blood calms down again.",
+   "Your blood feels a little less spiteful.", ""},
+  TILEG_MUT_GENERIC_BAD_MUTATION,
+},
+
 // Makhleb-specific mutations
 
 { MUT_MAKHLEB_DESTRUCTION_GEH, 0, 1, mutflag::good,

crawl-ref/source/dat/mons/erythrospite.yaml
@@ -1,0 +1,15 @@
+name: "erythrospite"
+glyph: {char: "E", colour: etc_blood}
+flags: [see_invis, unblindable, amorphous, no_skeleton, speaks, warm_blood]
+exp: 42
+will: 10
+attacks:
+ - {type: hit, flavour: vampiric, damage: 3}
+hd: 3
+hp_10x: 165
+ac: 0
+ev: 7
+intelligence: brainless
+habitat: amphibious
+size: small
+shape: blob

crawl-ref/source/duration-data.h
@@ -842,6 +842,7 @@ static const duration_def duration_data[] =
     { DUR_AUTODODGE, 0, "", "", "autododge", "", D_NO_FLAGS},
     { DUR_DAZED, 0, "", "", "dazed", "", D_NEGATIVE},
     { DUR_CONSTRICTED, 0, "", "", "constricted", "", D_NO_FLAGS},
+    { DUR_SPITEFUL_BLOOD_COOLDOWN, 0, "", "", "spiteful_blood", "", D_NO_FLAGS},
 
 #if TAG_MAJOR_VERSION == 34
     // And removed ones

crawl-ref/source/duration-type.h
@@ -298,5 +298,6 @@ enum duration_type
     DUR_PARRYING,
     DUR_STARDUST_COOLDOWN,
     DUR_MESMERISM_COOLDOWN,
+    DUR_SPITEFUL_BLOOD_COOLDOWN,
     NUM_DURATIONS
 };

crawl-ref/source/mon-act.cc
@@ -4059,6 +4059,9 @@ static bool _monster_move(monster* mons, coord_def& delta)
 
         if (mons->type == MONS_CURSE_TOE)
             place_cloud(CLOUD_MIASMA, mons->pos(), 2 + random2(3), mons);
+
+        if (mons->type == MONS_ERYTHROSPITE)
+            bleed_onto_floor(mons->pos(), MONS_ERYTHROSPITE, 100, false);
     }
     else
     {

crawl-ref/source/mon-death.cc
@@ -2839,6 +2839,8 @@ item_def* monster_die(monster& mons, killer_type killer,
             activate_tesseracts();
         }
     }
+    else if (mons.type == MONS_ERYTHROSPITE && !mons.is_abjurable())
+        bleed_onto_floor(mons.pos(), MONS_ERYTHROSPITE, 100, false);
 
     if (mons.has_ench(ENCH_MAGNETISED))
     {
@@ -3221,6 +3223,8 @@ item_def* monster_die(monster& mons, killer_type killer,
                 msg = " expends the last of its power.";
             else if (mons.type == MONS_RENDING_BLADE)
                 msg = " implodes with a snap.";
+            else if (mons.type == MONS_ERYTHROSPITE)
+                msg = " expends the last of its anger.";
             else
             {
                 if (mons.props.exists(KIKU_WRETCH_KEY))

crawl-ref/source/mon-enum.h
@@ -190,6 +190,7 @@ enum mon_summon_type
     MON_SUMM_MULTIPLICITY, // Bane of Multiplicity
     MON_SUMM_MORTALITY, // Bane of Mortality
     MON_SUMM_STARDUST,  // Orb of Stardust
+    MON_SUMM_SPITEFUL_BLOOD, // Spiteful Blood mutation
 };
 
 #include "mon-flags.h"

crawl-ref/source/mon-util.cc
@@ -1996,6 +1996,8 @@ mon_attack_def mons_attack_spec(const monster& m, int attk_number,
         else
             attk.damage = 2 + (m.get_hit_dice() * 3 / 2);
     }
+    else if (mon.type == MONS_ERYTHROSPITE)
+        attk.damage = 3 + m.get_experience_level();
 
     // Vampires get a bite aux in addition to normal attacks.
     if (mon.has_ench(ENCH_VAMPIRE_THRALL)

commit 15974c44edb5ae1abeafe8d4ffc6fd4efd5df69b

A few more recently-added mutation icons
 * e68fdf0 accidentally left out the icon tiles I made for for Spiteful
   Blood (using the same CC0 wraith effect from DUELYST's arcane devourer
   that erthoyspites were derived from, and Denzi's old eye of draining
   tile). I have thus added them back in here.

 * b55b5ce accidentally left out a second icon for Initially Attractive,
   which due to its two levels ends up making the second icon use the next
   mutation in dc-mutation.txt (Demonic Touch, before the current changes
   here). I have made a second one here.

Also, I've reorganized a small amount of dc-mutation.txt to keep the
alphabetical order it deploys in the loosely-piled-together icons
sections.

crawl-ref/source/mutation-data.h
@@ -2448,7 +2448,7 @@ static const mutation_def mut_data[] =
    "You feel your blood grow even more spiteful.", ""},
   {"Your blood calms down again.",
    "Your blood feels a little less spiteful.", ""},
-  TILEG_MUT_GENERIC_BAD_MUTATION,
+  TILEG_MUT_SPITEFUL_BLOOD,
 },
 
 // Makhleb-specific mutations