crawl/crawl

commit 62137d70225229b64a18d355573eb3cb027b4769

Heavily refactor mons_is_conjured/mons_is_object, simplify
mons_is_conjured was a category being used in multiple (only partially
overlapping) ways. In some contexts, it was used as an equivalent for
is_summoned() for non-abjurable things which is_summoned() did not return
true (but now does, since the recent rework). In other contexts, it was
used to exclude monsters too minor to be randomly targeted for certain
effects (eg: Obsidian's Axe's mesmerisation effect or Ignis's Fire
Champion). In others, it was used overly broadly to avoid prompting the
player about harming invulnerable allies (ie: orbs of destruction), with
sometimes odd effects like Shatter being unable to damage spatial vorticies
or boulders.

In still more, it was used (as part of mons_is_object) to control what
allies were angered by attacks and which ones gods would penance you for
attacking - which weren't even symetrical categories! A lightning spire
would be angered by you hitting it and vanish, but Ely would not min. But
a spellforged servitor would *not* be angered by you hitting it, and yet
Ely would penance you anyway.

mons_is_object also relied on a bespoke list of things we considered to be
'a mindless object' that often overlooked things which seemingly belonged
there (while also producing weird effects like Orb of Mayhem never
triggering off killing dancing weapons or lighting spires, even though
other seemingly similar things counted.)

This commit replaces both of those functions completely. M_PERIPHERAL
replaces M_CONJURED (since the ancillary nature of these monsters is the
relevant mechanical thing, and not the fact that they were made by magic),
actor::is_peripheral() replaces most uses of mons_is_conjured() and also
folds tentacles and firewood into that same category. The very large number
of places that checked firewood/conjured/tentacles together now need only
check is_peripheral(), which should largely encompass 'is a proper
monster'.

crawl-ref/source/religion.cc
@@ -1670,7 +1670,7 @@ bool is_follower(const monster& mon)
     else
     {
         return mon.alive() && mon.attitude == ATT_FRIENDLY
-               && !mons_is_conjured(mon.type);
+                           && !mon.is_summoned();
     }
 }
 
@@ -3270,7 +3270,8 @@ bool god_hates_attacking_friend(god_type god, const monster& fr)
 
     monster_type species = fr.mons_species();
 
-    if (mons_is_object(species))
+    // Nobody minds you hurting inanimate objects
+    if ((fr.holiness() & MH_NONLIVING) && mons_intel(fr) == I_BRAINLESS)
         return false;
     switch (god)
     {
@@ -4112,13 +4113,6 @@ bool god_hates_killing(god_type god, const monster& mon)
 {
     if (invalid_monster(&mon))
         return false;
-    // Must be at least a creature of sorts. Smacking down an enchanted
-    // weapon or disrupting a lightning doesn't count. Technically, this
-    // might raise a concern about necromancy but zombies traditionally
-    // count as creatures and that's the average person's (even if not ours)
-    // intuition.
-    if (mons_is_object(mon.type))
-        return false;
 
     // kill as many illusions as you want.
     if (mon.is_illusion())
@@ -4547,7 +4541,7 @@ int get_monster_tension(const monster& mons, god_type god)
     // or bomb is offhand, but they should count for _some_ minimal tension.
     if (exper <= 0)
     {
-        if (mons_is_conjured(mons.type))
+        if (mons.is_peripheral())
             exper = 50;
         else
             return 0;

commit 0a107a98f2d68832c89366adb7a6788aa377e927

Fix Xom acting around sufficient counts of firewood (Zadiel)
8b305a4 made conjured monsters (e.g. orbs of destruction, battlespheres)
count as having some minimal amount of experience value to help with
tension not really being equipped to assess their comparative danger
levels. This was adjusted into peripheral monsters in 62137d7, which
accidentally started counting plants, fungi, and other non-hostile
obstacle-monsters as having non-zero experience values for tension's
calculations. This doesn't have a lot of problems for most uses of tension,
but it does mean Xom will act in many pointless ways around sufficient
amounts of plants, which is extremely undesireable behaviour that is thus
fixed here.

A more ideal solution for this is most likely to add actual experience
values for various peripheral monsters for the sake of better tension
calculations (instead of setting orbs of destruction to be worth half an
ogre of threat), but with the version release looming, this should function
enough as a fix for now.

crawl-ref/source/religion.cc
@@ -4548,7 +4548,7 @@ int get_monster_tension(const monster& mons, god_type god)
     // is offhand, but they should count for _some_ minimal tension.
     if (exp <= 0)
     {
-        if (mons.is_peripheral())
+        if (mons.is_peripheral() && !mons.is_firewood())
             exp = 50;
         else
             return 0;