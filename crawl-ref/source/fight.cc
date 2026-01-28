crawl/crawl

commit fff38f6c25b3712b384d607337aab54b521f0bdc

Refactor handling of monster attack rounds
While melee_attack::launch_attack_set() handled players making multiple
attacks in one round (eg: coglins), it did not do this for monsters.
Instead, fight_melee() would construct one melee_attack per attack that a
monster has and call attack() on each one. But this meant both that any
attempt to make a monster attack *outside* of fight_melee() would only ever
use their first attack, and that it was impossible to apply special attack
properties to a monster's full attack round.

As practical consequences of this, Vhi's Electrolunge, any attacks a
monster made while Vexed, and Manifold Assault could only ever use the
first attack a monster had - just one weapon or one hydra head, etc.
By handling the full attack set inside melee_attack instead, these (and
future special attacks) can all work more properly.

There is one nit in that Manifold Assault still ends up being slightly
asymmetric with the player spell (just in a different way). Player auxes
never happen on projected attacks, but monsters don't define 'aux' as
distinct from any other attack. We can't rely just on it being a weapon
attack, since the 'primary' attack type for many monsters is something else
(eg: Bite or Claw). In interest of simplicity, I am accepting that monsters
with Manifold Assault can simply use all of their attacks.

crawl-ref/source/fight.cc
@@ -537,82 +537,9 @@ bool fight_melee(actor *attacker, actor *defender, bool *did_hit, bool simu)
         return false;
     }
 
-    const int nrounds = attacker->as_monster()->has_hydra_multi_attack()
-        ? attacker->heads() + (attacker->type == MONS_DRAUGR)
-        : MAX_NUM_ATTACKS;
-    coord_def pos = defender->pos();
-
-    // Melee combat, tell attacker to wield its melee weapon.
-    attacker->as_monster()->wield_melee_weapon();
-
-    bool was_hostile = !mons_aligned(attacker, defender);
-
-    int effective_attack_number = 0;
-    int attack_number;
-    for (attack_number = 0; attack_number < nrounds && attacker->alive();
-         ++attack_number, ++effective_attack_number)
-    {
-        if (!attacker->alive())
-            return false;
-
-        // Monster went away or become friendly?
-        if (!defender->alive()
-            || defender->pos() != pos
-            || defender->is_banished()
-            || was_hostile && mons_aligned(attacker, defender)
-               && !mons_is_confused(*attacker->as_monster())
-               && !attacker->as_monster()->has_ench(ENCH_FRENZIED))
-        {
-            if (attacker == defender
-               || !attacker->as_monster()->has_hydra_multi_attack())
-            {
-                break;
-            }
-
-            // Hydras can try and pick up a new monster to attack to
-            // finish out their round. -cao
-            bool end = true;
-            for (adjacent_iterator i(attacker->pos()); i; ++i)
-            {
-                if (*i == you.pos()
-                    && !mons_aligned(attacker, &you)
-                    && you.alive())
-                {
-                    attacker->as_monster()->foe = MHITYOU;
-                    attacker->as_monster()->target = you.pos();
-                    defender = &you;
-                    was_hostile = true;
-                    end = false;
-                    break;
-                }
-
-                monster* mons = monster_at(*i);
-                if (mons && !mons_aligned(attacker, mons))
-                {
-                    defender = mons;
-                    was_hostile = true;
-                    end = false;
-                    pos = mons->pos();
-                    break;
-                }
-            }
-
-            // No adjacent hostiles.
-            if (end)
-                break;
-        }
-
-        melee_attack melee_attk(attacker, defender, attack_number,
-                                effective_attack_number);
-
-        melee_attk.simu = simu;
-        melee_attk.attack();
-
-        if (did_hit && !(*did_hit))
-            *did_hit = melee_attk.did_hit;
-
-        fire_final_effects();
-    }
+    melee_attack attk(attacker, defender);
+    attk.simu = simu;
+    attk.launch_attack_set();
 
     if (!attacker->alive())
         return true;
@@ -624,16 +551,6 @@ bool fight_melee(actor *attacker, actor *defender, bool *did_hit, bool simu)
     ASSERT(energy > 0);
     ASSERT(delay > 0);
     attacker->as_monster()->speed_increment -= div_rand_round(energy * delay, 10);
-
-    // If this attack was supposed to be instant, refund the energy now.
-    if (monster* mons = attacker->as_monster())
-    {
-        if (mons->has_ench(ENCH_INSTANT_CLEAVE))
-        {
-            mons->del_ench(ENCH_INSTANT_CLEAVE);
-            mons->speed_increment += mons->action_energy(EUT_ATTACK);
-        }
-    }
 
     // Here, rather than in melee_attack, so that it only triggers on attack
     // actions, rather than additional times for bonus attacks (ie: from Autumn Katana)

crawl-ref/source/melee-attack.cc
@@ -1376,6 +1376,7 @@ void melee_attack::copy_params_to(melee_attack &other)
     other.is_involuntary        = is_involuntary;
     other.wu_jian_attack        = wu_jian_attack;
     other.wu_jian_number_of_targets = wu_jian_number_of_targets;
+    other.simu                  = simu;
 }
 
 // Perform followup attacks (from cleaving or quick blades).
@@ -1468,9 +1469,9 @@ bool melee_attack::swing_with(item_def &wpn, bool offhand)
 bool melee_attack::launch_attack_set(bool skip_player_post_attack)
 {
     if (!attacker->is_player())
-        return attack();
+        return run_monster_attack_set();
 
-    bool success = run_attack_set();
+    bool success = run_player_attack_set();
 
     if (!skip_player_post_attack)
     {
@@ -1484,7 +1485,99 @@ bool melee_attack::launch_attack_set(bool skip_player_post_attack)
     return success;
 }
 
-bool melee_attack::run_attack_set()
+bool melee_attack::run_monster_attack_set()
+{
+    ASSERT(attacker->is_monster());
+    monster* mon = attacker->as_monster();
+
+    const int nrounds = attacker->as_monster()->has_hydra_multi_attack()
+        ? attacker->heads() + (attacker->type == MONS_DRAUGR)
+        : MAX_NUM_ATTACKS;
+    coord_def pos = defender->pos();
+
+    // Melee combat, tell attacker to wield its melee weapon.
+    mon->wield_melee_weapon();
+
+    bool was_hostile = !mons_aligned(attacker, defender);
+
+    bool success = false;
+    int effective_attack_num = 0;
+    int attack_num;
+    for (attack_num = 0; attack_num < nrounds && attacker->alive();
+         ++attack_num, ++effective_attack_num)
+    {
+        if (!attacker->alive())
+            return false;
+
+        // Monster went away or become friendly?
+        if (!defender->alive()
+            || defender->pos() != pos
+            || defender->is_banished()
+            || was_hostile && mons_aligned(attacker, defender)
+               && !mons_is_confused(*attacker->as_monster())
+               && !attacker->as_monster()->has_ench(ENCH_FRENZIED))
+        {
+            if (attacker == defender
+               || !attacker->as_monster()->has_hydra_multi_attack())
+            {
+                break;
+            }
+
+            // Hydras can try and pick up a new monster to attack to
+            // finish out their round. -cao
+            bool end = true;
+            for (adjacent_iterator i(attacker->pos()); i; ++i)
+            {
+                if (*i == you.pos()
+                    && !mons_aligned(attacker, &you)
+                    && you.alive())
+                {
+                    attacker->as_monster()->foe = MHITYOU;
+                    attacker->as_monster()->target = you.pos();
+                    defender = &you;
+                    was_hostile = true;
+                    end = false;
+                    break;
+                }
+
+                monster* mons = monster_at(*i);
+                if (mons && !mons_aligned(attacker, mons))
+                {
+                    defender = mons;
+                    was_hostile = true;
+                    end = false;
+                    pos = mons->pos();
+                    break;
+                }
+            }
+
+            // No adjacent hostiles.
+            if (end)
+                break;
+        }
+
+        melee_attack melee_attk(attacker, defender, attack_num,
+                                effective_attack_num);
+
+        copy_params_to(melee_attk);
+        success |= melee_attk.attack();
+
+        did_hit |= melee_attk.did_hit;
+
+        fire_final_effects();
+    }
+
+    // If this attack was supposed to be instant, refund the energy now.
+    if (mon->has_ench(ENCH_INSTANT_CLEAVE))
+    {
+        mon->del_ench(ENCH_INSTANT_CLEAVE);
+        mon->speed_increment += mon->action_energy(EUT_ATTACK);
+    }
+
+    return success;
+}
+
+bool melee_attack::run_player_attack_set()
 {
     item_def *primary = you.weapon();
     item_def *offhand = you.offhand_weapon();

crawl-ref/source/melee-attack.h
@@ -124,8 +124,10 @@ private:
     bool attack_chops_heads(int damage_done);
     void decapitate();
 
-    bool run_attack_set();
+    bool run_player_attack_set();
     bool swing_with(item_def &weapon, bool offhand);
+
+    bool run_monster_attack_set();
 
     /* Axe cleaving */
     void cleave_setup();

commit 264ef1ee839f281b1efd8a78e6d17da17ed41195

Fix fsim not reporting monster attacks
Since it believed they always missed.

(Broken since fff38f6)

crawl-ref/source/fight.cc
@@ -411,11 +411,11 @@ static void _do_medusa_stinger()
  *
  * @param[in] attacker,defender The (non-null) participants in the attack.
  *                              Either may be killed as a result of the attack.
- * @param[out] did_hit If non-null, receives true if the attack hit the
- *                     defender, and false otherwise.
  * @param is_rampage   Is this an attack caused by rampaging? Adjusts damage of
  *                     the attack based on movement speed and possibly staggers
  *                     the target. (Only effect for player attackers)
+ * @param[out] did_hit If non-null, receives true if the attack hit the
+ *                     defender, and false otherwise.
  * @param simu Is this a simulated attack?  Disables a few problematic
  *             effects such as blood spatter and distortion teleports.
  *
@@ -551,6 +551,9 @@ bool fight_melee(actor *attacker, actor *defender, bool is_rampage,
     melee_attack attk(attacker, defender);
     attk.simu = simu;
     attk.launch_attack_set();
+
+    if (did_hit)
+        *did_hit = attk.did_hit;
 
     if (!attacker->alive())
         return true;