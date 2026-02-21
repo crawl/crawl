crawl/crawl

commit 17f7e79a93591f110b291c72cae6f524bdb830fb

Deduct monster energy in fight_melee instead of inside melee_attack
melee_attack automatically deducted monster energy if the effective attack
number of the attack was 0 (along with some code outside to make sure that
skipped attacks were tallied correctly. This needed compensation in some
other place and also resulted in some bugs on its own (Vexed monsters lost
*two* turns worth of energy for attacking, for instance)

The logic for whether attack::attack() returns true or false has some
unrelated problems that will require changes, so I'm decoupling one of the
two places that actually check this value ahead of time.

crawl-ref/source/melee-attack.cc
@@ -223,38 +223,9 @@ bool melee_attack::handle_phase_attempted()
         return false;
     }
 
-    if (attacker->is_monster())
-    {
-        // Only the first attack costs any energy normally.
-        // Projected attacks will have already had their energy costs paid
-        // elsewhere (ie: from casting Manifold Assault or making a normal attack
-        // with the Autumn Katana)
-        if (!effective_attack_number && !is_projected)
-        {
-            int energy = attacker->as_monster()->action_energy(EUT_ATTACK);
-            int delay = attacker->attack_delay().roll();
-            dprf(DIAG_COMBAT, "Attack delay %d, multiplier %1.1f", delay, energy * 0.1);
-            ASSERT(energy > 0);
-            ASSERT(delay > 0);
-
-            attacker->as_monster()->speed_increment
-                -= div_rand_round(energy * delay, 10);
-        }
-
-        // Statues and other special monsters which have AT_NONE need to lose
-        // energy, but otherwise should exit the melee attack now.
-        if (attk_type == AT_NONE)
-            return false;
-
-        // If we're a monster that was supposed to get a free instant cleave
-        // attack, refund the energy now.
-        monster* mons = attacker->as_monster();
-        if (mons->has_ench(ENCH_INSTANT_CLEAVE))
-        {
-            mons->del_ench(ENCH_INSTANT_CLEAVE);
-            mons->speed_increment += mons->action_energy(EUT_ATTACK);
-        }
-    }
+    // If this is a non-attack, exit now.
+    if (attk_type == AT_NONE)
+        return false;
 
     if (attacker != defender && !is_riposte)
     {
@@ -348,9 +319,7 @@ bool melee_attack::handle_phase_blocked()
                 needs_block_message = false;
             }
 
-            // Note: effective attack number must be non-zero or the monster
-            // will be charged energy a second time for this attack.
-            melee_attack rebuke(attacker, best_targ, attack_number, 1);
+            melee_attack rebuke(attacker, best_targ, attack_number, 0);
             copy_params_to(rebuke);
             rebuke.responsible = attacker;
             rebuke.never_cleave = true;

crawl-ref/source/fight.cc
@@ -603,15 +603,33 @@ bool fight_melee(actor *attacker, actor *defender, bool *did_hit, bool simu)
                                 effective_attack_number);
 
         melee_attk.simu = simu;
+        melee_attk.attack();
 
-        // If the attack fails out, keep effective_attack_number up to
-        // date so that we don't cause excess energy loss in monsters
-        if (!melee_attk.attack())
-            effective_attack_number = melee_attk.effective_attack_number;
-        else if (did_hit && !(*did_hit))
+        if (did_hit && !(*did_hit))
             *did_hit = melee_attk.did_hit;
 
         fire_final_effects();
+    }
+
+    if (!attacker->alive())
+        return true;
+
+    // Lose energy for the attack.
+    int energy = attacker->as_monster()->action_energy(EUT_ATTACK);
+    int delay = attacker->attack_delay().roll();
+    dprf(DIAG_COMBAT, "Attack delay %d, multiplier %1.1f", delay, energy * 0.1);
+    ASSERT(energy > 0);
+    ASSERT(delay > 0);
+    attacker->as_monster()->speed_increment -= div_rand_round(energy * delay, 10);
+
+    // If this attack was supposed to be instant, refund the energy now.
+    if (monster* mons = attacker->as_monster())
+    {
+        if (mons->has_ench(ENCH_INSTANT_CLEAVE))
+        {
+            mons->del_ench(ENCH_INSTANT_CLEAVE);
+            mons->speed_increment += mons->action_energy(EUT_ATTACK);
+        }
     }
 
     // Here, rather than in melee_attack, so that it only triggers on attack

crawl-ref/source/spl-summoning.cc
@@ -4009,8 +4009,6 @@ spret cast_surprising_crocodile(actor& agent, const coord_def& targ, int pow, bo
         atk.attack();
     }
 
-    croc->flags & ~MF_JUST_SUMMONED;
-
     if (you.can_see(agent))
     {
         mprf("%s dismount%s %s crocodile.",

crawl-ref/source/spl-transloc.cc
@@ -836,11 +836,6 @@ spret electric_charge(actor& agent, int powc, bool fail, const coord_def &target
         // things like the Dark Maul.
         you.time_taken = max(you.attack_delay().roll(), player_speed());
     }
-    // The attack will have automatically consumed energy from the monster, so
-    // let's refund that (as they will be automatically be charged for casting
-    // the spell *in addition* to that).
-    else
-        agent.as_monster()->speed_increment += agent.as_monster()->energy_cost(EUT_ATTACK);
 
     // Finally, apply traps at the agent's destination *after* the attack.
     agent.finalise_movement();

commit b7ba641fff6901ca65071d7b12de2ff6f5ba98fb

Fix anacondas taking no damage from cactus giant spines
Spines can only do damage to the first enemy attack of a round, and an
anaconda's first attack is constriction that the cactus giant is immune to
due to its size.

This was accidentally broken by 17f7e79 (as apparently one of the only
other things that cared about the value of effective_attack_number)

(This did not affect most constrictors as almost all of them have
constriction as their last attack.)

This fixes #5085

crawl-ref/source/melee-attack.cc
@@ -222,8 +222,6 @@ bool melee_attack::handle_phase_attempted()
         || attk_flavour == AF_CRUSH
            && !attacker->can_constrict(*defender, CONSTRICT_MELEE))
     {
-        --effective_attack_number;
-
         return false;
     }
 
@@ -1658,6 +1656,9 @@ bool melee_attack::run_monster_attack_set()
         // Charge up for monsters (player is handled in player_attempted_attack)
         if (melee_attk.is_sundering_weapon() && !is_projected && attack_succeeded)
             charge_sunder = true;
+
+        if (!attack_succeeded)
+            --effective_attack_num;
 
         fire_final_effects();
     }