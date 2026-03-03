crawl/crawl

commit a2d0413ad78aca55fcf74fb457f2b21844323982

fix: ^Fragile equipment being destroyed before unequip effects
The player could dodge negative unequip effects on a ^Fragile item. This
means Amulets of Faith wouldn't have a piety penalty for removal and
weapons of Distortion wouldn't do their distortion unwield effects. This
is apparently unintended and it turns a negative property into a
positive one which will surprise players and vault makers.

This commit breaks ^Fragile gear only after other unequip effects have
happened.

crawl-ref/source/player-equip.cc
@@ -1666,15 +1666,16 @@ void unequip_effect(int item_slot, bool meld, bool msg)
 
     const interrupt_block block_meld_interrupts(meld);
 
-   if (is_artefact(item))
-        unequip_artefact_effect(item, &msg, meld);
-
     if (is_weapon(item))
         _unequip_weapon_effect(item, msg, meld);
     else if (item.base_type == OBJ_ARMOUR)
         _unequip_armour_effect(item, meld);
     else if (item.base_type == OBJ_JEWELLERY)
         _unequip_jewellery_effect(item, meld);
+
+    // Artprops includes ^Fragile so the item may be destroyed after this.
+    if (is_artefact(item))
+        unequip_artefact_effect(item, &msg, meld);
 
     // Cursed items should always be destroyed on unequip.
     if (item.cursed() && !meld)

commit a76044d670ab946022b0ac20cd09e25dfd239dbb

Fix unequipping unrandarts failing to suppress brand messages
Unrands give their own custom messages when unequipped and should
suppress the default message for their brand. However, this was broken
in commit a2d0413

crawl-ref/source/player-equip.cc
@@ -1660,11 +1660,21 @@ void equip_effect(int item_slot, bool unmeld, bool msg)
         ash_check_bondage();
 }
 
+static void _unequip_maybe_destroy_item(item_def& item)
+{
+    // Cursed and fragile items should always be destroyed on unequip.
+    if (artefact_property(item, ARTP_FRAGILE) || item.cursed())
+        dec_inv_item_quantity(item.link, 1);
+}
+
 void unequip_effect(int item_slot, bool meld, bool msg)
 {
     item_def& item = you.inv[item_slot];
 
     const interrupt_block block_meld_interrupts(meld);
+
+    if (is_artefact(item))
+        unequip_artefact_effect(item, &msg, meld);
 
     if (is_weapon(item))
         _unequip_weapon_effect(item, msg, meld);
@@ -1673,13 +1683,8 @@ void unequip_effect(int item_slot, bool meld, bool msg)
     else if (item.base_type == OBJ_JEWELLERY)
         _unequip_jewellery_effect(item, meld);
 
-    // Artprops includes ^Fragile so the item may be destroyed after this.
-    if (is_artefact(item))
-        unequip_artefact_effect(item, &msg, meld);
-
-    // Cursed items should always be destroyed on unequip.
-    if (item.cursed() && !meld)
-        destroy_item(item);
+    if (!meld)
+        _unequip_maybe_destroy_item(item);
 }
 
 ///////////////////////////////////////////////////////////
@@ -1823,7 +1828,6 @@ void unequip_artefact_effect(item_def &item,  bool *show_msgs, bool meld)
     if (artefact_property(item, ARTP_FRAGILE) && !meld)
     {
         mprf("%s crumbles to dust!", item.name(DESC_THE).c_str());
-        dec_inv_item_quantity(item.link, 1);
 
         // Hide unwield messages for weapons that have already been destroyed.
         if (item.base_type == OBJ_WEAPONS)