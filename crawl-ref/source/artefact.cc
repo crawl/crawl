crawl/crawl

commit ec41f428ff522f823c513ed9ae8a54519824fe5b

Move _preferred_max_level() to art-data.txt.
Using the new field name MAX_LEVEL, and with a default value of 0
instead of -1 since art-data.pl makes all default number values 0.

crawl-ref/source/artefact.cc
@@ -1742,41 +1742,15 @@ const unrandart_entry* get_unrand_entry(int unrand_index)
         return &unranddata[unrand_index];
 }
 
-static int _preferred_max_level(int unrand_index)
-{
-    // TODO: turn this into a max preferred level field in art-data.txt
-    switch (unrand_index)
-    {
-    case UNRAND_DELATRAS_GLOVES:
-        return 6;
-    case UNRAND_WOODCUTTERS_AXE:
-    case UNRAND_THROATCUTTER:
-    case UNRAND_HERMITS_PENDANT:
-        return 9;
-    case UNRAND_DEVASTATOR:
-    case UNRAND_RATSKIN_CLOAK:
-    case UNRAND_KRYIAS:
-    case UNRAND_LEAR:
-    case UNRAND_OCTOPUS_KING:
-    case UNRAND_AUGMENTATION:
-    case UNRAND_MEEK:
-    case UNRAND_ELEMENTAL_VULNERABILITY:
-    case UNRAND_MISFORTUNE:
-    case UNRAND_FORCE_LANCE:
-    case UNRAND_VICTORY:
-        return 11;
-    default:
-        return -1;
-    }
-}
-
 static int _unrand_weight(int unrand_index, int item_level)
 {
-    // Early-game unrands (with a preferred max depth != -1) are
+    const unrandart_entry* entry = &unranddata[unrand_index];
+
+    // Early-game unrands (with a preferred max depth != 0) are
     // weighted higher within their depth and lower past it.
     // Normal unrands have a flat weight at all depths.
-    const int pref_max_level = _preferred_max_level(unrand_index);
-    if (pref_max_level == -1)
+    const int pref_max_level = entry->pref_max_level;
+    if (pref_max_level == 0)
         return 10;
     return item_level <= pref_max_level ? 100 : 1;
 }

crawl-ref/source/artefact.h
@@ -61,6 +61,8 @@ struct unrandart_entry
     const char *dbrand;      // description of extra brand
     const char *descrip;     // description of extra power
 
+    int pref_max_level;
+
     object_class_type base_type;
     uint8_t           sub_type;
     object_class_type fallback_base_type;

crawl-ref/source/util/art-data.pl
@@ -61,6 +61,7 @@ my %field_type = (
     INV      => "bool",
     FLY      => "bool",
     LIFE     => "num",
+    MAX_LEVEL => "num",
     MP       => "num",
     MUTATE   => "bool",
     NAME     => "str",
@@ -537,7 +538,7 @@ sub process_line
 my @art_order = (
     "NAME", "APPEAR", "TYPE", "\n",
     "INSCRIP", "DBRAND", "DESCRIP", "\n",
-    "base_type", "sub_type", "\n",
+    "MAX_LEVEL", "base_type", "sub_type", "\n",
     "fallback_base_type", "fallback_sub_type", "FB_BRAND", "\n",
     "plus", "plus2", "COLOUR", "VALUE", "\n",
     "flags",

commit 7f5b25da150b4fff5c674566834850eb42559884

Fix unrand rarity reading undefined values
ec41f42 accidentally broke all unrand weights by neglecting to notice that
UNRAND_START must be subtracted from all unrand enums to index their data.
This meant that all unrands were reading their max item levels from past
the end of the array, resulting in undefined behavior. In practice, this
meant that early unrands would not respect item level at all, and some
random handful of unrands would be much rarer than they were supposed to
be.

(I suspect early unrand generation, even as intended, doesn't function very
well for other reasons, but this is undefined-behavior on top of those
issues.)

crawl-ref/source/artefact.cc
@@ -1761,7 +1761,7 @@ const unrandart_entry* get_unrand_entry(int unrand_index)
 
 static int _unrand_weight(int unrand_index, int item_level)
 {
-    const unrandart_entry* entry = &unranddata[unrand_index];
+    const unrandart_entry* entry = &unranddata[unrand_index - UNRAND_START];
 
     // Early-game unrands (with a preferred max depth != 0) are
     // weighted higher within their depth and lower past it.