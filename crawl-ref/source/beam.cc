crawl/crawl

commit 22db19aefa22c8560fc22d7f6dfd6808393819d5

Remove impact damage from monster flame, noxious, and chaos cloud spells
This damage was very negligible and also sometimes weird: chaos cloud
impact damage followed different rules for its effects than the clouds
themselves did and noxious cloud impact could actually do poison damage
(which the clouds explicit never do). On top of this, it could print
somewhat confusing messages about 'missing' the player, regardless of
whether the player was now standing in a cloud or not.

These spells now function purely as cloud delivery and the direct beam
prints no additional messages about whether they 'hit' a target or not.

This comes with some additional tracer improvements where 0 damage cloud
beams will check the validity of their cloud against a monster target
(rather than assuming they will have no effect since they do no impact
damage)

crawl-ref/source/beam.cc
@@ -4387,11 +4387,12 @@ void bolt::affect_player()
     if (hit_verb.empty())
         hit_verb = engulfs ? "engulfs" : "hits";
 
-    if (flavour != BEAM_VISUAL && !is_enchantment())
+    if (flavour != BEAM_VISUAL && !is_enchantment()
+        && !(damage.num == 0 && is_big_cloud()))
     {
         mprf("The %s %s %s%s%s", name.c_str(), hit_verb.c_str(),
              you.hp > 0 ? "you" : "your lifeless body",
-             final_dam || damage.num == 0 ? "" : " but does no damage",
+             final_dam ? "" : " but does no damage",
              attack_strength_punctuation(final_dam).c_str());
     }
 
@@ -4825,13 +4826,9 @@ bool bolt::has_relevant_side_effect(monster* mon)
     else if (flavour == BEAM_ENSNARE || flavour == BEAM_LIGHT)
         return true;
 
-    if ((origin_spell == SPELL_NOXIOUS_CLOUD || origin_spell == SPELL_POISONOUS_CLOUD
-         || origin_spell == SPELL_NOXIOUS_BREATH)
-        && mon->res_poison() < 1)
-    {
-        return true;
-    }
-    else if (origin_spell == SPELL_RAVENOUS_SWARM && !(mon->holiness() & MH_UNDEAD))
+    const cloud_type cloud = flavour == BEAM_MEPHITIC ? CLOUD_MEPHITIC
+                                                      : get_cloud_type();
+    if (cloud != CLOUD_NONE && !actor_cloud_immune(*mon, cloud))
         return true;
 
     return false;
@@ -5785,13 +5782,15 @@ void bolt::affect_monster(monster* mon)
         // If the beam did no damage because of resistances,
         // mons_adjust_flavoured below will print "%s completely resists", so
         // no need to also say "does no damage" here.
-        mprf("The %s %s %s%s%s",
-             name.c_str(),
-             hit_verb.c_str(),
-             mon->name(DESC_THE).c_str(),
-             postac || damage.num == 0 ? "" : " but does no damage",
-             attack_strength_punctuation(final).c_str());
-
+        if (damage.num > 0 || !is_big_cloud())
+        {
+            mprf("The %s %s %s%s%s",
+                name.c_str(),
+                hit_verb.c_str(),
+                mon->name(DESC_THE).c_str(),
+                postac ? "" : " but does no damage",
+                attack_strength_punctuation(final).c_str());
+        }
     }
     else if (heard && !hit_noise_msg.empty())
         mprf(MSGCH_SOUND, "%s", hit_noise_msg.c_str());

crawl-ref/source/mon-cast.cc
@@ -2284,6 +2284,8 @@ bolt mons_spell_beam(const monster* mons, spell_type spell_cast, int power,
     case SPELL_DOOM_BOLT:
     case SPELL_WARP_BODY:
     case SPELL_ACID_BALL:
+    case SPELL_FLAMING_CLOUD:
+    case SPELL_CHAOS_BREATH:
         zappy(spell_to_zap(real_spell), power, true, beam);
         break;
 
@@ -2343,11 +2345,6 @@ bolt mons_spell_beam(const monster* mons, spell_type spell_cast, int power,
             power = power * 3/2;
         zappy(spell_to_zap(real_spell), power, true, beam);
         beam.aux_source  = "blast of searing breath";
-        break;
-
-    case SPELL_CHAOS_BREATH:
-        zappy(spell_to_zap(real_spell), power, true, beam);
-        beam.aux_source   = "blast of chaotic breath";
         break;
 
     case SPELL_COLD_BREATH:
@@ -2410,12 +2407,6 @@ bolt mons_spell_beam(const monster* mons, spell_type spell_cast, int power,
     //      damage is done via another means
     case SPELL_FREEZE:
         beam.flavour    = BEAM_COLD;
-        break;
-
-    case SPELL_FLAMING_CLOUD:
-        zappy(spell_to_zap(real_spell), power, true, beam);
-        beam.aux_source   = "blast of fiery breath";
-        beam.short_name   = "flames";
         break;
 
     case SPELL_RAVENOUS_SWARM:

crawl-ref/source/zap-data.h
@@ -50,14 +50,14 @@ static const zap_info zap_data[] =
 
 {
     ZAP_NOXIOUS_CLOUD,
-    "noxious blast",
+    "noxious fumes",
     nullptr,
     nullptr,
-    new dicedef_calculator<1, 0, 0, 1>,
-    new tohit_calculator<18, 1, 25>,
+    new dicedef_calculator<0, 1, 0, 1>,
+    new tohit_calculator<AUTOMATIC_HIT>,
     LIGHTGREEN,
     false,
-    BEAM_POISON,
+    BEAM_MMISSILE,
     DCHAR_FIRED_ZAP,
     true,
     false,
@@ -390,14 +390,14 @@ static const zap_info zap_data[] =
 
 {
     ZAP_CHAOS_BREATH,
-    "blast of chaos",
+    "seething chaos",
     nullptr,
     nullptr,
-    new dicedef_calculator<1, 0, 1, 8>,
-    new tohit_calculator<30>,
+    new dicedef_calculator<0, 1, 0, 1>,
+    new tohit_calculator<AUTOMATIC_HIT>,
     ETC_RANDOM,
     false,
-    BEAM_CHAOS,
+    BEAM_MISSILE,
     DCHAR_FIRED_ZAP,
     true,
     false,
@@ -465,14 +465,14 @@ static const zap_info zap_data[] =
 
 {
     ZAP_FLAMING_CLOUD,
-    "blast of flame",
+    "blazing flames",
     nullptr,
     nullptr,
-    new dicedef_calculator<1, 0, 1, 8>,
-    new tohit_calculator<30>,
+    new dicedef_calculator<0, 1, 0, 1>,
+    new tohit_calculator<AUTOMATIC_HIT>,
     RED,
     false,
-    BEAM_FIRE,
+    BEAM_MMISSILE,
     DCHAR_FIRED_ZAP,
     true,
     false,

commit 093d34702643516e0e3f2969e51977deb030fda3

Fix damageless projectiles (like pyre arrow) saying they did not damage
Of course they didn't do damage. They never do damage.

(Broken by 22db19a)

crawl-ref/source/beam.cc
@@ -4358,7 +4358,7 @@ void bolt::affect_player()
              you.hp > 0 ? "you" : "your lifeless body",
              real_flavour != BEAM_CHAOS ? ""
                     : make_stringf(" with %s", _beam_type_name(flavour).c_str()).c_str(),
-             final_dam ? "" : " but does no damage",
+             final_dam || damage.num == 0 ? "" : " but does no damage",
              attack_strength_punctuation(final_dam).c_str());
     }
 