74ec0dc3e08c27b365f45842ad89f3f0389be0a4
DracoOmega <draco.omega.dcss.dev@gmail.com>
2026-06-17 03:06:09 -0230

    Fix monster bot not handling unicode monster glyphs properly

--- crawl-ref/source/util/monster/monster-main.cc
+++ crawl-ref/source/util/monster/monster-main.cc
@@ -24,6 +24,7 @@
 #include "syscalls.h"
 #include "tag-version.h"
 #include "version.h"
+#include "viewchar.h"
 
 const coord_def MONSTER_PLACE(20, 20);
 
@@ -572,7 +573,7 @@ static string monster_symbol(const monster& mon)
     if (me)
     {
         monster_info mi(&mon, MILEV_NAME);
-        symbol += me->basechar;
+        symbol += stringize_glyph((me->basechar));
         symbol = colour(mi.colour(), symbol);
     }
     return symbol;
