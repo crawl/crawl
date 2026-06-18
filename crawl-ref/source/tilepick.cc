crawl/crawl

commit 45ea6cc7a576a0b1223e6fd027c509af01196111

Fix acidic and icy floors not using tile variations

crawl-ref/source/tilepick.cc
@@ -952,11 +952,7 @@ void apply_variations(const tile_flavour &flv, tileidx_t *bg,
             tile = TILE_DNGN_TRAP_WEB_N - 1 + solid;
     }
     else
-    {
-        dungeon_feature_type feat = env.map_knowledge(gc).feat();
-        needs_tile_picking = (feat != DNGN_FLOOR && feat != DNGN_ROCK_WALL)
-                             || is_torch_tile(tile);
-    }
+        needs_tile_picking = true;
 
     tileidx_t base = tile_dngn_basetile(tile);
     tileidx_t variety = tile - base;
@@ -1016,10 +1012,7 @@ static tileidx_t _tileidx_feature_no_overrides(const coord_def &gc)
         if (env.map_knowledge(gc).flags & MAP_ICY)
             return TILE_FLOOR_ICY;
 
-        return tile_env.flv(gc).floor;
-
-    case DNGN_ROCK_WALL:
-        return tile_env.flv(gc).wall;
+        return tileidx_feature_base(feat);
 
 #if TAG_MAJOR_VERSION == 34
     // New trap-type-specific features are handled in default case.

commit edf2d32dfa047c7c063b6cb287752df05bb66812

Fix walls with torches not animating in local tiles
This was broken in commit 45ea6cc when it added back some special casing
for floors and rock walls but didn't add back the special casing for
their animations.

crawl-ref/source/tilepick.cc
@@ -879,7 +879,10 @@ void apply_variations(const tile_flavour &flv, tileidx_t *bg,
     if (tile == TILE_FLOOR_NORMAL)
         tile = flv.floor;
     else if (tile == TILE_WALL_NORMAL)
+    {
         tile = flv.wall;
+        needs_tile_picking = is_torch_tile(tile);
+    }
     else if (is_door_tile(tile))
     {
         tileidx_t override = flv.feat;

crawl-ref/source/tileview.cc
@@ -1131,6 +1131,9 @@ static bool _tile_has_random_misc_animation(tileidx_t tile)
 // Unfortunately, these are all hard-coded for now.
 void tile_apply_animations(tileidx_t bg_idx, tile_flavour *flv)
 {
+    if (bg_idx == TILE_WALL_NORMAL)
+        bg_idx = flv->wall;
+
 #ifndef USE_TILE_WEB
     if (_tile_has_cycling_misc_animation(bg_idx))
         flv->special = (flv->special + 1) % tile_dngn_count(bg_idx);