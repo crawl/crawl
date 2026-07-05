1c7719eb9458bc4edbbd33e45cd496677b499202
DracoOmega <draco.omega.dcss.dev@gmail.com>
2026-07-05 00:03:12 -0230

    Silence an unused variable warning

--- crawl-ref/source/wiz-dgn.cc
+++ crawl-ref/source/wiz-dgn.cc
@@ -330,7 +330,6 @@ bool wizard_create_feature(dist &target, dungeon_feature_type feat, bool mimic)
             tile_env.flv(pos).feat = 0;
             tile_env.flv(pos).special = 0;
             env.grid_colours(pos) = 0;
-            const dungeon_feature_type old_feat = env.grid(pos);
             dungeon_terrain_changed(pos, feat, false, false, true);
             tile_init_flavour(pos);
             if (pos == you.pos() && cell_is_solid(pos))
