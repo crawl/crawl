crawl/crawl

commit 1c91e836e4129c3239f3230aa0b0b69c99ee43d4

Gently buff Concussion brand
While a number of players reported that the brand felt fun to play with,
they also felt the payoff for disrupting your own positioning was a bit
inadequate.

This increases the average ministun by 1 aut and the average wall slam
damage from 25% to 33%.

crawl-ref/source/describe.cc
@@ -5164,7 +5164,7 @@ static string _brand_damage_string(const monster_info &mi, brand_type brand,
         case SPWPN_FREEZING:
         case SPWPN_DRAINING:
         case SPWPN_CONCUSSION:
-            brand_dam = dam / 2;
+            brand_dam = dam * 3 / 4;
             break;
         case SPWPN_ELECTROCUTION:
             brand_dam = 20;

crawl-ref/source/melee-attack.cc
@@ -657,14 +657,14 @@ void melee_attack::handle_concussion_brand()
     {
         did_move = true;
         if (monster* mon = defender->as_monster())
-            mon->stagger(random2(min(10, attacker->attack_delay().roll())) * 3 / 4);
+            mon->stagger(1 + random2(min(10, attacker->attack_delay().roll())) * 3 / 4);
     }
     else if (damage_done > 0)
     {
         const coord_def back = defender->pos() + (defender->pos() - attacker->pos()).sgn();
         if (feat_is_solid(env.grid(back)))
         {
-            special_damage = random2(damage_done) / 2 + 1;
+            special_damage = random2(damage_done) * 3 / 4 + 1;
             if (needs_message)
             {
                 mprf("%s crush%s %s against the %s%s",

commit 88b023abf15bb920bec3197edea08f1963a476d2

Fix xv overestimating monster damage for some brands (ebonnov)
Accidentally broken by 1c91e83 (somehow)

crawl-ref/source/describe.cc
@@ -5187,6 +5187,8 @@ static string _brand_damage_string(const monster_info &mi, brand_type brand,
         case SPWPN_FLAMING:
         case SPWPN_FREEZING:
         case SPWPN_DRAINING:
+            brand_dam = dam / 2;
+            break;
         case SPWPN_CONCUSSION:
             brand_dam = dam * 3 / 4;
             break;