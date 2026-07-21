a221df3ca90abb52af14e80f3c27eed31e0ee846
Eivin Hatvik <e.o.h@outlook.com>
2026-07-21 22:33:12 +0000

    feat: show owned consumable counts in shops

--- crawl-ref/source/shopping.cc
+++ crawl-ref/source/shopping.cc
@@ -862,6 +862,26 @@ static int _count_identical(const vector<item_def>& stock, const item_def& item)
     return count;
 }
 
+int shop_owned_consumable_count(const item_def& item)
+{
+    if (item.base_type == OBJ_WANDS)
+    {
+        if (!item_type_known(item))
+            return 0;
+
+        for (const item_def& inv : you.inv)
+            if (inv.base_type == OBJ_WANDS && inv.sub_type == item.sub_type)
+                return inv.charges;
+        return 0;
+    }
+
+    for (const item_def& inv : you.inv)
+        if (items_stack(inv, item))
+            return inv.quantity;
+
+    return 0;
+}
+
 /** Buy an item from a shop!
  *
  *  @param shop  the shop to purchase from.
@@ -1021,7 +1041,10 @@ class ShopEntry : public InvEntry
         const string keystr = colour_to_str(keycol);
         const string itemstr =
             colour_to_str(menu_colour(text, item_prefix(*item, false), tag, false));
-        return make_stringf(" <%s>%c %c </%s><%s>%4d gold   %s%s</%s>",
+        const int owned = shop_owned_consumable_count(*item);
+        const string ownedstr =
+            owned > 0 ? make_stringf(" (owned: %d)", owned) : "";
+        return make_stringf(" <%s>%c %c </%s><%s>%4d gold   %s%s%s</%s>",
                             keystr.c_str(),
                             hotkeys[0],
                             selected() ? '+' : on_list ? '$' : '-',
@@ -1030,6 +1053,7 @@ class ShopEntry : public InvEntry
                             cost,
                             text.c_str(),
                             shop_item_unknown(*item) ? " (unknown)" : "",
+                            ownedstr.c_str(),
                             itemstr.c_str());
     }
 
