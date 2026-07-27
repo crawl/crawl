3e0666f4fb9a346e8caa653eb64fd14aff95c177
CrawlOdds <crawlodds@gmail.com>
2026-07-27 07:49:29 +0100

    Fix seed stability for unrand weapon generation
    Following f74bb9b, the Lochaber axe would be a candidate for different
    items depending on the player's skill, and gyre and gimble wouldn't be a
    candidate item for any short blade. We fix these checks not to use the
    full unrand.

--- crawl-ref/source/artefact.cc
+++ crawl-ref/source/artefact.cc
@@ -1791,25 +1791,40 @@ static bool _unrand_is_compatible(const unrandart_entry& unrand,
     required_item.base_type = aclass;
     required_item.sub_type = atype;
 
+    // We do item skill and hands required checks against a stub item with only
+    // the correct type and no unrand properties. This is because:
+    // - Item skills for the Lochaber axe depend on player skills, affecting
+    //   seed stability.
+    // - Handedness using the real item would stop Gyre and Gimble spawning.
     item_def unrand_item;
     unrand_item.base_type = unrand.base_type;
     unrand_item.sub_type = unrand.sub_type;
     unrand_item.quantity = 1;
-    unrand_item.flags |= ISFLAG_UNRANDART;
-    unrand_item.unrand_idx = unrand_index;
-
-    if (mons && !mons->could_wield(unrand_item))
-        return false;
 
     if (item_attack_skill(required_item) != item_attack_skill(unrand_item))
         return false;
+
     if (acquirement)
     {
-        return you.hands_reqd(required_item, true)
-               == you.hands_reqd(unrand_item, true);
+        if (you.hands_reqd(required_item, true)
+            != you.hands_reqd(unrand_item, true))
+        {
+            return false;
+        }
     }
-    return basic_hands_reqd(required_item, SIZE_MEDIUM)
-           == basic_hands_reqd(unrand_item, SIZE_MEDIUM);
+    else if (basic_hands_reqd(required_item, SIZE_MEDIUM)
+             != basic_hands_reqd(unrand_item, SIZE_MEDIUM))
+    {
+        return false;
+    }
+
+    // could_wield wants the real unrand to filter non-monster weapons
+    unrand_item.flags |= ISFLAG_UNRANDART;
+    unrand_item.unrand_idx = unrand_index;
+    if (mons && !mons->could_wield(unrand_item))
+        return false;
+
+    return true;
 }
 
 int find_okay_unrandart(uint8_t aclass, uint8_t atype, int item_level,
