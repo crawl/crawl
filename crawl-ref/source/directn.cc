e566e939d54aef8d3520edd1a0cba91555983d0a
DracoOmega <draco.omega.dcss.dev@gmail.com>
2026-08-02 19:32:21 -0230

    Unbrace

--- crawl-ref/source/directn.cc
+++ crawl-ref/source/directn.cc
@@ -1601,9 +1601,7 @@ void direction_chooser::update_previous_target() const
     // the player's primary target. Remember the initial target instead, unless
     // the player adjusted direction manually.
     if (is_ranged_attack && !player_changed_target && old_m && you.aware_of(*old_m))
-    {
         you.prev_targ = old_m->mid;
-    }
     // Otherwise, if directly targeting a monster, remember that monster
     else
     {
