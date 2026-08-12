crawl/crawl

commit 2985acfa1781d780586fb00ecea887bf4ee5d0b1

Improve display of item stacks on the ground (Ge0FF)
When multiple items are in a stack on the ground, only the top item in the
stack is displayed and an icon is added to indicate that more items are
beneath that one.

However, our implementation of this had several awkwardnesses. For one
thing, which item is on 'top' of a stack is largely coincidental (it's just
whichever fell last, which often depends on the order of items in a
monster's inventory). This means that important items are often hidden by
junk (and we don't even display the autopickup hilight on stacks that
contain autopickupable items, unless that item is on the top).

Moreover, the order of items in a stack isn't actually marshalled, so it
often *changes* whenever the player saves/reloads or peeks at another
floor. This can even be used tactically to change which item Apportation
will grab (since it always starts from the top of the stack).

This commit makes numerous changes aimed at making it easier to notice
interesting or important items in a stack (while Apportation itself will
be adjusted in a subsequent commit).

Items in stacks are now sorted based on some rough idea of their importance
to the player. This is approximately:
 autopickup-enabled -> artefacts -> branded and other special items ->
 everything else.

This increases the chance that player-relevant items will the ones which
are visible.

On top of this, the 'items are underneath here' marker now comes in 3
styles, indicating the presence of artefacts, special items, or only
mundane ones beneath the top item.

This means that the player can now often tell at a glance whether any
nearby piles could contain an artefact (rather than having to examine them
individually) and autopickup-able stacks should always show an autopickup
hilight.

This is implemented via the stash tracker itself (which now sorts items by
some criteria and can be asked to populate a map cell with the appropriate

crawl-ref/source/stash.cc
@@ -140,6 +140,36 @@ vector<item_def> Stash::get_items() const
     return items;
 }
 
+void Stash::populate_map_cell_with_item(map_cell& cell)
+{
+    if (items.empty())
+        return;
+
+    cell.set_item(items[0]);
+
+    // Staircases hide drawing items on their tile, so we need to use stack
+    // indicators that consider the top item to also be buried.
+    const bool top_hidden = feat_is_stair(cell.feat());
+    if (top_hidden)
+    {
+        if (has_artefact)
+            cell.flags |= MAP_MORE_ITEMS_ARTEFACT;
+        else if (has_special)
+            cell.flags |= MAP_MORE_ITEMS_GOOD;
+        else
+            cell.flags |= MAP_MORE_ITEMS;
+    }
+    else if (items.size() > 1)
+    {
+        if (artefact_in_stack)
+            cell.flags |= MAP_MORE_ITEMS_ARTEFACT;
+        else if (special_in_stack)
+            cell.flags |= MAP_MORE_ITEMS_GOOD;
+        else
+            cell.flags |= MAP_MORE_ITEMS;
+    }
+}
+
 vector<item_def> item_list_in_stash(const coord_def& pos)
 {
     vector<item_def> ret;
@@ -153,6 +183,17 @@ vector<item_def> item_list_in_stash(const coord_def& pos)
     }
 
     return ret;
+}
+
+void populate_map_cell_with_item(const coord_def& c, map_cell& cell)
+{
+    LevelStashes *ls = StashTrack.find_current_level();
+    if (ls)
+    {
+        Stash *s = ls->find_stash(c);
+        if (s)
+            s->populate_map_cell_with_item(cell);
+    }
 }
 
 static void _fully_identify_item(item_def *item)
@@ -251,6 +292,36 @@ static bool _grid_is_interesting(const coord_def& pos)
         || feat == DNGN_TRAP_SHAFT;
 }
 
+static bool _item_is_interesting(const item_def& item)
+{
+    return item.flags & ISFLAG_COSMETIC_MASK
+            || item.base_type == OBJ_TALISMANS
+            || item.base_type == OBJ_STAVES;
+}
+
+static int _item_sort_score(const item_def& item)
+{
+    int score = 10;
+    if (is_useless_item(item))
+        return 0;
+    if (item_needs_autopickup(item))
+        score += 1000;
+    // Prefer to show the runes/orb on top.
+    if (item.is_critical())
+        score += 2000;
+    if (item.base_type == OBJ_GEMS)
+        score += 200;
+    if (is_artefact(item))
+        score += 100;
+    if (_item_is_interesting(item))
+        score += 10;
+    // Gold is usually on autopickup, but less interesting than anything else that is.
+    if (item.base_type == OBJ_GOLD)
+        score -= 10;
+
+    return score;
+}
+
 void Stash::update()
 {
     feat = DNGN_FLOOR;
@@ -265,6 +336,10 @@ void Stash::update()
 
     // Zap existing items
     items.clear();
+    has_special = false;
+    has_artefact = false;
+    special_in_stack = false;
+    artefact_in_stack = false;
 
     if (!_grid_has_perceived_item(pos))
     {
@@ -279,9 +354,8 @@ void Stash::update()
     item_def *pitem = &env.item[you.visible_igrd(pos)];
     hints_first_item(*pitem);
 
-    bool glowing_item_on_square = false;
-    bool artefact_item_on_square = false;
     // Now, grab all items on that square and fill our vector
+    vector<item_def*> item_refs;
     for (stack_iterator si(pos, true); si; ++si)
     {
         ash_id_item(*si);
@@ -289,25 +363,59 @@ void Stash::update()
         if (!(si->flags & ISFLAG_UNOBTAINABLE))
         {
             lucky_upgrade_item(*si);
-            add_item(*si);
+            item_refs.push_back(&*si);
         }
+    }
 
-        if ((si->base_type == OBJ_STAVES || si->flags & ISFLAG_COSMETIC_MASK)
-            && !is_useless_item(*si))
+    // Find the item with the highest score to bring to the front of the stash.
+    int highest_score = INT_MIN;
+    item_def* best_item = nullptr;
+    for (item_def* item : item_refs)
+    {
+        const int score = _item_sort_score(*item);
+        if (score > highest_score)
         {
-            glowing_item_on_square = true;
+            highest_score = score;
+            best_item = item;
         }
+    }
 
-        if (si->flags & ISFLAG_ARTEFACT_MASK && !is_useless_item(*si))
-            artefact_item_on_square = true;
+    // Then add them all to the stash
+    add_item(*best_item);
+    for (item_def* item : item_refs)
+    {
+        if (item != best_item)
+            add_item(*item);
     }
 
     int current_size = items.size();
+    for (size_t i = 0; i < items.size(); ++i)
+    {
+        const item_def& item = items[i];
+
+        if (!special_in_stack
+            && _item_is_interesting(item)
+            && !is_useless_item(item))
+        {
+            has_special = true;
+            if (i > 0)
+                special_in_stack = true;
+        }
+
+        if (!artefact_in_stack
+            && item.flags & ISFLAG_ARTEFACT_MASK && !is_useless_item(item))
+        {
+            has_artefact = true;
+            if (i > 0)
+                artefact_in_stack = true;
+        }
+    }
+
     const bool stack_greed      =  current_size > 1
                                 && (Options.explore_greedy_visit & EG_STACK);
-    const bool glowing_greed    =  glowing_item_on_square
+    const bool glowing_greed    =  has_special
                                 && (Options.explore_greedy_visit & EG_GLOWING);
-    const bool artefact_greed   = artefact_item_on_square
+    const bool artefact_greed   = has_artefact
                                 && (Options.explore_greedy_visit & EG_ARTEFACT);
 
     visited = pos == you.pos()
@@ -525,7 +633,10 @@ void Stash::save(writer& outf) const
 
     marshallString(outf, feat_desc);
 
-    marshallByte(outf, visited? 1 : 0);
+    uint8_t flags = visited << 1
+                    + special_in_stack << 2 + artefact_in_stack << 3
+                    + has_special << 4 + has_artefact << 5;
+    marshallByte(outf, flags);
 
     // And dump the items individually. We don't bother saving fields we're
     // not interested in (and don't anticipate being interested in).
@@ -551,6 +662,10 @@ void Stash::load(reader& inf)
 
     uint8_t flags = unmarshallUByte(inf);
     visited = (flags & 1) != 0;
+    special_in_stack = (flags & 2) != 0;
+    artefact_in_stack = (flags & 3) != 0;
+    has_special = (flags & 4) != 0;
+    has_artefact = (flags & 5) != 0;
 
     // Zap out item vector, in case it's in use (however unlikely)
     items.clear();

crawl-ref/source/abyss.cc
@@ -261,7 +261,7 @@ static bool _sync_rune_knowledge(coord_def p)
             // found! make sure map memory is up-to-date
             if (!rune_memory)
             {
-                env.map_knowledge(p).set_item(*si, item != nullptr);
+                env.map_knowledge(p).set_item(*si);
                 if (!you.see_cell(p))
                     env.map_knowledge(p).flags |= MAP_DETECTED_ITEM;
                 redraw_view_at(p);

crawl-ref/source/map-cell.h
@@ -45,6 +45,10 @@
 #define MAP_DOOR_CONNECT_2 0x40000000
 #define MAP_DOOR_CONNECT_3 0x80000000
 
+#define MAP_MORE_ITEMS_GOOD     0x100000000
+#define MAP_MORE_ITEMS_ARTEFACT 0x200000000
+#define MAP_CELL_MORE_ITEMS_TYPE (MAP_MORE_ITEMS | MAP_MORE_ITEMS_GOOD | MAP_MORE_ITEMS_ARTEFACT)
+
 struct cloud_info
 {
     cloud_info() : type(CLOUD_NONE), colour(0), variety(3), tile(0), pos(0, 0),
@@ -200,12 +204,10 @@ struct map_cell
         return ret;
     }
 
-    void set_item(const item_def& ii, bool more_items)
+    void set_item(const item_def& ii)
     {
         clear_item();
         _item = make_unique<item_def>(ii);
-        if (more_items)
-            flags |= MAP_MORE_ITEMS;
     }
 
     void set_detected_item();
@@ -214,7 +216,8 @@ struct map_cell
     {
         // TODO: internal callers are doing a bit of duplicate work here
         _item.reset();
-        flags &= ~(MAP_DETECTED_ITEM | MAP_MORE_ITEMS);
+        flags &= ~(MAP_DETECTED_ITEM | MAP_MORE_ITEMS
+                   | MAP_MORE_ITEMS_GOOD | MAP_MORE_ITEMS_ARTEFACT);
     }
 
     monster_type mon_type() const

crawl-ref/source/show.cc
@@ -247,35 +247,18 @@ void update_item_at(const coord_def &gp, bool wizard)
         return;
 
     item_def eitem;
-    bool more_items = false;
 
     if (you.see_cell(gp) || wizard)
     {
         const int item_grid = wizard ? env.igrid(gp) : you.visible_igrd(gp);
         if (item_grid == NON_ITEM)
             return;
-        eitem = env.item[item_grid];
-
-        // monster(mimic)-owned items have link = NON_ITEM+1+midx
-        if (eitem.link > NON_ITEM)
-            more_items = true;
-        else if (eitem.link < NON_ITEM && !crawl_state.game_is_arena())
-            more_items = true;
 
         if (wizard)
             StashTrack.add_stash(gp);
     }
-    else
-    {
-        const vector<item_def> stash = item_list_in_stash(gp);
-        if (stash.empty())
-            return;
 
-        eitem = stash[0];
-        if (stash.size() > 1)
-            more_items = true;
-    }
-    env.map_knowledge(gp).set_item(eitem, more_items);
+    populate_map_cell_with_item(gp, env.map_knowledge(gp));
 }
 
 static int _get_cloud_variety(cloud_struct& cloud)

commit ca5ed2dcc3ede4241f9edb2329fdf718a66924a1

Fix marshalling and unmarshalling of stack flags
This fixes a bug introduced in 2985acf whereby the stash always loaded
zeroed flags on reload, which made autoexplore think stashes were
unvisited and revisit them.

Fixes #5369

crawl-ref/source/stash.cc
@@ -631,9 +631,11 @@ void Stash::save(writer& outf) const
 
     marshallString(outf, feat_desc);
 
-    uint8_t flags = visited << 1
-                    + special_in_stack << 2 + artefact_in_stack << 3
-                    + has_special << 4 + has_artefact << 5;
+    uint8_t flags = (visited             << 0)
+                    + (special_in_stack  << 1)
+                    + (artefact_in_stack << 2)
+                    + (has_special       << 3)
+                    + (has_artefact      << 4);
     marshallByte(outf, flags);
 
     // And dump the items individually. We don't bother saving fields we're
@@ -659,11 +661,11 @@ void Stash::load(reader& inf)
     feat_desc = unmarshallString(inf);
 
     uint8_t flags = unmarshallUByte(inf);
-    visited = (flags & 1) != 0;
-    special_in_stack = (flags & 2) != 0;
-    artefact_in_stack = (flags & 3) != 0;
-    has_special = (flags & 4) != 0;
-    has_artefact = (flags & 5) != 0;
+    visited           = (flags & (1 << 0)) != 0;
+    special_in_stack  = (flags & (1 << 1)) != 0;
+    artefact_in_stack = (flags & (1 << 2)) != 0;
+    has_special       = (flags & (1 << 3)) != 0;
+    has_artefact      = (flags & (1 << 4)) != 0;
 
     // Zap out item vector, in case it's in use (however unlikely)
     items.clear();