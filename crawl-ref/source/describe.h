69a93a9d6a80f24df862901212208affa17e2d77
David Lawrence Ramsey <pooka109@gmail.com>
2026-08-21 18:52:20 -0500

    Allow lookup-help searches of missile egos.

--- crawl-ref/source/describe.h
+++ crawl-ref/source/describe.h
@@ -68,6 +68,8 @@ command_type describe_item_popup(const item_def &item,
 bool describe_item(item_def &item, function<void (string&)> fixup_desc = nullptr,
     bool do_actions = true);
 brand_type weapon_ego_from_name(string name, vector<brand_type> *partial_matches = nullptr);
+special_missile_type missile_ego_from_name(string name,
+                                           vector<special_missile_type> *partial_matches = nullptr);
 special_armour_type armour_ego_from_name(string name,
                                          vector<special_armour_type> *partial_matches = nullptr);
 string describe_item_rarity(const item_def &item);
@@ -97,6 +99,7 @@ void describe_bane(bane_type bane);
 string bane_long_description(bane_type bane, bool ignore_player = false);
 void describe_weapon_ego(brand_type wpn);
 void describe_armour_ego(special_armour_type arm);
+void describe_missile_ego(special_missile_type msl);
 
 string short_ghost_description(const monster *mon, bool abbrev = false);
 string get_ghost_description(const monster_info &mi, bool concise = false);
