06d0210a78eb5aeeb90adde0996ab173807de392
desiderautumn <277820042+desiderautumn@users.noreply.github.com>
2026-08-28 19:58:52 -0400

    Fix unreachable Xom flavor text
    The function _xom_make_item has unique flavor text for when Xom
    gifts the player an orb.
    Previously, this flavor text was only emitted when the arguments
    to _xom_make_item specifically indicated an orb.
    However, _xom_make_item is only ever called from one place,
    with arguments of OBJ_RANDOM, so this flavor text was unreachable.
    Fix this by inspecting the type of the object that is ultimately
    generated, rather than the function arguments.
    
    [Committer's notes: Don't set up the item_def used to inspect the
    generated object until after the item is actually generated. The item
    could be destroyed if you're over lava when Xom tries to gift it, in
    which case inspecting it is useless.]

--- crawl-ref/source/xom.cc
+++ crawl-ref/source/xom.cc
@@ -640,14 +640,17 @@ static void _xom_make_item(object_class_type base, int subtype, int power)
         god_speaks(GOD_XOM, "\"No, never mind.\"");
         return;
     }
-    else if (base == OBJ_ARMOUR && subtype == ARM_ORB && one_chance_in(4))
+
+    item_def &item(env.item[thing_created]);
+
+    if (item.base_type == OBJ_ARMOUR && item.sub_type == ARM_ORB && one_chance_in(4))
         god_speaks(GOD_XOM, _get_xom_speech("orb gift").c_str());
 
     _try_brand_switch(thing_created);
 
     static char gift_buf[100];
     snprintf(gift_buf, sizeof(gift_buf), "god gift: %s",
-             env.item[thing_created].name(DESC_PLAIN).c_str());
+             item.name(DESC_PLAIN).c_str());
     take_note(Note(NOTE_XOM_EFFECT, you.raw_piety, -1, gift_buf), true);
 
     canned_msg(MSG_SOMETHING_APPEARS);
