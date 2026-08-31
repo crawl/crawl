crawl/crawl

commit 62137d70225229b64a18d355573eb3cb027b4769

Heavily refactor mons_is_conjured/mons_is_object, simplify
mons_is_conjured was a category being used in multiple (only partially
overlapping) ways. In some contexts, it was used as an equivalent for
is_summoned() for non-abjurable things which is_summoned() did not return
true (but now does, since the recent rework). In other contexts, it was
used to exclude monsters too minor to be randomly targeted for certain
effects (eg: Obsidian's Axe's mesmerisation effect or Ignis's Fire
Champion). In others, it was used overly broadly to avoid prompting the
player about harming invulnerable allies (ie: orbs of destruction), with
sometimes odd effects like Shatter being unable to damage spatial vorticies
or boulders.

In still more, it was used (as part of mons_is_object) to control what
allies were angered by attacks and which ones gods would penance you for
attacking - which weren't even symetrical categories! A lightning spire
would be angered by you hitting it and vanish, but Ely would not min. But
a spellforged servitor would *not* be angered by you hitting it, and yet
Ely would penance you anyway.

mons_is_object also relied on a bespoke list of things we considered to be
'a mindless object' that often overlooked things which seemingly belonged
there (while also producing weird effects like Orb of Mayhem never
triggering off killing dancing weapons or lighting spires, even though
other seemingly similar things counted.)

This commit replaces both of those functions completely. M_PERIPHERAL
replaces M_CONJURED (since the ancillary nature of these monsters is the
relevant mechanical thing, and not the fact that they were made by magic),
actor::is_peripheral() replaces most uses of mons_is_conjured() and also
folds tentacles and firewood into that same category. The very large number
of places that checked firewood/conjured/tentacles together now need only
check is_peripheral(), which should largely encompass 'is a proper
monster'.

crawl-ref/source/god-abil.cc
@@ -1283,7 +1283,7 @@ spret zin_imprison(const coord_def& target, bool fail)
         return spret::abort;
     }
 
-    if (mons->is_firewood() || mons_is_conjured(mons->type))
+    if (mons->is_peripheral())
     {
         mpr("You cannot imprison that!");
         return spret::abort;
@@ -6657,9 +6657,7 @@ spret okawaru_duel(const coord_def& target, bool fail)
         return spret::abort;
     }
 
-    if (mons->is_firewood()
-        || mons_is_conjured(mons->type)
-        || mons_is_tentacle_or_tentacle_segment(mons->type)
+    if (mons->is_peripheral()
         || mons_primary_habitat(*mons) == HT_LAVA
         || mons_primary_habitat(*mons) == HT_WATER
         || mons->wont_attack())

crawl-ref/source/mon-death.cc
@@ -668,9 +668,7 @@ static bool _ely_heal_monster(monster* mons, killer_type killer, int i)
         return false;
 
     if (mons->wont_attack()
-        || mons->is_firewood()
-        || mons_is_object(mons->type)
-        || mons_is_tentacle_or_tentacle_segment(mons->type)
+        || mons->is_peripheral()
         || mons->props.exists(ELY_WRATH_HEALED_KEY)
         || mons->get_experience_level() < random2(you.experience_level)
         || !one_chance_in(3))
@@ -2047,17 +2045,11 @@ static void _player_on_kill_effects(monster& mons, killer_type killer,
     if (YOU_KILL(killer) && gives_player_xp)
     {
         // TSO follower blessing.
-        if (!mons_is_object(mons.type)
-            && _god_will_bless_follower(&mons))
-        {
+        if (_god_will_bless_follower(&mons))
             bless_follower();
-        }
 
-        if (!mons_is_object(mons.type)
-            && you.wearing_ego(EQ_ALL_ARMOUR, SPARM_MAYHEM))
-        {
+        if (you.wearing_ego(EQ_ALL_ARMOUR, SPARM_MAYHEM))
             _orb_of_mayhem(you, mons);
-        }
     }
 
     // Various sources of berserk extension on kills.

commit 25e41938d57b93a4f07154319f15afaa3d80e107

Remove vestigial TSO follower blessings, pending rework
TSO follower blessings were broken completely almost two years ago (as a
result of a small error in 62137d7 which caused all summoned allies to be
ineligible for them). I am surprised nobody noticed in this time, but the
duration extension one is fairly consequential.

However, several parts of this blessing system were super marginal and/or
broken for even longer (for example, I think the duration extension for
charmed enemies hasn't functioned since 0e8e6b5) and I've wanted to rewrite
this in past. So I'm using this as a chance for a clean break, and will
be replacing it with entirely new code.

crawl-ref/source/MSVC/crawl.vcxproj
@@ -370,7 +370,6 @@ python.exe util/gen-all.py</Command>
     <ClCompile Include="..\glwrapper-ogl.cc" />
     <ClCompile Include="..\glwrapper.cc" />
     <ClCompile Include="..\god-abil.cc" />
-    <ClCompile Include="..\god-blessing.cc" />
     <ClCompile Include="..\god-companions.cc" />
     <ClCompile Include="..\god-conduct.cc" />
     <ClCompile Include="..\god-item.cc" />
@@ -762,7 +761,6 @@ python.exe util/gen-all.py</Command>
     <ClInclude Include="..\glwrapper-ogl.h" />
     <ClInclude Include="..\glwrapper.h" />
     <ClInclude Include="..\god-abil.h" />
-    <ClInclude Include="..\god-blessing.h" />
     <ClInclude Include="..\god-companions.h" />
     <ClInclude Include="..\god-conduct.h" />
     <ClInclude Include="..\god-item.h" />

crawl-ref/source/MSVC/crawl.vcxproj.filters
@@ -798,9 +798,6 @@
     <ClCompile Include="..\god-companions.cc">
       <Filter>cc</Filter>
     </ClCompile>
-    <ClCompile Include="..\god-blessing.cc">
-      <Filter>cc</Filter>
-    </ClCompile>
     <ClCompile Include="..\god-abil.cc">
       <Filter>cc</Filter>
     </ClCompile>
@@ -1345,9 +1342,6 @@
       <Filter>h</Filter>
     </ClInclude>
     <ClInclude Include="..\god-abil.h">
-      <Filter>h</Filter>
-    </ClInclude>
-    <ClInclude Include="..\god-blessing.h">
       <Filter>h</Filter>
     </ClInclude>
     <ClInclude Include="..\god-companions.h">

crawl-ref/source/Makefile.obj
@@ -74,7 +74,6 @@ game-options.o \
 geom2d.o \
 ghost.o \
 god-abil.o \
-god-blessing.o \
 god-companions.o \
 god-conduct.o \
 god-item.o \
@@ -413,7 +412,6 @@ game-type.h.o \
 gender-type.h.o \
 geom2d.h.o \
 god-abil.h.o \
-god-blessing.h.o \
 god-companions.h.o \
 god-item.h.o \
 god-passive.h.o \

crawl-ref/source/android-project/jni/src/Android.mk
@@ -93,7 +93,6 @@ LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
     $(CRAWL_PATH)/geom2d.cc \
     $(CRAWL_PATH)/ghost.cc \
     $(CRAWL_PATH)/god-abil.cc \
-    $(CRAWL_PATH)/god-blessing.cc \
     $(CRAWL_PATH)/god-companions.cc \
     $(CRAWL_PATH)/god-conduct.cc \
     $(CRAWL_PATH)/god-item.cc \

crawl-ref/source/cmd-keys.h
@@ -171,7 +171,6 @@
 {'x', CMD_TARGET_CANCEL},
 #ifdef WIZARD
 {'F', CMD_TARGET_WIZARD_MAKE_FRIENDLY},
-{'P', CMD_TARGET_WIZARD_BLESS_MONSTER},
 {'s', CMD_TARGET_WIZARD_MAKE_SHOUT},
 {'o', CMD_TARGET_WIZARD_GIVE_ITEM},
 {'m', CMD_TARGET_WIZARD_MOVE},

crawl-ref/source/command-type.h
@@ -287,7 +287,6 @@ enum command_type
     CMD_TARGET_CYCLE_QUIVER_FORWARD,
     CMD_TARGET_SELECT_ACTION,
     CMD_TARGET_WIZARD_MAKE_FRIENDLY,
-    CMD_TARGET_WIZARD_BLESS_MONSTER,
     CMD_TARGET_WIZARD_MAKE_SHOUT,
     CMD_TARGET_WIZARD_GIVE_ITEM,
     CMD_TARGET_WIZARD_MOVE,

crawl-ref/source/directn.cc
@@ -2147,10 +2147,6 @@ void direction_chooser::handle_wizard_command(command_type key_command,
     case CMD_TARGET_WIZARD_GIVE_ITEM:  wizard_give_monster_item(m); break;
     case CMD_TARGET_WIZARD_POLYMORPH:  wizard_polymorph_monster(m); break;
 
-    case CMD_TARGET_WIZARD_BLESS_MONSTER:
-        wizard_apply_monster_blessing(m);
-        break;
-
     case CMD_TARGET_WIZARD_MAKE_SUMMONED:
         wizard_make_monster_summoned(m);
         break;

crawl-ref/source/god-abil.cc
@@ -38,7 +38,6 @@
 #include "files.h"
 #include "fineff.h"
 #include "format.h" // formatted_string
-#include "god-blessing.h"
 #include "god-companions.h"
 #include "god-item.h"
 #include "god-passive.h"

crawl-ref/source/god-blessing.cc
@@ -1,262 +1,0 @@
-/**
- * @file
- * @brief Functions for gods blessing followers.
- **/
-
-#include "AppHdr.h"
-
-#include "god-blessing.h"
-
-#include "env.h"
-#include "message.h"
-#include "mon-place.h"
-#include "religion.h"
-#include "stringutil.h"
-#include "view.h"
-
-static bool _blessing_balms(monster* mon)
-{
-    // Remove poisoning, sickness and confusion, like a potion of
-    // curing, but without the healing. Also, remove slowing and
-    // fatigue.
-    bool success = false;
-
-    if (mon->del_ench(ENCH_POISON, true))
-        success = true;
-
-    if (mon->del_ench(ENCH_SICK, true))
-        success = true;
-
-    if (mon->del_ench(ENCH_CONFUSION, true))
-        success = true;
-
-    if (mon->del_ench(ENCH_SLOW, true))
-        success = true;
-
-    if (mon->del_ench(ENCH_FATIGUE, true))
-        success = true;
-
-    if (mon->del_ench(ENCH_WRETCHED, true))
-        success = true;
-
-    return success;
-}
-
-static bool _blessing_healing(monster* mon)
-{
-    const int healing = mon->max_hit_points / 4 + 1;
-
-    // Heal a monster.
-    if (mon->heal(healing + random2(healing + 1)))
-        return true;
-
-    return false;
-}
-
-static bool _increase_ench_duration(monster* mon,
-                                    mon_enchant ench,
-                                    const int increase)
-{
-    // Durations are saved as 16-bit signed ints, so clamp at the largest such.
-    const int MARSHALL_MAX = (1 << 15) - 1;
-
-    const int newdur = min(ench.duration + increase, MARSHALL_MAX);
-    if (ench.duration >= newdur)
-        return false;
-
-    ench.duration = newdur;
-    mon->update_ench(ench);
-
-    return true;
-}
-
-static bool _tso_blessing_extend_stay(monster* mon)
-{
-    if (!mon->has_ench(ENCH_SUMMON_TIMER))
-        return false;
-
-    mon_enchant timer = mon->get_ench(ENCH_SUMMON_TIMER);
-
-    // These numbers are tenths of a player turn. Holy monsters get a
-    // much bigger boost than random beasties, which get at most double
-    // their current summon duration.
-    if (mon->is_holy())
-        return _increase_ench_duration(mon, timer, 1100 + random2(1100));
-    else
-        return _increase_ench_duration(mon, timer, min(timer.duration,
-                                                       500 + random2(500)));
-}
-
-static bool _tso_blessing_friendliness(monster* mon)
-{
-    if (!mon->has_ench(ENCH_CHARM))
-        return false;
-
-    // [ds] Just increase charm duration, no permanent friendliness.
-    const int base_increase = 700;
-    return _increase_ench_duration(mon, mon->get_ench(ENCH_CHARM),
-                                   base_increase + random2(base_increase));
-}
-
-/**
- * Attempt to bless a follower with curing and/or healing.
- *
- * @param[in] follower      The follower to heal.
- * @return                  The type of healing that occurred; may be empty.
- */
-static string _bless_with_healing(monster* follower)
-{
-    string blessing = "";
-
-    // Maybe try to cure status conditions.
-    bool balms = false;
-    if (coinflip())
-    {
-        balms = _blessing_balms(follower);
-        if (balms)
-            blessing = "divine balms";
-        else
-            dprf("Couldn't apply balms.");
-    }
-
-    // Heal the follower.
-    bool healing = _blessing_healing(follower);
-
-    // Maybe heal the follower again.
-    if ((!healing || coinflip()) && _blessing_healing(follower))
-        healing = true;
-
-    if (healing)
-    {
-        if (balms)
-            blessing += " and ";
-        blessing += "healing";
-    }
-    else
-        dprf("Couldn't heal monster.");
-
-    return blessing;
-}
-
-
-/**
- * Print a message for a god blessing a follower.
- *
- * Also flash the screen, in local tiles.
- *
- * @param[in] follower  The follower being blessed.
- * @param god           The god doing the blessing.
- * @param blessing      The blessing being delivered.
- */
-static void _display_god_blessing(monster* follower, god_type god,
-                                  string blessing)
-{
-    ASSERT(follower); // XXX: change to monster &follower
-
-    string whom = you.can_see(*follower) ? follower->name(DESC_THE)
-    : "a follower";
-
-    simple_god_message(make_stringf(" blesses %s with %s.",
-                                    whom.c_str(), blessing.c_str()).c_str(),
-                       god);
-}
-
-/**
- * Attempt to increase the duration of a follower.
- *
- * Their summon duration, if summoned, or charm duration, if charmed.
- *
- * @param[in] follower    The follower to bless.
- * @return                The type of blessing that was given; may be empty.
- */
-static string _tso_bless_duration(monster* follower)
-{
-    // Extend a monster's stay if it's abjurable, or extend charm
-    // duration. If neither is possible, deliberately fall through.
-    const bool more_time = _tso_blessing_extend_stay(follower);
-    const bool friendliness = _tso_blessing_friendliness(follower);
-
-    if (!more_time && !friendliness)
-    {
-        dprf("Couldn't increase monster's friendliness or summon time.");
-        return "";
-    }
-
-    string blessing = "";
-    if (friendliness)
-    {
-        blessing += "friendliness";
-        if (more_time)
-            blessing += " and ";
-    }
-
-    if (more_time)
-        blessing += "more time in this world";
-
-    return blessing;
-}
-
-/**
- * Have The Shining One attempt to bless the specified follower.
- *
- * Blessings can increase duration, or heal & cure.
- *
- * @param[in] follower      The follower to try to bless.
- * @param[in] force         Whether to check follower validity.
- * @return Whether a blessing occurred.
- */
-static bool _tso_bless_follower(monster* follower, bool force)
-{
-
-    if (!follower || (!force && !is_follower(*follower)))
-        return false;
-
-    string blessing = "";
-    if (blessing.empty() && coinflip())
-        blessing = _bless_with_healing(follower);
-    if (blessing.empty())
-        blessing = _tso_bless_duration(follower);
-
-    if (blessing.empty())
-        return false;
-
-    _display_god_blessing(follower, GOD_SHINING_ONE, blessing);
-    return true;
-}
-
-static bool _is_friendly_follower(const monster& mon)
-{
-    return mon.friendly() && is_follower(mon);
-}
-
-// Bless the follower indicated in follower, if any. If there isn't
-// one, bless a random follower within sight of the player, if any, or
-// any follower on the level.
-// Blessing can be enforced with a wizard mode command.
-bool bless_follower(monster* follower,
-                    god_type god,
-                    bool force)
-{
-    // most blessings fail, tragically...
-    if (!force && !one_chance_in(4))
-        return false;
-
-    // If a follower was specified, and it's suitable, pick it.
-    // Otherwise, pick a random follower.
-    // XXX: factor out into another function?
-    if (!follower || (!force && !is_follower(*follower)))
-    {
-        // Choose a random follower in LOS
-        follower = choose_random_nearby_monster(_is_friendly_follower);
-    }
-
-    // Try *again*, on the entire level
-    if (!follower)
-        follower = choose_random_monster_on_level(_is_friendly_follower);
-
-    switch (god)
-    {
-        case GOD_SHINING_ONE:   return _tso_bless_follower(follower, force);
-        default: return false; // XXX: print something here?
-    }
-}

crawl-ref/source/god-blessing.h
@@ -1,12 +1,0 @@
-/**
- * @file
- * @brief Functions for gods blessing followers.
-**/
-
-#pragma once
-
-#include "player.h"
-
-bool bless_follower(monster* follower = nullptr,
-                    god_type god = you.religion,
-                    bool force = false);

crawl-ref/source/god-passive.cc
@@ -97,7 +97,6 @@ static const vector<god_passive> god_passives[] =
     {
         { -1, passive_t::protect_from_harm },
         { -1, passive_t::abjuration_protection_hd },
-        { -1, passive_t::bless_followers_vs_evil },
         { -1, passive_t::no_stabbing },
         {  0, passive_t::halo },
         {  1, passive_t::restore_hp_mp_vs_evil },

crawl-ref/source/god-passive.h
@@ -56,9 +56,6 @@ enum class passive_t
     /// You can walk on water.
     water_walk,
 
-    /// Your god blesses your followers when they kill evil things.
-    bless_followers_vs_evil,
-
     /// You cannot be hasted.
     no_haste,
 

crawl-ref/source/mon-death.cc
@@ -33,10 +33,9 @@
 #include "evoke.h"
 #include "fineff.h"
 #include "god-abil.h"
-#include "god-blessing.h"
 #include "god-companions.h"
 #include "god-conduct.h"
-#include "god-passive.h" // passive_t::bless_followers, convert_orcs
+#include "god-passive.h" // convert_orcs
 #include "hints.h"
 #include "hiscores.h"
 #include "item-name.h"
@@ -2074,13 +2073,6 @@ static bool _apply_necromancy(monster &mons, bool quiet, bool corpse_gone,
     return false;
 }
 
-static bool _god_will_bless_follower(monster* victim)
-{
-    return have_passive(passive_t::bless_followers_vs_evil)
-           && victim->evil()
-           && random2(you.piety()) >= piety_breakpoint(0);
-}
-
 static bool should_blame_you_for_kill(int killer_index, bool pet_kill) noexcept
 {
     if (killer_index == YOU_FAULTLESS)
@@ -2376,10 +2368,6 @@ static void _player_on_kill_effects(monster& mons, killer_type killer,
 
     if (YOU_KILL(killer) && gives_player_xp)
     {
-        // TSO follower blessing.
-        if (_god_will_bless_follower(&mons))
-            bless_follower();
-
         if (you.wearing_ego(OBJ_ARMOUR, SPARM_MAYHEM))
             _orb_of_mayhem(you, mons);
     }
@@ -3166,9 +3154,6 @@ item_def* monster_die(monster& mons, killer_type killer,
 
             if (killer_mon->wearing_ego(OBJ_ARMOUR, SPARM_MAYHEM))
                 _orb_of_mayhem(*killer_mon, mons);
-
-            if (pet_kill && _god_will_bless_follower(&mons))
-                bless_follower(killer_mon);
 
             break;
         }

crawl-ref/source/wiz-mon.cc
@@ -22,7 +22,6 @@
 #include "english.h"
 #include "files.h"
 #include "ghost.h"
-#include "god-blessing.h"
 #include "invent.h"
 #include "item-prop.h"
 #include "items.h"
@@ -550,24 +549,6 @@ void debug_make_monster_shout(monster* mon)
     }
 
     mpr("== Done ==");
-}
-
-void wizard_apply_monster_blessing(monster* mon)
-{
-    mprf(MSGCH_PROMPT, "Apply blessing of the (S)hining One? ");
-
-    char type = (char) getchm(KMC_DEFAULT);
-    type = toalower(type);
-
-    if (type != 's')
-    {
-        canned_msg(MSG_OK);
-        return;
-    }
-    god_type god = GOD_SHINING_ONE;
-
-    if (!bless_follower(mon, god, true))
-        mprf("%s won't bless this monster for you!", god_name(god).c_str());
 }
 
 void wizard_give_monster_item(monster* mon)

crawl-ref/source/wiz-mon.h
@@ -18,7 +18,6 @@ void debug_ghosts();
 class monster;
 struct coord_def;
 
-void wizard_apply_monster_blessing(monster* mon);
 void wizard_give_monster_item(monster* mon);
 void wizard_move_player_or_monster(const coord_def& where);
 void wizard_make_monster_summoned(monster* mon);