9656d26235da5ca5d236792fbe17d893a217c196
hellmonk <nldehaan91@gmail.com>
2026-09-09 13:09:13 -0500

    Display "master archer" in xv
    Let players know in advance when a monster is allowed to shoot in melee range.

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -6809,6 +6809,11 @@ static string _monster_stat_description(const monster_info& mi, bool mark_spells
     if (mons_class_flag(mi.type, M_BURROWS))
         result << uppercase_first(pronoun) << " can burrow through diggable terrain.\n";
 
+    // check item use to exclude ranged dancing weapons, who will fire in melee
+    // range, but don't really "use" a weapon.
+    if (mons_class_flag(mi.type, M_PREFER_RANGED) && mi.itemuse() > MONUSE_OPEN_DOORS)
+        result << uppercase_first(pronoun) << " can use ranged and thrown weapons in melee range.\n";
+
     if (mons_class_flag(mi.type, M_ACID_SPLASH))
     {
         result << uppercase_first(pronoun) << " "
