3ae2397b8f1b6776a6108255d56b10c7aae39b79
hellmonk <nldehaan91@gmail.com>
2026-09-13 23:34:55 -0500

    New unrand, Arcane Splint Mail
    The optimal armour for the power combo of 0.8, 0.10, and 0.11. It's archery
    chain that applies corona to anything hit by the wearer's projectiles and
    is otherwise "pretty good".

--- crawl-ref/source/ranged-attack.cc
+++ crawl-ref/source/ranged-attack.cc
@@ -21,6 +21,7 @@
 #include "mon-util.h"
 #include "monster.h"
 #include "player.h"
+#include "spl-monench.h" //corona_monster
 #include "stringutil.h"
 #include "teleport.h"
 #include "throw.h"
@@ -348,6 +349,15 @@ bool ranged_attack::handle_phase_hit()
         {
             return false;
         }
+    }
+
+    if (attacker->alive() && defender->alive()
+        && attacker->unrand_equipped(UNRAND_ARCANE_SPLINT))
+    {
+        if (defender->is_player())
+            you.backlight();
+        else
+            corona_monster(defender->as_monster(), attacker);
     }
 
     // XXX: unify this with melee_attack's code
